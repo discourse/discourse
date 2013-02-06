9f1c978f114de452c1e6d56d7905221a984277ae
Charles Oppenheimer <choppen5@yahoo.com>
2013-02-06 11:47:39 -0800

    initial rake db:migrate was failing if hstore was already set

--- db/migrate/20120921162512_add_meta_data_to_forum_threads.rb
+++ db/migrate/20120921162512_add_meta_data_to_forum_threads.rb
@@ -1,6 +1,6 @@
 class AddMetaDataToForumThreads < ActiveRecord::Migration
   def change
-    execute "CREATE EXTENSION hstore" 
+    execute "CREATE EXTENSION IF NOT EXITS hstore" 
     add_column :forum_threads, :meta_data, :hstore
   end
 end
