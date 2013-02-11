00883842f49d39800bf124ed97f7789ed3cbf27e
xdite <xuite.joke@gmai.com>
2013-02-11 19:18:17 +0800

    render_topic_next_page_link

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -3,4 +3,8 @@ module TopicsHelper
   def render_topic_title(topic)
     link_to(topic.title,topic.relative_url)
   end
+
+  def render_topic_next_page_link(topic, next_page)
+    link_to("next page", topic.relative_url + "?page=" + next_page )
+  end
 end
