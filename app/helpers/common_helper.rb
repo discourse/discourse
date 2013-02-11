c95629a781667f5af1a3cd489522ee9fae1ee7df
xdite <xuite.joke@gmai.com>
2013-02-11 19:34:14 +0800

    move google analytics to partial

---
+++ app/helpers/common_helper.rb
@@ -0,0 +1,7 @@
+module CommonHelper
+  def render_google_analytics_code
+    if Rails.env == "production" &&  SiteSetting.ga_tracking_code.present?
+      render :partial => "common/google_analytics"
+    end
+  end
+end
