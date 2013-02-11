1c12c91d0cb434f6dea0276c08d1983c9e79c20b
Sam Saffron <sam.saffron@gmail.com>
2013-02-11 17:14:36 +1100

    forgot to skip a filter

--- app/controllers/robots_txt_controller.rb
+++ app/controllers/robots_txt_controller.rb
@@ -1,6 +1,7 @@
 class RobotsTxtController < ApplicationController
   layout false
   skip_before_filter :check_xhr
+  skip_before_filter :check_restricted_access
 
   def index
     path = if SiteSetting.allow_index_in_robots_txt && !SiteSetting.restrict_access
