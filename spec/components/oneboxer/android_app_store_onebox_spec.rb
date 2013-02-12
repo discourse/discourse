8b626280e7fd7e8c4c36c0d20d049eb7616b8304
Jaime Iniesta <jaimeiniesta@gmail.com>
2013-02-12 09:46:45 -0500

    Replace Hpricot with Nokogiri

--- spec/components/oneboxer/android_app_store_onebox_spec.rb
+++ spec/components/oneboxer/android_app_store_onebox_spec.rb
@@ -25,7 +25,10 @@ private
     <img src="https://lh5.ggpht.com/wrYYVu74XNUu2WHk0aSZEqgdCDCNti9Fl0_dJnhgR6jY04ajQgVg5ABMatfcTDsB810=w124" class="thumbnail">
     <h3><a href="https://play.google.com/store/apps/details?id=com.moosoft.parrot" target="_blank">Talking Parrot</a></h3>
     
-    Listen to the parrot repeat what you say. A Fun application for all ages. Upgrade to Talking Parrot Pro to save sounds, set them as your ringtone and control recording.  Press the MENU button to access the settings where you can change the record time and repeat count.  This app uses anonymous usage stats to understand and improve performance.  Comments and feedback welcome. 
+    Listen to the parrot repeat what you say. A Fun application for all ages. Upgrade to Talking Parrot Pro to save sounds, set them as your ringtone and control recording. 
+ Press the MENU button to access the settings where you can change the record time and repeat count. 
+ This app uses anonymous usage stats to understand and improve performance. 
+ Comments and feedback welcome. 
   </div>
   <div class='clearfix'></div>
 </div>
