4ccb7354d196d239cc1c0c9db7d79b637a8606b7
Sam Saffron <sam.saffron@gmail.com>
2013-02-14 09:12:20 +1100

    nuking SSL = bad

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,17 +1,31 @@
 require 'openid/store/filesystem'
 require 'openssl'
 require 'openid_redis_store'
-module OpenSSL
-  module SSL
-    remove_const :VERIFY_PEER
-  end
-end
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+# if you need to test this and are having ssl issues see: 
+#  http://stackoverflow.com/questions/6756460/openssl-error-using-omniauth-specified-ssl-path-but-didnt-work
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :open_id, :store => OpenID::Store::Redis.new($redis), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :require => 'omniauth-openid' 
-  provider :open_id, :store => OpenID::Store::Redis.new($redis), :name => 'yahoo', :identifier => 'https://me.yahoo.com', :require => 'omniauth-openid' 
-  provider :facebook, SiteSetting.facebook_app_id, SiteSetting.facebook_app_secret, :scope => "email"
-  provider :twitter, SiteSetting.twitter_consumer_key , SiteSetting.twitter_consumer_secret
+
+  provider :open_id, 
+           :store => OpenID::Store::Redis.new($redis), 
+           :name => 'google', 
+           :identifier => 'https://www.google.com/accounts/o8/id', 
+           :require => 'omniauth-openid' 
+
+  provider :open_id, 
+           :store => OpenID::Store::Redis.new($redis), 
+           :name => 'yahoo', 
+           :identifier => 'https://me.yahoo.com', 
+           :require => 'omniauth-openid' 
+
+  provider :facebook, 
+           SiteSetting.facebook_app_id, 
+           SiteSetting.facebook_app_secret, 
+           :scope => "email"
+
+  provider :twitter, 
+           SiteSetting.twitter_consumer_key, 
+           SiteSetting.twitter_consumer_secret
+
 end
