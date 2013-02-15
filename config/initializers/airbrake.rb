ba05c9a07d6af559713471c11ee2f33d26d49701
Chris Sims <chris@jcsims.me>
2013-02-15 11:28:06 -0500

    Added airbrake initializer stub.

---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,6 @@
+# If you would like to use airbrake to track errors, you can sign up for a 
+# free account here: https://signup.airbrake.io/account/new/Free
+# and add your API key below
+Airbrake.configure do |config|                                                                                                     
+  # config.api_key = ''                           
+end
