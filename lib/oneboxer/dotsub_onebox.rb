89412f45dbef9eefb770eb97867b16e16374588e
Jeremy Banks <jeremy@jeremybanks.ca>
2013-02-19 01:49:58 -0500

    Include /^http... prefix in onebox whitelist URLs regexes to prevent XSS.

--- lib/oneboxer/dotsub_onebox.rb
+++ lib/oneboxer/dotsub_onebox.rb
@@ -3,7 +3,7 @@ require_dependency 'oneboxer/oembed_onebox'
 module Oneboxer
   class DotsubOnebox < OembedOnebox
 
-    matcher /dotsub\.com\/.+$/
+    matcher /^https?:\/\/(?:www\.)?dotsub\.com\/.+$/
 
     def oembed_endpoint
       "http://dotsub.com/services/oembed?url=#{BaseOnebox.uriencode(@url)}"
