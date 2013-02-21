6096a447b7c85fbd3cf3fe3ae1ee7ac07d2f828b
Robin Ward <robin.ward@gmail.com>
2013-02-21 12:59:38 -0500

    Fix Regression: In reply to

--- app/assets/javascripts/discourse/views/parent_view.js
+++ app/assets/javascripts/discourse/views/parent_view.js
@@ -2,16 +2,14 @@
 
   window.Discourse.ParentView = Discourse.EmbeddedPostView.extend({
     previousPost: true,
-    /* Nice animation for when the replies appear
-    */
-
+    
+    // Nice animation for when the replies appear
     didInsertElement: function() {
       var $parentPost;
       this._super();
-      $parentPost = this.get('postView').jQuery('section.parent-post');
-      /* Animate unless we're on a touch device
-      */
+      $parentPost = this.get('postView').$('section.parent-post');
 
+      // Animate unless we're on a touch device
       if (Discourse.get('touch')) {
         return $parentPost.show();
       } else {
