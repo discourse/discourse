21cc9ab1ff6ed1a9c83c82d15ce3e7c5f99c8774
Sam Saffron <sam.saffron@gmail.com>
2013-02-22 10:56:59 +1100

    work in progress

---
+++ lib/image_optimizer.rb
@@ -0,0 +1,15 @@
+# This class is used to download an optimize images.
+
+class ImageOptimizer
+
+  def initialize(url)
+    @url = url
+  end
+
+  def force_refresh!
+  end
+
+  def optimized_path(width=nil, height=nil)
+  end
+
+end
