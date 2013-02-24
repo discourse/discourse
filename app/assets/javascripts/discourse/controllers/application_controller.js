997b43621077a2837ff53326a828d1898b76eb39
Sam Saffron <sam.saffron@gmail.com>
2013-02-25 08:36:46 +1100

    attempt at fixing google analytics

--- app/assets/javascripts/discourse/controllers/application_controller.js
+++ app/assets/javascripts/discourse/controllers/application_controller.js
@@ -1,11 +1,22 @@
-(function() {
+/*global _gaq:true */
 
-  window.Discourse.ApplicationController = Ember.Controller.extend({
-    needs: ['modal'],
-    showLogin: function() {
-      var _ref;
-      return (_ref = this.get('controllers.modal')) ? _ref.show(Discourse.LoginView.create()) : void 0;
+window.Discourse.ApplicationController = Ember.Controller.extend({
+  needs: ['modal'],
+  showLogin: function() {
+    var _ref;
+    return (_ref = this.get('controllers.modal')) ? _ref.show(Discourse.LoginView.create()) : void 0;
+  },
+
+  routeChanged: function(){
+    if (window._gaq === undefined) { return; }
+
+    if(this.afterFirstHit) {
+      Em.run.next(function(){
+        _gaq.push(['_trackPageview']);
+      });
+    } else {
+      this.afterFirstHit = true;
     }
-  });
-
-}).call(this);
+  }.observes('currentPath')
+  
+});
