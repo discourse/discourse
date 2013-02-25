5b64050e86adfa896358db9759031819de26abc2
Neil Lalonde <neillalonde@gmail.com>
2013-02-25 16:21:52 -0500

    Dashboard JS checks version every 12 hours

--- app/assets/javascripts/admin/routes/admin_dashboard_route.js
+++ app/assets/javascripts/admin/routes/admin_dashboard_route.js
@@ -3,23 +3,30 @@
   /**
     Handles the default admin route
 
-    @class AdminDashboardRoute    
+    @class AdminDashboardRoute
     @extends Discourse.Route
     @namespace Discourse
     @module Discourse
   **/
   Discourse.AdminDashboardRoute = Discourse.Route.extend({
     setupController: function(c) {
-      if( Discourse.SiteSettings.version_checks ) {
-        Discourse.VersionCheck.find().then(function(vc) {
-          c.set('versionCheck', vc);
-          c.set('loading', false);
-        });
+      if( !c.get('versionCheckedAt') || Date.create('12 hours ago') > c.get('versionCheckedAt') ) {
+        this.checkVersion(c);
       }
     },
 
     renderTemplate: function() {
       this.render({into: 'admin/templates/admin'});
+    },
+
+    checkVersion: function(c) {
+      if( Discourse.SiteSettings.version_checks ) {
+        Discourse.VersionCheck.find().then(function(vc) {
+          c.set('versionCheck', vc);
+          c.set('versionCheckedAt', new Date());
+          c.set('loading', false);
+        });
+      }
     }
   });
 
