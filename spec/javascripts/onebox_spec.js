9a40789f4d0302a61640b3feb7e807aae1e83d60
Régis Hanol <regis@hanol.fr>
2013-02-28 03:26:20 +0100

    cleaned up all javascript specs for better readability

--- spec/javascripts/onebox_spec.js
+++ spec/javascripts/onebox_spec.js
@@ -1,26 +1,21 @@
 /*global waitsFor:true expect:true describe:true beforeEach:true it:true spyOn:true */
-  describe("Discourse.Onebox", function() {
-    beforeEach(function() {
-      return spyOn(jQuery, 'ajax').andCallThrough();
-    });
-    it("Stops rapid calls with cache true", function() {
-      Discourse.Onebox.lookup('http://bla.com', true, function(c) {
-        return c;
-      });
-      Discourse.Onebox.lookup('http://bla.com', true, function(c) {
-        return c;
-      });
-      return expect(jQuery.ajax.calls.length).toBe(1);
-    });
-    return it("Stops rapid calls with cache false", function() {
-      Discourse.Onebox.lookup('http://bla.com/a', false, function(c) {
-        return c;
-      });
-      Discourse.Onebox.lookup('http://bla.com/a', false, function(c) {
-        return c;
-      });
-      return expect(jQuery.ajax.calls.length).toBe(1);
-    });
+
+describe("Discourse.Onebox", function() {
+
+  beforeEach(function() {
+    spyOn(jQuery, 'ajax').andCallThrough();
   });
 
+  it("Stops rapid calls with cache true", function() {
+    Discourse.Onebox.lookup('http://bla.com', true, function(c) { return c; });
+    Discourse.Onebox.lookup('http://bla.com', true, function(c) { return c; });
+    expect(jQuery.ajax.calls.length).toBe(1);
+  });
 
+  it("Stops rapid calls with cache false", function() {
+    Discourse.Onebox.lookup('http://bla.com/a', false, function(c) { return c; });
+    Discourse.Onebox.lookup('http://bla.com/a', false, function(c) { return c; });
+    expect(jQuery.ajax.calls.length).toBe(1);
+  });
+
+});
