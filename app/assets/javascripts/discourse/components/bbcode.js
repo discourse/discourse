3901ddd153b9068e08c346381e85925bc78c5d0a
Robin Ward <robin.ward@gmail.com>
2013-03-01 15:15:37 -0500

    Fix jshint issue with `[^]` in regexp.

--- app/assets/javascripts/discourse/components/bbcode.js
+++ app/assets/javascripts/discourse/components/bbcode.js
@@ -9,7 +9,7 @@
 **/
 Discourse.BBCode = {
 
-  QUOTE_REGEXP: /\[quote=([^\]]*)\]((?:[^](?!\[quote=[^\]]*\]))*?)\[\/quote\]/im,
+  QUOTE_REGEXP: /\[quote=([^\]]*)\]((?:[\s\S](?!\[quote=[^\]]*\]))*?)\[\/quote\]/im,
 
   // Define our replacers
   replacers: {
