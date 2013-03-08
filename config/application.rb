3b4a4e8a566a508cdaef9306128a81dac7eebf25
tangramor <tangramor@gmail.com>
2013-03-09 00:47:17 +0800

    #393 - Completed the translation work on server.zh_CN.yml. And addressed the comments from @eviltrout and @linjunpop

--- config/application.rb
+++ config/application.rb
@@ -55,8 +55,6 @@ module Discourse
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :zh_CN
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = 'utf-8'
