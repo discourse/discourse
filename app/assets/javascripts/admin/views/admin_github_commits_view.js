bf37e1b9958c6a3aaaf26ead1a001742a87df5fa
Neil Lalonde <neillalonde@gmail.com>
2013-03-11 15:26:14 -0400

    Convert github commits widget to ember

---
+++ app/assets/javascripts/admin/views/admin_github_commits_view.js
@@ -0,0 +1,11 @@
+/**
+  A view for showing commits to the discourse repo.
+
+  @class AdminGithubCommitsView
+  @extends Discourse.View
+  @namespace Discourse
+  @module Discourse
+**/
+Discourse.AdminGithubCommitsView = Discourse.View.extend({
+  templateName: 'admin/templates/commits'
+});
