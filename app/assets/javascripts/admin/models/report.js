0b4339c103bc9b388c637cff5928c79be2e8ab5d
Robin Ward <robin.ward@gmail.com>
2013-03-17 15:02:36 -0400

    For Evil Trout blog part 2: switch between bar chart and table

--- app/assets/javascripts/admin/models/report.js
+++ app/assets/javascripts/admin/models/report.js
@@ -6,6 +6,18 @@ Discourse.Report.reopenClass({
     $.ajax("/admin/reports/" + type, {
       type: 'GET',
       success: function(json) {
+
+        // Add a percent field to each tuple
+        var maxY = 0;
+        json.report.data.forEach(function (row) {
+          if (row.y > maxY) maxY = row.y;
+        });
+        if (maxY > 0) {
+          json.report.data.forEach(function (row) {
+            row.percentage = Math.round((row.y / maxY) * 100);
+          });
+        }
+
         model.mergeAttributes(json.report);
         model.set('loaded', true);
       }
