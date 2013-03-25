622568fd57ab84c1fbb51a94dd3ff6a4f3965291
Sam <sam.saffron@gmail.com>
2013-03-24 20:36:55 -0700

    fix mp in prd

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -19,8 +19,11 @@ if defined?(Rack::MiniProfiler)
   #  with a load balancer in front this becomes really bad as some results can
   #  be stored associated with ip1 as the user and retrieved using ip2 causing 404s 
   Rack::MiniProfiler.config.user_provider = lambda do |env|
-    user = CurrentUser.lookup_from_env(env)
-    user ? user.id.to_s : Rack::Request.new(env).ip
+    request = Rack::Request.new(env)
+    id = request.cookies["_t"] || request.ip || "unknown"
+    id = id.to_s
+    # some security, lets not have these tokens floating about
+    Digest::MD5.hexdigest(id)
   end
 
   Rack::MiniProfiler.config.position = 'left'
