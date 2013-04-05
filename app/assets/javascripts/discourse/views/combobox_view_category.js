ee43ce693f06fb055f93a65aeaf9636c5087b59a
Neil Lalonde <neillalonde@gmail.com>
2013-04-05 15:43:56 -0400

    Add topic counts to category selector

--- app/assets/javascripts/discourse/views/combobox_view_category.js
+++ app/assets/javascripts/discourse/views/combobox_view_category.js
@@ -10,13 +10,14 @@ Discourse.ComboboxViewCategory = Discourse.ComboboxView.extend({
   none: 'category.none',
   classNames: ['combobox category-combobox'],
   overrideWidths: true,
-  dataAttributes: ['name', 'color', 'text_color', 'description'],
+  dataAttributes: ['name', 'color', 'text_color', 'description', 'topic_count'],
   valueBinding: Ember.Binding.oneWay('source'),
 
   template: function(text, templateData) {
     if (!templateData.color) return text;
     var result = "<span class='badge-category' style='background-color: #" + templateData.color + '; color: #' +
         templateData.text_color + ";'>" + templateData.name + "</span>";
+    result += " <span class='topic-count'>&times; " + templateData.topic_count + "</span>";
     if (templateData.description && templateData.description !== 'null') {
       result += '<div class="category-desc">' + Handlebars.Utils.escapeExpression(templateData.description) + '</div>';
     }
