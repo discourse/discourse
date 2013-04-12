8014d7fd25fef88383536add3122174eb390baa1
Sam <sam.saffron@gmail.com>
2013-04-12 21:59:52 +1000

    correct rack::cache so it always punches through users with auth cookies

--- config/initializers/99-rack-cache.rb
+++ config/initializers/99-rack-cache.rb
@@ -4,7 +4,21 @@ if Rails.configuration.respond_to?(:enable_rack_cache) && Rails.configuration.en
 
   url = DiscourseRedis.url
 
-  Rails.configuration.middleware.insert 0, Rack::Cache,
+  class Rack::Cache::Discourse < Rack::Cache::Context
+    def initialize(app, options={})
+      @app = app
+      super
+    end
+    def call(env)
+      if CurrentUser.has_auth_cookie?(env)
+        @app.call(env)
+      else
+        super
+      end
+    end
+  end
+
+  Rails.configuration.middleware.insert 0, Rack::Cache::Discourse,
     metastore: "#{url}/metastore",
     entitystore: "#{url}/entitystore",
     verbose: !Rails.env.production?
