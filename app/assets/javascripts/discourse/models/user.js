4bd4e4e1c7e90ed86b931006284efdcf1ba33ef2
Sam <sam.saffron@gmail.com>
2013-04-18 17:22:59 +1000

    missing bits from user page rediscovered

--- app/assets/javascripts/discourse/models/user.js
+++ app/assets/javascripts/discourse/models/user.js
@@ -25,7 +25,7 @@ Discourse.User = Discourse.Model.extend({
     @type {String}
   **/
   avatarSmall: (function() {
-  return Discourse.Utilities.avatarUrl(this.get('username'), 'small', this.get('avatar_template'));
+    return Discourse.Utilities.avatarUrl(this.get('username'), 'small', this.get('avatar_template'));
   }).property('username'),
 
   /**
@@ -34,9 +34,13 @@ Discourse.User = Discourse.Model.extend({
     @property websiteName
     @type {String}
   **/
-  websiteName: (function() {
+  websiteName: function() {
     return this.get('website').split("/")[2];
-  }).property('website'),
+  }.property('website'),
+
+  hasWebsite: function() {
+    return this.present('website');
+  }.property('website'),
 
   /**
     Path to this user.
@@ -334,6 +338,7 @@ Discourse.User = Discourse.Model.extend({
 
   onDetailsLoaded: function(callback){
     var _this = this;
+    this.set("loading",false);
 
     if(callback){
       this.onDetailsLoadedCallbacks = this.onDetailsLoadedCallbacks || [];
@@ -353,6 +358,7 @@ Discourse.User = Discourse.Model.extend({
   **/
   loadDetails: function() {
 
+    this.set("loading",true);
     // Check the preload store first
     var user = this;
     var username = this.get('username');
