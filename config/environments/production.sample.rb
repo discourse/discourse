467c76b2c03a69a5ab05806faa039e52320282f0
slainer68 <slainer68@gmail.com>
2013-04-22 11:16:58 +0200

    Implements support for rack-cors for API JavaScript access in end-user browser

--- config/environments/production.sample.rb
+++ config/environments/production.sample.rb
@@ -55,6 +55,13 @@ Discourse::Application.configure do
   # allows admins to use mini profiler
   config.enable_mini_profiler = true
 
+  # allows Cross-origin resource sharing (CORS) for API access in JavaScript (default to false for security).
+  # See the initializer and https://github.com/cyu/rack-cors for configuration documentation.
+  #
+  # config.enable_rack_cors = false
+  # config.rack_cors_origins = ['*']
+  # config.rack_cors_resource = ['*', { :headers => :any, :methods => [:get, :post, :options] }]
+
   # Discourse strongly recommend you use a CDN.
   # For origin pull cdns all you need to do is register an account and configure
   # config.action_controller.asset_host = "http://YOUR_CDN_HERE"
