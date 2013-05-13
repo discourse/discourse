7f3c0934b5724077618ed0f6cca904410bf6b236
Sam <sam.saffron@gmail.com>
2013-05-13 10:51:45 +1000

    fix spec

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe SearchController do
 
   it 'performs the query' do
-    Search.expects(:query).with('test', nil, 3)
+    Search.expects(:query).with('test', nil, nil, 3)
     xhr :get, :query, term: 'test'
   end
 
   it 'performs the query with a filter' do
-    Search.expects(:query).with('test', 'topic', 3)
+    Search.expects(:query).with('test', nil, 'topic', 3)
     xhr :get, :query, term: 'test', type_filter: 'topic'
   end
 
