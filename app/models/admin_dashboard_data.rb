21b4b8d5d5af5fae9921ba6643dcb775e43a9f2e
Neil Lalonde <neillalonde@gmail.com>
2013-05-14 16:17:17 -0400

    Expire dashboard data when you upgrade to a new discourse version. Version check data was being cached and causing confusion to people who upgraded.

--- app/models/admin_dashboard_data.rb
+++ app/models/admin_dashboard_data.rb
@@ -45,13 +45,13 @@ class AdminDashboardData
 
   def as_json
     @json ||= {
-      reports: REPORTS.map { |type| Report.find(type) },
+      reports: REPORTS.map { |type| Report.find(type).as_json },
       problems: problems,
       admins: User.admins.count,
       moderators: User.moderators.count,
-      top_referrers: IncomingLinksReport.find('top_referrers'),
-      top_traffic_sources: IncomingLinksReport.find('top_traffic_sources'),
-      top_referred_topics: IncomingLinksReport.find('top_referred_topics')
+      top_referrers: IncomingLinksReport.find('top_referrers').as_json,
+      top_traffic_sources: IncomingLinksReport.find('top_traffic_sources').as_json,
+      top_referred_topics: IncomingLinksReport.find('top_referred_topics').as_json
     }.merge(
       SiteSetting.version_checks? ? {version_check: DiscourseUpdates.check_version} : {}
     )
