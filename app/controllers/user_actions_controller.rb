e91ed83586106d489951d1598adf411178b2e69a
Sam <sam.saffron@gmail.com>
2013-05-20 16:44:06 +1000

    the private message stream is different to normal streams, improving the ui a bit and collapsing conversations

--- app/controllers/user_actions_controller.rb
+++ app/controllers/user_actions_controller.rb
@@ -2,14 +2,22 @@ class UserActionsController < ApplicationController
   def index
     requires_parameters(:user_id)
     per_chunk = 60
-    render json: UserAction.stream(
+
+    opts = {
       user_id: params[:user_id].to_i,
       offset: params[:offset],
       limit: per_chunk,
-      action_types: (params[:filter] || "").split(","),
+      action_types: (params[:filter] || "").split(",").map(&:to_i),
       guardian: guardian,
       ignore_private_messages: params[:filter] ? false : true
-    )
+    }
+
+    if opts[:action_types] == [UserAction::GOT_PRIVATE_MESSAGE] ||
+       opts[:action_types] == [UserAction::NEW_PRIVATE_MESSAGE]
+      render json: UserAction.private_message_stream(opts[:action_types][0], opts)
+    else
+      render json: UserAction.stream(opts)
+    end
   end
 
   def show
