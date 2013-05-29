dbf4d9b0dc2361ff808aaddeb85729f85e2c340f
Chris Hunt <c@chrishunt.co>
2013-05-29 09:36:15 -0700

    Set Sidekiq log level to WARN

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,3 +2,5 @@ sidekiq_redis = { url: $redis.url, namespace: 'sidekiq' }
 
 Sidekiq.configure_server { |config| config.redis = sidekiq_redis }
 Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
+
+Sidekiq.logger.level = Logger::WARN
