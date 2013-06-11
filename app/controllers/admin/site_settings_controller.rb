82b5f57e40640e52460d58ead3efb426c4ee0e4c
Neil Lalonde <neillalonde@gmail.com>
2013-06-11 14:31:38 -0400

    Make it possible to set a site setting to empty string

--- app/controllers/admin/site_settings_controller.rb
+++ app/controllers/admin/site_settings_controller.rb
@@ -7,7 +7,7 @@ class Admin::SiteSettingsController < Admin::AdminController
   end
 
   def update
-    params.require(:value)
+    raise ActionController::ParameterMissing.new(:value) unless params.has_key?(:value)
     SiteSetting.send("#{params[:id]}=", params[:value])
     render nothing: true
   end
