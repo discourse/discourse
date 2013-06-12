28ac110a3c2cdf66afe57927f2661605ad1c4b10
Neil Lalonde <neillalonde@gmail.com>
2013-06-12 12:02:32 -0400

    Add eu-west-1 to s3 regions

--- app/models/s3_region_site_setting.rb
+++ app/models/s3_region_site_setting.rb
@@ -4,6 +4,6 @@ class S3RegionSiteSetting
   end
 
   def self.all_values
-    @all_values ||= ['', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'sa-east-1'].sort
+    @all_values ||= ['', 'us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'sa-east-1'].sort
   end
 end
