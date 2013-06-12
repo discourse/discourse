c47239b536602a7f0a4ac1a9e5f3c9296e3cbeae
Sam <sam.saffron@gmail.com>
2013-06-13 09:39:18 +1000

    comment out twitter onebox, it is bust

--- lib/oneboxer/twitter_onebox.rb
+++ lib/oneboxer/twitter_onebox.rb
@@ -1,19 +1,28 @@
+# TODO implement per: http://meta.discourse.org/t/twitter-oneboxes-are-bust/7410/3
 require_dependency 'oneboxer/handlebars_onebox'
 
 module Oneboxer
   class TwitterOnebox < HandlebarsOnebox
 
-    matcher /^https?:\/\/(?:www\.)?twitter.com\/.*$/
+    REGEX = /^https?:\/\/(?:www\.)?twitter.com\/(?<user>[^\/]+)\/status\/(?<id>\d+)$/
+    # matcher REGEX
+
+    # TODO: use zocial instead
     favicon 'twitter.png'
 
-    def translate_url
-      m = @url.match(/\/(?<user>[^\/]+)\/status\/(?<id>\d+)/mi)
-      return "http://api.twitter.com/1/statuses/show/#{URI::encode(m[:id])}.json" if m.present?
-      @url
+    def fetch_html
+      m = @url.match(REGEX)
+
+      if SiteSetting.twitter_consumer_key.present? && SiteSetting.twitter_consumer_secret.present?
+        token = prepare_access_token(SiteSetting.twitter_consumer_key, SiteSetting.twitter_consumer_secret)
+        token.request(:get, "https://api.twitter.com/1.1/statuses/show/#{URI::encode(m[:id])}.json")
+      else
+        # perhaps?
+        raise Discourse::SiteSettingMissing
+      end
     end
 
     def parse(data)
-
       result = JSON.parse(data)
 
       result["created_at"] = Time.parse(result["created_at"]).strftime("%I:%M%p - %d %b %y")
@@ -25,6 +34,14 @@ module Oneboxer
 
       result
     end
+
+    protected
+
+    def get_message_json(consumer_key, consumer_secret)
+      raise "NOT IMPLEMENTED"
+      # implement per http://meta.discourse.org/t/twitter-oneboxes-are-bust/7410/3
+    end
+
 
   end
 end
