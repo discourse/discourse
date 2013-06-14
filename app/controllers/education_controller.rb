ee9a4efc78295e3e469291e396bec4737d515c0c
Vipul A M <vipulnsward@gmail.com>
2013-06-14 10:53:05 +0530

    Remove some unnecessary variable initializations

--- app/controllers/education_controller.rb
+++ app/controllers/education_controller.rb
@@ -9,7 +9,6 @@ class EducationController < ApplicationController
 
     education_posts_text = I18n.t('education.until_posts', count: SiteSetting.educate_until_posts)
 
-    markdown_content = ""
     if params[:id] == 'new-topic'
       markdown_content = SiteContent.content_for(:education_new_topic, education_posts_text: education_posts_text)
     else
