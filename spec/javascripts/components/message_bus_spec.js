d5191adfc9eed368a2dd0596a190ed42b9e4bcec
Robin Ward <robin.ward@gmail.com>
2013-06-18 18:08:37 -0400

    Remove useless jasmine spec

--- spec/javascripts/components/message_bus_spec.js
+++
@@ -1,9 +0,0 @@
-/*global waitsFor:true expect:true describe:true beforeEach:true it:true */
-
-describe("Discourse.MessageBus", function() {
-
-  describe("Long polling", function() {
-    Discourse.MessageBus.start();
-  });
-
-});
