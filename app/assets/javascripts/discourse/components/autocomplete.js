4c51f36db2baf42786a25a7915dcd5b83742e7f9
Sam <sam.saffron@gmail.com>
2013-06-25 17:53:12 +1000

    was dropping off the last key when inviting a user by email.

--- app/assets/javascripts/discourse/components/autocomplete.js
+++ app/assets/javascripts/discourse/components/autocomplete.js
@@ -203,18 +203,8 @@ $.fn.autocomplete = function(options) {
     closeAutocomplete();
   });
 
-  $(this).keypress(function(e) {
 
-    if(options.allowAny){
-      if(inputSelectedItems.length === 0) {
-        inputSelectedItems.push("");
-      }
-      inputSelectedItems.pop();
-      inputSelectedItems.push(me.val());
-      if (options.onChangeItems) {
-        options.onChangeItems(inputSelectedItems);
-      }
-    }
+  $(this).keypress(function(e) {
 
     if (!options.key) return;
 
@@ -232,6 +222,25 @@ $.fn.autocomplete = function(options) {
 
   return $(this).keydown(function(e) {
     var c, caretPosition, i, initial, next, nextIsGood, prev, prevIsGood, stopFound, term, total, userToComplete;
+
+    if(options.allowAny){
+      // saves us wiring up a change event as well, keypress is while its pressed
+      _.delay(function(){
+        if(inputSelectedItems.length === 0) {
+          inputSelectedItems.push("");
+        }
+
+        if(_.isString(inputSelectedItems[0])) {
+          inputSelectedItems.pop();
+          inputSelectedItems.push(me.val());
+          if (options.onChangeItems) {
+            options.onChangeItems(inputSelectedItems);
+          }
+        }
+
+      },50);
+    }
+
     if (!options.key) {
       completeStart = 0;
     }
