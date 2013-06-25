9f6b7889a8b516a9720e26496ae8235538166dce
Dan Johnson <ComputerDruid@gmail.com>
2013-06-25 00:18:54 -0400

    views: convert 'ip' (bigint) -> 'ip_address' (inet)
    This fixes all known issues when connecting to discourse over IPv6.
    
    This table has no primary key, so the migration is done with update_all,
    for each ip address in the views table. Since this table can potentially
    grow quite large, this process might take a long time. I don't know any
    way around this, though.
    
    This migration uses a SQL command to populate the new field from the old
    one, so as not to rely on the View model class, which should keep the
    migration from failing if that class is modified in the future.

---
+++ db/migrate/20130625022454_change_ip_to_inet_in_views.rb
@@ -0,0 +1,22 @@
+require 'ipaddr'
+
+class ChangeIpToInetInViews < ActiveRecord::Migration
+  def up
+    table = :views
+    add_column table, :ip_address, :inet
+
+    execute "UPDATE views SET ip_address = inet(
+      (ip >> 24 & 255) || '.' ||
+      (ip >> 16 & 255) || '.' ||
+      (ip >>  8 & 255) || '.' ||
+      (ip >>  0 & 255)
+    );"
+
+    change_column table, :ip_address, :inet, { :null => false }
+    remove_column table, :ip
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
