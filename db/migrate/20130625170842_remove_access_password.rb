dea75870e1002ca5cc771de359b9259c6e4cbd27
Neil Lalonde <neillalonde@gmail.com>
2013-06-26 10:30:57 -0400

    Fix: don't enable login_required on sites that used to use an access_password

--- db/migrate/20130625170842_remove_access_password.rb
+++ db/migrate/20130625170842_remove_access_password.rb
@@ -1,6 +1,6 @@
 class RemoveAccessPassword < ActiveRecord::Migration
   def up
-    result = execute("SELECT count(*) FROM site_settings where name='access_password'")
+    result = execute("SELECT count(*) FROM site_settings where name='access_password' and char_length(value) > 0")
     if result[0] and result[0]["count"].to_i > 0
       execute "DELETE FROM site_settings where name='access_password'"
       SiteSetting.login_required = true
