34dc6a41d9c1954afa23b2c76ba1d583dc0b1390
James A. Anderson & Jalil Mohammed <me@jamesaanderson.com>
2013-06-29 10:43:35 -0700

    Add flash video onebox support http://meta.discourse.org/t/will-there-be-flash-videos-in-the-future/7787

---
+++ lib/oneboxer/flash_video_onebox.rb
@@ -0,0 +1,13 @@
+require_dependency 'oneboxer/base_onebox'
+
+module Oneboxer
+  class FlashVideoOnebox < BaseOnebox
+    
+    matcher /^https?:\/\/.*\.(swf|flv)$/
+
+    def onebox
+      "<object width='100%' height='100%'><param name='#{@url}' value='#{@url}'><embed src='#{@url}' width='100%' height='100%'></embed></object>"
+    end
+
+  end
+end
