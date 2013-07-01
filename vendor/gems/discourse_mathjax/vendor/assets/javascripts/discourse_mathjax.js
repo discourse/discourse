1f68cbbc3fd776153bfa9f2597f1cdb81f4a843d
David Montoya <masda70@gmail.com>
2013-07-01 22:26:44 +0200

    more mathjax

--- vendor/gems/discourse_mathjax/vendor/assets/javascripts/discourse_mathjax.js
+++ vendor/gems/discourse_mathjax/vendor/assets/javascripts/discourse_mathjax.js
@@ -1,37 +1,24 @@
 (function() {
 
-  		var Script = {
-		  _loadedScripts: [],
-		  include: function(script){
-			// include script only once
-			if (this._loadedScripts.include(script)){
-			  return false;
-			}
-			// request file synchronous
-			var code = new Ajax.Request(script, {
-			  asynchronous: false, method: "GET",
-			  evalJS: false, evalJSON: false
-			}).transport.responseText;
-			// eval code on global level
-			if (Prototype.Browser.IE) {
-			  window.execScript(code);
-			} else if (Prototype.Browser.WebKit){
-			  $$("head").first().insert(Object.extend(
-				new Element("script", {type: "text/javascript"}), {text: code}
-			  ));
-			} else {
-			  window.eval(code);
-			}
-			// remember included script
-			this._loadedScripts.push(script);
-		  }
-		};
-	Script.load("http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"); 
-	MathJax.Hub.Config({
-		  tex2jax: {
-			ignoreClass: "ember-application",
-			processClass: "preview-wrapper|cooked"
-		  }
-	});
+ var head = document.getElementsByTagName("head")[0], script;
+  script = document.createElement("script");
+  script.type = "text/x-mathjax-config";
+  script[(window.opera ? "innerHTML" : "text")] =
+    "MathJax.Hub.Config({\n" +
+    "  tex2jax: { inlineMath: [['$$','$$'], ['\\\\(','\\\\)']] , ignoreClass: 'ember-application', processClass: 'preview-wrapper|cooked' }\n" +
+    "});" +
+	"MathJax.Hub.Startup.onload();";
+  head.appendChild(script);
+  script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+  head.appendChild(script);
+  
+    // Regiest a before cook event
+  Discourse.Markdown.on("beforeCook", function(event) {
+	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+  });
+  
+
 	  
 }).call(this);
