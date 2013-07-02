341214943d8f63f371eac0ba41b32045c5133579
Charles-Pierre Astolfi <cp.astolfi@gmail.com>
2013-07-02 09:33:36 +0000

    Revert "Mathjax register_js fix"
    This reverts commit e26b385b4f9a0637e386a65649a9c756a26470df.

--- vendor/gems/discourse_mathjax/lib/discourse_mathjax/plugin.rb
+++ vendor/gems/discourse_mathjax/lib/discourse_mathjax/plugin.rb
@@ -6,7 +6,8 @@ module DiscourseMathjax
 
     def setup
       # Add our Assets
-      register_js('http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML')
+      register_js('discourse_mathjax',
+                    server_side: 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML')
     end
 
   end
