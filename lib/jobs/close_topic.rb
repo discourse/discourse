50767be7229d4c4747a80fabe8499aea7d3db7f6
Sam <sam.saffron@gmail.com>
2013-07-05 15:21:04 +1000

    user where as opposed to find to compensate for deleted topics

--- lib/jobs/close_topic.rb
+++ lib/jobs/close_topic.rb
@@ -2,9 +2,9 @@ module Jobs
   class CloseTopic < Jobs::Base
 
     def execute(args)
-      topic = Topic.find(args[:topic_id])
+      topic = Topic.where(id: args[:topic_id]).first
       if topic and topic.auto_close_at and !topic.closed? and !topic.deleted_at
-        closer = User.find(args[:user_id])
+        closer = User.where(id: args[:user_id]).first
         if Guardian.new(closer).can_moderate?(topic)
           topic.update_status('autoclosed', true, closer)
         end
