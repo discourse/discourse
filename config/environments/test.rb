5086f4878445273a3c6de27847abe642a24d7cc7
Sam <sam.saffron@gmail.com>
2013-07-08 12:10:47 +1000

    reloading classes is just trouble in test, lets not do that so spork is squeaky clean and happy.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@ Discourse::Application.configure do
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
