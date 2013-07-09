114771f74828f3bfdb113fb8e0c16c0432c27dff
vykster <vyki.englert@gmail.com>
2013-07-09 01:40:21 -0700

    namespacing nfb oneboxer

--- lib/discourse/oneboxer/nfb_onebox.rb
+++ lib/discourse/oneboxer/nfb_onebox.rb
@@ -1,14 +1,16 @@
 require_dependency 'oneboxer/oembed_onebox'
 
-module Oneboxer
-  class NfbOnebox < OembedOnebox
+module Discourse	
+	module Oneboxer
+	  class NfbOnebox < OembedOnebox
 
-    matcher /^https?:\/\/(?:www\.)?nfb\.ca\/film\/[-\w]+\/?/
+	    matcher /^https?:\/\/(?:www\.)?nfb\.ca\/film\/[-\w]+\/?/
 
-    def oembed_endpoint
-      "http://www.nfb.ca/remote/services/oembed/?url=#{BaseOnebox.uriencode(@url)}&format=json"
-    end
+	    def oembed_endpoint
+	      "http://www.nfb.ca/remote/services/oembed/?url=#{BaseOnebox.uriencode(@url)}&format=json"
+	    end
 
 
-  end
+	  end
+	end
 end
