cb83e523ad473174bed411eb4eb8cd836824b25b
vykster <vyki.englert@gmail.com>
2013-07-09 01:46:29 -0700

    namespacing ted oneboxer

--- lib/discourse/oneboxer/ted_onebox.rb
+++ lib/discourse/oneboxer/ted_onebox.rb
@@ -1,10 +1,12 @@
 require_dependency 'oneboxer/oembed_onebox'
 
-module Oneboxer
-  class TedOnebox < OembedOnebox
-    matcher /^https?\:\/\/(www\.)?ted\.com\/talks\/.*$/
-    def oembed_endpoint
-      "http://www.ted.com/talks/oembed.json?url=#{BaseOnebox.uriencode(@url)}"
-    end
-  end
+module Discourse
+	module Oneboxer
+	  class TedOnebox < OembedOnebox
+	    matcher /^https?\:\/\/(www\.)?ted\.com\/talks\/.*$/
+	    def oembed_endpoint
+	      "http://www.ted.com/talks/oembed.json?url=#{BaseOnebox.uriencode(@url)}"
+	    end
+	  end
+	end
 end
