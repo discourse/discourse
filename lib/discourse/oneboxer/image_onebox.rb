66b16dc48db509be60ae62d5b5c9b30c018b7b7f
vykster <vyki.englert@gmail.com>
2013-07-09 01:38:36 -0700

    namespacing image oneboxer

--- lib/discourse/oneboxer/image_onebox.rb
+++ lib/discourse/oneboxer/image_onebox.rb
@@ -1,13 +1,15 @@
 require_dependency 'oneboxer/base_onebox'
 
-module Oneboxer
-  class ImageOnebox < BaseOnebox
+module Discourse	
+	module Oneboxer
+	  class ImageOnebox < BaseOnebox
 
-    matcher /^https?:\/\/.*\.(jpg|png|gif|jpeg)$/
+	    matcher /^https?:\/\/.*\.(jpg|png|gif|jpeg)$/
 
-    def onebox
-      Oneboxer::BaseOnebox.image_html(@url, nil, @url)
-    end
+	    def onebox
+	      Oneboxer::BaseOnebox.image_html(@url, nil, @url)
+	    end
 
-  end
+	  end
+	end
 end
