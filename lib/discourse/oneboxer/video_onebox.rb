819eec10ec6d149b4afee33d0d65d0d39452e201
vykster <vyki.englert@gmail.com>
2013-07-09 01:48:05 -0700

    namespacing video oneboxer

--- lib/discourse/oneboxer/video_onebox.rb
+++ lib/discourse/oneboxer/video_onebox.rb
@@ -1,13 +1,15 @@
 require_dependency 'oneboxer/base_onebox'
 
-module Oneboxer
-  class VideoOnebox < BaseOnebox
-    
-    matcher /^https?:\/\/.*\.(mov|mp4|ogg)$/
+module Discourse
+	module Oneboxer
+	  class VideoOnebox < BaseOnebox
+	    
+	    matcher /^https?:\/\/.*\.(mov|mp4|ogg)$/
 
-    def onebox
-      "<video width='100%' height='100%' controls><source src='#{@url}'><a href='#{@url}'>#{@url}</a></video>"
-    end
+	    def onebox
+	      "<video width='100%' height='100%' controls><source src='#{@url}'><a href='#{@url}'>#{@url}</a></video>"
+	    end
 
-  end
+	  end
+	end
 end
