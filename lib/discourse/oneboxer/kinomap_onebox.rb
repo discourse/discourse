ef4288e05cd5d7ceae3f368faa771976d87d2dfc
vykster <vyki.englert@gmail.com>
2013-07-09 01:39:30 -0700

    namespacing kinomap oneboxer

--- lib/discourse/oneboxer/kinomap_onebox.rb
+++ lib/discourse/oneboxer/kinomap_onebox.rb
@@ -1,14 +1,16 @@
 require_dependency 'oneboxer/oembed_onebox'
 
-module Oneboxer
-  class KinomapOnebox < OembedOnebox
+module Discourse
+	module Oneboxer
+	  class KinomapOnebox < OembedOnebox
 
-    matcher /^https?:\/\/(?:www\.)?kinomap\.com/
+	    matcher /^https?:\/\/(?:www\.)?kinomap\.com/
 
-    def oembed_endpoint
-      "http://www.kinomap.com/oembed?url=#{BaseOnebox.uriencode(@url)}&format=json"
-    end
+	    def oembed_endpoint
+	      "http://www.kinomap.com/oembed?url=#{BaseOnebox.uriencode(@url)}&format=json"
+	    end
 
 
-  end
+	  end
+	end
 end
