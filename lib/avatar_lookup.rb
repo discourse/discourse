9c243aa9ce10685e64a4f089aa9c10e0cfce4b96
Stephan Kaag <stephan@ka.ag>
2013-07-12 23:07:54 +0200

    Simplify avatar lookup class

--- lib/avatar_lookup.rb
+++ lib/avatar_lookup.rb
@@ -1,24 +1,19 @@
 class AvatarLookup
-  attr_accessor :user_ids, :users
 
   def initialize(user_ids=[])
-    self.user_ids = AvatarLookup.filtered_users(user_ids)
+    @user_ids = user_ids.tap(&:compact!).tap(&:uniq!)
   end
 
   # Lookup a user by id
   def [](user_id)
-    self.users = AvatarLookup.hashed_users(user_ids) if self.users.nil?
-    self.users[user_id]
+    users[user_id]
   end
 
   private
-  def self.filtered_users(user_ids=[])
-    user_ids.flatten.tap(&:compact!).tap(&:uniq!)
-  end
 
-  def self.hashed_users(user_ids=[])
-    users = User.where(:id => user_ids).select([:id, :email, :username])
-    users_with_ids = users.collect {|x| [x.id, x] }.flatten
-    Hash[*users_with_ids]
+  def users
+    @users ||= User.where(:id => @user_ids).select([:id, :email, :username]).inject({}) do |hash, user|
+      hash.merge({user.id => user})
+    end
   end
 end
