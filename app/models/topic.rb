0e3b8fbb24095d61e7dfb1f3573f996d82a19d1f
Stephan Kaag <stephan@ka.ag>
2013-07-22 20:44:11 +0200

    Remove some calls to `all`. They are not required, and Rails4 raises warnings about them.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -253,7 +253,6 @@ class Topic < ActiveRecord::Base
          .listable_topics
          .limit(SiteSetting.max_similar_results)
          .order('similarity desc')
-         .all
   end
 
   def update_status(status, enabled, user)
