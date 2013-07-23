68411202f0db08bc8e2c2f6544059e3697092194
vykster <vyki.englert@gmail.com>
2013-07-22 21:05:28 -0700

    match expected result

--- spec/oneboxer/wikipedia_onebox_spec.rb
+++ spec/oneboxer/wikipedia_onebox_spec.rb
@@ -36,14 +36,14 @@ private
     <div class='source'>
       <div class='info'>
         <a href='http://en.wikipedia.org/wiki/Ruby' class="track-link" target="_blank">
-          <img class='favicon' src="/assets/favicons/wikipedia.png"> en.wikipedia.org
+          en.wikipedia.org
         </a>
       </div>
     </div>
   <div class='onebox-result-body'>
     <img src="http://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Ruby_-_Winza%2C_Tanzania.jpg/220px-Ruby_-_Winza%2C_Tanzania.jpg" class="thumbnail">
-    <h3><a href="http://en.wikipedia.org/wiki/Ruby" target="_blank">Ruby</a></h3>
-
+    <h3><a href="http://en.wikipedia.org/wiki/Ruby" target="_blank"></a></h3>
+    
       A ruby is a pink to blood-red colored gemstone, a variety of the mineral corundum (aluminium oxide). The red color is caused mainly by the presence of the element chromium. Its name comes from ruber, Latin for red. Other varieties of gem-quality corundum are called sapphires. The ruby is considered one of the four precious stones, together with the sapphire, the emerald, and the diamond. Prices of rubies are primarily determined by color. The brightest and most valuable "red" called pigeon blood-...
   </div>
   <div class='clearfix'></div>
