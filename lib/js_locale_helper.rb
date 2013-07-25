e922f10f4e9e688f2fa0f9c6b9374909d9d7854d
Sam <sam.saffron@gmail.com>
2013-07-25 11:16:07 +1000

    correct locale helper so it falls back to english message format if mf is missing

--- lib/js_locale_helper.rb
+++ lib/js_locale_helper.rb
@@ -50,7 +50,11 @@ module JsLocaleHelper
     formats = message_formats.map{|k,v| k.inspect << " : " << compile_message_format(locale_str ,v)}.join(" , ")
 
     result = "MessageFormat = {locale: {}};\n"
-    result << File.read(Rails.root + "lib/javascripts/locale/#{locale_str}.js") << "\n"
+
+    filename = Rails.root + "lib/javascripts/locale/#{locale_str}.js"
+    filename = Rails.root + "lib/javascripts/locale/en.js" unless File.exists?(filename)
+
+    result << File.read(filename) << "\n"
 
     result << "I18n.messageFormat = (function(formats){
       var f = formats;
