d7d3be1130c665cc7fab9f05dbf32335229137a6
James Anderson <me@jamesaanderson.com>
2013-07-25 19:03:23 -0700

    Add audio onebox http://meta.discourse.org/t/audio-html5-tag/8168

--- lib/oneboxer/video_onebox.rb
+++ lib/oneboxer/video_onebox.rb
@@ -3,7 +3,7 @@ require_dependency 'oneboxer/base_onebox'
 module Oneboxer
   class VideoOnebox < BaseOnebox
     
-    matcher /^https?:\/\/.*\.(mov|mp4|ogg)$/
+    matcher /^https?:\/\/.*\.(mov|mp4)$/
 
     def onebox
       "<video width='100%' height='100%' controls><source src='#{@url}'><a href='#{@url}'>#{@url}</a></video>"
