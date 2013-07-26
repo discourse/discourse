234474af1b75d5e23465cf4907b56a617a778740
Neil Lalonde <neillalonde@gmail.com>
2013-07-26 10:31:56 -0400

    Fix 'undefined' in Create Category modal title

--- app/assets/javascripts/discourse/controllers/edit_category_controller.js
+++ app/assets/javascripts/discourse/controllers/edit_category_controller.js
@@ -23,12 +23,12 @@ Discourse.EditCategoryController = Discourse.ObjectController.extend(Discourse.M
 
   title: function() {
     if (this.get('id')) {
-      return I18n.t("category.edit_long") + ": " + this.get('model.name');
+      return I18n.t("category.edit_long") + " : " + this.get('model.name');
     }
     if (this.get('isUncategorized')){
       return I18n.t("category.edit_uncategorized");
     }
-    return I18n.t("category.create") + " : " + this.get('model.name');
+    return I18n.t("category.create") + (this.get('model.name') ? (" : " + this.get('model.name')) : '');
   }.property('id', 'model.name'),
 
   titleChanged: function() {
