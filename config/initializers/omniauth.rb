2162e3bbb53d59b0207173dcdc6ee5e3341e1708
Sam <sam.saffron@gmail.com>
2013-08-02 09:47:54 +1000

    we need email scope (also as I edit files I like to keep quoting consistent)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-require 'openssl'
-require 'openid_redis_store'
+require "openssl"
+require "openid_redis_store"
 
 # if you need to test this and are having ssl issues see:
 #  http://stackoverflow.com/questions/6756460/openssl-error-using-omniauth-specified-ssl-path-but-didnt-work
@@ -8,22 +8,22 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :open_id,
            :store => OpenID::Store::Redis.new($redis),
-           :name => 'google',
-           :identifier => 'https://www.google.com/accounts/o8/id',
-           :require => 'omniauth-openid'
+           :name => "google",
+           :identifier => "https://www.google.com/accounts/o8/id",
+           :require => "omniauth-openid"
 
   provider :open_id,
            :store => OpenID::Store::Redis.new($redis),
-           :name => 'yahoo',
-           :identifier => 'https://me.yahoo.com',
-           :require => 'omniauth-openid'
+           :name => "yahoo",
+           :identifier => "https://me.yahoo.com",
+           :require => "omniauth-openid"
 
   Discourse.auth_providers.each do |p|
     if p.type == :open_id
       provider :open_id, {
         :name => p.name,
         :store => OpenID::Store::Redis.new($redis),
-        :require => 'omniauth-openid'
+        :require => "omniauth-openid"
       }.merge(p.options)
     end
   end
@@ -32,7 +32,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #  without it subdomains will not function correctly
   provider :facebook,
            :setup => lambda { |env|
-              strategy = env['omniauth.strategy']
+              strategy = env["omniauth.strategy"]
               strategy.options[:client_id] = SiteSetting.facebook_app_id
               strategy.options[:client_secret] = SiteSetting.facebook_app_secret
            },
@@ -40,20 +40,21 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :twitter,
            :setup => lambda { |env|
-              strategy = env['omniauth.strategy']
+              strategy = env["omniauth.strategy"]
               strategy.options[:consumer_key] = SiteSetting.twitter_consumer_key
               strategy.options[:consumer_secret] = SiteSetting.twitter_consumer_secret
            }
 
   provider :github,
            :setup => lambda { |env|
-              strategy = env['omniauth.strategy']
+              strategy = env["omniauth.strategy"]
               strategy.options[:client_id] = SiteSetting.github_client_id
               strategy.options[:client_secret] = SiteSetting.github_client_secret
-           }
+           },
+           :scope => "email"
 
   provider :browser_id,
-           :name => 'persona'
+           :name => "persona"
 
   provider :cas,
            :host => SiteSetting.cas_hostname
