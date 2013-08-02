996de5db2bbb2818759f45ac43edb9f87e7e80c0
Mark IJbema <markijbema@gmail.com>
2013-08-02 14:54:35 +0200

    Refactored code to split matching, and checking
    now the code to check a valid hash is simple,
    and nicely isolated

--- lib/oneboxer/imgur_onebox.rb
+++ lib/oneboxer/imgur_onebox.rb
@@ -7,24 +7,18 @@ module Oneboxer
     matcher /^https?\:\/\/imgur\.com\/.*$/
 
     def translate_url
-      hash = image_hash_for(@url) or return nil
+      match = @url.match(%r<imgur\.com/(gallery/)?(?<hash>[^/]+)>mi)
 
-      "http://api.imgur.com/2/image/#{URI::encode(hash)}.json"
+      return nil unless valid_hash(match[:hash])
+
+      "http://api.imgur.com/2/image/#{URI::encode(match[:hash])}.json"
     end
 
-    def image_hash_for url
-      # length check, because imgur has their static pages and
-      # prefixes of other lengths
-      # new length seems to be pretty final: http://imgur.com/blog/2013/01/18/more-characters-in-filenames/
-      # ie. 'help', 'removalrequest', 'user/'
-      min_length = 5 # images used to be this long
-      max_length = 7 # this is the length for new images
-      case url
-        $~[:hash]
-      when %r<imgur\.com/(gallery/)?(?<hash>[^/]{#{min_length},#{max_length}})>mi
-      else
-        nil
-      end
+    def valid_hash hash
+      # http://imgur.com/blog/2013/01/18/more-characters-in-filenames/
+      # maximum length of 7 seems pretty final
+      # this check ensures we don't match non-image pages like 'help', 'removalrequest', 'user/'
+      hash and hash.length.between?(5,7)
     end
 
     def onebox
