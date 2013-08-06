91c6e05dee30b4d239e8020521ecf16e8c9a99b5
Mark IJbema <markijbema@gmail.com>
2013-08-06 22:43:51 +0200

    Improved robustness of translate_url method
    Make sure we don't create images for pages like help
    Also brought this object under test

--- lib/discourse/oneboxer/imgur_onebox.rb
+++ lib/discourse/oneboxer/imgur_onebox.rb
@@ -5,23 +5,32 @@ module Discourse
       matcher /^https?\:\/\/imgur\.com\/.*$/
 
       def translate_url
-        m = @url.match(/\/gallery\/(?<hash>[^\/]+)/mi)
-        return "http://api.imgur.com/2/image/#{URI::encode(m[:hash])}.json" if m.present?
+        match = @url.match(%r<imgur\.com/(gallery/)?(?<hash>[^/]+)>mi)
 
-        m = @url.match(/imgur\.com\/(?<hash>[^\/]+)/mi)
-        return "http://api.imgur.com/2/image/#{URI::encode(m[:hash])}.json" if m.present?
+        return nil unless valid_hash(match[:hash])
 
-        nil
+        "http://api.imgur.com/2/image/#{URI::encode(match[:hash])}.json"
+      end
+
+      def valid_hash hash
+        # http://imgur.com/blog/2013/01/18/more-characters-in-filenames/
+        # maximum length of 7 seems pretty final
+        # this check ensures we don't match non-image pages like 'help', 'removalrequest', 'user/'
+        hash and hash.length.between?(5,7)
       end
 
       def onebox
         url = translate_url
         return @url if url.blank?
 
-        parsed = MultiJson.load(open(translate_url).read)
-        image = parsed['image']
-        BaseOnebox.image_html(image['links']['original'], image['image']['caption'], image['links']['imgur_page'])
+        parsed = JSON.parse(open(translate_url).read)
+        image = parsed['image']['links']['original']
+        url   = parsed['image']['image']['caption']
+        title = parsed['image']['links']['imgur_page']
+
+        BaseOnebox.image_html(image, url, title)
       end
+
 
     end
   end
