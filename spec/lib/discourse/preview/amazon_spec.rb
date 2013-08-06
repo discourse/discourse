8b9cbe7ced8c08da162f6518547609a56f927642
jzeta <holla@jzeta.com>
2013-08-05 21:03:22 -0700

    test for amazon image

--- spec/lib/discourse/preview/amazon_spec.rb
+++ spec/lib/discourse/preview/amazon_spec.rb
@@ -2,13 +2,14 @@ require "spec_helper"
 
 describe Discourse::Oneboxer::Preview::Amazon do
   describe "#to_html" do
-    # it "returns template if given valid data" do
-    #   amazon = described_class.new(Nokogiri::HTML(File.read(File.join("spec", "fixtures", "amazon.response"))))
-    #   expect(amazon.to_html).to eq(onebox_view(%|\n<h1>Knit Noro: Accessories: 30 Colorful Little Knits [Hardcover]</h1>\n<h2 class="host">amazon.com</h2>\n<img src="foo.coms" />\n<p>Lorem Ipsum</p>\n<p>Price</p>\n|))
-    # end
     it "returns the product title" do
       amazon = described_class.new(Nokogiri::HTML(File.read(File.join("spec","fixtures","amazon.response"))))
       expect(amazon.to_html).to include("Knit Noro: Accessories: 30 Colorful Little Knits [Hardcover]")
-	end
+	  end
+
+    it "returns the product image" do 
+      amazon = described_class.new(Nokogiri::HTML(File.read(File.join("spec","fixtures","amazon.response"))))
+      expect(amazon.to_html).to eq("http://ecx.images-amazon.com/images/I/61mI3YmHVnL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA300_SH20_OU01_.jpg");
+    end
   end
 end
