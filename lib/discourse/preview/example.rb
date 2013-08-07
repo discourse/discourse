49d56565f2767aebd5cd5a78edfbbf07a80f0bff
vykster <vyki.englert@gmail.com>
2013-08-06 21:33:49 -0700

    remove Discourse namespace from oneboxers

--- lib/discourse/preview/example.rb
+++ lib/discourse/preview/example.rb
@@ -1,28 +1,27 @@
-module Discourse
-  module Oneboxer
-    class Preview
-      class Example
-        TEMPLATE = %|<div class="onebox">{{{header}}}</div>|
+module Oneboxer
+  class Preview
+    class Example
+      TEMPLATE = %|<div class="onebox">{{{header}}}</div>|
 
-        def initialize(document, link)
-          @url = link
-          @body = document
-          @data = extracted_data
-          @view = Mustache.render(TEMPLATE, @data)
-        end
+      def initialize(document, link)
+        @url = link
+        @body = document
+        @data = extracted_data
+        @view = Mustache.render(TEMPLATE, @data)
+      end
 
-        def to_html
-          @view
-        end
+      def to_html
+        @view
+      end
 
-        private
+      private
 
-        def extracted_data
-          {
-            header: @body.css("html body h1")
-          }
-        end
+      def extracted_data
+        {
+          header: @body.css("html body h1")
+        }
       end
     end
   end
 end
+
