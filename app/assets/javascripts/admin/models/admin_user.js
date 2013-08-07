b6b30dabd45c7ea325fe6e9e9e08cbb26413f6e4
Neil Lalonde <neillalonde@gmail.com>
2013-08-07 18:18:18 -0400

    Send context of where a user was deleted as a spammer so it can be logged

--- app/assets/javascripts/admin/models/admin_user.js
+++ app/assets/javascripts/admin/models/admin_user.js
@@ -277,7 +277,7 @@ Discourse.AdminUser = Discourse.User.extend({
       "callback": function() {
         Discourse.ajax("/admin/users/" + user.get('id') + '.json', {
           type: 'DELETE',
-          data: {delete_posts: true, block_email: true}
+          data: {delete_posts: true, block_email: true, context: window.location.pathname}
         }).then(function(data) {
           if (data.deleted) {
             bootbox.alert(I18n.t("admin.user.deleted"), function() {
