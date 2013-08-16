007aadca027f5cae5c75ee85fc34e4d58282b190
vykster <vyki.englert@gmail.com>
2013-08-15 23:02:22 -0700

    fix specs for amazon, qik, and stackexchange oneboxes b/c changed engine

--- spec/lib/onebox/engine/amazon_spec.rb
+++ spec/lib/onebox/engine/amazon_spec.rb
@@ -3,37 +3,30 @@ require "spec_helper"
 describe Onebox::Engine::Amazon do
   describe "#to_html" do
     let(:link) { "http://example.com" }
+    let(:amazon) { described_class.new(link).to_html }
+
+    before do
+      fake(link, response("amazon.response"))
+    end
 
     it "returns the product title" do
-      amazon = described_class.new(response("amazon.response"), link)
-      expect(amazon.to_html).to include("Knit Noro: Accessories: 30 Colorful Little Knits [Hardcover]")
+      expect(amazon).to include("Knit Noro: Accessories: 30 Colorful Little Knits [Hardcover]")
     end
 
     it "returns the product image" do
-      amazon = described_class.new(response("amazon.response"), link)
-      expect(amazon.to_html).to include("http://ecx.images-amazon.com/images/I/61mI3YmHVnL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA300_SH20_OU01_.jpg")
+      expect(amazon).to include("61mI3YmHVnL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA300_SH20_OU01_.jpg")
     end
 
     it "returns the product description" do
-      amazon = described_class.new(response("amazon.response"), link)
-      expect(amazon.to_html).to include("Using only the finest natural materials and ecologically sound " +
-                                        "manufacturing processes, Japanese designer Eisaku Noro has been " +
-                                        "producing some of the most extraordinary and popular yarns in the " +
-                                        "world for over 30 years. Hand colored in vivid combinations of " +
-                                        "painterly hues, Noro yarns are as striking to behold as they are " +
-                                        "easy to work with. This follow-up to Knit Noro features 32 small " +
-                                        "projects knitters can complete in a weekend, including Leg Warmers, " +
-                                        "Cabled Mittens, a Lace Flap Hat, and even an iPad cover.")
+      expect(amazon).to include("Using only the finest natural materials and ecologically sound")
     end
 
     it "returns the product price" do
-      amazon = described_class.new(response("amazon.response"), link)
-      expect(amazon.to_html).to include("$18.77")
+      expect(amazon).to include("$18.77")
     end
 
     it "returns the product URL" do
-      amazon = described_class.new(response("amazon.response"), link)
-      expect(amazon.to_html).to include(link)
+      expect(amazon).to include(link)
     end
 
   end
--- spec/lib/onebox/engine/stackexchange_spec.rb
+++ spec/lib/onebox/engine/stackexchange_spec.rb
@@ -3,20 +3,22 @@ require "spec_helper"
 describe Onebox::Engine::StackExchange do
   describe "#to_html" do
     let(:link) { "http://stackexchange.com" }
+    let(:stackexchange) { described_class.new(link).to_html }
+
+    before do
+      fake(link, response("stackexchange.response"))
+    end
 
     it "returns the question title" do
-      stackexchange = described_class.new(response("stackexchange.response"), link)
-      expect(stackexchange.to_html).to include("Concept behind these 4 lines of tricky C++ code")
+      expect(stackexchange).to include("Concept behind these 4 lines of tricky C++ code")
     end
 
     it "returns the question" do
-      stackexchange = described_class.new(response("stackexchange.response"), link)
-      expect(stackexchange.to_html).to include("Why does this code gives output C++Sucks? Can anyone explain the concept behind it?")
+      expect(stackexchange).to include("Why does this code gives output C++Sucks? Can anyone explain the concept behind it?")
     end
 
     it "returns the question URL" do
-      stackexchange = described_class.new(response("stackexchange.response"), link)
-      expect(stackexchange.to_html).to include(link)
+      expect(stackexchange).to include(link)
     end
   end
 end
