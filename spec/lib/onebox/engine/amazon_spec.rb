c184882aa14d01a031ec8f35d61f945a8181a8db
vykster <vyki.englert@gmail.com>
2013-08-17 16:42:21 -0700

    dry specs, use same name for let assignment in all onebox specs

--- spec/lib/onebox/engine/amazon_spec.rb
+++ spec/lib/onebox/engine/amazon_spec.rb
@@ -3,30 +3,30 @@ require "spec_helper"
 describe Onebox::Engine::Amazon do
   describe "#to_html" do
     let(:link) { "http://example.com" }
-    let(:amazon) { described_class.new(link).to_html }
+    let(:html) { described_class.new(link).to_html }
 
     before do
       fake(link, response("amazon.response"))
     end
 
     it "returns the product title" do
-      expect(amazon).to include("Knit Noro: Accessories: 30 Colorful Little Knits [Hardcover]")
+      expect(html).to include("Knit Noro: Accessories: 30 Colorful Little Knits [Hardcover]")
     end
 
     it "returns the product image" do
-      expect(amazon).to include("61mI3YmHVnL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA300_SH20_OU01_.jpg")
+      expect(html).to include("61mI3YmHVnL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA300_SH20_OU01_.jpg")
     end
 
     it "returns the product description" do
-      expect(amazon).to include("Using only the finest natural materials and ecologically sound")
+      expect(html).to include("Using only the finest natural materials and ecologically sound")
     end
 
     it "returns the product price" do
-      expect(amazon).to include("$18.77")
+      expect(html).to include("$18.77")
     end
 
     it "returns the product URL" do
-      expect(amazon).to include(link)
+      expect(html).to include(link)
     end
 
   end
