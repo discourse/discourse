1d74cc60a1132e560c9ac0eb882960741cec205c
Sam <sam.saffron@gmail.com>
2013-08-19 11:33:38 +1000

    don't try sending emails out in profile mode

--- config/environments/profile.rb
+++ config/environments/profile.rb
@@ -27,8 +27,8 @@ Discourse::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {arguments: '-i'}
+  # we recommend you use mailcatcher https://github.com/sj26/mailcatcher
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
