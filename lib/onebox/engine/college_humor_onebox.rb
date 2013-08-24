6b9ba1b2e72a8ddbafb429f5f3688317349b7979
jzeta <holla@jzeta.com>
2013-08-24 15:41:00 -0700

    create collegehumor engine/spec #62

---
+++ lib/onebox/engine/college_humor_onebox.rb
@@ -0,0 +1,25 @@
+module Onebox
+  module Engine
+    class CollegeHumorOnebox
+      include Engine
+      include OpenGraph
+
+      matches do
+        # /^https?:\/\/www.collegehumor.com\/video\/.*$/
+        find "collegehumor.com"
+      end
+
+      private
+
+      def extracted_data
+        {
+          url: @url,
+          # title: @body.title,
+          # image: @body.images[0],
+          # description: @body.description
+        }
+      end
+    end
+  end
+end
+
---
+++ spec/lib/onebox/engine/college_humor_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+describe Onebox::Engine::CollegeHumorOnebox do
+  let(:link) { "http://collegehumor.com" }
+  let(:html) { described_class.new(link).to_html }
+
+  before do
+    fake(link, response("collegehumor.response"))
+  end
+
+  # it "returns video title" do
+  #   expect(html).to include("The Awesomes: Pilot, Part 1")
+  # end
+
+  # it "returns photo" do
+  #   expect(html).to include("http://ib3.huluim.com/video/60245466?region=US&amp;size=600x400")
+  # end
+
+  # it "returns video description" do
+  #   expect(html).to include("After Mr. Awesome decides to retire and disband The Awesomes")
+  # end
+
+  # it "returns video URL" do
+  #   expect(html).to include("https://secure.hulu.com/embed/0-us7uHJgevua5TeiGwCxQ")
+  # end
+
+  it "returns URL" do
+    expect(html).to include(link)
+  end
+end
