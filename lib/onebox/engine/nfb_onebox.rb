f58fc215f0ffa6b3e106bdf9adbda3bfcb64b739
jzeta <holla@jzeta.com>
2013-08-24 16:25:59 -0700

    create nfb onebox/spec #74

---
+++ lib/onebox/engine/nfb_onebox.rb
@@ -0,0 +1,26 @@
+module Onebox
+  module Engine
+    class NFBOnebox
+      include Engine
+      include OpenGraph
+
+      matches do
+        # /^https?:\/\/(?:www\.)?nfb\.ca\/film\/[-\w]+\/?/
+        find "nfb.ca"
+      end
+
+      private
+
+      def extracted_data
+        {
+          url: @url,
+          # title: @body.title,
+          # image: @body.images[0],
+          # description: @body.description,
+          # video: @body.metadata[:video].first[:_value]
+        }
+      end
+    end
+  end
+end
+
