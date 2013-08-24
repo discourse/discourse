ee5c9eb34d1e59bd6e560593c6122211a4335e2e
jzeta <holla@jzeta.com>
2013-08-24 12:10:43 -0700

    require hulu in engine.rb, add matches block to hulu onebox

--- lib/onebox/engine/hulu_onebox.rb
+++ lib/onebox/engine/hulu_onebox.rb
@@ -1,7 +1,13 @@
 module Onebox
   module Engine
     class HuluOnebox
+      include Engine
       include OpenGraph
+
+      matches do
+        #/^https?\:\/\/www\.hulu\.com\/watch\/.*$/
+        find "hulu.com"
+      end
 
       private
 
