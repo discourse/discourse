32f717420d0f6443e1c672217b5342b9e475644a
Régis Hanol <regis@hanol.fr>
2013-08-26 00:24:24 +0200

    add max_image_height site setting

--- spec/components/image_sizer_spec.rb
+++ spec/components/image_sizer_spec.rb
@@ -4,10 +4,11 @@ require 'image_sizer'
 describe ImageSizer do
 
   before do
-    SiteSetting.expects(:max_image_width).returns(500)
+    SiteSetting.stubs(:max_image_width).returns(500)
+    SiteSetting.stubs(:max_image_height).returns(500)
   end
 
-  it 'returns the same dimensions if the width is less than the maximum' do
+  it 'returns the same dimensions when smaller than the maximums' do
     ImageSizer.resize(400, 200).should == [400, 200]
   end
 
@@ -23,7 +24,7 @@ describe ImageSizer do
     ImageSizer.resize('100', '101').should == [100, 101]
   end
 
-  describe 'when larger than the maximum' do
+  describe 'when larger than the maximum width' do
 
     before do
       @w, @h = ImageSizer.resize(600, 123)
@@ -35,6 +36,35 @@ describe ImageSizer do
 
     it 'resizes the height retaining the aspect ratio' do
       @h.should == 102
+    end
+
+  end
+
+  describe 'when larger than the maximum height' do
+
+    before do
+      @w, @h = ImageSizer.resize(123, 600)
+    end
+
+    it 'returns the maxmimum height if larger than the maximum' do
+      @h.should == 500
+    end
+
+    it 'resizes the width retaining the aspect ratio' do
+      @w.should == 102
+    end
+
+  end
+
+  describe 'when larger than the maximums' do
+
+    before do
+      @w, @h = ImageSizer.resize(533, 800)
+    end
+
+    it 'resizes both dimensions retaining the aspect ratio' do
+      @h.should == 500
+      @w.should == 333
     end
 
   end
