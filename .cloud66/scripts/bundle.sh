26a4ab45afac65f8af8431e27242a121108e58c0
Philip Kallberg <philip@cloud66.com>
2013-09-02 12:35:50 +0100

    update

--- .cloud66/scripts/bundle.sh
+++ .cloud66/scripts/bundle.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 source /var/.cloud66_env
 cd $RAILS_STACK_PATH
-su - postgres && psql -d discourse_prod < pg_dumps/production-image.sql && exit
-bundle exec rake db:test:prepare db:seed_fu
