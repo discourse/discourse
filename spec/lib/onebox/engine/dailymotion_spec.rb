6fe9e4dacd59d8571aa60775db84dc6a0450ef15
jzeta <holla@jzeta.com>
2013-09-02 18:34:11 -0700

    add dailymotion video title #63

--- spec/lib/onebox/engine/dailymotion_spec.rb
+++ spec/lib/onebox/engine/dailymotion_spec.rb
@@ -8,6 +8,10 @@ describe Onebox::Engine::DailymotionOnebox do
     fake(link, response("dailymotion.response"))
   end
 
+  it "returns video title" do
+    expect(html).to include("Two Door Cinema Club - Les Vielles Charrues 2013.")
+  end
+
   it "returns URL" do
     expect(html).to include(link)
   end
