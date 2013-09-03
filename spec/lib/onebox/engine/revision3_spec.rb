541f9f269e003a0f7088ea3d56971719ce57c547
jzeta <holla@jzeta.com>
2013-09-02 17:30:34 -0700

    add revision3 video image #75

--- spec/lib/onebox/engine/revision3_spec.rb
+++ spec/lib/onebox/engine/revision3_spec.rb
@@ -12,6 +12,10 @@ describe Onebox::Engine::Revision3Onebox do
     expect(html).to include("Blue Shark Bites Diver&#39;s Arm")
   end
 
+  it "returns video image" do
+    expect(html).to include("discoverysharks--0029--blue-sharks--medium.thumb.jpg")
+  end
+
   it "returns URL" do
     expect(html).to include(link)
   end
