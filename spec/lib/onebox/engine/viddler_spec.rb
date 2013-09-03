f3306d1dd0b5b35bdbcc83b4255d16cff97bb6d1
jzeta <holla@jzeta.com>
2013-09-02 18:01:18 -0700

    add viddler video title #91

--- spec/lib/onebox/engine/viddler_spec.rb
+++ spec/lib/onebox/engine/viddler_spec.rb
@@ -8,6 +8,10 @@ describe Onebox::Engine::ViddlerOnebox do
     fake(link, response("viddler.response"))
   end
 
+  it "returns video title" do
+    expect(html).to include("Viddler Demo")
+  end
+
   it "returns URL" do
     expect(html).to include(link)
   end
