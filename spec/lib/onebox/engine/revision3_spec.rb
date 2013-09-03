9872a5244e2e1bcb86ead9a4f575e9e6f2718dfa
jzeta <holla@jzeta.com>
2013-09-02 17:26:34 -0700

    add revision3 title #75

--- spec/lib/onebox/engine/revision3_spec.rb
+++ spec/lib/onebox/engine/revision3_spec.rb
@@ -5,7 +5,11 @@ describe Onebox::Engine::Revision3Onebox do
   let(:html) { described_class.new(link).to_html }
 
   before do
-    fake(link, response("collegehumor.response"))
+    fake(link, response("revision3.response"))
+  end
+
+  it "returns video title" do
+    expect(html).to include("Blue Shark Bites Diver&#39;s Arm")
   end
 
   it "returns URL" do
