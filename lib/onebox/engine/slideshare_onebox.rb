ee2373229fd2931f7bfe31fd8dc31740662e030c
vykster <vyki.englert@gmail.com>
2013-09-03 20:13:00 -0700

    create slideshare onebox #78

---
+++ lib/onebox/engine/slideshare_onebox.rb
@@ -0,0 +1,25 @@
+module Onebox
+  module Engine
+    class SlideshareOnebox
+      include Engine
+      include OpenGraph
+
+      matches do
+        # /^https?\:\/\/(www\.)?slideshare\.net\/*\/.*$/
+        find "slideshare.net"
+      end
+
+      private
+
+      def extracted_data
+        {
+          url: @url,
+          title: @body.title,
+          image: @body.images.first,
+          description: @body.description
+        }
+      end
+    end
+  end
+end
+
