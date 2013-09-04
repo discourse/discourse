cb66506a9f364e4525fef8bc606604d501546e41
jzeta <holla@jzeta.com>
2013-09-03 22:20:07 -0700

    add tests for cache storage and expired cache replacement #55

--- spec/lib/onebox_spec.rb
+++ spec/lib/onebox_spec.rb
@@ -8,5 +8,20 @@ describe Onebox do
       cache = preview.cache
       expect(cache).to respond_to(:key?, :store, :fetch)
     end
+
+    it "stores the value in cache if it doesn't exist" do
+      url = "http://www.example.com"
+      preview = Onebox.preview(url)
+      cache = preview.cache
+      expect(cache.key?(url)).to eq(true)
+    end
+
+    it "replaces the cache if the cache is expired" do
+      url = "http://www.example.com"
+      preview = Onebox.preview(url, cache: Moneta.new(:Memory, expires: 100000, serializer: :json))
+      cache = preview.cache
+      expect(cache.fetch(url)).to be(nil)
+    end
+
   end
 end
