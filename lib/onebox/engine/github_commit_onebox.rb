24f0fde531df7e66beee9ab986d9da7dd50c0bd1
vykster <vyki.englert@gmail.com>
2013-09-09 21:54:42 -0700

    create github commit onebox #68

---
+++ lib/onebox/engine/github_commit_onebox.rb
@@ -0,0 +1,25 @@
+module Onebox
+  module Engine
+    class GithubCommitOnebox
+      include Engine
+      include OpenGraph
+
+      matches do
+        # /^https?:\/\/(?:www\.)?github\.com\/[^\/]+\/[^\/]+\/commit\/.+/
+        find "github.com"
+      end
+
+      private
+
+      def extracted_data
+        {
+          url: @url,
+          title: @body.title,
+          image: @body.images.first,
+          description: @body.description,
+          video: @body.metadata[:video].first[:url].first[:_value]
+        }
+      end
+    end
+  end
+end
