85fe9ae1aca7793073af8a8ce3d6afbfb8bb861c
Philip Kallberg <philip@cloud66.com>
2013-09-17 15:11:47 +0100

    update

--- .cloud66/scripts/thin.sh
+++ .cloud66/scripts/thin.sh
@@ -1,6 +1,6 @@
 source /var/.cloud66_env
 cd $RAILS_STACK_PATH
 echo "" >> config/thin.yml
-echo "chdir: <%= ENV['RAILS_STACK_PATH'] %>" >> config/thin.yml
-echo "environment: <%= ENV['RAILS_ENV'] %>" >> config/thin.yml
-echo "log: <%= ENV['RAILS_STACK_PATH/log/thin.log'] %>" >> config/thin.yml
+echo "chdir: $RAILS_STACK_PATH" >> config/thin.yml
+echo "environment: $RAILS_ENV" >> config/thin.yml
+echo "log: $RAILS_STACK_PATH/log/thin.log" >> config/thin.yml
