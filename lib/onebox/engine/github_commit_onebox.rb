56757844bdf0ea16e65db7345ef9b9456830719d
jzeta <holla@jzeta.com>
2013-09-16 22:33:22 -0700

    refactor github commit onebox to use nokogiri #68

--- lib/onebox/engine/github_commit_onebox.rb
+++ lib/onebox/engine/github_commit_onebox.rb
@@ -2,7 +2,7 @@ module Onebox
   module Engine
     class GithubCommitOnebox
       include Engine
-      include OpenGraph
+      include HTML
 
       matches do
         # /^https?:\/\/(?:www\.)?github\.com\/[^\/]+\/[^\/]+\/commit\/.+/
@@ -14,8 +14,16 @@ module Onebox
       def data
         {
           url: @url,
-          title: raw.title,
-          image: raw.images[1],
+          owner: raw.css(".entry-title .author .url").inner_text,
+          repo: raw.css(".entry-title .js-current-repository").inner_text,
+          sha: raw.css(".sha").inner_text,
+          branch: raw.css(".commit-branches .branches-list .branch a").inner_text,
+          gravatar: raw.css(".gravatar").first["src"],
+          message: raw.css(".commit-title").inner_text,
+          description: raw.css(".commit-desc").inner_text,
+          author: raw.css(".author-name a").inner_text,
+          time_date: raw.css(".js-relative-date").first["title"],
+          diff_stats: raw.css(".details-collapse .explain").inner_text
         }
       end
     end
