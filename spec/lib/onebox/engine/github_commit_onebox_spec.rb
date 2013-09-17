c81adb15f024bdd0938f38c6fd97b8efdf8c796a
jzeta <holla@jzeta.com>
2013-09-16 21:52:59 -0700

    add more github commit spec tests for more onebox details

--- spec/lib/onebox/engine/github_commit_onebox_spec.rb
+++ spec/lib/onebox/engine/github_commit_onebox_spec.rb
@@ -11,15 +11,51 @@ describe Onebox::Engine::GithubCommitOnebox do
   describe "#to_html" do
     let(:html) { described_class.new(link).to_html }
 
-    it "returns repo title" do
+    it "has repo title" do
       expect(html).to include("discourse")
     end
 
-    it "returns user image aka gravatar" do
-      expect(html).to include("gravatar-user-420.png")
+    it "has commit sha" do
+      expect(html).to include("803d023e2307309f8b776ab3b8b7e38ba91c0919")
     end
 
-    it "returns URL" do
+    it "has tag" do
+      expect(html).to include("v0.9.6.3")
+    end
+
+    it "has branch" do
+      expect(html).to include("master")
+    end
+
+    it "has commit author gravatar" do
+      expect(html).to include("2F7d3010c11d08cf990b7614d2c2ca9098.png")
+    end
+
+    it "has commit message" do
+      expect(html).to include("Fixed Github auth")
+    end
+
+    it "has commit author" do
+      expect(html).to include("SamSaffron")
+    end
+
+    it "has commit time and date" do
+      expect(html).to include("2013-08-01 19:03:53")
+    end
+
+    it "has number of files changed" do
+      expect(html).to include("1 changed file")
+    end
+
+    it "has number of additions" do
+      expect(html).to include("18 additions")
+    end
+
+    it "has number of deletions" do
+      expect(html).to include("2 deletions")
+    end
+
+    it "has URL" do
       expect(html).to include(link)
     end
   end
