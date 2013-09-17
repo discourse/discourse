a74aa53ea48e87fe5c5bb171edec2fb52d5d350c
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-09-16 21:27:12 -0700

    Rephrasing for failing tests messages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,15 +16,15 @@ RSpec.configure do |config|
 end
 
 shared_examples_for "an engine" do
-  it "should behave like an engine" do
+  it "has engine behavior" do
     expect(described_class.private_instance_methods).to include(:data, :record, :raw)
   end
 
-  it "should have implemented a data method" do
+  it "has implemented the data functionality" do
     expect { described_class.new(link).send(:data) }.not_to raise_error
   end
 
-  it "should match the matching expression" do
+  it "correctly matches the url" do
     onebox = Onebox::Matcher.new(link).oneboxed
     expect(onebox).to be(described_class)
   end
