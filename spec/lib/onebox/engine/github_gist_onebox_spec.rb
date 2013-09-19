4dd9703054f399a0c4feb02fdffbc5030a47cc0b
jzeta <holla@jzeta.com>
2013-09-18 21:55:29 -0700

    fix github gist spec assignment and response #66

--- spec/lib/onebox/engine/github_gist_onebox_spec.rb
+++ spec/lib/onebox/engine/github_gist_onebox_spec.rb
@@ -1,19 +1,15 @@
 require "spec_helper"
 
 describe Onebox::Engine::GithubGistOnebox do
-  let(:link) { "http://github.com" }
+  let(:link) { "http://gist.github.com" }
   before do
-    fake(link, response("github_gist.response"))
+    fake(link, response("github_gist"))
   end
 
   it_behaves_like "engines"
 
   describe "#to_html" do
     let(:html) { described_class.new(link).to_html }
-
-    it "has repo owner" do
-      expect(html).to include("discourse")
-    end
 
     it "has URL" do
       expect(html).to include(link)
