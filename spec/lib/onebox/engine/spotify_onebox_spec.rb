e00a92e7fc91697e0edbab3ef0361a866a192544
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-09-18 20:48:54 -0700

    We wanted to switch over to the new engine spec

--- spec/lib/onebox/engine/spotify_onebox_spec.rb
+++ spec/lib/onebox/engine/spotify_onebox_spec.rb
@@ -6,7 +6,7 @@ describe Onebox::Engine::SpotifyOnebox do
     fake(link, response("spotify"))
   end
 
-  it_behaves_like "engines"
+  it_behaves_like "an engine"
 
   describe "#to_html" do
     let(:html) { described_class.new(link).to_html }
