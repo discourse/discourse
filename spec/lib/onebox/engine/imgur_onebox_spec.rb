cd4577f33b6faf0faeb54f82e531a0f331b9a8ca
jzeta <holla@jzeta.com>
2013-09-20 22:14:56 -0700

    edit imgur spec to conform to html engine spec format #71

--- spec/lib/onebox/engine/imgur_onebox_spec.rb
+++ spec/lib/onebox/engine/imgur_onebox_spec.rb
@@ -2,13 +2,17 @@ require "spec_helper"
 
 describe Onebox::Engine::ImgurOnebox do
   let(:link) { "http://imgur.com" }
-  let(:html) { described_class.new(link).to_html }
-
   before do
-    fake(link, response("imgur.response"))
+    fake(link, response("imgur"))
   end
 
-  it "returns URL" do
-    expect(html).to include(link)
+  it_behaves_like "engines"
+
+  describe "#to_html" do
+    let(:html) { described_class.new(link).to_html }
+
+    it "returns URL" do
+      expect(html).to include(link)
+    end
   end
 end
