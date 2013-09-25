3734e20d16698dbf807293650d8d24547b5465c1
vykster <vyki.englert@gmail.com>
2013-09-24 22:23:46 -0700

    resolve merge conflicts, silly vyki #76

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,11 +16,7 @@ RSpec.configure do |config|
 end
 
 shared_examples_for "an engine" do
-<<<<<<< HEAD
   it "has engine behavior" do
-=======
-  it "should behave like an engine" do
->>>>>>> Adding behavior testing for the matcher on all engines
     expect(described_class.private_instance_methods).to include(:data, :record, :raw)
   end
 
@@ -28,11 +24,8 @@ shared_examples_for "an engine" do
     expect { described_class.new(link).send(:data) }.not_to raise_error
   end
 
-<<<<<<< HEAD
+
   it "correctly matches the url" do
-=======
-  it "should match the matching expression" do
->>>>>>> Adding behavior testing for the matcher on all engines
     onebox = Onebox::Matcher.new(link).oneboxed
     expect(onebox).to be(described_class)
   end
