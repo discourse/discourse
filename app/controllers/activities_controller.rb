ae6f50d3b46f67577985425e4140953f21a75def
Borislav Roussev <borislav@udacity.com>
2013-09-25 17:31:16 -0700

    Adding activities, tests, API, backend, and everything

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,3 @@
+class ActivitiesController
+  # To change this template use File | Settings | File Templates.
+end
---
+++ app/serializers/activity_detailed_serializer.rb
@@ -0,0 +1,3 @@
+class ActivityDetailedSerializer
+  # To change this template use File | Settings | File Templates.
+end
