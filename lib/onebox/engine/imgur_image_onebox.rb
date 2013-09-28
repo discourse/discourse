1489630902909dfd1232f16b3c15049887f66882
vykster <vyki.englert@gmail.com>
2013-09-28 09:24:04 -0700

    rename imgur onebox to imgur image, and modify test to new format #71

--- lib/onebox/engine/imgur_image_onebox.rb
+++ lib/onebox/engine/imgur_image_onebox.rb
@@ -1,6 +1,6 @@
 module Onebox
   module Engine
-    class ImgurOnebox
+    class ImgurImageOnebox
       include Engine
       include HTML
 
--- spec/lib/onebox/engine/imgur_image_onebox_spec.rb
+++ spec/lib/onebox/engine/imgur_image_onebox_spec.rb
@@ -1,12 +1,13 @@
 require "spec_helper"
 
-describe Onebox::Engine::ImgurOnebox do
-  let(:link) { "http://imgur.com" }
+describe Onebox::Engine::ImgurImageOnebox do
+  let(:link) { "http://imgur.com/gallery/twoDTCU" }
+
   before do
     fake(link, response("imgur_photo"))
   end
 
-  it_behaves_like "engines"
+  it_behaves_like "an engine"
 
   describe "#to_html" do
     let(:html) { described_class.new(link).to_html }
