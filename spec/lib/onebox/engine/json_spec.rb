1c01376c50dd8f1f9e6e0105b16b554e3aa3e115
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-09-28 23:41:37 -0700

    we want to use the class template name for responses.

--- spec/lib/onebox/engine/json_spec.rb
+++ spec/lib/onebox/engine/json_spec.rb
@@ -11,11 +11,19 @@ describe Onebox::Engine::JSON do
 
   describe "#raw" do
 <<<<<<< HEAD
+<<<<<<< HEAD
     class OneboxEngineJson
 =======
     class OneboxEngineJSON
       include Onebox::Engine
 >>>>>>> We need the behavior of engine, renaming classes for clarity.
+=======
+    class OneboxEngineJSON
+      include Onebox::Engine
+=======
+    class OneboxEngineJson
+>>>>>>> we want to use the class template name for responses.
+>>>>>>> we want to use the class template name for responses.
       include Onebox::Engine::JSON
 
       def initialize(link)
@@ -25,10 +33,17 @@ describe Onebox::Engine::JSON do
 
     it "returns a hash" do
 <<<<<<< HEAD
+<<<<<<< HEAD
       object = OneboxEngineJson.new("http://gist.github.com").send(:raw)
 =======
       object = OneboxEngineJSON.new("http://gist.github.com").send(:raw)
 >>>>>>> We need the behavior of engine, renaming classes for clarity.
+=======
+      object = OneboxEngineJSON.new("http://gist.github.com").send(:raw)
+=======
+      object = OneboxEngineJson.new("http://gist.github.com").send(:raw)
+>>>>>>> we want to use the class template name for responses.
+>>>>>>> we want to use the class template name for responses.
       expect(object).to be_a(Hash)
     end
   end
--- spec/lib/onebox/engine/open_graph_spec.rb
+++ spec/lib/onebox/engine/open_graph_spec.rb
@@ -12,9 +12,16 @@ describe Onebox::Engine::OpenGraph do
   describe "#raw" do
     class OneboxEngineOpenGraph
 <<<<<<< HEAD:spec/lib/onebox/engine/open_graph_spec.rb
+<<<<<<< HEAD:spec/lib/onebox/engine/open_graph_spec.rb
 =======
       include Onebox::Engine
 >>>>>>> We need the behavior of engine, renaming classes for clarity.:spec/lib/onebox/engine/opengraph_spec.rb
+=======
+<<<<<<< HEAD:spec/lib/onebox/engine/opengraph_spec.rb
+      include Onebox::Engine
+=======
+>>>>>>> we want to use the class template name for responses.:spec/lib/onebox/engine/open_graph_spec.rb
+>>>>>>> we want to use the class template name for responses.:spec/lib/onebox/engine/open_graph_spec.rb
       include Onebox::Engine::OpenGraph
 
       def initialize(link)
