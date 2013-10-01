fa5360654dae2fe5bb7d81ba906baa8a1d7c2697
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-09-30 21:36:42 -0700

    We need the behavior of engine, renaming classes for clarity.

--- spec/lib/onebox/engine/json_spec.rb
+++ spec/lib/onebox/engine/json_spec.rb
@@ -10,7 +10,12 @@ describe Onebox::Engine::JSON do
   let(:link) { @link }
 
   describe "#raw" do
+<<<<<<< HEAD
     class OneboxEngineJson
+=======
+    class OneboxEngineJSON
+      include Onebox::Engine
+>>>>>>> We need the behavior of engine, renaming classes for clarity.
       include Onebox::Engine::JSON
 
       def initialize(link)
@@ -19,7 +24,11 @@ describe Onebox::Engine::JSON do
     end
 
     it "returns a hash" do
+<<<<<<< HEAD
       object = OneboxEngineJson.new("http://gist.github.com").send(:raw)
+=======
+      object = OneboxEngineJSON.new("http://gist.github.com").send(:raw)
+>>>>>>> We need the behavior of engine, renaming classes for clarity.
       expect(object).to be_a(Hash)
     end
   end
