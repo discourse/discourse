1a0e82dfd018e79fee20aa958b647cfe7fc3c8c6
jzeta <holla@jzeta.com>
2013-09-30 21:23:30 -0700

    create github blob onebox spec #67

---
+++ spec/lib/onebox/engine/github_blob_onebox_spec.rb
@@ -0,0 +1,58 @@
+require "spec_helper"
+
+describe Onebox::Engine::GithubBlobOnebox do
+  let(:link) { "https://github.com/discourse/discourse/blob/964809f73be42d049347aca352760a6f018bbaf9/lib/plugin/instance.rb" }
+  before do
+    fake(link, response("github_blob"))
+  end
+
+  it_behaves_like "an engine"
+
+  describe "#to_html" do
+    let(:html) { described_class.new(link).to_html }
+
+    it "has blob author" do
+      expect(html).to include("jamesaanderson")
+    end
+
+    it "has blob title" do
+      expect(html).to include("Add audio onebox")
+    end
+
+    it "has repo name" do
+      expect(html).to include("discourse")
+    end
+
+    it "has commit author gravatar" do
+      expect(html).to include("b3e9977094ce189bbb493cf7f9adea21")
+    end
+
+    it "has commit description" do
+      expect(html).to include("8168")
+    end
+
+    it "has commit time and date" do
+      expect(html).to include("2013-07-26T02:05:53Z")
+    end
+
+    it "has number of commits" do
+      expect(html).to include("1")
+    end
+
+    it "has number of files changed" do
+      expect(html).to include("4")
+    end
+
+    it "has number of additions" do
+      expect(html).to include("19")
+    end
+
+    it "has number of deletions" do
+      expect(html).to include("1")
+    end
+
+    it "has URL" do
+      expect(html).to include(link)
+    end
+  end
+end
