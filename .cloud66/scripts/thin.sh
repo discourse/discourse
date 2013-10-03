df734d875d5288053a3afad14e2d70daed0e90e8
Philip Kallberg <philip@cloud66.com>
2013-10-03 18:11:50 +0100

    update

--- .cloud66/scripts/thin.sh
+++ .cloud66/scripts/thin.sh
@@ -7,19 +7,9 @@ then
 else
 	source /var/.cloud66_env
     cd $RAILS_STACK_PATH
-    echo "
-address: localhost
-port: 3000
-timeout: 30
-pid: /tmp/web_server.pid
-socket: /tmp/web_server.sock
-max_conns: 1024
-max_persistent_conns: 100
-require: []
-wait: 30
-daemonize: true
-chdir: $RAILS_STACK_PATH
-environment: $RAILS_ENV
-log: $RAILS_STACK_PATH/log/thin.log" > config/thin.yml
+    echo "" >> config/thin.yml
+    echo "chdir: $RAILS_STACK_PATH" >> config/thin.yml
+    echo "environment: $RAILS_ENV" >> config/thin.yml
+    echo "log: $RAILS_STACK_PATH/log/thin.log" >> config/thin.yml
     touch /tmp/thin_done
 fi
