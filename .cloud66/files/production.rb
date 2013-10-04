d344ef874054ea9416f6c12747afed96cf2eec41
Philip Kallberg <philip@cloud66.com>
2013-10-04 11:07:53 +0100

    everything moved to .cloud66

--- .cloud66/files/production.rb
+++ .cloud66/files/production.rb
@@ -30,18 +30,18 @@ Discourse::Application.configure do
 
   # you may use other configuration here for mail eg: sendgrid
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address              => "smtp.sendgrid.net",
-  #   :port                 => 587,
-  #   :domain               => 'YOUR DOMAIN',
-  #   :user_name            => 'YOUR_USER',
-  #   :password             => 'YOUR_PASSWORD',
-  #   :authentication       => 'plain',
-  #   :enable_starttls_auto => true  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address              => ENV['SMTP_ADDRESS'],
+     :port                 => ENV['SMTP_PORT'],
+     :domain               => ENV['SMTP_DOMAIN'],
+     :user_name            => ENV['SMTP_USERNAME'],
+     :password             => ENV['SMTP_PASSWORD'],
+     :authentication       => 'plain',
+     :enable_starttls_auto => true  }
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {arguments: '-i'}
+  #config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.sendmail_settings = {arguments: '-i'}
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
