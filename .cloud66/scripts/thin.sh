27ab4acd6dc609f1c40d7a4a71111fe0dc4f834f
Philip Kallberg <philip@cloud66.com>
2013-10-04 15:53:26 +0100

    thin changes

--- .cloud66/scripts/thin.sh
+++ .cloud66/scripts/thin.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-FILE=/tmp/thin_done
-
-if [ -f $FILE ]
-then
-	echo "File $FILE exists..."
-else
-	source /var/.cloud66_env
-    cd $RAILS_STACK_PATH
-    echo "" >> config/thin.yml
-    echo "chdir: $RAILS_STACK_PATH" >> config/thin.yml
-    echo "environment: $RAILS_ENV" >> config/thin.yml
-    echo "log: $RAILS_STACK_PATH/log/thin.log" >> config/thin.yml
-    touch /tmp/thin_done
-fi
+source /var/.cloud66_env
+cd $RAILS_STACK_PATH
+echo "" >> config/thin.yml
+echo "chdir: $RAILS_STACK_PATH" >> config/thin.yml
+echo "environment: $RAILS_ENV" >> config/thin.yml
+echo "log: $RAILS_STACK_PATH/log/thin.log" >> config/thin.yml
+touch /tmp/thin_done
