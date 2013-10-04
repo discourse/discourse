a06c793e4ca347bbb18d24e7f2ac3e50a9a0e50e
Philip Kallberg <philip@cloud66.com>
2013-10-04 10:21:58 +0100

    moving everything into .cloud66

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-custom_web: bundle exec thin start -C config/thin.yml -e $RACK_ENV -d
+web: bundle exec rails server -p $PORT
 sidekiq: bundle exec sidekiq -e $RAILS_ENV
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,56 +1,47 @@
 Discourse::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+	# Settings specified here will take precedence over those in config/application.rb
 
-  # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+	# In the development environment your application's code is reloaded on
+	# every request.  This slows down response time but is perfect for development
+	# since you don't have to restart the web server when you make code changes.
+	config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true unless rails4?
-  config.eager_load = false if rails4?
+	# Log error messages when you accidentally call methods on nil.
+	config.whiny_nils = true unless rails4?
+	config.eager_load = false if rails4?
 
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+	# Show full error reports and disable caching
+	config.consider_all_requests_local       = true
+	config.action_controller.perform_caching = false
 
-  # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
+	# Print deprecation notices to the Rails logger
+	config.active_support.deprecation = :log
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
+	# Only use best-standards-support built into browsers
+	config.action_dispatch.best_standards_support = :builtin
 
-  # Do not compress assets
-  config.assets.compress = false
+	# Do not compress assets
+	config.assets.compress = false
 
-  # Don't Digest assets, makes debugging uglier
-  config.assets.digest = false
+	# Don't Digest assets, makes debugging uglier
+	config.assets.digest = false
 
-  config.assets.debug = true
+	config.assets.debug = true
 
-  config.watchable_dirs['lib'] = [:rb]
+	config.watchable_dirs['lib'] = [:rb]
 
-  config.sass.debug_info = false
-  config.handlebars.precompile = false
+	config.sass.debug_info = false
+	config.handlebars.precompile = false
 
-  # we recommend you use mailcatcher https://github.com/sj26/mailcatcher
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-	  :address              => ENV['SMTP_ADDRESS'],
-	  :port                 => ENV['SMTP_PORT'],
-	  :domain               => ENV['SMTP_DOMAIN'],
-	  :user_name            => ENV['SMTP_USERNAME'],
-	  :password             => ENV['SMTP_PASSWORD'],
-	  :authentication       => 'plain',
-	  :enable_starttls_auto => true  }
+	# we recommend you use mailcatcher https://github.com/sj26/mailcatcher
+	config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
 
-  config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.raise_delivery_errors = true
 
-  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
+	BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
 
-  config.enable_mini_profiler = true
+	config.enable_mini_profiler = true
 
-  require 'middleware/turbo_dev'
-  config.middleware.insert 0, Middleware::TurboDev
+	require 'middleware/turbo_dev'
+	config.middleware.insert 0, Middleware::TurboDev
 end
-
