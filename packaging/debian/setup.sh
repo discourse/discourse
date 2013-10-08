43ce16a6dc667217c6cb1fa61e3af17d2c16bede
Cyril Rohr <cyril.rohr@gmail.com>
2013-10-08 13:52:29 +0100

    Add script to install mandatory config files from their .sample version.

---
+++ packaging/debian/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This script installs the required example config files before buildpack compilation.
+
+set -ex
+
+cp config/database.yml.production-sample config/database.yml
+cp config/redis.yml.sample config/redis.yml
