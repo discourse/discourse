6c7b34271ca374a67370b2e3cbb79a9bf7810d1e
Philip Kallberg <philip@cloud66.com>
2013-10-08 14:17:18 +0100

    prod smtp

--- .cloud66/files/production.rb
+++ .cloud66/files/production.rb
@@ -32,11 +32,11 @@ Discourse::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-     :address              => <%= ENV['SMTP_ADDRESS'] %>,
-     :port                 => <%= ENV['SMTP_PORT'] %>,
-     :domain               => <%= ENV['SMTP_DOMAIN'] %>,
-     :user_name            => <%= ENV['SMTP_USERNAME'] %>,
-     :password             => <%= ENV['SMTP_PASSWORD'] %>,
+     :address              => ENV['SMTP_ADDRESS'],
+     :port                 => ENV['SMTP_PORT'],
+     :domain               => ENV['SMTP_DOMAIN'],
+     :user_name            => ENV['SMTP_USERNAME'],
+     :password             => ENV['SMTP_PASSWORD'],
      :authentication       => 'plain',
      :enable_starttls_auto => true  }
 
