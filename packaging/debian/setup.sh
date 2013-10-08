4f32b8a9ae1ab1790da955490d06b2d101d0ca43
Cyril Rohr <cyril.rohr@gmail.com>
2013-10-08 15:20:57 +0100

    Copy default production.rb file.

--- packaging/debian/setup.sh
+++ packaging/debian/setup.sh
@@ -5,3 +5,4 @@ set -ex
 
 cp config/database.yml.production-sample config/database.yml
 cp config/redis.yml.sample config/redis.yml
+cp config/environments/production.rb.sample config/environments/production.rb
