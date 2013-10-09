5ca83ba7de20bf6c75be04373743552bc92605f0
vykster <vyki.englert@gmail.com>
2013-10-08 21:04:58 -0700

    create spec for smug mug onebox #79

---
+++ spec/lib/onebox/engine/smugmug_onebox_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe Onebox::Engine::SmugMugOnebox do
+  before(:all) do
+    @link = "http://heyserphoto.smugmug.com/Feathers/Duck-feathers/18516485_5xM7Bj#!i=1179402985&k=kS5HMdV"
+    fake(@link, response(described_class.template_name))
+  end
+  before(:each) { Onebox.defaults.cache.clear }
+
+  let(:link) { @link }
+
+  it_behaves_like "an engine"
+
+  describe "#to_html" do
+    let(:html) { described_class.new(link).to_html }
+
+    it "returns the photographer name" do
+      expect(html).to include("Holly Heyser")
+    end
+
+    it "returns the photo caption" do
+      expect(html).to include("Tail feather from drake mallard")
+    end
+
+    it "returns the product URL" do
+      expect(html).to include(link)
+    end
+  end
+end
