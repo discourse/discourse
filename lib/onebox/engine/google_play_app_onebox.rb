481b96fccda34cfd217f82bd5ec06f4deade17bc
jzeta <holla@jzeta.com>
2013-10-11 20:18:22 -0700

    create google play app onebox engine #57

---
+++ lib/onebox/engine/google_play_app_onebox.rb
@@ -0,0 +1,27 @@
+module Onebox
+  module Engine
+    class GooglePlayAppOnebox
+      include Engine
+      include HTML
+
+      matches do
+        http
+        maybe("www.")
+        domain("amazon")
+        has(".").either("com", "ca").maybe("/")
+      end
+
+      private
+
+      def data
+        {
+          url: @url,
+          name: raw.css("h1").inner_text,
+          image: raw.css("#main-image").first["src"],
+          description: raw.css("#postBodyPS").inner_text,
+          price: raw.css(".priceLarge").inner_text
+        }
+      end
+    end
+  end
+end
