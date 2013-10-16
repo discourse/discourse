a580078ca768d3bf720af471be96fb7b849f94a5
Jeff Atwood <jatwood@discourse.org>
2013-10-16 14:59:33 -0700

    increase default max_post_size to 32k chars

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -34,7 +34,7 @@ class SiteSetting < ActiveRecord::Base
   client_setting(:anon_polling_interval, 30000)
   client_setting(:min_post_length, Rails.env.test? ? 5 : 20)
   client_setting(:min_private_message_post_length, Rails.env.test? ? 5 : 10)
-  client_setting(:max_post_length, 16000)
+  client_setting(:max_post_length, 32000)
   client_setting(:min_topic_title_length, 15)
   client_setting(:max_topic_title_length, 255)
   client_setting(:min_private_message_title_length, 2)
