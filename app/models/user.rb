0eaf32db459200f27abb77469335a2397d565f15
Neil Lalonde <neillalonde@gmail.com>
2013-10-17 15:08:11 -0400

    Add a message to flags created because of the newuser_spam_host_threshold site setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -456,7 +456,7 @@ class User < ActiveRecord::Base
     admin = Discourse.system_user
     topic_links.includes(:post).each do |tl|
       begin
-        PostAction.act(admin, tl.post, PostActionType.types[:spam])
+        PostAction.act(admin, tl.post, PostActionType.types[:spam], message: I18n.t('flag_reason.spam_hosts'))
       rescue PostAction::AlreadyActed
         # If the user has already acted, just ignore it
       end
