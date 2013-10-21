fe0dc8161a05b3168646d2958d4732e9a7d61c90
Erik Ordway <eriko@jumpsuit.org>
2013-10-21 09:28:35 -0700

    Update 20131017014509_add_post_count_to_categories.rb
    In case the category does not have any posts yet. You get a null value error running the migration if this is the case.

--- db/migrate/20131017014509_add_post_count_to_categories.rb
+++ db/migrate/20131017014509_add_post_count_to_categories.rb
@@ -3,7 +3,7 @@ class AddPostCountToCategories < ActiveRecord::Migration
     add_column :categories, :post_count, :integer, null: false, default: 0
     execute <<SQL
     UPDATE categories
-    SET post_count = (SELECT SUM(posts_count) FROM topics
+    SET post_count = (SELECT COALESCE(SUM(posts_count),0) FROM topics
                       WHERE category_id = categories.id AND deleted_at IS NULL)
 SQL
   end
