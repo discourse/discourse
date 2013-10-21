9637751e1400ccf501160dbc2bba75024e5cd176
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-10-20 22:38:45 -0700

    Want to use the right key

--- spec/lib/onebox/view_spec.rb
+++ spec/lib/onebox/view_spec.rb
@@ -2,13 +2,10 @@ require "spec_helper"
 
 describe Onebox::View do
   let(:view) { described_class.new("example", true) }
-  let(:html) { view.to_html({ url: "http://example.com" }) }
+  let(:html) { view.to_html(link: "http://example.com") }
 
   describe "#to_html" do
     it "renders engine partial within layout template" do
-      expect(html).to include(%|class="onebox"|)
-    end
-
       expect(html).to include(%|class="onebox|)
     end
   end
