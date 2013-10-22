6394d924c8b8c82a1660146eb277a3ee8fe5c636
Robin Ward <robin.ward@gmail.com>
2013-10-22 17:51:04 -0400

    Change migration to prevent down

--- db/migrate/20131022151218_create_api_keys.rb
+++ db/migrate/20131022151218_create_api_keys.rb
@@ -1,5 +1,6 @@
 class CreateApiKeys < ActiveRecord::Migration
-  def change
+
+  def up
     create_table :api_keys do |t|
       t.string :key, limit: 64, null: false
       t.integer :user_id, null: true
@@ -13,4 +14,9 @@ class CreateApiKeys < ActiveRecord::Migration
     execute "INSERT INTO api_keys (key, created_at, updated_at) SELECT value, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP FROM site_settings WHERE name = 'api_key'"
     execute "DELETE FROM site_settings WHERE name = 'api_key'"
   end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration.new
+  end
+
 end
