dc920cc2e672d3611690ce677c4f04281097f3fc
Scott Carleton <scott@artsicle.com>
2013-10-22 12:32:00 -0400

    Use existing request object if possible
    Recreating the request object from the env can strip important data
    such as api_key and api_username.  It's still alright for when you
    want to check on the current user or cookeis

--- lib/staff_constraint.rb
+++ lib/staff_constraint.rb
@@ -3,7 +3,7 @@ require_dependency 'current_user'
 class StaffConstraint
 
   def matches?(request)
-    provider = Discourse.current_user_provider.new(request.env)
+    provider = Discourse.current_user_provider.new(request)
     provider.current_user && provider.current_user.staff?
   end
 
