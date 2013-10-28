37e56249be5ad031b98b505769d5869fddff58da
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2013-10-28 19:15:40 +0100

    fixes wrong placement of expect in View Topic integration test

--- test/javascripts/integration/view_topic_test.js
+++ test/javascripts/integration/view_topic_test.js
@@ -1,12 +1,10 @@
 integration("View Topic");
 
 test("Enter a Topic", function() {
+  expect(2);
 
   visit("/t/internationalization-localization/280").then(function() {
-    expect(2);
-
     ok(exists("#topic"), "The was rendered");
     ok(exists("#topic .topic-post"), "The topic has posts");
   });
-
 });
