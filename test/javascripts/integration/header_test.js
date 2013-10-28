76602f635341a0b6a076c622190dd95825507e7c
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2013-10-28 19:01:36 +0100

    fixes wrong placement of expect in header integration test

--- test/javascripts/integration/header_test.js
+++ test/javascripts/integration/header_test.js
@@ -1,14 +1,10 @@
 integration("Header");
 
 test("/", function() {
+  expect(2);
 
   visit("/").then(function() {
-    expect(2);
-
     ok(exists("header"), "The header was rendered");
     ok(exists("#site-logo"), "The logo was shown");
   });
-
 });
-
-
