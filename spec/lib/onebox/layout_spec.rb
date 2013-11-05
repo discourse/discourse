2c31998fd47e0abdffdf598a1485471a64bc8388
jzeta <holla@jzeta.com>
2013-11-04 20:50:50 -0800

    DRY layout spec #171

--- spec/lib/onebox/layout_spec.rb
+++ spec/lib/onebox/layout_spec.rb
@@ -2,6 +2,8 @@ require "spec_helper"
 
 describe Onebox::Layout do
   let(:cache) { Moneta.new(:Memory, expires: true, serializer: :json) }
+  let(:record) { {} }
+  let(:html) { described_class.new("amazon", record, cache).to_html }
 
   describe "#to_html" do
     class OneboxEngineLayout
@@ -13,26 +15,22 @@ describe Onebox::Layout do
     end
 
     it "reads from cache if rendered template is cached" do
-      record = {}
       described_class.new("amazon", record, cache).to_html
       expect(cache).to receive(:fetch)
       described_class.new("amazon", record, cache).to_html
     end
 
     it "stores rendered template if it isn't cached" do
-      record = {}
       expect(cache).to receive(:store)
       described_class.new("wikipedia", record, cache).to_html
     end
 
     it "contains layout template" do
-      record = {}
-      html = described_class.new("amazon", record, cache).to_html
       expect(html).to include(%|class="onebox|)
     end
 
     it "contains the view" do
-      record = { link: "foo"}
+      record = { link: "foo" }
       html = described_class.new("amazon", record, cache).to_html
       expect(html).to include(%|"foo"|)
     end
