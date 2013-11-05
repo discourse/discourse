4234aacde7dc770eaa2e1170c0f79efbbed87a15
jzeta <holla@jzeta.com>
2013-11-04 20:32:09 -0800

    remove tests that no longer apply to view #171

--- spec/lib/onebox/view_spec.rb
+++ spec/lib/onebox/view_spec.rb
@@ -1,12 +1,8 @@
 require "spec_helper"
 
 describe Onebox::View do
-  let(:view) { described_class.new("amazon", true) }
-  let(:html) { view.to_html(link: "http://amazon.com") }
 
   describe "#to_html" do
-    it "renders engine partial within layout template" do
-      expect(html).to include(%|class="onebox|)
-    end
+
   end
 end
