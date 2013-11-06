ad94c5564716f18d8000e6e11853b43ec088d390
jzeta <holla@jzeta.com>
2013-11-05 21:14:05 -0800

    add #match to grab id from url, fix #raw client case, modularize data hash #170

--- lib/onebox/engine/twitter_status_onebox.rb
+++ lib/onebox/engine/twitter_status_onebox.rb
@@ -15,15 +15,67 @@ module Onebox
 
       private
 
+      def match
+        @match ||= @url.match(/twitter\.com\/.+?\/status\/(?<id>\d+)/)
+      end
+
       def client
         Onebox.options.twitter_client
       end
 
       def raw
         if client
-          @raw ||= client.status(id)
+          @raw ||= OpenStruct.new(client.status(match[:id]).to_hash)
         else
           super
+        end
+      end
+
+      def tweet
+        if raw.html?
+          raw.css(".tweet-text").inner_text
+        else
+          raw.tweet
+        end
+      end
+
+      def timestamp
+        if raw.html?
+          raw.css(".metadata span").inner_text
+        else
+          raw.timestamp
+        end
+      end
+
+      def title
+        if raw.html?
+          raw.css(".stream-item-header .username").inner_text
+        else
+          raw.title
+        end
+      end
+
+      def avatar
+        if raw.html?
+          raw.css(".avatar")[2]["src"]
+        else
+          raw.avatar
+        end
+      end
+
+      def favorites
+        if raw.html?
+          raw.css(".stats li .request-favorited-popup").inner_text
+        else
+          raw.favorites
+        end
+      end
+
+      def retweets
+        if raw.html?
+          raw.css(".stats li .request-retweeted-popup").inner_text
+        else
+          raw.retweets
         end
       end
 
@@ -32,12 +84,12 @@ module Onebox
           link: link,
           domain: "http://www.twitter.com",
           badge: "t",
-          tweet: raw.css(".tweet-text").inner_text,
-          timestamp: raw.css(".metadata span").inner_text,
-          title: raw.css(".stream-item-header .username").inner_text,
-          avatar: raw.css(".avatar")[2]["src"],
-          favorites: raw.css(".stats li .request-favorited-popup").inner_text,
-          retweets: raw.css(".stats li .request-retweeted-popup").inner_text
+          tweet: tweet,
+          timestamp: timestamp,
+          title: title,
+          avatar: avatar,
+          favorites: favorites,
+          retweets: retweets
         }
       end
     end
