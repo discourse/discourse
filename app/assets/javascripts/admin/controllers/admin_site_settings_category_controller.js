ca0a8a5970c93128f643d40acaec9ca33ba4e153
Neil Lalonde <neillalonde@gmail.com>
2013-11-14 14:46:17 -0500

    Move save and resetDefault actions to the adminSiteSettingsCategory controller

--- app/assets/javascripts/admin/controllers/admin_site_settings_category_controller.js
+++ app/assets/javascripts/admin/controllers/admin_site_settings_category_controller.js
@@ -14,6 +14,40 @@ Discourse.AdminSiteSettingsCategoryController = Ember.ObjectController.extend({
     } else {
       return Em.A();
     }
-  }.property('controllers.adminSiteSettings.content', 'categoryNameKey')
+  }.property('controllers.adminSiteSettings.content', 'categoryNameKey'),
+
+  actions: {
+
+    /**
+      Reset a setting to its default value
+
+      @method resetDefault
+      @param {Discourse.SiteSetting} setting The setting we want to revert
+    **/
+    resetDefault: function(setting) {
+      setting.set('value', setting.get('default'));
+      setting.save();
+    },
+
+    /**
+      Save changes to a site setting
+
+      @method save
+      @param {Discourse.SiteSetting} setting The setting we've changed
+    **/
+    save: function(setting) {
+      setting.save();
+    },
+
+    /**
+      Cancel changes to a site setting
+
+      @method cancel
+      @param {Discourse.SiteSetting} setting The setting we've changed but want to revert
+    **/
+    cancel: function(setting) {
+      setting.resetValue();
+    }
+  }
 
 });
--- app/assets/javascripts/admin/controllers/admin_site_settings_controller.js
+++ app/assets/javascripts/admin/controllers/admin_site_settings_controller.js
@@ -55,40 +55,6 @@ Discourse.AdminSiteSettingsController = Ember.ArrayController.extend(Discourse.P
     });
 
     this.set('model', matchesGroupedByCategory);
-  }.observes('filter', 'onlyOverridden'),
-
-  actions: {
-
-    /**
-      Reset a setting to its default value
-
-      @method resetDefault
-      @param {Discourse.SiteSetting} setting The setting we want to revert
-    **/
-    resetDefault: function(setting) {
-      setting.set('value', setting.get('default'));
-      setting.save();
-    },
-
-    /**
-      Save changes to a site setting
-
-      @method save
-      @param {Discourse.SiteSetting} setting The setting we've changed
-    **/
-    save: function(setting) {
-      setting.save();
-    },
-
-    /**
-      Cancel changes to a site setting
-
-      @method cancel
-      @param {Discourse.SiteSetting} setting The setting we've changed but want to revert
-    **/
-    cancel: function(setting) {
-      setting.resetValue();
-    }
-  }
+  }.observes('filter', 'onlyOverridden')
 
 });
