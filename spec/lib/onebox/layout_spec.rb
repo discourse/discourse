3dc7ede53fc41c47a61e60e275227f336f0bfe32
jzeta <holla@jzeta.com>
2013-11-18 22:44:10 -0800

    start writing spec for .template_path [#147]

--- spec/lib/onebox/layout_spec.rb
+++ spec/lib/onebox/layout_spec.rb
@@ -3,7 +3,36 @@ require "spec_helper"
 describe Onebox::Layout do
   let(:cache) { Moneta.new(:Memory, expires: true, serializer: :json) }
   let(:record) { {} }
-  let(:html) { described_class.new("amazon", record, cache).to_html }
+  let(:onebox) { described_class.new("amazon", record, cache) }
+  let(:html) { onebox.to_html }
+
+  describe ".template_path" do
+    before(:each) do
+      Onebox.options.load_paths << "directory_b"
+      Onebox.options.load_paths << "directory_c"
+    end
+
+    let(:template_path) { onebox.template_path }
+
+    it "looks in directory C for template" do
+      File.stub(:exist?) do |path|
+        path == "directory_c"
+      end
+      expect(template_path).to eq("directory_c")
+    end
+
+    it "looks in directory B if template doesn't exist in C" do
+      expect(template_path).to eq("directory_b")
+    end
+
+    it "looks in default directory if template doesn't exist in B or C" do
+      expect(template_path).to include("template")
+    end
+
+    after(:each) do
+      Onebox.options.load_paths.pop(2)
+    end
+  end
 
   describe "#to_html" do
     class OneboxEngineLayout
