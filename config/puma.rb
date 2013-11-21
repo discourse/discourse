ee5f35d4e94e70a51bfd5f355d1c0e9a3c427fc6
Cassius Chen <chzsh1995@gmail.com>
2013-11-21 23:00:14 +0800

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,9 @@
 # First, you need to change these below to your situation.
 APP_ROOT = '/home/discourse/discourse'
+num_worker=ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 4
 
 # Second, you can choose how many threads that you are going to run at same time.
-workers 4
+workers "#{num_workers}"
 threads 8,32
 
 # Unless you know what you are changing, do not change them.
