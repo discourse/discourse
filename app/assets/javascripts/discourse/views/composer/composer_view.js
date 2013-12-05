fac57db8804ea0b182c56d61a8a32e9ce1675dde
Kasper Peulen <kasperpeulen@gmail.com>
2013-12-05 18:46:04 +0100

    Add another previewRefreshed trigger that is not debounced
    I need this trigger for the mathjax plugin. The `previewRefreshed` already exists, but this is called in a debounced which causes the mathjax plugin to flicker. I've made a recording of my screen to show the the flickering in the preview, and how it good looks with this pull request:
    http://www.screenr.com/TxiH

--- app/assets/javascripts/discourse/views/composer/composer_view.js
+++ app/assets/javascripts/discourse/views/composer/composer_view.js
@@ -202,7 +202,8 @@ Discourse.ComposerView = Discourse.View.extend(Ember.Evented, {
     };
 
     this.editor.hooks.onPreviewRefresh = function() {
-      return composerView.afterRender();
+      composerView.afterRender();
+      composerView.trigger('previewRefreshed2');
     };
 
     this.editor.run();
