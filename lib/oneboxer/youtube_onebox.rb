7bb23c8ce8fb1c3dab8dc8128ad40d37d4b550f1
Régis Hanol <regis@hanol.fr>
2013-12-06 12:01:30 +0100

    FIX: better handle errors in youtube onebox

--- lib/oneboxer/youtube_onebox.rb
+++ lib/oneboxer/youtube_onebox.rb
@@ -3,12 +3,13 @@ require_dependency 'oneboxer/oembed_onebox'
 module Oneboxer
   class YoutubeOnebox < OembedOnebox
     matcher /^https?:\/\/(?:www\.)?(?:youtube\.com|youtu\.be)\/.+$/
+
     def oembed_endpoint
       "http://www.youtube.com/oembed?url=#{BaseOnebox.uriencode(@url.sub('https://', 'http://'))}&format=json"
     end
 
     def onebox
-      super.each do |entry|
+      (super || []).each do |entry|
         # Youtube allows HTTP and HTTPS, so replace them with the protocol-agnostic variant
         BaseOnebox.replace_agnostic entry
         # Add wmode=opaque to the iframe src URL so that the flash player is rendered within the document flow instead of on top
