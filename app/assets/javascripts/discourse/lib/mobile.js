2596f7dec2716d6b3087b4578e2753b2ab7025ef
Neil Lalonde <neillalonde@gmail.com>
2013-12-09 16:28:11 -0500

    Revert "Stop using user agent to detect mobile devices. Use a media query and yepnope to load the appropriate css and customizations."

--- app/assets/javascripts/discourse/lib/mobile.js
+++ app/assets/javascripts/discourse/lib/mobile.js
@@ -5,19 +5,30 @@
   @module Mobile
 **/
 Discourse.Mobile = {
-
+  isMobileDevice: false,
   mobileView: false,
 
   init: function() {
     var $html = $('html');
+    this.isMobileDevice = $html.hasClass('mobile-device');
     this.mobileView = $html.hasClass('mobile-view');
+
+    if (localStorage && localStorage.mobileView) {
+      var savedValue = (localStorage.mobileView === 'true' ? true : false);
+      if (savedValue !== this.mobileView) {
+        this.reloadPage(savedValue);
+      }
+    }
   },
 
   toggleMobileView: function() {
     if (localStorage) {
       localStorage.mobileView = !this.mobileView;
     }
-    window.location.reload();
-  }
+    this.reloadPage(!this.mobileView);
+  },
 
+  reloadPage: function(mobile) {
+    window.location.assign(window.location.pathname + '?mobile_view=' + (mobile ? '1' : '0'));
+  }
 };
