820e29e30e6c3b683dd362252b347c05246279eb
Sam <sam.saffron@gmail.com>
2013-12-16 12:48:45 +1100

    don't seed welcome topic in test db

--- db/fixtures/999_topics.rb
+++ db/fixtures/999_topics.rb
@@ -2,7 +2,7 @@ User.reset_column_information
 Topic.reset_column_information
 Post.reset_column_information
 
-if Topic.count == 0
+if Topic.count == 0 && !Rails.env.test?
   # seed welcome topic
   puts "Seeding welcome topic"
   welcome = File.read(Rails.root + 'docs/ADMIN-QUICK-START-GUIDE.md')
