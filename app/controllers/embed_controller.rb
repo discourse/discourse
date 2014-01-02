15faa01178c97c730a9a50f490d3a99bfc9997b6
Robin Ward <robin.ward@gmail.com>
2014-01-02 11:55:45 -0500

    FIX: Missing variable

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -25,7 +25,7 @@ class EmbedController < ApplicationController
 
       if !(Rails.env.development? && current_user.try(:admin?))
         raise Discourse::InvalidAccess.new('embeddable host not set') if SiteSetting.embeddable_host.blank?
-        raise Discourse::InvalidAccess.new('invalid referer host') if uri.host != SiteSetting.embeddable_host
+        raise Discourse::InvalidAccess.new('invalid referer host') if URI(request.referer || '').host != SiteSetting.embeddable_host
       end
 
       response.headers['X-Frame-Options'] = "ALLOWALL"
