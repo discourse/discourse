c762e3c4b126ef337ab5a9e9e2842dc2468f2b8e
Robin Ward <robin.ward@gmail.com>
2014-01-03 14:45:22 -0500

    Support for scrolling to another post in the iframe; more complicated than you'd think!

--- public/javascripts/embed.js
+++ public/javascripts/embed.js
@@ -11,6 +11,26 @@
   iframe.scrolling = "no";
   comments.appendChild(iframe);
 
+  // Thanks http://amendsoft-javascript.blogspot.ca/2010/04/find-x-and-y-coordinate-of-html-control.html
+  function findPosY(obj)
+  {
+    var top = 0;
+    if(obj.offsetParent)
+    {
+        while(1)
+        {
+          top += obj.offsetTop;
+          if(!obj.offsetParent)
+            break;
+          obj = obj.offsetParent;
+        }
+    }
+    else if(obj.y)
+    {
+        top += obj.y;
+    }
+    return top;
+  }
 
   function postMessageReceived(e) {
     if (!e) { return; }
@@ -19,6 +39,12 @@
     if (e.data) {
       if (e.data.type === 'discourse-resize' && e.data.height) {
         iframe.height = e.data.height + "px";
+      }
+
+      if (e.data.type === 'discourse-scroll' && e.data.top) {
+        // find iframe offset
+        var destY = findPosY(iframe) + e.data.top;
+        window.scrollTo(0, destY);
       }
     }
   }
