1dc0f34c4645df6a37b49b4e1f4bfaf1b3c07c30
Neil Lalonde <neillalonde@gmail.com>
2014-01-06 13:42:34 -0500

    Remove database.yml from vagrant chef cookbook. Don't need to copy database.yml anymore.

--- chef/cookbooks/discourse/recipes/default.rb
+++ chef/cookbooks/discourse/recipes/default.rb
@@ -1,8 +1,4 @@
 # Install the configuration files we need
-cookbook_file "/vagrant/config/database.yml" do
-  source "database.yml"
-end
-
 cookbook_file "/vagrant/config/redis.yml" do
   source "redis.yml"
 end
