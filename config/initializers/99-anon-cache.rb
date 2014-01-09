f3e587c55e80e3042198462efdd6cd891d074c30
Sam <sam.saffron@gmail.com>
2014-01-09 16:08:59 +1100

    Disable cache for a bit to see if its killing the build

--- config/initializers/99-anon-cache.rb
+++ config/initializers/99-anon-cache.rb
@@ -6,6 +6,9 @@ enabled = if Rails.configuration.respond_to?(:enable_anon_caching)
             Rails.env.production?
           end
 
+# DISABLE to check if its killing the smoke test
+enabled = false
+
 if !ENV['DISCOURSE_DISABLE_ANON_CACHE'] && enabled
   # in an ideal world this is position 0, but mobile detection uses ... session and request and params
   Rails.configuration.middleware.insert_after ActionDispatch::ParamsParser, Middleware::AnonymousCache
