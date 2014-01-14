d1d856988c79bbccda40a7dbf9001d3f971e317b
Robin Ward <robin.ward@gmail.com>
2014-01-14 15:20:46 -0500

    FIX: Allow click tracking to work with different origins

--- test/javascripts/lib/url_test.js
+++ test/javascripts/lib/url_test.js
@@ -1,5 +1,20 @@
 module("Discourse.URL");
 
+test("isInternal with a HTTP url", function() {
+  this.stub(Discourse.URL, "origin").returns("http://eviltrout.com");
+
+  ok(!Discourse.URL.isInternal(null), "a blank URL is not internal");
+  ok(Discourse.URL.isInternal("/test"), "relative URLs are internal");
+  ok(Discourse.URL.isInternal("http://eviltrout.com/tophat"), "a url on the same host is internal");
+  ok(Discourse.URL.isInternal("https://eviltrout.com/moustache"), "a url on a HTTPS of the same host is internal");
+  ok(!Discourse.URL.isInternal("http://twitter.com"), "a different host is not internal");
+});
+
+test("isInternal with a HTTPS url", function() {
+  this.stub(Discourse.URL, "origin").returns("https://eviltrout.com");
+  ok(Discourse.URL.isInternal("http://eviltrout.com/monocle"), "HTTPS urls match HTTP urls");
+});
+
 test("navigatedToHome", function() {
   var fakeListController = { refresh: function() { return true; } };
   var mock = sinon.mock(fakeListController);
