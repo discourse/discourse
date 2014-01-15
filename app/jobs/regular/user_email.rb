26be6874d8a9c55a1c4316dd44845c8ebb244e84
Noah Davis <noahd1@yahoo.com>
2014-01-15 02:27:21 -0500

    add duplication and complexity

--- app/jobs/regular/user_email.rb
+++ app/jobs/regular/user_email.rb
@@ -62,6 +62,72 @@ module Jobs
       Email::Sender.new(message, args[:type], user).send
     end
 
+    def notification_email(user, opts)
+      return unless @notification = opts[:notification]
+      return unless @post = opts[:post]
+
+      username = @notification.data_hash[:display_username]
+      notification_type = opts[:notification_type] || Notification.types[@notification.notification_type].to_s
+
+      context = ""
+      tu = TopicUser.get(@post.topic_id, user)
+
+      context_posts = Post.where(topic_id: @post.topic_id)
+                          .where("post_number < ?", @post.post_number)
+                          .where(user_deleted: false)
+                          .order('created_at desc')
+                          .limit(SiteSetting.email_posts_context)
+
+      if tu && tu.last_emailed_post_number
+        context_posts = context_posts.where("post_number > ?", tu.last_emailed_post_number)
+      end
+
+      # make .present? cheaper
+      context_posts = context_posts.to_a
+
+      if context_posts.present?
+        context << "---\n*#{I18n.t('user_notifications.previous_discussion')}*\n"
+        context_posts.each do |cp|
+          context << email_post_markdown(cp)
+        end
+      end
+
+      html = UserNotificationRenderer.new(Rails.configuration.paths["app/views"]).render(
+        template: 'email/notification',
+        format: :html,
+        locals: { context_posts: context_posts, post: @post }
+      )
+
+      if @post.topic.private_message?
+        opts[:subject_prefix] = "[#{I18n.t('private_message_abbrev')}] "
+      end
+
+      email_opts = {
+        topic_title: @notification.data_hash[:topic_title],
+        message: email_post_markdown(@post),
+        url: @post.url,
+        post_id: @post.id,
+        topic_id: @post.topic_id,
+        context: context,
+        username: username,
+        add_unsubscribe_link: true,
+        allow_reply_by_email: opts[:allow_reply_by_email],
+        template: "user_notifications.user_#{notification_type}",
+        html_override: html,
+        style: :notification,
+        subject_prefix: opts[:subject_prefix] || ''
+      }
+
+      # If we have a display name, change the from address
+      if username.present?
+        email_opts[:from_alias] = username
+      end
+
+      TopicUser.change(user.id, @post.topic_id, last_emailed_post_number: @post.post_number)
+
+      build_email(user.email, email_opts)
+    end
+
     private
 
     # If this email has a related post, don't send an email if it's been deleted or seen recently.
