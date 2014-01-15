9796b045e82bd4fa3115cf3473461da989b3fe50
Noah Davis <noahd1@yahoo.com>
2014-01-15 02:21:16 -0500

    Fix some complexity

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -216,22 +216,6 @@ class PostsController < ApplicationController
       :auto_close_time,
       :auto_track
     ]
-
-    # param munging for WordPress
-    params[:auto_track] = !(params[:auto_track].to_s == "false") if params[:auto_track]
-
-    if api_key_valid?
-      # php seems to be sending this incorrectly, don't fight with it
-      params[:skip_validations] = params[:skip_validations].to_s == "true"
-      permitted << :skip_validations
-    end
-
-    params.require(:raw)
-    params.permit(*permitted).tap do |whitelisted|
-        whitelisted[:image_sizes] = params[:image_sizes]
-        # TODO this does not feel right, we should name what meta_data is allowed
-        whitelisted[:meta_data] = params[:meta_data]
-    end
   end
 
 end
