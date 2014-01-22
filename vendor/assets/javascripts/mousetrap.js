7a82b653869515bc561b0a59db327b3f2379cad2
Régis Hanol <regis@hanol.fr>
2014-01-22 16:52:09 +0100

    BUGFIX: restore Mousetrap's bindGlobal plugin
    cleared deprecated warnings in admin_customize_view

--- vendor/assets/javascripts/mousetrap.js
+++ vendor/assets/javascripts/mousetrap.js
@@ -951,3 +951,54 @@
         define(Mousetrap);
     }
 }) (window, document);
+
+/**
+ * adds a bindGlobal method to Mousetrap that allows you to
+ * bind specific keyboard shortcuts that will still work
+ * inside a text input field
+ *
+ * usage:
+ * Mousetrap.bindGlobal('ctrl+s', _saveChanges);
+ * Mousetrap.unbindGlobal('ctrl+s');
+ */
+/* global Mousetrap:true */
+Mousetrap = (function(Mousetrap) {
+    var _globalCallbacks = {},
+        _originalStopCallback = Mousetrap.stopCallback;
+
+    Mousetrap.stopCallback = function(e, element, combo, sequence) {
+        if (_globalCallbacks[combo] || _globalCallbacks[sequence]) {
+            return false;
+        }
+
+        return _originalStopCallback(e, element, combo);
+    };
+
+    Mousetrap.bindGlobal = function(keys, callback, action) {
+        Mousetrap.bind(keys, callback, action);
+
+        if (keys instanceof Array) {
+            for (var i = 0; i < keys.length; i++) {
+                _globalCallbacks[keys[i]] = true;
+            }
+            return;
+        }
+
+        _globalCallbacks[keys] = true;
+    };
+
+    Mousetrap.unbindGlobal = function(keys, action) {
+        Mousetrap.unbind(keys, action);
+
+        if (keys instanceof Array) {
+            for (var i = 0; i < keys.length; i++) {
+                _globalCallbacks[keys[i]] = false;
+            }
+            return;
+        }
+
+        _globalCallbacks[keys] = false;
+    };
+
+    return Mousetrap;
+}) (Mousetrap);
