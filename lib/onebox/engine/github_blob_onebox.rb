927a06577e94292870e91d79fd0d167636d15188
Robin Ward <robin.ward@gmail.com>
2014-01-24 12:58:54 -0500

    FIX: Github blob parsing should use raw API + truncate when appropriate

--- lib/onebox/engine/github_blob_onebox.rb
+++ lib/onebox/engine/github_blob_onebox.rb
@@ -3,7 +3,6 @@ module Onebox
     class GithubBlobOnebox
       include Engine
       include LayoutSupport
-      include HTML
 
       matches do
         http
@@ -16,13 +15,33 @@ module Onebox
 
       private
 
+      def raw
+        return @raw if @raw
+        m = @url.match(/github\.com\/(?<user>[^\/]+)\/(?<repo>[^\/]+)\/blob\/(?<sha1>[^\/]+)\/(?<file>[^#]+)(#(L(?<from>[^-]*)(-L(?<to>.*))?))?/mi)
+        if m
+          from = (m[:from] || -1).to_i
+          to = (m[:to] || -1).to_i
+          @file = m[:file]
+          contents = open("https://raw.github.com/#{m[:user]}/#{m[:repo]}/#{m[:sha1]}/#{m[:file]}", read_timeout: timeout).read
+          if from > 0
+            if to < 0
+              from = from - 10
+              to = from + 20
+            end
+            if to > from
+              contents = contents.split("\n")[from..to].join("\n")
+            end
+          end
+          if contents.length > 5000
+            contents = contents[0..5000]
+            @truncated = true
+          end
+          @raw = contents
+        end
+      end
+
       def data
-        {
-          link: link,
-          title: raw.css(".final-path").inner_text,
-          lines: raw.css("#files .file .info .mode + span").inner_text,
-          file: raw.css("#files .file .blob-wrapper").inner_text
-        }
+        @data ||= {link: link, content: raw, truncated: @truncated} 
       end
     end
   end
--- spec/lib/onebox/engine/github_blob_onebox_spec.rb
+++ spec/lib/onebox/engine/github_blob_onebox_spec.rb
@@ -3,18 +3,14 @@ require "spec_helper"
 describe Onebox::Engine::GithubBlobOnebox do
   before(:all) do
     @link = "https://github.com/discourse/discourse/blob/master/lib/oneboxer/github_blob_onebox.rb"
+    fake("https://raw.github.com/discourse/discourse/master/lib/oneboxer/github_blob_onebox.rb", response(described_class.onebox_name))
   end
 
   include_context "engines"
-  it_behaves_like "an engine"
 
   describe "#to_html" do
     it "includes file name" do
       expect(html).to include("github_blob_onebox.rb")
-    end
-
-    it "includes number of lines" do
-      expect(html).to include("50")
     end
 
     it "includes blob contents" do
