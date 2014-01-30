f71c8bb533103f0c734a5417bc8115c20112df68
Neil Lalonde <neillalonde@gmail.com>
2014-01-30 18:53:48 -0500

    Revert "Add secure flag to cookie"

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Discourse::Application.config.session_store :cookie_store, {
-  key: '_forum_session',
-  secure: SiteSetting.use_https
-}
+Discourse::Application.config.session_store :cookie_store, key: '_forum_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
