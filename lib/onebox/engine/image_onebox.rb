2fc938733b62e33a2ebe8e7ef1aed8da96d61cf6
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-01 03:26:55 +0530

    Add image onebox.

---
+++ lib/onebox/engine/image_onebox.rb
@@ -0,0 +1,13 @@
+module Onebox
+  module Engine
+    class ImageOnebox
+      include Engine
+
+      matches_regexp /^(https?:)?\/\/.+\.(png|jpg|jpeg|gif|bmp|tif|tiff)$/i
+
+      def to_html
+        "<a href='#{@url} target='_blank'><img src='#{@url}'></a>"
+      end
+    end
+  end
+end
