8ade24ff3b2d3baf678b0f7eeb13abf6d7cb5142
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-08 06:52:09 +0530

    Avoid making HTTP requests for Youtube onebox if possible.

--- lib/onebox/engine/youtube_onebox.rb
+++ lib/onebox/engine/youtube_onebox.rb
@@ -6,8 +6,25 @@ module Onebox
 
       matches_regexp /^https?:\/\/(?:www\.)?(?:youtube\.com|youtu\.be)\/.+$/
 
+      # Try to get the video ID. Works for URLs of the form:
+      # * https://www.youtube.com/watch?v=Z0UISCEe52Y
+      # * http://youtu.be/afyK1HSFfgw
+      def video_id
+        match = @url.match(/^https?:\/\/(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_\-]{11})$/)
+        match && match[3]
+      end
+
       def to_html
-        rewrite_agnostic(append_params(raw[:html]))
+        if video_id
+          # Avoid making HTTP requests if we are able to get the video ID from the
+          # URL.
+          html = "<iframe width=\"480\" height=\"270\" src=\"https://www.youtube.com/embed/#{video_id}?feature=oembed\" frameborder=\"0\" allowfullscreen></iframe>"
+        else
+          # Fall back to making HTTP requests.
+          html = raw[:html]
+        end
+
+        rewrite_agnostic(append_params(html))
       end
 
       def append_params(html)
--- spec/lib/onebox/engine/youtube_onebox_spec.rb
+++ spec/lib/onebox/engine/youtube_onebox_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Onebox::Engine::YoutubeOnebox do
   before do
-    fake("https://www.youtube.com/watch?v=21Lk4YiASMo", response("youtube"))
+    fake("http://www.youtube.com/watch?feature=player_embedded&v=21Lk4YiASMo", response("youtube"))
     fake("http://www.youtube.com/oembed?format=json&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D21Lk4YiASMo", response("youtube-json"))
   end
 
@@ -12,6 +12,16 @@ describe Onebox::Engine::YoutubeOnebox do
 
   it "should rewrite URLs to be agnostic" do
     Onebox.preview('https://www.youtube.com/watch?v=21Lk4YiASMo').to_s.should match(/"\/\//)
+  end
+
+  it "should not make HTTP requests unless necessary" do
+    # We haven't defined any fixture for requests associated with this ID, so if
+    # any HTTP requests are made fakeweb will complain and the test will fail.
+    Onebox.preview('http://www.youtube.com/watch?v=q39Ce3zDScI').to_s
+  end
+
+  it "should not fail if we cannot get the video ID from the URL" do
+    Onebox.preview('http://www.youtube.com/watch?feature=player_embedded&v=21Lk4YiASMo').to_s.should match(/embed/)
   end
 end
 
