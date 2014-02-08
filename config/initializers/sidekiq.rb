87a07e235592858f02bc668abb4d656cc5f63e48
Sam <sam.saffron@gmail.com>
2014-02-08 15:18:54 +1100

    attempt to work around concurrency issue with active record

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,6 +5,14 @@ Sidekiq.configure_server do |config|
 end
 
 if Sidekiq.server?
+
+  # warm up AR
+  RailsMultisite::ConnectionManagement.each_connection do
+    (ActiveRecord::Base.connection.tables - %w[schema_migrations]).each do |table|
+      table.classify.constantize.first rescue nil
+    end
+  end
+
   require 'scheduler/scheduler'
 
   manager = Scheduler::Manager.new
