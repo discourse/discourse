b61b33c0fa2fbb5cbe2ae0c2985e5da8709ca530
Robin Ward <robin.ward@gmail.com>
2014-02-10 12:30:36 -0500

    Add groups list to the poster expansion when you click an avatar while in a topic.

--- app/assets/javascripts/discourse/views/group_index_view.js
+++ app/assets/javascripts/discourse/views/group_index_view.js
@@ -1,4 +1,13 @@
-Discourse.GroupIndexView = Discourse.View.extend(Discourse.LoadMore, {
+/**
+  Displays all posts within a group
+
+  @class Discourse.GroupIndexView
+  @extends Ember.Mixin
+  @namespace Discourse
+  @module Discourse
+**/
+Discourse.GroupIndexView = Discourse.View.extend(Discourse.ScrollTop, Discourse.LoadMore, {
   eyelineSelector: '.user-stream .item'
+
 });
 
