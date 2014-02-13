310a439f3d4320d5af825946805889af2b3cdfca
Régis Hanol <regis@hanol.fr>
2014-02-12 20:34:57 -0800

    update client-side admin routes

--- app/assets/javascripts/admin/routes/admin_routes.js
+++ app/assets/javascripts/admin/routes/admin_routes.js
@@ -5,31 +5,33 @@
   @for Discourse.AdminRoute
 **/
 Discourse.Route.buildRoutes(function() {
-  this.resource('admin', { path: '/admin' }, function() {
+  this.resource('admin', function() {
     this.route('dashboard', { path: '/' });
     this.resource('adminSiteSettings', { path: '/site_settings' }, function() {
       this.resource('adminSiteSettingsCategory', { path: 'category/:category_id'} );
     });
-
 
     this.resource('adminSiteContents', { path: '/site_contents' }, function() {
       this.resource('adminSiteContentEdit', {path: '/:content_type'});
     });
 
     this.resource('adminEmail', { path: '/email'}, function() {
-      this.route('logs', { path: '/logs' });
+      this.route('logs');
       this.route('previewDigest', { path: '/preview-digest' });
     });
 
-    this.route('customize', { path: '/customize' });
-    this.route('api', {path: '/api'});
+    this.route('customize');
+    this.route('api');
+
+    this.resource('admin.backups', { path: '/backups' }, function() {
+      this.route('logs');
+    });
 
     this.resource('adminReports', { path: '/reports/:type' });
 
     this.resource('adminFlags', { path: '/flags' }, function() {
-      this.route('index', { path: '/' });
-      this.route('active', { path: '/active' });
-      this.route('old', { path: '/old' });
+      this.route('active');
+      this.route('old');
     });
 
     this.resource('adminLogs', { path: '/logs' }, function() {
@@ -39,7 +41,7 @@ Discourse.Route.buildRoutes(function() {
       this.route('screenedUrls', { path: '/screened_urls' });
     });
 
-    this.route('groups', {path: '/groups'});
+    this.route('groups');
 
     this.resource('adminUsers', { path: '/users' }, function() {
       this.resource('adminUser', { path: '/:username' }, function() {
@@ -55,5 +57,3 @@ Discourse.Route.buildRoutes(function() {
 
   });
 });
-
-
