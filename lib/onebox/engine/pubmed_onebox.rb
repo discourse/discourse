77218e8ec07a578444bb28c75930adce4d1e65ef
Erick Matsen <matsen@fhcrc.org>
2014-02-15 11:33:32 -0800

    Adding pubmed support.
    Adding support sketch.
    
    It's doing a thing.
    
    Actually have something reasonable.
    
    Making it more like others.
    
    Better authors field.
    
    Adding month and year.
    
    Handling case when Month isn't given.
    
    Adding a response for XML.
    
    Fixed xml FakeWeb route, and cleaned up pubmed speccing code a touch
    
    Refactoring pubmed_onebox

---
+++ lib/onebox/engine/pubmed_onebox.rb
@@ -0,0 +1,56 @@
+module Onebox
+  module Engine
+    class PubmedOnebox
+      include Engine
+      include LayoutSupport
+
+      matches do
+        http
+        maybe("www.")
+        domain("ncbi.nlm.nih")
+        tld("gov")
+        has("/pubmed/")
+      end
+
+      private
+
+      def get_xml
+        doc = Nokogiri::XML(open(@url + "?report=xml&format=text"))
+        pre = doc.xpath("//pre")
+        Nokogiri::XML("<root>" + pre.text + "</root>")
+      end
+
+      def authors_of_xml(xml)
+        initials = xml.css("Initials").map{|x| x.content}
+        last_names = xml.css("LastName").map{|x| x.content}
+        author_list = (initials.zip(last_names)).map{|i,l| i + " " + l}
+        if author_list.length > 1 then
+          author_list[-2] = author_list[-2] + " and " + author_list[-1]
+          author_list.pop
+        end
+        author_list.join(", ")
+      end
+
+      def date_of_xml(xml)
+        (xml.css("PubDate")[0].children).map{|x| x.content}.reverse.join(" ")
+      end
+
+      def data
+         xml = get_xml()
+         {
+         title: xml.css("ArticleTitle")[0].content,
+         authors: authors_of_xml(xml),
+         journal: xml.css("Title")[0].content,
+         abstract: xml.css("AbstractText")[0].content,
+         date: date_of_xml(xml),
+         link: @url,
+         pmid: match[:pmid]
+        }
+      end
+
+      def match
+        @match ||= @url.match(%r{www\.ncbi\.nlm\.nih\.gov/pubmed/(?<pmid>[0-9]+)})
+      end
+    end
+  end
+end
