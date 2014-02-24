de92e9468dafbe1f97554a4eb60c6fc360608835
Allen Hancock <hancock@yesthatallen.com>
2014-02-24 00:01:37 -0600

    Optionally allow discourse to create new topics from email.

--- app/jobs/scheduled/poll_mailbox.rb
+++ app/jobs/scheduled/poll_mailbox.rb
@@ -3,11 +3,14 @@
 #
 require 'net/pop'
 require_dependency 'email/receiver'
+require_dependency 'email/sender'
+require_dependency 'email/message_builder'
 
 module Jobs
   class PollMailbox < Jobs::Scheduled
     every 5.minutes
     sidekiq_options retry: false
+    include Email::BuildEmailHelper
 
     def execute(args)
       if SiteSetting.pop3s_polling_enabled?
@@ -25,6 +28,12 @@ module Jobs
           pop.each do |mail|
             if Email::Receiver.new(mail.pop).process == Email::Receiver.results[:processed]
               mail.delete
+            else
+                @message = Mail::Message.new(mail.pop)
+                # One for you (mod), and one for me (sender)
+                GroupMessage.create(Group[:moderators].name, :email_reject_notification, {limit_once_per: false, message_params: {from: @message.from, body: @message.body}})
+                clientMessage = RejectionMailer.send_rejection(@message.from, @message.body)
+                Email::Sender.new(clientMessage, :email_reject_notification).send
             end
           end
         end
