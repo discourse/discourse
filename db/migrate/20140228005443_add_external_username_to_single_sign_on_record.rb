ca6e24ac82a2d48c93e05d602478f8fd5b8eaa10
Stephen Birarda <commit@birarda.com>
2014-02-27 17:08:15 -0800

    repair the field name in SingleSignOnRecord migration

--- db/migrate/20140228005443_add_external_username_to_single_sign_on_record.rb
+++ db/migrate/20140228005443_add_external_username_to_single_sign_on_record.rb
@@ -1,5 +1,5 @@
 class AddExternalUsernameToSingleSignOnRecord < ActiveRecord::Migration
   def change
-    add_column :single_sign_on_records, :username, :string
+    add_column :single_sign_on_records, :external_username, :string
   end
 end
