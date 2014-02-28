024597e643fb5d70799c28d3a29f8607ee3270ff
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-02-28 13:05:09 +0100

    Switch to proper exception handling system for better user feedback
     - Replace implicit return code-system in Email::Receiver with proper exception system
     - Update tests to check for exceptions instead
     - Test the PollMailbox for expected failures
     - Add proper email-handling of problematic emails
    "

--- app/jobs/scheduled/poll_mailbox.rb
+++ app/jobs/scheduled/poll_mailbox.rb
@@ -18,6 +18,26 @@ module Jobs
       end
     end
 
+    def handle_mail(mail)
+      begin
+        Email::Receiver.new(mail).process
+      rescue Email::Receiver::UserNotSufficientTrustLevelError => e
+        # inform the user about the rejection
+        @message = Mail::Message.new(mail)
+        clientMessage = RejectionMailer.send_trust_level(@message.from, @message.body)
+        email_sender = Email::Sender.new(clientMessage, :email_reject_trust_level)
+        email_sender.send
+      rescue Email::Receiver::ProcessingError
+        # all other ProcessingErrors are ok to be dropped
+      rescue StandardError => e
+        # Inform Admins about error
+        GroupMessage.create(Group[:admins].name, :email_error_notification,
+            {limit_once_per: false, message_params: {source: mail, error: e}})
+      ensure
+        mail.delete
+      end
+    end
+
     def poll_pop3s
       Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
       Net::POP3.start(SiteSetting.pop3s_polling_host,
@@ -26,15 +46,7 @@ module Jobs
                       SiteSetting.pop3s_polling_password) do |pop|
         unless pop.mails.empty?
           pop.each do |mail|
-            if Email::Receiver.new(mail.pop).process == Email::Receiver.results[:processed]
-              mail.delete
-            else
-                @message = Mail::Message.new(mail.pop)
-                # One for you (mod), and one for me (sender)
-                GroupMessage.create(Group[:moderators].name, :email_reject_notification, {limit_once_per: false, message_params: {from: @message.from, body: @message.body}})
-                clientMessage = RejectionMailer.send_rejection(@message.from, @message.body)
-                Email::Sender.new(clientMessage, :email_reject_notification).send
-            end
+            handle_mail mail.pop
           end
         end
       end
