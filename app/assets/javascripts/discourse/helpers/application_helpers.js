73d618c826a45164bd99fe27d68c06636c3d4143
Robin Ward <robin.ward@gmail.com>
2014-03-10 15:02:39 -0400

    Create a cold age helper, make sure the first age column is strictly `created_at`, add cold to the second column.

--- app/assets/javascripts/discourse/helpers/application_helpers.js
+++ app/assets/javascripts/discourse/helpers/application_helpers.js
@@ -16,6 +16,33 @@ Handlebars.registerHelper('breakUp', function(property, hint, options) {
   return new Handlebars.SafeString(Discourse.Formatter.breakUp(prop, hint));
 });
 
+// helper function for dates
+function daysSinceEpoch(dt) {
+  // 1000 * 60 * 60 * 24 = days since epoch
+  return dt.getTime() / 86400000;
+}
+
+/**
+  Converts a date to a coldmap class
+
+  @method coldDate
+**/
+Handlebars.registerHelper('coldAgeClass', function(property, options) {
+  var dt = Em.Handlebars.get(this, property, options);
+
+  if (!dt) { return 'age'; }
+
+  // Show heat on age
+  var nowDays = daysSinceEpoch(new Date()),
+      epochDays = daysSinceEpoch(new Date(dt));
+  if (nowDays - epochDays > 60) return 'age coldmap-high';
+  if (nowDays - epochDays > 30) return 'age coldmap-med';
+  if (nowDays - epochDays > 14) return 'age coldmap-low';
+
+  return 'age';
+});
+
+
 /**
   Truncates long strings
 
