0edcf3304cf325a2f4de526c798e7f8fffb83b99
julien <julien.genestoux@gmail.com>
2014-03-13 20:47:12 +0100

    removed the defaults from code as per @eviltrout's comment

--- lib/pubsubhubbub_hub.rb
+++ lib/pubsubhubbub_hub.rb
@@ -3,8 +3,8 @@ require_dependency 'rest_client'
 module PubSubHubbubHub
 
   def self.ping(urls)
-    hub_url = SiteSetting.pubsubhubbub_hub || 'https://pubsubhubbub.superfeedr.com/'
-    if Rails.env == 'production' || ENV['FORCE_PUSH']
+    hub_url = SiteSetting.pubsubhubbub_hub
+    if hub_url && (Rails.env == 'production' || ENV['FORCE_PUSH'])
       RestClient.post hub_url, :'hub.mode' => 'publish', :'hub.topic' => urls
     end
   end
