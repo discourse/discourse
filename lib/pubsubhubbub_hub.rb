e33f357f5b57955c6720e5e90284d0479e99d585
julien <julien.genestoux@gmail.com>
2014-03-13 12:12:30 +0100

    making sure we only send HTTP notifications in production on when forced by tests. This should fix all the other tests that failed because of FakeWeb::NetConnectNotAllowedError

--- lib/pubsubhubbub_hub.rb
+++ lib/pubsubhubbub_hub.rb
@@ -4,7 +4,9 @@ module PubSubHubbubHub
 
   def self.ping(urls)
     hub_url = SiteSetting.find_by_name('pubsubhubbub_hub').try(:value) || 'https://pubsubhubbub.superfeedr.com/'
-    RestClient.post hub_url, :'hub.mode' => 'publish', :'hub.topic' => urls
+    if Rails.env == 'production' || ENV['FORCE_PUSH']
+      RestClient.post hub_url, :'hub.mode' => 'publish', :'hub.topic' => urls
+    end
   end
 
 end
--- spec/components/pubsubhubbub_hub_spec.rb
+++ spec/components/pubsubhubbub_hub_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 require_dependency 'pubsubhubbub_hub'
 
 describe PubSubHubbubHub do
+  before do
+    ENV['FORCE_PUSH'] = 'true'
+  end
+
+  after do
+    ENV.delete 'FORCE_PUSH'
+  end
+
   describe '#ping' do
     it 'should send a POST request to the settinsg\'s hub with the right params' do
       urls = ['a', 'b', 'c']
