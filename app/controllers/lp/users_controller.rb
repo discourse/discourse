c8e022e32227c7749897a15af77fb7e9f22ebca1
Forest Carlisle <forestcarlisle@gmail.com>
2014-03-14 11:42:12 -0700

    Getting rid of LpUserInfo as it doesn't exist now that we are not using OAuth

--- app/controllers/lp/users_controller.rb
+++ app/controllers/lp/users_controller.rb
@@ -4,18 +4,22 @@ class Lp::UsersController < UsersController
 
     begin
       ActiveRecord::Base.connection.transaction do
-        user = params[:user]
+        user_params            = params[:user]
+        user_params[:name]     = User.suggest_name(user_params[:name] || user_params[:username] || user_params[:email])
+        user_params[:username] = UserNameSuggester.suggest(user_params[:username] || user_params[:name] || user_params[:email])
 
-        user_info = LpUserInfo.where(lp_user_id: user[:id]).first_or_initialize do |u|
-          u.screen_name = UsernameGenerator.generate(user[:email])
+        new_user = User.where(email: Email.downcase(user_params[:email])).first_or_initialize do |u|
+          u.name       = user_params[:name]
+          u.username   = user_params[:username]
+          u.active     = true
+          u.created_at = user_params[:created_at]
+          u.updated_at = user_params[:updated_at]
+          u.bio_raw    = user_params[:user_bio] || ''
         end
 
-        new_user = User.where(email: user[:email]).first_or_initialize
-        new_user.update_attributes! username: user_info.screen_name, active: true, name: "#{user[:firstname]} #{user[:lastname]}".strip, created_at: user[:created_at], updated_at: user[:updated_at], bio_raw: params[:user_bio] || ''
-        user_info.user = new_user
-        user_info.save!
+        new_user.save!
 
-        resp[:user] = { id: new_user.id, old_id: user_info[:lp_user_id], username: new_user.username }
+        resp[:user] = { id: new_user.id, name: new_user.name, username: new_user.username, email: new_user.email }
       end
     rescue Exception => e
       resp[:errors] = { exception: "#{e.class} #{e.message}", backtrace: e.backtrace }
