84e608079b4c960f5141de81da31ec6ec6b1804a
Robin Ward <robin.ward@gmail.com>
2014-03-18 15:56:17 -0400

    Ability to customize the logo in digest emails

--- app/helpers/user_notifications_helper.rb
+++ app/helpers/user_notifications_helper.rb
@@ -26,7 +26,8 @@ module UserNotificationsHelper
   end
 
   def logo_url
-    logo_url = SiteSetting.logo_url
+    logo_url = SiteSetting.digest_logo_url
+    logo_url = SiteSetting.logo_url if logo_url.blank?
     if logo_url !~ /http(s)?\:\/\//
       logo_url = "#{Discourse.base_url}#{logo_url}"
     end
