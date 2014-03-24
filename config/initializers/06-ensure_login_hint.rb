6715786630f9b5630958443b021690a04bc8beac
Jeff Atwood <jatwood@discourse.org>
2014-03-24 00:33:02 -0700

    minor login install hint copyedit

--- config/initializers/06-ensure_login_hint.rb
+++ config/initializers/06-ensure_login_hint.rb
@@ -2,13 +2,13 @@
 if User.limit(20).count < 20 && User.where(admin: true).count == 1
   notice =
     if GlobalSetting.developer_emails.blank?
-      "Welcome to your new Discourse instance! Unfortunately, no administrator emails were defined during setup, so finalizing the configuration <a href='https://meta.discourse.org/t/how-to-create-an-administrator-account-after-install/14046'>may be difficult</a>."
+      "Congratulations, you installed Discourse! Unfortunately, no administrator emails were defined during setup, so finalizing the configuration <a href='https://meta.discourse.org/t/how-to-create-an-administrator-account-after-install/14046'>may be difficult</a>."
     else
       emails = GlobalSetting.developer_emails.split(",")
       if emails.length > 1
         emails = emails[0..-2].join(' , ') << " or #{emails[-1]} "
       end
-      "Welcome to your new Discourse instance! An administrator should now log in with #{emails} to finalize configuration."
+      "Congratulations, you installed Discourse! An administrator should now log in with #{emails} to finalize configuration."
     end
 
   if notice != SiteSetting.global_notice
