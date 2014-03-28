dec644829730fd20cf7c249df512325d18fd2d81
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-03-28 12:39:29 +0530

    Drop opengraph_parser dependency.

--- lib/onebox/engine/standard_embed.rb
+++ lib/onebox/engine/standard_embed.rb
@@ -19,7 +19,7 @@ module Onebox
           end
         end
 
-        open_graph = OpenGraph.new(response.body, false)
+        open_graph = parse_open_graph(html_doc, url)
         if @raw
           @raw[:image] = open_graph.images.first if @raw[:image].nil? && open_graph && open_graph.images
 
@@ -30,6 +30,34 @@ module Onebox
       end
 
       private
+
+      def parse_open_graph(html, url)
+        og = Struct.new(:url, :type, :title, :description, :images, :metadata).new
+        og.url = url
+        og.images = []
+        og.metadata = {}
+
+        attrs_list = %w(title url type description)
+        html.css('meta').each do |m|
+          if m.attribute('property') && m.attribute('property').to_s.match(/^og:/i)
+            m_content = m.attribute('content').to_s.strip
+            m_name = m.attribute('property').to_s.gsub('og:', '')
+            og.metadata[m_name.to_sym] ||= []
+            og.metadata[m_name.to_sym].push m_content
+            if m_name == "image"
+              image_uri = URI.parse(m_content)
+              if image_uri.host.nil?
+                image_uri.host = URI.parse(url).host
+              end
+              og.images.push image_uri.to_s
+            elsif attrs_list.include? m_name
+              og.send("#{m_name}=", m_content) unless m_content.empty?
+            end
+          end
+        end
+
+        og
+      end
 
       def fetch_response(location, limit = 3)
         raise Net::HTTPError.new('HTTP redirect too deep', location) if limit == 0
