d064dd241fee40834dc199b73ba6f28c9f7d919a
Sam <sam.saffron@gmail.com>
2014-04-03 10:39:30 +1100

    BUGFIX: reload site settings on rails reload in dev

--- config/initializers/05-site_settings.rb
+++ config/initializers/05-site_settings.rb
@@ -1,7 +1,17 @@
-RailsMultisite::ConnectionManagement.each_connection do
-  begin
-    SiteSetting.refresh!
-  rescue ActiveRecord::StatementInvalid
-    # This will happen when migrating a new database
+reload_settings = lambda {
+  RailsMultisite::ConnectionManagement.each_connection do
+    begin
+      SiteSetting.refresh!
+    rescue ActiveRecord::StatementInvalid
+      # This will happen when migrating a new database
+    end
+  end
+}
+
+if Rails.configuration.cache_classes
+  reload_settings.call
+else
+  ActionDispatch::Reloader.to_prepare do
+    reload_settings.call
   end
 end
