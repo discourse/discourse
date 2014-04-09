165fd5dbeebaf99b8f7cafa6848497245fb54180
Robin Ward <robin.ward@gmail.com>
2014-04-09 15:55:53 -0400

    Add support for generic video oneboxes

---
+++ lib/onebox/engine/video_onebox.rb
@@ -0,0 +1,14 @@
+module Onebox
+  module Engine
+    class VideoOnebox
+      include Engine
+
+      matches_regexp /^https?:\/\/.*\.(mov|mp4)$/
+
+      def to_html
+        "<video width='100%' height='100%' controls><source src='#{@url}'><a href='#{@url}'>#{@url}</a></video>"
+      end
+    end
+  end
+end
+
