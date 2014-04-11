0c10f2525b73af98603cf551a4d7f4b1bc5143e2
Jason Rust <jason@lessonplanet.com>
2014-04-11 11:55:01 -0700

    Ensure that if a different user signs in to LP it is reflected in community
    We now record the user id from the warden cookie in the discourse session.  This allows to easily check on every request if the user id has changed, and if so, to do the dance.  Removed all the noonce stuff because it was never being set and was a holdover from the OAuth days.

--- app/models/lp_session.rb
+++ app/models/lp_session.rb
@@ -1,57 +1,63 @@
 class LpSession
-  attr_reader :controller, :cookies, :session, :logged_in_forum, :logged_in_lessonplanet
-
   SESSION_COOKIE_NAME = ENV['MAIN_SITE_COOKIE_NAME']
-  NOONCE_COOKIE_NAME  = "#{SESSION_COOKIE_NAME}_noonce"
+
+  class << self
+    def lp_user_id_from_cookie(cookies)
+      cookie = cookies[SESSION_COOKIE_NAME]
+      if cookie.present?
+        # need to decrypt and verify session cookie
+        # to check if use is logged in on lessonplanet.com ain site.
+        unescaped_content = URI.unescape(cookie)
+        secret_key_base   = ENV['SECRET_KEY_BASE']
+        key_generator     = ActiveSupport::KeyGenerator.new(secret_key_base, iterations: 1000)
+        key_generator     = ActiveSupport::CachingKeyGenerator.new(key_generator)
+        secret            = key_generator.generate_key('encrypted cookie')
+        sign_secret       = key_generator.generate_key('signed encrypted cookie')
+        encryptor         = ActiveSupport::MessageEncryptor.new(secret, sign_secret)
+        data              = encryptor.decrypt_and_verify(unescaped_content)
+
+        if data['warden.user.user.key'].present?
+          data['warden.user.user.key'].first.first
+        end
+      end
+    end
+
+    def lp_user_id_from_session(session)
+      session[:lp_user_id]
+    end
+
+    def set_lp_user_id(session, cookies)
+      session[:lp_user_id] = lp_user_id_from_cookie(cookies)
+    end
+  end
+
+  attr_reader :controller, :cookies, :session, :logged_in_forum, :logged_in_lp, :lp_user_id
 
   def initialize(controller)
     @controller      = controller
     @cookies         = controller.send :cookies
     @session         = controller.session
     @logged_in_forum = !!controller.current_user
-
-    @logged_in_lessonplanet = logged_in_lessonplanet?(cookies[SESSION_COOKIE_NAME])
+    @lp_user_id      = self.class.lp_user_id_from_cookie(cookies)
+    @logged_in_lp    = !!lp_user_id
   end
 
   def sync
-    if logged_in_forum && !logged_in_lessonplanet
+    if logged_in_forum && !logged_in_lp
       controller.log_off_user
       controller.redirect_to controller.request.path
-    elsif logged_in_as_different_user? || (!logged_in_forum && logged_in_lessonplanet)
+    elsif (logged_in_forum && logged_in_as_different_user?) || (!logged_in_forum && logged_in_lp)
       dance_sso
     end
   end
 
   private
 
-  def logged_in_lessonplanet?(content)
-    logged_in = false
-
-    if content.present?
-      # need to decrypt and verify session cookie
-      # to check if use is logged in on lessonplanet.com ain site.
-      unescaped_content = URI.unescape(content)
-      secret_key_base   = ENV['SECRET_KEY_BASE']
-      key_generator     = ActiveSupport::KeyGenerator.new(secret_key_base, iterations: 1000)
-      key_generator     = ActiveSupport::CachingKeyGenerator.new(key_generator)
-      secret            = key_generator.generate_key('encrypted cookie')
-      sign_secret       = key_generator.generate_key('signed encrypted cookie')
-      encryptor         = ActiveSupport::MessageEncryptor.new(secret, sign_secret)
-      data              = encryptor.decrypt_and_verify(unescaped_content)
-
-      # if logged in there will be a 'warden.user.user.key' key.
-      logged_in = data['warden.user.user.key'].present?
-    end
-
-    logged_in
-  end
-
   def dance_sso
     controller.redirect_to(controller.session_sso_path(:return_path => controller.request.path))
   end
 
   def logged_in_as_different_user?
-    different_cookies = cookies[NOONCE_COOKIE_NAME].present? && (cookies[NOONCE_COOKIE_NAME] != cookies[:forums_session_nonce])
-    logged_in_forum && logged_in_lessonplanet && different_cookies
+    self.class.lp_user_id_from_session(session) != lp_user_id
   end
 end
--- lib/auth/lp_current_user_provider.rb
+++ lib/auth/lp_current_user_provider.rb
@@ -4,6 +4,7 @@ class Auth::LpCurrentUserProvider < Auth::DefaultCurrentUserProvider
   def log_on_user(user, session, cookies)
     super
     set_cookies(cookies)
+    LpSession.set_lp_user_id(session, cookies)
   end
 
 
@@ -15,14 +16,11 @@ class Auth::LpCurrentUserProvider < Auth::DefaultCurrentUserProvider
   private
 
   def set_cookies(cookies)
-    cookies[:forums_session_nonce] = cookies[LpSession::NOONCE_COOKIE_NAME]
     cookies[LpSession::SESSION_COOKIE_NAME] = { value: '1', domain: cookies_domain } if cookies[LpSession::SESSION_COOKIE_NAME].blank?
   end
 
   def unset_cookies(cookies)
-    cookies.delete(:forums_session_nonce)
-    cookies.delete(LpSession::SESSION_COOKIE_NAME, :domain => cookies_domain)
-    cookies.delete(LpSession::NOONCE_COOKIE_NAME, :domain => cookies_domain)
+    cookies.delete LpSession::SESSION_COOKIE_NAME, domain: cookies_domain
   end
 
   def cookies_domain
