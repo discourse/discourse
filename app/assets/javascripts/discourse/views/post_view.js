8538e31fb4c1bf9d888c8e2eeee9243e932dcc42
Robin Ward <robin.ward@gmail.com>
2014-04-22 15:40:08 -0400

    Don't show link counts in the body of posts for internal links. They are already in the sidebar.

--- app/assets/javascripts/discourse/views/post_view.js
+++ app/assets/javascripts/discourse/views/post_view.js
@@ -137,7 +137,7 @@ Discourse.PostView = Discourse.GroupedView.extend(Ember.Evented, {
 
       self.$(".cooked a[href]").each(function() {
         var link = $(this);
-        if (link.attr('href') === lc.url) {
+        if (!lc.internal && link.attr('href') === lc.url) {
           // don't display badge counts on category badge
           if (link.closest('.badge-category').length === 0 && ((link.closest(".onebox-result").length === 0 && link.closest('.onebox-body').length === 0) || link.hasClass("track-link"))) {
             link.append("<span class='badge badge-notification clicks' title='" +
