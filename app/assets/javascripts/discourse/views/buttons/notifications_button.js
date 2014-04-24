2debf7d97d50b988091f7660f380833445fcf86e
Sam <sam.saffron@gmail.com>
2014-04-24 10:48:04 +1000

    Correct colors for tracking glyph in notification area

--- app/assets/javascripts/discourse/views/buttons/notifications_button.js
+++ app/assets/javascripts/discourse/views/buttons/notifications_button.js
@@ -15,16 +15,20 @@ Discourse.NotificationsButton = Discourse.DropdownButtonView.extend({
   isPrivateMessage: Em.computed.alias('topic.isPrivateMessage'),
   activeItem: Em.computed.alias('topic.details.notification_level'),
 
+  watchingClasses: 'fa fa-exclamation-circle watching',
+  trackingClasses: 'fa fa-circle tracking',
+  mutedClasses: 'fa fa-times-circle muted',
+
   dropDownContent: function() {
     var contents = [], postfix = '';
 
     if (this.get('isPrivateMessage')) { postfix = '_pm'; }
 
     _.each([
-      ['WATCHING', 'watching', 'fa fa-circle heatmap-high'],
-      ['TRACKING', 'tracking', 'fa fa-circle heatmap-low'],
-      ['REGULAR', 'regular', ''],
-      ['MUTED', 'muted', 'fa fa-times-circle']
+      ['WATCHING', 'watching', this.watchingClasses],
+      ['TRACKING', 'tracking', this.trackingClasses],
+      ['REGULAR', 'regular', 'tracking'],
+      ['MUTED', 'muted', this.mutedClasses]
     ], function(pair) {
 
       if (postfix === '_pm' && pair[1] === 'regular') { return; }
@@ -40,6 +44,8 @@ Discourse.NotificationsButton = Discourse.DropdownButtonView.extend({
   }.property(),
 
   text: function() {
+    var self = this;
+
     var key = (function() {
       switch (this.get('topic.details.notification_level')) {
         case Discourse.Topic.NotificationLevel.WATCHING: return 'watching';
@@ -51,9 +57,9 @@ Discourse.NotificationsButton = Discourse.DropdownButtonView.extend({
 
     var icon = (function() {
       switch (key) {
-        case 'watching': return '<i class="fa fa-circle heatmap-high"></i>&nbsp;';
-        case 'tracking': return '<i class="fa fa-circle heatmap-low"></i>&nbsp;';
-        case 'muted': return '<i class="fa fa-times-circle"></i>&nbsp;';
+        case 'watching': return '<i class="' + self.watchingClasses + '"></i>&nbsp;';
+        case 'tracking': return '<i class="' + self.trackingClasses +  '"></i>&nbsp;';
+        case 'muted': return '<i class="' + self.mutedClasses + '"></i>&nbsp;';
         default: return '';
       }
     })();
