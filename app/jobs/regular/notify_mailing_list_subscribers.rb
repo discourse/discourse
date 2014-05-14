b329e23f8511b7248c0e4aee370a9f8a249e1b84
Sam <sam.saffron@gmail.com>
2014-05-14 10:34:28 +1000

    don't attempt to send emails on deleted topics

--- app/jobs/regular/notify_mailing_list_subscribers.rb
+++ app/jobs/regular/notify_mailing_list_subscribers.rb
@@ -6,7 +6,8 @@ module Jobs
       post_id = args[:post_id]
       if post_id
         post = Post.with_deleted.find_by(id: post_id)
-        return if post && post.trashed?
+        # our topic can be deleted as well
+        return if (post && post.trashed?) || !post.topic
       end
 
       raise Discourse::InvalidParameters.new(:post_id) unless post
