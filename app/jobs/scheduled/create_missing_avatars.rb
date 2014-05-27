6e0f54ba9af0b11ed3d57035ecd96db39af17c34
Sam <sam.saffron@gmail.com>
2014-05-27 20:46:17 +1000

    slow down the back fill process a bit

--- app/jobs/scheduled/create_missing_avatars.rb
+++ app/jobs/scheduled/create_missing_avatars.rb
@@ -2,13 +2,16 @@ module Jobs
   class CreateMissingAvatars < Jobs::Scheduled
     every 1.hour
     def execute(args)
-      User.where(uploaded_avatar_id: nil).find_each do |u|
-        u.refresh_avatar
-        u.save
-      end
-
       UserAvatar.where(system_upload_id: nil).find_each do |a|
         a.update_system_avatar!
+      end
+
+      # backfill in batches 1000 an hour
+      User.where(uploaded_avatar_id: nil)
+          .order("last_posted_at desc")
+          .limit(1000).find_each do |u|
+        u.refresh_avatar
+        u.save
       end
     end
   end
---
+++ spec/jobs/create_missing_avatars_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+require_dependency 'jobs/scheduled/create_missing_avatars'
+
+describe Jobs::CreateMissingAvatars do
+  it "runs correctly without crashing" do
+    Jobs::CreateMissingAvatars.new.execute(nil)
+  end
+end
