95159fb82a60ece58cc89f6ade45bac801373613
Sam <sam.saffron@gmail.com>
2014-06-03 17:17:10 +1000

    BUGFIX: Sidekiq could be initialized incorrectly in some cases
    Symptom, no jobs run

--- lib/demon/sidekiq.rb
+++ lib/demon/sidekiq.rb
@@ -19,6 +19,11 @@ class Demon::Sidekiq < Demon::Base
   def after_fork
     STDERR.puts "Loading Sidekiq in process id #{Process.pid}"
     require 'sidekiq/cli'
+    # CLI will close the logger, if we have one set we can be in big
+    # trouble, if STDOUT is closed in our process all sort of weird
+    # will ensue, resetting the logger ensures it will reinit correctly
+    # parent process is in charge of the file anyway.
+    Sidekiq::Logging.logger = nil
     cli = Sidekiq::CLI.instance
     cli.parse(["-c", GlobalSetting.sidekiq_workers.to_s])
 
