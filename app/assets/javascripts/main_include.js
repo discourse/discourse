59ef38bb88098b5b5340040b6d5e13b9ad167df7
Robin Ward <robin.ward@gmail.com>
2014-06-09 15:15:20 -0400

    FIX: Load order for `pagedown-preview`

--- app/assets/javascripts/main_include.js
+++ app/assets/javascripts/main_include.js
@@ -25,6 +25,7 @@
 //= require ./discourse/views/buttons/button_view
 //= require ./discourse/views/buttons/dropdown_button_view
 //= require ./discourse/views/buttons/notifications_button
+//= require ./discourse/views/pagedown-preview
 //= require ./discourse/routes/discourse_route
 //= require ./discourse/routes/discourse_restricted_user_route
 
