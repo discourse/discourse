43496958feea02cae6f04cadf065854d34f66176
Régis Hanol <regis@hanol.fr>
2014-06-17 11:26:36 +0200

    PERF: improve 'has_enough_topics_to_redirect_to_top' for high scale

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -88,7 +88,10 @@ class SiteSetting < ActiveRecord::Base
 
   def self.has_enough_topics_to_redirect_to_top
     TopTopic.periods.each do |period|
-      return true if TopTopic.where("#{period}_score > 0").count >= SiteSetting.topics_per_period_in_top_page
+      topics_per_period = TopTopic.where("#{period}_score > 0")
+                                  .limit(SiteSetting.topics_per_period_in_top_page)
+                                  .count
+      return true if topics_per_period >= SiteSetting.topics_per_period_in_top_page
     end
     # nothing
     false
