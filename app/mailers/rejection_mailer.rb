d2823fc5eeaaa7ded73714ae5f953268402bfdbf
riking <rikingcoding@gmail.com>
2014-06-23 17:16:56 -0700

    More detailed email rejection responses

--- app/mailers/rejection_mailer.rb
+++ app/mailers/rejection_mailer.rb
@@ -3,11 +3,11 @@ require_dependency 'email/message_builder'
 class RejectionMailer < ActionMailer::Base
   include Email::BuildEmailHelper
 
-  def send_rejection(from, body, error)
-    build_email(from, template: 'email_error_notification', error: "#{error.message}\n\n#{error.backtrace.join("\n")}", source: body)
+  def send_rejection(from, body, template, error)
+    build_email(from, template: template, error: error, source: body)
   end
 
-  def send_trust_level(from, body)
+  def send_trust_level(from, template)
     build_email(from, template: 'email_reject_trust_level')
   end
 end
