84ef77066710de39141c9ea44573b456b3349990
Arpit Jalan <arpit@techapj.com>
2014-06-25 19:31:55 +0530

    Fix spec

--- spec/jobs/bulk_invite_spec.rb
+++ spec/jobs/bulk_invite_spec.rb
@@ -6,20 +6,20 @@ describe Jobs::BulkInvite do
   context '.execute' do
 
     it 'raises an error when the filename is missing' do
-      lambda { Jobs::InviteEmail.new.execute(identifier: '46-discoursecsv', chunks: '1') }.should raise_error(Discourse::InvalidParameters)
+      lambda { Jobs::BulkInvite.new.execute(identifier: '46-discoursecsv', chunks: '1') }.should raise_error(Discourse::InvalidParameters)
     end
 
     it 'raises an error when the identifier is missing' do
-      lambda { Jobs::InviteEmail.new.execute(filename: 'discourse.csv', chunks: '1') }.should raise_error(Discourse::InvalidParameters)
+      lambda { Jobs::BulkInvite.new.execute(filename: 'discourse.csv', chunks: '1') }.should raise_error(Discourse::InvalidParameters)
     end
 
     it 'raises an error when the chunks is missing' do
-      lambda { Jobs::InviteEmail.new.execute(filename: 'discourse.csv', identifier: '46-discoursecsv') }.should raise_error(Discourse::InvalidParameters)
+      lambda { Jobs::BulkInvite.new.execute(filename: 'discourse.csv', identifier: '46-discoursecsv') }.should raise_error(Discourse::InvalidParameters)
     end
 
     context 'read csv file' do
       Jobs::BulkInvite.any_instance.stubs(:get_csv_path).with(filename: 'discourse.csv', identifier: '46-discoursecsv', chunks: '1')
-      lambda { Jobs::InviteEmail.new.execute(filename: 'discourse.csv', identifier: '46-discoursecsv', chunks: '1') }
+      Jobs::BulkInvite.new.execute(filename: 'discourse.csv', identifier: '46-discoursecsv', chunks: '1')
 
     end
 
