da9048f3eac318130902c8e0ca1d78c1565523c9
riking <rikingcoding@gmail.com>
2014-06-27 12:03:07 -0700

    Fix email code & tests

--- app/jobs/scheduled/poll_mailbox.rb
+++ app/jobs/scheduled/poll_mailbox.rb
@@ -44,7 +44,7 @@ module Jobs
         if message_template
           # inform the user about the rejection
           message = Mail::Message.new(mail_string)
-          client_message = RejectionMailer.send_rejection(message.from, message.body, message_template.to_s, "#{e.message}\n\n#{e.backtrace.join("\n")}")
+          client_message = RejectionMailer.send_rejection(message.from, message.body, message.to, message_template)
           Email::Sender.new(client_message, message_template).send
         else
           Discourse.handle_exception(e, { context: "incoming email", mail: mail_string })
