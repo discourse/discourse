8395da5bd0619e43a2e8786fb6d631ae703884dc
Arpit Jalan <arpit@techapj.com>
2014-07-10 10:03:09 +0530

    FIX: include topic title and domain name in topic invite mail

--- spec/mailers/invite_mailer_spec.rb
+++ spec/mailers/invite_mailer_spec.rb
@@ -42,6 +42,14 @@ describe InviteMailer do
         expect(invite_mail.subject).to be_present
       end
 
+      it 'renders topic title in subject' do
+        expect(invite_mail.subject).to match(topic.title)
+      end
+
+      it 'renders site domain name in subject' do
+        expect(invite_mail.subject).to match(Discourse.current_hostname)
+      end
+
       it 'renders the body' do
         expect(invite_mail.body).to be_present
       end
