ec280913b22e60a5396636893fd9a185c458c54e
M Smart <mina.smart@thescore.com>
2014-07-22 19:46:43 -0400

    Enumerate deferred scripts with a sprockets helper

---
+++ config/initializers/deferred_javascripts_helper.rb
@@ -0,0 +1,20 @@
+module DeferredJavascriptsHelper
+
+  # Provides a javascript map of the files in the 'defer' directory
+  def deferred_javascripts
+    files = {}
+
+    Dir.glob("#{Rails.root}/app/assets/javascripts/defer/*.js").each do |file|
+      module_name = "defer/#{File.basename(file, '.js')}"
+      file_name = asset_path("defer/#{File.basename(file)}")
+      files[module_name] = file_name
+    end
+
+    return files.to_json.html_safe
+  end
+
+end
+
+module Sprockets::Rails::Helper
+  include DeferredJavascriptsHelper
+end
