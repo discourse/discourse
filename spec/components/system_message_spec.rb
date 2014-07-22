6c2e7294dcb505b430e6182906145cf4d92d5762
Neil Lalonde <neillalonde@gmail.com>
2014-07-22 10:28:10 -0400

    FIX: system_message_spec was failing because max_post_length was exceeded

--- spec/components/system_message_spec.rb
+++ spec/components/system_message_spec.rb
@@ -14,8 +14,11 @@ describe SystemMessage do
     let(:topic) { post.topic }
 
     it 'should create a post correctly' do
+      SiteSetting.stubs(:max_post_length).returns(100_000)
       post.should be_present
+      post.should be_valid
       topic.should be_private_message
+      topic.should be_valid
       topic.subtype.should == TopicSubtype.system_message
       topic.allowed_users.include?(user).should be_true
     end
