a51bf0834674f0db26802252d354d8d219c6af1a
Jeff Atwood <jatwood@codinghorror.com>
2014-08-11 16:08:55 -0700

    change "Sign In" back to "Log In" for now

--- spec/phantom_js/smoke_test.js
+++ spec/phantom_js/smoke_test.js
@@ -96,7 +96,7 @@ page.runTests = function(){
   });
 
   test("expect a log in button", function(){
-    return $('.login-button').text().trim() === 'Sign In';
+    return $('.login-button').text().trim() === 'Log In';
   });
 
   navigate("navigate to first topic", function(){
