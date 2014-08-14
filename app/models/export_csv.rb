8ba8120680514f8bf1b9369f080be656c7ce474b
Arpit Jalan <arpit@techapj.com>
2014-08-15 01:47:52 +0530

    clean up exports weekly

--- app/models/export_csv.rb
+++ app/models/export_csv.rb
@@ -9,6 +9,15 @@ class ExportCsv
     end
   end
 
+  def self.remove_old_exports
+    dir = Dir.new(ExportCsv.base_directory)
+    dir.each do |file|
+      if (File.mtime(File.join(ExportCsv.base_directory, file)) < 2.days.ago)
+        File.delete(File.join(ExportCsv.base_directory, file))
+      end
+    end
+  end
+
   def self.base_directory
     File.join(Rails.root, "public", "uploads", "csv_exports", RailsMultisite::ConnectionManagement.current_db)
   end
