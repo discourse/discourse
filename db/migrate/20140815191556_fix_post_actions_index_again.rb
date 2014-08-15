d02b39c3f5e6536036104672b68341c421d02d1e
Neil Lalonde <neillalonde@gmail.com>
2014-08-15 15:33:45 -0400

    FIX: handle deferred and disagreed post actions

---
+++ db/migrate/20140815191556_fix_post_actions_index_again.rb
@@ -0,0 +1,11 @@
+class FixPostActionsIndexAgain < ActiveRecord::Migration
+  def change
+    remove_index "post_actions", name: "idx_unique_actions"
+    add_index "post_actions",
+                ["user_id", "post_action_type_id",
+                 "post_id", "targets_topic"],
+                name: "idx_unique_actions",
+                unique: true,
+                where: 'deleted_at IS NULL AND disagreed_at IS NULL AND deferred_at IS NULL'
+  end
+end
