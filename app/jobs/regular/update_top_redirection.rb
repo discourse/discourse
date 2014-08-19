20dae0bc0a8ceb201d1e09edd4db81d88cad7b39
Sam <sam.saffron@gmail.com>
2014-08-19 17:26:20 +1000

    FIX: only update column if we have a user

--- app/jobs/regular/update_top_redirection.rb
+++ app/jobs/regular/update_top_redirection.rb
@@ -4,7 +4,9 @@ module Jobs
 
     def execute(args)
       user = User.find_by(id: args[:user_id])
-      user.update_column(:last_redirected_to_top_at, args[:redirected_at])
+      if user
+        user.update_column(:last_redirected_to_top_at, args[:redirected_at])
+      end
     end
   end
 
