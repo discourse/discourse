85317e7658f61bf86ee56ee32d1c67ed119ab767
Sam <sam.saffron@gmail.com>
2014-08-19 20:57:34 +1000

    unhack spec

--- spec/components/sidekiq/pausable_spec.rb
+++ spec/components/sidekiq/pausable_spec.rb
@@ -3,27 +3,9 @@ require_dependency 'sidekiq/pausable'
 
 describe Sidekiq do
   it "can pause and unpause" do
-
-    # Temporary work around
-
-    t = Thread.new do
-      Sidekiq.pause!
-      Sidekiq.paused?.should == true
-      Sidekiq.unpause!
-      Sidekiq.paused?.should == false
-    end
-
-    t2 = Thread.new do
-      sleep 5
-      t.kill
-    end
-
-    t.join
-    if t2.alive?
-      t2.kill
-    else
-      raise "Timed out running sidekiq pause test"
-    end
-
+    Sidekiq.pause!
+    Sidekiq.paused?.should == true
+    Sidekiq.unpause!
+    Sidekiq.paused?.should == false
   end
 end
