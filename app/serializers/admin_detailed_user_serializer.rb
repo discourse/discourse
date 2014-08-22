dafc63e3a0627d7bb253fdf940c575632eb5229f
Neil Lalonde <neillalonde@gmail.com>
2014-08-22 14:37:00 -0400

    Show how many likes users received in the last 100 days on their admin profile page

--- app/serializers/admin_detailed_user_serializer.rb
+++ app/serializers/admin_detailed_user_serializer.rb
@@ -7,6 +7,7 @@ class AdminDetailedUserSerializer < AdminUserSerializer
              :can_revoke_moderation,
              :can_impersonate,
              :like_count,
+             :like_count_last_100_days,
              :post_count,
              :topic_count,
              :flags_given_count,
@@ -50,6 +51,10 @@ class AdminDetailedUserSerializer < AdminUserSerializer
 
   def moderator
     object.moderator
+  end
+
+  def like_count_last_100_days
+    object.like_count_since(100.days.ago)
   end
 
   def topic_count
