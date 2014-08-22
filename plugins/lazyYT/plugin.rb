20b6f720c647be9f62cf2f80ff5391dd1c6b017a
riking <rikingcoding@gmail.com>
2014-08-22 12:21:02 -0700

    Update LazyYT for new onebox version
    Much simpler code!

--- plugins/lazyYT/plugin.rb
+++ plugins/lazyYT/plugin.rb
@@ -16,32 +16,13 @@ class Onebox::Engine::YoutubeOnebox
 
   def to_html
     if video_id
-      # Avoid making HTTP requests if we are able to get the video ID from the
-      # URL.
-      html = "<div class=\"lazyYT\" data-youtube-id=\"#{video_id}\" data-width=\"480\" data-height=\"270\" data-parameters=\"start=0\"></div>"
+      # Put in the LazyYT div instead of the iframe
+      "<div class=\"lazyYT\" data-youtube-id=\"#{video_id}\" data-width=\"480\" data-height=\"270\" data-parameters=\"#{embed_params}\"></div>"
     else
-      # Fall back to making HTTP requests.
-      html = raw[:html] || ""
+      super
     end
-
-    rewrite_agnostic(append_params(html))
   end
 
-  def append_params(html)
-    result = html.dup
-    result.gsub! /(src="[^"]+)/, '\1&wmode=opaque'
-    if url =~ /t=(\d+h)?(\d+m)?(\d+s?)?/
-      h = Regexp.last_match[1].to_i
-      m = Regexp.last_match[2].to_i
-      s = Regexp.last_match[3].to_i
-
-      total = (h * 60 * 60) + (m * 60) + s
-      start_time = "\"start=#{total}\""
-
-      result.gsub!('"start=0"', start_time)
-    end
-    result
-  end
 end
 
 after_initialize do
