1833b43ae2a7b52813e05a94e5178e1cd7028279
riking <rikingcoding@gmail.com>
2014-08-25 15:17:29 -0700

    FEATURE: Badge query validation, preview results, and EXPLAIN
    Upon saving a badge or requesting a badge result preview,
    BadgeGranter.contract_checks! will examine the provided badge SQL for
    some contractual obligations - namely, the returned columns and use of
    trigger parameters.
    
    Saving the badge is wrapped in a transaction to make this easier, by
    raising ActiveRecord::Rollback on a detected violation.
    
    On the client, a modal view is added for the badge query sample run
    results, named admin-badge-preview.
    The preview action is moved up to the route.
    The save action, on failure, triggers a 'saveError' action (also in the
    route).
    
    The preview action gains a new parameter, 'explain', which will give the
    output of an EXPLAIN query for the badge sql, which can be used by forum
    admins to estimate the cost of their badge queries.
    The preview link is replaced by two links, one which omits (false) and
    includes (true) the EXPLAIN query.
    
    The Badge.save() method is amended to propogate errors.
    
    Badge::Trigger gets some utility methods for use in the
    BadgeGranter.contract_checks! method.
    
    Additionally, extra checks outside of BadgeGranter.contract_checks! are
    added in the preview() method, to cover cases of null granted_at
    columns.
    
    An uninitialized variable path is removed in the backfill() method.
    
    TODO - it would be nice to be able to get the actual names of all
    columns the provided query returns, so we could give more errors

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -31,6 +31,18 @@ class Badge < ActiveRecord::Base
     PostRevision = 2
     TrustLevelChange = 4
     UserChange = 8
+
+    def self.is_none?(trigger)
+      [0].include? trigger
+    end
+
+    def self.uses_user_ids?(trigger)
+      [4, 8].include? trigger
+    end
+
+    def self.uses_post_ids?(trigger)
+      [1, 2].include? trigger
+    end
   end
 
   module Queries
