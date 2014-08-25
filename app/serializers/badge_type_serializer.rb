234956234bbb83082759cd1807068b51d7729ae0
riking <rikingcoding@gmail.com>
2014-08-25 12:48:31 -0700

    Fix badge type ordering

--- app/serializers/badge_type_serializer.rb
+++ app/serializers/badge_type_serializer.rb
@@ -1,3 +1,9 @@
 class BadgeTypeSerializer < ApplicationSerializer
-  attributes :id, :name
+  attributes :id, :name, :sort_order
+
+  # change this if/when we allow custom badge types
+  # correct for now, though
+  def sort_order
+    10 - object.id
+  end
 end
