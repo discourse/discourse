0a7a4eae99a76b988f63f708f694e936dd4e0e91
riking <rikingcoding@gmail.com>
2014-08-26 17:58:43 -0700

    Publish lightboxing on the message bus
    With this change, images appear to lightbox instantly on my development
    machine.

--- app/jobs/regular/process_post.rb
+++ app/jobs/regular/process_post.rb
@@ -16,7 +16,16 @@ module Jobs
       cp.post_process(args[:bypass_bump])
 
       # If we changed the document, save it
-      post.update_column(:cooked, cp.html) if cp.dirty?
+      if cp.dirty?
+        post.update_column(:cooked, cp.html)
+
+        MessageBus.publish("/topic/#{post.topic_id}", {
+            type: "revised",
+            id: post.id,
+            updated_at: Time.now,
+            post_number: post.post_number
+        }, group_ids: post.topic.secure_group_ids )
+      end
     end
 
   end
