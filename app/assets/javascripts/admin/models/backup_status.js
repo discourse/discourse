9e2c72e2777d614b9f919d98bdccdb7e41958b12
Robin Ward <robin.ward@gmail.com>
2014-08-28 17:02:26 -0400

    Allow restoring from backups in development mode even if the setting is off.

--- app/assets/javascripts/admin/models/backup_status.js
+++ app/assets/javascripts/admin/models/backup_status.js
@@ -1,16 +1,9 @@
-/**
-  Data model for representing the status of backup/restore
-
-  @class BackupStatus
-  @extends Discourse.Model
-  @namespace Discourse
-  @module Discourse
-**/
 Discourse.BackupStatus = Discourse.Model.extend({
 
   restoreDisabled: Em.computed.not("restoreEnabled"),
 
   restoreEnabled: function() {
-    return Discourse.SiteSettings.allow_restore && !this.get("isOperationRunning");
-  }.property("isOperationRunning")
+    return this.get('allowRestore') && !this.get("isOperationRunning");
+  }.property("isOperationRunning", "allowRestore")
+
 });
