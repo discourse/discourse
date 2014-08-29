2637439cdf74445cd9fd6c12408d3a993582b0c1
cpradio <forums@cpradio.net>
2014-08-29 08:04:57 -0400

    FIX: Added keyboard bindings for dismiss buttons

--- app/assets/javascripts/discourse/lib/keyboard_shortcuts.js
+++ app/assets/javascripts/discourse/lib/keyboard_shortcuts.js
@@ -35,6 +35,9 @@ Discourse.KeyboardShortcuts = Ember.Object.createWithMixins({
     'm r': 'div.notification-options li[data-id="1"] a',          // mark topic as regular
     'm t': 'div.notification-options li[data-id="2"] a',          // mark topic as tracking
     'm w': 'div.notification-options li[data-id="3"] a',          // mark topic as watching
+    'd n': '#dismiss-new',                                        // dismiss new
+    'd t': '#dismiss-topics',                                     // dismiss topics
+    'd p': '#dismiss-posts',                                      // dismiss posts
     'n': '#user-notifications',                                   // open notifications menu
     'o,enter': '.topic-list tr.selected a.title',                 // open selected topic
     'shift+r': '#topic-footer-buttons button.create',             // reply to topic
