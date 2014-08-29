6b41c6b3357d013eb701066e8d94f36effcf19aa
Neil Lalonde <neillalonde@gmail.com>
2014-08-29 11:28:16 -0400

    add permalinks route constraint

--- spec/controllers/permalinks_controller_spec.rb
+++ spec/controllers/permalinks_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe PermalinksController do
   describe 'show' do
-    pending "should redirect to a permalink's target_url with status 301" do
+    it "should redirect to a permalink's target_url with status 301" do
       permalink = Fabricate(:permalink)
       Permalink.any_instance.stubs(:target_url).returns('/t/the-topic-slug/42')
       get :show, url: permalink.url
@@ -10,7 +10,7 @@ describe PermalinksController do
       response.status.should == 301
     end
 
-    pending 'return 404 if permalink record does not exist' do
+    it 'return 404 if permalink record does not exist' do
       get :show, url: '/not/a/valid/url'
       response.status.should == 404
     end
