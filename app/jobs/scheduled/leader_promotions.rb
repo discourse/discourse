2d615e4f8fa1fbdb01cf9896ca69c5c9665bfb0a
Sam <sam.saffron@gmail.com>
2014-09-05 16:55:25 +1000

    lesson, this is why you don't mock

--- app/jobs/scheduled/leader_promotions.rb
+++ app/jobs/scheduled/leader_promotions.rb
@@ -9,7 +9,7 @@ module Jobs
       User.real.where(trust_level: TrustLevel[3]).find_each do |u|
         # Don't demote too soon after being promoted
         next if UserHistory.for(u, :auto_trust_level_change)
-                           .where('created_at >= ?', SiteSetting.leader_promotion_min_duration.to_i.days.ago)
+                           .where('created_at >= ?', SiteSetting.tl3_promotion_min_duration.to_i.days.ago)
                            .where(previous_value: TrustLevel[2].to_s)
                            .where(new_value: TrustLevel[3].to_s)
                            .exists?
