d15b609e0afc8bf632975e740920d949c0bc9bce
Neil Lalonde <neillalonde@gmail.com>
2014-09-10 13:58:52 -0400

    FIX: support Permalink urls with query string

--- app/controllers/permalinks_controller.rb
+++ app/controllers/permalinks_controller.rb
@@ -2,7 +2,9 @@ class PermalinksController < ApplicationController
   skip_before_filter :check_xhr, :preload_json
 
   def show
-    permalink = Permalink.find_by_url(params[:url])
+    url = params[:url]
+    url = "#{url}?#{request.query_string}" if request.query_string.present?
+    permalink = Permalink.find_by_url(url)
     if permalink && permalink.target_url
       redirect_to permalink.target_url, status: :moved_permanently
     else
