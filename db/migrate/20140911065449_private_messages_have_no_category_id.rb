3ae2bd7f92af18211db77dbeb34ca8e36e07e077
Régis Hanol <regis@hanol.fr>
2014-09-11 10:11:12 +0200

    FIX: migration name
    Was generating: `NameError: uninitialized constant PrivateMessagesHaveNoCategoryId`

--- db/migrate/20140911065449_private_messages_have_no_category_id.rb
+++ db/migrate/20140911065449_private_messages_have_no_category_id.rb
@@ -1,4 +1,4 @@
-class PrivateMessageShaveNoCategoryId < ActiveRecord::Migration
+class PrivateMessagesHaveNoCategoryId < ActiveRecord::Migration
   def up
     execute "UPDATE topics SET category_id = NULL WHERE category_id IS NOT NULL AND archetype = \'private_message\'"
     execute "ALTER TABLE topics ADD CONSTRAINT pm_has_no_category CHECK (category_id IS NULL OR archetype <> 'private_message')"
