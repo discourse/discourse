2f74e70fd15e4a7762fc18434da2763c5e25d5e3
Sam Saffron <sam.saffron@gmail.com>
2014-09-18 15:15:42 +1000

    Revert "Revert "FEATURE: fenced code blocks can hint with "pre" or "text"""
    This reverts commit 4487d6a4c25ba5d3a366e2a8ada489a6a08d67ab.

--- app/assets/javascripts/discourse/dialects/code_dialect.js
+++ app/assets/javascripts/discourse/dialects/code_dialect.js
@@ -10,6 +10,8 @@ var acceptableCodeClasses =
    "profile", "python", "r", "rib", "rsl", "ruby", "rust", "scala", "smalltalk", "sql", "tex", "text",
    "vala", "vbscript", "vhdl"];
 
+var textCodeClasses = ["text", "pre"];
+
 function flattenBlocks(blocks) {
   var result = "";
   blocks.forEach(function(b) {
@@ -28,7 +30,12 @@ Discourse.Dialect.replaceBlock({
     if (matches[1] && acceptableCodeClasses.indexOf(matches[1]) !== -1) {
       klass = matches[1];
     }
-    return ['p', ['pre', ['code', {'class': klass}, flattenBlocks(blockContents) ]]];
+
+    if (textCodeClasses.indexOf(matches[1]) !== -1) {
+      return ['p', ['pre', ['code', flattenBlocks(blockContents) ]]];
+    } else  {
+      return ['p', ['pre', ['code', {'class': klass}, flattenBlocks(blockContents) ]]];
+    }
   }
 });
 
