905e11d6fcf0e6a4976a75a5f0b95f3e11ab698d
Lidlanca <lidlanca@yahoo.com>
2014-09-19 00:54:44 -0400

    Add Engine to support github issues

---
+++ lib/onebox/engine/github_issue_onebox.rb
@@ -0,0 +1,49 @@
+module Onebox
+  module Engine
+    class GithubIssueOnebox
+      #Author Lidlanca 2014
+      include Engine
+      include LayoutSupport
+      include JSON
+      matches_regexp Regexp.new("^http(?:s)?:\/\/(?:www\.)?(?:(?:\w)+\.)?github\.com\/(?<org>.+)")
+
+      def url
+        m = match 
+        "https://api.github.com/repos/#{m["org"]}/#{m["repo"]}/issues/#{m["item_id"]}"
+      end
+
+      private
+
+      def match
+         @match ||= @url.match(/^http(?:s)?:\/\/(?:www\.)?(?:(?:\w)+\.)?github\.com\/(?<org>.+)\/(?<repo>.+)\/(?<type>issues)\/(?<item_id>[\d]+)/)
+      end
+
+      def data
+        
+        @raw ||= ::MultiJson.load(open(url,"Accept"=>"application/vnd.github.v3.text+json",:read_timeout=>timeout )) #custom Accept header so we can get body as text.
+        body_text=  @raw["body_text"]
+      
+        
+        content_words = body_text.gsub("\n\n","\n").gsub("\n","<br>").split(" ") #one pass of removing double newline, then we change \n to <br> and later on we revert it back to \n this is a workaround to avoid losing newlines after we join it back.
+        max_words = 20
+        short_content =  content_words[0..max_words].join(" ")
+        short_content << "..." if content_words.length > max_words
+
+        status_color = {"open"=>"#6cc644","closed"=>"#bd2c00","merged"=>"#6e5494"}
+        result = { link: @url,
+                   title: "Issue: " + @raw["title"],
+                   content:short_content.gsub("<br>","\n"),
+                   labels: @raw["labels"],
+                   user: @raw['user'],
+                   status: @raw['state'],
+                   status_color:  status_color[@raw['state']] || "#cfcfcf",
+                   created_at: @raw['created_at'].split("T")[0], #get only date for now
+                   closed_at: (@raw['closed_at'].nil? ? "" : @raw['closed_at'].split("T")[0] ) ,
+                   closed_by: @raw['closed_by'],
+                   # avatar: "https://www.gravatar.com/avatar/#{@raw['user']['gravatar_id']}?s=128",
+                   avatar:"https://avatars1.githubusercontent.com/u/#{@raw['user']['id']}?v=2&s=96"
+                  }
+      end
+    end
+  end
+end
