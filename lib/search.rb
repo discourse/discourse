16cb963d9afdfb24acae7e93b7798ff585d77309
Sam Saffron <sam.saffron@gmail.com>
2014-09-20 12:57:16 +1000

    FEATURE: add name and username to search within topic

--- lib/search.rb
+++ lib/search.rb
@@ -255,7 +255,8 @@ class Search
                   .where("topics.archetype <> ?", Archetype.private_message)
 
       if @search_context.present? && @search_context.is_a?(Topic)
-        posts = posts.where("posts.raw ilike ?", "%#{@term}%")
+        posts = posts.joins('JOIN users u ON u.id = posts.user_id')
+        posts = posts.where("posts.raw  || ' ' || u.username || ' ' || u.name ilike ?", "%#{@term}%")
       else
         posts = posts.where("post_search_data.search_data @@ #{ts_query}")
       end
