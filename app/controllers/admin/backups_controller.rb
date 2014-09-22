9428ad779f9b58dca30d0096c69822c7a471aefb
Sam <sam.saffron@gmail.com>
2014-09-23 09:25:53 +1000

    FIX: send content length with backups

--- app/controllers/admin/backups_controller.rb
+++ app/controllers/admin/backups_controller.rb
@@ -46,6 +46,7 @@ class Admin::BackupsController < Admin::AdminController
   def show
     filename = params.fetch(:id)
     if backup = Backup[filename]
+      headers['Content-Length'] = File.size(backup.path)
       send_file backup.path
     else
       render nothing: true, status: 404
