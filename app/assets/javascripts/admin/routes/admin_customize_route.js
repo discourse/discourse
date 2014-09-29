3209aaeaaae4cb0bec0e53d65514b6c655e3c07f
Robin Ward <robin.ward@gmail.com>
2014-09-29 11:54:14 -0400

    FIX: Back button broke on customize routes in admin

--- app/assets/javascripts/admin/routes/admin_customize_route.js
+++ app/assets/javascripts/admin/routes/admin_customize_route.js
@@ -1,13 +1,5 @@
-/**
-  Handles routes related to customization
-
-  @class AdminCustomizeIndexRoute
-  @extends Discourse.Route
-  @namespace Discourse
-  @module Discourse
-**/
 Discourse.AdminCustomizeIndexRoute = Discourse.Route.extend({
-  redirect: function() {
-    this.transitionTo('adminCustomize.colors');
+  beforeModel: function() {
+    this.replaceWith('adminCustomize.colors');
   }
 });
