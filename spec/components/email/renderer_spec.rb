ed9953b1f53551de2f9ef61c42127288304f47d4
Robin Ward <robin.ward@gmail.com>
2014-10-06 13:57:38 -0400

    FIX: Remove html entities from text emails

---
+++ spec/components/email/renderer_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'email/renderer'
+
+describe Email::Renderer do
+
+  let(:message) do
+    mail = Mail.new
+
+    mail.text_part = Mail::Part.new do
+      body 'Key &amp; Peele'
+    end
+
+    mail.html_part = Mail::Part.new do
+      content_type 'text/html; charset=UTF-8'
+      body '<h1>Key &amp; Peele</h1>'
+    end
+
+    mail
+  end
+
+  it "escapes HTML entities from text" do
+    renderer = Email::Renderer.new(message)
+    renderer.text.should == "Key & Peele"
+  end
+
+end
+
+
+
