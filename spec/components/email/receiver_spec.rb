d669bf09fe55923c6b09a816e1bcd981f66093c3
Arpit Jalan <arpit@techapj.com>
2014-10-08 19:45:46 +0530

    FIX: handle multiple paragraphs in email reply when parsing html

--- spec/components/email/receiver_spec.rb
+++ spec/components/email/receiver_spec.rb
@@ -82,6 +82,17 @@ Thanks for listening."
       )
     end
 
+    it "handles multiple paragraphs when parsing html" do
+      test_parse_body(fixture_file("emails/html_paragraphs.eml")).
+          should == (
+"Awesome!
+
+Pleasure to have you here!
+
+:boom:"
+      )
+    end
+
     it "converts back to UTF-8 at the end" do
       result = test_parse_body(fixture_file("emails/big5.eml"))
       result.encoding.should == Encoding::UTF_8
