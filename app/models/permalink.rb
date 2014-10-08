346e28437e4c3d48aa69c2704cb14f7173cd8398
Neil Lalonde <neillalonde@gmail.com>
2014-10-08 11:50:44 -0400

    FEATURE: permalinks lookup table can redirect to any arbitrary url

--- app/models/permalink.rb
+++ app/models/permalink.rb
@@ -13,9 +13,28 @@ class Permalink < ActiveRecord::Base
   end
 
   def target_url
+    return external_url if external_url
     return post.url if post
     return topic.relative_url if topic
     return category.url if category
     nil
   end
 end
+
+# == Schema Information
+#
+# Table name: permalinks
+#
+#  id           :integer          not null, primary key
+#  url          :string(1000)     not null
+#  topic_id     :integer
+#  post_id      :integer
+#  category_id  :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#  external_url :string(1000)
+#
+# Indexes
+#
+#  index_permalinks_on_url  (url) UNIQUE
+#
