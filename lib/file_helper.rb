ab1e216e3bf3f5cb3d633536076032fa1bf2d780
Jeff Atwood <jatwood@codinghorror.com>
2014-10-13 12:43:36 -0700

    FEATURE: add webp and svg to known image formats

--- lib/file_helper.rb
+++ lib/file_helper.rb
@@ -28,7 +28,7 @@ class FileHelper
   private
 
   def self.images
-    @@images ||= Set.new ["jpg", "jpeg", "png", "gif", "tif", "tiff", "bmp"]
+    @@images ||= Set.new ["jpg", "jpeg", "png", "gif", "tif", "tiff", "bmp", "svg", "webp"]
   end
 
   def self.images_regexp
