8a6f9425d46be9ed62e97460ca7dc901910dac84
Robin Ward <robin.ward@gmail.com>
2014-10-15 16:00:45 -0400

    Fix uglifier args

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Discourse::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = GlobalSetting.serve_static_assets
 
-  config.assets.js_compressor = Uglifier.new(max_line_len: 1024)
+  config.assets.js_compressor = Uglifier.new(output: {max_line_len: 1024})
   config.assets.css_compressor = :sass
 
   # stuff should be pre-compiled
