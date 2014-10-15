d329006e304e2b6d9f8d8cd65199aef8ad618c59
Robin Ward <robin.ward@gmail.com>
2014-10-15 16:50:58 -0400

    Let's try to change Uglifier settings properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,11 +12,7 @@ Discourse::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = GlobalSetting.serve_static_assets
 
-  if defined?(Uglifier)
-    config.assets.js_compressor = Uglifier.new(output: {max_line_len: 1024}, screw_ie8: false)
-  else
-    config.assets.js_compressor = :uglifier
-  end
+  config.assets.js_compressor = :uglifier
 
   config.assets.css_compressor = :sass
 
