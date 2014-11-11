782af661c3df5bda7d462760392c474f1d7a8de4
Sam <sam.saffron@gmail.com>
2014-11-11 12:58:56 +1100

    rails master fixes

--- config/initializers/sprockets.rb
+++ config/initializers/sprockets.rb
@@ -2,5 +2,20 @@ require_dependency 'sass/discourse_stylesheets'
 require_dependency 'sass/discourse_sass_importer'
 require_dependency 'sass/discourse_safe_sass_importer'
 
-Sprockets.send(:remove_const, :SassImporter)
-Sprockets::SassImporter = DiscourseSassImporter
+if defined?(Sass::Rails::SassTemplate)
+  DiscourseSassTemplate = Class.new(Sass::Rails::SassTemplate) do
+    def importer_class
+      DiscourseSassImporter
+    end
+  end
+  DiscourseScssTemplate = Class.new(DiscourseSassTemplate) do
+    def syntax
+      :scss
+    end
+  end
+  Rails.application.assets.register_engine '.sass', DiscourseSassTemplate
+  Rails.application.assets.register_engine '.scss', DiscourseScssTemplate
+else
+  Sprockets.send(:remove_const, :SassImporter)
+  Sprockets::SassImporter = DiscourseSassImporter
+end
