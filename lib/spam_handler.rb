7641d88224ec321baa789fd6cbfe731dd51fabf4
Régis Hanol <regis@hanol.fr>
2014-11-17 12:04:29 +0100

    FEATURE: new 'maximum new user accounts per registration IP' site setting

---
+++ lib/spam_handler.rb
@@ -0,0 +1,14 @@
+class SpamHandler
+
+  def self.should_prevent_registration_from_ip?(ip_address)
+    return false if SiteSetting.max_new_accounts_per_registration_ip <= 0
+
+    tl0_accounts_with_same_ip = User.unscoped
+                                    .where(trust_level: TrustLevel[0])
+                                    .where("ip_address = ?", ip_address.to_s)
+                                    .count
+
+    tl0_accounts_with_same_ip >= SiteSetting.max_new_accounts_per_registration_ip
+  end
+
+end
