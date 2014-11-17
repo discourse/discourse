c5f9dd4ef39990c419bc66766a2e83d630e39715
Régis Hanol <regis@hanol.fr>
2014-11-17 15:02:10 +0100

    FEATURE: only limit new registrations from that IP if all the users from that IP are TL1 or TL0

--- spec/components/spam_handler_spec.rb
+++ spec/components/spam_handler_spec.rb
@@ -22,6 +22,17 @@ describe SpamHandler do
       -> { Fabricate(:user, ip_address: "42.42.42.42", trust_level: TrustLevel[0]) }.should raise_error(ActiveRecord::RecordInvalid)
     end
 
+    it "only limit new registrations from an IP if *all* the users from that IP are TL1 or TL0" do
+      # setup
+      SiteSetting.stubs(:max_new_accounts_per_registration_ip).returns(0)
+      Fabricate(:user, ip_address: "42.42.42.42", trust_level: TrustLevel[0])
+      Fabricate(:user, ip_address: "42.42.42.42", trust_level: TrustLevel[2])
+
+      # should not limit registrations since there is a TL2 user with that IP
+      SiteSetting.stubs(:max_new_accounts_per_registration_ip).returns(1)
+      Fabricate(:user, ip_address: "42.42.42.42", trust_level: TrustLevel[0])
+    end
+
   end
 
 end
