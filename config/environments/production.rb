85e5e912b2c5c15681bc4fb21f8c2a0dd0a534b5
Sam <sam.saffron@gmail.com>
2014-11-17 16:58:14 +1100

    be explicit about log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ Discourse::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.log_level = :info
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
