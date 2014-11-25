515b616d30dfd974d18611a01f843f284f2acb51
Régis Hanol <regis@hanol.fr>
2014-11-25 18:45:45 +0100

    ooops. forgot the args :fired:

--- app/jobs/scheduled/clean_up_unmatched_ips.rb
+++ app/jobs/scheduled/clean_up_unmatched_ips.rb
@@ -3,7 +3,7 @@ module Jobs
   class CleanUpUnmatchedIPs < Jobs::Scheduled
     every 1.day
 
-    def execute
+    def execute(args)
       last_match_threshold = SiteSetting.max_age_unmatched_ips.days.ago
 
       ScreenedIpAddress.where(action_type: ScreenedIpAddress.actions[:block])
