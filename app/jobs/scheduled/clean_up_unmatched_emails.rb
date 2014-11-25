ec3a8f8f7ccdfb3349b587639028b5ee411f35a9
Régis Hanol <regis@hanol.fr>
2014-11-25 18:45:45 +0100

    ooops. forgot the args :fired:

--- app/jobs/scheduled/clean_up_unmatched_emails.rb
+++ app/jobs/scheduled/clean_up_unmatched_emails.rb
@@ -3,7 +3,7 @@ module Jobs
   class CleanUpUnmatchedEmails < Jobs::Scheduled
     every 1.day
 
-    def execute
+    def execute(args)
       last_match_threshold = SiteSetting.max_age_unmatched_emails.days.ago
 
       ScreenedEmail.where(action_type: ScreenedEmail.actions[:block])
