6a26b72f7826e11362f4584c7b78a114d6155ea7
Erick Guan <fantasticfears@gmail.com>
2014-12-26 22:09:08 +0800

    extends share links
    - extract font awesome class from share link model
    - add class to allow plugin hooking into the service

--- app/assets/javascripts/discourse/models/share_link.js
+++ app/assets/javascripts/discourse/models/share_link.js
@@ -15,8 +15,8 @@ Discourse.ShareLink = Discourse.Model.extend({
 
   title: Discourse.computed.i18n('target', 'share.%@'),
 
-  iconClass: function() {
-    return Discourse.ShareLink.iconClasses[this.get('target')];
+  serviceIcon: function() {
+    return Discourse.ShareLink.serviceIcons[this.get('target')];
   }.property('target'),
 
   openInPopup: function() {
@@ -28,17 +28,16 @@ Discourse.ShareLink = Discourse.Model.extend({
 Discourse.ShareLink.reopenClass({
   supportedTargets: [],
   urlFor: {},
-  iconClasses: {},
+  serviceIcons: {},
   popupHeights: {},
   shouldOpenInPopup: {},
 
   addTarget: function(id, object) {
-    var self = this;
-    self.supportedTargets.push(id);
-    self.urlFor[id] = object.generateUrl;
-    self.iconClasses[id] = object.iconClass;
-    self.popupHeights[id] = object.popupHeight || 315;
-    self.shouldOpenInPopup[id] = object.shouldOpenInPopup;
+    this.supportedTargets.push(id);
+    this.urlFor[id] = object.generateUrl;
+    this.serviceIcons[id] = object.serviceIcon;
+    this.popupHeights[id] = object.popupHeight || 315;
+    this.shouldOpenInPopup[id] = object.shouldOpenInPopup;
   },
 
   popupHeight: function(target) {
@@ -48,7 +47,7 @@ Discourse.ShareLink.reopenClass({
 
 (function() {
   Discourse.ShareLink.addTarget('twitter', {
-    iconClass: 'fa-twitter-square',
+    serviceIcon: '<i class="fa fa-twitter-square"></i>',
     generateUrl: function(link, title) {
       return ("http://twitter.com/intent/tweet?url=" + encodeURIComponent(link) + "&text=" + encodeURIComponent(title) );
     },
@@ -57,7 +56,7 @@ Discourse.ShareLink.reopenClass({
   });
 
   Discourse.ShareLink.addTarget('facebook', {
-    iconClass: 'fa-facebook-square',
+    serviceIcon: '<i class="fa fa-facebook-square"></i>',
     generateUrl: function(link, title) {
       return ("http://www.facebook.com/sharer.php?u=" + encodeURIComponent(link) + '&t=' + encodeURIComponent(title));
     },
@@ -66,7 +65,7 @@ Discourse.ShareLink.reopenClass({
   });
 
   Discourse.ShareLink.addTarget('google+', {
-    iconClass: 'fa-google-plus-square',
+    serviceIcon: '<i class="fa fa-google-plus-square"></i>',
     generateUrl: function(link) {
       return ("https://plus.google.com/share?url=" + encodeURIComponent(link));
     },
@@ -75,10 +74,47 @@ Discourse.ShareLink.reopenClass({
   });
 
   Discourse.ShareLink.addTarget('email', {
-    iconClass: 'fa-envelope-square',
+    serviceIcon: '<i class="fa fa-envelope-square"></i>',
     generateUrl: function(link, title) {
       return ("mailto:?to=&subject=" + encodeURIComponent('[' + Discourse.SiteSettings.title + '] ' + title) + "&body=" + encodeURIComponent(link));
     },
     shouldOpenInPopup: false
   });
+
+  Discourse.ShareLink.addTarget('blogger', {
+    serviceIcon: '<i class="zocial zocial-blogger"></i>',
+    generateUrl: function(link, title) {
+      return ("https://www.blogger.com/blog-this.g?u=" + encodeURIComponent(link) + "&n=" + encodeURIComponent(title) + "&t=" + encodeURIComponent(title));
+    },
+    shouldOpenInPopup: true,
+    popupHeight: 600
+  });
+
+  Discourse.ShareLink.addTarget('reddit', {
+    serviceIcon: '<i class="fa fa-reddit-square"></i>',
+    generateUrl: function(link, title) {
+      return ("http://www.reddit.com/submit?url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title));
+    },
+    shouldOpenInPopup: true,
+    popupHeight: 600
+  });
+
+  Discourse.ShareLink.addTarget('linkedin', {
+    serviceIcon: '<i class="fa fa-linkedin-square"></i>',
+    generateUrl: function(link, title) {
+      return ("http://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title) + "&summary=" + encodeURIComponent(title) + "&source=" + encodeURIComponent(link));
+    },
+    shouldOpenInPopup: true,
+    popupHeight: 600
+  });
+
+  Discourse.ShareLink.addTarget('tumblr', {
+    serviceIcon: '<i class="fa fa-tumblr-square"></i>',
+    generateUrl: function(link, title) {
+      return ("http://www.tumblr.com/share/link?url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title));
+    },
+    shouldOpenInPopup: true,
+    popupHeight: 500
+  });
+
 })();
