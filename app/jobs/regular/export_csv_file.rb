4ed396fa0cea1a126de6a96519e1f33c6637260e
Arpit Jalan <arpit@techapj.com>
2014-12-30 01:42:36 +0530

    FIX: deleted topics were failing user posts archive download

--- app/jobs/regular/export_csv_file.rb
+++ app/jobs/regular/export_csv_file.rb
@@ -132,6 +132,10 @@ module Jobs
         user_archive_array = []
         topic_data = user_archive.topic
         user_archive = user_archive.as_json
+        if topic_data.nil?
+          # deleted topic
+          topic_data = Topic.with_deleted.find_by(id: user_archive['topic_id'])
+        end
         category = topic_data.category
         sub_category = "-"
         if category
