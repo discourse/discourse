87d846c2c2ea7fef094aef6b550ad581333e8d4a
riking <rikingcoding@gmail.com>
2015-01-16 19:52:31 -0800

    Remove dead code in StaticPage model

--- app/assets/javascripts/discourse/models/static_page.js
+++ app/assets/javascripts/discourse/models/static_page.js
@@ -5,16 +5,9 @@ Discourse.StaticPage.reopenClass({
     return new Em.RSVP.Promise(function(resolve) {
       // Models shouldn't really be doing Ajax request, but this is a huge speed boost if we
       // preload content.
-      var $preloaded = $("noscript[data-path=\"/" + path + "\"]");
-      if ($preloaded.length) {
-        var text = $preloaded.text();
-        text = text.match(/<!-- preload-content: -->((?:.|[\n\r])*)<!-- :preload-content -->/)[1];
-        resolve(Discourse.StaticPage.create({path: path, html: text}));
-      } else {
-        Discourse.ajax(path + ".html", {dataType: 'html'}).then(function (result) {
-          resolve(Discourse.StaticPage.create({path: path, html: result}));
-        });
-      }
+      Discourse.ajax(path + ".html", {dataType: 'html'}).then(function (result) {
+        resolve(Discourse.StaticPage.create({path: path, html: result}));
+      });
     });
   }
 });
