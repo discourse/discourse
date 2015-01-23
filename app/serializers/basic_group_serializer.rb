5c9ca49bf9d6ebad296138055488a91af684cbcd
Régis Hanol <regis@hanol.fr>
2015-01-23 18:25:43 +0100

    FEATURE: automatic group membership based on email address

--- app/serializers/basic_group_serializer.rb
+++ app/serializers/basic_group_serializer.rb
@@ -1,3 +1,14 @@
 class BasicGroupSerializer < ApplicationSerializer
-  attributes :id, :automatic, :name, :user_count, :alias_level, :visible
+  attributes :id,
+             :automatic,
+             :name,
+             :user_count,
+             :alias_level,
+             :visible,
+             :automatic_membership_email_domains,
+             :automatic_membership_retroactive
+
+  def automatic_membership_email_domains
+    object.automatic_membership_email_domains.presence || ""
+  end
 end
