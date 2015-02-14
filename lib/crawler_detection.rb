026cdd8fc38760b2edfdb1410f446a5a3c5ffc9d
Erick Guan <fantasticfears@gmail.com>
2015-02-14 22:24:51 +0800

    FEATURE: add 360Spider UA to allow 360 crawl Discourse sites

--- lib/crawler_detection.rb
+++ lib/crawler_detection.rb
@@ -1,7 +1,8 @@
 module CrawlerDetection
   # added 'ia_archiver' based on https://meta.discourse.org/t/unable-to-archive-discourse-pages-with-the-internet-archive/21232
   # added 'Wayback Save Page' based on https://meta.discourse.org/t/unable-to-archive-discourse-with-the-internet-archive-save-page-now-button/22875
+
   def self.crawler?(user_agent)
-    !/Googlebot|Mediapartners|AdsBot|curl|Twitterbot|facebookexternalhit|bingbot|Baiduspider|ia_archiver|Wayback Save Page/.match(user_agent).nil?
+    !/Googlebot|Mediapartners|AdsBot|curl|Twitterbot|facebookexternalhit|bingbot|Baiduspider|ia_archiver|Wayback Save Page|360Spider/.match(user_agent).nil?
   end
 end
