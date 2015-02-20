d7f1ca84b2a5195bea0476e41f9e5b2a25515e47
Régis Hanol <regis@hanol.fr>
2015-02-20 17:43:29 +0100

    FIX: disable custom stylesheets in /admin

--- app/assets/javascripts/admin/routes/admin_route.js
+++ app/assets/javascripts/admin/routes/admin_route.js
@@ -5,5 +5,15 @@ Discourse.AdminRoute = Discourse.Route.extend({
 
   titleToken: function() {
     return I18n.t('admin_title');
+  },
+
+  activate: function() {
+    this._super();
+    $("link.custom-css").attr("rel", "");
+  },
+
+  deactivate: function() {
+    this._super();
+    $("link.custom-css").attr("rel", "stylesheet");
   }
 });
