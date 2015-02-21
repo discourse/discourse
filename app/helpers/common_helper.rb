49c2de424aec0ce6a42ba534807b33d682f82835
Mooash <mooash@gmail.com>
2015-02-21 14:54:18 +1100

    Adding Piwik as a analytics option
    As requested here (https://meta.discourse.org/t/support-for-piwik-analytics-as-an-alternative-to-google-analytics/7512/51) by several people this allows people to use Piwik as an alternative to GA

--- app/helpers/common_helper.rb
+++ app/helpers/common_helper.rb
@@ -10,4 +10,10 @@ module CommonHelper
       render partial: "common/google_analytics"
     end
   end
+
+  def render_piwik_analytics_code
+    if Rails.env.production? && SiteSetting.piwik_domain_name.present? && SiteSetting.piwik_site_id.present?
+      render partial: "common/piwik_analytics"
+    end
+  end
 end
