7adc1abba9c951c9ab43bc1c929cfe657a332593
Sam <sam.saffron@gmail.com>
2015-03-06 18:14:19 +1100

    remove migration bombs

--- db/migrate/20140408152401_add_default_value_to_top_topic_scores.rb
+++ db/migrate/20140408152401_add_default_value_to_top_topic_scores.rb
@@ -1,6 +1,6 @@
 class AddDefaultValueToTopTopicScores < ActiveRecord::Migration
   def change
-    TopTopic.periods.each do |period|
+    [:daily, :weekly, :monthly, :yearly].each do |period|
       change_column_default :top_topics, "#{period}_score".to_sym, 0
     end
   end
