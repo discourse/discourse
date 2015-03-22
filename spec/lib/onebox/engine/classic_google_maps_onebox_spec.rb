c1dc6794624d27175095a4aa8eb8a2b87968f04f
Jens Maier <jens@elberet.de>
2015-03-22 04:51:00 +0100

    FIX: resolve short URL via https, add placeholder

--- spec/lib/onebox/engine/classic_google_maps_onebox_spec.rb
+++ spec/lib/onebox/engine/classic_google_maps_onebox_spec.rb
@@ -1,38 +1,52 @@
 require "spec_helper"
 
 describe Onebox::Engine::ClassicGoogleMapsOnebox do
-  describe 'long form url' do
-    before do
-      @link = "https://maps.google.ca/maps?q=Eiffel+Tower,+Avenue+Anatole+France,+Paris,+France&hl=en&sll=43.656878,-79.32085&sspn=0.932941,1.50238&oq=eiffel+&t=m&z=17&iwloc=A"
+
+  URLS = {
+    short: "https://goo.gl/maps/rEG3D",
+    long: "https://www.google.de/maps/place/Statue+of+Liberty+National+Monument/@40.689249,-74.0445,17z/data=!3m1!4b1!4m2!3m1!1s0x89c25090129c363d:0x40c6a5770d25022b",
+    canonical: "https://maps.google.de/maps?sll=40.689249,-74.0445&sspn=0.0062479,0.0109864&cid=4667599994556318251&q=Statue+of+Liberty+National+Monument&output=classic&dg=ntvb",
+    embed: "https://maps.google.de/maps?sll=40.689249,-74.0445&sspn=0.0062479,0.0109864&cid=4667599994556318251&q=Statue+of+Liberty+National+Monument&output=embed&dg=ntvb&ll=40.689249,-74.0445&spn=0.0062479,0.0109864"
+  }
+
+  before(:all) do
+    FakeWeb.register_uri(:head, URLS[:short], response: "HTTP/1.1 302 Found\nLocation: #{URLS[:long]}\n\n")
+    FakeWeb.register_uri(:head, URLS[:long], response: "HTTP/1.1 301 Moved Permanently\nLocation: #{URLS[:canonical]}\n\n")
+  end
+
+  subject { described_class.new(link) }
+
+  it_behaves_like "an engine" do
+    let(:link) { URLS[:canonical] }
+    let(:data) { Onebox::Helpers.symbolize_keys(subject.send(:data)) }
+  end
+
+  shared_examples "embeddable" do |kind|
+    let(:link) { URLS[kind] }
+
+    it "resolves url correctly" do
+      subject.url.should == URLS[:embed]
     end
 
-    include_context "engines"
-    it_behaves_like "an engine"
+    it "produces an iframe" do
+      subject.to_html.should include("iframe", "output=embed")
+    end
 
-    describe "#to_html" do
-      it "embeds the iframe to display the map" do
-        expect(html).to include("iframe")
-      end
+    it "produces a placeholder image" do
+      subject.placeholder_html.should include("img")
     end
   end
 
-  describe 'short form url' do
-    let(:long_url) { "https://maps.google.ca/maps?q=Brooklyn+Bridge,+Brooklyn,+NY,+United+States&hl=en&sll=43.656878,-79.32085&sspn=0.932941,1.50238&oq=brooklyn+bridge&t=m&z=17&iwloc=A" }
-
-    it "retrieves the long form url" do
-      onebox = described_class.new("http://goo.gl/maps/XffUa")
-      onebox.expects(:get_long_url).once.returns(long_url)
-      expect(onebox.url).to eq(long_url)
-    end
+  context "canonical url" do
+    it_should_behave_like "embeddable", :canonical
   end
 
-  describe 'www form url' do
-    let(:embed_url) { "https://maps.google.de/maps?t=h&ll=48.398499,9.992333&spn=0.0038338,0.0056322&dg=ntvb&output=embed" }
-
-    it "retrieves the canonical url" do
-      onebox = described_class.new("https://www.google.de/maps/@48.3932366,9.992333,663a,20y,41.43t/data=!3m1!1e3")
-      onebox.expects(:get_canonical_url).once.returns(embed_url)
-      expect(onebox.url).to eq(embed_url)
-    end
+  context "long url" do
+    it_should_behave_like "embeddable", :long
   end
+
+  context "short url" do
+    it_should_behave_like "embeddable", :short
+  end
+
 end
