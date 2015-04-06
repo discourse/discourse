1a3fa52cde5d5eb102cda9eff4f40f0271e3c59f
Régis Hanol <regis@hanol.fr>
2015-04-06 12:12:27 +0200

    fix broken build (cc @riking)

--- spec/jobs/create_daily_backup_spec.rb
+++ spec/jobs/create_daily_backup_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
-require_dependency 'jobs/scheduled/create_backup'
+require_dependency 'jobs/regular/create_daily_backup'
 
-describe Jobs::CreateBackup do
+describe Jobs::CreateDailyBackup do
   it "does nothing when daily backups are disabled" do
     SiteSetting.stubs(:backup_daily?).returns(false)
     BackupRestore.expects(:backup!).never
-    Jobs::CreateBackup.new.execute({})
+    Jobs::CreateDailyBackup.new.execute({})
   end
 
   it "calls `backup!` when the daily backups are enabled" do
     SiteSetting.stubs(:backup_daily?).returns(true)
     BackupRestore.expects(:backup!).with(Discourse.system_user.id, { publish_to_message_bus: false }).once
-    Jobs::CreateBackup.new.execute({})
+    Jobs::CreateDailyBackup.new.execute({})
   end
 end
 
