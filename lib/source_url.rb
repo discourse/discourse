788b66e4a31533d0263cce28ee4cc55f08ba4393
Robin Ward <robin.ward@gmail.com>
2015-04-16 16:44:24 -0400

    Update SourceURL to work better

---
+++ lib/source_url.rb
@@ -0,0 +1,15 @@
+class SourceURL < Tilt::Template
+  self.default_mime_type = 'application/javascript'
+
+  def prepare
+  end
+
+  def evaluate(scope, locals, &block)
+    code = ''
+    code << 'eval('
+    code << data.inspect
+    code << " + \"\\n//@ sourceURL=/#{scope.logical_path}\""
+    code << ");\n"
+    code
+  end
+end
