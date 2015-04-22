27aa0bc0565d614f80a8351fc976588aac40125c
Sam Saffron <sam.saffron@gmail.com>
2015-04-23 00:10:09 +1000

    PERF: eliminate query that grabs every post number on topic page.

--- lib/topic_view.rb
+++ lib/topic_view.rb
@@ -416,10 +416,10 @@ class TopicView
   end
 
   def closest_post_to(post_number)
-    closest_posts = filter_post_ids_by("@(post_number - #{post_number})")
-    return nil if closest_posts.empty?
+    closest_post = @filtered_posts.order("@(post_number - #{post_number})").limit(1).pluck(:id)
+    return nil if closest_post.empty?
 
-    filtered_post_ids.index(closest_posts.first) || filtered_post_ids[0]
+    filtered_post_ids.index(closest_post.first) || filtered_post_ids[0]
   end
 
 end
