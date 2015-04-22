85320f919b5007338d13c37c4fb8a60dcd419ecc
Neil Lalonde <neillalonde@gmail.com>
2015-04-22 12:40:01 -0400

    FEATURE: user list csv includes approved, suspended_at, suspended_till, blocked

--- app/jobs/regular/export_csv_file.rb
+++ app/jobs/regular/export_csv_file.rb
@@ -8,7 +8,7 @@ module Jobs
 
     HEADER_ATTRS_FOR = {}
     HEADER_ATTRS_FOR['user_archive'] = ['topic_title','category','sub_category','is_pm','post','like_count','reply_count','url','created_at']
-    HEADER_ATTRS_FOR['user_list'] = ['id','name','username','email','title','created_at','trust_level','active','admin','moderator','ip_address']
+    HEADER_ATTRS_FOR['user_list'] = ['id','name','username','email','title','created_at','trust_level','approved','suspended_at','suspended_till','blocked','active','admin','moderator','ip_address']
     HEADER_ATTRS_FOR['user_stats'] = ['topics_entered','posts_read_count','time_read','topic_count','post_count','likes_given','likes_received']
     HEADER_ATTRS_FOR['user_sso'] = ['external_id','external_email', 'external_username', 'external_name', 'external_avatar_url']
     HEADER_ATTRS_FOR['staff_action'] = ['staff_user','action','subject','created_at','details', 'context']
