02e9336fd86873a637f7afcf86ebfaafcba024b7
Arthur Neves <arthurnn@gmail.com>
2015-04-25 11:47:20 -0400

    Fix warnings for rspec 3

--- spec/support/time_matcher.rb
+++ spec/support/time_matcher.rb
@@ -2,7 +2,7 @@ RSpec::Matchers.define :be_within_one_second_of do |expected_time|
   match do |actual_time|
     (actual_time - expected_time).abs < 1
   end
-  failure_message_for_should do |actual_time|
+  failure_message do |actual_time|
     "#{actual_time} is not within 1 second of #{expected_time}"
   end
 end
