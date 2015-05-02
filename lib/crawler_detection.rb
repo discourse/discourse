bbb1348118165087f6a8cd2244a65a9fb31fef63
Jeff Atwood <jatwood@codinghorror.com>
2015-05-02 03:18:58 -0700

    add Swiftbot to crawler regex

--- lib/crawler_detection.rb
+++ lib/crawler_detection.rb
@@ -1,8 +1,9 @@
 module CrawlerDetection
   # added 'ia_archiver' based on https://meta.discourse.org/t/unable-to-archive-discourse-pages-with-the-internet-archive/21232
   # added 'Wayback Save Page' based on https://meta.discourse.org/t/unable-to-archive-discourse-with-the-internet-archive-save-page-now-button/22875
+  # added 'Swiftbot' based on https://meta.discourse.org/t/how-to-add-html-markup-or-meta-tags-for-external-search-engine/28220
 
   def self.crawler?(user_agent)
-    !/Googlebot|Mediapartners|AdsBot|curl|Twitterbot|facebookexternalhit|bingbot|Baiduspider|ia_archiver|Wayback Save Page|360Spider/.match(user_agent).nil?
+    !/Googlebot|Mediapartners|AdsBot|curl|Twitterbot|facebookexternalhit|bingbot|Baiduspider|ia_archiver|Wayback Save Page|360Spider|Swiftbot/.match(user_agent).nil?
   end
 end
