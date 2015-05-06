7836c611c8e6b14e1e868590ae6e639ffc9ef5fe
Sam <sam.saffron@gmail.com>
2015-05-06 10:44:45 +1000

    do not alert on deleted posts

--- app/jobs/regular/post_alert.rb
+++ app/jobs/regular/post_alert.rb
@@ -2,8 +2,10 @@ module Jobs
   class PostAlert < Jobs::Base
 
     def execute(args)
-      post = Post.find(args[:post_id])
-      PostAlerter.post_created(post)
+      # maybe it was removed by the time we are making the post
+      if post = Post.find_by(id: args[:post_id])
+        PostAlerter.post_created(post)
+      end
     end
 
   end
