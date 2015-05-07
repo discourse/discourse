77cc087b1377ac395862eef9577759ad7ab0d8da
Sam <sam.saffron@gmail.com>
2015-05-07 11:00:22 +1000

    FIX: proper error message when account created is hit with no session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -436,7 +436,7 @@ class UsersController < ApplicationController
   end
 
   def account_created
-    @message = session['user_created_message']
+    @message = session['user_created_message'] || I18n.t('activation.missing_session')
     expires_now
     render layout: 'no_ember'
   end
