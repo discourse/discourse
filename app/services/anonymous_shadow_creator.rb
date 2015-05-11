824f3da1e473d70a710167fa3f08677960f7c810
Kaz Wolfe <pacificfils@gmail.com>
2015-05-11 13:58:54 -0700

    Give Anonymous a name
    Simple fix to ensure that Anonymous mode can be used even when names are required.
    
    Fixes:
    
        ActiveRecord::RecordInvalid (Validation failed: Name can't be blank)

--- app/services/anonymous_shadow_creator.rb
+++ app/services/anonymous_shadow_creator.rb
@@ -34,7 +34,7 @@ class AnonymousShadowCreator
       shadow = User.create!(
         password: SecureRandom.hex,
         email: "#{SecureRandom.hex}@anon.#{Discourse.current_hostname}",
-        name: "",
+        name: "Anonymous",
         username: UserNameSuggester.suggest(I18n.t(:anonymous).downcase),
         active: true,
         trust_level: 1,
