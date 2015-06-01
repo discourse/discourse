d970edb0328683bf2601e8696d2b66ba17246315
Sam <sam.saffron@gmail.com>
2015-06-01 15:43:16 +1000

    FIX: show category in subject not checking for category

--- app/mailers/user_notifications.rb
+++ app/mailers/user_notifications.rb
@@ -241,7 +241,7 @@ class UserNotifications < ActionMailer::Base
 
     # category name
     category = Topic.find_by(id: post.topic_id).category
-    if opts[:show_category_in_subject] && post.topic_id && !category.uncategorized?
+    if opts[:show_category_in_subject] && post.topic_id && category && !category.uncategorized?
       show_category_in_subject = category.name
 
       # subcategory case
