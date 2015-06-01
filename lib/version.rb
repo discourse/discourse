1ca1c0d146c91bc572ebc8f1df0cdc656a0ccdeb
Neil Lalonde <neillalonde@gmail.com>
2015-06-01 16:54:15 -0400

    Revert "Version bump to v1.3.0"

--- lib/version.rb
+++ lib/version.rb
@@ -3,8 +3,8 @@ module Discourse
   unless defined? ::Discourse::VERSION
     module VERSION #:nodoc:
       MAJOR = 1
-      MINOR = 3
-      TINY  = 0
+      MINOR = 2
+      TINY  = 4
       PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
