5db768b69f758e98f230315505c6340dffecf7e6
Sam Saffron <sam.saffron@gmail.com>
2015-06-10 00:28:01 +1000

    FEATURE: improve no-js topic list information
    - Provide links to pages
    - Provied link to last topic

---
+++ app/helpers/list_helper.rb
@@ -0,0 +1,20 @@
+module ListHelper
+  def page_links(topic)
+    posts = topic.posts_count
+    max_pages = 10
+    total_pages = (posts / TopicView.chunk_size) + (posts == TopicView.chunk_size ? 0 : 1)
+
+    return if total_pages < 2
+
+    page = [total_pages - (max_pages+1), 2].max
+
+    result = "("
+    while page <= total_pages
+      result << " <a href='#{topic.relative_url}?page=#{page}'>#{page}</a> "
+      page += 1
+    end
+
+    result << ")"
+    result.html_safe
+  end
+end
