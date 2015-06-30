8e76e6e4b72aec9ca8caabf1bd79e58bfbc0ff6a
Neil Lalonde <neillalonde@gmail.com>
2015-06-30 15:42:38 -0400

    topics with no reply data were strings

--- app/models/report.rb
+++ app/models/report.rb
@@ -114,7 +114,7 @@ class Report
   def self.report_topics_with_no_response(report)
     report.data = []
     Topic.with_no_response_per_day(report.start_date, report.end_date, report.category_id).each do |r|
-      report.data << { x: Date.parse(r["date"]), y: r["count"] }
+      report.data << { x: Date.parse(r["date"]), y: r["count"].to_i }
     end
     report.total = Topic.with_no_response_total(category_id: report.category_id)
     report.prev30Days = Topic.with_no_response_total(start_date: report.start_date - 30.days, end_date: report.start_date, category_id: report.category_id)
