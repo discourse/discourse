1cb45861c5d1ee5c86b58d950e76d754d8423392
Gerhard Schlager <mail@gerhard-schlager.at>
2015-07-05 23:17:03 +0200

    FEATURE: Lots of improvements to the phpBB3 importer
    - Extensive refactoring of the existing importer
    - Configuration of import with settings.yml instead of editing code
    - Supports importing from phpBB 3.0.x and 3.1.x
    - Imports all attachments (not just the ones embedded with [attachment])
      from posts and private messages
    - Imports all existing attachments without the need to configure allowed
      file extensions or file sizes
    - Imports polls
    - Imports bookmarks
    - Imports sticky topics and (global) announcements as pinned topics
    - Imports categories in the original order and sets the content of the
      category description topic
    - Sets the creation date of category description topics to the creation
      date of the first topic in each category
    - Imports additional user attributes: last seen date, registration
      IP address, website, date of birth, location
    - Optionally set the user's name to its username
    - Users that didn't activate their account in phpBB3 are imported as
      inactive users
    - All imported, active users are automatically approved
    - Users that were deactivated in phpBB3 get suspended for 200 years
      during the import
    - Anonymous user can be imported as suspended users instead of the
      system user
    - Forums of type "link" are not imported as categories anymore
    - Internal links to posts get rewritten during the import (previously
      only links to topics got rewritten)
    - Ordered lists with BBCode [list=a] (which are unsupported in
      Discourse) get imported as if they would be [list=1]
    - Importing of avatars, attachments, private messages, polls and
      bookmarks can be disabled via configuration file
    - Optional fixing of private messages for forums that have been upgraded
      from phpBB2 prevents the import of duplicate messages and tries to
      group related messages into topics
    - Table prefix (default: phpbb) is configurable
    - Most of phpBB's default smilies are mapped to Emojis and all other
      smilies get uploaded and embedded as images. Smiley mappings can be
      added or overridden in the settings.yml file.

---
+++ script/import_scripts/phpbb3/importers/post_importer.rb
@@ -0,0 +1,79 @@
+module ImportScripts::PhpBB3
+  class PostImporter
+    # @param lookup [ImportScripts::LookupContainer]
+    # @param text_processor [ImportScripts::PhpBB3::TextProcessor]
+    # @param attachment_importer [ImportScripts::PhpBB3::AttachmentImporter]
+    # @param poll_importer [ImportScripts::PhpBB3::PollImporter]
+    # @param settings [ImportScripts::PhpBB3::Settings]
+    def initialize(lookup, text_processor, attachment_importer, poll_importer, settings)
+      @lookup = lookup
+      @text_processor = text_processor
+      @attachment_importer = attachment_importer
+      @poll_importer = poll_importer
+      @settings = settings
+    end
+
+    def map_post(row)
+      imported_user_id = row[:post_username].blank? ? row[:poster_id] : row[:post_username]
+      user_id = @lookup.user_id_from_imported_user_id(imported_user_id) || Discourse.system_user.id
+      is_first_post = row[:post_id] == row[:topic_first_post_id]
+
+      attachments = import_attachments(row, user_id)
+
+      mapped = {
+        id: row[:post_id],
+        user_id: user_id,
+        created_at: Time.zone.at(row[:post_time]),
+        raw: @text_processor.process_post(row[:post_text], attachments)
+      }
+
+      if is_first_post
+        map_first_post(row, mapped)
+      else
+        map_other_post(row, mapped)
+      end
+    end
+
+    protected
+
+    def import_attachments(row, user_id)
+      if @settings.import_attachments && row[:post_attachment] > 0
+        @attachment_importer.import_attachments(user_id, row[:post_id], row[:topic_id])
+      end
+    end
+
+    def map_first_post(row, mapped)
+      mapped[:category] = @lookup.category_id_from_imported_category_id(row[:forum_id])
+      mapped[:title] = CGI.unescapeHTML(row[:topic_title]).strip[0...255]
+      mapped[:pinned_at] = mapped[:created_at] unless row[:topic_type] == Constants::POST_NORMAL
+      mapped[:pinned_globally] = row[:topic_type] == Constants::POST_GLOBAL
+
+      add_poll(row, mapped) if @settings.import_polls
+      mapped
+    end
+
+    def map_other_post(row, mapped)
+      parent = @lookup.topic_lookup_from_imported_post_id(row[:topic_first_post_id])
+
+      if parent.blank?
+        puts "Parent post #{row[:topic_first_post_id]} doesn't exist. Skipping #{row[:post_id]}: #{row[:topic_title][0..40]}"
+        return nil
+      end
+
+      mapped[:topic_id] = parent[:topic_id]
+      mapped
+    end
+
+    def add_poll(row, mapped_post)
+      return if row[:poll_title].blank?
+
+      poll = Poll.new(row[:poll_title], row[:poll_max_options], row[:poll_end])
+      mapped_poll = @poll_importer.map_poll(row[:topic_id], poll)
+
+      if mapped_poll.present?
+        mapped_post[:raw] = mapped_poll[:raw] << "\n" << mapped_post[:raw]
+        mapped_post[:custom_fields] = mapped_poll[:custom_fields]
+      end
+    end
+  end
+end
---
+++ script/import_scripts/phpbb3/support/text_processor.rb
@@ -0,0 +1,133 @@
+module ImportScripts::PhpBB3
+  class TextProcessor
+    # @param lookup [ImportScripts::LookupContainer]
+    # @param database [ImportScripts::PhpBB3::Database_3_0 | ImportScripts::PhpBB3::Database_3_1]
+    # @param smiley_processor [ImportScripts::PhpBB3::SmileyProcessor]
+    # @param settings [ImportScripts::PhpBB3::Settings]
+    def initialize(lookup, database, smiley_processor, settings)
+      @lookup = lookup
+      @database = database
+      @smiley_processor = smiley_processor
+
+      @new_site_prefix = settings.new_site_prefix
+      create_internal_link_regexps(settings.original_site_prefix)
+    end
+
+    def process_raw_text(raw)
+      text = raw.dup
+      text = CGI.unescapeHTML(text)
+
+      clean_bbcodes(text)
+      process_smilies(text)
+      process_links(text)
+      process_lists(text)
+
+      text
+    end
+
+    def process_post(raw, attachments)
+      text = process_raw_text(raw)
+      text = process_attachments(text, attachments) if attachments.present?
+      text
+    end
+
+    def process_private_msg(raw, attachments)
+      text = process_raw_text(raw)
+      text = process_attachments(text, attachments) if attachments.present?
+      text
+    end
+
+    protected
+
+    def clean_bbcodes(text)
+      # Many phpbb bbcode tags have a hash attached to them. Examples:
+      #   [url=https&#58;//google&#46;com:1qh1i7ky]click here[/url:1qh1i7ky]
+      #   [quote=&quot;cybereality&quot;:b0wtlzex]Some text.[/quote:b0wtlzex]
+      text.gsub!(/:(?:\w{8})\]/, ']')
+    end
+
+    def process_smilies(text)
+      @smiley_processor.replace_smilies(text)
+    end
+
+    def process_links(text)
+      # Internal forum links can have this forms:
+      # for topics: <!-- l --><a class="postlink-local" href="https://example.com/forums/viewtopic.php?f=26&amp;t=3412">viewtopic.php?f=26&amp;t=3412</a><!-- l -->
+      # for posts: <!-- l --><a class="postlink-local" href="https://example.com/forums/viewtopic.php?p=1732#p1732">viewtopic.php?p=1732#p1732</a><!-- l -->
+      text.gsub!(@long_internal_link_regexp) do |link|
+        replace_internal_link(link, $1, $2)
+      end
+
+      # Some links look like this: <!-- m --><a class="postlink" href="http://www.onegameamonth.com">http://www.onegameamonth.com</a><!-- m -->
+      text.gsub!(/<!-- \w --><a(?:.+)href="(\S+)"(?:.*)>(.+)<\/a><!-- \w -->/i, '[\2](\1)')
+
+      # Replace internal forum links that aren't in the <!-- l --> format
+      text.gsub!(@short_internal_link_regexp) do |link|
+        replace_internal_link(link, $1, $2)
+      end
+
+      # phpBB shortens link text like this, which breaks our markdown processing:
+      #   [http://answers.yahoo.com/question/index ... 223AAkkPli](http://answers.yahoo.com/question/index?qid=20070920134223AAkkPli)
+      #
+      # Work around it for now:
+      text.gsub!(/\[http(s)?:\/\/(www\.)?/i, '[')
+    end
+
+    def replace_internal_link(link, import_topic_id, import_post_id)
+      if import_post_id.nil?
+        replace_internal_topic_link(link, import_topic_id)
+      else
+        replace_internal_post_link(link, import_post_id)
+      end
+    end
+
+    def replace_internal_topic_link(link, import_topic_id)
+      import_post_id = @database.get_first_post_id(import_topic_id)
+      return link if import_post_id.nil?
+
+      replace_internal_post_link(link, import_post_id)
+    end
+
+    def replace_internal_post_link(link, import_post_id)
+      topic = @lookup.topic_lookup_from_imported_post_id(import_post_id)
+      topic ? "#{@new_site_prefix}#{topic[:url]}" : link
+    end
+
+    def process_lists(text)
+      # convert list tags to ul and list=1 tags to ol
+      # list=a is not supported, so handle it like list=1
+      # list=9 and list=x have the same result as list=1 and list=a
+      text.gsub!(/\[list\](.*?)\[\/list:u\]/mi, '[ul]\1[/ul]')
+      text.gsub!(/\[list=.*?\](.*?)\[\/list:o\]/mi, '[ol]\1[/ol]')
+
+      # convert *-tags to li-tags so bbcode-to-md can do its magic on phpBB's lists:
+      text.gsub!(/\[\*\](.*?)\[\/\*:m\]/mi, '[li]\1[/li]')
+    end
+
+    # This replaces existing [attachment] BBCodes with the corresponding HTML tags for Discourse.
+    # All attachments that haven't been referenced in the text are appended to the end of the text.
+    def process_attachments(text, attachments)
+      attachment_regexp = /\[attachment=([\d])+\]<!-- [\w]+ -->([^<]+)<!-- [\w]+ -->\[\/attachment\]?/i
+      unreferenced_attachments = attachments.dup
+
+      text = text.gsub(attachment_regexp) do
+        index = $1.to_i
+        real_filename = $2
+        unreferenced_attachments[index] = nil
+        attachments.fetch(index, real_filename)
+      end
+
+      unreferenced_attachments = unreferenced_attachments.compact
+      text << "\n" << unreferenced_attachments.join("\n") unless unreferenced_attachments.empty?
+      text
+    end
+
+    def create_internal_link_regexps(original_site_prefix)
+      host = original_site_prefix.gsub('.', '\.')
+      link_regex = "http(?:s)?://#{host}/viewtopic\\.php\\?(?:\\S*)(?:t=(\\d+)|p=(\\d+)(?:#p\\d+)?)"
+
+      @long_internal_link_regexp = Regexp.new(%Q|<!-- l --><a(?:.+)href="#{link_regex}"(?:.*)</a><!-- l -->|, Regexp::IGNORECASE)
+      @short_internal_link_regexp = Regexp.new(link_regex, Regexp::IGNORECASE)
+    end
+  end
+end
