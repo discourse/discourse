c54de7159c266657cad115aaa79061e0ede2c146
Arpit Jalan <arpit@techapj.com>
2015-07-20 17:40:22 +0530

    UX: more permalinks page improvements

--- app/serializers/permalink_serializer.rb
+++ app/serializers/permalink_serializer.rb
@@ -1,5 +1,5 @@
 class PermalinkSerializer < ApplicationSerializer
-  attributes :id, :url, :topic_id, :topic_title, :topic_url, :post_id, :post_url, :category_id, :category_name, :category_url, :external_url
+  attributes :id, :url, :topic_id, :topic_title, :topic_url, :post_id, :post_url, :post_number, :post_topic_title, :category_id, :category_name, :category_url, :external_url
 
   def topic_title
     object.try(:topic).try(:title)
@@ -11,6 +11,14 @@ class PermalinkSerializer < ApplicationSerializer
 
   def post_url
     object.try(:post).try(:url)
+  end
+
+  def post_number
+    object.try(:post).try(:post_number)
+  end
+
+  def post_topic_title
+    object.try(:post).try(:topic).try(:title)
   end
 
   def category_name
