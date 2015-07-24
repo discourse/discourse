0d704ff8e995aada1a2aef468c553336d7a22f06
Sam <sam.saffron@gmail.com>
2015-07-24 14:08:32 +1000

    FIX: properly support HTTPS CDN on HTTP site
    Previously we changed all CDN links to schemaless.
    
    This is desirable for non HTTPS sites, to ease migration to HTTPS.
    It is not desirable for secure sites.
    
    Once site is secure or CDN is secure a rebake should be required
    to move it back to non-secure.

--- spec/components/cooked_post_processor_spec.rb
+++ spec/components/cooked_post_processor_spec.rb
@@ -317,11 +317,19 @@ describe CookedPostProcessor do
 
     context "when CDN is enabled" do
 
-      it "uses schemaless CDN url for uploads" do
+      it "does use schemaless CDN url for http uploads" do
         Rails.configuration.action_controller.stubs(:asset_host).returns("http://my.cdn.com")
         cpp.optimize_urls
         expect(cpp.html).to match_html '<a href="//my.cdn.com/uploads/default/2/2345678901234567.jpg">Link</a>
        <img src="//my.cdn.com/uploads/default/1/1234567890123456.jpg"><a href="http://www.google.com">Google</a>
+       <img src="http://foo.bar/image.png">'
+      end
+
+      it "does not use schemaless CDN url for https uploads" do
+        Rails.configuration.action_controller.stubs(:asset_host).returns("https://my.cdn.com")
+        cpp.optimize_urls
+        expect(cpp.html).to match_html '<a href="https://my.cdn.com/uploads/default/2/2345678901234567.jpg">Link</a>
+       <img src="https://my.cdn.com/uploads/default/1/1234567890123456.jpg"><a href="http://www.google.com">Google</a>
        <img src="http://foo.bar/image.png">'
       end
 
