2473a00b267353a5b00afb990974cbf3a38568cf
Régis Hanol <regis@hanol.fr>
2015-07-27 11:07:42 +0200

    FIX: improve smoke tests

--- db/fixtures/009_users.rb
+++ db/fixtures/009_users.rb
@@ -22,4 +22,30 @@ User.seed do |u|
   u.trust_level = TrustLevel[4]
 end
 
-Group.user_trust_level_change!(-1 ,TrustLevel[4])
+Group.user_trust_level_change!(-1, TrustLevel[4])
+
+# User for the smoke tests
+if ENV["SMOKE"] == "1"
+  smoke_user = User.seed do |u|
+    u.id = 0
+    u.name = "smoke_user"
+    u.username = "smoke_user"
+    u.username_lower = "smoke_user"
+    u.email = "smoke_user@discourse.org"
+    u.password = "P4ssw0rd"
+    u.email_direct = false
+    u.email_digests = false
+    u.email_private_messages = false
+    u.active = true
+    u.approved = true
+    u.approved_at = Time.now
+    u.trust_level = TrustLevel[3]
+  end.first
+
+  EmailToken.seed do |et|
+    et.id = 1
+    et.user_id = smoke_user.id
+    et.email = smoke_user.email
+    et.confirmed = true
+  end
+end
