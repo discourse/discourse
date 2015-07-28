b0ceff14384e8a2adae2eacb44e3d4e18fe0b521
Robin Ward <robin.ward@gmail.com>
2015-07-28 17:35:33 -0400

    Migrate many old moderation action posts to small actions

--- db/migrate/20150727193414_create_user_field_options.rb
+++ db/migrate/20150727193414_create_user_field_options.rb
@@ -1,6 +1,6 @@
 class CreateUserFieldOptions < ActiveRecord::Migration
   def change
-    create_table :user_field_options do |t|
+    create_table :user_field_options, force: true do |t|
       t.references :user_field, null: false
       t.string :value, null: false
       t.timestamps
