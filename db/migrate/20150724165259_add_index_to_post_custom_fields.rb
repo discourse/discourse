dc79fef7fb8fef0f004976d6ec53ede76f45b010
Sam <sam.saffron@gmail.com>
2015-07-28 10:50:02 +1000

    correct index on name value to drop long values

--- db/migrate/20150724165259_add_index_to_post_custom_fields.rb
+++ db/migrate/20150724165259_add_index_to_post_custom_fields.rb
@@ -1,5 +1,14 @@
 class AddIndexToPostCustomFields < ActiveRecord::Migration
-  def change
-    add_index :post_custom_fields, [:name, :value]
+  def up
+    execute <<SQL
+      CREATE INDEX index_post_custom_fields_on_name_and_value ON post_custom_fields USING btree (name, left(value, 200))
+SQL
+  end
+
+  def down
+    execute <<SQL
+      DROP INDEX index_post_custom_fields_on_name_and_value
+SQL
+
   end
 end
