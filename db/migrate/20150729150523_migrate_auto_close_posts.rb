201617811ebf55470b6eac4d9f4f385a6271ff3c
Robin Ward <robin.ward@gmail.com>
2015-07-29 12:20:35 -0400

    Migrate auto closed topics too

---
+++ db/migrate/20150729150523_migrate_auto_close_posts.rb
@@ -0,0 +1,16 @@
+class MigrateAutoClosePosts < ActiveRecord::Migration
+  def up
+    strings = []
+    %w(days hours lastpost_days lastpost_hours lastpost_minutes).map do |k|
+      strings << I18n.t("topic_statuses.autoclosed_enabled_#{k}.one")
+      strings << I18n.t("topic_statuses.autoclosed_enabled_#{k}.other").sub("%{count}", "\\d+")
+    end
+
+    sql = "UPDATE posts SET action_code = 'autoclosed.enabled', post_type = 3 "
+    sql << "WHERE post_type = 2 AND ("
+    sql << strings.map {|s| "raw ~* #{ActiveRecord::Base.connection.quote(s)}" }.join(' OR ')
+    sql << ")"
+
+    execute sql
+  end
+end
