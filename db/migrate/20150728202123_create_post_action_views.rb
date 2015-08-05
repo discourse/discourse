feb2457cc7ce7c46ba23497c9e5d34af30f1e581
Kane York <rikingcoding@gmail.com>
2015-08-05 12:44:02 -0700

    Make flags view explicit, not SELECT *

--- db/migrate/20150728202123_create_post_action_views.rb
+++ db/migrate/20150728202123_create_post_action_views.rb
@@ -17,7 +17,7 @@ SQL
 
     execute <<SQL
 CREATE VIEW flags AS
-SELECT *
+SELECT id, post_id, user_id, created_at, updated_at, deleted_by_id, deleted_at, post_action_type_id, related_post_id, staff_took_action, deferred_by_id, targets_topic, agreed_at, agreed_by_id, deferred_at, disagreed_at, disagreed_by_id
 FROM post_actions
 WHERE post_action_type_id IN (3,4,6,7,8)
 SQL
