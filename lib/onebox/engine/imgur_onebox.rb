b68f2b75ee7ffbffc6678eaa30d2c7d99c585d0c
Arpit Jalan <arpit@techapj.com>
2015-08-05 16:20:57 +0530

    rename twitter_data to imgur_data

--- lib/onebox/engine/imgur_onebox.rb
+++ lib/onebox/engine/imgur_onebox.rb
@@ -8,26 +8,26 @@ module Onebox
       always_https
 
       def to_html
-        twitter_data = get_twitter_data
-        return "<video width='#{twitter_data[:"player:width"]}' height='#{twitter_data[:"player:height"]}' controls autoplay loop><source src='#{twitter_data[:"player:stream"]}' type='video/mp4'></video>" if twitter_data[:"player:stream"]
-        return "<a href='#{url}' target='_blank'><img src='#{twitter_data[:image]}' alt='Imgur' height='#{twitter_data[:"image:height"]}' width='#{twitter_data[:"image:width"]}'></a>" if twitter_data[:image]
-        return "<a href='#{url}' target='_blank'><img src='#{twitter_data[:"image0:src"]}' alt='Imgur'></a>" if twitter_data[:"image0:src"]
+        imgur_data = get_imgur_data
+        return "<video width='#{imgur_data[:"player:width"]}' height='#{imgur_data[:"player:height"]}' controls autoplay loop><source src='#{imgur_data[:"player:stream"]}' type='video/mp4'></video>" if imgur_data[:"player:stream"]
+        return "<a href='#{url}' target='_blank'><img src='#{imgur_data[:image]}' alt='Imgur' height='#{imgur_data[:"image:height"]}' width='#{imgur_data[:"image:width"]}'></a>" if imgur_data[:image]
+        return "<a href='#{url}' target='_blank'><img src='#{imgur_data[:"image0:src"]}' alt='Imgur'></a>" if imgur_data[:"image0:src"]
         return nil
       end
 
       private
-      def get_twitter_data
+      def get_imgur_data
         response = Onebox::Helpers.fetch_response(url)
         html = Nokogiri::HTML(response.body)
-        twitter_data = {}
+        imgur_data = {}
         html.css('meta').each do |m|
           if m.attribute('name') && m.attribute('name').to_s.match(/^twitter:/i)
             m_content = m.attribute('content').to_s.strip
             m_name = m.attribute('name').to_s.gsub('twitter:', '')
-            twitter_data[m_name.to_sym] = m_content
+            imgur_data[m_name.to_sym] = m_content
           end
         end
-        return twitter_data
+        return imgur_data
       end
     end
   end
