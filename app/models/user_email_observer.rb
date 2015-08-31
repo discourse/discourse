d6d3d5e8628869c3a861e9a42a6a06446f14c6d4
Arpit Jalan <arpit@techapj.com>
2015-08-31 11:24:37 +0530

    FIX: don't send notification emails to deactivated user

--- app/models/user_email_observer.rb
+++ app/models/user_email_observer.rb
@@ -39,7 +39,7 @@ class UserEmailObserver < ActiveRecord::Observer
     private
 
     def enqueue(type)
-      return unless notification.user.email_direct?
+      return unless (notification.user.email_direct? && notification.user.active?)
 
       Jobs.enqueue_in(delay,
                      :user_email,
@@ -49,7 +49,7 @@ class UserEmailObserver < ActiveRecord::Observer
     end
 
     def enqueue_private(type)
-      return unless notification.user.email_private_messages?
+      return unless (notification.user.email_private_messages? && notification.user.active?)
 
       Jobs.enqueue_in(delay,
                       :user_email,
