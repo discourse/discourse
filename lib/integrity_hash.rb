1822de181a440b89309089132c3bc57d7a8a2937
Kane York <rikingcoding@gmail.com>
2015-09-07 01:01:21 -0700

    fix for production

--- lib/integrity_hash.rb
+++ lib/integrity_hash.rb
@@ -8,14 +8,25 @@ class Discourse::IntegrityHash
     asset = Rails.application.assets[asset_name]
     @integrity_hashes ||= {}
     if !@integrity_hashes[asset_name] || @integrity_hashes[asset_name][:mtime] != asset.mtime
-      digest = new_hash
-      coder = {}
-      asset.encode_with(coder)
-      digest.update(coder['source'])
-      @integrity_hashes[asset_name] = {
-        digest: "sha256-#{digest.base64digest}",
-        mtime: asset.mtime
-      }
+      if Rails.env.production?
+        if asset_name == 'vendor'
+          # FIXME https://code.google.com/p/chromium/issues/detail?id=527286
+          return ''
+        end
+        @integrity_hashes[asset_name] = {
+          digest: for_file(File.join(Rails.root, 'public/assets', asset.digest_path)),
+          mtime: asset.mtime
+        }
+      else
+        digest = new_hash
+        coder = {}
+        asset.encode_with(coder)
+        digest.update(coder['source'])
+        @integrity_hashes[asset_name] = {
+          digest: "sha256-#{digest.base64digest}",
+          mtime: asset.mtime
+        }
+      end
     end
     @integrity_hashes[asset_name][:digest]
   end
