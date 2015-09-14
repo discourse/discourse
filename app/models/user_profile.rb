7acc93b2a09c897b3a3e778535fa4ce3b1356106
Guo Xiang Tan <tgx_world@hotmail.com>
2015-09-14 15:51:17 +0800

    FEATURE: Track user profile views.

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -7,6 +7,7 @@ class UserProfile < ActiveRecord::Base
   after_save :trigger_badges
 
   belongs_to :card_image_badge, class_name: 'Badge'
+  has_many :user_profile_views, dependent: :destroy
 
   BAKED_VERSION = 1
 
