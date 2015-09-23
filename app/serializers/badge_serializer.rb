dbc17e33d4e1cdceeeeb4296e27ffa298103f21e
Gerhard Schlager <mail@gerhard-schlager.at>
2015-09-23 22:52:43 +0200

    FIX: Use user's locale for badge notifications

--- app/serializers/badge_serializer.rb
+++ app/serializers/badge_serializer.rb
@@ -12,32 +12,4 @@ class BadgeSerializer < ApplicationSerializer
   def include_long_description?
     options[:include_long_description]
   end
-
-  def long_description
-    if object.long_description.present?
-      object.long_description
-    else
-      key = "badges.long_descriptions.#{i18n_name}"
-      if I18n.exists?(key)
-        I18n.t(key)
-      else
-        ""
-      end
-    end
-  end
-
-  def slug
-    Slug.for(display_name, '')
-  end
-
-  private
-
-  def i18n_name
-    object.name.downcase.gsub(' ', '_')
-  end
-
-  def display_name
-    key = "admin_js.badges.badge.#{i18n_name}.name"
-    I18n.t(key, default: object.name)
-  end
 end
