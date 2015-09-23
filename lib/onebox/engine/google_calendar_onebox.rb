5f6eed6b9f0747aa52bc388ec5ced2dc899bdf95
Kane York <rikingcoding@gmail.com>
2015-09-23 14:08:16 -0700

    FEATURE: Add placeholders for google docs/calendar oneboxes

--- lib/onebox/engine/google_calendar_onebox.rb
+++ lib/onebox/engine/google_calendar_onebox.rb
@@ -8,9 +8,18 @@ module Onebox
 
       def to_html
         url = @url.split('&').first
-        "<iframe src='#{url}&rm=minimal' style='border: 0' width='800' height='600' frameborder='0' scrolling='no' ></iframe>"
+        "<iframe src='#{url}&rm=minimal' style='border: 0' width='800' height='600' frameborder='0' scrolling='no'>#{placeholder_html}</iframe>"
       end
 
+      def placeholder_html
+        <<HTML
+<div placeholder><div class='gdocs-onebox gdocs-onebox-splash' style='display:table-cell;vertical-align:middle;width:800px;height:600px'>
+<div style='text-align:center;'>
+<div class='gdocs-onebox-logo g-calendar-logo'></div>
+<p>Google Calendar</p>
+</div></div></div>
+HTML
+      end
     end
   end
 end
