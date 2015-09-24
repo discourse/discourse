6b4d56dc60ec0fb50ee18454c48a5db30bb2b425
Robin Ward <robin.ward@gmail.com>
2015-09-24 13:52:32 -0400

    Add better error messages for rate limits.

--- lib/edit_rate_limiter.rb
+++ lib/edit_rate_limiter.rb
@@ -1,6 +1,10 @@
 require 'rate_limiter'
 class EditRateLimiter < RateLimiter
   def initialize(user)
-    super(user, "edit-post:#{Date.today}", SiteSetting.max_edits_per_day, 1.day.to_i)
+    super(user, "edit-post", SiteSetting.max_edits_per_day, 1.day.to_i)
+  end
+
+  def build_key(type)
+    "#{super(type)}:#{Date.today}"
   end
 end
--- lib/rate_limiter/limit_exceeded.rb
+++ lib/rate_limiter/limit_exceeded.rb
@@ -3,11 +3,13 @@ class RateLimiter
   # A rate limit has been exceeded.
   class LimitExceeded < StandardError
 
-    def initialize(available_in)
+    def initialize(available_in, type=nil)
       @available_in = available_in
+      @type = type
     end
 
     def description
+
       time_left = ""
       if @available_in < 1.minute.to_i
         time_left = I18n.t("rate_limiter.seconds", count: @available_in)
@@ -16,6 +18,13 @@ class RateLimiter
       else
         time_left = I18n.t("rate_limiter.hours", count: (@available_in / 1.hour.to_i))
       end
+
+      if @type.present?
+        type_key = @type.gsub(/-/, '_')
+        msg = I18n.t("rate_limiter.by_type.#{type_key}", time_left: time_left, default: "")
+        return msg if msg.present?
+      end
+
       I18n.t("rate_limiter.too_many_requests", time_left: time_left)
     end
   end
