5bea9333705baee3a9373046293e1a5b4694c334
Régis Hanol <regis@hanol.fr>
2015-10-16 23:39:01 +0200

    fix build

--- lib/post_revisor.rb
+++ lib/post_revisor.rb
@@ -397,7 +397,8 @@ class PostRevisor
     matches.each do |match|
       next if match[0] =~ /\<img(.*)src=/ || match[0].blank?
       new_description = match[0]
-      new_description = nil if new_description == I18n.t("category.replace_paragraph")
+      # first 50 characters should be fine to test they haven't changed the default description
+      new_description = nil if new_description.starts_with?(I18n.t("category.replace_paragraph")[0..50])
       category.update_column(:description, new_description)
       @category_changed = category
       break
