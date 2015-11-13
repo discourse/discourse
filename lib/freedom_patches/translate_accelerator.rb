c5fb2927b194681a7ebcbfccd4190abcf7ec9126
Robin Ward <robin.ward@gmail.com>
2015-11-13 15:42:01 -0500

    Refactor to our own Discourse I18n backend
    This removes some monkey patches and makes testing easier.
    It will also support database backed I18n changes.

--- lib/freedom_patches/translate_accelerator.rb
+++ lib/freedom_patches/translate_accelerator.rb
@@ -9,27 +9,7 @@
 # This patch depends on the convention that locale yml files must be named [locale_name].yml
 
 module I18n
-  module Backend
 
-    class Simple
-      def available_locales
-        # in case you are wondering this is:
-        # Dir.glob( File.join(Rails.root, 'config', 'locales', 'client.*.yml') )
-        #    .map {|x| x.split('.')[-2]}.sort
-        LocaleSiteSetting.supported_locales.map(&:to_sym)
-      end
-    end
-
-    module Base
-      # force explicit loading
-      def load_translations(*filenames)
-        unless filenames.empty?
-          filenames.flatten.each { |filename| load_file(filename) }
-        end
-      end
-
-    end
-  end
   # this accelerates translation a tiny bit (halves the time it takes)
   class << self
     alias_method :translate_no_cache, :translate
@@ -59,9 +39,13 @@ module I18n
       end
     end
 
+    def ensure_all_loaded!
+      backend.fallbacks(locale).each {|l| ensure_loaded!(l) }
+    end
+
     def ensure_loaded!(locale)
       @loaded_locales ||= []
-      load_locale locale unless @loaded_locales.include?(locale)
+      load_locale(locale) unless @loaded_locales.include?(locale)
     end
 
     def translate(key, *args)
