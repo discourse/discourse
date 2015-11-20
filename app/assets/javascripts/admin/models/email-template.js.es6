11932dd9d89d022e495a5e0e4c3a48a1e5255efa
Robin Ward <robin.ward@gmail.com>
2015-11-20 12:30:04 -0500

    Can revert changes to email templates

---
+++ app/assets/javascripts/admin/models/email-template.js.es6
@@ -0,0 +1,12 @@
+import RestModel from 'discourse/models/rest';
+
+const { getProperties } = Ember;
+console.log(getProperties);
+
+export default RestModel.extend({
+  revert() {
+    return Discourse.ajax(`/admin/customize/email_templates/${this.get('id')}`, {
+      method: 'DELETE'
+    }).then(result => getProperties(result.email_template, 'subject', 'body', 'can_revert'));
+  }
+});
--- app/serializers/admin_email_template_serializer.rb
+++ app/serializers/admin_email_template_serializer.rb
@@ -1,5 +1,5 @@
 class AdminEmailTemplateSerializer < ApplicationSerializer
-  attributes :id, :title, :subject, :body
+  attributes :id, :title, :subject, :body, :can_revert?
 
   def id
     object
@@ -10,10 +10,19 @@ class AdminEmailTemplateSerializer < ApplicationSerializer
   end
 
   def subject
-    I18n.t("#{object}.subject_template")
+    @subject ||= I18n.t("#{object}.subject_template")
   end
 
   def body
-    I18n.t("#{object}.text_body_template")
+    @body ||= I18n.t("#{object}.text_body_template")
+  end
+
+  def can_revert?
+    current_body, current_subject = body, subject
+
+    I18n.overrides_disabled do
+      return I18n.t("#{object}.subject_template") != current_subject ||
+             I18n.t("#{object}.text_body_template") != current_body
+    end
   end
 end
