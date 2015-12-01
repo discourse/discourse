1f7c03df5c70f93956620df17768d4b7541d54bd
Sam <sam.saffron@gmail.com>
2015-12-01 12:12:55 +1100

    FIX: missing email notifications for group mentions

--- app/mailers/user_notifications.rb
+++ app/mailers/user_notifications.rb
@@ -113,6 +113,13 @@ class UserNotifications < ActionMailer::Base
     notification_email(user, opts)
   end
 
+  def group_mentioned(user, opts)
+    opts[:allow_reply_by_email] = true
+    opts[:use_site_subject] = true
+    opts[:show_category_in_subject] = true
+    notification_email(user, opts)
+  end
+
   def user_posted(user, opts)
     opts[:allow_reply_by_email] = true
     opts[:use_site_subject] = true
