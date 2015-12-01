12ef6ce822724fbefa2c9ef1b6c11192ed9d530c
Sam <sam.saffron@gmail.com>
2015-12-01 12:12:55 +1100

    FIX: missing email notifications for group mentions

--- app/jobs/regular/user_email.rb
+++ app/jobs/regular/user_email.rb
@@ -57,9 +57,10 @@ module Jobs
       return skip(skip_reason) if skip_reason
 
       # Make sure that mailer exists
-      raise Discourse::InvalidParameters.new(:type) unless UserNotifications.respond_to?(args[:type])
+      raise Discourse::InvalidParameters.new("type=#{args[:type]}") unless UserNotifications.respond_to?(args[:type])
 
       message = UserNotifications.send(args[:type], @user, email_args)
+
       # Update the to address if we have a custom one
       if args[:to_address].present?
         message.to = [args[:to_address]]
