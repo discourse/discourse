ed7e167e88f2e18d7a6bc8b0d1e7135b483cc56f
Arpit Jalan <arpit@techapj.com>
2015-12-04 08:16:00 +0530

    FIX: switch to iframe when embedding YouTube playlist

--- plugins/lazyYT/plugin.rb
+++ plugins/lazyYT/plugin.rb
@@ -17,7 +17,7 @@ class Onebox::Engine::YoutubeOnebox
   alias_method :yt_onebox_to_html, :to_html
 
   def to_html
-    if video_id
+    if video_id && !params['list']
       video_width = (params['width'] && params['width'].to_i <= 695) ? params['width'] : 480 # embed width
       video_height = (params['height'] && params['height'].to_i <= 500) ? params['height'] : 270 # embed height
 
