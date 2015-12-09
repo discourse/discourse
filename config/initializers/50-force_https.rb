47e718f5b24cb2d8b991f5b1a51efe3f79f5f934
Sam <sam.saffron@gmail.com>
2015-12-09 11:44:38 +1100

    Revert message bus upgrade

--- config/initializers/50-force_https.rb
+++ config/initializers/50-force_https.rb
@@ -12,3 +12,6 @@ class Discourse::ForceHttpsMiddleware
 
 end
 
+# this code plays up, skip for now
+#Rails.configuration.middleware.insert_before MessageBus::Rack::Middleware, Discourse::ForceHttpsMiddleware
+
