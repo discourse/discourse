96aa5b865ffc50e7ba25f343c0cfeed967552353
Régis Hanol <regis@hanol.fr>
2016-01-12 09:53:09 +0100

    FIX: mentions regex server-side (ie. don't count email addresses)

--- spec/models/post_analyzer_spec.rb
+++ spec/models/post_analyzer_spec.rb
@@ -205,5 +205,10 @@ describe PostAnalyzer do
       post_analyzer = PostAnalyzer.new("@Jake @Finn @Jake_Old", default_topic_id)
       expect(post_analyzer.raw_mentions).to eq(['jake', 'finn', 'jake_old'])
     end
+
+    it "ignores emails" do
+      post_analyzer = PostAnalyzer.new("1@test.com 1@best.com @best @not", default_topic_id)
+      expect(post_analyzer.raw_mentions).to eq(['best', 'not'])
+    end
   end
 end
