0058d09e35bf765ce1e91bbd636549ecb24c7348
Guo Xiang Tan <tgx_world@hotmail.com>
2016-01-26 15:46:51 +0800

    Second attempt which removes any kind proxying.

--- spec/components/active_record/connection_adapters/postgresql_fallback_adapter_spec.rb
+++ spec/components/active_record/connection_adapters/postgresql_fallback_adapter_spec.rb
@@ -1,55 +1,67 @@
 require 'rails_helper'
 require_dependency 'active_record/connection_adapters/postgresql_fallback_adapter'
 
-describe ActiveRecord::ConnectionAdapters::PostgreSQLFallbackAdapter do
-  let(:master_connection) { ActiveRecord::Base.connection }
-  let(:replica_connection) { master_connection.dup }
-  let(:adapter) { described_class.new(master_connection, replica_connection, nil, nil) }
-
-  before :each do
-    ActiveRecord::Base.clear_all_connections!
+describe ActiveRecord::ConnectionHandling do
+  let(:config) do
+    ActiveRecord::Base.configurations["test"].merge({
+      "adapter" => "postgresql_fallback",
+      "replica_host" => "localhost",
+      "replica_port" => "6432"
+    })
   end
 
-  describe "proxy_method" do
-    context "when master connection is not active" do
+  after do
+    ActiveRecord::Base.clear_all_connections!
+    Discourse.disable_readonly_mode
+  end
+
+  describe "#postgresql_fallback_connection" do
+    it 'should return a PostgreSQL adapter' do
+      expect(ActiveRecord::Base.postgresql_fallback_connection(config))
+        .to be_an_instance_of(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+    end
+
+    context 'when master server is down' do
       before do
-        replica_connection.stubs(:send)
-        master_connection.stubs(:send).raises(ActiveRecord::StatementInvalid.new('PG::UnableToSend'))
-        master_connection.stubs(:reconnect!)
-        master_connection.stubs(:active?).returns(false)
+        @replica_connection = mock('replica_connection')
 
-        @old_const = described_class::HEARTBEAT_INTERVAL
-        described_class.const_set("HEARTBEAT_INTERVAL", 0.1)
+        ActiveRecord::Base.expects(:postgresql_connection).with(config).raises(PG::ConnectionBad)
+
+        ActiveRecord::Base.expects(:postgresql_connection).with(config.merge({
+          "host" => "localhost", "port" => "6432"
+        })).returns(@replica_connection)
+
+        ActiveRecord::Base.expects(:verify_replica).with(@replica_connection)
+
+        @replica_connection.expects(:disconnect!)
+
+        ActiveRecord::Base.stubs(:interval).returns(0.1)
+
+        Concurrent::TimerTask.any_instance.expects(:shutdown)
       end
 
-      after do
-        Discourse.disable_readonly_mode
-        described_class.const_set("HEARTBEAT_INTERVAL", @old_const)
-      end
+      it 'should failover to a replica server' do
+        ActiveRecord::Base.postgresql_fallback_connection(config)
 
-      it "should set site to readonly mode and carry out failover and switch back procedures" do
-        expect(adapter.main_connection).to eq(master_connection)
-        adapter.proxy_method('some method')
         expect(Discourse.readonly_mode?).to eq(true)
-        expect(adapter.main_connection).to eq(replica_connection)
 
-        master_connection.stubs(:active?).returns(true)
+        ActiveRecord::Base.unstub(:postgresql_connection)
         sleep 0.15
 
         expect(Discourse.readonly_mode?).to eq(false)
-        expect(adapter.main_connection).to eq(master_connection)
+
+        expect(ActiveRecord::Base.connection)
+          .to be_an_instance_of(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
       end
     end
 
-    it 'should raise errors not related to the database connection' do
-      master_connection.stubs(:send).raises(StandardError.new)
-      expect { adapter.proxy_method('some method') }.to raise_error(StandardError)
-    end
+    context 'when both master and replica server is down' do
+      it 'should raise the right error' do
+        ActiveRecord::Base.expects(:postgresql_connection).raises(PG::ConnectionBad).twice
 
-    it 'should proxy methods successfully' do
-      expect(adapter.proxy_method(:execute, 'SELECT 1').values[0][0]).to eq("1")
-      expect(adapter.proxy_method(:active?)).to eq(true)
-      expect(adapter.proxy_method(:raw_connection)).to eq(master_connection.raw_connection)
+        expect { ActiveRecord::Base.postgresql_fallback_connection(config) }
+          .to raise_error(PG::ConnectionBad)
+      end
     end
   end
 end
