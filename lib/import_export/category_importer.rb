3053c1ebd8df22085754e4ea9f6fe80ee0ee688f
Neil Lalonde <neillalonde@gmail.com>
2016-01-28 15:17:11 -0500

    category export: include category permissions and custom groups

--- lib/import_export/category_importer.rb
+++ lib/import_export/category_importer.rb
@@ -11,11 +11,26 @@ module ImportExport
       RateLimiter.disable
 
       import_users
+      import_groups
       import_categories
       import_topics
       self
     ensure
       RateLimiter.enable
+    end
+
+    def import_groups
+      return if @export_data[:groups].empty?
+
+      @export_data[:groups].each do |group_data|
+        g = group_data.dup
+        user_ids = g.delete(:user_ids)
+        external_id = g.delete(:id)
+        new_group = Group.find_by_name(g[:name]) || Group.create!(g)
+        user_ids.each do |external_user_id|
+          new_group.add( User.find(@topic_importer.new_user_id(external_user_id)) )
+        end
+      end
     end
 
     def import_users
@@ -24,18 +39,23 @@ module ImportExport
 
     def import_categories
       id = @export_data[:category].delete(:id)
+      permissions = @export_data[:category].delete(:permissions_params)
+
       parent = Category.new(@export_data[:category])
       parent.user_id = @topic_importer.new_user_id(@export_data[:category][:user_id]) # imported user's new id
       parent.custom_fields["import_id"] = id
+      parent.permissions = permissions if permissions
       parent.save!
       set_category_description(parent, @export_data[:category][:description])
 
       @export_data[:subcategories].each do |cat_attrs|
         id = cat_attrs.delete(:id)
+        permissions = cat_attrs.delete(:permissions_params)
         subcategory = Category.new(cat_attrs)
         subcategory.parent_category_id = parent.id
         subcategory.user_id = @topic_importer.new_user_id(cat_attrs[:user_id])
         subcategory.custom_fields["import_id"] = id
+        subcategory.permissions = permissions if permissions
         subcategory.save!
         set_category_description(subcategory, cat_attrs[:description])
       end
