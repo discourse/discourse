38ecd5925f203e69b3f06a73af59a753d8c0262c
Guo Xiang Tan <tgx_world@hotmail.com>
2016-01-29 13:31:11 +0800

    Convert to link as long as a valid link is received.

--- lib/pretty_text.rb
+++ lib/pretty_text.rb
@@ -50,7 +50,7 @@ module PrettyText
 
     def category_hashtag_lookup(category_slug)
       if category = Category.query_from_hashtag_slug(category_slug)
-        ['category', category.url_with_id]
+        category.url_with_id
       else
         nil
       end
