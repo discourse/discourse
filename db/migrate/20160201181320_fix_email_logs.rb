2c6ddf3f3328ce4575178412d1f3d0cc1dabfa25
Régis Hanol <regis@hanol.fr>
2016-02-01 19:37:10 +0100

    migration to fix email_logs not attached to a user

---
+++ db/migrate/20160201181320_fix_email_logs.rb
@@ -0,0 +1,16 @@
+class FixEmailLogs < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      UPDATE email_logs
+         SET user_id = u.id
+        FROM email_logs el
+   LEFT JOIN users u ON u.email = el.to_address
+       WHERE email_logs.id = el.id
+         AND email_logs.user_id IS NULL
+         AND NOT email_logs.skipped
+    SQL
+  end
+
+  def down
+  end
+end
