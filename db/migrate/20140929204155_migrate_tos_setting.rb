d41cbc5a3058ff4f4617860d26086a938500f879
Robin Ward <robin.ward@gmail.com>
2016-02-02 11:49:57 -0500

    FIX: Disable overrides in migrations
    Site texts did not exist when this migration was created.

--- db/migrate/20140929204155_migrate_tos_setting.rb
+++ db/migrate/20140929204155_migrate_tos_setting.rb
@@ -2,7 +2,12 @@ class MigrateTosSetting < ActiveRecord::Migration
   def up
     res = execute("SELECT * FROM site_settings WHERE name = 'tos_accept_required' AND value = 't'")
     if res.present? && res.cmd_tuples > 0
-      label = I18n.t("terms_of_service.signup_form_message")
+      label = nil
+
+      I18n.overrides_disabled do
+        label = I18n.t("terms_of_service.signup_form_message")
+      end
+
       res = execute("SELECT value FROM site_texts WHERE text_type = 'tos_signup_form_message'")
       if res.present? && res.cmd_tuples == 1
         label = res[0]['value']
