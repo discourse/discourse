886273f158bb677e924eedaf5a314b16fd51b742
Sam <sam.saffron@gmail.com>
2016-02-05 13:05:47 +1100

    FIX: when CDN assets are not in root path source maps fail

--- lib/global_path.rb
+++ lib/global_path.rb
@@ -6,4 +6,13 @@ module GlobalPath
   def cdn_path(p)
     "#{GlobalSetting.cdn_url}#{path(p)}"
   end
+
+  def cdn_relative_path(path)
+    if (cdn_url = GlobalSetting.cdn_url).present?
+      URI.parse(cdn_url).path + path
+    else
+      path
+    end
+  end
+
 end
