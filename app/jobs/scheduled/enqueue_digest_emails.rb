460665895c91b2f9018e361b393d7e00dc86b418
Régis Hanol <regis@hanol.fr>
2016-02-08 22:18:52 +0100

    properly name purge emails/email logs site settings

--- app/jobs/scheduled/enqueue_digest_emails.rb
+++ app/jobs/scheduled/enqueue_digest_emails.rb
@@ -19,7 +19,7 @@ module Jobs
                   .not_suspended
                   .where("COALESCE(last_emailed_at, '2010-01-01') <= CURRENT_TIMESTAMP - ('1 DAY'::INTERVAL * digest_after_days)")
                   .where("COALESCE(last_seen_at, '2010-01-01') <= CURRENT_TIMESTAMP - ('1 DAY'::INTERVAL * digest_after_days)")
-                  .where("COALESCE(last_seen_at, '2010-01-01') >= CURRENT_TIMESTAMP - ('1 DAY'::INTERVAL * #{SiteSetting.suppress_digest_email_after_days})")
+                  .where("COALESCE(last_seen_at, '2010-01-01') >= CURRENT_TIMESTAMP - ('1 DAY'::INTERVAL * #{SiteSetting.delete_digest_email_after_days})")
 
       # If the site requires approval, make sure the user is approved
       if SiteSetting.must_approve_users?
