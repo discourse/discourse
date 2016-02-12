06c9e799844a622971b2af4874d722a20896687c
Neil Lalonde <neillalonde@gmail.com>
2016-02-12 14:33:13 -0500

    FIX: pending flags reminder email was ignoring the 'notify about flags after' site setting.

--- app/jobs/scheduled/pending_flags_reminder.rb
+++ app/jobs/scheduled/pending_flags_reminder.rb
@@ -9,7 +9,7 @@ module Jobs
     def execute(args)
       if SiteSetting.notify_about_flags_after > 0 &&
          PostAction.flagged_posts_count > 0 &&
-         FlagQuery.flagged_post_actions('active').where('post_actions.created_at < ?', 48.hours.ago).pluck(:id).count > 0
+         FlagQuery.flagged_post_actions('active').where('post_actions.created_at < ?', SiteSetting.notify_about_flags_after.to_i.hours.ago).pluck(:id).count > 0
 
           message = PendingFlagsMailer.notify
           Email::Sender.new(message, :pending_flags_reminder).send
