899df009ff8906077a4211fbb45877cdf4e68d39
Gerhard Schlager <mail@gerhard-schlager.at>
2016-02-21 17:38:04 +0100

    Don't try to import invalid websites

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,8 +1,10 @@
 class UserProfile < ActiveRecord::Base
   belongs_to :user, inverse_of: :user_profile
 
+  WEBSITE_REGEXP = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,9}(([0-9]{1,5})?\/.*)?$)/ix
+
   validates :bio_raw, length: { maximum: 3000 }
-  validates :website, format: { with: /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,9}(([0-9]{1,5})?\/.*)?$)/ix }, allow_blank: true
+  validates :website, format: { with: WEBSITE_REGEXP }, allow_blank: true
   validates :user, presence: true
   before_save :cook
   after_save :trigger_badges
