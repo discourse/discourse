8a39577a35e22f3bbc9aa5c081147ef9ebe3ea87
Guo Xiang Tan <tgx_world@hotmail.com>
2016-03-02 22:01:48 +0800

    FEATURE: Master-Slave Redis configuration with fallback and switch over.

--- app/models/global_setting.rb
+++ app/models/global_setting.rb
@@ -43,6 +43,8 @@ class GlobalSetting
         c = {}
         c[:host] = redis_host if redis_host
         c[:port] = redis_port if redis_port
+        c[:slave_host] = redis_slave_host if redis_slave_host
+        c[:slave_port] = redis_slave_port if redis_slave_port
         c[:password] = redis_password if redis_password.present?
         c[:db] = redis_db if redis_db != 0
         c[:db] = 1 if Rails.env == "test"
@@ -52,6 +54,7 @@ class GlobalSetting
             {host: host, port: port}
           end.to_a
         end
+        c[:connector] = DiscourseRedis::Connector
         c.freeze
       end
   end
