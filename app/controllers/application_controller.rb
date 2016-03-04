777ee7e8a39827a37e208337134ba0c51971e688
Sam Saffron <sam.saffron@gmail.com>
2016-03-04 23:17:14 +1100

    Only pull in gem if it is being used, remove middleware

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -321,8 +321,10 @@ class ApplicationController < ActionController::Base
       begin
         # Rails I18n uses underscores between the locale and the region; the request
         # headers use hyphens.
+        require 'http_accept_language' unless defined? HttpAcceptLanguage
         available_locales = I18n.available_locales.map { |locale| locale.to_s.gsub(/_/, '-') }
-        http_accept_language.language_region_compatible_from(available_locales).gsub(/-/, '_')
+        parser = HttpAcceptLanguage::Parser.new(request.env["HTTP_ACCEPT_LANGUAGE"])
+        parser.language_region_compatible_from(available_locales).gsub(/-/, '_')
       rescue
         # If Accept-Language headers are not set.
         I18n.default_locale
