77367ae157416d2cf5f128d6e9b9f978dcd04ece
Neil Lalonde <neillalonde@gmail.com>
2016-03-07 13:40:14 -0500

    FIX: add path to cookie on subfolder installs

--- config/initializers/100-session_store.rb
+++ config/initializers/100-session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Discourse::Application.config.session_store :cookie_store, key: '_forum_session'
+Discourse::Application.config.session_store(
+  :cookie_store,
+  key: '_forum_session',
+  path: (Rails.application.config.relative_url_root.nil?) ? '/' : Rails.application.config.relative_url_root
+)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
