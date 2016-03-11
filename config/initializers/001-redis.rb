8aa3cdcb9f30dcabd065cc0b0fc30eee5d37c8fa
Guo Xiang Tan <tgx_world@hotmail.com>
2016-03-11 17:25:24 +0800

    FIX: Load Redis patch much earlier.

--- config/initializers/001-redis.rb
+++ config/initializers/001-redis.rb
@@ -1,18 +1,3 @@
-# https://github.com/redis/redis-rb/pull/591
-class Redis
-  class Client
-    alias_method :old_initialize, :initialize
-
-    def initialize(options = {})
-      old_initialize(options)
-
-      if options.include?(:connector) && options[:connector].is_a?(Class)
-        @connector = options[:connector].new(@options)
-      end
-    end
-  end
-end
-
 if Rails.env.development? && ENV['DISCOURSE_FLUSH_REDIS']
   puts "Flushing redis (development mode)"
   $redis.flushall
