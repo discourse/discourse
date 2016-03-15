9d36f9c33c97c1fcbc59c6dffdb905c36ba6bbc8
Erick Guan <fantasticfears@gmail.com>
2016-03-15 08:21:26 +0100

    FIX: https support for youku onebox

--- spec/lib/onebox/engine/youku_onebox_spec.rb
+++ spec/lib/onebox/engine/youku_onebox_spec.rb
@@ -6,8 +6,8 @@ describe Onebox::Engine::YoukuOnebox do
     FakeWeb.register_uri(:get, 'http://v.youku.com/player/getPlayList/VideoIDS/XNjM3MzAxNzc2', body: response('youku-meta'), content_type: 'text/html')
   end
 
-  it 'returns iframe as the placeholder' do
+  it 'returns embed as the placeholder' do
     expect(Onebox.preview('http://v.youku.com/v_show/id_XNjM3MzAxNzc2.html')
-        .placeholder_html).to match(/iframe/)
+        .placeholder_html).to match(/embed/)
   end
 end
