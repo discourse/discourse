699b0d3e575752bf5a706fe1502f830292509a64
Neil Lalonde <neillalonde@gmail.com>
2016-03-17 12:19:35 -0400

    Version bump to v1.5.0.beta13

--- lib/version.rb
+++ lib/version.rb
@@ -5,7 +5,7 @@ module Discourse
       MAJOR = 1
       MINOR = 5
       TINY  = 0
-      PRE   = 'beta12'
+      PRE   = 'beta13'
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     end
