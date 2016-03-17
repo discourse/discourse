95f2ac65354ad60a917b59bd4dbf3944785a17ea
Neil Lalonde <neillalonde@gmail.com>
2016-03-17 12:18:18 -0400

    Version bump to v1.4.7

--- lib/version.rb
+++ lib/version.rb
@@ -4,7 +4,7 @@ module Discourse
     module VERSION #:nodoc:
       MAJOR = 1
       MINOR = 4
-      TINY  = 6
+      TINY  = 7
       PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
