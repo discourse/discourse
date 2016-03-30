9256b7aa26035a63d481b1a07e429cba7cd1dd61
Sam <sam.saffron@gmail.com>
2016-03-30 11:17:52 +1100

    FIX: unread and new count not removing deleted topics on the fly FIX: unread PMs interfering with unread count

--- lib/post_destroyer.rb
+++ lib/post_destroyer.rb
@@ -65,6 +65,7 @@ class PostDestroyer
   def staff_recovered
     @post.recover!
     @post.publish_change_to_clients! :recovered
+    TopicTrackingState.publish_recover(@post.topic) if @post.topic && @post.post_number == 1
   end
 
   # When a post is properly deleted. Well, it's still soft deleted, but it will no longer
@@ -96,6 +97,7 @@ class PostDestroyer
 
     feature_users_in_the_topic if @post.topic
     @post.publish_change_to_clients! :deleted if @post.topic
+    TopicTrackingState.publish_delete(@post.topic) if @post.topic && @post.post_number == 1
   end
 
   # When a user 'deletes' their own post. We just change the text.
