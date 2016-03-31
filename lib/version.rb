aa91e4c1ac110063d7d1acaa3a541dbeb146e9d8
Neil Lalonde <neillalonde@gmail.com>
2016-03-31 17:51:08 -0400

    Version bump to v1.5.0

--- lib/version.rb
+++ lib/version.rb
@@ -3,8 +3,8 @@ module Discourse
   unless defined? ::Discourse::VERSION
     module VERSION #:nodoc:
       MAJOR = 1
-      MINOR = 4
-      TINY  = 7
+      MINOR = 5
+      TINY  = 0
       PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
