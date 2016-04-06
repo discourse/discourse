4f10b5e940e7b9bcc83279fb6a804f7457f7bd1a
Sam <sam.saffron@gmail.com>
2016-04-06 13:18:11 +1000

    avoid exception for general case

--- app/models/user_first.rb
+++ app/models/user_first.rb
@@ -5,6 +5,9 @@ class UserFirst < ActiveRecord::Base
   end
 
   def self.create_for(user_id, type, post_id=nil)
+    # the usual case will be that it is already in table, don't try to insert
+    return false if UserFirst.exists?(user_id: user_id, first_type: types[type])
+
     create!(user_id: user_id, first_type: types[type], post_id: post_id)
     true
   rescue PG::UniqueViolation, ActiveRecord::RecordNotUnique
