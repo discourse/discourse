b4abc761c776741af736fd9bed640c5a1e3ee06b
Robin Ward <robin.ward@gmail.com>
2016-04-07 15:07:24 -0400

    Try running once off jobs outside of db:migrate

--- app/jobs/onceoff.rb
+++ app/jobs/onceoff.rb
@@ -10,15 +10,21 @@ class Jobs::Onceoff < Jobs::Base
     job_name = self.class.name_for(self.class)
 
     if args[:force] || !OnceoffLog.where(job_name: job_name).exists?
-      execute_onceoff(args)
-      OnceoffLog.create(job_name: job_name)
+      DistributedMutex.synchronize(self.class.name) do
+        execute_onceoff(args)
+        OnceoffLog.create(job_name: job_name)
+      end
     end
   end
 
   def self.enqueue_all
+    previously_ran = OnceoffLog.pluck(:job_name).uniq
+
     ObjectSpace.each_object(Class).select { |klass| klass < self }.each do |klass|
-      job_name = name_for(klass).underscore.to_sym
-      Jobs.enqueue(job_name)
+      job_name = name_for(klass)
+      unless previously_ran.include?(job_name)
+        Jobs.enqueue(job_name.underscore.to_sym)
+      end
     end
   end
 
