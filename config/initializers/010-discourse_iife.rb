7f453b02f14329bfdea357cdffede370d171a920
Guo Xiang Tan <tgx_world@hotmail.com>
2016-04-11 11:36:35 +0800

    `Rails.application.assets` returns `nil` in production.

--- config/initializers/010-discourse_iife.rb
+++ config/initializers/010-discourse_iife.rb
@@ -2,9 +2,9 @@ require 'discourse_iife'
 
 Rails.application.config.assets.configure do |env|
   env.register_preprocessor('application/javascript', DiscourseIIFE)
-end
 
-unless Rails.env.production? || ENV["DISABLE_EVAL"]
-  require 'source_url'
-  Rails.application.config.assets.register_postprocessor('application/javascript', SourceURL)
+  unless Rails.env.production? || ENV["DISABLE_EVAL"]
+    require 'source_url'
+    env.register_postprocessor('application/javascript', SourceURL)
+  end
 end
