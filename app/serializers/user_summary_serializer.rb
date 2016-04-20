2016e1cda1d1bd8247f1b285e3ec955a04ebe556
Régis Hanol <regis@hanol.fr>
2016-04-20 22:58:30 +0200

    UX: show topics where the top links are extracted from in user summary

--- app/serializers/user_summary_serializer.rb
+++ app/serializers/user_summary_serializer.rb
@@ -1,6 +1,7 @@
 class UserSummarySerializer < ApplicationSerializer
-  class TopicSerializer < BasicTopicSerializer
-    attributes :like_count, :slug, :created_at
+
+  class TopicSerializer < ApplicationSerializer
+    attributes :id, :created_at, :fancy_title, :slug, :like_count
   end
 
   class ReplySerializer < ApplicationSerializer
@@ -9,7 +10,12 @@ class UserSummarySerializer < ApplicationSerializer
   end
 
   class LinkSerializer < ApplicationSerializer
-    attributes :url, :title, :clicks, :domain
+    attributes :url, :title, :clicks, :post_number
+    has_one :topic, serializer: TopicSerializer
+
+    def post_number
+      object.post.post_number
+    end
   end
 
   class MostLikedByUserSerializer < BasicUserSerializer
@@ -18,9 +24,9 @@ class UserSummarySerializer < ApplicationSerializer
 
   has_many :topics, serializer: TopicSerializer
   has_many :replies, serializer: ReplySerializer, embed: :object
-  has_many :badges, serializer: UserBadgeSerializer, embed: :object
   has_many :links, serializer: LinkSerializer, embed: :object
   has_many :most_liked_by_users, serializer: MostLikedByUserSerializer, embed: :object
+  has_many :badges, serializer: UserBadgeSerializer, embed: :object
 
   attributes :likes_given,
              :likes_received,
