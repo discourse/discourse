9b01edd96145c4afe51103dd42c0f3ed5e01c70c
Régis Hanol <regis@hanol.fr>
2016-05-14 00:08:19 +0200

    SECURITY: 2 XSSs in post gutter and local oneboxes

--- lib/onebox/engine/discourse_local_onebox.rb
+++ lib/onebox/engine/discourse_local_onebox.rb
@@ -96,7 +96,7 @@ module Onebox
 
             quote = post.excerpt(SiteSetting.post_onebox_maxlength)
             args = { original_url: url,
-                     title: PrettyText.unescape_emoji(topic.title),
+                     title: PrettyText.unescape_emoji(CGI::escapeHTML(topic.title)),
                      avatar: PrettyText.avatar_img(topic.user.avatar_template, 'tiny'),
                      posts_count: topic.posts_count,
                      last_post: FreedomPatches::Rails4.time_ago_in_words(topic.last_posted_at, false, scope: :'datetime.distance_in_words_verbose'),
