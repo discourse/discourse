9b96f234732d192b45b6da533dbc14c34c338d32
Guo Xiang Tan <tgx_world@hotmail.com>
2016-06-15 18:40:13 +0800

    Include cdn path in the stylesheet digest.

--- spec/components/discourse_stylesheets_spec.rb
+++ spec/components/discourse_stylesheets_spec.rb
@@ -29,4 +29,18 @@ describe DiscourseStylesheets do
     end
   end
 
+  describe "#digest" do
+    before do
+      described_class.expects(:max_file_mtime).returns(Time.new(2016, 06, 05, 12, 30, 0, 0))
+    end
+
+    it "should return a digest" do
+      expect(described_class.new.digest).to eq('0e6c2e957cfc92ed60661c90ec3345198ccef887')
+    end
+
+    it "should include the cdn url when generating the digest" do
+      GlobalSetting.expects(:cdn_url).returns('https://fastly.maxcdn.org')
+      expect(described_class.new.digest).to eq('4995163b1232c54c8ed3b44200d803a90bc47613')
+    end
+  end
 end
