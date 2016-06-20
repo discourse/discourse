e9a293beeb847a65bd39c08aaf919f1a8fef9404
Régis Hanol <regis@hanol.fr>
2016-06-20 22:05:41 +0200

    FIX: clean up uploads job

--- spec/jobs/clean_up_uploads_spec.rb
+++ spec/jobs/clean_up_uploads_spec.rb
@@ -3,8 +3,21 @@ require 'rails_helper'
 require_dependency 'jobs/scheduled/clean_up_uploads'
 
 describe Jobs::CleanUpUploads do
-  it "runs correctly without crashing" do
+
+  before do
+    Upload.destroy_all
     SiteSetting.clean_up_uploads = true
-    Jobs::CleanUpUploads.new.execute(nil)
+    SiteSetting.clean_orphan_uploads_grace_period_hours = 1
   end
+
+  it "deletes orphan uploads" do
+    Fabricate(:upload, created_at: 2.hours.ago)
+
+    expect(Upload.count).to be(1)
+
+    Jobs::CleanUpUploads.new.execute(nil)
+
+    expect(Upload.count).to be(0)
+  end
+
 end
