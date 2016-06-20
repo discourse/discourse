25f4747ea21e0977c021f68e7f21e275d84e4022
scossar <scossar3@gmail.com>
2016-06-20 14:52:53 -0700

    add m4a to audio onebox filetypes

--- lib/onebox/engine/audio_onebox.rb
+++ lib/onebox/engine/audio_onebox.rb
@@ -3,7 +3,7 @@ module Onebox
     class AudioOnebox
       include Engine
 
-      matches_regexp /^(https?:)?\/\/.*\.(mp3|ogg|wav)(\?.*)?$/
+      matches_regexp /^(https?:)?\/\/.*\.(mp3|ogg|wav|m4a)(\?.*)?$/
 
       def always_https?
         WhitelistedGenericOnebox.host_matches(uri, WhitelistedGenericOnebox.https_hosts)
