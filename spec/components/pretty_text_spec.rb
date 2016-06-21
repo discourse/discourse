9e15bcb9310c561541219ed5d921e9cd0d7faac8
James Kiesel <james.kiesel@gmail.com>
2016-06-21 11:12:30 -0400

    Apply notification styles to mailing list email manually (#4283)
    * Apply notification styles to mailing list email manually
    
    * Fix failing spec

--- spec/components/pretty_text_spec.rb
+++ spec/components/pretty_text_spec.rb
@@ -318,7 +318,7 @@ HTML
     it "adds base url to relative links" do
       html = "<p><a class=\"mention\" href=\"/users/wiseguy\">@wiseguy</a>, <a class=\"mention\" href=\"/users/trollol\">@trollol</a> what do you guys think? </p>"
       output = described_class.format_for_email(html, post)
-      expect(output).to eq("<p><a href=\"#{base_url}/users/wiseguy\">@wiseguy</a>, <a href=\"#{base_url}/users/trollol\">@trollol</a> what do you guys think? </p>")
+      expect(output).to eq("<p><a class=\"mention\" href=\"#{base_url}/users/wiseguy\">@wiseguy</a>, <a class=\"mention\" href=\"#{base_url}/users/trollol\">@trollol</a> what do you guys think? </p>")
     end
 
     it "doesn't change external absolute links" do
