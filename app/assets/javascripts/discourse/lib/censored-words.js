2ed6e540f2792f731cf51c58c40af214601b9bf0
Mark Wingerd <mawingerd@gmail.com>
2016-06-29 07:41:54 -0700

    Stop URLs from being censored (#4288)
    URLs that contained a censored word were being altered by
    censored-words.js and ulimately this broke the links. As an example
    www.expertsexchange.com would get censored when it would link to a
    legitimate website.  This URL blocking functionality should be
    handled through other settings.

--- app/assets/javascripts/discourse/lib/censored-words.js
+++ app/assets/javascripts/discourse/lib/censored-words.js
@@ -7,14 +7,14 @@ Discourse.CensoredWords = {
       if (!censorRegexp) {
         var split = censored.split("|");
         if (split && split.length) {
-          censorRegexp = new RegExp("\\b(?:" + split.map(function (t) { return "(" + t.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&') + ")"; }).join("|") + ")\\b", "ig");
+          censorRegexp = new RegExp("(\\b(?:" + split.map(function (t) { return "(" + t.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&') + ")"; }).join("|") + ")\\b)(?![^\\(]*\\))", "ig");
         }
       }
       if (censorRegexp) {
         var m = censorRegexp.exec(text);
         while (m && m[0]) {
           var replacement = new Array(m[0].length+1).join('&#9632;');
-          text = text.replace(new RegExp("\\b" + m[0] + "\\b", "ig"), replacement);
+          text = text.replace(new RegExp("(\\b" + m[0] + "\\b)(?![^\\(]*\\))", "ig"), replacement);
           m = censorRegexp.exec(text);
         }
 
