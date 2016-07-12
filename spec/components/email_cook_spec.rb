0a82847dddf8af31c74c7fc2e119ad21109661ec
Robin Ward <robin.ward@gmail.com>
2016-07-12 13:49:03 -0400

    Improvements to email cook text rendering

--- spec/components/email_cook_spec.rb
+++ spec/components/email_cook_spec.rb
@@ -3,8 +3,28 @@ require 'email_cook'
 
 describe EmailCook do
 
-  it 'adds linebreaks' do
+  it 'adds linebreaks to short lines' do
     expect(EmailCook.new("hello\nworld\n").cook).to eq("hello\n<br>world\n<br>")
+  end
+
+  it "doesn't add linebreaks to long lines" do
+    long = <<LONG_EMAIL
+Hello,
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc convallis volutpat
+risus. Nulla ac faucibus quam, quis cursus lorem. Sed rutrum eget nunc sed accumsan.
+Vestibulum feugiat mi vitae turpis tempor dignissim.
+LONG_EMAIL
+
+    long_cooked = <<LONG_COOKED
+Hello,
+<br>
+<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc convallis volutpat
+risus. Nulla ac faucibus quam, quis cursus lorem. Sed rutrum eget nunc sed accumsan.
+Vestibulum feugiat mi vitae turpis tempor dignissim.
+<br>
+LONG_COOKED
+    expect(EmailCook.new(long).cook).to eq(long_cooked.strip)
   end
 
   it 'autolinks' do
