2e71e6fc6f72442a6b93200debfaa7f9c18be9bc
Robin Ward <robin.ward@gmail.com>
2016-07-13 17:07:20 -0400

    Try warming up pretty text before we fork

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -48,6 +48,8 @@ before_fork do |server, worker|
       table.classify.constantize.first rescue nil
     end
 
+    PrettyText.cook "<p>initialize <em>pretty</em> <strong>text</strong></p>"
+
     # router warm up
     Rails.application.routes.recognize_path('abc') rescue nil
 
