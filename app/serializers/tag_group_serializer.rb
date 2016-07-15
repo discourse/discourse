a74606c87c7e762ae923253f180939bd627612a2
Neil Lalonde <neillalonde@gmail.com>
2016-07-15 17:16:13 -0400

    PERF: tag groups index query

--- app/serializers/tag_group_serializer.rb
+++ app/serializers/tag_group_serializer.rb
@@ -2,7 +2,7 @@ class TagGroupSerializer < ApplicationSerializer
   attributes :id, :name, :tag_names, :parent_tag_name, :one_per_topic
 
   def tag_names
-    object.tags.pluck(:name).sort
+    object.tags.map(&:name).sort
   end
 
   def parent_tag_name
