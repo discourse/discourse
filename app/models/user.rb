a9207dafa787baa1b77086213efdf85999e5edcb
Arpit Jalan <arpit@techapj.com>
2016-07-23 02:57:30 +0530

    FEATURE: configure session time via site setting for all the users (#4343)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1048,6 +1048,7 @@ end
 #  trust_level_locked      :boolean          default(FALSE), not null
 #  staged                  :boolean          default(FALSE), not null
 #  first_seen_at           :datetime
+#  auth_token_created_at   :datetime
 #
 # Indexes
 #
