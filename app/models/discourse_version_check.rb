547f9d1e9c37eb06bed62a0d466efab5d4cb93d1
cpradio <forums@cpradio.com>
2016-08-01 17:30:04 -0400

    FEATURE: Installed Version link shows GitHub Compare to branch being followed

--- app/models/discourse_version_check.rb
+++ app/models/discourse_version_check.rb
@@ -1,5 +1,5 @@
 class DiscourseVersionCheck
   include ActiveModel::Model
 
-  attr_accessor :latest_version, :critical_updates, :installed_version, :installed_sha, :installed_describe, :missing_versions_count, :updated_at, :version_check_pending
+  attr_accessor :latest_version, :critical_updates, :installed_version, :installed_sha, :installed_describe, :missing_versions_count, :git_branch, :updated_at, :version_check_pending
 end
