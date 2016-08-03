8850d18cad4adb448b870c1fd2f41fd2cd7be0fd
Guo Xiang Tan <tgx_world@hotmail.com>
2016-08-03 14:05:20 +0800

    Add specs for m4a audio onebox.

--- spec/lib/onebox/engine/audio_onebox_spec.rb
+++ spec/lib/onebox/engine/audio_onebox_spec.rb
@@ -13,6 +13,10 @@ describe Onebox::Engine::AudioOnebox do
     expect(Onebox.preview('http://download.wavetlan.com/SVV/Media/HTTP/sample14.wav').to_s).to match(/<audio/)
   end
 
+  it "supports m4a" do
+    expect(Onebox.preview('http://techslides.com/demos/samples/sample.m4a').to_s).to match(/<audio/)
+  end
+
   it "supports URLs with query parameters" do
     expect(Onebox.preview('https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg?foo=bar').to_s).to match(/<audio/)
   end
@@ -20,7 +24,7 @@ describe Onebox::Engine::AudioOnebox do
   it "supports protocol relative URLs" do
     expect(Onebox.preview('//upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg').to_s).to match(/<audio/)
   end
-  
+
   it "includes a fallback direct link to the audio" do
     expect(Onebox.preview('http://kolber.github.io/audiojs/demos/mp3/juicy.mp3').to_s).to match(/<a.*mp3/)
   end
