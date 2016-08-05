2a5a0bebb3b7f8772cf66e629d8344064d4453f4
Rafael dos Santos Silva <xfalcox@gmail.com>
2016-08-05 02:12:35 -0300

    Adjusts from review

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -70,9 +70,9 @@ class TopicsController < ApplicationController
     end
 
     if opts[:print]
-      raise Discourse::InvalidAccess unless SiteSetting.allow_print_page
+      raise Discourse::InvalidAccess unless SiteSetting.max_prints_per_hour_per_user > 0
       begin
-        RateLimiter.new(current_user, "print-topic-per-hour", 10, 1.hour).performed! unless current_user.admin?
+        RateLimiter.new(current_user, "print-topic-per-hour", 10, 1.hour).performed! unless @guardian.is_admin?
       rescue RateLimiter::LimitExceeded
         render_json_error(I18n.t("rate_limiter.slow_down"))
       end
