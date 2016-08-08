b8179788fecd48b074b37a625eec442068abb601
Arpit Jalan <arpit@techapj.com>
2016-08-08 20:22:17 +0530

    support amzn.com

--- lib/onebox/engine/amazon_onebox.rb
+++ lib/onebox/engine/amazon_onebox.rb
@@ -8,7 +8,7 @@ module Onebox
       include HTML
 
       always_https
-      matches_regexp(/^https?:\/\/(?:www)\.amazon\.(?<tld>com|ca|de|it|es|fr|co\.jp|co\.uk|cn|in|com\.br)\//)
+      matches_regexp(/^https?:\/\/(?:www\.)?(amazon|amzn)\.(?<tld>com|ca|de|it|es|fr|co\.jp|co\.uk|cn|in|com\.br)\//)
 
       def url
         if match && match[:id]
