2b5216a7aee133272e4ff9dc5c6d21bd7cf5b713
Arpit Jalan <arpit@techapj.com>
2016-08-10 19:14:05 +0530

    fix google docs spec

--- spec/lib/onebox/engine/google_docs_onebox_spec.rb
+++ spec/lib/onebox/engine/google_docs_onebox_spec.rb
@@ -4,64 +4,32 @@ describe Onebox::Engine::GoogleDocsOnebox do
   context "Spreadsheets" do
     let(:matcher) { described_class.new("https://docs.google.com/spreadsheets/d/SHEET_KEY/pubhtml") }
 
-    it "should have spreadsheet class in html" do
-      expect(matcher.to_html).to include "spreadsheet-onebox"
-    end
-
     it "should be a spreadsheet" do
-      expect(matcher.send(:spreadsheet?)).to be true
-    end
-
-    it "Should detect key" do
-      expect(matcher.send(:key)).to eq 'SHEET_KEY'
+      expect(matcher.send(:shorttype)).to eq (:sheets)
     end
   end
 
   context "Documents" do
     let(:matcher) { described_class.new("https://docs.google.com/document/d/DOC_KEY/pub") }
 
-    it "should have document class in html" do
-      expect(matcher.to_html).to include "document-onebox"
-    end
-
     it "should be a document" do
-      expect(matcher.send(:document?)).to be true
-    end
-
-    it "Should detect key" do
-      expect(matcher.send(:key)).to eq 'DOC_KEY'
+      expect(matcher.send(:shorttype)).to eq (:docs)
     end
   end
 
   context "Presentaions" do
     let(:matcher) { described_class.new("https://docs.google.com/presentation/d/PRESENTATION_KEY/pub") }
 
-    it "should have presentation class in html" do
-      expect(matcher.to_html).to include "presentation-onebox"
-    end
-
     it "should be a presentation" do
-      expect(matcher.send(:presentation?)).to be true
-    end
-
-    it "Should detect key" do
-      expect(matcher.send(:key)).to eq 'PRESENTATION_KEY'
+      expect(matcher.send(:shorttype)).to eq (:slides)
     end
   end
 
   context "Forms" do
     let(:matcher) { described_class.new("https://docs.google.com/forms/d/FORMS_KEY/viewform") }
 
-    it "should have forms class in html" do
-      expect(matcher.to_html).to include "forms-onebox"
-    end
-
     it "should be a form" do
-      expect(matcher.send(:forms?)).to be true
-    end
-
-    it "Should detect key" do
-      expect(matcher.send(:key)).to eq 'FORMS_KEY'
+      expect(matcher.send(:shorttype)).to eq (:forms)
     end
   end
 end
