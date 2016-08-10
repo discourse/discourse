748a9e008f6767e7555ff99ef4fddc4df0bd5a69
Robin Ward <robin.ward@gmail.com>
2016-08-10 17:54:30 -0400

    FIX: Missing `post_number` in searching within a topic

--- app/serializers/search_post_serializer.rb
+++ app/serializers/search_post_serializer.rb
@@ -1,7 +1,7 @@
 class SearchPostSerializer < BasicPostSerializer
   has_one :topic, serializer: TopicListItemSerializer
 
-  attributes :like_count, :blurb
+  attributes :like_count, :blurb, :post_number
 
   def blurb
     options[:result].blurb(object)
