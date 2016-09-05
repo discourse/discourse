02a29f87521268ee8dee0c6763681679d08e6260
Guo Xiang Tan <tgx_world@hotmail.com>
2016-09-05 19:33:03 +0800

    More randomly failing specs fixes.

--- spec/components/discourse_plugin_spec.rb
+++ spec/components/discourse_plugin_spec.rb
@@ -38,11 +38,11 @@ describe DiscoursePlugin do
   context 'registering for callbacks' do
     before do
       plugin.stubs(:hello)
-      plugin.listen_for(:hello)
+      @proc = plugin.listen_for(:hello).first
     end
 
     after do
-      DiscourseEvent.off(:hello)
+      DiscourseEvent.off(:hello, &@proc)
     end
 
     it "calls the method when it is triggered" do
