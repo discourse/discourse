742314082542c0cabaf0766ce3e24fe8dff88cef
Erick Guan <fantasticfears@gmail.com>
2016-09-12 17:48:54 +0800

    FIX: show event name in webhook headers

--- app/jobs/regular/emit_web_hook_event.rb
+++ app/jobs/regular/emit_web_hook_event.rb
@@ -49,7 +49,7 @@ module Jobs
           'X-Discourse-Event-Id' => web_hook_event.id,
           'X-Discourse-Event-Type' => @opts[:event_type]
         }
-        headers['X-Discourse-Event'] = @opts[:event_name] if @opts[:event_name].present?
+        headers['X-Discourse-Event'] = @opts[:event_name].to_s if @opts[:event_name].present?
 
         if @web_hook.secret.present?
           headers['X-Discourse-Event-Signature'] = "sha256=" + OpenSSL::HMAC.hexdigest("sha256", @web_hook.secret, body)
