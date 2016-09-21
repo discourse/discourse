afb1106c702fa8674c2962fdbeee228e68742b30
Robin Ward <robin.ward@gmail.com>
2016-09-21 17:15:57 -0400

    Show staff count on invites page, don't warn if you have 3

---
+++ app/assets/javascripts/wizard/components/staff-count.js.es6
@@ -0,0 +1,7 @@
+import computed from 'ember-addons/ember-computed-decorators';
+
+export default Ember.Component.extend({
+  @computed('field.value')
+  showStaffCount: staffCount => staffCount > 1
+});
+
--- lib/wizard/builder.rb
+++ lib/wizard/builder.rb
@@ -209,6 +209,10 @@ class Wizard
       end
 
       @wizard.append_step('invites') do |step|
+
+        staff_count = User.where("moderator = true or admin = true").where("id <> ?", Discourse.system_user.id).count
+        step.add_field(id: 'staff_count', type: 'component', value: staff_count)
+
         step.add_field(id: 'invite_list', type: 'component')
 
         step.on_update do |updater|
