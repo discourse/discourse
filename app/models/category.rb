be0771a253fa7cd1401f707d7c0502c8e488f756
Arpit Jalan <arpit@techapj.com>
2016-11-17 21:57:06 +0530

    FIX: update category permalink if record already exists

--- app/models/category.rb
+++ app/models/category.rb
@@ -467,9 +467,15 @@ SQL
   def create_category_permalink
     old_slug = changed_attributes["slug"]
     if self.parent_category
-      Permalink.create(url: "c/#{self.parent_category.slug}/#{old_slug}", category_id: id)
+      url = "c/#{self.parent_category.slug}/#{old_slug}"
     else
-      Permalink.create(url: "c/#{old_slug}", category_id: id)
+      url = "c/#{old_slug}"
+    end
+
+    if Permalink.where(url: url).exists?
+      Permalink.where(url: url).update_all(category_id: id)
+    else
+      Permalink.create(url: url, category_id: id)
     end
   end
 
