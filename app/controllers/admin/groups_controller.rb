712ff01f38fc5612f0b98da7b95d656b19d88c76
Guo Xiang Tan <tgx_world@hotmail.com>
2016-11-25 11:21:08 +0800

    PERF: Remove eager load.

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,9 +1,7 @@
 class Admin::GroupsController < Admin::AdminController
 
   def index
-    groups = Group.order(:name)
-      .where("id <> ?", Group::AUTO_GROUPS[:everyone])
-      .includes(:group_users)
+    groups = Group.order(:name).where("id <> ?", Group::AUTO_GROUPS[:everyone])
 
     if search = params[:search].to_s
       groups = groups.where("name ILIKE ?", "%#{search}%")
