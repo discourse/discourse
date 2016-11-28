87baf2be8e50cd597a10228d5bd41f775a2f9912
Régis Hanol <regis@hanol.fr>
2016-11-28 14:36:54 +0100

    FEATURE: new vimeo onebox

---
+++ lib/onebox/engine/vimeo_onebox.rb
@@ -0,0 +1,20 @@
+module Onebox
+  module Engine
+    class VimeoOnebox
+      include Engine
+      include StandardEmbed
+
+      matches_regexp(/^https?:\/\/(www\.)?vimeo\.com\/\d+$/)
+      always_https
+
+      def placeholder_html
+        oembed = get_oembed
+        "<img src='#{oembed[:thumbnail_url]}' width='#{oembed[:thumbnail_width]}' height='#{oembed[:thumbnail_height]}'>"
+      end
+
+      def to_html
+        get_oembed[:html]
+      end
+    end
+  end
+end
