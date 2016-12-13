18f400e652f3c4c72d7bf1e47820f16d9adabbc5
Guo Xiang Tan <tgx_world@hotmail.com>
2016-12-13 09:59:38 +0800

    Remove RSpec given.

--- spec/integration/topic_auto_close_spec.rb
+++ spec/integration/topic_auto_close_spec.rb
@@ -19,25 +19,35 @@ describe Topic do
     end
   end
 
-
-  before {
+  before do
+    @original_value = SiteSetting.queue_jobs
     SiteSetting.queue_jobs = true
     Jobs::CloseTopic.jobs.clear
-  }
+  end
+
+  after do
+    SiteSetting.queue_jobs = @original_value
+  end
 
   context 'creating a topic without auto-close' do
-    Given(:topic) { Fabricate(:topic, category: category) }
+    let(:topic) { Fabricate(:topic, category: category) }
 
     context 'uncategorized' do
-      Given(:category) { nil }
-      Then { expect(topic.auto_close_at).to eq(nil) }
-      And  { expect(scheduled_jobs_for(:close_topic)).to be_empty }
+      let(:category) { nil }
+
+      it 'should not schedule the topic to auto-close' do
+        expect(topic.auto_close_at).to eq(nil)
+        expect(scheduled_jobs_for(:close_topic)).to be_empty
+      end
     end
 
     context 'category without default auto-close' do
-      Given(:category) { Fabricate(:category, auto_close_hours: nil) }
-      Then { expect(topic.auto_close_at).to eq(nil) }
-      And  { expect(scheduled_jobs_for(:close_topic)).to be_empty }
+      let(:category) { Fabricate(:category, auto_close_hours: nil) }
+
+      it 'should not schedule the topic to auto-close' do
+        expect(topic.auto_close_at).to eq(nil)
+        expect(scheduled_jobs_for(:close_topic)).to be_empty
+      end
     end
 
     context 'jobs may be queued' do
@@ -51,50 +61,69 @@ describe Topic do
       end
 
       context 'category has a default auto-close' do
-        Given(:category) { Fabricate(:category, auto_close_hours: 2.0) }
-        Then { expect(topic.auto_close_at).to be_within_one_second_of(2.hours.from_now) }
-        And  { expect(topic.auto_close_started_at).to eq(Time.zone.now) }
-        And  { expect(scheduled_jobs_for(:close_topic, {topic_id: topic.id}).size).to eq(1) }
-        And  { expect(scheduled_jobs_for(:close_topic, {topic_id: category.topic.id})).to be_empty }
+        let(:category) { Fabricate(:category, auto_close_hours: 2.0) }
+
+        it 'should schedule the topic to auto-close' do
+          expect(topic.auto_close_at).to be_within_one_second_of(2.hours.from_now)
+          expect(topic.auto_close_started_at).to eq(Time.zone.now)
+          expect(scheduled_jobs_for(:close_topic, {topic_id: topic.id}).size).to eq(1)
+          expect(scheduled_jobs_for(:close_topic, {topic_id: category.topic.id})).to be_empty
+        end
 
         context 'topic was created by staff user' do
-          Given(:admin) { Fabricate(:admin) }
-          Given(:staff_topic) { Fabricate(:topic, user: admin, category: category) }
-          Then { expect(scheduled_jobs_for(:close_topic, {topic_id: staff_topic.id, user_id: admin.id}).size).to eq(1) }
+          let(:admin) { Fabricate(:admin) }
+          let(:staff_topic) { Fabricate(:topic, user: admin, category: category) }
+
+          it 'should schedule the topic to auto-close' do
+            expect(scheduled_jobs_for(:close_topic, {topic_id: staff_topic.id, user_id: admin.id}).size).to eq(1)
+          end
 
           context 'topic is closed manually' do
-            When { staff_topic.update_status('closed', true, admin) }
-            Then { expect(staff_topic.reload.auto_close_at).to eq(nil) }
-            And  { expect(staff_topic.auto_close_started_at).to eq(nil) }
+            it 'should remove the schedule to auto-close the topic' do
+              staff_topic.update_status('closed', true, admin)
+              expect(staff_topic.reload.auto_close_at).to eq(nil)
+              expect(staff_topic.auto_close_started_at).to eq(nil)
+            end
           end
         end
 
         context 'topic was created by a non-staff user' do
-          Given!(:system_user) { Discourse.system_user }
-          Given { Discourse.stubs(:system_user).returns(system_user) }
-          Given(:regular_user) { Fabricate(:user) }
-          Given(:regular_user_topic) { Fabricate(:topic, user: regular_user, category: category) }
-          Then { expect(scheduled_jobs_for(:close_topic, {topic_id: regular_user_topic.id, user_id: system_user.id}).size).to eq(1) }
+          let(:regular_user) { Fabricate(:user) }
+          let(:regular_user_topic) { Fabricate(:topic, user: regular_user, category: category) }
+
+          it 'should schedule the topic to auto-close' do
+            expect(scheduled_jobs_for(:close_topic, {topic_id: regular_user_topic.id, user_id: Discourse.system_user.id}).size).to eq(1)
+          end
         end
 
         context 'auto_close_hours of topic was set to 0' do
-          Given(:dont_close_topic) { Fabricate(:topic, auto_close_hours: 0, category: category) }
-          Then { expect(scheduled_jobs_for(:close_topic)).to be_empty }
+          let(:dont_close_topic) { Fabricate(:topic, auto_close_hours: 0, category: category) }
+
+          it 'should not schedule the topic to auto-close' do
+            expect(scheduled_jobs_for(:close_topic)).to be_empty
+          end
         end
 
         context 'two topics in the category' do
-          Given!(:other_topic) { Fabricate(:topic, category: category) }
-          When { topic } # create the second topic
-          Then { expect(scheduled_jobs_for(:close_topic).size).to eq(2) }
+          let!(:other_topic) { Fabricate(:topic, category: category) }
+
+          it 'should schedule the topic to auto-close' do
+            topic
+
+            expect(scheduled_jobs_for(:close_topic).size).to eq(2)
+          end
         end
       end
 
       context 'a topic that has been auto-closed' do
-        Given(:admin)              { Fabricate(:admin) }
-        Given!(:auto_closed_topic) { Fabricate(:topic, user: admin, closed: true, auto_close_at: 1.day.ago, auto_close_user_id: admin.id, auto_close_started_at: 6.days.ago) }
-        When { auto_closed_topic.update_status('closed', false, admin) }
-        Then { expect(auto_closed_topic.reload.auto_close_at).to eq(nil) }
-        And  { expect(auto_closed_topic.auto_close_started_at).to eq(nil) }
+        let(:admin)              { Fabricate(:admin) }
+        let!(:auto_closed_topic) { Fabricate(:topic, user: admin, closed: true, auto_close_at: 1.day.ago, auto_close_user_id: admin.id, auto_close_started_at: 6.days.ago) }
+
+        it 'should set the right attributes' do
+          auto_closed_topic.update_status('closed', false, admin)
+          expect(auto_closed_topic.reload.auto_close_at).to eq(nil)
+          expect(auto_closed_topic.auto_close_started_at).to eq(nil)
+        end
       end
     end
   end
