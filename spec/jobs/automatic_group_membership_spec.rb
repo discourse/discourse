076a08d8e14d173cc6326d5b95fdf1de65d63c29
Guo Xiang Tan <tgx_world@hotmail.com>
2016-12-21 18:15:01 +0800

    FIX: Unactivated users should not be automatically added into groups as well.

--- spec/jobs/automatic_group_membership_spec.rb
+++ spec/jobs/automatic_group_membership_spec.rb
@@ -11,6 +11,7 @@ describe Jobs::AutomaticGroupMembership do
     user1 = Fabricate(:user, email: "foo@wat.com")
     user2 = Fabricate(:user, email: "foo@bar.com")
     user3 = Fabricate(:user, email: "bar@wat.com", staged: true)
+    user4 = Fabricate(:user, email: "abc@wat.com", active: false)
     group = Fabricate(:group, automatic_membership_email_domains: "wat.com", automatic_membership_retroactive: true)
 
     Jobs::AutomaticGroupMembership.new.execute(group_id: group.id)
@@ -19,6 +20,7 @@ describe Jobs::AutomaticGroupMembership do
     expect(group.users.include?(user1)).to eq(true)
     expect(group.users.include?(user2)).to eq(false)
     expect(group.users.include?(user3)).to eq(false)
+    expect(group.users.include?(user4)).to eq(false)
   end
 
 end
