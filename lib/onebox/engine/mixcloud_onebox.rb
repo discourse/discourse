82610f1cc71bb1b85b9bf6ffac6dfc6da4723021
Régis Hanol <regis@hanol.fr>
2017-01-04 19:38:26 +0100

    mixcloud.com onebox

---
+++ lib/onebox/engine/mixcloud_onebox.rb
@@ -0,0 +1,20 @@
+module Onebox
+  module Engine
+    class MixcloudOnebox
+      include Engine
+      include StandardEmbed
+
+      matches_regexp(/^https?:\/\/www\.mixcloud\.com\//)
+      always_https
+
+      def placeholder_html
+        oembed = get_oembed
+        "<img src='#{oembed[:image]}' height='#{oembed[:height]}' #{Helpers.title_attr(oembed)}>"
+      end
+
+      def to_html
+        get_oembed[:html]
+      end
+    end
+  end
+end
