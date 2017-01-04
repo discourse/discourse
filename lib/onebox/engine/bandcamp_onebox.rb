7f417edf336f748d695c1fa8e03fd65e7f3e4baf
Régis Hanol <regis@hanol.fr>
2017-01-04 19:47:27 +0100

    bandcamp.com onebox

---
+++ lib/onebox/engine/bandcamp_onebox.rb
@@ -0,0 +1,32 @@
+module Onebox
+  module Engine
+    class BandCampOnebox
+      include Engine
+      include StandardEmbed
+
+      matches_regexp(/^https?:\/\/.*bandcamp\.com\/album\//)
+      always_https
+
+      def placeholder_html
+        og = get_opengraph
+        "<img src='#{og[:image]}' height='#{og[:video_height]}' #{Helpers.title_attr(og)}>"
+      end
+
+      def to_html
+        og = get_opengraph
+        src = og[:video_secure_url] || og[:video]
+
+        <<-HTML
+          <iframe src="#{src}"
+                  width="#{og[:video_width]}"
+                  height="#{og[:video_height]}"
+                  scrolling="no"
+                  frameborder="0"
+                  allowfullscreen>
+          </iframe>
+        HTML
+      end
+
+    end
+  end
+end
