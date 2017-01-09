c9ea04ffd25fe118a87e16360bd1b82f6811ce68
Guo Xiang Tan <tgx_world@hotmail.com>
2017-01-09 17:08:24 +0800

    oops fix specs.

--- lib/validators/censored_words_validator.rb
+++ lib/validators/censored_words_validator.rb
@@ -1,11 +1,11 @@
 class CensoredWordsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value =~ /#{SiteSetting.censored_words}/i
+    if !SiteSetting.censored_words.blank? && value =~ /#{SiteSetting.censored_words}/i
       record.errors.add(
         attribute, :contains_censored_words,
         censored_words: SiteSetting.censored_words
       )
-    elsif value =~ /#{SiteSetting.censored_pattern}/i
+    elsif !SiteSetting.censored_pattern.blank? && value =~ /#{SiteSetting.censored_pattern}/i
       record.errors.add(
         attribute, :matches_censored_pattern,
         censored_pattern: SiteSetting.censored_pattern
