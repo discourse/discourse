3fe583f8b74ff54683fea3048d021397fd23d253
Rimian Perkins <hello@rimian.com.au>
2017-01-17 13:02:27 +1100

    initial validator for censored words

--- lib/site_setting_validations.rb
+++ lib/site_setting_validations.rb
@@ -5,6 +5,10 @@ module SiteSettingValidations
     raise Discourse::InvalidParameters.new(I18n.t("errors.site_settings.#{key}"))
   end
 
+  def validate_censored_words(new_val)
+    validate_error :invalid_character if new_val.match(/^\w*$/).nil?
+  end
+
   def validate_min_username_length(new_val)
     validate_error :min_username_length_range if new_val > SiteSetting.max_username_length
     validate_error :min_username_length_exists if User.where('length(username) < ?', new_val).exists?
