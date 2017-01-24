29947f6e95d0d49297b37c410ade204270baf202
gotens1211 <nikeythe@gmail.com>
2017-01-24 22:27:37 +0530

    Added margin to the video container in lazyYt plugin

--- app/models/post_analyzer.rb
+++ app/models/post_analyzer.rb
@@ -12,36 +12,10 @@ class PostAnalyzer
     @found_oneboxes
   end
 
-#to add margin between youtube links
-def newlinereplace(*args)
-    sp = args[0].split(/\n+/)
-    a = 0
-    x = 0
-    replacem = []
- while a<sp.length do
-   if sp[a].length > 0
-    replacem[x] =sp[a]
-      x+=1
-   end
-   if a!= sp.length-1
-    replacem[x]="\n"
-      x+=1
-   end
-    a+=1
- end
-   args[0]=replacem.join()
-   return *args
-end
-
   # What we use to cook posts
   def cook(*args)
-    yt_string = args[0]
-    if yt_string.include? "www.youtube"
-      *args = newlinereplace(*args)
-    end
 
     cooked = PrettyText.cook(*args)
-
     result = Oneboxer.apply(cooked, topic_id: @topic_id) do |url, _|
       @found_oneboxes = true
       Oneboxer.invalidate(url) if args.last[:invalidate_oneboxes]
