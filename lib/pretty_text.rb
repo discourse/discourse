f932cb51f33ba25a1cc3f1880659c63081c87f6f
Sam <sam.saffron@gmail.com>
2017-02-01 10:42:05 -0500

    FIX: stop stripping local onebox links from tracker
    When a onebox was made to a local topic it was not tracked using link
    tracker

--- lib/pretty_text.rb
+++ lib/pretty_text.rb
@@ -284,7 +284,7 @@ module PrettyText
     links = []
     doc = Nokogiri::HTML.fragment(html)
     # remove href inside quotes & elided part
-    doc.css("aside.quote a, .elided a").each { |l| l["href"] = "" }
+    doc.css("aside.quote:not(.topic-onebox) a, .elided a").each { |l| l["href"] = "" }
 
     # extract all links from the post
     doc.css("a").each { |l|
