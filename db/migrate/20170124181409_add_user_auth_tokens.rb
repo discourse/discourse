4d57c95e9b29a9ff56f3237d105725adeadc112f
Sam <sam.saffron@gmail.com>
2017-02-07 09:39:41 -0500

    delay the removal of auth token column from user table

--- db/migrate/20170124181409_add_user_auth_tokens.rb
+++ db/migrate/20170124181409_add_user_auth_tokens.rb
@@ -1,7 +1,5 @@
 class AddUserAuthTokens < ActiveRecord::Migration
   def down
-    add_column :users, :auth_token, :string
-    add_column :users, :auth_token_updated_at, :datetime
     execute <<SQL
       UPDATE users
         SET auth_token = user_auth_tokens.auth_token,
@@ -36,8 +34,5 @@ SQL
     FROM users
     WHERE auth_token_updated_at IS NOT NULL AND auth_token IS NOT NULL
 SQL
-
-    remove_column :users, :auth_token
-    remove_column :users, :auth_token_updated_at
   end
 end
