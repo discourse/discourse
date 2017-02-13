6553ac6115a228e73b06f92f947ce35640b4ac0a
atanych <atanych@gmail.com>
2017-02-13 23:17:55 +0300

    remove migrates from deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "$(tput setaf 2) Fetching data from git$(tput sgr 0)"
 git pull origin master
 echo "$(tput setaf 2) Installing gems$(tput sgr 0)"
 bundle install
-rake db:migrate
+#rake db:migrate
 rake assets:precompile
 echo "$(tput setaf 2) Restarting unicorn server$(tput sgr 0)"
 sv restart unicorn
