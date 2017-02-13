3fb46a7b433fa8ed3e484f23d8848c88c2f0e6a2
atanych <atanych@gmail.com>
2017-02-13 23:21:03 +0300

    add comment in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ git pull origin master
 echo "$(tput setaf 2) Installing gems$(tput sgr 0)"
 bundle install
 #bundle exec rake db:migrate
+echo "$(tput setaf 2) Assets precompile$(tput sgr 0)"
 bundle exec rake assets:precompile
 echo "$(tput setaf 2) Restarting unicorn server$(tput sgr 0)"
 sv restart unicorn
