ecdae9f863fc684ac1bc04b0a07abeb216394b2f
Régis Hanol <regis@hanol.fr>
2017-02-24 11:35:33 +0100

    FIX: i18n integrity specs
    FIX: check all .yml files in the project for integrity
    FIX: ensure localized yamls are compatible with english

--- spec/integrity/i18n_spec.rb
+++ spec/integrity/i18n_spec.rb
@@ -1,127 +1,122 @@
-require 'rails_helper'
-require 'locale_file_walker'
+require "rails_helper"
+require "i18n/duplicate_key_finder"
+
+def extract_locale(path)
+  path[/\.([^.]{2,})\.yml$/, 1]
+end
+
+PLURALIZATION_KEYS ||= ['zero', 'one', 'two', 'few', 'many', 'other']
+
+def find_pluralizations(hash, parent_key = '', pluralizations = Hash.new)
+  hash.each do |key, value|
+    if Hash === value
+      current_key = parent_key.blank? ? key : "#{parent_key}.#{key}"
+      find_pluralizations(value, current_key, pluralizations)
+    elsif PLURALIZATION_KEYS.include? key
+      pluralizations[parent_key] = hash
+    end
+  end
+
+  pluralizations
+end
+
+def is_yaml_compatible?(english, translated)
+  english.each do |k, v|
+    if translated.has_key?(k)
+      if Hash === v
+        if Hash === translated[k]
+          return false unless is_yaml_compatible?(v, translated[k])
+        end
+      else
+        return false unless v.class == translated[k].class
+      end
+    end
+  end
+
+  true
+end
+
 
 describe "i18n integrity checks" do
 
-  it 'should have an i18n key for all trust levels' do
+  it 'has an i18n key for each Trust Levels' do
     TrustLevel.all.each do |ts|
       expect(ts.name).not_to match(/translation missing/)
     end
   end
 
-  it "needs an i18n key (description) for each Site Setting" do
+  it "has an i18n key for each Site Setting" do
     SiteSetting.all_settings.each do |s|
-      next if s[:setting] =~ /^test/
+      next if s[:setting][/^test_/]
       expect(s[:description]).not_to match(/translation missing/)
     end
   end
 
-  it "has an i18n key for each badge description" do
+  it "has an i18n key for each Badge description" do
     Badge.where(system: true).each do |b|
       expect(b.long_description).to be_present
       expect(b.description).to be_present
     end
   end
 
-  it "has valid YAML for client" do
-    Dir["#{Rails.root}/config/locales/client.*.yml"].each do |f|
-      locale = /.*\.([^.]{2,})\.yml$/.match(f)[1]
-      client = YAML.load_file("#{Rails.root}/config/locales/client.#{locale}.yml")
-      expect(client.count).to eq(1)
-      expect(client[locale]).not_to eq(nil)
-      expect(client[locale]["js"]).not_to eq(nil)
-      expect(client[locale]["admin_js"]).not_to eq(nil)
+  Dir["#{Rails.root}/config/locales/client.*.yml"].each do |path|
+    it "has valid client YAML for '#{path}'" do
+      yaml = YAML.load_file(path)
+      locale = extract_locale(path)
+
+      expect(yaml.keys).to eq([locale])
+
+      expect(yaml[locale]["js"]).to be
+      expect(yaml[locale]["admin_js"]).to be
+      # expect(yaml[locale]["wizard_js"]).to be
     end
   end
 
-  it "has valid YAML for server" do
-    Dir["#{Rails.root}/config/locales/server.*.yml"].each do |f|
-      locale = /.*\.([^.]{2,})\.yml$/.match(f)[1]
-      server = YAML.load_file("#{Rails.root}/config/locales/server.#{locale}.yml")
-      expect(server.count).to eq(1)
-      expect(server[locale]).not_to eq(nil)
-    end
-  end
+  Dir["#{Rails.root}/**/locale*/*.en.yml"].each do |english_path|
+    english_yaml = YAML.load_file(english_path)["en"]
 
-  it "does not overwrite another language" do
-    all = Dir["#{Rails.root}/config/locales/client.*.yml"] + Dir["#{Rails.root}/config/locales/server.*.yml"]
-    all.each do |f|
-      locale = /.*\.([^.]{2,})\.yml$/.match(f)[1] + ':'
-      IO.foreach(f) do |line|
-        line.strip!
-        next if line.start_with? "#"
-        next if line.start_with? "---"
-        next if line.blank?
-        expect(line).to eq locale
-        break
-      end
-    end
-  end
-
-  describe 'English locale file' do
-    locale_files = ['config/locales', 'plugins/**/locales']
-                     .product(['server.en.yml', 'client.en.yml'])
-                     .collect { |dir, filename| Dir["#{Rails.root}/#{dir}/#{filename}"] }
-                     .flatten
-                     .map { |path| Pathname.new(path).relative_path_from(Rails.root) }
-
-    class DuplicateKeyFinder < LocaleFileWalker
-      def find_duplicates(filename)
-        @keys_with_count = {}
-
-        document = Psych.parse_file(filename)
-        handle_document(document)
-
-        @keys_with_count.delete_if { |key, count| count <= 1 }.keys
+    context(english_path) do
+      it "has no duplicate keys" do
+        english_duplicates = DuplicateKeyFinder.new.find_duplicates(english_path)
+        expect(english_duplicates).to be_empty
       end
 
-      protected
+      find_pluralizations(english_yaml).each do |key, hash|
+        next if key["messages.restrict_dependent_destroy"]
 
-      def handle_scalar(node, depth, parents)
-        super(node, depth, parents)
-
-        key = parents.join('.')
-        @keys_with_count[key] = @keys_with_count.fetch(key, 0) + 1
-      end
-    end
-
-    module Pluralizations
-      def self.load(path)
-        whitelist = Regexp.union([/messages.restrict_dependent_destroy/])
-
-        yaml = YAML.load_file("#{Rails.root}/#{path}")
-        pluralizations = find_pluralizations(yaml['en'])
-        pluralizations.reject! { |key| key.match(whitelist) }
-        pluralizations
-      end
-
-      def self.find_pluralizations(hash, parent_key = '', pluralizations = Hash.new)
-        hash.each do |key, value|
-          if value.is_a? Hash
-            current_key = parent_key.blank? ? key : "#{parent_key}.#{key}"
-            find_pluralizations(value, current_key, pluralizations)
-          elsif key == 'one' || key == 'other'
-            pluralizations[parent_key] = hash
-          end
+        it "has valid pluralizations for '#{key}'" do
+          expect(hash.keys).to contain_exactly("one", "other")
         end
-
-        pluralizations
       end
     end
 
-    locale_files.each do |path|
-      context path do
-        it 'has no duplicate keys' do
-          duplicates = DuplicateKeyFinder.new.find_duplicates("#{Rails.root}/#{path}")
+    Dir[english_path.sub(".en.yml", ".*.yml")].each do |path|
+      next if path[".en.yml"]
+
+      context(path) do
+        locale = extract_locale(path)
+        yaml = YAML.load_file(path)
+
+        it "has no duplicate keys" do
+          duplicates = DuplicateKeyFinder.new.find_duplicates(path)
           expect(duplicates).to be_empty
         end
 
-        Pluralizations.load(path).each do |key, values|
-          it "key '#{key}' has valid pluralizations" do
-            expect(values.keys).to contain_exactly('one', 'other')
-          end
+        it "does not overwrite another locale" do
+          expect(yaml.keys).to eq([locale])
         end
+
+        unless path["transliterate"]
+
+          it "is compatible with english" do
+            expect(is_yaml_compatible?(english_yaml, yaml)).to eq(true)
+          end
+
+        end
+
       end
+
     end
   end
+
 end
