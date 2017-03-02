08875ba907ccfd1d10cf684244687e1699c436c4
Arpit Jalan <arpit@techapj.com>
2017-03-02 23:46:01 +0530

    FIX: grant trust level when bulk adding users to group

---
+++ app/services/trust_level_granter.rb
@@ -0,0 +1,18 @@
+class TrustLevelGranter
+
+  def initialize(trust_level, user)
+    @trust_level, @user = trust_level, user
+  end
+
+  def self.grant(trust_level, user)
+    TrustLevelGranter.new(trust_level, user).grant
+  end
+
+  def grant
+    if @user.trust_level < @trust_level
+      @user.change_trust_level!(@trust_level)
+      @user.trust_level_locked = true
+      @user.save!
+    end
+  end
+end
