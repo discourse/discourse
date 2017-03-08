354b6fbda62900f9879aae5ff8a8d9d58e2b3003
Régis Hanol <regis@hanol.fr>
2017-03-08 19:19:11 +0100

    FIX: should not try to send digest to users who reached the bounce threshold

--- app/jobs/scheduled/enqueue_digest_emails.rb
+++ app/jobs/scheduled/enqueue_digest_emails.rb
@@ -15,18 +15,18 @@ module Jobs
     def target_user_ids
       # Users who want to receive digest email within their chosen digest email frequency
       query = User.real
-                  .where(active: true, staged: false)
-                  .joins(:user_option)
                   .not_suspended
-                  .where(user_options: {email_digests: true})
+                  .activated
+                  .where(staged: false)
+                  .joins(:user_option, :user_stat)
+                  .where("user_options.email_digests")
+                  .where("user_stats.bounce_score < #{SiteSetting.bounce_score_threshold}")
                   .where("COALESCE(last_emailed_at, '2010-01-01') <= CURRENT_TIMESTAMP - ('1 MINUTE'::INTERVAL * user_options.digest_after_minutes)")
                   .where("COALESCE(last_seen_at, '2010-01-01') <= CURRENT_TIMESTAMP - ('1 MINUTE'::INTERVAL * user_options.digest_after_minutes)")
                   .where("COALESCE(last_seen_at, '2010-01-01') >= CURRENT_TIMESTAMP - ('1 DAY'::INTERVAL * #{SiteSetting.suppress_digest_email_after_days})")
 
       # If the site requires approval, make sure the user is approved
-      if SiteSetting.must_approve_users?
-        query = query.where("approved OR moderator OR admin")
-      end
+      query = query.where("approved OR moderator OR admin") if SiteSetting.must_approve_users?
 
       query.pluck(:id)
     end
