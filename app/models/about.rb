6da8fb29642ea3b7489d77bffe1cd6467f358448
Guo Xiang Tan <tgx_world@hotmail.com>
2017-03-11 14:25:09 +0800

    Add scope for human users.

--- app/models/about.rb
+++ app/models/about.rb
@@ -35,12 +35,12 @@ class About
 
   def moderators
     @moderators ||= User.where(moderator: true, admin: false)
-                        .where("id > 0")
+                        .human_users
                         .order(:username_lower)
   end
 
   def admins
-    @admins ||= User.where(admin: true).where("id > 0").order(:username_lower)
+    @admins ||= User.where(admin: true).human_users.order(:username_lower)
   end
 
   def stats
