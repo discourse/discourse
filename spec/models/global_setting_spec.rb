ef24fd54ba1d8ceba1915f04b1cd82047dcbc038
Sam <sam.saffron@gmail.com>
2017-03-13 10:19:02 -0400

    FEATUE: automatically validate token is stored in redis
    This ensures we have some handling for redis flushall
    
    We attempt to recover our in-memory session token once every 30 seconds
    
    Code is careful to only set the token if it is nil, to allow for manual
    cycling to remain safe if needed

--- spec/models/global_setting_spec.rb
+++ spec/models/global_setting_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 require 'tempfile'
 
 describe GlobalSetting do
+
+  describe '.safe_secret_key_base' do
+    it 'sets redis token if it is somehow flushed after 30 seconds' do
+      token = GlobalSetting.safe_secret_key_base
+      $redis.without_namespace.del(GlobalSetting::REDIS_SECRET_KEY)
+      freeze_time 20.seconds.from_now
+
+      GlobalSetting.safe_secret_key_base
+      new_token = $redis.without_namespace.get(GlobalSetting::REDIS_SECRET_KEY)
+      expect(new_token).to eq(nil)
+
+      freeze_time 31.seconds.from_now
+
+      GlobalSetting.safe_secret_key_base
+
+      new_token = $redis.without_namespace.get(GlobalSetting::REDIS_SECRET_KEY)
+      expect(new_token).to eq(token)
+
+    end
+  end
+
   describe '.redis_config' do
     describe 'when slave config is not present' do
       it "should not set any connector" do
