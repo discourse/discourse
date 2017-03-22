3193e8310f5668e904fa677aaebd0bf4f4888508
Robin Ward <robin.ward@gmail.com>
2017-03-22 16:39:18 -0400

    Use unminified resize to make debugging easier

--- app/assets/javascripts/vendor.js
+++ app/assets/javascripts/vendor.js
@@ -18,7 +18,7 @@
 //= require div_resizer
 //= require caret_position
 //= require favcount.js
-//= require jquery.ba-resize.min.js
+//= require jquery.ba-resize.js
 //= require jquery.color.js
 //= require jquery.cookie.js
 //= require jquery.fileupload.js
