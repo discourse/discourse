a818fa9831a85430231661171a4787e3847e3416
Guo Xiang Tan <tgx_world@hotmail.com>
2017-03-30 09:46:39 +0800

    FIX: Show stats of the last 30 days be default for admin reports.
    * `1.month.ago + 1.month` uses the calendar month for calculations
      such that `1.month.ago` from the 30th of March 2017 will give
      us the 28th of February 2017. Adding one month ahead from
      28th February 2017 will be 28th of March 2017.

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -7,12 +7,9 @@ class Admin::ReportsController < Admin::AdminController
 
     raise Discourse::NotFound unless report_type =~ /^[a-z0-9\_]+$/
 
-    start_date = 1.month.ago
-    start_date = Time.parse(params[:start_date]) if params[:start_date].present?
-
-    end_date = start_date + 1.month
-    end_date = Time.parse(params[:end_date]) if params[:end_date].present?
-
+    start_date = params[:start_date].present? ? Time.parse(params[:start_date]) : 30.days.ago
+    end_date = params[:end_date].present? ? Time.parse(params[:end_date]) : start_date + 30.days
+    
     if params.has_key?(:category_id) && params[:category_id].to_i > 0
       category_id = params[:category_id].to_i
     else
