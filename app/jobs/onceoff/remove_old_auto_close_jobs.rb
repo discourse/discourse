9429d1ac5c0a40430acaf4fbae10fcaa76575aaa
Guo Xiang Tan <tgx_world@hotmail.com>
2017-04-06 13:45:59 +0800

    Fix incorrect method name.

--- app/jobs/onceoff/remove_old_auto_close_jobs.rb
+++ app/jobs/onceoff/remove_old_auto_close_jobs.rb
@@ -1,7 +1,7 @@
 module Jobs
   class RemoveOldAutoCloseJobs < Jobs::Onceoff
     def execute_onceoff(args)
-      Jobs.cancel_scheduled_for(:close_topic)
+      Jobs.cancel_scheduled_job(:close_topic)
 
       # No need to enqueue new jobs since we have a scheduled job that will
       # automatically enqueue the new jobs.
