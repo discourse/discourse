3f0ac4b23da60706b670713e47ada5ad51e6bed4
cpradio <forums@cpradio.com>
2017-04-19 06:26:04 -0400

    FIX: Correct behavior of auto-notification state updating to exclude when the topic already has a state of normal/muted

--- spec/models/topic_user_spec.rb
+++ spec/models/topic_user_spec.rb
@@ -272,6 +272,28 @@ describe TopicUser do
         expect(tu.notification_level).to eq(TopicUser.notification_levels[:watching])
       end
 
+      it 'should not update tracking state when state manually set to normal you reply' do
+        new_user.user_option.update_column(:notification_level_when_replying, 3)
+        post_creator.create
+        TopicUser.exec_sql("UPDATE topic_users set notification_level=1
+                       WHERE topic_id = :topic_id AND user_id = :user_id", topic_id: topic_new_user.topic_id, user_id: topic_new_user.user_id)
+        TopicUser.auto_notification(topic_new_user.user_id, topic_new_user.topic_id, TopicUser.notification_reasons[:created_post], TopicUser.notification_levels[:tracking])
+
+        tu = TopicUser.find_by(user_id: topic_new_user.user_id, topic_id: topic_new_user.topic_id)
+        expect(tu.notification_level).to eq(TopicUser.notification_levels[:regular])
+      end
+
+      it 'should not update tracking state when state manually set to muted you reply' do
+        new_user.user_option.update_column(:notification_level_when_replying, 3)
+        post_creator.create
+        TopicUser.exec_sql("UPDATE topic_users set notification_level=0
+                       WHERE topic_id = :topic_id AND user_id = :user_id", topic_id: topic_new_user.topic_id, user_id: topic_new_user.user_id)
+        TopicUser.auto_notification(topic_new_user.user_id, topic_new_user.topic_id, TopicUser.notification_reasons[:created_post], TopicUser.notification_levels[:tracking])
+
+        tu = TopicUser.find_by(user_id: topic_new_user.user_id, topic_id: topic_new_user.topic_id)
+        expect(tu.notification_level).to eq(TopicUser.notification_levels[:muted])
+      end
+
       it 'should not automatically track topics you reply to and have set state manually' do
         post_creator.create
         TopicUser.change(new_user, topic, notification_level: TopicUser.notification_levels[:regular])
