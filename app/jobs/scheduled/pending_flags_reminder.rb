03320a05d2590a839df8d38d8e55f46f5afe1ade
Neil Lalonde <neillalonde@gmail.com>
2017-04-19 16:16:27 -0400

    FIX: New implementation of the "notify about flag after" setting. Only notify about new flags since the last notification. Send a private message to staff. Mention the 3 most active moderators in the message so they get notification emails.

--- app/jobs/scheduled/pending_flags_reminder.rb
+++ app/jobs/scheduled/pending_flags_reminder.rb
@@ -4,12 +4,15 @@ module Jobs
 
   class PendingFlagsReminder < Jobs::Scheduled
 
-    every 1.day
+    every 1.hour
 
     def execute(args)
       if SiteSetting.notify_about_flags_after > 0 &&
-         PostAction.flagged_posts_count > 0 &&
-         FlagQuery.flagged_post_actions('active').where('post_actions.created_at < ?', SiteSetting.notify_about_flags_after.to_i.hours.ago).pluck(:id).count > 0
+          PostAction.flagged_posts_count > 0 &&
+          flag_ids.size > 0 && last_notified_id.to_i < flag_ids.max
+
+        mentions = active_moderator_usernames.size > 0 ?
+          "@#{active_moderator_usernames.join(', @')} " : ""
 
         PostCreator.create(
           Discourse.system_user,
@@ -17,9 +20,40 @@ module Jobs
           archetype: Archetype.private_message,
           subtype: TopicSubtype.system_message,
           title: I18n.t('flags_reminder.subject_template', { count: PostAction.flagged_posts_count }),
-          raw: I18n.t('flags_reminder.flags_were_submitted', { count: SiteSetting.notify_about_flags_after })
+          raw: mentions + I18n.t('flags_reminder.flags_were_submitted', { count: SiteSetting.notify_about_flags_after })
         )
+
+        self.last_notified_id = flag_ids.max
       end
+
+      true
+    end
+
+    def flag_ids
+      @_flag_ids ||= FlagQuery.flagged_post_actions('active')
+        .where('post_actions.created_at < ?', SiteSetting.notify_about_flags_after.to_i.hours.ago)
+        .pluck(:id)
+    end
+
+    def last_notified_id
+      (i = $redis.get(self.class.last_notified_key)) && i.to_i
+    end
+
+    def last_notified_id=(arg)
+      $redis.set(self.class.last_notified_key, arg)
+    end
+
+    def self.last_notified_key
+      "last_notified_pending_flag_id"
+    end
+
+    def active_moderator_usernames
+      @_active_moderator_usernames ||=
+        User.where(moderator: true)
+          .human_users
+          .order('last_seen_at DESC')
+          .limit(3)
+          .pluck(:username)
     end
 
   end
