5a9bf4a0d9dda406cbf1e7e953a50779145bafbc
Guo Xiang Tan <tgx_world@hotmail.com>
2017-04-26 18:12:51 +0800

    Fix incorrect class name.

--- app/jobs/onceoff/clean_up_sidekiq_statistic.rb
+++ app/jobs/onceoff/clean_up_sidekiq_statistic.rb
@@ -1,5 +1,5 @@
 module Jobs
-  class GrantEmoji < Jobs::Onceoff
+  class CleanUpSidekiqStatistic < Jobs::Onceoff
     def execute_onceoff(args)
       $redis.without_namespace.del('sidekiq:sidekiq:statistic')
     end
