4b7be137cbe5961b549940b1ab5f1e244995ffd6
Régis Hanol <regis@hanol.fr>
2017-05-11 09:07:04 +0200

    don't generate errors if 'upload_id' is invalid

--- app/jobs/regular/create_avatar_thumbnails.rb
+++ app/jobs/regular/create_avatar_thumbnails.rb
@@ -7,8 +7,8 @@ module Jobs
 
       raise Discourse::InvalidParameters.new(:upload_id) if upload_id.blank?
 
-      upload = Upload.find(upload_id)
-      user   = User.find(args[:user_id] || upload.user_id)
+      return unless upload = Upload.find_by(id: upload_id)
+      return unless user = User.find(args[:user_id] || upload.user_id)
 
       Discourse.avatar_sizes.each do |size|
         OptimizedImage.create_for(upload, size, size, filename: upload.original_filename, allow_animation: SiteSetting.allow_animated_avatars)
