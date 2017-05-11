48557c89db771723ef9137c414256ccebd51d059
Sam <sam.saffron@gmail.com>
2017-05-11 13:51:38 -0400

    add bootsnap to speed up test and dev

--- config/boot.rb
+++ config/boot.rb
@@ -10,15 +10,18 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-if false && ENV['BOOTSNAP'] #&& ENV['RAILS_ENV'] != 'production'
+
+if ENV['RAILS_ENV'] != 'production'
+  is_mac = !!(RUBY_PLATFORM =~ /darwin/)
+
   require 'bootsnap'
+
   Bootsnap.setup(
     cache_dir:            'tmp/cache', # Path to your cache
-    development_mode:     ENV['BOOTSNAP_DEV'] == 'development',
     load_path_cache:      true,        # Should we optimize the LOAD_PATH with a cache?
     autoload_paths_cache: true,        # Should we optimize ActiveSupport autoloads with cache?
     disable_trace:        false,       # Sets `RubyVM::InstructionSequence.compile_option = { trace_instruction: false }`
-    compile_cache_iseq:   true,        # Should compile Ruby code into ISeq cache?
-    compile_cache_yaml:   true         # Should compile YAML into a cache?
+    compile_cache_iseq:   is_mac,      # Should compile Ruby code into ISeq cache?
+    compile_cache_yaml:   is_mac      # Should compile YAML into a cache?
   )
 end
