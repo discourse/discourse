a8d1e44943484103435da5a0ee5290632ecab4c2
Robin Ward <robin.ward@gmail.com>
2017-05-22 16:52:26 -0400

    FIX: Onebox will do a HEAD request first for redirects

--- spec/components/oneboxer_spec.rb
+++ spec/components/oneboxer_spec.rb
@@ -5,6 +5,7 @@ describe Oneboxer do
 
   it "returns blank string for an invalid onebox" do
     stub_request(:get, "http://boom.com").to_return(body: "")
+    stub_request(:head, "http://boom.com").to_return(body: "")
 
     expect(Oneboxer.preview("http://boom.com")).to eq("")
     expect(Oneboxer.onebox("http://boom.com")).to eq("")
