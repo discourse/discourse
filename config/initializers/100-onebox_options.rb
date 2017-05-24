a7885f6f23a8e87d7a1b0376728b1c86a5b43723
Felix Freiberger <felix@fefrei.de>
2017-05-24 20:21:18 +0200

    Typofix in Onebox user_agent

--- config/initializers/100-onebox_options.rb
+++ config/initializers/100-onebox_options.rb
@@ -3,5 +3,5 @@ require_dependency 'twitter_api'
 Onebox.options = {
   twitter_client: TwitterApi,
   redirect_limit: 1,
-  user_agent: "Discouse Forum Onebox v#{Discourse::VERSION::STRING}"
+  user_agent: "Discourse Forum Onebox v#{Discourse::VERSION::STRING}"
 }
