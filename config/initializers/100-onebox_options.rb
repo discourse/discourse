7126548414c73e1081f5a78b3d673d836f702c86
Robin Ward <robin.ward@gmail.com>
2017-05-24 12:19:17 -0400

    FEATURE: Add custom User-Agent to Discourse Oneboxes

--- config/initializers/100-onebox_options.rb
+++ config/initializers/100-onebox_options.rb
@@ -2,5 +2,6 @@ require_dependency 'twitter_api'
 
 Onebox.options = {
   twitter_client: TwitterApi,
-  redirect_limit: 1
+  redirect_limit: 1,
+  user_agent: "Discouse Forum Onebox v#{Discourse::VERSION::STRING}"
 }
