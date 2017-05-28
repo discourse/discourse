d106efde8e7b5ef38330bd38aba22d7e7785a31c
Evg <budo@narod.ru>
2017-05-28 08:15:22 +0300

    Update discourse_tagging.rb

--- lib/discourse_tagging.rb
+++ lib/discourse_tagging.rb
@@ -65,7 +65,7 @@ module DiscourseTagging
     term = opts[:term]
     if term.present?
       term.downcase!
-      term.gsub!(/[^a-z0-9\p{Alnum}\.\-\_]*/, '')
+      term.gsub!(/[^\p{Alnum}\.\-\_]*/, '')
       term.gsub!("_", "\\_")
       query = query.where('tags.name like ?', "%#{term}%")
     end
