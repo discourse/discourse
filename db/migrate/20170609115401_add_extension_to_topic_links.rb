67ce4b70a67ccefb03874375bf73e904f9d6cb8e
Jakub Macina <jakub.macina@gmail.com>
2017-06-20 13:01:31 +0200

    Add index to extension column in TopicLink.

--- db/migrate/20170609115401_add_extension_to_topic_links.rb
+++ db/migrate/20170609115401_add_extension_to_topic_links.rb
@@ -1,5 +1,6 @@
 class AddExtensionToTopicLinks < ActiveRecord::Migration
   def change
     add_column :topic_links, :extension, :string, limit: 5
+    add_index :topic_links, :extension
   end
 end
