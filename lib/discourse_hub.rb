1b3501b39b8034440b92c46546c64ca95a75f03d
Neil Lalonde <neillalonde@gmail.com>
2017-06-20 17:15:59 -0400

    FIX: discourse hub calls

--- lib/discourse_hub.rb
+++ lib/discourse_hub.rb
@@ -51,8 +51,8 @@ module DiscourseHub
   def self.collection_action(action, rel_url, params={})
     JSON.parse(Excon.send(action,
       "#{hub_base_url}#{rel_url}",
-      body: URI.encode_www_form(params),
-      headers: { 'Referer' => referer, 'Accept' => accepts.join(', '), "Content-Type" => "application/x-www-form-urlencoded" },
+      body: JSON[params],
+      headers: { 'Referer' => referer, 'Accept' => accepts.join(', '), "Content-Type" => "application/json" },
       omit_default_port: true
     ).body)
   end
