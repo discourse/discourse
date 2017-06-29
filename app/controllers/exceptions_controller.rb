7b35c55a1ec892f248dc5cbffa423b11bdb12f01
Guo Xiang Tan <tgx_world@hotmail.com>
2017-06-29 14:37:03 +0900

    FIX: Display Google search form when 404 page is rendered by Ember.

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -1,20 +1,21 @@
 class ExceptionsController < ApplicationController
   skip_before_filter :check_xhr, :preload_json
+  before_action :hide_google
 
   def not_found
-    @hide_google = true if SiteSetting.login_required
-
     # centralize all rendering of 404 into app controller
     raise Discourse::NotFound
   end
 
   # Give us an endpoint to use for 404 content in the ember app
   def not_found_body
-
-    # Don't show google search if it's embedded in the Ember app
-    @hide_google = true
-
     render html: build_not_found_page(200, false)
   end
+
+  private
+
+    def hide_google
+      @hide_google = true if SiteSetting.login_required
+    end
 
 end
