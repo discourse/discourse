56d6a769ca1a4e5508e2d1236bfae2a0b2a2c69d
Jakub Macina <jakub.macina@gmail.com>
2017-07-03 19:06:54 +0200

    Add migration with extension column to uploads.

--- db/migrate/20170609115401_add_extension_to_topic_links.rb
+++ db/migrate/20170609115401_add_extension_to_topic_links.rb
@@ -1,6 +1,6 @@
 class AddExtensionToTopicLinks < ActiveRecord::Migration
   def change
-    add_column :topic_links, :extension, :string, limit: 5
+    add_column :topic_links, :extension, :string, limit: 10
     add_index :topic_links, :extension
   end
 end
