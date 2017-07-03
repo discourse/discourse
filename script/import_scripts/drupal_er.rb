9a399864d5e612c97f2b58af763b4e3b04812955
damingo <daniel.ansorg@gmx.de>
2017-07-04 00:05:21 +0200

    Adapt import script site settings.

--- script/import_scripts/drupal_er.rb
+++ script/import_scripts/drupal_er.rb
@@ -15,23 +15,23 @@ class ImportScripts::DrupalER < ImportScripts::Drupal
 
   def execute
 
-    # site_settings = {
-    #   # Basic Setup
-    #   enable_badges: false,
-    #   # Login
-    #   invite_only: true,
-    #   login_required: true,
-    #   # Posting
-    #   allow_duplicate_topic_titles: true,
-    #   allow_html_tables: true,
-    #   suppress_reply_directly_below: false,
-    #   suppress_reply_directly_above: false,
-    #   # Email
-    #   disable_emails: true,
-    #   # Plugins
-    #   discourse_narrative_bot_enabled: false
-    # }
-    # site_settings.each { |key, value| SiteSetting.set(key, value) }
+    site_settings = {
+      # Basic Setup
+      enable_badges: false,
+      # Login
+      invite_only: true,
+      login_required: true,
+      # Posting
+      allow_duplicate_topic_titles: true,
+      allow_html_tables: true,
+      suppress_reply_directly_below: false,
+      suppress_reply_directly_above: false,
+      # Email
+      disable_emails: true,
+      # Plugins
+      discourse_narrative_bot_enabled: false
+    }
+    site_settings.each { |key, value| SiteSetting.set(key, value) }
 
 
     # # You'll need to edit the following query for your Drupal install:
