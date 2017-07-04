d0d60af510de3f2286702c48f6deec347461e67c
Sam Saffron <sam.saffron@gmail.com>
2017-07-04 10:31:10 -0400

    add back column temporarily to make deployment smoother

--- db/fixtures/002_groups.rb
+++ db/fixtures/002_groups.rb
@@ -4,3 +4,12 @@ if g = Group.find_by(name: 'trust_level_5', id: 15)
 end
 
 Group.where(name: 'everyone').update_all(visibility_level: Group.visibility_levels[:owners])
+
+ColumnDropper.drop(
+  table: 'groups',
+  after_migration: 'AddVisibleBackToGroups',
+  columns:  %w[visible],
+  on_drop: ->(){
+    STDERR.puts 'Removing superflous visible group column!'
+  }
+)
