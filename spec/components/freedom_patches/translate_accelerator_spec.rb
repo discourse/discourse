a4399c2eabcc32f277dc7b355a22fba2b9bffc2f
Guo Xiang Tan <tgx_world@hotmail.com>
2017-07-07 12:28:00 +0900

    FIX: Translation overrides were not cached by locale properly.

--- spec/components/freedom_patches/translate_accelerator_spec.rb
+++ spec/components/freedom_patches/translate_accelerator_spec.rb
@@ -14,4 +14,22 @@ describe "translate accelerator" do
     expect(I18n.t(key.to_sym)).to eq(text_overriden)
   end
 
+  describe '.overrides_by_locale' do
+    it 'should cache overrides for each locale' do
+      TranslationOverride.upsert!('en', 'got', "summer")
+      TranslationOverride.upsert!('zh_TW', 'got', "冬季")
+      I18n.backend.store_translations(:en, got: 'winter')
+
+      I18n.overrides_by_locale('en')
+      I18n.overrides_by_locale('zh_TW')
+
+      expect(I18n.instance_variable_get(:@overrides_by_site)).to eq(
+        "default" => {
+          "en" => { "got" => "summer" },
+          "zh_TW" => { "got" => "冬季" }
+        }
+      )
+    end
+  end
+
 end
