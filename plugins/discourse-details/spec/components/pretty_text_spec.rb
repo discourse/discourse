1d9e6d5232308257eac6cd88b666b07c43dcbf6e
Sam <sam.saffron@gmail.com>
2017-07-18 14:44:49 -0400

    fix broken spec

--- plugins/discourse-details/spec/components/pretty_text_spec.rb
+++ plugins/discourse-details/spec/components/pretty_text_spec.rb
@@ -4,9 +4,17 @@ require 'pretty_text'
 describe PrettyText do
 
   it "supports details tag" do
-    cooked_html = "<details><summary>foo</summary>\n\n<p>bar</p>\n\n</details>"
+    cooked_html = "<details><summary>foo</summary>bar</details>"
     expect(PrettyText.cook("<details><summary>foo</summary>bar</details>")).to match_html(cooked_html)
-    expect(PrettyText.cook("[details=foo]bar[/details]")).to match_html(cooked_html)
+
+    cooked_html = <<~HTML
+      <details>
+      <summary>
+      foo</summary>
+      <p>bar</p>
+      </details>
+    HTML
+    expect(PrettyText.cook("[details=foo]\nbar\n[/details]")).to eq(cooked_html.strip)
   end
 
 end
