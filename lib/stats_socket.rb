30ec87fcb58816bd1fabb5cd737246103aa911e3
Guo Xiang Tan <tgx_world@hotmail.com>
2017-07-20 13:07:22 +0900

    Expose `PrettyText.v8` heap_stats in stats socket.

--- lib/stats_socket.rb
+++ lib/stats_socket.rb
@@ -13,6 +13,8 @@ class StatsSocket < SocketServer
       case command
       when "gc_stat"
         GC.stat.to_json
+      when "v8_stat"
+        PrettyText.v8.heap_stats.to_json
       else
         "[\"UNKNOWN COMMAND\"]"
       end
