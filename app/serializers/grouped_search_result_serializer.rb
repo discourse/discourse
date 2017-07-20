7b40de5ac47e45a7c63a7976a93ab16c9de5932a
Jakub Macina <jakub.macina@gmail.com>
2017-07-20 18:07:13 +0200

    Add attribute to grouped search results for more available posts.

--- app/serializers/grouped_search_result_serializer.rb
+++ app/serializers/grouped_search_result_serializer.rb
@@ -2,7 +2,7 @@ class GroupedSearchResultSerializer < ApplicationSerializer
   has_many :posts, serializer: SearchPostSerializer
   has_many :users, serializer: SearchResultUserSerializer
   has_many :categories, serializer: BasicCategorySerializer
-  attributes :more_posts, :more_users, :more_categories, :term, :search_log_id
+  attributes :more_posts, :more_users, :more_categories, :term, :search_log_id, :more_full_page_results
 
   def search_log_id
     object.search_log_id
