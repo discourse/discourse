2f8f2aa1dd3cfe02a4a5b90f311ccd9b60ae400e
Robin Ward <robin.ward@gmail.com>
2017-07-21 15:29:04 -0400

    FEATURE: Whitelists for inline oneboxing

--- lib/inline_oneboxer.rb
+++ lib/inline_oneboxer.rb
@@ -1,38 +1,50 @@
+require_dependency 'retrieve_title'
+
 class InlineOneboxer
 
-  def initialize(urls)
+  def initialize(urls, opts=nil)
     @urls = urls
+    @opts = opts || {}
   end
 
   def process
-    @urls.map {|url| InlineOneboxer.lookup(url) }.compact
+    @urls.map {|url| InlineOneboxer.lookup(url, @opts) }.compact
   end
 
-  def self.clear_cache!
+  def self.purge(url)
+    Rails.cache.delete(cache_key(url))
   end
 
   def self.cache_lookup(url)
     Rails.cache.read(cache_key(url))
   end
 
-  def self.lookup(url)
-    cached = cache_lookup(url)
-    return cached if cached.present?
+  def self.lookup(url, opts=nil)
+    opts ||= {}
+
+    unless opts[:skip_cache]
+      cached = cache_lookup(url)
+      return cached if cached.present?
+    end
 
     if route = Discourse.route_for(url)
       if route[:controller] == "topics" &&
         route[:action] == "show" &&
         topic = (Topic.where(id: route[:topic_id].to_i).first rescue nil)
 
-        # Only public topics
-        if Guardian.new.can_see?(topic)
-          onebox = {
-            url: url,
-            title: Emoji.gsub_emoji_to_unicode(topic.title)
-          }
-          Rails.cache.write(cache_key(url), onebox, expires_in: 1.day)
-          return onebox
-        end
+        return onebox_for(url, topic.title, opts) if Guardian.new.can_see?(topic)
+      end
+    end
+
+    if whitelist = SiteSetting.inline_onebox_domains_whitelist
+      uri = URI(url) rescue nil
+
+      domains = whitelist.split('|')
+      if uri.present? &&
+        uri.hostname.present? &&
+        domains.include?(uri.hostname) &&
+        title = RetrieveTitle.crawl(url)
+          return onebox_for(url, title, opts)
       end
     end
 
@@ -40,6 +52,18 @@ class InlineOneboxer
   end
 
   private
+
+    def self.onebox_for(url, title, opts)
+      onebox = {
+        url: url,
+        title: Emoji.gsub_emoji_to_unicode(title)
+      }
+      unless opts[:skip_cache]
+        Rails.cache.write(cache_key(url), onebox, expires_in: 1.day)
+      end
+
+      onebox
+    end
 
     def self.cache_key(url)
       "inline_onebox:#{url}"
