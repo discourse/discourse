2b70ed7fbdae27049a92ac1dff861ba93a3bf58d
Sam <sam.saffron@gmail.com>
2017-07-26 10:59:32 -0400

    Update libv8 and benchmark

--- script/benchmarks/markdown/bench.rb
+++ script/benchmarks/markdown/bench.rb
@@ -42,32 +42,61 @@ Benchmark.ips do |x|
 end
 
 
-# 18-07-2017 - Sam's NUC
-
-# Calculating -------------------------------------
-# tiny post sanitize: true
-#                         162.766  (±13.5%) i/s -    812.000  in   5.101429s
-# giant post sanitize: true
-#                         133.957  (±11.2%) i/s -    663.000  in   5.029386s
-# most features sanitize: true
-#                          55.319  (±10.8%) i/s -    276.000  in   5.054290s
-# lots of mentions sanitize: true
-#                           0.313  (± 0.0%) i/s -      2.000  in   6.394343s
-# tiny post sanitize: false
-#                         456.209  (±13.6%) i/s -      2.288k in   5.117314s
-# giant post sanitize: false
-#                         331.357  (±10.9%) i/s -      1.650k in   5.046322s
-# most features sanitize: false
-#                          77.038  (±10.4%) i/s -    385.000  in   5.055062s
-# lots of mentions sanitize: false
-#                           0.312  (± 0.0%) i/s -      2.000  in   6.430657s
-# markdown it no extensions commonmark tiny post
-#                           6.916k (± 5.5%) i/s -     34.540k in   5.010354s
-# markdown it no extensions commonmark giant post
-#                           1.044k (± 9.3%) i/s -      5.247k in   5.090534s
-# markdown it no extensions commonmark most features
-#                           1.457k (± 5.0%) i/s -      7.314k in   5.034401s
-# markdown it no extensions commonmark lots of mentions
-#                           2.004k (± 5.2%) i/s -     10.192k in   5.100657s
-# sam@ubuntu markdown %
+# 27-07-2017 - Sam's NUC
 #
+# v8 5.7
+#
+#
+# tiny post sanitize: true
+#                         160.678  (±19.9%) i/s -    760.000  in   5.005630s
+# giant post sanitize: true
+#                         132.195  (±14.4%) i/s -    649.000  in   5.042695s
+# most features sanitize: true
+#                          56.205  (± 8.9%) i/s -    280.000  in   5.038138s
+# lots of mentions sanitize: true
+#                           0.318  (± 0.0%) i/s -      2.000  in   6.293644s
+# tiny post sanitize: false
+#                         404.304  (±20.8%) i/s -      1.920k in   5.019903s
+# giant post sanitize: false
+#                         327.721  (±11.9%) i/s -      1.624k in   5.033749s
+# most features sanitize: false
+#                          76.649  (±10.4%) i/s -    385.000  in   5.085552s
+# lots of mentions sanitize: false
+#                           0.306  (± 0.0%) i/s -      2.000  in   6.525968s
+# markdown it no extensions commonmark tiny post
+#                           5.871k (±19.1%) i/s -     28.544k in   5.073585s
+# markdown it no extensions commonmark giant post
+#                           1.006k (±12.5%) i/s -      4.960k in   5.041623s
+# markdown it no extensions commonmark most features
+#                           1.447k (± 8.9%) i/s -      7.205k in   5.029094s
+# markdown it no extensions commonmark lots of mentions
+#                           1.962k (± 8.3%) i/s -      9.850k in   5.061684s
+#
+#
+# v8 5.9
+#
+#
+# tiny post sanitize: true
+#                         156.179  (±16.0%) i/s -    765.000  in   5.059401s
+# giant post sanitize: true
+#                         129.972  (±10.8%) i/s -    650.000  in   5.071824s
+# most features sanitize: true
+#                          54.960  (± 9.1%) i/s -    275.000  in   5.051284s
+# lots of mentions sanitize: true
+#                           0.321  (± 0.0%) i/s -      2.000  in   6.251009s
+# tiny post sanitize: false
+#                         431.159  (±10.4%) i/s -      2.166k in   5.085303s
+# giant post sanitize: false
+#                         300.236  (±11.7%) i/s -      1.479k in   5.029557s
+# most features sanitize: false
+#                          73.808  (±10.8%) i/s -    371.000  in   5.092310s
+# lots of mentions sanitize: false
+#                           0.297  (± 0.0%) i/s -      2.000  in   6.729708s
+# markdown it no extensions commonmark tiny post
+#                           6.421k (±13.0%) i/s -     32.012k in   5.084672s
+# markdown it no extensions commonmark giant post
+#                         901.622  (± 9.2%) i/s -      4.452k in   5.016748s
+# markdown it no extensions commonmark most features
+#                           1.410k (± 6.5%) i/s -      7.112k in   5.070053s
+# markdown it no extensions commonmark lots of mentions
+#                           1.934k (± 6.4%) i/s -      9.672k in   5.025858s
