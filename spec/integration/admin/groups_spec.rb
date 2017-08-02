b6ce93a965beb31eca0b5c4616aed4b71c6673e3
Guo Xiang Tan <tgx_world@hotmail.com>
2017-08-02 20:58:35 +0900

    Add specs for https://github.com/discourse/discourse/commit/1c80c233cfe8d6dd8e6b7e0a988870760a0900fb

--- spec/integration/admin/groups_spec.rb
+++ spec/integration/admin/groups_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Managing groups as an admin" do
   let(:admin) { Fabricate(:admin) }
   let(:user) { Fabricate(:user) }
+  let(:group) { Fabricate(:group) }
 
   before do
     sign_in(admin)
@@ -24,6 +25,19 @@ RSpec.describe "Managing groups as an admin" do
       expect(group.name).to eq('testing')
       expect(group.users).to contain_exactly(admin, user)
       expect(group.allow_membership_requests).to eq(true)
+    end
+  end
+
+  describe 'adding a new owner' do
+    it 'should work' do
+      put "/admin/groups/#{group.id}/owners.json", group: {
+        usernames: [user.username, admin.username].join(",")
+      }
+
+      expect(response).to be_success
+
+      expect(group.group_users.where(owner: true).map(&:user))
+        .to contain_exactly(user, admin)
     end
   end
 end
