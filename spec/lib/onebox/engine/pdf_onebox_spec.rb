62d8aafa22096737ffb5071b7165220f61741807
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2017-08-07 19:33:02 +0200

    FIX: do not display filesize on pdf when unknown

--- spec/lib/onebox/engine/pdf_onebox_spec.rb
+++ spec/lib/onebox/engine/pdf_onebox_spec.rb
@@ -4,8 +4,12 @@ describe Onebox::Engine::PdfOnebox do
   let(:link) { "https://acrobatusers.com/assets/uploads/public_downloads/2217/adobe-acrobat-xi-merge-pdf-files-tutorial-ue.pdf" }
   let(:html) { described_class.new(link).to_html }
 
+  let(:no_content_length_link) { "https://dspace.lboro.ac.uk/dspace-jspui/bitstream/2134/14294/3/greiffenhagen-ca_and_consumption.pdf" }
+  let(:no_filesize_html) { described_class.new(no_content_length_link).to_html }
+
   before do
     FakeWeb.register_uri(:head, link, :content_length => "335562")
+    FakeWeb.register_uri(:head, no_content_length_link, :content_length => nil)
   end
 
   describe "#to_html" do
@@ -15,6 +19,10 @@ describe Onebox::Engine::PdfOnebox do
 
     it "includes filesize" do
       expect(html).to include("327.70 KB")
+    end
+
+    it "doesn’t include filesize when unknown" do
+      expect(no_filesize_html).to_not include("<p class='filesize'>")
     end
   end
 end
