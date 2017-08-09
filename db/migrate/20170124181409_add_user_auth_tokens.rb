7a4aee4a7e936f108eb86d9a76e631eecce13931
Guo Xiang Tan <tgx_world@hotmail.com>
2017-08-09 20:52:13 +0900

    FIX: `updated_at` needs to be set in migration as well.

--- db/migrate/20170124181409_add_user_auth_tokens.rb
+++ db/migrate/20170124181409_add_user_auth_tokens.rb
@@ -28,11 +28,11 @@ SQL
     add_index :user_auth_tokens, [:auth_token]
     add_index :user_auth_tokens, [:prev_auth_token]
 
-    execute <<SQL
-    INSERT INTO user_auth_tokens(user_id, auth_token, prev_auth_token, legacy, created_at, rotated_at)
-    SELECT id, auth_token, auth_token, true, auth_token_updated_at, auth_token_updated_at
+    execute <<~SQL
+    INSERT INTO user_auth_tokens(user_id, auth_token, prev_auth_token, legacy, updated_at, created_at, rotated_at)
+    SELECT id, auth_token, auth_token, true, auth_token_updated_at, auth_token_updated_at, auth_token_updated_at
     FROM users
     WHERE auth_token_updated_at IS NOT NULL AND auth_token IS NOT NULL
-SQL
+    SQL
   end
 end
