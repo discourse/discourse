0b2d65f77a05666d3ff5637f2cf1debc474e6aa6
Guo Xiang Tan <tgx_world@hotmail.com>
2017-08-16 11:42:08 +0900

    Include the site setting file path for which a default value is missing.

--- lib/site_settings/yaml_loader.rb
+++ lib/site_settings/yaml_loader.rb
@@ -15,7 +15,7 @@ class SiteSettings::YamlLoader
           if value.is_a?(Hash)
             raise Discourse::Deprecation, "The site setting `#{setting_name}` can no longer be set based on Rails environment. See also `config/environments/<env>.rb`."
           elsif value.nil?
-            raise StandardError, "The site setting `#{setting_name}` is missing default value."
+            raise StandardError, "The site setting `#{setting_name}` in '#{@file}' is missing default value."
           end
 
           if hash['hidden']&.is_a?(Hash)
