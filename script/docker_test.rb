ed6e1c3825a4865588b599b32aed337c3927fd33
David Taylor <david@taylorhq.com>
2017-08-22 13:47:29 +0100

    Combine docker:lint and docker:test into one command

--- script/docker_test.rb
+++ script/docker_test.rb
@@ -2,10 +2,7 @@
 # Available environment variables:
 # => COMMIT_HASH    used by the discourse_test docker image to load a specific commit of discourse
 #                   this can also be set to a branch, e.g. "origin/tests-passed"
-# => SKIP_LINT      if using the discourse_test docker image, this will skip the docker:test task, and only run the docker:lint task
-# => LINT_ONLY      if using the discourse_test docker image, this will skip the docker:lint task, and only run the docker:test task
-#
-# See lib/tasks/docker.rake for other available environment variables
+# See lib/tasks/docker.rake for more information
 
 def run_or_fail(command)
   pid = Process.spawn(command)
@@ -21,5 +18,4 @@ unless ENV['NO_UPDATE']
   run_or_fail("bundle")
 end
 
-run_or_fail("bundle exec rake docker:lint") if !ENV["SKIP_LINT"]
-run_or_fail("bundle exec rake docker:test") if !ENV["LINT_ONLY"]
+run_or_fail("bundle exec rake docker:test")
