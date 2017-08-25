c6ac41bb8d2efa951807eca3895facefca748085
damingo <daniel.ansorg@gmx.de>
2017-08-26 00:44:55 +0200

    Use asset-pipline for administrator

---
+++ config/initializers/100-administrate.rb
@@ -0,0 +1,5 @@
+# damingo (Github ID), 2017-08-26
+Administrate::Engine.add_stylesheet('vendor/font_awesome/font-awesome')
+Administrate::Engine.add_stylesheet('vendor/select2')
+
+Administrate::Engine.add_javascript('edgeryders/jquery.min')
