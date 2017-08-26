b5813e10add720336f640d93f89f191b7faacac0
damingo <daniel.ansorg@gmx.de>
2017-08-26 02:07:57 +0200

    Use asset-pipline for administrate

--- config/initializers/100-administrate.rb
+++ config/initializers/100-administrate.rb
@@ -2,7 +2,3 @@
 Administrate::Engine.add_stylesheet('administrate')
 
 Rails.application.config.assets.precompile += %w(administrate.scss)
-
-
-
-# Administrate::Engine.add_stylesheet('vendor/select2')
