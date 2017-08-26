eb71b58bff51e55882df074107713920f3df51b9
damingo <daniel.ansorg@gmx.de>
2017-08-26 02:07:14 +0200

    Use asset-pipline for administrate

--- config/initializers/100-administrate.rb
+++ config/initializers/100-administrate.rb
@@ -1,5 +1,8 @@
 # damingo (Github ID), 2017-08-26
-Administrate::Engine.add_stylesheet('vendor/font_awesome/font-awesome')
-Administrate::Engine.add_stylesheet('vendor/select2')
+Administrate::Engine.add_stylesheet('administrate')
 
-Administrate::Engine.add_javascript('edgeryders/jquery.min')
+Rails.application.config.assets.precompile += %w(administrate.scss)
+
+
+
+# Administrate::Engine.add_stylesheet('vendor/select2')
