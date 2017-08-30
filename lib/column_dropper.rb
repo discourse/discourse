7a988e8cd19a19c73c84cb8ea5adde12e9236e37
Guo Xiang Tan <tgx_world@hotmail.com>
2017-08-30 15:33:25 +0800

    FIX: Drop function first before removing trigger.

--- lib/column_dropper.rb
+++ lib/column_dropper.rb
@@ -31,8 +31,8 @@ class ColumnDropper
 
       columns.each do |column|
         ActiveRecord::Base.exec_sql <<~SQL
-        DROP TRIGGER IF EXISTS #{readonly_trigger_name(table, column)} ON #{table};
         DROP FUNCTION IF EXISTS #{readonly_function_name(table, column)};
+        DROP TRIGGER IF EXISTS #{readonly_trigger_name(table, column)} ON #{table};
         SQL
 
         # safe cause it is protected on method entry, can not be passed in params
