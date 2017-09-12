5d66ff2dd89a2bc72d554926909c2290740a9ddd
Neil Lalonde <neillalonde@gmail.com>
2017-09-12 15:43:03 -0400

    FIX: no notification was being sent when a post is hidden by community flags

--- spec/jobs/send_system_message_spec.rb
+++ spec/jobs/send_system_message_spec.rb
@@ -16,8 +16,14 @@ describe Jobs::SendSystemMessage do
     let(:user) { Fabricate(:user) }
 
     it "should call SystemMessage.create" do
-      SystemMessage.any_instance.expects(:create).with('welcome_invite')
+      SystemMessage.any_instance.expects(:create).with('welcome_invite', {})
       Jobs::SendSystemMessage.new.execute(user_id: user.id, message_type: 'welcome_invite')
+    end
+
+    it "can send message parameters" do
+      options = { url: "/t/no-spammers-please/123", edit_delay: 5, flag_reason: "Flagged by community" }
+      SystemMessage.any_instance.expects(:create).with('post_hidden', options)
+      Jobs::SendSystemMessage.new.execute(user_id: user.id, message_type: 'post_hidden', message_options: options)
     end
 
   end
