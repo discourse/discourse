793f933b6b28862dfb7995ef938da4029bc2e10c
Guo Xiang Tan <tgx_world@hotmail.com>
2017-09-12 15:13:50 +0800

    FIX: Move check to right serializer.

--- app/serializers/basic_user_serializer.rb
+++ app/serializers/basic_user_serializer.rb
@@ -1,10 +1,6 @@
 class BasicUserSerializer < ApplicationSerializer
   attributes :id, :username, :avatar_template
 
-  def include_name?
-    SiteSetting.enable_names?
-  end
-
   def avatar_template
     if Hash === object
       User.avatar_template(user[:username], user[:uploaded_avatar_id])
--- app/serializers/group_post_user_serializer.rb
+++ app/serializers/group_post_user_serializer.rb
@@ -1,3 +1,7 @@
 class GroupPostUserSerializer < BasicUserSerializer
   attributes :title, :name
+
+  def include_name?
+    SiteSetting.enable_names?
+  end
 end
