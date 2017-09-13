324b3ba83b76059035a05812f0531c9a4a40f768
Régis Hanol <regis@hanol.fr>
2017-09-13 18:25:58 +0200

    fix the build

--- spec/jobs/export_csv_file_spec.rb
+++ spec/jobs/export_csv_file_spec.rb
@@ -12,7 +12,7 @@ describe Jobs::ExportCsvFile do
     %w{
       id name username email title created_at last_seen_at last_posted_at
       last_emailed_at trust_level approved suspended_at suspended_till blocked
-      active admin moderator ip_address topics_entered posts_read_count
+      active admin moderator ip_address staged topics_entered posts_read_count
       time_read topic_count post_count likes_given likes_received location
       website views external_id external_email external_username external_name
       external_avatar_url
