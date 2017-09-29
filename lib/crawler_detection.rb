94d04827011cc87760e6b425fd20534a57378124
Sam <sam.saffron@gmail.com>
2017-09-29 12:31:50 +1000

    FEATURE: flexible crawler detection
    You can use the crawler user agents site setting to amend what user agents
    are considered crawlers based on a string match in the user agent
    
    Also improves performance of crawler detection slightly

--- lib/crawler_detection.rb
+++ lib/crawler_detection.rb
@@ -1,9 +1,17 @@
 module CrawlerDetection
+
   # added 'ia_archiver' based on https://meta.discourse.org/t/unable-to-archive-discourse-pages-with-the-internet-archive/21232
   # added 'Wayback Save Page' based on https://meta.discourse.org/t/unable-to-archive-discourse-with-the-internet-archive-save-page-now-button/22875
   # added 'Swiftbot' based on https://meta.discourse.org/t/how-to-add-html-markup-or-meta-tags-for-external-search-engine/28220
+  def self.to_matcher(string)
+    escaped = string.split('|').map { |agent| Regexp.escape(agent) }.join('|')
+    Regexp.new(escaped)
+  end
 
   def self.crawler?(user_agent)
-    !/Googlebot|Mediapartners|AdsBot|curl|HTTrack|Twitterbot|facebookexternalhit|bingbot|Baiduspider|ia_archiver|Wayback Save Page|360Spider|Swiftbot|YandexBot/.match(user_agent).nil?
+    # this is done to avoid regenerating regexes
+    @matchers ||= {}
+    matcher = (@matchers[SiteSetting.crawler_user_agents] ||= to_matcher(SiteSetting.crawler_user_agents))
+    matcher.match?(user_agent)
   end
 end
