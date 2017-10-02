a3860528c7fcfeb9046d62be88ba68f60f5b721b
pmusaraj <pmusaraj@gmail.com>
2017-10-02 09:15:00 -0400

    add i18n support for offline.html pages, cleanup

--- public/service-worker.js
+++ public/service-worker.js
@@ -67,7 +67,6 @@ self.addEventListener('activate', event => {
           if (expectedCacheNames.indexOf(cacheName) === -1) {
             // If this cache name isn't present in the array of "expected" cache names,
             // then delete it.
-            console.log('Deleting out of date cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -85,7 +84,6 @@ self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate' ||
       (event.request.method === 'GET' &&
        event.request.headers.get('accept').includes('text/html'))) {
-    console.log('Handling fetch event for', event.request.url);
     event.respondWith(
       fetch(event.request).catch(error => {
         // The catch is only triggered if fetch() throws an exception, which will most likely
@@ -93,7 +91,6 @@ self.addEventListener('fetch', event => {
         // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
         // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
         // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
-        console.log('Fetch failed; returning offline page instead.', error);
         return caches.match(OFFLINE_URL);
       })
     );
