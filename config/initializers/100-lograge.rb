39ed3b6425c889c744c95bc11dfe4d1b7391258b
Guo Xiang Tan <tgx_world@hotmail.com>
2017-11-01 08:37:11 +0800

    EXPERIMENTAL: Allow Logstash formatter to be enable for lograge logs.

--- config/initializers/100-lograge.rb
+++ config/initializers/100-lograge.rb
@@ -7,11 +7,18 @@ if (Rails.env.production? && SiteSetting.logging_provider == 'lograge') || ENV["
     config.lograge.custom_options = lambda do |event|
       exceptions = %w(controller action format id)
 
+      params = event.payload[:params].except(*exceptions)
+      params[:files].map!(&:headers) if params[:files]
+
       output = {
-        params: event.payload[:params].except(*exceptions),
+        params: params,
         database: RailsMultisite::ConnectionManagement.current_db,
         time: event.time,
       }
+    end
+
+    if ENV["LOGSTASH_FORMATTER"]
+      config.lograge.formatter = Lograge::Formatters::Logstash.new
     end
   end
 end
