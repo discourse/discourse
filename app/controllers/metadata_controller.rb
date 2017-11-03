4e0975039a491477a47b03ca15d38b378e556ba2
Sam <sam.saffron@gmail.com>
2017-11-03 16:19:31 +1100

    FEATURE: custom setting for large square site icon
    This icon is used for android splash screen

--- app/controllers/metadata_controller.rb
+++ app/controllers/metadata_controller.rb
@@ -13,7 +13,7 @@ class MetadataController < ApplicationController
   private
 
   def default_manifest
-    logo = SiteSetting.mobile_logo_url.presence || SiteSetting.logo_small_url.presence || SiteSetting.apple_touch_icon_url.presence
+    logo = SiteSetting.large_icon_url.presence || SiteSetting.logo_small_url.presence || SiteSetting.apple_touch_icon_url.presence
 
     manifest = {
       name: SiteSetting.title,
