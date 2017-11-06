83485a1dc73f538f519f77e4d84d69e688cb0ab7
Guo Xiang Tan <tgx_world@hotmail.com>
2017-11-06 16:40:11 +0800

    Ensure we log the datetime in the format that we want.

--- lib/unicorn/unicorn_json_log_formatter.rb
+++ lib/unicorn/unicorn_json_log_formatter.rb
@@ -4,7 +4,7 @@ class UnicornJSONLogFormatter < Logger::Formatter
   def call(severity, datetime, progname, message)
     default = {
       severity: severity,
-      datetime: datetime,
+      datetime: DateTime.parse(datetime).to_s,
       progname: progname || '',
       pid: $$,
     }
