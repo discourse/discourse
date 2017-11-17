7c6f99131390d0eebe3e161ff80dadb7b39b32b3
Neil Lalonde <neillalonde@gmail.com>
2017-11-17 17:08:49 -0500

    REFACTOR: remove AgeWord calls from serializers and do it in javascript

--- app/serializers/admin_user_list_serializer.rb
+++ app/serializers/admin_user_list_serializer.rb
@@ -80,7 +80,7 @@ class AdminUserListSerializer < BasicUserSerializer
 
   def last_emailed_age
     return nil if object.last_emailed_at.blank?
-    AgeWords.age_words(Time.now - object.last_emailed_at)
+    Time.now - object.last_emailed_at
   end
 
   def last_seen_at
@@ -90,16 +90,16 @@ class AdminUserListSerializer < BasicUserSerializer
 
   def last_seen_age
     return nil if object.last_seen_at.blank?
-    AgeWords.age_words(Time.now - object.last_seen_at)
+    Time.now - object.last_seen_at
   end
 
   def time_read
     return nil if object.user_stat.time_read.blank?
-    AgeWords.age_words(object.user_stat.time_read)
+    object.user_stat.time_read
   end
 
   def created_at_age
-    AgeWords.age_words(Time.now - object.created_at)
+    Time.now - object.created_at
   end
 
   def can_approve
