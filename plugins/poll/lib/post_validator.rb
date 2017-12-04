37dd6a5aae71a7a5a1b279ada854137d276ecc2b
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2017-12-04 11:30:46 -0500

    FEATURE: adds poll_allow_staff_to_create
    This setting will bypass poll_minimum_trust_level_to_create if set to true

--- plugins/poll/lib/post_validator.rb
+++ plugins/poll/lib/post_validator.rb
@@ -6,14 +6,13 @@ module DiscoursePoll
 
     def validate_post
       min_trust_level = SiteSetting.poll_minimum_trust_level_to_create
+      staff_allowed = SiteSetting.poll_allow_staff_to_create
       trusted = @post&.user&.admin ||
+                (@post&.user&.staff? && staff_allowed) ||
                 @post&.user&.trust_level >= TrustLevel[min_trust_level]
 
       if !trusted
-        message = I18n.t("poll.insufficient_trust_level_to_create",
-          current: @post&.user&.trust_level,
-          required: min_trust_level
-        )
+        message = I18n.t("poll.insufficient_rights_to_create")
 
         @post.errors.add(:base, message)
         return false
