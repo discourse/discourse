de1c5142d3b6536219344aa2d8726cc64e41d36b
Robert Riemann <robert@riemann.cc>
2017-12-12 00:38:10 +0100

    update audio onebox to accept opus files

--- lib/onebox/engine/audio_onebox.rb
+++ lib/onebox/engine/audio_onebox.rb
@@ -3,7 +3,7 @@ module Onebox
     class AudioOnebox
       include Engine
 
-      matches_regexp(/^(https?:)?\/\/.*\.(mp3|ogg|wav|m4a)(\?.*)?$/i)
+      matches_regexp(/^(https?:)?\/\/.*\.(mp3|ogg|opus|wav|m4a)(\?.*)?$/i)
 
       def always_https?
         WhitelistedGenericOnebox.host_matches(uri, WhitelistedGenericOnebox.https_hosts)
