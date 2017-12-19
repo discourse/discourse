24e89b6b38a0fc13c0cbbab7fe23d9011e0148dc
Régis Hanol <regis@hanol.fr>
2017-12-19 16:10:44 +0100

    FIX: validates 'ThemeField' name when used in a SCSS variable

--- spec/models/theme_field_spec.rb
+++ spec/models/theme_field_spec.rb
@@ -29,4 +29,19 @@ HTML
     expect(field.error).to eq(nil)
   end
 
+  def create_upload_theme_field!(name)
+    ThemeField.create!(
+      theme_id: 1,
+      target_id: 0,
+      value: "",
+      type_id: ThemeField.types[:theme_upload_var],
+      name: name,
+    )
+  end
+
+  it "ensures we don't use invalid SCSS variable names" do
+    expect { create_upload_theme_field!("42") }.to raise_error(ActiveRecord::RecordInvalid)
+    expect { create_upload_theme_field!("a42") }.not_to raise_error
+  end
+
 end
