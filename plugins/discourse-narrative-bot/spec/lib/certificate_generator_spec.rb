32171ad286c92f82ec9fe28f520a096cac37695d
Guo Xiang Tan <tgx_world@hotmail.com>
2017-12-22 09:30:24 +0800

    FIX: Invalid logo in discobot's certificate when `SiteSetting.logo_small_url` is blank.
    https://meta.discourse.org/t/broken-image-in-discobot-certificate-with-no-logo-small-url/76594/2

--- plugins/discourse-narrative-bot/spec/lib/certificate_generator_spec.rb
+++ plugins/discourse-narrative-bot/spec/lib/certificate_generator_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe DiscourseNarrativeBot::CertificateGenerator do
       expect { described_class.new(user, "2017-00-10") }.to_not raise_error
     end
   end
+
+  describe '#logo_group' do
+    describe 'when SiteSetting.logo_small_url is blank' do
+      before do
+        SiteSetting.logo_small_url = ''
+      end
+
+      it 'should not try to fetch a image' do
+        expect(described_class.new(user, "2017-02-10").send(:logo_group, 1, 1, 1))
+          .to eq(nil)
+      end
+    end
+  end
 end
