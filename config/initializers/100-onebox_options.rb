96eb57d895faf334d591f9adfb92fd83b8d89892
Arpit Jalan <arpit@techapj.com>
2017-12-26 21:52:39 +0530

    FIX: allow upto three url redirects in onebox

--- config/initializers/100-onebox_options.rb
+++ config/initializers/100-onebox_options.rb
@@ -2,6 +2,6 @@ require_dependency 'twitter_api'
 
 Onebox.options = {
   twitter_client: TwitterApi,
-  redirect_limit: 1,
+  redirect_limit: 3,
   user_agent: "Discourse Forum Onebox v#{Discourse::VERSION::STRING}"
 }
