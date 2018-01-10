f11b96ae5bf07613bc8ed5671c3eb26f96dd2ee1
damingo <daniel@ansorgs.de>
2018-01-10 12:54:07 +0100

    Closes #124

--- app/controllers/administration/annotator_store/users_controller.rb
+++ app/controllers/administration/annotator_store/users_controller.rb
@@ -3,9 +3,9 @@ class Administration::AnnotatorStore::UsersController < Administration::Applicat
 
   # URL: /administration/annotator/users.json
   def index
-    users = User.where(active: true).joins("LEFT JOIN user_custom_fields ON user_custom_fields.user_id = users.id AND user_custom_fields.name = 'consent_opencare'").select('users.id, users.username, user_custom_fields.value as consent_opencare').order('users.id ASC')
+    users = User.where(active: true).joins("LEFT JOIN user_custom_fields ON user_custom_fields.user_id = users.id AND user_custom_fields.name = 'edgeryders_consent'").select('users.id, users.username, user_custom_fields.value as edgeryders_consent').order('users.id ASC')
 
-    user_data = users.map {|u| {id: u.id, username: u.username, consent_opencare: u.consent_opencare} }
+    user_data = users.map {|u| {id: u.id, username: u.username, edgeryders_consent: u.edgeryders_consent} }
 
     respond_to do |format|
       format.json { render json: JSON.pretty_generate(user_data) }
