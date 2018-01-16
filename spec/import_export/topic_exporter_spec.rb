f274a5234f7bf68ea9aaf91d2a4843eab4cd186e
Neil Lalonde <neillalonde@gmail.com>
2018-01-16 12:51:37 -0500

    FIX: topic and category exporters were only exporting users who created the first post

--- spec/import_export/topic_exporter_spec.rb
+++ spec/import_export/topic_exporter_spec.rb
@@ -9,6 +9,7 @@ describe ImportExport::TopicExporter do
 
   let(:user) { Fabricate(:user) }
   let(:topic) { Fabricate(:topic, user: user) }
+  let!(:post) { Fabricate(:post, topic: topic, user: user) }
 
   context '.perform' do
     it 'export a single topic' do
@@ -22,12 +23,13 @@ describe ImportExport::TopicExporter do
 
     it 'export multiple topics' do
       topic2 = Fabricate(:topic, user: user)
+      post2 = Fabricate(:post, user: user, topic: topic2)
       data = ImportExport::TopicExporter.new([topic.id, topic2.id]).perform.export_data
 
       expect(data[:categories].blank?).to eq(true)
       expect(data[:groups].blank?).to eq(true)
       expect(data[:topics].count).to eq(2)
-      expect(data[:users].count).to eq(1)
+      expect(data[:users].map { |u| u[:id] }).to match_array([user.id])
     end
   end
 
