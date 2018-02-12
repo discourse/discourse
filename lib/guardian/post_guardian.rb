cdc571d2c0d62de7ff33435f3b2e4507b89e52dd
Robin Ward <robin.ward@gmail.com>
2018-02-12 14:56:21 -0500

    FEATURE: New site setting, `allow staff flags`, false by default
    For some large communities, it makes sense to disable flagging of
    staff posts.

--- lib/guardian/post_guardian.rb
+++ lib/guardian/post_guardian.rb
@@ -21,6 +21,9 @@ module PostGuardian
 
     result = if authenticated? && post && !@user.anonymous?
 
+      # post made by staff, but we don't allow staff flags
+      return false if !SiteSetting.allow_staff_flags? && post.user.staff?
+
       return false if [:notify_user, :notify_moderators].include?(action_key) &&
         !SiteSetting.enable_personal_messages?
 
