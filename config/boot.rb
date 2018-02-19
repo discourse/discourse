125d45351f825960693fba12585fd79aa67a2074
Sam <sam.saffron@gmail.com>
2018-02-19 17:00:28 +1100

    minor changes to discourse bench
    Ruby master is not compatible with bootsnap atm

--- config/boot.rb
+++ config/boot.rb
@@ -10,7 +10,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-if ENV['RAILS_ENV'] != 'production'
+if ENV['RAILS_ENV'] != 'production' && ENV['RAILS_ENV'] != 'profile'
   require 'bootsnap'
 
   Bootsnap.setup(
