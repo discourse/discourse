32106711375207b8740bda40d4e1124aec08d3f8
Maja Komel <maja.komel@gmail.com>
2018-02-25 00:37:21 +0100

    set topic notification level on change of post ownership

--- app/services/post_owner_changer.rb
+++ app/services/post_owner_changer.rb
@@ -15,6 +15,7 @@ class PostOwnerChanger
       @post_ids.each do |post_id|
         post = Post.with_deleted.where(id: post_id, topic_id: @topic.id).first
         next if post.blank?
+        old_owner = post.user
         @topic.user = @new_owner if post.is_first_post?
 
         if post.user == nil
@@ -23,6 +24,13 @@ class PostOwnerChanger
         post.topic = @topic
         post.set_owner(@new_owner, @acting_user, @skip_revision)
         PostAction.remove_act(@new_owner, post, PostActionType.types[:like])
+
+        if post.post_number == 1
+          TopicUser.change(old_owner.id, @topic.id, notification_level: 2) if old_owner
+          TopicUser.change(@new_owner.id, @topic.id, notification_level: 3)
+        else
+          TopicUser.change(@new_owner.id, @topic.id, notification_level: 2)
+        end
       end
 
       @topic.update_statistics
