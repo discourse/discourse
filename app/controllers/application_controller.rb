7627472c0ccb3d9033034132b8de5b90a265b6d8
Rafael dos Santos Silva <xfalcox@gmail.com>
2018-03-07 14:43:54 -0300

    FIX: Login redirect path was broken in subfolder installs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -644,7 +644,7 @@ class ApplicationController < ActionController::Base
         else
           # save original URL in a cookie (javascript redirects after login in this case)
           cookies[:destination_url] = destination_url
-          redirect_to "/login"
+          redirect_to path("/login")
         end
       end
     end
