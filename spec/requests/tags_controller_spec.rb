aac779612492825cc95d0493e2c00e7fb0a86d48
Arpit Jalan <arpit@techapj.com>
2018-03-09 20:47:01 +0530

    FIX: do not show tags with 0 count on /tags page

--- spec/requests/tags_controller_spec.rb
+++ spec/requests/tags_controller_spec.rb
@@ -5,6 +5,42 @@ describe TagsController do
     SiteSetting.tagging_enabled = true
   end
 
+  describe '#index' do
+
+    before do
+      tag = Fabricate(:tag, name: 'test')
+      topic_tag = Fabricate(:tag, name: 'topic-test', topic_count: 1)
+    end
+
+    shared_examples "successfully retrieve tags with topic_count > 0" do
+      it "should return the right response" do
+        get "/tags.json"
+
+        expect(response).to be_success
+
+        tags = JSON.parse(response.body)["tags"]
+        expect(tags.length).to eq(1)
+        expect(tags[0]['text']).to eq("topic-test")
+      end
+    end
+
+    context "with tags_listed_by_group enabled" do
+      before do
+        SiteSetting.tags_listed_by_group = true
+      end
+
+      include_examples "successfully retrieve tags with topic_count > 0"
+    end
+
+    context "with tags_listed_by_group disabled" do
+      before do
+        SiteSetting.tags_listed_by_group = false
+      end
+
+      include_examples "successfully retrieve tags with topic_count > 0"
+    end
+  end
+
   describe '#check_hashtag' do
     let(:tag) { Fabricate(:tag, name: 'test') }
 
