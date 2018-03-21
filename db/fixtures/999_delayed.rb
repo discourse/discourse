ed85a65bfeeb014db8a3e07f3c943fd855104129
Gerhard Schlager <mail@gerhard-schlager.at>
2018-03-21 12:07:47 +0100

    FIX: Drop unused tables with a delay

--- db/fixtures/999_delayed.rb
+++ db/fixtures/999_delayed.rb
@@ -10,3 +10,19 @@ Migration::TableDropper.delayed_rename(
     STDERR.puts "Dropping topic_status_updates. It was moved to topic_timers."
   }
 )
+
+Migration::TableDropper.delayed_drop(
+  table_name: 'category_featured_users',
+  after_migration: 'DropUnusedTables',
+  on_drop: ->() {
+    STDERR.puts "Dropping category_featured_users. It isn't used anymore."
+  }
+)
+
+Migration::TableDropper.delayed_drop(
+  table_name: 'versions',
+  after_migration: 'DropUnusedTables',
+  on_drop: ->() {
+    STDERR.puts "Dropping versions. It isn't used anymore."
+  }
+)
