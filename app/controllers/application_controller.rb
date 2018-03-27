ff9d7a9bfbee42aed6fb28a86c21ac8dddcb4c48
Vinoth Kannan <vinothkannan@vinkas.com>
2018-03-27 17:22:07 +0530

    FIX: authComplete query param should carry-forward to login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -641,6 +641,8 @@ class ApplicationController < ActionController::Base
           # save original URL in a session so we can redirect after login
           session[:destination_url] = destination_url
           redirect_to path('/session/sso')
+        elsif params[:authComplete].present?
+          redirect_to path("/login?authComplete=true")
         else
           # save original URL in a cookie (javascript redirects after login in this case)
           cookies[:destination_url] = destination_url
