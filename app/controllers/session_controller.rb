5925a581db02f3a58a25d64bae6ceb4c149fc370
Sam <sam.saffron@gmail.com>
2018-04-10 14:37:10 +1000

    array is not supported here, use a simple comma delimited list

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -52,7 +52,7 @@ class SessionController < ApplicationController
         sso.external_id = current_user.id.to_s
         sso.admin = current_user.admin?
         sso.moderator = current_user.moderator?
-        sso.groups = current_user.groups.pluck(:name)
+        sso.groups = current_user.groups.pluck(:name).join(",")
 
         if sso.return_sso_url.blank?
           render plain: "return_sso_url is blank, it must be provided", status: 400
