297d957f025749157d08026d646b4a1c2b95988c
Arpit Jalan <arpit@techapj.com>
2018-04-11 19:47:05 +0530

    FEATURE:  webhook for flag events

--- spec/models/post_action_spec.rb
+++ spec/models/post_action_spec.rb
@@ -710,4 +710,36 @@ describe PostAction do
     end
   end
 
+  describe "triggers webhook events" do
+    let(:post) { Fabricate(:post) }
+
+    it 'flag created' do
+      event = DiscourseEvent.track_events { PostAction.act(eviltrout, post, PostActionType.types[:spam]) }.last
+      expect(event[:event_name]).to eq(:flag_created)
+    end
+
+    context "resolving flags" do
+      before do
+        @flag = PostAction.act(eviltrout, post, PostActionType.types[:spam])
+      end
+
+      it 'flag agreed' do
+        event = DiscourseEvent.track_events { PostAction.agree_flags!(post, moderator) }.last
+        expect(event[:event_name]).to eq(:flag_agreed)
+        expect(event[:params].first).to eq(@flag)
+      end
+
+      it 'flag disagreed' do
+        event = DiscourseEvent.track_events { PostAction.clear_flags!(post, moderator) }.last
+        expect(event[:event_name]).to eq(:flag_disagreed)
+        expect(event[:params].first).to eq(@flag)
+      end
+
+      it 'flag deferred' do
+        event = DiscourseEvent.track_events { PostAction.defer_flags!(post, moderator) }.last
+        expect(event[:event_name]).to eq(:flag_deferred)
+        expect(event[:params].first).to eq(@flag)
+      end
+    end
+  end
 end
