11113f67f2818a1b4a80db5658350d7d7ebc3022
Guo Xiang Tan <tgx_world@hotmail.com>
2018-04-17 15:56:55 +0800

    FIX: Missing `Group#bio_raw` attribute for group owners.
    https://meta.discourse.org/t/group-description-does-not-load-in-editor-for-owners-who-are-not-staff/85345

--- spec/serializers/basic_group_serializer_spec.rb
+++ spec/serializers/basic_group_serializer_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe BasicGroupSerializer do
+  let(:guardian) { Guardian.new }
+  let(:group) { Fabricate(:group) }
   subject { described_class.new(group, scope: Guardian.new, root: false) }
 
   describe '#display_name' do
@@ -17,6 +19,24 @@ describe BasicGroupSerializer do
 
       it 'should not include the display name' do
         expect(subject.display_name).to eq(nil)
+      end
+    end
+  end
+
+  describe '#bio_raw' do
+    let(:group) { Fabricate(:group, bio_raw: 'testing') }
+
+    let(:user) do
+      user = Fabricate(:user)
+      group.add_owner(user)
+      user
+    end
+
+    let(:guardian) { Guardian.new(user) }
+
+    describe 'group owner' do
+      it 'should include bio_raw' do
+        expect(subject.bio_raw).to eq('testing')
       end
     end
   end
