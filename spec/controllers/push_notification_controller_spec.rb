7236139fdcae63119ebf60193bbdff9459c3df3a
Jeff Wong <awole20@gmail.com>
2018-05-03 12:00:56 -0700

    fix formatting

--- spec/controllers/push_notification_controller_spec.rb
+++ spec/controllers/push_notification_controller_spec.rb
@@ -4,7 +4,7 @@ describe PushNotificationController do
 
   context "logged out" do
     it "should not allow subscribe" do
-      get :subscribe, params: { username: "test", subscription: { endpoint: "endpoint", keys: { p256dh: "256dh", auth: "auth" }}, send_confirmation: false, format: :json }, format: :json
+      get :subscribe, params: { username: "test", subscription: { endpoint: "endpoint", keys: { p256dh: "256dh", auth: "auth" } }, send_confirmation: false, format: :json }, format: :json
       expect(response).not_to be_success
       json = JSON.parse(response.body)
 
@@ -17,7 +17,7 @@ describe PushNotificationController do
     let(:user) { log_in }
 
     it "should subscribe" do
-      get :subscribe, params: { username: user.username, subscription: { endpoint: "endpoint", keys: { p256dh: "256dh", auth: "auth" }}, send_confirmation: false, format: :json }, format: :json
+      get :subscribe, params: { username: user.username, subscription: { endpoint: "endpoint", keys: { p256dh: "256dh", auth: "auth" } }, send_confirmation: false, format: :json }, format: :json
       expect(response).to be_success
       json = JSON.parse(response.body)
 
@@ -25,7 +25,7 @@ describe PushNotificationController do
     end
 
     it "should unsubscribe with existing subscription" do
-      sub = { endpoint: "endpoint", keys: { p256dh: "256dh", auth: "auth" }}
+      sub = { endpoint: "endpoint", keys: { p256dh: "256dh", auth: "auth" } }
       PushSubscription.create(user: user, data: sub.to_json)
 
       get :unsubscribe, params: { username: user.username, subscription: sub, format: :json }, format: :json
@@ -37,7 +37,7 @@ describe PushNotificationController do
 
     it "should unsubscribe without subscription" do
 
-      get :unsubscribe, params: { username: user.username, subscription: { endpoint: "endpoint", keys: { p256dh: "256dh", auth: "auth" }}, format: :json }, format: :json
+      get :unsubscribe, params: { username: user.username, subscription: { endpoint: "endpoint", keys: { p256dh: "256dh", auth: "auth" } }, format: :json }, format: :json
       expect(response).to be_success
       json = JSON.parse(response.body)
 
