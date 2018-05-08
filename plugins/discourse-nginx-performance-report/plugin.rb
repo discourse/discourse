2d926756f2c8c1df74a41bf5c2f1989be78e36c2
Guo Xiang Tan <tgx_world@hotmail.com>
2018-05-08 10:30:33 +0800

    UX: Display site settings shortcut for `poll` and `discourse-nginx-performance-report`.
    https://meta.discourse.org/t/improving-admin-plugins/84585/29?u=tgxworld

--- plugins/discourse-nginx-performance-report/plugin.rb
+++ plugins/discourse-nginx-performance-report/plugin.rb
@@ -3,6 +3,9 @@
 # version: 0.1
 # url: https://github.com/discourse/discourse/tree/master/plugins/discourse-nginx-performance-report
 
+enabled_site_setting :daily_performance_report
+enabled_site_setting_filter "daily_performance_report"
+
 after_initialize do
   load File.expand_path("../app/jobs/scheduled/daily_performance_report.rb", __FILE__)
 end
