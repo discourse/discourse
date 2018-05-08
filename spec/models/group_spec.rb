f17d30488592deea08b2c7e5f6d6ddbb3c1e6ae2
Guo Xiang Tan <tgx_world@hotmail.com>
2018-05-08 10:50:11 +0800

    Fix brittle spec.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -685,7 +685,7 @@ describe Group do
       it "should publish the group's categories to the client" do
         group.update!(public_admission: true, categories: [category])
 
-        message = MessageBus.track_publish { group.add(user) }.first
+        message = MessageBus.track_publish("/categories") { group.add(user) }.first
 
         expect(message.data[:categories].count).to eq(1)
         expect(message.data[:categories].first[:id]).to eq(category.id)
