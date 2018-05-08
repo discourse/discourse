2002a7c4eff8e9de4046cda56809b77ddf658879
Jeff Wong <awole20@gmail.com>
2018-05-07 17:00:40 -0700

    FIX: regenerate vapid keys when the public key bytes is blank

--- config/initializers/100-push-notifications.rb
+++ config/initializers/100-push-notifications.rb
@@ -1,6 +1,6 @@
 require_dependency 'webpush'
 
-if SiteSetting.vapid_public_key.blank? || SiteSetting.vapid_private_key.blank?
+if SiteSetting.vapid_public_key.blank? || SiteSetting.vapid_private_key.blank? || SiteSetting.vapid_public_key_bytes.blank?
   vapid_key = Webpush.generate_key
   SiteSetting.vapid_public_key = vapid_key.public_key
   SiteSetting.vapid_private_key = vapid_key.private_key
