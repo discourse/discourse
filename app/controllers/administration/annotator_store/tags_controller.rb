5567ce02ce3d86a9e038a58c4a3633952a9467c4
damingo <daniel@ansorgs.de>
2018-05-14 02:04:07 +0200

    Close #107

--- app/controllers/administration/annotator_store/tags_controller.rb
+++ app/controllers/administration/annotator_store/tags_controller.rb
@@ -60,6 +60,13 @@ class Administration::AnnotatorStore::TagsController < Administration::Applicati
   end
 
 
+  def destroy
+    requested_resource.destroy
+    flash[:notice] = translate_with_resource("destroy.success")
+    redirect_to :back
+  end
+
+
   # Overwrite any of the RESTful controller actions to implement custom behavior
   # For example, you may want to send an email after a foo is updated.
   #
