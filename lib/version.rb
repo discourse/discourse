0e92dfa818595fff704d9953862bd928e7a30bc9
Sam <sam.saffron@gmail.com>
2018-05-17 12:09:19 +1000

    bump version to beta 10

--- lib/version.rb
+++ lib/version.rb
@@ -5,7 +5,7 @@ module Discourse
       MAJOR = 2
       MINOR = 0
       TINY  = 0
-      PRE   = 'beta9'
+      PRE   = 'beta10'
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     end
