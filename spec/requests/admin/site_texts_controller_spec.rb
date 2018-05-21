2ceb1070743f2d0081def11cc06bc3a9d8e7dd0e
Guo Xiang Tan <tgx_world@hotmail.com>
2018-05-21 09:49:46 +0800

    Refactor tests to use the json extension instead of headers.

--- spec/requests/admin/site_texts_controller_spec.rb
+++ spec/requests/admin/site_texts_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Admin::SiteTextsController do
   let(:admin) { Fabricate(:admin) }
   let(:user) { Fabricate(:user) }
-  let(:headers) { { ACCEPT: 'application/json' } }
 
   after do
     TranslationOverride.delete_all
@@ -12,17 +11,20 @@ RSpec.describe Admin::SiteTextsController do
 
   context "#update" do
     it "raises an error if you aren't logged in" do
-      put '/admin/customize/site_texts/some_key', params: {
+      put '/admin/customize/site_texts/some_key.json', params: {
         site_text: { value: 'foo' }
-      }, headers: headers
+      }
+
       expect(response.status).to eq(404)
     end
 
     it "raises an error if you aren't an admin" do
       sign_in(user)
+
       put '/admin/customize/site_texts/some_key', params: {
         site_text: { value: 'foo' }
-      }, headers: headers
+      }
+
       expect(response.status).to eq(404)
     end
 
@@ -32,39 +34,36 @@ RSpec.describe Admin::SiteTextsController do
       end
 
       it "returns 'not found' when an unknown key is used" do
-        put '/admin/customize/site_texts/some_key', params: {
+        put '/admin/customize/site_texts/some_key.json', params: {
           site_text: { value: 'foo' }
-        }, headers: headers
+        }
 
-        expect(response).not_to be_success
+        expect(response.status).to eq(404)
 
-        json = ::JSON.parse(response.body)
+        json = JSON.parse(response.body)
         expect(json['error_type']).to eq('not_found')
       end
 
       it "works as expectd with correct keys" do
-        put '/admin/customize/site_texts/title', params: {
+        put '/admin/customize/site_texts/title.json', params: {
           site_text: { value: 'foo' }
-        }, headers: headers
+        }
 
-        expect(response).to be_success
+        expect(response.status).to eq(200)
 
         json = ::JSON.parse(response.body)
-        expect(json).to be_present
-
         site_text = json['site_text']
-        expect(site_text).to be_present
 
         expect(site_text['id']).to eq('title')
         expect(site_text['value']).to eq('foo')
       end
 
       it "does not update restricted keys" do
-        put '/admin/customize/site_texts/user_notifications.confirm_old_email.title', params: {
+        put '/admin/customize/site_texts/user_notifications.confirm_old_email.title.json', params: {
           site_text: { value: 'foo' }
-        }, headers: headers
+        }
 
-        expect(response).not_to be_success
+        expect(response.status).to eq(404)
 
         json = ::JSON.parse(response.body)
         expect(json['error_type']).to eq('not_found')
