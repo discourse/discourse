e78f1d7589ffa25f901e925cfb0a7d8d71d18a1f
Guo Xiang Tan <tgx_world@hotmail.com>
2018-05-21 14:35:59 +0800

    Fix the build take 2.

--- config/routes.rb
+++ config/routes.rb
@@ -214,7 +214,8 @@ Discourse::Application.routes.draw do
       # They have periods in their URLs often:
       get 'site_texts'          => 'site_texts#index'
       get 'site_texts/:id'      => 'site_texts#show',   constraints: { id: /[\w.\-\+]+/i }
-      put 'site_texts/:id'      => 'site_texts#update', constraints: { id: /[\w.\-\+]+[^.json]/i }
+      put 'site_texts/:id.json' => 'site_texts#update', constraints: { id: /[\w.\-\+]+/i }
+      put 'site_texts/:id'      => 'site_texts#update', constraints: { id: /[\w.\-\+]+/i }
       delete 'site_texts/:id'   => 'site_texts#revert', constraints: { id: /[\w.\-\+]+/i }
 
       get 'email_templates'          => 'email_templates#index'
