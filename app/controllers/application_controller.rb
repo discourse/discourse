3db1032bfdbc545d1e25fa2623c4a6d4b687692a
Neil Lalonde <neillalonde@gmail.com>
2018-05-23 16:58:47 -0400

    FIX: not found page shouldn't include the Google search form for sites with login_required enabled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -681,6 +681,7 @@ class ApplicationController < ActionController::Base
       @slug =  params[:slug].class == String ? params[:slug] : ''
       @slug =  (params[:id].class == String ? params[:id] : '') if @slug.blank?
       @slug.tr!('-', ' ')
+      @hide_google = true if SiteSetting.login_required
       render_to_string status: status, layout: layout, formats: [:html], template: '/exceptions/not_found'
     end
 
