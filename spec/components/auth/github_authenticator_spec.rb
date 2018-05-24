d366f8d888f5260baf55d3397c0f147fe09f5cf7
Sam <sam.saffron@gmail.com>
2018-05-24 10:48:16 +1000

    remove hack that destabliazed tese suite

--- spec/components/auth/github_authenticator_spec.rb
+++ spec/components/auth/github_authenticator_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
-# In the ghetto ... getting the spec to run in autospec
-#  thing is we need to load up all auth really early pre-fork
-#  it means that the require is not going to get a new copy
-Auth.send(:remove_const, :GithubAuthenticator)
-load 'auth/github_authenticator.rb'
-
 def auth_token_for(user)
   {
     extra: {
