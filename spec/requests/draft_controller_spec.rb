29398ca0573a9098436a4a88019e898ea7eb8c6d
OsamaSayegh <asooomaasoooma90@gmail.com>
2018-06-06 11:41:48 +0300

    REFACTOR: draft controller specs to requests

--- spec/requests/draft_controller_spec.rb
+++ spec/requests/draft_controller_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 describe DraftController do
-
   it 'requires you to be logged in' do
-    post :update
+    post "/draft"
     expect(response.status).to eq(403)
   end
 
   it 'saves a draft on update' do
-    user = log_in
-    post :update, params: { draft_key: 'xyz', data: 'my data', sequence: 0 }, format: :json
+    user = sign_in(Fabricate(:user))
+    post "/draft.json", params: { draft_key: 'xyz', data: 'my data', sequence: 0 }
+    expect(response.status).to eq(200)
     expect(Draft.get(user, 'xyz', 0)).to eq('my data')
   end
 
   it 'destroys drafts when required' do
-    user = log_in
+    user = sign_in(Fabricate(:user))
     Draft.set(user, 'xxx', 0, 'hi')
-    delete :destroy, params: { draft_key: 'xxx', sequence: 0 }, format: :json
+    delete "/draft.json", params: { draft_key: 'xxx', sequence: 0 }
+    expect(response.status).to eq(200)
     expect(Draft.get(user, 'xxx', 0)).to eq(nil)
   end
-
 end
