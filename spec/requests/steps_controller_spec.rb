46bb4689908810526447c597bd2e8b58f0ee0a00
OsamaSayegh <asooomaasoooma90@gmail.com>
2018-06-06 12:49:43 +0300

    REFACTOR: steps controller specs to requests

--- spec/requests/steps_controller_spec.rb
+++ spec/requests/steps_controller_spec.rb
@@ -1,57 +1,55 @@
 require 'rails_helper'
 
 describe StepsController do
-
   before do
     SiteSetting.wizard_enabled = true
   end
 
   it 'needs you to be logged in' do
-    put :update, params: {
-      id: 'made-up-id', fields: { forum_title: "updated title" }
-    }, format: :json
+    put "/wizard/steps/made-up-id.json", params: {
+      fields: { forum_title: "updated title" }
+    }
     expect(response.status).to eq(403)
   end
 
   it "raises an error if you aren't an admin" do
-    log_in(:moderator)
+    sign_in(Fabricate(:moderator))
 
-    put :update, params: {
-      id: 'made-up-id', fields: { forum_title: "updated title" }
-    }, format: :json
+    put "/wizard/steps/made-up-id.json", params: {
+      fields: { forum_title: "updated title" }
+    }
 
     expect(response).to be_forbidden
   end
 
   context "as an admin" do
     before do
-      log_in(:admin)
+      sign_in(Fabricate(:admin))
     end
 
     it "raises an error if the wizard is disabled" do
       SiteSetting.wizard_enabled = false
-      put :update, params: {
-        id: 'contact', fields: { contact_email: "eviltrout@example.com" }
-      }, format: :json
+      put "/wizard/steps/contact.json", params: {
+        fields: { contact_email: "eviltrout@example.com" }
+      }
       expect(response).to be_forbidden
     end
 
     it "updates properly if you are staff" do
-      put :update, params: {
-        id: 'contact', fields: { contact_email: "eviltrout@example.com" }
-      }, format: :json
+      put "/wizard/steps/contact.json", params: {
+        fields: { contact_email: "eviltrout@example.com" }
+      }
 
       expect(response).to be_success
       expect(SiteSetting.contact_email).to eq("eviltrout@example.com")
     end
 
     it "returns errors if the field has them" do
-      put :update, params: {
-        id: 'contact', fields: { contact_email: "not-an-email" }
-      }, format: :json
+      put "/wizard/steps/contact.json", params: {
+        fields: { contact_email: "not-an-email" }
+      }
 
-      expect(response).to_not be_success
+      expect(response.status).to eq(422)
     end
   end
-
 end
