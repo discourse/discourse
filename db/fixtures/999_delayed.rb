69ec90f5a33df1d025a186a7f7080dc2e4f3373c
Sam <sam.saffron@gmail.com>
2018-06-07 20:20:15 +1000

    attempt to reset column info on user table explicitly

--- db/fixtures/999_delayed.rb
+++ db/fixtures/999_delayed.rb
@@ -183,3 +183,5 @@ Migration::TableDropper.delayed_drop(
 STDERR.puts "Resetting Active Record Cache"
 Discourse.reset_active_record_cache
 ActiveRecord::Base.connection.close
+# for some reason connection tables may be off?
+User.reset_column_information
