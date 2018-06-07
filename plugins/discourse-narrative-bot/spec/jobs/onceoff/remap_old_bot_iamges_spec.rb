a6bb289124f760658095cfb16ff969101da1f09d
Sam <sam.saffron@gmail.com>
2018-06-07 16:13:15 +1000

    correct specs

--- plugins/discourse-narrative-bot/spec/jobs/onceoff/remap_old_bot_iamges_spec.rb
+++ plugins/discourse-narrative-bot/spec/jobs/onceoff/remap_old_bot_iamges_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Jobs::DiscourseNarrativeBot::RemapOldBotImages do
   context "when bot's post contains an old link" do
     let(:post) do
       Fabricate(:post,
-        user_id: -2,
+        user: User.find(-2),
         raw: 'If you’d like to learn more, select <img src="/images/font-awesome-gear.png" width="16" height="16"> <img src="/images/font-awesome-ellipsis.png" width="16" height="16"> below  and <img src="/images/font-awesome-bookmark.png" width="16" height="16"> **bookmark this private message**.  If you do, there may be a :gift: in your future!'
       )
     end
@@ -25,7 +25,7 @@ RSpec.describe Jobs::DiscourseNarrativeBot::RemapOldBotImages do
     context 'subfolder' do
       let(:post) do
         Fabricate(:post,
-          user_id: -2,
+          user: User.find(-2),
           raw: 'If you’d like to learn more, select <img src="/community/images/font-awesome-ellipsis.png" width="16" height="16"> below  and <img src="/community/images/font-awesome-bookmark.png" width="16" height="16"> **bookmark this private message**.  If you do, there may be a :gift: in your future!'
         )
       end
