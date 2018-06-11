cb445ea18b4e9b848aa364335253253acde61b09
OsamaSayegh <asooomaasoooma90@gmail.com>
2018-06-11 07:02:03 +0300

    REFACTOR: admin controller specs to requests

--- spec/requests/admin/admin_controller_spec.rb
+++ spec/requests/admin/admin_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Admin::AdminController do
-  it "should return the right response if user isn't a staff" do
-    get "/admin", params: { api_key: 'asdiasiduga' }
-    expect(response.status).to eq(404)
+  describe '#index' do
+    it "needs you to be logged in" do
+      get "/admin.json"
+      expect(response.status).to eq(404)
+    end
 
-    get "/admin"
-    expect(response.status).to eq(404)
+    it "should return the right response if user isn't a staff" do
+      sign_in(Fabricate(:user))
+      get "/admin", params: { api_key: 'asdiasiduga' }
+      expect(response.status).to eq(404)
+
+      get "/admin"
+      expect(response.status).to eq(404)
+    end
   end
 end
