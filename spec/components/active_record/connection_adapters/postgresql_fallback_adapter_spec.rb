7265c32ff617ad70c9b1e84db71793180ceedce3
Guo Xiang Tan <tgx_world@hotmail.com>
2018-06-11 15:51:16 +0800

    Skip postgres failover tests until we figure out why.

--- spec/components/active_record/connection_adapters/postgresql_fallback_adapter_spec.rb
+++ spec/components/active_record/connection_adapters/postgresql_fallback_adapter_spec.rb
@@ -27,6 +27,7 @@ describe ActiveRecord::ConnectionHandling do
   let(:postgresql_fallback_handler) { PostgreSQLFallbackHandler.instance }
 
   before do
+    skip("Figure out why this test leaks connections")
     postgresql_fallback_handler.initialized = true
 
     ['default', multisite_db].each do |db|
