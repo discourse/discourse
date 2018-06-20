8651e4fd1270a3d6af5831770271f5b7a08066a0
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-06-20 22:26:02 +0200

    FIX: hides durability section in dashboard if backups are disabled

--- app/controllers/admin/dashboard_next_controller.rb
+++ app/controllers/admin/dashboard_next_controller.rb
@@ -4,7 +4,7 @@ class Admin::DashboardNextController < Admin::AdminController
   def index
     dashboard_data = AdminDashboardNextData.fetch_cached_stats
     dashboard_data.merge!(version_check: DiscourseUpdates.check_version.as_json) if SiteSetting.version_checks?
-    dashboard_data[:disk_space] = DiskSpace.cached_stats
+    dashboard_data[:disk_space] = DiskSpace.cached_stats if SiteSetting.enable_backups
     render json: dashboard_data
   end
 end
