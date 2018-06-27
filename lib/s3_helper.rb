9e438e6cda1f78173933ed99fe1cd362e5b073dc
Rishabh <rishabhn@protonmail.com>
2018-06-27 17:00:42 +0530

    Add custom S3 Endpoints and DigitalOcean Spaces support

--- lib/s3_helper.rb
+++ lib/s3_helper.rb
@@ -150,7 +150,8 @@ class S3Helper
   end
 
   def self.s3_options(obj)
-    opts = { region: obj.s3_region }
+    opts = { region: obj.s3_region,
+             endpoint: SiteSetting.s3_endpoint }
 
     unless obj.s3_use_iam_profile
       opts[:access_key_id] = obj.s3_access_key_id
