849b4b56853756a24f0646c04e733e5af7cc2a2b
Sam <sam.saffron@gmail.com>
2018-06-28 18:14:55 +1000

    SECURITY: category badges should HTML escape names

--- app/models/category.rb
+++ app/models/category.rb
@@ -217,7 +217,7 @@ SQL
 
     @@cache ||= LruRedux::ThreadSafeCache.new(1000)
     @@cache.getset(self.description) do
-      Nokogiri::HTML.fragment(self.description).text.strip
+      Nokogiri::HTML.fragment(self.description).text.strip.html_safe
     end
   end
 
