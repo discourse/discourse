501dc2252dd40c55aeedb704e40ddfe7699c4746
Neil Lalonde <neillalonde@gmail.com>
2018-07-03 17:54:45 -0400

    remove crawler blocking until multisite support

--- lib/middleware/request_tracker.rb
+++ lib/middleware/request_tracker.rb
@@ -175,11 +175,11 @@ class Middleware::RequestTracker
       return result
     end
 
-    if block_crawler(request)
-      log_request = false
-      result = [403, { 'Content-Type' => 'text/plain' }, ['Crawler is not allowed']]
-      return result
-    end
+    # if block_crawler(request)
+    #   log_request = false
+    #   result = [403, { 'Content-Type' => 'text/plain' }, ["Crawler is not allowed."]]
+    #   return result
+    # end
 
     env["discourse.request_tracker"] = self
     MethodProfiler.start
