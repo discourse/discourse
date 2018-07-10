10bc69a62f681fb2f2a02e7318403b48840da6aa
Jordan Seanor <jose@hms.se>
2018-07-09 21:28:57 -0400

    FEATURE: Event on topic merge (#6057)

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -913,6 +913,7 @@ class Topic < ActiveRecord::Base
 
     if opts[:destination_topic_id]
       post_mover.to_topic opts[:destination_topic_id]
+      DiscourseEvent.trigger(:topic_merged,  post_mover.original_topic, post_mover.destination_topic)
     elsif opts[:title]
       post_mover.to_new_topic(opts[:title], opts[:category_id], opts[:tags])
     end
