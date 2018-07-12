b8faf57d5d1e7de064aa9d5676ae98b076ad3f84
Arpit Jalan <arpit@techapj.com>
2018-07-12 18:52:41 +0530

    FIX: do not render typeform iframe in preview, render an image instead

--- lib/onebox/engine/typeform_onebox.rb
+++ lib/onebox/engine/typeform_onebox.rb
@@ -2,27 +2,23 @@ module Onebox
   module Engine
     class TypeformOnebox
       include Engine
+      include StandardEmbed
 
       matches_regexp(/^https?:\/\/[a-z0-9]+\.typeform\.com\/to\/[a-zA-Z0-9]+/)
       always_https
 
       def placeholder_html
-        escaped_url = ::Onebox::Helpers.normalize_url_for_output(@url)
-
-        <<-HTML
-          <iframe src="#{escaped_url}"
-                  width="100%"
-                  height="600px"
-                  scrolling="no"
-                  frameborder="0">
-          </iframe>
-        HTML
+        og = get_opengraph
+        escaped_src = ::Onebox::Helpers.normalize_url_for_output(og[:image])
+        "<img src='#{escaped_src}' #{Helpers.title_attr(og)}>"
       end
 
       def to_html
-        escaped_url = ::Onebox::Helpers.normalize_url_for_output(@url)
+        og = get_opengraph
+        escaped_src = ::Onebox::Helpers.normalize_url_for_output(og[:url])
+
         <<-HTML
-          <iframe src="#{escaped_url}"
+          <iframe src="#{escaped_src}"
                   width="100%"
                   height="600px"
                   scrolling="no"
