d7b5a374c2e96e52e67c6ca9c0500724016242f1
David Taylor <david@taylorhq.com>
2018-07-23 10:42:15 +0100

    Fix indentation

--- spec/helpers/transaction_helper_spec.rb
+++ spec/helpers/transaction_helper_spec.rb
@@ -5,28 +5,28 @@ describe TransactionHelper do
   it "runs callbacks after outermost transaction is committed" do
     outputString = "1"
 
-      # Main transaction
-      ActiveRecord::Base.transaction do
-        outputString += "2"
+    # Main transaction
+    ActiveRecord::Base.transaction do  
+      outputString += "2"
 
-          # Nested transaction
-          ActiveRecord::Base.transaction do
-            outputString += "3"
+        # Nested transaction
+        ActiveRecord::Base.transaction do
+          outputString += "3"
 
-              TransactionHelper.after_commit do
-                outputString += "6"
-              end
-              outputString += "4"
-          end
+            TransactionHelper.after_commit do
+              outputString += "6"
+            end
+            outputString += "4"
+        end
 
-          TransactionHelper.after_commit do
-            outputString += "7"
-          end
+        TransactionHelper.after_commit do
+          outputString += "7"
+        end
 
-          outputString += "5"
-      end
+        outputString += "5"
+    end
 
-      expect(outputString).to eq("1234567")
+    expect(outputString).to eq("1234567")
   end
 
 end
