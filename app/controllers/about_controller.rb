b41b897951938b50401a4b6c15f77f62b8433522
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-07-24 11:11:06 -0400

    Update about_controller.rb

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,12 +1,11 @@
 require_dependency 'rate_limiter'
+class AboutController<ApplicationController
 
-class AboutController < ApplicationController
-
   
   
   
   
-  requires_login only: [:live_post_counts]
+  requires_login only: [ :live_post_counts]
   skip_before_action :check_xhr, only: [:index]
   def index
     return redirect_to path('/login') if SiteSetting.login_required? && current_user.nil?
@@ -15,8 +14,8 @@ class AboutController < ApplicationController
       format.html do
         render :index
       end
-      format.json do
-render_serialized(@about, AboutSerializer)
+format.json do
+render_serialized(@about,AboutSerializer)
       end
     end
   end
@@ -25,8 +24,8 @@ render_serialized(@about, AboutSerializer)
     RateLimiter.new(current_user, "live_post_counts", 1, 10.minutes).performed! unless current_user.staff?
     category_topic_ids=Category.pluck(:topic_id).compact!
     public_topics = Topic.listable_topics.visible.secured(Guardian.new(nil)).where.not(id: category_topic_ids)
-    stats={public_topic_count: public_topics.count }
-    stats[:public_post_count] = public_topics.sum(:posts_count) - stats[:public_topic_count]
-    render json: stats
+    stats={public_topic_count:public_topics.count}
+    stats[:public_post_count]=public_topics.sum(:posts_count) - stats[:public_topic_count]
+render json: stats
   end
 end
