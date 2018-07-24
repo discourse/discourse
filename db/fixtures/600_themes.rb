7a3c5410770ec0752f72ce13f87bef6601c4aa0d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-07-24 09:00:20 -0400

    UX: Preview multiple color schemes in wizard (#6151)
    It was a dropdown to provide choices of color schemes,
    and only one scheme could be shown.
    With this commit, multiple color scheme previews can be displayed on
    one page at the same time, making admins choose color schemes more
    easily.
    
    Theme preview windows are shrinked.
    
    Imported default color schemes.
    
    Co-Authored-By: Misaka 0x4e21 <misaka4e21@gmail.com>

--- db/fixtures/600_themes.rb
+++ db/fixtures/600_themes.rb
@@ -2,9 +2,9 @@
 if !Theme.exists?
   STDERR.puts "> Seeding dark and light themes"
 
-  name = I18n.t("wizard.step.colors.fields.theme_id.choices.dark.label")
-  dark_scheme = ColorScheme.find_by(base_scheme_id: "dark")
-  dark_scheme ||= ColorScheme.create_from_base(name: name, via_wizard: true, base_scheme_id: "dark")
+  name = I18n.t("color_schemes.dark_theme_name")
+  dark_scheme = ColorScheme.find_by(base_scheme_id: "Dark")
+  dark_scheme ||= ColorScheme.create_from_base(name: name, via_wizard: true, base_scheme_id: "Dark")
 
   name = I18n.t('color_schemes.dark_theme_name')
   _dark_theme = Theme.create(name: name, user_id: -1,
