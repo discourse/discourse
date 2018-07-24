030824b7de61f9cfdaf3eed02af69910773c4919
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-07-24 10:57:00 -0400

    Update about_controller.rb

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,20 +2,21 @@ require_dependency 'rate_limiter'
 
 class AboutController < ApplicationController
 
+  
+  
+  
+  
   requires_login only: [:live_post_counts]
-
   skip_before_action :check_xhr, only: [:index]
-
   def index
     return redirect_to path('/login') if SiteSetting.login_required? && current_user.nil?
-
     @about = About.new
     respond_to do |format|
       format.html do
         render :index
       end
       format.json do
-        render_serialized(@about, AboutSerializer)
+render_serialized(@about, AboutSerializer)
       end
     end
   end
