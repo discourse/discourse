3ea08ee85c95ac49130d5f3b5bc816ba08812a9d
Gerhard Schlager <mail@gerhard-schlager.at>
2018-07-25 11:25:14 +0200

    FIX: Translation for default (light) color scheme was missing

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -97,7 +97,7 @@ class Admin::ThemesController < Admin::AdminController
   def index
     @theme = Theme.order(:name).includes(:theme_fields, :remote_theme)
     @color_schemes = ColorScheme.all.to_a
-    light = ColorScheme.new(name: I18n.t("color_schemes.default"))
+    light = ColorScheme.new(name: I18n.t("color_schemes.light"))
     @color_schemes.unshift(light)
 
     payload = {
