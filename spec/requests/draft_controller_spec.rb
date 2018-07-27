06cae7454b3202a0abb8648e1afcbb5f3c8e097b
pmusaraj <pmusaraj@gmail.com>
2018-07-27 12:15:24 -0400

    move DraftsController spec to its own file

--- spec/requests/draft_controller_spec.rb
+++ spec/requests/draft_controller_spec.rb
@@ -20,31 +20,4 @@ describe DraftController do
     expect(response.status).to eq(200)
     expect(Draft.get(user, 'xxx', 0)).to eq(nil)
   end
-
-end
-
-describe "#drafts" do
-  it 'requires you to be logged in' do
-    get "/drafts.json"
-    expect(response.status).to eq(403)
-  end
-
-  it 'returns correct stream length after adding a draft' do
-    user = sign_in(Fabricate(:user))
-    Draft.set(user, 'xxx', 0, '{}')
-    get "/drafts.json", params: { username: user.username }
-    expect(response.status).to eq(200)
-    parsed = JSON.parse(response.body)
-    expect(parsed["drafts"].length).to eq(1)
-  end
-
-  it 'has empty stream after deleting last draft' do
-    user = sign_in(Fabricate(:user))
-    Draft.set(user, 'xxx', 0, '{}')
-    Draft.clear(user, 'xxx', 0)
-    get "/drafts.json", params: { username: user.username }
-    expect(response.status).to eq(200)
-    parsed = JSON.parse(response.body)
-    expect(parsed["drafts"].length).to eq(0)
-  end
 end
