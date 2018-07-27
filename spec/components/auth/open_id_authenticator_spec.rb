cead3c99f5f164c84092bc2bc1f4766629b588e6
David Taylor <david@taylorhq.com>
2018-07-27 16:20:47 +0100

    FEATURE: Revoke and connect for Yahoo logins

--- spec/components/auth/open_id_authenticator_spec.rb
+++ spec/components/auth/open_id_authenticator_spec.rb
@@ -22,4 +22,39 @@ describe Auth::OpenIdAuthenticator do
     response = OpenStruct.new(identity_url: 'abc')
     expect { auth.after_authenticate(info: {}, extra: { response: response }) }.to raise_error(Discourse::InvalidParameters)
   end
+
+  it 'can connect to a different existing user account' do
+    authenticator = Auth::OpenIdAuthenticator.new("test", "id", "enable_yahoo_logins", trusted: true)
+    user1 = Fabricate(:user)
+    user2 = Fabricate(:user)
+
+    UserOpenId.create!(url: "id/123" , user_id: user1.id, email: "bob@example.com", active: true)
+
+    hash = {
+      info: { email: user1.email }, extra: { response: OpenStruct.new(identity_url: 'id/123') }
+    }
+
+    result = authenticator.after_authenticate(hash, existing_account: user2)
+
+    expect(result.user.id).to eq(user2.id)
+    expect(UserOpenId.exists?(user_id: user1.id)).to eq(false)
+    expect(UserOpenId.exists?(user_id: user2.id)).to eq(true)
+  end
+
+  context 'revoke' do
+    let(:user) { Fabricate(:user) }
+    let(:authenticator) { Auth::OpenIdAuthenticator.new("test", "id", "enable_yahoo_logins", trusted: true) }
+
+    it 'raises exception if no entry for user' do
+      expect { authenticator.revoke(user) }.to raise_error(Discourse::NotFound)
+    end
+
+      it 'revokes correctly' do
+        UserOpenId.create!(url: "id/123" , user_id: user.id, email: "bob@example.com", active: true)
+        expect(authenticator.can_revoke?).to eq(true)
+        expect(authenticator.revoke(user)).to eq(true)
+        expect(authenticator.description_for_user(user)).to eq("")
+      end
+
+  end
 end
