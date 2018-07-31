e1707c6d5b66118687d45d91cdaf87afab16a96e
Gerhard Schlager <mail@gerhard-schlager.at>
2018-07-31 12:02:18 +0200

    Use rchardet instead of charlock_holmes gem

--- lib/encodings.rb
+++ lib/encodings.rb
@@ -1,20 +1,12 @@
-require 'charlock_holmes'
+require 'rchardet'
 
 module Encodings
-  BINARY_SCAN_LENGTH = 0
+  def self.to_utf8(string)
+    result = CharDet.detect(string)
 
-  def self.to_utf8(string, encoding_hint: nil, delete_bom: true)
-    detector = CharlockHolmes::EncodingDetector.new(BINARY_SCAN_LENGTH)
-    result = detector.detect(string, encoding_hint&.to_s)
-
-    if result && result[:encoding]
-      string = CharlockHolmes::Converter.convert(string, result[:encoding], Encoding::UTF_8.name)
-    else
-      string = string.encode(Encoding::UTF_8, undef: :replace, invalid: :replace, replace: '')
-    end
-
-    delete_bom!(string) if delete_bom
-    string
+    encoded_string = try_utf8(string, result['encoding']) if result && result['encoding']
+    encoded_string = force_utf8(string) if encoded_string.nil?
+    encoded_string
   end
 
   def self.try_utf8(string, source_encoding)
@@ -24,6 +16,14 @@ module Encodings
     Encoding::UndefinedConversionError,
     Encoding::ConverterNotFoundError
     nil
+  end
+
+  def self.force_utf8(string)
+    encoded_string = string.encode(Encoding::UTF_8,
+                                   undef: :replace,
+                                   invalid: :replace,
+                                   replace: '')
+    delete_bom!(encoded_string)
   end
 
   def self.delete_bom!(string)
