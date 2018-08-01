693f81f7389de5fbe933ac9c30bb9dc49ce80955
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-08-01 09:45:20 -0400

    FIX: more resilient/consitent dashboard caching

--- app/jobs/regular/update_disk_space.rb
+++ app/jobs/regular/update_disk_space.rb
@@ -5,8 +5,8 @@ module Jobs
     sidekiq_options retry: false
 
     def execute(args)
-      $redis.set(DiskSpace::DISK_SPACE_STATS_CACHE_KEY, DiskSpace.stats.to_json)
-      $redis.set(DiskSpace::DISK_SPACE_STATS_UPDATED_CACHE_KEY, Time.now.to_i)
+      Discourse.cache.write(DiskSpace::DISK_SPACE_STATS_CACHE_KEY, DiskSpace.stats.to_json)
+      Discourse.cache.write(DiskSpace::DISK_SPACE_STATS_UPDATED_CACHE_KEY, Time.now.to_i)
     end
   end
 end
