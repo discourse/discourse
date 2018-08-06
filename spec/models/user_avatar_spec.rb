4e11811321cea64709a26f6ff9ba7570f8e45fa6
Guo Xiang Tan <tgx_world@hotmail.com>
2018-08-06 10:42:03 +0800

    FIX: `UserAvatar#update_gravatar!` does not update `User#uploaded_avatar`.
    https://meta.discourse.org/t/missing-user-profile-pictures/93844/4

--- spec/models/user_avatar_spec.rb
+++ spec/models/user_avatar_spec.rb
@@ -4,16 +4,66 @@ describe UserAvatar do
   let(:user) { Fabricate(:user) }
   let(:avatar) { user.create_user_avatar! }
 
-  it 'can update gravatars' do
-    temp = Tempfile.new('test')
-    temp.binmode
-    # tiny valid png
-    temp.write(Base64.decode64("R0lGODlhAQABALMAAAAAAIAAAACAAICAAAAAgIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//wBiZCH5BAEAAA8ALAAAAAABAAEAAAQC8EUAOw=="))
-    temp.rewind
-    FileHelper.expects(:download).returns(temp)
-    avatar.update_gravatar!
-    temp.unlink
-    expect(avatar.gravatar_upload).not_to eq(nil)
+  describe '#update_gravatar!' do
+    let(:temp) { Tempfile.new('test') }
+    let(:upload) { Fabricate(:upload, user: user) }
+
+    before do
+      temp.binmode
+      # tiny valid png
+      temp.write(Base64.decode64("R0lGODlhAQABALMAAAAAAIAAAACAAICAAAAAgIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//wBiZCH5BAEAAA8ALAAAAAABAAEAAAQC8EUAOw=="))
+      temp.rewind
+      FileHelper.expects(:download).returns(temp)
+    end
+
+    after do
+      temp.unlink
+    end
+
+    it 'can update gravatars' do
+      expect do
+        avatar.update_gravatar!
+      end.to change { Upload.count }.by(1)
+
+      upload = Upload.last
+
+      expect(avatar.gravatar_upload).to eq(upload)
+      expect(user.reload.uploaded_avatar).to eq(nil)
+    end
+
+    describe 'when user has an existing custom upload' do
+      it "should not change the user's uploaded avatar" do
+        user.update!(uploaded_avatar: upload)
+
+        avatar.update!(
+          custom_upload: upload,
+          gravatar_upload: Fabricate(:upload, user: user)
+        )
+
+        avatar.update_gravatar!
+
+        expect(upload.reload).to eq(upload)
+        expect(user.reload.uploaded_avatar).to eq(upload)
+        expect(avatar.reload.custom_upload).to eq(upload)
+        expect(avatar.gravatar_upload).to eq(Upload.last)
+      end
+    end
+
+    describe 'when user has an existing gravatar' do
+      it "should update the user's uploaded avatar correctly" do
+        user.update!(uploaded_avatar: upload)
+        avatar.update!(gravatar_upload: upload)
+
+        avatar.update_gravatar!
+
+        expect(Upload.find_by(id: upload.id)).to eq(nil)
+
+        new_upload = Upload.last
+
+        expect(user.reload.uploaded_avatar).to eq(new_upload)
+        expect(avatar.reload.gravatar_upload).to eq(new_upload)
+      end
+    end
   end
 
   context '.import_url_for_user' do
