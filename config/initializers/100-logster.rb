dadbf2edb441a14e6cefd75e82eb4c2ee1e5cdab
Guo Xiang Tan <tgx_world@hotmail.com>
2018-08-13 17:05:42 +0800

    DEV: Log to STDOUT in development like how `rails s` used to.

--- config/initializers/100-logster.rb
+++ config/initializers/100-logster.rb
@@ -1,3 +1,14 @@
+if Rails.env.development? && !Sidekiq.server?
+  console = ActiveSupport::Logger.new(STDOUT)
+  original_logger = Rails.logger.chained.first
+  console.formatter = original_logger.formatter
+  console.level = original_logger.level
+
+  unless ActiveSupport::Logger.logger_outputs_to?(original_logger, STDOUT)
+    original_logger.extend(ActiveSupport::Logger.broadcast(console))
+  end
+end
+
 if Rails.env.production?
   Logster.store.ignore = [
     # honestly, Rails should not be logging this, its real noisy
