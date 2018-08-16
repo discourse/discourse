9178d86eb08b24348d96ae1aaf4f204fccff1c3f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-08-16 12:59:25 +0200

    uses emoji versions for specs

--- spec/jobs/rebake_custom_emoji_posts_spec.rb
+++ spec/jobs/rebake_custom_emoji_posts_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Jobs::RebakeCustomEmojiPosts do
     post = Fabricate(:post, raw: 'some post with :test: yay')
 
     expect(post.reload.cooked).to eq(
-      "<p>some post with <img src=\"#{upload.url}?v=5\" title=\":test:\" class=\"emoji emoji-custom\" alt=\":test:\"> yay</p>"
+      "<p>some post with <img src=\"#{upload.url}?v=#{Emoji::EMOJI_VERSION}\" title=\":test:\" class=\"emoji emoji-custom\" alt=\":test:\"> yay</p>"
     )
 
     custom_emoji.destroy!
