f5ad731e36017518a84bce3619a9184b5fd24d74
James Kiesel <james.kiesel@gmail.com>
2018-08-21 13:23:15 -0500

    Match register controller TL to rake admin:create

--- app/controllers/finish_installation_controller.rb
+++ app/controllers/finish_installation_controller.rb
@@ -23,6 +23,7 @@ class FinishInstallationController < ApplicationController
       @user.password_required!
 
       if @user.save
+        @user.change_trust_level!(1)
         @email_token = @user.email_tokens.unconfirmed.active.first
         Jobs.enqueue(:critical_user_email, type: :signup, user_id: @user.id, email_token: @email_token.token)
         return redirect_confirm(@user.email)
--- spec/requests/finish_installation_controller_spec.rb
+++ spec/requests/finish_installation_controller_spec.rb
@@ -80,6 +80,16 @@ describe FinishInstallationController do
         expect(response).to be_redirect
         expect(User.where(username: 'eviltrout').exists?).to eq(true)
       end
+
+      it "sets the admins trust level" do
+        post "/finish-installation/register.json", params: {
+          email: 'robin@example.com',
+          username: 'eviltrout',
+          password: 'disismypasswordokay'
+        }
+
+        expect(User.find_by(username: 'eviltrout').trust_level).to eq 1
+      end
     end
   end
 
