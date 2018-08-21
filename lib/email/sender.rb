c91da622d40ce0d02660ac3a105c06410ba75409
Sam <sam.saffron@gmail.com>
2018-08-21 10:59:18 +1000

    FIX: handle concurrently creating post reply keys
    In some very rare conditions this would be called concurrently and fail

--- lib/email/sender.rb
+++ lib/email/sender.rb
@@ -262,7 +262,8 @@ module Email
         post_id &&
         header_value(Email::MessageBuilder::ALLOW_REPLY_BY_EMAIL_HEADER).present?
 
-      reply_key = PostReplyKey.find_or_create_by!(
+      # use safe variant here cause we tend to see concurrency issue
+      reply_key = PostReplyKey.find_or_create_by_safe!(
         post_id: post_id,
         user_id: user_id
       ).reply_key
