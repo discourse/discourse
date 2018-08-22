733a98899ab50ada38a6336d04b6cf07d227a1ad
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-08-22 17:59:45 +0200

    FIX: better handling of no prev30Days

--- app/models/report.rb
+++ app/models/report.rb
@@ -217,8 +217,9 @@ class Report
     report.icon = 'user'
 
     basic_report_about report, UserVisit, :by_day, report.start_date, report.end_date, report.group_id
-
     add_counts report, UserVisit, 'visited_at'
+
+    report.prev30Days = UserVisit.where(mobile: true).where("visited_at >= ? and visited_at < ?", report.start_date - 30.days, report.start_date).count
   end
 
   def self.report_mobile_visits(report)
