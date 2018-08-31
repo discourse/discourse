020e6248fe198bf23e5254a82e9f68f7ca799d9b
OsamaSayegh <asooomaasoooma90@gmail.com>
2018-08-31 11:47:40 +0300

    FIX: precompile `desktop_theme` and `mobile_theme` stylesheets

--- lib/stylesheet/manager.rb
+++ lib/stylesheet/manager.rb
@@ -86,8 +86,9 @@ class Stylesheet::Manager
     themes = Theme.where('user_selectable OR id = ?', SiteSetting.default_theme_id).pluck(:id, :name)
     themes << nil
     themes.each do |id, name|
-      [:desktop, :mobile, :desktop_rtl, :mobile_rtl].each do |target|
+      [:desktop, :mobile, :desktop_rtl, :mobile_rtl, :desktop_theme, :mobile_theme, :admin].each do |target|
         theme_id = id || SiteSetting.default_theme_id
+        next if target =~ THEME_REGEX && theme_id == -1
         cache_key = "#{target}_#{theme_id}"
 
         STDERR.puts "precompile target: #{target} #{name}"
