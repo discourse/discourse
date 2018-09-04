47156114f1bf298dbd0462a84ee390fa741d8e17
Gerhard Schlager <mail@gerhard-schlager.at>
2018-09-04 12:01:29 +0200

    FIX: Detect {{foo}} as interpolation key

--- lib/i18n/i18n_interpolation_keys_finder.rb
+++ lib/i18n/i18n_interpolation_keys_finder.rb
@@ -1,6 +1,6 @@
 class I18nInterpolationKeysFinder
   def self.find(text)
-    keys = text.scan(I18n::INTERPOLATION_PATTERN)
+    keys = text.scan(Regexp.union(I18n::INTERPOLATION_PATTERN, /\{\{(\w+)\}\}/))
     keys.flatten!
     keys.compact!
     keys.uniq!
