b6a139b58182015fb895b80dd239b5be83f14a37
Guo Xiang Tan <tgx_world@hotmail.com>
2018-09-06 12:41:43 +0800

    Fix broken spec.

--- app/jobs/regular/pull_hotlinked_images.rb
+++ app/jobs/regular/pull_hotlinked_images.rb
@@ -130,7 +130,7 @@ module Jobs
         changes = { raw: raw, edit_reason: I18n.t("upload.edit_reason") }
         post.revise(Discourse.system_user, changes, bypass_bump: true)
       elsif has_downloaded_image || has_new_large_image || has_new_broken_image
-        post.trigger_post_process(true)
+        post.trigger_post_process(bypass_bump: true)
       end
     end
 
