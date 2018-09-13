f1f0a6b3583f0712340f41918bb3b9a8c84a8de1
Guo Xiang Tan <tgx_world@hotmail.com>
2018-09-13 13:59:17 +0800

    FIX: Do not try to recover invalid `Upload#short_url` in `UploadRecovery`.

--- spec/lib/upload_recovery_spec.rb
+++ spec/lib/upload_recovery_spec.rb
@@ -18,31 +18,45 @@ RSpec.describe UploadRecovery do
     ).tap(&:link_post_uploads)
   end
 
+  let(:upload_recovery) { UploadRecovery.new }
+
   before do
     SiteSetting.queue_jobs = false
   end
 
   describe '#recover' do
-    it 'should recover the upload' do
-      begin
-        stub_request(:get, "http://test.localhost#{upload.url}")
-          .to_return(status: 200)
+    after do
+      public_path = "#{Discourse.store.public_dir}#{upload.url}"
 
-        expect do
-          upload.destroy!
-        end.to change { post.reload.uploads.count }.from(1).to(0)
+      [
+        public_path,
+        public_path.sub("uploads", "uploads/tombstone")
+      ].each { |path| File.delete(path) if File.exists?(path) }
+    end
 
-        expect do
-          UploadRecovery.new.recover
-        end.to change { post.reload.uploads.count }.from(0).to(1)
-      ensure
-        public_path = "#{Discourse.store.public_dir}#{upload.url}"
+    describe 'when given an invalid sha1' do
+      it 'should not do anything' do
+        upload_recovery.expects(:recover_from_local).never
 
-        [
-          public_path,
-          public_path.sub("uploads", "uploads/tombstone")
-        ].each { |path| File.delete(path) if File.exists?(path) }
+        post.update!(
+          raw: "![logo.png](upload://#{'a' * 28}.png)"
+        )
+
+        upload_recovery.recover
       end
+    end
+
+    it 'should recover the upload' do
+      stub_request(:get, "http://test.localhost#{upload.url}")
+        .to_return(status: 200)
+
+      expect do
+        upload.destroy!
+      end.to change { post.reload.uploads.count }.from(1).to(0)
+
+      expect do
+        upload_recovery.recover
+      end.to change { post.reload.uploads.count }.from(0).to(1)
     end
   end
 end
