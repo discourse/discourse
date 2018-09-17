2d1bcfac138a87354736c96648fe7820a5d673bf
Neil Lalonde <neillalonde@gmail.com>
2018-09-17 11:40:15 -0400

    FIX: tag groups page should only be visible to staff
    No security concern here because nothing private was visible,
    and no actions could be taken by non-staff users.

--- config/routes.rb
+++ config/routes.rb
@@ -793,7 +793,7 @@ Discourse::Application.routes.draw do
     end
   end
 
-  resources :tag_groups, except: [:new, :edit] do
+  resources :tag_groups, constraints: StaffConstraint.new, except: [:new, :edit] do
     collection do
       get '/filter/search' => 'tag_groups#search'
     end
