f2e5891cc5b692e95e7a5f56800b3bca7e4e5dbf
Vinoth Kannan <vinothkannan@vinkas.com>
2018-09-19 00:48:39 +0530

    FIX: Add missing variable in params

--- db/migrate/20180831182853_add_branch_to_remote_theme.rb
+++ db/migrate/20180831182853_add_branch_to_remote_theme.rb
@@ -1,5 +1,5 @@
 class AddBranchToRemoteTheme < ActiveRecord::Migration[5.2]
   def change
-      add_column :remote_themes, :branch, :string
+    add_column :remote_themes, :branch, :string
   end
 end
