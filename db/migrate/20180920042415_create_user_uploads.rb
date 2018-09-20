df45e82377c037a074e898f4e3363e200b8a403a
Sam <sam.saffron@gmail.com>
2018-09-20 15:33:10 +1000

    SECURITY: only allow picking of avatars created by self (#6417)
    * SECURITY: only allow picking of avatars created by self
    
    Also adds origin tracking to all uploads including de-duplicated uploads

---
+++ db/migrate/20180920042415_create_user_uploads.rb
@@ -0,0 +1,22 @@
+class CreateUserUploads < ActiveRecord::Migration[5.2]
+  def up
+    create_table :user_uploads do |t|
+      t.integer :upload_id, null: false
+      t.integer :user_id, null: false
+      t.datetime :created_at, null: false
+    end
+
+    add_index :user_uploads, [:upload_id, :user_id], unique: true
+
+    execute <<~SQL
+      INSERT INTO user_uploads(upload_id, user_id, created_at)
+      SELECT id, user_id, COALESCE(created_at, current_timestamp)
+      FROM uploads
+      WHERE user_id IS NOT NULL
+    SQL
+  end
+
+  def down
+    drop_table :user_uploads
+  end
+end
