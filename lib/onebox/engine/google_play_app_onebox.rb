8940da3b370fc00516fcdcdf06778aef2dc662c7
Arpit Jalan <arpit@techapj.com>
2018-10-03 16:16:02 +0530

    Fix google play store onebox

--- lib/onebox/engine/google_play_app_onebox.rb
+++ lib/onebox/engine/google_play_app_onebox.rb
@@ -15,13 +15,13 @@ module Onebox
       private
 
       def data
+        price = raw.css("meta[itemprop=price]").first["content"] rescue "Free"
         result = {
           link: link,
-          title: raw.css(".document-title div").inner_text,
-          developer: raw.css(".document-subtitle.primary").inner_text,
-          image: raw.css(".cover-image").first["src"],
-          description: raw.css(".text-body div").inner_text[0..DEFAULTS[:MAX_DESCRIPTION_CHARS]].chop + "...",
-          price: raw.css(".price.buy meta[itemprop=price]").first["content"]
+          title: raw.css("meta[property='og:title']").first["content"].gsub(" - Apps on Google Play", ""),
+          image: ::Onebox::Helpers.normalize_url_for_output(raw.css("meta[property='og:image']").first["content"]),
+          description: raw.css("meta[name=description]").first["content"][0..DEFAULTS[:MAX_DESCRIPTION_CHARS]].chop + "...",
+          price: price
         }
         if result[:price] == "0"
           result[:price] = "Free"
--- spec/lib/onebox/engine/google_play_app_onebox_spec.rb
+++ spec/lib/onebox/engine/google_play_app_onebox_spec.rb
@@ -3,22 +3,23 @@ require "spec_helper"
 describe Onebox::Engine::GooglePlayAppOnebox do
   before(:all) do
     @link = "https://play.google.com/store/apps/details?id=com.hulu.plus&hl=en"
+    fake("https://play.google.com/store/apps/details?id=com.hulu.plus&hl=en", response("googleplayapp"))
   end
 
   include_context "engines"
   it_behaves_like "an engine"
 
   describe "#to_html" do
-    it "has developer" do
-      expect(html).to include("Hulu")
+    it "has title" do
+      expect(html).to include("Hulu: Stream TV, Movies &amp; more")
     end
 
     it "has image" do
-      expect(html).to include("JH08z41G8hlCw=w300-rw")
+      expect(html).to include("4iScc4heC5Cog-i-es2hIYe0RuewYTkGiJfHAaXv0Kb2Q5b2qpbYWxWiooAPuUEhpg")
     end
 
     it "has description" do
-      expect(html).to include("Instantly watch current TV shows")
+      expect(html).to include("Enjoy all your TV in one place with a new Hulu experience")
     end
 
     it "has price" do
