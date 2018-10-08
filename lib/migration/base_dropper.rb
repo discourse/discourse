9630f575cc61d44c00604a39931949a6be280581
Guo Xiang Tan <gxtan1990@gmail.com>
2018-10-08 15:47:38 +0800

    FEATURE: Post deployment migrations. (#6406)
    This moves us away from the delayed drops pattern which
    was problematic on two counts. First, it uses a hardcoded "delay for"
    duration which may be too short for certain deployment strategies.
    Second, delayed drop doesn't ensure that it only runs after
    the latest application code has been deployed. If the migration runs
    and the application code fails to deploy, running the migration after
    "delay for" has been met will cause the application to blow up.
    
    The new strategy allows post deployment migrations to be skipped if the
    env `SKIP_POST_DEPLOYMENT_MIGRATIONS` is provided.
    
    ```
    SKIP_POST_DEPLOYMENT_MIGRATIONS=1 rake db:migrate
    -> deploy app servers
    SKIP_POST_DEPLOYMENT_MIGRATIONS=0 rake db:migrate
    ```
    
    To aid with the generation of a post deployment migration, a generator
    has been added. Simply run `rails generate post_migration`.

--- lib/migration/base_dropper.rb
+++ lib/migration/base_dropper.rb
@@ -2,51 +2,6 @@ module Migration
   class BaseDropper
     FUNCTION_SCHEMA_NAME = "discourse_functions".freeze
 
-    def initialize(after_migration, delay, on_drop, after_drop)
-      @after_migration = after_migration
-      @on_drop = on_drop
-      @after_drop = after_drop
-
-      # in production we need some extra delay to allow for slow migrations
-      @delay = delay || (Rails.env.production? ? 3600 : 0)
-    end
-
-    def delayed_drop
-      if droppable?
-        @on_drop&.call
-        execute_drop!
-        @after_drop&.call
-
-        Discourse.reset_active_record_cache
-      end
-    end
-
-    private
-
-    def droppable?
-      raise NotImplementedError
-    end
-
-    def execute_drop!
-      raise NotImplementedError
-    end
-
-    def previous_migration_done
-      <<~SQL
-        EXISTS(
-            SELECT 1
-            FROM schema_migration_details
-            WHERE name = :after_migration AND
-                  (created_at <= (current_timestamp AT TIME ZONE 'UTC' - INTERVAL :delay) OR
-                   (SELECT created_at
-                    FROM schema_migration_details
-                    ORDER BY id ASC
-                    LIMIT 1) > (current_timestamp AT TIME ZONE 'UTC' - INTERVAL '10 minutes')
-                  )
-        )
-      SQL
-    end
-
     def self.create_readonly_function(table_name, column_name = nil)
       DB.exec <<~SQL
         CREATE SCHEMA IF NOT EXISTS #{FUNCTION_SCHEMA_NAME};
@@ -63,15 +18,6 @@ module Migration
           END
         $rcr$ LANGUAGE plpgsql;
       SQL
-    end
-    private_class_method :create_readonly_function
-
-    def self.validate_table_name(table_name)
-      raise ArgumentError.new("Invalid table name passed: #{table_name}") if table_name =~ /[^a-z0-9_]/i
-    end
-
-    def self.validate_column_name(column_name)
-      raise ArgumentError.new("Invalid column name passed to drop #{column_name}") if column_name =~ /[^a-z0-9_]/i
     end
 
     def self.readonly_function_name(table_name, column_name = nil)
