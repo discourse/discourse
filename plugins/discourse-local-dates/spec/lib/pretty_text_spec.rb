f38caf30fb5471a1efd780a6469423ed18448a3f
Vinoth Kannan <vinothkannan@vinkas.com>
2018-10-12 03:32:38 +0530

    Add qunit test and fix the rspec method

--- plugins/discourse-local-dates/spec/lib/pretty_text_spec.rb
+++ plugins/discourse-local-dates/spec/lib/pretty_text_spec.rb
@@ -7,7 +7,7 @@ describe PrettyText do
       [date=2018-05-08 time=22:00 format=LLL timezones="Europe/Paris|America/Los_Angeles"]
     MD
     cooked_mail = <<~HTML
-      <p><span class="discourse-local-date" data-date="2018-05-08" data-time="22:00" data-format="LLL" data-timezones="Europe/Paris|America/Los_Angeles" data-email-preview="May 9, 2018 12:00 AM (Europe: Paris)">May 9, 2018 12:00 AM (Europe: Paris)</span></p>
+      <p><span class="discourse-local-date" data-date="2018-05-08" data-format="LLL" data-timezones="Europe/Paris|America/Los_Angeles" data-time="22:00" data-email-preview="May 9, 2018 12:00 AM (Europe: Paris)">May 9, 2018 12:00 AM (Europe: Paris)</span></p>
     HTML
 
     expect(PrettyText.format_for_email(cooked)).to match_html(cooked_mail)
