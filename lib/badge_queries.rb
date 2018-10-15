ae7d80dd8613f87da98b484efec3dd98777d2daf
Bianca Nenciu <nbianca@users.noreply.github.com>
2018-10-16 02:38:59 +0300

    FIX: Do not award badges for links in restricted categories. (#6492)

--- lib/badge_queries.rb
+++ lib/badge_queries.rb
@@ -189,8 +189,7 @@ SQL
     <<-SQL
         SELECT tl.user_id, post_id, current_timestamp granted_at
           FROM topic_links tl
-          JOIN posts p  ON p.id = post_id    AND p.deleted_at IS NULL
-          JOIN topics t ON t.id = p.topic_id AND t.deleted_at IS NULL AND t.archetype <> 'private_message'
+          JOIN badge_posts p ON p.id = post_id
          WHERE NOT tl.internal
            AND tl.clicks >= #{count}
       GROUP BY tl.user_id, tl.post_id
