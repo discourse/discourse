6a3767cde738863dff6d436d019ae06be93438d3
Bianca Nenciu <nbianca@users.noreply.github.com>
2018-10-25 12:45:31 +0300

    FEATURE: Warn users via email about suspicious logins. (#6520)
    * FEATURE: Warn users via email about suspicious logins.
    
    * DEV: Move suspicious login check to a job.

--- app/models/user_auth_token.rb
+++ app/models/user_auth_token.rb
@@ -30,6 +30,35 @@ class UserAuthToken < ActiveRecord::Base
     end
   end
 
+  # Returns the login location as it will be used by the the system to detect
+  # suspicious login.
+  #
+  # This should not be very specific because small variations in location
+  # (i.e. changes of network, small trips, etc) will be detected as suspicious
+  # logins.
+  #
+  # On the other hand, if this is too broad it will not report any suspicious
+  # logins at all.
+  #
+  # For example, let's choose the country as the only component in login
+  # locations. In general, this should be a pretty good choce with the
+  # exception that for users from huge countries it might not be specific
+  # enoguh. For US users where the real user and the malicious one could
+  # happen to live both in USA, this will not detect any suspicious activity.
+  def self.login_location(ip)
+    DiscourseIpInfo.get(ip)[:country]
+  end
+
+  def self.is_suspicious(user_id, user_ip)
+    ips = UserAuthTokenLog.where(user_id: user_id).pluck(:client_ip)
+    ips.delete_at(ips.index(user_ip) || ips.length) # delete one occurance (current)
+    ips.uniq!
+    return false if ips.empty? # first login is never suspicious
+
+    user_location = login_location(user_ip)
+    ips.none? { |ip| user_location == login_location(ip) }
+  end
+
   def self.generate!(info)
     token = SecureRandom.hex(16)
     hashed_token = hash_token(token)
@@ -50,6 +79,11 @@ class UserAuthToken < ActiveRecord::Base
         client_ip: info[:client_ip],
         path: info[:path],
         auth_token: hashed_token)
+
+    Jobs.enqueue(:suspicious_login,
+                 user_id: info[:user_id],
+                 client_ip: info[:client_ip],
+                 user_agent: info[:user_agent])
 
     user_auth_token
   end
