ed9c21e42c3e34955013fe34ba8443841bafbedd
Penar Musaraj <pmusaraj@gmail.com>
2018-10-25 20:34:39 -0400

    FEATURE: hide muted categories from /categories list (#6531)

--- spec/models/category_list_spec.rb
+++ spec/models/category_list_spec.rb
@@ -47,6 +47,16 @@ describe CategoryList do
       expect(CategoryList.new(Guardian.new(nil), include_topics: true).categories.find { |x| x.name == private_cat.name }).to eq(nil)
     end
 
+    it "properly hide muted categories" do
+      cat_muted = Fabricate(:category)
+      CategoryUser.create!(user_id: user.id,
+                           category_id: cat_muted.id,
+                           notification_level: CategoryUser.notification_levels[:muted])
+
+      # uncategorized + cat_muted for admin
+      expect(CategoryList.new(Guardian.new admin).categories.count).to eq(2)
+      expect(CategoryList.new(Guardian.new user).categories.count).to eq(1)
+    end
   end
 
   context "with a category" do
