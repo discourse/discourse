2539b283a0e37fa5085cdaa16f7929bfba23bd3d
Bianca Nenciu <nbianca@users.noreply.github.com>
2018-10-31 00:51:58 +0200

    FEATURE: Suspicious logins report. (#6544)

--- app/jobs/regular/suspicious_login.rb
+++ app/jobs/regular/suspicious_login.rb
@@ -4,6 +4,12 @@ module Jobs
 
     def execute(args)
       if UserAuthToken.is_suspicious(args[:user_id], args[:client_ip])
+
+        UserAuthToken.log(action: 'suspicious',
+                          user_id: args[:user_id],
+                          user_agent: args[:user_agent],
+                          client_ip: args[:client_ip])
+
         Jobs.enqueue(:critical_user_email,
                      type: :suspicious_login,
                      user_id: args[:user_id],
--- spec/jobs/suspicious_login_spec.rb
+++ spec/jobs/suspicious_login_spec.rb
@@ -13,6 +13,8 @@ describe Jobs::SuspiciousLogin do
   it "will not send an email on first login" do
     Jobs.expects(:enqueue).with(:critical_user_email, has_entries(type: :suspicious_login)).never
     described_class.new.execute(user_id: user.id, client_ip: "1.1.1.1")
+
+    expect(UserAuthTokenLog.where(action: "suspicious").count).to eq(0)
   end
 
   it "will not send an email when user log in from a known location" do
@@ -21,6 +23,8 @@ describe Jobs::SuspiciousLogin do
     Jobs.expects(:enqueue).with(:critical_user_email, has_entries(type: :suspicious_login)).never
     described_class.new.execute(user_id: user.id, client_ip: "1.1.1.1")
     described_class.new.execute(user_id: user.id, client_ip: "1.1.1.2")
+
+    expect(UserAuthTokenLog.where(action: "suspicious").count).to eq(0)
   end
 
   it "will send an email when user logs in from a new location" do
@@ -28,6 +32,8 @@ describe Jobs::SuspiciousLogin do
 
     Jobs.expects(:enqueue).with(:critical_user_email, has_entries(type: :suspicious_login))
     described_class.new.execute(user_id: user.id, client_ip: "1.1.2.1")
+
+    expect(UserAuthTokenLog.where(action: "suspicious").count).to eq(1)
   end
 
 end
