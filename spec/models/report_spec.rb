e0ccd36dbe5d172f51b268893229a5be0051bc4a
Bianca Nenciu <nbianca@users.noreply.github.com>
2018-10-31 00:51:58 +0200

    FEATURE: Suspicious logins report. (#6544)

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -973,4 +973,25 @@ describe Report do
       end
     end
   end
+
+  describe "report_suspicious_logins" do
+    let(:joffrey) { Fabricate(:user, username: "joffrey") }
+    let(:robin) { Fabricate(:user, username: "robin") }
+
+    context "with data" do
+      it "works" do
+        UserAuthToken.log(action: "suspicious", user_id: robin.id)
+        UserAuthToken.log(action: "suspicious", user_id: joffrey.id)
+        UserAuthToken.log(action: "suspicious", user_id: joffrey.id)
+
+        report = Report.find("suspicious_logins")
+
+        expect(report.data.length).to eq(3)
+        expect(report.data[0][:username]).to eq("robin")
+        expect(report.data[1][:username]).to eq("joffrey")
+        expect(report.data[2][:username]).to eq("joffrey")
+      end
+    end
+
+  end
 end
