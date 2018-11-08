57f92ac808a50dcc5b625da50a41628201f7eed0
Guo Xiang Tan <tgx_world@hotmail.com>
2018-11-08 14:20:09 +0800

    Revert "Swtich to regexp for `DbHelper.remap`."
    Regexp is so much slower.
    
    This reverts commit c3f89e3cd73ef4a4d42972bf19a53576d2228399.

--- lib/db_helper.rb
+++ lib/db_helper.rb
@@ -8,8 +8,10 @@ class DbHelper
        AND (data_type LIKE 'char%' OR data_type LIKE 'text%')
   ORDER BY table_name, column_name"
 
-  def self.remap(from, to, exclude_tables: [])
+  def self.remap(from, to, anchor_left: false, anchor_right: false, exclude_tables: [])
     results = DB.query(REMAP_SQL).to_a
+    like = "#{anchor_left ? '' : "%"}#{from}#{anchor_right ? '' : "%"}"
+
     remappable_columns = {}
 
     results.each do |result|
@@ -22,14 +24,14 @@ class DbHelper
     remappable_columns.each do |table_name, column_names|
       next if exclude_tables.include?(table_name)
       set_clause = column_names.map do |column_name|
-        "#{column_name} = REGEXP_REPLACE(#{column_name}, :from, :to)"
+        "#{column_name} = REPLACE(#{column_name}, :from, :to)"
       end.join(", ")
 
       where_clause = column_names.map do |column_name|
-        "#{column_name} ~* :from"
+        "#{column_name} LIKE :like"
       end.join(" OR ")
 
-      DB.exec(<<~SQL, from: from, to: to, from: from)
+      DB.exec(<<~SQL, from: from, to: to, like: like)
         UPDATE #{table_name}
         SET #{set_clause}
         WHERE #{where_clause}
