8fb89fc1dd71da6cff1c86ac9563c711aa9b0dab
Guo Xiang Tan <tgx_world@hotmail.com>
2018-11-12 11:30:38 +0800

    Correct post migration.

--- db/post_migrate/20181112013117_migrate_url_site_settings.rb
+++ db/post_migrate/20181112013117_migrate_url_site_settings.rb
@@ -1,24 +1,24 @@
 class MigrateUrlSiteSettings < ActiveRecord::Migration[5.2]
   def up
     [
-      'logo_url',
-      'logo_small_url',
-      'digest_logo_url',
-      'mobile_logo_url',
-      'large_icon_url',
-      'favicon_url',
-      'apple_touch_icon_url',
-      'default_opengraph_image_url',
-      'twitter_summary_large_image_url'
-    ].each do |url_site_setting|
-      old_logo_url = DB.query_single(
-        "SELECT value FROM site_settings WHERE name = '#{url_site_setting}'"
+      ['logo_url', 'logo'],
+      ['logo_small_url', 'logo_small'],
+      ['digest_logo_url', 'digest_logo'],
+      ['mobile_logo_url', 'mobile_logo'],
+      ['large_icon_url', 'large_icon'],
+      ['favicon_url', 'favicon'],
+      ['apple_touch_icon_url', 'apple_touch_icon'],
+      ['default_opengraph_image_url', 'opengraph_image'],
+      ['twitter_summary_large_image_url', 'twitter_summary_large_image']
+    ].each do |old_setting, new_setting|
+      old_url = DB.query_single(
+        "SELECT value FROM site_settings WHERE name = '#{old_setting}'"
       ).first
 
-      next if old_logo_url.blank?
+      next if old_url.blank?
 
       file = FileHelper.download(
-        UrlHelper.absolute(old_logo_url),
+        UrlHelper.absolute(old_url),
         max_file_size: 20.megabytes,
         tmp_file_name: 'tmp_site_setting_logo',
         skip_rate_limit: true,
@@ -30,12 +30,12 @@ class MigrateUrlSiteSettings < ActiveRecord::Migration[5.2]
       upload = UploadCreator.new(
         file,
         'site_setting_logo',
-        origin: UrlHelper.absolute(old_logo_url)
+        origin: UrlHelper.absolute(old_url)
       ).create_for(Discourse.system_user.id)
 
       execute <<~SQL
       INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
-      VALUES ('logo', 18, #{upload.id}, now(), now())
+      VALUES ('#{new_setting}', 18, #{upload.id}, now(), now())
       SQL
     end
   end
