0e73bed72e96f1049460a27466a3fe4603eef354
Kyle Zhao <kzhao.sw@gmail.com>
2018-11-15 09:46:02 -0500

    Tighten `script-src` by dropping 'self'

--- lib/content_security_policy.rb
+++ lib/content_security_policy.rb
@@ -15,7 +15,7 @@ class ContentSecurityPolicy
 
       return response unless html_response?(headers) && ContentSecurityPolicy.enabled?
 
-      policy = ContentSecurityPolicy.new.build
+      policy = ContentSecurityPolicy.new(request).build
       headers['Content-Security-Policy'] = policy if SiteSetting.content_security_policy
       headers['Content-Security-Policy-Report-Only'] = policy if SiteSetting.content_security_policy_report_only
 
@@ -33,7 +33,8 @@ class ContentSecurityPolicy
     SiteSetting.content_security_policy || SiteSetting.content_security_policy_report_only
   end
 
-  def initialize
+  def initialize(request = nil)
+    @request = request
     @directives = {
       script_src: script_src,
     }
@@ -57,14 +58,48 @@ class ContentSecurityPolicy
 
   private
 
-  def script_src
-    sources = [:self, :unsafe_eval]
+  attr_reader :request
 
-    sources << :https if SiteSetting.force_https
-    sources << Discourse.asset_host if Discourse.asset_host.present?
-    sources << 'www.google-analytics.com' if SiteSetting.ga_universal_tracking_code.present?
-    sources << 'www.googletagmanager.com' if SiteSetting.gtm_container_id.present?
+  SCRIPT_ASSET_DIRECTORIES = %w(
+    /assets/
+    /brotli_asset/
+    /extra-locales/
+    /highlight-js/
+    /javascripts/
+    /theme-javascripts/
+  )
+
+  def script_assets(base = base_url)
+    SCRIPT_ASSET_DIRECTORIES.map { |dir| base + dir }
+  end
+
+  def script_src
+    sources = [
+      :unsafe_eval,
+      "#{base_url}/logs/",
+      "#{base_url}/sidekiq/",
+      "#{base_url}/mini-profiler-resources/",
+    ]
+
+    sources.concat(script_assets)
+    sources.concat(script_assets(GlobalSetting.cdn_url)) if GlobalSetting.cdn_url
+    sources.concat(script_assets(GlobalSetting.s3_cdn_url)) if GlobalSetting.s3_cdn_url
+
+    sources << 'https://www.google-analytics.com' if SiteSetting.ga_universal_tracking_code.present?
+    sources << 'https://www.googletagmanager.com' if SiteSetting.gtm_container_id.present?
 
     sources.concat(SiteSetting.content_security_policy_script_src.split('|'))
+  end
+
+  def protocal
+    @protocal ||= SiteSetting.force_https ? 'https://' : 'http://'
+  end
+
+  def with_protocal(url)
+    protocal + url
+  end
+
+  def base_url
+    @base_url ||= Rails.env.development? ? with_protocal(request.host_with_port) : Discourse.base_url
   end
 end
