9e86b425bcca08a2f0d096d0bdb9b9e17a46e316
Guo Xiang Tan <tgx_world@hotmail.com>
2018-11-16 09:19:58 +0800

    FIX: Job to clean up old URL settings when new setting has been set.
    Related to https://github.com/discourse/discourse/commit/44391ee8ab7431ca572b6c340d6e770cdadd51b3

--- app/jobs/onceoff/migrate_url_site_settings.rb
+++ app/jobs/onceoff/migrate_url_site_settings.rb
@@ -1,18 +1,20 @@
 module Jobs
   class MigrateUrlSiteSettings < Jobs::Onceoff
+    SETTINGS = [
+      ['logo_url', 'logo'],
+      ['logo_small_url', 'logo_small'],
+      ['digest_logo_url', 'digest_logo'],
+      ['mobile_logo_url', 'mobile_logo'],
+      ['large_icon_url', 'large_icon'],
+      ['favicon_url', 'favicon'],
+      ['apple_touch_icon_url', 'apple_touch_icon'],
+      ['default_opengraph_image_url', 'opengraph_image'],
+      ['twitter_summary_large_image_url', 'twitter_summary_large_image'],
+      ['push_notifications_icon_url', 'push_notifications_icon'],
+    ]
+
     def execute_onceoff(args)
-      [
-        ['logo_url', 'logo'],
-        ['logo_small_url', 'logo_small'],
-        ['digest_logo_url', 'digest_logo'],
-        ['mobile_logo_url', 'mobile_logo'],
-        ['large_icon_url', 'large_icon'],
-        ['favicon_url', 'favicon'],
-        ['apple_touch_icon_url', 'apple_touch_icon'],
-        ['default_opengraph_image_url', 'opengraph_image'],
-        ['twitter_summary_large_image_url', 'twitter_summary_large_image'],
-        ['push_notifications_icon_url', 'push_notifications_icon'],
-      ].each do |old_setting, new_setting|
+      SETTINGS.each do |old_setting, new_setting|
         old_url = DB.query_single(
           "SELECT value FROM site_settings WHERE name = '#{old_setting}'"
         ).first
