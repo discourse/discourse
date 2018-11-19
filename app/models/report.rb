cbc4e65fddb511abd42f369b5a710b03d1864590
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-11-19 11:25:11 +0100

    FIX: adds support for missing reports from old dashboardfix linter

--- app/models/report.rb
+++ app/models/report.rb
@@ -673,8 +673,43 @@ class Report
       limit: report.limit || 8,
       category_id: report.category_id
     }
-    result = nil
+
     result = IncomingLinksReport.find(:top_traffic_sources, options)
+    report.data = result.data
+  end
+
+  def self.report_top_referrers(report)
+    report.modes = [:table]
+
+    report.labels = [
+      {
+        type: :user,
+        properties: {
+          username: :username,
+          id: :user_id,
+          avatar: :user_avatar_template,
+        },
+        title: I18n.t("reports.top_referrers.labels.user")
+      },
+      {
+        property: :num_clicks,
+        type: :number,
+        title: I18n.t("reports.top_referrers.labels.num_clicks")
+      },
+      {
+        property: :num_topics,
+        type: :number,
+        title: I18n.t("reports.top_referrers.labels.num_topics")
+      }
+    ]
+
+    options = {
+      end_date: report.end_date,
+      start_date: report.start_date,
+      limit: report.limit || 8
+    }
+
+    result = IncomingLinksReport.find(:top_referrers, options)
     report.data = result.data
   end
 
