939c43f9715e89b4642844088bff920209f19907
Rishabh <rishabh.nambiar@discourse.org>
2018-11-20 13:42:32 +0530

    FIX: Skip gsub for normalizing whitespaces when text is nil (#6631)

--- lib/text_cleaner.rb
+++ lib/text_cleaner.rb
@@ -57,7 +57,7 @@ class TextCleaner
   @@whitespaces_regexp = Regexp.new("(\u00A0|\u1680|\u180E|[\u2000-\u200A]|\u2028|\u2029|\u202F|\u205F|\u3000)", "u").freeze
 
   def self.normalize_whitespaces(text)
-    text.gsub(@@whitespaces_regexp, ' ')
+    text&.gsub(@@whitespaces_regexp, ' ')
   end
 
 end
