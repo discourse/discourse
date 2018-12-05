dbaf2b9fc813d950b199abf0896bba8daa8d88d2
Sam <sam.saffron@gmail.com>
2018-12-05 17:03:37 +1100

    DEV: only publish logs error count to admins

--- config/initializers/100-logster.rb
+++ config/initializers/100-logster.rb
@@ -94,7 +94,14 @@ RailsMultisite::ConnectionManagement.each_connection do
 
   if (error_rate_per_minute || 0) > 0
     store.register_rate_limit_per_minute(severities, error_rate_per_minute) do |rate|
-      MessageBus.publish("/logs_error_rate_exceeded", rate: rate, duration: 'minute', publish_at: Time.current.to_i)
+      MessageBus.publish("/logs_error_rate_exceeded",
+        {
+          rate: rate,
+          duration: 'minute',
+          publish_at: Time.current.to_i
+        },
+        group_ids: [Group::AUTO_GROUPS[:admins]]
+      )
     end
   end
 
@@ -102,7 +109,14 @@ RailsMultisite::ConnectionManagement.each_connection do
 
   if (error_rate_per_hour || 0) > 0
     store.register_rate_limit_per_hour(severities, error_rate_per_hour) do |rate|
-      MessageBus.publish("/logs_error_rate_exceeded", rate: rate, duration: 'hour', publish_at: Time.current.to_i)
+      MessageBus.publish("/logs_error_rate_exceeded",
+        {
+          rate: rate,
+          duration: 'hour',
+          publish_at: Time.current.to_i,
+        },
+        group_ids: [Group::AUTO_GROUPS[:admins]]
+      )
     end
   end
 end
