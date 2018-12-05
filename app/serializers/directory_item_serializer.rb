00c582f39e723acf26405682790f5002771882a7
Maja Komel <maja.komel@gmail.com>
2018-12-05 19:06:06 +0100

    FEATURE: show avatar flair on group, badges and directory pages

--- app/serializers/directory_item_serializer.rb
+++ app/serializers/directory_item_serializer.rb
@@ -1,9 +1,33 @@
 class DirectoryItemSerializer < ApplicationSerializer
 
+  class UserSerializer < UserNameSerializer
+    attributes :primary_group_name,
+               :primary_group_flair_url,
+               :primary_group_flair_bg_color,
+               :primary_group_flair_color
+
+    def primary_group_name
+      return nil unless object&.primary_group_id
+      object&.primary_group&.name
+    end
+
+    def primary_group_flair_url
+      object&.primary_group&.flair_url
+    end
+
+    def primary_group_flair_bg_color
+      object&.primary_group&.flair_bg_color
+    end
+
+    def primary_group_flair_color
+      object&.primary_group&.flair_color
+    end
+  end
+
   attributes :id,
              :time_read
 
-  has_one :user, embed: :objects, serializer: UserNameSerializer
+  has_one :user, embed: :objects, serializer: UserSerializer
   attributes *DirectoryItem.headings
 
   def id
--- app/serializers/group_user_serializer.rb
+++ app/serializers/group_user_serializer.rb
@@ -1,7 +1,32 @@
 class GroupUserSerializer < BasicUserSerializer
-  attributes :name, :title, :last_posted_at, :last_seen_at, :added_at
+  attributes :name,
+             :title,
+             :last_posted_at,
+             :last_seen_at,
+             :added_at,
+             :primary_group_name,
+             :primary_group_flair_url,
+             :primary_group_flair_bg_color,
+             :primary_group_flair_color
 
   def include_added_at
     object.respond_to? :added_at
+  end
+
+  def primary_group_name
+    return nil unless object&.primary_group_id
+    object&.primary_group&.name
+  end
+
+  def primary_group_flair_url
+    object&.primary_group&.flair_url
+  end
+
+  def primary_group_flair_bg_color
+    object&.primary_group&.flair_bg_color
+  end
+
+  def primary_group_flair_color
+    object&.primary_group&.flair_color
   end
 end
