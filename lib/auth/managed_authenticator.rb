5adde82b37a8e33d59b161471a1a9dfc52e50016
David Taylor <david@taylorhq.com>
2018-12-07 14:04:21 +0000

    DEV: Add profile fetching support to `ManagedAuthenticator`

--- lib/auth/managed_authenticator.rb
+++ lib/auth/managed_authenticator.rb
@@ -73,6 +73,7 @@ class Auth::ManagedAuthenticator < Auth::Authenticator
         extra: auth_token[:extra] || {}
       )
       retrieve_avatar(result.user, auth_token.dig(:info, :image))
+      retrieve_profile(result.user, auth_token[:info])
     end
 
     result.email_valid = true if result.email
@@ -95,11 +96,26 @@ class Auth::ManagedAuthenticator < Auth::Authenticator
     data = auth[:extra_data]
     create_association!(data.merge(user: user))
     retrieve_avatar(user, data.dig(:info, :image))
+    retrieve_profile(user, data[:info])
   end
 
   def retrieve_avatar(user, url)
     return unless user && url
     return if user.user_avatar.try(:custom_upload_id).present?
     Jobs.enqueue(:download_avatar_from_url, url: url, user_id: user.id, override_gravatar: false)
+  end
+
+  def retrieve_profile(user, info)
+    return unless user
+
+    bio = info[:description]
+    location = info[:location]
+
+    if bio || location
+      profile = user.user_profile
+      profile.bio_raw  = bio      unless profile.bio_raw.present?
+      profile.location = location unless profile.location.present?
+      profile.save
+    end
   end
 end
