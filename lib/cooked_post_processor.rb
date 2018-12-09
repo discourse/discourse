96539c298f88a2e03de148bbeaaf5783a4458582
Bianca Nenciu <nenciu.bianca@gmail.com>
2018-12-09 14:22:27 +0200

    FEATURE: Adds site setting to let quotes on direct replies.

--- lib/cooked_post_processor.rb
+++ lib/cooked_post_processor.rb
@@ -32,7 +32,7 @@ class CookedPostProcessor
   def post_process(bypass_bump = false)
     DistributedMutex.synchronize("post_process_#{@post.id}") do
       DiscourseEvent.trigger(:before_post_process_cooked, @doc, @post)
-      removed_direct_reply_full_quotes
+      removed_direct_reply_full_quotes if SiteSetting.remove_full_quote
       post_process_oneboxes
       post_process_images
       post_process_quotes
