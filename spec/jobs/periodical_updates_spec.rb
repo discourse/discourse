6142c5c54a3976cc74af98a10ca034d3c07e3212
Rishabh <rishabh.nambiar@discourse.org>
2018-12-09 22:06:04 +0530

    Add test to check if posts are auto rebaked by PeriodicalUpdates job when gravatar download is disabled follow-up for 8e307e633eb7e3ef2b928fa45aff5245c40c870e

--- spec/jobs/periodical_updates_spec.rb
+++ spec/jobs/periodical_updates_spec.rb
@@ -9,4 +9,18 @@ describe Jobs::PeriodicalUpdates do
     Jobs::PeriodicalUpdates.new.execute(nil)
   end
 
+  it "can rebake old posts when automatically_download_gravatars is false" do
+    SiteSetting.automatically_download_gravatars = false
+    post = create_post
+    post.update_columns(baked_at: Time.new(2000, 1, 1), baked_version: -1)
+    described_class.new.execute({})
+
+    post.reload
+    expect(post.baked_at).to be > 1.day.ago
+
+    baked = post.baked_at
+    described_class.new.execute({})
+    post.reload
+    expect(post.baked_at).to eq(baked)
+  end
 end
