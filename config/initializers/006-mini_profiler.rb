a34bc92e1a9c83a49a2fa8496951e0b59bcd2a31
Sam <sam.saffron@gmail.com>
2018-12-10 14:29:20 +1100

    DEV: update mini profiler
    This provides us with instrumentation missing after rails upgrade
    
    Latest version of rails uses exec_params internally which is no longer
    routed to intercepted methods in mini profiler 1.0.0

--- config/initializers/006-mini_profiler.rb
+++ config/initializers/006-mini_profiler.rb
@@ -13,8 +13,7 @@ if Rails.configuration.respond_to?(:load_mini_profiler) && Rails.configuration.l
   Rack::MiniProfilerRails.initialize!(Rails.application)
 end
 
-if defined?(Rack::MiniProfiler)
-
+if defined?(Rack::MiniProfiler) && defined?(Rack::MiniProfiler::Config)
   # note, we may want to add some extra security here that disables mini profiler in a multi hosted env unless user global admin
   #   raw_connection means results are not namespaced
   #
