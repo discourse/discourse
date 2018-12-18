a313b0114849d0000d5e408b2090ffd03192b4d9
Vinoth Kannan <vinothkannan@vinkas.com>
2018-12-18 20:06:59 +0530

    DEV: raise error if search term length is less than required

--- spec/requests/search_controller_spec.rb
+++ spec/requests/search_controller_spec.rb
@@ -127,6 +127,11 @@ describe SearchController do
   end
 
   context "#show" do
+    it "raises an error when the search term length is less than required" do
+      get "/search.json", params: { q: 'ba' }
+      expect(response.status).to eq(400)
+    end
+
     it "logs the search term" do
       SiteSetting.log_search_queries = true
       get "/search.json", params: { q: 'bantha' }
