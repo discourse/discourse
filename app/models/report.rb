0402f0f35714dfb25c19050611a9bf7555d0c2bc
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-12-26 10:29:07 +0100

    UX: new site setting to define activity metrics displayed on dashboard

--- app/models/report.rb
+++ app/models/report.rb
@@ -246,6 +246,8 @@ class Report
   def self.report_signups(report)
     report.group_filtering = true
 
+    report.icon = 'user-plus'
+
     if report.group_id
       basic_report_about report, User.real, :count_by_signup_date, report.start_date, report.end_date, report.group_id
       add_counts report, User.real, 'users.created_at'
