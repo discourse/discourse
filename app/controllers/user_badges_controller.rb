0fca3205b5c735c55e3f2cdf0ea3f9aa95dce358
Saurabh Patel <saurabh.finch@gmail.com>
2018-12-31 15:16:39 +0530

    FIX: show error msg on grant badge if message available from backend (#6801)

--- app/controllers/user_badges_controller.rb
+++ app/controllers/user_badges_controller.rb
@@ -58,7 +58,7 @@ class UserBadgesController < ApplicationController
 
     if params[:reason].present?
       unless is_badge_reason_valid? params[:reason]
-        return render json: { failed: I18n.t('invalid_grant_badge_reason_link') }, status: 400
+        return render json: failed_json.merge(message: I18n.t('invalid_grant_badge_reason_link')), status: 400
       end
 
       path = begin
