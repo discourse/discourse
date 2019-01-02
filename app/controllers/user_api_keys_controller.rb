35a46092b94fc704fbf5d4937c1a5b8dee2f20c0
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2019-01-02 12:11:02 +0100

    FEATURE: Show instructions when creating user-api-key w/out redirect
    This adds a view to show instructions when requesting a user-api-key
    without a redirect. It adds a erb template and json format.
    Also adds a i18n user_api_key.instructions for server.en.yml

--- app/controllers/user_api_keys_controller.rb
+++ app/controllers/user_api_keys_controller.rb
@@ -61,12 +61,13 @@ class UserApiKeysController < ApplicationController
     raise Discourse::InvalidAccess unless meets_tl?
 
     validate_params
+    @application_name = params[:application_name]
 
     # destroy any old keys we had
     UserApiKey.where(user_id: current_user.id, client_id: params[:client_id]).destroy_all
 
     key = UserApiKey.create!(
-      application_name: params[:application_name],
+      application_name: @application_name,
       client_id: params[:client_id],
       user_id: current_user.id,
       push_url: params[:push_url],
@@ -76,7 +77,7 @@ class UserApiKeysController < ApplicationController
 
     # we keep the payload short so it encrypts easily with public key
     # it is often restricted to 128 chars
-    payload = {
+    @payload = {
       key: key.key,
       nonce: params[:nonce],
       push: key.has_push?,
@@ -84,12 +85,15 @@ class UserApiKeysController < ApplicationController
     }.to_json
 
     public_key = OpenSSL::PKey::RSA.new(params[:public_key])
-    payload = Base64.encode64(public_key.public_encrypt(payload))
+    @payload = Base64.encode64(public_key.public_encrypt(@payload))
 
-    if params[:auth_redirect]
-      redirect_to "#{params[:auth_redirect]}?payload=#{CGI.escape(payload)}"
-    else
-      render plain: payload
+    (redirect_to("#{params[:auth_redirect]}?payload=#{CGI.escape(@payload)}") && return) if params[:auth_redirect]
+    respond_to do |format|
+      format.html { render :show }
+      format.json do
+        instructions = I18n.t("user_api_key.instructions", application_name: @application_name)
+        render json: { payload: @payload, instructions: instructions }
+      end
     end
   end
 
