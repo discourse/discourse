bea7a8a4d1ab28a52540551f8c7fa909be7bc8a0
Arpit Jalan <arpit@techapj.com>
2019-01-03 07:46:05 +0530

    FIX: show accurate error message based on invite token validity

--- spec/requests/invites_controller_spec.rb
+++ spec/requests/invites_controller_spec.rb
@@ -12,7 +12,7 @@ describe InvitesController do
 
       body = response.body
       expect(body).to_not have_tag(:script, with: { src: '/assets/application.js' })
-      expect(CGI.unescapeHTML(body)).to include(I18n.t('invite.not_found_template', site_name: SiteSetting.title, base_url: Discourse.base_url))
+      expect(CGI.unescapeHTML(body)).to include(I18n.t('invite.not_found', site_name: SiteSetting.title, base_url: Discourse.base_url))
     end
 
     it "renders the accept invite page if invite exists" do
