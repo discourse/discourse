c8d406c844cc9498a54a1664c3db057012127bd4
Vinoth Kannan <vinothkannan@vinkas.com>
2019-01-04 10:59:44 +0530

    Remove the down method in db migration

--- db/migrate/20181218071253_add_etag_to_uploads.rb
+++ db/migrate/20181218071253_add_etag_to_uploads.rb
@@ -6,12 +6,4 @@ class AddEtagToUploads < ActiveRecord::Migration[5.2]
     add_column :optimized_images, :etag, :string
     add_index :optimized_images, [:etag]
   end
-
-  def down
-    remove_column :uploads, :etag
-    remove_index :uploads, [:etag]
-
-    remove_column :optimized_images, :etag
-    remove_index :optimized_images, [:etag]
-  end
 end
