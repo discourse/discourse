51b19e945c819458e197f30c1c26677311e9b0d3
Guo Xiang Tan <tgx_world@hotmail.com>
2019-01-16 10:25:42 +0800

    DEV: Stablize the multisite tests.
    SiteSettingExtension triggers message bus which re-establishes a
    DB connection in `SiteSettingExtension#process_message`. That happens
    concurrently and a test that requires a connection to the db will
    fail when the reconnection is happening.

---
+++ spec/support/site_settings_helpers.rb
@@ -0,0 +1,11 @@
+module SiteSettingsHelpers
+  def new_settings(provider)
+    Class.new do
+      extend SiteSettingExtension
+      # we want to avoid leaking a big pile of MessageBus subscriptions here (1 per class)
+      # so we set listen_for_changes to false
+      self.listen_for_changes = false
+      self.provider = provider
+    end
+  end
+end
