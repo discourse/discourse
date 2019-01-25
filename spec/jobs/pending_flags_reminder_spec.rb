78ddc82952d8766f516100abd09580f79a5b9c59
Robin Ward <robin.ward@gmail.com>
2019-01-25 11:25:30 -0500

    FIX: Respect min_flags_staff_visibility for new flags too
    There was a situation where if:
    
    * There were new flags to review that met the visibility threshold
    
    AND
    
    * There were old flags that *didn't* meet the threshold
    
    THEN
    
    a pending flags notification would be sent out. This fixes that case.
    Staff should not be notified of flags if they do not meet the threshold
    and are old.

--- spec/jobs/pending_flags_reminder_spec.rb
+++ spec/jobs/pending_flags_reminder_spec.rb
@@ -37,26 +37,37 @@ describe Jobs::PendingFlagsReminder do
       expect(job.last_notified_id).to eq(old_flag.id)
     end
 
-    it "doesn't send a message when min_flags_staff_visibility is not met" do
-      SiteSetting.min_flags_staff_visibility = 2
-      Fabricate(:flag, created_at: 49.hours.ago)
-      Fabricate(:flag, created_at: 51.hours.ago)
-      PostCreator.expects(:create).never
-      described_class.new.execute({})
-    end
-
-    it "sends a message when min_flags_staff_visibility is met" do
-      SiteSetting.min_flags_staff_visibility = 2
-      f = Fabricate(:flag, created_at: 49.hours.ago)
-      Fabricate(:flag, post: f.post, created_at: 51.hours.ago)
-      PostCreator.expects(:create).once.returns(true)
-      described_class.new.execute({})
-    end
-
     it "sends message when there is a flag older than 48 hours" do
       Fabricate(:flag, created_at: 49.hours.ago)
       PostCreator.expects(:create).once.returns(true)
       described_class.new.execute({})
+    end
+
+    context "min_flags_staff_visibility" do
+      it "doesn't send a message when min_flags_staff_visibility is not met" do
+        SiteSetting.min_flags_staff_visibility = 2
+        Fabricate(:flag, created_at: 49.hours.ago)
+        Fabricate(:flag, created_at: 51.hours.ago)
+        PostCreator.expects(:create).never
+        described_class.new.execute({})
+      end
+
+      it "doesn't send a message when min_flags_staff_visibility is met on new flags but not old" do
+        SiteSetting.min_flags_staff_visibility = 2
+        flag = Fabricate(:flag, created_at: 24.hours.ago)
+        Fabricate(:flag, post: flag.post, created_at: 49.hours.ago)
+        Fabricate(:flag, created_at: 51.hours.ago)
+        PostCreator.expects(:create).never
+        described_class.new.execute({})
+      end
+
+      it "sends a message when min_flags_staff_visibility is met" do
+        SiteSetting.min_flags_staff_visibility = 2
+        f = Fabricate(:flag, created_at: 49.hours.ago)
+        Fabricate(:flag, post: f.post, created_at: 51.hours.ago)
+        PostCreator.expects(:create).once.returns(true)
+        described_class.new.execute({})
+      end
     end
 
   end
