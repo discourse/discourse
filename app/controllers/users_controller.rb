e7821a63e71edd1524d90343066255d478c4c24a
Vinoth Kannan <vinothkannan@vinkas.com>
2019-02-05 23:31:19 +0530

    FIX: Users should able check the emails for self

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -148,9 +148,11 @@ class UsersController < ApplicationController
 
   def check_emails
     user = fetch_user_from_params(include_inactive: true)
-    guardian.ensure_can_check_emails!(user)
 
-    StaffActionLogger.new(current_user).log_check_email(user, context: params[:context])
+    unless user == current_user
+      guardian.ensure_can_check_emails!(user)
+      StaffActionLogger.new(current_user).log_check_email(user, context: params[:context])
+    end
 
     email, *secondary_emails = user.emails
 
