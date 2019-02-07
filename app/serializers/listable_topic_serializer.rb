cbedb6a1acd5856f4eaf69329ecbb23edfc526fa
Gerhard Schlager <mail@gerhard-schlager.at>
2019-02-07 17:50:10 +0100

    FEATURE: Hidden site setting to always include topic excerpt

--- app/serializers/listable_topic_serializer.rb
+++ app/serializers/listable_topic_serializer.rb
@@ -108,7 +108,7 @@ class ListableTopicSerializer < BasicTopicSerializer
   alias :include_new_posts? :has_user_data
 
   def include_excerpt?
-    pinned
+    pinned || SiteSetting.always_include_topic_excerpts
   end
 
   def pinned
