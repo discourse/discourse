d96d561180c1271f63c32900e3669808997a7610
Gerhard Schlager <mail@gerhard-schlager.at>
2019-02-08 16:01:46 +0100

    Fix defect db migration
    Follow-up to fc999c04b34edb93cb3538718bcd22d295509a4a

--- db/post_migrate/20190205104116_drop_unused_auth_tables.rb
+++ db/post_migrate/20190205104116_drop_unused_auth_tables.rb
@@ -2,17 +2,6 @@ require 'migration/table_dropper'
 
 class DropUnusedAuthTables < ActiveRecord::Migration[5.2]
   def change
-    def up
-      %i{
-        facebook_user_infos
-        twitter_user_infos
-      }.each do |table|
-        Migration::TableDropper.execute_drop(table)
-      end
-    end
-
-    def down
-      raise ActiveRecord::IrreversibleMigration
-    end
+    # this migration was defect
   end
 end
