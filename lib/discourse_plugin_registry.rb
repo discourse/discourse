5cef5b34ef9943161a6fa9c3197c5938da8b6003
Penar Musaraj <pmusaraj@gmail.com>
2019-02-12 13:57:52 -0500

    DEV: Yarn-manage moment and moment-timezone libraries

--- lib/discourse_plugin_registry.rb
+++ lib/discourse_plugin_registry.rb
@@ -204,8 +204,8 @@ class DiscoursePluginRegistry
   end
 
   VENDORED_CORE_PRETTY_TEXT_MAP = {
-    "moment.js" => "lib/javascripts/moment.js",
-    "moment-timezone.js" => "lib/javascripts/moment-timezone-with-data.js"
+    "moment.js" => "vendor/assets/javascripts/moment.js",
+    "moment-timezone.js" => "vendor/assets/javascripts/moment-timezone-with-data.js"
   }
   def self.core_asset_for_name(name)
     asset = VENDORED_CORE_PRETTY_TEXT_MAP[name]
