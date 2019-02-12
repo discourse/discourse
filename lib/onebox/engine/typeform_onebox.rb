f65e6cbc23e7a96f3c62e88ec294621520d44ef9
Roman Rizzi <rizziromanalejandro@gmail.com>
2019-02-12 20:42:41 -0300

    FEATURE: Enable Typeform support (#394)
    This reverts commit 5a293d67626ab2c099b8a3acfd75ce42d885cc20.
    
    * Append necessary param to typeform URL so it can be aware that it's being embedded and avoid focus stealing on re-render
    
    * Revert placeholder_html to display the iframe preview instead of an image
    
    * If there are other params, append the embed type at the end.

---
+++ lib/onebox/engine/typeform_onebox.rb
@@ -0,0 +1,41 @@
+module Onebox
+  module Engine
+    class TypeformOnebox
+      include Engine
+
+      matches_regexp(/^https?:\/\/[a-z0-9]+\.typeform\.com\/to\/[a-zA-Z0-9]+/)
+      always_https
+
+      def to_html
+        typeform_src = build_typeform_src
+
+        <<~HTML
+          <iframe src="#{typeform_src}"
+                  width="100%"
+                  height="600px"
+                  scrolling="no"
+                  frameborder="0">
+          </iframe>
+        HTML
+      end
+      alias placeholder_html to_html
+
+      private
+
+      def build_typeform_src
+        escaped_src = ::Onebox::Helpers.normalize_url_for_output(@url)
+        query_params = CGI::parse(URI::parse(escaped_src).query || '')
+
+        return escaped_src if query_params.has_key?('typeform-embed')
+
+        if query_params.empty?
+          escaped_src << '?' unless escaped_src.end_with?('?')
+        else
+          escaped_src << '&'
+        end
+
+        escaped_src << 'typeform-embed=embed-widget'
+      end
+    end
+  end
+end
