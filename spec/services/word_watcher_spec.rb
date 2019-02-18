91ab111a75c31f17262ca4e5756a2efee8cd78f8
Arpit Jalan <arpit@techapj.com>
2019-02-18 10:00:17 +0530

    fix the build ❤️

--- spec/services/word_watcher_spec.rb
+++ spec/services/word_watcher_spec.rb
@@ -51,8 +51,8 @@ describe WordWatcher do
       context "word boundary" do
         it "handles word boundary" do
           Fabricate(:watched_word, word: "love", action: WatchedWord.actions[:require_approval])
-          expect(WordWatcher.new("I Love, bananas.").word_matches_for_action?(:require_approval)[1]).to eq("love")
-          expect(WordWatcher.new("I LOVE; apples.").word_matches_for_action?(:require_approval)[1]).to eq("love")
+          expect(WordWatcher.new("I Love, bananas.").word_matches_for_action?(:require_approval)[1]).to eq("Love")
+          expect(WordWatcher.new("I LOVE; apples.").word_matches_for_action?(:require_approval)[1]).to eq("LOVE")
           expect(WordWatcher.new("love: is a thing.").word_matches_for_action?(:require_approval)[1]).to eq("love")
           expect(WordWatcher.new("I love. oranges").word_matches_for_action?(:require_approval)[1]).to eq("love")
           expect(WordWatcher.new("I :love. pineapples").word_matches_for_action?(:require_approval)[1]).to eq("love")
