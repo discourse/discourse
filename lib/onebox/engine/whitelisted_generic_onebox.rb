d703d062a5ede16aebcb6109d78b1389e32045e2
Arpit Jalan <arpit@techapj.com>
2019-03-11 23:06:23 +0530

    FIX: URL encode whitespace character for card player

--- lib/onebox/engine/whitelisted_generic_onebox.rb
+++ lib/onebox/engine/whitelisted_generic_onebox.rb
@@ -307,7 +307,7 @@ module Onebox
       end
 
       def card_html
-        escaped_url = ::Onebox::Helpers.normalize_url_for_output(data[:player])
+        escaped_url = ::Onebox::Helpers.normalize_url_for_output(data[:player].gsub(' ', '%20'))
 
         <<~RAW
         <iframe src="#{escaped_url}"
