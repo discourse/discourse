d8c3c8234522bf39c70a2cece8d822ae34b730ff
Roman Rizzi <rizziromanalejandro@gmail.com>
2019-03-13 16:39:39 -0300

    Version bumped to v2.2.3

--- lib/version.rb
+++ lib/version.rb
@@ -6,7 +6,7 @@ module Discourse
     module VERSION #:nodoc:
       MAJOR = 2
       MINOR = 2
-      TINY  = 2
+      TINY  = 3
       PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
