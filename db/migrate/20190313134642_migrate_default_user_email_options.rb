0655941a1abc221e293f1c97258eaeefb7e00f0a
Penar Musaraj <pmusaraj@gmail.com>
2019-03-13 15:15:20 -0400

    Disable reversing db migrations

--- db/migrate/20190313134642_migrate_default_user_email_options.rb
+++ db/migrate/20190313134642_migrate_default_user_email_options.rb
@@ -26,25 +26,6 @@ class MigrateDefaultUserEmailOptions < ActiveRecord::Migration[5.2]
   end
 
   def down
-    default_email_level = DB.query_single("SELECT value::int FROM site_settings WHERE name = 'default_email_level'").first
-    default_email_messages_level = DB.query_single("SELECT value::int FROM site_settings WHERE name = 'default_email_messages_level'").first
-
-    if default_email_level == 0
-      execute "INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
-                VALUES ('default_email_always', 5, 't', now(), now())"
-    elsif default_email_level == 2
-      execute "INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
-                VALUES ('default_email_direct', 5, 'f', now(), now())"
-    end
-
-    if default_email_messages_level == 0
-      execute "INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
-                VALUES ('default_email_always', 5, 't', now(), now())"
-    elsif default_email_messages_level == 2
-      execute "INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
-                VALUES ('default_email_personal_messages', 5, 'f', now(), now())"
-    end
-
-    execute "DELETE from site_settings where name in ('default_email_messages_level', 'default_email_level')"
+    raise ActiveRecord::IrreversibleMigration
   end
 end
--- db/post_migrate/20190312194528_drop_email_user_options_columns.rb
+++ db/post_migrate/20190312194528_drop_email_user_options_columns.rb
@@ -2,33 +2,18 @@ require 'migration/column_dropper'
 
 class DropEmailUserOptionsColumns < ActiveRecord::Migration[5.2]
   def up
-    Migration::ColumnDropper.execute_drop(:user_options, %i{email_direct})
-    Migration::ColumnDropper.execute_drop(:user_options, %i{email_always})
-    Migration::ColumnDropper.execute_drop(:user_options, %i{email_private_messages})
+    {
+      user_options: %i{
+        email_direct
+        email_private_messages
+        email_always
+      },
+    }.each do |table, columns|
+      Migration::ColumnDropper.execute_drop(table, columns)
+    end
   end
 
   def down
-    add_column :user_options, :email_direct, :boolean, default: true, null: false
-    add_column :user_options, :email_private_messages, :boolean, default: true, null: false
-    add_column :user_options, :email_always, :boolean, default: false, null: false
-
-    execute <<~SQL
-      UPDATE user_options
-      SET email_direct = CASE
-        WHEN email_level = '0' OR email_level = '1'
-        THEN TRUE ELSE FALSE
-      END,
-      email_private_messages = CASE
-        WHEN email_messages_level = '0' OR email_messages_level = '1'
-        THEN TRUE ELSE FALSE
-      END,
-      email_always = CASE
-        WHEN email_level = '0'
-        THEN TRUE ELSE FALSE
-      END
-    SQL
-
-    remove_column :user_options, :email_level
-    remove_column :user_options, :email_messages_level
+    raise ActiveRecord::IrreversibleMigration
   end
 end
