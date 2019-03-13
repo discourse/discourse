5ca944e33450aa727defecdd5e2afc6e76bffab7
Bianca Nenciu <nbianca@users.noreply.github.com>
2019-03-13 12:34:47 +0200

    FEATURE: Add site contact group. (#7152)

--- spec/components/system_message_spec.rb
+++ spec/components/system_message_spec.rb
@@ -6,11 +6,14 @@ describe SystemMessage do
 
   context 'send' do
 
-    it 'should create a post correctly' do
+    let(:admin) { Fabricate(:admin) }
+    let(:user) { Fabricate(:user) }
 
-      admin = Fabricate(:admin)
-      user = Fabricate(:user)
+    before do
       SiteSetting.site_contact_username = admin.username
+    end
+
+    it 'should create a post correctly' do
       system_message = SystemMessage.new(user)
       post = system_message.create(:welcome_invite)
       topic = post.topic
@@ -23,6 +26,18 @@ describe SystemMessage do
       expect(topic.allowed_users.include?(admin)).to eq(true)
 
       expect(UserArchivedMessage.where(user_id: admin.id, topic_id: topic.id).length).to eq(1)
+    end
+
+    it 'should allow site_contact_group_name' do
+      group = Fabricate(:group)
+      SiteSetting.site_contact_group_name = group.name
+
+      post = SystemMessage.create(user, :welcome_invite)
+      expect(post.topic.allowed_groups).to contain_exactly(group)
+
+      group.update!(name: "anewname")
+      post = SystemMessage.create(user, :welcome_invite)
+      expect(post.topic.allowed_groups).to contain_exactly()
     end
   end
 
