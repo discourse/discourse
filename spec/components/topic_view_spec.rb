5f2cde1037fb02c458d470543369ba9436f182dd
Tarek Khalil <tarek@discourse.org>
2019-03-19 17:12:32 +0000

    Refactor specs

--- spec/components/topic_view_spec.rb
+++ spec/components/topic_view_spec.rb
@@ -44,7 +44,6 @@ describe TopicView do
           SiteSetting.ignore_user_enabled = false
 
           tv = TopicView.new(topic.id, evil_trout)
-          expect(tv.filtered_post_ids.size).to eq(3)
           expect(tv.filtered_post_ids).to match_array([post.id, post2.id, post3.id])
         end
       end
@@ -57,7 +56,6 @@ describe TopicView do
 
         it "filters out ignored user posts" do
           tv = TopicView.new(topic.id, evil_trout)
-          expect(tv.filtered_post_ids.size).to eq(2)
           expect(tv.filtered_post_ids).to match_array([post.id, post2.id])
         end
 
@@ -66,7 +64,6 @@ describe TopicView do
 
           it "filters out ignored user posts only" do
             tv = TopicView.new(topic.id, evil_trout)
-            expect(tv.filtered_post_ids.size).to eq(2)
             expect(tv.filtered_post_ids).to match_array([post.id, post2.id])
           end
         end
@@ -77,7 +74,6 @@ describe TopicView do
 
           it "filters out ignored user posts only" do
             tv = TopicView.new(topic.id, evil_trout)
-            expect(tv.filtered_post_ids.size).to eq(3)
             expect(tv.filtered_post_ids).to match_array([post.id, post2.id, post4.id])
           end
         end
@@ -88,7 +84,6 @@ describe TopicView do
 
           it "filters out ignored user posts only" do
             tv = TopicView.new(topic.id, nil)
-            expect(tv.filtered_post_ids.size).to eq(4)
             expect(tv.filtered_post_ids).to match_array([post.id, post2.id, post3.id, post4.id])
           end
         end
@@ -100,7 +95,6 @@ describe TopicView do
 
           it "filters out ignored user excluding the staff user" do
             tv = TopicView.new(topic.id, nil)
-            expect(tv.filtered_post_ids.size).to eq(3)
             expect(tv.filtered_post_ids).to match_array([post.id, post2.id, post4.id])
           end
         end
