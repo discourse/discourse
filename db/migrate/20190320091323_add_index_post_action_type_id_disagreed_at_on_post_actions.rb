816d8438c68259b8cb2ca7435abc556d6cd1021d
Guo Xiang Tan <tgx_world@hotmail.com>
2019-03-20 17:16:30 +0800

    PERF: Add index to speed up `User#number_of_flagged_posts`.
    On meta, we were seeing the query take almost 100ms.

---
+++ db/migrate/20190320091323_add_index_post_action_type_id_disagreed_at_on_post_actions.rb
@@ -0,0 +1,6 @@
+class AddIndexPostActionTypeIdDisagreedAtOnPostActions < ActiveRecord::Migration[5.2]
+  def change
+    add_index :post_actions, [:post_action_type_id, :disagreed_at],
+      where: "(disagreed_at IS NULL)"
+  end
+end
