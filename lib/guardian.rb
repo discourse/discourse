42aa94e71b22c27c21acfb6d9e6acd145f60e295
Tarek Khalil <tarek@discourse.org>
2019-03-20 18:00:59 +0000

    Refactor

--- lib/guardian.rb
+++ lib/guardian.rb
@@ -391,7 +391,8 @@ class Guardian
   end
 
   def can_ignore_user?(user_id)
-    @user.id != user_id &&
+    SiteSetting.ignore_user_enabled? &&
+      @user.id != user_id &&
       (@user.staff? || @user.trust_level >= TrustLevel.levels[:member]) &&
       User.where(id: user_id, admin: false, moderator: false).exists?
   end
