1d2e2122f3831ba2a0efcb1ac9ebf85de4cd952f
Tarek Khalil <tarek@discourse.org>
2019-03-25 15:36:35 +0000

    Change `state` param to `notification_level`

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -998,14 +998,14 @@ class UsersController < ApplicationController
   def notification_level
     raise Discourse::NotFound unless SiteSetting.ignore_user_enabled
 
-    if params[:state] == "ignore"
+    if params[:notification_level] == "ignore"
       guardian.ensure_can_ignore_user!(params[:notification_level_user_id])
       MutedUser.where(user: current_user, muted_user_id: params[:notification_level_user_id]).delete_all
       IgnoredUser.find_or_create_by!(user: current_user, ignored_user_id: params[:notification_level_user_id])
-    elsif params[:state] == "mute"
+    elsif params[:notification_level] == "mute"
       IgnoredUser.where(user: current_user, ignored_user_id: params[:notification_level_user_id]).delete_all
       MutedUser.find_or_create_by!(user: current_user, muted_user_id: params[:notification_level_user_id])
-    elsif params[:state] == "normal"
+    elsif params[:notification_level] == "normal"
       MutedUser.where(user: current_user, muted_user_id: params[:notification_level_user_id]).delete_all
       IgnoredUser.where(user: current_user, ignored_user_id: params[:notification_level_user_id]).delete_all
     end
