6ebadaed2c120e2bd57fe46d693cdb42eeef6e98
Robin Ward <robin.ward@gmail.com>
2019-04-02 10:26:27 -0400

    FIX: Do not allow `invite_only` and `enable_sso` at the same time
    This functionality was never supported but before the new review queue
    it didn't have any errors. Now the combination of settings is prevented
    and existing sites with sso enabled will be migrated to remove invite
    only.

--- spec/components/validators/enable_sso_validator_spec.rb
+++ spec/components/validators/enable_sso_validator_spec.rb
@@ -26,6 +26,24 @@ RSpec.describe EnableSsoValidator do
       end
     end
 
+    describe "when invite_only is set" do
+      before do
+        SiteSetting.invite_only = true
+        SiteSetting.sso_url = 'https://example.com/sso'
+      end
+
+      it 'allows a false value' do
+        expect(subject.valid_value?('f')).to eq(true)
+      end
+
+      it "doesn't allow true" do
+        expect(subject.valid_value?('t')).to eq(false)
+        expect(subject.error_message).to eq(I18n.t(
+          'site_settings.errors.sso_invite_only'
+        ))
+      end
+    end
+
     describe "when 'sso url' is present" do
       before do
         SiteSetting.sso_url = "https://www.example.com/sso"
