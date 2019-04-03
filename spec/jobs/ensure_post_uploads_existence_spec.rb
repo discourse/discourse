73b500095c3eb520c1f62a8477f3f1b5f97354af
Vinoth Kannan <vinothkannan@vinkas.com>
2019-04-04 02:23:28 +0530

    FEATURE: ensure consistency of post uploads in cooked content

---
+++ spec/jobs/ensure_post_uploads_existence_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Jobs::EnsurePostUploadsExistence do
+
+  context '.execute' do
+    let(:upload) { Fabricate(:upload) }
+    let(:optimized) { Fabricate(:optimized_image, url: '/uploads/default/optimized/1X/d1c2d40ab994e8410c_100x200.png') }
+
+    it 'should create post custom field for missing upload' do
+      post = Fabricate(:post, cooked: "A sample post <img src='#{upload.url}'>")
+      upload.destroy!
+      described_class.new.execute({})
+      field = PostCustomField.last
+      expect(field.name).to eq(Jobs::EnsurePostUploadsExistence::MISSING_UPLOADS)
+      expect(field.value).to eq(upload.url)
+    end
+
+    it 'should not create post custom fields' do
+      post = Fabricate(:post, cooked: "A sample post <a href='#{upload.url}'> <img src='#{optimized.url}'>")
+
+      expect {
+        described_class.new.execute({})
+      }.not_to change {
+        PostCustomField.count
+      }
+    end
+  end
+end
