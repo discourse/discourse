9c41696f6a1a3d28b60efd97fafd6e8d816bba59
Vinoth Kannan <vinothkannan@vinkas.com>
2019-04-04 20:37:44 +0530

    FEATURE: Add scheduled job to ensure s3 uploads existence

---
+++ app/jobs/scheduled/ensure_s3_uploads_existence.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Jobs
+
+  class EnsureS3UploadsExistence < Jobs::Scheduled
+    every 1.day
+
+    def execute(args)
+      return unless SiteSetting.enable_s3_inventory
+      Discourse.store.list_missing_uploads(skip_optimized: true)
+    end
+  end
+end
