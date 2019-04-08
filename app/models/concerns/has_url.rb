f5ac01253abd1ed2bf40b2e7ca9d290e43f8847e
Vinoth Kannan <vinothkannan@vinkas.com>
2019-04-09 02:25:26 +0530

    FIX: use new changed method name and remove whitespaces

--- app/models/concerns/has_url.rb
+++ app/models/concerns/has_url.rb
@@ -8,16 +8,16 @@ module HasUrl
 
     def get_from_url(url)
       return if url.blank?
-  
+
       uri = begin
         URI(URI.unescape(url))
       rescue URI::Error
       end
-  
+
       return if uri&.path.blank?
       data = extract_url(uri.path)
       return if data.blank?
-  
+
       self.find_by("url LIKE ?", "%#{data[1]}")
     end
   end
