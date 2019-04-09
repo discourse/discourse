692e09d813c26a48006ae4542f5116806a618b3f
Robin Ward <robin.ward@gmail.com>
2019-04-09 11:07:04 -0400

    FIX: Only show "Approve" button for the admin user if reviewable

--- app/serializers/admin_user_serializer.rb
+++ app/serializers/admin_user_serializer.rb
@@ -7,10 +7,20 @@ class AdminUserSerializer < AdminUserListSerializer
              :can_send_activation_email,
              :can_activate,
              :can_deactivate,
+             :can_approve,
              :ip_address,
              :registration_ip_address
 
   has_one :single_sign_on_record, serializer: SingleSignOnRecordSerializer, embed: :objects
+
+  def can_approve
+    reviewable = ReviewableUser.find_by(target: object)
+    reviewable.present? && reviewable.actions_for(scope).has?(:approve)
+  end
+
+  def include_can_approve?
+    SiteSetting.must_approve_users
+  end
 
   def can_send_activation_email
     scope.can_send_activation_email?(object)
