51923b34e4d14a5695dc2902d6b54aa8f5859a02
Nicolas Sebastian Vidal <nicolas.s.vidal@gmail.com>
2019-04-16 16:42:38 -0300

    Update shoulda gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,6 +45,16 @@ require 'rspec/rails'
 require 'shoulda'
 require 'sidekiq/testing'
 
+# The shoulda-matchers gem no longer detects the test framework
+# you're using or mixes itself into that framework automatically.
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+  end
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
