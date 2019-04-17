cd2e93b59882f908a670877f5aa904842928ba7f
Robin Ward <robin.ward@gmail.com>
2019-04-17 16:14:36 -0400

    FIX: Show when a post has been deleted in the flag review queue

--- app/serializers/reviewable_flagged_post_serializer.rb
+++ app/serializers/reviewable_flagged_post_serializer.rb
@@ -1,3 +1,12 @@
 class ReviewableFlaggedPostSerializer < ReviewableSerializer
   target_attributes :cooked, :raw, :reply_count
+  attributes :blank_post
+
+  def blank_post
+    true
+  end
+
+  def include_blank_post?
+    object.target.blank?
+  end
 end
