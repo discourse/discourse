7bff88505e59ffa122af90fe3aad1b9a0455ba01
Arpit Jalan <arpit@techapj.com>
2019-04-24 13:50:27 +0530

    FIX: prefer data-original-href attribute to get iframe URL

--- spec/components/email/styles_spec.rb
+++ spec/components/email/styles_spec.rb
@@ -106,6 +106,15 @@ describe Email::Styles do
       expect(frag.at('iframe')).to be_blank
       expect(frag.at('a')).to be_blank
     end
+
+    it "prefers data-original-href attribute to get iframe link" do
+      original_url = "https://vimeo.com/329875646"
+      iframe_url = "https://player.vimeo.com/video/329875646"
+      frag = html_fragment("<iframe src=\"#{iframe_url}\" data-original-href=\"#{original_url}\"></iframe>")
+      expect(frag.at('iframe')).to be_blank
+      expect(frag.at('a')).to be_present
+      expect(frag.at('a')['href']).to eq(original_url)
+    end
   end
 
   context "rewriting protocol relative URLs to the forum" do
