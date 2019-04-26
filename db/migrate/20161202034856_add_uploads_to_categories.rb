c9f6beba05d1fc6ecab60b6de305792c6ad25698
Guo Xiang Tan <tgx_world@hotmail.com>
2019-04-26 10:25:55 +0800

    FIX: Incorrect DB query for `AddUploadsToCategories` migration.
    This is a regression as a result of 7896c74c2bd161f85c976fcd8468797721df30d9. Most instances would have ran the migrations and some might have run this migration with the incorrect query. Impact of this is small for now but I'm fixing this for correctness purposes.

--- db/migrate/20161202034856_add_uploads_to_categories.rb
+++ db/migrate/20161202034856_add_uploads_to_categories.rb
@@ -4,19 +4,19 @@ class AddUploadsToCategories < ActiveRecord::Migration[4.2]
     add_column :categories, :uploaded_background_id, :integer, index: true
 
     execute <<~SQL
-    UPDATE categories
+    UPDATE categories c1
     SET uploaded_logo_id = u.id
-    FROM categories c
-    LEFT JOIN uploads u ON u.url = c.logo_url
-    WHERE u.url IS NOT NULL
+    FROM categories c2
+    INNER JOIN uploads u ON u.url = c2.logo_url
+    WHERE c1.id = c2.id
     SQL
 
     execute <<~SQL
-    UPDATE categories
+    UPDATE categories c1
     SET uploaded_background_id = u.id
-    FROM categories c
-    LEFT JOIN uploads u ON u.url = c.background_url
-    WHERE u.url IS NOT NULL
+    FROM categories c2
+    INNER JOIN uploads u ON u.url = c2.background_url
+    WHERE c1.id = c2.id
     SQL
   end
 end
