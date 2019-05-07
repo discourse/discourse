626534ad94fa55566aaa24c1c3f94c0d3b99a329
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2019-05-07 18:14:13 +0200

    FIX: lighten instead of adding transparency to consolidated pageviews (#7495)

--- app/models/reports/consolidated_page_views.rb
+++ app/models/reports/consolidated_page_views.rb
@@ -14,7 +14,7 @@ Report.add_report("consolidated_page_views") do |report|
     color = report.rgba_color(tertiary)
 
     if filter == "page_view_anon"
-      color = report.rgba_color(tertiary, 0.5)
+      color = report.lighten_color(tertiary, 0.25)
     end
 
     if filter == "page_view_crawler"
