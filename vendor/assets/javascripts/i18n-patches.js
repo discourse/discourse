d6452963cd0dc1a84d39b390f8d16cbc9d3ccf6c
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2019-05-08 08:14:09 +0200

    Revert "Revert "FIX: simpler w function check (#7496)""
    This reverts commit 0c0d05e8f2163336dc0306f15f5a942014a13177.

--- vendor/assets/javascripts/i18n-patches.js
+++ vendor/assets/javascripts/i18n-patches.js
@@ -13,7 +13,7 @@
       return oldI18ntoHumanSize.apply(this, [number, options]);
     };
 
-    if (Ember.typeOf(String.prototype.w) === "function") {
+    if ("w" in String.prototype) {
       String.prototype.i18n = function(options) {
         return I18n.t(String(this), options);
       };
