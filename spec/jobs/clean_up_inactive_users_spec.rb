be723a9b742454d077162c1aaa770811f788b1e3
Gerhard Schlager <mail@gerhard-schlager.at>
2019-05-13 21:44:03 +0200

    FIX: Don’t try to delete inactive moderators
    follow-up to 6d77156a

--- spec/jobs/clean_up_inactive_users_spec.rb
+++ spec/jobs/clean_up_inactive_users_spec.rb
@@ -42,4 +42,12 @@ RSpec.describe Jobs::CleanUpInactiveUsers do
     expect { described_class.new.execute({}) }.to_not change { User.count }
     expect(User.exists?(admin.id)).to eq(true)
   end
+
+  it "doesn't delete inactive mods" do
+    SiteSetting.clean_up_inactive_users_after_days = 4
+    moderator = Fabricate(:moderator, last_seen_at: 5.days.ago, trust_level: TrustLevel.levels[:newuser])
+
+    expect { described_class.new.execute({}) }.to_not change { User.count }
+    expect(User.exists?(moderator.id)).to eq(true)
+  end
 end
