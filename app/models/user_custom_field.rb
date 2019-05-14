88a68e55d5b1e48f08b73d9ede4ec374ef887f93
Sam Saffron <sam.saffron@gmail.com>
2019-05-14 16:02:55 +1000

    PERF: improve performance of publish_notifications_state
    User.publish_notifications_state is called every time a notification is
    created, this can become a very critical code path.
    
    On some heavy notification related sites this can be a major CPU user on PG
    
    This index makes it much cheaper to publish notification state, cause a
    simple index lookup does the trick.

--- app/models/user_custom_field.rb
+++ app/models/user_custom_field.rb
@@ -17,5 +17,6 @@ end
 #
 # Indexes
 #
+#  idx_user_custom_fields_last_reminded_at       (name,user_id) UNIQUE WHERE ((name)::text = 'last_reminded_at'::text)
 #  index_user_custom_fields_on_user_id_and_name  (user_id,name)
 #
