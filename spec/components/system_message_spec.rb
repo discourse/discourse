e5330b41634f9c8be312c0b578dcecc5a3eddedf
Arpit Jalan <arpit@techapj.com>
2019-05-29 16:26:10 +0530

    FIX: create system message in user selected locale
    DEV: refactor `create_from_system_user` to use `create` method

--- spec/components/system_message_spec.rb
+++ spec/components/system_message_spec.rb
@@ -30,6 +30,24 @@ describe SystemMessage do
       expect(UserArchivedMessage.where(user_id: admin.id, topic_id: topic.id).length).to eq(1)
     end
 
+    it 'can create a post from system user in user selected locale' do
+      SiteSetting.allow_user_locale = true
+      user_de = Fabricate(:user, locale: 'de')
+      system_user = Discourse.system_user
+
+      post = SystemMessage.create_from_system_user(user_de, :welcome_invite)
+      topic = post.topic
+
+      expect(post.valid?).to eq(true)
+      expect(topic).to be_private_message
+      expect(topic).to be_valid
+      expect(topic.title).to eq(I18n.with_locale(:de) { I18n.t("system_messages.welcome_invite.subject_template", site_name: SiteSetting.title) })
+      expect(topic.subtype).to eq(TopicSubtype.system_message)
+      expect(topic.allowed_users.include?(user_de)).to eq(true)
+      expect(topic.allowed_users.include?(system_user)).to eq(true)
+      expect(UserArchivedMessage.where(user_id: system_user.id, topic_id: topic.id).length).to eq(0)
+    end
+
     it 'should allow site_contact_group_name' do
       group = Fabricate(:group)
       SiteSetting.site_contact_group_name = group.name
