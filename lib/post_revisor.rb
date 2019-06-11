591b519120b232c3c2663d2f7603006f8c72d8a3
Bianca Nenciu <nbianca@users.noreply.github.com>
2019-06-11 18:06:54 +0300

    FIX: On tag change notify only users watching the tag. (#7707)

--- lib/post_revisor.rb
+++ lib/post_revisor.rb
@@ -94,8 +94,7 @@ class PostRevisor
         tc.record_change('tags', prev_tags, tags)
         DB.after_commit do
           post = tc.topic.ordered_posts.first
-          notified_user_ids = [post.user_id, post.last_editor_id].uniq
-          Jobs.enqueue(:notify_tag_change, post_id: post.id, notified_user_ids: notified_user_ids)
+          Jobs.enqueue(:notify_tag_change, post_id: post.id)
         end
       end
     end
