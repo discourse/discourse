discourse/discourse

commit 3af00a65e689a725fd081725c5d23aed51d6ea42

FIX: site settings loading default values when no db
This fixes a condition where an intermittent db connection could cause
invalid site settings to be stored

It also removes a catch all we had.

Somewhere around Rails 5 `db:create` started wanting full environment
this is a problem for Discourse since it needs to boot up data from the
db.

This removes the catch all and surgically adds a db / redis bypass to
db:create task.

lib/tasks/db.rake
@@ -21,10 +21,21 @@ task 'db:environment:set' => [:load_config]  do |_, args|
   end
 end
 
-task 'db:create' => [:load_config] do |_, args|
+task 'db:force_skip_persist' do
+  GlobalSetting.skip_db = true
+  GlobalSetting.skip_redis = true
+end
+
+task 'db:create' do |_, args|
   if MultisiteTestHelpers.load_multisite?
     system("RAILS_DB=discourse_test_multisite rake db:create")
   end
+end
+
+begin
+  reqs = Rake::Task['db:create'].prerequisites.map(&:to_sym)
+  Rake::Task['db:create'].clear_prerequisites
+  Rake::Task['db:create'].enhance(["db:force_skip_persist"] + reqs)
 end
 
 task 'db:drop' => [:load_config] do |_, args|

lib/site_settings/db_provider.rb
@@ -58,12 +58,7 @@ class SiteSettings::DbProvider
   # table is not in the db yet, initial migration, etc
   def table_exists?
     @table_exists ||= {}
-    begin
-      @table_exists[current_site] ||= ActiveRecord::Base.connection.table_exists?(@model.table_name)
-    rescue
-      STDERR.puts "No connection to db, unable to retrieve site settings! (normal when running db:create)"
-      @table_exists[current_site] = false
-    end
+    @table_exists[current_site] ||= ActiveRecord::Base.connection.table_exists?(@model.table_name)
   end
 
 end

commit 0b9dbe9abe4ea7882208563404597fcbc722bb79

DEV: improve on rake db:create
Followup on 3af00a65 which broke build

config/initializers/100-logster.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 if GlobalSetting.skip_redis?
-  Rails.logger = Rails.logger.chained.first
+  if Rails.logger.respond_to? :chained
+    Rails.logger = Rails.logger.chained.first
+  end
   return
 end
 
@@ -142,6 +144,8 @@ RailsMultisite::ConnectionManagement.each_connection do
 end
 
 if Rails.configuration.multisite
-  chained = Rails.logger.chained
-  chained && chained.first.formatter = RailsMultisite::Formatter.new
+  if Rails.logger.respond_to? :chained
+    chained = Rails.logger.chained
+    chained && chained.first.formatter = RailsMultisite::Formatter.new
+  end
 end

lib/tasks/db.rake
@@ -26,9 +26,9 @@ task 'db:force_skip_persist' do
   GlobalSetting.skip_redis = true
 end
 
-task 'db:create' do |_, args|
-  if MultisiteTestHelpers.load_multisite?
-    system("RAILS_DB=discourse_test_multisite rake db:create")
+task 'db:create' => [:load_config] do |_, args|
+  if !ENV['RAILS_ENV']
+    system("RAILS_ENV=test RAILS_DB=discourse_test_multisite rake db:create")
   end
 end
 