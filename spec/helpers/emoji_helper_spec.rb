e67b797902a821c03602d7257f16b34ac0cae602
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2019-06-25 13:58:18 +0200

    oops

--- spec/helpers/emoji_helper_spec.rb
+++ spec/helpers/emoji_helper_spec.rb
@@ -7,7 +7,6 @@ describe EmojiHelper do
 
   describe "emoji_codes_to_img" do
     it "replaces emoji codes by images" do
-      helper.request.env["HTTP_ACCEPT_ENCODING"] = 'br'
       str = "This is a good day :woman: :man:t4:"
       replaced_str = helper.emoji_codes_to_img(str)
 
