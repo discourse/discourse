544faef2cfe0dac5e8be89c3b8f2b711b62e604d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2019-06-26 14:37:28 +0200

    cleaner code

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -2,16 +2,16 @@
 
 module EmojiHelper
   def emoji_codes_to_img(str)
-    if str
-      str = str.gsub(/:([\w\-+]*(?::t\d)?):/) do |name|
-        code = $1
-        if Emoji.exists?(code)
-          "<img src=\"#{Emoji.url_for(code)}\" title=\"#{code}\" class=\"emoji\" alt=\"#{code}\">"
-        else
-          name
-        end
+    return if str.blank?
+
+    raw str.gsub(/:([\w\-+]*(?::t\d)?):/) do |name|
+      code = $1
+
+      if Emoji.exists?(code)
+        "<img src=\"#{Emoji.url_for(code)}\" title=\"#{code}\" class=\"emoji\" alt=\"#{code}\">"
+      else
+        name
       end
-      raw str
     end
   end
 end
