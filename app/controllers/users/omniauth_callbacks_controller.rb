dfc0165dca92e678e9a1e3f7587a908c4050ba6e
David Taylor <david@taylorhq.com>
2019-07-04 10:06:18 +0100

    Revert "DEV: Let OmniAuth strategies return auth result. (#7833)"
    This reverts commit dc5eb7655148742593257dbe690d829c264f26d2.
    
    It is better to keep any custom redirect logic within omniauth, without relying on the app

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -19,15 +19,6 @@ class Users::OmniauthCallbacksController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :complete
 
   def complete
-    if result = request.env["omniauth.result"]
-      @auth_result = result
-
-      return respond_to do |format|
-        format.html
-        format.json { render json: @auth_result.to_client_hash }
-      end
-    end
-
     auth = request.env["omniauth.auth"]
     raise Discourse::NotFound unless request.env["omniauth.auth"]
 
