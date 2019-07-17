d0e0c8844ab1b9732e2092e0bda996ddd3c50a98
romanrizzi <rizziromanalejandro@gmail.com>
2019-07-17 10:53:02 -0300

    Bump migration timestamp and don't set null when migrating is not possible.

--- db/migrate/20190717133743_migrate_group_list_site_settings.rb
+++ db/migrate/20190717133743_migrate_group_list_site_settings.rb
@@ -13,14 +13,14 @@ class MigrateGroupListSiteSettings < ActiveRecord::Migration[5.2]
     cast_type = from == :id ? '::int[]' : ''
     DB.exec <<~SQL
       UPDATE site_settings
-      SET value = array_to_string(
+      SET value = COALESCE(array_to_string(
         (
           SELECT array_agg(groups.#{to})
           FROM groups
           WHERE groups.#{from} = ANY (string_to_array(site_settings.value, '|', '')#{cast_type})
         ),
         '|', ''
-      )
+      ), site_settings.value)
       WHERE data_type = #{SiteSettings::TypeSupervisor.types[:group_list]}
     SQL
   end
