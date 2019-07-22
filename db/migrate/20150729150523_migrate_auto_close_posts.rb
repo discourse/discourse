89efcfcf28a2d66f88e30a350448d5d38db4f962
Gerhard Schlager <mail@gerhard-schlager.at>
2019-07-22 22:30:57 +0200

    Follow-up for a8cdd685 to fix nested array

--- db/migrate/20150729150523_migrate_auto_close_posts.rb
+++ db/migrate/20150729150523_migrate_auto_close_posts.rb
@@ -5,7 +5,7 @@ class MigrateAutoClosePosts < ActiveRecord::Migration[4.2]
     I18n.overrides_disabled do
       strings = []
       %w(days hours lastpost_days lastpost_hours lastpost_minutes).map do |k|
-        strings << I18n.t("topic_statuses.autoclosed_enabled_#{k}").values.map { |s| s.sub("%{count}", "\\d+") }
+        strings += I18n.t("topic_statuses.autoclosed_enabled_#{k}").values.map { |s| s.sub("%{count}", "\\d+") }
       end
 
       sql = <<~SQL
