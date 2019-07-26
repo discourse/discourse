d5c35be621e983181b990de72fc369d6a9762ed6
majkel <m@majkel.cc>
2019-07-26 19:44:22 +0200

    .chang

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Discourse::Application.configure do
-
+  config.active_record.dump_schema_after_migration = false
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
