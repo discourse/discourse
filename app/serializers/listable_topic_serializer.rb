fe52f4c467a2cf109e2c2d5b88762d34f76de121
romanrizzi <rizziromanalejandro@gmail.com>
2019-08-08 16:53:34 -0300

    When read state needs to be published, the minimum unread count is calculated in the topic query. This way, we can know if someone reads the last post

--- app/serializers/listable_topic_serializer.rb
+++ app/serializers/listable_topic_serializer.rb
@@ -123,7 +123,10 @@ class ListableTopicSerializer < BasicTopicSerializer
   end
 
   def read_by_group_member
-    object.topic_list.publish_read_state
+    return false unless object.topic_list.publish_read_state
+    return false unless object.respond_to?(:minimum_unread_count)
+
+    object.minimum_unread_count && object.minimum_unread_count >= 0
   end
 
   protected
