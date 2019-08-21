4d7b409f44176a587a3b496b7d71edadd259f0a3
scossar <scossar3@gmail.com>
2019-08-21 15:07:59 -0700

    Add hidden attribute to PostWordPressSerializer

--- app/serializers/post_wordpress_serializer.rb
+++ app/serializers/post_wordpress_serializer.rb
@@ -2,7 +2,7 @@
 
 # The most basic attributes of a topic that we need to create a link for it.
 class PostWordpressSerializer < BasicPostSerializer
-  attributes :post_number
+  attributes :post_number, :hidden
 
   def avatar_template
     if object.user
