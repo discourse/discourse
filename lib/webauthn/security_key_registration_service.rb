c0a1ac12d761a13ce436f07904d0fb2ad5594f1f
Martin Brennan <mjrbrennan@gmail.com>
2019-09-17 19:37:07 +1000

    Make CBOR and COSE gems require: false

--- lib/webauthn/security_key_registration_service.rb
+++ lib/webauthn/security_key_registration_service.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'cbor'
+require 'cose'
 
 module Webauthn
   class SecurityKeyRegistrationService < SecurityKeyBaseValidationService
--- spec/fabricators/user_security_key_fabricator.rb
+++ spec/fabricators/user_security_key_fabricator.rb
@@ -2,8 +2,11 @@
 
 Fabricator(:user_security_key) do
   user
-  credential_id { SecureRandom.hex(10) }
-  public_key { SecureRandom.hex(10) }
+  # Note: these values are valid and decode to a credential ID and COSE public key
+  # HOWEVER they are largely useless unless you have the device that created
+  # them. It is nice to have an approximation though.
+  credential_id { 'mJAJ4CznTO0SuLkJbYwpgK75ao4KMNIPlU5KWM92nq39kRbXzI9mSv6GxTcsMYoiPgaouNw7b7zBiS4vsQaO6A==' }
+  public_key { 'pQECAyYgASFYIMNgw4GCpwBUlR2SznJ1yY7B9yFvsuxhfo+C9kcA4IitIlggRdofrCezymy2B/YarX+gfB6gZKg648/cHIMjf6wWmmU=' }
   enabled true
   factor_type { UserSecurityKey.factor_types[:second_factor] }
   name { sequence(:name) { |i| "Security Key #{i + 1}" } }
