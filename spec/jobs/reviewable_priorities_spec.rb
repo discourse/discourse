55ec4502bf5d33ed928e371b3ac30c77f6310802
Robin Ward <robin.ward@gmail.com>
2019-09-19 13:42:50 -0400

    FIX: Require a min amount of reviewables before calculating thresholds
    On forums with very few flags you don't want to calculate averages
    because they won't be very useful. Stick with the defaults until we hit
    15 reviewables at least.

--- spec/jobs/reviewable_priorities_spec.rb
+++ spec/jobs/reviewable_priorities_spec.rb
@@ -4,21 +4,32 @@ require 'rails_helper'
 
 describe Jobs::ReviewablePriorities do
 
-  it "will set priorities based on the maximum score" do
-    (1..6).each { |i| Fabricate(:reviewable, score: i) }
+  it "needs returns 0s with no existing reviewables" do
     Jobs::ReviewablePriorities.new.execute({})
-
-    expect(Reviewable.min_score_for_priority(:low)).to eq(0.0)
-    expect(Reviewable.min_score_for_priority(:medium)).to eq(3.0)
-    expect(Reviewable.min_score_for_priority('medium')).to eq(3.0)
-    expect(Reviewable.min_score_for_priority(:high)).to eq(6.0)
-  end
-
-  it "will return 0 if no reviewables exist" do
-    Jobs::ReviewablePriorities.new.execute({})
-
     expect(Reviewable.min_score_for_priority(:low)).to eq(0.0)
     expect(Reviewable.min_score_for_priority(:medium)).to eq(0.0)
     expect(Reviewable.min_score_for_priority(:high)).to eq(0.0)
+    expect(Reviewable.score_required_to_hide_post).to eq(8.33)
   end
+
+  it "needs a minimum amount of reviewables before it calculates anything" do
+    (1..5).each { |i| Fabricate(:reviewable, score: i) }
+    Jobs::ReviewablePriorities.new.execute({})
+    expect(Reviewable.min_score_for_priority(:low)).to eq(0.0)
+    expect(Reviewable.min_score_for_priority(:medium)).to eq(0.0)
+    expect(Reviewable.min_score_for_priority(:high)).to eq(0.0)
+    expect(Reviewable.score_required_to_hide_post).to eq(8.33)
+  end
+
+  it "will set priorities based on the maximum score" do
+    (1..Jobs::ReviewablePriorities.min_reviewables).each { |i| Fabricate(:reviewable, score: i) }
+    Jobs::ReviewablePriorities.new.execute({})
+
+    expect(Reviewable.min_score_for_priority(:low)).to eq(0.0)
+    expect(Reviewable.min_score_for_priority(:medium)).to eq(8.0)
+    expect(Reviewable.min_score_for_priority('medium')).to eq(8.0)
+    expect(Reviewable.min_score_for_priority(:high)).to eq(13.0)
+    expect(Reviewable.score_required_to_hide_post).to eq(8.66)
+  end
+
 end
