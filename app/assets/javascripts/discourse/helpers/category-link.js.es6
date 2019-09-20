discourse/discourse

commit db14e10943aeb87f3a2e06f02ca788986f039077

SECURITY: category badges should HTML escape names

app/models/category.rb
@@ -217,7 +217,7 @@ class Category < ActiveRecord::Base
 
     @@cache ||= LruRedux::ThreadSafeCache.new(1000)
     @@cache.getset(self.description) do
-      Nokogiri::HTML.fragment(self.description).text.strip
+      Nokogiri::HTML.fragment(self.description).text.strip.html_safe
     end
   end
 

spec/components/category_badge_spec.rb
@@ -1,0 +1,15 @@
+require 'rails_helper'
+require 'category_badge'
+
+describe CategoryBadge do
+  it "escapes HTML in category names / descriptions" do
+    c = Fabricate(:category, name: '<b>name</b>', description: '<b>title</b>')
+
+    html = CategoryBadge.html_for(c)
+
+    expect(html).not_to include("<b>title</b>")
+    expect(html).not_to include("<b>name</b>")
+    expect(html).to include(ERB::Util.html_escape("<b>name</b>"))
+    expect(html).to include("title='title'")
+  end
+end

lib/category_badge.rb
@@ -79,7 +79,7 @@ module CategoryBadge
 
     # category name
     class_names = 'badge-category clear-badge'
-    description = category.description_text ? "title='#{category.description_text.html_safe}'" : ''
+    description = category.description_text ? "title='#{category.description_text}'" : ''
     category_url = opts[:absolute_url] ? "#{Discourse.base_url_no_prefix}#{category.url}" : category.url
 
     extra_span_classes =
@@ -102,7 +102,10 @@ module CategoryBadge
     result << "<span style='#{extra_span_classes}' data-drop-close='true' class='#{class_names}'
                  #{description}>"
 
-    result << category.name.html_safe << '</span>'
-    "<a class='badge-wrapper #{extra_classes}' href='#{category_url}'" + (opts[:inline_style] ? inline_badge_wrapper_style : '') + ">#{result}</a>"
+    result << ERB::Util.html_escape(category.name) << '</span>'
+
+    result = "<a class='badge-wrapper #{extra_classes}' href='#{category_url}'" + (opts[:inline_style] ? inline_badge_wrapper_style : '') + ">#{result}</a>"
+
+    result.html_safe
   end
 end

commit d89d0b775f16803f870484c6751450cf8b062af6

FIX: Correctly escape category description text
This bug has been introduced in db14e10943aeb87f3a2e06f02ca788986f039077.

app/assets/javascripts/discourse/helpers/category-link.js.es6
@@ -121,7 +121,7 @@ function defaultCategoryLinkRenderer(category, opts) {
     'data-drop-close="true" class="' +
     classNames +
     '"' +
-    (description ? 'title="' + escapeExpression(description) + '" ' : "") +
+    (description ? 'title="' + description + '" ' : "") +
     ">";
 
   let categoryName = escapeExpression(get(category, "name"));

app/models/category.rb
@@ -261,7 +261,8 @@ class Category < ActiveRecord::Base
 
     @@cache ||= LruRedux::ThreadSafeCache.new(1000)
     @@cache.getset(self.description) do
-      Nokogiri::HTML.fragment(self.description).text.strip.html_safe
+      text = Nokogiri::HTML.fragment(self.description).text.strip
+      Rack::Utils.escape_html(text).html_safe
     end
   end
 

spec/components/category_badge_spec.rb
@@ -14,4 +14,11 @@ describe CategoryBadge do
     expect(html).to include(ERB::Util.html_escape("<b>name</b>"))
     expect(html).to include("title='title'")
   end
+
+  it "escapes code block contents" do
+    c = Fabricate(:category, description: '<code>\' &lt;b id="x"&gt;</code>')
+    html = CategoryBadge.html_for(c)
+
+    expect(html).to include("title='&#x27; &lt;b id=&quot;x&quot;&gt;'")
+  end
 end

spec/models/category_spec.rb
@@ -357,7 +357,7 @@ describe Category do
       c = Category.new
       expect(c.description_text).to be_nil
       c.description = "&lt;hello <a>test</a>."
-      expect(c.description_text).to eq("<hello test.")
+      expect(c.description_text).to eq("&lt;hello test.")
     end
   end
 