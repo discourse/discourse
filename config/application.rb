f53184920538cdb66746417b400a16aad1d64f05
Jarek Radosz <jradosz@gmail.com>
2019-10-06 18:33:08 +0200

    Extract the plugin_initialization_guard

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ require 'action_mailer/railtie'
 require 'sprockets/railtie'
 
 # Plugin related stuff
+require_relative '../lib/plugin_initialization_guard'
 require_relative '../lib/discourse_event'
 require_relative '../lib/discourse_plugin'
 require_relative '../lib/discourse_plugin_registry'
@@ -52,43 +53,6 @@ if defined?(Bundler)
   end
 
   Bundler.require(*bundler_groups)
-end
-
-def with_plugin_guard(&block)
-  begin
-    block.call
-  rescue => error
-    plugins_directory = Rails.root + 'plugins'
-
-    plugin_path = error.backtrace_locations.lazy.map do |location|
-      Pathname.new(location.absolute_path)
-        .ascend
-        .lazy
-        .find { |path| path.parent == plugins_directory }
-    end.next
-
-    raise unless plugin_path
-
-    stack_trace = error.backtrace.each_with_index.inject([]) do |messages, (line, index)|
-      if index == 0
-        messages << "#{line}: #{error} (#{error.class})"
-      else
-        messages << "\t#{index}: from #{line}"
-      end
-    end.reverse.join("\n")
-
-    STDERR.puts <<~MESSAGE
-      #{stack_trace}
-
-      ** INCOMPATIBLE PLUGIN **
-
-      You are unable to build Discourse due to errors in the plugin at
-      #{plugin_path}
-
-      Please try removing this plugin and rebuilding again!
-    MESSAGE
-    exit 1
-  end
 end
 
 module Discourse
@@ -303,7 +267,7 @@ module Discourse
         Discourse.activate_plugins!
       end
     else
-      with_plugin_guard do
+      plugin_initialization_guard do
         Discourse.activate_plugins!
       end
     end
@@ -340,7 +304,7 @@ module Discourse
       OpenID::Util.logger = Rails.logger
 
       # Load plugins
-      with_plugin_guard do
+      plugin_initialization_guard do
         Discourse.plugins.each(&:notify_after_initialize)
       end
 
