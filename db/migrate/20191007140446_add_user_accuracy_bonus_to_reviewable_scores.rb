2f5ecae7a1bb48138886bbb9280e380a6b1477d4
Mark VanLandingham <markvanlan@gmail.com>
2019-10-07 15:24:01 -0500

    migration cleanup

--- db/migrate/20191007140446_add_user_accuracy_bonus_to_reviewable_scores.rb
+++ db/migrate/20191007140446_add_user_accuracy_bonus_to_reviewable_scores.rb
@@ -3,28 +3,19 @@
 class AddUserAccuracyBonusToReviewableScores < ActiveRecord::Migration[6.0]
   def up
     # Add user_accuracy_bonus to reviewable_scores
-    execute <<~SQL
-    ALTER TABLE reviewable_scores
-    ADD COLUMN user_accuracy_bonus float
-    DEFAULT 0
-    SQL
+    add_column :reviewable_scores, :user_accuracy_bonus, :float, default: 0
 
     # Set user_accuracy_bonus = score - user.trust_level - 1
     execute <<~SQL
     UPDATE reviewable_scores
-    SET user_accuracy_bonus = score - (
-    SELECT trust_level
+    SET user_accuracy_bonus = score - users.trust_level - 1
     FROM users
-    WHERE users.id = reviewable_scores.user_id
-    ) - 1;
+    WHERE reviewable_scores.user_id = users.id
     SQL
   end
 
   def down
     # Remove user_accuracy_bonus from reviewable_scores
-    execute <<~SQL
-    ALTER TABLE reviewable_scores
-    DROP user_accuracy_bonus
-    SQL
+    remove_column :reviewable_scores, :user_accuracy_bonus
   end
 end
