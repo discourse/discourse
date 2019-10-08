80f9f5ea86036568726189cebb5f75557f60d7a8
Mark VanLandingham <markvanlan@gmail.com>
2019-10-08 08:31:43 -0500

    user_accuracy_bonus column not nullable

--- db/migrate/20191007140446_add_user_accuracy_bonus_to_reviewable_scores.rb
+++ db/migrate/20191007140446_add_user_accuracy_bonus_to_reviewable_scores.rb
@@ -3,7 +3,7 @@
 class AddUserAccuracyBonusToReviewableScores < ActiveRecord::Migration[6.0]
   def up
     # Add user_accuracy_bonus to reviewable_scores
-    add_column :reviewable_scores, :user_accuracy_bonus, :float, default: 0
+    add_column :reviewable_scores, :user_accuracy_bonus, :float, default: 0, null: false
 
     # Set user_accuracy_bonus = score - user.trust_level - 1
     execute <<~SQL
