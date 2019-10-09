8f001bdb1b33a6e08d6f63576b9d1510caf63eb3
Sam Saffron <sam.saffron@gmail.com>
2019-10-09 12:49:22 +1100

    SECURITY: mini profiler enabled incorrectly for admins
    We expect mini profiler only to show up on accounts that are flagged as
    developer accounts.
    
    Unfortunately there was a bypass on any controllers that mix in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -187,10 +187,6 @@ module ApplicationHelper
     @guardian ||= Guardian.new(current_user)
   end
 
-  def mini_profiler_enabled?
-    defined?(Rack::MiniProfiler) && admin?
-  end
-
   def admin?
     current_user.try(:admin?)
   end
