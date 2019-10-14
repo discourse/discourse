086b46051cfb18e78a5e4ca004d23ee84dfd229f
Dan Ungureanu <dan@ungureanu.me>
2019-10-14 13:03:22 +0300

    FIX: Zeitwerk-related fixes for jobs. (#8187)

--- app/jobs/scheduled/clean_up_deprecated_url_site_settings.rb
+++ app/jobs/scheduled/clean_up_deprecated_url_site_settings.rb
@@ -5,7 +5,7 @@ module Jobs
     every 1.day
 
     def execute(args)
-      Jobs::MigrateUrlSiteSettings::SETTINGS.each do |old_setting, new_setting|
+      ::Jobs::MigrateUrlSiteSettings::SETTINGS.each do |old_setting, new_setting|
         if SiteSetting.where("name = ? AND value IS NOT NULL", new_setting).exists?
           SiteSetting.set(old_setting, nil, warn: false)
           SiteSetting.find_by(name: old_setting).destroy!
