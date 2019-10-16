48c123849228cc8a392e989558fe89734cd02656
Penar Musaraj <pmusaraj@gmail.com>
2019-10-16 11:27:07 -0400

    FIX: Do not send notification to empty push_url
    Completes e4424d7

--- app/services/post_alerter.rb
+++ app/services/post_alerter.rb
@@ -453,7 +453,7 @@ class PostAlerter
     if SiteSetting.allow_user_api_key_scopes.split("|").include?("push") && SiteSetting.allowed_user_api_push_urls.present?
       clients = user.user_api_keys
         .where("('push' = ANY(scopes) OR 'notifications' = ANY(scopes))")
-        .where("push_url IS NOT NULL")
+        .where("push_url IS NOT NULL AND push_url <> ''")
         .where("position(push_url IN ?) > 0", SiteSetting.allowed_user_api_push_urls)
         .where("revoked_at IS NULL")
         .pluck(:client_id, :push_url)
