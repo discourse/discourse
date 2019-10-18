2fa380892836a0f650905ae70235358e82ac26de
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2019-10-18 12:27:51 +0200

    FIX: polyfills forEach support on NodeList for IE11 (#8213)

--- app/assets/javascripts/polyfills.js
+++ app/assets/javascripts/polyfills.js
@@ -356,4 +356,14 @@ if (!String.prototype.repeat) {
   };
 }
 
+// https://developer.mozilla.org/fr/docs/Web/API/NodeList/forEach
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function(callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
 /* eslint-enable */
