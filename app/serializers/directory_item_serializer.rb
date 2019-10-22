eb502d09dad95545dc49abd628b179a7f3922ec7
Faizaan Gagan <fzngagan@gmail.com>
2019-10-22 15:26:27 +0530

    used ternary operator for conditionally loading serializer

--- app/serializers/directory_item_serializer.rb
+++ app/serializers/directory_item_serializer.rb
@@ -9,19 +9,10 @@ class DirectoryItemSerializer < ApplicationSerializer
   attributes :id,
              :time_read
 
-  serialize_all_user_attributes = SiteSetting.respond_to?(:user_directory_includes_profile) && SiteSetting.user_directory_includes_profile
+    has_one :user, embed: :objects, serializer: ( SiteSetting.respond_to?(:user_directory_includes_profile) && SiteSetting.user_directory_includes_profile ) ?
+                                                  ::UserSerializer.class_eval {include UserPrimaryGroupMixin}
+                                                  : DirectoryItemSerializer::UserSerializer
 
-  if serialize_all_user_attributes
-  class ::UserSerializer
-    include UserPrimaryGroupMixin
-  end
-
-  serializer = ::UserSerializer
-  else
-  serializer = UserSerializer
-  end
-
-  has_one :user, embed: :objects, serializer: serializer
   attributes *DirectoryItem.headings
 
   def id
