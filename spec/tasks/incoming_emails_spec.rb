acdeec1da6d465561ea487a0a2934996dfee732d
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2019-10-28 15:06:41 +1100

    FIX: better way to determine if raw changed in rake task

--- spec/tasks/incoming_emails_spec.rb
+++ spec/tasks/incoming_emails_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe "incoming emails tasks" do
   before do
+    Rake::Task.clear
     Discourse::Application.load_tasks
   end
 
@@ -11,14 +12,14 @@ RSpec.describe "incoming emails tasks" do
     fab!(:incoming_email) { Fabricate(:incoming_email, raw: email(:attached_txt_file)) }
 
     it 'updates record' do
-      expect { Rake::Task['incoming_emails:truncate_long'].invoke }.to change { incoming_email.reload.updated_at }
+      expect { Rake::Task['incoming_emails:truncate_long'].invoke }.to change { incoming_email.reload.raw }
     end
   end
 
   describe 'short email without attachment' do
     fab!(:incoming_email) { Fabricate(:incoming_email, raw: email(:html_reply)) }
     it 'does not update record' do
-      expect { Rake::Task['incoming_emails:truncate_long'].invoke }.not_to change { incoming_email.reload.updated_at }
+      expect { Rake::Task['incoming_emails:truncate_long'].invoke }.not_to change { incoming_email.reload.raw }
     end
   end
 end
