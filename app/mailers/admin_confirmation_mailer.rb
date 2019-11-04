e56076d72a30d55fb74be798d2cf1ebc75ea6ff0
Arpit Jalan <arpit@techapj.com>
2019-11-04 14:47:00 +0530

    UX: show user email address on "grant admin access" email and UI

--- app/mailers/admin_confirmation_mailer.rb
+++ app/mailers/admin_confirmation_mailer.rb
@@ -3,10 +3,11 @@
 class AdminConfirmationMailer < ActionMailer::Base
   include Email::BuildEmailHelper
 
-  def send_email(to_address, target_username, token)
+  def send_email(to_address, target_email, target_username, token)
     build_email(
       to_address,
       template: 'admin_confirmation_mailer',
+      target_email: target_email,
       target_username: target_username,
       admin_confirm_url: confirm_admin_url(token: token, host: Discourse.base_url)
     )
