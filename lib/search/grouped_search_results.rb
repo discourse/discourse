94eda0fd38c3a62195872d2825175699a94ba9d3
Penar Musaraj <pmusaraj@gmail.com>
2019-11-06 10:32:15 -0500

    FIX: skip invalid URLs when checking for audio/video in search blurbs
    Fixes 500 errors on search queries introduced in 580a4a8

--- lib/search/grouped_search_results.rb
+++ lib/search/grouped_search_results.rb
@@ -78,13 +78,15 @@ class Search
 
       urls = Set.new
       cooked.scan(URI.regexp(%w{http https})) { urls << $& }
-
       urls.each do |url|
-        case File.extname(URI(url).path || "")
-        when Oneboxer::VIDEO_REGEX
-          cooked.gsub!(url, I18n.t("search.video"))
-        when Oneboxer::AUDIO_REGEX
-          cooked.gsub!(url, I18n.t("search.audio"))
+        begin
+          case File.extname(URI(url).path || "")
+          when Oneboxer::VIDEO_REGEX
+            cooked.gsub!(url, I18n.t("search.video"))
+          when Oneboxer::AUDIO_REGEX
+            cooked.gsub!(url, I18n.t("search.audio"))
+          end
+        rescue URI::InvalidURIError
         end
       end
 
