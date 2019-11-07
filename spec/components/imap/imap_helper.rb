0481ea2b1d411382317c9a5f58a979226ace87ef
Dan Ungureanu <dan@ungureanu.me>
2019-11-07 14:00:00 +0200

    DEV: Add test suite for IMAP and SMTP synchronization
    Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>

---
+++ spec/components/imap/imap_helper.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class MockedImapProvider < Imap::Providers::Gmail
+  def connect!; end
+  def disconnect!; end
+  def open_mailbox(mailbox_name, write = false); end
+
+  def labels
+    ['INBOX']
+  end
+end
+
+def EmailFabricator(options)
+  email = ''
+
+  if options[:in_reply_to]
+    email += "In-Reply-To: #{options[:in_reply_to]}\n"
+    email += "References: #{options[:in_reply_to]}\n"
+  end
+
+  if options[:message_id]
+    email += "Message-ID: #{options[:message_id]}\n"
+  end
+
+  if options[:cc]
+    email += "Cc: #{options[:cc]}\n"
+  end
+
+  email += <<~TXT
+    MIME-Version: 1.0
+    To: #{options[:to] || "Joffrey <joffrey@discourse.org>"}
+    From: #{options[:from] || "Dan <dan@discourse.org>"}
+    Date: Sat, 31 Mar 2018 17:50:19 -0700
+    Subject: #{options[:subject] || "This is a test email subhect"}
+    Content-Type: #{options[:content_type] || 'text/plain; charset="UTF-8"'}
+
+    #{options[:body] || "This is an email *body*. :smile:"}
+  TXT
+end
