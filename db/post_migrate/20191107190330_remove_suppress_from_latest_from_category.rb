fb8789f5605fec6b97e83aaab830c4689410177f
Vinoth Kannan <vinothkannan@vinkas.com>
2019-11-08 07:53:38 +0530

    FIX: append the suppressed category ids to default categories muted instead of replacing.

--- db/post_migrate/20191107190330_remove_suppress_from_latest_from_category.rb
+++ db/post_migrate/20191107190330_remove_suppress_from_latest_from_category.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
 class RemoveSuppressFromLatestFromCategory < ActiveRecord::Migration[6.0]
   DROPPED_COLUMNS ||= {
     categories: %i{suppress_from_latest}
   }
 
   def up
-    SiteSetting.default_categories_muted = Category.where(suppress_from_latest: true).pluck(:id).join("|")
+    muted_category_ids = SiteSetting.default_categories_muted.split("|")
+    suppressed_category_ids = Category.where(suppress_from_latest: true).pluck(:id).map(&:to_s)
+    SiteSetting.default_categories_muted = (muted_category_ids + suppressed_category_ids).uniq.join("|")
 
     DROPPED_COLUMNS.each do |table, columns|
       Migration::ColumnDropper.execute_drop(table, columns)
