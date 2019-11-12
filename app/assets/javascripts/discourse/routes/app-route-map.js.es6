discourse/discourse

commit 59241df25179023089d506b84e7f647452a4521e

FIX: Display tags topic list correctly when none is selected for subcategories

app/assets/javascripts/discourse/routes/app-route-map.js.es6
@@ -199,6 +199,7 @@ export default function() {
   this.route("tags", { resetNamespace: true }, function() {
     this.route("show", { path: "/:tag_id" });
     this.route("showCategory", { path: "/c/:category/:tag_id" });
+    this.route("showCategoryNone", { path: "/c/:category/none/:tag_id" });
     this.route("showParentCategory", {
       path: "/c/:parent_category/:category/:tag_id"
     });
@@ -208,6 +209,9 @@ export default function() {
         path: "/:tag_id/l/" + filter
       });
       this.route("showCategory" + filter.capitalize(), {
+        path: "/c/:category/:tag_id/l/" + filter
+      });
+      this.route("showCategoryNone" + filter.capitalize(), {
         path: "/c/:category/:tag_id/l/" + filter
       });
       this.route("showParentCategory" + filter.capitalize(), {

app/assets/javascripts/discourse/pre-initializers/dynamic-route-builders.js.es6
@@ -95,6 +95,7 @@ export default {
     });
 
     app["TagsShowCategoryRoute"] = TagsShowRoute.extend();
+    app["TagsShowCategoryNoneRoute"] = TagsShowRoute.extend({ noSubcategories: true });
     app["TagsShowParentCategoryRoute"] = TagsShowRoute.extend();
 
     site.get("filters").forEach(function(filter) {
@@ -104,6 +105,9 @@ export default {
       app[
         "TagsShowCategory" + filter.capitalize() + "Route"
       ] = TagsShowRoute.extend({ navMode: filter });
+      app[
+        "TagsShowNoneCategory" + filter.capitalize() + "Route"
+      ] = TagsShowRoute.extend({ navMode: filter, noSubcategories: true });
       app[
         "TagsShowParentCategory" + filter.capitalize() + "Route"
       ] = TagsShowRoute.extend({ navMode: filter });

app/assets/javascripts/discourse/routes/tags-show.js.es6
@@ -85,6 +85,8 @@ export default DiscourseRoute.extend({
       );
       if (parentCategorySlug) {
         filter = `tags/c/${parentCategorySlug}/${categorySlug}/${tagId}/l/${topicFilter}`;
+      } else if (this.noSubcategories) {
+        filter = `tags/c/${categorySlug}/none/${tagId}/l/${topicFilter}`;
       } else {
         filter = `tags/c/${categorySlug}/${tagId}/l/${topicFilter}`;
       }
@@ -162,7 +164,8 @@ export default DiscourseRoute.extend({
       category: this.category,
       filterMode: this.filterMode,
       navMode: this.navMode,
-      tagNotification: this.tagNotification
+      tagNotification: this.tagNotification,
+      noSubcategories: this.noSubcategories
     });
   },
 

commit 241d9a3034aa628282d34d71fc7f75b6519ef615

FIX: Topic lists filtered by tag and a specified filter don't work
This was a regression caused by 59241df2

app/assets/javascripts/discourse/routes/app-route-map.js.es6
@@ -212,7 +212,7 @@ export default function() {
         path: "/c/:category/:tag_id/l/" + filter
       });
       this.route("showCategoryNone" + filter.capitalize(), {
-        path: "/c/:category/:tag_id/l/" + filter
+        path: "/c/:category/none/:tag_id/l/" + filter
       });
       this.route("showParentCategory" + filter.capitalize(), {
         path: "/c/:parent_category/:category/:tag_id/l/" + filter