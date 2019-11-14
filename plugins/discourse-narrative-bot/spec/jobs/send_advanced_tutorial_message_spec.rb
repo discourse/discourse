7f045d84b1f7e7e0a56c9d6ac54cb7f236621835
romanrizzi <rizziromanalejandro@gmail.com>
2019-11-14 17:46:20 -0300

    Use correct variable name

--- plugins/discourse-narrative-bot/spec/jobs/send_advanced_tutorial_message_spec.rb
+++ plugins/discourse-narrative-bot/spec/jobs/send_advanced_tutorial_message_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe Jobs::SendAdvancedTutorialMessage do
 
   it 'sends a message to the promoted user' do
     user = Fabricate(:user)
-    discobot_user = Discourse.system_user
+    system_user = Discourse.system_user
     Jobs.enqueue(:send_advanced_tutorial_message, user_id: user.id)
 
     topic = Topic.last
 
     expect(topic).not_to be_nil
-    expect(topic.user).to eq(discobot_user)
+    expect(topic.user).to eq(system_user)
     expect(topic.archetype).to eq(Archetype.private_message)
     expect(topic.topic_allowed_users.pluck(:user_id)).to contain_exactly(
-      discobot_user.id, user.id
+      system_user.id, user.id
     )
   end
 end
