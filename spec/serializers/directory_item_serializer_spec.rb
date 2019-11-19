c1c0ff732ab875a92f5ab21c58acb66e9e43e7b4
Faizaan Gagan <fzngagan@gmail.com>
2019-11-19 14:39:02 +0530

    added a test for the new site setting

---
+++ spec/serializers/directory_item_serializer_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe DirectoryItemSerializer do
+
+  context "serializes additional user attributes when site setting is enabled" do
+
+    it "serializes when the setting is on" do
+     SiteSetting.user_directory_includes_profile = true
+     expect(DirectoryItemSerializer.user_serializer).to eq(::UserSerializer)
+    end
+
+    it "doesn't serialize when the setting is off" do
+        SiteSetting.user_directory_includes_profile = false
+        expect(DirectoryItemSerializer.user_serializer).to eq(DirectoryItemSerializer::DirectoryItemUserSerializer)
+    end
+  end
+
+end
