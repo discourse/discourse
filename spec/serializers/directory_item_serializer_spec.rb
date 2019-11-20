2589377862bc9d377aae3075e0476894775cd300
Faizaan Gagan <fzngagan@gmail.com>
2019-11-20 09:13:22 +0530

    fixed formatting issues

--- spec/serializers/directory_item_serializer_spec.rb
+++ spec/serializers/directory_item_serializer_spec.rb
@@ -7,13 +7,13 @@ describe DirectoryItemSerializer do
   context "serializes additional user attributes when site setting is enabled" do
 
     it "serializes when the setting is on" do
-     SiteSetting.user_directory_includes_profile = true
-     expect(DirectoryItemSerializer.user_serializer).to eq(::UserSerializer)
+      SiteSetting.user_directory_includes_profile = true
+      expect(DirectoryItemSerializer.user_serializer).to eq(::UserSerializer)
     end
 
     it "doesn't serialize when the setting is off" do
-        SiteSetting.user_directory_includes_profile = false
-        expect(DirectoryItemSerializer.user_serializer).to eq(DirectoryItemSerializer::DirectoryItemUserSerializer)
+      SiteSetting.user_directory_includes_profile = false
+      expect(DirectoryItemSerializer.user_serializer).to eq(DirectoryItemSerializer::DirectoryItemUserSerializer)
     end
   end
 
