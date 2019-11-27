bc6a643f5cbde321732bbd1bff93f62504c0e0a5
Sam Saffron <sam.saffron@gmail.com>
2019-11-27 12:35:14 +1100

    DEV: use Discourse.cache over Rails.cache
    With manual merge on lib/oneboxer.rb

--- app/models/topic_embed.rb
+++ app/models/topic_embed.rb
@@ -228,7 +228,7 @@ class TopicEmbed < ActiveRecord::Base
   end
 
   def self.expanded_for(post)
-    Rails.cache.fetch("embed-topic:#{post.topic_id}", expires_in: 10.minutes) do
+    Discourse.cache.fetch("embed-topic:#{post.topic_id}", expires_in: 10.minutes) do
       url = TopicEmbed.where(topic_id: post.topic_id).pluck_first(:embed_url)
       response = TopicEmbed.find_remote(url)
 
