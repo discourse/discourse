600356ee769ca710e146598bca4423add1849462
Sam Saffron <sam.saffron@gmail.com>
2019-11-27 12:35:14 +1100

    DEV: use Discourse.cache over Rails.cache
    Discourse.cache is a more consistent method to use and offers clean fallback
    if you are skipping redis
    
    This is part of a larger change that both optimizes Discoruse.cache and omits
    use of setex on $redis in favor of consistently using discourse cache
    
    Bench does reveal that use of Rails.cache and Discourse.cache is 1.25x slower
    than redis.setex / get so a re-implementation will follow prior to porting

--- lib/oneboxer.rb
+++ lib/oneboxer.rb
@@ -51,7 +51,7 @@ module Oneboxer
   end
 
   def self.cached_onebox(url)
-    if c = Rails.cache.read(onebox_cache_key(url))
+    if c = Discourse.cache.read(onebox_cache_key(url))
       c[:onebox]
     end
   rescue => e
@@ -61,7 +61,7 @@ module Oneboxer
   end
 
   def self.cached_preview(url)
-    if c = Rails.cache.read(onebox_cache_key(url))
+    if c = Discourse.cache.read(onebox_cache_key(url))
       c[:preview]
     end
   rescue => e
@@ -71,7 +71,7 @@ module Oneboxer
   end
 
   def self.invalidate(url)
-    Rails.cache.delete(onebox_cache_key(url))
+    Discourse.cache.delete(onebox_cache_key(url))
   end
 
   # Parse URLs out of HTML, returning the document when finished.
@@ -281,7 +281,7 @@ module Oneboxer
   end
 
   def self.external_onebox(url)
-    Rails.cache.fetch(onebox_cache_key(url), expires_in: 1.day) do
+    Discourse.cache.fetch(onebox_cache_key(url), expires_in: 1.day) do
       fd = FinalDestination.new(url,
                               ignore_redirects: ignore_redirects,
                               ignore_hostnames: blacklisted_domains,
