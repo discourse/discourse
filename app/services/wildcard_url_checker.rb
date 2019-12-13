1c4987504853ea90025bcea6b78da76cbf84b01a
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2019-12-13 13:12:12 +1100

    SECURITY: vulnerability in WildcardUrlChecker

--- app/services/wildcard_url_checker.rb
+++ app/services/wildcard_url_checker.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 module WildcardUrlChecker
+  VALID_PROTOCOLS = %w(http https discourse).freeze
 
   def self.check_url(url, url_to_check)
+    return nil if !valid_url?(url_to_check)
+
     escaped_url = Regexp.escape(url).sub("\\*", '\S*')
-    url_regex = Regexp.new("^#{escaped_url}$", 'i')
+    url_regex = Regexp.new("\\A#{escaped_url}\\z", 'i')
 
     url_to_check.match(url_regex)
   end
 
+  private
+
+  def self.valid_url?(url)
+    uri = URI.parse(url)
+    VALID_PROTOCOLS.include?(uri&.scheme) && uri&.host.present?
+  rescue URI::InvalidURIError
+    false
+  end
 end
