1767e2321d9ab5be6e65fc5d40d9b2620f6b3937
Jeff Wong <awole20@gmail.com>
2019-12-18 18:22:43 -0800

    FIX: correct user serializer user method for extended serializer

--- spec/serializers/basic_user_serializer_spec.rb
+++ spec/serializers/basic_user_serializer_spec.rb
@@ -14,6 +14,16 @@ describe BasicUserSerializer do
       expect(json[:avatar_template]).to eq(user.avatar_template)
     end
 
+    describe 'extended serializers' do
+      Fabricator(:post_action) do
+      end
+      let(:post_action) { Fabricate.build(:post_action, user: user) }
+      let(:serializer) { PostActionUserSerializer.new(post_action, scope: Guardian.new(user), root: false) }
+      it "returns the user correctly" do
+        expect(serializer.user.username).to eq(user.username)
+      end
+    end
+
     it "doesn't return the name it when `enable_names` is false" do
       SiteSetting.enable_names = false
       expect(json[:name]).to eq(nil)
