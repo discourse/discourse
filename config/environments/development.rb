bc4c40abd44dd818aebdc6593bac952d5eaede9d
David Taylor <david@taylorhq.com>
2019-12-30 12:17:12 +0000

    DEV: Remove unsafe-eval from development CSP (#8569)
    - Refactor source_url to avoid using eval in development
    - Precompile handlebars in development
    - Include template compilers when running qunit
    - Remove unsafe-eval in development CSP
    - Include unsafe-eval only for qunit routes in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Discourse::Application.configure do
   config.active_record.migration_error = :page_load
   config.watchable_dirs['lib'] = [:rb]
 
-  config.handlebars.precompile = false
+  config.handlebars.precompile = true
 
   # we recommend you use mailcatcher https://github.com/sj26/mailcatcher
   config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
