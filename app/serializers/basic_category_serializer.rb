9d3d0b4771ef242943f94ab3c9e8c8934f5c680f
Robin Ward <robin.ward@gmail.com>
2020-01-03 11:46:18 -0500

    FIX: Show uncategorized description on categories page

--- app/serializers/basic_category_serializer.rb
+++ app/serializers/basic_category_serializer.rb
@@ -51,6 +51,10 @@ class BasicCategorySerializer < ApplicationSerializer
     object.uncategorized? ? I18n.t('category.uncategorized_description', locale: SiteSetting.default_locale) : object.description
   end
 
+  def description_excerpt
+    object.uncategorized? ? I18n.t('category.uncategorized_description', locale: SiteSetting.default_locale) : object.description_excerpt
+  end
+
   def can_edit
     true
   end
