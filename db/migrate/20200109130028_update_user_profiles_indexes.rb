95b62a3a14feda34bf762c4d17f897fd89993b1a
Jarek Radosz <jradosz@gmail.com>
2020-01-09 14:42:45 +0100

    DEV: Add missing indexes to user_profiles
    The columns were changed in https://github.com/discourse/discourse/commit/24347ace10ad54f7f3bbc687bc7bc37734f558e4#diff-baa5914c0c7cddf3c8b5cd9139e0d091

---
+++ db/migrate/20200109130028_update_user_profiles_indexes.rb
@@ -0,0 +1,9 @@
+class UpdateUserProfilesIndexes < ActiveRecord::Migration[6.0]
+  def change
+    remove_index :user_profiles, :card_background
+    add_index :user_profiles, :card_background_upload_id
+
+    remove_index :user_profiles, :profile_background
+    add_index :user_profiles, :profile_background_upload_id
+  end
+end
