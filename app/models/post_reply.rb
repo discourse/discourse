ab07b945c28682ce58c5149003d65e6f3fd26dff
Gerhard Schlager <mail@gerhard-schlager.at>
2020-01-17 17:24:49 +0100

    Merge pull request #8736 from gschlager/rename_reply_id_column
    REFACTOR: Rename `post_replies.reply_id` column to `post_replies.reply_post_id`

--- app/models/post_reply.rb
+++ app/models/post_reply.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class PostReply < ActiveRecord::Base
-  belongs_to :post
-  belongs_to :reply, class_name: 'Post'
+  self.ignored_columns = %w{
+    reply_id
+  }
 
-  validates_uniqueness_of :reply_id, scope: :post_id
+  belongs_to :post
+  belongs_to :reply, foreign_key: :reply_post_id, class_name: 'Post'
+
+  validates_uniqueness_of :reply_post_id, scope: :post_id
   validate :ensure_same_topic
 
   private
@@ -23,13 +27,13 @@ end
 #
 # Table name: post_replies
 #
-#  post_id    :integer
-#  reply_id   :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  post_id       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  reply_post_id :integer
 #
 # Indexes
 #
-#  index_post_replies_on_post_id_and_reply_id  (post_id,reply_id) UNIQUE
-#  index_post_replies_on_reply_id              (reply_id)
+#  index_post_replies_on_post_id_and_reply_post_id  (post_id,reply_post_id) UNIQUE
+#  index_post_replies_on_reply_post_id              (reply_post_id)
 #
