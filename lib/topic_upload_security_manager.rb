861c742b48c2cc19b824dbf9ae232aabe034ff10
Martin Brennan <martin@discourse.org>
2020-01-17 15:53:53 +1000

    Add logging to topic upload security manager

--- lib/topic_upload_security_manager.rb
+++ lib/topic_upload_security_manager.rb
@@ -6,16 +6,21 @@ class TopicUploadSecurityManager
   end
 
   def run
+    Rails.logger.debug("Updating upload security in topic #{@topic.id}")
     posts_owning_uploads.each do |post|
-      post.topic = @topic
+      Post.transaction do
+        Rails.logger.debug("Updating upload security in topic #{@topic.id} - post #{post.id}")
+        post.topic = @topic
 
-      secure_status_did_change = post.owned_uploads_via_access_control.any? do |upload|
-        # we have already got the post preloaded so we may as well
-        # attach it here to avoid another load in UploadSecurity
-        upload.access_control_post = post
-        upload.update_secure_status
+        secure_status_did_change = post.owned_uploads_via_access_control.any? do |upload|
+          # we have already got the post preloaded so we may as well
+          # attach it here to avoid another load in UploadSecurity
+          upload.access_control_post = post
+          upload.update_secure_status
+        end
+        post.rebake! if secure_status_did_change
+        Rails.logger.debug("Security updated & rebake complete in topic #{@topic.id} - post #{post.id}")
       end
-      post.rebake! if secure_status_did_change
     end
 
     return if !SiteSetting.secure_media
@@ -34,28 +39,32 @@ class TopicUploadSecurityManager
     #  -> a public topic is converted to a PM
     #  -> a topic is moved from a private to a public category
     posts_with_unowned_uploads.each do |post|
-      post.topic = @topic
+      Post.transaction do
+        Rails.logger.debug("Setting upload access control posts in topic #{@topic.id} - post #{post.id}")
+        post.topic = @topic
 
-      secure_status_did_change = post.uploads.any? do |upload|
-        first_post_upload_appeared_in = upload.post_uploads.first.post
-        if first_post_upload_appeared_in == post
-          upload.update(access_control_post: post)
-          upload.update_secure_status
-        else
-          false
+        secure_status_did_change = post.uploads.any? do |upload|
+          first_post_upload_appeared_in = upload.post_uploads.first.post
+          if first_post_upload_appeared_in == post
+            upload.update(access_control_post: post)
+            upload.update_secure_status
+          else
+            false
+          end
         end
-      end
 
-      post.rebake! if secure_status_did_change
+        post.rebake! if secure_status_did_change
+        Rails.logger.debug("Completed changing access control posts #{secure_status_did_change ? 'and rebaking' : ''} in topic #{@topic.id} - post #{post.id}")
+      end
     end
+
+    Rails.logger.debug("Completed updating upload security in topic #{@topic.id}!")
   end
 
   private
 
   def posts_owning_uploads
-    Post.where(
-      topic_id: @topic.id
-    ).joins('INNER JOIN uploads ON access_control_post_id = posts.id')
+    Post.where(topic_id: @topic.id).joins('INNER JOIN uploads ON access_control_post_id = posts.id')
   end
 
   def posts_with_unowned_uploads
