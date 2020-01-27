11396131a06c0b216a20fefafa29ac10db47a8b3
Jarek Radosz <jradosz@gmail.com>
2020-01-27 03:31:11 +0100

    FIX: Improve downsize_uploads (#8409)
    With this change the script:
    * Actually removes original large-sized images
    * Doesn't save processed files if their size has increased
    * Prevents inconsistent state

--- app/jobs/regular/process_post.rb
+++ app/jobs/regular/process_post.rb
@@ -30,7 +30,7 @@ module Jobs
       if cooked != (recooked || orig_cooked)
 
         if orig_cooked.present? && cooked.blank?
-          # TODO suicide if needed, let's gather a few here first
+          # TODO stop/restart the worker if needed, let's gather a few here first
           Rails.logger.warn("Cooked post processor in FATAL state, bypassing. You need to urgently restart sidekiq\norig: #{orig_cooked}\nrecooked: #{recooked}\ncooked: #{cooked}\npost id: #{post.id}")
         else
           post.update_column(:cooked, cp.html)
