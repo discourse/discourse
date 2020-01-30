e185f0acc908ee3fcd790365072b553f1878e927
Arpit Jalan <arpit@techapj.com>
2020-01-30 17:32:57 +0530

    DEV: migration to remove 'bounce_score_threshold_deactivate' setting record

---
+++ db/migrate/20200130115859_remove_bounce_score_threshold_deactivate_site_setting.rb
@@ -0,0 +1,9 @@
+class RemoveBounceScoreThresholdDeactivateSiteSetting < ActiveRecord::Migration[6.0]
+  def up
+    execute "DELETE FROM site_settings WHERE name = 'bounce_score_threshold_deactivate'"
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
