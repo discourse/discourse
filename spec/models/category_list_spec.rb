25c742f1b39b51d587d5ffedb615e48c3517cf6e
David Taylor <david@taylorhq.com>
2020-02-03 18:36:33 +0000

    Show muted categories on the category page by default
    This reverts commit ed9c21e42c3e34955013fe34ba8443841bafbedd.

--- spec/models/category_list_spec.rb
+++ spec/models/category_list_spec.rb
@@ -53,16 +53,6 @@ describe CategoryList do
       expect(CategoryList.new(Guardian.new(nil), include_topics: true).categories.find { |x| x.name == private_cat.name }).to eq(nil)
     end
 
-    it "properly hide muted categories" do
-      cat_muted = Fabricate(:category_with_definition)
-      CategoryUser.create!(user_id: user.id,
-                           category_id: cat_muted.id,
-                           notification_level: CategoryUser.notification_levels[:muted])
-
-      # uncategorized + cat_muted for admin
-      expect(CategoryList.new(Guardian.new admin).categories.count).to eq(2)
-      expect(CategoryList.new(Guardian.new user).categories.count).to eq(1)
-    end
   end
 
   context "when mute_all_categories_by_default enabled" do
