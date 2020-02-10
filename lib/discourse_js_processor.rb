471826fbeed0042e61c20993f0cc7d61d1cba8a0
Robin Ward <robin.ward@gmail.com>
2020-02-10 15:05:03 -0500

    DEV: Enable babel transpilation based on directory
    If it's in `app/assets/javascripts/dicourse` it will be transpiled
    even without the `.es6` extension.

--- lib/discourse_js_processor.rb
+++ lib/discourse_js_processor.rb
@@ -1,22 +1,40 @@
 # frozen_string_literal: true
 class DiscourseJsProcessor
-  def self.call(input)
-    { data: should_transpile?(input[:filename]) ? transpile(input) : input[:data] }
-  end
 
-  def self.transpile(input)
+  def self.call(input)
+
     root_path = input[:load_path] || ''
     logical_path = (input[:filename] || '').sub(root_path, '').gsub(/\.(js|es6).*$/, '').sub(/^\//, '')
-    source = input[:data]
+    data = input[:data]
 
-    template = Tilt::ES6ModuleTranspilerTemplate.new {}
-    template.skip_module = true if skip_module?(input[:data])
-    template.module_transpile(source, root_path, logical_path)
+    if should_transpile?(input)
+      data = transpile(data, root_path, logical_path)
+    end
+
+    # add sourceURL until we can do proper source maps
+    unless Rails.env.production?
+      data = "eval(#{data.inspect} + \"\\n//# sourceURL=#{logical_path}\");\n"
+    end
+
+    { data: data }
   end
 
-  def self.should_transpile?(filename)
-    filename ||= ''
-    filename.end_with?(".es6") || filename.end_with?(".es6.erb")
+  def self.transpile(data, root_path, logical_path)
+    template = Tilt::ES6ModuleTranspilerTemplate.new {}
+    template.skip_module = true if skip_module?(data)
+    template.module_transpile(data, root_path, logical_path)
+  end
+
+  def self.should_transpile?(input)
+    filename = input[:filename] || ''
+    # es6 is always transpiled
+    return true if filename.end_with?(".es6") || filename.end_with?(".es6.erb")
+
+    # For .js check the path...
+    return false unless filename.end_with?(".js") || filename.end_with?(".js.erb")
+
+    relative_path = filename.sub(Rails.root.to_s, '').sub(/^\/*/, '')
+    relative_path.start_with?("app/assets/javascripts/discourse/")
   end
 
   def self.skip_module?(data)
