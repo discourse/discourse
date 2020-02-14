329a4691ae756d316b165183311992df25029b63
Robin Ward <robin.ward@gmail.com>
2020-02-14 11:06:12 -0500

    FIX: Confirm `draft_key` is present on GET
    Also adds a test for the `show` action which did not exist

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -6,6 +6,8 @@ class DraftController < ApplicationController
   skip_before_action :check_xhr, :preload_json
 
   def show
+    raise Discourse::NotFound.new if params[:draft_key].blank?
+
     seq = params[:sequence] || DraftSequence.current(current_user, params[:draft_key])
     render json: { draft: Draft.get(current_user, params[:draft_key], seq), draft_sequence: seq }
   end
