c954d083df8985daf3d73870b4ad07de0315fe48
Robin Ward <robin.ward@gmail.com>
2020-02-19 10:18:05 -0500

    Link website when reviewing users

--- spec/services/group_mentions_updater_spec.rb
+++ spec/services/group_mentions_updater_spec.rb
@@ -28,7 +28,6 @@ RSpec.describe GroupMentionsUpdater do
         group.update!(name: new_group_name)
         post.reload
 
-        puts post.raw
         expect(post.raw_mentions).to eq([new_group_name])
         expect(post.raw).to eq(expected_raw)
 
