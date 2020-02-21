1cbe606be6903aad6721b4d4a8867921da668328
Dan Ungureanu <dan@ungureanu.me>
2020-02-21 19:11:50 +0200

    FEATURE: Publish a message when reviewable claimer changes (#9019)
    This commit ensures that all users are kept in sync and no user can claim
    a topic that has been claimed already.

--- app/controllers/reviewable_claimed_topics_controller.rb
+++ app/controllers/reviewable_claimed_topics_controller.rb
@@ -6,13 +6,18 @@ class ReviewableClaimedTopicsController < ApplicationController
   def create
     topic = Topic.find_by(id: params[:reviewable_claimed_topic][:topic_id])
     guardian.ensure_can_claim_reviewable_topic!(topic)
-    ReviewableClaimedTopic.create(user_id: current_user.id, topic_id: topic.id)
+
+    begin
+      ReviewableClaimedTopic.create!(user_id: current_user.id, topic_id: topic.id)
+    rescue ActiveRecord::RecordInvalid
+      return render_json_error(I18n.t('reviewables.conflict'), status: 409)
+    end
+
     topic.reviewables.find_each do |reviewable|
       reviewable.log_history(:claimed, current_user)
     end
-    render json: success_json
-  rescue ActiveRecord::RecordNotUnique
-    # This is just in case the validation fails under concurrency
+
+    notify_users(topic, current_user)
     render json: success_json
   end
 
@@ -26,6 +31,26 @@ class ReviewableClaimedTopicsController < ApplicationController
       reviewable.log_history(:unclaimed, current_user)
     end
 
+    notify_users(topic, nil)
     render json: success_json
+  end
+
+  private
+
+  def notify_users(topic, claimed_by)
+    user_ids = User.staff.pluck(:id)
+
+    if SiteSetting.enable_category_group_review? && group_id = topic.category&.reviewable_by_group_id.presence
+      user_ids.concat(GroupUser.where(group_id: group_id).pluck(:user_id))
+      user_ids.uniq!
+    end
+
+    if claimed_by.present?
+      data = { topic_id: topic.id, user: BasicUserSerializer.new(claimed_by, root: false).as_json }
+    else
+      data = { topic_id: topic.id }
+    end
+
+    MessageBus.publish("/reviewable_claimed", data, user_ids: user_ids)
   end
 end
