discourse/discourse

commit 58f16f2e2b5eb3903f6fe85dc2c8b454872fd39e

FIX: Make FooterNav work with PWAs on iPadOS

app/assets/javascripts/discourse/lib/utilities.js.es6
@@ -367,9 +367,10 @@ export function areCookiesEnabled() {
 }
 
 export function isiOSPWA() {
+  const caps = Discourse.__container__.lookup("capabilities:main");
   return (
     window.matchMedia("(display-mode: standalone)").matches &&
-    navigator.userAgent.match(/(iPad|iPhone|iPod)/g)
+    caps.isIOS
   );
 }
 

commit 1243d2af422588178b770198227f9fd9790dd80f

DEV: Fix lint error introduced in 58f16f2

app/assets/javascripts/discourse/lib/utilities.js.es6
@@ -368,10 +368,7 @@ export function areCookiesEnabled() {
 
 export function isiOSPWA() {
   const caps = Discourse.__container__.lookup("capabilities:main");
-  return (
-    window.matchMedia("(display-mode: standalone)").matches &&
-    caps.isIOS
-  );
+  return window.matchMedia("(display-mode: standalone)").matches && caps.isIOS;
 }
 
 export function isAppWebview() {