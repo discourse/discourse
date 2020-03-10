ab6800a2cf5c198c2b9c07582ac50a91fb2c8a58
romanrizzi <rizziromanalejandro@gmail.com>
2020-03-10 15:00:12 -0300

    Revert "DEV: enforces ember-template-lint: no-triple-curlies (#9150)"
    This reverts commit d436b600fba4cea846a5e96c235c507441965e05.
    
    Triple curlies are still necessary for some raw templates.

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     "no-unnecessary-concat": true,
     "no-unused-block-params": true,
     "no-unbound": true,
-    "simple-unless": true,
-    "no-triple-curlies": true
+    "simple-unless": true
   }
 };
--- app/assets/javascripts/discourse-common/helpers/html-safe.js.es6
+++
@@ -1,6 +0,0 @@
-import { registerUnbound } from "discourse-common/lib/helpers";
-import { htmlSafe } from "@ember/template";
-
-registerUnbound("html-safe", function(string) {
-  return htmlSafe(string);
-});
