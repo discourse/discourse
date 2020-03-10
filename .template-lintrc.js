4c2a099220f7800bf3506e29a23ac849a97c9ec3
jjaffeux <j.jaffeux@gmail.com>
2020-03-10 11:55:58 +0100

    activate no-triple-curlies

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     "no-unnecessary-concat": true,
     "no-unused-block-params": true,
     "no-unbound": true,
-    "simple-unless": true
-    // "no-triple-curlies": true
+    "simple-unless": true,
+    "no-triple-curlies": true
   }
 };
