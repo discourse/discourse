2ea15c6568c741247e9eddb5871adf78e2654076
Robin Ward <robin.ward@gmail.com>
2020-03-11 09:43:55 -0400

    Support for transpiling `.js` files (#9160)
    * Remove some `.es6` from comments where it does not matter
    
    * Use a post processor for transpilation
    
    This will allow us to eventually use the directory structure to
    transpile rather than the extension.
    
    * FIX: Some errors and clean up in confirm-new-email
    
    It would throw an error if the webauthn element wasn't present.
    Also I changed things so that no-module is not explicitly
    referenced.
    
    * Remove `no-module`
    
    Instead we allow a magic comment: `// discourse-skip-module` to prevent
    the asset pipeline from creating a module.
    
    * DEV: Enable babel transpilation based on directory
    
    If it's in `app/assets/javascripts/dicourse` it will be transpiled
    even without the `.es6` extension.
    
    * REFACTOR: Remove Tilt/ES6ModuleTranspiler

--- lib/discourse_js_processor.rb
+++ lib/discourse_js_processor.rb
@@ -1,28 +1,54 @@
 # frozen_string_literal: true
-
 require 'execjs'
 require 'mini_racer'
 
-module Tilt
+class DiscourseJsProcessor
 
-  class ES6ModuleTranspilerTemplate < Tilt::Template
-    self.default_mime_type = 'application/javascript'
+  def self.call(input)
+    root_path = input[:load_path] || ''
+    logical_path = (input[:filename] || '').sub(root_path, '').gsub(/\.(js|es6).*$/, '').sub(/^\//, '')
+    data = input[:data]
 
+    if should_transpile?(input[:filename])
+      data = transpile(data, root_path, logical_path)
+    end
+
+    # add sourceURL until we can do proper source maps
+    unless Rails.env.production?
+      data = "eval(#{data.inspect} + \"\\n//# sourceURL=#{logical_path}\");\n"
+    end
+
+    { data: data }
+  end
+
+  def self.transpile(data, root_path, logical_path)
+    transpiler = Transpiler.new(skip_module: skip_module?(data))
+    transpiler.perform(data, root_path, logical_path)
+  end
+
+  def self.should_transpile?(filename)
+    filename ||= ''
+
+    # es6 is always transpiled
+    return true if filename.end_with?(".es6") || filename.end_with?(".es6.erb")
+
+    # For .js check the path...
+    return false unless filename.end_with?(".js") || filename.end_with?(".js.erb")
+
+    relative_path = filename.sub(Rails.root.to_s, '').sub(/^\/*/, '')
+    relative_path.start_with?("app/assets/javascripts/discourse/")
+  end
+
+  def self.skip_module?(data)
+    !!(data.present? && data =~ /^\/\/ discourse-skip-module$/)
+  end
+
+  class Transpiler
     @mutex = Mutex.new
     @ctx_init = Mutex.new
 
-    def self.call(input)
-      filename = input[:filename]
-      source = input[:data]
-      context = input[:environment].context_class.new(input)
-
-      result = new(filename) { source }.render(context)
-      context.metadata.merge(data: result)
-    end
-
-    def prepare
-      # intentionally left empty
-      # Tilt requires this method to be defined
+    def self.mutex
+      @mutex
     end
 
     def self.create_new_context
@@ -66,33 +92,13 @@ JS
       @ctx
     end
 
-    class JavaScriptError < StandardError
-      attr_accessor :message, :backtrace
-
-      def initialize(message, backtrace)
-        @message = message
-        @backtrace = backtrace
-      end
-
+    def initialize(skip_module: false)
+      @skip_module = skip_module
     end
 
-    def self.protect
-      @mutex.synchronize do
-        yield
-      end
-    end
-
-    def babel_transpile(source)
+    def perform(source, root_path = nil, logical_path = nil)
       klass = self.class
-      klass.protect do
-        klass.v8.eval("console.prefix = 'BABEL: babel-eval: ';")
-        @output = klass.v8.eval(babel_source(source))
-      end
-    end
-
-    def module_transpile(source, root_path, logical_path)
-      klass = self.class
-      klass.protect do
+      klass.mutex.synchronize do
         klass.v8.eval("console.prefix = 'BABEL: babel-eval: ';")
         transpiled = babel_source(
           source,
@@ -103,42 +109,17 @@ JS
       end
     end
 
-    def evaluate(scope, locals, &block)
-      return @output if @output
-
-      klass = self.class
-      klass.protect do
-        klass.v8.eval("console.prefix = 'BABEL: #{scope.logical_path}: ';")
-
-        source = babel_source(
-          data,
-          module_name: module_name(scope.root_path, scope.logical_path),
-          filename: scope.logical_path
-        )
-
-        @output = klass.v8.eval(source)
-      end
-
-      @output
-    end
-
     def babel_source(source, opts = nil)
       opts ||= {}
 
       js_source = ::JSON.generate(source, quirks_mode: true)
 
-      if opts[:module_name] && transpile_into_module?
+      if opts[:module_name] && !@skip_module
         filename = opts[:filename] || 'unknown'
         "Babel.transform(#{js_source}, { moduleId: '#{opts[:module_name]}', filename: '#{filename}', ast: false, presets: ['es2015'], plugins: [['transform-es2015-modules-amd', {noInterop: true}], 'transform-decorators-legacy', exports.WidgetHbsCompiler] }).code"
       else
         "Babel.transform(#{js_source}, { ast: false, plugins: ['check-es2015-constants', 'transform-es2015-arrow-functions', 'transform-es2015-block-scoped-functions', 'transform-es2015-block-scoping', 'transform-es2015-classes', 'transform-es2015-computed-properties', 'transform-es2015-destructuring', 'transform-es2015-duplicate-keys', 'transform-es2015-for-of', 'transform-es2015-function-name', 'transform-es2015-literals', 'transform-es2015-object-super', 'transform-es2015-parameters', 'transform-es2015-shorthand-properties', 'transform-es2015-spread', 'transform-es2015-sticky-regex', 'transform-es2015-template-literals', 'transform-es2015-typeof-symbol', 'transform-es2015-unicode-regex', 'transform-regenerator', 'transform-decorators-legacy', exports.WidgetHbsCompiler] }).code"
       end
-    end
-
-    private
-
-    def transpile_into_module?
-      file.nil? || file.exclude?('.no-module')
     end
 
     def module_name(root_path, logical_path)
@@ -153,26 +134,8 @@ JS
         path = "discourse/plugins/#{plugin.name}/#{logical_path.sub(/javascripts\//, '')}" if plugin
       end
 
-      path ||= logical_path
-      if ES6ModuleTranspiler.transform
-        path = ES6ModuleTranspiler.transform.call(path)
-      end
-
-      path
+      path || logical_path
     end
 
-    def compiler_method
-      type = {
-        amd: 'AMD',
-        cjs: 'CJS',
-        globals: 'Globals'
-      }[ES6ModuleTranspiler.compile_to.to_sym]
-
-      "to#{type}"
-    end
-
-    def compiler_options
-      ::JSON.generate(ES6ModuleTranspiler.compiler_options, quirks_mode: true)
-    end
   end
 end
