b9aaa9718df45fc41aafccc3f7dc47ac7e3126a3
Roman Rizzi <rizziromanalejandro@gmail.com>
2020-03-11 17:05:44 -0300

    FIX: When must_approve_users is enabled, we don't want to send suspect users to the review queue. Only non-approved users should be sent. Provide a migration to auto-approve every problematic review item (#9179)

---
+++ db/migrate/20200311135425_clear_approved_users_from_the_review_queue.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+class ClearApprovedUsersFromTheReviewQueue < ActiveRecord::Migration[6.0]
+  def up
+    reviewables = DB.query_single <<~SQL
+      UPDATE reviewables r
+      SET status = #{Reviewable.statuses[:approved]}
+      FROM users u
+      WHERE u.approved = true AND r.type = 'ReviewableUser' AND r.status = #{Reviewable.statuses[:pending]}
+      RETURNING r.id
+    SQL
+
+    system_user_id = Discourse::SYSTEM_USER_ID
+    scores = reviewables.map do |id|
+      "(#{id}, 1, #{Reviewable.statuses[:approved]}, #{system_user_id}, NOW(), NOW())"
+    end
+
+    if scores.present?
+      DB.exec <<~SQL
+        INSERT INTO reviewable_histories (
+          reviewable_id,
+          reviewable_history_type,
+          status,
+          created_by_id,
+          created_at,
+          updated_at
+        )
+        VALUES #{scores.join(',') << ';'}
+      SQL
+    end
+  end
+
+  def down
+  end
+end
