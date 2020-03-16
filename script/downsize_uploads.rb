3e1db17117ec5b0405b1d6dd6e31bdeb550585c8
Jarek Radosz <jradosz@gmail.com>
2020-03-16 03:04:57 +0100

    DEV: Fix the onebox in cooked text heuristic

--- script/downsize_uploads.rb
+++ script/downsize_uploads.rb
@@ -87,7 +87,7 @@ def downsize_upload(upload, path)
 
     if post.raw_changed?
       puts "Updating post #{post.id}" if ENV["VERBOSE"]
-    elsif post.cooked.include?(UrlHelper.cook_url(original_upload.url))
+    elsif post.cooked.include?(original_upload.sha1)
       if post.raw.include?("#{Discourse.base_url.sub(/^https?:\/\//i, '')}/t/")
         puts "Updating a topic onebox in post #{post.id}" if ENV["VERBOSE"]
       else
