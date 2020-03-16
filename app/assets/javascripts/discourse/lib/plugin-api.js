56b26b038d0ce5d111bf3bee87628ff415af9d63
Faizaan Gagan <fzngagan@gmail.com>
2020-03-16 17:42:04 +0530

    DEV: fix "addNavigationBarItem" documentation (#9207)
    * the filter should return the boolean result
    * custom href should return the relative url value

--- app/assets/javascripts/discourse/lib/plugin-api.js
+++ app/assets/javascripts/discourse/lib/plugin-api.js
@@ -755,8 +755,8 @@ class PluginApi {
    *   displayName: "bugs"
    *   href: "/c/bugs",
    *   init: (navItem, category) => { if (category) { navItem.set("category", category)  } }
-   *   customFilter: (category, args, router) => { category && category.name !== 'bug' }
-   *   customHref: (category, args, router) => {  if (category && category.name) === 'not-a-bug') "/a-feature"; },
+   *   customFilter: (category, args, router) => { return category && category.name !== 'bug' }
+   *   customHref: (category, args, router) => {  if (category && category.name) === 'not-a-bug') return "/a-feature"; },
    *   before: "top",
    *   forceActive(category, args, router) => router.currentURL === "/a/b/c/d";
    * })
