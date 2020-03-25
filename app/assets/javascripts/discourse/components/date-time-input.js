440d8a070397daf959c39a34fa3f5dda92e14eb2
jjaffeux <j.jaffeux@gmail.com>
2020-03-25 18:03:33 +0100

    DEV: allows time-input to be clearable

--- app/assets/javascripts/discourse/components/date-time-input.js
+++ app/assets/javascripts/discourse/components/date-time-input.js
@@ -5,6 +5,7 @@ export default Component.extend({
   classNames: ["d-date-time-input"],
   date: null,
   showTime: true,
+  clearable: false,
 
   _hours: computed("date", function() {
     return this.date && this.showTime ? new Date(this.date).getHours() : null;
@@ -15,6 +16,10 @@ export default Component.extend({
   }),
 
   actions: {
+    onClear() {
+      this.onChange(null);
+    },
+
     onChangeTime(time) {
       if (this.onChange) {
         const date = new Date(this.date);
