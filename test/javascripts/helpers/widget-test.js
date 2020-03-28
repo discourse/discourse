515979a5a3a96d4d8bede6719399eed79f81daaf
jjaffeux <j.jaffeux@gmail.com>
2020-03-28 14:14:04 +0100

    DEV: Allows moduleForWidget to use before/afterEach options

--- test/javascripts/helpers/widget-test.js
+++ test/javascripts/helpers/widget-test.js
@@ -1,7 +1,15 @@
 import componentTest from "helpers/component-test";
+const { getProperties } = Ember;
 
-export function moduleForWidget(name) {
-  moduleForComponent(name, `widget:${name}`, { integration: true });
+export function moduleForWidget(name, options = {}) {
+  moduleForComponent(
+    name,
+    `widget:${name}`,
+    Object.assign(
+      { integration: true },
+      getProperties(options, ["beforeEach", "afterEach"])
+    )
+  );
 }
 
 export function widgetTest(name, opts) {
