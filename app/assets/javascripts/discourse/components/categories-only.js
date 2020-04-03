b92838bcb75058f0763f25f9e276a9b932b489fc
Jordan Vidrine <jordan@jordanvidrine.com>
2020-04-03 11:23:52 -0500

    UX: removes color on categories if no style chosen

--- app/assets/javascripts/discourse/components/categories-only.js
+++ app/assets/javascripts/discourse/components/categories-only.js
@@ -1,4 +1,11 @@
 import Component from "@ember/component";
+import computed from 'ember-addons/ember-computed-decorators';
+
 export default Component.extend({
-  tagName: ""
+  tagName: "",
+
+  @computed
+  noCategoryStyle() {
+    return this.siteSettings.category_style === 'none';
+  }
 });
