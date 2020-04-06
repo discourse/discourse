6364143adf1ee054bc363cdfd5bedbd5fa6d2e12
David Taylor <david@taylorhq.com>
2020-04-06 17:24:59 +0100

    DEV: Allow using .js extension for javascript modules in themes (#9358)

--- app/models/theme_field.rb
+++ app/models/theme_field.rb
@@ -134,7 +134,7 @@ class ThemeField < ActiveRecord::Base
     filename, extension = name.split(".", 2)
     begin
       case extension
-      when "js.es6"
+      when "js.es6", "js"
         js_compiler.append_module(content, filename)
       when "hbs"
         js_compiler.append_ember_template(filename.sub("discourse/templates/", ""), content)
