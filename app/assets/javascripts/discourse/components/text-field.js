5f18d5b35bfef2c4a605e907d79d929e7464fc13
Robin Ward <robin.ward@gmail.com>
2020-04-06 13:42:34 -0400

    DEV: Support for `onChange` on `{{text-field}}`
    This will automatically be debounced and only fired when the value
    changes.
    
    There is also `onChangeImmediate` which is not debounced in case you
    need that, but in almost all cases when observing text in an element you
    should debounce.

--- app/assets/javascripts/discourse/components/text-field.js
+++ app/assets/javascripts/discourse/components/text-field.js
@@ -1,8 +1,13 @@
 import { TextField } from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
 import { siteDir, isRTL, isLTR } from "discourse/lib/text-direction";
+import { next, debounce } from "@ember/runloop";
+
+const DEBOUNCE_MS = 500;
 
 export default TextField.extend({
+  _prevValue: null,
+
   attributeBindings: [
     "autocorrect",
     "autocapitalize",
@@ -10,6 +15,27 @@ export default TextField.extend({
     "maxLength",
     "dir"
   ],
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this._prevValue = this.value;
+  },
+
+  didUpdateAttrs() {
+    this._super(...arguments);
+    if (this._prevValue !== this.value) {
+      if (this.onChangeImmediate) {
+        next(() => this.onChangeImmediate(this.value));
+      }
+      if (this.onChange) {
+        debounce(this, this._debouncedChange, DEBOUNCE_MS);
+      }
+    }
+  },
+
+  _debouncedChange() {
+    next(() => this.onChange(this.value));
+  },
 
   @discourseComputed
   dir() {
