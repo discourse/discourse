0d16050b6484ab11a9bfb94789d640f776f016f1
Jarek Radosz <jradosz@gmail.com>
2020-04-08 16:28:23 +0200

    FIX: Quoting posts (#9378)
    Fixes to the quote feature. Most important changes listed below:
    
    * FIX: Correctly attribute quotes when using Reply button
    * FIX: Correctly attribute quotes when using replyAsNewTopic
    * FIX: Allow quoting a quote
    * FIX: Correctly mark quotes as "full"
    * FIX: Don't try to create a quote if it's empty
    * DEV: Remove an obsolete method `loadQuote`
      It isn't used in core anymore, the only use in core has been removed over 4 years ago in 3251bcb. It's not used in any plugins in all-the-plugins and all references to it on GitHub are from outdated forks (https://github.com/search?q=%22Post.loadQuote%22&type=Code)

--- app/assets/javascripts/discourse/controllers/composer.js
+++ app/assets/javascripts/discourse/controllers/composer.js
@@ -5,7 +5,7 @@ import { inject as service } from "@ember/service";
 import { inject } from "@ember/controller";
 import Controller from "@ember/controller";
 import DiscourseURL from "discourse/lib/url";
-import Quote from "discourse/lib/quote";
+import { buildQuote } from "discourse/lib/quote";
 import Draft from "discourse/models/draft";
 import Composer from "discourse/models/composer";
 import discourseComputed, {
@@ -501,15 +501,14 @@ export default Controller.extend({
 
       if (postId) {
         this.set("model.loading", true);
-        const composer = this;
 
         return this.store.find("post", postId).then(post => {
-          const quote = Quote.build(post, post.raw, {
-            raw: true,
+          const quote = buildQuote(post, post.raw, {
             full: true
           });
+
           toolbarEvent.addText(quote);
-          composer.set("model.loading", false);
+          this.set("model.loading", false);
         });
       }
     },
--- app/assets/javascripts/discourse/lib/quote.js
+++ app/assets/javascripts/discourse/lib/quote.js
@@ -1,43 +1,18 @@
-export default {
-  REGEXP: /\[quote=([^\]]*)\]((?:[\s\S](?!\[quote=[^\]]*\]))*?)\[\/quote\]/im,
+export const QUOTE_REGEXP = /\[quote=([^\]]*)\]((?:[\s\S](?!\[quote=[^\]]*\]))*?)\[\/quote\]/im;
 
-  // Build the BBCode quote around the selected text
-  build(post, contents, opts) {
-    if (!post) {
-      return "";
-    }
-
-    if (!contents) contents = "";
-    if (!opts) opts = {};
-
-    const sansQuotes = contents.replace(this.REGEXP, "").trim();
-    if (sansQuotes.length === 0) {
-      return "";
-    }
-
-    // Strip the HTML from cooked
-    const stripped = $("<div/>")
-      .html(post.get("cooked"))
-      .text();
-
-    // Let's remove any non-word characters as a kind of hash.
-    // Yes it's not accurate but it should work almost every time we need it to.
-    // It would be unlikely that the user would quote another post that matches in exactly this way.
-    const sameContent =
-      stripped.replace(/\W/g, "") === contents.replace(/\W/g, "");
-
-    const params = [
-      opts.username || post.username,
-      `post:${opts.post || post.post_number}`,
-      `topic:${opts.topic || post.topic_id}`
-    ];
-
-    opts = opts || {};
-
-    if (opts["full"] || sameContent) params.push("full:true");
-
-    return `[quote="${params.join(", ")}"]\n${
-      opts["raw"] ? contents : sansQuotes
-    }\n[/quote]\n\n`;
+// Build the BBCode quote around the selected text
+export function buildQuote(post, contents, opts = {}) {
+  if (!post || !contents) {
+    return "";
   }
-};
+
+  const params = [
+    opts.username || post.username,
+    `post:${opts.post || post.post_number}`,
+    `topic:${opts.topic || post.topic_id}`
+  ];
+
+  if (opts.full) params.push("full:true");
+
+  return `[quote="${params.join(", ")}"]\n${contents.trim()}\n[/quote]\n\n`;
+}
