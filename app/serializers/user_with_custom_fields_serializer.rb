0b115eb6e3fb74235a74a4a9f458f8e56db2846d
Jeff Wong <awole20@gmail.com>
2020-04-09 10:25:19 -0700

    Rename UserCustomFieldsSerializer to UserWithCustomFieldsSerializer

--- app/serializers/user_with_custom_fields_serializer.rb
+++ app/serializers/user_with_custom_fields_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # A basic user serializer, with custom fields
-class UserCustomFieldsSerializer < BasicUserSerializer
+class UserWithCustomFieldsSerializer < BasicUserSerializer
   attributes :custom_fields
 
   def custom_fields
