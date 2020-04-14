790193a3f662ba6274227e3b2c0ef0957d920462
Dan Ungureanu <dan@ungureanu.me>
2020-04-14 21:09:38 +0300

    FEATURE: Use include_subcategories filter in reports (part two)

--- app/models/reports/time_to_first_response.rb
+++ app/models/reports/time_to_first_response.rb
@@ -2,17 +2,17 @@
 
 Report.add_report('time_to_first_response') do |report|
   category_filter = report.filters.dig(:category)
-  report.add_filter('category', default: category_filter)
+  category_id, include_subcategories = report.add_category_filter
 
   report.icon = 'reply'
   report.higher_is_better = false
   report.data = []
 
-  Topic.time_to_first_response_per_day(report.start_date, report.end_date, category_id: category_filter).each do |r|
+  Topic.time_to_first_response_per_day(report.start_date, report.end_date, category_id: category_id, include_subcategories: include_subcategories).each do |r|
     report.data << { x: r['date'], y: r['hours'].to_f.round(2) }
   end
 
-  report.total = Topic.time_to_first_response_total(category_id: category_filter)
+  report.total = Topic.time_to_first_response_total(category_id: category_id, include_subcategories: include_subcategories)
 
-  report.prev30Days = Topic.time_to_first_response_total(start_date: report.start_date - 30.days, end_date: report.start_date, category_id: category_filter)
+  report.prev30Days = Topic.time_to_first_response_total(start_date: report.start_date - 30.days, end_date: report.start_date, category_id: category_id, include_subcategories: include_subcategories)
 end
