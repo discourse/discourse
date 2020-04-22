4d6ecdab3d31f04f5e2c17725b9078cb81570c4d
David Taylor <david@taylorhq.com>
2020-04-22 13:21:45 +0100

    Remove foreign key on image_upload_id

--- db/migrate/20200415095034_add_image_upload_id_to_posts_and_topics.rb
+++ db/migrate/20200415095034_add_image_upload_id_to_posts_and_topics.rb
@@ -2,8 +2,8 @@
 
 class AddImageUploadIdToPostsAndTopics < ActiveRecord::Migration[6.0]
   def change
-    add_reference :posts, :image_upload, foreign_key: { to_table: :uploads, on_delete: :nullify }
-    add_reference :topics, :image_upload, foreign_key: { to_table: :uploads, on_delete: :nullify }
+    add_reference :posts, :image_upload
+    add_reference :topics, :image_upload
 
     add_column :theme_modifier_sets, :topic_thumbnail_sizes, :string, array: true
   end
