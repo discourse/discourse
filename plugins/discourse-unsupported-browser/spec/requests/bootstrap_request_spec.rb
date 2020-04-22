a892032e7eeb34d959654070d961a2a833a99546
Dan Ungureanu <dan@ungureanu.me>
2020-04-22 21:50:57 +0300

    DEV: Rename discourse-internet-explorer to discourse-unsupported-browser

--- plugins/discourse-unsupported-browser/spec/requests/bootstrap_request_spec.rb
+++ plugins/discourse-unsupported-browser/spec/requests/bootstrap_request_spec.rb
@@ -7,31 +7,31 @@ describe 'Bootstrapping the Discourse App' do
 
   context "when disabled" do
     before do
-      SiteSetting.discourse_internet_explorer_enabled = false
+      SiteSetting.discourse_unsupported_browser_enabled = false
     end
 
     it "does not include the IE stylesheet or Javascript" do
       get "/categories", headers: { "HTTP_USER_AGENT" => ie_agent }
-      expect(response.body).not_to match(/discourse-internet-explorer-optional.js/)
-      expect(response.body).not_to match(/stylesheets\/discourse-internet-explorer/)
+      expect(response.body).not_to match(/discourse-unsupported-browser-optional.js/)
+      expect(response.body).not_to match(/stylesheets\/discourse-unsupported-browser/)
     end
   end
 
   context "when enabled" do
     before do
-      SiteSetting.discourse_internet_explorer_enabled = true
+      SiteSetting.discourse_unsupported_browser_enabled = true
     end
 
     it "includes the IE js and css" do
       get "/categories", headers: { "HTTP_USER_AGENT" => ie_agent }
-      expect(response.body).to match(/discourse-internet-explorer-optional.js/)
-      expect(response.body).to match(/stylesheets\/discourse-internet-explorer/)
+      expect(response.body).to match(/discourse-unsupported-browser-optional.js/)
+      expect(response.body).to match(/stylesheets\/discourse-unsupported-browser/)
     end
 
     it "doesn't include IE stuff for non-IE browsers" do
       get "/categories", headers: { "HTTP_USER_AGENT" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36" }
-      expect(response.body).not_to match(/discourse-internet-explorer-optional.js/)
-      expect(response.body).not_to match(/stylesheets\/discourse-internet-explorer/)
+      expect(response.body).not_to match(/discourse-unsupported-browser-optional.js/)
+      expect(response.body).not_to match(/stylesheets\/discourse-unsupported-browser/)
     end
   end
 end
