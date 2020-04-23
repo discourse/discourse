8b68465ecebb7b71b0916e257354d62f13668ab4
Martin Brennan <martin@discourse.org>
2020-04-23 13:27:44 +1000

    Remove check if copying upload from another post in CPP
    it was unnecessary and wasn't really adding anything, all it was
    doing was preventing thumbnails from generated which would cause
    issues if the posts were cooked out of order. the thumbnail code
    already handles cases where the thumbnail has already been generated

--- lib/cooked_post_processor.rb
+++ lib/cooked_post_processor.rb
@@ -339,15 +339,8 @@ class CookedPostProcessor
       width, height = ImageSizer.resize(width, height)
     end
 
-    # if the upload already exists and is attached to a different post,
-    # or the original_sha1 is missing meaning it was created before secure
-    # media was enabled. we want to re-thumbnail and re-optimize in this case
-    # to avoid using uploads linked to many other posts
     upload = Upload.get_from_url(src)
-
-    # do this if the upload exists and it has not been copied from another
-    # post, which is only relevant for secure uploads with an access control post
-    if upload.present? && !upload.copied_from_other_post?(@post)
+    if upload.present?
       upload.create_thumbnail!(width, height, crop: crop)
 
       each_responsive_ratio do |ratio|
