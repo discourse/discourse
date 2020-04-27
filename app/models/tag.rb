129642a0b0b181e245d4ce9f7933d0e2d4047292
Mark VanLandingham <markvanlan@gmail.com>
2020-04-27 11:33:11 -0500

    DEV: Add has_many category_tag_stats in tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -28,6 +28,7 @@ class Tag < ActiveRecord::Base
   has_many :topic_tags, dependent: :destroy
   has_many :topics, through: :topic_tags
 
+  has_many :category_tag_stats, dependent: :destroy
   has_many :category_tags, dependent: :destroy
   has_many :categories, through: :category_tags
 
