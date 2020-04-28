0d519c78fbf92ce7da6a930f3b10fbb62d21271d
Martin Brennan <mjrbrennan@gmail.com>
2020-04-29 08:45:06 +1000

    FIX: Do not save bookmark if close (X) on modal is clicked (#9541)
    * After this change the bookmark will still be saved if clicking out of the modal or pressing escape
    * To achieve this I implemented an initiatedBy parameter for modal closing from d-modal. If clicking on the cross it is initiated by close, if clicking out of the modal it is by click out.
    * These options can then be compared in controllers consuming onClose
    
    Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>

--- app/assets/javascripts/discourse/app/routes/application.js
+++ app/assets/javascripts/discourse/app/routes/application.js
@@ -155,7 +155,7 @@ const ApplicationRoute = DiscourseRoute.extend(OpenComposer, {
     },
 
     // Close the current modal, and destroy its state.
-    closeModal() {
+    closeModal(initiatedBy) {
       const route = getOwner(this).lookup("route:application");
       let modalController = route.controllerFor("modal");
       const controllerName = modalController.get("name");
@@ -178,7 +178,10 @@ const ApplicationRoute = DiscourseRoute.extend(OpenComposer, {
           `controller:${controllerName}`
         );
         if (controller && controller.onClose) {
-          controller.onClose();
+          controller.onClose({
+            initiatedByCloseButton: initiatedBy === "initiatedByCloseButton",
+            initiatedByClickOut: initiatedBy === "initiatedByClickOut"
+          });
         }
         modalController.set("name", null);
       }
