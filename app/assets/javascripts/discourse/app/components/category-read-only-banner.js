e2da0cb5654faa5217be8387d35c34e01aea6df5
Jeff Wong <awole20@gmail.com>
2020-04-29 14:05:39 -0700

    FIX: hide category banner properly when it is blank

--- app/assets/javascripts/discourse/app/components/category-read-only-banner.js
+++ app/assets/javascripts/discourse/app/components/category-read-only-banner.js
@@ -2,5 +2,5 @@ import Component from "@ember/component";
 import { and } from "@ember/object/computed";
 
 export default Component.extend({
-  shouldShow: and("category", "readOnly", "user")
+  shouldShow: and("category.read_only_banner", "readOnly", "user")
 });
