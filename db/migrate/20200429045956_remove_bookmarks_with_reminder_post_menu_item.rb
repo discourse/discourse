893bf229893fc90e39feb5eb250d329929c103e9
Martin Brennan <martin@discourse.org>
2020-04-29 15:08:02 +1000

    Fix migration

--- db/migrate/20200429045956_remove_bookmarks_with_reminder_post_menu_item.rb
+++ db/migrate/20200429045956_remove_bookmarks_with_reminder_post_menu_item.rb
@@ -3,14 +3,14 @@
 class RemoveBookmarksWithReminderPostMenuItem < ActiveRecord::Migration[6.0]
   def up
     post_menu = SiteSetting.post_menu
-    post_menu = post_menu.gsub("|bookmarkWithReminder|", "")
+    post_menu = post_menu.gsub("|bookmarkWithReminder|", "|")
     post_menu = post_menu.gsub("bookmarkWithReminder|", "")
     post_menu = post_menu.gsub("|bookmarkWithReminder", "")
 
     SiteSetting.post_menu = post_menu
 
     post_menu_hidden = SiteSetting.post_menu_hidden_items
-    post_menu_hidden = post_menu_hidden.gsub("|bookmarkWithReminder|", "")
+    post_menu_hidden = post_menu_hidden.gsub("|bookmarkWithReminder|", "|")
     post_menu_hidden = post_menu_hidden.gsub("bookmarkWithReminder|", "")
     post_menu_hidden = post_menu_hidden.gsub("|bookmarkWithReminder", "")
 
