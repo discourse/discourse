b6ca5ad4150a128604222d8ff6489ee5329e85c3
Robin Ward <robin.ward@gmail.com>
2020-04-29 16:14:35 -0400

    DEV: Add Handlebars support to Ember CLI in discourse-common

--- app/assets/javascripts/discourse-common/index.js
+++ app/assets/javascripts/discourse-common/index.js
@@ -1,5 +1,12 @@
 "use strict";
 
 module.exports = {
-  name: require("./package").name
+  name: require("./package").name,
+  options: {
+    autoImport: {
+      alias: {
+        handlebars: "handlebars/dist/cjs/handlebars.js"
+      }
+    }
+  }
 };
