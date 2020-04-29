aa9b274d3207b97e80bcf9daf2ae24fac5dd7785
Martin Brennan <martin@discourse.org>
2020-04-29 15:03:51 +1000

    Add migration to remove bookmarkWithReminder post menu item

---
+++ db/migrate/20200429045956_remove_bookmarks_with_reminder_post_menu_item.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class RemoveBookmarksWithReminderPostMenuItem < ActiveRecord::Migration[6.0]
+  def up
+    post_menu = SiteSetting.post_menu
+    post_menu = post_menu.gsub("|bookmarkWithReminder|", "")
+    post_menu = post_menu.gsub("bookmarkWithReminder|", "")
+    post_menu = post_menu.gsub("|bookmarkWithReminder", "")
+
+    SiteSetting.post_menu = post_menu
+
+    post_menu_hidden = SiteSetting.post_menu_hidden_items
+    post_menu_hidden = post_menu_hidden.gsub("|bookmarkWithReminder|", "")
+    post_menu_hidden = post_menu_hidden.gsub("bookmarkWithReminder|", "")
+    post_menu_hidden = post_menu_hidden.gsub("|bookmarkWithReminder", "")
+
+    SiteSetting.post_menu_hidden_items = post_menu_hidden
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
