dabced7048814a9d41d3f7224f38a7746f6c3a56
Arpit Jalan <arpit@techapj.com>
2020-05-06 17:20:43 +0530

    FEATURE: Facebook video onebox

---
+++ lib/onebox/engine/facebook_media_onebox.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Onebox
+  module Engine
+    class FacebookMediaOnebox
+      include Engine
+      include StandardEmbed
+
+      matches_regexp(/^https?:\/\/.*\.facebook\.com\/(\w+)\/(videos|\?).*/)
+      always_https
+
+      def to_html
+        metadata = get_twitter
+        if metadata.present? && metadata[:card] == "player" && metadata[:player].present?
+          <<-HTML
+            <iframe src="#{metadata[:player]}"
+                    width="#{metadata[:player_width]}"
+                    height="#{metadata[:player_height]}"
+                    scrolling="no"
+                    frameborder="0"
+                    allowfullscreen>
+            </iframe>
+          HTML
+        else
+          html = Onebox::Engine::WhitelistedGenericOnebox.new(@url, @timeout).to_html
+          return if Onebox::Helpers.blank?(html)
+          html
+        end
+      end
+    end
+  end
+end
