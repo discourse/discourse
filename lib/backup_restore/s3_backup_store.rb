9d555a5f983d5e035324c50fd6005f8e00c96faa
Andrew Schleifer <me@andrewschleifer.name>
2020-05-07 12:58:53 +0800

    DEV: remove no-longer-necessary MULTISITE_PREFIX
    we prevent s3_backup_bucket from being the parent or the same as
    s3_upload_bucket[^subdir], so this is no longer useful
    
    [^subdir]: spec/lib/site_settings/validations_spec.rb#validate_s3_upload_bucket
    
    currently, with s3_upload_bucket of "bucket_name" and s3_backup_bucket
    of "bucket_name/backups", multisite backups are actually ending up in
    "bucket_name/backups/backups/site_name"

--- lib/backup_restore/s3_backup_store.rb
+++ lib/backup_restore/s3_backup_store.rb
@@ -3,7 +3,6 @@
 module BackupRestore
   class S3BackupStore < BackupStore
     UPLOAD_URL_EXPIRES_AFTER_SECONDS ||= 21_600 # 6 hours
-    MULTISITE_PREFIX = "backups"
 
     def initialize(opts = {})
       s3_options = S3Helper.s3_options(SiteSetting)
@@ -100,7 +99,7 @@ module BackupRestore
 
     def s3_bucket_name_with_prefix
       if Rails.configuration.multisite
-        File.join(SiteSetting.s3_backup_bucket, MULTISITE_PREFIX, RailsMultisite::ConnectionManagement.current_db)
+        File.join(SiteSetting.s3_backup_bucket, RailsMultisite::ConnectionManagement.current_db)
       else
         SiteSetting.s3_backup_bucket
       end
