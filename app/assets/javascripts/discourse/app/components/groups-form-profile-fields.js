d4a252e1c78c0adfe712c19de126e662dfa05314
jjaffeux <j.jaffeux@gmail.com>
2020-05-07 12:11:51 +0200

    REFACTOR: removes use of undeclared variable

--- app/assets/javascripts/discourse/app/components/groups-form-profile-fields.js
+++ app/assets/javascripts/discourse/app/components/groups-form-profile-fields.js
@@ -31,25 +31,24 @@ export default Component.extend({
 
   @observes("nameInput")
   _validateName() {
-    name = this.nameInput;
-    if (name === this.get("model.name")) return;
+    if (this.nameInput === this.get("model.name")) return;
 
-    if (name === undefined) {
+    if (this.nameInput === undefined) {
       return this._failedInputValidation();
     }
 
-    if (name === "") {
+    if (this.nameInput === "") {
       this.set("uniqueNameValidation", null);
       return this._failedInputValidation(I18n.t("admin.groups.new.name.blank"));
     }
 
-    if (name.length < this.siteSettings.min_username_length) {
+    if (this.nameInput.length < this.siteSettings.min_username_length) {
       return this._failedInputValidation(
         I18n.t("admin.groups.new.name.too_short")
       );
     }
 
-    if (name.length > this.siteSettings.max_username_length) {
+    if (this.nameInput.length > this.siteSettings.max_username_length) {
       return this._failedInputValidation(
         I18n.t("admin.groups.new.name.too_long")
       );
