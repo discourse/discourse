3329e09474a0152a46b605fffc89b1db2e375c1c
Mark VanLandingham <markvanlan@gmail.com>
2020-05-13 14:50:41 -0500

    ensure existing email is digest

--- lib/email/receiver.rb
+++ lib/email/receiver.rb
@@ -191,7 +191,7 @@ module Email
           end
         end
 
-        raise ReplyToDigestError if EmailLog.find_by(message_id: @mail.in_reply_to).present?
+        raise ReplyToDigestError if EmailLog.find_by(email_type: "digest", message_id: @mail.in_reply_to).present?
         raise BadDestinationAddress
       end
     end
