8b42af0fe87921291f1f953c8ec9b16d265d6d19
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2020-05-25 21:44:15 +0200

    DEV: plugin api should have been bumped for #8825395 (#9874)

--- app/assets/javascripts/discourse/app/lib/plugin-api.js
+++ app/assets/javascripts/discourse/app/lib/plugin-api.js
@@ -56,7 +56,7 @@ import { on } from "@ember/object/evented";
 import KeyboardShortcuts from "discourse/lib/keyboard-shortcuts";
 
 // If you add any methods to the API ensure you bump up this number
-const PLUGIN_API_VERSION = "0.10.0";
+const PLUGIN_API_VERSION = "0.10.1";
 
 class PluginApi {
   constructor(version, container) {
