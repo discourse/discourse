ee82c858c5bc7b3f6e82d8b2b9ca839eb5953abc
Arpit Jalan <arpit@techapj.com>
2020-05-26 20:27:59 +0530

    FIX: use oEmbed for Instagram onebox.

--- lib/onebox/engine/instagram_onebox.rb
+++ lib/onebox/engine/instagram_onebox.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'htmlentities'
-
 module Onebox
   module Engine
     class InstagramOnebox
@@ -9,25 +7,43 @@ module Onebox
       include StandardEmbed
       include LayoutSupport
 
-      matches_regexp(/^https?:\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/?(?:.*)\/p\//)
+      matches_regexp(/^https?:\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/?(?:.*)\/p\/[a-zA-Z\d_-]+/)
       always_https
 
+      def clean_url
+        url.scan(/^https?:\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/?(?:.*)\/p\/[a-zA-Z\d_-]+/).flatten.first
+      end
+
       def data
-        og = get_opengraph
-        title = og.title.split(":")[0].strip.gsub(" on Instagram", "")
+        oembed = get_oembed
+        permalink = clean_url.gsub("/#{oembed.author_name}/", "/")
+        description = oembed.title
+        type = if description =~ /^Photos by/
+          "album"
+        elsif description =~ /^Video by/
+          "video"
+        else
+          "photo"
+        end
+        title = if type == "album"
+          "[Album] @#{oembed.author_name}"
+        else
+          "@#{oembed.author_name}"
+        end
 
-        json_data = html_doc.xpath('//script[contains(text(),"window._sharedData")]').text.to_s
-        title = "[Album] #{title}" if json_data =~ /"edge_sidecar_to_children"/
-
-        result = { link: og.url,
-                   title: Onebox::Helpers.truncate(title, 80),
-                   description: og.description(250)
+        result = { link: permalink,
+                   title: title,
+                   image: "#{permalink}/media/?size=l",
+                   description: Onebox::Helpers.truncate(description, 250)
                   }
-
-        result[:image] = og.image if !og.image.nil?
-        result[:video_link] = og.url if !og.video_secure_url.nil?
-
+        result[:video_link] = permalink if type == "video"
         result
+      end
+
+      protected
+
+      def get_oembed_url
+        oembed_url = "https://api.instagram.com/oembed/?url=#{clean_url}"
       end
     end
   end
--- spec/lib/onebox/engine/instagram_onebox_spec.rb
+++ spec/lib/onebox/engine/instagram_onebox_spec.rb
@@ -3,33 +3,34 @@
 require "spec_helper"
 
 describe Onebox::Engine::InstagramOnebox do
-  let(:link) { "https://www.instagram.com/p/BgSPalMjddb" }
+  let(:link) { "https://www.instagram.com/p/CARbvuYDm3Q/" }
+  let(:api_link) { "https://api.instagram.com/oembed/?url=https://www.instagram.com/p/CARbvuYDm3Q" }
   let(:html) { described_class.new(link).to_html }
 
   before do
-    fake(link, response("instagram"))
+    fake(api_link, response("instagram"))
   end
 
   it "includes title" do
-    expect(html).to include('<a href="https://www.instagram.com/p/BgSPalMjddb/" target="_blank" rel="noopener">National Geographic</a>')
+    expect(html).to include('<a href="https://www.instagram.com/p/CARbvuYDm3Q" target="_blank" rel="noopener">@natgeo</a>')
   end
 
   it "includes image" do
-    expect(html).to include("28751607_101336700703060_7002304208821026816_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=104&amp;_nc_ohc=NXYxExVGcLkAX8-FRp3&amp;oh=add21f207c8533dc3c254c9532b1bcca&amp;oe=5E769EDA")
+    expect(html).to include("https://www.instagram.com/p/CARbvuYDm3Q/media/?size=l")
   end
 
   it "includes description" do
-    expect(html).to include("1.2m Likes, 5,971 Comments - National Geographic (@natgeo) on Instagram")
+    expect(html).to include("Photo by Pete McBride @pedromcbride | For the first time in three decades")
   end
 
   it 'oneboxes links that include the username' do
-    link_with_profile = 'https://www.instagram.com/bennyblood24/p/Brc6FNRn9vu/'
+    link_with_profile = 'https://www.instagram.com/bennyblood24/p/CARbvuYDm3Q/'
     onebox_klass = Onebox::Matcher.new(link_with_profile).oneboxed
     expect(onebox_klass.name).to eq(described_class.name)
   end
 
   it 'oneboxes photo links' do
-    photo_link = 'https://www.instagram.com/p/Brc6FNRn9vu/'
+    photo_link = 'https://www.instagram.com/p/CARbvuYDm3Q/'
     onebox_klass = Onebox::Matcher.new(photo_link).oneboxed
     expect(onebox_klass.name).to eq(described_class.name)
   end
