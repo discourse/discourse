7cde1df021a8d920652a46eda44fafeb7a105c01
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2020-05-28 10:27:19 +1000

    FIX: improvements to the process
    * just one setting How often we should remind - SiteSettting.send_old_credential_reminder_days
    * one message sent to all admins instead of message per admin
    * better message content
      * improved wording
      * only yyyy-mm-dd when the key was created/updated without
    * disabled by default

--- app/jobs/scheduled/old_keys_reminder.rb
+++ app/jobs/scheduled/old_keys_reminder.rb
@@ -4,19 +4,20 @@ module Jobs
   class OldKeysReminder < ::Jobs::Scheduled
     every 1.month
 
+    OLD_CREDENTIALS_PERIOD = 2.years
+
     def execute(_args)
-      return if SiteSetting.notify_about_secrets_older_than == 'never'
+      return if SiteSetting.send_old_credential_reminder_days.to_i == 0
+      return if message_exists?
       return if old_site_settings_keys.blank? && old_api_keys.blank?
-      admins.each do |admin|
-        PostCreator.create!(
-          Discourse.system_user,
-          title: title,
-          raw: body,
-          archetype: Archetype.private_message,
-          target_usernames: admin.username,
-          validate: false
-        )
-      end
+      PostCreator.create!(
+        Discourse.system_user,
+        title: title,
+        raw: body,
+        archetype: Archetype.private_message,
+        target_usernames: admins.map(&:username),
+        validate: false
+      )
     end
 
     private
@@ -25,28 +26,29 @@ module Jobs
       @old_site_settings_keys ||= SiteSetting.secret_settings.each_with_object([]) do |secret_name, old_keys|
         site_setting = SiteSetting.find_by(name: secret_name)
         next if site_setting&.value.blank?
-        next if site_setting.updated_at + calculate_period > Time.zone.now
+        next if site_setting.updated_at + OLD_CREDENTIALS_PERIOD > Time.zone.now
         old_keys << site_setting
       end.sort_by { |key| key.updated_at }
     end
 
     def old_api_keys
       @old_api_keys ||= ApiKey.all.order(created_at: :asc).each_with_object([]) do |api_key, old_keys|
-        next if api_key.created_at + calculate_period > Time.zone.now
+        next if api_key.created_at + OLD_CREDENTIALS_PERIOD > Time.zone.now
         old_keys << api_key
       end
-    end
-
-    def calculate_period
-      SiteSetting.notify_about_secrets_older_than.to_i.years
     end
 
     def admins
       User.real.admins
     end
 
+    def message_exists?
+      message = Topic.private_messages.with_deleted.find_by(title: title)
+      message && message.created_at + SiteSetting.send_old_credential_reminder_days.to_i.days > Time.zone.now
+    end
+
     def title
-      I18n.t('old_keys_reminder.title', keys_count: old_site_settings_keys.count + old_api_keys.count)
+      I18n.t('old_keys_reminder.title')
     end
 
     def body
@@ -54,8 +56,8 @@ module Jobs
     end
 
     def keys_list
-      messages = old_site_settings_keys.map { |key| "#{key.name} - #{key.updated_at}" }
-      old_api_keys.each_with_object(messages) { |key, array| array << "#{[key.description, key.user&.username, key.created_at].compact.join(" - ")}" }
+      messages = old_site_settings_keys.map { |key| "#{key.name} - #{key.updated_at.to_date.to_s(:db)}" }
+      old_api_keys.each_with_object(messages) { |key, array| array << "#{[key.description, key.user&.username, key.created_at.to_date.to_s(:db)].compact.join(" - ")}" }
       messages.join("\n")
     end
   end
