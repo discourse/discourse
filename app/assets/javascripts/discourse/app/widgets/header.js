216ece4473d8fe9a443ae4fb01e8359c6f7a1f94
Mark VanLandingham <mark.vanlandingham@discourse.org>
2020-06-01 12:26:59 -0500

    DEV: Add option to only show icons to header widget (#9935)

--- app/assets/javascripts/discourse/app/widgets/header.js
+++ app/assets/javascripts/discourse/app/widgets/header.js
@@ -299,17 +299,20 @@ export default createWidget("header", {
 
   html(attrs, state) {
     let contents = () => {
-      const panels = [
-        this.attach("header-buttons", attrs),
-        this.attach("header-icons", {
-          hamburgerVisible: state.hamburgerVisible,
-          userVisible: state.userVisible,
-          searchVisible: state.searchVisible,
-          ringBackdrop: state.ringBackdrop,
-          flagCount: attrs.flagCount,
-          user: this.currentUser
-        })
-      ];
+      const headerIcons = this.attach("header-icons", {
+        hamburgerVisible: state.hamburgerVisible,
+        userVisible: state.userVisible,
+        searchVisible: state.searchVisible,
+        ringBackdrop: state.ringBackdrop,
+        flagCount: attrs.flagCount,
+        user: this.currentUser
+      });
+
+      if (attrs.onlyIcons) {
+        return headerIcons;
+      }
+
+      const panels = [this.attach("header-buttons", attrs), headerIcons];
 
       if (state.searchVisible) {
         const contextType = this.searchContextType();
