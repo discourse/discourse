3955c8d377ef0995dfc38a9ceb77cfae68f5162e
Jarek Radosz <jradosz@gmail.com>
2020-06-01 05:03:27 +0200

    DEV: Spec S3 image downsizing

--- spec/lib/shrink_uploaded_image_spec.rb
+++ spec/lib/shrink_uploaded_image_spec.rb
@@ -3,52 +3,87 @@
 require 'rails_helper'
 
 describe ShrinkUploadedImage do
-  let(:upload) { Fabricate(:image_upload, width: 200, height: 200) }
+  context "when local uploads are enabled" do
+    let(:upload) { Fabricate(:image_upload, width: 200, height: 200) }
 
-  it "resizes the image" do
-    filesize_before = upload.filesize
-    post = Fabricate(:post, raw: "<img src='#{upload.url}'>")
-    post.link_post_uploads
+    it "resizes the image" do
+      filesize_before = upload.filesize
+      post = Fabricate(:post, raw: "<img src='#{upload.url}'>")
+      post.link_post_uploads
 
-    result = ShrinkUploadedImage.new(
-      upload: upload,
-      path: Discourse.store.path_for(upload),
-      max_pixels: 10_000
-    ).perform
+      result = ShrinkUploadedImage.new(
+        upload: upload,
+        path: Discourse.store.path_for(upload),
+        max_pixels: 10_000
+      ).perform
 
-    expect(result).to be(true)
-    expect(upload.width).to eq(100)
-    expect(upload.height).to eq(100)
-    expect(upload.filesize).to be < filesize_before
+      expect(result).to be(true)
+      expect(upload.width).to eq(100)
+      expect(upload.height).to eq(100)
+      expect(upload.filesize).to be < filesize_before
+    end
+
+    it "returns false if the image is not used by any models" do
+      result = ShrinkUploadedImage.new(
+        upload: upload,
+        path: Discourse.store.path_for(upload),
+        max_pixels: 10_000
+      ).perform
+
+      expect(result).to be(false)
+    end
+
+    it "returns false if the image cannot be shrunk more" do
+      post = Fabricate(:post, raw: "<img src='#{upload.url}'>")
+      post.link_post_uploads
+      ShrinkUploadedImage.new(
+        upload: upload,
+        path: Discourse.store.path_for(upload),
+        max_pixels: 10_000
+      ).perform
+
+      upload.reload
+
+      result = ShrinkUploadedImage.new(
+        upload: upload,
+        path: Discourse.store.path_for(upload),
+        max_pixels: 10_000
+      ).perform
+
+      expect(result).to be(false)
+    end
   end
 
-  it "returns false if the image is not used by any models" do
-    result = ShrinkUploadedImage.new(
-      upload: upload,
-      path: Discourse.store.path_for(upload),
-      max_pixels: 10_000
-    ).perform
+  context "when S3 uploads are enabled" do
+    let(:upload) { Fabricate(:s3_image_upload, width: 200, height: 200) }
 
-    expect(result).to be(false)
-  end
+    before do
+      SiteSetting.enable_s3_uploads = true
+      SiteSetting.s3_access_key_id = "fakeid7974664"
+      SiteSetting.s3_secret_access_key = "fakesecretid7974664"
 
-  it "returns false if the image cannot be shrunk more" do
-    post = Fabricate(:post, raw: "<img src='#{upload.url}'>")
-    post.link_post_uploads
-    ShrinkUploadedImage.new(
-      upload: upload,
-      path: Discourse.store.path_for(upload),
-      max_pixels: 10_000
-    ).perform
+      store = FileStore::S3Store.new
+      s3_helper = store.instance_variable_get(:@s3_helper)
+      client = Aws::S3::Client.new(stub_responses: true)
+      s3_helper.stubs(:s3_client).returns(client)
+      Discourse.stubs(:store).returns(store)
+    end
 
-    upload.reload
+    it "resizes the image" do
+      filesize_before = upload.filesize
+      post = Fabricate(:post, raw: "<img src='#{upload.url}'>")
+      post.link_post_uploads
 
-    result = ShrinkUploadedImage.new(
-      upload: upload,
-      path: Discourse.store.path_for(upload),
-      max_pixels: 10_000
-    ).perform
+      result = ShrinkUploadedImage.new(
+        upload: upload,
+        path: Discourse.store.download(upload).path,
+        max_pixels: 10_000
+      ).perform
 
-    expect(result).to be(false)
+      expect(result).to be(true)
+      expect(upload.width).to eq(100)
+      expect(upload.height).to eq(100)
+      expect(upload.filesize).to be < filesize_before
+    end
   end
 end
