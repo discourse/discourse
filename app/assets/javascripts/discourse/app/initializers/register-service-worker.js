discourse/discourse

commit a1f870756852cc66bcb0d798aa056efeb70124fc

DEV: Move `Discourse.getURL` and related functions to a module (#9966)
* DEV: Move `Discourse.getURL` and related functions to a module

* DEV: Remove `Discourse.getURL` and `Discourse.getURLWithCDN`

* FIX: `get-url` is required for server side code

* DEV: Deprecate `BaseUri` too.

app/assets/javascripts/discourse/app/initializers/register-service-worker.js
@@ -1,3 +1,6 @@
+import { isAbsoluteURL } from "discourse-common/lib/get-url";
+import getURL from "discourse-common/lib/get-url";
+
 export default {
   name: "register-service-worker",
 
@@ -30,7 +33,7 @@ export default {
         });
 
         navigator.serviceWorker
-          .register(`${Discourse.BaseUri}/${Discourse.ServiceWorkerURL}`)
+          .register(`${getURL(Discourse.ServiceWorkerURL)}`)
           .catch(error => {
             // eslint-disable-next-line no-console
             console.info(`Failed to register Service Worker: ${error}`);
@@ -46,7 +49,7 @@ export default {
   },
 
   unregister(registration) {
-    if (registration.scope.startsWith(Discourse.BaseUrl)) {
+    if (isAbsoluteURL(registration.scope)) {
       registration.unregister();
     }
   }

commit 30ab93cee0fd94d7b2d77c129ee2c293abe35121

FIX: Service Worker URL was broken
Behavior was changed in #9966, which made the URL be relative.

If the user landed in a topic, for example, the browser was given a
service worker URL under that specific topic URL, which was a 404.

Fixes broken PWA install and broken push notifications

app/assets/javascripts/discourse/app/initializers/register-service-worker.js
@@ -1,5 +1,5 @@
 import { isAbsoluteURL } from "discourse-common/lib/get-url";
-import getURL from "discourse-common/lib/get-url";
+import getAbsoluteURL from "discourse-common/lib/get-url";
 
 export default {
   name: "register-service-worker",
@@ -33,7 +33,7 @@ export default {
         });
 
         navigator.serviceWorker
-          .register(`${getURL(Discourse.ServiceWorkerURL)}`)
+          .register(getAbsoluteURL(`/${Discourse.ServiceWorkerURL}`))
           .catch(error => {
             // eslint-disable-next-line no-console
             console.info(`Failed to register Service Worker: ${error}`);