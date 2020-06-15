3b54fac19b2b119e947e2c82bebc478c789a7b8d
Martin Brennan <martin@discourse.org>
2020-06-15 13:45:33 +1000

    Revert bootstrap change

--- app/assets/javascripts/discourse/app/pre-initializers/discourse-bootstrap.js
+++ app/assets/javascripts/discourse/app/pre-initializers/discourse-bootstrap.js
@@ -28,7 +28,7 @@ export default {
       Object.keys(preloaded).forEach(function(key) {
         PreloadStore.store(key, JSON.parse(preloaded[key]));
 
-        if (true) {
+        if (setupData.debugPreloadedAppData === "true") {
           /* eslint-disable no-console */
           console.log(key, PreloadStore.get(key));
           /* eslint-enable no-console */
