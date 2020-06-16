def4cd33ea948ff617d8edd2273095fd43317454
Guo Xiang Tan <gxtan1990@gmail.com>
2020-06-16 12:53:04 +0800

    DEV: Disable Redis warnings.

--- config/initializers/001-redis.rb
+++ config/initializers/001-redis.rb
@@ -4,3 +4,7 @@ if Rails.env.development? && ENV['DISCOURSE_FLUSH_REDIS']
   puts "Flushing redis (development mode)"
   Discourse.redis.flushdb
 end
+
+# Pending https://github.com/MiniProfiler/rack-mini-profiler/pull/450 and
+# upgrade to Sidekiq 6.1
+Redis.exists_returns_integer = true
