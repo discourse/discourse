d2a900f078017d149797be39e71627581a2483e5
David Taylor <david@taylorhq.com>
2020-06-17 15:57:32 +0100

    FIX: Cleanup migrations with timestamps in the future
    A future-dated migration was accidently introduced by me in 45c399f0. This was removed in b9762afc, but other migrations had already been generated based on its incorrect date. This commit removes the offending data in the schema_migrations table, and corrects the version in the published_pages migration.
    
    This commit also adds a check to db:migrate which raises an error when invalid migration timestamps are used.

--- db/migrate/20201006172701_add_public_field_to_published_pages.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddPublicFieldToPublishedPages < ActiveRecord::Migration[6.0]
-  def change
-    add_column :published_pages, :public, :boolean, null: false, default: false
-  end
-end
