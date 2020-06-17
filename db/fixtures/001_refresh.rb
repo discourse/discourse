e4a170a0ccdf8df9272ac100ecdaae57de2758f9
Guo Xiang Tan <gxtan1990@gmail.com>
2020-06-17 16:15:43 +0800

    FIX: Thread safety issues with `multisite:migrate` and `SeedFu`.

--- db/fixtures/001_refresh.rb
+++ db/fixtures/001_refresh.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
-# fix any bust caches post initial migration
-ActiveRecord::Base.connection.tables.each do |table|
-  table.classify.constantize.reset_column_information rescue nil
+class SeedData::Refresher
+  def self.refresh!
+    return if @refreshed
+
+    # Fix any bust caches post initial migration
+    # Not that reset_column_information is not thread safe so we have to becareful
+    # not to run it concurrently within the same process.
+    ActiveRecord::Base.connection.tables.each do |table|
+      table.classify.constantize.reset_column_information rescue nil
+    end
+
+    @refreshed = true
+  end
 end
 
+SeedData::Refresher.refresh!
 SiteSetting.refresh!
