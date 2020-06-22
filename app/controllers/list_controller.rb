685646540acee86e4edcf24725cb73942d70e896
Bianca Nenciu <nbianca@users.noreply.github.com>
2020-06-22 16:48:24 +0300

    FIX: Hide PM tags if the site setting is disabled (#10089)
    * FIX: Hide PM tags if the site setting is disabled
    
    * Apply code suggestions

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -144,6 +144,10 @@ class ListController < ApplicationController
 
   def self.generate_message_route(action)
     define_method("#{action}") do
+      if action == :private_messages_tag && !guardian.can_tag_pms?
+        raise Discourse::NotFound
+      end
+
       list_opts = build_topic_list_options
       target_user = fetch_user_from_params({ include_inactive: current_user.try(:staff?) }, [:user_stat, :user_option])
       guardian.ensure_can_see_private_messages!(target_user.id)
