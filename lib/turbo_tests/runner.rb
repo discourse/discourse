7747714e9726b87dadb64d0c7747433c6424f9ad
Daniel Waterworth <me@danielwaterworth.com>
2020-06-23 10:12:31 +0100

    DEV: Reduce size of begin-rescue region
    Follow-up-to: e3e7905d9ebd86943102917b2ff0bfee32035c01

--- lib/turbo_tests/runner.rb
+++ lib/turbo_tests/runner.rb
@@ -87,14 +87,15 @@ module TurboTests
 
       ActiveRecord::Tasks::DatabaseTasks.migrations_paths = ['db/migrate', 'db/post_migrate']
 
+      conn = ActiveRecord::Base.establish_connection(config).connection
+
       begin
-        conn = ActiveRecord::Base.establish_connection(config).connection
         ActiveRecord::Migration.check_pending!(conn)
       rescue ActiveRecord::PendingMigrationError
         puts "There are pending migrations, run rake parallel:migrate"
         exit 1
       ensure
-        conn&.close
+        conn.close
       end
     end
 
