b290bbd4533edadb58385cacbc835d000b4c2989
Ahmed <ahmedgagan6@gmail.com>
2020-07-01 02:43:45 +0530

    all bugs solved with tests written

--- app/assets/javascripts/discourse/app/components/user-notifications-large.js
+++ app/assets/javascripts/discourse/app/components/user-notifications-large.js
@@ -4,20 +4,18 @@ import { observes } from "discourse-common/utils/decorators";
 export default MountWidget.extend({
   widget: "user-notifications-large",
   notifications: null,
-  filter: null,
   args: null,
 
   init() {
     this._super(...arguments);
 
-    this.args = { notifications: this.notifications, filter: this.filter };
+    this.args = { notifications: this.notifications };
   },
 
-  @observes("notifications.length", "notifications.@each.read", "filter")
+  @observes("notifications.length", "notifications.@each.read")
   _triggerRefresh() {
     this.set("args", {
-      notifications: this.notifications,
-      filter: this.filter
+      notifications: this.notifications
     });
 
     this.queueRerender();
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -44,12 +44,16 @@ class NotificationsController < ApplicationController
         .includes(:topic)
         .order(created_at: :desc)
 
+      notifications = notifications.where(read: true) if params[:filter] == "read"
+
+      notifications = notifications.where(read: false) if params[:filter] == "unread"
+
       total_rows = notifications.dup.count
       notifications = notifications.offset(offset).limit(60)
       render_json_dump(notifications: serialize_data(notifications, NotificationSerializer),
                        total_rows_notifications: total_rows,
                        seen_notification_id: user.seen_notification_id,
-                       load_more_notifications: notifications_path(username: user.username, offset: offset + 60))
+                       load_more_notifications: notifications_path(username: user.username, offset: offset + 60, filter: params[:filter]))
     end
 
   end
