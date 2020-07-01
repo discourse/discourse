52cf7de67452a24a60f4da76639af04b459b9d4f
Dan Ungureanu <dan@ungureanu.me>
2020-07-01 12:29:31 +0300

    DEV: Simplify code

--- spec/requests/category_hashtags_controller_spec.rb
+++ spec/requests/category_hashtags_controller_spec.rb
@@ -82,7 +82,7 @@ describe CategoryHashtagsController do
           }
 
           expect(response.status).to eq(200)
-          expect(response.parsed_body).to eq("valid" => [
+          expect(response.parsed_body["valid"]).to contain_exactly(
             { "slug" => "foo",     "url" => foo.url },
             { "slug" => "bar",     "url" => foobar.url },
             { "slug" => "foo:bar", "url" => foobar.url },
@@ -90,7 +90,7 @@ describe CategoryHashtagsController do
             { "slug" => "bar:baz", "url" => foobarbaz.url },
             { "slug" => "qux",     "url" => qux.url },
             { "slug" => "qux:bar", "url" => quxbar.url }
-          ])
+          )
         end
       end
     end
