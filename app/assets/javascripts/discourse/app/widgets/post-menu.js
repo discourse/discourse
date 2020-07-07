6ef0e98f4e07efd1fba36e8315d53559e22a9a8f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2020-07-07 02:18:01 +0200

    DEV: flexify post actions (#9926)
    * DEV: flexify post actions
    
    * more tweaks

--- app/assets/javascripts/discourse/app/widgets/post-menu.js
+++ app/assets/javascripts/discourse/app/widgets/post-menu.js
@@ -395,7 +395,7 @@ function replaceButton(buttons, find, replace) {
 }
 
 export default createWidget("post-menu", {
-  tagName: "section.post-menu-area.clearfix",
+  tagName: "section.post-menu-area",
 
   settings: {
     collapseButtons: true,
@@ -573,7 +573,7 @@ export default createWidget("post-menu", {
 
     const contents = [
       h(
-        "nav.post-controls.clearfix" +
+        "nav.post-controls" +
           (this.state.collapsed ? ".collapsed" : ".expanded"),
         postControls
       )
