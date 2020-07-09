discourse/discourse

commit 3b51e05de2aeb0cff547befcfde442f5ceeb329d

FIX: CookText may be gone before promise resolves (#10097)
* FIX: CookText may be gone before promise resolves

Bug introduced in 293467a37ae33c5fd8fd40bbfc05bffa578b90ed.

* DEV: Drop the window.requireModule in cook-text

It was introduced in 2017 in 232311aa8c8e879c98dc061ac1755ee4d0dd9b0f but doesn't seem to be necessary anymore.

app/assets/javascripts/discourse/app/components/cook-text.js
@@ -1,7 +1,7 @@
-import { next } from "@ember/runloop";
 import Component from "@ember/component";
 import { cookAsync } from "discourse/lib/text";
 import { ajax } from "discourse/lib/ajax";
+import { resolveAllShortUrls } from "pretty-text/upload-short-url";
 
 const CookText = Component.extend({
   cooked: null,
@@ -10,13 +10,10 @@ const CookText = Component.extend({
     this._super(...arguments);
     cookAsync(this.rawText).then(cooked => {
       this.set("cooked", cooked);
-      // no choice but to defer this cause
-      // pretty text may only be loaded now
-      next(() =>
-        window
-          .requireModule("pretty-text/upload-short-url")
-          .resolveAllShortUrls(ajax, this.siteSettings, this.element)
-      );
+
+      if (this.element && !this.isDestroying && !this.isDestroyed) {
+        return resolveAllShortUrls(ajax, this.siteSettings, this.element);
+      }
     });
   }
 });

commit 76028465579833d6feebe397628162867ea1d64d

FIX: Short URL resolution in cook-text
Regressed in 3b51e05de2aeb0cff547befcfde442f5ceeb329d. Thanks to @romanrizzi for reporting!

app/assets/javascripts/discourse/app/components/cook-text.js
@@ -2,6 +2,7 @@ import Component from "@ember/component";
 import { cookAsync } from "discourse/lib/text";
 import { ajax } from "discourse/lib/ajax";
 import { resolveAllShortUrls } from "pretty-text/upload-short-url";
+import { afterRender } from "discourse-common/utils/decorators";
 
 const CookText = Component.extend({
   cooked: null,
@@ -10,11 +11,13 @@ const CookText = Component.extend({
     this._super(...arguments);
     cookAsync(this.rawText).then(cooked => {
       this.set("cooked", cooked);
-
-      if (this.element && !this.isDestroying && !this.isDestroyed) {
-        return resolveAllShortUrls(ajax, this.siteSettings, this.element);
-      }
+      this._resolveUrls();
     });
+  },
+
+  @afterRender
+  _resolveUrls() {
+    resolveAllShortUrls(ajax, this.siteSettings, this.element);
   }
 });
 

test/javascripts/components/cook-text-test.js
@@ -1,0 +1,46 @@
+import componentTest from "helpers/component-test";
+import pretender from "helpers/create-pretender";
+import { resetCache } from "pretty-text/upload-short-url";
+
+moduleForComponent("cook-text", { integration: true });
+
+componentTest("renders markdown", {
+  template: '{{cook-text "_foo_" class="post-body"}}',
+
+  test(assert) {
+    const html = find(".post-body")[0].innerHTML.trim();
+    assert.equal(html, "<p><em>foo</em></p>");
+  }
+});
+
+componentTest("resolves short URLs", {
+  template: `{{cook-text "![an image](upload://a.png)" class="post-body"}}`,
+
+  beforeEach() {
+    pretender.post("/uploads/lookup-urls", () => {
+      return [
+        200,
+        { "Content-Type": "application/json" },
+        [
+          {
+            short_url: "upload://a.png",
+            url: "/uploads/default/original/3X/c/b/1.png",
+            short_path: "/uploads/short-url/a.png"
+          }
+        ]
+      ];
+    });
+  },
+
+  afterEach() {
+    resetCache();
+  },
+
+  test(assert) {
+    const html = find(".post-body")[0].innerHTML.trim();
+    assert.equal(
+      html,
+      '<p><img src="/uploads/default/original/3X/c/b/1.png" alt="an image"></p>'
+    );
+  }
+});