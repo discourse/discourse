a900c99993aa029230b57d9fd0d96365e1579a21
Blake Erickson <o.blakeerickson@gmail.com>
2020-07-13 12:07:39 -0600

    DEV: Add external_id to notification payload
    If sso is enabled the notification payload will now include the
    external_id for the user.
    
    This was requested on meta: https://meta.discourse.org/t/-/129052/10

--- spec/serializers/notification_serializer_spec.rb
+++ spec/serializers/notification_serializer_spec.rb
@@ -13,5 +13,26 @@ describe NotificationSerializer do
       expect(json[:notification][:user_id]).to eq(user.id)
     end
 
+    it "does not include external_id when sso is disabled" do
+      expect(json[:notification].key?(:external_id)).to eq(false)
+    end
+  end
+
+  describe '#sso_enabled' do
+    let :user do
+      user = Fabricate(:user)
+      SingleSignOnRecord.create!(user_id: user.id, external_id: '12345', last_payload: '')
+      user
+    end
+    let(:notification) { Fabricate(:notification, user: user) }
+    let(:serializer) { NotificationSerializer.new(notification) }
+    let(:json) { serializer.as_json }
+
+    it "should include the external_id" do
+      SiteSetting.sso_url = "http://example.com/discourse_sso"
+      SiteSetting.sso_secret = "12345678910"
+      SiteSetting.enable_sso = true
+      expect(json[:notification][:external_id]).to eq("12345")
+    end
   end
 end
