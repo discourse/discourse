6e079f9ad7d27d358d27d2daa98c9d630224cd96
jbrw <jamie@goatforce5.org>
2020-07-17 09:50:56 -0400

    Apply suggestions from code review
    Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

--- test/javascripts/acceptance/post-admin-menu-test.js
+++ test/javascripts/acceptance/post-admin-menu-test.js
@@ -2,14 +2,13 @@ import { acceptance, updateCurrentUser } from "helpers/qunit-helpers";
 
 acceptance("Post - Admin Menu Anonymous Users", { loggedIn: false });
 
-QUnit.test("Enter as a regular user", async assert => {
+QUnit.test("Enter as a anon user", async assert => {
   await visit("/t/internationalization-localization/280");
   await click(".show-more-actions");
 
   assert.ok(exists("#topic"), "The topic was rendered");
   assert.ok(!exists(".show-post-admin-menu"), "The wrench button was not rendered");
 });
-
 
 acceptance("Post - Admin Menu", { loggedIn: true });
 
@@ -20,7 +19,7 @@ QUnit.test("Enter as a user with group moderator permissions", async assert => {
   await click(".show-more-actions");
   await click(".show-post-admin-menu");
 
-  assert.ok(exists(".add-notice"), "The add nottice button was rendered");
+  assert.ok(exists(".add-notice"), "The add notice button was rendered");
 });
 
 QUnit.test("Enter as a user with moderator and admin permissions", async assert => {
@@ -30,5 +29,5 @@ QUnit.test("Enter as a user with moderator and admin permissions", async assert 
   await click(".show-more-actions");
   await click(".show-post-admin-menu");
 
-  assert.ok(exists(".add-notice"), "The add nottice button was rendered");
+  assert.ok(exists(".add-notice"), "The add notice button was rendered");
 });
