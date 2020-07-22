1be583e051aa00730d98a453ced7dd0fde8a6752
David Taylor <david@taylorhq.com>
2020-07-22 16:08:39 +0100

    Handle nil header without `rescue`

--- lib/http_language_parser.rb
+++ lib/http_language_parser.rb
@@ -7,9 +7,7 @@ module HttpLanguageParser
     require 'http_accept_language' unless defined? HttpAcceptLanguage
     available_locales = I18n.available_locales.map { |locale| locale.to_s.tr('_', '-') }
     parser = HttpAcceptLanguage::Parser.new(header)
-    parser.language_region_compatible_from(available_locales).tr('-', '_')
-  rescue
-    # If Accept-Language headers are not set.
-    I18n.default_locale
+    matched = parser.language_region_compatible_from(available_locales)&.tr('-', '_')
+    matched || SiteSetting.default_locale
   end
 end
