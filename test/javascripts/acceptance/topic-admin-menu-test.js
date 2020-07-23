39fa1f91aa248c79e22ae0767623e13d7931ff53
Jarek Radosz <jradosz@gmail.com>
2020-07-23 14:32:07 +0200

    LINT: run prettier on test/*.js files

--- test/javascripts/acceptance/topic-admin-menu-test.js
+++ test/javascripts/acceptance/topic-admin-menu-test.js
@@ -5,9 +5,11 @@ acceptance("Topic - Admin Menu Anonymous Users", { loggedIn: false });
 QUnit.test("Enter as a regular user", async assert => {
   await visit("/t/internationalization-localization/280");
   assert.ok(exists("#topic"), "The topic was rendered");
-  assert.ok(!exists(".toggle-admin-menu"), "The admin menu button was not rendered");
+  assert.ok(
+    !exists(".toggle-admin-menu"),
+    "The admin menu button was not rendered"
+  );
 });
-
 
 acceptance("Topic - Admin Menu", { loggedIn: true });
 
