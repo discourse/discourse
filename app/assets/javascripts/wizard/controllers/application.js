665912c50873ddea29efb74b0a69c88d2f29b1ff
Bianca Nenciu <nenciu.bianca@gmail.com>
2020-07-23 14:37:37 +0300

    Fix test

--- app/assets/javascripts/wizard/controllers/application.js
+++ app/assets/javascripts/wizard/controllers/application.js
@@ -12,6 +12,10 @@ export default Controller.extend({
   @discourseComputed("model")
   fontStyles(model) {
     const fontsStep = model.steps.findBy("id", "fonts");
+    if (!fontsStep) {
+      return [];
+    }
+
     const fontField = fontsStep.get("fieldsById.font_previews");
     return fontField.choices.map(field =>
       `font-family: ${field.data.font_stack}`.htmlSafe()
