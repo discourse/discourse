80f33b5aaff3c23963468388344b925028171c7f
Guo Xiang Tan <gxtan1990@gmail.com>
2020-07-24 10:54:26 +0800

    DEV: Fix incorrect description in spec.

--- spec/requests/groups_controller_spec.rb
+++ spec/requests/groups_controller_spec.rb
@@ -1639,7 +1639,7 @@ describe GroupsController do
     describe 'for an admin user' do
       before { sign_in(Fabricate(:admin)) }
 
-      it 'should return 404' do
+      it 'should return 200' do
         get '/groups/custom/new'
 
         expect(response.status).to eq(200)
