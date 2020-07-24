64fd94a4f8e2c4ce0dfe3720b9fcba276c1737e8
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2020-07-24 13:25:27 +1000

    FIX: deprecate messeges for plugin api methods

--- lib/plugin/instance.rb
+++ lib/plugin/instance.rb
@@ -160,8 +160,18 @@ class Plugin::Instance
     end
   end
 
+  def whitelist_staff_user_custom_field(field)
+    Discourse.deprecate("whitelist_staff_user_custom_field is deprecated, use the allow_staff_user_custom_field.", drop_from: "2.6")
+    allow_staff_user_custom_field(field)
+  end
+
   def allow_staff_user_custom_field(field)
     DiscoursePluginRegistry.register_staff_user_custom_field(field, self)
+  end
+
+  def whitelist_public_user_custom_field(field)
+    Discourse.deprecate("whitelist_public_user_custom_field is deprecated, use the allow_public_user_custom_field.", drop_from: "2.6")
+    allow_public_user_custom_field(field)
   end
 
   def allow_public_user_custom_field(field)
@@ -254,6 +264,11 @@ class Plugin::Instance
 
       hidden_method_name
     end
+  end
+
+  def topic_view_post_custom_fields_whitelister(&block)
+    Discourse.deprecate("topic_view_post_custom_fields_whitelister is deprecated, use the topic_view_post_custom_fields_allowlister.", drop_from: "2.6")
+    topic_view_post_custom_fields_allowlister(&block)
   end
 
   # Add a post_custom_fields_allowlister block to the TopicView, respecting if the plugin is enabled
