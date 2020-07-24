9649e66fce86223779af03e9f280a199f830ece6
Martin Brennan <martin@discourse.org>
2020-07-24 11:15:14 +1000

    Do not show Email tab for group settings unless IMAP + SMTP enabled
    * also hide all the SMTP/IMAP related settings to avoid confusion

--- app/assets/javascripts/discourse/app/routes/group-manage-email.js
+++ app/assets/javascripts/discourse/app/routes/group-manage-email.js
@@ -4,6 +4,12 @@ import I18n from "I18n";
 export default DiscourseRoute.extend({
   showFooter: true,
 
+  beforeModel() {
+    if (!this.siteSettings.enable_imap && !this.siteSettings.enable_smtp) {
+      return this.transitionTo("group.manage.profile");
+    }
+  },
+
   titleToken() {
     return I18n.t("groups.manage.email.title");
   }
