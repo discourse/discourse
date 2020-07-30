3d9eb3e085e8017d1e2f92c5a8bbfba38f2b7614
Simon Cossar <scossar3@gmail.com>
2020-07-30 12:56:48 -0700

    Rename options passed to Readability::Document back to whitelist and blacklist (#10340)

--- app/models/topic_embed.rb
+++ app/models/topic_embed.rb
@@ -124,8 +124,8 @@ class TopicEmbed < ActiveRecord::Base
       remove_empty_nodes: false
     }
 
-    opts[:allowlist] = SiteSetting.allowed_embed_selectors if SiteSetting.allowed_embed_selectors.present?
-    opts[:blocklist] = SiteSetting.blocked_embed_selectors if SiteSetting.blocked_embed_selectors.present?
+    opts[:whitelist] = SiteSetting.allowed_embed_selectors if SiteSetting.allowed_embed_selectors.present?
+    opts[:blacklist] = SiteSetting.blocked_embed_selectors if SiteSetting.blocked_embed_selectors.present?
     allowed_embed_classnames = SiteSetting.allowed_embed_classnames if SiteSetting.allowed_embed_classnames.present?
 
     response = FetchResponse.new
