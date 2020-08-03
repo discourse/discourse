4e2c874aec2c6eb8040634fa718fbe49b208e913
Dan Ungureanu <dan@ungureanu.me>
2020-08-03 11:56:07 +0300

    FIX: Remove reference to discovery.parentCategory

--- app/assets/javascripts/discourse/app/routes/new-topic.js
+++ app/assets/javascripts/discourse/app/routes/new-topic.js
@@ -33,17 +33,9 @@ export default DiscourseRoute.extend({
         }
       }
 
-      if (Boolean(category)) {
-        let route = "discovery.parentCategory";
-        let params = { category, slug: category.slug };
-        if (category.parentCategory) {
-          route = "discovery.category";
-          params = {
-            category,
-            parentSlug: category.parentCategory.slug,
-            slug: category.slug,
-          };
-        }
+      if (category) {
+        let route = "discovery.category";
+        let params = { category, id: category.id };
 
         this.replaceWith(route, params).then((e) => {
           if (this.controllerFor("navigation/category").canCreateTopic) {
