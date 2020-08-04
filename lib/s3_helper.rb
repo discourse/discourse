aee3c2c34d5536bf7a6d1673815e50efbfedfd44
Andrew Schleifer <me@andrewschleifer.name>
2020-08-04 11:08:37 +0800

    FIX: attempt to output a useful error message
    currently AWS problems will just dump a stack trace

--- lib/s3_helper.rb
+++ lib/s3_helper.rb
@@ -228,9 +228,9 @@ class S3Helper
   end
 
   def download_file(filename, destination_path, failure_message = nil)
-    unless object(filename).download_file(destination_path)
-      raise failure_message&.to_s || "Failed to download file"
-    end
+    object(filename).download_file(destination_path)
+  rescue => err
+    raise failure_message&.to_s || "Failed to download #{filename} because #{err.message}"
   end
 
   def s3_client
