792bd3faff0e8c22edbd91862ae1eaff60ecd27d
Robin Ward <robin.ward@gmail.com>
2020-08-06 14:01:09 -0400

    Remove `getURL` and `getURLWithCDN` deprecations, they have been removed

--- app/assets/javascripts/discourse/app/app.js
+++ app/assets/javascripts/discourse/app/app.js
@@ -3,8 +3,6 @@ import Application from "@ember/application";
 import { computed } from "@ember/object";
 import { buildResolver } from "discourse-common/resolver";
 import discourseComputed from "discourse-common/utils/decorators";
-import { default as getURL, getURLWithCDN } from "discourse-common/lib/get-url";
-import deprecated from "discourse-common/lib/deprecated";
 
 const _pluginCallbacks = [];
 
@@ -19,22 +17,6 @@ const Discourse = Application.extend({
   reset() {
     this._super(...arguments);
     Mousetrap.reset();
-  },
-
-  getURL(url) {
-    deprecated(
-      "Import `getURL` from `discourse-common/lib/get-url` instead of `Discourse.getURL`",
-      { since: "2.5", dropFrom: "2.6" }
-    );
-    return getURL(url);
-  },
-
-  getURLWithCDN(url) {
-    deprecated(
-      "Import `getURLWithCDN` from `discourse-common/lib/get-url` instead of `Discourse.getURLWithCDN`",
-      { since: "2.5", dropFrom: "2.6" }
-    );
-    return getURLWithCDN(url);
   },
 
   Resolver: buildResolver("discourse"),
