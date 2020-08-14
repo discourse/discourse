a3e21522657d422a0203f93ca0c2dbbace66022a
Jarek Radosz <jradosz@gmail.com>
2020-08-14 17:07:17 +0200

    DEV: Add ember-buffered-proxy import shim (#10438)
    Just a bit of 🧹

--- app/assets/javascripts/discourse/app/controllers/reorder-categories.js
+++ app/assets/javascripts/discourse/app/controllers/reorder-categories.js
@@ -4,7 +4,7 @@ import EmberObjectProxy from "@ember/object/proxy";
 import Controller from "@ember/controller";
 import { ajax } from "discourse/lib/ajax";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
-const BufferedProxy = window.BufferedProxy; // import BufferedProxy from 'ember-buffered-proxy/proxy';
+import BufferedProxy from "ember-buffered-proxy/proxy";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import discourseComputed, { on } from "discourse-common/utils/decorators";
 
