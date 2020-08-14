dbe82782173e36125a80540572bb57d8c1d93fb6
romanrizzi <rizziromanalejandro@gmail.com>
2020-08-14 16:18:12 -0300

    UX: Help users understand the meaning of each scope.

--- app/serializers/api_key_scope_serializer.rb
+++ app/serializers/api_key_scope_serializer.rb
@@ -5,9 +5,23 @@ class ApiKeyScopeSerializer < ApplicationSerializer
   attributes :resource,
              :action,
              :parameters,
-             :allowed_parameters
+             :urls,
+             :allowed_parameters,
+             :key
 
   def parameters
     ApiKeyScope.scope_mappings.dig(object.resource.to_sym, object.action.to_sym, :params).to_a
+  end
+
+  def urls
+    ApiKeyScope.scope_mappings.dig(object.resource.to_sym, object.action.to_sym, :urls).to_a
+  end
+
+  def action
+    object.action.to_s.gsub('_', ' ')
+  end
+
+  def key
+    object.action
   end
 end
