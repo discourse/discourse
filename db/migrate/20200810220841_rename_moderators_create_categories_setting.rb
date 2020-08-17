5b2f5119f6fa984fbddbe6ae3eb276988e332106
jbrw <jamie.wilson@discourse.org>
2020-08-17 11:17:03 -0400

    Migration should rename old log entries

--- db/migrate/20200810220841_rename_moderators_create_categories_setting.rb
+++ db/migrate/20200810220841_rename_moderators_create_categories_setting.rb
@@ -2,11 +2,12 @@
 
 class RenameModeratorsCreateCategoriesSetting < ActiveRecord::Migration[6.0]
   def up
-    # TODO - do we need to rename old UserHistory log entries too?
     execute "UPDATE site_settings SET name = 'moderators_manage_categories_and_groups' WHERE name = 'moderators_create_categories'"
+    execute "UPDATE user_histories SET subject = 'moderators_manage_categories_and_groups' WHERE subject = 'moderators_create_categories'"
   end
 
   def down
     execute "UPDATE site_settings SET name = 'moderators_create_categories' WHERE name = 'moderators_manage_categories_and_groups'"
+    execute "UPDATE user_histories SET subject = 'moderators_create_categories' WHERE subject = 'moderators_manage_categories_and_groups'"
   end
 end
