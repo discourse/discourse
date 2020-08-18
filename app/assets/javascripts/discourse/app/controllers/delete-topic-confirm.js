bdf1e55bae559eba70b50173a0d1f64a849a44ef
Ricky Chon <rickychon99@gmail.com>
2020-08-18 15:54:15 -0400

    DEV: Combine imports

--- app/assets/javascripts/discourse/app/controllers/delete-topic-confirm.js
+++ app/assets/javascripts/discourse/app/controllers/delete-topic-confirm.js
@@ -1,7 +1,6 @@
 import I18n from "I18n";
 import discourseComputed from "discourse-common/utils/decorators";
-import { inject } from "@ember/controller";
-import Controller from "@ember/controller";
+import Controller, { inject } from "@ember/controller";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
 
 // Modal that displays confirmation text when user deletes a topic
