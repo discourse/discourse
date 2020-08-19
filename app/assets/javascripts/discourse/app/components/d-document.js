bf94936ee6392d52bcb7ff1db834115172fe1841
Robin Ward <robin.ward@gmail.com>
2020-08-19 12:40:03 -0400

    REFACTOR: Remove container from ajax

--- app/assets/javascripts/discourse/app/components/d-document.js
+++ app/assets/javascripts/discourse/app/components/d-document.js
@@ -1,10 +1,14 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 import { bind } from "discourse-common/utils/decorators";
+import logout from "discourse/lib/logout";
+import I18n from "I18n";
+import { setLogoffCallback } from "discourse/lib/ajax";
 
 export default Component.extend({
   tagName: "",
   documentTitle: service(),
+  _showingLogout: false,
 
   didInsertElement() {
     this._super(...arguments);
@@ -16,11 +20,13 @@ export default Component.extend({
     this.session.hasFocus = true;
 
     this.appEvents.on("notifications:changed", this, this._updateNotifications);
+    setLogoffCallback(() => this.displayLogoff());
   },
 
   willDestroyElement() {
     this._super(...arguments);
 
+    setLogoffCallback(null);
     document.removeEventListener("visibilitychange", this._focusChanged);
     document.removeEventListener("resume", this._focusChanged);
     document.removeEventListener("freeze", this._focusChanged);
@@ -52,5 +58,22 @@ export default Component.extend({
     } else if (!this.hasFocus) {
       this.documentTitle.setFocus(true);
     }
+  },
+
+  displayLogoff() {
+    if (this._showingLogout) {
+      return;
+    }
+
+    this._showingLogout = true;
+    this.messageBus.stop();
+    bootbox.dialog(
+      I18n.t("logout"),
+      { label: I18n.t("refresh"), callback: logout },
+      {
+        onEscape: () => logout(),
+        backdrop: "static"
+      }
+    );
   }
 });
