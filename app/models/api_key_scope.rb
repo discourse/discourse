2ac01a59ae0a6b7cc2a0dd41e478f83094b5e0c3
romanrizzi <rizziromanalejandro@gmail.com>
2020-08-20 15:58:59 -0300

    FEATURE: More API scopes

--- app/models/api_key_scope.rb
+++ app/models/api_key_scope.rb
@@ -18,22 +18,36 @@ class ApiKeyScope < ActiveRecord::Base
     end
 
     def default_mappings
-      write_actions = %w[posts#create]
-      read_actions = %w[topics#show topics#feed]
+      return @default_mappings unless @default_mappings.nil?
 
-      @default_mappings ||= {
+      mappings = {
         topics: {
-          write: { actions: write_actions, params: %i[topic_id], urls: find_urls(write_actions) },
+          write: { actions: %w[posts#create], params: %i[topic_id] },
           read: {
-            actions: read_actions, params: %i[topic_id],
-            aliases: { topic_id: :id }, urls: find_urls(read_actions)
+            actions: %w[topics#show topics#feed topics#posts],
+            params: %i[topic_id], aliases: { topic_id: :id }
           },
           read_lists: {
             actions: list_actions, params: %i[category_id],
-            aliases: { category_id: :category_slug_path_with_id }, urls: find_urls(list_actions)
-          }
+            aliases: { category_id: :category_slug_path_with_id }
+          },
+          wordpress: { actions: %w[topics#wordpress], params: %i[topic_id] }
+        },
+        users: {
+          bookmarks: { actions: %w[users#bookmarks], params: %i[username] },
+          sync_sso: { actions: %w[admin/users#sync_sso], params: %i[sso sig] },
+          show: { actions: %w[users#show], params: %i[username external_id] },
+          check_emails: { actions: %w[users#check_emails], params: %i[username] }
         }
       }
+
+      mappings.each_value do |resource_actions|
+        resource_actions.each_value do |action_data|
+          action_data[:urls] = find_urls(action_data[:actions])
+        end
+      end
+
+      @default_mappings = mappings
     end
 
     def scope_mappings
