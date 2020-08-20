f4a0d200eeac89e4035d6cb0e5e3b9d81d974f6b
Guo Xiang Tan <gxtan1990@gmail.com>
2020-08-21 07:27:28 +0800

    DEV: Drop search index on non-pm posts take 2.
    Follow-up to 87de8948c0719d259a5420ca707c4520474b0e88

--- db/migrate/20200820232017_drop_idx_regular_post_search_data.rb
+++ db/migrate/20200820232017_drop_idx_regular_post_search_data.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class DropIdxRegularPostSearchData < ActiveRecord::Migration[6.0]
+  disable_ddl_transaction!
+
   def up
     execute <<~SQL
     DROP INDEX CONCURRENTLY IF EXISTS idx_regular_post_search_data
