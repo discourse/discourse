ab0b0344048e7e7354615286486bf0508c7c2df6
Kane York <kanepyork@gmail.com>
2020-08-20 17:10:33 -0700

    FIX: Wizard could not send custom color schemes to the client correctly (#10484)
    This was likely introduced with the refactor to make ColorSchemeColor a database object. Add a test so this doesn't happen again.
    
    Also test other basics of the WizardSerializer.
    
    For some reason, the .as_json left Ruby objects in; I solved this with a round trip through JSON during the test.

--- app/serializers/wizard_serializer.rb
+++ app/serializers/wizard_serializer.rb
@@ -15,11 +15,11 @@ class WizardSerializer < ApplicationSerializer
 
   def current_color_scheme
     color_scheme = Theme.where(id: SiteSetting.default_theme_id).first&.color_scheme
-    colors = color_scheme ? color_scheme.colors : ColorScheme.base_colors
+    colors = color_scheme ? color_scheme.colors : ColorScheme.base.colors
 
     # The frontend expects the color hexs to start with '#'
     colors_with_hash = {}
-    colors.each { |color, hex| colors_with_hash[color] = "##{hex}" }
+    colors.each { |color| colors_with_hash[color.name] = color.hex_with_hash }
     colors_with_hash
   end
 end
