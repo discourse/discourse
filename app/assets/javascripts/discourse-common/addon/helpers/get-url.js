3cf1257347651773d55a0f95bd93b7c2686d4c7c
Gerhard Schlager <gerhard.schlager@discourse.org>
2020-08-25 00:04:16 +0200

    DEV: Deprecate `base-url` helper in favor of `base-path`
    Mostly for consistency reasons. Ruby uses `base_path` as well.
    See https://meta.discourse.org/t/94797/8

--- app/assets/javascripts/discourse-common/addon/helpers/get-url.js
+++ app/assets/javascripts/discourse-common/addon/helpers/get-url.js
@@ -1,5 +1,10 @@
 import { registerUnbound } from "discourse-common/lib/helpers";
 import getUrl from "discourse-common/lib/get-url";
+import deprecated from "discourse-common/lib/deprecated";
 
 registerUnbound("get-url", value => getUrl(value));
-registerUnbound("base-url", () => getUrl(""));
+registerUnbound("base-url", () => {
+  deprecated("Use `{{base-path}}` instead of `{{base-url}}`");
+  return getUrl("");
+});
+registerUnbound("base-path", () => getUrl(""));
