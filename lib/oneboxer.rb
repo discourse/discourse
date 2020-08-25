12b9900e945e577169b1e74a4e091826a7da14e3
David Taylor <david@taylorhq.com>
2020-08-25 19:22:16 +0100

    Pass allowed iframes to Oneboxer.engine

--- lib/oneboxer.rb
+++ lib/oneboxer.rb
@@ -138,7 +138,9 @@ module Oneboxer
   end
 
   def self.engine(url)
-    Onebox::Matcher.new(url).oneboxed
+    Onebox::Matcher.new(url, {
+      allowed_iframe_regexes: Onebox::Engine.origins_to_regexes(allowed_iframe_origins)
+    }).oneboxed
   end
 
   def self.recently_failed?(url)
@@ -300,6 +302,14 @@ module Oneboxer
     @preserve_fragment_url_hosts ||= ['http://github.com']
   end
 
+  def self.allowed_iframe_origins
+    allowed = SiteSetting.allowed_onebox_iframes.split("|")
+    if allowed.include?("*")
+      allowed = Onebox::Engine.all_iframe_origins
+    end
+    allowed += SiteSetting.allowed_iframes.split("|")
+  end
+
   def self.external_onebox(url)
     Discourse.cache.fetch(onebox_cache_key(url), expires_in: 1.day) do
       fd = FinalDestination.new(url,
@@ -310,12 +320,6 @@ module Oneboxer
                               preserve_fragment_url_hosts: preserve_fragment_url_hosts)
       uri = fd.resolve
       return blank_onebox if uri.blank? || blocked_domains.map { |hostname| uri.hostname.match?(hostname) }.any?
-
-      allowed_iframe_origins = SiteSetting.allowed_onebox_iframes.split("|")
-      if allowed_iframe_origins.include?("*")
-        allowed_iframe_origins = Onebox::Engine.all_iframe_origins
-      end
-      allowed_iframe_origins += SiteSetting.allowed_iframes.split("|")
 
       options = {
         max_width: 695,
