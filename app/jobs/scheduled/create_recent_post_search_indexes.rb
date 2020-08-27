a4ec4c06e30caa60df975df34c1dd69c6298ae45
Guo Xiang Tan <gxtan1990@gmail.com>
2020-08-27 13:49:24 +0800

    Fix the build.

--- app/jobs/scheduled/create_recent_post_search_indexes.rb
+++ app/jobs/scheduled/create_recent_post_search_indexes.rb
@@ -17,7 +17,7 @@ module Jobs
         return
       end
 
-      SiteSetting.search_recent_regular_posts_offset_post_id = self.regular_offset_post_id
+      SiteSetting.search_recent_regular_posts_offset_post_id = regular_offset_post_id
 
       DB.exec(<<~SQL)
       DROP INDEX IF EXISTS temp_idx_recent_regular_post_search_data;
