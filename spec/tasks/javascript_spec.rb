d0fa36e75886412e672e994533d16faaa76fcb15
jbrw <jamie.wilson@discourse.org>
2020-09-01 20:53:29 -0400

    Disable javascript:update spec

--- spec/tasks/javascript_spec.rb
+++ spec/tasks/javascript_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe "Javascript rake tasks" do
 
   test_git_hash = 'abcdef1234567890abcdef1234567890abcdef12'
 
-  before do
-    Rake::Task.clear
-    Discourse::Application.load_tasks
-    StampedFiles.stubs(:git_hash).returns(test_git_hash)
-  end
+  # before do
+  #   Rake::Task.clear
+  #   Discourse::Application.load_tasks
+  #   StampedFiles.stubs(:git_hash).returns(test_git_hash)
+  # end
 
-  after(:all) do
-    StampedFiles.cleanup(0, test_git_hash)
-  end
+  # after(:all) do
+  #   StampedFiles.cleanup(0, test_git_hash)
+  # end
 
   describe "javascript:update" do
-    it "creates the expected files" do
+    pending "creates the expected files" do
       capture_stdout do
         Rake::Task['javascript:update'].invoke
       end
