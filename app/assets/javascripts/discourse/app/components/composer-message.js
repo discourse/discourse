bbd4207a16f07c9c11fccff65205b51a2018e22b
jjaffeux <j.jaffeux@gmail.com>
2020-09-18 13:11:29 +0200

    event simpler

--- app/assets/javascripts/discourse/app/components/composer-message.js
+++ app/assets/javascripts/discourse/app/components/composer-message.js
@@ -1,7 +1,6 @@
 import discourseComputed from "discourse-common/utils/decorators";
 import Component from "@ember/component";
 import { getOwner } from "discourse-common/lib/get-owner";
-import { action } from "@ember/object";
 
 export default Component.extend({
   classNameBindings: [":composer-popup", "hidden", "message.extraClass"],
@@ -9,10 +8,5 @@ export default Component.extend({
   @discourseComputed("message.templateName")
   layout(templateName) {
     return getOwner(this).lookup(`template:composer/${templateName}`);
-  },
-
-  @action
-  closeMessage() {
-    this.closeMessage(this.message);
   },
 });
