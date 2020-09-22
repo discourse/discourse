01eb50460d65f5568a3167af3edf7f5eaf029d83
Bianca Nenciu <nenciu.bianca@gmail.com>
2020-09-22 19:00:56 +0300

    FIX: Make search filters case insensitive in client too

--- app/assets/javascripts/discourse/app/components/search-advanced-options.js
+++ app/assets/javascripts/discourse/app/components/search-advanced-options.js
@@ -114,7 +114,7 @@ export default Component.extend({
     this.setSearchedTermValueForTags();
 
     let regExpInMatch = this.inOptions.map((option) => option.value).join("|");
-    const REGEXP_IN_MATCH = new RegExp(`(in|with):(${regExpInMatch})`);
+    const REGEXP_IN_MATCH = new RegExp(`(in|with):(${regExpInMatch})`, "i");
 
     this.setSearchedTermValue(
       "searchedTerms.in",
@@ -145,7 +145,10 @@ export default Component.extend({
     let regExpStatusMatch = this.statusOptions
       .map((status) => status.value)
       .join("|");
-    const REGEXP_STATUS_MATCH = new RegExp(`status:(${regExpStatusMatch})`);
+    const REGEXP_STATUS_MATCH = new RegExp(
+      `status:(${regExpStatusMatch})`,
+      "i"
+    );
 
     this.setSearchedTermValue(
       "searchedTerms.status",
@@ -193,7 +196,7 @@ export default Component.extend({
 
     let val = this.get(key);
     if (match.length !== 0) {
-      const userInput = match[0].replace(replaceRegEx, "");
+      const userInput = match[0].replace(replaceRegEx, "").toLowerCase();
 
       if (val !== userInput && userInput.length) {
         this.set(key, userInput);
@@ -286,7 +289,9 @@ export default Component.extend({
 
     if (match.length !== 0) {
       const existingInputWhen = this.get("searchedTerms.time.when");
-      const userInputWhen = match[0].match(REGEXP_POST_TIME_WHEN)[0];
+      const userInputWhen = match[0]
+        .match(REGEXP_POST_TIME_WHEN)[0]
+        .toLowerCase();
       const existingInputDays = this.get("searchedTerms.time.days");
       const userInputDays = match[0].replace(REGEXP_POST_TIME_PREFIX, "");
       const properties = {};
@@ -531,7 +536,7 @@ export default Component.extend({
 
   _updateSearchTermForIn() {
     let regExpInMatch = this.inOptions.map((option) => option.value).join("|");
-    const REGEXP_IN_MATCH = new RegExp(`(in|with):(${regExpInMatch})`);
+    const REGEXP_IN_MATCH = new RegExp(`(in|with):(${regExpInMatch})`, "i");
 
     const match = this.filterBlocks(REGEXP_IN_MATCH);
     const inFilter = this.get("searchedTerms.in");
@@ -559,7 +564,10 @@ export default Component.extend({
     let regExpStatusMatch = this.statusOptions
       .map((status) => status.value)
       .join("|");
-    const REGEXP_STATUS_MATCH = new RegExp(`status:(${regExpStatusMatch})`);
+    const REGEXP_STATUS_MATCH = new RegExp(
+      `status:(${regExpStatusMatch})`,
+      "i"
+    );
 
     const match = this.filterBlocks(REGEXP_STATUS_MATCH);
     const statusFilter = this.get("searchedTerms.status");
