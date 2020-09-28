cad223c0125c1b6de4bddea6d8aef3797b74c2a6
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2020-09-28 14:05:01 +0200

    DEV: fix broken backend test due to #10749 (#10758)

--- spec/components/pretty_text_spec.rb
+++ spec/components/pretty_text_spec.rb
@@ -36,7 +36,7 @@ describe PrettyText do
           <aside class="quote no-group" data-username="EvilTrout" data-post="2" data-topic="#{topic.id}">
           <div class="title">
           <div class="quote-controls"></div>
-          <a href="http://test.localhost/t/this-is-a-test-topic/#{topic.id}/2">This is a test topic <img src="/images/emoji/twitter/slight_smile.png?v=#{Emoji::EMOJI_VERSION}" title="slight_smile" alt="slight_smile" class="emoji"></a>
+          <a href="http://test.localhost/t/this-is-a-test-topic/#{topic.id}/2">This is a test topic <img width="20" height="20" src="/images/emoji/twitter/slight_smile.png?v=#{Emoji::EMOJI_VERSION}" title="slight_smile" alt="slight_smile" class="emoji"></a>
           </div>
           <blockquote>
           <p>ddd</p>
