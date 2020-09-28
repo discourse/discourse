1c5fcc6e8494d7ec4892628ccab8ed24ae7da94c
David Taylor <david@taylorhq.com>
2020-09-28 18:02:11 +0100

    Add user api key scope annotations

--- app/models/user_api_key_scope.rb
+++ app/models/user_api_key_scope.rb
@@ -2,3 +2,18 @@
 
 class UserApiKeyScope < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: user_api_key_scopes
+#
+#  id              :bigint           not null, primary key
+#  user_api_key_id :integer          not null
+#  name            :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_user_api_key_scopes_on_user_api_key_id  (user_api_key_id)
+#
