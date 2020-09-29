6ac33361c75fc6d56d5f126ff7ba1a5421df0e46
awesomerobot <kris.aubuchon@discourse.org>
2020-09-29 17:27:43 -0400

    REFACTOR: Remove position fixed from the header and use sticky instead

--- app/assets/javascripts/discourse/app/lib/offset-calculator.js
+++ app/assets/javascripts/discourse/app/lib/offset-calculator.js
@@ -3,31 +3,30 @@ export function scrollTopFor(y) {
 }
 
 export function minimumOffset() {
-  const $header = $("header.d-header");
-  const headerHeight = $header.outerHeight(true) || 0;
-  const headerPositionTop = $header.position().top;
-  return headerHeight + headerPositionTop;
+  const header = document.querySelector("header.d-header");
+  const headerHeight = header.offsetHeight;
+  return headerHeight;
 }
 
 export default function offsetCalculator() {
   const min = minimumOffset();
 
   // on mobile, just use the header
-  if ($("html").hasClass("mobile-view")) {
+  if (document.querySelector("html").classList.contains("mobile-view")) {
     return min;
   }
 
-  const $window = $(window);
-  const windowHeight = $window.height();
-  const documentHeight = $(document).height();
-  const topicBottomOffsetTop = $("#topic-bottom").offset().top;
+  const windowHeight = window.innerWidth;
+  const documentHeight = document.body.clientHeight;
+  const topicBottomOffsetTop = document.getElementById("topic-bottom")
+    .offsetTop;
 
   // the footer is bigger than the window, we can scroll down past the last post
   if (documentHeight - windowHeight > topicBottomOffsetTop) {
     return min;
   }
 
-  const scrollTop = $window.scrollTop();
+  const scrollTop = window.scrollTop;
   const visibleBottomHeight = scrollTop + windowHeight - topicBottomOffsetTop;
 
   if (visibleBottomHeight > 0) {
