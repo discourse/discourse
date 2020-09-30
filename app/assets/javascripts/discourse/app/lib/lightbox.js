0f65983d264c6c014c4aebca3d28a077c7da1870
Joe <johani@discourse.org>
2020-09-30 08:34:19 +0800

    try using lightgallery instead of magnific-popup

--- app/assets/javascripts/discourse/app/lib/lightbox.js
+++ app/assets/javascripts/discourse/app/lib/lightbox.js
@@ -1,9 +1,5 @@
-import I18n from "I18n";
 import loadScript from "discourse/lib/load-script";
-import { escapeExpression } from "discourse/lib/utilities";
-import { renderIcon } from "discourse-common/lib/icon-library";
-import { isAppWebview, postRNWebviewMessage } from "discourse/lib/utilities";
-import { spinnerHTML } from "discourse/helpers/loading-spinner";
+import { iconHTML } from "discourse-common/lib/icon-library";
 import User from "discourse/models/user";
 
 export default function (elem, siteSettings) {
@@ -11,86 +7,109 @@ export default function (elem, siteSettings) {
     return;
   }
 
-  loadScript("/javascripts/jquery.magnific-popup.min.js").then(function () {
-    const lightboxes = elem.querySelectorAll(
-      "*:not(.spoiler):not(.spoiled) a.lightbox"
-    );
-    $(lightboxes).magnificPopup({
-      type: "image",
-      closeOnContentClick: false,
-      removalDelay: 300,
-      mainClass: "mfp-zoom-in",
-      tClose: I18n.t("lightbox.close"),
-      tLoading: spinnerHTML,
+  loadScript("/javascripts/light-gallery/lightgallery.min.js").then(() => {
+    loadScript("/javascripts/light-gallery/lg-zoom.min.js");
 
-      gallery: {
-        enabled: true,
-        tPrev: I18n.t("lightbox.previous"),
-        tNext: I18n.t("lightbox.next"),
-        tCounter: I18n.t("lightbox.counter"),
-      },
-
-      ajax: {
-        tError: I18n.t("lightbox.content_load_error"),
-      },
-
-      callbacks: {
-        open() {
-          const wrap = this.wrap,
-            img = this.currItem.img,
-            maxHeight = img.css("max-height");
-
-          wrap.on("click.pinhandler", "img", function () {
-            wrap.toggleClass("mfp-force-scrollbars");
-            img.css(
-              "max-height",
-              wrap.hasClass("mfp-force-scrollbars") ? "none" : maxHeight
-            );
-          });
-
-          if (isAppWebview()) {
-            postRNWebviewMessage(
-              "headerBg",
-              $(".mfp-bg").css("background-color")
-            );
-          }
-        },
-        beforeClose() {
-          this.wrap.off("click.pinhandler");
-          this.wrap.removeClass("mfp-force-scrollbars");
-          if (isAppWebview()) {
-            postRNWebviewMessage(
-              "headerBg",
-              $(".d-header").css("background-color")
-            );
-          }
-        },
-      },
-
-      image: {
-        tError: I18n.t("lightbox.image_load_error"),
-        titleSrc(item) {
-          const href = item.el.data("download-href") || item.src;
-          let src = [
-            escapeExpression(item.el.attr("title")),
-            $("span.informations", item.el).text(),
-          ];
-          if (
-            !siteSettings.prevent_anons_from_downloading_files ||
-            User.current()
-          ) {
-            src.push(
-              '<a class="image-source-link" href="' +
-                href +
-                '">' +
-                renderIcon("string", "download") +
-                I18n.t("lightbox.download") +
-                "</a>"
-            );
-          }
-          return src.join(" &middot; ");
-        },
-      },
+    lightGallery(elem, {
+      selector: "*:not(.spoiler):not(.spoiled) a.lightbox",
+      mode: "lg-fade",
+      speed: "300",
+      cssEasing: "cubic-bezier(0.25, 0, 0.25, 1)",
+      startClass: "lg-start-fade",
+      showAfterLoad: false,
+      supportLegacyBrowser: false,
+      preload: 5,
+      nextHtml: iconHTML("chevron-right"),
+      prevHtml: iconHTML("chevron-left"),
+      download:
+        !siteSettings.prevent_anons_from_downloading_files || User.current(),
     });
   });
+
+  /*
+
+loadScript("/javascripts/jquery.magnific-popup.min.js").then(function () {
+  const lightboxes = elem.querySelectorAll(
+    "*:not(.spoiler):not(.spoiled) a.lightbox"
+  );
+  $(lightboxes).magnificPopup({
+    type: "image",
+    closeOnContentClick: false,
+    removalDelay: 300,
+    mainClass: "mfp-zoom-in",
+    tClose: I18n.t("lightbox.close"),
+    tLoading: spinnerHTML,
+
+    gallery: {
+      enabled: true,
+      tPrev: I18n.t("lightbox.previous"),
+      tNext: I18n.t("lightbox.next"),
+      tCounter: I18n.t("lightbox.counter"),
+    },
+
+    ajax: {
+      tError: I18n.t("lightbox.content_load_error"),
+    },
+
+    callbacks: {
+      open() {
+        const wrap = this.wrap,
+          img = this.currItem.img,
+          maxHeight = img.css("max-height");
+
+        wrap.on("click.pinhandler", "img", function () {
+          wrap.toggleClass("mfp-force-scrollbars");
+          img.css(
+            "max-height",
+            wrap.hasClass("mfp-force-scrollbars") ? "none" : maxHeight
+          );
+        });
+
+        if (isAppWebview()) {
+          postRNWebviewMessage(
+            "headerBg",
+            $(".mfp-bg").css("background-color")
+          );
+        }
+      },
+      beforeClose() {
+        this.wrap.off("click.pinhandler");
+        this.wrap.removeClass("mfp-force-scrollbars");
+        if (isAppWebview()) {
+          postRNWebviewMessage(
+            "headerBg",
+            $(".d-header").css("background-color")
+          );
+        }
+      },
+    },
+
+    image: {
+      tError: I18n.t("lightbox.image_load_error"),
+      titleSrc(item) {
+        const href = item.el.data("download-href") || item.src;
+        let src = [
+          escapeExpression(item.el.attr("title")),
+          $("span.informations", item.el).text(),
+        ];
+        if (
+          !siteSettings.prevent_anons_from_downloading_files ||
+          User.current()
+        ) {
+          src.push(
+            '<a class="image-source-link" href="' +
+              href +
+              '">' +
+              renderIcon("string", "download") +
+              I18n.t("lightbox.download") +
+              "</a>"
+          );
+        }
+        return src.join(" &middot; ");
+      },
+    },
+  });
+});
+
+*/
 }
