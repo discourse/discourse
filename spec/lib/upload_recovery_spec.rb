74b04de4471b27d6b799fad7f37d73a0c42ea39d
Jarek Radosz <jradosz@gmail.com>
2020-10-01 13:06:05 +0200

    Spec the regular S3 path
    For the sake of completeness

--- spec/lib/upload_recovery_spec.rb
+++ spec/lib/upload_recovery_spec.rb
@@ -113,15 +113,16 @@ RSpec.describe UploadRecovery do
         .to eq(File.read(file_from_fixtures("smallest.png")))
     end
 
-    describe 'when the upload exists but its file is missing' do
+    context 'S3 store' do
       before do
         setup_s3
         stub_s3_store
       end
 
-      it 'recovers the file' do
-        upload.verification_status = Upload.verification_statuses[:invalid_etag]
-        upload.save!
+      it 'recovers the upload' do
+        expect do
+          upload.destroy!
+        end.to change { post.reload.uploads.count }.from(1).to(0)
 
         original_key = Discourse.store.get_path_for_upload(upload)
         tombstone_key = original_key.sub("original", "tombstone/original")
@@ -133,29 +134,53 @@ RSpec.describe UploadRecovery do
         Discourse.store.s3_helper.expects(:list).with("#{FileStore::S3Store::TOMBSTONE_PREFIX}original").returns([tombstone_copy])
         Discourse.store.s3_helper.expects(:copy).with(tombstone_key, original_key, options: { acl: "public-read" })
 
+        FileHelper.expects(:download).returns(file_from_fixtures("smallest.png"))
+        stub_request(:get, upload.url).to_return(body: file_from_fixtures("smallest.png"))
+
         expect do
           upload_recovery.recover
-        end.to_not change { [post.reload.uploads.count, Upload.count] }
+        end.to change { post.reload.uploads.count }.from(0).to(1)
       end
 
-      it 'does not create a duplicate upload when secure uploads are enabled' do
-        SiteSetting.secure_media = true
-        upload.verification_status = Upload.verification_statuses[:invalid_etag]
-        upload.save!
+      describe 'when the upload exists but its file is missing' do
+        it 'recovers the file' do
+          upload.verification_status = Upload.verification_statuses[:invalid_etag]
+          upload.save!
 
-        original_key = Discourse.store.get_path_for_upload(upload)
-        tombstone_key = original_key.sub("original", "tombstone/original")
+          original_key = Discourse.store.get_path_for_upload(upload)
+          tombstone_key = original_key.sub("original", "tombstone/original")
 
-        tombstone_copy = stub
-        tombstone_copy.expects(:key).returns(tombstone_key)
+          tombstone_copy = stub
+          tombstone_copy.expects(:key).returns(tombstone_key)
 
-        Discourse.store.s3_helper.expects(:list).with("original").returns([])
-        Discourse.store.s3_helper.expects(:list).with("#{FileStore::S3Store::TOMBSTONE_PREFIX}original").returns([tombstone_copy])
-        Discourse.store.s3_helper.expects(:copy).with(tombstone_key, original_key, options: { acl: "public-read" })
+          Discourse.store.s3_helper.expects(:list).with("original").returns([])
+          Discourse.store.s3_helper.expects(:list).with("#{FileStore::S3Store::TOMBSTONE_PREFIX}original").returns([tombstone_copy])
+          Discourse.store.s3_helper.expects(:copy).with(tombstone_key, original_key, options: { acl: "public-read" })
 
-        expect do
-          upload_recovery.recover
-        end.to_not change { [post.reload.uploads.count, Upload.count] }
+          expect do
+            upload_recovery.recover
+          end.to_not change { [post.reload.uploads.count, Upload.count] }
+        end
+
+        it 'does not create a duplicate upload when secure uploads are enabled' do
+          SiteSetting.secure_media = true
+          upload.verification_status = Upload.verification_statuses[:invalid_etag]
+          upload.save!
+
+          original_key = Discourse.store.get_path_for_upload(upload)
+          tombstone_key = original_key.sub("original", "tombstone/original")
+
+          tombstone_copy = stub
+          tombstone_copy.expects(:key).returns(tombstone_key)
+
+          Discourse.store.s3_helper.expects(:list).with("original").returns([])
+          Discourse.store.s3_helper.expects(:list).with("#{FileStore::S3Store::TOMBSTONE_PREFIX}original").returns([tombstone_copy])
+          Discourse.store.s3_helper.expects(:copy).with(tombstone_key, original_key, options: { acl: "public-read" })
+
+          expect do
+            upload_recovery.recover
+          end.to_not change { [post.reload.uploads.count, Upload.count] }
+        end
       end
     end
 
