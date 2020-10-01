52a9a5878ebb94dd1c0db5d5f64351e8f6512797
Joe <johani@discourse.org>
2020-10-02 06:01:30 +0800

    lightbox function improvements

--- app/assets/javascripts/discourse/app/lib/lightbox.js
+++ app/assets/javascripts/discourse/app/lib/lightbox.js
@@ -2,29 +2,39 @@ import loadScript, { loadCSS } from "discourse/lib/load-script";
 import { iconHTML } from "discourse-common/lib/icon-library";
 import User from "discourse/models/user";
 
+const LIGHTBOX_SELECTOR = "*:not(.spoiler):not(.spoiled) a.lightbox";
+
 export default function (elem, siteSettings) {
-  const isRTL = document.querySelector("html").classList.contains("rtl");
+  if (!elem) {
+    return;
+  }
 
-  const options = {
-    mode: "lg-fade",
-    startClass: "", // prevents default zoom transition
-    selector: "*:not(.spoiler):not(.spoiled) a.lightbox",
-    preload: 3, // how many extra images to preload when opened
-    nextHtml: isRTL ? iconHTML("chevron-left") : iconHTML("chevron-right"),
-    prevHtml: isRTL ? iconHTML("chevron-right") : iconHTML("chevron-left"),
-    download:
-      !siteSettings.prevent_anons_from_downloading_files || User.current(),
-  };
+  const hasLightboxes = elem.querySelectorAll(LIGHTBOX_SELECTOR).length;
 
-  // main lib
-  loadScript("/javascripts/light-gallery/lightgallery.min.js").then(() => {
-    // lib zoom module
-    loadScript("/javascripts/light-gallery/lg-zoom.min.js").then(() => {
-      // lib base css
-      loadCSS("/javascripts/light-gallery/lightgallery.min.css").then(() => {
-        // eslint-disable-next-line
-        lightGallery(elem, options);
+  if (hasLightboxes) {
+    const isRTL = document.querySelector("html").classList.contains("rtl");
+
+    const options = {
+      mode: "lg-fade",
+      startClass: "", // prevents default zoom transition
+      selector: LIGHTBOX_SELECTOR,
+      preload: 3, // how many extra images to preload when opened
+      nextHtml: isRTL ? iconHTML("chevron-left") : iconHTML("chevron-right"),
+      prevHtml: isRTL ? iconHTML("chevron-right") : iconHTML("chevron-left"),
+      download:
+        !siteSettings.prevent_anons_from_downloading_files || User.current(),
+    };
+
+    // main lib
+    loadScript("/javascripts/light-gallery/lightgallery.min.js").then(() => {
+      // lib zoom module
+      loadScript("/javascripts/light-gallery/lg-zoom.min.js").then(() => {
+        // lib base css
+        loadCSS("/javascripts/light-gallery/lightgallery.min.css").then(() => {
+          // eslint-disable-next-line
+          lightGallery(elem, options);
+        });
       });
     });
-  });
+  }
 }
