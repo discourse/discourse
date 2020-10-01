d1410b20b38c93ba78e813024e3c00954fc17143
Joe <johani@discourse.org>
2020-10-01 11:27:00 +0800

    allow themes to set lightbox options

--- app/assets/javascripts/discourse/app/lib/lightbox.js
+++ app/assets/javascripts/discourse/app/lib/lightbox.js
@@ -3,22 +3,28 @@ import { iconHTML } from "discourse-common/lib/icon-library";
 import { isRTL } from "discourse/lib/text-direction";
 import User from "discourse/models/user";
 
-const LIGHTBOX_SELECTOR = "*:not(.spoiler):not(.spoiled) a.lightbox";
-const NEXT_ICON = isRTL()
-  ? iconHTML("chevron-left")
-  : iconHTML("chevron-right");
-const PREV_ICON = isRTL()
-  ? iconHTML("chevron-right")
-  : iconHTML("chevron-left");
-const PRELOAD_COUNT = 10;
-
 export default function (elem, siteSettings) {
-  if (!elem) {
-    return;
-  }
-
-  const showDownloadIcon =
+  // Discourse defaults
+  const LIGHTBOX_SELECTOR = "*:not(.spoiler):not(.spoiled) a.lightbox";
+  const PRELOAD_COUNT = 10;
+  const SHOW_DOWNLOAD_ICON =
     !siteSettings.prevent_anons_from_downloading_files || User.current();
+  const NEXT_ICON = isRTL()
+    ? iconHTML("chevron-left")
+    : iconHTML("chevron-right");
+  const PREV_ICON = isRTL()
+    ? iconHTML("chevron-right")
+    : iconHTML("chevron-left");
+
+  // window.lightboxOptions is for theme extensibility, We fallback to the
+  // defaults defined above
+  const options = window.lightboxOptions || {
+    selector: LIGHTBOX_SELECTOR,
+    preload: PRELOAD_COUNT,
+    nextHtml: NEXT_ICON,
+    prevHtml: PREV_ICON,
+    download: SHOW_DOWNLOAD_ICON,
+  };
 
   // main lib
   loadScript("/javascripts/light-gallery/lightgallery.min.js").then(() => {
@@ -27,16 +33,7 @@ export default function (elem, siteSettings) {
       // lib base css
       loadCSS("/javascripts/light-gallery/lightgallery.min.css").then(() => {
         // eslint-disable-next-line
-        lightGallery(elem, {
-          mode: "lg-fade", // default is ugly
-          startClass: "", // default is ugly
-          selector: LIGHTBOX_SELECTOR,
-          preload: PRELOAD_COUNT,
-          nextHtml: NEXT_ICON,
-          prevHtml: PREV_ICON,
-          enableDrag: false, // no support for RTL for now
-          download: showDownloadIcon,
-        });
+        lightGallery(elem, options);
       });
     });
   });
