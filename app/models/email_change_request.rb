40de36f3b54328ded946a7c3096c9d3dcbb1e1cd
Martin Brennan <martin@discourse.org>
2020-10-06 13:50:50 +1000

    Add requested by to email change requests

--- app/models/email_change_request.rb
+++ app/models/email_change_request.rb
@@ -4,6 +4,7 @@ class EmailChangeRequest < ActiveRecord::Base
   belongs_to :old_email_token, class_name: 'EmailToken'
   belongs_to :new_email_token, class_name: 'EmailToken'
   belongs_to :user
+  belongs_to :requested_by, class_name: "User", foreign_key: :requested_by_user_id
 
   validates :new_email, presence: true, format: { with: EmailValidator.email_regex }
 
@@ -11,23 +12,32 @@ class EmailChangeRequest < ActiveRecord::Base
     @states ||= Enum.new(authorizing_old: 1, authorizing_new: 2, complete: 3)
   end
 
+  def requested_by_admin?
+    self.requested_by.staff?
+  end
+
+  def self_requested?
+    self.requested_by == self.user
+  end
 end
 
 # == Schema Information
 #
 # Table name: email_change_requests
 #
-#  id                 :integer          not null, primary key
-#  user_id            :integer          not null
-#  old_email          :string
-#  new_email          :string           not null
-#  old_email_token_id :integer
-#  new_email_token_id :integer
-#  change_state       :integer          not null
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
+#  id                   :integer          not null, primary key
+#  user_id              :integer          not null
+#  old_email            :string
+#  new_email            :string           not null
+#  old_email_token_id   :integer
+#  new_email_token_id   :integer
+#  change_state         :integer          not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  requested_by_user_id :integer
 #
 # Indexes
 #
-#  index_email_change_requests_on_user_id  (user_id)
+#  idx_email_change_requests_on_requested_by  (requested_by_user_id)
+#  index_email_change_requests_on_user_id     (user_id)
 #
