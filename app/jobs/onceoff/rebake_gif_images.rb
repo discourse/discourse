ae03540150203925ba01700744c7da1f3bffbb94
Bianca Nenciu <nenciu.bianca@gmail.com>
2020-10-06 23:36:42 +0300

    DEV: Apply code suggestions

--- app/jobs/onceoff/rebake_gif_images.rb
+++ app/jobs/onceoff/rebake_gif_images.rb
@@ -11,26 +11,25 @@ module Jobs
       User
         .joins(:uploaded_avatar)
         .where("uploads.original_filename LIKE '%.gif'")
+        .where("uploads.animated")
         .find_each do |user|
 
-        if user.uploaded_avatar.animated
-          if path = Discourse.store.path_for(user.uploaded_avatar)
-            file = File.new(path)
-          else
-            find = Discourse.store.download(user.uploaded_avatar)
-          end
-
-          # We want to reupload the avatar, but we want to keep the avatar
-          # until the new one is ready, so we will have to keep it under a
-          # different hash (like we do for secure media).
-          user.uploaded_avatar.update!(sha1: SecureRandom.hex(20))
-
-          upload = UploadCreator.new(file, user.uploaded_avatar.original_filename, type: "avatar").create_for(user.id)
-          user.update!(uploaded_avatar: upload)
+        if path = Discourse.store.path_for(user.uploaded_avatar)
+          file = File.new(path)
+        else
+          file = Discourse.store.download(user.uploaded_avatar)
         end
+
+        # We want to reupload the avatar, but we also want to keep the
+        # avatar until the new one is ready, so we will have to keep it
+        # under a different hash (like we do for secure media).
+        user.uploaded_avatar.update!(sha1: SecureRandom.hex(20))
+
+        upload = UploadCreator.new(file, user.uploaded_avatar.original_filename, type: "avatar").create_for(user.id)
+        user.update!(uploaded_avatar: upload)
       end
 
-      # Destroy all optimized image for animated GIFs and let the user
+      # Destroy all optimized image for animated GIFs and let the system
       # recreate them on demand.
       OptimizedImage
         .joins(:upload)
