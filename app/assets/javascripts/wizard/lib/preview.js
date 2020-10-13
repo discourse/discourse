discourse/discourse

commit a4356b99af0a7f4cd746b5d09960ab7635fe91a4

FEATURE: Separate base and heading font site_settings (#10807)
Allows site administrators to pick different fonts for headings in the wizard and in their site settings. Also correctly displays the header logos in wizard previews.

app/assets/javascripts/wizard/lib/preview.js
@@ -93,6 +93,10 @@ export function createPreviewComponent(width, height, obj) {
         }
 
         const font = this.wizard.getCurrentFont(this.fontId);
+        const headingFont = this.wizard.getCurrentFont(
+          this.fontId,
+          "heading_font"
+        );
         if (!font) {
           return;
         }
@@ -102,7 +106,15 @@ export function createPreviewComponent(width, height, obj) {
         ctx.fillStyle = colors.secondary;
         ctx.fillRect(0, 0, width, height);
 
-        this.paint(ctx, colors, font, this.width, this.height);
+        const options = {
+          ctx,
+          colors,
+          font,
+          headingFont,
+          width: this.width,
+          height: this.height,
+        };
+        this.paint(options);
 
         // draw border
         ctx.beginPath();
@@ -142,7 +154,7 @@ export function createPreviewComponent(width, height, obj) {
         ctx.drawImage(scaled[key], x, y, w, h);
       },
 
-      drawFullHeader(colors, font) {
+      drawFullHeader(colors, font, logo) {
         const { ctx } = this;
 
         const headerHeight = height * 0.15;
@@ -154,10 +166,16 @@ export function createPreviewComponent(width, height, obj) {
         const headerMargin = headerHeight * 0.2;
         const logoHeight = headerHeight - headerMargin * 2;
 
-        ctx.beginPath();
-        ctx.fillStyle = colors.header_primary;
-        ctx.font = `bold ${logoHeight}px '${font}'`;
-        ctx.fillText("Discourse", headerMargin, headerHeight - headerMargin);
+        const ratio = logoHeight / logo.height;
+        this.scaleImage(
+          logo,
+          headerMargin,
+          headerMargin,
+          logo.width * ratio,
+          logoHeight
+        );
+
+        this.scaleImage(logo, width, headerMargin);
 
         // Top right menu
         this.scaleImage(

app/assets/javascripts/wizard/components/font-preview.js
@@ -1,30 +1,26 @@
 import I18n from "I18n";
-import discourseComputed from "discourse-common/utils/decorators";
 import { observes } from "discourse-common/utils/decorators";
 import {
   createPreviewComponent,
   darkLightDiff,
   chooseDarker,
-  LOREM,
 } from "wizard/lib/preview";
 
-export default createPreviewComponent(305, 165, {
+const LOREM = `
+Lorem ipsum dolor sit amet, consectetur adipiscing.
+Nullam eget sem non elit tincidunt rhoncus. Fusce
+velit nisl, porttitor sed nisl ac, consectetur interdum
+metus. Fusce in consequat augue, vel facilisis felis.`;
+
+export default createPreviewComponent(659, 320, {
   logo: null,
   avatar: null,
 
-  classNameBindings: ["isSelected"],
-
-  @discourseComputed("selectedId", "fontId")
-  isSelected(selectedId, fontId) {
-    return selectedId === fontId;
-  },
-
-  click() {
-    this.onChange(this.fontId);
-  },
-
-  @observes("step.fieldsById.base_scheme_id.value")
-  themeChanged() {
+  @observes(
+    "step.fieldsById.body_font.value",
+    "step.fieldsById.heading_font.value"
+  )
+  fontChanged() {
     this.triggerRepaint();
   },
 
@@ -35,47 +31,44 @@ export default createPreviewComponent(305, 165, {
     };
   },
 
-  paint(ctx, colors, font, width, height) {
+  paint({ ctx, colors, font, headingFont, width, height }) {
     const headerHeight = height * 0.3;
-    this.drawFullHeader(colors, font);
+
+    this.drawFullHeader(colors, headingFont, this.logo);
 
     const margin = width * 0.04;
     const avatarSize = height * 0.2;
-    const lineHeight = height / 9.5;
+    const lineHeight = height / 11;
 
     // Draw a fake topic
     this.scaleImage(
       this.avatar,
       margin,
-      headerHeight + height * 0.085,
+      headerHeight + height * 0.11,
       avatarSize,
       avatarSize
     );
 
-    const titleFontSize = headerHeight / 44;
+    const titleFontSize = headerHeight / 55;
 
     ctx.beginPath();
     ctx.fillStyle = colors.primary;
-    ctx.font = `bold ${titleFontSize}em '${font}'`;
-    ctx.fillText(
-      I18n.t("wizard.previews.font_title", { font }),
-      margin,
-      height * 0.3
-    );
+    ctx.font = `bold ${titleFontSize}em '${headingFont}'`;
+    ctx.fillText(I18n.t("wizard.previews.topic_title"), margin, height * 0.3);
 
-    const bodyFontSize = height / 220.0;
+    const bodyFontSize = height / 330.0;
     ctx.font = `${bodyFontSize}em '${font}'`;
 
     let line = 0;
     const lines = LOREM.split("\n");
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 5; i++) {
       line = height * 0.35 + i * lineHeight;
       ctx.fillText(lines[i], margin + avatarSize + margin, line);
     }
 
     // Share Button
     ctx.beginPath();
-    ctx.rect(margin, line + lineHeight, width * 0.14, height * 0.14);
+    ctx.rect(margin, line + lineHeight, width * 0.1, height * 0.12);
     ctx.fillStyle = darkLightDiff(colors.primary, colors.secondary, 90, 65);
     ctx.fill();
     ctx.fillStyle = chooseDarker(colors.primary, colors.secondary);
@@ -89,10 +82,10 @@ export default createPreviewComponent(305, 165, {
     // Reply Button
     ctx.beginPath();
     ctx.rect(
-      margin * 2 + width * 0.14,
+      margin + width * 0.12,
       line + lineHeight,
-      width * 0.14,
-      height * 0.14
+      width * 0.1,
+      height * 0.12
     );
     ctx.fillStyle = colors.tertiary;
     ctx.fill();
@@ -100,12 +93,12 @@ export default createPreviewComponent(305, 165, {
     ctx.font = `${bodyFontSize}em '${font}'`;
     ctx.fillText(
       I18n.t("wizard.previews.reply_button"),
-      margin * 2 + width * 0.14 + width / 55,
+      margin + width * 0.12 + width / 55,
       line + lineHeight * 1.85
     );
 
     // Draw Timeline
-    const timelineX = width * 0.8;
+    const timelineX = width * 0.86;
     ctx.beginPath();
     ctx.strokeStyle = colors.tertiary;
     ctx.lineWidth = 0.5;

app/assets/javascripts/wizard/components/font-previews.js
@@ -1,8 +1,0 @@
-import Component from "@ember/component";
-export default Component.extend({
-  actions: {
-    changed(value) {
-      this.set("field.value", value);
-    },
-  },
-});

app/assets/javascripts/wizard/components/homepage-preview.js
@@ -21,8 +21,8 @@ export default createPreviewComponent(659, 320, {
     };
   },
 
-  paint(ctx, colors, font, width, height) {
-    this.drawFullHeader(colors, font);
+  paint({ ctx, colors, font, width, height }) {
+    this.drawFullHeader(colors, font, this.logo);
 
     if (this.get("step.fieldsById.homepage_style.value") === "latest") {
       this.drawPills(colors, font, height * 0.15);

app/assets/javascripts/wizard/components/image-preview-favicon.js
@@ -14,7 +14,8 @@ export default createPreviewComponent(371, 124, {
     return { tab: "/images/wizard/tab.png", image: this.get("field.value") };
   },
 
-  paint(ctx, colors, font, width, height) {
+  paint(options) {
+    const { ctx, width, height } = options;
     this.scaleImage(this.tab, 0, 0, width, height);
     this.scaleImage(this.image, 40, 25, 30, 30);
 

app/assets/javascripts/wizard/components/image-preview-large-icon.js
@@ -17,7 +17,8 @@ export default createPreviewComponent(325, 125, {
     };
   },
 
-  paint(ctx, colors, font, width, height) {
+  paint(options) {
+    const { width, height } = options;
     this.scaleImage(this.image, 10, 8, 87, 87);
     this.scaleImage(this.ios, 0, 0, width, height);
   },

app/assets/javascripts/wizard/components/image-preview-logo-small.js
@@ -13,7 +13,8 @@ export default createPreviewComponent(375, 100, {
     return { image: this.get("field.value") };
   },
 
-  paint(ctx, colors, font, width, height) {
+  paint(options) {
+    const { ctx, colors, font, headingFont, width, height } = options;
     const headerHeight = height / 2;
 
     drawHeader(ctx, colors, width, headerHeight);
@@ -39,7 +40,8 @@ export default createPreviewComponent(375, 100, {
 
     const afterLogo = headerMargin * 1.7 + imageWidth;
     const fontSize = Math.round(headerHeight * 0.4);
-    ctx.font = `Bold ${fontSize}px '${font}'`;
+
+    ctx.font = `Bold ${fontSize}px '${headingFont}'`;
     ctx.fillStyle = colors.primary;
     const title = LOREM.substring(0, 27);
     ctx.fillText(

app/assets/javascripts/wizard/components/image-preview-logo.js
@@ -13,12 +13,13 @@ export default createPreviewComponent(400, 100, {
     return { image: this.get("field.value") };
   },
 
-  paint(ctx, colors, font, width, height) {
+  paint({ ctx, colors, font, width, height }) {
     const headerHeight = height / 2;
 
     drawHeader(ctx, colors, width, headerHeight);
 
     const image = this.image;
+
     const headerMargin = headerHeight * 0.2;
 
     const imageHeight = headerHeight - headerMargin * 2;

commit 24029e916d5e9a17d6ea644835158120810404d5

FIX: Wizard theme preview when logo is missing
Fixes empty theme previews in the wizard, a bug introduced by yours
truly in a4356b99af0a7f4cd746b5d09960ab7635fe91a4

app/assets/javascripts/wizard/lib/preview.js
@@ -161,21 +161,22 @@ export function createPreviewComponent(width, height, obj) {
         drawHeader(ctx, colors, width, headerHeight);
 
         const avatarSize = height * 0.1;
-
-        // Logo
         const headerMargin = headerHeight * 0.2;
-        const logoHeight = headerHeight - headerMargin * 2;
 
-        const ratio = logoHeight / logo.height;
-        this.scaleImage(
-          logo,
-          headerMargin,
-          headerMargin,
-          logo.width * ratio,
-          logoHeight
-        );
+        if (logo) {
+          const logoHeight = headerHeight - headerMargin * 2;
 
-        this.scaleImage(logo, width, headerMargin);
+          const ratio = logoHeight / logo.height;
+          this.scaleImage(
+            logo,
+            headerMargin,
+            headerMargin,
+            logo.width * ratio,
+            logoHeight
+          );
+
+          this.scaleImage(logo, width, headerMargin);
+        }
 
         // Top right menu
         this.scaleImage(