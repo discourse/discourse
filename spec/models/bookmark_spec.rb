7d46de9c1586337b87e6b35b7d0d5d5b5f322adf
Martin Brennan <martin@discourse.org>
2020-10-13 11:07:14 +1000

    Improve spec

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -7,7 +7,7 @@ describe Bookmark do
     it "deletes bookmarks attached to a deleted post which has been deleted for > 3 days" do
       post = Fabricate(:post)
       bookmark = Fabricate(:bookmark, post: post, topic: post.topic)
-      bookmark2 = Fabricate(:bookmark)
+      bookmark2 = Fabricate(:bookmark, post: Fabricate(:post, topic: post.topic))
       post.trash!
       post.update(deleted_at: 4.days.ago)
       Bookmark.cleanup!
@@ -18,12 +18,14 @@ describe Bookmark do
     it "deletes bookmarks attached to a deleted topic which has been deleted for > 3 days" do
       post = Fabricate(:post)
       bookmark = Fabricate(:bookmark, post: post, topic: post.topic)
-      bookmark2 = Fabricate(:bookmark)
+      bookmark2 = Fabricate(:bookmark, topic: post.topic, post: Fabricate(:post, topic: post.topic))
+      bookmark3 = Fabricate(:bookmark)
       post.topic.trash!
       post.topic.update(deleted_at: 4.days.ago)
       Bookmark.cleanup!
       expect(Bookmark.find_by(id: bookmark.id)).to eq(nil)
-      expect(Bookmark.find_by(id: bookmark2.id)).to eq(bookmark2)
+      expect(Bookmark.find_by(id: bookmark2.id)).to eq(nil)
+      expect(Bookmark.find_by(id: bookmark3.id)).to eq(bookmark3)
     end
 
     it "does not delete bookmarks attached to posts that are not deleted or that have not met the 3 day grace period" do
