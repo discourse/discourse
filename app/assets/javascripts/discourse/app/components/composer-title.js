7dbad0ba06f047fe2202d8f0d7a2a39e6acede9c
Gerhard Schlager <gerhard.schlager@discourse.org>
2020-10-19 00:10:42 +0200

    Pluralize `js.composer.error.title_too_short`
    This fixes https://discourse.crowdin.com/translate/f3230e7607a36bb0a2f97fd90605a44e/246/en-ar#41172

--- app/assets/javascripts/discourse/app/components/composer-title.js
+++ app/assets/javascripts/discourse/app/components/composer-title.js
@@ -44,7 +44,7 @@ export default Component.extend({
       reason = I18n.t("composer.error.title_missing");
     } else if (missingTitleChars > 0) {
       reason = I18n.t("composer.error.title_too_short", {
-        min: minimumTitleLength,
+        count: minimumTitleLength,
       });
     } else if (titleLength > this.siteSettings.max_topic_title_length) {
       reason = I18n.t("composer.error.title_too_long", {
