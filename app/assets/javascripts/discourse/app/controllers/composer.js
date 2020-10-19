c8e0547bcc673031ba65baf3664b4813a50c8fad
Sam <sam.saffron@gmail.com>
2020-10-19 17:21:50 +1100

    FEATURE: CTRL-SHIFT-ENTER and SHIFT-Click do not scroll on post (#10939)
    * FEATURE: CTRL-SHIFT-ENTER and SHIFT-Click do not scroll on post
    
    This allows for an advanced feature where hitting control on click or
    CTRL-SHIFT-ENTER will lead to a post being made but the browser not to
    scroll to the end.

--- app/assets/javascripts/discourse/app/controllers/composer.js
+++ app/assets/javascripts/discourse/app/controllers/composer.js
@@ -546,8 +546,8 @@ export default Controller.extend({
       this.cancelComposer(differentDraftContext);
     },
 
-    save() {
-      this.save();
+    save(ignore, event) {
+      this.save(false, { jump: !(event && event.shiftKey) });
     },
 
     displayEditReason() {
@@ -625,7 +625,7 @@ export default Controller.extend({
 
   disableSubmit: or("model.loading", "isUploading"),
 
-  save(force) {
+  save(force, options = {}) {
     if (this.disableSubmit) {
       return;
     }
@@ -763,7 +763,8 @@ export default Controller.extend({
         this.currentUser.set("any_posts", true);
 
         const post = result.target;
-        if (post && !staged) {
+
+        if (post && !staged && options.jump !== false) {
           DiscourseURL.routeTo(post.url, { skipIfOnScreen: true });
         }
       })
