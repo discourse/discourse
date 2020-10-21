6866de87a58799b9c6de9f88bb62f8bef54fe93c
Vinoth Kannan <svkn.87@gmail.com>
2020-10-21 23:23:20 +0530

    UX: add stock suspension reasons to suspend dialog.

--- app/assets/javascripts/admin/addon/components/suspension-details.js
+++ app/assets/javascripts/admin/addon/components/suspension-details.js
@@ -1,4 +1,44 @@
 import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+import I18n from "I18n";
+import { observes } from "discourse-common/utils/decorators";
+
+const CUSTOM_REASON_KEY = "custom";
+
 export default Component.extend({
   tagName: "",
+  selectedReason: CUSTOM_REASON_KEY,
+  customReason: "",
+  reasonKeys: [
+    "not_listening_to_staff",
+    "consuming_staff_time",
+    "combatative",
+    "in_wrong_place",
+    "no_constructive_purpose",
+    CUSTOM_REASON_KEY,
+  ],
+
+  @discourseComputed("reasonKeys")
+  reasons(keys) {
+    return keys.map((key) => {
+      return { id: key, name: I18n.t(`admin.user.suspend_reasons.${key}`) };
+    });
+  },
+
+  @discourseComputed("selectedReason")
+  isCustomReason(selectedReason) {
+    return selectedReason === CUSTOM_REASON_KEY;
+  },
+
+  @observes("isCustomReason", "customReason", "selectedReason")
+  setReason() {
+    if (this.isCustomReason) {
+      this.set("reason", this.customReason);
+    } else {
+      this.set(
+        "reason",
+        I18n.t(`admin.user.suspend_reasons.${this.selectedReason}`)
+      );
+    }
+  },
 });
