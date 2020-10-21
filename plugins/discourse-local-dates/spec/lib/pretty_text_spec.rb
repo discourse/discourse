f4842ae28337fc75c6be368f1497d0ba8937e24f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2020-10-21 16:36:51 +0200

    FIX: attempts to convert french quotes to regular quotes in dates (#10986)

--- plugins/discourse-local-dates/spec/lib/pretty_text_spec.rb
+++ plugins/discourse-local-dates/spec/lib/pretty_text_spec.rb
@@ -89,4 +89,13 @@ describe PrettyText do
       expect(excerpt).to eq('Wednesday, October 16, 2019 6:00 PM (UTC)')
     end
   end
+
+  context 'french quotes' do
+    let(:post) { Fabricate(:post, raw: '[date=2019-10-16 time=14:00:00 format="LLLL" timezone=«America/New_York»]') }
+
+    it 'converts french quotes to regular quotes' do
+      excerpt = PrettyText.excerpt(post.cooked, 200)
+      expect(excerpt).to eq('Wednesday, October 16, 2019 6:00 PM (UTC)')
+    end
+  end
 end
