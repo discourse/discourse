0e8b28811ea4d2ca74d0958bdc9ef32d45101e3a
Gerhard Schlager <gerhard.schlager@discourse.org>
2020-11-01 22:32:16 +0100

    FIX: Show an example username in Review Settings

--- app/assets/javascripts/discourse/app/controllers/review-settings.js
+++ app/assets/javascripts/discourse/app/controllers/review-settings.js
@@ -1,6 +1,8 @@
 import Controller from "@ember/controller";
 import { ajax } from "discourse/lib/ajax";
 import { popupAjaxError } from "discourse/lib/ajax-error";
+import discourseComputed from "discourse-common/utils/decorators";
+import I18n from "I18n";
 
 export default Controller.extend({
   saving: false,
@@ -24,5 +26,17 @@ export default Controller.extend({
         .catch(popupAjaxError)
         .finally(() => this.set("saving", false));
     },
+  },
+
+  @discourseComputed("settings.reviewable_score_types")
+  scoreTypes() {
+    const username = I18n.t("review.example_username");
+    const types = this.get("settings.reviewable_score_types");
+
+    types.forEach((type) => {
+      type.title = type.title.replace("%{username}", username);
+    });
+
+    return types;
   },
 });
