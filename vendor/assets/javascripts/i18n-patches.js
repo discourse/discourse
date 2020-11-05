004cb06a22bf3432c9b62bab0e570f8de4af05bb
Jarek Radosz <jradosz@gmail.com>
2020-11-05 20:15:32 +0100

    DEV: Remove String.prototype.i18n
    I don't think this patch is needed. Let the CI prove me wrong. :P

--- vendor/assets/javascripts/i18n-patches.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  if (typeof I18n !== "undefined") {
-    if ("w" in String.prototype) {
-      String.prototype.i18n = function(options) {
-        return I18n.t(String(this), options);
-      };
-    }
-  }
-})();
