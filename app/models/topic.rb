0863c36221130b161d63562a58ac97171475dca5
Bianca Nenciu <nbianca@users.noreply.github.com>
2020-11-06 16:58:10 +0200

    FIX: Improve errors when invite to topic fails (#11133)
    It used to simply say "not allowed" without giving any hint what the
    problem could be. This commit refactors the code and tries to improve
    readability.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1031,12 +1031,40 @@ class Topic < ActiveRecord::Base
         raise UserExists.new(I18n.t("topic_invite.user_exists"))
       end
 
-      if invite_existing_muted?(target_user, invited_by)
-        raise NotAllowed.new(I18n.t("topic_invite.not_allowed"))
+      if MutedUser
+          .where(user: target_user, muted_user: invited_by)
+          .joins(:muted_user)
+          .where('NOT admin AND NOT moderator')
+          .exists?
+        raise NotAllowed.new(I18n.t("topic_invite.muted_invitee"))
       end
 
-      if !allowed_pm_user?(target_user, invited_by)
-        raise NotAllowed.new(I18n.t("topic_invite.not_allowed"))
+      if TopicUser
+          .where(topic: self,
+                 user: target_user,
+                 notification_level: TopicUser.notification_levels[:muted])
+          .exists?
+        raise NotAllowed.new(I18n.t("topic_invite.muted_topic"))
+      end
+
+      if !target_user.staff? &&
+         target_user&.user_option&.enable_allowed_pm_users &&
+         !AllowedPmUser.where(user: target_user, allowed_pm_user: invited_by).exists?
+        raise NotAllowed.new(I18n.t("topic_invite.receiver_does_not_allow_pm"))
+      end
+
+      if !target_user.staff? &&
+         invited_by&.user_option&.enable_allowed_pm_users &&
+         !AllowedPmUser.where(user: invited_by, allowed_pm_user: target_user).exists?
+        raise NotAllowed.new(I18n.t("topic_invite.sender_does_not_allow_pm"))
+      end
+
+      if !target_user.staff? && target_user&.user_option&.enable_allowed_pm_users
+        topic_users = self.topic_allowed_users.pluck(:user_id)
+        allowed_users = AllowedPmUser.where(user: target_user.id, allowed_pm_user_id: topic_users)
+        if (allowed_users - topic_users).size > 0
+          raise NotAllowed.new(I18n.t("topic_invite.receiver_does_not_allow_other_user_pm"))
+        end
       end
 
       if private_message?
@@ -1049,63 +1077,6 @@ class Topic < ActiveRecord::Base
         username_or_email, invited_by, self, group_ids, custom_message
       )
     end
-  end
-
-  def invite_existing_muted?(target_user, invited_by)
-    if invited_by.id &&
-       MutedUser.where(user_id: target_user.id, muted_user_id: invited_by.id)
-           .joins(:muted_user)
-           .where('NOT admin AND NOT moderator')
-           .exists?
-      return true
-    end
-
-    if TopicUser.where(
-         topic: self,
-         user: target_user,
-         notification_level: TopicUser.notification_levels[:muted]
-        ).exists?
-      return true
-    end
-
-    false
-  end
-
-  def allowed_pm_user?(target_user, invited_by)
-    return true if target_user.staff?
-    users = TopicAllowedUser.where(topic: self).pluck(:user_id)
-    users << target_user.id << invited_by.id
-    users.uniq
-
-    users_with_allowed_pms = allowed_pms_enabled(users).pluck(:id).uniq
-
-    if users_with_allowed_pms.any?
-      users_sender_can_pm = allowed_pms_enabled(users)
-        .where("allowed_pm_users.allowed_pm_user_id" => invited_by.id)
-        .pluck(:id).uniq
-
-      return false unless users_sender_can_pm.include? target_user.id
-
-      can_users_receive_from_sender = allowed_pms_enabled([invited_by.id])
-        .where("allowed_pm_users.allowed_pm_user_id IN (:user_ids)", user_ids: users.delete(invited_by.id))
-        .pluck(:id).uniq
-
-      users_not_allowed = users_with_allowed_pms - users_sender_can_pm - can_users_receive_from_sender
-      return false if users_not_allowed.any?
-    end
-
-    true
-  end
-
-  def allowed_pms_enabled(user_ids)
-    User
-      .joins("LEFT JOIN user_options ON user_options.user_id = users.id")
-      .joins("LEFT JOIN allowed_pm_users ON allowed_pm_users.user_id = users.id")
-      .where("
-        user_options.user_id IS NOT NULL AND
-        user_options.user_id IN (:user_ids) AND
-        user_options.enable_allowed_pm_users
-      ", user_ids: user_ids)
   end
 
   def email_already_exists_for?(invite)
