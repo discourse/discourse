565d5c61db2dc7061f85c1943a0c0ff6f5282579
David Taylor <david@taylorhq.com>
2020-11-10 11:14:05 +0000

    DEV: Drop github_user_infos table
    Follow-up to cf21de0e7a8293d48e17d5004e910f415b2b4e6b

---
+++ db/post_migrate/20201110110952_drop_github_user_infos.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'migration/table_dropper'
+
+class DropGithubUserInfos < ActiveRecord::Migration[6.0]
+  DROPPED_TABLES ||= %i{ github_user_infos }
+
+  def up
+    DROPPED_TABLES.each do |table|
+      Migration::TableDropper.execute_drop(table)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
