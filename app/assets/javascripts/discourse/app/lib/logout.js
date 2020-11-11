f9c0e72dcba6d4ea39bc8c4c442cce4e5eb62ce3
David Taylor <david@taylorhq.com>
2020-11-11 15:34:43 +0000

    Avoid snake_case in javascript code

--- app/assets/javascripts/discourse/app/lib/logout.js
+++ app/assets/javascripts/discourse/app/lib/logout.js
@@ -2,13 +2,13 @@ import getURL from "discourse-common/lib/get-url";
 import { isEmpty } from "@ember/utils";
 import { helperContext } from "discourse-common/lib/helpers";
 
-export default function logout({ redirect_url } = {}) {
+export default function logout({ redirect } = {}) {
   const ctx = helperContext();
   let keyValueStore = ctx.keyValueStore;
   keyValueStore.abandonLocal();
 
-  if (!isEmpty(redirect_url)) {
-    window.location.href = redirect_url;
+  if (!isEmpty(redirect)) {
+    window.location.href = redirect;
     return;
   }
 
