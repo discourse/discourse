aa197a87d8a1f845a3ef43c469b3f851ef896029
Mark VanLandingham <markvanlan@gmail.com>
2020-11-12 10:28:26 -0600

    Add fallback

--- app/assets/javascripts/discourse/app/routes/new-category.js
+++ app/assets/javascripts/discourse/app/routes/new-category.js
@@ -5,24 +5,36 @@ import { Promise } from "rsvp";
 
 export default DiscourseRoute.extend({
   model() {
-    return Promise.resolve(this.defaultGroupPermissions()).then(
-      (permissions) => {
-        return this.store.createRecord("category", {
-          color: "0088CC",
-          text_color: "FFFFFF",
-          group_permissions: permissions,
-          available_groups: this.site.groups.map((g) => g.name),
-          allow_badges: true,
-          topic_featured_link_allowed: true,
-          custom_fields: {},
-          search_priority: SEARCH_PRIORITIES.normal,
-        });
-      }
-    );
+    return Promise.resolve(this.groupPermissions())
+      .then((permissions) => {
+        return this.newCategoryWithPermissions(permissions);
+      })
+      .catch(() => {
+        return this.newCategoryWithPermissions(this.defaultGroupPermissions());
+      });
+  },
+
+  newCategoryWithPermissions(group_permissions) {
+    return this.store.createRecord("category", {
+      color: "0088CC",
+      text_color: "FFFFFF",
+      group_permissions,
+      available_groups: this.site.groups.map((g) => g.name),
+      allow_badges: true,
+      topic_featured_link_allowed: true,
+      custom_fields: {},
+      search_priority: SEARCH_PRIORITIES.normal,
+    });
   },
 
   titleToken() {
     return I18n.t("category.create");
+  },
+
+  groupPermissions() {
+    // Override this function if you want different groupPermissions from a plugin.
+    // If your plugin override fails, permissions will fallback to defaultGroupPermissions
+    return this.defaultGroupPermissions();
   },
 
   defaultGroupPermissions() {
