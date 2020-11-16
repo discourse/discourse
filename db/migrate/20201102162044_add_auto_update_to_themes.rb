bc5f40cc8d9c4cce6e089d29d7c22adc32f0adfb
Dan Ungureanu <dan@ungureanu.me>
2020-11-16 13:45:09 +0200

    FEATURE: Auto update themes by default

--- db/migrate/20201102162044_add_auto_update_to_themes.rb
+++ db/migrate/20201102162044_add_auto_update_to_themes.rb
@@ -2,6 +2,6 @@
 
 class AddAutoUpdateToThemes < ActiveRecord::Migration[6.0]
   def change
-    add_column :themes, :auto_update, :boolean, null: false, default: false
+    add_column :themes, :auto_update, :boolean, null: false, default: true
   end
 end
