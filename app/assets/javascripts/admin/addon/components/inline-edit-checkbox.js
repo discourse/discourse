a41164f7ee03689fd38b2aec55e9eec3b81315b7
Dan Ungureanu <dan@ungureanu.me>
2020-11-23 16:38:06 +0200

    HACK: Ensure didInsertElement is called only once

--- app/assets/javascripts/admin/addon/components/inline-edit-checkbox.js
+++ app/assets/javascripts/admin/addon/components/inline-edit-checkbox.js
@@ -6,11 +6,24 @@ export default Component.extend({
   classNames: ["inline-edit"],
 
   buffer: null,
+  bufferModelId: null,
 
   didReceiveAttrs() {
     this._super(...arguments);
 
-    this.set("buffer", this.checked);
+    if (this.modelId !== this.bufferModelId) {
+      // HACK: The condition above ensures this method is called only when its
+      // attributes are changed (i.e. only when `checked` changes).
+      //
+      // Reproduction steps: navigate to theme #1, switch to theme #2 from the
+      // left-side panel, then switch back to theme #1 and click on the <input>
+      // element wrapped by this component. It will call `didReceiveAttrs` even
+      // though none of the attributes have changed (only `buffer` does).
+      this.setProperties({
+        buffer: this.checked,
+        bufferModelId: this.modelId,
+      });
+    }
   },
 
   @discourseComputed("checked", "buffer")
