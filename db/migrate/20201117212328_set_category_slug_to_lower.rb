23577ca885e22cfeebc07b74d7970176b4099040
Bianca Nenciu <nenciu.bianca@gmail.com>
2020-11-23 21:43:27 +0200

    FIX: Different algorithm for migrating old slugs

--- db/migrate/20201117212328_set_category_slug_to_lower.rb
+++ db/migrate/20201117212328_set_category_slug_to_lower.rb
@@ -4,28 +4,61 @@ class SetCategorySlugToLower < ActiveRecord::Migration[6.0]
   def up
     remove_index(:categories, name: 'unique_index_categories_on_slug')
 
-    # Reset uncategorized category
-    execute <<~SQL
-      UPDATE categories
-      SET slug = 'uncategorized'
-      WHERE id = 1 AND
-            LOWER(slug) IN (SELECT LOWER(slug)
-                            FROM categories
-                            GROUP BY LOWER(slug)
-                            HAVING COUNT(*) > 1)
-    SQL
+    categories = DB.query("SELECT id, name, slug, parent_category_id FROM categories")
+    updates = {}
 
-    # Resolve duplicate slugs by replacing all mixed case slugs to
-    # "ID-lower_case_slug".
-    execute <<~SQL
-      UPDATE categories
-      SET slug = id || '-' || LOWER(slug)
-      WHERE slug != LOWER(slug) AND
-            LOWER(slug) IN (SELECT LOWER(slug)
-                            FROM categories
-                            GROUP BY LOWER(slug)
-                            HAVING COUNT(*) > 1)
-    SQL
+    # Resolve duplicate tags by replacing mixed case slugs with new ones
+    # extracted from category names
+    slugs = categories
+      .filter { |category| category.slug != '' }
+      .group_by { |category| [category.parent_category_id, category.slug.downcase] }
+      .map { |slug, cats| [slug, cats.size] }
+      .to_h
+
+    categories.each do |category|
+      old_parent_and_slug = [category.parent_category_id, category.slug.downcase]
+      next if category.slug == '' ||
+              category.slug == category.slug.downcase ||
+              slugs[old_parent_and_slug] <= 1
+
+      new_slug = category.name.parameterize
+        .tr("_", "-").squeeze('-').gsub(/\A-+|-+\z/, '')
+        .truncate(255, omission: '')
+      new_slug = '' if (new_slug =~ /[^\d]/).blank?
+      new_parent_and_slug = [category.parent_category_id, new_slug]
+      next if new_slug == '' ||
+              (slugs[new_parent_and_slug].present? && slugs[new_parent_and_slug] > 0)
+
+      updates[category.id] = category.slug = new_slug
+      slugs[old_parent_and_slug] -= 1
+      slugs[new_parent_and_slug] = 1
+    end
+
+    # Reset left conflicting slugs
+    slugs = categories
+      .filter { |category| category.slug != '' }
+      .group_by { |category| [category.parent_category_id, category.slug.downcase] }
+      .map { |slug, cats| [slug, cats.size] }
+      .to_h
+
+    categories.each do |category|
+      old_parent_and_slug = [category.parent_category_id, category.slug.downcase]
+      next if category.slug == '' ||
+              category.slug == category.slug.downcase ||
+              slugs[old_parent_and_slug] <= 1
+
+      updates[category.id] = category.slug = ''
+      slugs[old_parent_and_slug] -= 1
+    end
+
+    # Update all category slugs
+    updates.each do |id, slug|
+      execute <<~SQL
+        UPDATE categories
+        SET slug = '#{PG::Connection.escape_string(slug)}'
+        WHERE id = #{id}
+      SQL
+    end
 
     # Ensure all slugs are lowercase
     execute "UPDATE categories SET slug = LOWER(slug)"
