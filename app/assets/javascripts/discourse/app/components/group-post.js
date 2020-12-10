d5ddfadab046f0fa5657d51279f6d7311c12f48a
Robin Ward <robin.ward@gmail.com>
2020-12-10 14:55:53 -0500

    FIX: Posts with Staff Colors were excluded from the group activity
    Now they are included, with the correct color applied.

--- app/assets/javascripts/discourse/app/components/group-post.js
+++ app/assets/javascripts/discourse/app/components/group-post.js
@@ -1,10 +1,17 @@
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
 import getURL from "discourse-common/lib/get-url";
+import { propertyEqual } from "discourse/lib/computed";
 
 export default Component.extend({
+  classNameBindings: [":user-stream-item", ":item", "moderatorAction"],
+
   @discourseComputed("post.url")
   postUrl(url) {
     return getURL(url);
   },
+  moderatorAction: propertyEqual(
+    "post.post_type",
+    "site.post_types.moderator_action"
+  ),
 });
