60c68a4704298a887da459cc81171e39d035f389
Mark VanLandingham <markvanlan@gmail.com>
2020-12-15 10:25:21 -0600

    Tweak other things

--- app/assets/javascripts/discourse/app/controllers/do-not-disturb.js
+++ app/assets/javascripts/discourse/app/controllers/do-not-disturb.js
@@ -11,7 +11,7 @@ export default Controller.extend(ModalFunctionality, {
 
   @discourseComputed("saving", "duration")
   saveDisabled(saving, duration) {
-    return saving || duration === null;
+    return saving || !duration;
   },
 
   @action
