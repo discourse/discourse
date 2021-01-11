5ae1e073f3ba05a576d7c8814188ec796a2f6aea
Jamie Wilson <jamie.wilson@discourse.org>
2021-01-11 14:27:05 -0500

    DEV: TopicTrackingState calls should happen in the background
    It was observed that calling TopicTrackingState on popular topics could result in a large number of calls to redis, resulting in slow response times when posting replies.
    
    These calls should be moved to a background job.

---
+++ app/jobs/regular/post_update_topic_tracking_state.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Jobs
+  class PostUpdateTopicTrackingState < ::Jobs::Base
+
+    sidekiq_options queue: 'critical'
+
+    def execute(args)
+      post = Post.find_by(id: args[:post_id])
+
+      if post
+        TopicTrackingState.publish_unmuted(post.topic)
+        if post.post_number > 1
+          TopicTrackingState.publish_muted(post.topic)
+          TopicTrackingState.publish_unread(post)
+        end
+        TopicTrackingState.publish_latest(post.topic, post.whisper?)
+      end
+    end
+
+  end
+end
