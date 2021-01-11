414a44138433565053b4608d0b05e66f63d3f8a2
Martin Brennan <martin@discourse.org>
2021-01-11 16:04:25 +1000

    Get rid of topic_status_update wording on topic timers

--- app/jobs/regular/publish_topic_to_category.rb
+++ app/jobs/regular/publish_topic_to_category.rb
@@ -3,7 +3,7 @@
 module Jobs
   class PublishTopicToCategory < ::Jobs::Base
     def execute(args)
-      topic_timer = TopicTimer.find_by(id: args[:topic_timer_id] || args[:topic_status_update_id])
+      topic_timer = TopicTimer.find_by(id: args[:topic_timer_id])
       return if topic_timer.blank?
 
       topic = topic_timer.topic
