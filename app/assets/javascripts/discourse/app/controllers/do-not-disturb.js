5546ea947d28c123b2b271b5e611f7773af40e62
Mark VanLandingham <markvanlan@gmail.com>
2021-01-11 10:17:02 -0600

    show link to schedule in modal

--- app/assets/javascripts/discourse/app/controllers/do-not-disturb.js
+++ app/assets/javascripts/discourse/app/controllers/do-not-disturb.js
@@ -1,26 +1,18 @@
 import Controller from "@ember/controller";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
 import { action } from "@ember/object";
-import discourseComputed from "discourse-common/utils/decorators";
 import { extractError } from "discourse/lib/ajax-error";
 
 export default Controller.extend(ModalFunctionality, {
   duration: null,
-  saving: false,
-
-  @discourseComputed("saving", "duration")
-  saveDisabled(saving, duration) {
-    return saving || !duration;
-  },
 
   @action
   setDuration(duration) {
     this.set("duration", duration);
+    this.save();
   },
 
-  @action
   save() {
-    this.set("saving", true);
     this.currentUser
       .enterDoNotDisturbFor(this.duration)
       .then(() => {
@@ -28,9 +20,12 @@ export default Controller.extend(ModalFunctionality, {
       })
       .catch((e) => {
         this.flash(extractError(e), "error");
-      })
-      .finally(() => {
-        this.set("saving", false);
       });
+  },
+
+  @action
+  navigateToNotificationSchedule() {
+    this.transitionToRoute("preferences.notifications", this.currentUser);
+    this.send("closeModal");
   },
 });
--- spec/jobs/process_user_notification_schedules_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-describe Jobs::ProcessUserNotificationSchedules do
-  it 'works' do
-    UserNotificationSchedule.new.gT
-  end
-end
