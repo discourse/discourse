4321a55b611b2a40ba0491a1628a97871d4fc81e
Robin Ward <robin.ward@gmail.com>
2021-01-14 12:52:51 -0500

    Sync up master with Ember CLI branch where possible (#11707)
    These changes should all be safe to live in master even if they're not
    being used yet.

--- app/assets/javascripts/discourse/app/initializers/keyboard-shortcuts.js
+++ app/assets/javascripts/discourse/app/initializers/keyboard-shortcuts.js
@@ -1,10 +1,14 @@
 import KeyboardShortcuts from "discourse/lib/keyboard-shortcuts";
 import Mousetrap from "mousetrap";
+import bindGlobal from "mousetrap-global-bind";
 
 export default {
   name: "keyboard-shortcuts",
 
   initialize(container) {
+    // Ensure mousetrap-global-bind is executed
+    void bindGlobal;
+
     KeyboardShortcuts.init(Mousetrap, container);
     KeyboardShortcuts.bindEvents();
   },
