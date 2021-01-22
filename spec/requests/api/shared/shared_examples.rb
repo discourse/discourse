c889b676f80565b4b65dfe45d53e93cb6ff9d885
Blake Erickson <o.blakeerickson@gmail.com>
2021-01-21 18:23:23 -0700

    DEV: Updates to api docs schema validation (#11801)
    - Read in schemas from actual json files instead of a ruby hash. This is
    helpful because we will be automatically generating .json schema files
    from json responses and don't want to manually write ruby hash schema
    files.
    
    - Create a helper method for rspec schema validation tests to dry up code

--- spec/requests/api/shared/shared_examples.rb
+++ spec/requests/api/shared/shared_examples.rb
@@ -5,6 +5,16 @@ RSpec.shared_examples "a JSON endpoint" do |expected_response_status|
     submit_request(example.metadata)
   end
 
+  def expect_schema_valid(schemer, params)
+    valid = schemer.valid?(params)
+    unless valid # for debugging
+      puts
+      puts "RESPONSE: #{params}"
+      puts "VALIDATION DETAILS: #{schemer.validate(params).to_a[0]["details"]}"
+    end
+    expect(valid).to eq(true)
+  end
+
   describe "response status" do
     it "returns expected response status" do
       expect(response.status).to eq(expected_response_status)
@@ -13,13 +23,8 @@ RSpec.shared_examples "a JSON endpoint" do |expected_response_status|
 
   describe "request body" do
     it "matches the documented request schema" do |example|
-      schemer = JSONSchemer.schema(expected_request_schema.schemer)
-      valid = schemer.valid?(params)
-      unless valid # for debugging
-        puts params
-        puts schemer.validate(params).to_a[0]["details"]
-      end
-      expect(valid).to eq(true)
+      schemer = JSONSchemer.schema(expected_request_schema)
+      expect_schema_valid(schemer, params)
     end
   end
 
@@ -28,14 +33,9 @@ RSpec.shared_examples "a JSON endpoint" do |expected_response_status|
 
     it "matches the documented response schema" do  |example|
       schemer = JSONSchemer.schema(
-        expected_response_schema.schemer,
+        expected_response_schema,
       )
-      valid = schemer.valid?(json_response)
-      unless valid # for debugging
-        puts json_response
-        puts schemer.validate(json_response).to_a[0]["details"]
-      end
-      expect(valid).to eq(true)
+      expect_schema_valid(schemer, json_response)
     end
   end
 end
