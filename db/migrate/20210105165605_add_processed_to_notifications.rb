809274fe0d73497f34247f48433d1efb568fa58c
Mark VanLandingham <mark.vanlandingham@discourse.org>
2021-01-27 10:29:24 -0600

    DEV: Replace 'processed' column on notifications with new table (#11864)

--- db/migrate/20210105165605_add_processed_to_notifications.rb
+++ db/migrate/20210105165605_add_processed_to_notifications.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 class AddProcessedToNotifications < ActiveRecord::Migration[6.0]
-  def up
-    add_column :notifications, :processed, :boolean, default: false
-    execute "UPDATE notifications SET processed = true"
-    change_column_null(:notifications, :processed, false)
-    add_index :notifications, [:processed], unique: false
-  end
-
-  def down
-    remove_column :notifications, :processed
+  def change
   end
 end
