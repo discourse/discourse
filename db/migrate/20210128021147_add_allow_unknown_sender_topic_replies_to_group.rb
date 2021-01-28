97a539d78ef93ccc56d0097460f253938b255399
Martin Brennan <martin@discourse.org>
2021-01-29 09:33:36 +1000

    Review fixes

--- db/migrate/20210128021147_add_allow_unknown_sender_topic_replies_to_group.rb
+++ db/migrate/20210128021147_add_allow_unknown_sender_topic_replies_to_group.rb
@@ -2,11 +2,10 @@
 
 class AddAllowUnknownSenderTopicRepliesToGroup < ActiveRecord::Migration[6.0]
   def up
-    add_column :groups, :allow_unknown_sender_topic_replies, :boolean, default: false
-    DB.exec("UPDATE groups SET allow_unknown_sender_topic_replies = false")
+    add_column :groups, :allow_unknown_sender_topic_replies, :boolean, default: false, null: false
   end
 
   def down
-    remove_column :groups, :allow_unknown_sender_topic_replies if column_exists?(:groups, :allow_unknown_sender_topic_replies)
+    remove_column :groups, :allow_unknown_sender_topic_replies
   end
 end
