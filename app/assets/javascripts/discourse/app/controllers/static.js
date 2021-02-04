cddc62f260ddee85a500829ec3053c1d474d320f
awesomerobot <kris.aubuchon@discourse.org>
2021-02-03 21:55:21 -0500

    alpha

--- app/assets/javascripts/discourse/app/controllers/static.js
+++ app/assets/javascripts/discourse/app/controllers/static.js
@@ -1,7 +1,7 @@
 import Controller, { inject as controller } from "@ember/controller";
 import { ajax } from "discourse/lib/ajax";
 import discourseComputed from "discourse-common/utils/decorators";
-import { or, equal } from "@ember/object/computed";
+import { equal, or } from "@ember/object/computed";
 import { userPath } from "discourse/lib/url";
 
 export default Controller.extend({
