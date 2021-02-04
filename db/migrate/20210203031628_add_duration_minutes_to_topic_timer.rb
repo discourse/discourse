2f04a5cf737d1046586326036354a0b68790f89f
Martin Brennan <martin@discourse.org>
2021-02-04 14:24:06 +1000

    Make sure duration minutes is calculated right in migration

--- db/migrate/20210203031628_add_duration_minutes_to_topic_timer.rb
+++ db/migrate/20210203031628_add_duration_minutes_to_topic_timer.rb
@@ -3,7 +3,11 @@
 class AddDurationMinutesToTopicTimer < ActiveRecord::Migration[6.0]
   def up
     add_column :topic_timers, :duration_minutes, :integer
-    DB.exec("UPDATE topic_timers SET duration_minutes = duration WHERE duration_minutes != duration")
+
+    # 7 is delete_replies type, this duration is measured in days, the other
+    # duration is measured in hours
+    DB.exec("UPDATE topic_timers SET duration_minutes = (duration * 60 * 24) WHERE duration_minutes != duration AND status_type = 7 AND duration IS NOT NULL")
+    DB.exec("UPDATE topic_timers SET duration_minutes = (duration * 60) WHERE duration_minutes != duration AND status_type != 7 AND duration IS NOT NULL")
   end
 
   def down
--- db/post_migrate/20210203031729_remove_duration_from_topic_timer.rb
+++ db/post_migrate/20210203031729_remove_duration_from_topic_timer.rb
@@ -6,7 +6,11 @@ class RemoveDurationFromTopicTimer < ActiveRecord::Migration[6.0]
   }
 
   def up
-    DB.exec("UPDATE topic_timers SET duration_minutes = duration WHERE duration_minutes != duration")
+    # 7 is delete_replies type, this duration is measured in days, the other
+    # duration is measured in hours
+    DB.exec("UPDATE topic_timers SET duration_minutes = (duration * 60 * 24) WHERE duration_minutes != duration AND status_type = 7 AND duration IS NOT NULL")
+    DB.exec("UPDATE topic_timers SET duration_minutes = (duration * 60) WHERE duration_minutes != duration AND status_type != 7 AND duration IS NOT NULL")
+
     DROPPED_COLUMNS.each do |table, columns|
       Migration::ColumnDropper.execute_drop(table, columns)
     end
