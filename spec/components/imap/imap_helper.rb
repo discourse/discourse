547fd2a51f912babd7ff92b462165451d59ee6da
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-02-17 23:16:41 +0200

    DEV: Fix tests

--- spec/components/imap/imap_helper.rb
+++ spec/components/imap/imap_helper.rb
@@ -18,7 +18,7 @@ def EmailFabricator(options)
   email += "Cc: #{options[:cc]}\n" if options[:cc]
   email += "In-Reply-To: #{options[:in_reply_to]}\n" if options[:in_reply_to]
   email += "References: #{options[:in_reply_to]}\n" if options[:in_reply_to]
-  email += "Message-ID: #{options[:message_id]}\n" if options[:message_id]
+  email += "Message-ID: <#{options[:message_id]}>\n" if options[:message_id]
   email += "Subject: #{options[:subject] || "This is a test email subhect"}\n"
   email += "Mime-Version: 1.0\n"
   email += "Content-Type: #{options[:content_type] || "text/plain;\n charset=UTF-8"}\n"
