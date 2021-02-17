acd44b00f8fd113c38ea31cd225d99b35a3c10a1
clemkeirua <clement@keiruaprod.fr>
2021-02-17 11:47:36 +0100

    disable content-security policy on localhost

--- lib/content_security_policy.rb
+++ lib/content_security_policy.rb
@@ -5,17 +5,20 @@ require 'content_security_policy/extension'
 class ContentSecurityPolicy
   class << self
     def policy(theme_ids = [], base_url: Discourse.base_url, path_info: "/")
-      new.build(theme_ids, base_url: base_url, path_info: path_info)
+      if !Rails.env.development?
+        new.build(theme_ids, base_url: base_url, path_info: path_info)
+      end
     end
   end
 
   def build(theme_ids, base_url:, path_info: "/")
     builder = Builder.new(base_url: base_url)
-
-    Extension.theme_extensions(theme_ids).each { |extension| builder << extension }
-    Extension.plugin_extensions.each { |extension| builder << extension }
-    builder << Extension.site_setting_extension
-    builder << Extension.path_specific_extension(path_info)
+    if !Rails.env.development?
+      Extension.theme_extensions(theme_ids).each { |extension| builder << extension }
+      Extension.plugin_extensions.each { |extension| builder << extension }
+      builder << Extension.site_setting_extension
+      builder << Extension.path_specific_extension(path_info)
+    end
 
     builder.build
   end
