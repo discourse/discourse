5117f0a3e80222aa17ce9c1682992e787ab55320
Rafael dos Santos Silva <xfalcox@gmail.com>
2021-02-19 16:06:18 -0300

    FIX: Subfolder replace should only affect URL prefix (#12135)
    * FIX: Subfolder replace should only affect URL prefix
    
    Issue was reported in https://meta.discourse.org/t/-/179504
    
    * DEV: Test subfolder handling in get-url when called twice on the same path

--- app/assets/javascripts/discourse-common/addon/lib/get-url.js
+++ app/assets/javascripts/discourse-common/addon/lib/get-url.js
@@ -1,9 +1,10 @@
-let cdn, baseUrl, baseUri;
+let cdn, baseUrl, baseUri, baseUriMatcher;
 let S3BaseUrl, S3CDN;
 
 export default function getURL(url) {
   if (baseUri === undefined) {
     baseUri = $('meta[name="discourse-base-uri"]').attr("content") || "";
+    baseUriMatcher = new RegExp(`^${baseUri}`);
   }
 
   if (!url) {
@@ -47,18 +48,23 @@ export function isAbsoluteURL(url) {
 }
 
 export function withoutPrefix(path) {
-  const rootURL = (!baseUri ? "/" : baseUri).replace(/\/$/, "");
-  return path.replace(rootURL, "");
+  if (!baseUri) {
+    return path;
+  } else {
+    return path.replace(baseUriMatcher, "");
+  }
 }
 
 export function setPrefix(configBaseUri) {
   baseUri = configBaseUri;
+  baseUriMatcher = new RegExp(`^${baseUri}`);
 }
 
 export function setupURL(configCdn, configBaseUrl, configBaseUri) {
   cdn = configCdn;
   baseUrl = configBaseUrl;
   baseUri = configBaseUri;
+  baseUriMatcher = new RegExp(`^${baseUri}`);
 }
 
 export function setupS3CDN(configS3BaseUrl, configS3CDN) {
