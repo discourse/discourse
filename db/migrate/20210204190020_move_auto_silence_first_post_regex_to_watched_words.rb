dad1ee38dc9ab821aa6129b892d745b3261f3cef
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-02-25 13:35:54 +0200

    DEV: Apply code suggestions

--- db/migrate/20210204190020_move_auto_silence_first_post_regex_to_watched_words.rb
+++ db/migrate/20210204190020_move_auto_silence_first_post_regex_to_watched_words.rb
@@ -4,7 +4,7 @@ class MoveAutoSilenceFirstPostRegexToWatchedWords < ActiveRecord::Migration[6.0]
   def up
     execute <<~SQL
       INSERT INTO watched_words (word, action, created_at, updated_at)
-      SELECT value, 5, created_at, updated_at
+      SELECT value, 3, created_at, updated_at
       FROM site_settings
       WHERE name = 'auto_silence_first_post_regex'
       ON CONFLICT DO NOTHING
@@ -17,19 +17,8 @@ class MoveAutoSilenceFirstPostRegexToWatchedWords < ActiveRecord::Migration[6.0]
       WHERE name = 'auto_silence_first_post_regex'
       ON CONFLICT DO NOTHING
     SQL
-
-    execute "DELETE FROM site_settings WHERE name = 'auto_silence_first_post_regex'"
   end
 
   def down
-    execute <<~SQL
-      INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
-      SELECT 'auto_silence_first_post_regex', 1, word, created_at, updated_at
-      FROM watched_words
-      WHERE action = 5
-      LIMIT 1
-    SQL
-
-    execute "DELETE FROM watched_words WHERE action = 5"
   end
 end
