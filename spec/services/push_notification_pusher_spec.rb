50bc02c695e6816734192d3b4d68b0eaa95e2f72
Vinoth Kannan <svkn.87@gmail.com>
2021-02-25 22:59:50 +0530

    FIX: send notification in user's locale if available.
    Previously, it was sending notifications in site's default locale.

--- spec/services/push_notification_pusher_spec.rb
+++ spec/services/push_notification_pusher_spec.rb
@@ -16,4 +16,30 @@ RSpec.describe PushNotificationPusher do
       .to eq(UrlHelper.absolute(upload.url))
   end
 
+  it "sends notification in user's locale" do
+    SiteSetting.allow_user_locale = true
+    user = Fabricate(:user, locale: 'pt_BR')
+    PushSubscription.create!(user_id: user.id, data: "{\"endpoint\": \"endpoint\"}")
+
+    PushNotificationPusher.expects(:send_notification).with(user, { "endpoint" => "endpoint" }, {
+      title: "system mencionou você em \"Topic\" - Discourse",
+      body: "description",
+      badge: "/assets/push-notifications/discourse.png",
+      icon: "/assets/push-notifications/mentioned.png",
+      tag: "test.localhost-1",
+      base_url: "http://test.localhost",
+      url: "https://example.com/t/1/2",
+      hide_when_active: true
+    }).once
+
+    PushNotificationPusher.push(user, {
+      topic_title: 'Topic',
+      username: 'system',
+      excerpt: 'description',
+      topic_id: 1,
+      post_url: "https://example.com/t/1/2",
+      notification_type: 1
+    })
+  end
+
 end
