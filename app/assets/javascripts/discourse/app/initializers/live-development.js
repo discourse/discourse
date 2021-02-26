e74bdfdf8e47c8fb15ba5e1cd3870d9bba1c78c8
Penar Musaraj <pmusaraj@gmail.com>
2021-02-26 08:20:39 -0500

    Revert "DEV: Use separate files for theme component stylesheets (#12214)" (#12224)
    This reverts commit f57a49c2f97c78865a4ad806339a2f847d6bc98c.
    
    This had some unexpected side effects, needs some more work.

--- app/assets/javascripts/discourse/app/initializers/live-development.js
+++ app/assets/javascripts/discourse/app/initializers/live-development.js
@@ -1,7 +1,7 @@
+import { currentThemeIds, refreshCSS } from "discourse/lib/theme-selector";
 import DiscourseURL from "discourse/lib/url";
 import Handlebars from "handlebars";
 import { isDevelopment } from "discourse-common/config/environment";
-import { refreshCSS } from "discourse/lib/theme-selector";
 
 //  Use the message bus for live reloading of components for faster development.
 export default {
@@ -63,11 +63,13 @@ export default {
           // Refresh if necessary
           document.location.reload(true);
         } else {
+          const themeIds = currentThemeIds();
           $("link").each(function () {
             if (me.hasOwnProperty("theme_id") && me.new_href) {
               const target = $(this).data("target");
               const themeId = $(this).data("theme-id");
               if (
+                themeIds.indexOf(me.theme_id) !== -1 &&
                 target === me.target &&
                 (!themeId || themeId === me.theme_id)
               ) {
