cc2c4f652404c297ea04ac6b0708fc5861bfe13e
Dan Ungureanu <dan@ungureanu.me>
2021-03-02 11:22:05 +0200

    DEV: Use vanilla JS and existing element

--- app/assets/javascripts/discourse/app/components/copy-button.js
+++ app/assets/javascripts/discourse/app/components/copy-button.js
@@ -1,16 +1,16 @@
 import Component from "@ember/component";
 import { action } from "@ember/object";
-import copyText from "discourse/lib/copy-text";
 
 export default Component.extend({
   tagName: "",
 
   @action
   copy() {
-    const $copyRange = $('<p id="copy-range"></p>');
-    $copyRange.html(this.content);
-    $(document.body).append($copyRange);
-    copyText(this.content, $copyRange[0]);
-    $copyRange.remove();
+    const target = document.querySelector(this.selector);
+    target.select();
+    target.setSelectionRange(0, target.value.length);
+    try {
+      document.execCommand("copy");
+    } catch (err) {}
   },
 });
