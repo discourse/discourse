7f67b888362b2471458f038ed1e7ca51cb7bf8de
David Taylor <david@taylorhq.com>
2021-03-11 20:06:39 +0000

    FEATURE: Allow a cluster_name to be configured and used for /srv/status
    The cluster name can be configured by setting the `DISCOURSE_CLUSTER_NAME` environment variable. If set, you can then call /srv/status with a `?cluster=` parameter. If the cluster does not match, an error will be returned. This is useful if you need a load balancer to be able to verify the identity, as well as the presence, of an application container.

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -9,6 +9,14 @@ class ForumsController < ActionController::Base
   after_action  :add_readonly_header
 
   def status
+    if params[:cluster]
+      if GlobalSetting.cluster_name.nil?
+        return render plain: "cluster name not configured", status: 500
+      elsif GlobalSetting.cluster_name != params[:cluster]
+        return render plain: "cluster name does not match", status: 500
+      end
+    end
+
     if $shutdown # rubocop:disable Style/GlobalVars
       render plain: "shutting down", status: (params[:shutdown_ok] ? 200 : 500)
     else
