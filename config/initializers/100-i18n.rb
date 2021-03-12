1533cbb38ba02eda0dcb1c58a9da0b70fbdddfee
David Taylor <david@taylorhq.com>
2021-03-12 13:48:46 +0000

    Revert "FIX: deprecation warning - initialization autoloaded the constant (#12347)" (#12381)
    This reverts commit 9a5a38a529dfdb7579a015845c01a80679923f17.
    
    This change was preventing `rake multisite:migrate` from working

--- config/initializers/100-i18n.rb
+++ config/initializers/100-i18n.rb
@@ -5,17 +5,15 @@
 require 'i18n/backend/discourse_i18n'
 require 'i18n/backend/fallback_locale_list'
 
-Rails.application.reloader.to_prepare do
-  I18n.backend = I18n::Backend::DiscourseI18n.new
-  I18n.fallbacks = I18n::Backend::FallbackLocaleList.new
-  I18n.config.missing_interpolation_argument_handler = proc { throw(:exception) }
-  I18n.reload!
-  I18n.init_accelerator!(overrides_enabled: ENV['DISABLE_TRANSLATION_OVERRIDES'] != '1')
+I18n.backend = I18n::Backend::DiscourseI18n.new
+I18n.fallbacks = I18n::Backend::FallbackLocaleList.new
+I18n.config.missing_interpolation_argument_handler = proc { throw(:exception) }
+I18n.reload!
+I18n.init_accelerator!(overrides_enabled: ENV['DISABLE_TRANSLATION_OVERRIDES'] != '1')
 
-  unless Rails.env.test?
-    MessageBus.subscribe("/i18n-flush") do
-      I18n.reload!
-      ExtraLocalesController.clear_cache!
-    end
+unless Rails.env.test?
+  MessageBus.subscribe("/i18n-flush") do
+    I18n.reload!
+    ExtraLocalesController.clear_cache!
   end
 end
