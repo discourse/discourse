e10a74694a2ed445d57e1740ba349e72dcc4842d
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2021-03-16 09:47:57 +1100

    FIX: deprecation warning - initialization autoloaded the constant (#12400)
    Get rid of deprecation related to Zeitwerk autoloader.
    
    Original PR was reverted because of multisite bug #12381 - thank you @davidtaylorhq for fixing it.
    
    I added the last commit to fix that multisite problem.

--- config/initializers/005-site_settings.rb
+++ config/initializers/005-site_settings.rb
@@ -16,24 +16,26 @@ if GlobalSetting.skip_redis?
   return
 end
 
-reload_settings = lambda {
-  RailsMultisite::ConnectionManagement.safe_each_connection do
-    begin
-      SiteSetting.refresh!
+Rails.application.reloader.to_prepare do
+  reload_settings = lambda {
+    RailsMultisite::ConnectionManagement.safe_each_connection do
+      begin
+        SiteSetting.refresh!
 
-      unless String === SiteSetting.push_api_secret_key && SiteSetting.push_api_secret_key.length == 32
-        SiteSetting.push_api_secret_key = SecureRandom.hex
+        unless String === SiteSetting.push_api_secret_key && SiteSetting.push_api_secret_key.length == 32
+          SiteSetting.push_api_secret_key = SecureRandom.hex
+        end
+      rescue ActiveRecord::StatementInvalid
+        # This will happen when migrating a new database
       end
-    rescue ActiveRecord::StatementInvalid
-      # This will happen when migrating a new database
     end
-  end
-}
+  }
 
-reload_settings.call
+  reload_settings.call
 
-if !Rails.configuration.cache_classes
-  ActiveSupport::Reloader.to_prepare do
-    reload_settings.call
+  if !Rails.configuration.cache_classes
+    ActiveSupport::Reloader.to_prepare do
+      reload_settings.call
+    end
   end
 end
