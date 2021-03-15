768a0c122c82eb8d249aead3dd3d0fb20d8a30ca
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2021-03-15 16:01:16 +1100

    FIX: multisite problem

--- config/initializers/002-freedom_patches.rb
+++ config/initializers/002-freedom_patches.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+RUN_WITHOUT_PREPARE = ["#{Rails.root}/lib/freedom_patches/rails_multisite.rb"]
+RUN_WITHOUT_PREPARE.each { |path| require(path) }
+
 Rails.application.reloader.to_prepare do
   Dir["#{Rails.root}/lib/freedom_patches/*.rb"].each do |f|
+    next if RUN_WITHOUT_PREPARE.any? { |path| path == f }
     require(f)
   end
 end
