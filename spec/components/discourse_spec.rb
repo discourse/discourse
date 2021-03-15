7828970b70bb53292a035ba6ba50fa8bef6b27a3
Sam Saffron <sam.saffron@gmail.com>
2021-03-15 15:30:22 +1100

    DEV: ensure state is clean prior to spec
    Corrects flaky spec
    
    Previously we were only clearing state after our spec ran, leaving possible
    edge cases where `Discourse.plugins` had data.

--- spec/components/discourse_spec.rb
+++ spec/components/discourse_spec.rb
@@ -78,8 +78,14 @@ describe Discourse do
     let(:plugin1) { plugin_class.new.tap { |p| p.enabled = true; p.path = "my-plugin-1" } }
     let(:plugin2) { plugin_class.new.tap { |p| p.enabled = false; p.path = "my-plugin-1" } }
 
-    before { Discourse.plugins.append(plugin1, plugin2) }
-    after { Discourse.plugins.clear }
+    before do
+      Discourse.plugins.clear
+      Discourse.plugins.append(plugin1, plugin2)
+    end
+
+    after do
+      Discourse.plugins.clear
+    end
 
     before do
       plugin_class.any_instance.stubs(:css_asset_exists?).returns(true)
