discourse/discourse

commit eb7f0ec766622ca0c0e1ae237bbeb062e7545093

FIX: In components, fall back to default theme color variables (#12423)
Component SCSS compilation should use the current theme's SCSS color
variables as a fallback before using the default core colors.

This is mostly a backwards-compatibility fix, new themes and components
should use CSS custom properties, which offer better support for on-the-fly
color scheme changes (dark mode support, etc.).

lib/stylesheet/importer.rb
@@ -135,8 +135,11 @@ module Stylesheet
         rescue
           ColorScheme.base_colors
         end
+      elsif (@theme_id && theme.color_scheme)
+        colors = theme.color_scheme.resolved_colors
       else
-        colors = (@theme_id && theme.color_scheme) ? theme.color_scheme.resolved_colors : ColorScheme.base_colors
+        colors = Theme.find_by_id(SiteSetting.default_theme_id)&.color_scheme&.resolved_colors ||
+          ColorScheme.base_colors
       end
 
       colors.each do |n, hex|

spec/components/stylesheet/manager_spec.rb
@@ -415,6 +415,24 @@ describe Stylesheet::Manager do
 
         expect { stylesheet.compile }.not_to raise_error
       end
+
+      it "child theme SCSS includes the default theme's color scheme variables" do
+        SiteSetting.default_theme_id = theme.id
+        custom_scheme = ColorScheme.create_from_base(name: "Neutral", base_scheme_id: "Neutral")
+        ColorSchemeRevisor.revise(custom_scheme, colors: [{ name: "primary", hex: "CC0000" }])
+        theme.color_scheme_id = custom_scheme.id
+        theme.save!
+
+        scss = "body{ border: 2px solid $primary;}"
+        child.set_field(target: :common, name: "scss", value: scss)
+        child.save!
+
+        child_theme_manager = Stylesheet::Manager.new(:desktop_theme, child.id)
+        child_theme_manager.compile(force: true)
+
+        child_css = File.read(child_theme_manager.stylesheet_fullpath)
+        expect(child_css).to include("body{border:2px solid #c00}")
+      end
     end
 
     context 'encoded slugs' do

commit b7bf6e18ccf1fc41c4065c42a069aff5afd92ab5

FIX: Regression in colors used by non-default theme
eb7f0ec caused this regression, where a non-default theme set to use
the base color scheme was resolving to the default theme's color scheme.

lib/stylesheet/importer.rb
@@ -135,9 +135,12 @@ module Stylesheet
         rescue
           ColorScheme.base_colors
         end
-      elsif (@theme_id && theme.color_scheme)
-        colors = theme.color_scheme.resolved_colors
+      elsif (@theme_id && !theme.component)
+        colors = theme&.color_scheme&.resolved_colors || ColorScheme.base_colors
       else
+        # this is a slightly ugly backwards compatibility fix,
+        # we shouldn't be using the default theme color scheme for components
+        # (most components use CSS custom properties which work fine without this)
         colors = Theme.find_by_id(SiteSetting.default_theme_id)&.color_scheme&.resolved_colors ||
           ColorScheme.base_colors
       end

spec/components/stylesheet/manager_spec.rb
@@ -331,6 +331,7 @@ describe Stylesheet::Manager do
 
     it "uses the correct color scheme when a non-default theme is selected and it uses the base 'Light' scheme" do
       cs = Fabricate(:color_scheme, name: 'Not This')
+      ColorSchemeRevisor.revise(cs, colors: [{ name: "primary", hex: "CC0000" }])
       default_theme = Fabricate(:theme, color_scheme_id: cs.id)
       SiteSetting.default_theme_id = default_theme.id
 
@@ -338,6 +339,10 @@ describe Stylesheet::Manager do
 
       link = Stylesheet::Manager.color_scheme_stylesheet_link_tag(nil, "all", [user_theme.id])
       expect(link).to include("/stylesheets/color_definitions_base_")
+
+      stylesheet = Stylesheet::Manager.new(:color_definitions, user_theme.id, nil).compile(force: true)
+      expect(stylesheet).not_to include("--primary: #c00;")
+      expect(stylesheet).to include("--primary: #222;") # from base scheme
     end
 
     it "uses the correct scheme when a valid scheme id is used" do