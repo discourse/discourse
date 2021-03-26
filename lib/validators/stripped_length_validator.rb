95edcf2c0de6b7d5592a113bb66d256d877935dd
Arpit Jalan <arpit@techapj.com>
2021-03-26 16:20:41 +0530

    FIX: post merging was failing silently
    https://meta.discourse.org/t/merging-very-long-posts-removes-them/183597

--- lib/validators/stripped_length_validator.rb
+++ lib/validators/stripped_length_validator.rb
@@ -3,13 +3,7 @@
 class StrippedLengthValidator < ActiveModel::EachValidator
   def self.validate(record, attribute, value, range)
     if !value.nil?
-      value = value.dup
-      value.gsub!(/<!--(.*?)-->/, '') # strip HTML comments
-      value.gsub!(/:\w+(:\w+)?:/, "X") # replace emojis with a single character
-      value.gsub!(/\.{2,}/, '…') # replace multiple ... with …
-      value.gsub!(/\,{2,}/, ',') # replace multiple ,,, with ,
-      value.strip!
-
+      value = get_sanitized_value(value)
       record.errors.add attribute, (I18n.t('errors.messages.too_short', count: range.begin)) if value.length < range.begin
       record.errors.add attribute, (I18n.t('errors.messages.too_long_validation', max: range.end, length: value.length)) if value.length > range.end
     else
@@ -21,5 +15,14 @@ class StrippedLengthValidator < ActiveModel::EachValidator
     # the `in` parameter might be a lambda when the range is dynamic
     range = options[:in].lambda? ? options[:in].call : options[:in]
     self.class.validate(record, attribute, value, range)
+  end
+
+  def self.get_sanitized_value(value)
+    value = value.dup
+    value.gsub!(/<!--(.*?)-->/, '') # strip HTML comments
+    value.gsub!(/:\w+(:\w+)?:/, "X") # replace emojis with a single character
+    value.gsub!(/\.{2,}/, '…') # replace multiple ... with …
+    value.gsub!(/\,{2,}/, ',') # replace multiple ,,, with ,
+    value.strip
   end
 end
