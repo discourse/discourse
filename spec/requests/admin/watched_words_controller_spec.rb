5e089dbce5c9c1e866b20584795d8d37bfeaf067
Arpit Jalan <arpit@techapj.com>
2021-03-31 17:43:52 +0530

    Move `/admin/logs/watched_words` to `/admin/customize/watched_words`
    https://meta.discourse.org/t/where-is-auto-tag-and-auto-replace/184261

--- spec/requests/admin/watched_words_controller_spec.rb
+++ spec/requests/admin/watched_words_controller_spec.rb
@@ -14,13 +14,13 @@ RSpec.describe Admin::WatchedWordsController do
     end
 
     it 'should return the right response when given an invalid id param' do
-      delete '/admin/logs/watched_words/9999.json'
+      delete '/admin/customize/watched_words/9999.json'
 
       expect(response.status).to eq(400)
     end
 
     it 'should be able to delete a watched word' do
-      delete "/admin/logs/watched_words/#{watched_word.id}.json"
+      delete "/admin/customize/watched_words/#{watched_word.id}.json"
 
       expect(response.status).to eq(200)
       expect(WatchedWord.find_by(id: watched_word.id)).to eq(nil)
@@ -34,7 +34,7 @@ RSpec.describe Admin::WatchedWordsController do
       end
 
       it 'creates the words from the file' do
-        post '/admin/logs/watched_words/upload.json', params: {
+        post '/admin/customize/watched_words/upload.json', params: {
           action_key: 'flag',
           file: Rack::Test::UploadedFile.new(file_from_fixtures("words.csv", "csv"))
         }
@@ -50,7 +50,7 @@ RSpec.describe Admin::WatchedWordsController do
       end
 
       it 'creates the words from the file' do
-        post '/admin/logs/watched_words/upload.json', params: {
+        post '/admin/customize/watched_words/upload.json', params: {
           action_key: 'tag',
           file: Rack::Test::UploadedFile.new(file_from_fixtures("words_tag.csv", "csv"))
         }
@@ -71,7 +71,7 @@ RSpec.describe Admin::WatchedWordsController do
   describe '#download' do
     context 'not logged in as admin' do
       it "doesn't allow performing #download" do
-        get "/admin/logs/watched_words/action/block/download"
+        get "/admin/customize/watched_words/action/block/download"
         expect(response.status).to eq(404)
       end
     end
@@ -88,17 +88,17 @@ RSpec.describe Admin::WatchedWordsController do
         autotag_1 = Fabricate(:watched_word, action: WatchedWord.actions[:tag], replacement: "tag1,tag2")
         autotag_2 = Fabricate(:watched_word, action: WatchedWord.actions[:tag], replacement: "tag3,tag2")
 
-        get "/admin/logs/watched_words/action/block/download"
+        get "/admin/customize/watched_words/action/block/download"
         expect(response.status).to eq(200)
         block_words = response.body.split("\n")
         expect(block_words).to contain_exactly(block_word_1.word, block_word_2.word)
 
-        get "/admin/logs/watched_words/action/censor/download"
+        get "/admin/customize/watched_words/action/censor/download"
         expect(response.status).to eq(200)
         censor_words = response.body.split("\n")
         expect(censor_words).to contain_exactly(censor_word_1.word)
 
-        get "/admin/logs/watched_words/action/tag/download"
+        get "/admin/customize/watched_words/action/tag/download"
         expect(response.status).to eq(200)
         tag_words = response.body.split("\n").map(&:parse_csv)
         expect(tag_words).to contain_exactly(
@@ -113,7 +113,7 @@ RSpec.describe Admin::WatchedWordsController do
     context 'non admins' do
       it "doesn't allow them to perform #clear_all" do
         word = Fabricate(:watched_word, action: WatchedWord.actions[:block])
-        delete "/admin/logs/watched_words/action/block"
+        delete "/admin/customize/watched_words/action/block"
         expect(response.status).to eq(404)
         expect(WatchedWord.pluck(:word)).to include(word.word)
       end
@@ -126,7 +126,7 @@ RSpec.describe Admin::WatchedWordsController do
 
       it "allows them to perform #clear_all" do
         word = Fabricate(:watched_word, action: WatchedWord.actions[:block])
-        delete "/admin/logs/watched_words/action/block.json"
+        delete "/admin/customize/watched_words/action/block.json"
         expect(response.status).to eq(200)
         expect(WatchedWord.pluck(:word)).not_to include(word.word)
       end
@@ -135,7 +135,7 @@ RSpec.describe Admin::WatchedWordsController do
         block_word = Fabricate(:watched_word, action: WatchedWord.actions[:block])
         flag_word = Fabricate(:watched_word, action: WatchedWord.actions[:flag])
 
-        delete "/admin/logs/watched_words/action/flag.json"
+        delete "/admin/customize/watched_words/action/flag.json"
         expect(response.status).to eq(200)
         all_words = WatchedWord.pluck(:word)
         expect(all_words).to include(block_word.word)
