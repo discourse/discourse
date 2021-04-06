1c785bd7c94a94cecbbab9543b6cfc6270384723
Martin Brennan <martin@discourse.org>
2021-04-06 16:11:05 +1000

    Make it so the topic_user.bookmarked sync job works with no topic id
    This way we can run it in production for all topics if necessary

--- app/jobs/regular/sync_topic_user_bookmarked.rb
+++ app/jobs/regular/sync_topic_user_bookmarked.rb
@@ -2,7 +2,7 @@
 
 module Jobs
   class SyncTopicUserBookmarked < ::Jobs::Base
-    def execute(args)
+    def execute(args = {})
       topic_id = args[:topic_id]
 
       DB.exec(<<~SQL, topic_id: topic_id)
@@ -10,8 +10,7 @@ module Jobs
         FROM bookmarks AS b
         WHERE NOT topic_users.bookmarked AND
           topic_users.topic_id = b.topic_id AND
-          topic_users.user_id = b.user_id AND
-          topic_users.topic_id = :topic_id
+          topic_users.user_id = b.user_id #{topic_id.present? ? "AND topic_users.topic_id = :topic_id" : ""}
       SQL
 
       DB.exec(<<~SQL, topic_id: topic_id)
@@ -21,7 +20,7 @@ module Jobs
             SELECT COUNT(*) FROM bookmarks
             WHERE topic_id = topic_users.topic_id
             AND user_id = topic_users.user_id
-        ) = 0 AND topic_users.topic_id = :topic_id
+        ) = 0 #{topic_id.present? ? "AND topic_users.topic_id = :topic_id" : ""}
       SQL
     end
   end
--- spec/jobs/sync_topic_user_bookmarked_spec.rb
+++ spec/jobs/sync_topic_user_bookmarked_spec.rb
@@ -26,4 +26,28 @@ RSpec.describe Jobs::SyncTopicUserBookmarked do
     expect(tu4.reload.bookmarked).to eq(true)
     expect(tu5.reload.bookmarked).to eq(false)
   end
+
+  it "works when no topic id is provided (runs for all topics)" do
+    topic = Fabricate(:topic)
+    Fabricate(:post, topic: topic)
+    Fabricate(:post, topic: topic)
+    Fabricate(:post, topic: topic)
+
+    tu1 = Fabricate(:topic_user, topic: topic, bookmarked: false)
+    tu2 = Fabricate(:topic_user, topic: topic, bookmarked: false)
+    tu3 = Fabricate(:topic_user, topic: topic, bookmarked: true)
+    tu4 = Fabricate(:topic_user, topic: topic, bookmarked: true)
+    tu5 = Fabricate(:topic_user, bookmarked: false)
+
+    Fabricate(:bookmark, user: tu1.user, topic: topic, post: topic.posts.sample)
+    Fabricate(:bookmark, user: tu4.user, topic: topic, post: topic.posts.sample)
+
+    subject.execute
+
+    expect(tu1.reload.bookmarked).to eq(true)
+    expect(tu2.reload.bookmarked).to eq(false)
+    expect(tu3.reload.bookmarked).to eq(false)
+    expect(tu4.reload.bookmarked).to eq(true)
+    expect(tu5.reload.bookmarked).to eq(false)
+  end
 end
