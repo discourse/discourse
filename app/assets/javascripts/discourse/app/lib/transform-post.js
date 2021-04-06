e8a9917db1731636f8659db63e925e743ae4ed11
Neil Lalonde <neillalonde@gmail.com>
2021-04-06 11:13:06 -0400

    FEATURE: Allow setting avatar flair for automatic groups (#12586)

--- app/assets/javascripts/discourse/app/lib/transform-post.js
+++ app/assets/javascripts/discourse/app/lib/transform-post.js
@@ -81,6 +81,7 @@ export function transformBasicPost(post) {
     userCustomFields: post.user_custom_fields,
     readCount: post.readers_count,
     canPublishPage: false,
+    trustLevel: post.trust_level,
   };
 
   _additionalAttributes.forEach((a) => (postAtts[a] = post[a]));
