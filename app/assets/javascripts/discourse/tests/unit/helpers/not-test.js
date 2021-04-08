a0bf24fd27b40d201cc72de24cdbc96d5e152384
jjaffeux <j.jaffeux@gmail.com>
2021-04-08 13:10:16 +0200

    converts tests to our ember mix

--- app/assets/javascripts/discourse/tests/unit/helpers/not-test.js
+++ app/assets/javascripts/discourse/tests/unit/helpers/not-test.js
@@ -1,33 +1,34 @@
 // https://github.com/jmurphyau/ember-truth-helpers/blob/master/tests/unit/helpers/not-test.js
-import { module, test } from "qunit";
-import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
-import { hbs } from "ember-cli-htmlbars";
+import componentTest, {
+  setupRenderingTest,
+} from "discourse/tests/helpers/component-test";
+import { discourseModule, query } from "discourse/tests/helpers/qunit-helpers";
+import hbs from "htmlbars-inline-precompile";
 
-module("Unit | Helper | not", function (hooks) {
+discourseModule("Unit | Helper | not", function (hooks) {
   setupRenderingTest(hooks);
 
-  test("simple test 1", async function (assert) {
-    await render(
-      hbs`[{{not true}}] [{{not false}}] [{{not null}}] [{{not undefined}}] [{{not ''}}] [{{not ' '}}]`
-    );
+  componentTest("simple test 1", {
+    template: hbs`<div id="not-test">[{{not true}}] [{{not false}}] [{{not null}}] [{{not undefined}}] [{{not ''}}] [{{not ' '}}]</div>`,
 
-    assert.equal(
-      this.element.textContent,
-      "[false] [true] [true] [true] [true] [false]",
-      'value should be "[false] [true] [true] [true] [true] [false]"'
-    );
+    test(assert) {
+      assert.equal(
+        query("#not-test").textContent,
+        "[false] [true] [true] [true] [true] [false]",
+        'value should be "[false] [true] [true] [true] [true] [false]"'
+      );
+    },
   });
 
-  test("simple test 2", async function (assert) {
-    await render(
-      hbs`[{{not true false}}] [{{not true false}}] [{{not null null false null}}] [{{not false null ' ' true}}]`
-    );
+  componentTest("simple test 2", {
+    template: hbs`<div id="not-test">[{{not true false}}] [{{not true false}}] [{{not null null false null}}] [{{not false null ' ' true}}]</div>`,
 
-    assert.equal(
-      this.element.textContent,
-      "[false] [false] [true] [false]",
-      'value should be "[false] [false] [true] [false]"'
-    );
+    test(assert) {
+      assert.equal(
+        query("#not-test").textContent,
+        "[false] [false] [true] [false]",
+        'value should be "[false] [false] [true] [false]"'
+      );
+    },
   });
 });
