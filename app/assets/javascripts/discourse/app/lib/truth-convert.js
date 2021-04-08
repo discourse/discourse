e463f5ce0826cf3e3ee86f1d6caf9b28c87c7072
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2021-04-08 13:25:16 +0200

    DEV: introduces {{not}} helper (#12651)
    Code is coming from https://github.com/jmurphyau/ember-truth-helpers, for now I only ported {{not}} which would have tons of use cases in our code base.
    
    We might want to use more helpers in the future, also Ember should have this kind of helpers natively in the future:
    
    - https://github.com/cibernox/rfcs/blob/add-logical-operators-to-templates/text/0000-add-logical-operators.md
    - https://github.com/cibernox/rfcs/blob/add-equality-operators-to-templates/text/0000-add-equality-operators.md
    - https://github.com/cibernox/rfcs/blob/add-numeric-comparison-operators-to-templates/text/0561-add-numeric-comparison-operators.md

---
+++ app/assets/javascripts/discourse/app/lib/truth-convert.js
@@ -0,0 +1,16 @@
+// https://github.com/jmurphyau/ember-truth-helpers/blob/master/addon/utils/truth-convert.js
+import { isArray } from "@ember/array";
+import { get } from "@ember/object";
+
+export default function truthConvert(result) {
+  const truthy = result && get(result, "isTruthy");
+  if (typeof truthy === "boolean") {
+    return truthy;
+  }
+
+  if (isArray(result)) {
+    return get(result, "length") !== 0;
+  } else {
+    return !!result;
+  }
+}
