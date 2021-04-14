c0c8d10cdb324975d84a2ef8c641e8ce71870e7c
Martin Brennan <martin@discourse.org>
2021-04-14 15:14:27 +1000

    Do not allow null allow_unlimited_owner_edits_on_first_post

--- db/migrate/20210414013318_add_category_setting_allow_unlimited_owner_edits_op.rb
+++ db/migrate/20210414013318_add_category_setting_allow_unlimited_owner_edits_op.rb
@@ -2,6 +2,6 @@
 
 class AddCategorySettingAllowUnlimitedOwnerEditsOp < ActiveRecord::Migration[6.0]
   def change
-    add_column :categories, :allow_unlimited_owner_edits_on_first_post, :boolean, default: false
+    add_column :categories, :allow_unlimited_owner_edits_on_first_post, :boolean, default: false, null: false
   end
 end
