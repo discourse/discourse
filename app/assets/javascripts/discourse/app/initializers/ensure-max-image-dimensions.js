1e95247e2b0aef4c69d9e1b627cdff2789b8d2b8
Mark VanLandingham <markvanlan@gmail.com>
2021-04-16 09:39:33 -0500

    DEV: Site setting to disable setting width of images

--- app/assets/javascripts/discourse/app/initializers/ensure-max-image-dimensions.js
+++ app/assets/javascripts/discourse/app/initializers/ensure-max-image-dimensions.js
@@ -9,22 +9,24 @@ export default {
     // This enforces maximum dimensions of images based on site settings
     // for mobile we use the window width as a safeguard
     // This rule should never really be at play unless for some reason images do not have dimensions
-
     const siteSettings = container.lookup("site-settings:main");
-    let width = siteSettings.max_image_width;
-    let height = siteSettings.max_image_height;
+    if (siteSettings.enable_max_image_dimensions) {
+      let width = siteSettings.max_image_width;
+      let height = siteSettings.max_image_height;
 
-    const site = container.lookup("site:main");
-    if (site.mobileView) {
-      width = $(window).width() - 20;
+      const site = container.lookup("site:main");
+      if (site.mobileView) {
+        width = $(window).width() - 20;
+      }
+
+      const style =
+        "max-width:" + width + "px;" + "max-height:" + height + "px;";
+
+      $(
+        '<style id="image-sizing-hack">#reply-control .d-editor-preview img:not(.thumbnail):not(.ytp-thumbnail-image), .cooked img:not(.thumbnail):not(.ytp-thumbnail-image) {' +
+          style +
+          "}</style>"
+      ).appendTo("head");
     }
-
-    const style = "max-width:" + width + "px;" + "max-height:" + height + "px;";
-
-    $(
-      '<style id="image-sizing-hack">#reply-control .d-editor-preview img:not(.thumbnail):not(.ytp-thumbnail-image), .cooked img:not(.thumbnail):not(.ytp-thumbnail-image) {' +
-        style +
-        "}</style>"
-    ).appendTo("head");
   },
 };
