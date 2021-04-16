8b684ce93ef8f51b6b41bc4dc0b509102914391d
Vinoth Kannan <svkn.87@gmail.com>
2021-04-16 09:43:08 +0530

    UX: add css classes on body tag based on topic statuses.
    This commit will add css classes like `unlisted`, `pinned` and `unpinned` on the body tag.

---
+++ app/assets/javascripts/discourse/app/components/add-topic-status-classes.js
@@ -0,0 +1,51 @@
+import Component from "@ember/component";
+import { observes } from "discourse-common/utils/decorators";
+import { scheduleOnce } from "@ember/runloop";
+
+export default Component.extend({
+  _slug: null,
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.refreshClass();
+  },
+
+  _updateClass() {
+    if (this.isDestroying || this.isDestroyed) {
+      return;
+    }
+    const topic = this.topic;
+
+    this._removeClass();
+
+    let classes = [];
+    if (topic.invisible) {
+      classes.push(`topic-status-unlisted`);
+    }
+    if (topic.pinned) {
+      classes.push(`topic-status-pinned`);
+    }
+    if (topic.unpinned) {
+      classes.push(`topic-status-unpinned`);
+    }
+    if (classes.length > 0) {
+      $("body").addClass(classes.join(" "));
+    }
+  },
+
+  @observes("topic.invisible", "topic.pinned", "topic.unpinned")
+  refreshClass() {
+    scheduleOnce("afterRender", this, this._updateClass);
+  },
+
+  _removeClass() {
+    $("body").removeClass((_, css) =>
+      (css.match(/\btopic-status-\S+/g) || []).join(" ")
+    );
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    this._removeClass();
+  },
+});
