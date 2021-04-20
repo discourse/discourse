af402e874a52e4616bb075789b6ab01b46f085ea
Josh Soref <jsoref@users.noreply.github.com>
2021-04-19 21:40:26 -0400

    spelling: embeddable
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2334,7 +2334,7 @@ describe Topic do
       expect(topic.expandable_first_post?).to eq(false)
     end
 
-    describe 'with an emeddable host' do
+    describe 'with an embeddable host' do
       before do
         Fabricate(:embeddable_host)
         SiteSetting.embed_truncate = true
