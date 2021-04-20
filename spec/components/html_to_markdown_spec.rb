095a725e12fb28b8f3395a22fd2ebcb0ab871969
Josh Soref <jsoref@users.noreply.github.com>
2021-04-19 21:52:26 -0400

    spelling: with
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- spec/components/html_to_markdown_spec.rb
+++ spec/components/html_to_markdown_spec.rb
@@ -149,7 +149,7 @@ describe HtmlToMarkdown do
     expect(html_to_markdown(%Q{<img src="foo.bar">})).to eq("")
   end
 
-  it "keeps <img> with src='cid:' whith 'keep_cid_imgs'" do
+  it "keeps <img> with src='cid:' with 'keep_cid_imgs'" do
     expect(html_to_markdown(HTML_WITH_CID_IMG, keep_cid_imgs: true)).to eq(HTML_WITH_CID_IMG)
   end
 
