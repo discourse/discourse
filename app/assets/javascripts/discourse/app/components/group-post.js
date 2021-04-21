f878e46f236ca78d2f48b2ed8652323d2fb5db2a
Jeff Wong <awole20@gmail.com>
2021-04-21 10:04:01 -0700

    Group posts adhere to prioritize name in UX option
    Displays either username or name on the group activity page.
    Removes hardcoded comma, and adds classes to share padding styling

--- app/assets/javascripts/discourse/app/components/group-post.js
+++ app/assets/javascripts/discourse/app/components/group-post.js
@@ -1,6 +1,7 @@
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
 import getURL from "discourse-common/lib/get-url";
+import { prioritizeNameInUx } from "discourse/lib/settings";
 import { propertyEqual } from "discourse/lib/computed";
 
 export default Component.extend({
@@ -14,4 +15,12 @@ export default Component.extend({
     "post.post_type",
     "site.post_types.moderator_action"
   ),
+
+  @discourseComputed("post.user")
+  name() {
+    if (prioritizeNameInUx(this.post.user.name)) {
+      return this.post.user.name;
+    }
+    return this.post.user.username;
+  },
 });
