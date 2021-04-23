ce4017ab337163dd37b1fb62e1788c7475c7d8ec
Bianca Nenciu <nbianca@users.noreply.github.com>
2021-04-23 16:56:13 +0300

    FEATURE: Show a notice if video cannot be rendered (#12718)
    Not all videos can be rendered everywhere because some browser may be
    missing some codecs. This commit adds a notice on top of video to let
    the user know about it.

--- app/assets/javascripts/discourse/app/initializers/post-decorations.js
+++ app/assets/javascripts/discourse/app/initializers/post-decorations.js
@@ -1,3 +1,5 @@
+import { later } from "@ember/runloop";
+import I18n from "I18n";
 import highlightSyntax from "discourse/lib/highlight-syntax";
 import lightbox from "discourse/lib/lightbox";
 import { iconHTML } from "discourse-common/lib/icon-library";
@@ -109,6 +111,31 @@ export default {
           });
         },
         { id: "onebox-source-icons" }
+      );
+
+      api.decorateCookedElement(
+        (element) => {
+          element
+            .querySelectorAll(".video-container")
+            .forEach((videoContainer) => {
+              const video = videoContainer.getElementsByTagName("video")[0];
+              video.addEventListener("loadeddata", () => {
+                later(() => {
+                  if (video.videoWidth === 0 || video.videoHeight === 0) {
+                    const notice = document.createElement("div");
+                    notice.className = "notice";
+                    notice.innerHTML =
+                      iconHTML("exclamation-triangle") +
+                      " " +
+                      I18n.t("cannot_render_video");
+
+                    videoContainer.appendChild(notice);
+                  }
+                }, 500);
+              });
+            });
+        },
+        { id: "discourse-video-codecs" }
       );
     });
   },
