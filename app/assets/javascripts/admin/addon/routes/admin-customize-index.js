discourse/discourse

commit 61860098d979933348dd95e144d1b9ccec1fcdd8

UX: move logs/watched_words to customize/watched_words in admin section (#12571)
https://meta.discourse.org/t/where-is-auto-tag-and-auto-replace/184261

app/assets/javascripts/admin/addon/templates/customize.hbs
@@ -8,6 +8,7 @@
   {{nav-item route="adminEmojis" label="admin.emoji.title" class="admin-customize-emojis"}}
   {{nav-item route="adminPermalinks" label="admin.permalink.title" class="admin-customize-permalinks"}}
   {{nav-item route="adminEmbedding" label="admin.embedding.title" class="admin-customize-embedding"}}
+  {{nav-item route="adminWatchedWords" label="admin.watched_words.title" class="admin-customize-watched-words"}}
 {{/admin-nav}}
 
 <div class="admin-container">

config/routes.rb
@@ -182,19 +182,15 @@ Discourse::Application.routes.draw do
           end
         end
         resources :screened_urls,         only: [:index]
-        resources :watched_words, only: [:index, :create, :update, :destroy] do
-          collection do
-            get "action/:id" => "watched_words#index"
-            get "action/:id/download" => "watched_words#download"
-            delete "action/:id" => "watched_words#clear_all"
-          end
-        end
-        post "watched_words/upload" => "watched_words#upload"
         resources :search_logs,           only: [:index]
         get 'search_logs/term/' => 'search_logs#term'
       end
 
       get "/logs" => "staff_action_logs#index"
+
+      # alias
+      get '/logs/watched_words', to: redirect(relative_url_root + 'admin/customize/watched_words'), constraints: AdminConstraint.new
+      get '/logs/watched_words/*path', to: redirect(relative_url_root + 'admin/customize/watched_words/%{path}'), constraints: AdminConstraint.new
 
       get "customize" => "color_schemes#index", constraints: AdminConstraint.new
       get "customize/themes" => "themes#index", constraints: AdminConstraint.new
@@ -243,6 +239,15 @@ Discourse::Application.routes.draw do
 
         resource :email_style, only: [:show, :update]
         get 'email_style/:field' => 'email_styles#show', constraints: { field: /html|css/ }
+
+        resources :watched_words, only: [:index, :create, :update, :destroy] do
+          collection do
+            get "action/:id" => "watched_words#index"
+            get "action/:id/download" => "watched_words#download"
+            delete "action/:id" => "watched_words#clear_all"
+          end
+        end
+        post "watched_words/upload" => "watched_words#upload"
       end
 
       resources :embeddable_hosts, constraints: AdminConstraint.new

app/assets/javascripts/admin/addon/components/watched-word-uploader.js
@@ -8,7 +8,7 @@ import discourseComputed from "discourse-common/utils/decorators";
 export default Component.extend(UploadMixin, {
   type: "txt",
   classNames: "watched-words-uploader",
-  uploadUrl: "/admin/logs/watched_words/upload",
+  uploadUrl: "/admin/customize/watched_words/upload",
   addDisabled: alias("uploading"),
 
   validateUploadedFilesOptions() {

app/assets/javascripts/admin/addon/controllers/admin-watched-words-action.js
@@ -18,7 +18,7 @@ export default Controller.extend({
   ),
   downloadLink: fmt(
     "actionNameKey",
-    "/admin/logs/watched_words/action/%@/download"
+    "/admin/customize/watched_words/action/%@/download"
   ),
 
   findAction(actionName) {
@@ -97,7 +97,7 @@ export default Controller.extend({
         I18n.t("yes_value"),
         (result) => {
           if (result) {
-            ajax(`/admin/logs/watched_words/action/${actionKey}.json`, {
+            ajax(`/admin/customize/watched_words/action/${actionKey}.json`, {
               type: "DELETE",
             }).then(() => {
               const action = this.findAction(actionKey);

app/assets/javascripts/admin/addon/models/watched-word.js
@@ -5,7 +5,9 @@ import { ajax } from "discourse/lib/ajax";
 const WatchedWord = EmberObject.extend({
   save() {
     return ajax(
-      "/admin/logs/watched_words" + (this.id ? "/" + this.id : "") + ".json",
+      "/admin/customize/watched_words" +
+        (this.id ? "/" + this.id : "") +
+        ".json",
       {
         type: this.id ? "PUT" : "POST",
         data: {
@@ -19,7 +21,7 @@ const WatchedWord = EmberObject.extend({
   },
 
   destroy() {
-    return ajax("/admin/logs/watched_words/" + this.id + ".json", {
+    return ajax("/admin/customize/watched_words/" + this.id + ".json", {
       type: "DELETE",
     });
   },
@@ -27,7 +29,7 @@ const WatchedWord = EmberObject.extend({
 
 WatchedWord.reopenClass({
   findAll() {
-    return ajax("/admin/logs/watched_words.json").then((list) => {
+    return ajax("/admin/customize/watched_words.json").then((list) => {
       const actions = {};
       list.words.forEach((s) => {
         if (!actions[s.action]) {

app/assets/javascripts/admin/addon/routes/admin-route-map.js
@@ -97,6 +97,14 @@ export default function () {
             this.route("edit", { path: "/:field_name" });
           }
         );
+        this.route(
+          "adminWatchedWords",
+          { path: "/watched_words", resetNamespace: true },
+          function () {
+            this.route("index", { path: "/" });
+            this.route("action", { path: "/action/:action_id" });
+          }
+        );
       }
     );
 
@@ -150,14 +158,6 @@ export default function () {
           function () {
             this.route("index", { path: "/" });
             this.route("term", { path: "/term" });
-          }
-        );
-        this.route(
-          "adminWatchedWords",
-          { path: "/watched_words", resetNamespace: true },
-          function () {
-            this.route("index", { path: "/" });
-            this.route("action", { path: "/action/:action_id" });
           }
         );
       }

app/assets/javascripts/admin/addon/templates/logs.hbs
@@ -3,7 +3,6 @@
   {{nav-item route="adminLogs.screenedEmails" label="admin.logs.screened_emails.title"}}
   {{nav-item route="adminLogs.screenedIpAddresses" label="admin.logs.screened_ips.title"}}
   {{nav-item route="adminLogs.screenedUrls" label="admin.logs.screened_urls.title"}}
-  {{nav-item route="adminWatchedWords" label="admin.watched_words.title"}}
   {{nav-item route="adminSearchLogs" label="admin.logs.search_logs.title"}}
   {{#if currentUser.admin}}
     {{nav-item path="/logs" label="admin.logs.logster.title"}}

app/assets/javascripts/discourse/tests/acceptance/admin-watched-words-test.js
@@ -10,7 +10,7 @@ acceptance("Admin - Watched Words", function (needs) {
   needs.user();
 
   test("list words in groups", async function (assert) {
-    await visit("/admin/logs/watched_words/action/block");
+    await visit("/admin/customize/watched_words/action/block");
 
     assert.ok(
       !exists(".watched-words-list"),
@@ -51,7 +51,7 @@ acceptance("Admin - Watched Words", function (needs) {
   });
 
   test("add words", async function (assert) {
-    await visit("/admin/logs/watched_words/action/block");
+    await visit("/admin/customize/watched_words/action/block");
 
     click(".show-words-checkbox");
     fillIn(".watched-word-form input", "poutine");
@@ -68,7 +68,7 @@ acceptance("Admin - Watched Words", function (needs) {
   });
 
   test("remove words", async function (assert) {
-    await visit("/admin/logs/watched_words/action/block");
+    await visit("/admin/customize/watched_words/action/block");
     await click(".show-words-checkbox");
 
     let word = null;

commit 1afca8c816194ac11173b6b591e42cbca99dbf1c

FIX: allow moderators to access /admin/customize/watched_words
Moderators were unable to access `/admin/customize/watched_words`
feature. This was regressed in https://github.com/discourse/discourse/commit/61860098d979933348dd95e144d1b9ccec1fcdd8

app/assets/javascripts/admin/addon/routes/admin-customize-index.js
@@ -1,6 +1,10 @@
 import Route from "@ember/routing/route";
 export default Route.extend({
   beforeModel() {
-    this.transitionTo("adminCustomizeThemes");
+    if (this.currentUser.admin) {
+      this.transitionTo("adminCustomizeThemes");
+    } else {
+      this.transitionTo("adminWatchedWords");
+    }
   },
 });

app/assets/javascripts/admin/addon/templates/admin.hbs
@@ -18,8 +18,8 @@
             {{nav-item route="adminEmail" label="admin.email.title"}}
           {{/if}}
           {{nav-item route="adminLogs" label="admin.logs.title"}}
+          {{nav-item route="adminCustomize" label="admin.customize.title"}}
           {{#if currentUser.admin}}
-            {{nav-item route="adminCustomize" label="admin.customize.title"}}
             {{nav-item route="adminApi" label="admin.api.title"}}
             {{#if siteSettings.enable_backups}}
               {{nav-item route="admin.backups" label="admin.backups.title"}}

app/assets/javascripts/admin/addon/templates/customize.hbs
@@ -1,13 +1,15 @@
 {{#admin-nav}}
-  {{nav-item route="adminCustomizeThemes" label="admin.customize.theme.title" class="admin-customize-themes"}}
-  {{nav-item route="adminCustomize.colors" label="admin.customize.colors.title" class="admin-customize-colors"}}
-  {{nav-item route="adminSiteText" label="admin.site_text.title" class="admin-customize-site-text"}}
-  {{nav-item route="adminCustomizeEmailTemplates" label="admin.customize.email_templates.title" class="admin-customize-email-templates"}}
-  {{nav-item route="adminCustomizeEmailStyle" label="admin.customize.email_style.title" class="admin-customize-email-styles"}}
-  {{nav-item route="adminUserFields" label="admin.user_fields.title" class="admin-customize-user-fields"}}
-  {{nav-item route="adminEmojis" label="admin.emoji.title" class="admin-customize-emojis"}}
-  {{nav-item route="adminPermalinks" label="admin.permalink.title" class="admin-customize-permalinks"}}
-  {{nav-item route="adminEmbedding" label="admin.embedding.title" class="admin-customize-embedding"}}
+  {{#if currentUser.admin}}
+    {{nav-item route="adminCustomizeThemes" label="admin.customize.theme.title" class="admin-customize-themes"}}
+    {{nav-item route="adminCustomize.colors" label="admin.customize.colors.title" class="admin-customize-colors"}}
+    {{nav-item route="adminSiteText" label="admin.site_text.title" class="admin-customize-site-text"}}
+    {{nav-item route="adminCustomizeEmailTemplates" label="admin.customize.email_templates.title" class="admin-customize-email-templates"}}
+    {{nav-item route="adminCustomizeEmailStyle" label="admin.customize.email_style.title" class="admin-customize-email-styles"}}
+    {{nav-item route="adminUserFields" label="admin.user_fields.title" class="admin-customize-user-fields"}}
+    {{nav-item route="adminEmojis" label="admin.emoji.title" class="admin-customize-emojis"}}
+    {{nav-item route="adminPermalinks" label="admin.permalink.title" class="admin-customize-permalinks"}}
+    {{nav-item route="adminEmbedding" label="admin.embedding.title" class="admin-customize-embedding"}}
+  {{/if}}
   {{nav-item route="adminWatchedWords" label="admin.watched_words.title" class="admin-customize-watched-words"}}
 {{/admin-nav}}
 

config/routes.rb
@@ -189,8 +189,8 @@ Discourse::Application.routes.draw do
       get "/logs" => "staff_action_logs#index"
 
       # alias
-      get '/logs/watched_words', to: redirect(relative_url_root + 'admin/customize/watched_words'), constraints: AdminConstraint.new
-      get '/logs/watched_words/*path', to: redirect(relative_url_root + 'admin/customize/watched_words/%{path}'), constraints: AdminConstraint.new
+      get '/logs/watched_words', to: redirect(relative_url_root + 'admin/customize/watched_words')
+      get '/logs/watched_words/*path', to: redirect(relative_url_root + 'admin/customize/watched_words/%{path}')
 
       get "customize" => "color_schemes#index", constraints: AdminConstraint.new
       get "customize/themes" => "themes#index", constraints: AdminConstraint.new
@@ -239,7 +239,13 @@ Discourse::Application.routes.draw do
 
         resource :email_style, only: [:show, :update]
         get 'email_style/:field' => 'email_styles#show', constraints: { field: /html|css/ }
+      end
 
+      resources :embeddable_hosts, constraints: AdminConstraint.new
+      resources :color_schemes, constraints: AdminConstraint.new
+      resources :permalinks, constraints: AdminConstraint.new
+
+      scope "/customize" do
         resources :watched_words, only: [:index, :create, :update, :destroy] do
           collection do
             get "action/:id" => "watched_words#index"
@@ -249,11 +255,6 @@ Discourse::Application.routes.draw do
         end
         post "watched_words/upload" => "watched_words#upload"
       end
-
-      resources :embeddable_hosts, constraints: AdminConstraint.new
-      resources :color_schemes, constraints: AdminConstraint.new
-
-      resources :permalinks, constraints: AdminConstraint.new
 
       get "version_check" => "versions#show"
 