127d856a168bf50bf14b8bb1c2898dfe8a5a2336
Rafael dos Santos Silva <xfalcox@gmail.com>
2021-04-28 17:01:10 -0300

    FIX: Be more strict about allowed chars in class name

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -34,7 +34,9 @@ class EmbedController < ApplicationController
       raise Discourse::InvalidParameters.new(:embed_id) unless @embed_id =~ /^de\-[a-zA-Z0-9]+$/
     end
 
-    @embed_class = params[:embed_class]
+    if @embed_class = params[:embed_class]
+      raise Discourse::InvalidParameters.new(:embed_class) unless @embed_class =~ /^[a-zA-Z0-9\-_]+$/
+    end
 
     if params.has_key?(:template) && params[:template] == "complete"
       @template = "complete"
