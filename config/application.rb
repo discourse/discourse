ad8c7714c8c706519485faf33a1d68cdaa410362
David Taylor <david@taylorhq.com>
2021-04-30 10:54:49 +0100

    DEV: Use filesystem-based SchemaCache in development (#12901)
    In development we regularly restart/reload Rails, which wipes out the schema cache. This then has to be regenerated using DDL queries on the database.
    
    Instead, we can make use of the `rake db:schema:cache:dump` command. This will dump the schema cache to a YAML file, and then load it when needed. This is significantly faster than rebuilding the cache from DDL queries every time.

--- config/application.rb
+++ config/application.rb
@@ -235,6 +235,9 @@ module Discourse
     # see: http://stackoverflow.com/questions/11894180/how-does-one-correctly-add-custom-sql-dml-in-migrations/11894420#11894420
     config.active_record.schema_format = :sql
 
+    # We use this in development-mode only (see development.rb)
+    config.active_record.use_schema_cache_dump = false
+
     # per https://www.owasp.org/index.php/Password_Storage_Cheat_Sheet
     config.pbkdf2_iterations = 64000
     config.pbkdf2_algorithm = "sha256"
