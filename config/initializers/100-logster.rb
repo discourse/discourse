2e63e3f13f35e6758ba1ed3b5813bc6f636502b8
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2021-05-06 10:42:08 +0800

    DEV: Remove autoloading in initializers depreciation message.

--- config/initializers/100-logster.rb
+++ config/initializers/100-logster.rb
@@ -19,13 +19,15 @@ if Rails.env.development? && RUBY_VERSION.match?(/^2\.5\.[23]/)
 end
 
 if Rails.env.development? && !Sidekiq.server? && ENV["RAILS_LOGS_STDOUT"] == "1"
-  console = ActiveSupport::Logger.new(STDOUT)
-  original_logger = Rails.logger.chained.first
-  console.formatter = original_logger.formatter
-  console.level = original_logger.level
+  Rails.application.config.after_initialize do
+    console = ActiveSupport::Logger.new(STDOUT)
+    original_logger = Rails.logger.chained.first
+    console.formatter = original_logger.formatter
+    console.level = original_logger.level
 
-  unless ActiveSupport::Logger.logger_outputs_to?(original_logger, STDOUT)
-    original_logger.extend(ActiveSupport::Logger.broadcast(console))
+    unless ActiveSupport::Logger.logger_outputs_to?(original_logger, STDOUT)
+      original_logger.extend(ActiveSupport::Logger.broadcast(console))
+    end
   end
 end
 
