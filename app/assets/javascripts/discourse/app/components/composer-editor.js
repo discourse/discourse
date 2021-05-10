eb5e39b4b4b604a40370b8bfcac26084ac6bc7a4
Rafael dos Santos Silva <xfalcox@gmail.com>
2021-05-10 15:01:06 -0300

    FEATURE: New API hook to allow for client side upload transforms

--- app/assets/javascripts/discourse/app/components/composer-editor.js
+++ app/assets/javascripts/discourse/app/components/composer-editor.js
@@ -53,6 +53,13 @@ export function addComposerUploadHandler(extensions, method) {
   });
 }
 
+const uploadProcessorQueue = [];
+const uploadProcessorActions = {};
+export function addComposerUploadProcessor(queueItem, actionItem) {
+  uploadProcessorQueue.push(queueItem);
+  Object.assign(uploadProcessorActions, actionItem);
+}
+
 const uploadMarkdownResolvers = [];
 export function addComposerUploadMarkdownResolver(resolver) {
   uploadMarkdownResolvers.push(resolver);
@@ -634,11 +641,33 @@ export default Component.extend({
 
     const $element = $(this.element);
 
+    $.blueimp.fileupload.prototype.processActions = uploadProcessorActions;
+
     $element.fileupload({
       url: getURL(`/uploads.json?client_id=${this.messageBus.clientId}`),
       dataType: "json",
       pasteZone: $element,
+      processQueue: uploadProcessorQueue,
     });
+
+    $element
+      .on("fileuploadprocess", (e, data) => {
+        this.appEvents.trigger(
+          "composer:insert-text",
+          `[${I18n.t("processing_filename", {
+            filename: data.files[data.index].name,
+          })}]()\n`
+        );
+      })
+      .on("fileuploadprocessalways", (e, data) => {
+        this.appEvents.trigger(
+          "composer:replace-text",
+          `[${I18n.t("processing_filename", {
+            filename: data.files[data.index].name,
+          })}]()\n`,
+          ""
+        );
+      });
 
     $element.on("fileuploadpaste", (e) => {
       this._pasted = true;
