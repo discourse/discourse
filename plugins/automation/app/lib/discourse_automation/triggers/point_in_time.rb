d5bf24ea7eae323a9ebbe01e92c596d74a861282
jjaffeux <j.jaffeux@gmail.com>
2021-05-12 14:40:12 +0200

    FIX: prevents sending gift exchange on save

--- plugins/automation/app/lib/discourse_automation/triggers/point_in_time.rb
+++ plugins/automation/app/lib/discourse_automation/triggers/point_in_time.rb
@@ -4,8 +4,11 @@ DiscourseAutomation::Triggerable::POINT_IN_TIME = 'point_in_time'
 
 DiscourseAutomation::Triggerable.add(DiscourseAutomation::Triggerable::POINT_IN_TIME) do
   on_update do |automation, metadata|
-    automation
-      .pending_automations
-      .create!(execute_at: metadata[:execute_at])
+    # prevents creating a new pending automation on save when date is expired
+    if Time.parse(metadata[:execute_at]) > Time.zone.now
+      automation
+        .pending_automations
+        .create!(execute_at: metadata[:execute_at])
+    end
   end
 end
