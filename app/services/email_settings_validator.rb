85b505374b0c56d401733839c812f6f5f0b5872b
Martin Brennan <martin@discourse.org>
2021-05-13 14:05:14 +1000

    Improve comments

--- app/services/email_settings_validator.rb
+++ app/services/email_settings_validator.rb
@@ -11,6 +11,7 @@ require 'net/pop'
 #
 #   # or for specific host preset
 #   EmailSettingsValidator.validate_imap(**{ username: "test@gmail.com", password: "test" }.merge(Email.gmail_imap_settings))
+#
 # rescue => err
 #   EmailSettingsValidator.friendly_exception_message(err)
 # end
@@ -20,19 +21,17 @@ class EmailSettingsValidator
     when Net::POPAuthenticationError
       I18n.t("email_settings.pop3_authentication_error")
     when Net::IMAP::NoResponseError
-      # Most of IMAP's errors are lumped under here, including invalid
+
+      # Most of IMAP's errors are lumped under the NoResponseError, including invalid
       # credentials errors, because it is raised when a "NO" response is
       # raised from the IMAP server https://datatracker.ietf.org/doc/html/rfc3501#section-7.1.2
       #
       # Generally, it should be fairly safe to just return the error message as is.
-
       if exception.message.match(/Invalid credentials/)
-        return I18n.t("email_settings.imap_authentication_error")
+        I18n.t("email_settings.imap_authentication_error")
+      else
+        I18n.t("email_settings.imap_no_response_error", message: exception.message.gsub(" (Failure)", ""))
       end
-
-      I18n.t("email_settings.imap_no_response_error", message: exception.message.gsub(" (Failure)", ""))
-
-      # special case for bad creds
     when Net::SMTPAuthenticationError
       I18n.t("email_settings.smtp_authentication_error")
     when Net::SMTPServerBusy
@@ -48,6 +47,12 @@ class EmailSettingsValidator
     end
   end
 
+  ##
+  # Attempts to authenticate and disconnect a POP3 session and if that raises
+  # an error then it is assumed the credentials or some other settings are wrong.
+  #
+  # @param debug [Boolean] - When set to true, any errors will be logged at a warning
+  #                          level before being re-raised.
   def self.validate_pop3(
     host:,
     port:,
@@ -78,8 +83,19 @@ class EmailSettingsValidator
     end
   end
 
-  # domain: used for HELO, will be the email sender's domain, so often
-  # will just be the host e.g. the domain for test@gmail.com is gmail.com
+  ##
+  # Attempts to start an SMTP session and if that raises an error then it is
+  # assumed the credentials or other settings are wrong.
+  #
+  # For Gmail, the port should be 587, enable_starttls_auto should be true,
+  # and enable_tls should be false.
+  #
+  # @param domain [String] - Used for HELO, will be the email sender's domain, so often
+  #                          will just be the host e.g. the domain for test@gmail.com is gmail.com.
+  #                          localhost can be used in development mode.
+  #                          See https://datatracker.ietf.org/doc/html/rfc788#section-4
+  # @param debug [Boolean] - When set to true, any errors will be logged at a warning
+  #                          level before being re-raised.
   def self.validate_smtp(
     host:,
     port:,
@@ -127,6 +143,12 @@ class EmailSettingsValidator
     end
   end
 
+  ##
+  # Attempts to login, logout, and disconnect an IMAP session and if that raises
+  # an error then it is assumed the credentials or some other settings are wrong.
+  #
+  # @param debug [Boolean] - When set to true, any errors will be logged at a warning
+  #                          level before being re-raised.
   def self.validate_imap(
     host:,
     port:,
