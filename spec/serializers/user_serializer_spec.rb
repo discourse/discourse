discourse/discourse

commit 72648dd576968e4c46627bd0b88891e868a9d7f8

FIX: Base topic details message on current category and tag tracking state (#12937)
The user may have changed their category or tag tracking settings since a topic was tracked/watched based on those settings in the past. In that case we need to alter the reason message we show them otherwise it is very confusing for the end user to be told they are tracking a topic because of a category, when they are no longer tracking that category.

For example: "You will see a count of new replies because you are tracking this category." becomes: "You will see a count of new replies because you were tracking this category in the past."

To do this, it was necessary to add tag and category tracking info to current user serializer. I improved the serializer code so it only does 3 SQL queries instead of 9 to get the tracking information for tags and categories for the current user.

app/models/category_user.rb
@@ -202,15 +202,21 @@ class CategoryUser < ActiveRecord::Base
   end
 
   def self.notification_levels_for(guardian)
+    # Anonymous users have all default categories set to regular tracking,
+    # except for default muted categories which stay muted.
     if guardian.anonymous?
       notification_levels = [
         SiteSetting.default_categories_watching.split("|"),
         SiteSetting.default_categories_tracking.split("|"),
         SiteSetting.default_categories_watching_first_post.split("|"),
         SiteSetting.default_categories_regular.split("|")
-      ].flatten.map { |id| [id.to_i, self.notification_levels[:regular]] }
+      ].flatten.map do |id|
+        [id.to_i, self.notification_levels[:regular]]
+      end
 
-      notification_levels += SiteSetting.default_categories_muted.split("|").map { |id| [id.to_i, self.notification_levels[:muted]] }
+      notification_levels += SiteSetting.default_categories_muted.split("|").map do |id|
+        [id.to_i, self.notification_levels[:muted]]
+      end
     else
       notification_levels = CategoryUser.where(user: guardian.user).pluck(:category_id, :notification_level)
     end

app/models/tag_user.rb
@@ -192,6 +192,28 @@ class TagUser < ActiveRecord::Base
                  auto_track_tag: TopicUser.notification_reasons[:auto_track_tag])
   end
 
+  def self.notification_levels_for(guardian)
+    # Anonymous users have all default tags set to regular tracking,
+    # except for default muted tags which stay muted.
+    if guardian.anonymous?
+      notification_levels = [
+        SiteSetting.default_tags_watching_first_post.split("|"),
+        SiteSetting.default_tags_watching.split("|"),
+        SiteSetting.default_tags_tracking.split("|")
+      ].flatten.map do |name|
+        [name, self.notification_levels[:regular]]
+      end
+
+      notification_levels += SiteSetting.default_tags_muted.split("|").map do |name|
+        [name, self.notification_levels[:muted]]
+      end
+    else
+      notification_levels = TagUser.where(user: guardian.user).joins(:tag).pluck("tags.name", :notification_level)
+    end
+
+    Hash[*notification_levels.flatten]
+  end
+
 end
 
 # == Schema Information

app/serializers/basic_user_serializer.rb
@@ -22,4 +22,24 @@ class BasicUserSerializer < ApplicationSerializer
   def user
     object[:user] || object.try(:user) || object
   end
+
+  def categories_with_notification_level(lookup_level)
+    category_user_notification_levels.select do |id, level|
+      level == CategoryUser.notification_levels[lookup_level]
+    end.keys
+  end
+
+  def category_user_notification_levels
+    @category_user_notification_levels ||= CategoryUser.notification_levels_for(scope)
+  end
+
+  def tags_with_notification_level(lookup_level)
+    tag_user_notification_levels.select do |id, level|
+      level == TagUser.notification_levels[lookup_level]
+    end.keys
+  end
+
+  def tag_user_notification_levels
+    @tag_user_notification_levels ||= TagUser.notification_levels_for(scope)
+  end
 end

app/serializers/current_user_serializer.rb
@@ -28,7 +28,16 @@ class CurrentUserSerializer < BasicUserSerializer
              :redirected_to_top,
              :custom_fields,
              :muted_category_ids,
+             :regular_category_ids,
+             :tracked_category_ids,
+             :watched_first_post_category_ids,
+             :watched_category_ids,
              :muted_tag_ids,
+             :watched_tags,
+             :watching_first_post_tags,
+             :tracked_tags,
+             :muted_tags,
+             :regular_tags,
              :dismissed_banner_key,
              :is_anonymous,
              :reviewable_count,
@@ -53,7 +62,7 @@ class CurrentUserSerializer < BasicUserSerializer
              :skip_new_user_tips,
              :do_not_disturb_until,
              :has_topic_draft,
-             :can_review
+             :can_review,
 
   def groups
     object.visible_groups.pluck(:id, :name).map { |id, name| { id: id, name: name } }
@@ -181,11 +190,49 @@ class CurrentUserSerializer < BasicUserSerializer
   end
 
   def muted_category_ids
-    CategoryUser.lookup(object, :muted).pluck(:category_id)
+    categories_with_notification_level(:muted)
   end
 
+  def regular_category_ids
+    categories_with_notification_level(:regular)
+  end
+
+  def tracked_category_ids
+    categories_with_notification_level(:tracking)
+  end
+
+  def watched_category_ids
+    categories_with_notification_level(:watching)
+  end
+
+  def watched_first_post_category_ids
+    categories_with_notification_level(:watching_first_post)
+  end
+
+  # this is a weird outlier that is used for topic tracking state which
+  # needs the actual ids, which is why it is duplicated with muted_tags
   def muted_tag_ids
     TagUser.lookup(object, :muted).pluck(:tag_id)
+  end
+
+  def muted_tags
+    tags_with_notification_level(:muted)
+  end
+
+  def tracked_tags
+    tags_with_notification_level(:tracked)
+  end
+
+  def watching_first_post_tags
+    tags_with_notification_level(:watching_first_post)
+  end
+
+  def watched_tags
+    tags_with_notification_level(:watching)
+  end
+
+  def regular_tags
+    tags_with_notification_level(:regular)
   end
 
   def ignored_users

app/serializers/user_serializer.rb
@@ -207,39 +207,39 @@ class UserSerializer < UserCardSerializer
   ### PRIVATE ATTRIBUTES
   ###
   def muted_tags
-    TagUser.lookup(object, :muted).joins(:tag).pluck('tags.name')
+    tags_with_notification_level(:muted)
   end
 
   def tracked_tags
-    TagUser.lookup(object, :tracking).joins(:tag).pluck('tags.name')
+    tags_with_notification_level(:tracked)
   end
 
   def watching_first_post_tags
-    TagUser.lookup(object, :watching_first_post).joins(:tag).pluck('tags.name')
+    tags_with_notification_level(:watching_first_post)
   end
 
   def watched_tags
-    TagUser.lookup(object, :watching).joins(:tag).pluck('tags.name')
+    tags_with_notification_level(:watching)
   end
 
   def muted_category_ids
-    CategoryUser.lookup(object, :muted).pluck(:category_id)
+    categories_with_notification_level(:muted)
   end
 
   def regular_category_ids
-    CategoryUser.lookup(object, :regular).pluck(:category_id)
+    categories_with_notification_level(:regular)
   end
 
   def tracked_category_ids
-    CategoryUser.lookup(object, :tracking).pluck(:category_id)
+    categories_with_notification_level(:tracking)
   end
 
   def watched_category_ids
-    CategoryUser.lookup(object, :watching).pluck(:category_id)
+    categories_with_notification_level(:watching)
   end
 
   def watched_first_post_category_ids
-    CategoryUser.lookup(object, :watching_first_post).pluck(:category_id)
+    categories_with_notification_level(:watching_first_post)
   end
 
   def muted_usernames

spec/models/category_user_spec.rb
@@ -219,6 +219,54 @@ describe CategoryUser do
 
       expect(CategoryUser.where(user_id: user.id).count).to eq(0)
     end
+  end
 
+  describe "#notification_levels_for" do
+    let(:guardian) { Guardian.new(user) }
+    let!(:category1) { Fabricate(:category) }
+    let!(:category2) { Fabricate(:category) }
+    let!(:category3) { Fabricate(:category) }
+    let!(:category4) { Fabricate(:category) }
+    let!(:category5) { Fabricate(:category) }
+
+    context "for anon" do
+      let(:user) { nil }
+      before do
+        SiteSetting.default_categories_watching = category1.id.to_s
+        SiteSetting.default_categories_tracking = category2.id.to_s
+        SiteSetting.default_categories_watching_first_post = category3.id.to_s
+        SiteSetting.default_categories_regular = category4.id.to_s
+        SiteSetting.default_categories_muted = category5.id.to_s
+      end
+      it "every category from the default_categories_* site settings get overridden to regular, except for muted" do
+        levels = CategoryUser.notification_levels_for(guardian)
+        expect(levels[category1.id]).to eq(CategoryUser.notification_levels[:regular])
+        expect(levels[category2.id]).to eq(CategoryUser.notification_levels[:regular])
+        expect(levels[category3.id]).to eq(CategoryUser.notification_levels[:regular])
+        expect(levels[category4.id]).to eq(CategoryUser.notification_levels[:regular])
+        expect(levels[category5.id]).to eq(CategoryUser.notification_levels[:muted])
+      end
+    end
+
+    context "for a user" do
+      before do
+        CategoryUser.create(user: user, category: category1, notification_level: CategoryUser.notification_levels[:watching])
+        CategoryUser.create(user: user, category: category2, notification_level: CategoryUser.notification_levels[:tracking])
+        CategoryUser.create(user: user, category: category3, notification_level: CategoryUser.notification_levels[:watching_first_post])
+        CategoryUser.create(user: user, category: category4, notification_level: CategoryUser.notification_levels[:regular])
+        CategoryUser.create(user: user, category: category5, notification_level: CategoryUser.notification_levels[:muted])
+      end
+      it "gets the category_user notification levels for all categories the user is tracking and does not
+      include categories the user is not tracking at all" do
+        category6 = Fabricate(:category)
+        levels = CategoryUser.notification_levels_for(guardian)
+        expect(levels[category1.id]).to eq(CategoryUser.notification_levels[:watching])
+        expect(levels[category2.id]).to eq(CategoryUser.notification_levels[:tracking])
+        expect(levels[category3.id]).to eq(CategoryUser.notification_levels[:watching_first_post])
+        expect(levels[category4.id]).to eq(CategoryUser.notification_levels[:regular])
+        expect(levels[category5.id]).to eq(CategoryUser.notification_levels[:muted])
+        expect(levels.key?(category6.id)).to eq(false)
+      end
+    end
   end
 end

spec/models/tag_user_spec.rb
@@ -232,4 +232,49 @@ describe TagUser do
       end
     end
   end
+
+  describe "#notification_levels_for" do
+    let(:guardian) { Guardian.new(user) }
+    let!(:tag1) { Fabricate(:tag) }
+    let!(:tag2) { Fabricate(:tag) }
+    let!(:tag3) { Fabricate(:tag) }
+    let!(:tag4) { Fabricate(:tag) }
+
+    context "for anon" do
+      let(:user) { nil }
+      before do
+        SiteSetting.default_tags_watching = tag1.name
+        SiteSetting.default_tags_tracking = tag2.name
+        SiteSetting.default_tags_watching_first_post = tag3.name
+        SiteSetting.default_tags_muted = tag4.name
+      end
+      it "every tag from the default_tags_* site settings get overridden to watching_first_post, except for muted" do
+        levels = TagUser.notification_levels_for(guardian)
+        expect(levels[tag1.name]).to eq(TagUser.notification_levels[:regular])
+        expect(levels[tag2.name]).to eq(TagUser.notification_levels[:regular])
+        expect(levels[tag3.name]).to eq(TagUser.notification_levels[:regular])
+        expect(levels[tag4.name]).to eq(TagUser.notification_levels[:muted])
+      end
+    end
+
+    context "for a user" do
+      let(:user) { Fabricate(:user) }
+      before do
+        TagUser.create(user: user, tag: tag1, notification_level: TagUser.notification_levels[:watching])
+        TagUser.create(user: user, tag: tag2, notification_level: TagUser.notification_levels[:tracking])
+        TagUser.create(user: user, tag: tag3, notification_level: TagUser.notification_levels[:watching_first_post])
+        TagUser.create(user: user, tag: tag4, notification_level: TagUser.notification_levels[:muted])
+      end
+      it "gets the tag_user notification levels for all tags the user is tracking and does not
+      include tags the user is not tracking at all" do
+        tag5 = Fabricate(:tag)
+        levels = TagUser.notification_levels_for(guardian)
+        expect(levels[tag1.name]).to eq(TagUser.notification_levels[:watching])
+        expect(levels[tag2.name]).to eq(TagUser.notification_levels[:tracking])
+        expect(levels[tag3.name]).to eq(TagUser.notification_levels[:watching_first_post])
+        expect(levels[tag4.name]).to eq(TagUser.notification_levels[:muted])
+        expect(levels.key?(tag5.name)).to eq(false)
+      end
+    end
+  end
 end

app/assets/javascripts/discourse/app/models/topic-details.js
@@ -1,6 +1,4 @@
 import EmberObject from "@ember/object";
-import I18n from "I18n";
-import { NotificationLevels } from "discourse/lib/notification-levels";
 import RestModel from "discourse/models/rest";
 import User from "discourse/models/user";
 import { ajax } from "discourse/lib/ajax";
@@ -9,8 +7,6 @@ import { ajax } from "discourse/lib/ajax";
   A model representing a Topic's details that aren't always present, such as a list of participants.
   When showing topics in lists and such this information should not be required.
 **/
-import discourseComputed from "discourse-common/utils/decorators";
-import getURL from "discourse-common/lib/get-url";
 
 const TopicDetails = RestModel.extend({
   loaded: false,
@@ -33,34 +29,6 @@ const TopicDetails = RestModel.extend({
 
     this.setProperties(details);
     this.set("loaded", true);
-  },
-
-  @discourseComputed("notification_level", "notifications_reason_id")
-  notificationReasonText(level, reason) {
-    if (typeof level !== "number") {
-      level = 1;
-    }
-
-    let localeString = `topic.notifications.reasons.${level}`;
-    if (typeof reason === "number") {
-      const tmp = localeString + "_" + reason;
-      // some sane protection for missing translations of edge cases
-      if (I18n.lookup(tmp, { locale: "en" })) {
-        localeString = tmp;
-      }
-    }
-
-    if (
-      User.currentProp("mailing_list_mode") &&
-      level > NotificationLevels.MUTED
-    ) {
-      return I18n.t("topic.notifications.reasons.mailing_list_mode");
-    } else {
-      return I18n.t(localeString, {
-        username: User.currentProp("username_lower"),
-        basePath: getURL(""),
-      });
-    }
   },
 
   updateNotifications(level) {

commit 38742bc2080eb4ba3d55ff05288b1b2507747774

FIX: Wrong scope used for notification levels user serializer (#13039)
This is a recent regression introduced by https://github.com/discourse/discourse/pull/12937 which makes it so that when looking at a user profile that is not your own, specifically the category and tag notification settings, you would see your own settings instead of the target user. This is only a problem for admins because regular users cannot see these details for other users.

The issue was that we were using `scope` in the serializer, which refers to the current user, rather than using a scope for the target user via `Guardian.new(user)`.

However, on further inspection the `notification_levels_for` method for `TagUser` and `CategoryUser` did not actually need to be accepting an instance of Guardian, all that it was using it for was to check guardian.anonymous? which is just a fancy way of saying user.blank?. Changed this method to just accept a user instead and send the user in from the serializer.

app/models/category_list.rb
@@ -100,7 +100,7 @@ class CategoryList
 
     @categories = @categories.to_a
 
-    notification_levels = CategoryUser.notification_levels_for(@guardian)
+    notification_levels = CategoryUser.notification_levels_for(@guardian.user)
     default_notification_level = CategoryUser.default_notification_level
 
     allowed_topic_create = Set.new(Category.topic_create_allowed(@guardian).pluck(:id))

app/models/category_user.rb
@@ -201,10 +201,10 @@ class CategoryUser < ActiveRecord::Base
     SiteSetting.mute_all_categories_by_default ? notification_levels[:muted] : notification_levels[:regular]
   end
 
-  def self.notification_levels_for(guardian)
+  def self.notification_levels_for(user)
     # Anonymous users have all default categories set to regular tracking,
     # except for default muted categories which stay muted.
-    if guardian.anonymous?
+    if user.blank?
       notification_levels = [
         SiteSetting.default_categories_watching.split("|"),
         SiteSetting.default_categories_tracking.split("|"),
@@ -218,7 +218,7 @@ class CategoryUser < ActiveRecord::Base
         [id.to_i, self.notification_levels[:muted]]
       end
     else
-      notification_levels = CategoryUser.where(user: guardian.user).pluck(:category_id, :notification_level)
+      notification_levels = CategoryUser.where(user: user).pluck(:category_id, :notification_level)
     end
 
     Hash[*notification_levels.flatten]

app/models/site.rb
@@ -55,7 +55,7 @@ class Site
 
       by_id = {}
 
-      notification_levels = CategoryUser.notification_levels_for(@guardian)
+      notification_levels = CategoryUser.notification_levels_for(@guardian.user)
       default_notification_level = CategoryUser.default_notification_level
 
       categories.each do |category|

app/models/tag_user.rb
@@ -192,10 +192,10 @@ class TagUser < ActiveRecord::Base
                  auto_track_tag: TopicUser.notification_reasons[:auto_track_tag])
   end
 
-  def self.notification_levels_for(guardian)
+  def self.notification_levels_for(user)
     # Anonymous users have all default tags set to regular tracking,
     # except for default muted tags which stay muted.
-    if guardian.anonymous?
+    if user.blank?
       notification_levels = [
         SiteSetting.default_tags_watching_first_post.split("|"),
         SiteSetting.default_tags_watching.split("|"),
@@ -208,7 +208,7 @@ class TagUser < ActiveRecord::Base
         [name, self.notification_levels[:muted]]
       end
     else
-      notification_levels = TagUser.where(user: guardian.user).joins(:tag).pluck("tags.name", :notification_level)
+      notification_levels = TagUser.where(user: user).joins(:tag).pluck("tags.name", :notification_level)
     end
 
     Hash[*notification_levels.flatten]

app/serializers/basic_user_serializer.rb
@@ -23,6 +23,10 @@ class BasicUserSerializer < ApplicationSerializer
     object[:user] || object.try(:user) || object
   end
 
+  def user_is_current_user
+    object.id == scope.user&.id
+  end
+
   def categories_with_notification_level(lookup_level)
     category_user_notification_levels.select do |id, level|
       level == CategoryUser.notification_levels[lookup_level]
@@ -30,7 +34,7 @@ class BasicUserSerializer < ApplicationSerializer
   end
 
   def category_user_notification_levels
-    @category_user_notification_levels ||= CategoryUser.notification_levels_for(scope)
+    @category_user_notification_levels ||= CategoryUser.notification_levels_for(user)
   end
 
   def tags_with_notification_level(lookup_level)
@@ -40,6 +44,6 @@ class BasicUserSerializer < ApplicationSerializer
   end
 
   def tag_user_notification_levels
-    @tag_user_notification_levels ||= TagUser.notification_levels_for(scope)
+    @tag_user_notification_levels ||= TagUser.notification_levels_for(user)
   end
 end

app/serializers/current_user_serializer.rb
@@ -225,7 +225,7 @@ class CurrentUserSerializer < BasicUserSerializer
   end
 
   def tracked_tags
-    tags_with_notification_level(:tracked)
+    tags_with_notification_level(:tracking)
   end
 
   def watching_first_post_tags

app/serializers/user_serializer.rb
@@ -89,15 +89,15 @@ class UserSerializer < UserCardSerializer
   end
 
   def include_group_users?
-    (object.id && object.id == scope.user.try(:id)) || scope.is_admin?
+    user_is_current_user || scope.is_admin?
   end
 
   def include_associated_accounts?
-    (object.id && object.id == scope.user.try(:id))
+    user_is_current_user
   end
 
   def include_second_factor_enabled?
-    (object&.id == scope.user&.id) || scope.is_admin?
+    user_is_current_user || scope.is_admin?
   end
 
   def second_factor_enabled
@@ -105,7 +105,7 @@ class UserSerializer < UserCardSerializer
   end
 
   def include_second_factor_backup_enabled?
-    object&.id == scope.user&.id
+    user_is_current_user
   end
 
   def second_factor_backup_enabled
@@ -113,7 +113,7 @@ class UserSerializer < UserCardSerializer
   end
 
   def include_second_factor_remaining_backup_codes?
-    (object&.id == scope.user&.id) && object.backup_codes_enabled?
+    user_is_current_user && object.backup_codes_enabled?
   end
 
   def second_factor_remaining_backup_codes
@@ -211,7 +211,7 @@ class UserSerializer < UserCardSerializer
   end
 
   def tracked_tags
-    tags_with_notification_level(:tracked)
+    tags_with_notification_level(:tracking)
   end
 
   def watching_first_post_tags

spec/models/category_user_spec.rb
@@ -222,7 +222,6 @@ describe CategoryUser do
   end
 
   describe "#notification_levels_for" do
-    let(:guardian) { Guardian.new(user) }
     let!(:category1) { Fabricate(:category) }
     let!(:category2) { Fabricate(:category) }
     let!(:category3) { Fabricate(:category) }
@@ -239,7 +238,7 @@ describe CategoryUser do
         SiteSetting.default_categories_muted = category5.id.to_s
       end
       it "every category from the default_categories_* site settings get overridden to regular, except for muted" do
-        levels = CategoryUser.notification_levels_for(guardian)
+        levels = CategoryUser.notification_levels_for(user)
         expect(levels[category1.id]).to eq(CategoryUser.notification_levels[:regular])
         expect(levels[category2.id]).to eq(CategoryUser.notification_levels[:regular])
         expect(levels[category3.id]).to eq(CategoryUser.notification_levels[:regular])
@@ -259,7 +258,7 @@ describe CategoryUser do
       it "gets the category_user notification levels for all categories the user is tracking and does not
       include categories the user is not tracking at all" do
         category6 = Fabricate(:category)
-        levels = CategoryUser.notification_levels_for(guardian)
+        levels = CategoryUser.notification_levels_for(user)
         expect(levels[category1.id]).to eq(CategoryUser.notification_levels[:watching])
         expect(levels[category2.id]).to eq(CategoryUser.notification_levels[:tracking])
         expect(levels[category3.id]).to eq(CategoryUser.notification_levels[:watching_first_post])

spec/models/group_user_spec.rb
@@ -57,7 +57,7 @@ describe GroupUser do
       group.watching_first_post_category_ids = [category5.id]
       group.save!
       expect { group.add(user) }.to change { CategoryUser.count }.by(5)
-      h = CategoryUser.notification_levels_for(Guardian.new(user))
+      h = CategoryUser.notification_levels_for(user)
       expect(h[category1.id]).to eq(levels[:muted])
       expect(h[category2.id]).to eq(levels[:regular])
       expect(h[category3.id]).to eq(levels[:tracking])
@@ -74,7 +74,7 @@ describe GroupUser do
       group.watching_first_post_category_ids = [category2.id, category3.id, category4.id]
       group.save!
       group.add(user)
-      h = CategoryUser.notification_levels_for(Guardian.new(user))
+      h = CategoryUser.notification_levels_for(user)
       expect(h[category1.id]).to eq(levels[:regular])
       expect(h[category2.id]).to eq(levels[:watching_first_post])
       expect(h[category3.id]).to eq(levels[:watching_first_post])
@@ -89,7 +89,7 @@ describe GroupUser do
       group.tracking_category_ids = [category4.id]
       group.save!
       group.add(user)
-      h = CategoryUser.notification_levels_for(Guardian.new(user))
+      h = CategoryUser.notification_levels_for(user)
       expect(h[category1.id]).to eq(levels[:tracking])
       expect(h[category2.id]).to eq(levels[:watching])
       expect(h[category3.id]).to eq(levels[:muted])

spec/models/tag_user_spec.rb
@@ -234,7 +234,6 @@ describe TagUser do
   end
 
   describe "#notification_levels_for" do
-    let(:guardian) { Guardian.new(user) }
     let!(:tag1) { Fabricate(:tag) }
     let!(:tag2) { Fabricate(:tag) }
     let!(:tag3) { Fabricate(:tag) }
@@ -249,7 +248,7 @@ describe TagUser do
         SiteSetting.default_tags_muted = tag4.name
       end
       it "every tag from the default_tags_* site settings get overridden to watching_first_post, except for muted" do
-        levels = TagUser.notification_levels_for(guardian)
+        levels = TagUser.notification_levels_for(user)
         expect(levels[tag1.name]).to eq(TagUser.notification_levels[:regular])
         expect(levels[tag2.name]).to eq(TagUser.notification_levels[:regular])
         expect(levels[tag3.name]).to eq(TagUser.notification_levels[:regular])
@@ -268,7 +267,7 @@ describe TagUser do
       it "gets the tag_user notification levels for all tags the user is tracking and does not
       include tags the user is not tracking at all" do
         tag5 = Fabricate(:tag)
-        levels = TagUser.notification_levels_for(guardian)
+        levels = TagUser.notification_levels_for(user)
         expect(levels[tag1.name]).to eq(TagUser.notification_levels[:watching])
         expect(levels[tag2.name]).to eq(TagUser.notification_levels[:tracking])
         expect(levels[tag3.name]).to eq(TagUser.notification_levels[:watching_first_post])

spec/serializers/user_serializer_spec.rb
@@ -60,12 +60,57 @@ describe UserSerializer do
   end
 
   context "with a user" do
+    let(:admin_user) { Fabricate(:admin) }
     let(:scope) { Guardian.new }
     fab!(:user) { Fabricate(:user) }
     let(:serializer) { UserSerializer.new(user, scope: scope, root: false) }
     let(:json) { serializer.as_json }
     fab!(:upload) { Fabricate(:upload) }
     fab!(:upload2) { Fabricate(:upload) }
+
+    context "when the scope user is admin" do
+      let(:scope) { Guardian.new(admin_user) }
+
+      it "returns the user's category notification levels, not the scope user's" do
+        category1 = Fabricate(:category)
+        category2 = Fabricate(:category)
+        category3 = Fabricate(:category)
+        category4 = Fabricate(:category)
+        CategoryUser.create(category: category1, user: user, notification_level: CategoryUser.notification_levels[:muted])
+        CategoryUser.create(category: Fabricate(:category), user: admin_user, notification_level: CategoryUser.notification_levels[:muted])
+        CategoryUser.create(category: category2, user: user, notification_level: CategoryUser.notification_levels[:tracking])
+        CategoryUser.create(category: Fabricate(:category), user: admin_user, notification_level: CategoryUser.notification_levels[:tracking])
+        CategoryUser.create(category: category3, user: user, notification_level: CategoryUser.notification_levels[:watching])
+        CategoryUser.create(category: Fabricate(:category), user: admin_user, notification_level: CategoryUser.notification_levels[:watching])
+        CategoryUser.create(category: category4, user: user, notification_level: CategoryUser.notification_levels[:regular])
+        CategoryUser.create(category: Fabricate(:category), user: admin_user, notification_level: CategoryUser.notification_levels[:regular])
+
+        expect(json[:muted_category_ids]).to eq([category1.id])
+        expect(json[:tracked_category_ids]).to eq([category2.id])
+        expect(json[:watched_category_ids]).to eq([category3.id])
+        expect(json[:regular_category_ids]).to eq([category4.id])
+      end
+
+      it "returns the user's tag notification levels, not the scope user's" do
+        tag1 = Fabricate(:tag)
+        tag2 = Fabricate(:tag)
+        tag3 = Fabricate(:tag)
+        tag4 = Fabricate(:tag)
+        TagUser.create(tag: tag1, user: user, notification_level: TagUser.notification_levels[:muted])
+        TagUser.create(tag: Fabricate(:tag), user: admin_user, notification_level: TagUser.notification_levels[:muted])
+        TagUser.create(tag: tag2, user: user, notification_level: TagUser.notification_levels[:tracking])
+        TagUser.create(tag: Fabricate(:tag), user: admin_user, notification_level: TagUser.notification_levels[:tracking])
+        TagUser.create(tag: tag3, user: user, notification_level: TagUser.notification_levels[:watching])
+        TagUser.create(tag: Fabricate(:tag), user: admin_user, notification_level: TagUser.notification_levels[:watching])
+        TagUser.create(tag: tag4, user: user, notification_level: TagUser.notification_levels[:watching_first_post])
+        TagUser.create(tag: Fabricate(:tag), user: admin_user, notification_level: TagUser.notification_levels[:watching_first_post])
+
+        expect(json[:muted_tags]).to eq([tag1.name])
+        expect(json[:tracked_tags]).to eq([tag2.name])
+        expect(json[:watched_tags]).to eq([tag3.name])
+        expect(json[:watching_first_post_tags]).to eq([tag4.name])
+      end
+    end
 
     context "with `enable_names` true" do
       before do