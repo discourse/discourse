8e235ed3dfc6492438cdb2998785136d2e1c7e43
Jarek Radosz <jradosz@gmail.com>
2021-05-19 17:08:47 +0200

    Remove poor unit tests

--- spec/lib/onebox/layout_spec.rb
+++ spec/lib/onebox/layout_spec.rb
@@ -5,47 +5,8 @@ require "onebox_helper"
 
 describe Onebox::Layout do
   let(:record) { {} }
-  let(:onebox) { described_class.new("amazon", record) }
-  let(:html) { onebox.to_html }
-
-  describe ".template_path" do
-    let(:template_path) { onebox.template_path }
-
-    before do
-      Onebox.options.load_paths << "directory_a"
-      Onebox.options.load_paths << "directory_b"
-    end
-
-    after do
-      Onebox.options.load_paths.pop(2)
-    end
-
-    context "when template exists in directory_b" do
-      before do
-        allow_any_instance_of(described_class).to receive(:template?) { |_, path| path == "directory_b" }
-      end
-
-      it "returns directory_b" do
-        expect(template_path).to eq("directory_b")
-      end
-    end
-
-    context "when template exists in directory_a" do
-      before do
-        allow_any_instance_of(described_class).to receive(:template?) { |_, path| path == "directory_a" }
-      end
-
-      it "returns directory_a" do
-        expect(template_path).to eq("directory_a")
-      end
-    end
-
-    context "when template doesn't exist in directory_a or directory_b" do
-      it "returns default path" do
-        expect(template_path).to include("template")
-      end
-    end
-  end
+  let(:layout) { described_class.new("amazon", record) }
+  let(:html) { layout.to_html }
 
   describe "#to_html" do
     it "contains layout template" do
