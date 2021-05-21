f700f3ef009d7a149cddb706b78c807b1469d2a9
Bianca Nenciu <nbianca@users.noreply.github.com>
2021-05-21 17:50:24 +0300

    FEATURE: Support tag and replace in watched words in test modal (#13100)
    The modal showed only the matches, without the replacement or tags.

--- app/serializers/watched_word_serializer.rb
+++ app/serializers/watched_word_serializer.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class WatchedWordSerializer < ApplicationSerializer
-  attributes :id, :word, :replacement, :action
+  attributes :id, :word, :regexp, :replacement, :action
+
+  def regexp
+    WordWatcher.word_to_regexp(word)
+  end
+
+  def include_regexp?
+    WatchedWord.has_replacement?(action)
+  end
 
   def action
     WatchedWord.actions[object.action]
