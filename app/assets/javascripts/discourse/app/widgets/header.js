96fd202d9cdac98196abd952c53f583b4af9d8f2
Robin Ward <robin.ward@gmail.com>
2021-05-26 17:30:27 -0400

    FIX: We can't use `?.` yet (#13168)

--- app/assets/javascripts/discourse/app/widgets/header.js
+++ app/assets/javascripts/discourse/app/widgets/header.js
@@ -508,9 +508,12 @@ export default createWidget("header", {
     this.toggleBodyScrolling(this.state.userVisible);
 
     // auto focus on first button in dropdown
-    schedule("afterRender", () =>
-      document.querySelector(".user-menu button")?.focus()
-    );
+    schedule("afterRender", () => {
+      let elem = document.querySelector(".user-menu button");
+      if (elem) {
+        elem.focus();
+      }
+    });
   },
 
   toggleHamburger() {
@@ -519,7 +522,10 @@ export default createWidget("header", {
 
     // auto focus on first link in dropdown
     schedule("afterRender", () => {
-      document.querySelector(".hamburger-panel .menu-links a")?.focus();
+      let elem = document.querySelector(".hamburger-panel .menu-links a");
+      if (elem) {
+        elem.focus();
+      }
     });
   },
 
