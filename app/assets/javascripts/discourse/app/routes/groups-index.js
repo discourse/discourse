fa0f95f50fe9ad9879f44295dbe1eb72314938dc
Robin Ward <robin.ward@gmail.com>
2021-05-27 13:17:13 -0400

    DEV: Add support for class properties in babel
    This allows us to start using JS classes instead of Ember's classes.

--- app/assets/javascripts/discourse/app/routes/groups-index.js
+++ app/assets/javascripts/discourse/app/routes/groups-index.js
@@ -1,24 +1,24 @@
 import DiscourseRoute from "discourse/routes/discourse";
 import I18n from "I18n";
 
-export default DiscourseRoute.extend({
+export default class GroupsIndexRoute extends DiscourseRoute {
   titleToken() {
     return I18n.t("groups.index.title");
-  },
+  }
 
-  queryParams: {
+  queryParams = {
     order: { refreshModel: true, replace: true },
     asc: { refreshModel: true, replace: true },
     filter: { refreshModel: true },
     type: { refreshModel: true, replace: true },
     username: { refreshModel: true },
-  },
+  };
 
   model(params) {
     return params;
-  },
+  }
 
   setupController(controller, params) {
     controller.loadGroups(params);
-  },
-});
+  }
+}
