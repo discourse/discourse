e940aefe6471eb14eac2147da08044e737a281aa
Dan Ungureanu <dan@ungureanu.me>
2021-05-28 10:58:52 +0300

    FIX: Use date of first migration to check for new sites

--- db/migrate/20210527114834_set_tagging_enabled.rb
+++ db/migrate/20210527114834_set_tagging_enabled.rb
@@ -2,10 +2,15 @@
 
 class SetTaggingEnabled < ActiveRecord::Migration[6.1]
   def up
-    result = execute('SELECT COUNT(*) FROM topics')
+    result = execute <<~SQL
+      SELECT created_at
+      FROM schema_migration_details
+      ORDER BY created_at
+      LIMIT 1
+    SQL
 
     # keep tagging disabled for existent sites
-    if result.first['count'] > 0
+    if result.first['created_at'].to_datetime < 1.hour.ago
       execute <<~SQL
         INSERT INTO site_settings(name, data_type, value, created_at, updated_at)
         VALUES('tagging_enabled', 5, 'f', NOW(), NOW())
