bd8e890f9ef0f6b516e5532c8b3382bff4751e06
Gerhard Schlager <gerhard.schlager@discourse.org>
2021-06-01 11:06:12 +0200

    FIX: Don't store translated trust level names in anonymous cache
    Refactors `TrustLevel` and moves translations from server to client
    
    Additional changes:
      * "staff" and "admin" wasn't translatable in site settings
      * it replaces a concatenated string with a translation
      * uses translation for trust levels in users_by_trust_level report
      * adds a DB migration to rename keys of translation overrides affected by this commit

--- app/assets/javascripts/discourse/tests/fixtures/site-fixtures.js
+++ app/assets/javascripts/discourse/tests/fixtures/site-fixtures.js
@@ -12,6 +12,13 @@ export default {
         small_action: 3,
         whisper: 4,
       },
+      trust_levels: {
+        newuser: 0,
+        basic: 1,
+        member: 2,
+        regular: 3,
+        leader: 4,
+      },
       groups: [
         { id: 0, name: "everyone" },
         { id: 1, name: "admins" },
@@ -533,28 +540,6 @@ export default {
           icon: null,
           id: 7,
           is_custom_flag: true,
-        },
-      ],
-      trust_levels: [
-        {
-          id: 0,
-          name: "new user",
-        },
-        {
-          id: 1,
-          name: "basic user",
-        },
-        {
-          id: 2,
-          name: "member",
-        },
-        {
-          id: 3,
-          name: "regular",
-        },
-        {
-          id: 4,
-          name: "leader",
         },
       ],
       archetypes: [
