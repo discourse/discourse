0d22dea7642366f8e7efe1ab30b2518036cc65f4
Mark VanLandingham <markvanlan@gmail.com>
2021-06-02 14:27:12 -0500

    pass args with keys to helpers

--- app/assets/javascripts/discourse/app/helpers/directory-item-label.js
+++ app/assets/javascripts/discourse/app/helpers/directory-item-label.js
@@ -2,8 +2,9 @@ import { htmlSafe } from "@ember/template";
 import { registerUnbound } from "discourse-common/lib/helpers";
 import I18n from "I18n";
 
-export default registerUnbound("directory-item-label", function (item, column) {
-  const count = item.get(column.name);
+export default registerUnbound("mobile-directory-item-label", function (args) {
+  // Args should include key/values { item, column }
 
-  return htmlSafe(I18n.t(`directory.${column.name}`, { count }));
+  const count = args.item.get(args.column.name);
+  return htmlSafe(I18n.t(`directory.${args.column.name}`, { count }));
 });
--- app/assets/javascripts/discourse/app/helpers/directory-item-value.js
+++ app/assets/javascripts/discourse/app/helpers/directory-item-value.js
@@ -2,8 +2,10 @@ import { htmlSafe } from "@ember/template";
 import { registerUnbound } from "discourse-common/lib/helpers";
 import { number } from "discourse/lib/formatter";
 
-export default registerUnbound("directory-item-value", function (item, column) {
+export default registerUnbound("directory-item-value", function (args) {
+  // Args should include key/values { item, column }
+
   return htmlSafe(
-    `<span class='number'>${number(item.get(column.name))}</span>`
+    `<span class='number'>${number(args.item.get(args.column.name))}</span>`
   );
 });
