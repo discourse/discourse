85b7bcfc32b443bf5cf771aad4e342a196b3b66f
Mark VanLandingham <markvanlan@gmail.com>
2021-06-02 12:24:19 -0500

    Fix tests?

--- app/assets/javascripts/discourse/app/helpers/directory-item-user-field-value.js
+++ app/assets/javascripts/discourse/app/helpers/directory-item-user-field-value.js
@@ -4,9 +4,10 @@ import { registerUnbound } from "discourse-common/lib/helpers";
 export default registerUnbound(
   "directory-item-user-field-value",
   function (item, column) {
-    const value = item.user
-      ? item.user.user_fields[column.user_field_id]
-      : null;
+    const value =
+      item.user && item.user.user_fields
+        ? item.user.user_fields[column.user_field_id]
+        : null;
     const content = value || "-";
     return htmlSafe(`<span class='user-field-value'>${content}</span>`);
   }
