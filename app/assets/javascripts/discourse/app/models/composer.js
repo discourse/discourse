c5cf9a3ea2420672499dae3a0e99bc98b2ab056a
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-06-02 12:45:41 +0300

    DEV: Add tests

--- app/assets/javascripts/discourse/app/models/composer.js
+++ app/assets/javascripts/discourse/app/models/composer.js
@@ -946,7 +946,9 @@ const Composer = RestModel.extend({
     this.set("composeState", SAVING);
 
     const rollback = throwAjaxError((error) => {
-      post.set("cooked", oldCooked);
+      post.setProperties({ cooked: oldCooked, staged: false });
+      this.appEvents.trigger("post-stream:refresh", { id: post.id });
+
       this.set("composeState", OPEN);
       if (error.jqXHR && error.jqXHR.status === 409) {
         this.set("editConflict", true);
--- app/assets/javascripts/discourse/tests/acceptance/composer-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/composer-test.js
@@ -365,6 +365,22 @@ acceptance("Composer", function (needs) {
     );
   });
 
+  test("Editing a post can rollback to old content", async function (assert) {
+    await visit("/t/internationalization-localization/280");
+    await click(".topic-post:nth-of-type(1) button.show-more-actions");
+    await click(".topic-post:nth-of-type(1) button.edit");
+
+    await fillIn(".d-editor-input", "this will 409");
+    await fillIn("#reply-title", "This is the new text for the title");
+    await click("#reply-control button.create");
+
+    assert.equal(find(".topic-post.staged").length, 0);
+    assert.equal(
+      find(".topic-post .cooked")[0].innerText,
+      "Any plans to support localization of UI elements, so that I (for example) could set up a completely German speaking forum?"
+    );
+  });
+
   test("Composer can switch between edits", async function (assert) {
     await visit("/t/this-is-a-test-topic/9");
 
