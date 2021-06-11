5fe62f1ad3a7145d8330ebea109e2bd9ba375d21
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2021-06-11 16:09:07 +0800

    PERF: Remove extra PG query.
    In `Theme.list_baked_fields`, common is always included as a target.

--- lib/stylesheet/manager.rb
+++ lib/stylesheet/manager.rb
@@ -395,8 +395,7 @@ class Stylesheet::Manager
 
   def theme_digest
     if [:mobile_theme, :desktop_theme].include?(@target)
-      scss_digest = theme.resolve_baked_field(:common, :scss)
-      scss_digest += theme.resolve_baked_field(@target.to_s.sub("_theme", ""), :scss)
+      scss_digest = theme.resolve_baked_field(@target.to_s.sub("_theme", ""), :scss)
     elsif @target == :embedded_theme
       scss_digest = theme.resolve_baked_field(:common, :embedded_scss)
     else
