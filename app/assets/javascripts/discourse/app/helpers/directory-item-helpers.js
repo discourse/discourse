689e40cfa973f02dc857fe88c70d1500366bbfe1
Mark VanLandingham <markvanlan@gmail.com>
2021-06-16 09:37:12 -0500

    prettier

--- app/assets/javascripts/discourse/app/helpers/directory-item-helpers.js
+++ app/assets/javascripts/discourse/app/helpers/directory-item-helpers.js
@@ -16,18 +16,15 @@ registerUnbound("directory-item-value", function (args) {
   );
 });
 
-registerUnbound(
-  "directory-item-user-field-value",
-  function (args) {
-    // Args should include key/values { item, column }
-    const value =
-      args.item.user && args.item.user.user_fields
-        ? args.item.user.user_fields[args.column.user_field_id]
-        : null;
-    const content = value || "-";
-    return htmlSafe(`<span class='user-field-value'>${content}</span>`);
-  }
-);
+registerUnbound("directory-item-user-field-value", function (args) {
+  // Args should include key/values { item, column }
+  const value =
+    args.item.user && args.item.user.user_fields
+      ? args.item.user.user_fields[args.column.user_field_id]
+      : null;
+  const content = value || "-";
+  return htmlSafe(`<span class='user-field-value'>${content}</span>`);
+});
 
 registerUnbound("directory-column-is-automatic", function (args) {
   // Args should include key/values { column }
