9bf0a4d8b5d529650b3c824a1d3abc29b9f3c855
jjaffeux <j.jaffeux@gmail.com>
2021-06-24 10:16:47 +0200

    disable_ddl_transaction! for concurrently adding index

--- db/migrate/20210624080131_add_partial_index_pinned_until.rb
+++ db/migrate/20210624080131_add_partial_index_pinned_until.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AddPartialIndexPinnedUntil < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
   def change
     add_index :topics, :pinned_until,
       where: 'pinned_until IS NOT NULL',
