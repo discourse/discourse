fa1c8188dab8bb9f575ba18938f2c8f3214e904a
Dan Ungureanu <dan@ungureanu.me>
2021-07-01 18:42:56 +0300

    DEV: Prefer .pluck_first over .pluck.first

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -406,7 +406,7 @@ describe ApplicationHelper do
     end
 
     it 'returns two color scheme link tags when dark mode is enabled' do
-      SiteSetting.default_dark_mode_color_scheme_id = ColorScheme.where(name: "Dark").pluck(:id).first
+      SiteSetting.default_dark_mode_color_scheme_id = ColorScheme.where(name: "Dark").pluck_first(:id)
       cs_stylesheets = helper.discourse_color_scheme_stylesheets
 
       expect(cs_stylesheets).to include("(prefers-color-scheme: dark)")
@@ -462,7 +462,7 @@ describe ApplicationHelper do
         helper.request.env[Auth::DefaultCurrentUserProvider::CURRENT_USER_KEY] = user
         @new_cs = Fabricate(:color_scheme, name: 'Custom Color Scheme')
 
-        SiteSetting.default_dark_mode_color_scheme_id = ColorScheme.where(name: "Dark").pluck(:id).first
+        SiteSetting.default_dark_mode_color_scheme_id = ColorScheme.where(name: "Dark").pluck_first(:id)
       end
 
       it "returns no dark scheme stylesheet when user has disabled that option" do
