646d74de21557207fc9bf2becfa0430f85883341
Martin Brennan <martin@discourse.org>
2021-07-05 15:27:44 +1000

    Add job_retry sidekiq comment

--- app/jobs/concerns/smtp_retryable.rb
+++ app/jobs/concerns/smtp_retryable.rb
@@ -8,6 +8,8 @@ module SmtpRetryable
     # or use default sidekiq retry formula. returning
     # nil/0 will trigger the default sidekiq
     # retry formula
+    #
+    # See https://github.com/mperham/sidekiq/blob/3330df0ee37cfd3e0cd3ef01e3e66b584b99d488/lib/sidekiq/job_retry.rb#L216-L234
     case exception.wrapped
     when Net::SMTPServerBusy
       return 1.hour + (rand(30) * (count + 1))
