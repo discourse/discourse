discourse/discourse

commit aa1fc0130754d54696f9fa44b953be9b5f1c763d

FEATURE - Moderators can create and manage groups (#10432)
Enabling the moderators_manage_categories_and_groups site setting will allow moderator users to create/manage groups.

* show New Group form to moderators

* Allow moderators to update groups and read logs, where appropriate

* Rename site setting from create -> manage

* improved tests

* Migration should rename old log entries

* Log group changes, even if those changes mean you can no longer see the group

* Slight reshuffle

* RouteTo /g if they no longer have permissions to view group

app/assets/javascripts/discourse/app/routes/groups-new.js
@@ -18,7 +18,7 @@ export default DiscourseRoute.extend({
   },
 
   afterModel() {
-    if (!(this.currentUser && this.currentUser.admin)) {
+    if (!this.get("currentUser.can_create_group")) {
       this.transitionTo("groups");
     }
   }

app/assets/javascripts/discourse/app/components/group-manage-save-button.js
@@ -3,6 +3,7 @@ import discourseComputed from "discourse-common/utils/decorators";
 import Component from "@ember/component";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import { popupAutomaticMembershipAlert } from "discourse/controllers/groups-new";
+import DiscourseURL from "discourse/lib/url";
 
 export default Component.extend({
   saving: null,
@@ -24,7 +25,11 @@ export default Component.extend({
 
       return group
         .save()
-        .then(() => {
+        .then(data => {
+          if (data.route_to) {
+            DiscourseURL.routeTo(data.route_to);
+          }
+
           this.set("saved", true);
         })
         .catch(popupAjaxError)

app/assets/javascripts/discourse/app/components/groups-form-interaction-fields.js
@@ -70,5 +70,14 @@ export default Component.extend({
   )
   showEmailSettings(emailIn, automatic, isAdmin) {
     return emailIn && isAdmin && !automatic;
+  },
+
+  @discourseComputed(
+    "model.isCreated",
+    "model.can_admin_group",
+    "currentUser.can_create_group"
+  )
+  canAdminGroup(isCreated, canAdmin, canCreate) {
+    return (!isCreated && canCreate) || (isCreated && canAdmin);
   }
 });

app/assets/javascripts/discourse/app/controllers/group.js
@@ -128,7 +128,7 @@ export default Controller.extend({
     return (
       this.currentUser &&
       (this.currentUser.canManageGroup(model) ||
-        (this.currentUser.admin && automatic))
+        (model.can_admin_group && automatic))
     );
   },
 

app/assets/javascripts/discourse/app/models/user.js
@@ -833,7 +833,7 @@ const User = RestModel.extend({
   canManageGroup(group) {
     return group.get("automatic")
       ? false
-      : this.admin || group.get("is_group_owner");
+      : group.get("can_admin_group") || group.get("is_group_owner");
   },
 
   @discourseComputed("groups.@each.title", "badges.[]")

app/assets/javascripts/discourse/app/routes/group-manage.js
@@ -15,7 +15,7 @@ export default DiscourseRoute.extend({
   afterModel(group) {
     if (
       !this.currentUser ||
-      (!(this.currentUser.admin && group.get("automatic")) &&
+      (!(this.modelFor("group").can_admin_group && group.get("automatic")) &&
         !this.currentUser.canManageGroup(group))
     ) {
       this.transitionTo("group.members", group);

app/assets/javascripts/discourse/app/templates/components/groups-form-interaction-fields.hbs
@@ -1,4 +1,4 @@
-{{#if currentUser.admin}}
+{{#if canAdminGroup}}
   <div class="control-group">
     <label class="control-label">{{i18n "admin.groups.manage.interaction.visibility"}}</label>
     <label for="visiblity">{{i18n "admin.groups.manage.interaction.visibility_levels.title"}}</label>
@@ -62,7 +62,7 @@
   }}
 </div>
 
-{{#if currentUser.admin}}
+{{#if canAdminGroup}}
   <div class="control-group">
     <label>
       {{input type="checkbox"

app/assets/javascripts/discourse/app/templates/components/groups-form-membership-fields.hbs
@@ -1,4 +1,4 @@
-{{#if currentUser.admin}}
+{{#if model.can_admin_group}}
   <div class="control-group">
     <label class="control-label">{{i18n "admin.groups.manage.membership.automatic"}}</label>
 

commit e831576d0a1cb814ee7d75ff6027a01a54691d5d

FIX: can_admin_group should be true when creating a new group.
It looks like this regressed in #10432.

A user can create a group if they're an admin or if they're a mod and the "moderators_manage_categories_and_groups" setting is enabled, so it's safe to always set "can_admin_group" to true for new groups.

It will let us configure automatic membership, default title, and effects on create.

app/assets/javascripts/discourse/app/routes/groups-new.js
@@ -10,7 +10,11 @@ export default DiscourseRoute.extend({
   },
 
   model() {
-    return Group.create({ automatic: false, visibility_level: 0 });
+    return Group.create({
+      automatic: false,
+      visibility_level: 0,
+      can_admin_group: true,
+    });
   },
 
   setupController(controller, model) {