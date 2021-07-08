c6f2459cc46c705e0ac26188c4b49565d3c1b908
Martin Brennan <mjrbrennan@gmail.com>
2021-07-08 12:49:58 +1000

    FIX: Do not prevent other topic timers running on error (#13665)
    There was an issue with the TopicTimerEnqueuer where any timer
    that failed to enqueue_typed_job with an error would prevent
    all other pending timers after the one that errored from running.
    
    To mitigate this we just capture the error and log it (so we can
    still fix it if needed for bug crushing) and proceed with the
    rest of the timer enqueues.
    
    The commit https://github.com/discourse/discourse/pull/13544 highlighted
    this issue originally in hosted sites.
    
    <!-- NOTE: All pull requests should have tests (rspec in Ruby, qunit in JavaScript). If your code does not include test coverage, please include an explanation of why it was omitted. -->

--- app/jobs/scheduled/topic_timer_enqueuer.rb
+++ app/jobs/scheduled/topic_timer_enqueuer.rb
@@ -12,13 +12,15 @@ module Jobs
     every 1.minute
 
     def execute(_args = nil)
-      timers = TopicTimer.pending_timers
-
-      timers.find_each do |timer|
+      TopicTimer.pending_timers.find_each do |timer|
 
         # the typed job may not enqueue if it has already
         # been scheduled with enqueue_at
-        timer.enqueue_typed_job
+        begin
+          timer.enqueue_typed_job
+        rescue => err
+          Discourse.warn_exception(err, message: "Error when attempting to enqueue topic timer job for timer #{timer.id}")
+        end
       end
     end
   end
