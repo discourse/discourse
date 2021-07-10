discourse/discourse

commit 87c1e98571631d83e0d9f0846bd95a2c7e9bce87

FEATURE: Let users select flair (#13587)
User flair was given by user's primary group. This PR separates the
two, adds a new field to the user model for flair group ID and users
can select their flair from user preferences now.

lib/user_lookup.rb
@@ -12,13 +12,33 @@ class UserLookup
   end
 
   def primary_groups
-    @groups ||= group_lookup_hash
+    @primary_groups ||= begin
+      hash = {}
+      users.values.each do |u|
+        if u.primary_group_id
+          hash[u.id] = groups[u.primary_group_id]
+        end
+      end
+      hash
+    end
+  end
+
+  def flair_groups
+    @flair_groups ||= begin
+      hash = {}
+      users.values.each do |u|
+        if u.flair_group_id
+          hash[u.id] = groups[u.flair_group_id]
+        end
+      end
+      hash
+    end
   end
 
   private
 
   def self.lookup_columns
-    @user_lookup_columns ||= %i{id username name uploaded_avatar_id primary_group_id admin moderator trust_level}
+    @user_lookup_columns ||= %i{id username name uploaded_avatar_id primary_group_id flair_group_id admin moderator trust_level}
   end
 
   def self.group_lookup_columns
@@ -37,23 +57,21 @@ class UserLookup
     hash
   end
 
-  def group_lookup_hash
-    users_with_primary_group = users.values.reject { |u| u.primary_group_id.nil? }
+  def groups
+    @group_lookup = begin
+      group_ids = users.values.map { |u| [u.primary_group_id, u.flair_group_id] }
+      group_ids.flatten!
+      group_ids.uniq!
+      group_ids.compact!
 
-    group_lookup = {}
-    group_ids = users_with_primary_group.map { |u| u.primary_group_id }
-    group_ids.uniq!
+      hash = {}
 
-    Group.includes(:flair_upload)
-      .where(id: group_ids)
-      .select(self.class.group_lookup_columns)
-      .each { |g| group_lookup[g.id] = g }
+      Group.includes(:flair_upload)
+        .where(id: group_ids)
+        .select(self.class.group_lookup_columns)
+        .each { |g| hash[g.id] = g }
 
-    hash = {}
-    users_with_primary_group.each do |u|
-      hash[u.id] = group_lookup[u.primary_group_id]
+      hash
     end
-    hash
   end
-
 end

commit b330f511997e37d5c5e54e837a47f0a72e924eec

FIX: A memoization bug in UserLookup and refactor
`@group_lookup` memoization bug was introduced in #13587.

lib/user_lookup.rb
@@ -1,42 +1,6 @@
 # frozen_string_literal: true
 
 class UserLookup
-
-  def initialize(user_ids = [])
-    @user_ids = user_ids.tap(&:compact!).tap(&:uniq!).tap(&:flatten!)
-  end
-
-  # Lookup a user by id
-  def [](user_id)
-    users[user_id]
-  end
-
-  def primary_groups
-    @primary_groups ||= begin
-      hash = {}
-      users.values.each do |u|
-        if u.primary_group_id
-          hash[u.id] = groups[u.primary_group_id]
-        end
-      end
-      hash
-    end
-  end
-
-  def flair_groups
-    @flair_groups ||= begin
-      hash = {}
-      users.values.each do |u|
-        if u.flair_group_id
-          hash[u.id] = groups[u.flair_group_id]
-        end
-      end
-      hash
-    end
-  end
-
-  private
-
   def self.lookup_columns
     @user_lookup_columns ||= %i{id username name uploaded_avatar_id primary_group_id flair_group_id admin moderator trust_level}
   end
@@ -45,33 +9,56 @@ class UserLookup
     @group_lookup_columns ||= %i{id name flair_icon flair_upload_id flair_bg_color flair_color}
   end
 
-  def users
-    @users ||= user_lookup_hash
+  def initialize(user_ids = [])
+    @user_ids = user_ids.compact.uniq.flatten
   end
 
-  def user_lookup_hash
-    hash = {}
-    User.where(id: @user_ids)
+  # Lookup a user by id
+  def [](user_id)
+    users[user_id]
+  end
+
+  def primary_groups
+    @primary_groups ||= users
+      .values
+      .filter(&:primary_group_id)
+      .each_with_object({}) do |user, hash|
+        hash[user.id] = groups[user.primary_group_id]
+      end
+  end
+
+  def flair_groups
+    @flair_groups ||= users
+      .values
+      .filter(&:flair_group_id)
+      .each_with_object({}) do |user, hash|
+        hash[user.id] = groups[user.flair_group_id]
+      end
+  end
+
+  private
+
+  def users
+    @users ||= User
+      .where(id: @user_ids)
       .select(self.class.lookup_columns)
-      .each { |user| hash[user.id] = user }
-    hash
+      .index_by(&:id)
   end
 
   def groups
-    @group_lookup = begin
-      group_ids = users.values.map { |u| [u.primary_group_id, u.flair_group_id] }
-      group_ids.flatten!
-      group_ids.uniq!
-      group_ids.compact!
+    @group_lookup ||= begin
+      group_ids = users
+        .values
+        .map { |u| [u.primary_group_id, u.flair_group_id] }
+        .flatten
+        .uniq
+        .compact
 
-      hash = {}
-
-      Group.includes(:flair_upload)
+      Group
+        .includes(:flair_upload)
         .where(id: group_ids)
         .select(self.class.group_lookup_columns)
-        .each { |g| hash[g.id] = g }
-
-      hash
+        .index_by(&:id)
     end
   end
 end