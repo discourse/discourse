47af1c1cdeda525ddc84568d87b44cba2f609707
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-07-13 18:44:51 +0300

    FIX: Set flair_group_id only if group has flair
    Follow up to 4ba93aac667f8e50ffb63635bb4e42ff34dcd58e.

--- db/migrate/20210713092503_set_users_flair_group_id.rb
+++ db/migrate/20210713092503_set_users_flair_group_id.rb
@@ -5,7 +5,10 @@ class SetUsersFlairGroupId < ActiveRecord::Migration[6.1]
     execute <<~SQL
       UPDATE users
       SET flair_group_id = primary_group_id
-      WHERE flair_group_id IS NULL
+      FROM groups
+      WHERE users.primary_group_id = groups.id AND
+            users.flair_group_id IS NULL AND
+            (groups.flair_icon IS NOT NULL OR groups.flair_upload_id IS NOT NULL)
     SQL
   end
 end
