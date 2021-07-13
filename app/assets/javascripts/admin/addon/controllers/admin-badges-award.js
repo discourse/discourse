bb187a0f89d391a3feab6e061f0aa87b7bc81a52
OsamaSayegh <asooomaasoooma90@gmail.com>
2021-07-13 23:45:36 +0300

    don't use bootbox

--- app/assets/javascripts/admin/addon/controllers/admin-badges-award.js
+++ app/assets/javascripts/admin/addon/controllers/admin-badges-award.js
@@ -2,65 +2,91 @@ import Controller from "@ember/controller";
 import I18n from "I18n";
 import { ajax } from "discourse/lib/ajax";
 import bootbox from "bootbox";
-import { popupAjaxError } from "discourse/lib/ajax-error";
-import { escapeExpression } from "discourse/lib/utilities";
+import { extractError } from "discourse/lib/ajax-error";
+import { action } from "@ember/object";
+import discourseComputed from "discourse-common/utils/decorators";
 
 export default Controller.extend({
   saving: false,
   replaceBadgeOwners: false,
   grantExistingHolders: false,
+  fileSelected: false,
+  unmatchedEntries: null,
+  resultsMessage: null,
+  success: false,
 
-  actions: {
-    massAward() {
-      const file = document.querySelector("#massAwardCSVUpload").files[0];
+  resetState() {
+    this.setProperties({
+      saving: false,
+      unmatchedEntries: null,
+      resultsMessage: null,
+      success: false,
+    });
+    this.updateFileSelected();
+  },
 
-      if (this.model && file) {
-        const options = {
-          type: "POST",
-          processData: false,
-          contentType: false,
-          data: new FormData(),
-        };
+  updateFileSelected() {
+    this.set(
+      "fileSelected",
+      !!document.querySelector("#massAwardCSVUpload")?.files?.length
+    );
+  },
 
-        options.data.append("file", file);
-        options.data.append("replace_badge_owners", this.replaceBadgeOwners);
-        options.data.append(
-          "grant_existing_holders",
-          this.grantExistingHolders
-        );
+  @discourseComputed("fileSelected", "saving")
+  massAwardButtonDisabled(fileSelected, saving) {
+    return !fileSelected || saving;
+  },
 
-        this.set("saving", true);
+  @action
+  onFileInputChange() {
+    this.updateFileSelected();
+  },
 
-        ajax(`/admin/badges/award/${this.model.id}`, options)
-          .then(
-            ({
-              matched_users_count: matchedCount,
-              unmatched_entries: unmatchedEntries,
-            }) => {
-              if (unmatchedEntries.length) {
-                const entriesToList = unmatchedEntries
-                  .map((entry) => `<li>${escapeExpression(entry)}</li>`)
-                  .join("");
-                bootbox.alert(
-                  I18n.t(
-                    "admin.badges.mass_award.success_with_unmatched_entries",
-                    { count: matchedCount, users: `<ul>${entriesToList}</ul>` }
-                  )
-                );
-              } else {
-                bootbox.alert(
-                  I18n.t("admin.badges.mass_award.success", {
-                    count: matchedCount,
-                  })
-                );
-              }
+  @action
+  massAward() {
+    const file = document.querySelector("#massAwardCSVUpload").files[0];
+
+    if (this.model && file) {
+      const options = {
+        type: "POST",
+        processData: false,
+        contentType: false,
+        data: new FormData(),
+      };
+
+      options.data.append("file", file);
+      options.data.append("replace_badge_owners", this.replaceBadgeOwners);
+      options.data.append("grant_existing_holders", this.grantExistingHolders);
+
+      this.resetState();
+      this.set("saving", true);
+
+      ajax(`/admin/badges/award/${this.model.id}`, options)
+        .then(
+          ({
+            matched_users_count: matchedCount,
+            unmatched_entries: unmatchedEntries,
+          }) => {
+            this.setProperties({
+              resultsMessage: I18n.t("admin.badges.mass_award.success", {
+                count: matchedCount,
+              }),
+              success: true,
+            });
+            if (unmatchedEntries.length) {
+              this.set("unmatchedEntries", unmatchedEntries);
             }
-          )
-          .catch(popupAjaxError)
-          .finally(() => this.set("saving", false));
-      } else {
-        bootbox.alert(I18n.t("admin.badges.mass_award.aborted"));
-      }
-    },
+          }
+        )
+        .catch((error) => {
+          this.setProperties({
+            resultsMessage: extractError(error),
+            success: false,
+          });
+        })
+        .finally(() => this.set("saving", false));
+    } else {
+      bootbox.alert(I18n.t("admin.badges.mass_award.aborted"));
+    }
   },
 });
