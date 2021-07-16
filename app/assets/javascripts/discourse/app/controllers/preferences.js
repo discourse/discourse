9b95ea8ab1b9e469e0fbeca058b30077ea382a07
Neil Lalonde <neillalonde@gmail.com>
2021-07-16 14:17:17 -0400

    Refactor to use (and in template

--- app/assets/javascripts/discourse/app/controllers/preferences.js
+++ app/assets/javascripts/discourse/app/controllers/preferences.js
@@ -1,12 +1,3 @@
-import { and, readOnly } from "@ember/object/computed";
 import Controller from "@ember/controller";
 
-export default Controller.extend({
-  canChangeCategoryPreferences: readOnly(
-    "model.can_change_tracking_preferences"
-  ),
-  canChangeTagPreferences: and(
-    "model.can_change_tracking_preferences",
-    "siteSettings.tagging_enabled"
-  ),
-});
+export default Controller.extend({});
