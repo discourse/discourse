46dc189850c1a55e177a342f048b58405f1271c2
David Taylor <david@taylorhq.com>
2021-08-05 17:36:34 +0100

    DEV: Improve robustness of associate_accounts_controller
    This handles a few edge cases which are extremely rare (due to the UI layout), but still technically possible:
    
    - Ensure users are authenticated before attempting association.
    
    - Add a message and logic for when a user already has an association for a given auth provider.

--- app/controllers/users/associate_accounts_controller.rb
+++ app/controllers/users/associate_accounts_controller.rb
@@ -3,41 +3,51 @@
 class Users::AssociateAccountsController < ApplicationController
   SECURE_SESSION_PREFIX ||= "omniauth_reconnect"
 
+  before_action :ensure_logged_in
+
   def connect_info
-    auth = get_auth_hash
-
-    provider_name = auth.provider
-    authenticator = Discourse.enabled_authenticators.find { |a| a.name == provider_name }
-    raise Discourse::InvalidAccess.new(I18n.t('authenticator_not_found')) if authenticator.nil?
-
-    account_description = authenticator.description_for_auth_hash(auth)
-
-    render json: { token: params[:token], provider_name: provider_name, account_description: account_description }
+    account_description = authenticator.description_for_auth_hash(auth_hash)
+    existing_account_description = authenticator.description_for_user(current_user).presence
+    render json: {
+      token: params[:token],
+      provider_name: auth_hash.provider,
+      account_description: account_description,
+      existing_account_description: existing_account_description
+    }
   end
 
   def connect
-    auth = get_auth_hash
-    secure_session[self.class.key(params[:token])] = nil
+    if authenticator.description_for_user(current_user).present? && authenticator.can_revoke?
+      authenticator.revoke(current_user)
+    end
 
-    provider_name = auth.provider
-    authenticator = Discourse.enabled_authenticators.find { |a| a.name == provider_name }
-    raise Discourse::InvalidAccess.new(I18n.t('authenticator_not_found')) if authenticator.nil?
-
-    DiscourseEvent.trigger(:before_auth, authenticator, auth, session, cookies, request)
+    DiscourseEvent.trigger(:before_auth, authenticator, auth_hash, session, cookies, request)
     auth_result = authenticator.after_authenticate(auth, existing_account: current_user)
     DiscourseEvent.trigger(:after_auth, authenticator, auth_result, session, cookies, request)
+
+    secure_session[self.class.key(params[:token])] = nil
 
     render json: success_json
   end
 
   private
 
-  def get_auth_hash
-    token = params[:token]
-    json = secure_session[self.class.key(token)]
-    raise Discourse::NotFound if json.nil?
+  def auth_hash
+    @auth_hash ||= begin
+      token = params[:token]
+      json = secure_session[self.class.key(token)]
+      raise Discourse::NotFound if json.nil?
 
-    OmniAuth::AuthHash.new(JSON.parse(json))
+      OmniAuth::AuthHash.new(JSON.parse(json))
+    end
+  end
+
+  def authenticator
+    provider_name = auth_hash.provider
+    authenticator = Discourse.enabled_authenticators.find { |a| a.name == provider_name }
+    raise Discourse::InvalidAccess.new(I18n.t('authenticator_not_found')) if authenticator.nil?
+    raise Discourse::InvalidAccess.new(I18n.t('authenticator_no_connect')) if !authenticator.can_connect_existing_user?
+    authenticator
   end
 
   def self.key(token)
