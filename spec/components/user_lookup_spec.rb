eb4befbb4ba45c178d019e2e0f5da274f1d1004d
Arpit Jalan <arpit@techapj.com>
2021-08-10 13:36:19 +0530

    REVERT "FIX: do not show private group flair on user avatars"
    This reverts commit fe3e18f9814d94cf5ca19891262b9376861ce3d0 and 0d8fd9ace60ad676af0cfb58d65191821a77e8a9

--- spec/components/user_lookup_spec.rb
+++ spec/components/user_lookup_spec.rb
@@ -53,33 +53,4 @@ describe UserLookup do
       expect(user_lookup_group.name).to eq("testgroup")
     end
   end
-
-  describe '#flair_groups' do
-    fab!(:group) { Fabricate(:group, name: "flair_group", flair_icon: "icon", flair_bg_color: "40E0D0", visibility_level: Group.visibility_levels[:public], members_visibility_level: Group.visibility_levels[:public]) }
-    fab!(:user2) { Fabricate(:user, flair_group: group) }
-
-    before do
-      @user_lookup = UserLookup.new([user.id, user2.id, nil])
-    end
-
-    it 'returns nil if user_id does not exists' do
-      expect(@user_lookup.flair_groups[0]).to eq(nil)
-    end
-
-    it 'returns nil if user_id is nil' do
-      expect(@user_lookup.flair_groups[nil]).to eq(nil)
-    end
-
-    it 'returns nil if user has no flair group' do
-      expect(@user_lookup.flair_groups[user.id]).to eq(nil)
-    end
-
-    it 'returns group if user has flair group' do
-      user_lookup_group = @user_lookup.flair_groups[user2.id]
-      expect(user_lookup_group).to eq(group)
-      expect(user_lookup_group.name).to eq("flair_group")
-      expect(user_lookup_group.flair_url).to eq("icon")
-      expect(user_lookup_group.flair_bg_color).to eq("40E0D0")
-    end
-  end
 end
