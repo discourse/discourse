657b7905742b42d90842cfdc6f2ad034b8757d6b
jjaffeux <j.jaffeux@gmail.com>
2021-08-18 11:04:57 +0200

    120s

--- app/assets/javascripts/discourse/testem.js
+++ app/assets/javascripts/discourse/testem.js
@@ -1,7 +1,7 @@
 module.exports = {
   test_page: "tests/index.html?hidepassed",
   disable_watching: true,
-  browser_start_timeout: 60,
+  browser_start_timeout: 120,
   launch_in_ci: ["Chrome"],
   launch_in_dev: ["Chrome"],
   browser_args: {
