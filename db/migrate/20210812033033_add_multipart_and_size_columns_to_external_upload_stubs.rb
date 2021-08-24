cabbf570557d8b3115311f826dc33b2f4ab060cc
Martin Brennan <martin@discourse.org>
2021-08-24 13:38:01 +1000

    DEV: Fix migration index and annotate ExternalUploadStub model

--- db/migrate/20210812033033_add_multipart_and_size_columns_to_external_upload_stubs.rb
+++ db/migrate/20210812033033_add_multipart_and_size_columns_to_external_upload_stubs.rb
@@ -3,8 +3,10 @@
 class AddMultipartAndSizeColumnsToExternalUploadStubs < ActiveRecord::Migration[6.1]
   def up
     add_column :external_upload_stubs, :multipart, :boolean, default: false, null: false
-    add_column :external_upload_stubs, :external_upload_identifier, :string, null: true, index: true
+    add_column :external_upload_stubs, :external_upload_identifier, :string, null: true
     add_column :external_upload_stubs, :filesize, :integer
+
+    add_index :external_upload_stubs, :external_upload_identifier
 
     # this feature is not actively used yet so this will be safe, also the rows in this
     # table are regularly deleted
