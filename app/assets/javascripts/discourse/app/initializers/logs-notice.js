cfbf69848a4247b43d9a9ed7b0437058bb9f8cb7
Robin Ward <robin.ward@gmail.com>
2021-08-25 17:03:07 -0400

    Revert "FIX: The `LogsNotice` service was never unsubscribing from the mbus"
    This reverts commit 14b76dece6126cd0a04a6390d39517a383c72bf9.

--- app/assets/javascripts/discourse/app/initializers/logs-notice.js
+++ app/assets/javascripts/discourse/app/initializers/logs-notice.js
@@ -5,7 +5,7 @@ export default {
   name: "logs-notice",
   after: "message-bus",
 
-  initialize(container) {
+  initialize: function (container) {
     const siteSettings = container.lookup("site-settings:main");
     const messageBus = container.lookup("message-bus:main");
     const keyValueStore = container.lookup("key-value-store:main");
@@ -20,9 +20,5 @@ export default {
         });
       },
     });
-  },
-
-  teardown() {
-    LogsNotice.current().destroy();
   },
 };
