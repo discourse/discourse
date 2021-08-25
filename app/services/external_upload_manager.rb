95ec030d4b41b4c85ea75d62b362f9ab8c045e86
Martin Brennan <martin@discourse.org>
2021-08-25 15:33:33 +1000

    DEV: Do not delete failed uploads from S3 in debug mode
    See also 58e9fffe4cd0156284257c64306a6af0133367e0

--- app/services/external_upload_manager.rb
+++ app/services/external_upload_manager.rb
@@ -84,12 +84,11 @@ class ExternalUploadManager
       external_upload_stub.created_by_id
     )
   rescue
-    # We don't need to do anything special to abort multipart uploads here,
-    # because at this point (calling promote_to_upload!), the multipart
-    # upload would already be complete.
-    Discourse.store.delete_file(external_upload_stub.key)
-
     if !SiteSetting.enable_upload_debug_mode
+      # We don't need to do anything special to abort multipart uploads here,
+      # because at this point (calling promote_to_upload!), the multipart
+      # upload would already be complete.
+      Discourse.store.delete_file(external_upload_stub.key)
       external_upload_stub.destroy!
     else
       external_upload_stub.update(status: ExternalUploadStub.statuses[:failed])
