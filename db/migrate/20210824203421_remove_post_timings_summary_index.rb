d11f19f09928c2f26d215c357c3f76330323075b
Daniel Waterworth <me@danielwaterworth.com>
2021-08-26 10:50:34 -0500

    PERF: Remove redundant post_timings_summary index (#14164)
    It's redundant since post_timings_unique exists which has a superset of
    the columns with the same prefix.

---
+++ db/migrate/20210824203421_remove_post_timings_summary_index.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class RemovePostTimingsSummaryIndex < ActiveRecord::Migration[6.1]
+  def change
+    remove_index :post_timings, column: [:topic_id, :post_number], name: :post_timings_summary, if_exists: true
+  end
+end
