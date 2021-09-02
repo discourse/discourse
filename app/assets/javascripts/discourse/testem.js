b1c7b544fe1f47418c95660ac0a3310ab9bbdda2
Rafael dos Santos Silva <xfalcox@gmail.com>
2021-09-02 15:21:56 -0300

    Run tests in Firefox ESR

--- app/assets/javascripts/discourse/testem.js
+++ app/assets/javascripts/discourse/testem.js
@@ -1,8 +1,9 @@
 module.exports = {
   test_page: "tests/index.html?hidepassed",
   disable_watching: true,
-  launch_in_ci: ["Chrome"],
+  launch_in_ci: ["Chrome", "Firefox"],
   launch_in_dev: ["Chrome"],
+  parallel: -1, // run Firefox and Chrome in parallel
   browser_args: {
     Chrome: [
       // --no-sandbox is needed when running Chrome inside a container
@@ -14,5 +15,6 @@ module.exports = {
       "--remote-debugging-port=4201",
       "--window-size=1440,900",
     ].filter(Boolean),
+    Firefox: ["-headless", "--width=1440", "--height=900"],
   },
 };
