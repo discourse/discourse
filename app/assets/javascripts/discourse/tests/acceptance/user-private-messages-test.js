94341cf7571d5adc06a5241abc79cb6e7947b255
Rafael dos Santos Silva <xfalcox@gmail.com>
2021-09-09 13:25:20 -0300

    DEV: Work around HTMLElement.innerText differences between browsers

--- app/assets/javascripts/discourse/tests/acceptance/user-private-messages-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/user-private-messages-test.js
@@ -633,9 +633,12 @@ acceptance(
     test("suggested messages for group messages without new or unread", async function (assert) {
       await visit("/t/13");
 
-      assert.equal(
-        query(".suggested-topics-message").innerText.trim(),
-        "Want to read more? Browse other messages in  awesome_group.",
+      assert.ok(
+        query(".suggested-topics-message")
+          .innerText.trim()
+          .match(
+            /Want to read more\? Browse other messages in\s+awesome_group\./
+          ),
         "displays the right browse more message"
       );
     });
@@ -647,9 +650,12 @@ acceptance(
 
       await visit("/t/13"); // await re-render
 
-      assert.equal(
-        query(".suggested-topics-message").innerText.trim(),
-        "There is 1 new message remaining, or browse other messages in  awesome_group",
+      assert.ok(
+        query(".suggested-topics-message")
+          .innerText.trim()
+          .match(
+            /There is 1 new message remaining, or browse other messages in\s+awesome_group/
+          ),
         "displays the right browse more message"
       );
 
@@ -657,9 +663,12 @@ acceptance(
 
       await visit("/t/13"); // await re-render
 
-      assert.equal(
-        query(".suggested-topics-message").innerText.trim(),
-        "There is 1 unread and 1 new message remaining, or browse other messages in  awesome_group",
+      assert.ok(
+        query(".suggested-topics-message")
+          .innerText.trim()
+          .match(
+            /There is 1 unread and 1 new message remaining, or browse other messages in\s+awesome_group/
+          ),
         "displays the right browse more message"
       );
     });
