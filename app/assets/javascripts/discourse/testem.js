789613fe51733f108f97a926e22f409421e18fbc
Rafael dos Santos Silva <xfalcox@gmail.com>
2021-09-09 16:32:56 -0300

    DEV: Run Ember CLI tests in Firefox evergreen too (#14283)
    Increases timeout and removes parallelism to make it stable.

--- app/assets/javascripts/discourse/testem.js
+++ app/assets/javascripts/discourse/testem.js
@@ -1,9 +1,9 @@
 module.exports = {
   test_page: "tests/index.html?hidepassed",
   disable_watching: true,
-  launch_in_ci: ["Chrome", "Firefox"],
+  launch_in_ci: ["Chrome", "Firefox", "Headless Firefox"], // Firefox is old ESR version, Headless Firefox is up-to-date evergreen version
   launch_in_dev: ["Chrome"],
-  parallel: -1, // run Firefox and Chrome in parallel
+  parallel: 1, // disable parallel tests for stability
   browser_args: {
     Chrome: [
       // --no-sandbox is needed when running Chrome inside a container
@@ -16,5 +16,9 @@ module.exports = {
       "--window-size=1440,900",
     ].filter(Boolean),
     Firefox: ["-headless", "--width=1440", "--height=900"],
+    "Headless Firefox": ["--width=1440", "--height=900"],
+  },
+  browser_paths: {
+    "Headless Firefox": "/opt/firefox-evergreen/firefox",
   },
 };
