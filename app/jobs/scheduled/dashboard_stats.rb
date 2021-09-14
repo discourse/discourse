d99735e24de739e48720e2eed05ae3f8a3f5983f
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2021-09-15 08:59:25 +1000

    FEATURE: remove duplicated messages about new advices (#14319)
    Discourse is sending regularly message to admins when potential problems are persisted. Most of the time they have exactly the same content. In that case, when there are no replies, the old one should be trashed before a new one is created.

--- app/jobs/scheduled/dashboard_stats.rb
+++ app/jobs/scheduled/dashboard_stats.rb
@@ -5,12 +5,22 @@ module Jobs
     every 30.minutes
 
     def execute(args)
-      problems_started_at = AdminDashboardData.problems_started_at
-      if problems_started_at && problems_started_at < 2.days.ago
+      if persistent_problems?
         # If there have been problems reported on the dashboard for a while,
         # send a message to admins no more often than once per week.
-        GroupMessage.create(Group[:admins].name, :dashboard_problems, limit_once_per: 7.days.to_i)
+        group_message = GroupMessage.new(Group[:admins].name, :dashboard_problems, limit_once_per: 7.days.to_i)
+        Topic.transaction do
+          group_message.delete_previous!
+          group_message.create
+        end
       end
+    end
+
+    private
+
+    def persistent_problems?
+      problems_started_at = AdminDashboardData.problems_started_at
+      problems_started_at && problems_started_at < 2.days.ago
     end
   end
 end
