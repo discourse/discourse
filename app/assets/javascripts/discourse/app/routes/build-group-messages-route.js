804274af47bb01e5bf91f3e1ca5a0550495740e1
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-09-16 22:24:27 +0400

    FEATURE: improve blank page syndrome on the activity/topics, activity/read and group messages pages (#14313)

--- app/assets/javascripts/discourse/app/routes/build-group-messages-route.js
+++ app/assets/javascripts/discourse/app/routes/build-group-messages-route.js
@@ -14,7 +14,14 @@ export default (type) => {
       if (this._isArchive()) {
         filter = `${filter}/archive`;
       }
-      return this.store.findFiltered("topicList", { filter });
+      return this.store.findFiltered("topicList", { filter }).then((model) => {
+        // andrei: we agreed that this is an anti pattern,
+        // it's better to avoid mutating a rest model like this
+        // this place we'll be refactored later
+        // see https://github.com/discourse/discourse/pull/14313#discussion_r708784704
+        model.set("emptyState", this.emptyState());
+        return model;
+      });
     },
 
     setupController() {
@@ -38,6 +45,13 @@ export default (type) => {
         id: currentUser.get("username_lower"),
         user: currentUser,
       });
+    },
+
+    emptyState() {
+      return {
+        title: I18n.t("no_group_messages_title"),
+        body: "",
+      };
     },
 
     _isArchive() {
