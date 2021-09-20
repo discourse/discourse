02f7035cbef2a20d3e1728d4f7657d664202d126
Martin Brennan <mjrbrennan@gmail.com>
2021-09-21 08:41:07 +1000

    DEV: Improve uppy plugin base and large file handling (#14395)
    We want to be able to skip plugins from doing any work under
    certain conditions, and to be able raise their own errors if
    a file being uploaded is completely incompatible with the concept
    of the plugin if it is enabled. For example, the UppyChecksum plugin
    is happy to skip hashing large files, but the UppyUploadEncrypt
    plugin from discourse-encrypt relies on the file being encrypted
    to do anything with the upload, so it is considered a blocking
    error if the user uploads a file that is too large.
    
    This improves the base functions available in uppy-plugin-base and
    extendable-uploader to handle this, as well as introducing a
    HUGE_FILE_THRESHOLD_BYTES variable which represents 100MB in bytes,
    matching the ExternalUploadManager::DOWNLOAD_LIMIT on the
    server side.
    
    discourse-encrypt to take advantage of this new functionality will
    follow in discourse/discourse-encrypt#141

--- app/assets/javascripts/discourse/app/lib/uppy-checksum-plugin.js
+++ app/assets/javascripts/discourse/app/lib/uppy-checksum-plugin.js
@@ -1,5 +1,6 @@
 import { UploadPreProcessorPlugin } from "discourse/lib/uppy-plugin-base";
 import { Promise } from "rsvp";
+import { HUGE_FILE_THRESHOLD_BYTES } from "discourse/mixins/uppy-upload";
 
 export default class UppyChecksum extends UploadPreProcessorPlugin {
   static pluginId = "uppy-checksum";
@@ -34,13 +35,19 @@ export default class UppyChecksum extends UploadPreProcessorPlugin {
 
   _generateChecksum(fileIds) {
     if (!this._canUseSubtleCrypto()) {
-      return Promise.resolve();
+      return this._skipAll(fileIds, true);
     }
 
     let promises = fileIds.map((fileId) => {
       let file = this._getFile(fileId);
-
       this._emitProgress(file);
+
+      if (file.size > HUGE_FILE_THRESHOLD_BYTES) {
+        this._consoleWarn(
+          "The file provided is too large to checksum, skipping."
+        );
+        return this._skip(file);
+      }
 
       return file.data.arrayBuffer().then((arrayBuffer) => {
         return window.crypto.subtle
