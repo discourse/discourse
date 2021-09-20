8f93156661d7eab8702b3eac38fc1d5de982a092
Vinoth Kannan <svkn.87@gmail.com>
2021-09-20 11:08:51 +0530

    minor code refactoring

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -538,8 +538,7 @@ describe Category do
     it 'is deleted correctly' do
       @category.destroy
       expect(Category.exists?(id: @category_id)).to be false
-      expect(Topic.exists?(id: @topic_id)).to be false
-      expect(Topic.with_deleted.exists?(id: @topic_id)).to be true
+      expect(Topic.with_deleted.where.not(deleted_at: nil).exists?(id: @topic_id)).to be true
       expect(SiteSetting.shared_drafts_category).to be_blank
     end
 
