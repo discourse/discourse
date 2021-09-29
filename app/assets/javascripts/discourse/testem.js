2d4b9595e8771eeebd8dd2299a3473481781bd10
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2021-09-29 13:25:34 +0200

    DEV: uses standard browser_start_timeout (#14472)

--- app/assets/javascripts/discourse/testem.js
+++ app/assets/javascripts/discourse/testem.js
@@ -29,6 +29,7 @@ module.exports = {
   launch_in_dev: ["Chrome"],
   tap_failed_tests_only: true,
   parallel: 1, // disable parallel tests for stability
+  browser_start_timeout: 120,
   browser_args: {
     Chrome: [
       // --no-sandbox is needed when running Chrome inside a container
