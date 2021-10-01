afb683182a4ca4a62c56412dfc74a0cb6c2357f7
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-10-01 19:35:09 +0200

    Handle the case when the fallback username contains disallowed characters

--- lib/user_name_suggester.rb
+++ lib/user_name_suggester.rb
@@ -2,6 +2,7 @@
 
 module UserNameSuggester
   GENERIC_NAMES = ['i', 'me', 'info', 'support', 'admin', 'webmaster', 'hello', 'mail', 'office', 'contact', 'team']
+  LAST_RESORT_USERNAME = "user"
 
   def self.suggest(name_or_email, allowed_username = nil)
     return unless name_or_email.present?
@@ -103,7 +104,11 @@ module UserNameSuggester
 
   def self.fix_username(name)
     fixed_username = sanitize_username(name)
-    fixed_username << I18n.t('fallback_username') if fixed_username.empty?
+    if fixed_username.empty?
+      fixed_username << sanitize_username(I18n.t('fallback_username'))
+      fixed_username << LAST_RESORT_USERNAME if fixed_username.empty?
+    end
+
     rightsize_username(fixed_username)
   end
 
