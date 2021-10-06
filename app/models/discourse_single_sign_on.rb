5276a490e608cdbaab91f347d4738a9f5c3e0ec9
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-10-06 21:17:05 +0200

    Don't use email as a source for username suggestions

--- app/models/discourse_single_sign_on.rb
+++ app/models/discourse_single_sign_on.rb
@@ -243,7 +243,7 @@ class DiscourseSingleSignOn < SingleSignOn
         user_params = {
           primary_email: UserEmail.new(email: email, primary: true),
           name: try_name || User.suggest_name(try_username || email),
-          username: UserNameSuggester.suggest(try_username || try_name || email),
+          username: UserNameSuggester.suggest(try_username || try_name),
           ip_address: ip_address
         }
 
