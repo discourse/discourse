74706bab101176913be1f84f85a8b33c2463950c
Penar Musaraj <pmusaraj@gmail.com>
2021-10-13 12:09:28 -0400

    UX: Add `Enter` hint to search dropdown (#14587)

--- app/assets/javascripts/discourse/tests/acceptance/search-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/search-test.js
@@ -53,7 +53,7 @@ acceptance("Search - Anonymous", function (needs) {
 
     assert.equal(
       query(
-        ".search-menu .results ul.search-menu-initial-options li:first-child"
+        ".search-menu .results ul.search-menu-initial-options li:first-child .search-item-slug"
       ).innerText.trim(),
       `dev ${I18n.t("search.in_topics_posts")}`,
       "shows topic search as first dropdown item"
