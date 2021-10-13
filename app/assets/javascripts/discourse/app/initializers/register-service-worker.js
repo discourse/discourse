44155e056afb6cb3b8fb64b92d107a7c1042c722
Mark VanLandingham <markvanlan@gmail.com>
2021-10-13 12:34:08 -0500

    DEV: Export register service worker logic

--- app/assets/javascripts/discourse/app/initializers/register-service-worker.js
+++ app/assets/javascripts/discourse/app/initializers/register-service-worker.js
@@ -1,50 +1,53 @@
 import getAbsoluteURL, { isAbsoluteURL } from "discourse-common/lib/get-url";
 
+export function registerServiceWorker(container, serviceWorkerURL) {
+  const isSecured = document.location.protocol === "https:";
+
+  if (isSecured && "serviceWorker" in navigator) {
+    const caps = container.lookup("capabilities:main");
+    const isAppleBrowser =
+      caps.isSafari ||
+      (caps.isIOS && !window.matchMedia("(display-mode: standalone)").matches);
+
+    if (serviceWorkerURL && !isAppleBrowser) {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (let registration of registrations) {
+          if (
+            registration.active &&
+            !registration.active.scriptURL.includes(serviceWorkerURL)
+          ) {
+            unregister(registration);
+          }
+        }
+      });
+
+      navigator.serviceWorker
+        .register(getAbsoluteURL(`/${serviceWorkerURL}`))
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.info(`Failed to register Service Worker: ${error}`);
+        });
+    } else {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (let registration of registrations) {
+          unregister(registration);
+        }
+      });
+    }
+  }
+}
+
+function unregister(registration) {
+  if (isAbsoluteURL(registration.scope)) {
+    registration.unregister();
+  }
+}
+
 export default {
   name: "register-service-worker",
 
   initialize(container) {
-    const isSecured = document.location.protocol === "https:";
-
-    if (isSecured && "serviceWorker" in navigator) {
-      let { serviceWorkerURL } = container.lookup("session:main");
-      const caps = container.lookup("capabilities:main");
-      const isAppleBrowser =
-        caps.isSafari ||
-        (caps.isIOS &&
-          !window.matchMedia("(display-mode: standalone)").matches);
-
-      if (serviceWorkerURL && !isAppleBrowser) {
-        navigator.serviceWorker.getRegistrations().then((registrations) => {
-          for (let registration of registrations) {
-            if (
-              registration.active &&
-              !registration.active.scriptURL.includes(serviceWorkerURL)
-            ) {
-              this.unregister(registration);
-            }
-          }
-        });
-
-        navigator.serviceWorker
-          .register(getAbsoluteURL(`/${serviceWorkerURL}`))
-          .catch((error) => {
-            // eslint-disable-next-line no-console
-            console.info(`Failed to register Service Worker: ${error}`);
-          });
-      } else {
-        navigator.serviceWorker.getRegistrations().then((registrations) => {
-          for (let registration of registrations) {
-            this.unregister(registration);
-          }
-        });
-      }
-    }
-  },
-
-  unregister(registration) {
-    if (isAbsoluteURL(registration.scope)) {
-      registration.unregister();
-    }
+    let { serviceWorkerURL } = container.lookup("session:main");
+    registerServiceWorker(container, serviceWorkerURL);
   },
 };
