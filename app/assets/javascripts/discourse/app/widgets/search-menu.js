87ffeee0ae775212ccbd668091af6a79e03f578c
Penar Musaraj <pmusaraj@gmail.com>
2021-10-14 14:51:34 -0400

    Code review refactor

--- app/assets/javascripts/discourse/app/widgets/search-menu.js
+++ app/assets/javascripts/discourse/app/widgets/search-menu.js
@@ -15,7 +15,7 @@ import { CANCELLED_STATUS } from "discourse/lib/autocomplete";
 const CATEGORY_SLUG_REGEXP = /(\#[a-zA-Z0-9\-:]*)$/gi;
 const USERNAME_REGEXP = /(\@[a-zA-Z0-9\-\_]*)$/gi;
 const SUGGESTIONS_REGEXP = /(in:|status:|order:|:)([a-zA-Z]*)$/gi;
-export const TOPIC_REPLACE_REGEXP = /topic:\d+\s?/i;
+export const TOPIC_REPLACE_REGEXP = /\btopic:\d+\s?/i;
 export const MODIFIER_REGEXP = /.*(\#|\@|:).*$/gi;
 export const DEFAULT_TYPE_FILTER = "exclude_topics";
 
