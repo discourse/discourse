d1b46dddf54ff3251484e76ba8cee5ecfd81102c
Rafael dos Santos Silva <xfalcox@gmail.com>
2021-10-14 23:41:08 -0300

    hashrocket

--- config/initializers/004-message_bus.rb
+++ config/initializers/004-message_bus.rb
@@ -31,7 +31,7 @@ def setup_message_bus_env(env)
       "Access-Control-Allow-Origin" => Discourse.base_url_no_prefix,
       "Access-Control-Allow-Methods" => "GET, POST",
       "Access-Control-Allow-Headers" => "X-SILENCE-LOGGER, X-Shared-Session-Key, Dont-Chunk, Discourse-Present",
-      "Access-Control-Max-Age" = "7200",
+      "Access-Control-Max-Age" => "7200",
     }
 
     user = nil
