e945dc51b822d59f20de472551e672509b0674f6
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-10-18 19:03:43 +0200

    Add a comment

--- app/services/username_overrider.rb
+++ app/services/username_overrider.rb
@@ -3,6 +3,7 @@
 class UsernameOverrider
   def self.override(user, new_username)
     if user.username_equals_to?(new_username)
+      # override anyway since case could've been changed:
       UsernameChanger.change(user, new_username, user)
       true
     elsif user.username != UserNameSuggester.fix_username(new_username)
