0e7a2ffddb8b3d0d576c57a4aa278cd6be1b9f15
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-10-26 18:45:24 +0300

    DEV: Apply code suggestions

--- app/jobs/onceoff/migrate_normalized_emails.rb
+++ app/jobs/onceoff/migrate_normalized_emails.rb
@@ -3,7 +3,7 @@
 module Jobs
   class MigrateNormalizedEmails < ::Jobs::Onceoff
     def execute_onceoff(args)
-      UserEmail.find_each do |user_email|
+      ::UserEmail.find_each do |user_email|
         user_email.update(normalized_email: user_email.normalize_email)
       end
     end
