b659e94a8eb444094839df633a1a189423e42eb3
Martin Brennan <mjrbrennan@gmail.com>
2021-10-28 07:53:21 +1000

    DEV: Delete vacate_legacy_prefix_backups code (#14735)
    Introduced in 3037617327147602d84e584c56a7d69dd001da0a, we no
    longer need this code, as all of the backups have been
    migrated.

--- lib/backup_restore/s3_backup_store.rb
+++ lib/backup_restore/s3_backup_store.rb
@@ -51,22 +51,6 @@ module BackupRestore
       raise StorageError.new(e.message.presence || e.class.name)
     end
 
-    def vacate_legacy_prefix
-      legacy_s3_helper = S3Helper.new(s3_bucket_name_with_legacy_prefix, '', @s3_options.clone)
-      bucket, prefix = s3_bucket_name_with_prefix.split('/', 2)
-      legacy_keys = legacy_s3_helper.list
-        .reject { |o| o.key.starts_with? prefix }
-        .map { |o| o.key }
-      legacy_keys.each do |legacy_key|
-        @s3_helper.s3_client.copy_object({
-          copy_source: File.join(bucket, legacy_key),
-          bucket: bucket,
-          key: File.join(prefix, legacy_key.split('/').last)
-        })
-        legacy_s3_helper.delete_object(legacy_key)
-      end
-    end
-
     private
 
     def unsorted_files
@@ -115,14 +99,6 @@ module BackupRestore
 
     def s3_bucket_name_with_prefix
       File.join(SiteSetting.s3_backup_bucket, RailsMultisite::ConnectionManagement.current_db)
-    end
-
-    def s3_bucket_name_with_legacy_prefix
-      if Rails.configuration.multisite
-        File.join(SiteSetting.s3_backup_bucket, "backups", RailsMultisite::ConnectionManagement.current_db)
-      else
-        SiteSetting.s3_backup_bucket
-      end
     end
 
     def file_regex
