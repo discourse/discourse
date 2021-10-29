discourse/discourse

commit 69f0f48dc084c1ac4d5d441094b15cf01790ac98

DEV: Fix rubocop issues (#14715)

lib/discourse_updates.rb
@@ -61,16 +61,32 @@ module DiscourseUpdates
       Discourse.redis.get last_installed_version_key
     end
 
+    def last_installed_version=(arg)
+      Discourse.redis.set(last_installed_version, arg)
+    end
+
     def latest_version
       Discourse.redis.get latest_version_key
+    end
+
+    def latest_version=(arg)
+      Discourse.redis.set(latest_version, arg)
     end
 
     def missing_versions_count
       Discourse.redis.get(missing_versions_count_key).try(:to_i)
     end
 
+    def missing_versions_count=(arg)
+      Discourse.redis.set(missing_versions_count, arg)
+    end
+
     def critical_updates_available?
       (Discourse.redis.get(critical_updates_available_key) || false) == 'true'
+    end
+
+    def critical_updates_available=(arg)
+      Discourse.redis.set(critical_updates_available, arg)
     end
 
     def updated_at
@@ -80,12 +96,6 @@ module DiscourseUpdates
 
     def updated_at=(time_with_zone)
       Discourse.redis.set updated_at_key, time_with_zone.as_json
-    end
-
-    ['last_installed_version', 'latest_version', 'missing_versions_count', 'critical_updates_available'].each do |name|
-      eval "define_method :#{name}= do |arg|
-        Discourse.redis.set #{name}_key, arg
-      end"
     end
 
     def missing_versions=(versions)

app/models/locale_site_setting.rb
@@ -23,7 +23,7 @@ class LocaleSiteSetting < EnumSiteSetting
 
     @lock.synchronize do
       @language_names ||= begin
-        names = YAML.load(File.read(File.join(Rails.root, 'config', 'locales', 'names.yml')))
+        names = YAML.safe_load(File.read(File.join(Rails.root, 'config', 'locales', 'names.yml')))
 
         DiscoursePluginRegistry.locales.each do |locale, options|
           if !names.key?(locale) && options[:name] && options[:nativeName]

app/models/topic_embed.rb
@@ -117,8 +117,8 @@ class TopicEmbed < ActiveRecord::Base
       follow_canonical: true,
     )
 
-    url = fd.resolve
-    return if url.blank?
+    uri = fd.resolve
+    return if uri.blank?
 
     opts = {
       tags: %w[div p code pre h1 h2 h3 b em i strong a img ul li ol blockquote],
@@ -132,7 +132,7 @@ class TopicEmbed < ActiveRecord::Base
 
     response = FetchResponse.new
     begin
-      html = open(url, allow_redirections: :safe).read
+      html = uri.read(allow_redirections: :safe)
     rescue OpenURI::HTTPError, Net::OpenTimeout
       return
     end
@@ -255,10 +255,6 @@ class TopicEmbed < ActiveRecord::Base
       body << TopicEmbed.imported_from_html(url)
       body
     end
-  end
-
-  def self.open(uri, **kwargs)
-    URI.open(uri, **kwargs)
   end
 end
 

app/services/site_settings_task.rb
@@ -16,7 +16,7 @@ class SiteSettingsTask
     counts = { updated: 0, not_found: 0, errors: 0 }
     log = []
 
-    site_settings = YAML::load(yml)
+    site_settings = YAML::safe_load(yml)
     site_settings.each do |site_setting|
       key = site_setting[0]
       val = site_setting[1]

lib/cache.rb
@@ -86,7 +86,7 @@ class Cache
 
       if raw
         begin
-          Marshal.load(raw)
+          Marshal.load(raw) # rubocop:disable Security/MarshalLoad
         rescue => e
           log_first_exception(e)
         end
@@ -113,7 +113,7 @@ class Cache
 
   def read_entry(key)
     if data = redis.get(key)
-      Marshal.load(data)
+      Marshal.load(data) # rubocop:disable Security/MarshalLoad
     end
   rescue => e
     # corrupt cache, this can happen if Marshal version

lib/i18n/locale_file_checker.rb
@@ -165,7 +165,7 @@ class LocaleFileChecker
 
   def plural_keys
     @plural_keys ||= begin
-      eval(File.read("#{Rails.root}/#{PLURALS_FILE}")).map do |locale, value|
+      eval(File.read("#{Rails.root}/#{PLURALS_FILE}")).map do |locale, value| # rubocop:disable Security/Eval
         [locale.to_s, value[:i18n][:plural][:keys].map(&:to_s)]
       end.to_h
     end

lib/middleware/anonymous_cache.rb
@@ -29,7 +29,7 @@ module Middleware
         method << "|#{k}=#\{h.#{v}}"
       end
       method << "\"\nend"
-      eval(method)
+      eval(method) # rubocop:disable Security/Eval
       @@compiled = true
     end
 

lib/onebox/engine/github_actions_onebox.rb
@@ -71,7 +71,7 @@ module Onebox
           raw["head_commit"]["message"].lines.first
         elsif type == :pr_run
           pr_url = "https://api.github.com/repos/#{match[:org]}/#{match[:repo]}/pulls/#{match[:pr_id]}"
-          ::MultiJson.load(URI.open(pr_url, read_timeout: timeout))["title"]
+          ::MultiJson.load(URI.parse(pr_url).open(read_timeout: timeout))["title"]
         end
 
         {

commit dbbeb133f09c94be33b4b363424897bd52948587

FIX: Regression introduced in #14715

lib/discourse_updates.rb
@@ -78,7 +78,7 @@ module DiscourseUpdates
     end
 
     def missing_versions_count=(arg)
-      Discourse.redis.set(missing_versions_count, arg)
+      Discourse.redis.set(missing_versions_count_key, arg)
     end
 
     def critical_updates_available?
@@ -86,7 +86,7 @@ module DiscourseUpdates
     end
 
     def critical_updates_available=(arg)
-      Discourse.redis.set(critical_updates_available, arg)
+      Discourse.redis.set(critical_updates_available_key, arg)
     end
 
     def updated_at