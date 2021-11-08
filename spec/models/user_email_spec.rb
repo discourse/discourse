31c6fc74e4b3d4e173f317e4be3aed70b218faa8
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-11-08 22:38:26 +0200

    Revert "DEV: Drop normalized_email column"
    This reverts commit 92304f5552095e87e6b528bcedb210579f5ad7c6.

--- spec/models/user_email_spec.rb
+++ spec/models/user_email_spec.rb
@@ -26,6 +26,32 @@ describe UserEmail do
     end
   end
 
+  describe 'normalized_email' do
+    it 'checks if normalized email is unique' do
+      SiteSetting.normalize_emails = true
+
+      user_email = user.user_emails.create(email: "a.b+c@example.com", primary: false)
+      expect(user_email.normalized_email).to eq("ab@example.com")
+      expect(user_email).to be_valid
+
+      user_email = user.user_emails.create(email: "a.b+d@example.com", primary: false)
+      expect(user_email.normalized_email).to eq("ab@example.com")
+      expect(user_email).not_to be_valid
+    end
+
+    it 'does not check uniqueness if email normalization is not enabled' do
+      SiteSetting.normalize_emails = false
+
+      user_email = user.user_emails.create(email: "a.b+c@example.com", primary: false)
+      expect(user_email.normalized_email).to eq("ab@example.com")
+      expect(user_email).to be_valid
+
+      user_email = user.user_emails.create(email: "a.b+d@example.com", primary: false)
+      expect(user_email.normalized_email).to eq("ab@example.com")
+      expect(user_email).to be_valid
+    end
+  end
+
   context "indexes" do
     it "allows only one primary email" do
       expect {
@@ -37,32 +63,6 @@ describe UserEmail do
       Fabricate.build(:secondary_email, user: user, primary: false).save(validate: false)
       Fabricate.build(:secondary_email, user: user, primary: false).save(validate: false)
       expect(user.user_emails.count).to eq 3
-    end
-  end
-
-  context '#strip_downcase_email' do
-    it 'checks if normalized email is unique' do
-      SiteSetting.normalize_emails = true
-
-      user_email = user.user_emails.create(email: "a.b+c@example.com", primary: false)
-      expect(user_email.email).to eq("ab@example.com")
-      expect(user_email).to be_valid
-
-      user_email = user.user_emails.create(email: "a.b+d@example.com", primary: false)
-      expect(user_email.email).to eq("ab@example.com")
-      expect(user_email).not_to be_valid
-    end
-
-    it 'does not check uniqueness if email normalization is not enabled' do
-      SiteSetting.normalize_emails = false
-
-      user_email = user.user_emails.create(email: "a.b+c@example.com", primary: false)
-      expect(user_email.email).to eq("a.b+c@example.com")
-      expect(user_email).to be_valid
-
-      user_email = user.user_emails.create(email: "a.b+d@example.com", primary: false)
-      expect(user_email.email).to eq("a.b+d@example.com")
-      expect(user_email).to be_valid
     end
   end
 end
