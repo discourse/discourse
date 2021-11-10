105a9c54cb09b51ee31b2493863a8b2ceb5e7f95
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-11-10 13:01:41 +0200

    Apply code suggestions

--- spec/requests/admin/api_controller_spec.rb
+++ spec/requests/admin/api_controller_spec.rb
@@ -237,7 +237,7 @@ describe Admin::ApiController do
 
         scopes = response.parsed_body['scopes']
 
-        expect(scopes.keys).to contain_exactly('topics', 'users', 'email', 'posts')
+        expect(scopes.keys).to contain_exactly('topics', 'users', 'email', 'posts', 'global')
       end
     end
   end
