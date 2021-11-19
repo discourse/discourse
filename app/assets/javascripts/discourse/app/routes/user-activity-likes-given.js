571b7c03a927bd72aec0b652704224c3b1eb5d32
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-11-19 13:49:29 +0100

    Revert changes two routes that can cause problems because of actions overriding

--- app/assets/javascripts/discourse/app/routes/user-activity-likes-given.js
+++ app/assets/javascripts/discourse/app/routes/user-activity-likes-given.js
@@ -2,7 +2,6 @@ import UserAction from "discourse/models/user-action";
 import UserActivityStreamRoute from "discourse/routes/user-activity-stream";
 import { iconHTML } from "discourse-common/lib/icon-library";
 import I18n from "I18n";
-import { action } from "@ember/object";
 
 export default UserActivityStreamRoute.extend({
   userActionType: UserAction.TYPES["likes_given"],
@@ -17,9 +16,10 @@ export default UserActivityStreamRoute.extend({
     return { title, body };
   },
 
-  @action
-  didTransition() {
-    this.controllerFor("application").set("showFooter", true);
-    return true;
+  actions: {
+    didTransition() {
+      this.controllerFor("application").set("showFooter", true);
+      return true;
+    },
   },
 });
--- app/assets/javascripts/discourse/app/routes/user-activity-replies.js
+++ app/assets/javascripts/discourse/app/routes/user-activity-replies.js
@@ -1,7 +1,6 @@
 import UserAction from "discourse/models/user-action";
 import UserActivityStreamRoute from "discourse/routes/user-activity-stream";
 import I18n from "I18n";
-import { action } from "@ember/object";
 
 export default UserActivityStreamRoute.extend({
   userActionType: UserAction.TYPES["posts"],
@@ -13,9 +12,10 @@ export default UserActivityStreamRoute.extend({
     return { title, body };
   },
 
-  @action
-  didTransition() {
-    this.controllerFor("application").set("showFooter", true);
-    return true;
+  actions: {
+    didTransition() {
+      this.controllerFor("application").set("showFooter", true);
+      return true;
+    },
   },
 });
