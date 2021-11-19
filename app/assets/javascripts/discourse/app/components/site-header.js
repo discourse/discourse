6a038e95aac9caa1b27d4dd3bf25822e4c54f81a
Penar Musaraj <pmusaraj@gmail.com>
2021-11-19 15:15:13 -0500

    FIX: Software update prompt position on themes with extra header

--- app/assets/javascripts/discourse/app/components/site-header.js
+++ app/assets/javascripts/discourse/app/components/site-header.js
@@ -186,18 +186,17 @@ const SiteHeaderComponent = MountWidget.extend(
       const headerRect = header.getBoundingClientRect(),
         headerOffset = headerRect.top + headerRect.height,
         doc = document.documentElement;
+      doc.style.setProperty("--header-offset", `${headerOffset}px`);
       if (offset >= this.docAt) {
         if (!this.dockedHeader) {
           document.body.classList.add("docked");
           this.dockedHeader = true;
-          doc.style.setProperty("--header-offset", `${headerOffset}px`);
         }
       } else {
         if (this.dockedHeader) {
           document.body.classList.remove("docked");
           this.dockedHeader = false;
         }
-        doc.style.setProperty("--header-offset", `${headerOffset}px`);
       }
     },
 
