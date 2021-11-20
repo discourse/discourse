8082b4c29a8e5b1a7b4efa2ff48097f7259f13f4
angusmcleod <angus@mcleod.org.au>
2021-11-20 13:33:38 +0800

    Update group.rb annotations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1132,7 +1132,6 @@ end
 #  imap_enabled                       :boolean          default(FALSE)
 #  imap_updated_at                    :datetime
 #  imap_updated_by_id                 :integer
-#  associated_groups                  :string
 #
 # Indexes
 #
