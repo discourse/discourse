1336bb8c0003fed3fec647f0072f6866adfe974b
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2021-11-23 16:20:08 +0800

    DEV: Allow callback to be registered to remove post menu button.
    This will allow buttons in the post menu to be remove based on a post's
    attributes or site settings.

--- app/assets/javascripts/discourse/tests/integration/widgets/post-menu-test.js
+++ app/assets/javascripts/discourse/tests/integration/widgets/post-menu-test.js
@@ -7,18 +7,23 @@ import {
   exists,
 } from "discourse/tests/helpers/qunit-helpers";
 import hbs from "htmlbars-inline-precompile";
+import { resetPostMenuExtraButtons } from "discourse/widgets/post-menu";
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 discourseModule(
   "Integration | Component | Widget | post-menu",
   function (hooks) {
+    hooks.afterEach(() => {
+      resetPostMenuExtraButtons();
+    });
+
     setupRenderingTest(hooks);
 
     componentTest("add extra button", {
       template: hbs`{{mount-widget widget="post-menu" args=args}}`,
       beforeEach() {
         this.set("args", {});
-        withPluginApi("0.8", (api) => {
+        withPluginApi("0.14.0", (api) => {
           api.addPostMenuButton("coffee", () => {
             return {
               action: "drinkCoffee",
@@ -30,6 +35,7 @@ discourseModule(
           });
         });
       },
+
       async test(assert) {
         assert.strictEqual(
           count(".actions .extra-buttons .hot-coffee"),
@@ -39,19 +45,54 @@ discourseModule(
       },
     });
 
-    componentTest("remove extra button", {
+    componentTest("removes button based on callback", {
       template: hbs`{{mount-widget widget="post-menu" args=args}}`,
+
       beforeEach() {
-        this.set("args", {});
-        withPluginApi("0.8", (api) => {
-          api.removePostMenuButton("coffee");
+        this.set("args", { canCreatePost: true, canRemoveReply: true });
+
+        withPluginApi("0.14.0", (api) => {
+          api.removePostMenuButton("reply", (attrs) => {
+            return attrs.canRemoveReply;
+          });
         });
       },
+
       async test(assert) {
-        assert.ok(
-          !exists(".actions .extra-buttons .hot-coffee"),
-          "It doesn't removes coffee button"
-        );
+        assert.ok(!exists(".actions .reply"), "it removes reply button");
+      },
+    });
+
+    componentTest("does not remove butto", {
+      template: hbs`{{mount-widget widget="post-menu" args=args}}`,
+
+      beforeEach() {
+        this.set("args", { canCreatePost: true, canRemoveReply: false });
+
+        withPluginApi("0.14.0", (api) => {
+          api.removePostMenuButton("reply", (attrs) => {
+            return attrs.canRemoveReply;
+          });
+        });
+      },
+
+      async test(assert) {
+        assert.ok(exists(".actions .reply"), "it does not remove reply button");
+      },
+    });
+
+    componentTest("removes button", {
+      template: hbs`{{mount-widget widget="post-menu" args=args}}`,
+      beforeEach() {
+        this.set("args", { canCreatePost: true });
+
+        withPluginApi("0.14.0", (api) => {
+          api.removePostMenuButton("reply");
+        });
+      },
+
+      async test(assert) {
+        assert.ok(!exists(".actions .reply"), "it removes reply button");
       },
     });
   }
