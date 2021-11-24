c90d33e5792da81d18eadd7f894f7109f0bff902
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2021-11-24 10:25:44 +0800

    DEV: Make it clear that plugin-api on the client side follows semver.

--- app/assets/javascripts/discourse/app/lib/plugin-api.js
+++ app/assets/javascripts/discourse/app/lib/plugin-api.js
@@ -93,7 +93,8 @@ import {
 import { CUSTOM_USER_SEARCH_OPTIONS } from "select-kit/components/user-chooser";
 import { downloadCalendar } from "discourse/lib/download-calendar";
 
-// If you add any methods to the API ensure you bump up this number
+// If you add any methods to the API ensure you bump up the version number
+// based on Semantic Versioning 2.0.0.
 const PLUGIN_API_VERSION = "0.14.0";
 
 // This helper prevents us from applying the same `modifyClass` over and over in test mode.
