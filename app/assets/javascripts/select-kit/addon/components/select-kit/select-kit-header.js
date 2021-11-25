4238953f4019f031644322d86ff879e0ea55aa46
Jarek Radosz <jradosz@gmail.com>
2021-11-25 14:13:47 +0100

    huh

--- app/assets/javascripts/select-kit/addon/components/select-kit/select-kit-header.js
+++ app/assets/javascripts/select-kit/addon/components/select-kit/select-kit-header.js
@@ -70,7 +70,7 @@ export default Component.extend(UtilsMixin, {
   },
 
   keyDown(event) {
-    if (this.selectKit.isDisabled) {
+    if (this.selectKit.isDisabled || this.selectKit.options.disabled) {
       return;
     }
 
