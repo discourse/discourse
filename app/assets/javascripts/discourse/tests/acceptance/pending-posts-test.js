7f3053a8f11ff8626055c537b9f8fdefecbcc3b5
Jarek Radosz <jradosz@gmail.com>
2021-11-29 22:46:43 +0100

    DEV: Fix pending posts page, make tests work in legacy env
    * `ember-test-selectors` was in both dependencies and devDependencies in discourse/package.json
    * `qunit-dom` in package.json was not only unused but also defunct, as it wasn't pulled into the legacy env app
    * Running the tests only in the ember cli env hid the fact that the pending posts feature wasn't working in the legacy environment
    * Tests were using ember-cli-only APIs while there are widely used testing APIs in Discourse that support both ember envs

--- app/assets/javascripts/discourse/tests/acceptance/pending-posts-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/pending-posts-test.js
@@ -1,31 +1,26 @@
-import { acceptance } from "discourse/tests/helpers/qunit-helpers";
+import {
+  acceptance,
+  count,
+  exists,
+} from "discourse/tests/helpers/qunit-helpers";
 import { test } from "qunit";
 import { click, visit } from "@ember/test-helpers";
-import { setupApplicationTest as EMBER_CLI_ENV } from "ember-qunit";
 
 acceptance("Pending posts - no existing pending posts", function (needs) {
-  if (!EMBER_CLI_ENV) {
-    return; // dom helpers not available in legacy env
-  }
-
   needs.user();
 
   test("No link to pending posts", async function (assert) {
     await visit("/u/eviltrout");
-    assert.dom(".action-list").doesNotIncludeText("Pending");
+    assert.ok(!exists(".action-list [href='/u/eviltrout/activity/pending']"));
   });
 });
 
 acceptance("Pending posts - existing pending posts", function (needs) {
-  if (!EMBER_CLI_ENV) {
-    return; // dom helpers not available in legacy env
-  }
-
   needs.user({ pending_posts_count: 2 });
 
   test("Navigate to pending posts", async function (assert) {
     await visit("/u/eviltrout");
     await click("[href='/u/eviltrout/activity/pending']");
-    assert.dom(".user-stream-item").exists({ count: 2 });
+    assert.strictEqual(count(".user-stream-item"), 2);
   });
 });
