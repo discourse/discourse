6662101208089def86ed18a81ac90d1c52670569
Jarek Radosz <jradosz@gmail.com>
2021-11-30 03:45:26 +0100

    DEV: Fix a test leak (#15135)
    The leak was introduced in #11722 and a test was added that relied on it in #14563
    
    This PR fixes the leak (bookmarks-test), fixes the test that relied on it (fast-edit-test), and repleces some ad-hoc code with cloneJSON helper (other files)

--- app/assets/javascripts/discourse/tests/acceptance/fast-edit-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/fast-edit-test.js
@@ -1,29 +1,36 @@
 import {
   acceptance,
   exists,
-  queryAll,
+  query,
   selectText,
 } from "discourse/tests/helpers/qunit-helpers";
 import { click, fillIn, triggerKeyEvent, visit } from "@ember/test-helpers";
 import { test } from "qunit";
+import postFixtures from "discourse/tests/fixtures/post";
+import { cloneJSON } from "discourse-common/lib/object";
 
 acceptance("Fast Edit", function (needs) {
   needs.user();
   needs.settings({
     enable_fast_edit: true,
   });
+  needs.pretender((server, helper) => {
+    server.get("/posts/419", () => {
+      return helper.response(cloneJSON(postFixtures["/posts/398"]));
+    });
+  });
 
   test("Fast edit button works", async function (assert) {
     await visit("/t/internationalization-localization/280");
 
-    const textNode = document.querySelector("#post_1 .cooked p").childNodes[0];
+    const textNode = query("#post_1 .cooked p").childNodes[0];
 
     await selectText(textNode, 9);
     await click(".quote-button .quote-edit-label");
 
     assert.ok(exists("#fast-edit-input"), "fast editor is open");
     assert.strictEqual(
-      queryAll("#fast-edit-input").val(),
+      query("#fast-edit-input").value,
       "Any plans",
       "contains selected text"
     );
@@ -37,14 +44,14 @@ acceptance("Fast Edit", function (needs) {
   test("Works with keyboard shortcut", async function (assert) {
     await visit("/t/internationalization-localization/280");
 
-    const textNode = document.querySelector("#post_1 .cooked p").childNodes[0];
+    const textNode = query("#post_1 .cooked p").childNodes[0];
 
     await selectText(textNode, 9);
     await triggerKeyEvent(document, "keypress", "e".charCodeAt(0));
 
     assert.ok(exists("#fast-edit-input"), "fast editor is open");
     assert.strictEqual(
-      queryAll("#fast-edit-input").val(),
+      query("#fast-edit-input").value,
       "Any plans",
       "contains selected text"
     );
@@ -58,7 +65,7 @@ acceptance("Fast Edit", function (needs) {
   test("Opens full composer for multi-line selection", async function (assert) {
     await visit("/t/internationalization-localization/280");
 
-    const textNode = document.querySelector("#post_1 .cooked");
+    const textNode = query("#post_2 .cooked");
 
     await selectText(textNode);
     await click(".quote-button .quote-edit-label");
