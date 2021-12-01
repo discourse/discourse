093a523cf8b504425e0d645fa7eb069cc3fdd236
Sam Saffron <sam.saffron@gmail.com>
2021-12-02 10:57:48 +1100

    PERF: migrate normalized_emails in a migrations
    Old OnceOff job could perform pretty slowly on sites with millions of emails
    
    New implementation operates in batches in a migration, minimizing locking.

---
+++ db/migrate/20211201221028_migrate_email_to_normalized_email.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class MigrateEmailToNormalizedEmail < ActiveRecord::Migration[6.1]
+
+  # minimize locking on user_email table
+  disable_ddl_transaction!
+
+  def up
+
+    min, max, total = DB.query_single "SELECT MIN(id), MAX(id), COUNT(*) FROM user_emails"
+    # scaling is needed to compensate for "holes" where records were deleted
+    # and pathological cases where for some reason id 100_000_000 and 0 exist
+    scaling = total.to_f / (max - min).to_f
+
+    batch_size = 100_000
+
+    enum = Enumerator.new do |yielder|
+      low = min
+      loop do
+        high = (low + (batch_size / scaling)).to_i
+        yielder << [low, high]
+        low = high
+      end
+    end
+
+    loop do
+      low_id, high_id = enum.next
+
+      break if low_id > max
+
+      # using execute cause MiniSQL is not logging at the moment
+      # to_i is not needed, but specified so it is explicit there is no SQL injection
+      execute <<~SQL
+        UPDATE user_emails
+        SET normalized_email = REPLACE(REGEXP_REPLACE(email,'([+@].*)',''),'.','') || REGEXP_REPLACE(email, '[^@]*', '')
+        WHERE (normalized_email IS NULL OR normalized_email <> (REPLACE(REGEXP_REPLACE(email,'([+@].*)',''),'.','') || REGEXP_REPLACE(email, '[^@]*', '')))
+          AND (id >= #{low_id.to_i} AND id < #{high_id.to_i})
+      SQL
+    end
+  end
+
+  def down
+    execute "UPDATE user_emails SET normalized_email = null"
+  end
+end
