b0c8505d3f6375891950b7ab5eb3a8f5d7e1949c
Sam Saffron <sam.saffron@gmail.com>
2021-12-02 11:56:21 +1100

    re-implemented without guessing scaling for windows
    should be a lot more reusable in future

--- db/post_migrate/20211201221028_migrate_email_to_normalized_email.rb
+++ db/post_migrate/20211201221028_migrate_email_to_normalized_email.rb
@@ -7,35 +7,28 @@ class MigrateEmailToNormalizedEmail < ActiveRecord::Migration[6.1]
 
   def up
 
-    min, max, total = DB.query_single "SELECT MIN(id), MAX(id), COUNT(*) FROM user_emails"
+    min, max = DB.query_single "SELECT MIN(id), MAX(id) FROM user_emails"
     # scaling is needed to compensate for "holes" where records were deleted
     # and pathological cases where for some reason id 100_000_000 and 0 exist
 
     # avoid doing any work on empty dbs
     return if min.nil?
 
-    scaling =
-      if min == max
-        1
-      else
-        total.to_f / (max - min).to_f
-      end
+    bounds = DB.query_single <<~SQL
+      SELECT t.id
+      FROM (
+        SELECT *, row_number() OVER(ORDER BY id ASC) AS row
+        FROM user_emails
+      ) t
+      WHERE t.row % 100000 = 0
+    SQL
 
-    batch_size = 100_000
+    # subtle but loop does < not <=
+    # includes low, excludes high
+    bounds << (max + 1)
 
-    enum = Enumerator.new do |yielder|
-      low = min
-      loop do
-        high = (low + (batch_size / scaling)).to_i
-        yielder << [low, high]
-        low = high
-      end
-    end
-
-    loop do
-      low_id, high_id = enum.next
-
-      break if low_id > max
+    low_id = min
+    bounds.each do |high_id|
 
       # using execute cause MiniSQL is not logging at the moment
       # to_i is not needed, but specified so it is explicit there is no SQL injection
@@ -45,7 +38,10 @@ class MigrateEmailToNormalizedEmail < ActiveRecord::Migration[6.1]
         WHERE (normalized_email IS NULL OR normalized_email <> (REPLACE(REGEXP_REPLACE(email,'([+@].*)',''),'.','') || REGEXP_REPLACE(email, '[^@]*', '')))
           AND (id >= #{low_id.to_i} AND id < #{high_id.to_i})
       SQL
+
+      low_id = high_id
     end
+
   end
 
   def down
