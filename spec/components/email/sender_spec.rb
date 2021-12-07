discourse/discourse

commit 3b13f1146b2a406238c50d6b45bc9aa721094f46

FIX: Add random suffix to outbound Message-ID for email (#15179)
Currently the Message-IDs we send out for outbound email
are not unique; for a post they look like:

topic/TOPIC_ID/POST_ID@HOST

And for a topic they look like:

topic/TOPIC_ID@HOST

This commit changes the outbound Message-IDs to also have
a random suffix before the host, so the new format is
like this:

topic/TOPIC_ID/POST_ID.RANDOM_SUFFIX@HOST

Or:

topic/TOPIC_ID.RANDOM_SUFFIX@HOST

This should help with email deliverability. This change
is backwards-compatible, the old Message-ID format will
still be recognized in the mail receiver flow, so people
will still be able to reply using Message-IDs, In-Reply-To,
and References headers that have already been sent.

This commit also refactors Message-ID related logic
to a central location, and adds judicious amounts of
tests and documentation.

lib/email/message_id_service.rb
@@ -1,0 +1,103 @@
+# frozen_string_literal: true
+
+module Email
+  ##
+  # Email Message-IDs are used in both our outbound and inbound email
+  # flow. For the outbound flow via Email::Sender, we assign a unique
+  # Message-ID for any emails sent out from the application.
+  # If we are sending an email related to a topic, such as through the
+  # PostAlerter class, then the Message-ID will contain references to
+  # the topic ID, and if it is for a specific post, the post ID,
+  # along with a random suffix to make the Message-ID truly unique.
+  # The host must also be included on the Message-IDs.
+  #
+  # For the inbound email flow via Email::Receiver, we use Message-IDs
+  # to discern which topic or post the inbound email reply should be
+  # in response to. In this case, the Message-ID is extracted from the
+  # References and/or In-Reply-To headers, and compared with either
+  # the IncomingEmail table, the Post table, or the IncomingEmail to
+  # determine where to send the reply.
+  #
+  # See https://datatracker.ietf.org/doc/html/rfc2822#section-3.6.4 for
+  # more specific information around Message-IDs in email.
+  #
+  # See https://tools.ietf.org/html/rfc850#section-2.1.7 for the
+  # Message-ID format specification.
+  class MessageIdService
+    class << self
+      def generate_default
+        "<#{SecureRandom.uuid}@#{host}>"
+      end
+
+      def generate_for_post(post, use_incoming_email_if_present: false)
+        if use_incoming_email_if_present && post.incoming_email&.message_id.present?
+          return "<#{post.incoming_email.message_id}>"
+        end
+
+        "<topic/#{post.topic_id}/#{post.id}.#{random_suffix}@#{host}>"
+      end
+
+      def generate_for_topic(topic, use_incoming_email_if_present: false)
+        first_post = topic.ordered_posts.first
+
+        if use_incoming_email_if_present && first_post.incoming_email&.message_id.present?
+          return "<#{first_post.incoming_email.message_id}>"
+        end
+
+        "<topic/#{topic.id}.#{random_suffix}@#{host}>"
+      end
+
+      def find_post_from_message_ids(message_ids)
+        message_ids = message_ids.map { |message_id| message_id_clean(message_id) }
+        post_ids =  message_ids.map { |message_id| message_id[message_id_post_id_regexp, 1] }.compact.map(&:to_i)
+        post_ids << Post.where(
+          topic_id: message_ids.map { |message_id| message_id[message_id_topic_id_regexp, 1] }.compact,
+          post_number: 1
+        ).pluck(:id)
+        post_ids << EmailLog.where(message_id: message_ids).pluck(:post_id)
+        post_ids << IncomingEmail.where(message_id: message_ids).pluck(:post_id)
+
+        post_ids.flatten!
+        post_ids.compact!
+        post_ids.uniq!
+
+        return if post_ids.empty?
+
+        Post.where(id: post_ids).order(:created_at).last
+      end
+
+      def random_suffix
+        SecureRandom.hex(12)
+      end
+
+      def discourse_generated_message_id?(message_id)
+        !!(message_id =~ message_id_post_id_regexp) ||
+          !!(message_id =~ message_id_topic_id_regexp)
+      end
+
+      def message_id_post_id_regexp
+        @message_id_post_id_regexp ||= Regexp.new "topic/\\d+/(\\d+|\\d+\.\\w+)@#{Regexp.escape(host)}"
+      end
+
+      def message_id_topic_id_regexp
+        @message_id_topic_id_regexp ||= Regexp.new "topic/(\\d+|\\d+\.\\w+)@#{Regexp.escape(host)}"
+      end
+
+      def message_id_rfc_format(message_id)
+        message_id.present? && !is_message_id_rfc?(message_id) ? "<#{message_id}>" : message_id
+      end
+
+      def message_id_clean(message_id)
+        message_id.present? && is_message_id_rfc?(message_id) ? message_id.gsub(/^<|>$/, "") : message_id
+      end
+
+      def is_message_id_rfc?(message_id)
+        message_id.start_with?('<') && message_id.include?('@') && message_id.end_with?('>')
+      end
+
+      def host
+        @host ||= Email::Sender.host_for(Discourse.base_url)
+      end
+    end
+  end
+end

lib/email/sender.rb
@@ -109,7 +109,7 @@ module Email
       ).pluck_first(:id)
 
       # always set a default Message ID from the host
-      @message.header['Message-ID'] = "<#{SecureRandom.uuid}@#{host}>"
+      @message.header['Message-ID'] = Email::MessageIdService.generate_default
 
       if topic_id.present? && post_id.present?
         post = Post.find_by(id: post_id, topic_id: topic_id)
@@ -121,15 +121,9 @@ module Email
         return skip(SkippedEmailLog.reason_types[:sender_topic_deleted]) if topic.blank?
 
         add_attachments(post)
-        first_post = topic.ordered_posts.first
 
-        topic_message_id = first_post.incoming_email&.message_id.present? ?
-          "<#{first_post.incoming_email.message_id}>" :
-          "<topic/#{topic_id}@#{host}>"
-
-        post_message_id = post.incoming_email&.message_id.present? ?
-          "<#{post.incoming_email.message_id}>" :
-          "<topic/#{topic_id}/#{post_id}@#{host}>"
+        topic_message_id = Email::MessageIdService.generate_for_topic(topic, use_incoming_email_if_present: true)
+        post_message_id = Email::MessageIdService.generate_for_post(post, use_incoming_email_if_present: true)
 
         referenced_posts = Post.includes(:incoming_email)
           .joins("INNER JOIN post_replies ON post_replies.post_id = posts.id ")
@@ -141,9 +135,9 @@ module Email
             "<#{referenced_post.incoming_email.message_id}>"
           else
             if referenced_post.post_number == 1
-              "<topic/#{topic_id}@#{host}>"
+              Email::MessageIdService.generate_for_topic(topic)
             else
-              "<topic/#{topic_id}/#{referenced_post.id}@#{host}>"
+              Email::MessageIdService.generate_for_post(referenced_post)
             end
           end
         end

spec/components/email/sender_spec.rb
@@ -260,6 +260,7 @@ describe Email::Sender do
     end
 
     context "email threading" do
+      let(:random_message_id_suffix) { "5f1330cfd941f323d7f99b9e" }
       fab!(:topic) { Fabricate(:topic) }
 
       fab!(:post_1) { Fabricate(:post, topic: topic, post_number: 1) }
@@ -271,14 +272,17 @@ describe Email::Sender do
       let!(:post_reply_2_4) { PostReply.create(post: post_2, reply: post_4) }
       let!(:post_reply_3_4) { PostReply.create(post: post_3, reply: post_4) }
 
-      before { message.header['X-Discourse-Topic-Id'] = topic.id }
+      before do
+        message.header['X-Discourse-Topic-Id'] = topic.id
+        Email::MessageIdService.stubs(:random_suffix).returns(random_message_id_suffix)
+      end
 
       it "doesn't set the 'In-Reply-To' and 'References' headers on the first post" do
         message.header['X-Discourse-Post-Id'] = post_1.id
 
         email_sender.send
 
-        expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}@test.localhost>")
+        expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}.#{random_message_id_suffix}@test.localhost>")
         expect(message.header['In-Reply-To'].to_s).to be_blank
         expect(message.header['References'].to_s).to be_blank
       end
@@ -288,8 +292,8 @@ describe Email::Sender do
 
         email_sender.send
 
-        expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}/#{post_2.id}@test.localhost>")
-        expect(message.header['In-Reply-To'].to_s).to eq("<topic/#{topic.id}@test.localhost>")
+        expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}/#{post_2.id}.#{random_message_id_suffix}@test.localhost>")
+        expect(message.header['In-Reply-To'].to_s).to eq("<topic/#{topic.id}.#{random_message_id_suffix}@test.localhost>")
       end
 
       it "sets the 'In-Reply-To' header to the newest replied post" do
@@ -297,8 +301,8 @@ describe Email::Sender do
 
         email_sender.send
 
-        expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}/#{post_4.id}@test.localhost>")
-        expect(message.header['In-Reply-To'].to_s).to eq("<topic/#{topic.id}/#{post_3.id}@test.localhost>")
+        expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}/#{post_4.id}.#{random_message_id_suffix}@test.localhost>")
+        expect(message.header['In-Reply-To'].to_s).to eq("<topic/#{topic.id}/#{post_3.id}.#{random_message_id_suffix}@test.localhost>")
       end
 
       it "sets the 'References' header to the topic and all replied posts" do
@@ -307,9 +311,9 @@ describe Email::Sender do
         email_sender.send
 
         references = [
-          "<topic/#{topic.id}@test.localhost>",
-          "<topic/#{topic.id}/#{post_3.id}@test.localhost>",
-          "<topic/#{topic.id}/#{post_2.id}@test.localhost>",
+          "<topic/#{topic.id}.#{random_message_id_suffix}@test.localhost>",
+          "<topic/#{topic.id}/#{post_3.id}.#{random_message_id_suffix}@test.localhost>",
+          "<topic/#{topic.id}/#{post_2.id}.#{random_message_id_suffix}@test.localhost>",
         ]
 
         expect(message.header['References'].to_s).to eq(references.join(" "))
@@ -328,7 +332,7 @@ describe Email::Sender do
 
         references = [
           "<#{topic_incoming_email.message_id}>",
-          "<topic/#{topic.id}/#{post_3.id}@test.localhost>",
+          "<topic/#{topic.id}/#{post_3.id}.#{random_message_id_suffix}@test.localhost>",
           "<#{post_2_incoming_email.message_id}>",
         ]
 

spec/lib/message_id_service_spec.rb
@@ -1,0 +1,117 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Email::MessageIdService do
+  fab!(:topic) { Fabricate(:topic) }
+  fab!(:post) { Fabricate(:post, topic: topic) }
+  fab!(:second_post) { Fabricate(:post, topic: topic) }
+
+  subject { described_class }
+
+  describe "#generate_for_post" do
+    it "generates for the post using the message_id on the post's incoming_email" do
+      Fabricate(:incoming_email, message_id: "test@test.localhost", post: post)
+      post.reload
+      expect(subject.generate_for_post(post, use_incoming_email_if_present: true)).to eq("<test@test.localhost>")
+    end
+
+    it "generates for the post without an incoming_email record" do
+      expect(subject.generate_for_post(post)).to match(subject.message_id_post_id_regexp)
+      expect(subject.generate_for_post(post, use_incoming_email_if_present: true)).to match(subject.message_id_post_id_regexp)
+    end
+  end
+
+  describe "#generate_for_topic" do
+    it "generates for the topic using the message_id on the first post's incoming_email" do
+      Fabricate(:incoming_email, message_id: "test@test.localhost", post: post)
+      post.reload
+      expect(subject.generate_for_topic(topic, use_incoming_email_if_present: true)).to eq("<test@test.localhost>")
+    end
+
+    it "generates for the topic without an incoming_email record" do
+      expect(subject.generate_for_topic(topic)).to match(subject.message_id_topic_id_regexp)
+      expect(subject.generate_for_topic(topic, use_incoming_email_if_present: true)).to match(subject.message_id_topic_id_regexp)
+    end
+  end
+
+  describe "find_post_from_message_ids" do
+    let(:post_format_message_id) { "<topic/#{topic.id}/#{post.id}.test123@test.localhost>" }
+    let(:topic_format_message_id) { "<topic/#{topic.id}.test123@test.localhost>" }
+    let(:default_format_message_id) { "<36ac1ddd-5083-461d-b72c-6372fb0e7f33@test.localhost>" }
+    let(:gmail_format_message_id) { "<CAPGrNgZ7QEFuPcsxJBRZLhBhAYPO_ruYpCANSdqiQEbc9Otpiw@mail.gmail.com>" }
+
+    it "finds a post based only on a post-format message id" do
+      expect(subject.find_post_from_message_ids([post_format_message_id])).to eq(post)
+    end
+
+    it "finds a post based only on a topic-format message id" do
+      expect(subject.find_post_from_message_ids([topic_format_message_id])).to eq(post)
+    end
+
+    it "finds a post from the email log" do
+      email_log = Fabricate(:email_log, message_id: subject.message_id_clean(default_format_message_id))
+      expect(subject.find_post_from_message_ids([default_format_message_id])).to eq(email_log.post)
+    end
+
+    it "finds a post from the incoming email log" do
+      incoming_email = Fabricate(
+        :incoming_email,
+        message_id: subject.message_id_clean(gmail_format_message_id),
+        post: Fabricate(:post)
+      )
+      expect(subject.find_post_from_message_ids([gmail_format_message_id])).to eq(incoming_email.post)
+    end
+
+    it "gets the last created post if multiple are returned" do
+      incoming_email = Fabricate(
+        :incoming_email,
+        message_id: subject.message_id_clean(post_format_message_id),
+        post: Fabricate(:post, created_at: 10.days.ago)
+      )
+      expect(subject.find_post_from_message_ids([post_format_message_id])).to eq(post)
+    end
+  end
+
+  describe "#discourse_generated_message_id?" do
+    def check_format(message_id)
+      subject.discourse_generated_message_id?(message_id)
+    end
+
+    it "works correctly for the different possible formats" do
+      expect(check_format("topic/1223/4525.3c4f8n9@test.localhost")).to eq(true)
+      expect(check_format("<topic/1223/4525.3c4f8n9@test.localhost>")).to eq(true)
+      expect(check_format("topic/1223.fc3j4843@test.localhost")).to eq(true)
+      expect(check_format("<topic/1223.fc3j4843@test.localhost>")).to eq(true)
+      expect(check_format("topic/1223/4525@test.localhost")).to eq(true)
+      expect(check_format("<topic/1223/4525@test.localhost>")).to eq(true)
+      expect(check_format("topic/1223@test.localhost")).to eq(true)
+      expect(check_format("<topic/1223@test.localhost>")).to eq(true)
+
+      expect(check_format("topic/1223@blah")).to eq(false)
+      expect(check_format("<CAPGrNgZ7QEFuPcsxJBRZLhBhAYPO_ruYpCANSdqiQEbc9Otpiw@mail.gmail.com>")).to eq(false)
+      expect(check_format("t/1223@test.localhost")).to eq(false)
+    end
+  end
+
+  describe "#message_id_rfc_format" do
+    it "returns message ID in RFC format" do
+      expect(Email::MessageIdService.message_id_rfc_format("test@test")).to eq("<test@test>")
+    end
+
+    it "returns input if already in RFC format" do
+      expect(Email::MessageIdService.message_id_rfc_format("<test@test>")).to eq("<test@test>")
+    end
+  end
+
+  describe "#message_id_clean" do
+    it "returns message ID if in RFC format" do
+      expect(Email::MessageIdService.message_id_clean("<test@test>")).to eq("test@test")
+    end
+
+    it "returns input if a clean message ID is not in RFC format" do
+      message_id = "<" + "@" * 50
+      expect(Email::MessageIdService.message_id_clean(message_id)).to eq(message_id)
+    end
+  end
+end

app/controllers/webhooks_controller.rb
@@ -13,7 +13,7 @@ class WebhooksController < ActionController::Base
   def sendgrid
     events = params["_json"] || [params]
     events.each do |event|
-      message_id = Email.message_id_clean((event["smtp-id"] || ""))
+      message_id = Email::MessageIdService.message_id_clean((event["smtp-id"] || ""))
       to_address = event["email"]
       if event["event"] == "bounce"
         if event["status"]["4."]
@@ -150,7 +150,7 @@ class WebhooksController < ActionController::Base
     return mailgun_failure unless valid_mailgun_signature?(params["token"], params["timestamp"], params["signature"])
 
     event = params["event"]
-    message_id = Email.message_id_clean(params["Message-Id"])
+    message_id = Email::MessageIdService.message_id_clean(params["Message-Id"])
     to_address = params["recipient"]
 
     # only handle soft bounces, because hard bounces are also handled

lib/email.rb
@@ -52,20 +52,7 @@ module Email
     SiteSetting.email_site_title.presence || SiteSetting.title
   end
 
-  # https://tools.ietf.org/html/rfc850#section-2.1.7
-  def self.message_id_rfc_format(message_id)
-    message_id.present? && !is_message_id_rfc?(message_id) ? "<#{message_id}>" : message_id
-  end
-
-  def self.message_id_clean(message_id)
-    message_id.present? && is_message_id_rfc?(message_id) ? message_id.gsub(/^<|>$/, "") : message_id
-  end
-
   private
-
-  def self.is_message_id_rfc?(message_id)
-    message_id.start_with?('<') && message_id.include?('@') && message_id.end_with?('>')
-  end
 
   def self.obfuscate_part(part)
     if part.size < 3

lib/email/receiver.rb
@@ -107,7 +107,7 @@ module Email
       # server (e.g. a message_id generated by Gmail) and does not need to
       # be updated, because message_ids from the IMAP server are not guaranteed
       # to be unique.
-      return unless discourse_generated_message_id?(@message_id)
+      return unless Email::MessageIdService.discourse_generated_message_id?(@message_id)
 
       incoming_email.update(
         imap_uid_validity: @opts[:imap_uid_validity],
@@ -801,7 +801,7 @@ module Email
       # if the user is directly replying to an email send to them from discourse,
       # there will be a corresponding EmailLog record, so we can use that as the
       # reply post if it exists
-      if discourse_generated_message_id?(mail.in_reply_to)
+      if Email::MessageIdService.discourse_generated_message_id?(mail.in_reply_to)
         post_id_from_email_log = EmailLog.where(message_id: mail.in_reply_to)
           .addressed_to_user(user)
           .order(created_at: :desc)
@@ -1056,35 +1056,7 @@ module Email
       message_ids = Email::Receiver.extract_reply_message_ids(@mail, max_message_id_count: 5)
       return if message_ids.empty?
 
-      post_ids =  message_ids.map { |message_id| message_id[message_id_post_id_regexp, 1] }.compact.map(&:to_i)
-      post_ids << Post.where(topic_id: message_ids.map { |message_id| message_id[message_id_topic_id_regexp, 1] }.compact, post_number: 1).pluck(:id)
-      post_ids << EmailLog.where(message_id: message_ids).pluck(:post_id)
-      post_ids << IncomingEmail.where(message_id: message_ids).pluck(:post_id)
-
-      post_ids.flatten!
-      post_ids.compact!
-      post_ids.uniq!
-
-      return if post_ids.empty?
-
-      Post.where(id: post_ids).order(:created_at).last
-    end
-
-    def host
-      @host ||= Email::Sender.host_for(Discourse.base_url)
-    end
-
-    def discourse_generated_message_id?(message_id)
-      !!(message_id =~ message_id_post_id_regexp) ||
-        !!(message_id =~ message_id_topic_id_regexp)
-    end
-
-    def message_id_post_id_regexp
-      @message_id_post_id_regexp ||= Regexp.new "topic/\\d+/(\\d+)@#{Regexp.escape(host)}"
-    end
-
-    def message_id_topic_id_regexp
-      @message_id_topic_id_regexp ||= Regexp.new "topic/(\\d+)@#{Regexp.escape(host)}"
+      Email::MessageIdService.find_post_from_message_ids(message_ids)
     end
 
     def self.extract_reply_message_ids(mail, max_message_id_count:)
@@ -1100,7 +1072,7 @@ module Email
         references
       elsif references.present?
         references.split(/[\s,]/).map do |r|
-          Email.message_id_clean(r)
+          Email::MessageIdService.message_id_clean(r)
         end
       end
     end

commit 73504f94e8f6fafeb48639e355fb160e2fedce20

FIX: References header leading to broken email threading
Since 3b13f1146b2a406238c50d6b45bc9aa721094f46 the email threading
in mail clients has been broken, because the random suffix meant
that the References header would always be different for non-group
SMTP email notifications sent out.

This commit fixes the issue by always using the "canonical" topic
reference ID inside the References header in the format:

topic/TOPIC_ID@HOST

Which was the old format. We also add the References header to
notifications sent for the first post arriving, so the threading
works for subsequent emails. The Message-ID header is still random
as per the previous change.

lib/email/message_id_service.rb
@@ -37,14 +37,18 @@ module Email
         "<topic/#{post.topic_id}/#{post.id}.#{random_suffix}@#{host}>"
       end
 
-      def generate_for_topic(topic, use_incoming_email_if_present: false)
+      def generate_for_topic(topic, use_incoming_email_if_present: false, canonical: false)
         first_post = topic.ordered_posts.first
 
         if use_incoming_email_if_present && first_post.incoming_email&.message_id.present?
           return "<#{first_post.incoming_email.message_id}>"
         end
 
-        "<topic/#{topic.id}.#{random_suffix}@#{host}>"
+        if canonical
+          "<topic/#{topic.id}@#{host}>"
+        else
+          "<topic/#{topic.id}.#{random_suffix}@#{host}>"
+        end
       end
 
       def find_post_from_message_ids(message_ids)

lib/email/sender.rb
@@ -122,8 +122,20 @@ module Email
 
         add_attachments(post)
 
-        topic_message_id = Email::MessageIdService.generate_for_topic(topic, use_incoming_email_if_present: true)
-        post_message_id = Email::MessageIdService.generate_for_post(post, use_incoming_email_if_present: true)
+        # If the topic was created from an incoming email, then the Message-ID from
+        # that email will be the canonical reference, otherwise the canonical reference
+        # will be <topic/TOPIC_ID@host>. The canonical reference is used in the
+        # References header.
+        #
+        # This is so the sender of the original email still gets their nice threading
+        # maintained (because their mail client will initiate threading based on
+        # the Message-ID it generated) in the case where there is an incoming email.
+        #
+        # In the latter case, everyone will start their thread with the canonical reference,
+        # because we send it in the References header for all emails.
+        topic_canonical_reference_id = Email::MessageIdService.generate_for_topic(
+          topic, canonical: true, use_incoming_email_if_present: true
+        )
 
         referenced_posts = Post.includes(:incoming_email)
           .joins("INNER JOIN post_replies ON post_replies.post_id = posts.id ")
@@ -135,23 +147,29 @@ module Email
             "<#{referenced_post.incoming_email.message_id}>"
           else
             if referenced_post.post_number == 1
-              Email::MessageIdService.generate_for_topic(topic)
+              topic_canonical_reference_id
             else
               Email::MessageIdService.generate_for_post(referenced_post)
             end
           end
         end
 
-        # https://www.ietf.org/rfc/rfc2822.txt
+        # See https://www.ietf.org/rfc/rfc2822.txt for the message format
+        # specification, more useful information can be found in Email::MessageIdService
+        #
+        # The References header is how mail clients handle threading. The Message-ID
+        # must always be unique.
         if post.post_number == 1
-          @message.header['Message-ID']  = topic_message_id
+          @message.header['Message-ID']  = Email::MessageIdService.generate_for_topic(topic)
+          @message.header['References']  = [topic_canonical_reference_id]
         else
-          @message.header['Message-ID']  = post_message_id
-          @message.header['In-Reply-To'] = referenced_post_message_ids[0] || topic_message_id
-          @message.header['References']  = [topic_message_id, referenced_post_message_ids].flatten.compact.uniq
+          @message.header['Message-ID']  = Email::MessageIdService.generate_for_post(post)
+          @message.header['In-Reply-To'] = referenced_post_message_ids[0] || topic_canonical_reference_id
+          @message.header['References']  = [topic_canonical_reference_id, referenced_post_message_ids].flatten.compact.uniq
         end
 
-        # https://www.ietf.org/rfc/rfc2919.txt
+        # See https://www.ietf.org/rfc/rfc2919.txt for the List-ID
+        # specification.
         if topic&.category && !topic.category.uncategorized?
           list_id = "#{SiteSetting.title} | #{topic.category.name} <#{topic.category.name.downcase.tr(' ', '-')}.#{host}>"
 

spec/components/email/sender_spec.rb
@@ -277,23 +277,34 @@ describe Email::Sender do
         Email::MessageIdService.stubs(:random_suffix).returns(random_message_id_suffix)
       end
 
-      it "doesn't set the 'In-Reply-To' and 'References' headers on the first post" do
+      it "doesn't set the 'In-Reply-To' header but does set the 'References' header on the first post" do
         message.header['X-Discourse-Post-Id'] = post_1.id
 
         email_sender.send
 
         expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}.#{random_message_id_suffix}@test.localhost>")
         expect(message.header['In-Reply-To'].to_s).to be_blank
-        expect(message.header['References'].to_s).to be_blank
+        expect(message.header['References'].to_s).to eq("<topic/#{topic.id}@test.localhost>")
       end
 
-      it "sets the 'In-Reply-To' header to the topic by default" do
+      it "sets the 'References' header with the incoming email Message-ID if it exists on the first post" do
+        incoming = Fabricate(:incoming_email, topic: topic, post: post_1, message_id: "blah1234@someemailprovider.com")
+        message.header['X-Discourse-Post-Id'] = post_1.id
+
+        email_sender.send
+
+        expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}.#{random_message_id_suffix}@test.localhost>")
+        expect(message.header['In-Reply-To'].to_s).to be_blank
+        expect(message.header['References'].to_s).to eq("<blah1234@someemailprovider.com>")
+      end
+
+      it "sets the 'In-Reply-To' header to the topic canonical reference by default" do
         message.header['X-Discourse-Post-Id'] = post_2.id
 
         email_sender.send
 
         expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}/#{post_2.id}.#{random_message_id_suffix}@test.localhost>")
-        expect(message.header['In-Reply-To'].to_s).to eq("<topic/#{topic.id}.#{random_message_id_suffix}@test.localhost>")
+        expect(message.header['In-Reply-To'].to_s).to eq("<topic/#{topic.id}@test.localhost>")
       end
 
       it "sets the 'In-Reply-To' header to the newest replied post" do
@@ -305,13 +316,13 @@ describe Email::Sender do
         expect(message.header['In-Reply-To'].to_s).to eq("<topic/#{topic.id}/#{post_3.id}.#{random_message_id_suffix}@test.localhost>")
       end
 
-      it "sets the 'References' header to the topic and all replied posts" do
+      it "sets the 'References' header to the topic canonical reference and all replied posts" do
         message.header['X-Discourse-Post-Id'] = post_4.id
 
         email_sender.send
 
         references = [
-          "<topic/#{topic.id}.#{random_message_id_suffix}@test.localhost>",
+          "<topic/#{topic.id}@test.localhost>",
           "<topic/#{topic.id}/#{post_3.id}.#{random_message_id_suffix}@test.localhost>",
           "<topic/#{topic.id}/#{post_2.id}.#{random_message_id_suffix}@test.localhost>",
         ]
@@ -319,7 +330,7 @@ describe Email::Sender do
         expect(message.header['References'].to_s).to eq(references.join(" "))
       end
 
-      it "uses the incoming_email message_id when available" do
+      it "uses the incoming_email message_id when available, but always uses a random message-id" do
         topic_incoming_email  = IncomingEmail.create(topic: topic, post: post_1, message_id: "foo@bar")
         post_2_incoming_email = IncomingEmail.create(topic: topic, post: post_2, message_id: "bar@foo")
         post_4_incoming_email = IncomingEmail.create(topic: topic, post: post_4, message_id: "wat@wat")
@@ -328,7 +339,7 @@ describe Email::Sender do
 
         email_sender.send
 
-        expect(message.header['Message-Id'].to_s).to eq("<#{post_4_incoming_email.message_id}>")
+        expect(message.header['Message-Id'].to_s).to eq("<topic/#{topic.id}/#{post_4.id}.5f1330cfd941f323d7f99b9e@test.localhost>")
 
         references = [
           "<#{topic_incoming_email.message_id}>",

spec/lib/message_id_service_spec.rb
@@ -24,14 +24,20 @@ describe Email::MessageIdService do
 
   describe "#generate_for_topic" do
     it "generates for the topic using the message_id on the first post's incoming_email" do
-      Fabricate(:incoming_email, message_id: "test@test.localhost", post: post)
+      Fabricate(:incoming_email, message_id: "test213428@somemailservice.com", post: post)
       post.reload
-      expect(subject.generate_for_topic(topic, use_incoming_email_if_present: true)).to eq("<test@test.localhost>")
+      expect(subject.generate_for_topic(topic, use_incoming_email_if_present: true)).to eq("<test213428@somemailservice.com>")
     end
 
     it "generates for the topic without an incoming_email record" do
       expect(subject.generate_for_topic(topic)).to match(subject.message_id_topic_id_regexp)
       expect(subject.generate_for_topic(topic, use_incoming_email_if_present: true)).to match(subject.message_id_topic_id_regexp)
+    end
+
+    it "generates canonical for the topic" do
+      canonical_topic_id = subject.generate_for_topic(topic, canonical: true)
+      expect(canonical_topic_id).to match(subject.message_id_topic_id_regexp)
+      expect(canonical_topic_id).to eq("<topic/#{topic.id}@test.localhost>")
     end
   end
 