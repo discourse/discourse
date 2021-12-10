db0725ea4c87cb799548d35671df3eaef9c9d44e
Penar Musaraj <pmusaraj@gmail.com>
2021-12-10 12:54:21 -0500

    DEV Refactor popup-tip
    Replaces jQuery animation with CSS.

--- app/assets/javascripts/discourse/app/components/popup-input-tip.js
+++ app/assets/javascripts/discourse/app/components/popup-input-tip.js
@@ -1,16 +1,11 @@
 import { alias, not } from "@ember/object/computed";
-import discourseComputed, { observes } from "discourse-common/utils/decorators";
+import discourseComputed from "discourse-common/utils/decorators";
 import Component from "@ember/component";
-import { iconHTML } from "discourse-common/lib/icon-library";
 
 export default Component.extend({
   classNameBindings: [":popup-tip", "good", "bad", "lastShownAt::hide"],
   attributeBindings: ["role"],
-  animateAttribute: null,
-  bouncePixels: 6,
-  bounceDelay: 100,
   rerenderTriggers: ["validation.reason"],
-  closeIcon: `${iconHTML("times-circle")}`.htmlSafe(),
   tipReason: null,
 
   @discourseComputed("bad")
@@ -33,21 +28,6 @@ export default Component.extend({
     return shownAt || lastShownAt;
   },
 
-  @observes("lastShownAt")
-  bounce() {
-    if (this.lastShownAt) {
-      let $elem = $(this.element);
-      if (!this.animateAttribute) {
-        this.animateAttribute = $elem.css("left") === "auto" ? "right" : "left";
-      }
-      if (this.animateAttribute === "left") {
-        this.bounceLeft($elem);
-      } else {
-        this.bounceRight($elem);
-      }
-    }
-  },
-
   didReceiveAttrs() {
     this._super(...arguments);
     let reason = this.get("validation.reason");
@@ -55,22 +35,6 @@ export default Component.extend({
       this.set("tipReason", `${reason}`.htmlSafe());
     } else {
       this.set("tipReason", null);
-    }
-  },
-
-  bounceLeft($elem) {
-    for (let i = 0; i < 5; i++) {
-      $elem
-        .animate({ left: "+=" + this.bouncePixels }, this.bounceDelay)
-        .animate({ left: "-=" + this.bouncePixels }, this.bounceDelay);
-    }
-  },
-
-  bounceRight($elem) {
-    for (let i = 0; i < 5; i++) {
-      $elem
-        .animate({ right: "-=" + this.bouncePixels }, this.bounceDelay)
-        .animate({ right: "+=" + this.bouncePixels }, this.bounceDelay);
     }
   },
 });
