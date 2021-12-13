18a209bd0dea9a77953d7f405ed3e37a7fb52b90
Martin Brennan <mjrbrennan@gmail.com>
2021-12-13 15:23:44 +1000

    DEV: Use Uppy in wizard-field-image uploads (#15269)
    We cannot use any of the uppy mixins or core code, because
    the code there is not shared with the wizard, and to move
    it all to discourse-common would be a task almost equal
    difficulty to taking the ring to Mordor.
    
    Therefore, we can just use the uppy vendor libraries in the
    wizard, and do a quick-n-dirty version of the uppy upload
    code for the wizard-field-image uploader.

--- app/assets/javascripts/wizard-shims.js
+++ app/assets/javascripts/wizard-shims.js
@@ -4,3 +4,40 @@ define("@popperjs/core", ["exports"], function (__exports__) {
   __exports__.defaultModifiers = window.Popper.defaultModifiers;
   __exports__.popperGenerator = window.Popper.popperGenerator;
 });
+
+define("@uppy/core", ["exports"], function (__exports__) {
+  __exports__.default = window.Uppy.Core;
+  __exports__.BasePlugin = window.Uppy.Core.BasePlugin;
+});
+
+define("@uppy/aws-s3", ["exports"], function (__exports__) {
+  __exports__.default = window.Uppy.AwsS3;
+});
+
+define("@uppy/aws-s3-multipart", ["exports"], function (__exports__) {
+  __exports__.default = window.Uppy.AwsS3Multipart;
+});
+
+define("@uppy/xhr-upload", ["exports"], function (__exports__) {
+  __exports__.default = window.Uppy.XHRUpload;
+});
+
+define("@uppy/drop-target", ["exports"], function (__exports__) {
+  __exports__.default = window.Uppy.DropTarget;
+});
+
+define("@uppy/utils/lib/delay", ["exports"], function (__exports__) {
+  __exports__.default = window.Uppy.Utils.delay;
+});
+
+define("@uppy/utils/lib/EventTracker", ["exports"], function (__exports__) {
+  __exports__.default = window.Uppy.Utils.EventTracker;
+});
+
+define("@uppy/utils/lib/AbortController", ["exports"], function (__exports__) {
+  __exports__.AbortController =
+    window.Uppy.Utils.AbortControllerLib.AbortController;
+  __exports__.AbortSignal = window.Uppy.Utils.AbortControllerLib.AbortSignal;
+  __exports__.createAbortError =
+    window.Uppy.Utils.AbortControllerLib.createAbortError;
+});
