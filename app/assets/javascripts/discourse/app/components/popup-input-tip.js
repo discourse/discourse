954ab4e0ece91e181b868ed4a463e849a3037614
Penar Musaraj <pmusaraj@gmail.com>
2021-12-13 11:22:02 -0500

    DEV: Refactor popup-tip component (#15257)

--- app/assets/javascripts/discourse/app/components/popup-input-tip.js
+++ app/assets/javascripts/discourse/app/components/popup-input-tip.js
@@ -1,17 +1,16 @@
-import { alias, not } from "@ember/object/computed";
-import discourseComputed, { observes } from "discourse-common/utils/decorators";
+import { alias, not, or } from "@ember/object/computed";
+import discourseComputed from "discourse-common/utils/decorators";
 import Component from "@ember/component";
-import { iconHTML } from "discourse-common/lib/icon-library";
+import { getOwner } from "discourse-common/lib/get-owner";
 
 export default Component.extend({
   classNameBindings: [":popup-tip", "good", "bad", "lastShownAt::hide"],
   attributeBindings: ["role"],
-  animateAttribute: null,
-  bouncePixels: 6,
-  bounceDelay: 100,
   rerenderTriggers: ["validation.reason"],
-  closeIcon: `${iconHTML("times-circle")}`.htmlSafe(),
   tipReason: null,
+  lastShownAt: or("shownAt", "validation.lastShownAt"),
+  bad: alias("validation.failed"),
+  good: not("bad"),
 
   @discourseComputed("bad")
   role(bad) {
@@ -22,30 +21,8 @@ export default Component.extend({
 
   click() {
     this.set("shownAt", null);
-    this.set("validation.lastShownAt", null);
-  },
-
-  bad: alias("validation.failed"),
-  good: not("bad"),
-
-  @discourseComputed("shownAt", "validation.lastShownAt")
-  lastShownAt(shownAt, lastShownAt) {
-    return shownAt || lastShownAt;
-  },
-
-  @observes("lastShownAt")
-  bounce() {
-    if (this.lastShownAt) {
-      let $elem = $(this.element);
-      if (!this.animateAttribute) {
-        this.animateAttribute = $elem.css("left") === "auto" ? "right" : "left";
-      }
-      if (this.animateAttribute === "left") {
-        this.bounceLeft($elem);
-      } else {
-        this.bounceRight($elem);
-      }
-    }
+    const composer = getOwner(this).lookup("controller:composer");
+    composer.clearLastValidatedAt();
   },
 
   didReceiveAttrs() {
@@ -55,22 +32,6 @@ export default Component.extend({
       this.set("tipReason", `${reason}`.htmlSafe());
     } else {
       this.set("tipReason", null);
-    }
-  },
-
-  bounceLeft($elem) {
-    for (let i = 0; i < 5; i++) {
-      $elem
-        .animate({ left: "+=" + this.bouncePixels }, this.bounceDelay)
-        .animate({ left: "-=" + this.bouncePixels }, this.bounceDelay);
-    }
-  },
-
-  bounceRight($elem) {
-    for (let i = 0; i < 5; i++) {
-      $elem
-        .animate({ right: "-=" + this.bouncePixels }, this.bounceDelay)
-        .animate({ right: "+=" + this.bouncePixels }, this.bounceDelay);
     }
   },
 });
