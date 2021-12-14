031f4f06d558128c6696801456bcc1b120c72a12
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2021-12-14 17:21:49 +0100

    DEV: update and improvements to json editor (#15294)
    - changes on how errors are handled to prevent weird cases
    - uses didInsert/willDestroy to setup/clean state
    - updates json editor library to 2.6.1

--- app/assets/javascripts/discourse/app/lib/public-js-versions.js
+++ app/assets/javascripts/discourse/app/lib/public-js-versions.js
@@ -3,7 +3,7 @@
 
 export const PUBLIC_JS_VERSIONS = {
   "ace/ace.js": "ace.js/1.4.12/ace.js",
-  "jsoneditor.js": "@json-editor/json-editor/2.5.2/jsoneditor.js",
+  "jsoneditor.js": "@json-editor/json-editor/2.6.1/jsoneditor.js",
   "Chart.min.js": "chart.js/3.5.1/Chart.min.js",
   "chartjs-plugin-datalabels.min.js":
     "chartjs-plugin-datalabels/2.0.0/chartjs-plugin-datalabels.min.js",
