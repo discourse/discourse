07c3d220cd4ef43c3924d50a8004cb303a3471f1
Rafael dos Santos Silva <xfalcox@gmail.com>
2021-12-20 14:34:50 -0300

    FIX: Enable Babel plugins in PrettyText bundle

--- app/assets/javascripts/discourse/lib/pretty-text-engine.js
+++ app/assets/javascripts/discourse/lib/pretty-text-engine.js
@@ -6,7 +6,36 @@ const concat = require("broccoli-concat");
 
 module.exports = function prettyTextEngine(vendorJs, engine) {
   let engineTree = babel(`../pretty-text/engines/${engine}`, {
-    plugins: ["@babel/plugin-transform-modules-amd"],
+    plugins: [
+      "@babel/plugin-transform-modules-amd",
+      "@babel/plugin-proposal-json-strings",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      "@babel/plugin-proposal-logical-assignment-operators",
+      "@babel/plugin-proposal-numeric-separator",
+      "@babel/plugin-proposal-optional-catch-binding",
+      "@babel/plugin-transform-dotall-regex",
+      "@babel/plugin-proposal-unicode-property-regex",
+      "@babel/plugin-transform-named-capturing-groups-regex",
+      "@babel/plugin-proposal-object-rest-spread",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-transform-arrow-functions",
+      "@babel/plugin-transform-block-scoped-functions",
+      "@babel/plugin-transform-block-scoping",
+      "@babel/plugin-transform-computed-properties",
+      "@babel/plugin-transform-destructuring",
+      "@babel/plugin-transform-duplicate-keys",
+      "@babel/plugin-transform-for-of",
+      "@babel/plugin-transform-function-name",
+      "@babel/plugin-transform-literals",
+      "@babel/plugin-transform-object-super",
+      "@babel/plugin-transform-parameters",
+      "@babel/plugin-transform-shorthand-properties",
+      "@babel/plugin-transform-spread",
+      "@babel/plugin-transform-sticky-regex",
+      "@babel/plugin-transform-template-literals",
+      "@babel/plugin-transform-typeof-symbol",
+      "@babel/plugin-transform-unicode-regex",
+    ],
     moduleIds: true,
 
     getModuleId(name) {
