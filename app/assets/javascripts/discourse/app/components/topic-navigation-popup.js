dc78efaef1a213b9f3758a2ee930780577c92700
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-01-13 22:22:42 +0200

    FEATURE: Store dismissed state of topic nav popups
    The dismissed state will be stored between sessions if noticeId
    attribute is present.

--- app/assets/javascripts/discourse/app/components/topic-navigation-popup.js
+++ app/assets/javascripts/discourse/app/components/topic-navigation-popup.js
@@ -1,12 +1,32 @@
 import Component from "@ember/component";
 import { action } from "@ember/object";
+import discourseComputed from "discourse-common/utils/decorators";
 
 export default Component.extend({
   tagName: "",
   hidden: false,
 
+  didInsertElement() {
+    this._super(...arguments);
+
+    if (this.noticeKey && this.keyValueStore.getItem(this.noticeKey)) {
+      this.set("hidden", true);
+    }
+  },
+
+  @discourseComputed("noticeId")
+  noticeKey(noticeId) {
+    if (noticeId) {
+      return `dismiss_topic_nav_popup_${noticeId}`;
+    }
+  },
+
   @action
   close() {
     this.set("hidden", true);
+
+    if (this.noticeId) {
+      this.keyValueStore.setItem(this.noticeKey, true);
+    }
   },
 });
