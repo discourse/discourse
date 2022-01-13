49f1a60a2783cb594b386fdbe81a2abd060c9628
Martin Brennan <martin@discourse.org>
2022-01-13 10:00:13 +1000

    Revert "DEV: Support for running theme test with Ember CLI (second attempt)"
    This reverts commit 2c7906999a6f651ec25f15dd7a6dd11c5e892d68.
    
    The changes break some things in local development (putting JS files
    into minified files, not allowing debugger, and others)

--- app/assets/javascripts/discourse/tests/theme_qunit_tests_vendor.js
+++ app/assets/javascripts/discourse/tests/theme_qunit_tests_vendor.js
@@ -5,10 +5,11 @@
 //= require fake_xml_http_request
 //= require route-recognizer
 //= require pretender
-//= require sinon
-//= require break_string
-//= require test-shims
-//= require jquery.magnific-popup.min.js
+
+// These are not loaded in prod or development
+// But we need them for testing handlebars templates in qunit
 //= require handlebars
 //= require ember-template-compiler
-//= require markdown-it-bundle
+
+//= require sinon
+//= require break_string
