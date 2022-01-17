31b27b37129d6b764f5941ec996a643e6c4c48ed
Jarek Radosz <jradosz@gmail.com>
2022-01-17 18:32:07 +0100

    FIX: Broken GitHub folder onebox logic (#15612)
    1. `html_doc.css('.Box.md')` always returns a truthy value (e.g. `[]`) so the second branch of the if-elsif never ran
    2. `node&.css('text()')` was invalid code that would raise an error
    3. Matching on h3 elements is no longer correct with the current html structure returned by GitHub

--- spec/lib/onebox/engine/github_folder_onebox_spec.rb
+++ spec/lib/onebox/engine/github_folder_onebox_spec.rb
@@ -41,4 +41,17 @@ describe Onebox::Engine::GithubFolderOnebox do
       expect(@onebox.to_html).to include("<a href=\"https://github.com/discourse/discourse#setting-up-discourse\" target=\"_blank\" rel=\"noopener\">discourse/discourse - Setting up Discourse</a>")
     end
   end
+
+  context 'with rdoc fragments' do
+    before do
+      @link = "https://github.com/ruby/rdoc#description-"
+      @uri = "https://github.com/ruby/rdoc"
+      stub_request(:get, @uri).to_return(status: 200, body: onebox_response("githubfolder-rdoc-root"))
+      @onebox = described_class.new(@link)
+    end
+
+    it "extracts subtitles when linking to docs" do
+      expect(@onebox.to_html).to include("<a href=\"https://github.com/ruby/rdoc#description-\" target=\"_blank\" rel=\"noopener\">GitHub - ruby/rdoc: RDoc produces HTML and online documentation for Ruby... - Description¶ ↑</a>")
+    end
+  end
 end
