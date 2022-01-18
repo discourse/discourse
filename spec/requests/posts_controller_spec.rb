830f472bc30a74dc9990c55650d6ad9331511f7d
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-01-18 16:17:53 +0800

    FIX: Add DB constraints for post & topic counter cache for `UserStat`
    Ensures that `UserStat#post_count` and `UserStat#topic_count` does not
    go below 0. When it does like it did now, we tend to have bugs in our
    code since we're usually coding with the assumption that the count isn't
    negative.
    
    In order to support the constraints, our post and topic fabricators in
    tests will now automatically increment the count for the respective
    user's `UserStat` as well. We have to do this because our fabricators
    bypasss `PostCreator` which holds the responsibility of updating `UserStat#post_count` and
    `UserStat#topic_count`.

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -853,6 +853,8 @@ describe PostsController do
     end
 
     describe "when logged in" do
+      fab!(:user) { Fabricate(:user) }
+
       before do
         sign_in(user)
       end
@@ -898,7 +900,7 @@ describe PostsController do
         end
 
         it "doesn't enqueue posts when user first creates a topic" do
-          user.user_stat.update_column(:topic_count, 1)
+          Fabricate(:topic, user: user)
 
           Draft.set(user, "should_clear", 0, "{'a' : 'b'}")
 
