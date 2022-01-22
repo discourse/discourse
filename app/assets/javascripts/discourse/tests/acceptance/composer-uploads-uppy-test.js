23cbc4583b331d94f6e48d307c590795ee644252
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-01-22 16:51:05 +0100

    FIX: Caret moves to a wrong position when uploading an image via toolbar

--- app/assets/javascripts/discourse/tests/acceptance/composer-uploads-uppy-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/composer-uploads-uppy-test.js
@@ -310,6 +310,30 @@ acceptance("Uppy Composer Attachment - Upload Placeholder", function (needs) {
     const image = createFile("avatar.png");
     appEvents.trigger("composer:add-files", image);
   });
+
+  test("should place cursor properly after inserting a placeholder", async function (assert) {
+    const appEvents = loggedInUser().appEvents;
+    const done = assert.async();
+
+    await visit("/");
+    await click("#create-topic");
+    await fillIn(".d-editor-input", "The image:\ntext after image");
+    const input = query(".d-editor-input");
+    input.selectionStart = 10;
+    input.selectionEnd = 10;
+
+    appEvents.on("composer:all-uploads-complete", () => {
+      // after uploading we have this in the textarea:
+      // "The image:\n![avatar.PNG|690x320](upload://yoj8pf9DdIeHRRULyw7i57GAYdz.jpeg)\ntext after image"
+      // cursor should be just before "text after image":
+      assert.equal(input.selectionStart, 76);
+      assert.equal(input.selectionEnd, 76);
+      done();
+    });
+
+    const image = createFile("avatar.png");
+    appEvents.trigger("composer:add-files", image);
+  });
 });
 
 acceptance("Uppy Composer Attachment - Upload Error", function (needs) {
