3a5b75435ecc240877608d54dfc1490734f2e640
Neil Lalonde <neillalonde@gmail.com>
2022-01-27 10:22:23 -0500

    Version bump to v2.8.0

--- lib/version.rb
+++ lib/version.rb
@@ -10,7 +10,7 @@ module Discourse
       MAJOR = 2
       MINOR = 8
       TINY  = 0
-      PRE   = 'beta11'
+      PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     end
