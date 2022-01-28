138a20abdafc6d22c46fcf4373e91ec06be79c02
1resu <user1@systemausfall.org>
2022-01-28 18:32:29 +0100

    Add possibility to change APP_ROOT by ENV variable
    This changes makes it possible to change the application's root by an environment variable.

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@
 if ENV['RAILS_ENV'] == 'production'
 
   # First, you need to change these below to your situation.
-  APP_ROOT = '/home/discourse/discourse'
+  APP_ROOT = ENV["APP_ROOT"] || '/home/discourse/discourse'
   num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 4
 
   # Second, you can choose how many threads that you are going to run at same time.
