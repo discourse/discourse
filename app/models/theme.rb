d33abf2c6856176950118ffdcd43684b995c25f2
Ayke Halder <rr-it@users.noreply.github.com>
2022-01-30 00:43:07 +0100

    [!!!] add defer-attribute to javascript from themes
    Important: clear theme cache to apply - e.g. via rails console:
    ThemeField.force_recompilation!
    Theme.expire_site_cache!
    Stylesheet::Manager.cache.clear
    
    @see: https://github.com/discourse/discourse/blob/e2c415457c6527680129475c07458401a54c08e0/lib/backup_restore/restorer.rb#L170-L174

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -394,7 +394,7 @@ class Theme < ActiveRecord::Base
       end
       caches = JavascriptCache.where(theme_id: theme_ids)
       caches = caches.sort_by { |cache| theme_ids.index(cache.theme_id) }
-      return caches.map { |c| "<script src='#{c.url}' data-theme-id='#{c.theme_id}'></script>" }.join("\n")
+      return caches.map { |c| "<script defer src='#{c.url}' data-theme-id='#{c.theme_id}'></script>" }.join("\n")
     end
     list_baked_fields(theme_ids, target, name).map { |f| f.value_baked || f.value }.join("\n")
   end
