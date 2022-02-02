bb015636822bebf7f4d29f50cbc2685f0894ea34
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2022-02-02 16:22:52 +1100

    FIX: post mover validation color and message (#15688)
    When the record is not saved, we should display a proper message.
    One potential reason can be plugins for example discourse-calendar is specifying that only first post can contain event

--- spec/requests/topics_controller_spec.rb
+++ spec/requests/topics_controller_spec.rb
@@ -326,6 +326,21 @@ RSpec.describe TopicsController do
           expect(result['success']).to eq(false)
           expect(result['url']).to be_blank
         end
+
+        it 'returns plugin validation error' do
+          # stub here is to simulate validation added by plugin which would be triggered when post is moved
+          PostCreator.any_instance.stubs(:skip_validations?).returns(false)
+
+          p1.update_columns(raw: "i", cooked: "")
+          post "/t/#{topic.id}/move-posts.json", params: {
+            post_ids: [p1.id],
+            destination_topic_id: dest_topic.id
+          }
+
+          expect(response.status).to eq(422)
+          result = response.parsed_body
+          expect(result['errors']).to eq(["Body is too short (minimum is 5 characters) and Body seems unclear, is it a complete sentence?"])
+        end
       end
     end
 
