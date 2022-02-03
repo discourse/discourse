81c4491391c48997fed2f93e0950751450d2a148
Jordan Vidrine <jordan@jordanvidrine.com>
2022-02-03 12:42:16 -0600

    DEV: Add category-color variable

--- app/assets/javascripts/discourse/app/helpers/border-color.js
+++ app/assets/javascripts/discourse/app/helpers/border-color.js
@@ -1,3 +1,3 @@
 import { htmlHelper } from "discourse-common/lib/helpers";
 
-export default htmlHelper((color) => `border-color: #${color}`);
+export default htmlHelper((color) => `border-color: #${color}; `);
