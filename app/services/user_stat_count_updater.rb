b8b285ac35eba3f7fc54daec51309c0f0560f6d1
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-02-09 20:57:15 +0800

    FIX: Avoid errors when updating post and topic count user stats.
    In ab5361d69ae4b55a7635676eef7d80dd96d8460c, we rescue from the PG error
    but the transaction is already aborted causing any DB query after to
    fail. As such, we avoid triggering the error in the first place by
    checking that we would not be insertin a negative number into the
    counter cache.
    
    Follow-up to ab5361d69ae4b55a7635676eef7d80dd96d8460c

--- app/services/user_stat_count_updater.rb
+++ app/services/user_stat_count_updater.rb
@@ -17,23 +17,24 @@ class UserStatCountUpdater
       return if post.topic.private_message?
       stat = user_stat || post.user.user_stat
 
-      if post.is_first_post?
-        stat.public_send(action, :topic_count)
-      elsif post.post_type == Post.types[:regular]
-        stat.public_send(action, :post_count)
-      end
-    rescue ActiveRecord::StatementInvalid => e
-      if e.cause.is_a?(PG::CheckViolation)
+      column =
+        if post.is_first_post?
+          :topic_count
+        elsif post.post_type == Post.types[:regular]
+          :post_count
+        end
+
+      return if column.blank?
+
+      if action == :decrement! && stat.public_send(column) < 1
         # There are still spots in the code base which results in the counter cache going out of sync. However,
-        # we have a job that runs on a daily basis which will correct the count. Therefore, avoid raising an error for now
-        # and log the exception instead.
-        Discourse.warn_exception(
-          e,
-          message: "Attempted to insert negative count into UserStat#post_count or UserStat#topic_count"
-        )
-      else
-        raise
+        # we have a job that runs on a daily basis which will correct the count. Therefore, we always check that we
+        # wouldn't end up with a negative count first before inserting.
+        Rails.logger.warn("Attempted to insert negative count into UserStat##{column}\n#{caller.join('\n')}")
+        return
       end
+
+      stat.public_send(action, column)
     end
   end
 end
