ab5361d69ae4b55a7635676eef7d80dd96d8460c
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-02-09 12:51:13 +0800

    FIX: Avoid raising error when updating post and topic count user stats. (#15873)
    There are still spots in the code base which results in us trying to turn the post and topic count negative. However,
    we have a job that runs on a daily basis which will correct the count. Therefore, avoid raising an error for now
    and log the exception instead.

--- app/services/user_stat_count_updater.rb
+++ app/services/user_stat_count_updater.rb
@@ -22,6 +22,18 @@ class UserStatCountUpdater
       elsif post.post_type == Post.types[:regular]
         stat.public_send(action, :post_count)
       end
+    rescue ActiveRecord::StatementInvalid => e
+      if e.cause.is_a?(PG::CheckViolation)
+        # There are still spots in the code base which results in the counter cache going out of sync. However,
+        # we have a job that runs on a daily basis which will correct the count. Therefore, avoid raising an error for now
+        # and log the exception instead.
+        Discourse.warn_exception(
+          e,
+          message: "Attempted to insert negative count into UserStat#post_count or UserStat#topic_count"
+        )
+      else
+        raise
+      end
     end
   end
 end
