075762ee9f1248219580ce76d099d173de0776c5
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-02-11 01:14:31 +0300

    Add documentation for the auth manager

--- lib/second_factor/auth_manager.rb
+++ lib/second_factor/auth_manager.rb
@@ -1,5 +1,91 @@
 # frozen_string_literal: true
 
+=begin
+This class is responsible for managing any actions that require second factor
+authentication before a user is allowed to perform them. Such actions include
+granting another user admin access, changing password and signing in. In a more
+technical sense, an action is the logic encapsulated in a Rails controller
+action without the logic related to 2fa enforcement/handling.
+
+When a user attempts to perform a 2fa-protected action, there are 3 possible
+outcomes:
+
+1. the user doesn't have any suitable 2fa methods enabled, so they should be
+allowed to perform the action right away.
+
+2. the user has a suitable 2fa method enabled, in which case there are 2
+possibilities:
+  a. the user hasn't done 2fa for the action so they should be redirected to
+  the 2fa page and complete the 2fa before they are allowed to proceed.
+  b. the user has done 2fa for the action so they should be allowed to perform
+  the action.
+
+This class, the auth manager, contains the logic for deciding which outcome
+should happen and performing it.
+
+To use the auth manager for requiring 2fa for an action, it needs to be invoked
+from the controller action using the `run_second_factor!` method which is
+available in all controllers. This method takes a single argument which is a
+class that inherits from the `SecondFactor::Actions::Base` class and implements
+the following methods:
+
+1. no_second_factors_enabled!(params):
+  This method corresponds to outcome (1) above, i.e. it's called when the user
+  performing the action has no suitable 2fa method enabled. It receives the
+  request params of the controller action. Return value is insignificant.
+
+2. second_factor_auth_required!(params):
+  This method corresponds to outcome (2a) above. It also receives the request
+  params of the controller action. The purpose of this method is to keep track
+  of the params that are needed to perform the action and where they should be
+  redirected after the user completes the 2fa.
+
+  To communicate this information to the auth manager, the return value of this
+  method is utilized for this purpose. This method must return a Hash that
+  should have 2 keys:
+
+  :callback_params => another Hash containing the params that are needed to
+  finish the action once 2fa is completed. Everything in this Hash must be
+  serializable to JSON.
+
+  :redirect_path => relative subfolder-aware path that the user should be
+  redirected to after the action is finished. When this key is omitted, the
+  redirect path is set to the homepage (/).
+
+  After this method is called, the auth manager will send a 403 response with a
+  JSON body. It does that by raising an exception that's then rescued by a
+  `rescue_from` handler. The JSON response contains a challenge nonce which the
+  client/frontend will need to complete the 2fa. More on this later.
+
+3. second_factor_auth_completed!(callback_params):
+  This method corresponds to outcome (2b) above. It's called after the user has
+  successfully completed the 2fa for the 2fa-protected action and the purpose
+  of this method is to actually perform that action.
+
+  The `callback_params` param of this method is the `callback_params` Hash from
+  the return value of the previous method.
+
+If there are permission/security checks that the current user must pass in
+order to perform the 2fa-protected action, it's important to run the checks in
+all of the 3 methods of the action class and raise errors if the user doesn't
+pass the checks.
+
+Rendering a response to the client in the outcomes (1) and (2b) is a task for
+the controller action. The return value of the `run_second_factor!` method,
+which is an instance of `SecondFactor::AuthManagerResult`, can be used to know
+which outcome the auth manager has picked and render a different response based
+on the outcome.
+
+For a real example where the auth manager is used, please refer to:
+
+* `SecondFactor::Actions::GrantAdmin` action class. This is a class that
+  inherits `SecondFactor::Actions::Base` and implements the 3 methods mentioned
+  above.
+
+* `Admin::UsersController#grant_admin` controller action.
+
+=end
+
 class SecondFactor::AuthManager
   MAX_CHALLENGE_AGE = 5.minutes
 
