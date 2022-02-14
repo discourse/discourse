ee050e5063213eda195ffd4f9331e700bf1cbbf9
janzenisaac <50783505+janzenisaac@users.noreply.github.com>
2022-02-14 11:25:41 -0600

    DEV: Make user card non-clickable for anon users: (#15938)
    Make name / avatar non-clickable for anon users

--- app/assets/javascripts/discourse/app/widgets/post.js
+++ app/assets/javascripts/discourse/app/widgets/post.js
@@ -173,6 +173,8 @@ createWidget("post-avatar", {
 
   html(attrs) {
     let body;
+    let hideFromAnonUser =
+      this.siteSettings.hide_user_profiles_from_public && !this.currentUser;
     if (!attrs.user_id) {
       body = iconNode("far-trash-alt", { class: "deleted-user-avatar" });
     } else {
@@ -181,7 +183,7 @@ createWidget("post-avatar", {
         username: attrs.username,
         name: attrs.name,
         url: attrs.usernameUrl,
-        className: "main-avatar",
+        className: `main-avatar ${hideFromAnonUser ? "non-clickable" : ""}`,
         hideTitle: true,
       });
     }
