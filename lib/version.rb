cb9678ed3429c23a34743b2bd759edf1f08be2bd
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2022-02-14 17:17:31 +1100

    Version bump to v2.8.1

--- lib/version.rb
+++ lib/version.rb
@@ -9,7 +9,7 @@ module Discourse
     module VERSION #:nodoc:
       MAJOR = 2
       MINOR = 8
-      TINY  = 0
+      TINY  = 1
       PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
