23d98361a3ad459bc4c90b12a8a206cf13f8347a
David Taylor <david@taylorhq.com>
2022-02-14 23:42:05 +0000

    DEV: Allow access to ember-computed-decorators under ember-cli
    This was deprecated in Discourse 2.4, but no end version was put on the deprecation. Many plugins/themes are still using it. This commit restores it under ember-cli so that it does not block the Ember CLI rollout, and can be removed in a future commit.

--- app/assets/javascripts/discourse/public/assets/scripts/module-shims.js
+++ app/assets/javascripts/discourse/public/assets/scripts/module-shims.js
@@ -7,3 +7,14 @@ define("htmlbars-inline-precompile", ["exports"], function (exports) {
     return Ember.Handlebars.compile(strings[0]);
   };
 });
+
+define("ember-addons/ember-computed-decorators", [
+  "discourse-common/utils/decorators",
+  "discourse-common/lib/deprecated",
+], function (decorators, deprecated) {
+  deprecated.default(
+    "ember-addons/ember-computed-decorators is deprecated. Use discourse-common/utils/decorators instead.",
+    { since: "v2.4", dropFrom: "v3.0" }
+  );
+  return decorators;
+});
