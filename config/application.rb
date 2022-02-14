9d4cd1ddb7b53936ebf89df1ec911e856e270d7a
David Taylor <david@taylorhq.com>
2022-02-14 19:52:13 +0000

    DEV: Prefix deprecation notices with plugin name
    To make this possible in development mode, the `sourceURL=` implementation needs to include something plugin-specific. This has no effect on production.
    
    The asset version is bumped in order to trigger a re-compilation of plugin JS assets.

--- config/application.rb
+++ config/application.rb
@@ -241,7 +241,7 @@ module Discourse
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.2.4'
+    config.assets.version = '1.2.5'
 
     # see: http://stackoverflow.com/questions/11894180/how-does-one-correctly-add-custom-sql-dml-in-migrations/11894420#11894420
     config.active_record.schema_format = :sql
