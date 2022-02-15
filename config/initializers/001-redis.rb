d83da596be54a211019d60a99107a01272505843
Rafael dos Santos Silva <xfalcox@gmail.com>
2022-02-15 12:25:22 -0300

    FIX: Redis may not be availiable on Redis initializer (#15955)

--- config/initializers/001-redis.rb
+++ config/initializers/001-redis.rb
@@ -9,7 +9,11 @@ end
 # upgrade to Sidekiq 6.1
 Redis.exists_returns_integer = true
 
-if Gem::Version.new(Discourse.redis.info['redis_version']) < Gem::Version.new("6.2.0")
-  STDERR.puts "Discourse requires Redis 6.2.0 or up"
-  exit 1
+begin
+  if Gem::Version.new(Discourse.redis.info['redis_version']) < Gem::Version.new("6.2.0")
+    STDERR.puts "Discourse requires Redis 6.2.0 or up"
+    exit 1
+  end
+rescue Redis::CannotConnectError
+  STDERR.puts "Couldn't connect to Redis"
 end
