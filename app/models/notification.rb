573935cd6dcb02a0592846ecc5a6bde7e4d3b0d6
Martin Brennan <martin@discourse.org>
2022-02-16 13:26:55 +1000

    DEV: Add chat_quoted notification type
    This is needed for the notification sent when quoting
    chat messages inside a post.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -109,7 +109,8 @@ class Notification < ActiveRecord::Base
                         chat_mention: 29,
                         chat_message: 30,
                         chat_invitation: 31,
-                        chat_group_mention: 32
+                        chat_group_mention: 32,
+                        chat_quoted: 33
                        )
   end
 
