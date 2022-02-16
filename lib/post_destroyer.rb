f9ec2b90a097d4fe6164ad5398b6f0e709f15db8
Martin Brennan <martin@discourse.org>
2022-02-16 11:49:11 +1000

    DEV: Drop user_stats count column constraints (#15949)
    We added this constraint in 5bd55acf835e79ba8f8b655c684035c3f2293652
    but it is causing problems in hosted sites and is catching the
    issue too far down the line. This commit removes the constraint
    for now, and also fixes an issue found with PostDestroyer
    which wasn't using the UserStatCountUpdater when updating post_count
    and thus was causing negative numbers to occur.

--- lib/post_destroyer.rb
+++ lib/post_destroyer.rb
@@ -5,7 +5,6 @@
 # this class contains the logic to delete it.
 #
 class PostDestroyer
-
   def self.destroy_old_hidden_posts
     Post.where(deleted_at: nil, hidden: true)
       .where("hidden_at < ?", 30.days.ago)
@@ -132,12 +131,7 @@ class PostDestroyer
 
       if @post.is_first_post?
         # Update stats of all people who replied
-        counts = Post.where(post_type: Post.types[:regular], topic_id: @post.topic_id).where('post_number > 1').group(:user_id).count
-        counts.each do |user_id, count|
-          if user_stat = UserStat.where(user_id: user_id).first
-            user_stat.update(post_count: user_stat.post_count + count)
-          end
-        end
+        update_post_counts(:increment)
       end
     end
 
@@ -400,13 +394,7 @@ class PostDestroyer
 
     if @post.is_first_post? && @post.topic && !@post.topic.private_message?
       # Update stats of all people who replied
-      counts = Post.where(post_type: Post.types[:regular], topic_id: @post.topic_id).where('post_number > 1').group(:user_id).count
-
-      counts.each do |user_id, count|
-        if user_stat = UserStat.where(user_id: user_id).first
-          user_stat.update(post_count: user_stat.post_count - count)
-        end
-      end
+      update_post_counts(:decrement)
     end
   end
 
@@ -417,4 +405,19 @@ class PostDestroyer
     incoming.update(imap_sync: sync)
   end
 
+  def update_post_counts(operator)
+    counts = Post.where(
+      post_type: Post.types[:regular], topic_id: @post.topic_id
+    ).where('post_number > 1').group(:user_id).count
+
+    counts.each do |user_id, count|
+      if user_stat = UserStat.where(user_id: user_id).first
+        if operator == :decrement
+          UserStatCountUpdater.set!(user_stat: user_stat, count: user_stat.post_count - count, count_column: :post_count)
+        else
+          UserStatCountUpdater.set!(user_stat: user_stat, count: user_stat.post_count + count, count_column: :post_count)
+        end
+      end
+    end
+  end
 end
