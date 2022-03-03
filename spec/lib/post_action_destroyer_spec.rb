d8b2a821f52f81d734322f91f2333e2ea089bf89
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-03-03 09:30:39 +0100

    reorg spec

--- spec/lib/post_action_destroyer_spec.rb
+++ spec/lib/post_action_destroyer_spec.rb
@@ -5,13 +5,13 @@ describe PostActionDestroyer do
   fab!(:user) { Fabricate(:user) }
   fab!(:post) { Fabricate(:post) }
 
-  context 'like' do
-    context 'post action exists' do
-      before do
-        PostActionCreator.new(user, post, PostActionType.types[:like]).perform
-      end
+  describe '#perform' do
+    context 'like' do
+      context 'post action exists' do
+        before do
+          PostActionCreator.new(user, post, PostActionType.types[:like]).perform
+        end
 
-      describe 'perform' do
         it 'destroys the post action' do
           expect {
             PostActionDestroyer.destroy(user, post, :like)
@@ -30,25 +30,23 @@ describe PostActionDestroyer do
           expect(message[:likes_count]).to eq(0)
         end
       end
-    end
 
-    context 'post action doesn’t exist' do
-      describe 'perform' do
-        it 'fails' do
-          result = PostActionDestroyer.destroy(user, post, :like)
-          expect(result.success).to eq(false)
-          expect(result.not_found).to eq(true)
+      context 'post action doesn’t exist' do
+        describe 'perform' do
+          it 'fails' do
+            result = PostActionDestroyer.destroy(user, post, :like)
+            expect(result.success).to eq(false)
+            expect(result.not_found).to eq(true)
+          end
         end
       end
     end
-  end
 
-  context 'any other notifiable type' do
-    before do
-      PostActionCreator.new(user, post, PostActionType.types[:spam]).perform
-    end
+    context 'any other notifiable type' do
+      before do
+        PostActionCreator.new(user, post, PostActionType.types[:spam]).perform
+      end
 
-    describe 'perform' do
       it 'destroys the post action' do
         expect {
           PostActionDestroyer.destroy(user, post, :spam)
@@ -63,14 +61,12 @@ describe PostActionDestroyer do
         expect(messages.last.data[:type]).to eq(:acted)
       end
     end
-  end
 
-  context 'not notifyable type' do
-    before do
-      PostActionCreator.new(user, post, PostActionType.types[:bookmark]).perform
-    end
+    context 'not notifyable type' do
+      before do
+        PostActionCreator.new(user, post, PostActionType.types[:bookmark]).perform
+      end
 
-    describe 'perform' do
       it 'destroys the post action' do
         expect {
           PostActionDestroyer.destroy(user, post, :bookmark)
