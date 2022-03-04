cac98d2b44c2ea1c84916e6ad44859defcf67571
Blake Erickson <o.blakeerickson@gmail.com>
2022-03-04 10:12:35 -0700

    DEV: Add api scoping for updating topics.
    This change adds a new api scope for updating topics.
    
    See: https://meta.discourse.org/t/219805

--- app/models/api_key_scope.rb
+++ app/models/api_key_scope.rb
@@ -22,6 +22,7 @@ class ApiKeyScope < ActiveRecord::Base
         },
         topics: {
           write: { actions: %w[posts#create], params: %i[topic_id] },
+          update: { actions: %w[topics#update], params: %i[topic_id] },
           read: {
             actions: %w[topics#show topics#feed topics#posts],
             params: %i[topic_id], aliases: { topic_id: :id }
