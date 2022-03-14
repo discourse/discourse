discourse/discourse

commit 5bd55acf835e79ba8f8b655c684035c3f2293652

FIX: Add DB constraints for post & topic counter cache for `UserStat` (#15626)
Ensures that `UserStat#post_count` and `UserStat#topic_count` does not
go below 0. When it does like it did now, we tend to have bugs in our
code since we're usually coding with the assumption that the count isn't
negative.

In order to support the constraints, our post and topic fabricators in
tests will now automatically increment the count for the respective
user's `UserStat` as well. We have to do this because our fabricators
bypasss `PostCreator` which holds the responsibility of updating `UserStat#post_count` and
`UserStat#topic_count`.

app/models/topic_converter.rb
@@ -63,7 +63,7 @@ class TopicConverter
   private
 
   def posters
-    @posters ||= @topic.posts.distinct.pluck(:user_id).to_a
+    @posters ||= @topic.posts.where("post_number > 1").distinct.pluck(:user_id)
   end
 
   def update_user_stats

spec/models/topic_converter_spec.rb
@@ -68,7 +68,7 @@ describe TopicConverter do
         expect(private_message.user.user_stat.post_count).to eq(0)
         private_message.convert_to_public_topic(admin)
         expect(private_message.reload.user.user_stat.topic_count).to eq(1)
-        expect(private_message.user.user_stat.post_count).to eq(1)
+        expect(private_message.user.user_stat.post_count).to eq(0)
         expect(topic_user.reload.notification_level).to eq(TopicUser.notification_levels[:watching])
       end
 

app/services/user_stat_count_updater.rb
@@ -1,0 +1,27 @@
+# frozen_string_literal: true
+
+class UserStatCountUpdater
+  class << self
+    def increment!(post, user_stat: nil)
+      update!(post, user_stat: user_stat)
+    end
+
+    def decrement!(post, user_stat: nil)
+      update!(post, user_stat: user_stat, action: :decrement!)
+    end
+
+    private
+
+    def update!(post, user_stat: nil, action: :increment!)
+      return if !post.topic
+      return if post.topic.private_message?
+      stat = user_stat || post.user.user_stat
+
+      if post.is_first_post?
+        stat.public_send(action, :topic_count)
+      elsif post.post_type == Post.types[:regular]
+        stat.public_send(action, :post_count)
+      end
+    end
+  end
+end

db/migrate/20220118065658_add_constraints_to_user_stat.rb
@@ -1,0 +1,25 @@
+# frozen_string_literal: true
+
+class AddConstraintsToUserStat < ActiveRecord::Migration[6.1]
+  def up
+    execute(<<~SQL)
+    UPDATE user_stats
+    SET post_count = 0
+    WHERE post_count < 0
+    SQL
+
+    execute(<<~SQL)
+    UPDATE user_stats
+    SET topic_count = 0
+    WHERE topic_count < 0
+    SQL
+
+    execute "ALTER TABLE user_stats ADD CONSTRAINT topic_count_positive CHECK (topic_count >= 0)"
+    execute "ALTER TABLE user_stats ADD CONSTRAINT post_count_positive CHECK (post_count >= 0)"
+  end
+
+  def down
+    execute "ALTER TABLE user_stats DROP CONSTRAINT topic_count_positive"
+    execute "ALTER TABLE user_stats DROP CONSTRAINT post_count_positive"
+  end
+end

lib/guardian/user_guardian.rb
@@ -60,6 +60,7 @@ module UserGuardian
 
   def can_delete_user?(user)
     return false if user.nil? || user.admin?
+
     if is_me?(user)
       !SiteSetting.enable_discourse_connect &&
       !user.has_more_posts_than?(SiteSetting.delete_user_self_max_post_count)

lib/post_creator.rb
@@ -599,15 +599,10 @@ class PostCreator
     @user.create_user_stat if @user.user_stat.nil?
 
     if @user.user_stat.first_post_created_at.nil?
-      @user.user_stat.first_post_created_at = @post.created_at
+      @user.user_stat.update!(first_post_created_at: @post.created_at)
     end
 
-    unless @post.topic.private_message?
-      @user.user_stat.post_count += 1 if @post.post_type == Post.types[:regular] && !@post.is_first_post?
-      @user.user_stat.topic_count += 1 if @post.is_first_post?
-    end
-
-    @user.user_stat.save!
+    UserStatCountUpdater.increment!(@post)
 
     if !@topic.private_message? && @post.post_type != Post.types[:whisper]
       @user.update(last_posted_at: @post.created_at)

lib/post_destroyer.rb
@@ -155,8 +155,9 @@ class PostDestroyer
         make_previous_post_the_last_one
         mark_topic_changed
         clear_user_posted_flag
-        Topic.reset_highest(@post.topic_id)
       end
+
+      Topic.reset_highest(@post.topic_id)
       trash_public_post_actions
       trash_revisions
       trash_user_actions
@@ -177,7 +178,7 @@ class PostDestroyer
       end
       TopicLink.where(link_post_id: @post.id).destroy_all
       update_associated_category_latest_topic
-      update_user_counts
+      update_user_counts if !permanent?
       TopicUser.update_post_action_cache(post_id: @post.id)
 
       DB.after_commit do
@@ -387,17 +388,10 @@ class PostDestroyer
     author.create_user_stat if author.user_stat.nil?
 
     if @post.created_at == author.user_stat.first_post_created_at
-      author.user_stat.first_post_created_at = author.posts.order('created_at ASC').first.try(:created_at)
+      author.user_stat.update!(first_post_created_at: author.posts.order('created_at ASC').first.try(:created_at))
     end
 
-    if @post.topic && !@post.topic.private_message?
-      if @post.post_type == Post.types[:regular] && !@post.is_first_post? && !@topic.nil?
-        author.user_stat.post_count -= 1
-      end
-      author.user_stat.topic_count -= 1 if @post.is_first_post?
-    end
-
-    author.user_stat.save!
+    UserStatCountUpdater.decrement!(@post)
 
     if @post.created_at == author.last_posted_at
       author.last_posted_at = author.posts.order('created_at DESC').first.try(:created_at)
@@ -407,6 +401,7 @@ class PostDestroyer
     if @post.is_first_post? && @post.topic && !@post.topic.private_message?
       # Update stats of all people who replied
       counts = Post.where(post_type: Post.types[:regular], topic_id: @post.topic_id).where('post_number > 1').group(:user_id).count
+
       counts.each do |user_id, count|
         if user_stat = UserStat.where(user_id: user_id).first
           user_stat.update(post_count: user_stat.post_count - count)

lib/post_revisor.rb
@@ -437,22 +437,19 @@ class PostRevisor
       private_message = @topic.private_message?
 
       prev_owner_user_stat = prev_owner.user_stat
+
       unless private_message
-        prev_owner_user_stat.post_count -= 1 if @post.post_type == Post.types[:regular]
-        prev_owner_user_stat.topic_count -= 1 if @post.is_first_post?
+        UserStatCountUpdater.decrement!(@post, user_stat: prev_owner_user_stat) if !@post.trashed?
         prev_owner_user_stat.likes_received -= likes
       end
 
       if @post.created_at == prev_owner.user_stat.first_post_created_at
-        prev_owner_user_stat.first_post_created_at = prev_owner.posts.order('created_at ASC').first.try(:created_at)
+        prev_owner_user_stat.update!(first_post_created_at: prev_owner.posts.order('created_at ASC').first.try(:created_at))
       end
-
-      prev_owner_user_stat.save!
 
       new_owner_user_stat = new_owner.user_stat
       unless private_message
-        new_owner_user_stat.post_count += 1 if @post.post_type == Post.types[:regular]
-        new_owner_user_stat.topic_count += 1 if @post.is_first_post?
+        UserStatCountUpdater.increment!(@post, user_stat: new_owner_user_stat) if !@post.trashed?
         new_owner_user_stat.likes_received += likes
       end
       new_owner_user_stat.save!

commit ddfaa767ce60fa4c977ecee848decd83f60b94f3

FIX: Always include the first poster when converting to PM
Regressed in #15626

app/models/topic_converter.rb
@@ -63,7 +63,7 @@ class TopicConverter
   private
 
   def posters
-    @posters ||= @topic.posts.where("post_number > 1").distinct.pluck(:user_id)
+    @posters ||= @topic.posts.distinct.pluck(:user_id)
   end
 
   def increment_users_post_count

spec/models/topic_converter_spec.rb
@@ -200,6 +200,11 @@ describe TopicConverter do
         expect(private_message.topic_allowed_users.count).to eq(1)
       end
 
+      it "includes the poster of a single-post topic" do
+        moderator = Fabricate(:moderator)
+        private_message = topic.convert_to_private_message(moderator)
+        expect(private_message.allowed_users).to match_array([topic.user, moderator])
+      end
     end
 
     context 'topic has replies' do