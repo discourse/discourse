733bfd1d47699ee30f7558b206e9d6c4c6b0b28f
David Taylor <david@taylorhq.com>
2022-03-17 17:31:11 +0000

    DEV: Refactor tag-show route
    Previously we were loading almost all the data in an afterModel hook, storing it temporarily in route properties, and then passing it to the controller in `setupController`.
    
    This does not follow Ember best-practices, and causes a number of unexpected behaviors. For example, Ember only calls `setupController` **when the model value changes**. Since `model()` was only returning the tag, that meant that category changes and `additionalTag` changes wouldn't always trigger a `setupController` call, and things would get into a very weird state. This is visible when using the 'loading-slider' component because the category navigation dropdown gets 'stuck' when switching categories.
    
    This commit moves all the data-fetching into `model()`. To make things cleaner, it also:
    - removes most uses of route-level variables
    - introduces async/await in the model() function
    - removes some unneeded `get()` usage
    - re-uses DiscoverySortableController for queryParam default handling
    - Removes override of `renderTemplate()` so that queryParams are correctly passed through to the controller
    - Removes some `transitionToRoute` hacks which were working around the queryParams issue

--- app/assets/javascripts/discourse/app/controllers/tag-show.js
+++ app/assets/javascripts/discourse/app/controllers/tag-show.js
@@ -1,226 +1,198 @@
-import Controller, { inject as controller } from "@ember/controller";
+import DiscoverySortableController from "discourse/controllers/discovery-sortable";
+import { inject as controller } from "@ember/controller";
 import discourseComputed, { observes } from "discourse-common/utils/decorators";
 import BulkTopicSelection from "discourse/mixins/bulk-topic-selection";
 import FilterModeMixin from "discourse/mixins/filter-mode";
 import I18n from "I18n";
 import NavItem from "discourse/models/nav-item";
 import Topic from "discourse/models/topic";
-import { alias } from "@ember/object/computed";
+import { readOnly } from "@ember/object/computed";
 import bootbox from "bootbox";
-import { queryParams } from "discourse/controllers/discovery-sortable";
 import { endWith } from "discourse/lib/computed";
 
-export default Controller.extend(BulkTopicSelection, FilterModeMixin, {
-  application: controller(),
+export default DiscoverySortableController.extend(
+  BulkTopicSelection,
+  FilterModeMixin,
+  {
+    application: controller(),
 
-  tag: null,
-  additionalTags: null,
-  list: null,
-  canAdminTag: alias("currentUser.staff"),
-  navMode: "latest",
-  loading: false,
-  canCreateTopic: false,
-  order: "default",
-  ascending: false,
-  status: null,
-  state: null,
-  search: null,
-  max_posts: null,
-  q: null,
-  showInfo: false,
-  top: endWith("list.filter", "top"),
-  period: alias("list.for_period"),
+    tag: null,
+    additionalTags: null,
+    list: null,
+    canAdminTag: readOnly("currentUser.staff"),
+    navMode: "latest",
+    loading: false,
+    canCreateTopic: false,
+    showInfo: false,
+    top: endWith("list.filter", "top"),
 
-  @discourseComputed(
-    "canCreateTopic",
-    "category",
-    "canCreateTopicOnCategory",
-    "tag",
-    "canCreateTopicOnTag"
-  )
-  createTopicDisabled(
-    canCreateTopic,
-    category,
-    canCreateTopicOnCategory,
-    tag,
-    canCreateTopicOnTag
-  ) {
-    return (
-      !canCreateTopic ||
-      (category && !canCreateTopicOnCategory) ||
-      (tag && !canCreateTopicOnTag)
-    );
-  },
-
-  queryParams: Object.keys(queryParams),
-
-  @discourseComputed("category", "tag.id", "filterType", "noSubcategories")
-  navItems(category, tagId, filterType, noSubcategories) {
-    return NavItem.buildList(category, {
-      tagId,
-      filterType,
-      noSubcategories,
-      siteSettings: this.siteSettings,
-    });
-  },
-
-  @discourseComputed("category")
-  showTagFilter() {
-    return true;
-  },
-
-  @observes("list.canLoadMore")
-  _showFooter() {
-    this.set("application.showFooter", !this.get("list.canLoadMore"));
-  },
-
-  @discourseComputed("navMode", "list.topics.length", "loading")
-  footerMessage(navMode, listTopicsLength, loading) {
-    if (loading) {
-      return;
-    }
-
-    if (listTopicsLength === 0) {
-      return I18n.t(`tagging.topics.none.${navMode}`, {
-        tag: this.get("tag.id"),
-      });
-    } else {
-      return I18n.t(`topics.bottom.tag`, {
-        tag: this.get("tag.id"),
-      });
-    }
-  },
-
-  @discourseComputed("list.filter", "list.topics.length")
-  showDismissRead(filter, topicsLength) {
-    return this._isFilterPage(filter, "unread") && topicsLength > 0;
-  },
-
-  @discourseComputed("list.filter", "list.topics.length")
-  showResetNew(filter, topicsLength) {
-    return this._isFilterPage(filter, "new") && topicsLength > 0;
-  },
-
-  actions: {
-    resetNew() {
-      const tracked =
-        (this.router.currentRoute.queryParams["f"] ||
-          this.router.currentRoute.queryParams["filter"]) === "tracked";
-
-      let topicIds = this.selected
-        ? this.selected.map((topic) => topic.id)
-        : null;
-
-      Topic.resetNew(this.category, !this.noSubcategories, {
-        tracked,
-        tag: this.tag,
-        topicIds,
-      }).then(() =>
-        this.send(
-          "refresh",
-          tracked ? { skipResettingParams: ["filter", "f"] } : {}
-        )
+    @discourseComputed(
+      "canCreateTopic",
+      "category",
+      "canCreateTopicOnCategory",
+      "tag",
+      "canCreateTopicOnTag"
+    )
+    createTopicDisabled(
+      canCreateTopic,
+      category,
+      canCreateTopicOnCategory,
+      tag,
+      canCreateTopicOnTag
+    ) {
+      return (
+        !canCreateTopic ||
+        (category && !canCreateTopicOnCategory) ||
+        (tag && !canCreateTopicOnTag)
       );
     },
 
-    showInserted() {
-      const tracker = this.topicTrackingState;
-      this.list.loadBefore(tracker.get("newIncoming"), true);
-      tracker.resetTracking();
-      return false;
+    @discourseComputed("category", "tag.id", "filterType", "noSubcategories")
+    navItems(category, tagId, filterType, noSubcategories) {
+      return NavItem.buildList(category, {
+        tagId,
+        filterType,
+        noSubcategories,
+        siteSettings: this.siteSettings,
+      });
     },
 
-    changeSort(order) {
-      if (order === this.order) {
-        this.toggleProperty("ascending");
+    @discourseComputed("category")
+    showTagFilter() {
+      return true;
+    },
+
+    @observes("list.canLoadMore")
+    _showFooter() {
+      this.set("application.showFooter", !this.get("list.canLoadMore"));
+    },
+
+    @discourseComputed("navMode", "list.topics.length", "loading")
+    footerMessage(navMode, listTopicsLength, loading) {
+      if (loading) {
+        return;
+      }
+
+      if (listTopicsLength === 0) {
+        return I18n.t(`tagging.topics.none.${navMode}`, {
+          tag: this.get("tag.id"),
+        });
       } else {
-        this.setProperties({ order, ascending: false });
-      }
-
-      let params = { order, ascending: this.ascending };
-      if (this.period) {
-        params.period = this.period;
-      }
-
-      this.transitionToRoute({
-        queryParams: params,
-      });
-    },
-
-    changePeriod(p) {
-      this.set("period", p);
-
-      let params = { period: this.period };
-
-      if (!(this.order === "default" && this.ascending === false)) {
-        params = Object.assign(params, {
-          order: this.order,
-          ascending: this.ascending,
+        return I18n.t(`topics.bottom.tag`, {
+          tag: this.get("tag.id"),
         });
       }
-
-      this.transitionToRoute({
-        queryParams: params,
-      });
     },
 
-    toggleInfo() {
-      this.toggleProperty("showInfo");
+    @discourseComputed("list.filter", "list.topics.length")
+    showDismissRead(filter, topicsLength) {
+      return this._isFilterPage(filter, "unread") && topicsLength > 0;
     },
 
-    refresh() {
-      return this.store
-        .findFiltered("topicList", {
-          filter: this.get("list.filter"),
-        })
-        .then((list) => {
-          this.set("list", list);
-          this.resetSelected();
-        });
+    @discourseComputed("list.filter", "list.topics.length")
+    showResetNew(filter, topicsLength) {
+      return this._isFilterPage(filter, "new") && topicsLength > 0;
     },
 
-    deleteTag(tagInfo) {
-      const numTopics =
-        this.get("list.topic_list.tags.firstObject.topic_count") || 0;
+    actions: {
+      resetNew() {
+        const tracked =
+          (this.router.currentRoute.queryParams["f"] ||
+            this.router.currentRoute.queryParams["filter"]) === "tracked";
 
-      let confirmText =
-        numTopics === 0
-          ? I18n.t("tagging.delete_confirm_no_topics")
-          : I18n.t("tagging.delete_confirm", { count: numTopics });
+        let topicIds = this.selected
+          ? this.selected.map((topic) => topic.id)
+          : null;
 
-      if (tagInfo.synonyms.length > 0) {
-        confirmText +=
-          " " +
-          I18n.t("tagging.delete_confirm_synonyms", {
-            count: tagInfo.synonyms.length,
+        Topic.resetNew(this.category, !this.noSubcategories, {
+          tracked,
+          tag: this.tag,
+          topicIds,
+        }).then(() =>
+          this.send(
+            "refresh",
+            tracked ? { skipResettingParams: ["filter", "f"] } : {}
+          )
+        );
+      },
+
+      showInserted() {
+        const tracker = this.topicTrackingState;
+        this.list.loadBefore(tracker.get("newIncoming"), true);
+        tracker.resetTracking();
+        return false;
+      },
+
+      changeSort(order) {
+        if (order === this.order) {
+          this.toggleProperty("ascending");
+        } else {
+          this.setProperties({ order, ascending: false });
+        }
+      },
+
+      changePeriod(p) {
+        this.set("period", p);
+      },
+
+      toggleInfo() {
+        this.toggleProperty("showInfo");
+      },
+
+      refresh() {
+        return this.store
+          .findFiltered("topicList", {
+            filter: this.get("list.filter"),
+          })
+          .then((list) => {
+            this.set("list", list);
+            this.resetSelected();
           });
-      }
+      },
 
-      bootbox.confirm(confirmText, (result) => {
-        if (!result) {
-          return;
+      deleteTag(tagInfo) {
+        const numTopics =
+          this.get("list.topic_list.tags.firstObject.topic_count") || 0;
+
+        let confirmText =
+          numTopics === 0
+            ? I18n.t("tagging.delete_confirm_no_topics")
+            : I18n.t("tagging.delete_confirm", { count: numTopics });
+
+        if (tagInfo.synonyms.length > 0) {
+          confirmText +=
+            " " +
+            I18n.t("tagging.delete_confirm_synonyms", {
+              count: tagInfo.synonyms.length,
+            });
         }
 
-        this.tag
-          .destroyRecord()
-          .then(() => this.transitionToRoute("tags.index"))
-          .catch(() => bootbox.alert(I18n.t("generic_error")));
-      });
-    },
+        bootbox.confirm(confirmText, (result) => {
+          if (!result) {
+            return;
+          }
 
-    changeTagNotificationLevel(notificationLevel) {
-      this.tagNotification
-        .update({ notification_level: notificationLevel })
-        .then((response) => {
-          this.currentUser.set(
-            "muted_tag_ids",
-            this.currentUser.calculateMutedIds(
-              notificationLevel,
-              response.responseJson.tag_id,
-              "muted_tag_ids"
-            )
-          );
+          this.tag
+            .destroyRecord()
+            .then(() => this.transitionToRoute("tags.index"))
+            .catch(() => bootbox.alert(I18n.t("generic_error")));
         });
+      },
+
+      changeTagNotificationLevel(notificationLevel) {
+        this.tagNotification
+          .update({ notification_level: notificationLevel })
+          .then((response) => {
+            this.currentUser.set(
+              "muted_tag_ids",
+              this.currentUser.calculateMutedIds(
+                notificationLevel,
+                response.responseJson.tag_id,
+                "muted_tag_ids"
+              )
+            );
+          });
+      },
     },
-  },
-});
+  }
+);
