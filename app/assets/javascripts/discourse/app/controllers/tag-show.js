98e9f2708e20c40e0471a9f33f1bba0a5aabb7fd
David Taylor <david@taylorhq.com>
2022-03-18 15:11:58 +0000

    Switch to `@action` decorator

--- app/assets/javascripts/discourse/app/controllers/tag-show.js
+++ app/assets/javascripts/discourse/app/controllers/tag-show.js
@@ -9,6 +9,7 @@ import Topic from "discourse/models/topic";
 import { readOnly } from "@ember/object/computed";
 import bootbox from "bootbox";
 import { endWith } from "discourse/lib/computed";
+import { action } from "@ember/object";
 
 export default DiscoverySortableController.extend(
   BulkTopicSelection,
@@ -89,105 +90,106 @@ export default DiscoverySortableController.extend(
       return this._isFilterPage(filter, "new") && topicsLength > 0;
     },
 
-    actions: {
-      resetNew() {
-        const tracked =
-          (this.router.currentRoute.queryParams["f"] ||
-            this.router.currentRoute.queryParams["filter"]) === "tracked";
+    @action
+    resetNew() {
+      const tracked =
+        (this.router.currentRoute.queryParams["f"] ||
+          this.router.currentRoute.queryParams["filter"]) === "tracked";
 
-        let topicIds = this.selected
-          ? this.selected.mapBy("id")
-          : null;
+      let topicIds = this.selected ? this.selected.mapBy("id") : null;
 
-        Topic.resetNew(this.category, !this.noSubcategories, {
-          tracked,
-          tag: this.tag,
-          topicIds,
-        }).then(() =>
-          this.send(
-            "refresh",
-            tracked ? { skipResettingParams: ["filter", "f"] } : {}
-          )
-        );
-      },
+      Topic.resetNew(this.category, !this.noSubcategories, {
+        tracked,
+        tag: this.tag,
+        topicIds,
+      }).then(() =>
+        this.refresh(tracked ? { skipResettingParams: ["filter", "f"] } : {})
+      );
+    },
 
-      showInserted() {
-        const tracker = this.topicTrackingState;
-        this.list.loadBefore(tracker.newIncoming, true);
-        tracker.resetTracking();
-        return false;
-      },
+    @action
+    showInserted() {
+      const tracker = this.topicTrackingState;
+      this.list.loadBefore(tracker.newIncoming, true);
+      tracker.resetTracking();
+      return false;
+    },
 
-      changeSort(order) {
-        if (order === this.order) {
-          this.toggleProperty("ascending");
-        } else {
-          this.setProperties({ order, ascending: false });
-        }
-      },
+    @action
+    changeSort(order) {
+      if (order === this.order) {
+        this.toggleProperty("ascending");
+      } else {
+        this.setProperties({ order, ascending: false });
+      }
+    },
 
-      changePeriod(p) {
-        this.set("period", p);
-      },
+    @action
+    changePeriod(p) {
+      this.set("period", p);
+    },
 
-      toggleInfo() {
-        this.toggleProperty("showInfo");
-      },
+    @action
+    toggleInfo() {
+      this.toggleProperty("showInfo");
+    },
 
-      refresh() {
-        return this.store
-          .findFiltered("topicList", {
-            filter: this.list?.filter,
-          })
-          .then((list) => {
-            this.set("list", list);
-            this.resetSelected();
-          });
-      },
-
-      deleteTag(tagInfo) {
-        const numTopics =
-          this.get("list.topic_list.tags.firstObject.topic_count") || 0;
-
-        let confirmText =
-          numTopics === 0
-            ? I18n.t("tagging.delete_confirm_no_topics")
-            : I18n.t("tagging.delete_confirm", { count: numTopics });
-
-        if (tagInfo.synonyms.length > 0) {
-          confirmText +=
-            " " +
-            I18n.t("tagging.delete_confirm_synonyms", {
-              count: tagInfo.synonyms.length,
-            });
-        }
-
-        bootbox.confirm(confirmText, (result) => {
-          if (!result) {
-            return;
-          }
-
-          this.tag
-            .destroyRecord()
-            .then(() => this.transitionToRoute("tags.index"))
-            .catch(() => bootbox.alert(I18n.t("generic_error")));
+    @action
+    refresh() {
+      return this.store
+        .findFiltered("topicList", {
+          filter: this.list?.filter,
+        })
+        .then((list) => {
+          this.set("list", list);
+          this.resetSelected();
         });
-      },
+    },
 
-      changeTagNotificationLevel(notificationLevel) {
-        this.tagNotification
-          .update({ notification_level: notificationLevel })
-          .then((response) => {
-            this.currentUser.set(
-              "muted_tag_ids",
-              this.currentUser.calculateMutedIds(
-                notificationLevel,
-                response.responseJson.tag_id,
-                "muted_tag_ids"
-              )
-            );
+    @action
+    deleteTag(tagInfo) {
+      const numTopics =
+        this.get("list.topic_list.tags.firstObject.topic_count") || 0;
+
+      let confirmText =
+        numTopics === 0
+          ? I18n.t("tagging.delete_confirm_no_topics")
+          : I18n.t("tagging.delete_confirm", { count: numTopics });
+
+      if (tagInfo.synonyms.length > 0) {
+        confirmText +=
+          " " +
+          I18n.t("tagging.delete_confirm_synonyms", {
+            count: tagInfo.synonyms.length,
           });
-      },
+      }
+
+      bootbox.confirm(confirmText, (result) => {
+        if (!result) {
+          return;
+        }
+
+        this.tag
+          .destroyRecord()
+          .then(() => this.transitionToRoute("tags.index"))
+          .catch(() => bootbox.alert(I18n.t("generic_error")));
+      });
+    },
+
+    @action
+    changeTagNotificationLevel(notificationLevel) {
+      this.tagNotification
+        .update({ notification_level: notificationLevel })
+        .then((response) => {
+          this.currentUser.set(
+            "muted_tag_ids",
+            this.currentUser.calculateMutedIds(
+              notificationLevel,
+              response.responseJson.tag_id,
+              "muted_tag_ids"
+            )
+          );
+        });
     },
   }
 );
