70126891e8cd94a57a5f728c50aa86f24b8754c9
Loïc Guitaut <loic@discourse.org>
2022-03-23 17:16:56 +0100

    WIP: add more comments to zeitwerk patches/config

--- lib/discourse_inflector.rb
+++ lib/discourse_inflector.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# This custom inflector is needed because of our jobs directory structure.
+# Ideally, we should not prefix our jobs with a `Jobs` namespace but instead
+# have a `Job` suffix to follow the Rails conventions on naming.
 class DiscourseInflector < Zeitwerk::Inflector
   def camelize(basename, abspath)
     return basename.camelize if abspath.ends_with?("onceoff.rb")
--- lib/require_dependency_backward_compatibility.rb
+++ lib/require_dependency_backward_compatibility.rb
@@ -3,6 +3,13 @@
 # Patch `require_dependency` to maintain backward compatibility with some
 # plugins. Calls to `require_dependency` are deprecated and we should remove
 # them whenever possible.
+#
+# Here we do nothing if `jobs/base` is required since all our jobs are
+# autoloaded through Zeitwerk. Requiring explicitly `jobs/base` actually breaks
+# the app with the “new” autoloader.
+# `lib` should not appear in a path that is required but we had probably a bug
+# at some point regarding this matter so we need to maintain compatibility with
+# some plugins that rely on this.
 module RequireDependencyBackwardCompatibility
   def require_dependency(filename)
     name = filename.to_s
