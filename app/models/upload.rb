5ed11b40f921947adcb7c7f030c66a3874b79564
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-03-30 19:02:40 +0300

    FIX: Use upload_references in with_no_non_post_relations

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -88,6 +88,8 @@ class Upload < ActiveRecord::Base
 
   def self.with_no_non_post_relations
     scope = self
+      .joins("LEFT JOIN upload_references ur ON ur.upload_id = uploads.id AND ur.target_type != 'Post'")
+      .where("ur.upload_id IS NULL")
       .joins(<<~SQL)
         LEFT JOIN site_settings ss
         ON NULLIF(ss.value, '')::integer = uploads.id
