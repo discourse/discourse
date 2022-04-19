14eb5bc57081187076b24c4a59552d96ba67cabc
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-04-19 12:46:59 +0200

    REFACTOR: badge-button
    - drops jquery
    - uses native class syntax
    - tagless
    - tests
    - removes unnecessary alias

--- app/assets/javascripts/discourse/app/components/badge-button.js
+++ app/assets/javascripts/discourse/app/components/badge-button.js
@@ -1,20 +1,13 @@
 import Component from "@ember/component";
-import { alias } from "@ember/object/computed";
 import discourseComputed from "discourse-common/utils/decorators";
+import domFromString from "discourse-common/lib/dom-from-string";
 
-export default Component.extend({
-  tagName: "span",
-  classNameBindings: [
-    ":user-badge",
-    "badge.badgeTypeClassName",
-    "badge.enabled::disabled",
-  ],
+export default class BadgeButtonComponent extends Component {
+  tagName = "";
+  badge = null;
 
   @discourseComputed("badge.description")
   title(badgeDescription) {
-    return $("<div>" + badgeDescription + "</div>").text();
-  },
-
-  attributeBindings: ["data-badge-name", "title"],
-  "data-badge-name": alias("badge.name"),
-});
+    return domFromString(`<div>${badgeDescription}</div>`)[0].innerText;
+  }
+}
