8dd3d6cb97872bf8a88689d023decf73145db9fc
Kris <kris.aubuchon@discourse.org>
2022-04-26 15:01:43 -0400

    UX: Larger clickable area for mobile topic list (#16473)

--- app/assets/javascripts/discourse/app/components/topic-list-item.js
+++ app/assets/javascripts/discourse/app/components/topic-list-item.js
@@ -258,6 +258,19 @@ export default Component.extend({
       return this.navigateToTopic(topic, e.target.getAttribute("href"));
     }
 
+    // make full row click target on mobile, due to size constraints
+    if (
+      this.site.mobileView &&
+      (e.target.classList.contains("right") ||
+        e.target.classList.contains("topic-item-stats") ||
+        e.target.classList.contains("main-link"))
+    ) {
+      if (wantsNewWindow(e)) {
+        return true;
+      }
+      return this.navigateToTopic(topic, topic.lastUnreadUrl);
+    }
+
     if (e.target.closest("a.topic-status")) {
       this.topic.togglePinnedForUser();
       return false;
