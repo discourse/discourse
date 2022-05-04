5946f25385598c2355a700f27a6e3f567083a3ae
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-05-04 21:19:05 +0300

    DEV: Copy post uploads again after deploy

--- db/post_migrate/20220309132720_copy_post_uploads_to_upload_references_for_sync.rb
+++ db/post_migrate/20220309132720_copy_post_uploads_to_upload_references_for_sync.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class MovePostUploadsToUploadReferences < ActiveRecord::Migration[6.1]
+class CopyPostUploadsToUploadReferencesForSync < ActiveRecord::Migration[6.1]
   def up
+    # Migrates any post uploads that might have been created between the first
+    # migration and when the deploy process finished.
     execute <<~SQL
       INSERT INTO upload_references(upload_id, target_type, target_id, created_at, updated_at)
       SELECT upload_id, 'Post', post_id, NOW(), NOW()
