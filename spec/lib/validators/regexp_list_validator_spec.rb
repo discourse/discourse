8695449cfc5d26ef24bf08aa9f3e74879dc4dc1a
Bianca Nenciu <nbianca@users.noreply.github.com>
2022-05-04 14:33:06 +0300

    FIX: Validate permalink_normalizations setting (#16604)
    When an admin enters a badly formed regular expression in the
    permalink_normalizations site setting, a RegexpError exception is
    generated everytime a URL is normalized (see Permalink.normalize_url).
    
    The new validator validates every regular expression present in the
    setting value (delimited by '|').

---
+++ spec/lib/validators/regexp_list_validator_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe RegexpListValidator do
+  subject { described_class.new }
+
+  it "allows lists of valid regular expressions" do
+    expect(subject.valid_value?('\d+|[0-9]?|\w+')).to eq(true)
+  end
+
+  it "does not allow lists of invalid regular expressions do" do
+    expect(subject.valid_value?('\d+|[0-9?|\w+')).to eq(false)
+    expect(subject.error_message).to eq(
+      I18n.t("site_settings.errors.invalid_regex_with_message", regex: '[0-9?', message: 'premature end of char-class: /[0-9?/')
+    )
+  end
+end
