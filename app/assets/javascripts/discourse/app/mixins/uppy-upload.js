f88d797ea37b3ea430d853fbb3f402631c6ddc78
Isaac Janzen <isaac.janzen@discourse.org>
2022-05-04 06:53:00 -0500

    uppy references

--- app/assets/javascripts/discourse/app/mixins/uppy-upload.js
+++ app/assets/javascripts/discourse/app/mixins/uppy-upload.js
@@ -120,10 +120,10 @@ export default Mixin.create(UppyS3Multipart, ExtendableUploader, {
       onBeforeUpload: (files) => {
         let tooMany = false;
         const fileCount = Object.keys(files).length;
-        const maxFiles = this.getWithDefault(
-          "maxFiles",
-          this.siteSettings.simultaneous_uploads
-        );
+        const maxFiles = this.get("maxFiles");
+        if (maxFiles === undefined) {
+          maxFiles = this.siteSettings.simultaneous_uploads;
+        }
 
         if (this.allowMultipleFiles) {
           tooMany = maxFiles > 0 && fileCount > maxFiles;
@@ -396,11 +396,11 @@ export default Mixin.create(UppyS3Multipart, ExtendableUploader, {
   },
 
   _xhrUploadUrl() {
-    return (
-      getUrl(this.getWithDefault("uploadUrl", this.uploadRootPath)) +
-      ".json?client_id=" +
-      this.messageBus?.clientId
-    );
+    let uploadUrl = this.get("uploadUrl");
+    if (uploadUrl === undefined) {
+      uploadUrl = this.uploadRootPath;
+    }
+    return getUrl(uploadUrl) + ".json?client_id=" + this.messageBus?.clientId;
   },
 
   _bindFileInputChange() {
