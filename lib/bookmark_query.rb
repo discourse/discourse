f225e3d7873cad26f4958e0f2468b7719d815c6d
Martin Brennan <martin@discourse.org>
2022-05-12 09:36:17 +1000

    DEV: Minor bookmark tweaks for polymorphism
    * Make the modal for bookmarks display more consistently
    * Make sure bookmark query can handle empty results for certain
      bookmarkable queries

--- lib/bookmark_query.rb
+++ lib/bookmark_query.rb
@@ -72,6 +72,11 @@ class BookmarkQuery
     queries = Bookmark.registered_bookmarkables.map do |bookmarkable|
       interim_results = bookmarkable.perform_list_query(@user, @guardian)
 
+      # this could occur if there is some security reason that the user cannot
+      # access the bookmarkables that they have bookmarked, e.g. if they had 1 bookmark
+      # on a topic and that topic was moved into a private category
+      next if interim_results.blank?
+
       if search_term.present?
         interim_results = bookmarkable.perform_search_query(
           interim_results, search_term_wildcard, ts_query
@@ -81,7 +86,12 @@ class BookmarkQuery
       # this is purely to make the query easy to read and debug, otherwise it's
       # all mashed up into a massive ball in MiniProfiler :)
       "---- #{bookmarkable.model.to_s} bookmarkable ---\n\n #{interim_results.to_sql}"
-    end
+    end.compact
+
+    # same for interim results being blank, the user might have been locked out
+    # from all their various bookmarks, in which case they will see nothing and
+    # no further pagination/ordering/etc is required
+    return [] if queries.empty?
 
     union_sql = queries.join("\n\nUNION\n\n")
     results = Bookmark.select("bookmarks.*").from("(\n\n#{union_sql}\n\n) as bookmarks")
