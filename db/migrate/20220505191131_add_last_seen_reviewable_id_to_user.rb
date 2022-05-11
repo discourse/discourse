e599b5b08f4323d50fb2dd584dbb06333d60b995
David Taylor <david@taylorhq.com>
2022-05-11 11:28:34 +0100

    DEV: Update add_column migration to remove transaction (#16715)
    This migration is failing to acquire a lock under some production conditions. We're only performing one action, so removing the transaction is safe and may help to resolve the issue.

--- db/migrate/20220505191131_add_last_seen_reviewable_id_to_user.rb
+++ db/migrate/20220505191131_add_last_seen_reviewable_id_to_user.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class AddLastSeenReviewableIdToUser < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
-    add_column :users, :last_seen_reviewable_id, :integer
+    add_column :users, :last_seen_reviewable_id, :integer, if_not_exists: true
   end
 end
