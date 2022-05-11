bbe32c2dee45a9c617ded2f63d7bc26566d82c6c
Vinoth Kannan <svkn.87@gmail.com>
2022-05-11 14:09:45 +0530

    FIX: Make f query param sticky when navigating between nav items
    Also, hides categories navigation link when f query param is present.

--- app/assets/javascripts/discourse/app/components/navigation-item.js
+++ app/assets/javascripts/discourse/app/components/navigation-item.js
@@ -56,10 +56,12 @@ export default Component.extend(FilterModeMixin, {
     // If no query param is present, add an empty one to ensure a ? is
     // appended to the URL.
     if (content.currentRouteQueryParams) {
-      if (content.currentRouteQueryParams.filter) {
-        if (queryParams.length === 0) {
-          queryParams.push("");
-        }
+      if (content.currentRouteQueryParams.filter && queryParams.length === 0) {
+        queryParams.push("");
+      }
+
+      if (content.currentRouteQueryParams.f) {
+        queryParams.push(`f=${content.currentRouteQueryParams.f}`);
       }
     }
 
