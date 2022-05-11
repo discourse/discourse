d90065e0efb7f68b92d06690a047a4d9cb972766
David Taylor <david@taylorhq.com>
2022-05-11 12:36:11 +0100

    Revert "DEV: Update add_column migration to remove transaction (#16715)" (#16716)
    This reverts commit e599b5b08f4323d50fb2dd584dbb06333d60b995.
    
    This didn't solve the problem we were trying to fix - reverting back to the standard migration pattern

--- db/migrate/20220505191131_add_last_seen_reviewable_id_to_user.rb
+++ db/migrate/20220505191131_add_last_seen_reviewable_id_to_user.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class AddLastSeenReviewableIdToUser < ActiveRecord::Migration[7.0]
-  disable_ddl_transaction!
-
   def change
-    add_column :users, :last_seen_reviewable_id, :integer, if_not_exists: true
+    add_column :users, :last_seen_reviewable_id, :integer
   end
 end
