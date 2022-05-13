17ff3d321fa8e41f2a3776648fa263746d218825
Harphuna <86975697+Hakunasc11@users.noreply.github.com>
2022-05-13 22:22:19 +0200

    Delete tag_serializer.rb

--- app/serializers/tag_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class TagSerializer < ApplicationSerializer
-  attributes :id, :name, :topic_count, :staff, :description
-
-  def staff
-    DiscourseTagging.staff_tag_names.include?(name)
-  end
-end
