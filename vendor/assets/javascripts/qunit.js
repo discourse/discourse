a76b33bc50abac7a0f8ac6eee38e68b3f77220e3
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-05-25 10:41:26 +0800

    debug test failuire

--- vendor/assets/javascripts/qunit.js
+++ vendor/assets/javascripts/qunit.js
@@ -2370,9 +2370,9 @@
       /**
         `finally` will be invoked regardless of the promise's fate just as native
         try/catch/finally behaves
-      
+
         Synchronous example:
-      
+
         ```js
         findAuthor() {
           if (Math.random() > 0.5) {
@@ -2380,7 +2380,7 @@
           }
           return new Author();
         }
-      
+
         try {
           return findAuthor(); // succeed or fail
         } catch(error) {
@@ -2390,9 +2390,9 @@
           // doesn't affect the return value
         }
         ```
-      
+
         Asynchronous example:
-      
+
         ```js
         findAuthor().catch(function(reason){
           return findOtherAuther();
@@ -2400,7 +2400,7 @@
           // author was either found, or not
         });
         ```
-      
+
         @method finally
         @param {Function} callback
         @return {Promise}
@@ -4282,6 +4282,12 @@
   				return;
   			}
   		} else {
+				try {
+					// Code throwing an exception
+					throw new Error();
+				} catch(e) {
+					console.log(e.stack);
+				}
   			throw new Error("QUnit.start cannot be called inside a test context.");
   		}
 
