28ba3307ac47fc63f9e6b6e5ace51b81008bcf3c
Martin Brennan <martin@discourse.org>
2022-05-25 11:12:47 +1000

    FIX: Improve bookmark-icon title
    This improves the bookmark-icon title to be more like the
    post bookmark icons, to include the special formatted date
    as well as the name of the bookmark.

--- app/assets/javascripts/discourse/tests/integration/components/bookmark-icon-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/bookmark-icon-test.js
@@ -1,9 +1,16 @@
 import Bookmark from "discourse/models/bookmark";
+import I18n from "I18n";
+import { formattedReminderTime } from "discourse/lib/bookmark";
+import { tomorrow } from "discourse/lib/time-utils";
 import componentTest, {
   setupRenderingTest,
 } from "discourse/tests/helpers/component-test";
 import hbs from "htmlbars-inline-precompile";
-import { discourseModule, exists } from "discourse/tests/helpers/qunit-helpers";
+import {
+  discourseModule,
+  exists,
+  query,
+} from "discourse/tests/helpers/qunit-helpers";
 
 discourseModule("Component | bookmark-icon", function (hooks) {
   setupRenderingTest(hooks);
@@ -12,17 +19,27 @@ discourseModule("Component | bookmark-icon", function (hooks) {
     template: hbs`{{bookmark-icon bookmark=bookmark}}`,
 
     beforeEach() {
-      this.set(
-        "bookmark",
-        Bookmark.create({
-          reminder_at: moment(),
+      this.currentUser.set("timezone", "Australia/Brisbane");
+      this.setProperties({
+        bookmark: Bookmark.create({
+          reminder_at: tomorrow(this.currentUser.timezone),
           name: "some name",
-        })
-      );
+        }),
+      });
     },
 
     async test(assert) {
       assert.ok(exists(".d-icon-discourse-bookmark-clock"));
+      assert.strictEqual(
+        query(".svg-icon-title")["title"],
+        I18n.t("bookmarks.created_with_reminder_generic", {
+          date: formattedReminderTime(
+            this.bookmark.reminder_at,
+            this.currentUser.timezone
+          ),
+          name: "some name",
+        })
+      );
     },
   });
 
@@ -40,6 +57,12 @@ discourseModule("Component | bookmark-icon", function (hooks) {
 
     async test(assert) {
       assert.ok(exists(".d-icon-bookmark"));
+      assert.strictEqual(
+        query(".svg-icon-title")["title"],
+        I18n.t("bookmarks.created_generic", {
+          name: "some name",
+        })
+      );
     },
   });
 });
