f551129b6f94b2d8cae3ee70a364790f6c6ed5c3
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-05-25 10:42:14 +0800

    Revert "FIX: Improve bookmark-icon title (#16908)"
    This reverts commit 583704f603afc75f0e6f7937713463d21d8cba2d.

--- app/assets/javascripts/discourse/tests/integration/components/bookmark-icon-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/bookmark-icon-test.js
@@ -1,16 +1,9 @@
 import Bookmark from "discourse/models/bookmark";
-import I18n from "I18n";
-import { formattedReminderTime } from "discourse/lib/bookmark";
-import { tomorrow } from "discourse/lib/time-utils";
 import componentTest, {
   setupRenderingTest,
 } from "discourse/tests/helpers/component-test";
 import hbs from "htmlbars-inline-precompile";
-import {
-  discourseModule,
-  exists,
-  query,
-} from "discourse/tests/helpers/qunit-helpers";
+import { discourseModule, exists } from "discourse/tests/helpers/qunit-helpers";
 
 discourseModule("Component | bookmark-icon", function (hooks) {
   setupRenderingTest(hooks);
@@ -19,27 +12,17 @@ discourseModule("Component | bookmark-icon", function (hooks) {
     template: hbs`{{bookmark-icon bookmark=bookmark}}`,
 
     beforeEach() {
-      this.currentUser.set("timezone", "Australia/Brisbane");
-      this.setProperties({
-        bookmark: Bookmark.create({
-          reminder_at: tomorrow(this.currentUser.timezone),
+      this.set(
+        "bookmark",
+        Bookmark.create({
+          reminder_at: moment(),
           name: "some name",
-        }),
-      });
+        })
+      );
     },
 
     async test(assert) {
       assert.ok(exists(".d-icon-discourse-bookmark-clock"));
-      assert.strictEqual(
-        query(".svg-icon-title")["title"],
-        I18n.t("bookmarks.created_with_reminder_generic", {
-          date: formattedReminderTime(
-            this.bookmark.reminder_at,
-            this.currentUser.timezone
-          ),
-          name: "some name",
-        })
-      );
     },
   });
 
@@ -57,12 +40,6 @@ discourseModule("Component | bookmark-icon", function (hooks) {
 
     async test(assert) {
       assert.ok(exists(".d-icon-bookmark"));
-      assert.strictEqual(
-        query(".svg-icon-title")["title"],
-        I18n.t("bookmarks.created_generic", {
-          name: "some name",
-        })
-      );
     },
   });
 });
