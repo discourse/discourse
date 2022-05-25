84ea5a40d07649f2576e696bcf52a5caaa39aa05
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-05-25 17:48:17 +0400

    Fix suspending

--- app/mailers/user_notifications.rb
+++ app/mailers/user_notifications.rb
@@ -185,12 +185,13 @@ class UserNotifications < ActionMailer::Base
         reason: user_history.details
       )
     else
+      suspended_till = user.suspended_till.in_time_zone(user.user_option.timezone)
       build_email(
         user.email,
         template: "user_notifications.account_suspended",
         locale: user_locale(user),
         reason: user_history.details,
-        suspended_till: I18n.l(user.suspended_till, format: :long)
+        suspended_till: I18n.l(suspended_till, format: :long)
       )
     end
   end
--- spec/mailers/user_notifications_spec.rb
+++ spec/mailers/user_notifications_spec.rb
@@ -1216,4 +1216,36 @@ describe UserNotifications do
       end
     end
   end
+
+  describe ".account_suspended" do
+    fab!(:user_history) { Fabricate(:user_history, action: UserHistory.actions[:suspend_user]) }
+
+    it "adds the suspended_till date in user's timezone" do
+      user.user_option.timezone = "Asia/Tbilisi" # GMT+4
+      user.suspended_till = DateTime.parse("May 25, 2020, 12:00pm")
+
+      mail = UserNotifications.account_suspended(user, { user_history: user_history })
+
+      expect(mail.body).to include("May 25, 2020,  4:00pm")
+    end
+
+    context "user doesn't have timezone set" do
+      before do
+        user.user_option.timezone = nil
+      end
+
+      it "doesn't raise error" do
+        expect { UserNotifications.account_suspended(user) }.not_to raise_error
+      end
+
+      it "adds the suspended_till date in UTC" do
+        date = "May 25, 2020, 12:00pm"
+        user.suspended_till = DateTime.parse(date)
+
+        mail = UserNotifications.account_suspended(user, { user_history: user_history })
+
+        expect(mail.body).to include(date)
+      end
+    end
+  end
 end
