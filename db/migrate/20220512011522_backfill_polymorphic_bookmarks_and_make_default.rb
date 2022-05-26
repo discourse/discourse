f00e28206727da7a80d7a415cf825e746fd8d186
Daniel Waterworth <me@danielwaterworth.com>
2022-05-26 11:58:15 -0500

    FIX: Delete extraneous topic bookmarks (#16926)
    Otherwise the migration can fail
    
    Co-authored-by: David Taylor <david@taylorhq.com>

--- db/migrate/20220512011522_backfill_polymorphic_bookmarks_and_make_default.rb
+++ db/migrate/20220512011522_backfill_polymorphic_bookmarks_and_make_default.rb
@@ -15,6 +15,17 @@ class BackfillPolymorphicBookmarksAndMakeDefault < ActiveRecord::Migration[7.0]
     SQL
 
     DB.exec(<<~SQL)
+      DELETE FROM bookmarks
+      WHERE id NOT IN (
+        SELECT MIN(bookmarks.id)
+        FROM bookmarks
+        INNER JOIN posts ON bookmarks.post_id = posts.id
+        WHERE bookmarks.for_topic
+        GROUP BY (bookmarks.user_id, posts.topic_id)
+      )
+    SQL
+
+    DB.exec(<<~SQL)
       UPDATE bookmarks
       SET bookmarkable_id = posts.topic_id, bookmarkable_type = 'Topic'
       FROM posts
