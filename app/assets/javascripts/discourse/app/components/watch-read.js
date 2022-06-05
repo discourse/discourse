7bf8a00db530925625efa22bf44c8932f36173c0
Penar Musaraj <pmusaraj@gmail.com>
2022-06-05 14:40:47 -0400

    Limit change to is-element-in-viewport

--- app/assets/javascripts/discourse/app/components/watch-read.js
+++ app/assets/javascripts/discourse/app/components/watch-read.js
@@ -1,4 +1,3 @@
-import { bind } from "discourse-common/utils/decorators";
 import Component from "@ember/component";
 import isElementInViewport from "discourse/lib/is-element-in-viewport";
 
@@ -12,28 +11,17 @@ export default Component.extend({
 
     const path = this.path;
     if (path === "faq" || path === "guidelines") {
-      this._markRead();
-      window.addEventListener("resize", this._markRead, false);
-      window.addEventListener("scroll", this._markRead, false);
+      $(window).on("load.faq resize.faq scroll.faq", () => {
+        const faqUnread = !currentUser.get("read_faq");
+        if (faqUnread && isElementInViewport($(".contents p").last()[0])) {
+          this.action();
+        }
+      });
     }
   },
 
   willDestroyElement() {
     this._super(...arguments);
-
-    window.removeEventListener("resize", this._markRead);
-    window.removeEventListener("scroll", this._markRead);
-  },
-
-  @bind
-  _markRead() {
-    const faqUnread = !this.currentUser.read_faq;
-
-    if (
-      faqUnread &&
-      isElementInViewport(document.querySelector(".contents p:last-child"))
-    ) {
-      this.action();
-    }
+    $(window).off("load.faq resize.faq scroll.faq");
   },
 });
