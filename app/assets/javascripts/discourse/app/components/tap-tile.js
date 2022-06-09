3f569f118545daf039c072e73037095d53be43b4
Penar Musaraj <pmusaraj@gmail.com>
2022-06-09 15:05:01 +0000

    A11Y: Add keyboard support for do-not-disturb modal (#17043)

--- app/assets/javascripts/discourse/app/components/tap-tile.js
+++ app/assets/javascripts/discourse/app/components/tap-tile.js
@@ -5,24 +5,24 @@ import { propertyEqual } from "discourse/lib/computed";
 export default Component.extend({
   init() {
     this._super(...arguments);
-
     this.set("elementId", `tap_tile_${this.tileId}`);
   },
 
   classNames: ["tap-tile"],
-
   classNameBindings: ["active"],
-
   attributeBindings: ["role", "ariaPressed", "tabIndex"],
-
   role: "button",
-
   tabIndex: 0,
-
   ariaPressed: reads("active"),
 
   click() {
     this.onChange(this.tileId);
+  },
+
+  keyDown(e) {
+    if (e.key === "Enter") {
+      this.onChange(this.tileId);
+    }
   },
 
   active: propertyEqual("activeTile", "tileId"),
