c190f2a342b0c3fa5c6be53697d329dc7215f8fe
Martin Brennan <martin@discourse.org>
2022-06-15 09:24:14 +1000

    DEV: Review fixes

--- db/migrate/db/migrate/20220606061813_add_smtp_transaction_response_to_email_logs.rb
+++ db/migrate/db/migrate/20220606061813_add_smtp_transaction_response_to_email_logs.rb
@@ -2,6 +2,6 @@
 
 class AddSmtpTransactionResponseToEmailLogs < ActiveRecord::Migration[7.0]
   def change
-    add_column :email_logs, :smtp_transaction_response, :string, null: true, limit: 500, index: true
+    add_column :email_logs, :smtp_transaction_response, :string, null: true, limit: 500
   end
 end
