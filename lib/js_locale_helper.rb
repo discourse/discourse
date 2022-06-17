discourse/discourse

commit fcb4e5a1a15631cef9c20489866a68600ecf768d

DEV: Make wizard an ember addon (#17027)
Co-authored-by: David Taylor <david@taylorhq.com>

lib/js_locale_helper.rb
@@ -210,7 +210,19 @@ module JsLocaleHelper
       end
     end
 
-    translations.present? ? "I18n.extras = #{translations.to_json};" : ""
+    return "" if translations.blank?
+
+    <<~JS
+      if (!I18n.extras) {
+        I18n.extras = {}
+      }
+
+      if (!I18n.extras["#{locale}"]) {
+        I18n.extras["#{locale}"] = {};
+      }
+
+      Object.assign(I18n.extras["#{locale}"], #{translations[locale].to_json});
+    JS
   end
 
   MOMENT_LOCALE_MAPPING ||= {

commit be652c90920311323eee19fc55ffafaf8445399f

FIX: Extra-locale merging didn't account for fallbacks
Regressed in #17027

lib/js_locale_helper.rb
@@ -202,8 +202,9 @@ module JsLocaleHelper
 
   def self.output_extra_locales(bundle, locale)
     translations = translations_for(locale)
+    locales = translations.keys
 
-    translations.keys.each do |l|
+    locales.each do |l|
       translations[l].keys.each do |k|
         bundle_translations = translations[l].delete(k)
         translations[l].deep_merge!(bundle_translations) if k == bundle
@@ -212,17 +213,15 @@ module JsLocaleHelper
 
     return "" if translations.blank?
 
-    <<~JS
-      if (!I18n.extras) {
-        I18n.extras = {}
-      }
+    output = +"if (!I18n.extras) { I18n.extras = {}; }"
+    locales.each do |l|
+      output << <<~JS
+        if (!I18n.extras["#{l}"]) { I18n.extras["#{l}"] = {}; }
+        Object.assign(I18n.extras["#{l}"], #{translations[l].to_json});
+      JS
+    end
 
-      if (!I18n.extras["#{locale}"]) {
-        I18n.extras["#{locale}"] = {};
-      }
-
-      Object.assign(I18n.extras["#{locale}"], #{translations[locale].to_json});
-    JS
+    output
   end
 
   MOMENT_LOCALE_MAPPING ||= {