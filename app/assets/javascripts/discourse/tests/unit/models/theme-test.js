ba2c7b8f3508fbb6d55e9618c32859999063c499
Jarek Radosz <jradosz@gmail.com>
2022-06-20 15:42:10 +0200

    DEV: Use the block form of `module()` (#17151)

--- app/assets/javascripts/discourse/tests/unit/models/theme-test.js
+++ app/assets/javascripts/discourse/tests/unit/models/theme-test.js
@@ -1,22 +1,26 @@
 import { module, test } from "qunit";
 import Theme from "admin/models/theme";
 
-module("Unit | Model | theme");
+module("Unit | Model | theme", function () {
+  test("can add an upload correctly", function (assert) {
+    let theme = Theme.create();
 
-test("can add an upload correctly", function (assert) {
-  let theme = Theme.create();
+    assert.strictEqual(
+      theme.get("uploads.length"),
+      0,
+      "uploads should be an empty array"
+    );
 
-  assert.strictEqual(
-    theme.get("uploads.length"),
-    0,
-    "uploads should be an empty array"
-  );
+    theme.setField("common", "bob", "", 999, 2);
+    let fields = theme.get("theme_fields");
+    assert.strictEqual(fields.length, 1, "expecting 1 theme field");
+    assert.strictEqual(
+      fields[0].upload_id,
+      999,
+      "expecting upload id to be set"
+    );
+    assert.strictEqual(fields[0].type_id, 2, "expecting type id to be set");
 
-  theme.setField("common", "bob", "", 999, 2);
-  let fields = theme.get("theme_fields");
-  assert.strictEqual(fields.length, 1, "expecting 1 theme field");
-  assert.strictEqual(fields[0].upload_id, 999, "expecting upload id to be set");
-  assert.strictEqual(fields[0].type_id, 2, "expecting type id to be set");
-
-  assert.strictEqual(theme.get("uploads.length"), 1, "expecting an upload");
+    assert.strictEqual(theme.get("uploads.length"), 1, "expecting an upload");
+  });
 });
