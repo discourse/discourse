ef5b504e9ab97f9ad8ee157e91921972db3ec651
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-06-20 10:07:06 +0800

    DEV: Remove ember legacy env conditional in sidebar tests (#17140)

--- app/assets/javascripts/discourse/tests/acceptance/sidebar-messages-section-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/sidebar-messages-section-test.js
@@ -1,13 +1,13 @@
+import { test } from "qunit";
+
 import { click, currentURL, visit } from "@ember/test-helpers";
 
 import {
   acceptance,
-  conditionalTest,
   exists,
   queryAll,
   updateCurrentUser,
 } from "discourse/tests/helpers/qunit-helpers";
-import { isLegacyEmber } from "discourse-common/config/environment";
 
 acceptance(
   "Sidebar - Messages Section - enable_personal_messages disabled",
@@ -20,18 +20,14 @@ acceptance(
       enable_personal_messages: false,
     });
 
-    conditionalTest(
-      "clicking on section header button",
-      !isLegacyEmber(),
-      async function (assert) {
-        await visit("/");
+    test("clicking on section header button", async function (assert) {
+      await visit("/");
 
-        assert.ok(
-          !exists(".sidebar-section-messages"),
-          "does not display messages section in sidebar"
-        );
-      }
-    );
+      assert.ok(
+        !exists(".sidebar-section-messages"),
+        "does not display messages section in sidebar"
+      );
+    });
   }
 );
 
@@ -69,57 +65,85 @@ acceptance(
       });
     });
 
-    conditionalTest(
-      "clicking on section header button",
-      !isLegacyEmber(),
-      async function (assert) {
+    test("clicking on section header button", async function (assert) {
+      await visit("/");
+
+      await click(".sidebar-section-messages .sidebar-section-header-button");
+
+      assert.ok(
+        exists("#reply-control.private-message"),
+        "it opens the composer"
+      );
+    });
+
+    test("clicking on section header link", async function (assert) {
+      await visit("/");
+      await click(".sidebar-section-messages .sidebar-section-header-link");
+
+      assert.strictEqual(
+        currentURL(),
+        `/u/eviltrout/messages`,
+        "it should transistion to the user's messages"
+      );
+    });
+
+    test("personal messages section links", async function (assert) {
+      await visit("/");
+
+      assert.ok(
+        exists(
+          ".sidebar-section-messages .sidebar-section-link-personal-messages-inbox"
+        ),
+        "displays the personal message inbox link"
+      );
+
+      assert.strictEqual(
+        queryAll(".sidebar-section-messages .sidebar-section-link").length,
+        1,
+        "only displays the personal message inbox link"
+      );
+
+      await click(
+        ".sidebar-section-messages .sidebar-section-link-personal-messages-inbox"
+      );
+
+      assert.ok(
+        exists(
+          ".sidebar-section-messages .sidebar-section-link-personal-messages-inbox.active"
+        ),
+        "personal message inbox link is marked as active"
+      );
+
+      assert.strictEqual(
+        queryAll(".sidebar-section-messages .sidebar-section-link").length,
+        5,
+        "expands and displays the links for personal messages"
+      );
+    });
+
+    ["new", "archive", "sent", "unread"].forEach((type) => {
+      test(`${type} personal messages section link`, async function (assert) {
         await visit("/");
-
-        await click(".sidebar-section-messages .sidebar-section-header-button");
-
-        assert.ok(
-          exists("#reply-control.private-message"),
-          "it opens the composer"
-        );
-      }
-    );
-
-    conditionalTest(
-      "clicking on section header link",
-      !isLegacyEmber(),
-      async function (assert) {
-        await visit("/");
-        await click(".sidebar-section-messages .sidebar-section-header-link");
-
-        assert.strictEqual(
-          currentURL(),
-          `/u/eviltrout/messages`,
-          "it should transistion to the user's messages"
-        );
-      }
-    );
-
-    conditionalTest(
-      "personal messages section links",
-      !isLegacyEmber(),
-      async function (assert) {
-        await visit("/");
-
-        assert.ok(
-          exists(
-            ".sidebar-section-messages .sidebar-section-link-personal-messages-inbox"
-          ),
-          "displays the personal message inbox link"
-        );
-
-        assert.strictEqual(
-          queryAll(".sidebar-section-messages .sidebar-section-link").length,
-          1,
-          "only displays the personal message inbox link"
-        );
 
         await click(
           ".sidebar-section-messages .sidebar-section-link-personal-messages-inbox"
+        );
+
+        await click(
+          `.sidebar-section-messages .sidebar-section-link-personal-messages-${type}`
+        );
+
+        assert.strictEqual(
+          currentURL(),
+          `/u/eviltrout/messages/${type}`,
+          `it should transition to user's ${type} personal messages`
+        );
+
+        assert.strictEqual(
+          queryAll(".sidebar-section-messages .sidebar-section-link.active")
+            .length,
+          2,
+          "only two links are marked as active in the sidebar"
         );
 
         assert.ok(
@@ -129,63 +153,85 @@ acceptance(
           "personal message inbox link is marked as active"
         );
 
-        assert.strictEqual(
-          queryAll(".sidebar-section-messages .sidebar-section-link").length,
-          5,
-          "expands and displays the links for personal messages"
+        assert.ok(
+          exists(
+            `.sidebar-section-messages .sidebar-section-link-personal-messages-${type}.active`
+          ),
+          `personal message ${type} link is marked as active`
         );
-      }
-    );
+      });
+    });
 
-    ["new", "archive", "sent", "unread"].forEach((type) => {
-      conditionalTest(
-        `${type} personal messages section link`,
-        !isLegacyEmber(),
-        async function (assert) {
-          await visit("/");
+    test("group messages section links", async function (assert) {
+      updateCurrentUser({
+        groups: [
+          {
+            name: "group1",
+            has_messages: true,
+          },
+          {
+            name: "group2",
+            has_messages: false,
+          },
+          {
+            name: "group3",
+            has_messages: true,
+          },
+        ],
+      });
 
-          await click(
-            ".sidebar-section-messages .sidebar-section-link-personal-messages-inbox"
-          );
+      await visit("/");
 
-          await click(
-            `.sidebar-section-messages .sidebar-section-link-personal-messages-${type}`
-          );
+      assert.ok(
+        exists(
+          ".sidebar-section-messages .sidebar-section-link-group-messages-inbox.group1"
+        ),
+        "displays group1 inbox link"
+      );
 
-          assert.strictEqual(
-            currentURL(),
-            `/u/eviltrout/messages/${type}`,
-            `it should transition to user's ${type} personal messages`
-          );
+      assert.ok(
+        exists(
+          ".sidebar-section-messages .sidebar-section-link-group-messages-inbox.group3"
+        ),
+        "displays group3 inbox link"
+      );
 
-          assert.strictEqual(
-            queryAll(".sidebar-section-messages .sidebar-section-link.active")
-              .length,
-            2,
-            "only two links are marked as active in the sidebar"
-          );
+      await visit("/u/eviltrout/messages/group/GrOuP1");
 
-          assert.ok(
-            exists(
-              ".sidebar-section-messages .sidebar-section-link-personal-messages-inbox.active"
-            ),
-            "personal message inbox link is marked as active"
-          );
+      assert.strictEqual(
+        queryAll(".sidebar-section-messages .sidebar-section-link").length,
+        6,
+        "expands and displays the links for group1 group messages"
+      );
 
-          assert.ok(
-            exists(
-              `.sidebar-section-messages .sidebar-section-link-personal-messages-${type}.active`
-            ),
-            `personal message ${type} link is marked as active`
-          );
-        }
+      assert.strictEqual(
+        queryAll(".sidebar-section-messages .sidebar-section-link.group1")
+          .length,
+        4,
+        "expands the links for group1 group messages"
+      );
+
+      await click(
+        ".sidebar-section-messages .sidebar-section-link-group-messages-inbox.group3"
+      );
+
+      assert.strictEqual(
+        queryAll(".sidebar-section-messages .sidebar-section-link.group1")
+          .length,
+        1,
+        "collapses the links for group1 group messages"
+      );
+
+      assert.strictEqual(
+        queryAll(".sidebar-section-messages .sidebar-section-link.group3")
+          .length,
+        4,
+        "expands the links for group3 group messages"
       );
     });
 
-    conditionalTest(
-      "group messages section links",
-      !isLegacyEmber(),
-      async function (assert) {
+    ["new", "archive", "unread"].forEach((type) => {
+      test(`${type} group messages section link`, async function (assert) {
         updateCurrentUser({
           groups: [
             {
@@ -205,115 +251,41 @@ acceptance(
 
         await visit("/");
 
-        assert.ok(
-          exists(
-            ".sidebar-section-messages .sidebar-section-link-group-messages-inbox.group1"
-          ),
-          "displays group1 inbox link"
-        );
-
-        assert.ok(
-          exists(
-            ".sidebar-section-messages .sidebar-section-link-group-messages-inbox.group3"
-          ),
-          "displays group3 inbox link"
-        );
-
-        await visit("/u/eviltrout/messages/group/GrOuP1");
-
-        assert.strictEqual(
-          queryAll(".sidebar-section-messages .sidebar-section-link").length,
-          6,
-          "expands and displays the links for group1 group messages"
-        );
-
-        assert.strictEqual(
-          queryAll(".sidebar-section-messages .sidebar-section-link.group1")
-            .length,
-          4,
-          "expands the links for group1 group messages"
+        await click(
+          `.sidebar-section-messages .sidebar-section-link-group-messages-inbox.group1`
         );
 
         await click(
-          ".sidebar-section-messages .sidebar-section-link-group-messages-inbox.group3"
+          `.sidebar-section-messages .sidebar-section-link-group-messages-${type}.group1`
         );
 
         assert.strictEqual(
-          queryAll(".sidebar-section-messages .sidebar-section-link.group1")
-            .length,
-          1,
-          "collapses the links for group1 group messages"
+          currentURL(),
+          `/u/eviltrout/messages/group/group1/${type}`,
+          `it should transition to user's ${type} personal messages`
         );
 
         assert.strictEqual(
-          queryAll(".sidebar-section-messages .sidebar-section-link.group3")
+          queryAll(".sidebar-section-messages .sidebar-section-link.active")
             .length,
-          4,
-          "expands the links for group3 group messages"
+          2,
+          "only two links are marked as active in the sidebar"
         );
-      }
-    );
 
-    ["new", "archive", "unread"].forEach((type) => {
-      conditionalTest(
-        `${type} group messages section link`,
-        !isLegacyEmber(),
-        async function (assert) {
-          updateCurrentUser({
-            groups: [
-              {
-                name: "group1",
-                has_messages: true,
-              },
-              {
-                name: "group2",
-                has_messages: false,
-              },
-              {
-                name: "group3",
-                has_messages: true,
-              },
-            ],
-          });
+        assert.ok(
+          exists(
+            ".sidebar-section-messages .sidebar-section-link-group-messages-inbox.group1.active"
+          ),
+          "group1 group message inbox link is marked as active"
+        );
 
-          await visit("/");
-
-          await click(
-            `.sidebar-section-messages .sidebar-section-link-group-messages-inbox.group1`
-          );
-
-          await click(
-            `.sidebar-section-messages .sidebar-section-link-group-messages-${type}.group1`
-          );
-
-          assert.strictEqual(
-            currentURL(),
-            `/u/eviltrout/messages/group/group1/${type}`,
-            `it should transition to user's ${type} personal messages`
-          );
-
-          assert.strictEqual(
-            queryAll(".sidebar-section-messages .sidebar-section-link.active")
-              .length,
-            2,
-            "only two links are marked as active in the sidebar"
-          );
-
-          assert.ok(
-            exists(
-              ".sidebar-section-messages .sidebar-section-link-group-messages-inbox.group1.active"
-            ),
-            "group1 group message inbox link is marked as active"
-          );
-
-          assert.ok(
-            exists(
-              `.sidebar-section-messages .sidebar-section-link-group-messages-${type}.group1.active`
-            ),
-            `group1 group message ${type} link is marked as active`
-          );
-        }
-      );
+        assert.ok(
+          exists(
+            `.sidebar-section-messages .sidebar-section-link-group-messages-${type}.group1.active`
+          ),
+          `group1 group message ${type} link is marked as active`
+        );
+      });
     });
   }
 );
