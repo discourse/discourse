61cf7919299acdcad79bdc92a8b5164cb8269582
Penar Musaraj <pmusaraj@gmail.com>
2022-06-21 19:05:00 +0000

    Version bump to v2.8.5 (#17187)

--- lib/version.rb
+++ lib/version.rb
@@ -9,7 +9,7 @@ module Discourse
     module VERSION #:nodoc:
       MAJOR = 2
       MINOR = 8
-      TINY  = 4
+      TINY  = 5
       PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
