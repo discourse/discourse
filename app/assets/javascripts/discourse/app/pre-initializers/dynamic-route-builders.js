68c3424d97ecab338441118476d64a0eb8d46b9d
Peter Wagenet <peter.wagenet@gmail.com>
2022-06-21 15:19:05 -0700

    Modernize route builder

--- app/assets/javascripts/discourse/app/pre-initializers/dynamic-route-builders.js
+++ app/assets/javascripts/discourse/app/pre-initializers/dynamic-route-builders.js
@@ -4,73 +4,120 @@ import TagShowRoute from "discourse/routes/tag-show";
 import User from "discourse/models/user";
 import buildCategoryRoute from "discourse/routes/build-category-route";
 import buildTopicRoute from "discourse/routes/build-topic-route";
-import { capitalize } from "@ember/string";
 
 export default {
   after: "inject-discourse-objects",
   name: "dynamic-route-builders",
 
   initialize(registry, app) {
-    app.DiscoveryCategoryController = DiscoverySortableController.extend();
-    app.DiscoveryCategoryNoneController = DiscoverySortableController.extend();
-    app.DiscoveryCategoryAllController = DiscoverySortableController.extend();
+    app.register(
+      "controller:discovery.category",
+      DiscoverySortableController.extend()
+    );
+    app.register(
+      "controller:discovery.category-none",
+      DiscoverySortableController.extend()
+    );
+    app.register(
+      "controller:discovery.category-all",
+      DiscoverySortableController.extend()
+    );
 
-    app.DiscoveryCategoryRoute = buildCategoryRoute("default");
-    app.DiscoveryCategoryNoneRoute = buildCategoryRoute("default", {
-      no_subcategories: true,
-    });
-    app.DiscoveryCategoryAllRoute = buildCategoryRoute("default", {
-      no_subcategories: false,
-    });
+    app.register("route:discovery.category", buildCategoryRoute("default"));
+    app.register(
+      "route:discovery.category-none",
+      buildCategoryRoute("default", {
+        no_subcategories: true,
+      })
+    );
+    app.register(
+      "route:discovery.category-all",
+      buildCategoryRoute("default", {
+        no_subcategories: false,
+      })
+    );
 
     const site = Site.current();
     site.get("filters").forEach((filter) => {
-      const filterCapitalized = capitalize(filter);
-      app[`Discovery${filterCapitalized}Controller`] =
-        DiscoverySortableController.extend();
-      app[`Discovery${filterCapitalized}CategoryController`] =
-        DiscoverySortableController.extend();
-      app[`Discovery${filterCapitalized}CategoryNoneController`] =
-        DiscoverySortableController.extend();
+      const filterDasherized = filter.dasherize();
+      app.register(
+        `controller:discovery.${filterDasherized}`,
+        DiscoverySortableController.extend()
+      );
+      app.register(
+        `controller:discovery.${filterDasherized}-category`,
+        DiscoverySortableController.extend()
+      );
+      app.register(
+        `controller:discovery.${filterDasherized}-category-none`,
+        DiscoverySortableController.extend()
+      );
 
       if (filter === "top") {
-        app.DiscoveryTopRoute = buildTopicRoute("top", {
-          actions: {
-            willTransition() {
-              User.currentProp("should_be_redirected_to_top", false);
-              User.currentProp("redirected_to_top.reason", null);
-              return this._super(...arguments);
+        app.register(
+          "route:discovery.top",
+          buildTopicRoute("top", {
+            actions: {
+              willTransition() {
+                User.currentProp("should_be_redirected_to_top", false);
+                User.currentProp("redirected_to_top.reason", null);
+                return this._super(...arguments);
+              },
             },
-          },
-        });
+          })
+        );
       } else {
-        app[`Discovery${filterCapitalized}Route`] = buildTopicRoute(filter);
+        app.register(
+          `route:discovery.${filterDasherized}`,
+          buildTopicRoute(filter)
+        );
       }
 
-      app[`Discovery${filterCapitalized}CategoryRoute`] =
-        buildCategoryRoute(filter);
-      app[`Discovery${filterCapitalized}CategoryNoneRoute`] =
-        buildCategoryRoute(filter, { no_subcategories: true });
+      app.register(
+        `route:discovery.${filterDasherized}-category`,
+        buildCategoryRoute(filter)
+      );
+      app.register(
+        `route:discovery.${filterDasherized}-category-none`,
+        buildCategoryRoute(filter, { no_subcategories: true })
+      );
     });
 
-    app["TagsShowCategoryRoute"] = TagShowRoute.extend();
-    app["TagsShowCategoryNoneRoute"] = TagShowRoute.extend({
-      noSubcategories: true,
-    });
-    app["TagsShowCategoryAllRoute"] = TagShowRoute.extend({
-      noSubcategories: false,
-    });
+    app.register("route:tags.show-category", TagShowRoute.extend());
+    app.register(
+      "route:tags.show-category-none",
+      TagShowRoute.extend({
+        noSubcategories: true,
+      })
+    );
+    app.register(
+      "route:tags.show-category-all",
+      TagShowRoute.extend({
+        noSubcategories: false,
+      })
+    );
 
     site.get("filters").forEach(function (filter) {
-      app["TagShow" + capitalize(filter) + "Route"] = TagShowRoute.extend({
-        navMode: filter,
-      });
-      app["TagsShowCategory" + capitalize(filter) + "Route"] =
-        TagShowRoute.extend({ navMode: filter });
-      app["TagsShowCategoryNone" + capitalize(filter) + "Route"] =
-        TagShowRoute.extend({ navMode: filter, noSubcategories: true });
-      app["TagsShowCategoryAll" + capitalize(filter) + "Route"] =
-        TagShowRoute.extend({ navMode: filter, noSubcategories: false });
+      const filterDasherized = filter.dasherize();
+
+      app.register(
+        `route:tag.show-${filterDasherized}`,
+        TagShowRoute.extend({
+          navMode: filter,
+        })
+      );
+      app.register(
+        `route:tag.show-${filterDasherized}-category`,
+        TagShowRoute.extend({ navMode: filter })
+      );
+      app.register(
+        `route:tag.show-${filterDasherized}-category-none`,
+        TagShowRoute.extend({ navMode: filter, noSubcategories: true })
+      );
+      app.register(
+        `route:tag.show-${filterDasherized}-category-all`,
+        TagShowRoute.extend({ navMode: filter, noSubcategories: false })
+      );
     });
   },
 };
