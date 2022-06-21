b59e3b0b7ce915b322e3400ef6aa80bca6fd7e58
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-06-21 13:37:29 -0500

    DEV: Remove `isLegacyEmber()` from tests (#17184)

--- app/assets/javascripts/discourse/tests/integration/components/watched-word-uploader-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/watched-word-uploader-test.js
@@ -8,50 +8,44 @@ import {
 import hbs from "htmlbars-inline-precompile";
 import pretender, { response } from "discourse/tests/helpers/create-pretender";
 import { click, waitFor } from "@ember/test-helpers";
-import { isLegacyEmber } from "discourse-common/config/environment";
 
-if (!isLegacyEmber()) {
-  discourseModule(
-    "Integration | Component | watched-word-uploader",
-    function (hooks) {
-      setupRenderingTest(hooks);
+discourseModule(
+  "Integration | Component | watched-word-uploader",
+  function (hooks) {
+    setupRenderingTest(hooks);
 
-      hooks.beforeEach(function () {
-        pretender.post(
-          "/admin/customize/watched_words/upload.json",
-          function () {
-            return response(200, {});
-          }
-        );
+    hooks.beforeEach(function () {
+      pretender.post("/admin/customize/watched_words/upload.json", function () {
+        return response(200, {});
       });
+    });
 
-      componentTest("sets the proper action key on uploads", {
-        template: hbs`{{watched-word-uploader
+    componentTest("sets the proper action key on uploads", {
+      template: hbs`{{watched-word-uploader
         id="watched-word-uploader"
         actionKey=actionNameKey
         done=doneUpload
       }}`,
 
-        async test(assert) {
-          const done = assert.async();
-          this.set("actionNameKey", "flag");
-          this.set("doneUpload", function () {
-            assert.equal(
-              Object.entries(this._uppyInstance.getState().files)[0][1].meta
-                .action_key,
-              "flag"
-            );
-            done();
-          });
+      async test(assert) {
+        const done = assert.async();
+        this.set("actionNameKey", "flag");
+        this.set("doneUpload", function () {
+          assert.equal(
+            Object.entries(this._uppyInstance.getState().files)[0][1].meta
+              .action_key,
+            "flag"
+          );
+          done();
+        });
 
-          const words = createFile("watched-words.txt");
-          await this.container
-            .lookup("service:app-events")
-            .trigger("upload-mixin:watched-word-uploader:add-files", words);
-          await waitFor(".bootbox span.d-button-label");
-          await click(".bootbox span.d-button-label");
-        },
-      });
-    }
-  );
-}
+        const words = createFile("watched-words.txt");
+        await this.container
+          .lookup("service:app-events")
+          .trigger("upload-mixin:watched-word-uploader:add-files", words);
+        await waitFor(".bootbox span.d-button-label");
+        await click(".bootbox span.d-button-label");
+      },
+    });
+  }
+);
