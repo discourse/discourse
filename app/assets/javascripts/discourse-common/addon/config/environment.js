61092fad5cf6e6f15bf59eeafe26a420c8c79b32
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-06-21 14:09:36 -0500

    Revert "DEV: add deprecation notice to `isLegacyEmber` (#17186)"
    This reverts commit 41028a84efc8dd2c3f86c151f1ed46048f82476b.

--- app/assets/javascripts/discourse-common/addon/config/environment.js
+++ app/assets/javascripts/discourse-common/addon/config/environment.js
@@ -1,5 +1,3 @@
-import deprecated from "discourse-common/lib/deprecated";
-
 export const INPUT_DELAY = 250;
 
 let environment = "unknown";
@@ -21,9 +19,6 @@ export function isTesting() {
 // eslint-disable-next-line no-undef
 let _isLegacy = Ember.VERSION.startsWith("3.12");
 export function isLegacyEmber() {
-  deprecated("`isLegacyEmber()` is now deprecated and always returns true", {
-    dropFrom: "3.0.0.beta1",
-  });
   return _isLegacy;
 }
 
