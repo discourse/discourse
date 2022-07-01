a44ff74eb9633e1ec75f860ef335b1e0ff3c5080
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-07-01 14:47:40 +0800

    TGX changes

--- app/assets/javascripts/discourse/app/lib/sidebar/custom-sections.js
+++ app/assets/javascripts/discourse/app/lib/sidebar/custom-sections.js
@@ -1,17 +1,30 @@
 import BaseSectionHeader from "discourse/lib/sidebar/base-section-header";
-import BaseSectionLink from "discourse/lib/sidebar/topics-section/base-section-link";
 import { A } from "@ember/array";
+import { tracked } from "@glimmer/tracking";
 
-export const customSections = A([]);
+export const customSections = [];
 
 export function addSidebarSection(arg) {
-  let header,
-    links = A([]);
+  customSections.push(
+    class extends BaseSectionHeader {
+      @tracked sectionLinks = A([]);
 
-  if (typeof arg.header === "function") {
-    header = arg.header.call(this, BaseSectionHeader);
-  } else {
-    header = class extends BaseSectionHeader {
+      constructor() {
+        super(...arguments);
+
+        arg.chatService.getChannels().then((channels) => {
+          channels.publicChannels.forEach((channel) => {
+            this.sectionLinks.pushObject(
+              new (class {
+                get text() {
+                  return channel.title;
+                }
+              })()
+            );
+          });
+        });
+      }
+
       get name() {
         return arg.header.name;
       }
@@ -39,33 +52,10 @@ export function addSidebarSection(arg) {
       get actionTitle() {
         return arg.header.actionTitle;
       }
-    };
-  }
 
-  if (typeof arg.links === "function") {
-    links = arg.links.call(this, BaseSectionLink);
-  } else {
-    arg.links?.forEach((link) => {
-      const klass = class extends BaseSectionLink {
-        get name() {
-          return link.name;
-        }
-
-        get route() {
-          return link.route;
-        }
-
-        get text() {
-          return link.text;
-        }
-
-        get title() {
-          return link.title;
-        }
-      };
-      links.pushObject(klass);
-    });
-  }
-
-  customSections.push({ header, links });
+      get links() {
+        return this.sectionLinks;
+      }
+    }
+  );
 }
