56c0d8cf92b9f57489efe772318026bd703bc22b
Kris <kris.aubuchon@discourse.org>
2022-07-04 23:45:02 -0400

    UX: mobile experimental sidebar improvement (#17302)
    First pass at the mobile experimental sidebar improvement.
    
    Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

--- app/assets/javascripts/discourse/app/controllers/application.js
+++ app/assets/javascripts/discourse/app/controllers/application.js
@@ -1,12 +1,23 @@
 import Controller from "@ember/controller";
 import discourseComputed from "discourse-common/utils/decorators";
+import discourseDebounce from "discourse-common/lib/debounce";
 import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
 
 export default Controller.extend({
   showTop: true,
   showFooter: false,
   router: service(),
-  showSidebar: true,
+  showSidebar: null,
+  hideSidebarKey: "sidebar-hidden",
+
+  init() {
+    this._super(...arguments);
+
+    this.showSidebar = this.site.mobileView
+      ? false
+      : this.currentUser && !this.keyValueStore.getItem(this.hideSidebarKey);
+  },
 
   @discourseComputed
   canSignUp() {
@@ -25,5 +36,32 @@ export default Controller.extend({
   @discourseComputed
   showFooterNav() {
     return this.capabilities.isAppWebview || this.capabilities.isiOSPWA;
+  },
+
+  _mainOutletAnimate() {
+    document
+      .querySelector("#main-outlet")
+      .classList.remove("main-outlet-animate");
+  },
+
+  @action
+  toggleSidebar() {
+    // enables CSS transitions, but not on did-insert
+    document.querySelector("body").classList.add("sidebar-animate");
+
+    // reduces CSS transition jank
+    document.querySelector("#main-outlet").classList.add("main-outlet-animate");
+
+    discourseDebounce(this, this._mainOutletAnimate, 250);
+
+    this.toggleProperty("showSidebar");
+
+    if (!this.site.mobileView) {
+      if (this.showSidebar) {
+        this.keyValueStore.removeItem(this.hideSidebarKey);
+      } else {
+        this.keyValueStore.setItem(this.hideSidebarKey);
+      }
+    }
   },
 });
