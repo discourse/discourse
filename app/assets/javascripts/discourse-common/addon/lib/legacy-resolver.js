acabfe5e67104defedf1843ab3bc9836ff8dfd5f
David Taylor <david@taylorhq.com>
2022-07-06 14:04:43 +0100

    Update app/assets/javascripts/discourse-common/addon/lib/legacy-resolver.js
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>

--- app/assets/javascripts/discourse-common/addon/lib/legacy-resolver.js
+++ app/assets/javascripts/discourse-common/addon/lib/legacy-resolver.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+/* global Ember */
 import { classify, dasherize, decamelize } from "@ember/string";
 import deprecated from "discourse-common/lib/deprecated";
 import { findHelper } from "discourse-common/lib/helpers";
