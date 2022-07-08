9408ea4461740de914e9e6ade79225720003c0d2
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-07-08 15:28:40 +0200

    Revert "DEV: glimmerify discourse-root (#17390)" (#17392)
    This reverts commit 27408c7e14b11ca234ec57e59fa1f575295fb79f.

--- app/assets/javascripts/discourse/app/components/discourse-root.js
+++ app/assets/javascripts/discourse/app/components/discourse-root.js
@@ -1,3 +1,11 @@
-import GlimmerComponent from "discourse/components/glimmer";
+import Component from "@ember/component";
 
-export default class DiscourseRoot extends GlimmerComponent {}
+let componentArgs = { tagName: "div" };
+
+// TODO: Once we've moved to Ember CLI completely we can remove this block
+// eslint-disable-next-line no-undef
+if (!Ember.FEATURES.EMBER_GLIMMER_SET_COMPONENT_TEMPLATE) {
+  componentArgs = { tagName: "" };
+}
+
+export default Component.extend(componentArgs);
