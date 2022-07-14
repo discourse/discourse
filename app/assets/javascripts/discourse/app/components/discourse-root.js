e140db9f55df93298872cda8fbb772f25b66b98c
David Taylor <david@taylorhq.com>
2022-07-14 11:58:30 +0100

    DEV: Remove 'legacy ember' logic from discourse-root component
    This logic should no longer be triggered. The EMBER_GLIMMER_SET_COMPONENT_TEMPLATE gets removed in recent versions of Ember, which can cause it to accidently trigger and cause layout issues with some plugins/themes.

--- app/assets/javascripts/discourse/app/components/discourse-root.js
+++ app/assets/javascripts/discourse/app/components/discourse-root.js
@@ -2,10 +2,4 @@ import Component from "@ember/component";
 
 let componentArgs = { tagName: "div", classNames: ["discourse-root"] };
 
-// TODO: Once we've moved to Ember CLI completely we can remove this block
-// eslint-disable-next-line no-undef
-if (!Ember.FEATURES.EMBER_GLIMMER_SET_COMPONENT_TEMPLATE) {
-  componentArgs = { tagName: "" };
-}
-
 export default Component.extend(componentArgs);
