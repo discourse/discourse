discourse/discourse

commit 9028df0fda0daee258537ce875f8561923b44e27

DEV: Use `includes()` instead of `indexOf() >= 0` (#17553)
Missed those cases in #17541

app/assets/javascripts/discourse-common/addon/lib/icon-library.js
@@ -121,7 +121,7 @@ export function setIconList(iconList) {
 }
 
 export function isExistingIconId(id) {
-  return _iconList && _iconList.indexOf(id) >= 0;
+  return _iconList.includes(id);
 }
 
 function warnIfMissing(id) {

app/assets/javascripts/admin/addon/mixins/setting-object.js
@@ -63,7 +63,7 @@ export default Mixin.create({
 
   @discourseComputed("valid_values")
   allowsNone(validValues) {
-    if (validValues && validValues.indexOf("") >= 0) {
+    if (validValues?.includes("")) {
       return "admin.settings.none";
     }
   },

app/assets/javascripts/discourse/app/components/color-picker-choice.js
@@ -10,7 +10,7 @@ export default Component.extend({
 
   @discourseComputed("color", "usedColors")
   isUsed(color, usedColors) {
-    return (usedColors || []).indexOf(color.toUpperCase()) >= 0;
+    return (usedColors || []).includes(color.toUpperCase());
   },
 
   @discourseComputed("isUsed")

app/assets/javascripts/discourse/app/lib/uploads.js
@@ -198,8 +198,8 @@ function authorizedImagesExtensions(staff, siteSettings) {
 
 export function authorizesAllExtensions(staff, siteSettings) {
   return (
-    siteSettings.authorized_extensions.indexOf("*") >= 0 ||
-    (siteSettings.authorized_extensions_for_staff.indexOf("*") >= 0 && staff)
+    siteSettings.authorized_extensions.includes("*") ||
+    (siteSettings.authorized_extensions_for_staff.includes("*") && staff)
   );
 }
 

app/assets/javascripts/discourse/app/models/topic-list.js
@@ -120,7 +120,7 @@ const TopicList = RestModel.extend({
   loadBefore(topic_ids, storeInSession) {
     // refresh dupes
     this.topics.removeObjects(
-      this.topics.filter((topic) => topic_ids.indexOf(topic.id) >= 0)
+      this.topics.filter((topic) => topic_ids.includes(topic.id))
     );
 
     const url = `${getURL("/")}${this.filter}.json?topic_ids=${topic_ids.join(

app/assets/javascripts/discourse/app/models/topic.js
@@ -61,9 +61,7 @@ const Topic = RestModel.extend({
   @discourseComputed("posters.[]")
   lastPoster(posters) {
     if (posters && posters.length > 0) {
-      const latest = posters.filter(
-        (p) => p.extras && p.extras.indexOf("latest") >= 0
-      )[0];
+      const latest = posters.filter((p) => p.extras?.includes("latest"))[0];
       return latest || posters.firstObject;
     }
   },

app/assets/javascripts/discourse/app/models/user-action.js
@@ -31,7 +31,7 @@ const UserAction = RestModel.extend({
 
   @discourseComputed("action_type")
   descriptionKey(action) {
-    if (action === null || UserAction.TO_SHOW.indexOf(action) >= 0) {
+    if (action === null || UserAction.TO_SHOW.includes(action)) {
       if (this.isPM) {
         return this.sameUser ? "sent_by_you" : "sent_by_user";
       } else {
@@ -180,7 +180,7 @@ UserAction.reopenClass({
       const found = uniq[key];
       if (found === void 0) {
         let current;
-        if (UserAction.TO_COLLAPSE.indexOf(item.action_type) >= 0) {
+        if (UserAction.TO_COLLAPSE.includes(item.action_type)) {
           current = UserAction.create(item);
           item.switchToActing();
           current.addChild(item);
@@ -191,7 +191,7 @@ UserAction.reopenClass({
         collapsed[pos] = current;
         pos += 1;
       } else {
-        if (UserAction.TO_COLLAPSE.indexOf(item.action_type) >= 0) {
+        if (UserAction.TO_COLLAPSE.includes(item.action_type)) {
           item.switchToActing();
           collapsed[found].addChild(item);
         } else {

app/assets/javascripts/discourse/app/routes/discovery-categories.js
@@ -47,7 +47,7 @@ const DiscoveryCategoriesRoute = DiscourseRoute.extend(OpenComposer, {
     return function (topic_ids, storeInSession) {
       // refresh dupes
       this.topics.removeObjects(
-        this.topics.filter((topic) => topic_ids.indexOf(topic.id) >= 0)
+        this.topics.filter((topic) => topic_ids.includes(topic.id))
       );
 
       const url = `${getURL("/")}latest.json?topic_ids=${topic_ids.join(",")}`;

commit ed1d7f591cf5538aa925c0c132445fcc3372052d

FIX: `isExistingIconId()` regressed in #17553
I missed a single `?` character 😔

app/assets/javascripts/discourse-common/addon/lib/icon-library.js
@@ -121,7 +121,7 @@ export function setIconList(iconList) {
 }
 
 export function isExistingIconId(id) {
-  return _iconList.includes(id);
+  return _iconList?.includes(id);
 }
 
 function warnIfMissing(id) {