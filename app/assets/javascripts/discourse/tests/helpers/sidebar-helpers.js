a229a97cdb85096ac111f0df8adcf4193d955adf
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-07-21 10:59:12 +0800

    Adds site settings link to Sidebar Footer

--- app/assets/javascripts/discourse/tests/helpers/sidebar-helpers.js
+++ app/assets/javascripts/discourse/tests/helpers/sidebar-helpers.js
@@ -1,5 +1,5 @@
 import { click } from "@ember/test-helpers";
 
 export async function undockSidebar() {
-  await click("button.sidebar-footer-button-dock-toggle");
+  await click("button.sidebar-footer-actions-dock-toggle");
 }
