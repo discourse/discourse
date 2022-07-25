90c72f2247447a83bd7ee5d95415aa65275c77b1
David Taylor <david@taylorhq.com>
2022-07-25 22:00:13 +0100

    DEV: Add optional timezone support to date-time-input-range
    This allows consumers to pass in, and receive, timestamps for a different timezone. Previously, attempting this would lead to very strange behavior which would become worse the further the input timestamp's timezone was from the browser's timezone.
    
    The default behavior is unchanged - the browser's timezone will be assumed.

--- app/assets/javascripts/discourse/app/components/date-time-input.js
+++ app/assets/javascripts/discourse/app/components/date-time-input.js
@@ -28,16 +28,19 @@ export default Component.extend({
         ? this.date
         : this.relativeDate
         ? this.relativeDate
-        : moment();
+        : moment.tz(this.resolvedTimezone);
 
       this.onChange(
-        moment({
-          year: date.year(),
-          month: date.month(),
-          day: date.date(),
-          hours: time.hours,
-          minutes: time.minutes,
-        })
+        moment.tz(
+          {
+            year: date.year(),
+            month: date.month(),
+            day: date.date(),
+            hours: time.hours,
+            minutes: time.minutes,
+          },
+          this.resolvedTimezone
+        )
       );
     }
   },
@@ -51,13 +54,25 @@ export default Component.extend({
 
     this.onChange &&
       this.onChange(
-        moment({
-          year: date.year(),
-          month: date.month(),
-          day: date.date(),
-          hours: this.hours || 0,
-          minutes: this.minutes || 0,
-        })
+        moment.tz(
+          {
+            year: date.year(),
+            month: date.month(),
+            day: date.date(),
+            hours: this.hours || 0,
+            minutes: this.minutes || 0,
+          },
+          this.resolvedTimezone
+        )
       );
+  },
+
+  @computed
+  get resolvedTimezone() {
+    if (this.timezone) {
+      return this.timezone;
+    } else {
+      return moment.tz.guess();
+    }
   },
 });
