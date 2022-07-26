b6e7a07384c8841e4cebc052788ecbefe0f863ac
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-07-26 06:03:52 +0300

    remove variable in reviewable-user-item.js

--- app/assets/javascripts/discourse/app/components/user-menu/reviewable-user-item.js
+++ app/assets/javascripts/discourse/app/components/user-menu/reviewable-user-item.js
@@ -3,8 +3,9 @@ import I18n from "I18n";
 
 export default class UserMenuReviewableUserItem extends UserMenuDefaultReviewableItem {
   get description() {
-    const username = this.reviewable.username;
-    return I18n.t("user_menu.reviewable.suspicious_user", { username });
+    return I18n.t("user_menu.reviewable.suspicious_user", {
+      username: this.reviewable.username,
+    });
   }
 
   get icon() {
