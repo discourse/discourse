discourse/discourse

commit fc36ac6cde2afaf8c5e9e6df22e71a7b3044c0a2

DEV: Modernize Ember Resolver (#17353)
This switches us to use the modern ember resolver package, and re-implements a number of our custom resolution rules within it. The legacy resolver remains for now, and is used as a fallback if the modern resolver is unable to resolve a package. When this happens, a warning will be printed to the console.

Co-authored-by: Peter Wagenet <peter.wagenet@gmail.com>

app/assets/javascripts/discourse-common/addon/resolver.js
@@ -1,9 +1,10 @@
-/* eslint-disable no-undef */
-import { classify, dasherize, decamelize } from "@ember/string";
+/* global Ember */
+import { dasherize, decamelize } from "@ember/string";
 import deprecated from "discourse-common/lib/deprecated";
 import { findHelper } from "discourse-common/lib/helpers";
-import { get } from "@ember/object";
 import SuffixTrie from "discourse-common/lib/suffix-trie";
+import Resolver from "ember-resolver";
+import { buildResolver as buildLegacyResolver } from "discourse-common/lib/legacy-resolver";
 
 let _options = {};
 let moduleSuffixTrie = null;
@@ -20,23 +21,6 @@ export function clearResolverOptions() {
   _options = {};
 }
 
-function parseName(fullName) {
-  const nameParts = fullName.split(":");
-  const type = nameParts[0];
-  let fullNameWithoutType = nameParts[1];
-  const namespace = get(this, "namespace");
-  const root = namespace;
-
-  return {
-    fullName,
-    type,
-    fullNameWithoutType,
-    name: fullNameWithoutType,
-    root,
-    resolveMethodName: "resolve" + classify(type),
-  };
-}
-
 function lookupModuleBySuffix(suffix) {
   if (!moduleSuffixTrie) {
     moduleSuffixTrie = new SuffixTrie("/");
@@ -50,25 +34,32 @@ function lookupModuleBySuffix(suffix) {
 }
 
 export function buildResolver(baseName) {
-  return Ember.DefaultResolver.extend({
-    parseName,
+  let LegacyResolver = buildLegacyResolver(baseName);
 
-    resolveRouter(parsedName) {
+  return class extends Resolver {
+    LegacyResolver = LegacyResolver;
+
+    init(props) {
+      super.init(props);
+      this.legacyResolver = this.LegacyResolver.create(props);
+    }
+
+    resolveRouter(/* parsedName */) {
       const routerPath = `${baseName}/router`;
       if (requirejs.entries[routerPath]) {
         const module = requirejs(routerPath, null, null, true);
         return module.default;
       }
-      return this._super(parsedName);
-    },
+    }
 
-    normalize(fullName) {
+    // We overwrite this instead of `normalize` so we still get the benefits of the cache.
+    _normalize(fullName) {
       if (fullName === "app-events:main") {
         deprecated(
           "`app-events:main` has been replaced with `service:app-events`",
           { since: "2.4.0", dropFrom: "2.9.0.beta1" }
         );
-        return "service:app-events";
+        fullName = "service:app-events";
       }
 
       for (const [key, value] of Object.entries({
@@ -85,119 +76,110 @@ export function buildResolver(baseName) {
       })) {
         if (fullName === key) {
           deprecated(`${key} was replaced with ${value}`, { since: "2.6.0" });
-          return value;
+          fullName = value;
         }
       }
 
+      let normalized = super._normalize(fullName);
+
+      // This is code that we don't really want to keep long term. The main situation where we need it is for
+      // doing stuff like `controllerFor('adminWatchedWordsAction')` where the real route name
+      // is actually `adminWatchedWords.action`. The default behavior for the former is to
+      // normalize to `adminWatchedWordsAction` where the latter becomes `adminWatchedWords.action`.
+      // While these end up looking up the same file ultimately, they are treated as different
+      // items and so we can end up with two distinct version of the controller!
       const split = fullName.split(":");
-      if (split.length > 1) {
-        const appBase = `${baseName}/${split[0]}s/`;
-        const adminBase = "admin/" + split[0] + "s/";
-        const wizardBase = "wizard/" + split[0] + "s/";
+      const type = split[0];
+      if (
+        split.length > 1 &&
+        (type === "controller" || type === "route" || type === "template")
+      ) {
+        let corrected;
+        // This should only apply when there's a dot or slash in the name
+        if (split[1].includes(".") || split[1].includes("/")) {
+          // Check to see if the dasherized version exists. If it does we want to
+          // normalize to that eagerly so the normalized versions of the dotted/slashed and
+          // dotless/slashless match.
+          const dashed = dasherize(split[1].replace(/[\.\/]/g, "-"));
 
-        // Allow render 'admin/templates/xyz' too
-        split[1] = split[1].replace(".templates", "").replace("/templates", "");
-
-        // Try slashes
-        let dashed = dasherize(split[1].replace(/\./g, "/"));
-        if (
-          requirejs.entries[appBase + dashed] ||
-          requirejs.entries[adminBase + dashed] ||
-          requirejs.entries[wizardBase + dashed]
-        ) {
-          return split[0] + ":" + dashed;
+          const adminBase = `admin/${type}s/`;
+          const wizardBase = `wizard/${type}s/`;
+          if (
+            lookupModuleBySuffix(`${type}s/${dashed}`) ||
+            requirejs.entries[adminBase + dashed] ||
+            requirejs.entries[adminBase + dashed.replace(/^admin[-]/, "")] ||
+            requirejs.entries[
+              adminBase + dashed.replace(/^admin[-]/, "").replace(/-/g, "_")
+            ] ||
+            requirejs.entries[wizardBase + dashed] ||
+            requirejs.entries[wizardBase + dashed.replace(/^wizard[-]/, "")] ||
+            requirejs.entries[
+              wizardBase + dashed.replace(/^wizard[-]/, "").replace(/-/g, "_")
+            ]
+          ) {
+            corrected = type + ":" + dashed;
+          }
         }
 
-        // Try with dashes instead of slashes
-        dashed = dasherize(split[1].replace(/\./g, "-"));
-        if (
-          requirejs.entries[appBase + dashed] ||
-          requirejs.entries[adminBase + dashed] ||
-          requirejs.entries[wizardBase + dashed]
-        ) {
-          return split[0] + ":" + dashed;
-        }
-      }
-      return this._super(fullName);
-    },
-
-    customResolve(parsedName) {
-      // If we end with the name we want, use it. This allows us to define components within plugins.
-      const suffix = parsedName.type + "s/" + parsedName.fullNameWithoutType,
-        dashed = dasherize(suffix),
-        moduleName = lookupModuleBySuffix(dashed);
-
-      let module;
-      if (moduleName) {
-        module = requirejs(moduleName, null, null, true /* force sync */);
-        if (module && module["default"]) {
-          module = module["default"];
+        if (corrected && corrected !== normalized) {
+          normalized = corrected;
         }
       }
-      return module;
-    },
 
-    resolveWidget(parsedName) {
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
+      return normalized;
+    }
 
-    resolveAdapter(parsedName) {
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
+    chooseModuleName(moduleName, parsedName) {
+      let resolved = super.chooseModuleName(moduleName, parsedName);
+      if (resolved) {
+        return resolved;
+      }
 
-    resolveModel(parsedName) {
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
+      const standard = parsedName.fullNameWithoutType;
 
-    resolveView(parsedName) {
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
+      let variants = [standard];
+
+      if (standard.includes("/")) {
+        variants.push(parsedName.fullNameWithoutType.replace(/\//g, "-"));
+      }
+
+      for (let name of variants) {
+        // If we end with the name we want, use it. This allows us to define components within plugins.
+        const suffix = parsedName.type + "s/" + name;
+        resolved = lookupModuleBySuffix(dasherize(suffix));
+        if (resolved) {
+          return resolved;
+        }
+      }
+    }
+
+    resolveOther(parsedName) {
+      let resolved = super.resolveOther(parsedName);
+      if (!resolved) {
+        let legacyParsedName = this.legacyResolver.parseName(
+          `${parsedName.type}:${parsedName.fullName}`
+        );
+        resolved = this.legacyResolver.resolveOther(legacyParsedName);
+        if (resolved) {
+          deprecated(
+            `Unable to resolve with new resolver, but resolved with legacy resolver: ${parsedName.fullName}`
+          );
+        }
+      }
+      return resolved;
+    }
 
     resolveHelper(parsedName) {
-      return (
-        findHelper(parsedName.fullNameWithoutType) ||
-        this.customResolve(parsedName) ||
-        this._super(parsedName)
-      );
-    },
+      return findHelper(parsedName.fullNameWithoutType);
+    }
 
-    resolveController(parsedName) {
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
-
-    resolveComponent(parsedName) {
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
-
-    resolveService(parsedName) {
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
-
-    resolveRawView(parsedName) {
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
-
+    // If no match is found here, the resolver falls back to `resolveOther`.
     resolveRoute(parsedName) {
       if (parsedName.fullNameWithoutType === "basic") {
         return requirejs("discourse/routes/discourse", null, null, true)
           .default;
       }
-
-      return this.customResolve(parsedName) || this._super(parsedName);
-    },
-
-    findLoadingTemplate(parsedName) {
-      if (parsedName.fullNameWithoutType.match(/loading$/)) {
-        return Ember.TEMPLATES.loading;
-      }
-    },
-
-    findConnectorTemplate(parsedName) {
-      const full = parsedName.fullNameWithoutType.replace("components/", "");
-      if (full.indexOf("connectors") === 0) {
-        return Ember.TEMPLATES[`javascripts/${full}`];
-      }
-    },
+    }
 
     resolveTemplate(parsedName) {
       return (
@@ -205,132 +187,135 @@ export function buildResolver(baseName) {
         this.findPluginTemplate(parsedName) ||
         this.findMobileTemplate(parsedName) ||
         this.findTemplate(parsedName) ||
+        this.findAdminTemplate(parsedName) ||
+        this.findWizardTemplate(parsedName) ||
         this.findLoadingTemplate(parsedName) ||
         this.findConnectorTemplate(parsedName) ||
         Ember.TEMPLATES.not_found
       );
-    },
+    }
+
+    findLoadingTemplate(parsedName) {
+      if (parsedName.fullNameWithoutType.match(/loading$/)) {
+        return Ember.TEMPLATES.loading;
+      }
+    }
+
+    findConnectorTemplate(parsedName) {
+      if (parsedName.fullName.startsWith("template:connectors/")) {
+        const connectorParsedName = this.parseName(
+          parsedName.fullName
+            .replace("template:connectors/", "template:")
+            .replace("components/", "")
+        );
+        return this.findTemplate(connectorParsedName, "javascripts/");
+      }
+    }
 
     findPluginTemplate(parsedName) {
-      const pluginParsedName = this.parseName(
-        parsedName.fullName.replace("template:", "template:javascripts/")
-      );
-      return this.findTemplate(pluginParsedName);
-    },
+      return this.findTemplate(parsedName, "javascripts/");
+    }
 
     findPluginMobileTemplate(parsedName) {
       if (_options.mobileView) {
-        let pluginParsedName = this.parseName(
-          parsedName.fullName.replace(
-            "template:",
-            "template:javascripts/mobile/"
-          )
-        );
-        return this.findTemplate(pluginParsedName);
+        return this.findTemplate(parsedName, "javascripts/mobile/");
       }
-    },
+    }
 
     findMobileTemplate(parsedName) {
       if (_options.mobileView) {
-        let mobileParsedName = this.parseName(
-          parsedName.fullName.replace("template:", "template:mobile/")
-        );
-        return this.findTemplate(mobileParsedName);
+        return this.findTemplate(parsedName, "mobile/");
       }
-    },
+    }
 
-    findTemplate(parsedName) {
+    findTemplate(parsedName, prefix) {
+      prefix = prefix || "";
+
       const withoutType = parsedName.fullNameWithoutType,
-        slashedType = withoutType.replace(/\./g, "/"),
-        decamelized = decamelize(withoutType),
-        dashed = decamelized.replace(/\./g, "-").replace(/\_/g, "-"),
+        underscored = decamelize(withoutType).replace(/-/g, "_"),
+        segments = withoutType.split("/"),
         templates = Ember.TEMPLATES;
 
       return (
-        this._super(parsedName) ||
-        templates[slashedType] ||
-        templates[withoutType] ||
-        templates[withoutType.replace(/\.raw$/, "")] ||
-        templates[dashed] ||
-        templates[decamelized.replace(/\./, "/")] ||
-        templates[decamelized.replace(/\_/, "/")] ||
-        templates[`${baseName}/templates/${withoutType}`] ||
-        this.findAdminTemplate(parsedName) ||
-        this.findWizardTemplate(parsedName) ||
-        this.findUnderscoredTemplate(parsedName)
+        // Convert dots and dashes to slashes
+        templates[prefix + withoutType.replace(/[\.-]/g, "/")] ||
+        // Default unmodified behavior of original resolveTemplate.
+        templates[prefix + withoutType] ||
+        // Underscored without namespace
+        templates[prefix + underscored] ||
+        // Underscored with first segment as directory
+        templates[prefix + underscored.replace("_", "/")] ||
+        // Underscore only the last segment
+        templates[
+          `${prefix}${segments.slice(0, -1).join("/")}/${segments[
+            segments.length - 1
+          ].replace(/-/g, "_")}`
+        ] ||
+        // All dasherized
+        templates[prefix + withoutType.replace(/\//g, "-")]
       );
-    },
-
-    findUnderscoredTemplate(parsedName) {
-      let decamelized = decamelize(parsedName.fullNameWithoutType);
-      let underscored = decamelized.replace(/\-/g, "_");
-      return Ember.TEMPLATES[underscored];
-    },
+    }
 
     // Try to find a template within a special admin namespace, e.g. adminEmail => admin/templates/email
     // (similar to how discourse lays out templates)
     findAdminTemplate(parsedName) {
-      let decamelized = decamelize(parsedName.fullNameWithoutType);
-      if (decamelized.indexOf("components") === 0) {
-        let comPath = `admin/templates/${decamelized}`;
-        const compTemplate =
-          Ember.TEMPLATES[`javascripts/${comPath}`] || Ember.TEMPLATES[comPath];
-        if (compTemplate) {
-          return compTemplate;
-        }
-      }
-
-      if (decamelized === "javascripts/admin") {
+      if (parsedName.fullNameWithoutType === "admin") {
         return Ember.TEMPLATES["admin/templates/admin"];
       }
 
-      if (
-        decamelized.indexOf("admin") === 0 ||
-        decamelized.indexOf("javascripts/admin") === 0
-      ) {
-        decamelized = decamelized.replace(/^admin\_/, "admin/templates/");
-        decamelized = decamelized.replace(/^admin\./, "admin/templates/");
-        decamelized = decamelized.replace(/\./g, "_");
+      let namespaced, match;
 
-        const dashed = decamelized.replace(/_/g, "-");
-        return (
-          Ember.TEMPLATES[decamelized] ||
-          Ember.TEMPLATES[dashed] ||
-          Ember.TEMPLATES[dashed.replace("admin-", "admin/")]
-        );
+      if (parsedName.fullNameWithoutType.startsWith("components/")) {
+        // Look up components as-is
+      } else if (/^admin[_\.-]/.test(parsedName.fullNameWithoutType)) {
+        namespaced = parsedName.fullNameWithoutType.slice(6);
+      } else if (
+        (match = parsedName.fullNameWithoutType.match(/^admin([A-Z])(.+)$/))
+      ) {
+        namespaced = `${match[1].toLowerCase()}${match[2]}`;
       }
-    },
+
+      let resolved;
+
+      if (namespaced) {
+        let adminParsedName = this.parseName(`template:${namespaced}`);
+        resolved =
+          // Built-in
+          this.findTemplate(adminParsedName, "admin/templates/") ||
+          // Plugin
+          this.findTemplate(adminParsedName, "javascripts/admin/");
+      }
+
+      resolved ??=
+        // Built-in
+        this.findTemplate(parsedName, "admin/templates/") ||
+        // Plugin
+        this.findTemplate(parsedName, "javascripts/admin/");
+
+      return resolved;
+    }
 
     findWizardTemplate(parsedName) {
-      let decamelized = decamelize(parsedName.fullNameWithoutType);
-      if (decamelized.startsWith("components")) {
-        let comPath = `wizard/templates/${decamelized}`;
-        const compTemplate =
-          Ember.TEMPLATES[`javascripts/${comPath}`] || Ember.TEMPLATES[comPath];
-        if (compTemplate) {
-          return compTemplate;
-        }
-      }
-
-      if (decamelized === "javascripts/wizard") {
+      if (parsedName.fullNameWithoutType === "wizard") {
         return Ember.TEMPLATES["wizard/templates/wizard"];
       }
 
-      if (
-        decamelized.startsWith("wizard") ||
-        decamelized.startsWith("javascripts/wizard")
-      ) {
-        decamelized = decamelized.replace(/^wizard\_/, "wizard/templates/");
-        decamelized = decamelized.replace(/^wizard\./, "wizard/templates/");
-        decamelized = decamelized.replace(/\./g, "_");
+      let namespaced;
 
-        const dashed = decamelized.replace(/_/g, "-");
-        return (
-          Ember.TEMPLATES[decamelized] ||
-          Ember.TEMPLATES[dashed] ||
-          Ember.TEMPLATES[dashed.replace("wizard-", "wizard/")]
-        );
+      if (parsedName.fullNameWithoutType.startsWith("components/")) {
+        // Look up components as-is
+        namespaced = parsedName.fullNameWithoutType;
+      } else if (/^wizard[_\.-]/.test(parsedName.fullNameWithoutType)) {
+        // This may only get hit for the loading routes and may be removable.
+        namespaced = parsedName.fullNameWithoutType.slice(7);
       }
-    },
-  });
+
+      if (namespaced) {
+        let adminParsedName = this.parseName(
+          `template:wizard/templates/${namespaced}`
+        );
+        return this.findTemplate(adminParsedName);
+      }
+    }
+  };
 }

commit 547598776173d95f7e0bd7e22a1d6b00257fac2c

FIX: Ensure admin templates are not used for non-admin controllers
Previously, if a non-admin controller did not have a template defined, then the resolver would return an admin template with the same name. This is not the desired behavior, and regressed in fc36ac6cde2afaf8c5e9e6df22e71a7b3044c0a2. However, we *do* want this behavior for components defined in the admin bundle (because admin components are not namespaced).

This was noticed because the non-admin `badges` route was using the `admin/badges` template

This commit fixes the behavior, and adds a tests for these cases.

app/assets/javascripts/discourse-common/addon/resolver.js
@@ -274,7 +274,12 @@ export function buildResolver(baseName) {
       let namespaced, match;
 
       if (parsedName.fullNameWithoutType.startsWith("components/")) {
-        // Look up components as-is
+        return (
+          // Built-in
+          this.findTemplate(parsedName, "admin/templates/") ||
+          // Plugin
+          this.findTemplate(parsedName, "javascripts/admin/")
+        );
       } else if (/^admin[_\.-]/.test(parsedName.fullNameWithoutType)) {
         namespaced = parsedName.fullNameWithoutType.slice(6);
       } else if (
@@ -293,12 +298,6 @@ export function buildResolver(baseName) {
           // Plugin
           this.findTemplate(adminParsedName, "javascripts/admin/");
       }
-
-      resolved ??=
-        // Built-in
-        this.findTemplate(parsedName, "admin/templates/") ||
-        // Plugin
-        this.findTemplate(parsedName, "javascripts/admin/");
 
       return resolved;
     }

app/assets/javascripts/discourse/tests/unit/ember/resolver-test.js
@@ -317,6 +317,7 @@ module("Unit | Ember | resolver", function (hooks) {
       "admin/templates/dashboard_general",
       "admin-baz-qux",
       "javascripts/admin/plugin-template",
+      "admin/templates/components/my-admin-component",
     ]);
 
     // Switches prefix to admin/templates when camelized
@@ -394,6 +395,20 @@ module("Unit | Ember | resolver", function (hooks) {
       "template:admin-plugin/template",
       "javascripts/admin/plugin-template",
       "looks up templates in plugins"
+    );
+
+    lookupTemplate(
+      assert,
+      "template:foo",
+      undefined,
+      "doesn't return admin templates for regular controllers"
+    );
+
+    lookupTemplate(
+      assert,
+      "template:components/my-admin-component",
+      "admin/templates/components/my-admin-component",
+      "returns admin-defined component templates"
     );
   });
 