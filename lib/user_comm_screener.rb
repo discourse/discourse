b5064f67b9814006190ef39a41b5f59204900fd4
Martin Brennan <martin@discourse.org>
2022-07-28 12:30:39 +1000

    DEV: Review fix

--- lib/user_comm_screener.rb
+++ lib/user_comm_screener.rb
@@ -99,7 +99,8 @@ class UserCommScreener
   def initialize(acting_user: nil, acting_user_id: nil, target_user_ids:)
     raise ArgumentError if acting_user.blank? && acting_user_id.blank?
     @acting_user = acting_user.present? ? acting_user : User.find(acting_user_id)
-    @target_users = User.where(id: target_user_ids).where.not(id: @acting_user.id).pluck(:id, :username).to_h
+    target_user_ids = Array.wrap(target_user_ids) - [@acting_user.id]
+    @target_users = User.where(id: target_user_ids).pluck(:id, :username).to_h
     @preferences = load_preference_map
   end
 
