956c79dd1e06982c33b49b258c0951078e2d1009
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-08-03 23:36:00 +0400

    Add new copy

--- app/assets/javascripts/discourse/app/routes/user-activity-replies.js
+++ app/assets/javascripts/discourse/app/routes/user-activity-replies.js
@@ -2,6 +2,8 @@ import UserAction from "discourse/models/user-action";
 import UserActivityStreamRoute from "discourse/routes/user-activity-stream";
 import I18n from "I18n";
 import { action } from "@ember/object";
+import { htmlSafe } from "@ember/template";
+import getURL from "discourse-common/lib/get-url";
 
 export default UserActivityStreamRoute.extend({
   userActionType: UserAction.TYPES["posts"],
@@ -12,7 +14,11 @@ export default UserActivityStreamRoute.extend({
     let title, body;
     if (this.isCurrentUser(user)) {
       title = I18n.t("user_activity.no_replies_title");
-      body = "";
+      body = htmlSafe(
+        I18n.t("user_activity.no_replies_body", {
+          searchUrl: getURL("/search"),
+        })
+      );
     } else {
       title = I18n.t("user_activity.no_replies_title_others", {
         username: user.username,
