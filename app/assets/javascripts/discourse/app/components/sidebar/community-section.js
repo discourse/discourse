c3a242ccb2c5ff41c053c90159e05811d249a350
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-08-04 14:18:20 +0800

    Support old hamburger menu custom footer links in Sidebar
    This commit extends the existing API bridge for supporting custom
    general links in the old hamburger menu in Sidebar to support custom
    footer links. Custom footer links can be added to the old hamburger
    menu via the `api.decorateWidget("hamburger-menu:footerLinks")` API.
    Footer links are added into the secondary section of the "More..." links
    drawer in the Community section of the sidebar.

--- app/assets/javascripts/discourse/app/components/sidebar/community-section.js
+++ app/assets/javascripts/discourse/app/components/sidebar/community-section.js
@@ -2,7 +2,10 @@ import GlimmerComponent from "discourse/components/glimmer";
 import Composer from "discourse/models/composer";
 import { getOwner } from "discourse-common/lib/get-owner";
 import PermissionType from "discourse/models/permission-type";
-import { customSectionLinks } from "discourse/lib/sidebar/custom-community-section-links";
+import {
+  customSectionLinks,
+  secondaryCustomSectionLinks,
+} from "discourse/lib/sidebar/custom-community-section-links";
 import EverythingSectionLink from "discourse/lib/sidebar/community-section/everything-section-link";
 import TrackedSectionLink from "discourse/lib/sidebar/community-section/tracked-section-link";
 import MyPostsSectionLink from "discourse/lib/sidebar/community-section/my-posts-section-link";
@@ -36,11 +39,12 @@ export default class SidebarCommunitySection extends GlimmerComponent {
     }
   );
 
-  moreSecondarySectionLinks = MORE_SECONDARY_SECTION_LINKS.map(
-    (sectionLinkClass) => {
-      return this.#initializeSectionLink(sectionLinkClass);
-    }
-  );
+  moreSecondarySectionLinks = [
+    ...MORE_SECONDARY_SECTION_LINKS,
+    ...secondaryCustomSectionLinks,
+  ].map((sectionLinkClass) => {
+    return this.#initializeSectionLink(sectionLinkClass);
+  });
 
   #mainSectionLinks = this.currentUser.staff
     ? [...MAIN_SECTION_LINKS, ...ADMIN_MAIN_SECTION_LINKS]
