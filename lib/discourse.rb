c7ad4126ca47a6bb1b11c8f6d021d5810a5b68a2
Sam <sam.saffron@gmail.com>
2022-08-05 11:26:25 +1000

    Update lib/discourse.rb
    Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

--- lib/discourse.rb
+++ lib/discourse.rb
@@ -186,7 +186,7 @@ module Discourse
     end
 
     # internal reporting
-    if job.class == Class && ::Jobs::Base > job
+    if job.class == Class && job.is_a?(::Jobs::Base)
       job_exception_stats[job] += 1
     end
 
