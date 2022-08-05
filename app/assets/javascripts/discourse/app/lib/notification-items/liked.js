a29fad12279debc114ffcd554256ddbec3be6da6
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-08-05 06:27:30 +0300

    private-ize username2

--- app/assets/javascripts/discourse/app/lib/notification-items/liked.js
+++ app/assets/javascripts/discourse/app/lib/notification-items/liked.js
@@ -7,12 +7,12 @@ export default class extends NotificationItemBase {
     if (this.count === 2) {
       return I18n.t("notifications.liked_by_2_users", {
         username: this.username,
-        username2: this.username2,
+        username2: this.#username2,
       });
     } else if (this.count > 2) {
       return I18n.t("notifications.liked_by_multiple_users", {
         username: this.username,
-        username2: this.username2,
+        username2: this.#username2,
         count: this.count - 2,
       });
     } else {
@@ -32,7 +32,7 @@ export default class extends NotificationItemBase {
     return this.notification.data.count;
   }
 
-  get username2() {
+  get #username2() {
     return formatUsername(this.notification.data.username2);
   }
 }
