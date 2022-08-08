5200d236379875e74ce06d574d7805e257fd4db2
Arpit Jalan <er.ajalan@gmail.com>
2022-08-08 12:10:18 +0530

    FEATURE: update bootstrap mode notice to add invite and wizard links

---
+++ app/assets/javascripts/discourse/app/components/bootstrap_mode_notice.js
@@ -0,0 +1,39 @@
+import discourseComputed from "discourse-common/utils/decorators";
+import Component from "@ember/component";
+import I18n from "I18n";
+import { alias } from "@ember/object/computed";
+import { htmlSafe } from "@ember/template";
+import { inject as service } from "@ember/service";
+
+export default Component.extend({
+  router: service(),
+  classNameBindings: ["hidden:hidden", ":bootstrap-notice"],
+
+  wizardRequired: alias("site.wizard_required"),
+  bootstrapModeEnabled: alias("siteSettings.bootstrap_mode_enabled"),
+  bootstrapModeMinUsers: alias("siteSettings.bootstrap_mode_min_users"),
+
+  @discourseComputed("bootstrapModeEnabled", "router.currentRouteName")
+  hidden(bootstrapModeEnabled, currentRouteName) {
+    const user = this.currentUser;
+    return !(
+      user &&
+      user.get("staff") &&
+      bootstrapModeEnabled &&
+      !currentRouteName.startsWith("wizard")
+    );
+  },
+
+  @discourseComputed("bootstrapModeMinUsers")
+  message(bootstrapModeMinUsers) {
+    let msg = null;
+
+    if (bootstrapModeMinUsers > 0) {
+      msg = "bootstrap_mode_enabled";
+    } else {
+      msg = "bootstrap_mode_disabled";
+    }
+
+    return htmlSafe(I18n.t(msg, { count: bootstrapModeMinUsers }));
+  },
+});
