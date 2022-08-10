186ad7fecc6befabe452a822e7bc12e05f5f2a81
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-08-10 09:44:13 +0800

    Minor review fixes

--- app/assets/javascripts/discourse/app/widgets/header-contents.js
+++ app/assets/javascripts/discourse/app/widgets/header-contents.js
@@ -4,13 +4,13 @@ import hbs from "discourse/widgets/hbs-compiler";
 createWidget("header-contents", {
   tagName: "div.contents.clearfix",
   template: hbs`
-    {{#unless this.site.mobileView}}
+    {{#if this.site.desktopView}}
       {{#if this.siteSettings.enable_experimental_sidebar_hamburger}}
         {{#if attrs.sidebarEnabled}}
           {{sidebar-toggle attrs=attrs}}
         {{/if}}
       {{/if}}
-    {{/unless}}
+    {{/if}}
     {{home-logo attrs=attrs}}
     {{#if attrs.topic}}
       {{header-topic-info attrs=attrs}}
