d3ed07837873452fe02d42843f89466a25b94575
Loïc Guitaut <loic@discourse.org>
2022-08-10 11:56:39 +0200

    Version bump to v2.9.0.beta9

--- lib/version.rb
+++ lib/version.rb
@@ -10,7 +10,7 @@ module Discourse
       MAJOR = 2
       MINOR = 9
       TINY  = 0
-      PRE   = 'beta8'
+      PRE   = 'beta9'
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     end
