55770cda55ec4fa143fb55622ade30e04886db02
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-08-12 11:04:45 +0800

    DEV: Remove undefined in Sidebar::SectionLink class attribute

---
+++ app/assets/javascripts/discourse/tests/integration/components/sidebar/section-link-test.js
@@ -0,0 +1,35 @@
+import { module, test } from "qunit";
+
+import { hbs } from "ember-cli-htmlbars";
+import { render } from "@ember/test-helpers";
+
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+import { query } from "discourse/tests/helpers/qunit-helpers";
+
+module("Integration | Component | sidebar | section-link", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("default class attribute for link", async function (assert) {
+    const template = hbs`<Sidebar::SectionLink @linkName="test" @route="discovery.latest" />`;
+
+    await render(template);
+
+    assert.strictEqual(
+      query("a").className,
+      "sidebar-section-link sidebar-section-link-test ember-view",
+      "has the right class attribute for the link"
+    );
+  });
+
+  test("custom class attribute for link", async function (assert) {
+    const template = hbs`<Sidebar::SectionLink @linkName="test" @route="discovery.latest" @class="123 abc" />`;
+
+    await render(template);
+
+    assert.strictEqual(
+      query("a").className,
+      "sidebar-section-link sidebar-section-link-test 123 abc ember-view",
+      "has the right class attribute for the link"
+    );
+  });
+});
