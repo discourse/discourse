e49167b311db1ef6f18c7bfdaf44a552802592d9
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-08-18 18:00:15 +0400

    UX: Improve empty state copy on the activity/replies page (#17784)

--- app/assets/javascripts/discourse/app/routes/user-activity-replies.js
+++ app/assets/javascripts/discourse/app/routes/user-activity-replies.js
@@ -2,6 +2,8 @@ import UserAction from "discourse/models/user-action";
 import UserActivityStreamRoute from "discourse/routes/user-activity-stream";
 import I18n from "I18n";
 import { action } from "@ember/object";
+import { htmlSafe } from "@ember/template";
+import getURL from "discourse-common/lib/get-url";
 
 export default UserActivityStreamRoute.extend({
   userActionType: UserAction.TYPES["posts"],
@@ -9,12 +11,21 @@ export default UserActivityStreamRoute.extend({
   emptyState() {
     const user = this.modelFor("user");
 
-    const title = this.isCurrentUser(user)
-      ? I18n.t("user_activity.no_replies_title")
-      : I18n.t("user_activity.no_replies_title_others", {
-          username: user.username,
-        });
-    const body = "";
+    let title, body;
+    if (this.isCurrentUser(user)) {
+      title = I18n.t("user_activity.no_replies_title");
+      body = htmlSafe(
+        I18n.t("user_activity.no_replies_body", {
+          searchUrl: getURL("/search"),
+        })
+      );
+    } else {
+      title = I18n.t("user_activity.no_replies_title_others", {
+        username: user.username,
+      });
+      body = "";
+    }
+
     return { title, body };
   },
 
