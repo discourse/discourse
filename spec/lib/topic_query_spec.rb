73b25222614d0ce91735d079651bc35df69ae829
jbrw <jamie.wilson@discourse.org>
2022-08-19 15:41:56 -0400

    FIX: Allow `match_all_tags` to be passed as a URL param (#17972)
    `TopicQueryParams` allows for `match_all_tags` to be passed as a query parameter. `TagsController` forces the value to be true.
    
    This change allows a value to be passed, and only sets it to true if no value has been set. It then uses `ActiveModel::Type::Boolean.new.cast` to compare the value.

--- spec/lib/topic_query_spec.rb
+++ spec/lib/topic_query_spec.rb
@@ -427,6 +427,10 @@ RSpec.describe TopicQuery do
         expect(TopicQuery.new(moderator, tags: [tag.name, other_tag.name], match_all_tags: true).list_latest.topics.map(&:id)).to eq([tagged_topic3.id])
       end
 
+      it "can return topics with tag intersections using truthy/falsey values" do
+        expect(TopicQuery.new(moderator, tags: [tag.name, other_tag.name], match_all_tags: "false").list_latest.topics.map(&:id).sort).to eq([tagged_topic1.id, tagged_topic2.id, tagged_topic3.id].sort)
+      end
+
       it "returns an empty relation when an invalid tag is passed" do
         expect(TopicQuery.new(moderator, tags: [tag.name, 'notatag'], match_all_tags: true).list_latest.topics).to be_empty
       end
