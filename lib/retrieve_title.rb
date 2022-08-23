e12d58ba820138bbef8e77ee9be6c5ea0579b8f6
Sam Saffron <sam.saffron@gmail.com>
2022-08-23 15:13:33 +1000

    improve comment

--- lib/retrieve_title.rb
+++ lib/retrieve_title.rb
@@ -25,6 +25,7 @@ module RetrieveTitle
     rescue ArgumentError
       # invalid HTML (Eg: too many attributes, status tree too deep) - ignore
       # Error in nokogumbo is not specialized, uses generic ArgumentError
+      # see: https://www.rubydoc.info/gems/nokogiri/Nokogiri/HTML5#label-Error+reporting
     end
 
     if doc
