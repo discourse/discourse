1b73c8bb32ee7bd94d6b27e5c2470ec4af9d807d
David Taylor <david@taylorhq.com>
2022-08-23 09:52:06 +0100

    Cleanup and prettier

--- app/assets/javascripts/discourse/public/assets/scripts/discourse-test-load-dynamic-js.js
+++ app/assets/javascripts/discourse/public/assets/scripts/discourse-test-load-dynamic-js.js
@@ -3,23 +3,26 @@ const dynamicJsTemplate = document.querySelector("#dynamic-test-js");
 const params = new URLSearchParams(document.location.search);
 const skipPlugins = params.get("qunit_skip_plugins");
 
-for(const element of Array.from(dynamicJsTemplate.content.childNodes)){
-  const clone = element.cloneNode(true);
-
-  if(skipPlugins && clone.dataset?.discoursePlugin){
+for (const element of Array.from(dynamicJsTemplate.content.childNodes)) {
+  if (skipPlugins && element.dataset?.discoursePlugin) {
     continue;
   }
 
-  if(clone.tagName === "SCRIPT" && clone.innerHTML.includes("EmberENV.TESTS_FILE_LOADED")){
+  if (
+    element.tagName === "SCRIPT" &&
+    element.innerHTML.includes("EmberENV.TESTS_FILE_LOADED")
+  ) {
     // Inline script introduced by ember-cli. Incompatible with CSP and our custom plugin JS loading system
     // https://github.com/ember-cli/ember-cli/blob/04a38fda2c/lib/utilities/ember-app-utils.js#L131
     // We re-implement in test-boot-ember-cli.js
     continue;
   }
 
-  if(clone.tagName=== "SCRIPT"){
+  const clone = element.cloneNode(true);
+
+  if (clone.tagName === "SCRIPT") {
     clone.async = false;
   }
 
-  document.body.appendChild(clone)
+  document.body.appendChild(clone);
 }
