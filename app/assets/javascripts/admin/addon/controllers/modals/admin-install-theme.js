a5dd8e005faf9ddc73384847827f33401f301a29
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-08-25 22:54:22 +0300

    FIX: Allow .git end to miss from Git repo URL
    When installing private themes and theme components, the public key does
    not show until the administrator types a valid Git repo URL. The regular
    expression that checked the URL was too strict and it required the URL
    to end with ".git".

--- app/assets/javascripts/admin/addon/controllers/modals/admin-install-theme.js
+++ app/assets/javascripts/admin/addon/controllers/modals/admin-install-theme.js
@@ -24,7 +24,10 @@ export default Controller.extend(ModalFunctionality, {
   keyGenUrl: "/admin/themes/generate_key_pair",
   importUrl: "/admin/themes/import",
   recordType: "theme",
-  checkPrivate: match("uploadUrl", /^ssh\:\/\/.*\@.*\.git$|.*\@.*\:.*\.git$/),
+  checkPrivate: match(
+    "uploadUrl",
+    /^ssh\:\/\/.*\@.*(?:\.git)?$|.*\@.*\:.*(?:\.git)?$/
+  ),
   localFile: null,
   uploadUrl: null,
   uploadName: null,
