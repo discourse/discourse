bbdffff41f983c64052d5e58b4951ef41d4bf921
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-08-29 15:21:07 +0400

    DEV: use the format-age helper on the DnD button on the new user menu (#18108)

--- app/assets/javascripts/discourse/app/components/user-menu/profile-tab-content.js
+++ app/assets/javascripts/discourse/app/components/user-menu/profile-tab-content.js
@@ -2,7 +2,6 @@ import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 import showModal from "discourse/lib/show-modal";
-import { longDate, relativeAge } from "discourse/lib/formatter";
 
 export default class UserMenuProfileTabContent extends Component {
   @service currentUser;
@@ -20,14 +19,6 @@ export default class UserMenuProfileTabContent extends Component {
 
   get isInDoNotDisturb() {
     return !!this.#doNotDisturbUntilDate;
-  }
-
-  get doNotDisturbDateTitle() {
-    return longDate(this.#doNotDisturbUntilDate);
-  }
-
-  get doNotDisturbDateContent() {
-    return relativeAge(this.#doNotDisturbUntilDate);
   }
 
   get doNotDisturbDateTime() {
