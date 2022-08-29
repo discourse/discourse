58840a788894393c6faaadbfd39489888fd20232
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-08-29 14:50:18 +0400

    Remove an unused getter

--- app/assets/javascripts/discourse/app/components/user-menu/profile-tab-content.js
+++ app/assets/javascripts/discourse/app/components/user-menu/profile-tab-content.js
@@ -2,7 +2,6 @@ import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 import showModal from "discourse/lib/show-modal";
-import { relativeAge } from "discourse/lib/formatter";
 
 export default class UserMenuProfileTabContent extends Component {
   @service currentUser;
@@ -20,10 +19,6 @@ export default class UserMenuProfileTabContent extends Component {
 
   get isInDoNotDisturb() {
     return !!this.#doNotDisturbUntilDate;
-  }
-
-  get doNotDisturbDateContent() {
-    return relativeAge(this.#doNotDisturbUntilDate);
   }
 
   get doNotDisturbDateTime() {
