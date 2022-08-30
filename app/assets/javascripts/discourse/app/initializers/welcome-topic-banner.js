0c1a2402c876a8a43aade4c16ab66db683603e4b
Arpit Jalan <er.ajalan@gmail.com>
2022-08-30 07:42:54 +0530

    Do not lookup for messageBus service if it's not required

--- app/assets/javascripts/discourse/app/initializers/welcome-topic-banner.js
+++ app/assets/javascripts/discourse/app/initializers/welcome-topic-banner.js
@@ -3,13 +3,13 @@ export default {
   after: "message-bus",
 
   initialize(container) {
-    const messageBus = container.lookup("service:message-bus");
-    if (!messageBus) {
-      return;
-    }
-
     const site = container.lookup("service:site");
+
     if (site.get("show_welcome_topic_banner")) {
+      const messageBus = container.lookup("service:message-bus");
+      if (!messageBus) {
+        return;
+      }
       messageBus.subscribe("/site/welcome-topic-banner", function (disabled) {
         site.set("show_welcome_topic_banner", disabled);
       });
