discourse/discourse

commit 32346f4ba5a4c1d9f30ca56fdab3d4e2915ba0e8

FIX: Ensure lazy-load-images does not remove entire `img.style` (#16553)
Other things may have added things to the style attribute (e.g. the `image-aspect-ratio` decorator).

Unfortunately this is difficult to add a test for because `lazy-load-images` leans on the `onload` event. We have no control over image loading in tests, so race conditions would be very likely.

app/assets/javascripts/discourse/app/lib/lazy-load-images.js
@@ -18,11 +18,16 @@ export function nativeLazyLoading(api) {
           if (!img.complete) {
             if (!img.onload) {
               img.onload = () => {
-                img.removeAttribute("style");
+                img.style.removeProperty("background-image");
+                img.style.removeProperty("background-size");
               };
             }
 
-            img.style = `background-image: url(${img.dataset.smallUpload}); background-size: cover;`;
+            img.style.setProperty(
+              "background-image",
+              `url(${img.dataset.smallUpload});`
+            );
+            img.style.setProperty("background-size", "cover");
           }
         }
       }),

commit 85a34f36fe178a40ad477ce5dfb9256318284b39

FIX: Ensure low-resolution placeholders are used while loading images
This regressed in 32346f4ba5. Unfortunately this behaviour is very difficult to test because it involves onload callbacks and scrolling-triggered lazy-loading

app/assets/javascripts/discourse/app/lib/lazy-load-images.js
@@ -25,7 +25,7 @@ export function nativeLazyLoading(api) {
 
             img.style.setProperty(
               "background-image",
-              `url(${img.dataset.smallUpload});`
+              `url(${img.dataset.smallUpload})`
             );
             img.style.setProperty("background-size", "cover");
           }