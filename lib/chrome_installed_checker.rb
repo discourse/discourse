9e1a8be982d3be888fcf8855b8f90fa989415405
David Taylor <david@taylorhq.com>
2022-09-23 17:32:18 +0100

    Revert ChromeInstalledChecker changes
    This refactoring doesn't seem to be required any more - reverting to clean up the diff

--- lib/chrome_installed_checker.rb
+++ lib/chrome_installed_checker.rb
@@ -8,7 +8,7 @@ class ChromeInstalledChecker
   class ChromeNotInstalled < ChromeError; end
   class ChromeVersionTooLow < ChromeError; end
 
-  def self.binary_name
+  def self.run
     if RbConfig::CONFIG['host_os'][/darwin|mac os/]
       binary = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
     elsif system("command -v google-chrome-stable >/dev/null;")
@@ -16,12 +16,6 @@ class ChromeInstalledChecker
     end
     binary ||= "google-chrome" if system("command -v google-chrome >/dev/null;")
     binary ||= "chromium" if system("command -v chromium >/dev/null;")
-
-    binary
-  end
-
-  def self.run
-    binary = ChromeInstalledChecker.binary_name
 
     if !binary
       raise ChromeNotInstalled.new("Chrome is not installed. Download from https://www.google.com/chrome/browser/desktop/index.html")
