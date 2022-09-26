0f7a5a9aad9d9f4a0b139a1633632d1203f73662
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-09-26 17:43:07 +0400

    FEATURE: Optionally show user status on email group user chooser

--- app/assets/javascripts/discourse/tests/integration/components/select-kit/email-group-user-chooser-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/select-kit/email-group-user-chooser-test.js
@@ -1,9 +1,10 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
-import { render } from "@ember/test-helpers";
+import { fillIn, render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import selectKit from "discourse/tests/helpers/select-kit-helper";
-import { paste, query } from "discourse/tests/helpers/qunit-helpers";
+import { exists, paste, query } from "discourse/tests/helpers/qunit-helpers";
+import pretender, { response } from "../../../helpers/create-pretender";
 
 module(
   "Integration | Component | select-kit/email-group-user-chooser",
@@ -21,6 +22,63 @@ module(
       await paste(query(".filter-input"), "foo,bar");
 
       assert.equal(this.subject.header().value(), "foo,bar");
+    });
+
+    test("doesn't show user status by default", async function (assert) {
+      pretender.get("/u/search/users", () =>
+        response({
+          users: [
+            {
+              username: "test-user",
+              status: {
+                description: "off to dentist",
+                emoji: "tooth",
+              },
+            },
+          ],
+        })
+      );
+
+      await render(hbs`<EmailGroupUserChooser />`);
+      await this.subject.expand();
+      await fillIn(".filter-input", "test-user");
+
+      assert.notOk(exists(".user-status-message"));
+    });
+
+    test("shows user status if enabled", async function (assert) {
+      const status = {
+        description: "off to dentist",
+        emoji: "tooth",
+      };
+      pretender.get("/u/search/users", () =>
+        response({
+          users: [
+            {
+              username: "test-user",
+              status,
+            },
+          ],
+        })
+      );
+
+      await render(hbs`<EmailGroupUserChooser @showUserStatus=true />`);
+      await this.subject.expand();
+      await fillIn(".filter-input", "test-user");
+
+      assert.ok(exists(".user-status-message"), "user status is rendered");
+      assert.equal(
+        query(".user-status-message .emoji").alt,
+        status.emoji,
+        "status emoji is correct"
+      );
+      assert.equal(
+        query(
+          ".user-status-message .user-status-message-description"
+        ).innerText.trim(),
+        status.description,
+        "status description is correct"
+      );
     });
   }
 );
