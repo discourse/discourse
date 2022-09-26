b81afa07560bf1eb2ed3c867792e1e0ee6be840a
Bianca Nenciu <nbianca@users.noreply.github.com>
2022-09-26 16:37:56 +0300

    FIX: Update user results page when no users found (#18363)
    The page was not updated if the server did not return any results. This
    caused the page to be either empty or display the previous result set.

--- app/assets/javascripts/discourse/tests/helpers/create-pretender.js
+++ app/assets/javascripts/discourse/tests/helpers/create-pretender.js
@@ -670,6 +670,12 @@ export function applyDefaultHandlers(pretender) {
       },
     ];
 
+    if (request.queryParams.filter) {
+      store = store.filter((user) =>
+        user.username.includes(request.queryParams.filter)
+      );
+    }
+
     const showEmails = request.queryParams.show_emails;
 
     if (showEmails === "false") {
