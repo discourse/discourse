827abf098b56b36c1a06830cefb83169fae013da
Martin Brennan <martin@discourse.org>
2022-09-29 16:37:00 +1000

    DEV: Load client site settings YML into JS tests
    Our method of loading a subset of client settings into
    tests via tests/helpers/site-settings.js can be improved
    upon. Currently we have a hardcoded subset of the client
    settings, which may get out of date and not have the correct
    defaults. As well as this plugins do not get their settings
    into the tests, so whenever you need a setting from a plugin,
    even if it has a default, you have to do needs.setting({ ... })
    which is inconvenient.
    
    This commit introduces an ember CLI build step to take the
    site_settings.yml and all the plugin settings.yml files,
    pull out the client settings, and dump them into a variable
    in a single JS file we can load in our tests, so we have the
    correct selection of settings and default values in our JS
    tests.

---
+++ app/assets/javascripts/discourse/lib/site-settings-plugin.js
@@ -0,0 +1,79 @@
+const Plugin = require("broccoli-plugin");
+const Yaml = require("js-yaml");
+const fs = require("fs");
+const concat = require("broccoli-concat");
+const mergeTrees = require("broccoli-merge-trees");
+const deepmerge = require("deepmerge");
+const glob = require("glob");
+const { shouldLoadPluginTestJs } = require("discourse/lib/plugin-js");
+
+let built = false;
+
+class SiteSettingsPlugin extends Plugin {
+  constructor(inputNodes, inputFile, options) {
+    super(inputNodes, {
+      ...options,
+      persistentOutput: true,
+    });
+  }
+
+  build() {
+    if (built) {
+      return;
+    }
+
+    let parsed = {};
+
+    this.inputPaths.forEach((path) => {
+      let inputFile;
+      if (path.includes("plugins")) {
+        inputFile = "settings.yml";
+      } else {
+        inputFile = "site_settings.yml";
+      }
+      let file = path + "/" + inputFile;
+      let yaml = fs.readFileSync(file, { encoding: "UTF-8" });
+      let loaded = Yaml.load(yaml, { json: true });
+      parsed = deepmerge(parsed, loaded);
+    });
+
+    let clientSettings = {};
+    // eslint-disable-next-line no-unused-vars
+    for (const [category, settings] of Object.entries(parsed)) {
+      for (const [setting, details] of Object.entries(settings)) {
+        if (details.client) {
+          clientSettings[setting] = details.default;
+        }
+      }
+    }
+    let contents = `var CLIENT_SITE_SETTINGS_WITH_DEFAULTS  = ${JSON.stringify(
+      clientSettings
+    )}`;
+
+    fs.writeFileSync(`${this.outputPath}/` + "settings_out.js", contents);
+    built = true;
+  }
+}
+
+module.exports = function siteSettingsPlugin(...params) {
+  return new SiteSettingsPlugin(...params);
+};
+
+module.exports.parsePluginClientSettings = function (discourseRoot) {
+  let settings = [discourseRoot + "/config"];
+
+  if (shouldLoadPluginTestJs()) {
+    settings = settings.concat(glob.sync(discourseRoot + "/plugins/*/config"));
+  }
+
+  let loadedSettings = new SiteSettingsPlugin(settings, "site_settings.yml");
+
+  return concat(mergeTrees([loadedSettings]), {
+    inputFiles: [],
+    headerFiles: [],
+    footerFiles: [],
+    outputFile: `assets/test-site-settings.js`,
+  });
+};
+
+module.exports.SiteSettingsPlugin = SiteSettingsPlugin;
