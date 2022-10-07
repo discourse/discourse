dfbab78c7e5e12050cd523a8907911d016b7f905
Penar Musaraj <pmusaraj@gmail.com>
2022-10-07 11:26:54 -0400

    Cleanup

--- app/assets/javascripts/discourse/app/components/category-logo.js
+++ app/assets/javascripts/discourse/app/components/category-logo.js
@@ -4,10 +4,6 @@ import { inject as service } from "@ember/service";
 export default class CategoryLogo extends Component {
   @service session;
 
-  constructor() {
-    super(...arguments);
-  }
-
   get defaultCategoryLogo() {
     // use dark logo by default in edge case
     // when scheme is dark and dark logo is present
