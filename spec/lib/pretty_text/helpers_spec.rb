227a971082225384e87b8f522cfbb5c02ee73411
Martin Brennan <martin@discourse.org>
2022-10-10 16:52:22 +1000

    DEV: Add specs for hashtags related code
    Adding a couple of small specs to code around hashtags
    for tags and categories since I will be working in this
    area soon and these were not covered.

--- spec/lib/pretty_text/helpers_spec.rb
+++ spec/lib/pretty_text/helpers_spec.rb
@@ -15,4 +15,35 @@ RSpec.describe PrettyText::Helpers do
       expect(result[short_url][:url]).to eq("https://awesome.com#{upload.url}")
     end
   end
+
+  describe ".category_tag_hashtag_lookup" do
+    fab!(:tag) { Fabricate(:tag, name: "somecooltag") }
+    fab!(:category) do
+      Fabricate(:category, name: "Some Awesome Category", slug: "someawesomecategory")
+    end
+
+    it "handles tags based on slug with TAG_HASHTAG_POSTFIX" do
+      expect(
+        PrettyText::Helpers.category_tag_hashtag_lookup(
+          +"somecooltag#{PrettyText::Helpers::TAG_HASHTAG_POSTFIX}",
+        ),
+      ).to eq([tag.url, "somecooltag"])
+    end
+
+    it "handles categories based on slug" do
+      expect(PrettyText::Helpers.category_tag_hashtag_lookup("someawesomecategory")).to eq(
+        [category.url, "someawesomecategory"],
+      )
+    end
+
+    it "handles tags based on slug without TAG_HASHTAG_POSTFIX" do
+      expect(PrettyText::Helpers.category_tag_hashtag_lookup(+"somecooltag")).to eq(
+        [tag.url, "somecooltag"],
+      )
+    end
+
+    it "returns nil when no tag or category that matches exists" do
+      expect(PrettyText::Helpers.category_tag_hashtag_lookup("blah")).to eq(nil)
+    end
+  end
 end
