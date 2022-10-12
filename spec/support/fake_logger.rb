a4437ab30be496cb0e63816989afbc20215ed4b1
Jarek Radosz <jradosz@gmail.com>
2022-10-12 16:06:00 +0200

    DEV: Fix FakeLogger compat (#18562)
    TaggedLogger requires these methods (see plugin backend failures in #18534)

--- spec/support/fake_logger.rb
+++ spec/support/fake_logger.rb
@@ -16,20 +16,40 @@ class FakeLogger
     @debug << message
   end
 
+  def debug?
+    @level <= Logger::DEBUG
+  end
+
   def info(message = nil)
     @infos << message
+  end
+
+  def info?
+    @level <= Logger::INFO
   end
 
   def warn(message)
     @warnings << message
   end
 
+  def warn?
+    @level <= Logger::WARN
+  end
+
   def error(message)
     @errors << message
   end
 
+  def error?
+    @level <= Logger::ERROR
+  end
+
   def fatal(message)
     @fatals << message
+  end
+
+  def fatal?
+    @level <= Logger::FATAL
   end
 
   def formatter
