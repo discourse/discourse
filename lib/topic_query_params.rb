97a4b70dc99eaca8ae6c2b96439dd3e706978662
Blake Erickson <o.blakeerickson@gmail.com>
2022-10-17 16:03:48 -0600

    FEATURE: Hide welcome topic if it hasn't been edited
    For non-admins this change will hide the welcome topic if it hasn't been
    edited yet.

--- lib/topic_query_params.rb
+++ lib/topic_query_params.rb
@@ -18,6 +18,21 @@ module TopicQueryParams
     options[:topic_ids] = param_to_integer_list(:topic_ids)
     options[:no_subcategories] = options[:no_subcategories] == 'true' if options[:no_subcategories].present?
 
+    if hide_welcome_topic?
+      if options[:except_topic_ids].nil?
+        options[:except_topic_ids] = [SiteSetting.welcome_topic_id]
+      else
+        options[:except_topic_ids] << SiteSetting.welcome_topic_id
+      end
+    end
+
     options
+  end
+
+  private
+
+  def hide_welcome_topic?
+    return false if @guardian.is_admin?
+    Site.welcome_topic_exists_and_is_not_edited?
   end
 end
