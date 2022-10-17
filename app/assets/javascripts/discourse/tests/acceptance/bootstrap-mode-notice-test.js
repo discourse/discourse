d0b852f40991d3db76c723a0246c4af25532551d
Jordan Vidrine <jordan@jordanvidrine.com>
2022-10-17 14:58:45 -0500

    DEV: Remove bootstrap js tests

--- app/assets/javascripts/discourse/tests/acceptance/bootstrap-mode-notice-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/bootstrap-mode-notice-test.js
@@ -17,24 +17,6 @@ acceptance("Bootstrap Mode Notice", function (needs) {
       exists(".bootstrap-mode-notice"),
       "has the bootstrap mode notice"
     );
-    assert.ok(
-      exists(".bootstrap-invite-button"),
-      "bootstrap notice has invite button"
-    );
-    assert.ok(
-      exists(".bootstrap-wizard-link"),
-      "bootstrap notice has wizard link"
-    );
-
-    await click(".bootstrap-invite-button");
-    assert.ok(exists(".create-invite-modal"), "opens create invite modal");
-
-    await click(".bootstrap-wizard-link");
-    assert.strictEqual(
-      currentURL(),
-      "/wizard/steps/hello-world",
-      "it transitions to the wizard page"
-    );
 
     await visit("/");
     set(this.siteSettings, "bootstrap_mode_enabled", false);
