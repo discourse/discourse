13ae710a2ea87a819e4529754e43845803ddf9d5
Blake Erickson <o.blakeerickson@gmail.com>
2022-10-17 19:10:15 -0600

    fix indentation

--- app/models/site.rb
+++ app/models/site.rb
@@ -208,12 +208,12 @@ class Site
   end
 
   def self.welcome_topic_exists_and_is_not_edited?
-      Post.joins(:topic)
-        .find_by(
-          "topics.id = :topic_id AND topics.deleted_at IS NULL AND posts.post_number = 1 AND posts.version = 1 AND posts.created_at > :created_at",
-          topic_id: SiteSetting.welcome_topic_id,
-          created_at: 1.month.ago
-        ).exists?
+    Post.joins(:topic)
+      .find_by(
+        "topics.id = :topic_id AND topics.deleted_at IS NULL AND posts.post_number = 1 AND posts.version = 1 AND posts.created_at > :created_at",
+        topic_id: SiteSetting.welcome_topic_id,
+        created_at: 1.month.ago
+      ).exists?
   end
 
   def self.show_welcome_topic_banner?(guardian)
