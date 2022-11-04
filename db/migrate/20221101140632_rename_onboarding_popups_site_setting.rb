8d84ce2e668c13f767e5247f907e03881f404d23
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-11-04 23:14:14 +0200

    Apply code suggestions

--- db/migrate/20221101140632_rename_onboarding_popups_site_setting.rb
+++ db/migrate/20221101140632_rename_onboarding_popups_site_setting.rb
@@ -2,10 +2,10 @@
 
 class RenameOnboardingPopupsSiteSetting < ActiveRecord::Migration[7.0]
   def up
-    execute "UPDATE site_settings SET name = 'enable_user_tips' WHERE name = 'enable_user_tips'"
+    execute "UPDATE site_settings SET name = 'enable_user_tips' WHERE name = 'enable_onboarding_popups'"
   end
 
   def down
-    execute "UPDATE site_settings SET name = 'enable_user_tips' WHERE name = 'enable_user_tips'"
+    execute "UPDATE site_settings SET name = 'enable_onboarding_popups' WHERE name = 'enable_user_tips'"
   end
 end
