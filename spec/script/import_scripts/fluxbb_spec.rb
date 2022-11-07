cac0222bb8608a77c5c6b4dfb51e5dee31390685
Harry Wood <github@harrywood.co.uk>
2022-11-07 00:25:34 +0000

    Test fluxbb-flavoured bbcode examples
    Add post content examples with a variety of FluxBB-flavoured bbcode. This is covering the full set of examples given on the FluxBB help pagei e.g. https://bbs.archlinux.org/help.php#bbcode and https://forum.openstreetmap.org/help.php
    
    The tests are grouped into
    * Examples which converts correctly to markdown (or other discourse supported bbcode/html)
    * Examples which are best left unconverted, because discourse supports the bbcode and that is the best way to format it.
    * Examples of FluxBB color bbcode which cannot be converted to any syntax supported by discourse (at least not without plugin). It's debatable what the most desirable treatment of these examples is, but the test describes current behaviour which is to strip away the bbcodes to leave plain text.
    ...and finally (perhaps most importantly)
    * A disabled set of examples which currently do not work! This is FluxBB-flavoured bbcode which should be converted to markdown (or other discourse supported bbcode/html), but which is either ignored or incorrectly converted. Expected output is included, although in some cases there are multiple ways we might do it e.g. lists could be converted to markdown or html.
    
    Note: While we could have done a similar bunch of testing within the ruby-bbcode-to-md gem, it is this import script which claims to be handling specifically FluxBB flavoured bbcode. It does so (or attempts to do so) by running some regexp replacements in the `process_fluxbb_post` method as preproccessing before handing over to the generic bbcode_to_md gem. These tests cover both of those steps taken together.

--- spec/script/import_scripts/fluxbb_spec.rb
+++ spec/script/import_scripts/fluxbb_spec.rb
@@ -79,12 +79,102 @@ RSpec.describe "ImportScripts::FluxBB" do
           importer.import_posts
         end
 
-        let(:example_post_content) {
-          "My post [b]content[/b]"
-        }
+        context "with bbcode syntax examples to be converted" do
+          let(:example_post_content) {
+            "[b]Bold text[/b]\n" +
+            "[i]Italic text[/i]\n" +
+            "[url=https://www.discourse.org/]Link with link text[/url]\n" +
+            "[url]https://www.discourse.org/[/url] - Link without link text\n" +
+            "[url=/about]Relative link[/url]"
+          }
 
-        it "converts bbcode to markdown in the resulting Post" do
-          expect(Post.last.raw).to eq("My post **content**")
+          it "converts bbcode to markdown in the resulting Post" do
+            expect(Post.last.raw).to eq(
+              "**Bold text**\n" +
+              "*Italic text*\n" +
+              "[Link with link text](https://www.discourse.org/)\n" +
+              "[https://www.discourse.org/](https://www.discourse.org/) - Link without link text\n" +
+              "[Relative link](/about)"
+            )
+          end
+        end
+
+        context "with bbcode examples which are best left in bbcode format" do
+          let(:example_post_content) {
+            "[code]Code[/code]\n" +
+            "[quote=James]This is the text I want to quote.[/quote]\n" +
+            "[quote]This is the text I want to quote.[/quote] \n" +
+            ":) :| :( :D :O ;) :/ :P :p :lol: :cool:"
+          }
+
+          it "does no conversion leaving discourse to handle the bbcode" do
+            expect(Post.last.raw).to eq(example_post_content)
+          end
+        end
+
+        context "with color bbcode examples which have no support in discourse" do
+          let(:example_post_content) {
+            "[color=#FF0000]Red text[/color]\n" +
+            "[color=blue]Blue text[/color]"
+          }
+
+          it "drops the unsupported bbcode leaving plain unformatted text" do
+            expect(Post.last.raw).to eq(
+              "Red text\n" +
+              "Blue text"
+            )
+          end
+        end
+
+        context "with FluxBB internal linking syntax which will be hard to convert" do
+          let(:example_post_content) {
+            "[topic=1]Internal link to fluxbb topic with link text[/topic]\n" +
+            "[topic]1[/topic] - Internal link to fluxbb topic without link text\n" +
+            "[post=1]Internal link to fluxbb post with link text[/post]\n" +
+            "[post]1[/post] - Internal link to fluxbb post without link text\n" +
+            "[forum=1]Internal link to fluxbb forum with link text[/forum]\n" +
+            "[forum]1[/forum] - Internal link to fluxbb forum without link text\n" +
+            "[user=2]Internal link to fluxbb user profile with link text[/user]\n" +
+            "[user]2[/user] - Internal link to fluxbb user without link text"
+          }
+
+          it "does no conversion leaving bbcode in place for manual conversion" do
+            expect(Post.last.raw).to eq(example_post_content)
+          end
+        end
+
+        context "with fluxbb bbcode syntax examples which are not yet working!" do
+          let(:example_post_content) {
+            "[u]Underlined text[/u]\n" +
+            "[s]Strike-through text[/s]\n"
+            "[del]Deleted text[/del]\n" +
+            "[ins]Inserted text[/ins]\n" +
+            "[em]Emphasised text[/em]\n" +
+            "[h]Heading[/h]\n" +
+            "[list][*]Example list item 1.[/*][*]Example list item 2.[/*][*]Example list item 3.[/*][/list]\n" +
+            "[list=1][*]Example list item 1.[/*][*]Example list item 2.[/*][*]Example list item 3.[/*][/list]\n" +
+            "[email]myname@example.com[/email] - email link without link text\n" +
+            "[email=myname@example.com]Email link with link text[/email]\n"
+            "=) =| =( =D :o\n"
+            ":mad: :rolleyes:"
+          }
+
+          xit "converts bbcode to markdown in the resulting Post" do
+            expect(Post.last.raw).to eq(
+              "[u]Underlined text[/u]\n" + #should be unaltered!
+              "~~Strike-through text~~\n" +
+              "<del>Deleted text</del>\n" +
+              "<ins>Inserted text<ins>\n" +
+              "*Emphasised text*\n" +
+              "## Heading\n" +
+              "<ul><li>Example list item 1.</li><li>Example list item 2.</li><li>Example list item 3.</li></ul>\n" +
+              "<ol><li>Example list item 1.</li><li>Example list item 2.</li><li>Example list item 3.</li></ol>\n" +
+              "[mailto:myname@example.com](mailto:myname@example.com) - email link without link text\n" +
+              "[Email link with link text](mailto:myname@example.com)\n" +
+              ":) :| :( :D :O\n" +
+              ":rage: :roll_eyes:"
+            )
+          end
         end
       end
     end
