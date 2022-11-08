b7f3783008de1b008e6b460e023e46a843b89ad0
Keegan George <kgeorge13@gmail.com>
2022-11-08 14:20:58 -0800

    DEV: Remove add related test

--- app/assets/javascripts/discourse/tests/acceptance/admin-permalink-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/admin-permalink-test.js
@@ -1,9 +1,5 @@
-import {
-  acceptance,
-  exists,
-  queryAll,
-} from "discourse/tests/helpers/qunit-helpers";
-import { click, fillIn, visit } from "@ember/test-helpers";
+import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
+import { fillIn, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 
 acceptance("Admin - Permalinks", function (needs) {
@@ -31,33 +27,9 @@ acceptance("Admin - Permalinks", function (needs) {
   needs.user();
   needs.pretender((server, helper) => {
     server.get("/admin/permalinks.json", (response) => {
-      // console.log(response);
       const result =
         response.queryParams.filter !== "feature" ? [] : startingData;
       return helper.response(200, result);
-    });
-
-    server.post("/admin/permalinks.json", (request) => {
-      const data = helper.parsePostData(request.requestBody);
-      const newData = {
-        id: 39,
-        url: data.url,
-        topic_id: null,
-        topic_title: null,
-        post_id: null,
-        post_url: null,
-        post_number: null,
-        post_topic_title: null,
-        category_id: null,
-        category_name: null,
-        category_url: null,
-        external_url: data.permalink_type_value,
-        tag_id: null,
-        tag_name: null,
-        tag_url: null,
-      };
-      startingData.push(newData);
-      return helper.response(200, startingData);
     });
   });
 
@@ -81,26 +53,4 @@ acceptance("Admin - Permalinks", function (needs) {
 
     assert.ok(exists(".permalink-search"), "search input still visible");
   });
-
-  test("add permalinks", async function (assert) {
-    await visit("/admin/customize/permalinks");
-    await this.pauseTest();
-    // console.log(startingData);
-    await this.pauseTest();
-    await fillIn(".permalink-url", "settings");
-    await click(".permalink-type .select-kit-header");
-    await click(".select-kit-row[data-value='external_url']");
-    await fillIn(".permalink-destination", "/admin/site_settings");
-    await click(".permalink-add");
-    // todo fix not inserting text values
-    assert.ok(
-      queryAll(".admin-logs-table.permalinks tbody tr").length === 2,
-      "a new permalink is successfully added"
-    );
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  // test("remove permalink", async function (assert) {
-  //   // todo
-  // });
 });
