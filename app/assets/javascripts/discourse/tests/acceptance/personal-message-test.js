fe621c09a85e115e9dd405269fb54731cdad268e
Gerhard Schlager <gerhard.schlager@discourse.org>
2022-11-09 13:53:47 +0100

    DEV: Remove unused code and translations
    The edit PM button was removed in ecc8e559ecd615542f766d74ded50a6cdbc320eb

--- app/assets/javascripts/discourse/tests/acceptance/personal-message-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/personal-message-test.js
@@ -1,23 +1,10 @@
-import {
-  acceptance,
-  exists,
-  query,
-} from "discourse/tests/helpers/qunit-helpers";
+import { acceptance, query } from "discourse/tests/helpers/qunit-helpers";
 import I18n from "I18n";
 import { test } from "qunit";
 import { visit } from "@ember/test-helpers";
 
 acceptance("Personal Message", function (needs) {
   needs.user();
-
-  test("footer edit button", async function (assert) {
-    await visit("/t/pm-for-testing/12");
-
-    assert.ok(
-      !exists(".edit-message"),
-      "does not show edit first post button on footer by default"
-    );
-  });
 
   test("suggested messages", async function (assert) {
     await visit("/t/pm-for-testing/12");
