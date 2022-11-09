discourse/discourse

commit 991b62b6f123e9df63f443bba411eee4f3c3066a

DEV: Drop old hotlinked image data from post_custom_fields (#16594)
`20220428094026_create_post_hotlinked_media` moved this data into a dedicated table

app/models/post.rb
@@ -72,20 +72,11 @@ class Post < ActiveRecord::Base
   # We can pass several creating options to a post via attributes
   attr_accessor :image_sizes, :quoted_post_numbers, :no_bump, :invalidate_oneboxes, :cooking_options, :skip_unique_check, :skip_validation
 
-  LARGE_IMAGES            ||= "large_images"
-  BROKEN_IMAGES           ||= "broken_images"
-  DOWNLOADED_IMAGES       ||= "downloaded_images"
   MISSING_UPLOADS         ||= "missing uploads"
   MISSING_UPLOADS_IGNORED ||= "missing uploads ignored"
   NOTICE                  ||= "notice"
 
   SHORT_POST_CHARS ||= 1200
-
-  # TODO: Drop the data from these three custom fields,
-  # drop the indexes, and remove the relavent constants
-  register_custom_field_type(LARGE_IMAGES, :json)
-  register_custom_field_type(BROKEN_IMAGES, :json)
-  register_custom_field_type(DOWNLOADED_IMAGES, :json)
 
   register_custom_field_type(MISSING_UPLOADS, :json)
   register_custom_field_type(MISSING_UPLOADS_IGNORED, :boolean)

app/models/post_custom_field.rb
@@ -23,7 +23,4 @@ end
 #  index_post_custom_fields_on_post_id           (post_id) UNIQUE WHERE ((name)::text = 'missing uploads'::text)
 #  index_post_custom_fields_on_post_id_and_name  (post_id,name)
 #  index_post_id_where_missing_uploads_ignored   (post_id) UNIQUE WHERE ((name)::text = 'missing uploads ignored'::text)
-#  post_custom_field_broken_images_idx           (post_id) UNIQUE WHERE ((name)::text = 'broken_images'::text)
-#  post_custom_field_downloaded_images_idx       (post_id) UNIQUE WHERE ((name)::text = 'downloaded_images'::text)
-#  post_custom_field_large_images_idx            (post_id) UNIQUE WHERE ((name)::text = 'large_images'::text)
 #

db/post_migrate/20220429164301_delete_hotlinked_image_custom_fields.rb
@@ -1,0 +1,32 @@
+# frozen_string_literal: true
+
+class DeleteHotlinkedImageCustomFields < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
+  def up
+    execute <<~SQL
+      DELETE FROM post_custom_fields
+      WHERE name IN (
+        'downloaded_images',
+        'broken_images',
+        'large_images'
+      )
+    SQL
+
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS post_custom_field_broken_images_idx
+    SQL
+
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS post_custom_field_downloaded_images_idx
+    SQL
+
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS post_custom_field_large_images_idx
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end

commit 54b70e767a50abb07b3b8099cf4d019cc9dda7bd

FIX: Shrinking images where smaller image upload exists
…has regressed in 991b62b6f123e9df63f443bba411eee4f3c3066a / #16594

app/models/post.rb
@@ -989,10 +989,6 @@ class Post < ActiveRecord::Base
     end
   end
 
-  def downloaded_images
-    self.custom_fields[Post::DOWNLOADED_IMAGES] || {}
-  end
-
   def each_upload_url(fragments: nil, include_local_upload: true)
     current_db = RailsMultisite::ConnectionManagement.current_db
     upload_patterns = [

lib/shrink_uploaded_image.rb
@@ -83,7 +83,7 @@ class ShrinkUploadedImage
 
       if post.raw_changed?
         log "Updating post"
-      elsif post.downloaded_images.has_value?(original_upload.id)
+      elsif post.post_hotlinked_media.exists?(upload_id: original_upload.id)
         log "A hotlinked, unreferenced image"
       elsif post.raw.include?(upload.short_url)
         log "Already processed"
@@ -161,13 +161,10 @@ class ShrinkUploadedImage
           )
         end
 
-        if existing_upload && post.downloaded_images.present?
-          downloaded_images = post.downloaded_images.transform_values do |upload_id|
-            upload_id == original_upload.id ? upload.id : upload_id
-          end
-
-          post.custom_fields[Post::DOWNLOADED_IMAGES] = downloaded_images
-          post.save_custom_fields
+        if existing_upload
+          post.post_hotlinked_media
+            .where(upload_id: original_upload.id)
+            .update_all(upload_id: upload.id)
         end
 
         post.rebake!

spec/lib/shrink_uploaded_image_spec.rb
@@ -21,6 +21,29 @@ RSpec.describe ShrinkUploadedImage do
       expect(upload.filesize).to be < filesize_before
     end
 
+    it "updates HotlinkedMedia records when there is an upload for downsized image" do
+      OptimizedImage.downsize(Discourse.store.path_for(upload), "/tmp/smaller.png", "10000@", filename: upload.original_filename)
+      smaller_sha1 = Upload.generate_digest("/tmp/smaller.png")
+      smaller_upload = Fabricate(:image_upload, sha1: smaller_sha1)
+
+      post = Fabricate(:post, raw: "<img src='#{upload.url}'>")
+      post.link_post_uploads
+      post_hotlinked_media = PostHotlinkedMedia.create!(
+        post: post,
+        url: "http://example.com/images/2/2e/Longcat1.png",
+        upload: upload,
+        status: :downloaded,
+      )
+
+      ShrinkUploadedImage.new(
+        upload: upload,
+        path: Discourse.store.path_for(upload),
+        max_pixels: 10_000,
+      ).perform
+
+      expect(post_hotlinked_media.reload.upload).to eq(smaller_upload)
+    end
+
     it "returns false if the image is not used by any models" do
       result = ShrinkUploadedImage.new(
         upload: upload,