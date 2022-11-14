5521331836722eb3ee534a7da7a05b7b87b2b3ab
Martin Brennan <martin@discourse.org>
2022-11-14 12:51:33 +1000

    Version bump to v2.9.0.beta12

--- lib/version.rb
+++ lib/version.rb
@@ -10,7 +10,7 @@ module Discourse
       MAJOR = 2
       MINOR = 9
       TINY  = 0
-      PRE   = 'beta11'
+      PRE   = 'beta12'
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     end
