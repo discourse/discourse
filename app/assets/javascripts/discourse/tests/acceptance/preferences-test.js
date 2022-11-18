fde8d9b4bb001aaf14f20327c0f4784bd8c84fee
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-11-18 11:40:22 +0800

    DEV: Move tests for user preferences change email into own file (#19097)
    This helps to make it easier to identify the test file for the
    particular route.

--- app/assets/javascripts/discourse/tests/acceptance/preferences-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/preferences-test.js
@@ -56,12 +56,6 @@ function preferencesPretender(server, helper) {
     });
   });
 
-  server.put("/u/eviltrout/preferences/email", () => {
-    return helper.response({
-      success: true,
-    });
-  });
-
   server.post("/user_avatar/eviltrout/refresh_gravatar.json", () => {
     return helper.response({
       gravatar_upload_id: 6543,
@@ -156,34 +150,6 @@ acceptance("User Preferences", function (needs) {
   test("username", async function (assert) {
     await visit("/u/eviltrout/preferences/username");
     assert.ok(exists("#change_username"), "it has the input element");
-  });
-
-  test("email", async function (assert) {
-    await visit("/u/eviltrout/preferences/email");
-
-    assert.ok(exists("#change-email"), "it has the input element");
-
-    await fillIn("#change-email", "invalid-email");
-
-    assert.strictEqual(
-      query(".tip.bad").innerText.trim(),
-      I18n.t("user.email.invalid"),
-      "it should display invalid email tip"
-    );
-  });
-
-  test("email field always shows up", async function (assert) {
-    await visit("/u/eviltrout/preferences/email");
-
-    assert.ok(exists("#change-email"), "it has the input element");
-
-    await fillIn("#change-email", "eviltrout@discourse.org");
-    await click(".user-preferences button.btn-primary");
-
-    await visit("/u/eviltrout/preferences");
-    await visit("/u/eviltrout/preferences/email");
-
-    assert.ok(exists("#change-email"), "it has the input element");
   });
 
   test("connected accounts", async function (assert) {
