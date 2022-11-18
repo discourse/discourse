a9d0f9be09b740104f43365a74b6a295651c1c63
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-11-18 11:50:26 +0800

    DEV: Centralise more user preferences tests into individual files
    Makes it easier to associate the route with the tests

--- app/assets/javascripts/discourse/tests/acceptance/user-preferences-account-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/user-preferences-account-test.js
@@ -1,17 +1,27 @@
-import { acceptance, query } from "discourse/tests/helpers/qunit-helpers";
+import {
+  acceptance,
+  exists,
+  query,
+} from "discourse/tests/helpers/qunit-helpers";
 import { click, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import DiscourseURL from "discourse/lib/url";
 import I18n from "I18n";
 import sinon from "sinon";
 
-acceptance("User Profile - Account - Self Delete", function (needs) {
+acceptance("User Preferences - Account", function (needs) {
   needs.user({
     username: "charlie",
   });
 
   needs.pretender((server, helper) => {
     server.delete("/u/charlie.json", () => helper.response({ success: true }));
+
+    server.post("/u/eviltrout/preferences/revoke-account", () => {
+      return helper.response({
+        success: true,
+      });
+    });
   });
 
   test("Delete dialog", async function (assert) {
@@ -33,6 +43,32 @@ acceptance("User Profile - Account - Self Delete", function (needs) {
     assert.ok(
       DiscourseURL.redirectAbsolute.calledWith("/"),
       "redirects to home after deleting"
+    );
+  });
+
+  test("connected accounts", async function (assert) {
+    await visit("/u/eviltrout/preferences/account");
+
+    assert.ok(
+      exists(".pref-associated-accounts"),
+      "it has the connected accounts section"
+    );
+
+    assert.ok(
+      query(
+        ".pref-associated-accounts table tr:nth-of-type(1) td:nth-of-type(1)"
+      ).innerHTML.includes("Facebook"),
+      "it lists facebook"
+    );
+
+    await click(
+      ".pref-associated-accounts table tr:nth-of-type(1) td:last-child button"
+    );
+
+    assert.ok(
+      query(
+        ".pref-associated-accounts table tr:nth-of-type(1) td:last-of-type"
+      ).innerHTML.includes("Connect")
     );
   });
 });
