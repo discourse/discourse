1dc973a3ea8a7b0b7b07ccaaa501c7a0c6fd01f1
awesomerobot <kris.aubuchon@discourse.org>
2022-11-21 12:07:16 -0500

    A11Y: add title & aria-expanded for sidebar toggle

--- app/assets/javascripts/discourse/app/widgets/sidebar-toggle.js
+++ app/assets/javascripts/discourse/app/widgets/sidebar-toggle.js
@@ -4,12 +4,17 @@ export default createWidget("sidebar-toggle", {
   tagName: "span.header-sidebar-toggle",
 
   html() {
+    const attrs = this.attrs;
     return [
       this.attach("button", {
-        title: "",
+        title: attrs.showSidebar
+          ? "sidebar.hide_sidebar"
+          : "sidebar.show_sidebar",
         icon: "bars",
         action: "toggleSidebar",
         className: "btn btn-flat btn-sidebar-toggle",
+        ariaExpanded: attrs.showSidebar ? "true" : "false",
+        ariaControls: "d-sidebar",
       }),
     ];
   },
