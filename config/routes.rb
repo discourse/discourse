6357a3ce337bdf59c7a6a0f362897a46ffbd6304
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-11-24 19:16:28 +0400

    FEATURE: User Status API (#19149)
    This adds API scope for the user status. This also adds a get method to the user status controller. We didn't need a dedicated method that returns status before because the server returns status with user objects, but I think we need to provide this method for API clients.

--- config/routes.rb
+++ config/routes.rb
@@ -1037,6 +1037,7 @@ Discourse::Application.routes.draw do
     post "/presence/update" => "presence#update"
     get "/presence/get" => "presence#get"
 
+    get "user-status" => "user_status#get"
     put "user-status" => "user_status#set"
     delete "user-status" => "user_status#clear"
 
