ffd93354f57dcfaa61eb7a79dcc4295bbbb87f07
rvanasa <ryan.vandersmith@dfinity.org>
2022-12-01 20:41:38 -0700

    Mock HTTP request in tests

--- spec/lib/onebox/engine/motoko_onebox_spec.rb
+++ spec/lib/onebox/engine/motoko_onebox_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe Onebox::Engine::MotokoOnebox do
-  # before do
-  #   body = '{"version":"1.0","provider_name":"Embed Motoko","provider_url":"https://embed.smartcontracts.org","type":"rich","width":800,"height":500,"html":"<iframe src=\"https://embed.smartcontracts.org\" width=\"800\" height=\"500\" style=\"border:0\" />"}'
+  before do
+    body = '{"version":"1.0","provider_name":"Embed Motoko","provider_url":"https://embed.smartcontracts.org","type":"rich","width":800,"height":500,"html":"<iframe src=\"https://embed.smartcontracts.org\" width=\"800\" height=\"500\" style=\"border:0\" />"}'
 
-  #   stub_request(:get, "https://embed.smartcontracts.org/api/onebox?url=https://embed.smartcontracts.org")
-  #     .to_return(status: 200, body: body, headers: {})
-  # end
+    stub_request(:get, "https://embed.smartcontracts.org/api/onebox?url=https://embed.smartcontracts.org")
+      .to_return(status: 200, body: body, headers: {})
+  end
 
   it "returns the expected iframe markup" do
     expect(Onebox.preview('https://embed.smartcontracts.org/api/onebox?url=https://embed.smartcontracts.org').to_s.chomp).to eq(
