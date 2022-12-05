93859037ef776b83b9840e528a739050c600922c
Bianca Nenciu <nbianca@users.noreply.github.com>
2022-12-05 20:22:05 +0200

    FEATURE: Improve composer warnings for mentions (#18796)
    * FEATURE: Show warning if group cannot be mentioned
    
    A similar warning is displayed when the user cannot be mentioned because
    they have not been invited to the topic.
    
    * FEATURE: Resolve mentions for new topic
    
    This commit improves several improvements and refactors
    /u/is_local_username route to a better /composer/mentions route that
    can handle new topics too.
    
    * FEATURE: Show warning if only some are notified
    
    Sometimes users are still notified even if the group that was mentioned
    was not invited to the message. This happens because its members were
    invited directly or are members of other groups that were invited.
    
    * DEV: Refactor _warnCannotSeeMention

--- app/assets/javascripts/discourse/tests/integration/components/composer-editor-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/composer-editor-test.js
@@ -8,26 +8,28 @@ module("Integration | Component | ComposerEditor", function (hooks) {
   setupRenderingTest(hooks);
 
   test("warns about users that will not see a mention", async function (assert) {
-    assert.expect(1);
+    assert.expect(2);
 
     this.set("model", {});
     this.set("noop", () => {});
-    this.set("expectation", (warnings) => {
-      assert.deepEqual(warnings, [
-        { name: "user-no", reason: "a reason" },
-        { name: "user-nope", reason: "a reason" },
-      ]);
+    this.set("expectation", (warning) => {
+      if (warning.name === "user-no") {
+        assert.deepEqual(warning, { name: "user-no", reason: "a reason" });
+      } else if (warning.name === "user-nope") {
+        assert.deepEqual(warning, { name: "user-nope", reason: "a reason" });
+      }
     });
 
-    pretender.get("/u/is_local_username", () => {
+    pretender.get("/composer/mentions", () => {
       return response({
-        cannot_see: {
+        users: ["user-ok", "user-no", "user-nope"],
+        user_reasons: {
           "user-no": "a reason",
           "user-nope": "a reason",
         },
-        mentionable_groups: [],
-        valid: ["user-ok", "user-no", "user-nope"],
-        valid_groups: [],
+        groups: {},
+        group_reasons: {},
+        max_users_notified_per_group_mention: 100,
       });
     });
 
