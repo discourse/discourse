4d68274e8713cd3c9befc2b7fcaaaf3fa9d2ef26
Bianca Nenciu <nbianca@users.noreply.github.com>
2022-12-06 20:30:47 +0200

    Update 20221125173217_remove_enable_whispers_site_setting.rb

--- db/migrate/20221125173217_remove_enable_whispers_site_setting.rb
+++ db/migrate/20221125173217_remove_enable_whispers_site_setting.rb
@@ -5,9 +5,16 @@ class RemoveEnableWhispersSiteSetting < ActiveRecord::Migration[7.0]
     execute <<~SQL
       UPDATE site_settings
       SET value = array_to_string(array_append(string_to_array(value, '|'), '3'), '|')
-      WHERE EXISTS(SELECT 1 FROM site_settings WHERE name = 'enable_whispers' AND value = 't') AND
-            name = 'whispers_allowed_groups' AND
+      WHERE name = 'whispers_allowed_groups' AND
+            EXISTS(SELECT 1 FROM site_settings WHERE name = 'enable_whispers' AND value = 't') AND
             NOT '3' = ANY(string_to_array(value, '|'))
+    SQL
+
+    execute <<~SQL
+      UPDATE site_settings
+      SET value = ''
+      WHERE name = 'whispers_allowed_groups' AND
+            EXISTS(SELECT 1 FROM site_settings WHERE name = 'enable_whispers' AND value = 'f')
     SQL
 
     execute <<~SQL
