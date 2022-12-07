discourse/discourse

commit 7d7551adfcee90f61297d261742338b8bfe72255

DEV: Remove user options from current user serializer (#19089)
User options were serialized at the root level of CurrentUserSerializer,
but UserSerializer has a user_option field. This inconsistency caused
issues in the past because user_option fields had to be duplicated on
the frontend.

app/assets/javascripts/admin/addon/routes/admin-customize-themes-show.js
@@ -31,7 +31,7 @@ export default Route.extend({
       model,
       parentController,
       allThemes: parentController.get("model"),
-      colorSchemeId: model.get("color_scheme_id"),
+      colorSchemeId: model.get("user_option.color_scheme_id"),
       colorSchemes: parentController.get("model.extras.color_schemes"),
       editingName: false,
     });

commit f4a64a9dcac6f94d14aba983cdab7be57062cd0e

FIX: Use correct property for theme's `color_scheme_id`
Regressed in 7d7551adfcee90f61297d261742338b8bfe72255

app/assets/javascripts/admin/addon/routes/admin-customize-themes-show.js
@@ -31,7 +31,7 @@ export default Route.extend({
       model,
       parentController,
       allThemes: parentController.get("model"),
-      colorSchemeId: model.get("user_option.color_scheme_id"),
+      colorSchemeId: model.get("color_scheme_id"),
       colorSchemes: parentController.get("model.extras.color_schemes"),
       editingName: false,
     });