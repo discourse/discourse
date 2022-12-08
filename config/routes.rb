9001e340f8ab694c961d2a9a12510464ebab00eb
ruq <hosch@mailbox.org>
2022-12-08 03:26:10 +0100

    FEATURE: mailpace webhook
    adds mailpace (formerly known as ohmystmp) webhook

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,8 @@ Discourse::Application.routes.draw do
     post "webhooks/aws" => "webhooks#aws"
     post "webhooks/mailgun" => "webhooks#mailgun"
     post "webhooks/mailjet" => "webhooks#mailjet"
+    post "webhooks/mailgun" => "webhooks#mailgun"
+    post "webhooks/mailpace" => "webhooks#mailpace"
     post "webhooks/mandrill" => "webhooks#mandrill"
     get "webhooks/mandrill" => "webhooks#mandrill_head"
     post "webhooks/postmark" => "webhooks#postmark"
