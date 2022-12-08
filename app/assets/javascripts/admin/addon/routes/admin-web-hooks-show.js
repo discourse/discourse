discourse/discourse

commit 321b14d40c8a6fbdac583e2767e2c260f9f25c70

DEV: Remove unused `render()` calls (#19224)
`Route#render` and `Route#renderTemplate` have been deprecated and are removed in Ember 4.x (see: https://deprecations.emberjs.com/v3.x#toc_route-render-template)

The templates of modified routes in this PR are already automatically inserted into `{{outlet}}`s.

app/assets/javascripts/admin/addon/routes/admin-web-hooks-show.js
@@ -23,8 +23,4 @@ export default DiscourseRoute.extend({
     model.set("group_ids", model.get("group_ids"));
     controller.setProperties({ model, saved: false });
   },
-
-  renderTemplate() {
-    this.render("admin/templates/web-hooks-show", { into: "adminApi" });
-  },
 });

app/assets/javascripts/admin/addon/routes/admin-logs-screened-emails.js
@@ -1,10 +1,6 @@
 import DiscourseRoute from "discourse/routes/discourse";
 
 export default DiscourseRoute.extend({
-  renderTemplate() {
-    this.render("admin/templates/logs/screened-emails", { into: "adminLogs" });
-  },
-
   setupController() {
     return this.controllerFor("adminLogsScreenedEmails").show();
   },

app/assets/javascripts/admin/addon/routes/admin-logs-screened-ip-addresses.js
@@ -1,12 +1,6 @@
 import DiscourseRoute from "discourse/routes/discourse";
 
 export default DiscourseRoute.extend({
-  renderTemplate() {
-    this.render("admin/templates/logs/screened-ip-addresses", {
-      into: "adminLogs",
-    });
-  },
-
   setupController() {
     return this.controllerFor("adminLogsScreenedIpAddresses").show();
   },

app/assets/javascripts/admin/addon/routes/admin-logs-screened-urls.js
@@ -1,10 +1,6 @@
 import DiscourseRoute from "discourse/routes/discourse";
 
 export default DiscourseRoute.extend({
-  renderTemplate() {
-    this.render("admin/templates/logs/screened-urls", { into: "adminLogs" });
-  },
-
   setupController() {
     return this.controllerFor("adminLogsScreenedUrls").show();
   },

app/assets/javascripts/admin/addon/routes/admin-logs-staff-action-logs.js
@@ -34,13 +34,6 @@ export default DiscourseRoute.extend({
     return this._super(value, urlKey, defaultValueType);
   },
 
-  // TODO: make this automatic using an `{{outlet}}`
-  renderTemplate() {
-    this.render("admin/templates/logs/staff-action-logs", {
-      into: "adminLogs",
-    });
-  },
-
   actions: {
     onFiltersChange(filters) {
       if (filters && Object.keys(filters) === 0) {

app/assets/javascripts/admin/addon/routes/admin-user.js
@@ -14,10 +14,6 @@ export default DiscourseRoute.extend({
     return AdminUser.find(get(params, "user_id"));
   },
 
-  renderTemplate() {
-    this.render({ into: "admin" });
-  },
-
   afterModel(adminUser) {
     return adminUser.loadDetails().then(function () {
       adminUser.setOriginalTrustLevel();

app/assets/javascripts/discourse/app/routes/build-admin-user-posts-route.js
@@ -33,7 +33,7 @@ export default function (filter) {
     },
 
     renderTemplate() {
-      this.render("user/posts", { into: "user" });
+      this.render("user/posts");
     },
   });
 }

app/assets/javascripts/discourse/app/routes/user-badges.js
@@ -17,7 +17,7 @@ export default DiscourseRoute.extend(ViewingActionType, {
   },
 
   renderTemplate() {
-    this.render("user/badges", { into: "user" });
+    this.render("user/badges");
   },
 
   @action

commit ce7a23ff5b5cb3c6b581088569c2b97d34f25f00

FIX: Admin webhook regression in 321b14d
In the meantime, before #19360 lands.

app/assets/javascripts/admin/addon/routes/admin-web-hooks-show.js
@@ -23,4 +23,8 @@ export default DiscourseRoute.extend({
     model.set("group_ids", model.get("group_ids"));
     controller.setProperties({ model, saved: false });
   },
+
+  renderTemplate() {
+    this.render("admin/templates/web-hooks-show", { into: "adminApi" });
+  },
 });