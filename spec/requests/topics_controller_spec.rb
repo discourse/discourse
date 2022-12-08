289e887c372e8dccf0b994987bc9d5e2fb4d9dfd
Rafael dos Santos Silva <xfalcox@gmail.com>
2022-12-08 16:13:02 -0300

    Update spec/requests/topics_controller_spec.rb
    Co-authored-by: Penar Musaraj <pmusaraj@gmail.com>

--- spec/requests/topics_controller_spec.rb
+++ spec/requests/topics_controller_spec.rb
@@ -4027,7 +4027,7 @@ RSpec.describe TopicsController do
         expect(response.parsed_body["error_type"]).to eq('invalid_access')
       end
 
-      it "raises an error if the user sets a delete_replies timer" do
+      it "raises an error if a TL4 user sets a delete_replies timer" do
         user.update!(trust_level: TrustLevel[4])
         sign_in(user)
 
