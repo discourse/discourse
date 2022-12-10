e3fb7a7c7e0a095c3f9d76f6b35a92d71ebed577
Justin René Back <j.back@jrbit.de>
2022-12-10 10:03:38 +0100

    Fix DBHelper.find

--- lib/db_helper.rb
+++ lib/db_helper.rb
@@ -86,7 +86,7 @@ class DbHelper
       rows = DB.query(<<~SQL, like: like)
         SELECT \"#{r.column_name}\"
           FROM \"#{r.table_name}\"
-         WHERE \""#{r.column_name}"\" LIKE :like
+         WHERE \"#{r.column_name}\" LIKE :like
       SQL
 
       if rows.size > 0
