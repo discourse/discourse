4908a669e0169e9071cf913d09f607eb4e126809
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-12-16 16:35:39 +0400

    FEATURE: integrate DnD with user status (#19410)
    This PR adds a new "Pause notifications" checkbox to the user status modal. This checkbox allows enabling the Do-Not-Disturb mode together with user status. Note that we don't remove and don't rename the existing DnD menu item in this PR, so the old way of entering the DnD mode is still available.
    
    Also, we're not making DnD mode a part of user status on backend and in database. The reason is that the DnD mode should still be available on sites with disabled user status, having them separated helps keep the implementation simple.

--- app/assets/javascripts/discourse/app/widgets/do-not-disturb.js
+++ app/assets/javascripts/discourse/app/widgets/do-not-disturb.js
@@ -4,6 +4,7 @@ import { dateNode } from "discourse/helpers/node";
 import { h } from "virtual-dom";
 import { iconNode } from "discourse-common/lib/icon-library";
 import showModal from "discourse/lib/show-modal";
+import DoNotDisturb from "discourse/lib/do-not-disturb";
 
 export default createWidget("do-not-disturb", {
   tagName: "li.do-not-disturb",
@@ -14,10 +15,7 @@ export default createWidget("do-not-disturb", {
       return [
         h("button.btn-flat.do-not-disturb-inner-container", [
           iconNode("toggle-on"),
-          h("span.do-not-disturb-label", [
-            h("span", I18n.t("do_not_disturb.label")),
-            dateNode(this.currentUser.do_not_disturb_until),
-          ]),
+          this.label(),
         ]),
       ];
     } else {
@@ -44,5 +42,16 @@ export default createWidget("do-not-disturb", {
       this.saving = false;
       return showModal("do-not-disturb");
     }
+  },
+
+  label() {
+    const content = [h("span", I18n.t("do_not_disturb.label"))];
+
+    const until = this.currentUser.do_not_disturb_until;
+    if (!DoNotDisturb.isEternal(until)) {
+      content.push(dateNode(until));
+    }
+
+    return h("span.do-not-disturb-label", content);
   },
 });
