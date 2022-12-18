e24ed79e16959657f4194974926c9a61433afc5c
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2022-12-19 08:50:40 +1100

    Update spec/lib/category_guardian_spec.rb
    Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>

--- spec/lib/category_guardian_spec.rb
+++ spec/lib/category_guardian_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe CategoryGuardian do
       expect(Guardian.new(user).can_post_in_category?(category)).to eq(true)
     end
 
-    it 'returns true for admin and memebers of priviliged groups for restricted category' do
+    it 'returns true for admin and members of priviliged groups for restricted category' do
       category.update!(read_restricted: true)
       group = Fabricate(:group)
       GroupUser.create(group: group, user: user)
