b4a9c705226c1aaf90b16b5a56f57b59b029683e
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-12-21 22:46:36 +0100

    more

--- plugins/chat/spec/system/unfollow_dm_channel_spec.rb
+++ plugins/chat/spec/system/unfollow_dm_channel_spec.rb
@@ -14,11 +14,6 @@ RSpec.describe "Unfollow dm channel", type: :system, js: true do
     sign_in(current_user)
   end
 
-  def create_message(text: "this is fine")
-    chat_channel_page.send_message(text)
-    expect(chat_channel_page).to have_message(text: text)
-  end
-
   context "when receiving a message after unfollowing" do
     it "correctly shows the channel" do
       find(".channel-#{dm_channel_1.id}").hover
@@ -27,10 +22,11 @@ RSpec.describe "Unfollow dm channel", type: :system, js: true do
       expect(page).to have_no_css(".channel-#{dm_channel_1.id}")
 
       using_session(:user_1) do
+        text = "this is fine"
         sign_in(other_user)
         chat_page.visit_channel(dm_channel_1)
-        chat_channel_page.send_message("this is fine")
-        expect(chat_channel_page).to have_message(text: "this is fine")
+        chat_channel_page.send_message(text)
+        expect(chat_channel_page).to have_message(text: text)
       end
 
       expect(page).to have_css(".channel-#{dm_channel_1.id} .urgent")
