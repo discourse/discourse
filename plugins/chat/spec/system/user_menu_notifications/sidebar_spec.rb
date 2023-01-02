e9a7d75093a9a67d9a29da1f272cf5856a28629a
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-01-02 23:01:40 +0100

    general fix for autocomplete

--- plugins/chat/spec/system/user_menu_notifications/sidebar_spec.rb
+++ plugins/chat/spec/system/user_menu_notifications/sidebar_spec.rb
@@ -170,8 +170,6 @@ RSpec.describe "User menu notifications | sidebar", type: :system, js: true do
     it "shows an invitation notification" do
       chat.visit_channel(channel_1)
       channel.send_message("this is fine @#{other_user.username}")
-
-      find(".chat-composer-input").click # ensures autocomplete is closed and not masking invite link
       find(".invite-link", wait: 5).click
 
       using_session(:user_1) do
