918dd4d63596ac5ea1ae5f9a55d1533e2400db5c
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-01-05 06:09:17 +0800

    SECURITY: use rstrip instead of regex gsub to prevent ReDOS (#19737)
    `rstrip` implementation is much more performant than regex
    
    Co-authored-by: Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>

--- lib/post_revisor.rb
+++ lib/post_revisor.rb
@@ -274,7 +274,7 @@ class PostRevisor
   end
 
   def cleanup_whitespaces(raw)
-    raw.present? ? TextCleaner.normalize_whitespaces(raw).gsub(/\s+\z/, "") : ""
+    raw.present? ? TextCleaner.normalize_whitespaces(raw).rstrip : ""
   end
 
   def should_revise?
