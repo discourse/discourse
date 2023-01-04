9f28ec49981202379200c5df02ee1ac58f3cd858
Martin Brennan <martin@discourse.org>
2023-01-05 07:58:05 +1000

    DEV: Review fixes

--- db/migrate/20230103004613_make_experimental_hashtag_feature_default_for_new_sites.rb
+++ db/migrate/20230103004613_make_experimental_hashtag_feature_default_for_new_sites.rb
@@ -2,21 +2,10 @@
 
 class MakeExperimentalHashtagFeatureDefaultForNewSites < ActiveRecord::Migration[7.0]
   def up
-    enable_experimental_hashtag_autocomplete =
-      DB.query_single(
-        "SELECT value FROM site_settings WHERE name = 'enable_experimental_hashtag_autocomplete'",
-      )[
-        0
-      ]
-
-    if enable_experimental_hashtag_autocomplete == "t" ||
-         enable_experimental_hashtag_autocomplete == "f"
-      return
-    end
-
     execute(<<~SQL)
       INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
       VALUES ('enable_experimental_hashtag_autocomplete', 5, 'f', now(), now())
+      ON CONFLICT DO NOTHING
     SQL
   end
 
