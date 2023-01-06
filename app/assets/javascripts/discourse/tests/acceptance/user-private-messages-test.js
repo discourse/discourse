18d229fd60d80b1063a3322a4bf215ada5e3261c
awesomerobot <kris.aubuchon@discourse.org>
2023-01-06 14:17:58 -0500

    fix frontend tests

--- app/assets/javascripts/discourse/tests/acceptance/user-private-messages-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/user-private-messages-test.js
@@ -365,7 +365,7 @@ function testUserPrivateMessagesWithGroupMessages(needs, customUserProps) {
     await publishNewToMessageBus({ topicId: 1 });
 
     assert.strictEqual(
-      query(".messages-nav li a.new").innerText.trim(),
+      query(".messages-nav .user-nav__messages-new").innerText.trim(),
       I18n.t("user.messages.new_with_count", { count: 1 }),
       "displays the right count"
     );
@@ -379,7 +379,7 @@ function testUserPrivateMessagesWithGroupMessages(needs, customUserProps) {
     await publishUnreadToMessageBus();
 
     assert.strictEqual(
-      query(".messages-nav li a.unread").innerText.trim(),
+      query(".messages-nav .user-nav__messages-unread").innerText.trim(),
       I18n.t("user.messages.unread_with_count", { count: 1 }),
       "displays the right count"
     );
@@ -394,13 +394,13 @@ function testUserPrivateMessagesWithGroupMessages(needs, customUserProps) {
     await publishNewToMessageBus({ groupIds: [14], topicId: 2 });
 
     assert.strictEqual(
-      query(".messages-nav li a.unread").innerText.trim(),
+      query(".messages-nav .user-nav__messages-unread").innerText.trim(),
       I18n.t("user.messages.unread_with_count", { count: 1 }),
       "displays the right count"
     );
 
     assert.strictEqual(
-      query(".messages-nav li a.new").innerText.trim(),
+      query(".messages-nav .user-nav__messages-new").innerText.trim(),
       I18n.t("user.messages.new_with_count", { count: 1 }),
       "displays the right count"
     );
@@ -410,13 +410,13 @@ function testUserPrivateMessagesWithGroupMessages(needs, customUserProps) {
     await visit("/u/charlie/messages/unread");
 
     assert.strictEqual(
-      query(".messages-nav li a.unread").innerText.trim(),
+      query(".messages-nav .user-nav__messages-unread").innerText.trim(),
       I18n.t("user.messages.unread"),
       "displays the right count"
     );
 
     assert.strictEqual(
-      query(".messages-nav li a.new").innerText.trim(),
+      query(".messages-nav .user-nav__messages-new").innerText.trim(),
       I18n.t("user.messages.new"),
       "displays the right count"
     );
@@ -453,7 +453,7 @@ function testUserPrivateMessagesWithGroupMessages(needs, customUserProps) {
     await click("#dismiss-read-confirm");
 
     assert.strictEqual(
-      query(".messages-nav li a.unread").innerText.trim(),
+      query(".messages-nav .user-nav__messages-unread").innerText.trim(),
       I18n.t("user.messages.unread"),
       "displays the right count"
     );
@@ -519,7 +519,7 @@ function testUserPrivateMessagesWithGroupMessages(needs, customUserProps) {
     await click(".btn.dismiss-read");
 
     assert.strictEqual(
-      query(".messages-nav li a.new").innerText.trim(),
+      query(".messages-nav .user-nav__messages-new").innerText.trim(),
       I18n.t("user.messages.new"),
       "displays the right count"
     );
