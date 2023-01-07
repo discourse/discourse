7c77cc6a580d7cb49f8c19ceee8cfdd08862259d
David Taylor <david@taylorhq.com>
2023-01-07 11:59:28 +0000

    DEV: Apply syntax_tree formatting to `config/*`

--- config/initializers/000-post_migration.rb
+++ config/initializers/000-post_migration.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 unless Discourse.skip_post_deployment_migrations?
-  ActiveRecord::Tasks::DatabaseTasks.migrations_paths << Rails.root.join(
-    Discourse::DB_POST_MIGRATE_PATH
-  ).to_s
+  ActiveRecord::Tasks::DatabaseTasks.migrations_paths << Rails
+    .root
+    .join(Discourse::DB_POST_MIGRATE_PATH)
+    .to_s
 end
