e76d6e1319783f135306d8074f2f6fa20f5efb0c
Jarek Radosz <jradosz@gmail.com>
2023-01-09 00:53:37 +0100

    FIX: Correctly support DiscourseEvent kwargs
    Fixes the support for kwargs in `DiscourseEvent.trigger()` on Ruby 3, e.g.
    
    ```rb
    DiscourseEvent.trigger(:before_system_message_sent, message_type: type, recipient: @recipient, post_creator_args: post_creator_args, params: method_params)
    ```
    
    Fixes https://github.com/discourse/discourse-local-site-contacts

--- spec/support/discourse_event_helper.rb
+++ spec/support/discourse_event_helper.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 module DiscourseEvent::TestHelper
-  def trigger(event_name, *params)
-    super(event_name, *params)
+  def trigger(event_name, *params, **kwargs)
+    super(event_name, *params, **kwargs)
 
     if @events_trigger
-      @events_trigger << { event_name: event_name, params: params }
+      @events_trigger << { event_name: event_name, params: params, kwargs: kwargs }
     end
   end
 
-  def track_events(event_name = nil, args: nil)
+  def track_events(event_name = nil, args: nil, kwargs: nil)
     @events_trigger = events_trigger = []
     yield
     @events_trigger = nil
@@ -18,6 +18,7 @@ module DiscourseEvent::TestHelper
       events_trigger = events_trigger.filter do |event|
         next if event[:event_name] != event_name
         next if args && event[:params] != args
+        next if kwargs && event[:kwargs] != kwargs
         true
       end
     end
@@ -25,11 +26,10 @@ module DiscourseEvent::TestHelper
     events_trigger
   end
 
-  def track(event_name, args: nil)
-    events = track_events(event_name, args: args) { yield }
+  def track(event_name, args: nil, kwargs: nil)
+    events = track_events(event_name, args: args, kwargs: kwargs) { yield }
     events.first
   end
-
 end
 
 DiscourseEvent.singleton_class.prepend DiscourseEvent::TestHelper
