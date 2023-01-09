8bc1c05781e0cd1cf6d5a8847f9a6272d7e714b0
Joe <xyz.hunabku@gmail.com>
2023-01-09 08:19:17 +0800

    DEV:  adds a couple of updated lightbox utility functions
    This commit also adds a deprecation for importing the default export in the current lightbox lib file.
    
    It also adds a layer of compatibility that will call the new utility functions without requiring any immediate code changes for third-party devs.

--- app/assets/javascripts/discourse/app/lib/lightbox.js
+++ app/assets/javascripts/discourse/app/lib/lightbox.js
@@ -2,15 +2,44 @@ import {
   escapeExpression,
   postRNWebviewMessage,
 } from "discourse/lib/utilities";
+
 import I18n from "I18n";
 import User from "discourse/models/user";
+import deprecated from "discourse-common/lib/deprecated";
+import { getOwner } from "discourse-common/lib/get-owner";
+import { helperContext } from "discourse-common/lib/helpers";
+import { isTesting } from "discourse-common/config/environment";
 import loadScript from "discourse/lib/load-script";
 import { renderIcon } from "discourse-common/lib/icon-library";
 import { spinnerHTML } from "discourse/helpers/loading-spinner";
-import { helperContext } from "discourse-common/lib/helpers";
-import { isTesting } from "discourse-common/config/environment";
 
-export default function (elem, siteSettings) {
+export async function setupLightboxes({ container, selector }) {
+  const lightboxService = getOwner(this).lookup("service:lightbox");
+  lightboxService.setupLightboxes({ container, selector });
+}
+
+export function cleanupLightboxes() {
+  const lightboxService = getOwner(this).lookup("service:lightbox");
+  return lightboxService.cleanupLightboxes();
+}
+
+export default function lightbox(elem, siteSettings) {
+  if (siteSettings.enable_experimental_lightbox) {
+    deprecated(
+      "Accessing the default `lightbox` export is deprecated. Import setupLightboxes and cleanupLightboxes from `discourse/lib/lightbox` instead.",
+      {
+        since: "3.0.0.beta16",
+        dropFrom: "3.2.0",
+        id: "discourse.lightbox.default-export",
+      }
+    );
+
+    return setupLightboxes({
+      container: elem,
+      selector: "*:not(.spoiler):not(.spoiled) a.lightbox",
+    });
+  }
+
   if (!elem) {
     return;
   }
