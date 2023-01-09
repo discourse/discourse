8b042858d0e67b5592004f965ebbdc425dec22f7
Joe <xyz.hunabku@gmail.com>
2023-01-09 08:13:01 +0800

    DEV: export lightbox helpers from one file

---
+++ app/assets/javascripts/discourse/app/lib/lightbox/helpers.js
@@ -0,0 +1,18 @@
+export {
+  setSiteThemeColor,
+  getSiteThemeColor,
+} from "./helpers/site-theme-color";
+
+export { getSwipeDirection } from "./helpers/get-swipe-direction";
+
+export { preloadItemImages } from "./helpers/preload-item-images";
+
+export { createDownloadLink } from "./helpers/create-download-link";
+
+export { scrollParentToElementCenter } from "./helpers/scroll-parent-to-element-center";
+
+export { openImageInNewTab } from "./helpers/open-image-in-new-tab";
+
+export { setCarouselScrollPosition } from "./helpers/set-carousel-scroll-position";
+
+export { findNearestSharedParent } from "./helpers/find-nearest-shared-parent";
