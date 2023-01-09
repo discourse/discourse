ee4e181d054da3ea7c4721ef0b676506ac5ecb85
Joe <xyz.hunabku@gmail.com>
2023-01-09 08:10:50 +0800

    DEV: adds a few lightbox constants

---
+++ app/assets/javascripts/discourse/app/lib/lightbox/constants.js
@@ -0,0 +1,55 @@
+import { isTesting } from "discourse-common/config/environment";
+
+export const ANIMATION_DURATION =
+  isTesting() || window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    ? 0
+    : 150;
+
+export const MIN_CAROUSEL_ARROW_ITEM_COUNT = 5;
+
+export const SWIPE_THRESHOLD = 50;
+
+export const SWIPE_DIRECTIONS = {
+  DOWN: "down",
+  LEFT: "left",
+  RIGHT: "right",
+  UP: "up",
+};
+
+export const DOCUMENT_ELEMENT_LIGHTBOX_OPEN_CLASS = "has-lightbox";
+export const LIGHTBOX_ELEMENT_ID = "discourse-lightbox";
+export const TITLE_ELEMENT_ID = "d-lightbox-image-title";
+
+export const SELECTORS = {
+  ACTIVE_CAROUSEL_ITEM: "[data-lightbox-carousel-item='current']",
+  DEFAULT_ITEM_SELECTOR: "*:not(.spoiler):not(.spoiled) a.lightbox",
+  FILE_DETAILS_CONTAINER: ".informations",
+};
+
+export const LIGHTBOX_APP_EVENT_NAMES = {
+  CLEAN: "dom:clean",
+  CLOSE: "lightbox:close",
+  CLOSED: "lightbox:closed",
+  ITEM_DID_CHANGE: "lightbox:item-did-change",
+  ITEM_WILL_CHANGE: "lightbox:item-will-change",
+  OPEN: "lightbox:open",
+  OPENED: "lightbox:opened",
+};
+
+export const LAYOUT_TYPES = {
+  HORIZONTAL: "horizontal",
+  VERTICAL: "vertical",
+};
+
+export const KEYBOARD_SHORTCUTS = {
+  CAROUSEL: "a",
+  CLOSE: "Escape",
+  DOWNLOAD: "d",
+  FULLSCREEN: "m",
+  NEWTAB: "n",
+  NEXT: ["ArrowRight", "ArrowDown"],
+  PREVIOUS: ["ArrowLeft", "ArrowUp"],
+  ROTATE: "r",
+  TITLE: "t",
+  ZOOM: "z",
+};
