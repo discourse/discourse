8a7b62b126111ff48e5c79b63bb068cd43d908e7
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-01-10 13:41:25 +0800

    DEV: Fix threading error when running jobs immediately in system tests (#19811)
    ```
    class Jobs::DummyDelayedJob < Jobs::Base
      def execute(args = {})
      end
    end
    
    RSpec.describe "Jobs.run_immediately!" do
      before { Jobs.run_immediately! }
    
      it "explodes" do
        current_user = Fabricate(:user)
        Jobs.enqueue_in(1.seconds, :dummy_delayed_job)
        sign_in(current_user)
      end
    end
    ```
    
    The test above will fail with the following error if `ActiveRecord::Base.connection_handler.clear_active_connections!` is called before the configured Capybara server checks out a connection from the connection pool.
    
    ```
         ActiveRecord::ActiveRecordError:
           Cannot expire connection, it is owned by a different thread: #<Thread:0x00007f437391df58@puma srv tp 001 /home/tgxworld/.asdf/installs/ruby/3.1.3/lib/ruby/gems/3.1.0/gems/puma-6.0.2/lib/puma/thread_pool.rb:106 sleep_forever>. Current thread: #<Thread:0x00007f437d6cfc60 run>.
    ```
    
    We're not exactly sure if this is an ActiveRecord bug or not but we've
    invested too much time into investigating this problem. Fundamentally,
    we also no longer understand why `ActiveRecord::Base.connection_handler.clear_active_connections!` is being called in an ensure block
    within `Jobs::Base#perform` which was added in
    ceddb6e0da92e874b3bee9543915726f1d27d60b 10 years ago. This
    commit moves the logic for running jobs immediately out of the
    `Jobs::Base#perform` method into another `Jobs::Base#perform_immediately` method such that
    `ActiveRecord::Base.connection_handler.clear_active_connections!` is not
    called. This change will only impact the test environment.

--- spec/jobs/jobs_base_spec.rb
+++ spec/jobs/jobs_base_spec.rb
@@ -59,12 +59,12 @@ RSpec.describe ::Jobs::Base do
 
   it "delegates the process call to execute" do
     ::Jobs::Base.any_instance.expects(:execute).with({ "hello" => "world" })
-    ::Jobs::Base.new.perform("hello" => "world", "sync_exec" => true)
+    ::Jobs::Base.new.perform("hello" => "world")
   end
 
   it "converts to an indifferent access hash" do
     ::Jobs::Base.any_instance.expects(:execute).with(instance_of(HashWithIndifferentAccess))
-    ::Jobs::Base.new.perform("hello" => "world", "sync_exec" => true)
+    ::Jobs::Base.new.perform("hello" => "world")
   end
 
   context "with fake jobs" do
