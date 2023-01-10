427ade469f8f8f397bb5cc9830bab4b338df0d67
awesomerobot <kris.aubuchon@discourse.org>
2023-01-10 14:07:01 -0500

    A11Y: more descriptive user page titles

--- app/assets/javascripts/discourse/app/routes/preferences.js
+++ app/assets/javascripts/discourse/app/routes/preferences.js
@@ -1,7 +1,12 @@
 import RestrictedUserRoute from "discourse/routes/restricted-user";
+import I18n from "I18n";
 
 export default RestrictedUserRoute.extend({
   model() {
     return this.modelFor("user");
+  },
+
+  titleToken() {
+    return I18n.t("user.preferences");
   },
 });
