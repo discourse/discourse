7798799b4a7c710f822fa57519de80f55eafcfaf
awesomerobot <kris.aubuchon@discourse.org>
2023-01-10 18:04:28 -0500

    add titles for subpages

--- app/assets/javascripts/discourse/app/routes/preferences.js
+++ app/assets/javascripts/discourse/app/routes/preferences.js
@@ -1,12 +1,19 @@
 import RestrictedUserRoute from "discourse/routes/restricted-user";
 import I18n from "I18n";
+import { inject as service } from "@ember/service";
 
 export default RestrictedUserRoute.extend({
+  router: service(),
+
   model() {
     return this.modelFor("user");
   },
 
   titleToken() {
-    return I18n.t("user.preferences");
+    let controller = this.controllerFor(this.router.currentRouteName);
+    let subpageTitle = controller?.subpageTitle;
+    return subpageTitle
+      ? `${subpageTitle} - ${I18n.t("user.preferences")}`
+      : I18n.t("user.preferences");
   },
 });
