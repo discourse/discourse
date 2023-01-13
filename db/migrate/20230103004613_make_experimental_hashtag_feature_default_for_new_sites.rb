b42ae299eb8c76c6c4791239b16ef21307146c2e
Martin Brennan <martin@discourse.org>
2023-01-13 10:11:39 +1000

    DEV: Fix incorrect hashtag setting migration
    Added in c2013865d78d353280ae68135b4c218e21ffb566,
    this migration was supposed to only turn off the hashtag
    setting for existing sites (since that was the old default)
    but its doing it for new ones too because we run all migrations
    on new sites.
    
    Instead, we should only run this if the first migration was
    only just created, meaning its a new site.

--- db/migrate/20230103004613_make_experimental_hashtag_feature_default_for_new_sites.rb
+++ db/migrate/20230103004613_make_experimental_hashtag_feature_default_for_new_sites.rb
@@ -2,11 +2,21 @@
 
 class MakeExperimentalHashtagFeatureDefaultForNewSites < ActiveRecord::Migration[7.0]
   def up
-    execute(<<~SQL)
-      INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
-      VALUES ('enable_experimental_hashtag_autocomplete', 5, 'f', now(), now())
-      ON CONFLICT DO NOTHING
+    result = execute <<~SQL
+      SELECT created_at
+      FROM schema_migration_details
+      ORDER BY created_at
+      LIMIT 1
     SQL
+
+    # keep enable_experimental_hashtag_autocomplete disabled for for existing sites
+    if result.first['created_at'].to_datetime < 1.hour.ago
+      execute <<~SQL
+        INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
+        VALUES ('enable_experimental_hashtag_autocomplete', 5, 'f', now(), now())
+        ON CONFLICT DO NOTHING
+      SQL
+    end
   end
 
   def down
