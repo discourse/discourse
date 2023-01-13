ed70b48df079514e6d4d8ff141443bcbda3ba3b3
Martin Brennan <martin@discourse.org>
2023-01-13 10:46:35 +1000

    DEV: Test fix

--- db/migrate/20230103004613_make_experimental_hashtag_feature_default_for_new_sites.rb
+++ db/migrate/20230103004613_make_experimental_hashtag_feature_default_for_new_sites.rb
@@ -9,14 +9,14 @@ class MakeExperimentalHashtagFeatureDefaultForNewSites < ActiveRecord::Migration
       LIMIT 1
     SQL
 
+    settings_insert_query = <<~SQL
+      INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
+      VALUES ('enable_experimental_hashtag_autocomplete', 5, 'f', now(), now())
+      ON CONFLICT DO NOTHING
+    SQL
+
     # keep enable_experimental_hashtag_autocomplete disabled for for existing sites
-    if result.first['created_at'].to_datetime < 1.hour.ago
-      execute <<~SQL
-        INSERT INTO site_settings (name, data_type, value, created_at, updated_at)
-        VALUES ('enable_experimental_hashtag_autocomplete', 5, 'f', now(), now())
-        ON CONFLICT DO NOTHING
-      SQL
-    end
+    execute settings_insert_query if result.first["created_at"].to_datetime < 1.hour.ago
   end
 
   def down
