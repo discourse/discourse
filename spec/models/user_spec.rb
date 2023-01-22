87c9393558c19d08e9fd10b7e2ad4a173a9fee38
Vinoth Kannan <svkn.87@gmail.com>
2023-01-22 15:44:29 +0530

    FIX: skip email if blank while syncing SSO attributes.
    Also, return email blank error in `EmailValidator`  when the email is blank.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2418,6 +2418,10 @@ RSpec.describe User do
       expect(user.secondary_emails.count).to eq(0)
     end
 
+    it 'returns error if email is nil' do
+      user.email = nil
+      expect { user.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 
   describe "set_random_avatar" do
