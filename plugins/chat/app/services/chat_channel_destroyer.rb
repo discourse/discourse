19e13cf9ccfcefd1e9f8ed8cdb320c839e8112b7
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-01-26 11:27:05 +0100

    removes unnecessary indirection

--- plugins/chat/app/services/chat_channel_destroyer.rb
+++ plugins/chat/app/services/chat_channel_destroyer.rb
@@ -13,19 +13,16 @@ class Chat::ChannelDestroyer
   end
 
   service do
-    delete_channel
-    enqueue_delete_channel_relations_job
-  end
-
-  private
-
-  def delete_channel
     ChatChannel.transaction do
       prevents_slug_collision
       soft_delete_channel
       log_channel_deletion
     end
+
+    enqueue_delete_channel_relations_job
   end
+
+  private
 
   def soft_delete_channel
     context.channel.trash!(context.guardian.user)
