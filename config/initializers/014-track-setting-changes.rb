ef583c0cf6fa63943814504f156f46c02acb06b3
Blake Erickson <o.blakeerickson@gmail.com>
2023-01-27 10:41:06 -0700

    Move constant declaration

--- config/initializers/014-track-setting-changes.rb
+++ config/initializers/014-track-setting-changes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+PRIVATE_BOOTSTRAP_MODE_MIN_USERS = 10
+
 DiscourseEvent.on(:site_setting_changed) do |name, old_value, new_value|
   Category.clear_subcategory_ids if name === :max_category_nesting
 
@@ -30,7 +32,7 @@ DiscourseEvent.on(:site_setting_changed) do |name, old_value, new_value|
   end
 
   # Set bootstrap min users for private sites to a lower default
-  PRIVATE_BOOTSTRAP_MODE_MIN_USERS ||= 10.freeze
+  #PRIVATE_BOOTSTRAP_MODE_MIN_USERS ||= 10.freeze
   if name == :login_required && new_value == true && SiteSetting.bootstrap_mode_enabled == true
     if SiteSetting.bootstrap_mode_min_users == SiteSetting.defaults.get(:bootstrap_mode_min_users)
       SiteSetting.bootstrap_mode_min_users = PRIVATE_BOOTSTRAP_MODE_MIN_USERS
