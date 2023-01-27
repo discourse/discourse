2c8dfc3dbc401074e91d930dafe98f38c88a396f
Sam <sam.saffron@gmail.com>
2023-01-28 05:05:27 +1100

    FEATURE: rate limit anon searches per second (#19708)

--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -36,4 +36,46 @@ describe "Search", type: :system, js: true do
       expect(search_page.heading_text).to eq("Search")
     end
   end
+
+  describe "when using full page search on desktop" do
+    before do
+      SearchIndexer.enable
+      SearchIndexer.index(topic, force: true)
+      SiteSetting.rate_limit_search_anon_user_per_minute = 15
+      RateLimiter.enable
+    end
+
+    after { SearchIndexer.disable }
+
+    it "rate limits searches for anonymous users" do
+      queries = %w[
+        one
+        two
+        three
+        four
+        five
+        six
+        seven
+        eight
+        nine
+        ten
+        eleven
+        twelve
+        thirteen
+        fourteen
+        fifteen
+      ]
+
+      visit("/search?expanded=true")
+
+      queries.each do |query|
+        search_page.clear_search_input
+        search_page.type_in_search(query)
+        search_page.click_search_button
+      end
+
+      # Rate limit error should kick in after 15 queries
+      expect(search_page).to have_warning_message
+    end
+  end
 end
