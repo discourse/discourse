5dbc36c75bbd46fea47248e082b673a8f779d813
Osama Sayegh <asooomaasoooma90@gmail.com>
2023-01-28 07:39:25 +0300

    Update app/assets/javascripts/rtlcss-miniracer/webpack.config.js
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>

--- app/assets/javascripts/rtlcss-miniracer/webpack.config.js
+++ app/assets/javascripts/rtlcss-miniracer/webpack.config.js
@@ -7,26 +7,4 @@ module.exports = {
     libraryTarget: "var",
     library: "rtlcss",
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-          {
-            loader: "postcss-loader",
-          },
-        ],
-      },
-    ],
-  },
 };
