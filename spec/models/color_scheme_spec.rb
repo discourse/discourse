66256c15bd81c633d4a363acd262766618573396
David Taylor <david@taylorhq.com>
2023-02-01 09:55:21 +0000

    UX: Calculate missing hover/selected colors from existing colors (#20105)
    `--d-hover` is calculated to be equivalent to primary-100 in light mode, or primary-low in dark mode
    
    `--d-selected` is calculated to be equivalent to primary-low in light mode, or primary-100 in dark mode
    
    `lib/color_math` is introduced to provide some utilities for making these calculations.

--- spec/models/color_scheme_spec.rb
+++ spec/models/color_scheme_spec.rb
@@ -134,4 +134,32 @@ RSpec.describe ColorScheme do
       )
     end
   end
+
+  describe "#resolved_colors" do
+    it "merges database colors with base scheme" do
+      color_scheme = ColorScheme.new
+      color_scheme.color_scheme_colors << ColorSchemeColor.new(name: "primary", hex: "121212")
+      resolved = color_scheme.resolved_colors
+      expect(resolved["primary"]).to eq("121212")
+      expect(resolved["secondary"]).to eq(ColorScheme.base_colors["secondary"])
+    end
+
+    it "calculates 'hover' and 'selected' from existing db colors in dark mode" do
+      color_scheme = ColorScheme.new
+      color_scheme.color_scheme_colors << ColorSchemeColor.new(name: "primary", hex: "ddd")
+      color_scheme.color_scheme_colors << ColorSchemeColor.new(name: "secondary", hex: "222")
+      resolved = color_scheme.resolved_colors
+      expect(resolved["hover"]).to eq("313131")
+      expect(resolved["selected"]).to eq("2c2c2c")
+    end
+
+    it "calculates 'hover' and 'selected' from existing db colors in light mode" do
+      color_scheme = ColorScheme.new
+      color_scheme.color_scheme_colors << ColorSchemeColor.new(name: "primary", hex: "222")
+      color_scheme.color_scheme_colors << ColorSchemeColor.new(name: "secondary", hex: "fff")
+      resolved = color_scheme.resolved_colors
+      expect(resolved["hover"]).to eq("f2f2f2")
+      expect(resolved["selected"]).to eq("e9e9e9")
+    end
+  end
 end
