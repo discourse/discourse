77c8fc51ef5c8197f9fd4953782abd5e965fed1c
Keegan George <kgeorge13@gmail.com>
2023-02-06 10:54:04 -0800

    DEV: Remove edit-category-topic-template updates
    Will address in a separate PR

--- app/assets/javascripts/discourse/app/components/edit-category-topic-template.js
+++ app/assets/javascripts/discourse/app/components/edit-category-topic-template.js
@@ -1,33 +1,11 @@
 import { buildCategoryPanel } from "discourse/components/edit-category-panel";
 import { observes } from "discourse-common/utils/decorators";
 import { schedule } from "@ember/runloop";
-import { ajax } from "discourse/lib/ajax";
-import { computed } from "@ember/object";
 
 export default buildCategoryPanel("topic-template", {
   // Modals are defined using the singleton pattern.
   // Opening the insert link modal will destroy the edit category modal.
   showInsertLinkButton: false,
-  selectedTemplate: "Freeform",
-
-  init() {
-    this._super(...arguments);
-
-    //TODO(keegan): Use an better approach to get templates listed here
-    ajax(`/admin/customize/form-templates.json`).then((result) => {
-      this.set("templates", [
-        { id: 0, name: "Freeform" },
-        ...result.form_templates,
-      ]);
-    });
-  },
-
-  @computed("selectedTemplate")
-  get templateContents() {
-    return this.templates.find(
-      (template) => template.name === this.selectedTemplate
-    ).template;
-  },
 
   @observes("activeTab")
   _activeTabChanged() {
