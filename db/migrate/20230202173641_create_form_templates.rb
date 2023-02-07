ed4cb13864b474a9b07a824b0f26806a46893776
Keegan George <kgeorge13@gmail.com>
2023-02-07 11:05:12 -0800

    DEV: Change migrations

--- db/migrate/20230202173641_create_form_templates.rb
+++ db/migrate/20230202173641_create_form_templates.rb
@@ -4,7 +4,7 @@ class CreateFormTemplates < ActiveRecord::Migration[7.0]
   def change
     create_table :form_templates do |t|
       t.string :name, null: false
-      t.string :template, null: false
+      t.text :template, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20230206182727_change_template_to_text.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeTemplateToText < ActiveRecord::Migration[7.0]
-  def change
-    change_column :form_templates, :template, :text
-  end
-end
