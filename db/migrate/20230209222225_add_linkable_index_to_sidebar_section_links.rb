6b81e5627ebd118d9157b9f614e2c44dcb3e864b
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-02-10 10:26:25 +1100

    FIX: create index concurrently

--- db/migrate/20230209222225_add_linkable_index_to_sidebar_section_links.rb
+++ db/migrate/20230209222225_add_linkable_index_to_sidebar_section_links.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class AddLinkableIndexToSidebarSectionLinks < ActiveRecord::Migration[7.0]
-  def change
-    add_index :sidebar_section_links, %i[linkable_type linkable_id]
+  disable_ddl_transaction!
+
+  def up
+    execute <<~SQL
+    CREATE INDEX CONCURRENTLY index_sidebar_section_links_on_linkable_type_and_linkable_id
+    ON sidebar_section_links (linkable_type,linkable_id)
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+    DROP INDEX CONCURRENTLY IF EXISTS index_sidebar_section_links_on_linkable_type_and_linkable_id
+    SQL
   end
 end
