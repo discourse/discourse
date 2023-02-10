1a9cb0914cc45aa72458ca390ed03d7c16dc0d6a
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-02-10 12:47:10 +0100

    re-adds @tracked activeChannel

--- plugins/chat/assets/javascripts/discourse/services/chat.js
+++ plugins/chat/assets/javascripts/discourse/services/chat.js
@@ -35,8 +35,8 @@ export default class Chat extends Service {
   @service router;
   @service site;
   @service chatChannelsManager;
-
-  @tracked activeThread = null;  
+  @tracked activeChannel = null;
+  @tracked activeThread = null;
   cook = null;
   presenceChannel = null;
   sidebarActive = false;
