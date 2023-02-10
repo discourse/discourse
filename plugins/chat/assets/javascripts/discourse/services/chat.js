c9776fe84d84c986e79c50ccbbdf27cb5e87a38a
Martin Brennan <martin@discourse.org>
2023-02-10 20:41:29 +1000

    DEV: Change setActiveChannel to get/set in services/chat (#20237)
    Per PR comment in #20209, we don't need to do setActiveChannel,
    we can just use native get/set instead.

--- plugins/chat/assets/javascripts/discourse/services/chat.js
+++ plugins/chat/assets/javascripts/discourse/services/chat.js
@@ -1,4 +1,5 @@
 import deprecated from "discourse-common/lib/deprecated";
+import { tracked } from "@glimmer/tracking";
 import userSearch from "discourse/lib/user-search";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import Service, { inject as service } from "@ember/service";
@@ -35,7 +36,8 @@ export default class Chat extends Service {
   @service site;
   @service chatChannelsManager;
 
-  activeChannel = null;
+  @tracked activeChannel = null;
+
   cook = null;
   presenceChannel = null;
   sidebarActive = false;
@@ -114,10 +116,6 @@ export default class Chat extends Service {
     if (this.userCanChat) {
       this.chatSubscriptionsManager.stopChannelsSubscriptions();
     }
-  }
-
-  setActiveChannel(channel) {
-    this.set("activeChannel", channel);
   }
 
   loadCookFunction(categories) {
@@ -281,12 +279,12 @@ export default class Chat extends Service {
         this.router.currentRouteName === "chat.channel.near-message") &&
       this.activeChannel?.id === channel.id
     ) {
-      this.setActiveChannel(channel);
+      this.activeChannel = channel;
       this._fireOpenMessageAppEvent(messageId);
       return Promise.resolve();
     }
 
-    this.setActiveChannel(channel);
+    this.activeChannel = channel;
 
     if (
       this.chatStateManager.isFullPageActive ||
