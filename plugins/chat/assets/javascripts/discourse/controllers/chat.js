a6af08549b4a1f1f39f44a0fce9e56fb13b44e1a
Martin Brennan <martin@discourse.org>
2023-02-10 15:09:20 +1000

    DEV: Add mainOutletModifierClasses for chat main outlet
    The main outlet needs different styles based on the route,
    and whether the sidebar is expanded.

--- plugins/chat/assets/javascripts/discourse/controllers/chat.js
+++ plugins/chat/assets/javascripts/discourse/controllers/chat.js
@@ -4,6 +4,7 @@ import { inject as service } from "@ember/service";
 export default class ChatController extends Controller {
   @service chat;
   @service chatStateManager;
+  @service router;
 
   get shouldUseChatSidebar() {
     if (this.site.mobileView) {
@@ -19,5 +20,19 @@ export default class ChatController extends Controller {
 
   get shouldUseCoreSidebar() {
     return this.siteSettings.navigation_menu === "sidebar";
+  }
+
+  get mainOutletModifierClasses() {
+    let modifierClasses = [];
+
+    if (this.chatStateManager.isSidePanelExpanded) {
+      modifierClasses.push("has-side-panel-expanded");
+    }
+
+    if (this.router.currentRouteName.startsWith("chat.channel.info")) {
+      modifierClasses.push("chat-blah");
+    }
+
+    return modifierClasses.join(" ");
   }
 }
