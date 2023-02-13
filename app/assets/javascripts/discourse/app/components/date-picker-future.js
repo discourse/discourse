878867289087e45448dd0c928e067903923e5189
David Taylor <david@taylorhq.com>
2023-02-13 18:20:05 +0000

    DEV: Remove unneeded layoutName properties
    Now that templates are colocated, they are automatically associated with the Component class (and all child classes)

--- app/assets/javascripts/discourse/app/components/date-picker-future.js
+++ app/assets/javascripts/discourse/app/components/date-picker-future.js
@@ -1,8 +1,6 @@
 import DatePicker from "discourse/components/date-picker";
 
 export default DatePicker.extend({
-  layoutName: "components/date-picker",
-
   _opts() {
     return {
       defaultDate: this.defaultDate || moment().add(1, "day").toDate(),
