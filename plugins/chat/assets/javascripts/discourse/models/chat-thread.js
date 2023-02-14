7ea58a0dc0564c370c8f0d306915c56a1d50f3e7
Martin Brennan <martin@discourse.org>
2023-02-14 11:20:32 +1000

    DEV: Review fixes

--- plugins/chat/assets/javascripts/discourse/models/chat-thread.js
+++ plugins/chat/assets/javascripts/discourse/models/chat-thread.js
@@ -1,5 +1,4 @@
 import RestModel from "discourse/models/rest";
-import I18n from "I18n";
 import User from "discourse/models/user";
 import { escapeExpression } from "discourse/lib/utilities";
 import { tracked } from "@glimmer/tracking";
@@ -11,60 +10,12 @@ export const THREAD_STATUSES = {
   archived: "archived",
 };
 
-export function threadStatusName(status) {
-  switch (status) {
-    case THREAD_STATUSES.open:
-      return I18n.t("chat.thread_status.open");
-    case THREAD_STATUSES.readOnly:
-      return I18n.t("chat.thread_status.read_only");
-    case THREAD_STATUSES.closed:
-      return I18n.t("chat.thread_status.closed");
-    case THREAD_STATUSES.archived:
-      return I18n.t("chat.thread_status.archived");
-  }
-}
-
-const READONLY_STATUSES = [
-  THREAD_STATUSES.closed,
-  THREAD_STATUSES.readOnly,
-  THREAD_STATUSES.archived,
-];
-
-const STAFF_READONLY_STATUSES = [
-  THREAD_STATUSES.readOnly,
-  THREAD_STATUSES.archived,
-];
-
 export default class ChatThread extends RestModel {
   @tracked title;
   @tracked status;
 
   get escapedTitle() {
     return escapeExpression(this.title);
-  }
-
-  get isOpen() {
-    return this.status === THREAD_STATUSES.open;
-  }
-
-  get isReadOnly() {
-    return this.status === THREAD_STATUSES.readOnly;
-  }
-
-  get isClosed() {
-    return this.status === THREAD_STATUSES.closed;
-  }
-
-  get isArchived() {
-    return this.status === THREAD_STATUSES.archived;
-  }
-
-  canModifyMessages(user) {
-    if (user.staff) {
-      return !STAFF_READONLY_STATUSES.includes(this.status);
-    }
-
-    return !READONLY_STATUSES.includes(this.status);
   }
 }
 
