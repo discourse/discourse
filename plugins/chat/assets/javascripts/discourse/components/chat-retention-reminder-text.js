5a47dc708e0cc8ef8b7b10a1230e1136d07377b7
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-02-15 14:08:17 +0100

    Update plugins/chat/assets/javascripts/discourse/components/chat-retention-reminder-text.js
    Co-authored-by: Gerhard Schlager <mail@gerhard-schlager.at>

--- plugins/chat/assets/javascripts/discourse/components/chat-retention-reminder-text.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-retention-reminder-text.js
@@ -6,26 +6,17 @@ export default class ChatRetentionReminderText extends Component {
   @service siteSettings;
 
   get text() {
-    let prefix = `${this.#baseKey}.${this.#channelTypeKey}`;
-
-    if (this.#countForChannelType > 0) {
-      return I18n.t(prefix, { count: this.#countForChannelType });
+    if (this.args.channel.isDirectMessageChannel) {
+      if (this.#countForChannelType > 0) {
+        return I18n.t("chat.retention_reminders.dm", { count: this.siteSettings.chat_dm_retention_days });
+      } else {
+        return I18n.t("chat.retention_reminders.dm_none");
+      }
+    } else {
+      if (this.#countForChannelType > 0) {
+        return I18n.t("chat.retention_reminders.public", { count: this.siteSettings.chat_channel_retention_days });
+      } else {
+        return I18n.t("chat.retention_reminders.public_none");
+      }
     }
-
-    return I18n.t(`${prefix}_none`);
   }
-
-  get #baseKey() {
-    return "chat.retention_reminders";
-  }
-
-  get #channelTypeKey() {
-    return this.args.channel.isDirectMessageChannel ? "dm" : "public";
-  }
-
-  get #countForChannelType() {
-    return this.args.channel.isDirectMessageChannel
-      ? this.siteSettings.chat_dm_retention_days
-      : this.siteSettings.chat_channel_retention_days;
-  }
-}
