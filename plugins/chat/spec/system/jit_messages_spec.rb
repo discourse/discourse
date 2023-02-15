cf83d45491861e5944d4fe623bc22baedf492ec0
Jan Cernik <jancernik12@gmail.com>
2023-02-15 19:33:09 -0300

    Fix JIT messages Setting `everyone` as one of the category groups makes the category non `read_restricted`. Thus, the `can_post_in_category?` guardian method was allowing the invitation of people who shouldn't be allowed.

--- plugins/chat/spec/system/jit_messages_spec.rb
+++ plugins/chat/spec/system/jit_messages_spec.rb
@@ -14,12 +14,13 @@ RSpec.describe "JIT messages", type: :system, js: true do
     sign_in(current_user)
   end
 
-  context "when mentioning a user not on the channel" do
-    it "displays a mention warning" do
-      Jobs.run_immediately!
+  context "when mentioning a user" do
+    context "when user is not on the channel" do
+      it "displays a mention warning" do
+        Jobs.run_immediately!
 
-      chat.visit_channel(channel_1)
-      channel.send_message("hi @#{other_user.username}")
+        chat.visit_channel(channel_1)
+        channel.send_message("hi @#{other_user.username}")
 
       expect(page).to have_content(
         I18n.t("js.chat.mention_warning.without_membership", username: other_user.username),
@@ -28,7 +29,59 @@ RSpec.describe "JIT messages", type: :system, js: true do
     end
   end
 
-  context "when mentioning a user who can’t access the channel" do
+    context "when user can’t access the channel" do
+      fab!(:group_1) { Fabricate(:group) }
+      fab!(:private_channel_1) { Fabricate(:private_category_channel, group: group_1) }
+
+      before do
+        group_1.add(current_user)
+        private_channel_1.add(current_user)
+      end
+
+      it "displays a mention warning" do
+        Jobs.run_immediately!
+
+        chat.visit_channel(private_channel_1)
+        channel.send_message("hi @#{other_user.username}")
+
+        expect(page).to have_content(
+          I18n.t("js.chat.mention_warning.cannot_see.one", username: other_user.username),
+          wait: 5,
+        )
+      end
+    end
+
+    context "when user can’t access a non read_restrictd channel" do
+      let!(:everyone) { Group.find(Group::AUTO_GROUPS[:everyone]) }
+      fab!(:category) { Fabricate(:category) }
+      fab!(:readonly_channel) { Fabricate(:category_channel, chatable: category) }
+
+      before do
+        Fabricate(
+          :category_group,
+          category: category,
+          group: everyone,
+          permission_type: CategoryGroup.permission_types[:readonly],
+        )
+        everyone.add(other_user)
+        readonly_channel.add(current_user)
+      end
+
+      it "displays a mention warning" do
+        Jobs.run_immediately!
+
+        chat.visit_channel(readonly_channel)
+        channel.send_message("hi @#{other_user.username}")
+
+        expect(page).to have_content(
+          I18n.t("js.chat.mention_warning.cannot_see.one", username: other_user.username),
+          wait: 5,
+        )
+      end
+    end
+  end
+
+  context "when category channel permission is readonly for everyone" do
     fab!(:group_1) { Fabricate(:group) }
     fab!(:private_channel_1) { Fabricate(:private_category_channel, group: group_1) }
 
