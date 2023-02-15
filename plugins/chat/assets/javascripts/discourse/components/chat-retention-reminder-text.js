fa329cdeddfce94b32678a5935c6c07424e498ed
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-02-15 14:25:07 +0100

    fix after merge commit on github UI

--- plugins/chat/assets/javascripts/discourse/components/chat-retention-reminder-text.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-retention-reminder-text.js
@@ -8,15 +8,26 @@ export default class ChatRetentionReminderText extends Component {
   get text() {
     if (this.args.channel.isDirectMessageChannel) {
       if (this.#countForChannelType > 0) {
-        return I18n.t("chat.retention_reminders.dm", { count: this.siteSettings.chat_dm_retention_days });
+        return I18n.t("chat.retention_reminders.dm", {
+          count: this.siteSettings.chat_dm_retention_days,
+        });
       } else {
         return I18n.t("chat.retention_reminders.dm_none");
       }
     } else {
       if (this.#countForChannelType > 0) {
-        return I18n.t("chat.retention_reminders.public", { count: this.siteSettings.chat_channel_retention_days });
+        return I18n.t("chat.retention_reminders.public", {
+          count: this.siteSettings.chat_channel_retention_days,
+        });
       } else {
         return I18n.t("chat.retention_reminders.public_none");
       }
     }
   }
+
+  get #countForChannelType() {
+    return this.args.channel.isDirectMessageChannel
+      ? this.siteSettings.chat_dm_retention_days
+      : this.siteSettings.chat_channel_retention_days;
+  }
+}
