67ed12b02a7e34528fc9125e3611899f0bfc7768
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-02-17 22:29:08 +0100

    uses qunit dom

--- app/assets/javascripts/discourse/tests/integration/components/char-counter-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/char-counter-test.js
@@ -1,42 +1,41 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
 import { fillIn, render } from "@ember/test-helpers";
-import { query } from "discourse/tests/helpers/qunit-helpers";
 import { hbs } from "ember-cli-htmlbars";
 
 module("Integration | Component | char-counter", function (hooks) {
   setupRenderingTest(hooks);
 
-  test("character counter shows the number of characters in value property", async function (assert) {
+  test("shows the number of characters", async function (assert) {
     this.value = "Hello World";
     this.max = 12;
+
     await render(
       hbs`<CharCounter @value={{this.value}} @max={{this.max}}></CharCounter>`
     );
-    assert.strictEqual("11/12", this.element.innerText.trim());
+
+    assert.dom(this.element).includesText("11/12");
   });
 
-  test("updating value with textarea updates counter", async function (assert) {
+  test("updating value updates counter", async function (assert) {
     this.max = 50;
 
     await render(
       hbs`<CharCounter @value={{this.charCounterContent}} @max={{this.max}}><textarea {{on "input" (action (mut this.charCounterContent) value="target.value")}}></textarea></CharCounter>`
     );
-    assert.strictEqual(
-      "/50",
-      this.element.innerText.trim(),
-      "initial value appears as expected"
-    );
+
+    assert
+      .dom(this.element)
+      .includesText("/50", "initial value appears as expected");
+
     await fillIn("textarea", "Hello World, this is a longer string");
 
-    assert.strictEqual(
-      "36/50",
-      this.element.innerText.trim(),
-      "updated value appears as expected"
-    );
+    assert
+      .dom(this.element)
+      .includesText("36/50", "updated value appears as expected");
   });
 
-  test("exceeding max applies classes", async function (assert) {
+  test("exceeding max length", async function (assert) {
     this.max = 10;
     this.value = "Hello World";
 
@@ -44,10 +43,6 @@ module("Integration | Component | char-counter", function (hooks) {
       hbs`<CharCounter @value={{this.value}} @max={{this.max}}></CharCounter>`
     );
 
-    assert.strictEqual(
-      query(".char-counter").classList.contains("exceeded"),
-      true,
-      "exceeded styling is applied when value exceeds max"
-    );
+    assert.dom(".char-counter.exceeded").exists("exceeded class is applied");
   });
 });
