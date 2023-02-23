a433b30650d125e6685fb13f679f613003f246aa
David Taylor <david@taylorhq.com>
2023-02-23 15:32:53 +0000

    DEV: Convert admin component definitions to native class syntax (#20311)
    This conversion was achieved using the ember-native-class-codemod, plus a handful of manual fixes/tweaks

--- app/assets/javascripts/admin/addon/components/embeddable-host.js
+++ app/assets/javascripts/admin/addon/components/embeddable-host.js
@@ -1,85 +1,91 @@
+import { action } from "@ember/object";
+import { tagName } from "@ember-decorators/component";
+import { inject as service } from "@ember/service";
+import { or } from "@ember/object/computed";
 import Category from "discourse/models/category";
 import Component from "@ember/component";
 import I18n from "I18n";
 import { bufferedProperty } from "discourse/mixins/buffered-content";
 import discourseComputed from "discourse-common/utils/decorators";
-import { inject as service } from "@ember/service";
 import { isEmpty } from "@ember/utils";
-import { or } from "@ember/object/computed";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 
-export default Component.extend(bufferedProperty("host"), {
-  editToggled: false,
-  tagName: "tr",
-  categoryId: null,
-  category: null,
-  dialog: service(),
+@tagName("tr")
+export default class EmbeddableHost extends Component.extend(
+  bufferedProperty("host")
+) {
+  @service dialog;
+  editToggled = false;
+  categoryId = null;
+  category = null;
 
-  editing: or("host.isNew", "editToggled"),
+  @or("host.isNew", "editToggled") editing;
 
-  init() {
-    this._super(...arguments);
+  constructor() {
+    super(...arguments);
 
     const host = this.host;
     const categoryId = host.category_id || this.site.uncategorized_category_id;
     const category = Category.findById(categoryId);
 
     host.set("category", category);
-  },
+  }
 
   @discourseComputed("buffered.host", "host.isSaving")
   cantSave(host, isSaving) {
     return isSaving || isEmpty(host);
-  },
+  }
 
-  actions: {
-    edit() {
-      this.set("categoryId", this.get("host.category.id"));
-      this.set("editToggled", true);
-    },
+  @action
+  edit() {
+    this.set("categoryId", this.get("host.category.id"));
+    this.set("editToggled", true);
+  }
 
-    save() {
-      if (this.cantSave) {
-        return;
-      }
+  @action
+  save() {
+    if (this.cantSave) {
+      return;
+    }
 
-      const props = this.buffered.getProperties(
-        "host",
-        "allowed_paths",
-        "class_name"
-      );
-      props.category_id = this.categoryId;
+    const props = this.buffered.getProperties(
+      "host",
+      "allowed_paths",
+      "class_name"
+    );
+    props.category_id = this.categoryId;
 
-      const host = this.host;
+    const host = this.host;
 
-      host
-        .save(props)
-        .then(() => {
-          host.set("category", Category.findById(this.categoryId));
-          this.set("editToggled", false);
-        })
-        .catch(popupAjaxError);
-    },
-
-    delete() {
-      return this.dialog.confirm({
-        message: I18n.t("admin.embedding.confirm_delete"),
-        didConfirm: () => {
-          return this.host.destroyRecord().then(() => {
-            this.deleteHost(this.host);
-          });
-        },
-      });
-    },
-
-    cancel() {
-      const host = this.host;
-      if (host.get("isNew")) {
-        this.deleteHost(host);
-      } else {
-        this.rollbackBuffer();
+    host
+      .save(props)
+      .then(() => {
+        host.set("category", Category.findById(this.categoryId));
         this.set("editToggled", false);
-      }
-    },
-  },
-});
+      })
+      .catch(popupAjaxError);
+  }
+
+  @action
+  delete() {
+    return this.dialog.confirm({
+      message: I18n.t("admin.embedding.confirm_delete"),
+      didConfirm: () => {
+        return this.host.destroyRecord().then(() => {
+          this.deleteHost(this.host);
+        });
+      },
+    });
+  }
+
+  @action
+  cancel() {
+    const host = this.host;
+    if (host.get("isNew")) {
+      this.deleteHost(host);
+    } else {
+      this.rollbackBuffer();
+      this.set("editToggled", false);
+    }
+  }
+}
--- app/assets/javascripts/admin/addon/components/simple-list.js
+++ app/assets/javascripts/admin/addon/components/simple-list.js
@@ -1,34 +1,36 @@
+import { classNameBindings } from "@ember-decorators/component";
+import { empty } from "@ember/object/computed";
 import Component from "@ember/component";
 import { action } from "@ember/object";
-import { empty } from "@ember/object/computed";
 import discourseComputed, { on } from "discourse-common/utils/decorators";
 
-export default Component.extend({
-  classNameBindings: [":simple-list", ":value-list"],
-  inputEmpty: empty("newValue"),
-  inputDelimiter: null,
-  newValue: "",
-  collection: null,
-  values: null,
+@classNameBindings(":simple-list", ":value-list")
+export default class SimpleList extends Component {
+  @empty("newValue") inputEmpty;
+
+  inputDelimiter = null;
+  newValue = "";
+  collection = null;
+  values = null;
 
   @on("didReceiveAttrs")
   _setupCollection() {
     this.set("collection", this._splitValues(this.values, this.inputDelimiter));
-  },
+  }
 
   keyDown(event) {
     if (event.which === 13) {
       this.addValue(this.newValue);
       return;
     }
-  },
+  }
 
   @action
   changeValue(index, event) {
     this.collection.replace(index, 1, [event.target.value]);
     this.collection.arrayContentDidChange(index);
     this._onChange();
-  },
+  }
 
   @action
   addValue(newValue) {
@@ -39,13 +41,13 @@ export default Component.extend({
     this.set("newValue", null);
     this.collection.addObject(newValue);
     this._onChange();
-  },
+  }
 
   @action
   removeValue(value) {
     this.collection.removeObject(value);
     this._onChange();
-  },
+  }
 
   @action
   shift(operation, index) {
@@ -62,20 +64,20 @@ export default Component.extend({
     this.collection.insertAt(futureIndex, shiftedValue);
 
     this._onChange();
-  },
+  }
 
   _onChange() {
     this.onChange?.(this.collection);
-  },
+  }
 
   @discourseComputed("collection")
   showUpDownButtons(collection) {
     return collection.length - 1 ? true : false;
-  },
+  }
 
   _splitValues(values, delimiter) {
     return values && values.length
       ? values.split(delimiter || "\n").filter(Boolean)
       : [];
-  },
-});
+  }
+}
