3273388e8c7909e581932c71dcd1d3add8e7a2ad
Penar Musaraj <pmusaraj@gmail.com>
2023-02-23 10:49:04 -0500

    FIX: Deleting security keys was not working
    Bug was introduced in e313190fdb186e1bc04bdb99eef40d365c23a560. There is
    a workaround, using the trash icon in the edit modal, but that UI is
    quite confusing to users.
    
    PR is missing tests (due to time constraints on my part).

--- app/assets/javascripts/discourse/app/controllers/second-factor-edit-security-key.js
+++ app/assets/javascripts/discourse/app/controllers/second-factor-edit-security-key.js
@@ -3,25 +3,6 @@ import ModalFunctionality from "discourse/mixins/modal-functionality";
 
 export default Controller.extend(ModalFunctionality, {
   actions: {
-    disableSecurityKey() {
-      this.user
-        .updateSecurityKey(this.model.id, this.model.name, true)
-        .then((response) => {
-          if (response.error) {
-            return;
-          }
-          this.markDirty();
-        })
-        .catch((error) => {
-          this.send("closeModal");
-          this.onError(error);
-        })
-        .finally(() => {
-          this.set("loading", false);
-          this.send("closeModal");
-        });
-    },
-
     editSecurityKey() {
       this.user
         .updateSecurityKey(this.model.id, this.model.name, false)
