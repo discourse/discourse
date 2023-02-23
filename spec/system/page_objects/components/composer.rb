7c53c01d4fcb3cc634f32090154dd14984397413
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-02-23 10:02:31 +0800

    DEV: Port sidebar mobile view acceptance tests to system tests
    The acceptance tests are flaky so I've decided to port them to system
    test which makes them easier to work with and reason about.

--- spec/system/page_objects/components/composer.rb
+++ spec/system/page_objects/components/composer.rb
@@ -3,13 +3,19 @@
 module PageObjects
   module Components
     class Composer < PageObjects::Components::Base
+      COMPOSER_ID = "#reply-control"
+
+      def opened?
+        page.has_css?("#{COMPOSER_ID}.open")
+      end
+
       def open_composer_actions
         find(".composer-action-title .btn").click
         self
       end
 
       def fill_title(title)
-        find("#reply-control #reply-title").fill_in(with: title)
+        find("#{COMPOSER_ID} #reply-title").fill_in(with: title)
         self
       end
 
@@ -37,7 +43,7 @@ module PageObjects
       end
 
       def create
-        find("#reply-control .btn-primary").click
+        find("#{COMPOSER_ID} .btn-primary").click
       end
 
       def action(action_title)
@@ -45,11 +51,11 @@ module PageObjects
       end
 
       def button_label
-        find("#reply-control .btn-primary .d-button-label")
+        find("#{COMPOSER_ID} .btn-primary .d-button-label")
       end
 
       def composer_input
-        find("#reply-control .d-editor .d-editor-input")
+        find("#{COMPOSER_ID} .d-editor .d-editor-input")
       end
     end
   end
