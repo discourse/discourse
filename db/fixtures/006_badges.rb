62d9a95cec82a00d43f0f6d33c700e397ec3efd0
Nat <natalie.tay@discourse.org>
2023-02-27 18:54:14 +0800

    Update wiki badge icon

--- db/fixtures/006_badges.rb
+++ db/fixtures/006_badges.rb
@@ -247,7 +247,7 @@ end
 Badge.seed do |b|
   b.id = Badge::WikiEditor
   b.name = "Wiki Editor"
-  b.default_icon = "pen"
+  b.default_icon = "far-edit"
   b.badge_type_id = BadgeType::Bronze
   b.multiple_grant = false
   b.target_posts = true
