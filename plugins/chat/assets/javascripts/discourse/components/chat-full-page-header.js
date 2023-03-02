2b5e1ba82c2aa28e51a46918cb9e942cac70e805
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-03-02 15:05:02 +0100

    hides full page header logic

--- plugins/chat/assets/javascripts/discourse/components/chat-full-page-header.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-full-page-header.js
@@ -3,4 +3,9 @@ import Component from "@glimmer/component";
 
 export default class ChatFullPageHeader extends Component {
   @service site;
+  @service chatStateManager;
+
+  get displayed() {
+    return this.args.displayed ?? true;
+  }
 }
