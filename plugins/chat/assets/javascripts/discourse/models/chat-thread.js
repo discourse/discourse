21517fd14117b244f5fbfac5b9ec5901b8a4f087
Martin Brennan <martin@discourse.org>
2023-03-08 15:50:12 +1000

    WIP: Chat message loading in thread
    Now that the live pane refactor is done, restart
    this against the main branch.

--- plugins/chat/assets/javascripts/discourse/models/chat-thread.js
+++ plugins/chat/assets/javascripts/discourse/models/chat-thread.js
@@ -1,4 +1,4 @@
-import RestModel from "discourse/models/rest";
+import { TrackedArray } from "@ember-compat/tracked-built-ins";
 import User from "discourse/models/user";
 import { escapeExpression } from "discourse/lib/utilities";
 import { tracked } from "@glimmer/tracking";
@@ -10,22 +10,64 @@ export const THREAD_STATUSES = {
   archived: "archived",
 };
 
-export default class ChatThread extends RestModel {
+export default class ChatThread {
   @tracked title;
   @tracked status;
+  @tracked messages = new TrackedArray();
+
+  constructor(args = {}) {
+    this.title = args.title;
+    this.id = args.id;
+    this.status = args.status;
+
+    this.originalMessageUser = this.#initUserModel(args.original_message_user);
+    // TODO (martin) Not sure if ChatMessage is needed here, original_message
+    // only has a small subset of message stuff.
+    // this.originalMessage = new ChatMessage(args.original_message);
+    this.originalMessage = args.original_message;
+    this.originalMessage.user = this.originalMessageUser;
+  }
 
   get escapedTitle() {
     return escapeExpression(this.title);
   }
-}
 
-ChatThread.reopenClass({
-  create(args) {
-    args = args || {};
-    if (!args.original_message_user instanceof User) {
-      args.original_message_user = User.create(args.original_message_user);
+  clearMessages() {
+    this.messages.clear();
+
+    this.canLoadMoreFuture = null;
+    this.canLoadMorePast = null;
+  }
+
+  appendMessages(messages) {
+    this.messages.pushObjects(messages);
+  }
+
+  prependMessages(messages) {
+    this.messages.unshiftObjects(messages);
+  }
+
+  findMessage(messageId) {
+    return this.messages.find(
+      (message) => message.id === parseInt(messageId, 10)
+    );
+  }
+
+  removeMessage(message) {
+    return this.messages.removeObject(message);
+  }
+
+  findStagedMessage(stagedMessageId) {
+    return this.messages.find(
+      (message) => message.stagedId === stagedMessageId
+    );
+  }
+
+  #initUserModel(user) {
+    if (!user || user instanceof User) {
+      return user;
     }
-    args.original_message.user = args.original_message_user;
-    return this._super(args);
-  },
-});
+
+    return User.create(user);
+  }
+}
