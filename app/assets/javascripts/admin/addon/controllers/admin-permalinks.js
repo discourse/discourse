b013cc378f506ae995f253c87d93f798ee2f35d8
David Taylor <david@taylorhq.com>
2023-03-14 11:24:47 +0000

    Apply codemod to modified files

--- app/assets/javascripts/admin/addon/controllers/admin-permalinks.js
+++ app/assets/javascripts/admin/addon/controllers/admin-permalinks.js
@@ -1,3 +1,6 @@
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { or } from "@ember/object/computed";
 import Controller from "@ember/controller";
 import I18n from "I18n";
 import { INPUT_DELAY } from "discourse-common/config/environment";
@@ -5,55 +8,56 @@ import Permalink from "admin/models/permalink";
 import discourseDebounce from "discourse-common/lib/debounce";
 import { observes } from "discourse-common/utils/decorators";
 import { clipboardCopy } from "discourse/lib/utilities";
-import { inject as service } from "@ember/service";
-import { or } from "@ember/object/computed";
 
-export default Controller.extend({
-  dialog: service(),
-  loading: false,
-  filter: null,
-  showSearch: or("model.length", "filter"),
+export default class AdminPermalinksController extends Controller {
+  @service dialog;
+
+  loading = false;
+  filter = null;
+
+  @or("model.length", "filter") showSearch;
 
   _debouncedShow() {
     Permalink.findAll(this.filter).then((result) => {
       this.set("model", result);
       this.set("loading", false);
     });
-  },
+  }
 
   @observes("filter")
   show() {
     discourseDebounce(this, this._debouncedShow, INPUT_DELAY);
-  },
+  }
 
-  actions: {
-    recordAdded(arg) {
-      this.model.unshiftObject(arg);
-    },
+  @action
+  recordAdded(arg) {
+    this.model.unshiftObject(arg);
+  }
 
-    copyUrl(pl) {
-      let linkElement = document.querySelector(`#admin-permalink-${pl.id}`);
-      clipboardCopy(linkElement.textContent);
-    },
+  @action
+  copyUrl(pl) {
+    let linkElement = document.querySelector(`#admin-permalink-${pl.id}`);
+    clipboardCopy(linkElement.textContent);
+  }
 
-    destroyRecord(record) {
-      return this.dialog.yesNoConfirm({
-        message: I18n.t("admin.permalink.delete_confirm"),
-        didConfirm: () => {
-          return record.destroy().then(
-            (deleted) => {
-              if (deleted) {
-                this.model.removeObject(record);
-              } else {
-                this.dialog.alert(I18n.t("generic_error"));
-              }
-            },
-            function () {
+  @action
+  destroyRecord(record) {
+    return this.dialog.yesNoConfirm({
+      message: I18n.t("admin.permalink.delete_confirm"),
+      didConfirm: () => {
+        return record.destroy().then(
+          (deleted) => {
+            if (deleted) {
+              this.model.removeObject(record);
+            } else {
               this.dialog.alert(I18n.t("generic_error"));
             }
-          );
-        },
-      });
-    },
-  },
-});
+          },
+          function () {
+            this.dialog.alert(I18n.t("generic_error"));
+          }
+        );
+      },
+    });
+  }
+}
--- app/assets/javascripts/admin/addon/controllers/admin-user-fields.js
+++ app/assets/javascripts/admin/addon/controllers/admin-user-fields.js
@@ -1,73 +1,79 @@
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 import { gte, sort } from "@ember/object/computed";
 import Controller from "@ember/controller";
 import I18n from "I18n";
 import { popupAjaxError } from "discourse/lib/ajax-error";
-import { inject as service } from "@ember/service";
 
 const MAX_FIELDS = 30;
 
-export default Controller.extend({
-  dialog: service(),
-  fieldTypes: null,
-  createDisabled: gte("model.length", MAX_FIELDS),
-  sortedFields: sort("model", "fieldSortOrder"),
+export default class AdminUserFieldsController extends Controller {
+  @service dialog;
+
+  fieldTypes = null;
+
+  @gte("model.length", MAX_FIELDS) createDisabled;
+
+  @sort("model", "fieldSortOrder") sortedFields;
 
   init() {
-    this._super(...arguments);
+    super.init(...arguments);
 
     this.fieldSortOrder = ["position"];
-  },
+  }
 
-  actions: {
-    createField() {
-      const f = this.store.createRecord("user-field", {
-        field_type: "text",
-        position: MAX_FIELDS,
+  @action
+  createField() {
+    const f = this.store.createRecord("user-field", {
+      field_type: "text",
+      position: MAX_FIELDS,
+    });
+    this.model.pushObject(f);
+  }
+
+  @action
+  moveUp(f) {
+    const idx = this.sortedFields.indexOf(f);
+    if (idx) {
+      const prev = this.sortedFields.objectAt(idx - 1);
+      const prevPos = prev.get("position");
+
+      prev.update({ position: f.get("position") });
+      f.update({ position: prevPos });
+    }
+  }
+
+  @action
+  moveDown(f) {
+    const idx = this.sortedFields.indexOf(f);
+    if (idx > -1) {
+      const next = this.sortedFields.objectAt(idx + 1);
+      const nextPos = next.get("position");
+
+      next.update({ position: f.get("position") });
+      f.update({ position: nextPos });
+    }
+  }
+
+  @action
+  destroyField(f) {
+    const model = this.model;
+
+    // Only confirm if we already been saved
+    if (f.get("id")) {
+      this.dialog.yesNoConfirm({
+        message: I18n.t("admin.user_fields.delete_confirm"),
+        didConfirm: () => {
+          return f
+            .destroyRecord()
+            .then(function () {
+              model.removeObject(f);
+            })
+            .catch(popupAjaxError);
+        },
       });
-      this.model.pushObject(f);
-    },
-
-    moveUp(f) {
-      const idx = this.sortedFields.indexOf(f);
-      if (idx) {
-        const prev = this.sortedFields.objectAt(idx - 1);
-        const prevPos = prev.get("position");
-
-        prev.update({ position: f.get("position") });
-        f.update({ position: prevPos });
-      }
-    },
-
-    moveDown(f) {
-      const idx = this.sortedFields.indexOf(f);
-      if (idx > -1) {
-        const next = this.sortedFields.objectAt(idx + 1);
-        const nextPos = next.get("position");
-
-        next.update({ position: f.get("position") });
-        f.update({ position: nextPos });
-      }
-    },
-
-    destroyField(f) {
-      const model = this.model;
-
-      // Only confirm if we already been saved
-      if (f.get("id")) {
-        this.dialog.yesNoConfirm({
-          message: I18n.t("admin.user_fields.delete_confirm"),
-          didConfirm: () => {
-            return f
-              .destroyRecord()
-              .then(function () {
-                model.removeObject(f);
-              })
-              .catch(popupAjaxError);
-          },
-        });
-      } else {
-        model.removeObject(f);
-      }
-    },
-  },
-});
+    } else {
+      model.removeObject(f);
+    }
+  }
+}
