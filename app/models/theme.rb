84f590ab83b90d4c6754e247744567f75274fb69
Daniel Waterworth <me@danielwaterworth.com>
2023-03-14 13:11:45 -0500

    DEV: Store theme sprites in the DB (#20501)
    Let's avoid fetching sprites from the CDN during page rendering.

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -33,6 +33,7 @@ class Theme < ActiveRecord::Base
   has_many :color_schemes
   belongs_to :remote_theme, dependent: :destroy
   has_one :theme_modifier_set, dependent: :destroy
+  has_one :theme_svg_sprite, dependent: :destroy
 
   has_one :settings_field,
           -> { where(target_id: Theme.targets[:settings], name: "yaml") },
--- spec/lib/svg_sprite/svg_sprite_spec.rb
+++ spec/lib/svg_sprite/svg_sprite_spec.rb
@@ -3,7 +3,10 @@
 RSpec.describe SvgSprite do
   fab!(:theme) { Fabricate(:theme) }
 
-  before { SvgSprite.expire_cache }
+  before do
+    SvgSprite.clear_plugin_svg_sprite_cache!
+    SvgSprite.expire_cache
+  end
 
   it "can generate a bundle" do
     bundle = SvgSprite.bundle
@@ -185,14 +188,14 @@ RSpec.describe SvgSprite do
       expect(sprite_files).to match(/my-custom-theme-icon/)
 
       SvgSprite.bundle(theme.id)
-      expect(SvgSprite.cache.hash.keys).to include("custom_svg_sprites_#{theme.id}")
+      expect(SvgSprite.cache.hash.keys).to include("theme_svg_sprites_#{theme.id}")
 
       external_copy = Discourse.store.download(upload_s3)
       File.delete external_copy.try(:path)
 
       SvgSprite.bundle(theme.id)
       # after a temp file is missing, bundling still works
-      expect(SvgSprite.cache.hash.keys).to include("custom_svg_sprites_#{theme.id}")
+      expect(SvgSprite.cache.hash.keys).to include("theme_svg_sprites_#{theme.id}")
     end
   end
 
