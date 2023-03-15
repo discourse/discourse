c19099404621168178300be846145b4406a0cd2a
David Taylor <david@taylorhq.com>
2023-03-15 17:39:33 +0000

    DEV: Update admin modal controllers to native class syntax (#20685)
    This commit was generated using the ember-native-class-codemod along with a handful of manual updates

--- app/assets/javascripts/admin/addon/controllers/modals/admin-edit-badge-groupings.js
+++ app/assets/javascripts/admin/addon/controllers/modals/admin-edit-badge-groupings.js
@@ -1,13 +1,16 @@
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 import { A } from "@ember/array";
 import Controller from "@ember/controller";
 import I18n from "I18n";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
 import { ajax } from "discourse/lib/ajax";
 import { observes } from "discourse-common/utils/decorators";
-import { inject as service } from "@ember/service";
 
-export default Controller.extend(ModalFunctionality, {
-  dialog: service(),
+export default class AdminEditBadgeGroupingsController extends Controller.extend(
+  ModalFunctionality
+) {
+  @service dialog;
 
   @observes("model")
   modelChanged() {
@@ -22,7 +25,7 @@ export default Controller.extend(ModalFunctionality, {
     }
 
     this.set("workingCopy", copy);
-  },
+  }
 
   moveItem(item, delta) {
     const copy = this.workingCopy;
@@ -33,55 +36,68 @@ export default Controller.extend(ModalFunctionality, {
 
     copy.removeAt(index);
     copy.insertAt(index + delta, item);
-  },
+  }
 
-  actions: {
-    up(item) {
-      this.moveItem(item, -1);
-    },
-    down(item) {
-      this.moveItem(item, 1);
-    },
-    delete(item) {
-      this.workingCopy.removeObject(item);
-    },
-    cancel() {
-      this.setProperties({ model: null, workingCopy: null });
-      this.send("closeModal");
-    },
-    edit(item) {
-      item.set("editing", true);
-    },
-    save(item) {
-      item.set("editing", false);
-    },
-    add() {
-      const obj = this.store.createRecord("badge-grouping", {
-        editing: true,
-        name: I18n.t("admin.badges.badge_grouping"),
-      });
-      this.workingCopy.pushObject(obj);
-    },
-    saveAll() {
-      let items = this.workingCopy;
-      const groupIds = items.map((i) => i.get("id") || -1);
-      const names = items.map((i) => i.get("name"));
+  @action
+  up(item) {
+    this.moveItem(item, -1);
+  }
 
-      ajax("/admin/badges/badge_groupings", {
-        data: { ids: groupIds, names },
-        type: "POST",
-      }).then(
-        (data) => {
-          items = this.model;
-          items.clear();
-          data.badge_groupings.forEach((g) => {
-            items.pushObject(this.store.createRecord("badge-grouping", g));
-          });
-          this.setProperties({ model: null, workingCopy: null });
-          this.send("closeModal");
-        },
-        () => this.dialog.alert(I18n.t("generic_error"))
-      );
-    },
-  },
-});
+  @action
+  down(item) {
+    this.moveItem(item, 1);
+  }
+
+  @action
+  delete(item) {
+    this.workingCopy.removeObject(item);
+  }
+
+  @action
+  cancel() {
+    this.setProperties({ model: null, workingCopy: null });
+    this.send("closeModal");
+  }
+
+  @action
+  edit(item) {
+    item.set("editing", true);
+  }
+
+  @action
+  save(item) {
+    item.set("editing", false);
+  }
+
+  @action
+  add() {
+    const obj = this.store.createRecord("badge-grouping", {
+      editing: true,
+      name: I18n.t("admin.badges.badge_grouping"),
+    });
+    this.workingCopy.pushObject(obj);
+  }
+
+  @action
+  saveAll() {
+    let items = this.workingCopy;
+    const groupIds = items.map((i) => i.get("id") || -1);
+    const names = items.map((i) => i.get("name"));
+
+    ajax("/admin/badges/badge_groupings", {
+      data: { ids: groupIds, names },
+      type: "POST",
+    }).then(
+      (data) => {
+        items = this.model;
+        items.clear();
+        data.badge_groupings.forEach((g) => {
+          items.pushObject(this.store.createRecord("badge-grouping", g));
+        });
+        this.setProperties({ model: null, workingCopy: null });
+        this.send("closeModal");
+      },
+      () => this.dialog.alert(I18n.t("generic_error"))
+    );
+  }
+}
