e700f0af93ff86130acebf541121e3f93d977a0f
David Taylor <david@taylorhq.com>
2023-03-15 13:17:51 +0000

    DEV: Update admin routes to native class syntax (#20686)

--- app/assets/javascripts/admin/addon/routes/admin-api-keys-show.js
+++ app/assets/javascripts/admin/addon/routes/admin-api-keys-show.js
@@ -1,7 +1,7 @@
 import Route from "@ember/routing/route";
 
-export default Route.extend({
+export default class AdminApiKeysShowRoute extends Route {
   model(params) {
     return this.store.find("api-key", params.api_key_id);
-  },
-});
+  }
+}
--- app/assets/javascripts/admin/addon/routes/admin-customize-form-templates-index.js
+++ app/assets/javascripts/admin/addon/routes/admin-customize-form-templates-index.js
@@ -1,6 +1,7 @@
 import DiscourseRoute from "discourse/routes/discourse";
 import { action } from "@ember/object";
 import FormTemplate from "admin/models/form-template";
+
 export default class AdminCustomizeFormTemplatesIndex extends DiscourseRoute {
   model() {
     return FormTemplate.findAll();
