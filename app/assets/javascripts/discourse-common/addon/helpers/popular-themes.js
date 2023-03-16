8fd8dd41e3f6e16a604771e52dbbb926b3d2f068
TheJammiestDodger <james@discourse.org>
2023-03-16 20:46:32 +0000

    Update popular-themes.js

--- app/assets/javascripts/discourse-common/addon/helpers/popular-themes.js
+++ app/assets/javascripts/discourse-common/addon/helpers/popular-themes.js
@@ -4,16 +4,13 @@ export const POPULAR_THEMES = [
     value: "https://github.com/discourse/graceful",
     preview: "https://discourse.theme-creator.io/theme/awesomerobot/graceful",
     description: "A light and graceful theme for Discourse.",
-    meta_url:
-      "https://meta.discourse.org/t/a-graceful-theme-for-discourse/93040",
+    meta_url: "https://meta.discourse.org/t/a-graceful-theme-for-discourse/93040",
   },
   {
     name: "Material Design Theme",
     value: "https://github.com/discourse/material-design-stock-theme",
-    preview:
-      "https://discourse.theme-creator.io/theme/tshenry/material-design",
-    description:
-      "Inspired by Material Design, this theme comes with several color palettes (incl. a dark one).",
+    preview: "https://discourse.theme-creator.io/theme/tshenry/material-design",
+    description: "Inspired by Material Design, this theme comes with several color palettes (incl. a dark one).",
     meta_url: "https://meta.discourse.org/t/material-design-stock-theme/47142",
   },
   {
@@ -21,33 +18,27 @@ export const POPULAR_THEMES = [
     value: "https://github.com/discourse/minima",
     preview: "https://discourse.theme-creator.io/theme/awesomerobot/minima",
     description: "A minimal theme with reduced UI elements and focus on text.",
-    meta_url:
-      "https://meta.discourse.org/t/minima-a-minimal-theme-for-discourse/108178",
+    meta_url: "https://meta.discourse.org/t/minima-a-minimal-theme-for-discourse/108178",
   },
   {
     name: "Sam's Simple Theme",
     value: "https://github.com/discourse/discourse-simple-theme",
     preview: "https://discourse.theme-creator.io/theme/sam/simple",
-    description:
-      "Simplified front page design with classic colors and typography.",
-    meta_url:
-      "https://meta.discourse.org/t/sams-personal-minimal-topic-list-design/23552",
+    description: "Simplified front page design with classic colors and typography.",
+    meta_url: "https://meta.discourse.org/t/sams-personal-minimal-topic-list-design/23552",
   },
   {
     name: "Brand Header",
     value: "https://github.com/discourse/discourse-brand-header",
-    preview:
-      "https://discourse.theme-creator.io/theme/vinothkannans/brand-header",
-    description:
-      "Add an extra top header with your logo, navigation links and social icons.",
+    preview: "https://discourse.theme-creator.io/theme/vinothkannans/brand-header",
+    description: "Add an extra top header with your logo, navigation links and social icons.",
     meta_url: "https://meta.discourse.org/t/brand-header-theme-component/77977",
     component: true,
   },
   {
     name: "Custom Header Links",
     value: "https://github.com/discourse/discourse-custom-header-links",
-    preview:
-      "https://discourse.theme-creator.io/theme/awesomerobot/custom-header-links",
+    preview: "https://discourse.theme-creator.io/theme/awesomerobot/custom-header-links",
     description: "Easily add custom text-based links to the header.",
     meta_url: "https://meta.discourse.org/t/custom-header-links/90588",
     component: true,
@@ -62,10 +53,8 @@ export const POPULAR_THEMES = [
   {
     name: "Category Banners",
     value: "https://github.com/discourse/discourse-category-banners",
-    preview:
-      "https://discourse.theme-creator.io/theme/awesomerobot/discourse-category-banners",
-    description:
-      "Show banners on category pages using your existing category details.",
+    preview: "https://discourse.theme-creator.io/theme/awesomerobot/discourse-category-banners",
+    description: "Show banners on category pages using your existing category details.",
     meta_url: "https://meta.discourse.org/t/discourse-category-banners/86241",
     component: true,
   },
@@ -74,15 +63,13 @@ export const POPULAR_THEMES = [
     value: "https://github.com/discourse/discourse-kanban-theme",
     preview: "https://discourse.theme-creator.io/theme/david/kanban",
     description: "Display and organize topics using a Kanban board interface.",
-    meta_url:
-      "https://meta.discourse.org/t/kanban-board-theme-component/118164",
+    meta_url: "https://meta.discourse.org/t/kanban-board-theme-component/118164",
     component: true,
   },
   {
     name: "Hamburger Theme Selector",
     value: "https://github.com/discourse/discourse-hamburger-theme-selector",
-    description:
-      "Displays a theme selector in the hamburger menu provided there is more than one user-selectable theme.",
+    description: "Displays a theme selector in the hamburger menu provided there is more than one user-selectable theme.",
     meta_url: "https://meta.discourse.org/t/hamburger-theme-selector/61210",
     component: true,
   },
@@ -104,10 +91,8 @@ export const POPULAR_THEMES = [
   {
     name: "Automatic Table of Contents",
     value: "https://github.com/discourse/DiscoTOC",
-    description:
-      "Generates an interactive table of contents on the sidebar of your topic with a simple click in the composer.",
-    meta_url:
-      "https://meta.discourse.org/t/discotoc-automatic-table-of-contents/111143",
+    description: "Generates an interactive table of contents on the sidebar of your topic with a simple click in the composer.",
+    meta_url: "https://meta.discourse.org/t/discotoc-automatic-table-of-contents/111143",
     component: true,
   },
   {
