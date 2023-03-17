64557c407611783506db7172be81ed5d63aa2172
David Taylor <david@taylorhq.com>
2023-03-17 10:18:42 +0000

    DEV: Update admin models to native class syntax (#20704)
    This commit was generated using the ember-native-class-codemod along with a handful of manual updates

--- app/assets/javascripts/admin/addon/models/backup-status.js
+++ app/assets/javascripts/admin/addon/models/backup-status.js
@@ -1,12 +1,12 @@
+import { not } from "@ember/object/computed";
 import EmberObject from "@ember/object";
 import discourseComputed from "discourse-common/utils/decorators";
-import { not } from "@ember/object/computed";
 
-export default EmberObject.extend({
-  restoreDisabled: not("restoreEnabled"),
+export default class BackupStatus extends EmberObject {
+  @not("restoreEnabled") restoreDisabled;
 
   @discourseComputed("allowRestore", "isOperationRunning")
   restoreEnabled(allowRestore, isOperationRunning) {
     return allowRestore && !isOperationRunning;
-  },
-});
+  }
+}
