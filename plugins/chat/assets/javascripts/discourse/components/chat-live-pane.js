0654492509f8e8f2643b1dedb269ffa0a14a2ad6
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-03-21 11:17:46 +0100

    compute on line to avoid issues where data has a wrong position due to absolute

--- plugins/chat/assets/javascripts/discourse/components/chat-live-pane.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-live-pane.js
@@ -1310,21 +1310,24 @@ export default class ChatLivePane extends Component {
       dates
         .map((date, index) => {
           const item = { bottom: 0, date };
+          const line = date.nextElementSibling;
 
           if (index > 0) {
             const prevDate = dates[index - 1];
-            item.bottom = height - prevDate.offsetTop;
+            const prevLine = prevDate.nextElementSibling;
+            item.bottom = height - prevLine.offsetTop;
           }
 
           if (dates.length === 1) {
             item.height = height;
           } else {
             if (index === 0) {
-              item.height = height - date.offsetTop;
+              item.height = height - line.offsetTop;
             } else {
               const prevDate = dates[index - 1];
+              const prevLine = prevDate.nextElementSibling;
               item.height =
-                height - date.offsetTop - (height - prevDate.offsetTop);
+                height - line.offsetTop - (height - prevLine.offsetTop);
             }
           }
 
