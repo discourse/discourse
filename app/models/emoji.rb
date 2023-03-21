22bf33f8ad716562ec3dbc38fd1af4b544552f08
Daniel Waterworth <me@danielwaterworth.com>
2023-03-21 11:44:49 -0500

    DEV: Remove emoji cache dead code
    The cache is already not shared between app servers that have difference
    app_versions, so this check was redundant.

--- app/models/emoji.rb
+++ app/models/emoji.rb
@@ -65,19 +65,12 @@ class Emoji
     found_emoji = nil
 
     [[global_emoji_cache, :standard], [site_emoji_cache, :custom]].each do |cache, list_key|
-      cache_postfix, found_emoji =
+      found_emoji =
         cache.defer_get_set(normalized_name) do
-          emoji =
-            Emoji
-              .public_send(list_key)
-              .detect { |e| e.name == normalized_name && (!is_toned || (is_toned && e.tonable)) }
-          [self.cache_postfix, emoji]
+          Emoji
+            .public_send(list_key)
+            .detect { |e| e.name == normalized_name && (!is_toned || (is_toned && e.tonable)) }
         end
-
-      if found_emoji && (cache_postfix != self.cache_postfix)
-        cache.delete(normalized_name)
-        redo
-      end
 
       break if found_emoji
     end
