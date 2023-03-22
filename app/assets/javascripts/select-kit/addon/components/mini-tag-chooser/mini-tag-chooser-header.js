6b32dfa38d76f4de60fd4fa612ca1f0b4b977fc5
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-22 16:40:37 -0400

    Delete mini-tag-chooser-header because even pre-migration it was importing a file that did not exist

--- app/assets/javascripts/select-kit/addon/components/mini-tag-chooser/mini-tag-chooser-header.js
+++
@@ -1,7 +0,0 @@
-import ComboBoxSelectBoxHeaderComponent from "select-kit/components/combo-box/combo-box-header";
-import layout from "select-kit/templates/components/mini-tag-chooser/mini-tag-chooser-header";
-
-export default ComboBoxSelectBoxHeaderComponent.extend({
-  layout,
-  classNames: ["mini-tag-chooser-header"],
-});
