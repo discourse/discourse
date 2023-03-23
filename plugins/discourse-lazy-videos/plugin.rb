bfd7ba852f639e2320f4d3124c7ea0851da1e824
Jan Cernik <jancernik12@gmail.com>
2023-03-23 14:11:56 -0300

    Improve email formatting and add spec

--- plugins/discourse-lazy-videos/plugin.rb
+++ plugins/discourse-lazy-videos/plugin.rb
@@ -18,10 +18,11 @@ require_relative "lib/lazy-videos/lazy_tiktok"
 after_initialize do
   on(:reduce_cooked) do |fragment|
     fragment
-      .css(".lazy-video-container a")
+      .css(".lazy-video-container")
       .each do |video|
-        href = video["href"]
-        video.inner_html += "<p><a href=\"#{href}\">#{href}</a></p>"
+        title = video["data-video-title"]
+        href = video.at_css("a")["href"]
+        video.replace("<p><a href=\"#{href}\">#{title}</a></p>")
       end
   end
 end
