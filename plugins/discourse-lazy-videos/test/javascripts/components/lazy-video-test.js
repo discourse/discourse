43846696b7fbc2f615975eaac4382f5efa12e28a
Jan Cernik <jancernik12@gmail.com>
2023-03-23 11:51:26 -0300

    Refactor `attributes` in test file

--- plugins/discourse-lazy-videos/test/javascripts/components/lazy-video-test.js
+++ plugins/discourse-lazy-videos/test/javascripts/components/lazy-video-test.js
@@ -3,36 +3,30 @@ import hbs from "htmlbars-inline-precompile";
 import { module, test } from "qunit";
 import { click, render } from "@ember/test-helpers";
 
-const videoAttributes = {
-  url: "https://www.youtube.com/watch?v=kPRA0W1kECg",
-  thumbnail: "thumbnail.jpeg",
-  title: "15 Sorting Algorithms in 6 Minutes",
-  providerName: "youtube",
-  id: "kPRA0W1kECg",
-};
-
 module("Discourse Lazy Videos | Component | lazy-video", function (hooks) {
   setupRenderingTest(hooks);
 
-  test("displays the correct video title", async function (assert) {
-    this.set("attributes", videoAttributes);
+  this.attributes = {
+    url: "https://www.youtube.com/watch?v=kPRA0W1kECg",
+    thumbnail: "thumbnail.jpeg",
+    title: "15 Sorting Algorithms in 6 Minutes",
+    providerName: "youtube",
+    id: "kPRA0W1kECg",
+  };
 
+  test("displays the correct video title", async function (assert) {
     await render(hbs`<LazyVideo @videoAttributes={{this.attributes}} />`);
 
     assert.dom(".title-link").hasText(this.attributes.title);
   });
 
   test("displays the correct provider icon", async function (assert) {
-    this.set("attributes", videoAttributes);
-
     await render(hbs`<LazyVideo @videoAttributes={{this.attributes}} />`);
 
     assert.dom(".icon.youtube-icon").exists();
   });
 
   test("loads the iframe when clicked", async function (assert) {
-    this.set("attributes", videoAttributes);
-
     await render(hbs`<LazyVideo @videoAttributes={{this.attributes}}/>`);
     assert.dom(".lazy-video-container.video-loaded").doesNotExist();
 
@@ -41,7 +35,6 @@ module("Discourse Lazy Videos | Component | lazy-video", function (hooks) {
   });
 
   test("accepts an optional callback function", async function (assert) {
-    this.set("attributes", videoAttributes);
     this.set("foo", 1);
     this.set("callback", () => this.set("foo", 2));
 
