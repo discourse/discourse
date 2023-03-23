58909d3610957b27ca3dafdcc6a4322a2d88913a
Ella <ella.estigoy@gmail.com>
2023-03-23 16:59:21 -0600

    UX: improve static login page

--- app/assets/javascripts/discourse/app/controllers/static.js
+++ app/assets/javascripts/discourse/app/controllers/static.js
@@ -1,8 +1,10 @@
 import Controller, { inject as controller } from "@ember/controller";
+import getUrl from "discourse-common/lib/get-url";
 import { ajax } from "discourse/lib/ajax";
 import discourseComputed from "discourse-common/utils/decorators";
 import { equal, or } from "@ember/object/computed";
 import { userPath } from "discourse/lib/url";
+import { wavingHandURL } from "discourse/lib/waving-hand-url";
 
 export default Controller.extend({
   application: controller(),
@@ -19,6 +21,9 @@ export default Controller.extend({
       this.get("model.path") === "login" && this.get("application.canSignUp")
     );
   },
+
+  @discourseComputed
+  wavingHandURL: () => wavingHandURL(),
 
   actions: {
     markFaqRead() {
