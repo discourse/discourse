70c0955722d296dd6a3576f4b7e5587d08b67613
Martin Brennan <martin@discourse.org>
2023-03-29 00:55:01 +1000

    DEV: Uncomment chat upload system specs (#20859)
    These were commented in  daaa41ae0760c863c91bfcda659a4f966ea7545c,
    doesn't seem to have made an observable difference, uncommenting
    these now to see if chat system specs still pass.

--- plugins/chat/spec/system/channel_message_upload_spec.rb
+++ plugins/chat/spec/system/channel_message_upload_spec.rb
@@ -1,39 +1,39 @@
 # frozen_string_literal: true
 
-# RSpec.describe "Channel message selection", type: :system, js: true do
-#   fab!(:current_user) { Fabricate(:user) }
-#   fab!(:channel_1) { Fabricate(:chat_channel) }
-#   fab!(:message_1) { Fabricate(:chat_message, chat_channel: channel_1) }
+RSpec.describe "Channel message selection", type: :system, js: true do
+  fab!(:current_user) { Fabricate(:user) }
+  fab!(:channel_1) { Fabricate(:chat_channel) }
+  fab!(:message_1) { Fabricate(:chat_message, chat_channel: channel_1) }
 
-#   let(:chat) { PageObjects::Pages::Chat.new }
-#   let(:channel) { PageObjects::Pages::ChatChannel.new }
-#   let(:image) do
-#     Fabricate(
-#       :upload,
-#       original_filename: "test_image.jpg",
-#       width: 400,
-#       height: 300,
-#       extension: "jpg",
-#     )
-#   end
+  let(:chat) { PageObjects::Pages::Chat.new }
+  let(:channel) { PageObjects::Pages::ChatChannel.new }
+  let(:image) do
+    Fabricate(
+      :upload,
+      original_filename: "test_image.jpg",
+      width: 400,
+      height: 300,
+      extension: "jpg",
+    )
+  end
 
-#   before do
-#     chat_system_bootstrap
-#     channel_1.add(current_user)
-#     sign_in(current_user)
-#     message_1.attach_uploads([image])
-#   end
+  before do
+    chat_system_bootstrap
+    channel_1.add(current_user)
+    sign_in(current_user)
+    message_1.attach_uploads([image])
+  end
 
-#   it "can collapse/expand an image and still have lightbox working" do
-#     chat.visit_channel(channel_1)
+  it "can collapse/expand an image and still have lightbox working" do
+    chat.visit_channel(channel_1)
 
-#     find(".chat-message-collapser-button").click
-#     expect(page).to have_css(".chat-message-collapser-body.hidden", visible: :false)
-#     find(".chat-message-collapser-button").click
-#     expect(page).to have_no_css(".chat-message-collapser-body.hidden")
-#     find(".chat-img-upload").click
+    find(".chat-message-collapser-button").click
+    expect(page).to have_css(".chat-message-collapser-body.hidden", visible: :false)
+    find(".chat-message-collapser-button").click
+    expect(page).to have_no_css(".chat-message-collapser-body.hidden")
+    find(".chat-img-upload").click
 
-#     # visible false is because the upload doesn’t exist but it's enough to know lightbox is working
-#     expect(page).to have_css(".mfp-image-holder img[src*='#{image.url}']", visible: false)
-#   end
-# end
+    # visible false is because the upload doesn’t exist but it's enough to know lightbox is working
+    expect(page).to have_css(".mfp-image-holder img[src*='#{image.url}']", visible: false)
+  end
+end
