1ecac603a2ef6c8a98acbb35c7a1bebfd6fb78ac
David Battersby <info@davidbattersby.com>
2023-03-30 11:26:14 +0800

    FEATURE: remove denied emoji from chat reactions and chat attach emoji picker

--- plugins/chat/app/controllers/chat/emojis_controller.rb
+++ plugins/chat/app/controllers/chat/emojis_controller.rb
@@ -3,7 +3,15 @@
 module Chat
   class EmojisController < ::Chat::BaseController
     def index
-      emojis = Emoji.all.group_by(&:group)
+      emoji_deny_list = SiteSetting.emoji_deny_list
+
+      if emoji_deny_list.present?
+        denied_emojis = emoji_deny_list.split("|")
+        emojis = Emoji.all.filter{ |e| !denied_emojis.include?(e.name) }.group_by(&:group)
+      else
+        emojis = Emoji.all.group_by(&:group)
+      end
+
       render json: MultiJson.dump(emojis)
     end
   end
