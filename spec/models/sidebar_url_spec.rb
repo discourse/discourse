645a2d4c7c4739a3c20bd6a7fb5695d4d53b1bb8
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-03-30 14:13:21 +1100

    FIX: drop internal URL validation for paths in sidebar
    `Rails.application.routes.recognize_path(value)` was not working for /admin paths because StaffConstraint.new requires user to check permission.
    
    This validation is not bringing much value, and the easiest way is to drop it. In the worse case scenario, a user will have an incorrect link in their sidebar.
    
    Bug reported: https://meta.discourse.org/t/custom-sidebar-sections-being-tested-on-meta/255303/66

--- spec/models/sidebar_url_spec.rb
+++ spec/models/sidebar_url_spec.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe SidebarUrl do
-  it "validates path" do
-    expect(SidebarUrl.new(icon: "link", name: "categories", value: "/categories").valid?).to eq(
-      true,
-    )
-    expect(SidebarUrl.new(icon: "link", name: "categories", value: "/invalid_path").valid?).to eq(
-      false,
-    )
+  it "validates external URLs" do
     expect(
       SidebarUrl.new(
         icon: "link",
