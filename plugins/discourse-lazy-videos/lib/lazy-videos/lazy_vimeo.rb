0ea417f4fd6bf87a7a91f37479c70f4600e866f2
Jan Cernik <jancernik12@gmail.com>
2023-04-03 18:19:40 -0300

    Revert changes and fix the url issue

--- plugins/discourse-lazy-videos/lib/lazy-videos/lazy_vimeo.rb
+++ plugins/discourse-lazy-videos/lib/lazy-videos/lazy_vimeo.rb
@@ -7,18 +7,26 @@ class Onebox::Engine::VimeoOnebox
   alias_method :default_onebox_to_html, :to_html
 
   def to_html
-    if SiteSetting.lazy_videos_enabled && SiteSetting.lazy_vimeo_enabled &&
-         !oembed_data[:uri].match?(%r{videos/\d+:.+})
-      video_id = oembed_data[:video_id]
+    if SiteSetting.lazy_videos_enabled && SiteSetting.lazy_vimeo_enabled
+      full_video_id = oembed_data[:uri].sub("/videos/", "").sub(":", "/")
+
+      if !oembed_data[:uri].match?(%r{videos/\d+:.+})
+        iframe_id = full_video_id
+      else
+        iframe_src = Nokogiri::HTML5.fragment(oembed_data[:html]).at_css("iframe")&.[]("src")
+        iframe_id = iframe_src.sub("https://player.vimeo.com/video/", "")
+      end
+
       thumbnail_url = "https://vumbnail.com/#{oembed_data[:video_id]}.jpg"
+      puts thumbnail_url
       escaped_title = ERB::Util.html_escape(og_data.title)
 
       <<~HTML
         <div class="vimeo-onebox lazy-video-container"
-          data-video-id="#{video_id}"
+          data-video-id="#{iframe_id}"
           data-video-title="#{escaped_title}"
           data-provider-name="vimeo">
-          <a href="https://vimeo.com/#{video_id}" target="_blank">
+          <a href="https://vimeo.com/#{full_video_id}" target="_blank">
             <img class="vimeo-thumbnail"
               src="#{thumbnail_url}"
               title="#{escaped_title}">
