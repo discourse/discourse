discourse/discourse

commit 7c6a8f1c74ea7ea3e81d9ffa80ca2227ee36a006

DEV: Refactor `TopicQuery#default_results` to improve readability (#20800)
This commit breaks down the code in TopicQuery#default_results into
smaller private methods to improve readability.

lib/topic_query.rb
@@ -715,65 +715,7 @@ class TopicQuery
       end
     end
 
-    if SiteSetting.tagging_enabled
-      result = result.preload(:tags)
-
-      tags_arg = @options[:tags]
-
-      if tags_arg && tags_arg.size > 0
-        tags_arg = tags_arg.split if String === tags_arg
-
-        tags_arg =
-          tags_arg.map do |t|
-            if String === t
-              t.downcase
-            else
-              t
-            end
-          end
-
-        tags_query = tags_arg[0].is_a?(String) ? Tag.where_name(tags_arg) : Tag.where(id: tags_arg)
-        tags = tags_query.select(:id, :target_tag_id).map { |t| t.target_tag_id || t.id }.uniq
-
-        if ActiveModel::Type::Boolean.new.cast(@options[:match_all_tags])
-          # ALL of the given tags:
-          if tags_arg.length == tags.length
-            tags.each_with_index do |tag, index|
-              sql_alias = ["t", index].join
-              result =
-                result.joins(
-                  "INNER JOIN topic_tags #{sql_alias} ON #{sql_alias}.topic_id = topics.id AND #{sql_alias}.tag_id = #{tag}",
-                )
-            end
-          else
-            result = result.none # don't return any results unless all tags exist in the database
-          end
-        else
-          # ANY of the given tags:
-          result = result.joins(:tags).where("tags.id in (?)", tags)
-        end
-
-        # TODO: this is very side-effecty and should be changed
-        # It is done cause further up we expect normalized tags
-        @options[:tags] = tags
-      elsif @options[:no_tags]
-        # the following will do: ("topics"."id" NOT IN (SELECT DISTINCT "topic_tags"."topic_id" FROM "topic_tags"))
-        result = result.where.not(id: TopicTag.distinct.pluck(:topic_id))
-      end
-
-      if @options[:exclude_tag].present? &&
-           !DiscourseTagging.hidden_tag_names(@guardian).include?(@options[:exclude_tag])
-        result = result.where(<<~SQL, name: @options[:exclude_tag])
-          topics.id NOT IN (
-            SELECT topic_tags.topic_id
-            FROM topic_tags
-            INNER JOIN tags ON tags.id = topic_tags.tag_id
-            WHERE tags.name = :name
-          )
-          SQL
-      end
-    end
-
+    result = filter_by_tags(result)
     result = apply_ordering(result, options)
 
     all_listable_topics =
@@ -1201,5 +1143,57 @@ class TopicQuery
       results = results.where("topics.updated_at >= ?", unread_at)
     end
     results
+  end
+
+  def filter_by_tags(result)
+    return result if !SiteSetting.tagging_enabled
+
+    tags_arg = @options[:tags]
+
+    if tags_arg && tags_arg.size > 0
+      tags_arg = tags_arg.split if String === tags_arg
+      tags_query = tags_arg[0].is_a?(String) ? Tag.where_name(tags_arg) : Tag.where(id: tags_arg)
+      tags = tags_query.select(:id, :target_tag_id).map { |t| t.target_tag_id || t.id }.uniq
+
+      if ActiveModel::Type::Boolean.new.cast(@options[:match_all_tags])
+        # ALL of the given tags:
+        if tags_arg.length == tags.length
+          tags.each_with_index do |tag, index|
+            sql_alias = ["t", index].join
+
+            result =
+              result.joins(
+                "INNER JOIN topic_tags #{sql_alias} ON #{sql_alias}.topic_id = topics.id AND #{sql_alias}.tag_id = #{tag}",
+              )
+          end
+        else
+          result = result.none # don't return any results unless all tags exist in the database
+        end
+      else
+        # ANY of the given tags:
+        result = result.joins(:tags).where("tags.id in (?)", tags)
+      end
+
+      # TODO: this is very side-effecty and should be changed
+      # It is done cause further up we expect normalized tags
+      @options[:tags] = tags
+    elsif @options[:no_tags]
+      # the following will do: ("topics"."id" NOT IN (SELECT DISTINCT "topic_tags"."topic_id" FROM "topic_tags"))
+      result = result.where.not(id: TopicTag.distinct.select(:topic_id))
+    end
+
+    if @options[:exclude_tag].present? &&
+         !DiscourseTagging.hidden_tag_names(@guardian).include?(@options[:exclude_tag])
+      result = result.where(<<~SQL, name: @options[:exclude_tag])
+        topics.id NOT IN (
+          SELECT topic_tags.topic_id
+          FROM topic_tags
+          INNER JOIN tags ON tags.id = topic_tags.tag_id
+          WHERE tags.name = :name
+        )
+        SQL
+    end
+
+    result
   end
 end

commit 20edea3ed3843f59e8f439e3e3b47c5262ebe70d

PERF: Fix N+1 queries problem when listing topics list
This performance regression was introduced in
7c6a8f1c74ea7ea3e81d9ffa80ca2227ee36a006 where the preloading of tags in
`TopicQuery` was accidentally removed.

lib/topic_query.rb
@@ -721,7 +721,11 @@ class TopicQuery
       end
     end
 
-    result = filter_by_tags(result)
+    if SiteSetting.tagging_enabled
+      result = result.includes(:tags)
+      result = filter_by_tags(result)
+    end
+
     result = apply_ordering(result, options)
 
     all_listable_topics =
@@ -1151,8 +1155,6 @@ class TopicQuery
   end
 
   def filter_by_tags(result)
-    return result if !SiteSetting.tagging_enabled
-
     tags_arg = @options[:tags]
 
     if tags_arg && tags_arg.size > 0

spec/requests/list_controller_spec.rb
@@ -106,6 +106,50 @@ RSpec.describe ListController do
       expect(first_item.css('[itemprop="url"]')[0]["href"]).to eq(topic.url)
     end
 
+    it "does not result in N+1 queries when topics have tags and tagging_enabled site setting is enabled" do
+      SiteSetting.tagging_enabled = true
+      tag = Fabricate(:tag)
+      topic.tags << tag
+
+      # warm up
+      get "/latest.json"
+      expect(response.status).to eq(200)
+
+      initial_sql_queries_count =
+        track_sql_queries do
+          get "/latest.json"
+
+          expect(response.status).to eq(200)
+
+          body = response.parsed_body
+
+          expect(body["topic_list"]["topics"].map { |t| t["id"] }).to contain_exactly(topic.id)
+          expect(body["topic_list"]["topics"][0]["tags"]).to contain_exactly(tag.name)
+        end.count
+
+      tag2 = Fabricate(:tag)
+      topic2 = Fabricate(:topic, tags: [tag2])
+
+      new_sql_queries_count =
+        track_sql_queries do
+          get "/latest.json"
+
+          expect(response.status).to eq(200)
+
+          body = response.parsed_body
+
+          expect(body["topic_list"]["topics"].map { |t| t["id"] }).to contain_exactly(
+            topic.id,
+            topic2.id,
+          )
+
+          expect(body["topic_list"]["topics"][0]["tags"]).to contain_exactly(tag2.name)
+          expect(body["topic_list"]["topics"][1]["tags"]).to contain_exactly(tag.name)
+        end.count
+
+      expect(new_sql_queries_count).to eq(initial_sql_queries_count)
+    end
+
     it "does not N+1 queries when topic featured users have different primary groups" do
       user.update!(primary_group: group)
 