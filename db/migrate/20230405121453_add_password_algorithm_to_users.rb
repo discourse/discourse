0d5553e641d49a7641debe95c9204636f01feb20
David Taylor <david@taylorhq.com>
2023-04-06 09:35:25 +0100

    Update in batches

--- db/migrate/20230405121453_add_password_algorithm_to_users.rb
+++ db/migrate/20230405121453_add_password_algorithm_to_users.rb
@@ -1,12 +1,28 @@
 # frozen_string_literal: true
 
 class AddPasswordAlgorithmToUsers < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+  BATCH_SIZE = 5000
+
   def up
-    add_column :users, :password_algorithm, :string, limit: 64
-    execute <<~SQL
+    if !column_exists?(:users, :password_algorithm)
+      add_column :users, :password_algorithm, :string, limit: 64
+    end
+
+    sql = <<~SQL
       UPDATE users SET password_algorithm = '$pbkdf2-sha256$i=64000,l=32$'
-      WHERE users.password_hash IS NOT NULL
+      WHERE id IN (
+        SELECT id FROM users
+        WHERE users.password_hash IS NOT NULL
+        AND users.password_algorithm IS NULL
+        LIMIT #{BATCH_SIZE}
+      )
     SQL
+
+    loop do
+      changed_count = execute(sql).cmd_tuples
+      break if changed_count < BATCH_SIZE
+    end
   end
 
   def down
