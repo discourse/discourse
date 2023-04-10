2660632c568e48ed1e15a733c0d62469d59a7b7e
Rafael dos Santos Silva <xfalcox@gmail.com>
2023-04-10 16:27:17 -0300

    Show which test is running in CI for plugin system specs

---
+++ spec/requests/gamification_score_events_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "GamificationScoreEvents", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
