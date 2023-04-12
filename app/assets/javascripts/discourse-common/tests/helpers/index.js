bf8a42a01fa6c1fbe5a2d7f40584a99bb49d635e
Jarek Radosz <jradosz@gmail.com>
2023-04-12 11:33:42 +0200

    lint

--- app/assets/javascripts/discourse-common/tests/helpers/index.js
+++ app/assets/javascripts/discourse-common/tests/helpers/index.js
@@ -2,7 +2,7 @@ import {
   setupApplicationTest as upstreamSetupApplicationTest,
   setupRenderingTest as upstreamSetupRenderingTest,
   setupTest as upstreamSetupTest,
-} from 'ember-qunit';
+} from "ember-qunit";
 
 // This file exists to provide wrappers around ember-qunit's / ember-mocha's
 // test setup functions. This way, you can easily extend the setup that is
