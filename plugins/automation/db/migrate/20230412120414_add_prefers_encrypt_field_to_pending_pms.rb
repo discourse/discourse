208c79a438d12abe2a0b678f1d9dc176efa4553e
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-04-14 10:23:10 +0800

    FIX: Update migration to prevent `PG::NotNullViolation` error (#177)
    Since there are already rows in `discourse_automation_pending_pms`, we
    cannot add a column with `null: false`. Instead we need to first add the
    column, fill it up with values and then change the column default.

--- plugins/automation/db/migrate/20230412120414_add_prefers_encrypt_field_to_pending_pms.rb
+++ plugins/automation/db/migrate/20230412120414_add_prefers_encrypt_field_to_pending_pms.rb
@@ -2,10 +2,13 @@
 
 class AddPrefersEncryptFieldToPendingPms < ActiveRecord::Migration[7.0]
   def change
-    add_column :discourse_automation_pending_pms,
-               :prefers_encrypt,
-               :boolean,
-               null: false,
-               default: false
+    add_column :discourse_automation_pending_pms, :prefers_encrypt, :boolean, default: false
+
+    execute <<~SQL
+      UPDATE discourse_automation_pending_pms
+      SET prefers_encrypt = false
+    SQL
+
+    change_column_null :discourse_automation_pending_pms, :prefers_encrypt, false
   end
 end
