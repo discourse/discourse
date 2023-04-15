discourse/discourse

commit bd5c5c4b5f7b33a64cc12e2ba13e81767ac00edc

FEATURE: Reacting to MessageBus in chat thread panel (#21070)
This commit introduces a ChatChannelPaneSubscriptionsManager
and a ChatChannelThreadPaneSubscriptionsManager that inherits
from the first service that handle MessageBus subscriptions
for the main channel and the thread panel respectively.

This necessitated a change to Chat::Publisher to be able to
send MessageBus messages to multiple channels based on whether
a message was an OM for a thread, a thread reply, or a regular
channel message.

An initial change to update the thread indicator with new replies
has been done too, but that will be improved in future as we have
more data to update on the indicators.

Still remaining is to fully move over the handleSentMessage
functionality which includes scrolling and new message indicator
things.

Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>

plugins/chat/app/services/chat/publisher.rb
@@ -10,8 +10,27 @@ module Chat
       "/chat/#{chat_channel_id}"
     end
 
+    def self.thread_message_bus_channel(chat_channel_id, thread_id)
+      "#{root_message_bus_channel(chat_channel_id)}/thread/#{thread_id}"
+    end
+
+    def self.calculate_publish_targets(channel, message)
+      targets =
+        if message.thread_om?
+          [
+            root_message_bus_channel(channel.id),
+            thread_message_bus_channel(channel.id, message.thread_id),
+          ]
+        elsif message.thread_reply?
+          [thread_message_bus_channel(channel.id, message.thread_id)]
+        else
+          [root_message_bus_channel(channel.id)]
+        end
+      targets
+    end
+
     def self.publish_new!(chat_channel, chat_message, staged_id)
-      return if chat_message.thread_reply?
+      message_bus_targets = calculate_publish_targets(chat_channel, chat_message)
 
       content =
         Chat::MessageSerializer.new(
@@ -22,19 +41,38 @@ module Chat
       content[:staged_id] = staged_id
       permissions = permissions(chat_channel)
 
-      MessageBus.publish(root_message_bus_channel(chat_channel.id), content.as_json, permissions)
+      message_bus_targets.each do |message_bus_channel|
+        MessageBus.publish(message_bus_channel, content.as_json, permissions)
+      end
 
-      MessageBus.publish(
-        self.new_messages_message_bus_channel(chat_channel.id),
-        {
-          channel_id: chat_channel.id,
-          message_id: chat_message.id,
-          user_id: chat_message.user.id,
-          username: chat_message.user.username,
-          thread_id: chat_message.thread_id,
-        },
-        permissions,
-      )
+      if chat_message.thread_reply?
+        MessageBus.publish(
+          root_message_bus_channel(chat_channel.id),
+          {
+            type: :update_thread_original_message,
+            original_message_id: chat_message.thread.original_message_id,
+            action: :increment_reply_count,
+          }.as_json,
+          permissions,
+        )
+      end
+
+      # NOTE: This means that the read count is only updated in the client
+      # for new messages in the main channel stream, maybe in future we want to
+      # do this for thread messages as well?
+      if !chat_message.thread_reply?
+        MessageBus.publish(
+          self.new_messages_message_bus_channel(chat_channel.id),
+          {
+            channel_id: chat_channel.id,
+            message_id: chat_message.id,
+            user_id: chat_message.user.id,
+            username: chat_message.user.username,
+            thread_id: chat_message.thread_id,
+          },
+          permissions,
+        )
+      end
     end
 
     def self.publish_thread_created!(chat_channel, chat_message)
@@ -50,7 +88,7 @@ module Chat
     end
 
     def self.publish_processed!(chat_message)
-      return if chat_message.thread_reply?
+      message_bus_targets = calculate_publish_targets(chat_channel, chat_message)
 
       chat_channel = chat_message.chat_channel
       content = {
@@ -60,15 +98,14 @@ module Chat
           cooked: chat_message.cooked,
         },
       }
-      MessageBus.publish(
-        root_message_bus_channel(chat_channel.id),
-        content.as_json,
-        permissions(chat_channel),
-      )
+
+      message_bus_targets.each do |message_bus_channel|
+        MessageBus.publish(message_bus_channel, content.as_json, permissions(chat_channel))
+      end
     end
 
     def self.publish_edit!(chat_channel, chat_message)
-      return if chat_message.thread_reply?
+      message_bus_targets = calculate_publish_targets(chat_channel, chat_message)
 
       content =
         Chat::MessageSerializer.new(
@@ -76,15 +113,14 @@ module Chat
           { scope: anonymous_guardian, root: :chat_message },
         ).as_json
       content[:type] = :edit
-      MessageBus.publish(
-        root_message_bus_channel(chat_channel.id),
-        content.as_json,
-        permissions(chat_channel),
-      )
+
+      message_bus_targets.each do |message_bus_channel|
+        MessageBus.publish(message_bus_channel, content.as_json, permissions(chat_channel))
+      end
     end
 
     def self.publish_refresh!(chat_channel, chat_message)
-      return if chat_message.thread_reply?
+      message_bus_targets = calculate_publish_targets(chat_channel, chat_message)
 
       content =
         Chat::MessageSerializer.new(
@@ -92,15 +128,14 @@ module Chat
           { scope: anonymous_guardian, root: :chat_message },
         ).as_json
       content[:type] = :refresh
-      MessageBus.publish(
-        root_message_bus_channel(chat_channel.id),
-        content.as_json,
-        permissions(chat_channel),
-      )
+
+      message_bus_targets.each do |message_bus_channel|
+        MessageBus.publish(message_bus_channel, content.as_json, permissions(chat_channel))
+      end
     end
 
     def self.publish_reaction!(chat_channel, chat_message, action, user, emoji)
-      return if chat_message.thread_reply?
+      message_bus_targets = calculate_publish_targets(chat_channel, chat_message)
 
       content = {
         action: action,
@@ -109,11 +144,10 @@ module Chat
         type: :reaction,
         chat_message_id: chat_message.id,
       }
-      MessageBus.publish(
-        root_message_bus_channel(chat_channel.id),
-        content.as_json,
-        permissions(chat_channel),
-      )
+
+      message_bus_targets.each do |message_bus_channel|
+        MessageBus.publish(message_bus_channel, content.as_json, permissions(chat_channel))
+      end
     end
 
     def self.publish_presence!(chat_channel, user, typ)
@@ -121,16 +155,20 @@ module Chat
     end
 
     def self.publish_delete!(chat_channel, chat_message)
-      return if chat_message.thread_reply?
+      message_bus_targets = calculate_publish_targets(chat_channel, chat_message)
 
-      MessageBus.publish(
-        root_message_bus_channel(chat_channel.id),
-        { type: "delete", deleted_id: chat_message.id, deleted_at: chat_message.deleted_at },
-        permissions(chat_channel),
-      )
+      message_bus_targets.each do |message_bus_channel|
+        MessageBus.publish(
+          message_bus_channel,
+          { type: "delete", deleted_id: chat_message.id, deleted_at: chat_message.deleted_at },
+          permissions(chat_channel),
+        )
+      end
     end
 
     def self.publish_bulk_delete!(chat_channel, deleted_message_ids)
+      # TODO (martin) Handle sending this through for all the threads that
+      # may contain the deleted messages as well.
       MessageBus.publish(
         root_message_bus_channel(chat_channel.id),
         { typ: "bulk_delete", deleted_ids: deleted_message_ids, deleted_at: Time.zone.now },
@@ -139,7 +177,7 @@ module Chat
     end
 
     def self.publish_restore!(chat_channel, chat_message)
-      return if chat_message.thread_reply?
+      message_bus_targets = calculate_publish_targets(chat_channel, chat_message)
 
       content =
         Chat::MessageSerializer.new(
@@ -147,33 +185,36 @@ module Chat
           { scope: anonymous_guardian, root: :chat_message },
         ).as_json
       content[:type] = :restore
-      MessageBus.publish(
-        root_message_bus_channel(chat_channel.id),
-        content.as_json,
-        permissions(chat_channel),
-      )
+
+      message_bus_targets.each do |message_bus_channel|
+        MessageBus.publish(message_bus_channel, content.as_json, permissions(chat_channel))
+      end
     end
 
     def self.publish_flag!(chat_message, user, reviewable, score)
-      return if chat_message.thread_reply?
+      message_bus_targets = calculate_publish_targets(chat_message.chat_channel, chat_message)
 
-      # Publish to user who created flag
-      MessageBus.publish(
-        "/chat/#{chat_message.chat_channel_id}",
-        {
-          type: "self_flagged",
-          user_flag_status: score.status_for_database,
-          chat_message_id: chat_message.id,
-        }.as_json,
-        user_ids: [user.id],
-      )
+      message_bus_targets.each do |message_bus_channel|
+        # Publish to user who created flag
+        MessageBus.publish(
+          message_bus_channel,
+          {
+            type: "self_flagged",
+            user_flag_status: score.status_for_database,
+            chat_message_id: chat_message.id,
+          }.as_json,
+          user_ids: [user.id],
+        )
+      end
 
-      # Publish flag with link to reviewable to staff
-      MessageBus.publish(
-        "/chat/#{chat_message.chat_channel_id}",
-        { type: "flag", chat_message_id: chat_message.id, reviewable_id: reviewable.id }.as_json,
-        group_ids: [Group::AUTO_GROUPS[:staff]],
-      )
+      message_bus_targets.each do |message_bus_channel|
+        # Publish flag with link to reviewable to staff
+        MessageBus.publish(
+          message_bus_channel,
+          { type: "flag", chat_message_id: chat_message.id, reviewable_id: reviewable.id }.as_json,
+          group_ids: [Group::AUTO_GROUPS[:staff]],
+        )
+      end
     end
 
     def self.user_tracking_state_message_bus_channel(user_id)

plugins/chat/assets/javascripts/discourse/services/chat-pane-base-subscriptions-manager.js
@@ -1,0 +1,238 @@
+import Service, { inject as service } from "@ember/service";
+import EmberObject from "@ember/object";
+import ChatMessage from "discourse/plugins/chat/discourse/models/chat-message";
+import { cloneJSON } from "discourse-common/lib/object";
+import { bind } from "discourse-common/utils/decorators";
+
+// TODO (martin) This export can be removed once we move the handleSentMessage
+// code completely out of ChatLivePane
+export function handleStagedMessage(messagesManager, data) {
+  const stagedMessage = messagesManager.findStagedMessage(data.staged_id);
+
+  if (!stagedMessage) {
+    return;
+  }
+
+  stagedMessage.error = null;
+  stagedMessage.id = data.chat_message.id;
+  stagedMessage.staged = false;
+  stagedMessage.excerpt = data.chat_message.excerpt;
+  stagedMessage.threadId = data.chat_message.thread_id;
+  stagedMessage.channelId = data.chat_message.chat_channel_id;
+  stagedMessage.createdAt = data.chat_message.created_at;
+
+  const inReplyToMsg = messagesManager.findMessage(
+    data.chat_message.in_reply_to?.id
+  );
+  if (inReplyToMsg && !inReplyToMsg.threadId) {
+    inReplyToMsg.threadId = data.chat_message.thread_id;
+  }
+
+  // some markdown is cooked differently on the server-side, e.g.
+  // quotes, avatar images etc.
+  if (data.chat_message?.cooked !== stagedMessage.cooked) {
+    stagedMessage.cooked = data.chat_message.cooked;
+  }
+
+  return stagedMessage;
+}
+
+/**
+ * Handles subscriptions for MessageBus messages sent from Chat::Publisher
+ * to the channel and thread panes. There are individual services for
+ * each (ChatChannelPaneSubscriptionsManager and ChatChannelThreadPaneSubscriptionsManager)
+ * that implement their own logic where necessary. Functions which will
+ * always be different between the two raise a "not implemented" error in
+ * the base class, and the child class must define the associated function,
+ * even if it is a noop in that context.
+ *
+ * For example, in the thread context there is no need to handle the thread
+ * creation event, because the panel will not be open in that case.
+ */
+export default class ChatPaneBaseSubscriptionsManager extends Service {
+  @service chat;
+  @service currentUser;
+
+  get messageBusChannel() {
+    throw "not implemented";
+  }
+
+  get messageBusLastId() {
+    throw "not implemented";
+  }
+
+  get messagesManager() {
+    return this.model.messagesManager;
+  }
+
+  subscribe(model) {
+    this.unsubscribe();
+    this.model = model;
+    this.messageBus.subscribe(
+      this.messageBusChannel,
+      this.onMessage,
+      this.messageBusLastId
+    );
+  }
+
+  unsubscribe() {
+    if (!this.model) {
+      return;
+    }
+    this.messageBus.unsubscribe(this.messageBusChannel, this.onMessage);
+    this.model = null;
+  }
+
+  // TODO (martin) This can be removed once we move the handleSentMessage
+  // code completely out of ChatLivePane
+  handleStagedMessageInternal(data) {
+    return handleStagedMessage(this.messagesManager, data);
+  }
+
+  @bind
+  onMessage(busData) {
+    switch (busData.type) {
+      case "sent":
+        this.handleSentMessage(busData);
+        break;
+      case "reaction":
+        this.handleReactionMessage(busData);
+        break;
+      case "processed":
+        this.handleProcessedMessage(busData);
+        break;
+      case "edit":
+        this.handleEditMessage(busData);
+        break;
+      case "refresh":
+        this.handleRefreshMessage(busData);
+        break;
+      case "delete":
+        this.handleDeleteMessage(busData);
+        break;
+      case "bulk_delete":
+        this.handleBulkDeleteMessage(busData);
+        break;
+      case "restore":
+        this.handleRestoreMessage(busData);
+        break;
+      case "mention_warning":
+        this.handleMentionWarning(busData);
+        break;
+      case "self_flagged":
+        this.handleSelfFlaggedMessage(busData);
+        break;
+      case "flag":
+        this.handleFlaggedMessage(busData);
+        break;
+      case "thread_created":
+        this.handleThreadCreated(busData);
+        break;
+      case "update_thread_original_message":
+        this.handleThreadOriginalMessageUpdate(busData);
+        break;
+    }
+  }
+
+  handleSentMessage() {
+    throw "not implemented";
+  }
+
+  handleProcessedMessage(data) {
+    const message = this.messagesManager.findMessage(data.chat_message.id);
+    if (message) {
+      message.cooked = data.chat_message.cooked;
+      this.afterProcessedMessage(message);
+    }
+  }
+
+  afterProcessedMessage() {
+    throw "not implemented";
+  }
+
+  handleReactionMessage(data) {
+    const message = this.messagesManager.findMessage(data.chat_message_id);
+    if (message) {
+      message.react(data.emoji, data.action, data.user, this.currentUser.id);
+    }
+  }
+
+  handleEditMessage(data) {
+    const message = this.messagesManager.findMessage(data.chat_message.id);
+    if (message) {
+      message.message = data.chat_message.message;
+      message.cooked = data.chat_message.cooked;
+      message.excerpt = data.chat_message.excerpt;
+      message.uploads = cloneJSON(data.chat_message.uploads || []);
+      message.edited = true;
+      message.incrementVersion();
+    }
+  }
+
+  handleRefreshMessage(data) {
+    const message = this.messagesManager.findMessage(data.chat_message.id);
+    if (message) {
+      message.incrementVersion();
+    }
+  }
+
+  handleBulkDeleteMessage() {
+    throw "not implemented";
+  }
+
+  handleDeleteMessage(data) {
+    const deletedId = data.deleted_id;
+    const targetMsg = this.messagesManager.findMessage(deletedId);
+
+    if (!targetMsg) {
+      return;
+    }
+
+    if (this.currentUser.staff || this.currentUser.id === targetMsg.user.id) {
+      targetMsg.deletedAt = data.deleted_at;
+      targetMsg.expanded = false;
+    } else {
+      this.messagesManager.removeMessage(targetMsg);
+    }
+  }
+
+  handleRestoreMessage(data) {
+    const message = this.messagesManager.findMessage(data.chat_message.id);
+    if (message) {
+      message.deletedAt = null;
+    } else {
+      this.messagesManager.addMessages([
+        ChatMessage.create(this.args.channel, data.chat_message),
+      ]);
+    }
+  }
+
+  handleMentionWarning(data) {
+    const message = this.messagesManager.findMessage(data.chat_message_id);
+    if (message) {
+      message.mentionWarning = EmberObject.create(data);
+    }
+  }
+
+  handleSelfFlaggedMessage(data) {
+    const message = this.messagesManager.findMessage(data.chat_message_id);
+    if (message) {
+      message.userFlagStatus = data.user_flag_status;
+    }
+  }
+
+  handleFlaggedMessage(data) {
+    const message = this.messagesManager.findMessage(data.chat_message_id);
+    if (message) {
+      message.reviewableId = data.reviewable_id;
+    }
+  }
+
+  handleThreadCreated() {
+    throw "not implemented";
+  }
+
+  handleThreadOriginalMessageUpdate() {
+    throw "not implemented";
+  }
+}

plugins/chat/app/models/chat/message.rb
@@ -312,11 +312,11 @@ module Chat
     end
 
     def thread_reply?
-      in_thread? && !is_thread_om?
+      in_thread? && !thread_om?
     end
 
-    def is_thread_om?
-      self.thread.original_message_id == self.id
+    def thread_om?
+      in_thread? && self.thread.original_message_id == self.id
     end
 
     private

plugins/chat/app/serializers/chat/thread_serializer.rb
@@ -5,6 +5,22 @@ module Chat
     has_one :original_message_user, serializer: BasicUserWithStatusSerializer, embed: :objects
     has_one :original_message, serializer: Chat::ThreadOriginalMessageSerializer, embed: :objects
 
-    attributes :id, :title, :status
+    attributes :id, :title, :status, :channel_id, :meta
+
+    def initialize(object, opts)
+      super(object, opts)
+      @opts = opts
+    end
+
+    def meta
+      { message_bus_last_ids: { thread_message_bus_last_id: thread_message_bus_last_id } }
+    end
+
+    private
+
+    def thread_message_bus_last_id
+      @opts[:thread_message_bus_last_id] ||
+        MessageBus.last_id(Chat::Publisher.thread_message_bus_channel(object.channel_id, object.id))
+    end
   end
 end

commit 87d66dd1fa49c889a4fd2be1385fba0384bf49a1

FIX: prevents exception when publishing processed
This regression happened in https://github.com/discourse/discourse/commit/bd5c5c4b5f7b33a64cc12e2ba13e81767ac00edc and is due to `message_bus_targets = calculate_publish_targets(chat_channel, chat_message)` expecting a `chat_channel` which was only defined after.

This commit also:
- adds a spec to ensure oneboxing is not regressing anymore
- increment the version on message processed to ensure callbacks are correctly ran

plugins/chat/app/services/chat/publisher.rb
@@ -88,9 +88,9 @@ module Chat
     end
 
     def self.publish_processed!(chat_message)
+      chat_channel = chat_message.chat_channel
       message_bus_targets = calculate_publish_targets(chat_channel, chat_message)
 
-      chat_channel = chat_message.chat_channel
       content = {
         type: :processed,
         chat_message: {

plugins/chat/assets/javascripts/discourse/services/chat-pane-base-subscriptions-manager.js
@@ -142,6 +142,7 @@ export default class ChatPaneBaseSubscriptionsManager extends Service {
     const message = this.messagesManager.findMessage(data.chat_message.id);
     if (message) {
       message.cooked = data.chat_message.cooked;
+      message.incrementVersion();
       this.afterProcessedMessage(message);
     }
   }

plugins/chat/spec/system/chat_message_onebox_spec.rb
@@ -1,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe "Chat message onebox", type: :system, js: true do
+  let(:chat_page) { PageObjects::Pages::Chat.new }
+  let(:channel_page) { PageObjects::Pages::ChatChannel.new }
+
+  fab!(:current_user) { Fabricate(:user) }
+  fab!(:channel_1) { Fabricate(:category_channel) }
+
+  before do
+    chat_system_bootstrap
+    channel_1.add(current_user)
+    sign_in(current_user)
+  end
+
+  context "when sending a message with a link" do
+    before do
+      SiteSetting.enable_inline_onebox_on_all_domains = true
+
+      full_onebox_html = <<~HTML.chomp
+        <aside class="onebox wikipedia" data-onebox-src="https://en.wikipedia.org/wiki/Hyperlink">
+          <article class="onebox-body">
+            <p>This is a test</p>
+          </article>
+        </aside>
+      HTML
+      Oneboxer
+        .stubs(:cached_onebox)
+        .with("https://en.wikipedia.org/wiki/Hyperlink")
+        .returns(full_onebox_html)
+
+      stub_request(:get, "https://en.wikipedia.org/wiki/Hyperlink").to_return(
+        status: 200,
+        body: "<html><head><title>a</title></head></html>",
+      )
+    end
+
+    it "is oneboxed" do
+      chat_page.visit_channel(channel_1)
+      channel_page.send_message("https://en.wikipedia.org/wiki/Hyperlink")
+
+      expect(page).to have_content("This is a test")
+    end
+  end
+end