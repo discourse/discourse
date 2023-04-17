ss-test-production-org/WWE1

DEV: Refactor STI/polymorphic associations in chat

plugins/chat/lib/chat/bookmark_extension.rb
@@ -4,19 +4,8 @@ module Chat
   module BookmarkExtension
     extend ActiveSupport::Concern
 
-    prepended do
-      def valid_bookmarkable_type
-        return true if self.bookmarkable_type == Chat::Message.sti_name
-        super if defined?(super)
-      end
+    prepended { include Mappable }
 
-      CLASS_MAPPING = { "ChatMessage" => Chat::Message }
-
-      # the model used when loading chatable_type column
-      def self.polymorphic_class_for(name)
-        return CLASS_MAPPING[name] if CLASS_MAPPING.key?(name)
-        super if defined?(super)
-      end
-    end
+    class_methods { def polymorphic_class_mapping = { "ChatMessage" => Chat::Message } }
   end
 end

plugins/chat/lib/chat/reviewable_extension.rb
@@ -4,24 +4,11 @@ module Chat
   module ReviewableExtension
     extend ActiveSupport::Concern
 
-    prepended do
-      # the model used when loading type column
-      def self.sti_class_for(name)
-        return Chat::ReviewableMessage if name == "ReviewableChatMessage"
-        super(name)
-      end
+    prepended { include Mappable }
 
-      # the model used when loading target_type column
-      def self.polymorphic_class_for(name)
-        return Chat::Message if name == Chat::Message.sti_name
-        super(name)
-      end
-
-      # the type column value when saving a Chat::ReviewableMessage
-      def self.sti_name
-        return "ReviewableChatMessage" if self.to_s == "Chat::ReviewableMessage"
-        super
-      end
+    class_methods do
+      def sti_class_mapping = { "ReviewableChatMessage" => Chat::ReviewableMessage }
+      def polymorphic_class_mapping = { "ChatMessage" => Chat::Message }
     end
   end
 end

discourse/discourse

plugins/chat/lib/chat/bookmark_extension.rb
@@ -4,19 +4,8 @@ module Chat
   module BookmarkExtension
     extend ActiveSupport::Concern
 
-    prepended do
-      def valid_bookmarkable_type
-        return true if self.bookmarkable_type == Chat::Message.sti_name
-        super if defined?(super)
-      end
+    prepended { include TypeMappable }
 
-      CLASS_MAPPING = { "ChatMessage" => Chat::Message }
-
-      # the model used when loading chatable_type column
-      def self.polymorphic_class_for(name)
-        return CLASS_MAPPING[name] if CLASS_MAPPING.key?(name)
-        super if defined?(super)
-      end
-    end
+    class_methods { def polymorphic_class_mapping = { "ChatMessage" => Chat::Message } }
   end
 end

plugins/chat/lib/chat/reviewable_extension.rb
@@ -4,24 +4,11 @@ module Chat
   module ReviewableExtension
     extend ActiveSupport::Concern
 
-    prepended do
-      # the model used when loading type column
-      def self.sti_class_for(name)
-        return Chat::ReviewableMessage if name == "ReviewableChatMessage"
-        super(name)
-      end
+    prepended { include TypeMappable }
 
-      # the model used when loading target_type column
-      def self.polymorphic_class_for(name)
-        return Chat::Message if name == Chat::Message.sti_name
-        super(name)
-      end
-
-      # the type column value when saving a Chat::ReviewableMessage
-      def self.sti_name
-        return "ReviewableChatMessage" if self.to_s == "Chat::ReviewableMessage"
-        super
-      end
+    class_methods do
+      def sti_class_mapping = { "ReviewableChatMessage" => Chat::ReviewableMessage }
+      def polymorphic_class_mapping = { "ChatMessage" => Chat::Message }
     end
   end
 end