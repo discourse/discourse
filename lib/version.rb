59dd20c4153cef96ff0e848a087af4464a4bdcce
Ted Johansson <ted@discourse.org>
2023-04-18 16:14:45 +0800

    Version bump to v3.0.3 (#21136)

--- lib/version.rb
+++ lib/version.rb
@@ -9,7 +9,7 @@ module Discourse
     module VERSION #:nodoc:
       MAJOR = 3
       MINOR = 0
-      TINY = 2
+      TINY = 3
       PRE = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
