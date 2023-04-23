069fbb9eafe4d0caa0d086251228bc325c31ef2d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-04-23 22:32:41 +0200

    fix placeholder tests

--- plugins/chat/test/javascripts/components/chat-composer-placeholder-test.js
+++ plugins/chat/test/javascripts/components/chat-composer-placeholder-test.js
@@ -5,6 +5,7 @@ import ChatChannel from "discourse/plugins/chat/discourse/models/chat-channel";
 import { module, test } from "qunit";
 import { render } from "@ember/test-helpers";
 import pretender from "discourse/tests/helpers/create-pretender";
+import { getOwner } from "discourse-common/lib/get-owner";
 
 module(
   "Discourse Chat | Component | chat-composer placeholder",
@@ -15,20 +16,21 @@ module(
       pretender.get("/chat/emojis.json", () => [200, [], {}]);
 
       this.currentUser.set("id", 1);
-      this.set(
-        "chatChannel",
-        ChatChannel.create({
-          chatable_type: "DirectMessage",
-          chatable: {
-            users: [{ id: 1 }],
-          },
-        })
+      this.channel = ChatChannel.create({
+        chatable_type: "DirectMessage",
+        chatable: {
+          users: [{ id: 1 }],
+        },
+      });
+      this.composer = getOwner(this).lookup("service:chat-channel-composer");
+      getOwner(this).lookup("service:chat").activeChannel = this.channel;
+
+      await render(
+        hbs`<ChatComposer @channel={{this.channel}} @composerService={{this.composer}} />`
       );
 
-      await render(hbs`<ChatComposer @chatChannel={{this.chatChannel}} />`);
-
       assert.strictEqual(
-        query(".chat-composer-input").placeholder,
+        query(".chat-composer__input").placeholder,
         "Jot something down"
       );
     });
@@ -36,24 +38,25 @@ module(
     test("direct message to multiple folks shows their names", async function (assert) {
       pretender.get("/chat/emojis.json", () => [200, [], {}]);
 
-      this.set(
-        "chatChannel",
-        ChatChannel.create({
-          chatable_type: "DirectMessage",
-          chatable: {
-            users: [
-              { name: "Tomtom" },
-              { name: "Steaky" },
-              { username: "zorro" },
-            ],
-          },
-        })
+      this.channel = ChatChannel.create({
+        chatable_type: "DirectMessage",
+        chatable: {
+          users: [
+            { name: "Tomtom" },
+            { name: "Steaky" },
+            { username: "zorro" },
+          ],
+        },
+      });
+      this.composer = getOwner(this).lookup("service:chat-channel-composer");
+      getOwner(this).lookup("service:chat").activeChannel = this.channel;
+
+      await render(
+        hbs`<ChatComposer @channel={{this.channel}} @composerService={{this.composer}} />`
       );
 
-      await render(hbs`<ChatComposer @chatChannel={{this.chatChannel}} />`);
-
       assert.strictEqual(
-        query(".chat-composer-input").placeholder,
+        query(".chat-composer__input").placeholder,
         "Chat with Tomtom, Steaky, @zorro"
       );
     });
@@ -61,19 +64,20 @@ module(
     test("message to channel shows send message to channel name", async function (assert) {
       pretender.get("/chat/emojis.json", () => [200, [], {}]);
 
-      this.set(
-        "chatChannel",
-        ChatChannel.create({
-          chatable_type: "Category",
-          title: "just-cats",
-        })
+      this.channel = ChatChannel.create({
+        chatable_type: "Category",
+        title: "just-cats",
+      });
+      this.composer = getOwner(this).lookup("service:chat-channel-composer");
+      getOwner(this).lookup("service:chat").activeChannel = this.channel;
+
+      await render(
+        hbs`<ChatComposer @channel={{this.channel}} @composerService={{this.composer}} />`
       );
 
-      await render(hbs`<ChatComposer @chatChannel={{this.chatChannel}} />`);
-
       assert.strictEqual(
-        query(".chat-composer-input").placeholder,
-        "Chat with #just-cats"
+        query(".chat-composer__input").placeholder,
+        "Chat in #just-cats"
       );
     });
   }
