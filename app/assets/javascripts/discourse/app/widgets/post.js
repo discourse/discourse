c03f83bbea558caee8090dfc43fb4c817028c464
Jan Cernik <66427541+jancernik@users.noreply.github.com>
2023-04-24 16:04:26 -0300

    FIX: Show auto-group flair according to user preferences (#21221)

--- app/assets/javascripts/discourse/app/widgets/post.js
+++ app/assets/javascripts/discourse/app/widgets/post.js
@@ -226,13 +226,15 @@ createWidget("post-avatar", {
 
     const postAvatarBody = [body];
 
-    if (attrs.flair_url || attrs.flair_bg_color) {
-      postAvatarBody.push(this.attach("avatar-flair", attrs));
-    } else {
-      const autoFlairAttrs = autoGroupFlairForUser(this.site, attrs);
+    if (attrs.flair_group_id) {
+      if (attrs.flair_url || attrs.flair_bg_color) {
+        postAvatarBody.push(this.attach("avatar-flair", attrs));
+      } else {
+        const autoFlairAttrs = autoGroupFlairForUser(this.site, attrs);
 
-      if (autoFlairAttrs) {
-        postAvatarBody.push(this.attach("avatar-flair", autoFlairAttrs));
+        if (autoFlairAttrs) {
+          postAvatarBody.push(this.attach("avatar-flair", autoFlairAttrs));
+        }
       }
     }
 
