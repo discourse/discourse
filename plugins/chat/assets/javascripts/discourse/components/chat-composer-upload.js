8ca299cc878122aef31dfb8063bbf823969a232f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-04-24 10:02:35 +0200

    glimmerify

--- plugins/chat/assets/javascripts/discourse/components/chat-composer-upload.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-composer-upload.js
@@ -1,25 +1,16 @@
-import Component from "@ember/component";
-import discourseComputed from "discourse-common/utils/decorators";
+import Component from "@glimmer/component";
 import { isImage } from "discourse/lib/uploads";
 
-export default Component.extend({
-  IMAGE_TYPE: "image",
+export default class ChatComposerUpload extends Component {
+  get isImage() {
+    return isImage(
+      this.args.upload.original_filename || this.args.upload.upload.fileName
+    );
+  }
 
-  tagName: "",
-  classNames: "chat-upload",
-  isDone: false,
-  upload: null,
-  onCancel: null,
-
-  @discourseComputed("upload.{original_filename,fileName}")
-  type(upload) {
-    if (isImage(upload.original_filename || upload.fileName)) {
-      return this.IMAGE_TYPE;
-    }
-  },
-
-  @discourseComputed("isDone", "upload.{original_filename,fileName}")
-  fileName(isDone, upload) {
-    return isDone ? upload.original_filename : upload.fileName;
-  },
-});
+  get fileName() {
+    return this.isDone
+      ? this.args.upload.original_filename
+      : this.args.upload.fileName;
+  }
+}
