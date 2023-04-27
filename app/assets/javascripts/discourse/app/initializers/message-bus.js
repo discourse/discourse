959a8b48849184b23a6b9e32f279179e8538fb3d
Mark VanLandingham <markvanlan@gmail.com>
2023-04-27 12:51:43 -0500

    Revert "FEATURE: Service to track message bus connectivity + offline indicator(#21259)"
    This reverts commit 6bba514b646fef86b22ee2eafdda36842ec25598.

--- app/assets/javascripts/discourse/app/initializers/message-bus.js
+++ app/assets/javascripts/discourse/app/initializers/message-bus.js
@@ -5,14 +5,23 @@ import { handleLogoff } from "discourse/lib/ajax";
 import userPresent, { onPresenceChange } from "discourse/lib/user-presence";
 
 const LONG_POLL_AFTER_UNSEEN_TIME = 1200000; // 20 minutes
+const CONNECTIVITY_ERROR_CLASS = "message-bus-offline";
 
-function ajax(opts, messageBusConnectivity) {
+function updateConnectivityIndicator(stat) {
+  if (stat === "error") {
+    document.documentElement.classList.add(CONNECTIVITY_ERROR_CLASS);
+  } else {
+    document.documentElement.classList.remove(CONNECTIVITY_ERROR_CLASS);
+  }
+}
+
+function ajax(opts) {
   if (opts.complete) {
     const oldComplete = opts.complete;
     opts.complete = function (xhr, stat) {
       handleLogoff(xhr);
       oldComplete(xhr, stat);
-      messageBusConnectivity.setConnectivity(stat !== "error");
+      updateConnectivityIndicator(stat);
     };
   } else {
     opts.complete = handleLogoff;
@@ -33,10 +42,7 @@ export default {
 
     const messageBus = container.lookup("service:message-bus"),
       user = container.lookup("service:current-user"),
-      siteSettings = container.lookup("service:site-settings"),
-      messageBusConnectivity = container.lookup(
-        "service:message-bus-connectivity"
-      );
+      siteSettings = container.lookup("service:site-settings");
 
     messageBus.alwaysLongPoll = !isProduction();
     messageBus.shouldLongPollCallback = () =>
@@ -91,7 +97,7 @@ export default {
         if (userPresent()) {
           opts.headers["Discourse-Present"] = "true";
         }
-        return ajax(opts, messageBusConnectivity);
+        return ajax(opts);
       };
     } else {
       messageBus.ajax = function (opts) {
@@ -99,7 +105,7 @@ export default {
         if (userPresent()) {
           opts.headers["Discourse-Present"] = "true";
         }
-        return ajax(opts, messageBusConnectivity);
+        return ajax(opts);
       };
 
       messageBus.baseUrl = getURL("/");
