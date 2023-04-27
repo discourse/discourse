fe0f01908095a3dd1dfcf87228cc141dad873314
Jarek Radosz <jradosz@gmail.com>
2023-04-27 10:54:16 +0200

    Updates

--- app/assets/javascripts/discourse/app/routes/guidelines.js
+++ app/assets/javascripts/discourse/app/routes/guidelines.js
@@ -1,6 +1,7 @@
 import staticRouteBuilder from "discourse/lib/static-route-builder";
 
 export default class GuidelinesRoute extends staticRouteBuilder("guidelines") {
+  // Guidelines and faq share the template (and the implicit controller)
   controllerName = "faq";
   templateName = "faq";
 }
