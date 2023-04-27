d8835812d568140cb4c6b3c707718eaffe6cdfb2
Mark VanLandingham <markvanlan@gmail.com>
2023-04-27 10:45:44 -0500

    lil tweak

--- spec/system/network_disconnected_spec.rb
+++ spec/system/network_disconnected_spec.rb
@@ -3,6 +3,12 @@
 RSpec.describe "Network Disconnected", type: :system, js: true do
   fab!(:current_user) { Fabricate(:user) }
 
+  def with_network_disconnected
+    page.driver.browser.network_conditions = { offline: true }
+    yield
+    page.driver.browser.network_conditions = { offline: false }
+  end
+
   it "Message bus connectivity service adds class to DOM and displays offline indicator" do
     sign_in(current_user)
     visit("/c")
@@ -10,12 +16,12 @@ RSpec.describe "Network Disconnected", type: :system, js: true do
     expect(page).to have_no_css("html.message-bus-offline")
     expect(page).to have_no_css(".offline-indicator")
 
-    page.driver.browser.network_conditions = { offline: true, latency: 0, throughput: 0 }
+    with_network_disconnected do
+      # Message bus connectivity services adds the disconnected class to the DOM
+      expect(page).to have_css("html.message-bus-offline")
 
-    # Message bus connectivity services adds the disconnected class to the DOM
-    expect(page).to have_css("html.message-bus-offline")
-
-    # Offline indicator is rendered
-    expect(page).to have_css(".offline-indicator")
+      # Offline indicator is rendered
+      expect(page).to have_css(".offline-indicator")
+    end
   end
 end
