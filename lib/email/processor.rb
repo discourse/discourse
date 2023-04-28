ff37d6cbb33dfb2cf4291e24521a96c58e362ecd
David Battersby <info@davidbattersby.com>
2023-04-28 15:39:20 +0800

    dont raise error if mail is blank or nil, warning will be handled by rejection mailer

--- lib/email/processor.rb
+++ lib/email/processor.rb
@@ -10,7 +10,7 @@ module Email
     end
 
     def self.process!(mail, opts = {})
-      Email::Processor.new(mail, opts).process! if mail.present?
+      Email::Processor.new(mail, opts).process!
     end
 
     def process!
@@ -156,7 +156,7 @@ module Email
     end
 
     def can_send_rejection_email?(email, type)
-      return false if @receiver.sent_to_mailinglist_mirror?
+      return false if @receiver&.sent_to_mailinglist_mirror?
       return true if type == :email_reject_unrecognized_error
 
       key = "rejection_email:#{email}:#{type}:#{Date.today}"
