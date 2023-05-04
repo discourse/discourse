a42fc8441f0967b8420a64c53e072a054f2eba9f
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-05-04 10:34:20 +1000

    FIX: after rebase

--- app/assets/javascripts/discourse/app/lib/sidebar/community-section.js
+++ app/assets/javascripts/discourse/app/lib/sidebar/community-section.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
-import { tracked } from "@glimmer/tracking";
-
-=======
 import I18n from "I18n";
 import SectionLink from "discourse/lib/sidebar/section-link";
 import Composer from "discourse/models/composer";
@@ -26,92 +20,6 @@ import {
   secondaryCustomSectionLinks,
 } from "discourse/lib/sidebar/custom-community-section-links";
 
-export default class SidebarCommunitySection extends Component {
-  @service router;
-  @service topicTrackingState;
-  @service currentUser;
-  @service appEvents;
-  @service site;
-  @service siteSettings;
-
-  @tracked sectionLinks;
-  @tracked moreSectionLinks;
-  @tracked moreSecondarySectionLinks;
-
-  callbackId;
-  headerActionsIcon;
-  headerActions;
-
-  constructor() {
-    super(...arguments);
-
-    this.moreSectionLinks = this.#initializeSectionLinks(
-      [...this.defaultMoreSectionLinks, ...customSectionLinks],
-      { inMoreDrawer: true }
-    );
-
-    this.moreSecondarySectionLinks = this.#initializeSectionLinks(
-      [
-        ...this.defaultMoreSecondarySectionLinks,
-        ...secondaryCustomSectionLinks,
-      ],
-      { inMoreDrawer: true }
-    );
-
-    this.sectionLinks = this.#initializeSectionLinks(
-      this.defaultMainSectionLinks,
-      { inMoreDrawer: false }
-    );
-
-    this.callbackId = this.topicTrackingState.onStateChange(() => {
-      this.sectionLinks.forEach((sectionLink) => {
-        sectionLink.onTopicTrackingStateChange();
-      });
-    });
-  }
-
-  willDestroy() {
-    [
-      ...this.sectionLinks,
-      ...this.moreSectionLinks,
-      ...this.moreSecondarySectionLinks,
-    ].forEach((sectionLink) => {
-      sectionLink.teardown?.();
-    });
-
-    this.topicTrackingState.offStateChange(this.callbackId);
-  }
-
-  // Override in child
-  get defaultMainSectionLinks() {
-    return [];
-  }
-
-  // Override in child
-  get defaultMoreSectionLinks() {
-    return [];
-  }
-
-  // Override in child
-  get defaultMoreSecondarySectionLinks() {
-    return [];
-  }
-
-  get isDesktopDropdownMode() {
-    const headerDropdownMode =
-      this.siteSettings.navigation_menu === "header dropdown";
-
-    return !this.site.mobileView && headerDropdownMode;
-  }
-
-  #initializeSectionLinks(sectionLinkClasses, { inMoreDrawer } = {}) {
-    return sectionLinkClasses.map((sectionLinkClass) => {
-      return this.#initializeSectionLink(sectionLinkClass, inMoreDrawer);
-    });
-  }
-
-  #initializeSectionLink(sectionLinkClass, inMoreDrawer) {
-=======
 const LINKS_IN_BOTH_SEGMENTS = ["/review"];
 const SPECIAL_LINKS_MAP = {
   "/latest": EverythingSectionLink,
