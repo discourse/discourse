f64e5f9b0a17d3f8f6bf21830432f59a14a0edb8
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-05-04 17:26:38 +0200

    fixes spec

--- plugins/chat/spec/system/chat_message/thread_spec.rb
+++ plugins/chat/spec/system/chat_message/thread_spec.rb
@@ -25,13 +25,13 @@ RSpec.describe "Chat message - thread", type: :system, js: true do
 
   context "when hovering a message" do
     it "adds an active class" do
-      last_message = thread_1.chat_messages.last
+      first_message = thread_1.chat_messages.first
       chat.visit_thread(thread_1)
 
-      thread.hover_message(last_message)
+      thread.hover_message(first_message)
 
       expect(page).to have_css(
-        ".chat-thread[data-id='#{thread_1.id}'] [data-id='#{last_message.id}'] .chat-message.is-active",
+        ".chat-thread[data-id='#{thread_1.id}'] [data-id='#{first_message.id}'] .chat-message.is-active",
       )
     end
   end
