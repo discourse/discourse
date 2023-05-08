b12afa65f2e930f106333fcaebde61309af21389
Jarek Radosz <jradosz@gmail.com>
2023-05-08 22:41:22 +0200

    Resolve todo and the controllerFor issue

--- app/assets/javascripts/discourse/app/routes/login.js
+++ app/assets/javascripts/discourse/app/routes/login.js
@@ -1,9 +1,14 @@
 import DiscourseRoute from "discourse/routes/discourse";
+import { inject as service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 import { next } from "@ember/runloop";
 import StaticPage from "discourse/models/static-page";
 
 export default class LoginRoute extends DiscourseRoute {
+  @service siteSettings;
+
+  controllerName = "login-page";
+
   beforeModel() {
     if (!this.siteSettings.login_required) {
       this.replaceWith(`/${defaultHomepage()}`).then((e) => {
@@ -12,7 +17,10 @@ export default class LoginRoute extends DiscourseRoute {
     }
   }
 
-  model() {
-    return StaticPage.find("login");
+  async model() {
+    return {
+      page: await StaticPage.find("login"),
+      canSignUp: this.controllerFor("application").canSignUp,
+    };
   }
 }
