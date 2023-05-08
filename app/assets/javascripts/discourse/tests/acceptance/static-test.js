3743efa777f52097f9b1c1892bd18ee1f6297dfb
Jarek Radosz <jradosz@gmail.com>
2023-05-08 16:20:58 +0200

    qunit-dom

--- app/assets/javascripts/discourse/tests/acceptance/static-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/static-test.js
@@ -1,4 +1,4 @@
-import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import { currentRouteName, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 
@@ -9,42 +9,42 @@ acceptance("Static", function () {
       document.body.classList.contains("static-faq"),
       "has the body class"
     );
-    assert.true(exists(".body-page"), "The content is present");
+    assert.dom(".body-page").exists("The content is present");
 
     await visit("/guidelines");
     assert.true(
       document.body.classList.contains("static-guidelines"),
       "has the body class"
     );
-    assert.true(exists(".body-page"), "The content is present");
+    assert.dom(".body-page").exists("The content is present");
 
     await visit("/conduct");
     assert.true(
       document.body.classList.contains("static-conduct"),
       "has the body class"
     );
-    assert.true(exists(".body-page"), "The content is present");
+    assert.dom(".body-page").exists("The content is present");
 
     await visit("/tos");
     assert.true(
       document.body.classList.contains("static-tos"),
       "has the body class"
     );
-    assert.true(exists(".body-page"), "The content is present");
+    assert.dom(".body-page").exists("The content is present");
 
     await visit("/privacy");
     assert.true(
       document.body.classList.contains("static-privacy"),
       "has the body class"
     );
-    assert.true(exists(".body-page"), "The content is present");
+    assert.dom(".body-page").exists("The content is present");
 
     await visit("/rules");
     assert.true(
       document.body.classList.contains("static-rules"),
       "has the body class"
     );
-    assert.true(exists(".body-page"), "The content is present");
+    assert.dom(".body-page").exists("The content is present");
   });
 
   test("Login redirect", async function (assert) {
@@ -62,7 +62,7 @@ acceptance("Static", function () {
     await visit("/login");
 
     assert.strictEqual(currentRouteName(), "login");
-    assert.true(exists(".body-page"), "The content is present");
+    assert.dom(".body-page").exists("The content is present");
     assert.dom(".sign-up-button").exists();
     assert.dom(".login-button").exists();
   });
@@ -82,6 +82,6 @@ acceptance("Static", function () {
     await visit("/signup");
 
     assert.strictEqual(currentRouteName(), "login");
-    assert.true(exists(".body-page"), "The content is present");
+    assert.dom(".body-page").exists("The content is present");
   });
 });
