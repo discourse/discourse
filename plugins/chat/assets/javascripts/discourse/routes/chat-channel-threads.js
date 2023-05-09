ce08d63c218ecadb1e7362c50df61709d0d07be0
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-05-09 18:53:57 +0200

    correctly close the side panel on 404

--- plugins/chat/assets/javascripts/discourse/routes/chat-channel-threads.js
+++ plugins/chat/assets/javascripts/discourse/routes/chat-channel-threads.js
@@ -11,12 +11,13 @@ export default class ChatChannelThreads extends DiscourseRoute {
 
   beforeModel(transition) {
     const channel = this.modelFor("chat.channel");
-    this.chatChannelThreadListPane.open();
 
     if (!channel.threadingEnabled) {
       transition.abort();
       this.router.transitionTo("chat.channel", ...channel.routeModels);
       return;
     }
+
+    this.chatChannelThreadListPane.open();
   }
 }
