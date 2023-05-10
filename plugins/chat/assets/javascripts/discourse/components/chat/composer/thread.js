6ae404e33fac5f9a96f6895d47b9d6df0ced1858
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-05-10 19:04:28 +0200

    no need for tracked

--- plugins/chat/assets/javascripts/discourse/components/chat/composer/thread.js
+++ plugins/chat/assets/javascripts/discourse/components/chat/composer/thread.js
@@ -2,15 +2,12 @@ import ChatComposer from "../../chat-composer";
 import { inject as service } from "@ember/service";
 import I18n from "I18n";
 import { action } from "@ember/object";
-import { tracked } from "@glimmer/tracking";
 
 export default class ChatComposerThread extends ChatComposer {
   @service("chat-channel-thread-composer") composer;
   @service("chat-channel-composer") channelComposer;
   @service("chat-channel-thread-pane") pane;
   @service router;
-
-  @tracked thread;
 
   context = "thread";
 
