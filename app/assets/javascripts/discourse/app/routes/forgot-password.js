1bb1290f093d54c5bd56be2c288f448e2809c9f3
Jarek Radosz <jradosz@gmail.com>
2023-05-11 19:02:11 +0200

    DEV: Rework `static` controllers/routes (#19466)
    The issues fixed:
    
    1. Previously all static pages (e.g. login-required landing page, /tos, /privacy, forgot-password) were wrapped in the faq-read-tracking component
    2. All these pages shared one controller with methods that were relevant to one route
    3. There were two route-generating functions: `static-route-builder` and `build-static-route` 🤣 
    4. They were using the deprecated `renderTemplate()` API
    5. A slight misuse of Ember API (`controllerFor()`)
    6. Small mark-faq-read related bugs

--- app/assets/javascripts/discourse/app/routes/forgot-password.js
+++ app/assets/javascripts/discourse/app/routes/forgot-password.js
@@ -1,19 +1,15 @@
-import buildStaticRoute from "discourse/routes/build-static-route";
+import DiscourseRoute from "discourse/routes/discourse";
 import { defaultHomepage } from "discourse/lib/utilities";
 import { next } from "@ember/runloop";
 
-const ForgotPasswordRoute = buildStaticRoute("password-reset");
+export default class ForgotPasswordRoute extends DiscourseRoute {
+  async beforeModel() {
+    const { loginRequired } = this.controllerFor("application");
 
-ForgotPasswordRoute.reopen({
-  beforeModel() {
-    const loginRequired =
-      this.controllerFor("application").get("loginRequired");
-    this.replaceWith(
+    const e = await this.replaceWith(
       loginRequired ? "login" : `discovery.${defaultHomepage()}`
-    ).then((e) => {
-      next(() => e.send("showForgotPassword"));
-    });
-  },
-});
+    );
 
-export default ForgotPasswordRoute;
+    next(() => e.send("showForgotPassword"));
+  }
+}
