164b6a596d1529091b343629e1c37028fe8d32d6
Martin Brennan <martin@discourse.org>
2023-05-11 09:16:25 +0200

    FEATURE: Create and update thread membership for user
    When the user sends a message in a thread, we want to
    create a membership for them in the background (default
    to notification level of Watching) so we can track whether
    they have read the thread.
    
    Then, for now since we don't have granular message reading/
    scrolling in the thread panel, we just update the thread
    last_read_message_id for the user to the latest reply in the
    thread when they open the thread panel. This at least will
    mark the thread as read.
    
    In future PRs we want to show the blue dot indicator in various
    places in the UI for unread threads which will also require
    some MessageBus functionality.

--- plugins/chat/app/services/chat/update_user_thread_last_read.rb
+++ plugins/chat/app/services/chat/update_user_thread_last_read.rb
@@ -3,8 +3,9 @@
 module Chat
   # Service responsible for marking messages in a thread
   # as read. For now this just marks any mentions in the thread
-  # as read, but as we add user tracking state to threads it
-  # will work in a similar way to Chat::UpdateUserLastRead
+  # as read and marks the entire thread as read.
+  # As we add finer-grained user tracking state to threads it
+  # will work in a similar way to Chat::UpdateUserLastRead.
   #
   # @example
   #  Chat::UpdateUserThreadLastRead.call(channel_id: 2, thread_id: 3, guardian: guardian)
@@ -22,6 +23,7 @@ module Chat
     model :thread
     policy :invalid_access
     step :mark_associated_mentions_as_read
+    step :mark_thread_read
     step :publish_new_last_read_to_clients
 
     # @!visibility private
@@ -40,6 +42,23 @@ module Chat
 
     def invalid_access(guardian:, thread:, **)
       guardian.can_join_chat_channel?(thread.channel)
+    end
+
+    # NOTE: In future we will pass in the last_read_message_id
+    # to the service and this query will be unnecessary.
+    def mark_thread_read(thread:, guardian:, **)
+      query = <<~SQL
+        UPDATE user_chat_thread_memberships
+        SET last_read_message_id = (
+          SELECT id FROM chat_messages
+          WHERE thread_id = :thread_id
+          AND deleted_at IS NULL
+          ORDER BY created_at DESC, id DESC
+          LIMIT 1
+        )
+        WHERE user_id = :user_id AND thread_id = :thread_id
+      SQL
+      DB.exec(query, thread_id: thread.id, user_id: guardian.user.id)
     end
 
     def mark_associated_mentions_as_read(thread:, guardian:, **)
