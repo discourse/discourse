2996ec5b4ec3ad81c526b105b9a89b3357208e6d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-05-11 15:41:25 +0200

    uses double raf and abandon pendingupdate hack
    This was causing invalid values in android

--- plugins/chat/assets/javascripts/discourse/components/chat-vh.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-vh.js
@@ -3,7 +3,6 @@ import Component from "@ember/component";
 import isZoomed from "discourse/plugins/chat/discourse/lib/zoom-check";
 
 const CSS_VAR = "--chat-vh";
-let pendingUpdate = false;
 
 export default class ChatVh extends Component {
   tagName = "";
@@ -42,16 +41,10 @@ export default class ChatVh extends Component {
         this.setVHFromVisualViewPort
       );
     }
-
-    pendingUpdate = false;
   }
 
   @bind
   setVHFromKeyboard(event) {
-    if (pendingUpdate) {
-      return;
-    }
-
     if (this.isDestroying || this.isDestroyed) {
       return;
     }
@@ -60,25 +53,20 @@ export default class ChatVh extends Component {
       return;
     }
 
-    pendingUpdate = true;
-
-    const { height } = event.target.boundingRect;
-
     requestAnimationFrame(() => {
-      const vhInPixels =
-        ((window.visualViewport?.height || window.innerHeight) - height) * 0.01;
-      document.documentElement.style.setProperty(CSS_VAR, `${vhInPixels}px`);
+      requestAnimationFrame(() => {
+        const keyboardHeight = event.target.boundingRect.height;
+        const viewportHeight =
+          window.visualViewport?.height || window.innerHeight;
 
-      pendingUpdate = false;
+        const vhInPixels = (viewportHeight - keyboardHeight) * 0.01;
+        document.documentElement.style.setProperty(CSS_VAR, `${vhInPixels}px`);
+      });
     });
   }
 
   @bind
   setVHFromVisualViewPort() {
-    if (pendingUpdate) {
-      return;
-    }
-
     if (this.isDestroying || this.isDestroyed) {
       return;
     }
@@ -87,14 +75,10 @@ export default class ChatVh extends Component {
       return;
     }
 
-    pendingUpdate = true;
-
     requestAnimationFrame(() => {
       const vhInPixels =
         (window.visualViewport?.height || window.innerHeight) * 0.01;
       document.documentElement.style.setProperty(CSS_VAR, `${vhInPixels}px`);
-
-      pendingUpdate = false;
     });
   }
 }
