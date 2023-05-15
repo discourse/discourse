4b3800b447747e92cc6d57f5312f3a379e1806d9
Martin Brennan <martin@discourse.org>
2023-05-15 17:51:04 +0200

    FIX: Reinstate chatStateManager in ChannelTitle component
    It is actually used in the template, but no warning from Ember

--- plugins/chat/assets/javascripts/discourse/components/chat-drawer/header/channel-title.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-drawer/header/channel-title.js
@@ -3,6 +3,7 @@ import { inject as service } from "@ember/service";
 
 export default class ChatDrawerChannelHeaderTitle extends Component {
   @service chatTrackingState;
+  @service chatStateManager;
 
   get channelUnreadCount() {
     return this.chatTrackingState.getChannelState(this.args.channel.id)
