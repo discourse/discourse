35f29bd25b4de61188ab7141af7752a6eec24d4e
David Taylor <david@taylorhq.com>
2023-05-15 11:15:07 +0100

    DEV: Document `showModal`, allow setting more config via DModalBody
    Having these things configured at the invocation of showModal is a strange API, and means that any changes to the modal require updating the call sites. It makes much more sense for these to be defined as part of the modal's own template. This was already supported for many of the properties. This commit adds support for the `modalClass` and `titleAriaElementId` config to be passed to DModalBody.
    
    For now there is no deprecation message. Support for passing these things to `showModal` will be dropped as part of an upcoming conversion of modals from controllers to components.

--- app/assets/javascripts/discourse/app/components/d-modal-body.js
+++ app/assets/javascripts/discourse/app/components/d-modal-body.js
@@ -41,6 +41,8 @@ export default class DModalBody extends Component {
         "submitOnEnter",
         "dismissable",
         "headerClass",
+        "modalClass",
+        "titleAriaElementId",
       ])
     );
   }
