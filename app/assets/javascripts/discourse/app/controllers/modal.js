c6dccc10a7e1711bf09d919f6bd408a72772548a
David Taylor <david@taylorhq.com>
2023-05-15 21:43:21 +0100

    FIX: Hide d-modal during page load (#21563)
    Followup to 4bc769cac083dd90c2c312e691dca91c65725ee7

--- app/assets/javascripts/discourse/app/controllers/modal.js
+++ app/assets/javascripts/discourse/app/controllers/modal.js
@@ -2,5 +2,5 @@ import Controller from "@ember/controller";
 import { tracked } from "@glimmer/tracking";
 
 export default class ModalController extends Controller {
-  @tracked hidden;
+  @tracked hidden = true;
 }
