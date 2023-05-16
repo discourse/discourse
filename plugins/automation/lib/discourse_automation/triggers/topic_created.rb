654527aedd9350ea2c9f0443cd7b539b9129abcd
Penar Musaraj <pmusaraj@gmail.com>
2023-05-16 09:54:26 -0400

    DEV: Allow clearing choices dropdowns, cleanup (#186)

--- plugins/automation/lib/discourse_automation/triggers/topic_created.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-DiscourseAutomation::Triggerable::TOPIC_CREATED = "topic_created"
-
-DiscourseAutomation::Triggerable.add(DiscourseAutomation::Triggerable::TOPIC_CREATED) do
-  field :valid_trust_levels, component: :"trust-levels"
-end
