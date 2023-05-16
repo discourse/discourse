7c69c81980cab35836c7ef3ab6266ee8377d8403
Keegan George <kgeorge13@gmail.com>
2023-05-16 10:10:50 -0700

    DEV: Remove old user menu related tests

--- app/assets/javascripts/discourse/tests/unit/routes/review-index-test.js
+++ app/assets/javascripts/discourse/tests/unit/routes/review-index-test.js
@@ -5,34 +5,6 @@ import { module, test } from "qunit";
 module("Unit | Route | review-index", function (hooks) {
   setupTest(hooks);
 
-  test("subscribes and unsubscribes /reviewable_counts MessageBus channel when user menu not enabled", function (assert) {
-    const currentUser = User.create();
-    this.owner.unregister("service:current-user");
-    this.owner.register("service:current-user", currentUser, {
-      instantiate: false,
-    });
-    this.owner.inject("route", "currentUser", "service:current-user");
-
-    const reviewIndexRoute = this.owner.lookup("route:review-index");
-    const messageBus = this.owner.lookup("service:message-bus");
-
-    let channels = messageBus.callbacks.map((c) => c.channel);
-    assert.false(channels.includes("/reviewable_counts"));
-    assert.false(channels.includes("/reviewable_claimed"));
-
-    reviewIndexRoute.activate();
-
-    channels = messageBus.callbacks.map((c) => c.channel);
-    assert.true(channels.includes("/reviewable_counts"));
-    assert.true(channels.includes("/reviewable_claimed"));
-
-    reviewIndexRoute.deactivate();
-
-    channels = messageBus.callbacks.map((c) => c.channel);
-    assert.false(channels.includes("/reviewable_counts"));
-    assert.false(channels.includes("/reviewable_claimed"));
-  });
-
   test("subscribes and unsubscribes /reviewable_counts(with id) when user menu enabled", function (assert) {
     const currentUser = User.create({
       id: "the-id",
