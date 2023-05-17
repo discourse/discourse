5111c516f7ee7ec0642778a1dcfdd5e772e95e3b
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-05-18 00:01:35 +0400

    Test for updated message

--- plugins/chat/test/javascripts/acceptance/user-status-on-mentions-test.js
+++ plugins/chat/test/javascripts/acceptance/user-status-on-mentions-test.js
@@ -20,6 +20,7 @@ acceptance("Chat | User status on mentions", function (needs) {
     username: "acting_user",
   };
   const channelId = 1;
+  const messageId = 1891;
   const mentionedUser1 = {
     id: 1000,
     username: "user1",
@@ -28,20 +29,6 @@ acceptance("Chat | User status on mentions", function (needs) {
       emoji: "surfing_man",
     },
   };
-  const messagesResponse = {
-    meta: {
-      channel_id: channelId,
-    },
-    chat_messages: [
-      {
-        id: 1891,
-        message: `Hey @${mentionedUser1.username}`,
-        cooked: `<p>Hey <a class="mention" href="/u/${mentionedUser1.username}">@${mentionedUser1.username}</a></p>`,
-        mentioned_users: [mentionedUser1],
-        user: actingUser,
-      },
-    ],
-  };
   const mentionedUser2 = {
     id: 2000,
     username: "user2",
@@ -49,6 +36,28 @@ acceptance("Chat | User status on mentions", function (needs) {
       description: "vacation",
       emoji: "desert_island",
     },
+  };
+  const mentionedUser3 = {
+    id: 3000,
+    username: "user3",
+    status: {
+      description: "off to dentist",
+      emoji: "tooth",
+    },
+  };
+  const messagesResponse = {
+    meta: {
+      channel_id: channelId,
+    },
+    chat_messages: [
+      {
+        id: messageId,
+        message: `Hey @${mentionedUser1.username}`,
+        cooked: `<p>Hey <a class="mention" href="/u/${mentionedUser1.username}">@${mentionedUser1.username}</a></p>`,
+        mentioned_users: [mentionedUser1],
+        user: actingUser,
+      },
+    ],
   };
 
   needs.settings({ chat_enabled: true });
@@ -79,16 +88,19 @@ acceptance("Chat | User status on mentions", function (needs) {
     pretender.post("/chat/1", () => {
       return [200, {}, {}];
     });
+    pretender.put(`/chat/1/edit/${messageId}`, () => {
+      return [200, {}, {}];
+    });
     pretender.post("/chat/drafts", () => {
       return [200, {}, {}];
     });
 
-    setupAutocompleteResponses([mentionedUser2]);
+    setupAutocompleteResponses([mentionedUser2, mentionedUser3]);
   });
 
   test("it shows status on mentions on just posted messages", async function (assert) {
     await visit(`/chat/c/-/${channelId}`);
-    await typeAndApplyAutocompleteSuggestion("mentioning @u");
+    await typeWithAutocompleteAndSend(`mentioning @${mentionedUser2.username}`);
 
     assert
       .dom(`.mention[href='/u/${mentionedUser2.username}'] .user-status`)
@@ -107,7 +119,7 @@ acceptance("Chat | User status on mentions", function (needs) {
 
   test("it updates status on mentions on just posted messages", async function (assert) {
     await visit(`/chat/c/-/${channelId}`);
-    await typeAndApplyAutocompleteSuggestion("mentioning @u");
+    await typeWithAutocompleteAndSend(`mentioning @${mentionedUser2.username}`);
 
     const newStatus = {
       description: "working remotely",
@@ -138,7 +150,7 @@ acceptance("Chat | User status on mentions", function (needs) {
   test("it deletes status on mentions on just posted messages", async function (assert) {
     await visit(`/chat/c/-/${channelId}`);
 
-    await typeAndApplyAutocompleteSuggestion("mentioning @u");
+    await typeWithAutocompleteAndSend(`mentioning @${mentionedUser2.username}`);
 
     loggedInUser().appEvents.trigger("user-status:changed", {
       [mentionedUser2.id]: null,
@@ -152,26 +164,31 @@ acceptance("Chat | User status on mentions", function (needs) {
   test("it shows status on mentions on updated messages", async function (assert) {
     await visit(`/chat/c/-/${channelId}`);
 
-    await editMessage(".chat-message-content");
+    await editMessage(
+      ".chat-message-content",
+      `mentioning @${mentionedUser3.username}`
+    );
 
     assert
-      .dom(`.mention[href='/u/${mentionedUser2.username}'] .user-status`)
+      .dom(`.mention[href='/u/${mentionedUser3.username}'] .user-status`)
       .exists("status is rendered")
       .hasAttribute(
         "title",
-        mentionedUser2.status.description,
+        mentionedUser3.status.description,
         "status description is correct"
       )
       .hasAttribute(
         "src",
-        new RegExp(`${mentionedUser2.status.emoji}.png`),
+        new RegExp(`${mentionedUser3.status.emoji}.png`),
         "status emoji is updated"
       );
   });
 
-  async function editMessage(messageSelector) {
+  async function editMessage(messageSelector, text) {
     await triggerEvent(query(messageSelector), "mouseenter");
-    await click(".more-buttons");
+    await click(".more-buttons .select-kit-header-wrapper");
+    await click(".select-kit-collection .select-kit-row[data-value='edit']");
+    await typeWithAutocompleteAndSend(text);
   }
 
   async function emulateAutocomplete(inputSelector, text) {
@@ -184,7 +201,7 @@ acceptance("Chat | User status on mentions", function (needs) {
     await triggerKeyEvent(inputSelector, "keyup", "Backspace");
   }
 
-  async function typeAndApplyAutocompleteSuggestion(text) {
+  async function typeWithAutocompleteAndSend(text) {
     await emulateAutocomplete(".chat-composer__input", text);
     await click(".autocomplete.ac-user .selected");
     await triggerKeyEvent(".chat-composer__input", "keydown", "Enter");
