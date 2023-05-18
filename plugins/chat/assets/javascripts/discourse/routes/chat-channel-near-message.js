73f6103b5f6e671de8360b54d3799f93d3b4db91
Jarek Radosz <jradosz@gmail.com>
2023-05-18 14:56:49 +0200

    DEV: Fix random typos

--- plugins/chat/assets/javascripts/discourse/routes/chat-channel-near-message.js
+++ plugins/chat/assets/javascripts/discourse/routes/chat-channel-near-message.js
@@ -1,7 +1,7 @@
 import DiscourseRoute from "discourse/routes/discourse";
 import { inject as service } from "@ember/service";
 
-// This route is only here as a convience method for a clean `/c/:channelTitle/:channelId/:messageId` URL.
+// This route is only here as a convenience method for a clean `/c/:channelTitle/:channelId/:messageId` URL.
 // It's not a real route, it just redirects to the real route after setting a param on the controller.
 export default class ChatChannelNearMessage extends DiscourseRoute {
   @service router;
