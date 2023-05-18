77b469eaa85fd022b575d25c302f2fca3ae3090b
Rafael dos Santos Silva <xfalcox@gmail.com>
2023-05-18 12:51:34 -0300

    FEATURE: Try to load plugin gems platform variants

--- lib/plugin_gem.rb
+++ lib/plugin_gem.rb
@@ -9,10 +9,8 @@ module PluginGem
     spec_path = gems_path + "/specifications"
 
     spec_file = spec_path + "/#{name}-#{version}"
-    spec_file += "-#{opts[:platform]}" if opts[:platform]
-    spec_file += ".gemspec"
 
-    unless File.exist? spec_file
+    unless existing_variant(spec_file).present?
       command =
         "gem install #{name} -v #{version} -i #{gems_path} --no-document --ignore-dependencies --no-user-install"
       command += " --source #{opts[:source]}" if opts[:source]
@@ -21,15 +19,30 @@ module PluginGem
       Bundler.with_unbundled_env { puts `#{command}` }
     end
 
-    if File.exist? spec_file
+    spec_file_variant = existing_variant(spec_file)
+    if spec_file_variant.present?
       Gem.path << gems_path
-      Gem::Specification.load(spec_file).activate
+      Gem::Specification.load(spec_file_variant).activate
 
       require opts[:require_name] ? opts[:require_name] : name unless opts[:require] == false
     else
       puts "You are specifying the gem #{name} in #{path}, however it does not exist!"
-      puts "Looked for: #{spec_file}"
+      puts "Looked for: #{spec_file} and #{spec_file_variant}"
       exit(-1)
     end
+  end
+
+  def self.existing_variant(spec_file)
+    platform_less = "#{spec_file}.gemspec"
+    return platform_less if File.exist? platform_less
+
+    platform_full = "#{spec_file}-#{RUBY_PLATFORM}.gemspec"
+    return platform_full if File.exist? platform_full
+
+    platform_version_less =
+      "#{spec_file}-#{Gem::Platform.local.cpu}-#{Gem::Platform.local.os}.gemspec"
+    return platform_version_less if File.exist? platform_version_less
+
+    nil
   end
 end
