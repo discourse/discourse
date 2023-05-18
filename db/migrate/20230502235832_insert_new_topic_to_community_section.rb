2b84a7aa8d7338748ce1370a69592ee9e4188605
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-05-18 13:18:40 +1000

    FIX: irreversible migration

--- db/migrate/20230502235832_insert_new_topic_to_community_section.rb
+++ db/migrate/20230502235832_insert_new_topic_to_community_section.rb
@@ -20,21 +20,6 @@ class InsertNewTopicToCommunitySection < ActiveRecord::Migration[7.0]
   end
 
   def down
-    community_section_query = DB.query <<~SQL
-      SELECT id FROM sidebar_sections WHERE section_type = 0
-    SQL
-    community_section_id = community_section_query.last&.id
-
-    result = DB.query <<~SQL
-      DELETE FROM sidebar_section_links
-      WHERE id IN (SELECT id from sidebar_section_links WHERE sidebar_section_id = #{community_section_id} ORDER BY POSITION DESC LIMIT 1)
-        RETURNING sidebar_section_links.linkable_id
-    SQL
-    sidebar_url_ids = result.map(&:linkable_id)
-
-    DB.query <<~SQL
-      DELETE FROM sidebar_urls
-      WHERE id IN (#{sidebar_url_ids.join(",")})
-    SQL
+    raise ActiveRecord::IrreversibleMigration
   end
 end
