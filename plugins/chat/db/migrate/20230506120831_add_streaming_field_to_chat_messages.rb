4460d6265fd8359da99c5f2852670e373d28543d
Jan Cernik <jancernik12@gmail.com>
2023-05-22 09:35:50 -0300

    Add frozen_string_literal

--- plugins/chat/db/migrate/20230506120831_add_streaming_field_to_chat_messages.rb
+++ plugins/chat/db/migrate/20230506120831_add_streaming_field_to_chat_messages.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddStreamingFieldToChatMessages < ActiveRecord::Migration[7.0]
   def change
     add_column :chat_messages, :streaming, :boolean, null: false, default: false
