6dd1c6d1d81db1ee62c5a8954bb7a972b9a10567
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-05-24 20:30:25 +0200

    FEATURE: apply hashtag styles to autocomplete
    This commit uses improvements done in https://github.com/discourse/discourse/commit/0b3cf83e3c7d03c2dcd54f86de60efe32f385a44 to apply these styles to the autocomplete in composer.

--- app/assets/javascripts/discourse/app/lib/hashtag-autocomplete.js
+++ app/assets/javascripts/discourse/app/lib/hashtag-autocomplete.js
@@ -239,6 +239,12 @@ function _searchRequest(term, contextualHashtagConfiguration, resultFunc) {
       response.results?.forEach((result) => {
         // Convert :emoji: in the result text to HTML safely.
         result.text = htmlSafe(emojiUnescape(escapeExpression(result.text)));
+
+        const hashtagType = getHashtagTypeClasses()[result.type];
+        result.icon = hashtagType.generateIconHTML({
+          icon: result.icon,
+          id: result.id,
+        });
       });
       resultFunc(response.results || CANCELLED_STATUS);
     })
