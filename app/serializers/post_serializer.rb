2ef7e4ba5649359a6b018badcb40fe546a8804cf
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-05-26 12:23:32 +0200

    correctly check for SiteSetting.enable_user_status

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -570,7 +570,9 @@ class PostSerializer < BasicPostSerializer
       if @topic_view && (mentioned_users = @topic_view.mentioned_users[object.id])
         mentioned_users
       else
-        User.includes(:user_status).where(username: object.mentions)
+        query = User
+        query = query.includes(:user_status) if SiteSetting.enable_user_status
+        query = query.where(username: object.mentions)
       end
 
     users.map { |user| BasicUserWithStatusSerializer.new(user, root: false) }
