b82818001a02ba14f25f5da0a7d2b520974e83ed
Martin Brennan <martin@discourse.org>
2023-05-29 18:50:39 +0200

    DEV: Review fixes

--- plugins/chat/assets/javascripts/discourse/components/chat-drawer/header/right-actions.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-drawer/header/right-actions.js
@@ -3,8 +3,6 @@ import { inject as service } from "@ember/service";
 
 export default class ChatDrawerHeaderRightActions extends Component {
   @service chat;
-  @service router;
-  @service chatStateManager;
 
   get showThreadsListButton() {
     return this.chat.activeChannel?.threadingEnabled;
