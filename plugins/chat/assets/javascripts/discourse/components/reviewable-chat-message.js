fd036af91cd7e4905780d582acb9bb3b0d99c014
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-05-29 09:46:27 +0200

    correctly init a chat message object

--- plugins/chat/assets/javascripts/discourse/components/reviewable-chat-message.js
+++ plugins/chat/assets/javascripts/discourse/components/reviewable-chat-message.js
@@ -1,15 +1,14 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { cached } from "@glimmer/tracking";
+import ChatChannel from "discourse/plugins/chat/discourse/models/chat-channel";
 
 export default class ReviewableChatMessage extends Component {
   @service store;
+  @service chatChannelsManager;
 
   @cached
   get chatChannel() {
-    return this.store.createRecord(
-      "chat-channel",
-      this.args.reviewable.chat_channel
-    );
+    return ChatChannel.create(this.args.reviewable.chat_channel);
   }
 }
