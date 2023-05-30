dd60cc986f87c45ae331a3315e772f2560963cf4
Jarek Radosz <jradosz@gmail.com>
2023-05-30 23:30:16 +0200

    More details in the test

--- app/assets/javascripts/discourse/tests/acceptance/user-deleted-posts-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/user-deleted-posts-test.js
@@ -1,6 +1,7 @@
 import { acceptance } from "../helpers/qunit-helpers";
 import { test } from "qunit";
 import postFixtures from "discourse/tests/fixtures/post";
+import { cloneJSON } from "discourse-common/lib/object";
 import { visit } from "@ember/test-helpers";
 
 acceptance("User's deleted posts page", function (needs) {
@@ -8,15 +9,21 @@ acceptance("User's deleted posts page", function (needs) {
 
   needs.pretender((server, helper) => {
     server.get(`/posts/eviltrout/deleted`, () => {
-      return helper.response([
-        postFixtures["/posts/398"],
-        postFixtures["/posts/98737532"],
-      ]);
+      const post1 = cloneJSON(postFixtures["/posts/398"]);
+      post1.excerpt = "Topic #1";
+      const post2 = cloneJSON(postFixtures["/posts/98737532"]);
+      post2.excerpt = "Another topic's text";
+
+      return helper.response([post1, post2]);
     });
   });
 
   test("Displays the posts", async function (assert) {
     await visit(`/u/eviltrout/deleted-posts`);
-    assert.dom(".user-stream-item").exists({ count: 2 });
+
+    assert.dom(".user-stream-item [data-topic-id='280']").hasText("Topic #1");
+    assert
+      .dom(".user-stream-item [data-topic-id='34']")
+      .hasText("Another topic's text");
   });
 });
