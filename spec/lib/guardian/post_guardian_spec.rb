2217c423ae2bae4afeb39725d5cde4cc881f24a4
Lhcfl <lhcfl@outlook.com>
2023-05-30 17:13:34 +0800

    DEV:Aallow expanding hidden posts focustom groups

--- spec/lib/guardian/post_guardian_spec.rb
+++ spec/lib/guardian/post_guardian_spec.rb
@@ -12,20 +12,7 @@ RSpec.describe PostGuardian do
   fab!(:hidden_post) { Fabricate(:post, topic: topic, hidden: true) }
 
   describe "#can_see_hidden_post?" do
-    it "returns false for anonymous users" do
-      expect(Guardian.new(anon).can_see_hidden_post?(hidden_post)).to eq(false)
-    end
-
-    it "returns false for TL3 users" do
-      expect(Guardian.new(tl3_user).can_see_hidden_post?(hidden_post)).to eq(false)
-    end
-
-    it "returns true for TL4 users" do
-      expect(Guardian.new(tl4_user).can_see_hidden_post?(hidden_post)).to eq(true)
-    end
-
-    it "returns true for staff users" do
-      expect(Guardian.new(moderator).can_see_hidden_post?(hidden_post)).to eq(true)
+    it "returns true for admin users" do
       expect(Guardian.new(admin).can_see_hidden_post?(hidden_post)).to eq(true)
     end
   end
