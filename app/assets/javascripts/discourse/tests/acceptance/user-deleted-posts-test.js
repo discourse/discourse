discourse/discourse

commit 48987af324c54b3305b12e302cdb4bdc7901c190

DEV: Use `templateName` instead of `renderTemplate()` (#21517)
`renderTemplate()` is deprecated and has been removed in Ember 4.0

app/assets/javascripts/discourse/app/routes/build-admin-user-posts-route.js
@@ -5,11 +5,7 @@ import { action } from "@ember/object";
 
 export default function (filter) {
   return DiscourseRoute.extend({
-    @action
-    didTransition() {
-      this.controllerFor("user-posts")._showFooter();
-      return true;
-    },
+    templateName: "user/posts",
 
     model() {
       return this.modelFor("user").get("postsStream");
@@ -32,8 +28,10 @@ export default function (filter) {
       this.controllerFor("user-posts").set("model", model);
     },
 
-    renderTemplate() {
-      this.render("user/posts");
+    @action
+    didTransition() {
+      this.controllerFor("user-posts")._showFooter();
+      return true;
     },
   });
 }

app/assets/javascripts/discourse/app/routes/exception-unknown.js
@@ -1,7 +1,5 @@
 import UnknownRoute from "discourse/routes/unknown";
 
 export default UnknownRoute.extend({
-  renderTemplate() {
-    this.render("unknown");
-  },
+  templateName: "unknown",
 });

app/assets/javascripts/discourse/app/routes/group-activity-posts.js
@@ -5,6 +5,8 @@ import { action, get } from "@ember/object";
 export function buildGroupPage(type) {
   return DiscourseRoute.extend({
     type,
+    templateName: "group-activity-posts",
+    controllerName: "group-activity-posts",
 
     titleToken() {
       return I18n.t(`groups.${type}`);
@@ -17,16 +19,12 @@ export function buildGroupPage(type) {
 
     setupController(controller, model) {
       let loadedAll = model.length < 20;
-      this.controllerFor("group-activity-posts").setProperties({
+      controller.setProperties({
         model,
         type,
         canLoadMore: !loadedAll,
       });
       this.controllerFor("application").set("showFooter", loadedAll);
-    },
-
-    renderTemplate() {
-      this.render("group-activity-posts");
     },
 
     @action

app/assets/javascripts/discourse/app/routes/user-activity-bookmarks.js
@@ -5,6 +5,8 @@ import { Promise } from "rsvp";
 import I18n from "I18n";
 
 export default DiscourseRoute.extend({
+  templateName: "user/bookmarks",
+
   queryParams: {
     acting_username: { refreshModel: true },
     q: { refreshModel: true },
@@ -45,10 +47,6 @@ export default DiscourseRoute.extend({
       })
       .catch(() => controller.set("permissionDenied", true))
       .finally(() => controller.set("loading", false));
-  },
-
-  renderTemplate() {
-    this.render("user_bookmarks");
   },
 
   titleToken() {

app/assets/javascripts/discourse/app/routes/user-activity-drafts.js
@@ -3,6 +3,8 @@ import I18n from "I18n";
 import { action } from "@ember/object";
 
 export default DiscourseRoute.extend({
+  templateName: "user/stream",
+
   model() {
     const user = this.modelFor("user");
     const draftsStream = user.get("userDraftsStream");
@@ -14,14 +16,6 @@ export default DiscourseRoute.extend({
         emptyState: this.emptyState(),
       };
     });
-  },
-
-  renderTemplate() {
-    this.render("user_stream");
-  },
-
-  setupController(controller, model) {
-    controller.set("model", model);
   },
 
   emptyState() {

app/assets/javascripts/discourse/app/routes/user-activity-stream.js
@@ -4,6 +4,8 @@ import { action } from "@ember/object";
 import I18n from "I18n";
 
 export default DiscourseRoute.extend(ViewingActionType, {
+  templateName: "user/stream",
+
   queryParams: {
     acting_username: { refreshModel: true },
   },
@@ -29,12 +31,8 @@ export default DiscourseRoute.extend(ViewingActionType, {
     });
   },
 
-  renderTemplate() {
-    this.render("user_stream");
-  },
-
-  setupController(controller, model) {
-    controller.set("model", model);
+  setupController() {
+    this._super(...arguments);
     this.viewingActionType(this.userActionType);
   },
 

app/assets/javascripts/discourse/app/routes/user-badges.js
@@ -5,6 +5,8 @@ import { action } from "@ember/object";
 import I18n from "I18n";
 
 export default DiscourseRoute.extend(ViewingActionType, {
+  templateName: "user/badges",
+
   model() {
     return UserBadge.findByUsername(
       this.modelFor("user").get("username_lower"),
@@ -12,13 +14,9 @@ export default DiscourseRoute.extend(ViewingActionType, {
     );
   },
 
-  setupController(controller, model) {
+  setupController() {
+    this._super(...arguments);
     this.viewingActionType(-1);
-    controller.set("model", model);
-  },
-
-  renderTemplate() {
-    this.render("user/badges");
   },
 
   titleToken() {

app/assets/javascripts/discourse/app/routes/user-notifications-index.js
@@ -3,9 +3,7 @@ import I18n from "I18n";
 
 export default DiscourseRoute.extend({
   controllerName: "user-notifications",
-  renderTemplate() {
-    this.render("user/notifications-index");
-  },
+  templateName: "user/notifications-index",
 
   titleToken() {
     return I18n.t("user.filters.all");

commit 1e3a5124da49d18b4b930ddf3ad1b615b511265a

FIX: `u/:username/deleted-posts` route regression (#21834)
Regressed in 48987af

app/assets/javascripts/discourse/app/routes/build-admin-user-posts-route.js
@@ -1,37 +1,0 @@
-import DiscourseRoute from "discourse/routes/discourse";
-import { emojiUnescape } from "discourse/lib/text";
-import { escapeExpression } from "discourse/lib/utilities";
-import { action } from "@ember/object";
-
-export default function (filter) {
-  return DiscourseRoute.extend({
-    templateName: "user/posts",
-
-    model() {
-      return this.modelFor("user").get("postsStream");
-    },
-
-    afterModel(model) {
-      return model.filterBy({ filter });
-    },
-
-    setupController(controller, model) {
-      // initialize "canLoadMore"
-      model.set("canLoadMore", model.get("itemsLoaded") === 60);
-
-      model.get("content").forEach((item) => {
-        if (item.get("title")) {
-          item.set("title", emojiUnescape(escapeExpression(item.title)));
-        }
-      });
-
-      this.controllerFor("user-posts").set("model", model);
-    },
-
-    @action
-    didTransition() {
-      this.controllerFor("user-posts")._showFooter();
-      return true;
-    },
-  });
-}

app/assets/javascripts/discourse/app/routes/user-deleted-posts.js
@@ -1,3 +1,35 @@
-import createAdminUserPostsRoute from "discourse/routes/build-admin-user-posts-route";
+import DiscourseRoute from "discourse/routes/discourse";
+import { emojiUnescape } from "discourse/lib/text";
+import { escapeExpression } from "discourse/lib/utilities";
+import { action } from "@ember/object";
 
-export default createAdminUserPostsRoute("deleted");
+export default class UserDeletedPosts extends DiscourseRoute {
+  templateName = "user/posts";
+  controllerName = "user-posts";
+
+  model() {
+    return this.modelFor("user").postsStream;
+  }
+
+  afterModel(model) {
+    return model.filterBy({ filter: "deleted" });
+  }
+
+  setupController(controller, model) {
+    super.setupController(...arguments);
+
+    model.set("canLoadMore", model.itemsLoaded === 60);
+
+    model.content.forEach((item) => {
+      if (item.title) {
+        item.set("title", emojiUnescape(escapeExpression(item.title)));
+      }
+    });
+  }
+
+  @action
+  didTransition() {
+    this.controller._showFooter();
+    return true;
+  }
+}

app/assets/javascripts/discourse/tests/acceptance/user-deleted-posts-test.js
@@ -1,0 +1,29 @@
+import { acceptance } from "../helpers/qunit-helpers";
+import { test } from "qunit";
+import postFixtures from "discourse/tests/fixtures/post";
+import { cloneJSON } from "discourse-common/lib/object";
+import { visit } from "@ember/test-helpers";
+
+acceptance("User's deleted posts page", function (needs) {
+  needs.user();
+
+  needs.pretender((server, helper) => {
+    server.get(`/posts/eviltrout/deleted`, () => {
+      const post1 = cloneJSON(postFixtures["/posts/398"]);
+      post1.excerpt = "Topic #1";
+      const post2 = cloneJSON(postFixtures["/posts/98737532"]);
+      post2.excerpt = "Another topic's text";
+
+      return helper.response([post1, post2]);
+    });
+  });
+
+  test("Displays the posts", async function (assert) {
+    await visit(`/u/eviltrout/deleted-posts`);
+
+    assert.dom(".user-stream-item [data-topic-id='280']").hasText("Topic #1");
+    assert
+      .dom(".user-stream-item [data-topic-id='34']")
+      .hasText("Another topic's text");
+  });
+});