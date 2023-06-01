45984c6b625afbb2ef9ee0b69d818a4ba0f7a60a
OsamaSayegh <asooomaasoooma90@gmail.com>
2023-06-01 04:55:05 +0300

    fix syntax_tree

--- lib/stylesheet/manager.rb
+++ lib/stylesheet/manager.rb
@@ -43,16 +43,7 @@ class Stylesheet::Manager
   end
 
   def self.precompile_css
-    targets = %i[
-      desktop 
-      mobile 
-      admin 
-      wizard 
-      desktop_rtl 
-      mobile_rtl 
-      admin_rtl 
-      wizard_rtl
-    ]
+    targets = %i[desktop mobile admin wizard desktop_rtl mobile_rtl admin_rtl wizard_rtl]
 
     targets +=
       Discourse.find_plugin_css_assets(
