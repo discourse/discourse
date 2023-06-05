3bfc6805ce0e096562c6b5ee52df26e93a412e94
Mark VanLandingham <markvanlan@gmail.com>
2023-06-05 11:08:04 -0500

    FEATURE: Offline indicator (#21369)

---
+++ spec/system/network_disconnected_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+RSpec.describe "Network Disconnected", type: :system, js: true do
+  fab!(:current_user) { Fabricate(:user) }
+
+  def with_network_disconnected
+    page.driver.browser.network_conditions = { offline: true }
+    yield
+    page.driver.browser.network_conditions = { offline: false }
+  end
+
+  it "Message bus connectivity service adds class to DOM and displays offline indicator" do
+    SiteSetting.enable_offline_indicator = true
+
+    visit("/c")
+
+    expect(page).to have_no_css("html.message-bus-offline")
+    expect(page).to have_no_css(".offline-indicator")
+
+    with_network_disconnected do
+      # Message bus connectivity services adds the disconnected class to the DOM
+      expect(page).to have_css("html.message-bus-offline")
+
+      # Offline indicator is rendered
+      expect(page).to have_css(".offline-indicator")
+    end
+  end
+
+  it "Doesn't show the offline indicator when the site setting isn't present" do
+    SiteSetting.enable_offline_indicator = false
+
+    visit("/c")
+
+    with_network_disconnected do
+      expect(page).to have_css("html.message-bus-offline")
+      expect(page).not_to have_css(".offline-indicator")
+    end
+  end
+end
