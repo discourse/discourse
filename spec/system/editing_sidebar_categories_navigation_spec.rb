f0c1395afc369885db44efa6c905dc6bb4b957e8
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-06-07 09:02:01 +0800

    UX: First pass at edit categories navigation modal for sidebar
    What this change?
    
    We are currently not fully satisfied with the current way to edit the
    categories and tags that appears in the sidebar where the user is
    redirected to the tracking preferences tab in the user's profile causing
    the user to lose context of the current page. In addition, the dropdown
    to select categories or tags limits the amount of information we can
    display.
    
    Since editing or adding a custom categories section is already using a
    modal, we have decided to switch editing the categories and tags that
    appear in the sidebar to use a modal as well.
    
    This commit ships a first pass of the edit categories modal such that we
    can keep the commit small and reviewable. The incomplete nature of the
    feature is also reflected in the fact that the feature is hidden behind
    a new `new_edit_sidebar_categories_tags_interface_groups` site setting.

---
+++ spec/system/editing_sidebar_categories_navigation_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+RSpec.describe "Editing sidebar categories navigation", type: :system do
+  fab!(:user) { Fabricate(:user) }
+  fab!(:group) { Fabricate(:group).tap { |g| g.add(user) } }
+  fab!(:category) { Fabricate(:category) }
+  fab!(:category_subcategory) { Fabricate(:category, parent_category_id: category.id) }
+  fab!(:category_subcategory2) { Fabricate(:category, parent_category_id: category.id) }
+  fab!(:category2) { Fabricate(:category) }
+  fab!(:category2_subcategory) { Fabricate(:category, parent_category_id: category2.id) }
+
+  let(:sidebar) { PageObjects::Components::Sidebar.new }
+
+  before do
+    SiteSetting.new_edit_sidebar_categories_tags_interface_groups = group.name
+    SiteSetting.default_sidebar_categories = "#{category.id}|#{category2.id}"
+    sign_in(user)
+  end
+
+  it "allows a user to edit the sidebar categories navigation" do
+    visit "/latest"
+
+    expect(sidebar).to have_categories_section
+
+    modal = sidebar.click_edit_categories_button
+
+    expect(modal).to have_right_title(I18n.t("js.sidebar.categories_form.title"))
+
+    modal
+      .toggle_category_checkbox(category)
+      .toggle_category_checkbox(category_subcategory2)
+      .toggle_category_checkbox(category2)
+      .save
+
+    expect(modal).to be_closed
+    expect(sidebar).to have_section_link(category.name)
+    expect(sidebar).to have_section_link(category_subcategory2.name)
+    expect(sidebar).to have_section_link(category2.name)
+
+    visit "/latest"
+
+    expect(sidebar).to have_categories_section
+    expect(sidebar).to have_section_link(category.name)
+    expect(sidebar).to have_section_link(category_subcategory2.name)
+    expect(sidebar).to have_section_link(category2.name)
+
+    modal = sidebar.click_edit_categories_button
+    modal.toggle_category_checkbox(category_subcategory2).toggle_category_checkbox(category2).save
+
+    expect(modal).to be_closed
+
+    expect(sidebar).to have_section_link(category.name)
+    expect(sidebar).to have_no_section_link(category_subcategory2.name)
+    expect(sidebar).to have_no_section_link(category2.name)
+  end
+end
