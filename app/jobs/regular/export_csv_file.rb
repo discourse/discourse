80c4d5dc91cdbc1c0b4af7335776335c2129d6ca
Juan David Martinez <juan@discourse.org>
2023-06-08 11:09:05 -0500

    PERF: Increase performance of large csv exports for User Histories

--- app/jobs/regular/export_csv_file.rb
+++ app/jobs/regular/export_csv_file.rb
@@ -92,7 +92,6 @@ module Jobs
 
       # ensure directory exists
       FileUtils.mkdir_p(dirname) unless Dir.exist?(dirname)
-
       # Generate a compressed CSV file
       begin
         entities.each do |entity|
@@ -177,7 +176,7 @@ module Jobs
           UserHistory.where(admin_only: false).only_staff_actions.order("id DESC")
         end
 
-      staff_action_data.each { |staff_action| yield get_staff_action_fields(staff_action) }
+      staff_action_data.find_each { |staff_action| yield get_staff_action_fields(staff_action) }
     end
 
     def screened_email_export
