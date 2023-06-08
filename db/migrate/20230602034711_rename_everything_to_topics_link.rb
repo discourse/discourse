59792410a31632435e51b10f350269b8ca1136c6
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-06-08 13:20:29 +1000

    FIX: check name as well

--- db/migrate/20230602034711_rename_everything_to_topics_link.rb
+++ db/migrate/20230602034711_rename_everything_to_topics_link.rb
@@ -7,7 +7,7 @@ class RenameEverythingToTopicsLink < ActiveRecord::Migration[7.0]
       FROM sidebar_urls su2
       INNER JOIN sidebar_section_links ON sidebar_section_links.linkable_id = su2.id
       INNER JOIN sidebar_sections ON sidebar_sections.id = sidebar_section_links.sidebar_section_id AND sidebar_sections.section_type = 0
-      WHERE su1.id = su2.id AND su2.value = '/latest'
+      WHERE su1.id = su2.id AND su2.value = '/latest' AND su2.name = 'Everything'
     SQL
   end
 
@@ -18,7 +18,7 @@ class RenameEverythingToTopicsLink < ActiveRecord::Migration[7.0]
       FROM sidebar_urls su2
       INNER JOIN sidebar_section_links ON sidebar_section_links.linkable_id = su2.id
       INNER JOIN sidebar_sections ON sidebar_sections.id = sidebar_section_links.sidebar_section_id AND sidebar_sections.section_type = 0
-      WHERE su1.id = su2.id AND su2.value = '/topics'
+      WHERE su1.id = su2.id AND su2.value = '/topics' AND su2.name = 'Topics'
     SQL
   end
 end
