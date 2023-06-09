99f0ace2292e108ade3fc2aaa73c65c4eb12d5f2
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-06-09 18:40:34 +0200

    DEV: implements styleguide for chat header icon (#22033)
    This commit also uses this opportunity to change component name from: `ChatHeaderIcon` to `Chat::Header::Icon`

--- plugins/chat/assets/javascripts/discourse/components/chat/header/icon/unread-indicator.js
+++ plugins/chat/assets/javascripts/discourse/components/chat/header/icon/unread-indicator.js
@@ -4,15 +4,36 @@ import {
   HEADER_INDICATOR_PREFERENCE_ALL_NEW,
   HEADER_INDICATOR_PREFERENCE_DM_AND_MENTIONS,
   HEADER_INDICATOR_PREFERENCE_NEVER,
-} from "../controllers/preferences-chat";
+} from "discourse/plugins/chat/discourse/controllers/preferences-chat";
 
 export default class ChatHeaderIconUnreadIndicator extends Component {
   @service chatTrackingStateManager;
   @service currentUser;
 
+  get urgentCount() {
+    return (
+      this.args.urgentCount ||
+      this.chatTrackingStateManager.allChannelUrgentCount
+    );
+  }
+
+  get unreadCount() {
+    return (
+      this.args.unreadCount ||
+      this.chatTrackingStateManager.publicChannelUnreadCount
+    );
+  }
+
+  get indicatorPreference() {
+    return (
+      this.args.indicatorPreference ||
+      this.currentUser.user_option.chat_header_indicator_preference
+    );
+  }
+
   get showUrgentIndicator() {
     return (
-      this.chatTrackingStateManager.allChannelUrgentCount > 0 &&
+      this.urgentCount > 0 &&
       this.#hasAnyIndicatorPreference([
         HEADER_INDICATOR_PREFERENCE_ALL_NEW,
         HEADER_INDICATOR_PREFERENCE_DM_AND_MENTIONS,
@@ -22,13 +43,13 @@ export default class ChatHeaderIconUnreadIndicator extends Component {
 
   get showUnreadIndicator() {
     return (
-      this.chatTrackingStateManager.publicChannelUnreadCount > 0 &&
+      this.unreadCount > 0 &&
       this.#hasAnyIndicatorPreference([HEADER_INDICATOR_PREFERENCE_ALL_NEW])
     );
   }
 
-  get indicatorPreference() {
-    return this.currentUser.user_option.chat_header_indicator_preference;
+  get unreadCountLabel() {
+    return this.urgentCount > 99 ? "99+" : this.urgentCount;
   }
 
   #hasAnyIndicatorPreference(preferences) {
@@ -40,11 +61,5 @@ export default class ChatHeaderIconUnreadIndicator extends Component {
     }
 
     return preferences.includes(this.indicatorPreference);
-  }
-
-  get unreadCountLabel() {
-    return this.chatTrackingStateManager.allChannelUrgentCount > 99
-      ? "99+"
-      : this.chatTrackingStateManager.allChannelUrgentCount;
   }
 }
