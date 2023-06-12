c65eafbee1ea10dffc3c4ae6c287fe17ffa914c8
chapoi <charlie@discourse.org>
2023-06-12 14:27:55 +0700

    UX: chat message thread indicator mobile styling

--- plugins/chat/assets/javascripts/discourse/components/chat-message-thread-indicator.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-message-thread-indicator.js
@@ -1,7 +1,9 @@
 import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
 import { escapeExpression } from "discourse/lib/utilities";
 
 export default class ChatMessageThreadIndicator extends Component {
+  @service site;
   get threadTitle() {
     return escapeExpression(this.args.message.threadTitle);
   }
