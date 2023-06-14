777f74604c4c8346f571bcdc89731e940f8ed695
Martin Brennan <martin@discourse.org>
2023-06-14 10:14:28 +1000

    FIX: Use enclosing span for username in indicator
    Also add chatStateManager so we can do different things
    to the indicator based on whether we are in the drawer.

--- plugins/chat/assets/javascripts/discourse/components/chat-message-thread-indicator.js
+++ plugins/chat/assets/javascripts/discourse/components/chat-message-thread-indicator.js
@@ -4,6 +4,7 @@ import { escapeExpression } from "discourse/lib/utilities";
 
 export default class ChatMessageThreadIndicator extends Component {
   @service site;
+  @service chatStateManager;
 
   get threadTitle() {
     return escapeExpression(this.args.message.threadTitle);
