cf7246c5489cff8c106f0d22703f97e1683e94c5
Jarek Radosz <jradosz@gmail.com>
2023-06-14 12:42:00 +0200

    DEV: Remove renderTemplate from 2fa/email

--- app/assets/javascripts/discourse/app/models/user.js
+++ app/assets/javascripts/discourse/app/models/user.js
@@ -423,6 +423,8 @@ const User = RestModel.extend({
     return ajax(userPath(`${this.username_lower}/preferences/email`), {
       type: "POST",
       data: { email },
+    }).then(() => {
+      this.unconfirmed_emails.pushObject(email);
     });
   },
 
@@ -430,6 +432,8 @@ const User = RestModel.extend({
     return ajax(userPath(`${this.username_lower}/preferences/email`), {
       type: "PUT",
       data: { email },
+    }).then(() => {
+      this.unconfirmed_emails.pushObject(email);
     });
   },
 
