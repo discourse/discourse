4b9f1e25295e46608439491947637938b3cf9c47
Martin Brennan <martin@discourse.org>
2023-06-16 15:17:10 +1000

    DEV: Refactor DM channel creation into new service pattern
    This will be used when we move the channel creation for DMs
    to happen when we first send a message in a DM channel to avoid
    a double-request. For now we can just have a new API endpoint
    for creating this that the existing frontend code can use,
    that uses the new service pattern.
    
    This also uses the new policy pattern for services where the policy
    can be defined in a class so a more dynamic reason for the policy
    failing can be sent to the controller.

---
+++ plugins/chat/app/controllers/chat/api/direct_messages_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# TODO (martin) Remove this endpoint when we move to do the channel creation
+# when a message is first sent to avoid double-request round trips for DMs.
+class Chat::Api::DirectMessagesController < Chat::ApiController
+  def create
+    with_service(Chat::CreateDirectMessageChannel) do
+      on_success do
+        render_serialized(
+          result.channel,
+          Chat::ChannelSerializer,
+          root: "channel",
+          membership: result.membership,
+        )
+      end
+      on_model_not_found(:target_users) { raise ActiveRecord::RecordNotFound }
+      on_failed_policy(:does_not_exceed_max_direct_message_users) do |policy|
+        raise Discourse::InvalidParameters.new(:target_usernames, policy.reason)
+      end
+      on_failed_policy(:acting_user_not_disallowing_all_messages) do
+        render_json_error(I18n.t("chat.errors.actor_disallowed_dms"))
+      end
+      on_failed_policy(:acting_user_can_communicate_with_all_parties) do |policy|
+        render_json_error(policy.reason)
+      end
+      on_model_errors(:direct_message) do |model|
+        render_json_error(model, type: :record_invalid, status: 422)
+      end
+      on_model_errors(:channel) do |model|
+        render_json_error(model, type: :record_invalid, status: 422)
+      end
+    end
+  end
+end
---
+++ plugins/chat/app/services/chat/create_direct_message_channel.rb
@@ -0,0 +1,129 @@
+# frozen_string_literal: true
+
+module Chat
+  # Service responsible for creating a new direct message chat channel.
+  # The guardian passed in is the "acting user" when creating the channel
+  # and deciding whether the actor can communicate with the users that
+  # are passed in.
+  #
+  # @example
+  #  Service::Chat::CreateDirectMessageChannel.call(
+  #    guardian: guardian,
+  #    target_usernames: ["bob", "alice"]
+  #  )
+  #
+  class CreateDirectMessageChannel
+    include Service::Base
+
+    # @!method call(guardian:, **params_to_create)
+    #   @param [Guardian] guardian
+    #   @param [Hash] params_to_create
+    #   @option params_to_create [Array<String>] target_usernames
+    #   @return [Service::Base::Context]
+
+    policy :can_create_direct_message
+    contract
+    model :target_users
+    policy :does_not_exceed_max_direct_message_users,
+           class_name: Chat::DirectMessageChannel::MaxUsersExcessPolicy
+    model :user_comm_screener
+    policy :acting_user_not_disallowing_all_messages
+    policy :acting_user_can_communicate_with_all_parties,
+           class_name: Chat::DirectMessageChannel::CanCommunicateAllPartiesPolicy
+    model :direct_message, :fetch_or_create_direct_message
+    model :channel, :fetch_or_create_channel
+    step :update_memberships
+    step :publish_channel
+
+    # @!visibility private
+    class Contract
+      attribute :target_usernames
+
+      before_validation do
+        self.target_usernames =
+          (
+            if target_usernames.is_a?(String)
+              target_usernames.split(",")
+            else
+              target_usernames
+            end
+          )
+      end
+
+      validates :target_usernames, presence: true, length: { minimum: 1 }
+    end
+
+    private
+
+    def can_create_direct_message(guardian:, **)
+      guardian.can_create_direct_message?
+    end
+
+    def fetch_target_users(guardian:, contract:, **)
+      users = [guardian.user]
+      other_usernames = contract.target_usernames - [guardian.user.username]
+      users.concat(User.where(username: other_usernames).to_a) if other_usernames.any?
+      users.uniq
+    end
+
+    def fetch_user_comm_screener(target_users:, guardian:, **)
+      UserCommScreener.new(acting_user: guardian.user, target_user_ids: target_users.map(&:id))
+    end
+
+    def acting_user_not_disallowing_all_messages(user_comm_screener:, **)
+      !user_comm_screener.actor_disallowing_all_pms?
+    end
+
+    def fetch_or_create_direct_message(target_users:, **)
+      direct_message = Chat::DirectMessage.for_user_ids(target_users.map(&:id))
+      return direct_message if direct_message.present?
+      Chat::DirectMessage.create(user_ids: target_users.map(&:id))
+    end
+
+    def fetch_or_create_channel(direct_message:, **)
+      channel = Chat::Channel.find_by(chatable: direct_message)
+      channel.present? ? channel : direct_message.create_chat_channel
+    end
+
+    def update_memberships(guardian:, channel:, target_users:, **)
+      sql_params = {
+        acting_user_id: guardian.user.id,
+        user_ids: target_users.map(&:id),
+        chat_channel_id: channel.id,
+        always_notification_level: Chat::UserChatChannelMembership::NOTIFICATION_LEVELS[:always],
+      }
+
+      DB.exec(<<~SQL, sql_params)
+        INSERT INTO user_chat_channel_memberships(
+          user_id,
+          chat_channel_id,
+          muted,
+          following,
+          desktop_notification_level,
+          mobile_notification_level,
+          created_at,
+          updated_at
+        )
+        VALUES(
+          unnest(array[:user_ids]),
+          :chat_channel_id,
+          false,
+          false,
+          :always_notification_level,
+          :always_notification_level,
+          NOW(),
+          NOW()
+        )
+        ON CONFLICT (user_id, chat_channel_id) DO NOTHING;
+
+        UPDATE user_chat_channel_memberships
+        SET following = true
+        WHERE user_id = :acting_user_id AND chat_channel_id = :chat_channel_id;
+      SQL
+    end
+
+    def publish_channel(channel:, target_users:, **)
+      Chat::Publisher.publish_new_channel(channel, target_users)
+    end
+  end
+end
