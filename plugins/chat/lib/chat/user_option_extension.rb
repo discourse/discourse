fd38f921f876d2a065b209c7c1cad1df898a3f1f
David Taylor <david@taylorhq.com>
2023-06-16 12:31:17 +0100

    DEV: Namespace chat_header_indicator UserOption enum
    Previously this was defining methods like `UserOption#never?`, `UserOption#all_new?`, `UserOption#dm_and_mentions?`. Now they will be prefixed like `UserOption#chat_header_indicator_never?`

--- plugins/chat/lib/chat/user_option_extension.rb
+++ plugins/chat/lib/chat/user_option_extension.rb
@@ -22,8 +22,10 @@ module Chat
         base.enum :chat_email_frequency, base.chat_email_frequencies, prefix: "send_chat_email"
       end
 
-      if !base.method_defined?(:never?) # Avoid attempting to override when autoloading
-        base.enum :chat_header_indicator_preference, base.chat_header_indicator_preferences
+      if !base.method_defined?(:chat_header_indicator_never?) # Avoid attempting to override when autoloading
+        base.enum :chat_header_indicator_preference,
+                  base.chat_header_indicator_preferences,
+                  prefix: "chat_header_indicator"
       end
     end
   end
