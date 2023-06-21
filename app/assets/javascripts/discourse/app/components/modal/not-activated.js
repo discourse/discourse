7f47e9ae9f3ab851d2d686251e787b1ceeb5cb02
Isaac Janzen <isaac.janzen@discourse.org>
2023-06-21 12:51:48 -0500

    update

--- app/assets/javascripts/discourse/app/components/modal/not-activated.js
+++ app/assets/javascripts/discourse/app/components/modal/not-activated.js
@@ -1,4 +1,4 @@
-import Component from "@ember/component";
+import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { resendActivationEmail } from "discourse/lib/user-activation";
