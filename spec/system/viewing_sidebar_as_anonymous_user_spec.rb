discourse/discourse

commit 9fad71809c254752479b0faf4f23ba197e742e60

UX: Improve defaults shown for categories and tags section in sidebar (#22062)
Why is this change required?

When a site is newly setup and a user has just been created, the
categories and tags sections are hidden from the user. This happens
because the admin has not configured the `default_navigation_menu_categories` or
`default_navigation_menu_tags` site settings. When the categories and tags
sections are hidden from the user, the sidebar looks extremely bare and
does not create a good experience.

What is being change?

In this commit, we're changing the logic such that the site's top
categories and tags are displayed if the user does not have any
categories/tags configured in each respective section. The only
regression introduced in this change is that the categories and tags
section can no longer be hidden as a result. However, we have plans to
address this in the future by allowing sidebar sections to be configured
to be hidden by each individual user.

app/assets/javascripts/discourse/tests/acceptance/sidebar-anonymous-tags-section-test.js
@@ -22,10 +22,10 @@ acceptance("Sidebar - Anonymous Tags Section", function (needs) {
     await visit("/");
 
     const categories = queryAll(
-      ".sidebar-section[data-section-name='tags'] .sidebar-section-link-wrapper"
+      ".sidebar-section[data-section-name='tags'] .sidebar-section-link-wrapper[data-tag-name]"
     );
 
-    assert.strictEqual(categories.length, 4);
+    assert.strictEqual(categories.length, 3);
     assert.strictEqual(categories[0].textContent.trim(), "design");
     assert.strictEqual(categories[1].textContent.trim(), "development");
     assert.strictEqual(categories[2].textContent.trim(), "fun");

app/assets/javascripts/discourse/app/components/sidebar/anonymous/categories-section.js
@@ -1,4 +1,3 @@
-import { canDisplayCategory } from "discourse/lib/sidebar/helpers";
 import SidebarCommonCategoriesSection from "discourse/components/sidebar/common/categories-section";
 import Category from "discourse/models/category";
 
@@ -19,14 +18,7 @@ export default class SidebarAnonymousCategoriesSection extends SidebarCommonCate
           .map((categoryId) => parseInt(categoryId, 10))
       );
     } else {
-      return this.site.categoriesList
-        .filter((category) => {
-          return (
-            !category.parent_category_id &&
-            canDisplayCategory(category.id, this.siteSettings)
-          );
-        })
-        .slice(0, 5);
+      return this.topSiteCategories;
     }
   }
 }

app/assets/javascripts/discourse/app/components/sidebar/anonymous/tags-section.js
@@ -1,9 +1,10 @@
 import { cached } from "@glimmer/tracking";
-import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
+
+import SidebarCommonTagsSection from "discourse/components/sidebar/common/tags-section";
 import TagSectionLink from "discourse/lib/sidebar/user/tags-section/tag-section-link";
 
-export default class SidebarAnonymousTagsSection extends Component {
+export default class SidebarAnonymousTagsSection extends SidebarCommonTagsSection {
   @service router;
   @service topicTrackingState;
   @service site;
@@ -11,20 +12,15 @@ export default class SidebarAnonymousTagsSection extends Component {
   get displaySection() {
     return (
       this.site.anonymous_default_navigation_menu_tags?.length > 0 ||
-      this.site.top_tags?.length > 0
+      this.topSiteTags?.length > 0
     );
   }
 
   @cached
   get sectionLinks() {
-    let tags;
-
-    if (this.site.anonymous_default_navigation_menu_tags) {
-      tags = this.site.anonymous_default_navigation_menu_tags;
-    } else {
-      tags = this.site.top_tags.slice(0, 5);
-    }
-    return tags.map((tagName) => {
+    return (
+      this.site.anonymous_default_navigation_menu_tags || this.topSiteTags
+    ).map((tagName) => {
       return new TagSectionLink({
         tagName,
         topicTrackingState: this.topicTrackingState,

app/assets/javascripts/discourse/app/components/sidebar/common/categories-section.js
@@ -6,6 +6,8 @@ import Category from "discourse/models/category";
 import CategorySectionLink from "discourse/lib/sidebar/user/categories-section/category-section-link";
 import { canDisplayCategory } from "discourse/lib/sidebar/helpers";
 
+export const TOP_SITE_CATEGORIES_TO_SHOW = 5;
+
 export default class SidebarCommonCategoriesSection extends Component {
   @service topicTrackingState;
   @service siteSettings;
@@ -19,6 +21,17 @@ export default class SidebarCommonCategoriesSection extends Component {
    * @returns {Object[]} An array of Category objects
    */
   get categories() {}
+
+  get topSiteCategories() {
+    return this.site.categoriesList
+      .filter((category) => {
+        return (
+          !category.parent_category_id &&
+          canDisplayCategory(category.id, this.siteSettings)
+        );
+      })
+      .slice(0, TOP_SITE_CATEGORIES_TO_SHOW);
+  }
 
   get sortedCategories() {
     if (!this.shouldSortCategoriesByDefault) {

app/assets/javascripts/discourse/app/components/sidebar/common/tags-section.js
@@ -1,0 +1,20 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+
+export const TOP_SITE_TAGS_TO_SHOW = 5;
+
+export default class SidebarCommonTagsSection extends Component {
+  @service site;
+
+  topSiteTags = [];
+
+  constructor() {
+    super(...arguments);
+
+    if (this.site.top_tags?.length > 0) {
+      this.site.top_tags.splice(0, TOP_SITE_TAGS_TO_SHOW).forEach((tagName) => {
+        this.topSiteTags.push(tagName);
+      });
+    }
+  }
+}

app/assets/javascripts/discourse/app/components/sidebar/user/categories-section.hbs
@@ -1,81 +1,49 @@
-{{#if (or this.shouldDisplay this.shouldDisplayDefaultConfig)}}
-  <Sidebar::Section
-    @sectionName="categories"
-    @headerLinkText={{i18n "sidebar.sections.categories.header_link_text"}}
-    @headerActions={{array
-      (hash
-        action=this.editTracked
-        title=(i18n "sidebar.sections.categories.header_action_title")
-      )
-    }}
-    @headerActionsIcon="pencil-alt"
-    @collapsable={{@collapsable}}
-  >
+<Sidebar::Section
+  @sectionName="categories"
+  @headerLinkText={{i18n "sidebar.sections.categories.header_link_text"}}
+  @headerActions={{array
+    (hash
+      action=this.editTracked
+      title=(i18n "sidebar.sections.categories.header_action_title")
+    )
+  }}
+  @headerActionsIcon="pencil-alt"
+  @collapsable={{@collapsable}}
+>
 
-    {{#if this.shouldDisplay}}
-      {{#if (gt this.sectionLinks.length 0)}}
-        {{#each this.sectionLinks as |sectionLink|}}
-          <Sidebar::SectionLink
-            @route={{sectionLink.route}}
-            @query={{sectionLink.query}}
-            @title={{sectionLink.title}}
-            @content={{sectionLink.text}}
-            @currentWhen={{sectionLink.currentWhen}}
-            @model={{sectionLink.model}}
-            @badgeText={{sectionLink.badgeText}}
-            @prefixBadge={{sectionLink.prefixBadge}}
-            @prefixType={{sectionLink.prefixType}}
-            @prefixValue={{sectionLink.prefixValue}}
-            @prefixColor={{sectionLink.prefixColor}}
-            @suffixCSSClass={{sectionLink.suffixCSSClass}}
-            @suffixValue={{sectionLink.suffixValue}}
-            @suffixType={{sectionLink.suffixType}}
-            data-category-id={{sectionLink.category.id}}
-          />
-        {{/each}}
-      {{else if this.currentUser.new_edit_sidebar_categories_tags_interface_groups_enabled}}
-        <Sidebar::SectionLink
-          @linkName="configure-categories"
-          @prefixType="icon"
-          @prefixValue="pencil-alt"
-          @model={{this.currentUser}}
-          @content={{i18n
-            "sidebar.sections.categories.links.add_categories.content"
-          }}
-          @title={{i18n
-            "sidebar.sections.categories.links.add_categories.title"
-          }}
-          {{on "click" this.editTracked}}
-        />
-      {{else}}
-        <Sidebar::SectionLink
-          @linkName="configure-categories"
-          @route="preferences.navigation-menu"
-          @prefixType="icon"
-          @prefixValue="pencil-alt"
-          @model={{this.currentUser}}
-          @content={{i18n
-            "sidebar.sections.categories.links.add_categories.content"
-          }}
-          @title={{i18n
-            "sidebar.sections.categories.links.add_categories.title"
-          }}
-        />
-      {{/if}}
-
-      <Sidebar::Common::AllCategoriesSectionLink />
-    {{/if}}
-
-    {{#if this.shouldDisplayDefaultConfig}}
+  {{#if (gt this.sectionLinks.length 0)}}
+    {{#each this.sectionLinks as |sectionLink|}}
       <Sidebar::SectionLink
-        @linkName="configure-default-navigation-menu-categories"
-        @content={{i18n "sidebar.sections.categories.configure_defaults"}}
-        @prefixType="icon"
-        @prefixValue="wrench"
-        @route="adminSiteSettingsCategory"
-        @model="sidebar"
-        @query={{hash filter="default_navigation_menu_categories"}}
+        @route={{sectionLink.route}}
+        @query={{sectionLink.query}}
+        @title={{sectionLink.title}}
+        @content={{sectionLink.text}}
+        @currentWhen={{sectionLink.currentWhen}}
+        @model={{sectionLink.model}}
+        @badgeText={{sectionLink.badgeText}}
+        @prefixBadge={{sectionLink.prefixBadge}}
+        @prefixType={{sectionLink.prefixType}}
+        @prefixValue={{sectionLink.prefixValue}}
+        @prefixColor={{sectionLink.prefixColor}}
+        @suffixCSSClass={{sectionLink.suffixCSSClass}}
+        @suffixValue={{sectionLink.suffixValue}}
+        @suffixType={{sectionLink.suffixType}}
+        data-category-id={{sectionLink.category.id}}
       />
-    {{/if}}
-  </Sidebar::Section>
-{{/if}}
+    {{/each}}
+  {{/if}}
+
+  <Sidebar::Common::AllCategoriesSectionLink />
+
+  {{#if this.shouldDisplayDefaultConfig}}
+    <Sidebar::SectionLink
+      @linkName="configure-default-navigation-menu-categories"
+      @content={{i18n "sidebar.sections.categories.configure_defaults"}}
+      @prefixType="icon"
+      @prefixValue="wrench"
+      @route="adminSiteSettingsCategory"
+      @model="sidebar"
+      @query={{hash filter="default_navigation_menu_categories"}}
+    />
+  {{/if}}
+</Sidebar::Section>

app/assets/javascripts/discourse/app/components/sidebar/user/categories-section.js
@@ -6,6 +6,7 @@ import { debounce } from "discourse-common/utils/decorators";
 import Category from "discourse/models/category";
 import SidebarCommonCategoriesSection from "discourse/components/sidebar/common/categories-section";
 import showModal from "discourse/lib/show-modal";
+import { hasDefaultSidebarCategories } from "discourse/lib/sidebar/helpers";
 
 export const REFRESH_COUNTS_APP_EVENT_NAME =
   "sidebar:refresh-categories-section-counts";
@@ -48,20 +49,10 @@ export default class SidebarUserCategoriesSection extends SidebarCommonCategorie
 
   @cached
   get categories() {
-    return Category.findByIds(this.currentUser.sidebarCategoryIds);
-  }
-
-  /**
-   * If a site has no default sidebar categories configured, show categories section if the user has categories configured.
-   * Otherwise, hide the categories section from the sidebar for the user.
-   *
-   * If a site has default sidebar categories configured, always show categories section for the user.
-   */
-  get shouldDisplay() {
-    if (this.hasDefaultSidebarCategories) {
-      return true;
+    if (this.currentUser.sidebarCategoryIds?.length > 0) {
+      return Category.findByIds(this.currentUser.sidebarCategoryIds);
     } else {
-      return this.categories.length > 0;
+      return this.topSiteCategories;
     }
   }
 
@@ -70,7 +61,7 @@ export default class SidebarUserCategoriesSection extends SidebarCommonCategorie
   }
 
   get hasDefaultSidebarCategories() {
-    return this.siteSettings.default_navigation_menu_categories.length > 0;
+    return hasDefaultSidebarCategories(this.siteSettings);
   }
 
   @action

app/assets/javascripts/discourse/app/components/sidebar/user/tags-section.hbs
@@ -1,61 +1,47 @@
-{{#if (or this.shouldDisplay this.shouldDisplayDefaultConfig)}}
-  <Sidebar::Section
-    @sectionName="tags"
-    @headerLinkText={{i18n "sidebar.sections.tags.header_link_text"}}
-    @headerActions={{array
-      (hash
-        action=this.editTracked
-        title=(i18n "sidebar.sections.tags.header_action_title")
-      )
-    }}
-    @headerActionsIcon="pencil-alt"
-    @collapsable={{@collapsable}}
-  >
+<Sidebar::Section
+  @sectionName="tags"
+  @headerLinkText={{i18n "sidebar.sections.tags.header_link_text"}}
+  @headerActions={{array
+    (hash
+      action=this.editTracked
+      title=(i18n "sidebar.sections.tags.header_action_title")
+    )
+  }}
+  @headerActionsIcon="pencil-alt"
+  @collapsable={{@collapsable}}
+>
 
-    {{#if this.shouldDisplay}}
-      {{#if (gt this.sectionLinks.length 0)}}
-        {{#each this.sectionLinks as |sectionLink|}}
-          <Sidebar::SectionLink
-            @route={{sectionLink.route}}
-            @title={{sectionLink.title}}
-            @content={{sectionLink.text}}
-            @currentWhen={{sectionLink.currentWhen}}
-            @prefixType={{sectionLink.prefixType}}
-            @prefixValue={{sectionLink.prefixValue}}
-            @prefixColor={{sectionLink.prefixColor}}
-            @badgeText={{sectionLink.badgeText}}
-            @models={{sectionLink.models}}
-            @suffixCSSClass={{sectionLink.suffixCSSClass}}
-            @suffixValue={{sectionLink.suffixValue}}
-            @suffixType={{sectionLink.suffixType}}
-            data-tag-name={{sectionLink.tagName}}
-          />
-        {{/each}}
-      {{else}}
-        <Sidebar::SectionLink
-          @linkName="configure-tags"
-          @route="preferences.navigation-menu"
-          @prefixType="icon"
-          @prefixValue="pencil-alt"
-          @model={{this.currentUser}}
-          @content={{i18n "sidebar.sections.tags.links.add_tags.content"}}
-          @title={{i18n "sidebar.sections.tags.links.add_tags.title"}}
-        />
-      {{/if}}
-
-      <Sidebar::Common::AllTagsSectionLink />
-    {{/if}}
-
-    {{#if this.shouldDisplayDefaultConfig}}
+  {{#if (gt this.sectionLinks.length 0)}}
+    {{#each this.sectionLinks as |sectionLink|}}
       <Sidebar::SectionLink
-        @linkName="configure-default-sidebar-tags"
-        @content={{i18n "sidebar.sections.tags.configure_defaults"}}
-        @prefixType="icon"
-        @prefixValue="wrench"
-        @route="adminSiteSettingsCategory"
-        @model="sidebar"
-        @query={{hash filter="default_navigation_menu_tags"}}
+        @route={{sectionLink.route}}
+        @title={{sectionLink.title}}
+        @content={{sectionLink.text}}
+        @currentWhen={{sectionLink.currentWhen}}
+        @prefixType={{sectionLink.prefixType}}
+        @prefixValue={{sectionLink.prefixValue}}
+        @prefixColor={{sectionLink.prefixColor}}
+        @badgeText={{sectionLink.badgeText}}
+        @models={{sectionLink.models}}
+        @suffixCSSClass={{sectionLink.suffixCSSClass}}
+        @suffixValue={{sectionLink.suffixValue}}
+        @suffixType={{sectionLink.suffixType}}
+        data-tag-name={{sectionLink.tagName}}
       />
-    {{/if}}
-  </Sidebar::Section>
-{{/if}}
+    {{/each}}
+  {{/if}}
+
+  <Sidebar::Common::AllTagsSectionLink />
+
+  {{#if this.shouldDisplayDefaultConfig}}
+    <Sidebar::SectionLink
+      @linkName="configure-default-navigation-menu-tags"
+      @content={{i18n "sidebar.sections.tags.configure_defaults"}}
+      @prefixType="icon"
+      @prefixValue="wrench"
+      @route="adminSiteSettingsCategory"
+      @model="sidebar"
+      @query={{hash filter="default_navigation_menu_tags"}}
+    />
+  {{/if}}
+</Sidebar::Section>

commit ae5f752c88497160d9270d5e209a96eefe989c05

FIX: Site's top tags not shown for anonymous user
When a site does not have `default_navigation_menu_tags`
site setting set, anonymous users should be shown the site's top tags as
a default in the tags section. However, this regressed in 9fad71809c254752479b0faf4f23ba197e742e60
and we ended up showing anonymous users a tags section with only the
`All Tags` section link.

As part of this commit, I have also refactored the QUnit acceptance
tests to system tests which are much easier to work with.

app/serializers/site_serializer.rb
@@ -251,12 +251,14 @@ class SiteSerializer < ApplicationSerializer
   end
 
   def anonymous_default_navigation_menu_tags
-    SiteSetting.default_navigation_menu_tags.split("|") - DiscourseTagging.hidden_tag_names(scope)
+    @anonymous_default_navigation_menu_tags ||=
+      SiteSetting.default_navigation_menu_tags.split("|") - DiscourseTagging.hidden_tag_names(scope)
   end
 
   def include_anonymous_default_navigation_menu_tags?
     scope.anonymous? && !SiteSetting.legacy_navigation_menu? && SiteSetting.tagging_enabled &&
-      SiteSetting.default_navigation_menu_tags.present?
+      SiteSetting.default_navigation_menu_tags.present? &&
+      anonymous_default_navigation_menu_tags.present?
   end
 
   def anonymous_sidebar_sections

app/assets/javascripts/discourse/tests/acceptance/sidebar-anonymous-tags-section-test.js
@@ -1,84 +1,0 @@
-import { test } from "qunit";
-import { visit } from "@ember/test-helpers";
-import {
-  acceptance,
-  exists,
-  queryAll,
-} from "discourse/tests/helpers/qunit-helpers";
-import Site from "discourse/models/site";
-
-acceptance("Sidebar - Anonymous Tags Section", function (needs) {
-  needs.settings({
-    navigation_menu: "sidebar",
-    suppress_uncategorized_badge: false,
-    tagging_enabled: true,
-  });
-
-  needs.site({
-    top_tags: ["design", "development", "fun"],
-  });
-
-  test("tag section links when site has top tags", async function (assert) {
-    await visit("/");
-
-    const categories = queryAll(
-      ".sidebar-section[data-section-name='tags'] .sidebar-section-link-wrapper[data-tag-name]"
-    );
-
-    assert.strictEqual(categories.length, 3);
-    assert.strictEqual(categories[0].textContent.trim(), "design");
-    assert.strictEqual(categories[1].textContent.trim(), "development");
-    assert.strictEqual(categories[2].textContent.trim(), "fun");
-
-    assert.ok(
-      exists("a.sidebar-section-link[data-link-name='all-tags']"),
-      "all tags link is visible"
-    );
-  });
-
-  test("tag section links when site has default sidebar tags configured", async function (assert) {
-    const site = Site.current();
-    site.set("anonymous_default_navigation_menu_tags", ["random", "meta"]);
-
-    await visit("/");
-
-    const categories = queryAll(
-      ".sidebar-section[data-section-name='tags'] .sidebar-section-link-wrapper"
-    );
-    assert.strictEqual(categories.length, 3);
-    assert.strictEqual(categories[0].textContent.trim(), "random");
-    assert.strictEqual(categories[1].textContent.trim(), "meta");
-
-    assert.ok(
-      exists("a.sidebar-section-link[data-link-name='all-tags']"),
-      "all tags link is visible"
-    );
-  });
-
-  test("tag section is hidden when tagging is disabled", async function (assert) {
-    this.siteSettings.tagging_enabled = false;
-
-    await visit("/");
-
-    assert.ok(
-      !exists(".sidebar-section[data-section-name='tags']"),
-      "section is not visible"
-    );
-  });
-
-  test("tag section is hidden when anonymous has no visible top tags and site has not default sidebar tags configured", async function (assert) {
-    const site = Site.current();
-
-    site.setProperties({
-      top_tags: [],
-      anonymous_default_navigation_menu_tags: [],
-    });
-
-    await visit("/");
-
-    assert.ok(
-      !exists(".sidebar-section[data-section-name='tags']"),
-      "section is not visible"
-    );
-  });
-});

spec/system/page_objects/components/sidebar.rb
@@ -83,6 +83,27 @@ module PageObjects
         has_section?("Tags")
       end
 
+      def has_no_tags_section?
+        has_no_section?("Tags")
+      end
+
+      def has_all_tags_section_link?
+        has_section_link?(I18n.t("js.sidebar.all_tags"))
+      end
+
+      def has_tags_section_links?(tags)
+        section_selector = ".sidebar-section[data-section-name='tags']"
+        tag_names = tags.map(&:name)
+
+        has_css?(
+          "#{section_selector} .sidebar-section-link-wrapper[data-tag-name]",
+          count: tag_names.length,
+        ) &&
+          all("#{section_selector} .sidebar-section-link-wrapper[data-tag-name]").all? do |row|
+            tag_names.include?(row["data-tag-name"].to_s)
+          end
+      end
+
       def has_no_section?(name)
         find(SIDEBAR_WRAPPER_SELECTOR).has_no_button?(name)
       end

spec/system/viewing_sidebar_as_anonymous_user_spec.rb
@@ -1,0 +1,59 @@
+# frozen_string_literal: true
+
+describe "Viewing sidebar as anonymous user", type: :system do
+  fab!(:tag1) { Fabricate(:tag).tap { |tag| Fabricate.times(1, :topic, tags: [tag]) } }
+  fab!(:tag2) { Fabricate(:tag).tap { |tag| Fabricate.times(2, :topic, tags: [tag]) } }
+  fab!(:tag3) { Fabricate(:tag).tap { |tag| Fabricate.times(3, :topic, tags: [tag]) } }
+  fab!(:tag4) { Fabricate(:tag).tap { |tag| Fabricate.times(2, :topic, tags: [tag]) } }
+  fab!(:tag5) { Fabricate(:tag).tap { |tag| Fabricate.times(2, :topic, tags: [tag]) } }
+  fab!(:tag6) { Fabricate(:tag).tap { |tag| Fabricate.times(1, :topic, tags: [tag]) } }
+
+  let(:sidebar) { PageObjects::Components::Sidebar.new }
+
+  describe "when viewing the tags section" do
+    it "should not display the tags section when tagging has been disabled" do
+      SiteSetting.tagging_enabled = false
+
+      visit("/latest")
+
+      expect(sidebar).to have_no_tags_section
+    end
+
+    it "should not display the tags section when site has no top tags and `default_navigation_menu_tags` site setting has not been set" do
+      Tag.delete_all
+
+      visit("/latest")
+
+      expect(sidebar).to have_no_tags_section
+    end
+
+    it "should display the site's top tags when `default_navigation_menu_tags` site setting has not been set" do
+      visit("/latest")
+
+      expect(sidebar).to have_tags_section
+      expect(sidebar).to have_all_tags_section_link
+      expect(sidebar).to have_tags_section_links([tag1, tag2, tag3, tag4, tag5])
+    end
+
+    it "should display the site's top tags when `default_navigation_menu_tags` site setting has been set but the tags configured are hidden to the user" do
+      SiteSetting.default_navigation_menu_tags = "#{tag5.name}"
+      Fabricate(:tag_group, permissions: { "staff" => 1 }, tag_names: [tag5.name])
+
+      visit("/latest")
+
+      expect(sidebar).to have_tags_section
+      expect(sidebar).to have_all_tags_section_link
+      expect(sidebar).to have_tags_section_links([tag1, tag2, tag3, tag4, tag6])
+    end
+
+    it "should display the tags configured in `default_navigation_menu_tags` site setting when it has been set" do
+      SiteSetting.default_navigation_menu_tags = "#{tag3.name}|#{tag4.name}"
+
+      visit("/latest")
+
+      expect(sidebar).to have_tags_section
+      expect(sidebar).to have_all_tags_section_link
+      expect(sidebar).to have_tags_section_links([tag3, tag4])
+    end
+  end
+end