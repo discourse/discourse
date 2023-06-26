f2c1f95cdf7bf1c1c58967aebdb7c71d0714303b
Loïc Guitaut <loic@discourse.org>
2023-06-26 16:01:53 +0200

    DEV: Introduce an `array` type for Chat contracts
    This small patch registers a new `ActiveModel` type: `array`.
    It will split a string on `,` to create a new array. If the value is
    already an array, nothing will happen and for all other types, it will
    wrap the value in an array.
    
    Here’s an example on an existing contract:
    ```ruby
      attribute :target_usernames
    
      before_validation do
        self.target_usernames =
          (
            if target_usernames.is_a?(String)
              target_usernames.split(",")
            else
              target_usernames
            end
          )
      end
    
      # can be rewritten as:
    
      attribute :target_usernames, :array
    ```

---
+++ plugins/chat/lib/chat/types/array.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Chat
+  module Types
+    class Array < ActiveModel::Type::Value
+      def serializable?(_)
+        false
+      end
+
+      def cast_value(value)
+        case value
+        when String
+          value.split(",")
+        else
+          ::Array.wrap(value)
+        end
+      end
+    end
+  end
+end
