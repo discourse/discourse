965f557ff30167b3706f3e8645a959496dd04663
Guhyoun Nam <rngus2344@gmail.com>
2023-06-26 10:42:20 -0500

    Removed unnecessary lines

--- db/migrate/20230509214723_separate_trusted_users_can_edit_others_site_setting.rb
+++ db/migrate/20230509214723_separate_trusted_users_can_edit_others_site_setting.rb
@@ -6,7 +6,6 @@ class SeparateTrustedUsersCanEditOthersSiteSetting < ActiveRecord::Migration[7.0
          "SELECT 1 FROM site_settings WHERE name = 'trusted_users_can_edit_others' AND value = 'f'",
        )
       execute <<~SQL
-        DELETE FROM site_settings WHERE name = 'trusted_users_can_edit_others';
         INSERT INTO site_settings (name, data_type, value, created_at, updated_at) VALUES ('edit_all_topic_groups', 20, '', now(), now());
         INSERT INTO site_settings (name, data_type, value, created_at, updated_at) VALUES ('edit_all_post_groups', 20, '', now(), now());
       SQL
@@ -14,14 +13,9 @@ class SeparateTrustedUsersCanEditOthersSiteSetting < ActiveRecord::Migration[7.0
   end
 
   def down
-    if select_value(
-         "SELECT 1 FROM site_settings WHERE name IN ('edit_all_topic_groups', 'edit_all_post_groups')",
-       )
-      execute <<~SQL
-        DELETE FROM site_settings WHERE name = 'edit_all_topic_groups';
-        DELETE FROM site_settings WHERE name = 'edit_all_post_groups';
-        INSERT INTO site_settings (name, data_type, value, created_at, updated_at) VALUES ('trusted_users_can_edit_others', 5, 'f', now(), now());
-      SQL
-    end
+    execute <<~SQL
+      DELETE FROM site_settings WHERE name = 'edit_all_topic_groups';
+      DELETE FROM site_settings WHERE name = 'edit_all_post_groups';
+    SQL
   end
 end
