46b5ea2e20602e6423e00168bec5a8cecd95ed91
OsamaSayegh <asooomaasoooma90@gmail.com>
2023-06-26 20:11:59 +0300

    SECURITY: Don't reuse CSP nonce between requests

--- config/initializers/099-anon-cache.rb
+++ config/initializers/099-anon-cache.rb
@@ -6,10 +6,11 @@ enabled =
   if Rails.configuration.respond_to?(:enable_anon_caching)
     Rails.configuration.enable_anon_caching
   else
-    Rails.env.production?
+    Rails.env.production? || Rails.env.test?
   end
 
 if !ENV["DISCOURSE_DISABLE_ANON_CACHE"] && enabled
   # in an ideal world this is position 0, but mobile detection uses ... session and request and params
-  Rails.configuration.middleware.insert_after ActionDispatch::Flash, Middleware::AnonymousCache
+  Rails.configuration.middleware.insert_after Middleware::GtmScriptNonceInjector,
+                                              Middleware::AnonymousCache
 end
