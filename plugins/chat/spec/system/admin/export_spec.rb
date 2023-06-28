d28b6c44cfec95268e9e8ff9b2e502457380483a
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-06-28 20:51:36 +0400

    Setup jobs in the before each hook

--- plugins/chat/spec/system/admin/export_spec.rb
+++ plugins/chat/spec/system/admin/export_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe "Chat exports", type: :system do
   let(:browse_page) { PageObjects::Pages::ChatBrowse.new }
 
   before do
+    Jobs.run_immediately!
     sign_in(current_user)
     chat_system_bootstrap
   end
 
   it "exports chat messages" do
-    Jobs.run_immediately!
     visit "/admin/plugins/chat"
     click_button "Create export"
     click_button "OK"
@@ -22,7 +22,6 @@ RSpec.describe "Chat exports", type: :system do
   end
 
   it "exports user list" do
-    Jobs.run_immediately!
     visit("admin/users/list/active")
     expect(page).to have_current_path("/latest")
   end
