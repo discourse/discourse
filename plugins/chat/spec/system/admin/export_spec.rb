455117af88291da61a6228616ccf0b1ebcb3dbb7
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-06-29 15:03:48 +0400

    WIP

--- plugins/chat/spec/system/admin/export_spec.rb
+++ plugins/chat/spec/system/admin/export_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe "Chat exports", type: :system do
     chat_system_bootstrap
   end
 
+  after do
+    # fixme clean the downloads folder
+  end
+
   it "exports chat messages" do
     visit "/admin/plugins/chat"
     click_button "Create export"
@@ -22,10 +26,16 @@ RSpec.describe "Chat exports", type: :system do
     click_link "chat-message-"
 
     sleep 3 # fixme try to get rid of sleep
-    DOWNLOAD_PATH = Rails.root.join("tmp/downloads").to_s
-    full_path = DOWNLOAD_PATH + "/partners-#{Date.today}.csv"
-    assert File.exist?(full_path)
-    headers = CSV.open(full_path, "r") { |csv| csv.first.to_s }
+
+    file_name = find("a.attachment").text
+
+    # DOWNLOAD_PATH = Rails.root.join("tmp/downloads").to_s
+    # full_path = DOWNLOAD_PATH + "/partners-#{Date.today}.csv"
+    assert File.exist?("tmp/downloads/#{file_name}")
+
+    extract_zip("tmp/downloads/#{file_name}", "tmp/downloads/")
+
+    headers = CSV.open(file, "r") { |csv| csv.first.to_s }
     assert_equal(
       headers,
       "[\"id\", \"name\", \"partner_type_id\", \"parent_id\", \"phone\", \"website\"]",
@@ -36,5 +46,16 @@ RSpec.describe "Chat exports", type: :system do
   it "exports user list" do
     visit("admin/users/list/active")
     expect(page).to have_current_path("/latest")
+  end
+
+  def extract_zip(file, destination)
+    FileUtils.mkdir_p(destination)
+
+    Zip::File.open(file) do |zip_file|
+      zip_file.each do |f|
+        path = File.join(destination, f.name)
+        zip_file.extract(f, path) unless File.exist?(path)
+      end
+    end
   end
 end
