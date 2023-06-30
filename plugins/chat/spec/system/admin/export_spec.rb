fa9aa01dc0016898ec0fae5b7c8cd98432a18035
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-06-30 17:41:09 +0400

    Assert time

--- plugins/chat/spec/system/admin/export_spec.rb
+++ plugins/chat/spec/system/admin/export_spec.rb
@@ -55,6 +55,7 @@ RSpec.describe "Chat exports", type: :system do
     )
 
     data_row = data[1]
+    time_format = "%Y-%m-%d %k:%M:%S UTC"
     expect(data_row[0]).to eq(message.id.to_s)
     expect(data_row[1]).to eq(message.chat_channel.id.to_s)
     expect(data_row[2]).to eq(message.chat_channel.name)
@@ -62,8 +63,8 @@ RSpec.describe "Chat exports", type: :system do
     expect(data_row[4]).to eq(message.user.username)
     expect(data_row[5]).to eq(message.message)
     expect(data_row[6]).to eq(message.cooked)
-    # expect(Time.parse(data_row[7])).to eq_time(message.created_at)
-    # expect(Time.parse(data_row[8])).to eq_time(message.updated_at)
+    expect(data_row[7]).to eq(message.created_at.strftime(time_format))
+    expect(data_row[8]).to eq(message.updated_at.strftime(time_format))
     expect(data_row[9]).to be_nil
     expect(data_row[10]).to be_nil
     expect(data_row[11]).to eq(message.last_editor.id.to_s)
