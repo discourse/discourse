e80ff5554ef187046b8d6024a0b6156bc284f4bf
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-06-30 17:54:27 +0400

    WIP: test for user list export

--- plugins/chat/spec/system/admin/export_spec.rb
+++ plugins/chat/spec/system/admin/export_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "Chat exports", type: :system do
 
     visit "/admin/plugins/chat"
     click_button "Create export"
-    click_button "OK"
+    click_button "OK" # fixme maybe remove this
 
     visit "/u/#{current_user.username}/messages"
     click_link "[Chat Message] Data export complete"
@@ -76,8 +76,79 @@ RSpec.describe "Chat exports", type: :system do
   end
 
   it "exports user list" do
-    visit("admin/users/list/active")
-    expect(page).to have_current_path("/latest")
+    visit "admin/users/list/active"
+    click_button "Export"
+    click_button "OK" # fixme maybe remove this
+
+    visit "/u/#{current_user.username}/messages"
+    click_link "[User List] Data export complete"
+    click_link "user-list-"
+
+    sleep 3 # fixme try to get rid of sleep
+
+    file_name = find("a.attachment").text
+
+    expect(File.exist?("tmp/downloads/#{file_name}")).to be_truthy
+
+    csv_path = extract_zip("tmp/downloads/#{file_name}", "tmp/downloads/")
+    data = CSV.read(csv_path)
+
+    expect(data[0]).to match_array(
+      %w[
+        id
+        name
+        username
+        email
+        title
+        created_at
+        last_seen_at
+        last_posted_at
+        last_emailed_at
+        trust_level
+        approved
+        suspended_at
+        suspended_till
+        silenced_till
+        active
+        admin
+        moderator
+        ip_address
+        staged
+        secondary_emails
+        topics_entered
+        posts_read_count
+        time_read
+        topic_count
+        post_count
+        likes_given
+        likes_received
+        group_names
+        location
+        website
+        views
+      ],
+    )
+
+    data_row = data[1]
+    time_format = "%Y-%m-%d %k:%M:%S UTC"
+    # fixme implement
+    # expect(data_row).to eq(
+    #   [
+    #     message.id.to_s,
+    #     message.chat_channel.id.to_s,
+    #     message.chat_channel.name,
+    #     message.user.id.to_s,
+    #     message.user.username,
+    #     message.message,
+    #     message.cooked,
+    #     message.created_at.strftime(time_format),
+    #     message.updated_at.strftime(time_format),
+    #     nil,
+    #     nil,
+    #     message.last_editor.id.to_s,
+    #     message.last_editor.username,
+    #   ],
+    # )
   end
 
   def extract_zip(file, destination)
