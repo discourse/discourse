b3a23bd9d6abdff2d87d4f32866a27cb6770f499
David Taylor <david@taylorhq.com>
2023-07-03 10:51:27 +0100

    DEV: Introduce new component-based DModal API (#21304)
    Ember 4.x will be removing the 'named outlet' feature, which were previously relying on to render modal 'controllers' and their associated templates. This commit updates the modal.show API to accept a component class, and also introduces a declarative API which can be used by including the <DModal component directly in your template.
    
    For more information on the API design, and conversion instructions from the current API, see these Meta topics:
    
    DModal API: https://meta.discourse.org/t/268304
    Conversion: https://meta.discourse.org/t/268057

--- app/assets/javascripts/discourse/config/deprecation-workflow.js
+++ app/assets/javascripts/discourse/config/deprecation-workflow.js
@@ -8,5 +8,6 @@ globalThis.deprecationWorkflow.config = {
     { handler: "silence", matchId: "route-disconnect-outlet" },
     { handler: "silence", matchId: "this-property-fallback" },
     { handler: "silence", matchId: "discourse.select-kit" },
+    { handler: "silence", matchId: "discourse.modal-controllers" },
   ],
 };
