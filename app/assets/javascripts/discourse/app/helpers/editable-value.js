c1ca7a45ea7b3172fe4e2cf517db75d37d729f2e
Jarek Radosz <jradosz@gmail.com>
2023-07-04 10:25:49 +0200

    fix

--- app/assets/javascripts/discourse/app/helpers/editable-value.js
+++ app/assets/javascripts/discourse/app/helpers/editable-value.js
@@ -1,10 +1,12 @@
-export default function formatCurrency(reviewable, fieldId) {
+import { get } from "@ember/object";
+
+export default function editableValue(reviewable, fieldId) {
   // The field `category_id` corresponds to `category`
   if (fieldId === "category_id") {
     fieldId = "category.id";
   }
 
-  const value = reviewable?.fieldId;
+  const value = get(reviewable, fieldId);
 
   // If it's an array, say tags, make a copy so we aren't mutating the original
   if (Array.isArray(value)) {
