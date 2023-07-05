c200fbc0e369ade0689d784c94235b656d4af5da
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-07-05 07:42:28 +0200

    uses user search

--- plugins/chat/app/services/chat/search_chatable.rb
+++ plugins/chat/app/services/chat/search_chatable.rb
@@ -29,7 +29,7 @@ module Chat
     def fetch_users(contract:, guardian:, **)
       return if contract.term&.start_with?("#")
 
-      context.users = search_user(contract.term, guardian)
+      context.users = search_users(contract.term, guardian)
     end
 
     def fetch_category_channels(contract:, guardian:, **)
@@ -50,7 +50,7 @@ module Chat
       return if contract.term&.start_with?("@")
 
       user_ids =
-        (context.users.nil? ? search_user(contract.term, guardian) : context.users).map(&:id)
+        (context.users.nil? ? search_users(contract.term, guardian) : context.users).map(&:id)
       return if user_ids.blank?
 
       context.direct_message_channels =
@@ -62,38 +62,9 @@ module Chat
         )
     end
 
-    def default_user_scope(guardian)
-      users =
-        User
-          .strict_loading
-          .includes(:user_option, :groups, :user_status)
-          .where(active: true, staged: false)
-          .not_suspended
-      users = users.where(approved: true) if SiteSetting.must_approve_users?
-      users = users.limit(10)
-      users
-    end
-
-    def search_user(term, guardian)
-      if term.blank? || term == "@"
-        return default_user_scope(guardian).order("last_seen_at DESC NULLS LAST")
-      end
-
+    def search_users(term, guardian)
       term = term.downcase.gsub(/^#+/, "").gsub(/^@+/, "").strip
-      term_like = term.gsub("_", "\\_") + "%"
-
-      if SiteSetting.enable_names? && term !~ /[_\.-]/
-        query = Search.ts_query(term: term, ts_config: "simple")
-        order = DB.sql_fragment("CASE WHEN username_lower LIKE ? THEN 0 ELSE 1 END ASC", term_like)
-
-        default_user_scope(guardian)
-          .includes(:user_search_data)
-          .references(:user_search_data)
-          .where("user_search_data.search_data @@ #{query}")
-          .order(order)
-      else
-        default_user_scope(guardian).where("username_lower LIKE :term_like", term_like: term_like)
-      end
+      UserSearch.new(term, limit: 10).search.includes(:user_option)
     end
   end
 end
