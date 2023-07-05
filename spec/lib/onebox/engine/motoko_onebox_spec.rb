c26c00473e32b9307419207c2adc0d2f7d8d3126
rvanasa <ryan.vandersmith@dfinity.org>
2023-07-05 11:36:05 -0600

    Adjust unit test

--- spec/lib/onebox/engine/motoko_onebox_spec.rb
+++ spec/lib/onebox/engine/motoko_onebox_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe Onebox::Engine::MotokoOnebox do
   before do
     body =
-      '{"version":"1.0","provider_name":"Embed Motoko","provider_url":"https://embed.smartcontracts.org","type":"rich","width":800,"height":500,"html":"<iframe src=\"https://embed.motoko.org\" width=\"800\" height=\"500\" style=\"border:0\" />"}'
+      '{"version":"1.0","provider_name":"Embed Motoko","provider_url":"https://embed.motoko.org","type":"rich","width":800,"height":500,"html":"<iframe src=\"https://embed.motoko.org\" width=\"800\" height=\"500\" style=\"border:0\" />"}'
 
     stub_request(
       :get,
