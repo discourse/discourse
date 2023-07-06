0744d242c6c066eb467c9df3aa6df3b6a8dd7b54
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-07-06 11:27:23 +1000

    FIX: post alerter notification when topic directly watched (#22433)
    In previous PR https://github.com/discourse/discourse/pull/22340 bug was introduced. Notifications were blocked when, even if topic was watched directly. New query is taking TopicUser into consideration.
    
    In addition, in user interface, when `watched_precedence_over_muted` is not set, then value from SiteSetting should be displayed.

--- app/services/post_alerter.rb
+++ app/services/post_alerter.rb
@@ -272,6 +272,8 @@ class PostAlerter
       SELECT user_id FROM category_users WHERE category_id = #{topic.category_id.to_i} AND notification_level = #{CategoryUser.notification_levels[:muted]}
       UNION
       SELECT user_id FROM tag_users tu JOIN topic_tags tt ON tt.tag_id = tu.tag_id AND tt.topic_id = #{topic.id} AND tu.notification_level = #{TagUser.notification_levels[:muted]}
+      EXCEPT
+      SELECT user_id FROM topic_users tus WHERE tus.topic_id = #{topic.id} AND tus.notification_level = #{TopicUser.notification_levels[:watching]}
     SQL
     User
       .where("id IN (#{user_ids_sql})")
