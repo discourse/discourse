discourse/discourse

commit 8b80132f8868500571ec1c80bb3e02f1873dbac2

DEV: Convert dismiss modals to component-based API (#22262)
This PR converts the following modals:
- `dismiss-new`
- `dismiss-read`
- `dismiss-notification-confirmation`

to make use of the new component-based API

# Additional Changes
## Before
By default we display a warning modal when dismissing a notification however we bypass the warning modal for specific notification types when they are a 'low priority' type of notification (eg. likes). To do this we were overwriting `dismissWarningModal` on a given notification type component

```javascript
dismissWarningModal() {
  return null
}
```

but in the case we wanted to change the text within the modal we were calling `showModal` and then passing in the respective options all over again, putting the logic of rendering the modal in multiple places.

```javascript
dismissWarningModal() {
  const modalController = showModal("dismiss-notification-confirmation");
  modalController.set(
    "confirmationMessage",
    I18n.t("notifications.dismiss_confirmation.body.assigns", {
      count: this._unreadAssignedNotificationsCount,
    })
  );
  return modalController;
}
```
 

## After
I simplified this by adding an extensible `dismissConfirmationText` function that can be updated on a per component basis as that was the only option being overridden. 

eg

```javascript
get dismissConfirmationText() {
  return I18n.t("notifications.dismiss_confirmation.body.bookmarks", {
    count: this.#unreadBookmarkRemindersCount,
});
```

This saves us from importing the entire modal again and keeps the core logic in one place.

Instead of overwriting the `dismissWarningModal` function and returning `null` to bypass the confirmation modal, I added another extension point of `renderDismissConfirmation` (defaults to true) to _toggle_ whether we should display a confirmation when dismissing notifications.

eg

```javascript
get renderDismissConfirmation() {
  return false;
}
```

app/assets/javascripts/discourse/app/components/modal/dismiss-new.hbs
@@ -1,0 +1,33 @@
+<DModal
+  @closeModal={{@closeModal}}
+  @title={{i18n "topics.bulk.dismiss_new_modal.title"}}
+>
+  <:body>
+    <p>
+      <PreferenceCheckbox
+        @labelKey="topics.bulk.dismiss_new_modal.topics"
+        @checked={{@model.dismissTopics}}
+        @class="dismiss-topics"
+      />
+      <PreferenceCheckbox
+        @labelKey="topics.bulk.dismiss_new_modal.posts"
+        @checked={{@model.dismissPosts}}
+        @class="dismiss-posts"
+      />
+      <PreferenceCheckbox
+        @labelKey="topics.bulk.dismiss_new_modal.untrack"
+        @checked={{@model.untrack}}
+        @class="untrack"
+      />
+    </p>
+  </:body>
+  <:footer>
+    <DButton
+      id="dismiss-read-confirm"
+      @action={{this.dismissed}}
+      @icon="check"
+      @label="topics.bulk.dismiss"
+      class="btn-primary"
+    />
+  </:footer>
+</DModal>

app/assets/javascripts/discourse/app/components/modal/dismiss-new.js
@@ -1,0 +1,10 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+
+export default class DismissNew extends Component {
+  @action
+  dismissed() {
+    this.args.model.dismissCallback();
+    this.args.closeModal();
+  }
+}

app/assets/javascripts/discourse/app/mixins/dismiss-topics.js
@@ -1,30 +1,29 @@
 import Mixin from "@ember/object/mixin";
 import User from "discourse/models/user";
-import showModal from "discourse/lib/show-modal";
-import I18n from "I18n";
+import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import DismissNewModal from "discourse/components/modal/dismiss-new";
 
 export default Mixin.create({
-  actions: {
-    resetNew() {
-      const user = User.current();
-      if (!user.new_new_view_enabled) {
-        return this.callResetNew();
-      }
-      const controller = showModal("dismiss-new", {
-        model: {
-          dismissTopics: true,
-          dismissPosts: true,
-        },
-        titleTranslated: I18n.t("topics.bulk.dismiss_new_modal.title"),
-      });
+  modal: service(),
 
-      controller.set("dismissCallback", () => {
-        this.callResetNew(
-          controller.model.dismissPosts,
-          controller.model.dismissTopics,
-          controller.model.untrack
-        );
-      });
-    },
+  @action
+  resetNew() {
+    const user = User.current();
+    if (!user.new_new_view_enabled) {
+      return this.callResetNew();
+    }
+    this.modal.show(DismissNewModal, {
+      model: {
+        dismissTopics: true,
+        dismissPosts: true,
+        dismissCallback: () =>
+          this.callResetNew(
+            this.model.dismissPosts,
+            this.model.dismissTopics,
+            this.model.untrack
+          ),
+      },
+    });
   },
 });

app/assets/javascripts/discourse/app/controllers/dismiss-new.js
@@ -1,13 +1,0 @@
-import Controller from "@ember/controller";
-import ModalFunctionality from "discourse/mixins/modal-functionality";
-import { action } from "@ember/object";
-
-export default class DismissNewController extends Controller.extend(
-  ModalFunctionality
-) {
-  @action
-  dismiss() {
-    this.dismissCallback();
-    this.send("closeModal");
-  }
-}

app/assets/javascripts/discourse/app/templates/modal/dismiss-new.hbs
@@ -1,29 +1,0 @@
-<DModalBody>
-  <p>
-    <PreferenceCheckbox
-      @labelKey="topics.bulk.dismiss_new_modal.topics"
-      @checked={{this.model.dismissTopics}}
-      @class="dismiss-topics"
-    />
-    <PreferenceCheckbox
-      @labelKey="topics.bulk.dismiss_new_modal.posts"
-      @checked={{this.model.dismissPosts}}
-      @class="dismiss-posts"
-    />
-    <PreferenceCheckbox
-      @labelKey="topics.bulk.dismiss_new_modal.untrack"
-      @checked={{this.model.untrack}}
-      @class="untrack"
-    />
-  </p>
-</DModalBody>
-
-<div class="modal-footer">
-  <DButton
-    @class="btn-primary"
-    @action="dismiss"
-    @icon="check"
-    @id="dismiss-read-confirm"
-    @label="topics.bulk.dismiss"
-  />
-</div>

app/assets/javascripts/discourse/app/components/modal/dismiss-notification-confirmation.hbs
@@ -1,0 +1,22 @@
+<DModal
+  @headerClass="hidden"
+  class="dismiss-notification-confirmation"
+  @closeModal={{@closeModal}}
+>
+  <:body>
+    {{@model.confirmationMessage}}
+  </:body>
+  <:footer>
+    <DButton
+      @icon="check"
+      class="btn-primary"
+      @action={{this.dismiss}}
+      @label="notifications.dismiss_confirmation.dismiss"
+    />
+    <DButton
+      @action={{@closeModal}}
+      @label="notifications.dismiss_confirmation.cancel"
+      class="btn-default"
+    />
+  </:footer>
+</DModal>

app/assets/javascripts/discourse/app/components/modal/dismiss-notification-confirmation.js
@@ -1,0 +1,10 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+
+export default class DismissNotificationConfirmation extends Component {
+  @action
+  dismiss() {
+    this.args.model?.dismissNotifications?.();
+    this.args.closeModal();
+  }
+}

app/assets/javascripts/discourse/app/components/modal/dismiss-read.hbs
@@ -1,0 +1,23 @@
+<DModal
+  @closeModal={{@closeModal}}
+  @title={{i18n @model.title count=@model.count}}
+  class="dismiss-read-modal"
+>
+  <:body>
+    <p>
+      <PreferenceCheckbox
+        @labelKey="topics.bulk.also_dismiss_topics"
+        @checked={{this.dismissTopics}}
+      />
+    </p>
+  </:body>
+  <:footer>
+    <DButton
+      class="btn-primary"
+      @action={{route-action "dismissReadTopics" this.dismissTopics}}
+      @icon="check"
+      id="dismiss-read-confirm"
+      @label="topics.bulk.dismiss"
+    />
+  </:footer>
+</DModal>

commit cf98c89a0f9dbe359b9cff30a00707579682e23c

FIX: Can't dismiss new topics for experimental new new view
Regressed in 8b80132f8868500571ec1c80bb3e02f1873dbac2 due to a lack of
tests.

app/assets/javascripts/discourse/app/components/modal/dismiss-new.hbs
@@ -6,17 +6,17 @@
     <p>
       <PreferenceCheckbox
         @labelKey="topics.bulk.dismiss_new_modal.topics"
-        @checked={{@model.dismissTopics}}
+        @checked={{this.dismissTopics}}
         @class="dismiss-topics"
       />
       <PreferenceCheckbox
         @labelKey="topics.bulk.dismiss_new_modal.posts"
-        @checked={{@model.dismissPosts}}
+        @checked={{this.dismissPosts}}
         @class="dismiss-posts"
       />
       <PreferenceCheckbox
         @labelKey="topics.bulk.dismiss_new_modal.untrack"
-        @checked={{@model.untrack}}
+        @checked={{this.untrack}}
         @class="untrack"
       />
     </p>

app/assets/javascripts/discourse/app/components/modal/dismiss-new.js
@@ -2,9 +2,18 @@ import Component from "@glimmer/component";
 import { action } from "@ember/object";
 
 export default class DismissNew extends Component {
+  dismissTopics = true;
+  dismissPosts = true;
+  untrack = false;
+
   @action
   dismissed() {
-    this.args.model.dismissCallback();
+    this.args.model.dismissCallback({
+      dismissTopics: this.dismissTopics,
+      dismissPosts: this.dismissPosts,
+      untrack: this.untrack,
+    });
+
     this.args.closeModal();
   }
 }

app/assets/javascripts/discourse/app/mixins/dismiss-topics.js
@@ -1,28 +1,23 @@
 import Mixin from "@ember/object/mixin";
-import User from "discourse/models/user";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 import DismissNewModal from "discourse/components/modal/dismiss-new";
 
 export default Mixin.create({
   modal: service(),
+  currentUser: service(),
 
   @action
   resetNew() {
-    const user = User.current();
-    if (!user.new_new_view_enabled) {
+    if (!this.currentUser.new_new_view_enabled) {
       return this.callResetNew();
     }
+
     this.modal.show(DismissNewModal, {
       model: {
-        dismissTopics: true,
-        dismissPosts: true,
-        dismissCallback: () =>
-          this.callResetNew(
-            this.model.dismissPosts,
-            this.model.dismissTopics,
-            this.model.untrack
-          ),
+        dismissCallback: ({ dismissPosts, dismissTopics, untrack }) => {
+          this.callResetNew(dismissPosts, dismissTopics, untrack);
+        },
       },
     });
   },

spec/system/dismiss_topics_spec.rb
@@ -2,10 +2,11 @@
 
 describe "Filtering topics", type: :system, js: true do
   fab!(:user) { Fabricate(:user) }
-  let(:topic_list) { PageObjects::Components::TopicList.new }
-  let(:dismiss_new_modal) { PageObjects::Modals::DismissNew.new }
   fab!(:group) { Fabricate(:group).tap { |g| g.add(user) } }
   fab!(:topic) { Fabricate(:topic) }
+
+  let(:topic_list) { PageObjects::Components::TopicList.new }
+  let(:dismiss_new_modal) { PageObjects::Modals::DismissNew.new }
 
   before { SiteSetting.experimental_new_new_view_groups = group.id }
 
@@ -15,9 +16,15 @@ describe "Filtering topics", type: :system, js: true do
     visit("/new")
 
     expect(topic_list).to have_topic(topic)
+
     find(".dismiss-read", text: "Dismiss…").click
+
     expect(dismiss_new_modal).to have_dismiss_topics_checked
     expect(dismiss_new_modal).to have_dismiss_posts_checked
     expect(dismiss_new_modal).to have_untrack_unchecked
+
+    dismiss_new_modal.click_dismiss
+
+    expect(topic_list).to have_no_topics
   end
 end

spec/system/page_objects/components/topic_list.rb
@@ -14,6 +14,10 @@ module PageObjects
         page.has_css?(TOPIC_LIST_ITEM_SELECTOR, count: count)
       end
 
+      def has_no_topics?
+        page.has_no_css?(TOPIC_LIST_ITEM_SELECTOR)
+      end
+
       def has_topic?(topic)
         page.has_css?(topic_list_item_class(topic))
       end

spec/system/page_objects/modals/dismiss_new.rb
@@ -14,6 +14,11 @@ module PageObjects
       def has_untrack_unchecked?
         find(".untrack label").has_no_checked_field?
       end
+
+      def click_dismiss
+        click_button("dismiss-read-confirm")
+        self
+      end
     end
   end
 end
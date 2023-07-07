cf98c89a0f9dbe359b9cff30a00707579682e23c
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-07-07 08:05:06 +0800

    FIX: Can't dismiss new topics for experimental new new view
    Regressed in 8b80132f8868500571ec1c80bb3e02f1873dbac2 due to a lack of
    tests.

--- app/assets/javascripts/discourse/app/mixins/dismiss-topics.js
+++ app/assets/javascripts/discourse/app/mixins/dismiss-topics.js
@@ -1,28 +1,23 @@
 import Mixin from "@ember/object/mixin";
-import User from "discourse/models/user";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 import DismissNewModal from "discourse/components/modal/dismiss-new";
 
 export default Mixin.create({
   modal: service(),
+  currentUser: service(),
 
   @action
   resetNew() {
-    const user = User.current();
-    if (!user.new_new_view_enabled) {
+    if (!this.currentUser.new_new_view_enabled) {
       return this.callResetNew();
     }
+
     this.modal.show(DismissNewModal, {
       model: {
-        dismissTopics: true,
-        dismissPosts: true,
-        dismissCallback: () =>
-          this.callResetNew(
-            this.model.dismissPosts,
-            this.model.dismissTopics,
-            this.model.untrack
-          ),
+        dismissCallback: ({ dismissPosts, dismissTopics, untrack }) => {
+          this.callResetNew(dismissPosts, dismissTopics, untrack);
+        },
       },
     });
   },
