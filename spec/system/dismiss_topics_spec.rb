bf582290ba0a1bfacea3394a939b2dff0459ad41
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-07-07 08:28:27 +0800

    FIX: Can't dismiss new topics for experimental new new view (#22484)
    Regressed in 8b80132f8868500571ec1c80bb3e02f1873dbac2 due to a lack of
    tests.

--- spec/system/dismiss_topics_spec.rb
+++ spec/system/dismiss_topics_spec.rb
@@ -2,10 +2,11 @@
 
 describe "Filtering topics", type: :system, js: true do
   fab!(:user) { Fabricate(:user) }
-  let(:topic_list) { PageObjects::Components::TopicList.new }
-  let(:dismiss_new_modal) { PageObjects::Modals::DismissNew.new }
   fab!(:group) { Fabricate(:group).tap { |g| g.add(user) } }
   fab!(:topic) { Fabricate(:topic) }
+
+  let(:topic_list) { PageObjects::Components::TopicList.new }
+  let(:dismiss_new_modal) { PageObjects::Modals::DismissNew.new }
 
   before { SiteSetting.experimental_new_new_view_groups = group.id }
 
@@ -15,9 +16,15 @@ describe "Filtering topics", type: :system, js: true do
     visit("/new")
 
     expect(topic_list).to have_topic(topic)
+
     find(".dismiss-read", text: "Dismiss…").click
+
     expect(dismiss_new_modal).to have_dismiss_topics_checked
     expect(dismiss_new_modal).to have_dismiss_posts_checked
     expect(dismiss_new_modal).to have_untrack_unchecked
+
+    dismiss_new_modal.click_dismiss
+
+    expect(topic_list).to have_no_topics
   end
 end
