7258df5aeaa3e86da502170e792cc795600a155d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-07-09 17:48:26 +0200

    return promise

--- plugins/chat/assets/javascripts/discourse/components/chat/thread/header.js
+++ plugins/chat/assets/javascripts/discourse/components/chat/thread/header.js
@@ -73,7 +73,7 @@ export default class ChatThreadHeader extends Component {
       });
     }
 
-    this.chatApi
+    return this.chatApi
       .updateCurrentUserThreadNotificationsSettings(
         this.args.thread.channel.id,
         this.args.thread.id,
