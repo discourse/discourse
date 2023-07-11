461966e0284875bf0eea7fc51b4fffc225c9275c
Blake Erickson <o.blakeerickson@gmail.com>
2023-07-11 17:09:02 -0600

    Version bump to v3.0.5 (#22556)

--- lib/version.rb
+++ lib/version.rb
@@ -9,7 +9,7 @@ module Discourse
     module VERSION #:nodoc:
       MAJOR = 3
       MINOR = 0
-      TINY = 4
+      TINY = 5
       PRE = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
