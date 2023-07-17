ae25457bdf8bb33fb25ea8c73a362709f5388500
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-07-17 13:43:55 +1000

    FIX: refactoring after code review

--- app/assets/javascripts/discourse/app/components/sidebar/user/sections.js
+++ app/assets/javascripts/discourse/app/components/sidebar/user/sections.js
@@ -1,16 +1,16 @@
 import Component from "@glimmer/component";
 import { getOwner, setOwner } from "@ember/application";
 import { inject as service } from "@ember/service";
-import { cached } from "@glimmer/tracking";
 
 export default class SidebarUserSections extends Component {
   @service siteSettings;
   @service currentUser;
   @service site;
 
-  @cached
-  get customSections() {
-    return this.args.panel?.sections?.map((customSection) => {
+  constructor() {
+    super(...arguments);
+
+    this.customSections = this.args.panel?.sections?.map((customSection) => {
       const section = new customSection({ sidebar: this });
       setOwner(section, getOwner(this));
       return section;
--- app/assets/javascripts/discourse/app/instance-initializers/sidebar.js
+++ app/assets/javascripts/discourse/app/instance-initializers/sidebar.js
@@ -1,29 +1,27 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
 import I18n from "I18n";
+import { addSidebarPanel } from "discourse/lib/sidebar/custom-sections";
 
 export default {
   initialize() {
-    withPluginApi("1.7.1", (api) => {
-      api.addSidebarPanel((BaseCustomSidebarPanel) => {
-        const MainSidebarPanel = class extends BaseCustomSidebarPanel {
-          get key() {
-            return "main";
-          }
-          get switchButtonLabel() {
-            return I18n.t("sidebar.panels.forum.label");
-          }
+    addSidebarPanel((BaseCustomSidebarPanel) => {
+      const MainSidebarPanel = class extends BaseCustomSidebarPanel {
+        get key() {
+          return "main";
+        }
+        get switchButtonLabel() {
+          return I18n.t("sidebar.panels.forum.label");
+        }
 
-          get switchButtonIcon() {
-            return "random";
-          }
+        get switchButtonIcon() {
+          return "random";
+        }
 
-          get switchButtonDefaultUrl() {
-            return "/";
-          }
-        };
+        get switchButtonDefaultUrl() {
+          return "/";
+        }
+      };
 
-        return MainSidebarPanel;
-      });
+      return MainSidebarPanel;
     });
   },
 };
