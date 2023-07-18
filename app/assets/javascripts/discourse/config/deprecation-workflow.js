bd88b7d6c4981b03a82062c5de000c20efdad7ec
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2023-07-18 16:09:28 -0500

    DEV: Remove `transition-methods` deprecation (#22673)
    All https://deprecations.emberjs.com/v3.x/#toc_routing-transition-methods deprecations have been resolved.

--- app/assets/javascripts/discourse/config/deprecation-workflow.js
+++ app/assets/javascripts/discourse/config/deprecation-workflow.js
@@ -4,7 +4,6 @@ globalThis.deprecationWorkflow.config = {
   // `throwOnUnhandled` here since it is easier to toggle.
   workflow: [
     { handler: "silence", matchId: "route-render-template" },
-    { handler: "silence", matchId: "routing.transition-methods" },
     { handler: "silence", matchId: "route-disconnect-outlet" },
     { handler: "silence", matchId: "this-property-fallback" },
     { handler: "silence", matchId: "discourse.select-kit" },
