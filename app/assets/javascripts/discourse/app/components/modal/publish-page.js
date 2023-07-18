62682f4236f692d6eaf746d7989d8f198341acd6
Ted Johansson <ted@discourse.org>
2023-07-18 17:53:26 +0800

    DEV: Convert PublishPageModal to a Glimmer component

--- app/assets/javascripts/discourse/app/components/modal/publish-page.js
+++ app/assets/javascripts/discourse/app/components/modal/publish-page.js
@@ -1,6 +1,7 @@
-import { action, computed } from "@ember/object";
-import { equal, not } from "@ember/object/computed";
-import Component from "@ember/component";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+import Component from "@glimmer/component";
 import { ajax } from "discourse/lib/ajax";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 
@@ -16,39 +17,75 @@ const States = {
   unpublished: "unpublished",
 };
 
-const StateHelpers = {};
-Object.keys(States).forEach((name) => {
-  StateHelpers[name] = equal("state", name);
-});
+export default class PublishPageModal extends Component {
+  @service store;
 
-const PublishPageModal = Component.extend(StateHelpers, {
-  state: null,
-  reason: null,
-  publishedPage: null,
-  disabled: not("valid"),
+  @tracked state = States.initializing;
+  @tracked reason = null;
+  @tracked publishedPage = null;
 
-  showUrl: computed("state", function () {
+  constructor() {
+    super(...arguments);
+    this.store
+      .find("published_page", this.args.model.id)
+      .then((page) => {
+        this.state = States.existing;
+        this.publishedPage = page;
+      })
+      .catch(this.startNew);
+  }
+
+  get initializing() {
+    return this.state === States.initializing;
+  }
+
+  get checking() {
+    return this.state === States.checking;
+  }
+
+  get valid() {
+    return this.state === States.valid;
+  }
+
+  get invalid() {
+    return this.state === States.invalid;
+  }
+
+  get saving() {
+    return this.state === States.saving;
+  }
+
+  get new() {
+    return this.state === States.new;
+  }
+
+  get existing() {
+    return this.state === States.existing;
+  }
+
+  get unpublishing() {
+    return this.state === States.unpublishing;
+  }
+
+  get unpublished() {
+    return this.state === States.unpublished;
+  }
+
+  get disabled() {
+    return this.state !== States.valid;
+  }
+
+  get showUrl() {
     return (
       this.state === States.valid ||
       this.state === States.saving ||
       this.state === States.existing
     );
-  }),
+  }
 
-  showUnpublish: computed("state", function () {
+  get showUnpublish() {
     return this.state === States.existing || this.state === States.unpublishing;
-  }),
-
-  init() {
-    this._super(...arguments);
-    this.set("state", States.initializing);
-    this.store
-      .find("published_page", this.model.id)
-      .then((page) => {
-        this.setProperties({ state: States.existing, publishedPage: page });
-      })
-      .catch(this.startNew);
-  },
+  }
 
   @action
   startCheckSlug() {
@@ -56,8 +93,8 @@ const PublishPageModal = Component.extend(StateHelpers, {
       return;
     }
 
-    this.set("state", States.checking);
-  },
+    this.state = States.checking;
+  }
 
   @action
   checkSlug() {
@@ -68,64 +105,61 @@ const PublishPageModal = Component.extend(StateHelpers, {
       data: { slug: this.publishedPage.slug },
     }).then((result) => {
       if (result.valid_slug) {
-        this.set("state", States.valid);
+        this.state = States.valid;
       } else {
-        this.setProperties({ state: States.invalid, reason: result.reason });
+        this.state = States.invalid;
+        this.reason = result.reason;
       }
     });
-  },
+  }
 
   @action
   unpublish() {
-    this.set("state", States.unpublishing);
+    this.state = States.unpublishing;
     return this.publishedPage
       .destroyRecord()
       .then(() => {
-        this.set("state", States.unpublished);
-        this.model.set("publishedPage", null);
+        this.state = States.unpublished;
+        this.args.model.set("publishedPage", null);
       })
       .catch((result) => {
-        this.set("state", States.existing);
+        this.state = States.existing;
         popupAjaxError(result);
       });
-  },
+  }
 
   @action
   publish() {
-    this.set("state", States.saving);
+    this.state = States.saving;
 
     return this.publishedPage
       .update(this.publishedPage.getProperties("slug", "public"))
       .then(() => {
-        this.set("state", States.existing);
-        this.model.set("publishedPage", this.publishedPage);
+        this.state = States.existing;
+        this.args.model.set("publishedPage", this.publishedPage);
       })
       .catch((errResult) => {
         popupAjaxError(errResult);
-        this.set("state", States.existing);
+        this.state = States.existing;
       });
-  },
+  }
 
   @action
   startNew() {
-    this.setProperties({
-      state: States.new,
-      publishedPage: this.store.createRecord(
-        "published_page",
-        this.model.getProperties("id", "slug", "public")
-      ),
-    });
+    this.state = States.new;
+    this.publishedPage = this.store.createRecord(
+      "published_page",
+      this.args.model.getProperties("id", "slug", "public")
+    );
     this.checkSlug();
-  },
+  }
 
   @action
-  onChangePublic(isPublic) {
-    this.publishedPage.set("public", isPublic);
+  onChangePublic(event) {
+    this.publishedPage.set("public", event.target.checked);
 
     if (this.showUnpublish) {
       this.publish();
     }
-  },
-});
-
-export default PublishPageModal;
+  }
+}
