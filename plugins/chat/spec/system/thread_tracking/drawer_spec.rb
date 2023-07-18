f0dd72a35858b107bcd52327ee4a8083c6dc2c94
Martin Brennan <martin@discourse.org>
2023-07-18 11:08:50 +1000

    DEV: Fix and re-enable chat flakys
    The early return in JS was added to prevent an error
    from channel being null, and it's better to use known
    users for the message fabrications in the specs.

--- plugins/chat/spec/system/thread_tracking/drawer_spec.rb
+++ plugins/chat/spec/system/thread_tracking/drawer_spec.rb
@@ -15,6 +15,7 @@ describe "Thread tracking state | drawer", type: :system do
   before do
     SiteSetting.enable_experimental_chat_threaded_discussions = true
     chat_system_bootstrap(current_user, [channel])
+    chat_system_user_bootstrap(user: other_user, channel: channel)
     sign_in(current_user)
     thread.add(current_user)
   end
@@ -53,12 +54,6 @@ describe "Thread tracking state | drawer", type: :system do
     end
 
     it "shows unread indicators for the header icon and the list when a new unread arrives" do
-      skip(<<~TEXT)
-      Flaky at the following assertion:
-
-      expected `#<PageObjects::Components::Chat::ThreadList:0x00007f082393ada0>.has_unread_item?(2)` to be truthy, got false
-      TEXT
-
       thread.membership_for(current_user).update!(last_read_message_id: message_2.id)
       visit("/")
       chat_page.open_from_header
@@ -66,7 +61,7 @@ describe "Thread tracking state | drawer", type: :system do
       drawer_page.open_thread_list
       expect(drawer_page).to have_no_unread_thread_indicator
       expect(thread_list_page).to have_no_unread_item(thread.id)
-      Fabricate(:chat_message, chat_channel: channel, thread: thread)
+      Fabricate(:chat_message, chat_channel: channel, thread: thread, user: other_user)
       expect(drawer_page).to have_unread_thread_indicator(count: 1)
       expect(thread_list_page).to have_unread_item(thread.id)
     end
@@ -103,18 +98,18 @@ describe "Thread tracking state | drawer", type: :system do
         chat_page.open_from_header
         expect(drawer_page).to have_unread_channel(channel)
         drawer_page.open_channel(channel)
-        Fabricate(:chat_message, thread: thread)
+        Fabricate(:chat_message, thread: thread, user: other_user)
         drawer_page.back
         expect(drawer_page).to have_no_unread_channel(channel)
       end
 
-      xit "shows an unread indicator for the channel index if a new thread message arrives while the user is not looking at the channel" do
+      it "shows an unread indicator for the channel index if a new thread message arrives while the user is not looking at the channel" do
         visit("/")
         chat_page.open_from_header
         drawer_page.open_channel(channel)
         drawer_page.back
         expect(drawer_page).to have_no_unread_channel(channel)
-        Fabricate(:chat_message, thread: thread)
+        Fabricate(:chat_message, thread: thread, user: other_user)
         expect(drawer_page).to have_unread_channel(channel)
       end
     end
