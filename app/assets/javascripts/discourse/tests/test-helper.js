1562a539478e8ec99fbba38fbb97837e41901c76
Godfrey Chan <godfreykfc@gmail.com>
2023-07-19 16:50:24 -0700

    DEV: start testing embroider in CI
    Discourse core now builds and runs with Embroider! This commit adds
    the Embroider-based build pipeline (`USE_EMBROIDER=1`) and start
    testing it on CI.
    
    The new pipeline uses Embroider's compat mode + webpack bundler to
    build discourse code, and leave everything else (admin, wizard,
    markdown-it, plugins, etc) exactly the same using the existing
    Broccoli-based build as external bundles (<script> tags), passed
    to the build as `extraPublicTress` (which just means they get
    placed in the `/public` folder).
    
    At runtime, these "external" bundles are glued back together with
    `loader.js`. Specifically, the external bundles are compiled as
    AMD modules (just as they were before) and registered with the
    global `loader.js` instance. They expect their `import`s (outside
    of whatever is included in the bundle) to be already available in
    the `loader.js` runtime registry.
    
    In the classic build, _every_ module gets compiled into AMD and
    gets added to the `loader.js` runtime registry. In Embroider,
    the goal is to do this as little as possible, to give the bundler
    more flexibility to optimize modules, or omit them entirely if it
    is confident that the module is unused (i.e. tree-shaking).
    
    Even in the most compatible mode, there are cases where Embroider
    is confident enough to omit modules in the runtime `loader.js`
    registry (notably, "auto-imported" non-addon NPM packages). So we
    have to be mindful of that an manage those dependencies ourselves,
    as seen in #22703.
    
    In the longer term, we will look into using modern features (such
    as `import()`) to express these inter-dependencies.
    
    - - -
    
    In the meantime, the Embroider build is not enabled by default as
    we found that there is a small regression in initial load, but
    once that is resolved it will replace the classic build pipeline
    (i.e. we don't plan on keep both options in parallel for longer
    than necessary).

---
+++ app/assets/javascripts/discourse/tests/test-helper.js
@@ -0,0 +1,7 @@
+/**
+ * 👋 Greetings Discourse Developer. This file is unused but is needed to keep
+ * the Embroider/Webpack build happy. In the classic build, test-helpers.js
+ * (plural!) is synthesized in ember-cli-build.js. In Embroider, it expects
+ * this file (singular!) to be here but is incorporated (`require()`-ed) in
+ * the test bundle unconditionally.
+ */
