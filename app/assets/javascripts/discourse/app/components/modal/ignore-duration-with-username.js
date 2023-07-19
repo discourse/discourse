00468136a9a60b7fcc498f1bb34c793cad607728
Ted Johansson <ted@discourse.org>
2023-07-19 16:37:53 +0800

    DEV: Convert IgnoreDurationWithUsername to Glimmer component

--- app/assets/javascripts/discourse/app/components/modal/ignore-duration-with-username.js
+++ app/assets/javascripts/discourse/app/components/modal/ignore-duration-with-username.js
@@ -1,17 +1,22 @@
-import Component from "@ember/component";
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 import I18n from "I18n";
 import User from "discourse/models/user";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import { timeShortcuts } from "discourse/lib/time-shortcut";
-import discourseComputed from "discourse-common/utils/decorators";
 
-export default Component.extend({
-  loading: false,
-  ignoredUntil: null,
-  ignoredUsername: null,
+export default class IgnoreDurationModal extends Component {
+  @service currentUser;
 
-  @discourseComputed
-  timeShortcuts() {
+  @tracked flash;
+
+  @tracked loading = false;
+  @tracked ignoredUntil = null;
+  @tracked ignoredUsername = null;
+
+  get timeShortcuts() {
     const timezone = this.currentUser.user_option.timezone;
     const shortcuts = timeShortcuts(timezone);
     return [
@@ -29,36 +34,35 @@ export default Component.extend({
       shortcuts.oneYear(),
       shortcuts.forever(),
     ];
-  },
+  }
 
-  actions: {
-    ignore() {
-      if (!this.ignoredUntil || !this.ignoredUsername) {
-        this.flash(
-          I18n.t("user.user_notifications.ignore_duration_time_frame_required"),
-          "error"
-        );
-        return;
-      }
-      this.set("loading", true);
-      User.findByUsername(this.ignoredUsername).then((user) => {
-        user
-          .updateNotificationLevel({
-            level: "ignore",
-            expiringAt: this.ignoredUntil,
-            actingUser: this.model,
-          })
-          .then(() => {
-            this.onUserIgnored(this.ignoredUsername);
-            this.args.closeModal();
-          })
-          .catch(popupAjaxError)
-          .finally(() => this.set("loading", false));
-      });
-    },
+  @action
+  ignore() {
+    if (!this.ignoredUntil || !this.ignoredUsername) {
+      this.flash = I18n.t(
+        "user.user_notifications.ignore_duration_time_frame_required"
+      );
+      return;
+    }
+    this.loading = true;
+    User.findByUsername(this.ignoredUsername).then((user) => {
+      user
+        .updateNotificationLevel({
+          level: "ignore",
+          expiringAt: this.ignoredUntil,
+          actingUser: this.args.model.actingUser,
+        })
+        .then(() => {
+          this.args.model.onUserIgnored(this.ignoredUsername);
+          this.args.closeModal();
+        })
+        .catch(popupAjaxError)
+        .finally(() => (this.loading = false));
+    });
+  }
 
-    updateIgnoredUsername(selected) {
-      this.set("ignoredUsername", selected.firstObject);
-    },
-  },
-});
+  @action
+  updateIgnoredUsername(selected) {
+    this.ignoredUsername = selected.firstObject;
+  }
+}
