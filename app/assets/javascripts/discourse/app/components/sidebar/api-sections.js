7762e0b9ddceb3191916d72706c12cadbf3f5450
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-07-25 12:02:26 +1000

    FIX: introduce api-section to avoid constructor in get

--- app/assets/javascripts/discourse/app/components/sidebar/api-sections.js
+++ app/assets/javascripts/discourse/app/components/sidebar/api-sections.js
@@ -1,23 +1,14 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
-import { getOwner, setOwner } from "@ember/application";
 
 export default class SidebarApiSections extends Component {
   @service sidebarState;
 
   get sections() {
     if (this.sidebarState.combinedMode) {
-      this.allSections = this.sidebarState.panels
-        .map((panel) => panel.sections)
-        .flat();
+      return this.sidebarState.panels.map((panel) => panel.sections).flat();
     } else {
-      this.allSections = this.sidebarState.currentPanel.sections;
+      return this.sidebarState.currentPanel.sections;
     }
-
-    return this.allSections.map((customSection) => {
-      const section = new customSection();
-      setOwner(section, getOwner(this));
-      return section;
-    });
   }
 }
