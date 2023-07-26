87412163ff7ef378837c4f5b337e6a49d09074e4
Jarek Radosz <jradosz@gmail.com>
2023-07-26 12:08:01 +0200

    DEV: Move cursor to the end of fast-edit input

--- app/assets/javascripts/discourse/app/modifiers/auto-focus.js
+++ app/assets/javascripts/discourse/app/modifiers/auto-focus.js
@@ -3,10 +3,17 @@ import Modifier from "ember-modifier";
 export default class AutoFocusModifier extends Modifier {
   didFocus = false;
 
-  modify(element) {
-    if (!this.didFocus) {
-      element.focus();
-      this.didFocus = true;
+  modify(element, _, { cursorPosition }) {
+    if (this.didFocus) {
+      return;
     }
+
+    if (cursorPosition === "end") {
+      const end = element.value.length;
+      element.setSelectionRange(end, end);
+    }
+
+    element.focus();
+    this.didFocus = true;
   }
 }
