discourse/discourse

commit 0b3cf83e3c7d03c2dcd54f86de60efe32f385a44

FIX: Do not cook icon with hashtags (#21676)
This commit makes some fundamental changes to how hashtag cooking and
icon generation works in the new experimental hashtag autocomplete mode.
Previously we cooked the appropriate SVG icon with the cooked hashtag,
though this has proved inflexible especially for theming purposes.

Instead, we now cook a data-ID attribute with the hashtag and add a new
span as an icon placeholder. This is replaced on the client side with an
icon (or a square span in the case of categories) on the client side via
the decorateCooked API for posts and chat messages.

This client side logic uses the generated hashtag, category, and channel
CSS classes added in a previous commit.

This is missing changes to the sidebar to use the new generated CSS
classes and also colors and the split square for categories in the
hashtag autocomplete menu -- I will tackle this in a separate PR so it
is clearer.

lib/excerpt_parser.rb
@@ -22,6 +22,7 @@ class ExcerptParser < Nokogiri::XML::SAX::Document
     @keep_svg = options[:keep_svg] == true
     @remap_emoji = options[:remap_emoji] == true
     @start_excerpt = false
+    @start_hashtag_icon = false
     @in_details_depth = 0
     @summary_contents = +""
     @detail_contents = +""
@@ -112,10 +113,14 @@ class ExcerptParser < Nokogiri::XML::SAX::Document
     when "header"
       @in_quote = !@keep_onebox_source if attributes.include?(%w[class source])
     when "div", "span"
-      if attributes.include?(%w[class excerpt])
+      attributes = Hash[*attributes.flatten]
+      if attributes["class"]&.include?("excerpt")
         @excerpt = +""
         @current_length = 0
         @start_excerpt = true
+      elsif attributes["class"]&.include?("hashtag-icon-placeholder")
+        @start_hashtag_icon = true
+        include_tag(name, attributes)
       end
     when "details"
       @detail_contents = +"" if @in_details_depth == 0
@@ -180,6 +185,7 @@ class ExcerptParser < Nokogiri::XML::SAX::Document
       @in_summary = false if @in_details_depth == 1
     when "div", "span"
       throw :done if @start_excerpt
+      characters("</span>", truncate: false, count_it: false, encode: false) if @start_hashtag_icon
     when "svg"
       characters("</svg>", truncate: false, count_it: false, encode: false) if @keep_svg
       @in_svg = false

app/assets/javascripts/discourse/app/initializers/hashtag-css-generator.js
@@ -25,8 +25,7 @@ export default {
 
     let generatedCssClasses = [];
 
-    Object.values(getHashtagTypeClasses()).forEach((hashtagTypeClass) => {
-      const hashtagType = new hashtagTypeClass(container);
+    Object.values(getHashtagTypeClasses()).forEach((hashtagType) => {
       hashtagType.preloadedData.forEach((model) => {
         generatedCssClasses = generatedCssClasses.concat(
           hashtagType.generateColorCssClasses(model)

app/assets/javascripts/discourse/app/initializers/hashtag-post-decorations.js
@@ -1,0 +1,24 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { replaceHashtagIconPlaceholder } from "discourse/lib/hashtag-autocomplete";
+
+export default {
+  name: "hashtag-post-decorations",
+  after: "hashtag-css-generator",
+
+  initialize(container) {
+    const siteSettings = container.lookup("service:site-settings");
+    const site = container.lookup("service:site");
+
+    withPluginApi("0.8.7", (api) => {
+      if (siteSettings.enable_experimental_hashtag_autocomplete) {
+        api.decorateCookedElement(
+          (post) => replaceHashtagIconPlaceholder(post, site),
+          {
+            onlyStream: true,
+            id: "hashtag-icons",
+          }
+        );
+      }
+    });
+  },
+};

app/assets/javascripts/discourse/app/initializers/register-hashtag-types.js
@@ -6,10 +6,10 @@ export default {
   name: "register-hashtag-types",
   before: "hashtag-css-generator",
 
-  initialize() {
+  initialize(container) {
     withPluginApi("0.8.7", (api) => {
-      api.registerHashtagType("category", CategoryHashtagType);
-      api.registerHashtagType("tag", TagHashtagType);
+      api.registerHashtagType("category", new CategoryHashtagType(container));
+      api.registerHashtagType("tag", new TagHashtagType(container));
     });
   },
 };

app/assets/javascripts/discourse/app/lib/click-track.js
@@ -46,7 +46,7 @@ export function isValidLink(link) {
   return (
     link.classList.contains("track-link") ||
     !link.closest(
-      ".hashtag, .hashtag-cooked, .badge-category, .onebox-result, .onebox-body"
+      ".hashtag, .hashtag-cooked, .hashtag-icon-placeholder, .badge-category, .onebox-result, .onebox-body"
     )
   );
 }

app/assets/javascripts/discourse/app/lib/hashtag-autocomplete.js
@@ -1,4 +1,5 @@
 import { findRawTemplate } from "discourse-common/lib/raw-templates";
+import domFromString from "discourse-common/lib/dom-from-string";
 import discourseLater from "discourse-common/lib/later";
 import { INPUT_DELAY, isTesting } from "discourse-common/config/environment";
 import { cancel } from "@ember/runloop";
@@ -15,14 +16,32 @@ import { emojiUnescape } from "discourse/lib/text";
 import { htmlSafe } from "@ember/template";
 
 let hashtagTypeClasses = {};
-export function registerHashtagType(type, typeClass) {
-  hashtagTypeClasses[type] = typeClass;
+export function registerHashtagType(type, typeClassInstance) {
+  hashtagTypeClasses[type] = typeClassInstance;
 }
 export function cleanUpHashtagTypeClasses() {
   hashtagTypeClasses = {};
 }
 export function getHashtagTypeClasses() {
   return hashtagTypeClasses;
+}
+export function replaceHashtagIconPlaceholder(element, site) {
+  element.querySelectorAll(".hashtag-cooked").forEach((hashtagEl) => {
+    const iconPlaceholderEl = hashtagEl.querySelector(
+      ".hashtag-icon-placeholder"
+    );
+    const hashtagType = hashtagEl.dataset.type;
+    const hashtagTypeClass = getHashtagTypeClasses()[hashtagType];
+    if (iconPlaceholderEl && hashtagTypeClass) {
+      const hashtagIconHTML = hashtagTypeClass
+        .generateIconHTML({
+          icon: site.hashtag_icons[hashtagType],
+          id: hashtagEl.dataset.id,
+        })
+        .trim();
+      iconPlaceholderEl.replaceWith(domFromString(hashtagIconHTML)[0]);
+    }
+  });
 }
 
 /**
@@ -216,12 +235,12 @@ function _searchRequest(term, contextualHashtagConfiguration, resultFunc) {
     data: { term, order: contextualHashtagConfiguration },
   });
   currentSearch
-    .then((r) => {
-      r.results?.forEach((result) => {
+    .then((response) => {
+      response.results?.forEach((result) => {
         // Convert :emoji: in the result text to HTML safely.
         result.text = htmlSafe(emojiUnescape(escapeExpression(result.text)));
       });
-      resultFunc(r.results || CANCELLED_STATUS);
+      resultFunc(response.results || CANCELLED_STATUS);
     })
     .finally(() => {
       currentSearch = null;
@@ -235,7 +254,7 @@ function _findAndReplaceSeenHashtagPlaceholder(
   hashtagSpan
 ) {
   contextualHashtagConfiguration.forEach((type) => {
-    // replace raw span for the hashtag with a cooked one
+    // Replace raw span for the hashtag with a cooked one
     const matchingSeenHashtag = seenHashtags[type]?.[slugRef];
     if (matchingSeenHashtag) {
       // NOTE: When changing the HTML structure here, you must also change
@@ -244,8 +263,12 @@ function _findAndReplaceSeenHashtagPlaceholder(
       link.classList.add("hashtag-cooked");
       link.href = matchingSeenHashtag.relative_url;
       link.dataset.type = type;
+      link.dataset.id = matchingSeenHashtag.id;
       link.dataset.slug = matchingSeenHashtag.slug;
-      link.innerHTML = `<svg class="fa d-icon d-icon-${matchingSeenHashtag.icon} svg-icon svg-node"><use href="#${matchingSeenHashtag.icon}"></use></svg><span>${matchingSeenHashtag.text}</span>`;
+      const hashtagType = new getHashtagTypeClasses()[type];
+      link.innerHTML = `${hashtagType.generateIconHTML(
+        matchingSeenHashtag
+      )}<span>${emojiUnescape(matchingSeenHashtag.text)}</span>`;
       hashtagSpan.replaceWith(link);
     }
   });

app/assets/javascripts/discourse/app/lib/hashtag-types/base.js
@@ -16,4 +16,8 @@ export default class HashtagTypeBase {
   generateColorCssClasses() {
     throw "not implemented";
   }
+
+  generateIconHTML() {
+    throw "not implemented";
+  }
 }

app/assets/javascripts/discourse/app/lib/hashtag-types/category.js
@@ -12,21 +12,25 @@ export default class CategoryHashtagType extends HashtagTypeBase {
     return this.site.categories || [];
   }
 
-  generateColorCssClasses(model) {
+  generateColorCssClasses(category) {
     const generatedCssClasses = [];
-    const backgroundGradient = [`var(--category-${model.id}-color) 50%`];
-    if (model.parentCategory) {
+    const backgroundGradient = [`var(--category-${category.id}-color) 50%`];
+    if (category.parentCategory) {
       backgroundGradient.push(
-        `var(--category-${model.parentCategory.id}-color) 50%`
+        `var(--category-${category.parentCategory.id}-color) 50%`
       );
     } else {
-      backgroundGradient.push(`var(--category-${model.id}-color) 50%`);
+      backgroundGradient.push(`var(--category-${category.id}-color) 50%`);
     }
 
-    generatedCssClasses.push(`.hashtag-color--category-${model.id} {
+    generatedCssClasses.push(`.hashtag-color--category-${category.id} {
   background: linear-gradient(90deg, ${backgroundGradient.join(", ")});
 }`);
 
     return generatedCssClasses;
+  }
+
+  generateIconHTML(hashtag) {
+    return `<span class="hashtag-category-badge hashtag-color--${this.type}-${hashtag.id}"></span>`;
   }
 }

commit 62bbbbda737d2da60912c21f60d7fc71a804ce52

FIX: Internal oneboxes with github links
Internal oneboxes to posts that contained oneboxed github links to
commits or PRs with long enough commit messages to have the `show-more`
and the `excerpt hidden` classes in their html were being stripped of
their content resulting in empty internal oneboxes.

see: https://meta.discourse.org/t/269436

This fixes a regression introduced in:

0b3cf83e3c7d03c2dcd54f86de60efe32f385a44

lib/excerpt_parser.rb
@@ -114,7 +114,11 @@ class ExcerptParser < Nokogiri::XML::SAX::Document
       @in_quote = !@keep_onebox_source if attributes.include?(%w[class source])
     when "div", "span"
       attributes = Hash[*attributes.flatten]
-      if attributes["class"]&.include?("excerpt")
+
+      # Only match "excerpt" class if it does specifically equal "excerpt
+      # hidden" in order to prevent internal links with GitHub oneboxes from
+      # being empty https://meta.discourse.org/t/269436
+      if attributes["class"]&.include?("excerpt") && !attributes["class"]&.match?("excerpt hidden")
         @excerpt = +""
         @current_length = 0
         @start_excerpt = true

spec/lib/excerpt_parser_spec.rb
@@ -110,6 +110,57 @@ RSpec.describe ExcerptParser do
         The first paragraph of this pinned topic will be &hellip;
       HTML
     end
+
+    it "keeps the content for internal oneboxes that contain github oneboxes" do
+      html = <<~HTML.strip
+        <p>Another commit to test out.</p>
+        <p>This time this commit has a longer commit message.</p>
+        <aside class="onebox githubcommit" data-onebox-src="https://github.com/discourse/discourse/commit/90f395a11895e9cfb7edd182b0bf5ec3d51d7892">
+          <header class="source">
+              <a href="https://github.com/discourse/discourse/commit/90f395a11895e9cfb7edd182b0bf5ec3d51d7892" target="_blank" rel="noopener">github.com/discourse/discourse</a>
+          </header>
+          <article class="onebox-body">
+            <div class="github-row">
+          <div class="github-icon-container" title="Commit">
+            <svg width="60" height="60" class="github-icon" viewBox="0 0 14 16" aria-hidden="true"><path fill-rule="evenodd" d="M10.86 7c-.45-1.72-2-3-3.86-3-1.86 0-3.41 1.28-3.86 3H0v2h3.14c.45 1.72 2 3 3.86 3 1.86 0 3.41-1.28 3.86-3H14V7h-3.14zM7 10.2c-1.22 0-2.2-.98-2.2-2.2 0-1.22.98-2.2 2.2-2.2 1.22 0 2.2.98 2.2 2.2 0 1.22-.98 2.2-2.2 2.2z"></path></svg>
+          </div>
+          <div class="github-info-container">
+            <h4>
+              <a href="https://github.com/discourse/discourse/commit/90f395a11895e9cfb7edd182b0bf5ec3d51d7892" target="_blank" rel="noopener">DEV: Skip srcset for onebox thumbnails (#22621)</a>
+            </h4>
+            <div class="github-info">
+              <div class="date">
+                committed <span class="discourse-local-date" data-format="ll" data-date="2023-07-19" data-time="18:21:34" data-timezone="UTC">06:21PM - 19 Jul 23 UTC (UTC)</span>
+              </div>
+              <div class="user">
+                <a href="https://github.com/oblakeerickson" target="_blank" rel="noopener">
+                  <img alt="oblakeerickson" src="//localhost:3000/uploads/default/original/1X/741ac99d6a66d71cdd46dd99fb5156506e13fdf2.jpeg" class="onebox-avatar-inline" width="20" height="20" data-dominant-color="3C3C3C">
+                  oblakeerickson
+                </a>
+              </div>
+              <div class="lines" title="changed 2 files with 24 additions and 15 deletions">
+                <a href="https://github.com/discourse/discourse/commit/90f395a11895e9cfb7edd182b0bf5ec3d51d7892" target="_blank" rel="noopener">
+                  <span class="added">+24</span>
+                  <span class="removed">-15</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+          <div class="github-row">
+            <p class="github-body-container">* DEV: Test commit message
+        This is a longer commit message <span class="show-more-container"><a href="https://github.com/discourse/discourse/commit/90f395a11895e9cfb7edd182b0bf5ec3d51d7892" target="_blank" rel="noopener" class="show-more">…</a></span><span class="excerpt hidden">that has the show-more class along with the exerpt hidden class</span></p>
+          </div>
+          </article>
+          <div class="onebox-metadata">
+          </div>
+          <div style="clear: both"></div>
+        </aside>
+      HTML
+      expect(ExcerptParser.get_excerpt(html, 100, keep_onebox_body: false)).to eq(<<~HTML.strip)
+        Another commit to test out. \nThis time this commit has a longer commit message.
+      HTML
+    end
   end
 
   describe "keep_quotes parameter" do