400ff70e64eeb82f37a3032ba2a93c5158f488ad
Isaac Janzen <isaac.janzen@discourse.org>
2023-07-27 16:00:27 -0500

    cleanup v2

--- app/assets/javascripts/discourse/app/components/modal/convert-to-public-topic.js
+++ app/assets/javascripts/discourse/app/components/modal/convert-to-public-topic.js
@@ -1,5 +1,5 @@
 import Component from "@glimmer/component";
-import tracked from "@glimmer/tracking";
+import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 
@@ -8,6 +8,7 @@ export default class ConvertToPublicTopic extends Component {
 
   @tracked publicCategoryId;
   @tracked saving = false;
+  @tracked flash;
 
   @action
   async makePublic() {
