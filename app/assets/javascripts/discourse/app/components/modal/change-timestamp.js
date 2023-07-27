66e2dd52e1944eaf6cc3e8735f69b0ac284510ab
Isaac Janzen <isaac.janzen@discourse.org>
2023-07-27 14:06:07 -0500

    cleanup

--- app/assets/javascripts/discourse/app/components/modal/change-timestamp.js
+++ app/assets/javascripts/discourse/app/components/modal/change-timestamp.js
@@ -6,6 +6,7 @@ import { next } from "@ember/runloop";
 import { isEmpty } from "@ember/utils";
 import moment from "moment";
 import I18n from "I18n";
+import Topic from "discourse/models/topic";
 
 // Modal related to changing the timestamp of posts
 export default class ChangeTimestamp extends Component {
