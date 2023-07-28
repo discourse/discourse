23c2fd5efeaf446d43ffacb068bedfde0941569b
Penar Musaraj <pmusaraj@gmail.com>
2023-07-28 12:56:38 +0100

    SECURITY: Limit length of edit reason column

--- app/models/post.rb
+++ app/models/post.rb
@@ -63,6 +63,7 @@ class Post < ActiveRecord::Base
   has_many :post_hotlinked_media, dependent: :destroy, class_name: "PostHotlinkedMedia"
 
   validates_with PostValidator, unless: :skip_validation
+  validates :edit_reason, length: { maximum: 1000 }
 
   after_commit :index_search
 
