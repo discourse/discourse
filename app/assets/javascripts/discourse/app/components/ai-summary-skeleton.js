65094fb5f3790e981529213285b886ed2ec74ebb
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-07-28 16:09:28 +0200

    trying a different pattern

--- app/assets/javascripts/discourse/app/components/ai-summary-skeleton.js
+++ app/assets/javascripts/discourse/app/components/ai-summary-skeleton.js
@@ -1,49 +1,72 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+import { TrackedArray } from "@ember-compat/tracked-built-ins";
+import discourseLater from "discourse-common/lib/later";
+import { cancel } from "@ember/runloop";
+
+class Block {
+  @tracked show = true;
+  @tracked shown = true;
+  @tracked blinking = false;
+}
+
+const ANIMATION_TIME = 500;
+const BLOCKS_SIZE = 20; // changing this requires to change css accordingly
 
 export default class AiSummarySkeleton extends Component {
-  intId = null;
+  @tracked blocks = new TrackedArray([]);
+
+  #onBlockBlinkingTimer;
+  #onBlockAddedTimer;
 
   @action
-  startAnimation(element) {
-    let ulSize = 20;
-    let index = 0;
-
-    const elements = element.getElementsByTagName("li");
-
-    const show = () => {
-      let liElement = elements[index];
-      if (liElement && index < ulSize) {
-        liElement.classList.remove("blink");
-        liElement.classList.add("show");
-        liElement.classList.add("is-shown");
-        index++;
-      } else if (index === ulSize) {
-        index = 0;
-        clearInterval(this.intId);
-        this.intId = setInterval(() => blink(), 250);
-      }
-    };
-
-    const blink = () => {
-      let liElement = elements[index];
-      if (liElement && index < ulSize) {
-        liElement.classList.remove("show");
-        liElement.classList.add("blink");
-        index++;
-      } else if (index === ulSize) {
-        for (let i = 0; i <= elements.length - 1; i++) {
-          elements[i].classList.remove("blink");
-        }
-        index = 0;
-      }
-    };
-
-    this.intId = setInterval(() => show(), 250);
+  setupAnimation() {
+    this.blocks.push(new Block());
   }
 
   @action
-  stopAnimation() {
-    clearInterval(this.intId);
+  onBlinking(block) {
+    if (!block.blinking) {
+      return;
+    }
+
+    block.show = false;
+
+    this.#onBlockBlinkingTimer = discourseLater(
+      this,
+      () => {
+        block.blinking = false;
+        const currentIndex = this.blocks.indexOf(block);
+        if (currentIndex === this.blocks.length - 1) {
+          this.blocks.firstObject.blinking = true;
+        } else {
+          this.blocks.objectAt(currentIndex + 1).blinking = true;
+        }
+      },
+      ANIMATION_TIME
+    );
+  }
+
+  @action
+  teardownAnimation() {
+    cancel(this.#onBlockBlinkingTimer);
+    cancel(this.#onBlockAddedTimer);
+  }
+
+  @action
+  onBlockAdded() {
+    if (this.blocks.length === BLOCKS_SIZE) {
+      this.blocks.firstObject.blinking = true;
+      return;
+    }
+
+    this.#onBlockAddedTimer = discourseLater(
+      this,
+      () => {
+        this.blocks.push(new Block());
+      },
+      ANIMATION_TIME
+    );
   }
 }
