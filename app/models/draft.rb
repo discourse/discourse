c4d0bbce6207f08e56a63a51716572dcb43107a5
Ted Johansson <ted@discourse.org>
2023-07-31 10:16:23 +0800

    DEV: Delete upload references upon deleting draft (#22851)
    We currently are accumulating orphaned upload references whenever drafts are deleted.
    
    This change deals with future cases by adding a dependent strategy of delete_all on the Draft#upload_references association. (We don't really need destroy strategy here, since UploadReference is a simple data bag and there are no validations or callbacks on the model.)
    
    It deals with existing cases through a migration that deletes all existing, orphaned draft upload references.

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -7,6 +7,8 @@ class Draft < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :upload_references, as: :target, dependent: :delete_all
+
   after_commit :update_draft_count, on: %i[create destroy]
 
   class OutOfSequence < StandardError
