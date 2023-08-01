6c8af90f5dce7ca94efffd053459cc77e10bb7d3
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2023-08-01 08:40:25 -0500

    DEV: Convert `start-backup` modal to component-based API (#22812)

--- app/assets/javascripts/discourse/app/services/modal.js
+++ app/assets/javascripts/discourse/app/services/modal.js
@@ -51,7 +51,6 @@ const KNOWN_LEGACY_MODALS = [
   "topic-summary",
   "user-status",
   "admin-merge-users-prompt",
-  "admin-start-backup",
   "admin-watched-word-test",
   "admin-api-key-urls",
   "admin-delete-user-posts-progress",
