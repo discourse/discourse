0e9ddca64672fbbe165d2ab2cd86f1bd3a479b3c
Jordan Vidrine <jordan@jordanvidrine.com>
2023-08-02 13:17:03 -0500

    UX: Add loading animations

--- app/assets/javascripts/admin/addon/controllers/admin-plugins-index.js
+++ app/assets/javascripts/admin/addon/controllers/admin-plugins-index.js
@@ -2,19 +2,23 @@ import Controller from "@ember/controller";
 import { action, set } from "@ember/object";
 import { inject as service } from "@ember/service";
 import SiteSetting from "admin/models/site-setting";
+import { tracked } from "@glimmer/tracking";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 
 export default class AdminPluginsIndexController extends Controller {
   @service session;
+  @tracked isLoading = false;
 
   @action
   async togglePluginEnabled(plugin) {
     const enabledSettingName = plugin.enabled_setting;
     const enabled = plugin.enabled;
+    this.isLoading = true;
 
     try {
       await SiteSetting.update(enabledSettingName, !enabled);
       set(plugin, "enabled", !enabled);
+      this.isLoading = false;
       this.session.requiresRefresh = true;
     } catch (e) {
       popupAjaxError(e);
