discourse/discourse

commit f5e8e737ad3fd7939f2c102d9dcbc5948294e0bf

UX: Compact option for multi-selects (#22239)
Adds an alternative to the default multi select item, better suited for quickly adding/removing tags.

app/assets/javascripts/select-kit/addon/components/select-kit.js
@@ -114,6 +114,7 @@ export default Component.extend(
           highlightPrevious: bind(this, this._highlightPrevious),
           highlightLast: bind(this, this._highlightLast),
           highlightFirst: bind(this, this._highlightFirst),
+          deselectLast: bind(this, this._deselectLast),
           change: bind(this, this._onChangeWrapper),
           select: bind(this, this.select),
           deselect: bind(this, this.deselect),
@@ -295,6 +296,7 @@ export default Component.extend(
       minimum: null,
       autoInsertNoneItem: true,
       closeOnChange: true,
+      useHeaderFilter: false,
       limitMatches: null,
       placement: isDocumentRTL() ? "bottom-end" : "bottom-start",
       verticalOffset: 3,
@@ -798,6 +800,12 @@ export default Component.extend(
       if (highlighted) {
         this._scrollToRow(highlighted, false);
         this.set("selectKit.highlighted", highlighted);
+      }
+    },
+
+    _deselectLast() {
+      if (this.selectKit.hasSelection) {
+        this.deselectByValue(this.value[this.value.length - 1]);
       }
     },
 

app/assets/javascripts/discourse/app/templates/topic.hbs
@@ -72,6 +72,8 @@
                   filterable=true
                   categoryId=this.buffered.category_id
                   minimum=this.minimumRequiredTags
+                  filterPlaceholder="topic_edit.tag_filter_placeholder"
+                  useHeaderFilter=true
                 }}
               />
             {{/if}}

app/assets/javascripts/discourse/tests/integration/components/select-kit/mini-tag-chooser-test.js
@@ -1,6 +1,6 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
-import { render } from "@ember/test-helpers";
+import { click, render, triggerKeyEvent } from "@ember/test-helpers";
 import { exists, query, queryAll } from "discourse/tests/helpers/qunit-helpers";
 import I18n from "I18n";
 import { hbs } from "ember-cli-htmlbars";
@@ -145,6 +145,67 @@ module(
         ),
         ["bar"]
       );
+    });
+  }
+);
+
+module(
+  "Integration | Component | select-kit/mini-tag-chooser useHeaderFilter=true",
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    hooks.beforeEach(function () {
+      this.set("subject", selectKit());
+    });
+
+    test("displays tags and filter in header", async function (assert) {
+      this.set("value", ["apple", "orange", "potato"]);
+
+      await render(
+        hbs`<MiniTagChooser @value={{this.value}} @options={{hash filterable=true useHeaderFilter=true}} />`
+      );
+
+      assert.strictEqual(this.subject.header().value(), "apple,orange,potato");
+
+      assert.dom(".select-kit-header--filter").exists();
+      assert.dom(".select-kit-header button[data-name='apple']").exists();
+      assert.dom(".select-kit-header button[data-name='orange']").exists();
+      assert.dom(".select-kit-header button[data-name='potato']").exists();
+
+      const filterInput = ".select-kit-header .filter-input";
+      await click(filterInput);
+
+      await triggerKeyEvent(filterInput, "keydown", "ArrowDown");
+      await triggerKeyEvent(filterInput, "keydown", "Enter");
+
+      assert.dom(".select-kit-header button[data-name='monkey']").exists();
+
+      await triggerKeyEvent(filterInput, "keydown", "Backspace");
+
+      assert
+        .dom(".select-kit-header button[data-name='monkey']")
+        .doesNotExist();
+
+      await this.subject.fillInFilter("foo");
+      await triggerKeyEvent(filterInput, "keydown", "Backspace");
+
+      assert.dom(".select-kit-header button[data-name='potato']").exists();
+    });
+
+    test("removing a tag does not display the dropdown", async function (assert) {
+      this.set("value", ["apple", "orange", "potato"]);
+
+      await render(
+        hbs`<MiniTagChooser @value={{this.value}} @options={{hash filterable=true useHeaderFilter=true}} />`
+      );
+
+      assert.strictEqual(this.subject.header().value(), "apple,orange,potato");
+
+      await click(".select-kit-header button[data-name='apple']");
+
+      assert.dom(".select-kit-collection").doesNotExist();
+      assert.dom(".select-kit-header button[data-name='apple']").doesNotExist();
+      assert.strictEqual(this.subject.header().value(), "orange,potato");
     });
   }
 );

app/assets/javascripts/select-kit/addon/components/mini-tag-chooser.js
@@ -26,6 +26,7 @@ export default MultiSelectComponent.extend(TagsMixin, {
     closeOnChange: false,
     maximum: "maxTagsPerTopic",
     autoInsertNoneItem: false,
+    useHeaderFilter: false,
   },
 
   modifyComponentForRow(collection, item) {

app/assets/javascripts/select-kit/addon/components/multi-select.hbs
@@ -12,23 +12,25 @@
     @selectKit={{this.selectKit}}
     @id={{concat this.selectKit.uniqueID "-body"}}
   >
-    {{component
-      this.selectKit.options.filterComponent
-      selectKit=this.selectKit
-      id=(concat this.selectKit.uniqueID "-filter")
-    }}
+    {{#unless this.selectKit.options.useHeaderFilter}}
+      {{component
+        this.selectKit.options.filterComponent
+        selectKit=this.selectKit
+        id=(concat this.selectKit.uniqueID "-filter")
+      }}
 
-    {{#if this.selectedContent.length}}
-      <div class="selected-content">
-        {{#each this.selectedContent as |item|}}
-          {{component
-            this.selectKit.options.selectedChoiceComponent
-            item=item
-            selectKit=this.selectKit
-          }}
-        {{/each}}
-      </div>
-    {{/if}}
+      {{#if this.selectedContent.length}}
+        <div class="selected-content">
+          {{#each this.selectedContent as |item|}}
+            {{component
+              this.selectKit.options.selectedChoiceComponent
+              item=item
+              selectKit=this.selectKit
+            }}
+          {{/each}}
+        </div>
+      {{/if}}
+    {{/unless}}
 
     {{#each this.collections as |collection|}}
       {{component

app/assets/javascripts/select-kit/addon/components/multi-select.js
@@ -21,6 +21,7 @@ export default SelectKitComponent.extend({
     autoFilterable: true,
     caretDownIcon: "caretIcon",
     caretUpIcon: "caretIcon",
+    useHeaderFilter: false,
   },
 
   caretIcon: computed("value.[]", function () {

app/assets/javascripts/select-kit/addon/components/multi-select/multi-select-header.hbs
@@ -3,10 +3,30 @@
     {{d-icon icon}}
   {{/each}}
 
-  <MultiSelect::FormatSelectedContent
-    @content={{or this.selectedContent this.selectKit.noneItem}}
-    @selectKit={{this.selectKit}}
-  />
+  {{#if this.selectKit.options.useHeaderFilter}}
+    <div class="select-kit-header--filter">
+      {{#if this.selectedContent.length}}
+        {{#each this.selectedContent as |item|}}
+          {{component
+            this.selectKit.options.selectedChoiceComponent
+            item=item
+            selectKit=this.selectKit
+          }}
+        {{/each}}
+      {{/if}}
 
-  {{d-icon this.caretIcon class="caret-icon"}}
+      {{component
+        this.selectKit.options.filterComponent
+        selectKit=this.selectKit
+        id=(concat this.selectKit.uniqueID "-filter")
+      }}
+    </div>
+  {{else}}
+    <MultiSelect::FormatSelectedContent
+      @content={{or this.selectedContent this.selectKit.noneItem}}
+      @selectKit={{this.selectKit}}
+    />
+
+    {{d-icon this.caretIcon class="caret-icon"}}
+  {{/if}}
 </div>

app/assets/javascripts/select-kit/addon/components/select-kit/select-kit-filter.js
@@ -79,6 +79,12 @@ export default Component.extend(UtilsMixin, {
       return true;
     }
 
+    if (event.key === "Backspace" && !this.selectKit.filter) {
+      this.selectKit.deselectLast();
+      event.preventDefault();
+      return false;
+    }
+
     if (event.key === "ArrowUp") {
       this.selectKit.highlightLast();
       event.preventDefault();
@@ -86,6 +92,9 @@ export default Component.extend(UtilsMixin, {
     }
 
     if (event.key === "ArrowDown") {
+      if (!this.selectKit.isExpanded) {
+        this.selectKit.open(event);
+      }
       this.selectKit.highlightFirst();
       event.preventDefault();
       return false;

commit 5dc3a276c8a628e449e5dad9d347e528a42d0c74

FIX: Compact tag picker input not focused in iOS (#22922)
Should fix an iOS regression in f5e8e73. iOS does not pull up the keyboard if the `.focus()` call is delayed by a rendering timeout or an asynchronous ajax call. This PR adds earlier `.focus()` calls if the input element is present.

app/assets/javascripts/select-kit/addon/components/select-kit.js
@@ -1004,6 +1004,10 @@ export default Component.extend(
           this.selectKit.options.filterable || this.selectKit.options.allowAny,
       });
 
+      if (this.selectKit.options.useHeaderFilter) {
+        this._focusFilterInput();
+      }
+
       this.triggerSearch();
 
       this._safeAfterRender(() => {
@@ -1044,19 +1048,33 @@ export default Component.extend(
         return;
       }
 
+      // setting focus as early as possible is best for iOS
+      // because render/promise delays may cause keyboard not to show
+      if (!forceHeader) {
+        this._focusFilterInput();
+      }
+
       this._safeAfterRender(() => {
         const input = this.getFilterInput();
         if (!forceHeader && input) {
-          input.focus({ preventScroll: true });
-
-          if (typeof input.selectionStart === "number") {
-            input.selectionStart = input.selectionEnd = input.value.length;
-          }
+          this._focusFilterInput();
         } else if (!this.selectKit.options.preventHeaderFocus) {
           const headerContainer = this.getHeader();
           headerContainer && headerContainer.focus({ preventScroll: true });
         }
       });
+    },
+
+    _focusFilterInput() {
+      const input = this.getFilterInput();
+
+      if (input && document.activeElement !== input) {
+        input.focus({ preventScroll: true });
+
+        if (typeof input.selectionStart === "number") {
+          input.selectionStart = input.selectionEnd = input.value.length;
+        }
+      }
     },
 
     getFilterInput() {