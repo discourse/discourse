bb0e3ce26f26967fc8183a5a53c8279905ff1abc
Keegan George <kgeorge13@gmail.com>
2023-08-03 11:55:40 -0700

    UX: New skeleton style

--- app/assets/javascripts/discourse-common/addon/helpers/i18n.js
+++ app/assets/javascripts/discourse-common/addon/helpers/i18n.js
@@ -1,6 +1,10 @@
 import I18n from "I18n";
 import { registerUnbound } from "discourse-common/lib/helpers";
 
+export default function i18n(key, params) {
+  return I18n.t(key, params);
+}
+
 registerUnbound("i18n", (key, params) => I18n.t(key, params));
 registerUnbound("i18n-yes-no", (value, params) =>
   I18n.t(value ? "yes_value" : "no_value", params)
