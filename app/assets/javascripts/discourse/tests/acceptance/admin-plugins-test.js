06e8c78ba09c318a39f3fff42637abca72921e85
David Taylor <david@taylorhq.com>
2023-08-03 11:31:09 +0100

    Update tests

--- app/assets/javascripts/discourse/tests/acceptance/admin-plugins-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/admin-plugins-test.js
@@ -1,9 +1,5 @@
-import {
-  acceptance,
-  exists,
-  query,
-} from "discourse/tests/helpers/qunit-helpers";
-import { visit } from "@ember/test-helpers";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
+import { click, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 
 acceptance("Admin - Plugins", function (needs) {
@@ -35,26 +31,39 @@ acceptance("Admin - Plugins", function (needs) {
         ],
       })
     );
+
+    server.put("/admin/site_settings/testplugin_enabled", () =>
+      helper.response()
+    );
   });
 
-  test("shows plugin list", async function (assert) {
+  test("shows plugin list and can toggle state", async function (assert) {
     await visit("/admin/plugins");
-    const table = query("table.admin-plugins");
-    assert.strictEqual(
-      table.querySelector("tr .plugin-name .name").innerText,
-      "some-test-plugin",
-      "displays the plugin in the table"
-    );
 
-    assert.true(
-      exists(".admin-plugins .admin-container .alert-error"),
-      "displays an error for unknown routes"
-    );
+    assert
+      .dom("table.admin-plugins tr .plugin-name .name")
+      .hasText("some-test-plugin", "displays the plugin in the table");
 
-    assert.strictEqual(
-      table.querySelector("tr .version a.commit-hash").href,
-      "https://github.com/username/some-test-plugin/commit/1234567890abcdef",
-      "displays a commit hash with a link to commit url"
-    );
+    assert
+      .dom(".admin-plugins .admin-container .alert-error")
+      .exists("shows an error for unknown routes");
+
+    assert
+      .dom("table.admin-plugins tr .version a.commit-hash")
+      .hasAttribute(
+        "href",
+        "https://github.com/username/some-test-plugin/commit/1234567890abcdef",
+        "displays a commit hash with a link to commit url"
+      );
+
+    const toggleSelector = "table.admin-plugins tr .col-enabled button";
+    assert
+      .dom(toggleSelector)
+      .hasAttribute("aria-checked", "true", "displays the plugin as enabled");
+
+    await click(toggleSelector);
+    assert
+      .dom(toggleSelector)
+      .hasAttribute("aria-checked", "false", "displays the plugin as enabled");
   });
 });
