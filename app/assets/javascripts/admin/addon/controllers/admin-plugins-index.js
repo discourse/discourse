593119671be158b8c8bbb4bf95e7ee340ae4a844
David Taylor <david@taylorhq.com>
2023-08-03 09:16:27 +0100

    Set loading state per-plugin

--- app/assets/javascripts/admin/addon/controllers/admin-plugins-index.js
+++ app/assets/javascripts/admin/addon/controllers/admin-plugins-index.js
@@ -2,26 +2,32 @@ import Controller from "@ember/controller";
 import { action, set } from "@ember/object";
 import { inject as service } from "@ember/service";
 import SiteSetting from "admin/models/site-setting";
-import { tracked } from "@glimmer/tracking";
 import { popupAjaxError } from "discourse/lib/ajax-error";
+import { TrackedMap } from "@ember-compat/tracked-built-ins";
 
 export default class AdminPluginsIndexController extends Controller {
   @service session;
-  @tracked isLoading = false;
+  loading = new TrackedMap();
 
   @action
   async togglePluginEnabled(plugin) {
     const enabledSettingName = plugin.enabled_setting;
     const enabled = plugin.enabled;
-    this.isLoading = true;
+    this.loading.set(plugin, true);
 
     try {
       await SiteSetting.update(enabledSettingName, !enabled);
       set(plugin, "enabled", !enabled);
-      this.isLoading = false;
       this.session.requiresRefresh = true;
     } catch (e) {
       popupAjaxError(e);
+    } finally {
+      this.loading.delete(plugin);
     }
+  }
+
+  @action
+  isLoading(plugin) {
+    return !!this.loading.get(plugin);
   }
 }
