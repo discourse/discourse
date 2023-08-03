discourse/discourse

commit e214fc38ed14143e702a9e3f2d2b7a2a0ffbc572

FIX: in modal should check for body (#22642)
To decide to use flip behavior select-kit will check if it's located inside a modal as a modal will scroll if overflown, however, when locating the select-kit element in the footer or header this is not the case. This commit will deactivate `flip` modifier only when used inside modal body.

app/assets/javascripts/select-kit/addon/components/select-kit.js
@@ -876,7 +876,7 @@ export default Component.extend(
       this.selectKit.onOpen(event);
 
       if (!this.popper) {
-        const inModal = this.element.closest("#discourse-modal");
+        const inModal = this.element.closest("#discourse-modal .modal-body");
         const anchor = document.querySelector(
           `#${this.selectKit.uniqueID}-header`
         );

commit 00729c72c392c48f76be0663881da2b5770cc50a

FIX: A select-kit+modal regression
Regressed in https://github.com/discourse/discourse/pull/22642

`inModal` var is `.modal-body` itself now, so the changing the modal height failed.

app/assets/javascripts/select-kit/addon/components/select-kit.js
@@ -988,10 +988,9 @@ export default Component.extend(
               enabled: !!(inModal && this.site.mobileView),
               phase: "afterWrite",
               fn: ({ state }) => {
-                const modalBody = inModal.querySelector(".modal-body");
-                modalBody.style = "";
-                modalBody.style.height =
-                  modalBody.clientHeight + state.rects.popper.height + "px";
+                inModal.style = "";
+                inModal.style.height =
+                  inModal.clientHeight + state.rects.popper.height + "px";
               },
             },
           ],