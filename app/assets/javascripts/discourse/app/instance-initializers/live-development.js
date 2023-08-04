5018bab2c3689a0d0f630fd232bafaf01684b475
David Taylor <david@taylorhq.com>
2023-08-04 10:50:59 +0100

    DEV: In development, refresh client when theme changes are made
    This brings the theme development experience (via the discourse_theme cli) closer to the experience of making javascript changes in Discourse core/plugins via Ember CLI. Whenever a change is made to a non-css theme field, all clients will be instructed to immediately refresh via message-bus.

--- app/assets/javascripts/discourse/app/instance-initializers/live-development.js
+++ app/assets/javascripts/discourse/app/instance-initializers/live-development.js
@@ -7,7 +7,7 @@ import { bind } from "discourse-common/utils/decorators";
 export default {
   initialize(owner) {
     this.messageBus = owner.lookup("service:message-bus");
-    const session = owner.lookup("service:session");
+    this.session = owner.lookup("service:session");
 
     // Preserve preview_theme_id=## and pp=async-flamegraph parameters across pages
     const params = new URLSearchParams(window.location.search);
@@ -40,7 +40,7 @@ export default {
     this.messageBus.subscribe(
       "/file-change",
       this.onFileChange,
-      session.mbLastFileChangeId
+      this.session.mbLastFileChangeId
     );
   },
 
@@ -54,6 +54,14 @@ export default {
       if (me === "refresh") {
         // Refresh if necessary
         document.location.reload(true);
+      } else if (me === "development-mode-theme-changed") {
+        if (window.location.pathname.startsWith("/admin/customize/themes")) {
+          // don't refresh users on routes which make theme changes - would be very inconvenient.
+          // Instead, refresh on their next route navigation.
+          this.session.requiresRefresh = true;
+        } else {
+          document.location.reload(true);
+        }
       } else if (me.new_href && me.target) {
         let query = `link[data-target='${me.target}']`;
 
