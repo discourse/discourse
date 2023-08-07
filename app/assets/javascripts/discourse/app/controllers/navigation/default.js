a0e45a1e0c2b5a59fff9bb1f7cd9aa1c40af94bc
David Taylor <david@taylorhq.com>
2023-08-07 15:11:54 +0100

    DEV: Replace filter-mode mixin with lib functions (#22986)
    The filter-mode mixin was previously serving two distinct purposes via a complex arrangement of getters/setters:
    
    1. To calculate a filterMode, given values for category, filterType and noSubcategories
    
    2. To calculate a filterType, given a filterMode
    
    This commit splits the mixin into two functions, and updates all call sites to use them instead of the mixin.

--- app/assets/javascripts/discourse/app/controllers/navigation/default.js
+++ app/assets/javascripts/discourse/app/controllers/navigation/default.js
@@ -1,13 +1,26 @@
 import { inject as service } from "@ember/service";
 import Controller, { inject as controller } from "@ember/controller";
-import FilterModeMixin from "discourse/mixins/filter-mode";
 import { TRACKED_QUERY_PARAM_VALUE } from "discourse/lib/topic-list-tracked-filter";
+import { calculateFilterMode } from "discourse/lib/filter-mode";
+import { dependentKeyCompat } from "@ember/object/compat";
+import { tracked } from "@glimmer/tracking";
 
-export default class NavigationDefaultController extends Controller.extend(
-  FilterModeMixin
-) {
+export default class NavigationDefaultController extends Controller {
   @service router;
   @controller discovery;
+
+  @tracked category;
+  @tracked filterType;
+  @tracked noSubcategories;
+
+  @dependentKeyCompat
+  get filterMode() {
+    return calculateFilterMode({
+      category: this.category,
+      filterType: this.filterType,
+      noSubcategories: this.noSubcategories,
+    });
+  }
 
   get skipCategoriesNavItem() {
     return this.router.currentRoute.queryParams.f === TRACKED_QUERY_PARAM_VALUE;
