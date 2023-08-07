discourse/discourse

commit 30c152c5a7127790a16482321b68650514ad16c9

FIX: Only use lastViewedTopic when going 'back' to a topic list (#22594)
Using the lastViewedTopicId indiscriminately can cause strange scrolling behavior when navigating to a **different** topic list after viewing a topic. We only want to refocus the topic when going 'back' to the same topic list which originally triggered the navigation.

app/assets/javascripts/discourse/app/components/topic-list-item.js
@@ -13,6 +13,7 @@ import { schedule } from "@ember/runloop";
 import { topicTitleDecorators } from "discourse/components/topic-title";
 import { wantsNewWindow } from "discourse/lib/intercept-click";
 import { htmlSafe } from "@ember/template";
+import { inject as service } from "@ember/service";
 
 export function showEntrance(e) {
   let target = $(e.target);
@@ -39,11 +40,18 @@ export function navigateToTopic(topic, href) {
     // so skip setting it early.
     this.appEvents.trigger("header:update-topic", topic);
   }
+
+  this.session.set("lastTopicIdViewed", {
+    topicId: topic.id,
+    historyUuid: this.router.location.getState?.().uuid,
+  });
+
   DiscourseURL.routeTo(href || topic.get("url"));
   return false;
 }
 
 export default Component.extend({
+  router: service(),
   tagName: "tr",
   classNameBindings: [":topic-list-item", "unboundClassNames", "topic.visited"],
   attributeBindings: ["data-topic-id", "role", "ariaLevel:aria-level"],
@@ -328,7 +336,14 @@ export default Component.extend({
 
   _highlightIfNeeded: on("didInsertElement", function () {
     // highlight the last topic viewed
-    if (this.session.get("lastTopicIdViewed") === this.get("topic.id")) {
+    const lastViewedTopicInfo = this.session.get("lastTopicIdViewed");
+
+    const isLastViewedTopic =
+      lastViewedTopicInfo?.topicId === this.topic.id &&
+      lastViewedTopicInfo?.historyUuid ===
+        this.router.location.getState?.().uuid;
+
+    if (isLastViewedTopic) {
       this.session.set("lastTopicIdViewed", null);
       this.highlight({ isLastViewedTopic: true });
     } else if (this.get("topic.highlight")) {

app/assets/javascripts/discourse/app/routes/topic.js
@@ -323,9 +323,6 @@ const TopicRoute = DiscourseRoute.extend({
   activate() {
     this._super(...arguments);
     this.set("isTransitioning", false);
-
-    const topic = this.modelFor("topic");
-    this.session.set("lastTopicIdViewed", parseInt(topic.get("id"), 10));
   },
 
   deactivate() {

spec/system/page_objects/components/topic_list.rb
@@ -34,6 +34,10 @@ module PageObjects
         find("#{TOPIC_LIST_BODY_SELECTOR} a", text: title).click
       end
 
+      def visit_topic(topic)
+        find("#{topic_list_item_class(topic)} a.raw-topic-link").click
+      end
+
       private
 
       def topic_list_item_class(topic)

spec/system/topic_list_focus_spec.rb
@@ -1,0 +1,40 @@
+# frozen_string_literal: true
+
+describe "Topic list focus", type: :system do
+  let!(:topics) { Fabricate.times(10, :post).map(&:topic) }
+
+  before { Fabricate(:admin) }
+
+  let(:discovery) { PageObjects::Pages::Discovery.new }
+  let(:topic) { PageObjects::Pages::Topic.new }
+
+  def focussed_topic_id
+    page.evaluate_script(
+      "document.activeElement.closest('.topic-list-item')?.dataset.topicId",
+    )&.to_i
+  end
+
+  it "refocusses last clicked topic when going back to topic list" do
+    visit("/")
+    expect(page).to have_css("body.navigation-topics")
+    expect(discovery.topic_list).to have_topics
+
+    # Click a topic
+    discovery.topic_list.visit_topic(topics[5])
+    expect(topic).to have_topic_title(topics[5].title)
+
+    # Going back to the topic-list should re-focus
+    page.go_back
+    expect(page).to have_css("body.navigation-topics")
+    expect(focussed_topic_id).to eq(topics[5].id)
+
+    # Click topic again
+    discovery.topic_list.visit_topic(topics[5])
+    expect(topic).to have_topic_title(topics[5].title)
+
+    # Visiting a topic list another way should not focus
+    find(".sidebar-section-link[data-link-name='everything']").click
+    expect(page).to have_css("body.navigation-topics")
+    expect(focussed_topic_id).to eq(nil)
+  end
+end

app/assets/javascripts/discourse/tests/acceptance/last-visited-topic-focus-test.js
@@ -1,26 +1,0 @@
-import { acceptance, query } from "discourse/tests/helpers/qunit-helpers";
-import { test } from "qunit";
-import { visit } from "@ember/test-helpers";
-import { cloneJSON } from "discourse-common/lib/object";
-import topicFixtures from "discourse/tests/fixtures/topic";
-
-acceptance("Last Visited Topic Focus", function (needs) {
-  needs.pretender((server, helper) => {
-    const fixture = cloneJSON(topicFixtures["/t/54077.json"]);
-    fixture.id = 11996;
-    fixture.slug =
-      "its-really-hard-to-navigate-the-create-topic-reply-pane-with-the-keyboard";
-    server.get("/t/11996.json", () => helper.response(fixture));
-  });
-  test("last visited topic receives focus when you return back to the topic list", async function (assert) {
-    await visit("/");
-    await visit(
-      "/t/its-really-hard-to-navigate-the-create-topic-reply-pane-with-the-keyboard/11996"
-    );
-    await visit("/");
-    const visitedTopicTitle = query(
-      '.topic-list-body tr[data-topic-id="11996"] .main-link'
-    );
-    assert.ok(visitedTopicTitle.classList.contains("focused"));
-  });
-});

commit bcdcea52bc86c043a0c970ec253ff7f49a76f393

A11Y: Fix selecting topic when navigation via keyboard
This fixes:
- a regression from 30c152c, where navigating to a topic's last reply
  via keyboard would lose track of the topic when returning to the topic
  list
- an issue where if a topic's last post is a small post, navigating to it
   via keyboard would not focus the post

app/assets/javascripts/discourse/app/components/topic-list-item.js
@@ -38,8 +38,6 @@ export function showEntrance(e) {
 export function navigateToTopic(topic, href) {
   const owner = getOwner(this);
   const siteSettings = owner.lookup("service:site-settings");
-  const router = owner.lookup("service:router");
-  const session = owner.lookup("service:session");
   const appEvents = owner.lookup("service:appEvents");
 
   if (siteSettings.page_loading_indicator !== "slider") {
@@ -47,11 +45,6 @@ export function navigateToTopic(topic, href) {
     // so skip setting it early.
     appEvents.trigger("header:update-topic", topic);
   }
-
-  session.set("lastTopicIdViewed", {
-    topicId: topic.id,
-    historyUuid: router.location.getState?.().uuid,
-  });
 
   DiscourseURL.routeTo(href || topic.get("url"));
   return false;

app/assets/javascripts/discourse/app/lib/utilities.js
@@ -76,6 +76,9 @@ export function highlightPost(postNumber) {
   if (!container) {
     return;
   }
+
+  container.querySelector(".tabLoc")?.focus();
+
   const element = container.querySelector(".topic-body");
   if (!element || element.classList.contains("highlighted")) {
     return;
@@ -88,7 +91,6 @@ export function highlightPost(postNumber) {
     element.removeEventListener("animationend", removeHighlighted);
   };
   element.addEventListener("animationend", removeHighlighted);
-  container.querySelector(".tabLoc").focus();
 }
 
 export function emailValid(email) {

app/assets/javascripts/discourse/app/routes/topic.js
@@ -21,6 +21,7 @@ const TopicRoute = DiscourseRoute.extend({
   composer: service(),
   screenTrack: service(),
   modal: service(),
+  router: service(),
 
   scheduledReplace: null,
   lastScrollPos: null,
@@ -327,6 +328,12 @@ const TopicRoute = DiscourseRoute.extend({
   activate() {
     this._super(...arguments);
     this.set("isTransitioning", false);
+
+    const topic = this.modelFor("topic");
+    this.session.set("lastTopicIdViewed", {
+      topicId: parseInt(topic.get("id"), 10),
+      historyUuid: this.router.location.getState?.().uuid,
+    });
   },
 
   deactivate() {

app/assets/javascripts/discourse/app/widgets/post-small-action.js
@@ -186,6 +186,9 @@ export default createWidget("post-small-action", {
     }
 
     return [
+      h("span.tabLoc", {
+        attributes: { "aria-hidden": true, tabindex: -1 },
+      }),
       h("div.topic-avatar", iconNode(icons[attrs.actionCode] || "exclamation")),
       h("div.small-action-desc", [
         h("div.small-action-contents", contents),

spec/system/page_objects/components/topic_list.rb
@@ -38,6 +38,10 @@ module PageObjects
         find("#{topic_list_item_class(topic)} a.raw-topic-link").click
       end
 
+      def visit_topic_last_reply_via_keyboard(topic)
+        find("#{topic_list_item_class(topic)} a.post-activity").native.send_keys(:return)
+      end
+
       private
 
       def topic_list_item_class(topic)

spec/system/topic_list_focus_spec.rb
@@ -36,5 +36,14 @@ describe "Topic list focus", type: :system do
     find(".sidebar-section-link[data-link-name='everything']").click
     expect(page).to have_css("body.navigation-topics")
     expect(focussed_topic_id).to eq(nil)
+
+    # Visit topic via keyboard
+    discovery.topic_list.visit_topic_last_reply_via_keyboard(topics[2])
+    expect(topic).to have_topic_title(topics[2].title)
+
+    # Going back to the topic-list should re-focus
+    page.go_back
+    expect(page).to have_css("body.navigation-topics")
+    expect(focussed_topic_id).to eq(topics[2].id)
   end
 end