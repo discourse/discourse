c63618a11c16c0e0a03a86732e23e033325e6b9b
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-08-08 13:38:50 +1000

    FIX: disable button

--- app/assets/javascripts/discourse/app/components/sidebar/switch-panel-buttons.js
+++ app/assets/javascripts/discourse/app/components/sidebar/switch-panel-buttons.js
@@ -1,18 +1,20 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
 export default class SwitchPanelButtons extends Component {
   @service router;
   @service sidebarState;
+  @tracked switching = false;
 
   @action
   switchPanel(panel) {
+    this.switching = true;
     this.sidebarState.currentPanel.lastKnownURL = this.router.currentURL;
     const url = panel.lastKnownURL || panel.switchButtonDefaultUrl;
     const destination = url === "/" ? "discovery.latest" : url;
-    this.router
-      .transitionTo(destination)
-      .then(() => this.sidebarState.setPanel(panel.key));
+    this.router.transitionTo(destination).then(() => (this.switching = false));
+    this.sidebarState.setPanel(panel.key);
   }
 }
