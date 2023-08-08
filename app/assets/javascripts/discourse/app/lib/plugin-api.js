35249dd2117c105193deb445375e44bd39bdd2c7
hawm <20796940+hawm@users.noreply.github.com>
2023-08-08 14:01:28 +0000

    FIX: Correct semantic versioning

--- app/assets/javascripts/discourse/app/lib/plugin-api.js
+++ app/assets/javascripts/discourse/app/lib/plugin-api.js
@@ -131,7 +131,7 @@ import { _addBulkButton } from "discourse/components/modal/topic-bulk-actions";
 // based on Semantic Versioning 2.0.0. Please update the changelog at
 // docs/CHANGELOG-JAVASCRIPT-PLUGIN-API.md whenever you change the version
 // using the format described at https://keepachangelog.com/en/1.0.0/.
-export const PLUGIN_API_VERSION = "1.8.1";
+export const PLUGIN_API_VERSION = "1.8.0";
 
 // This helper prevents us from applying the same `modifyClass` over and over in test mode.
 function canModify(klass, type, resolverName, changes) {
--- docs/CHANGELOG-JAVASCRIPT-PLUGIN-API.md
+++ docs/CHANGELOG-JAVASCRIPT-PLUGIN-API.md
@@ -7,7 +7,7 @@ in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.8.1] - 2023-07-26
+## [1.8.0] - 2023-07-26
 
 ### Added
 
