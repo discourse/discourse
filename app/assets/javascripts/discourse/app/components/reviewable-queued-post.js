61571bee43eae88755b5e258e96d03c146f9d2cb
Keegan George <kgeorge13@gmail.com>
2023-08-14 10:11:30 -0700

    UX: Add show more button to long post queued reviewables (#23075)

--- app/assets/javascripts/discourse/app/components/reviewable-queued-post.js
+++ app/assets/javascripts/discourse/app/components/reviewable-queued-post.js
@@ -1,11 +1,51 @@
-import Component from "@ember/component";
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import showModal from "discourse/lib/show-modal";
 
-export default Component.extend({
+export default class ReviewableQueuedPost extends Component {
+  @tracked isCollapsed = false;
+  @tracked isLongPost = false;
+  @tracked postBodyHeight = 0;
+  maxPostHeight = 300;
+
   @action
   showRawEmail(event) {
     event?.preventDefault();
-    showModal("raw-email").set("rawEmail", this.reviewable.payload.raw_email);
-  },
-});
+    showModal("raw-email").set(
+      "rawEmail",
+      this.args.reviewable.payload.raw_email
+    );
+  }
+
+  @action
+  toggleContent() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  get collapseButtonProps() {
+    if (this.isCollapsed) {
+      return {
+        label: "review.show_more",
+        icon: "chevron-down",
+      };
+    }
+    return {
+      label: "review.show_less",
+      icon: "chevron-up",
+    };
+  }
+
+  @action
+  setPostBodyHeight(offsetHeight) {
+    this.postBodyHeight = offsetHeight;
+
+    if (this.postBodyHeight > this.maxPostHeight) {
+      this.isCollapsed = true;
+      this.isLongPost = true;
+    } else {
+      this.isCollapsed = false;
+      this.isLongPost = false;
+    }
+  }
+}
