d75991dcbe6b4d769785ef6f81f60cd667044e12
Isaac Janzen <isaac.janzen@discourse.org>
2023-08-15 15:28:28 -0500

    WIP - async `login` action

--- app/assets/javascripts/discourse/app/components/modal/login.js
+++ app/assets/javascripts/discourse/app/components/modal/login.js
@@ -9,7 +9,6 @@ import { ajax } from "discourse/lib/ajax";
 import { findAll } from "discourse/models/login-method";
 import showModal from "discourse/lib/show-modal";
 import { areCookiesEnabled, escapeExpression } from "discourse/lib/utilities";
-import { flashAjaxError } from "discourse/lib/ajax-error";
 import { setting } from "discourse/lib/computed";
 import { wavingHandURL } from "discourse/lib/waving-hand-url";
 import { getOwner } from "discourse-common/lib/get-owner";
@@ -155,146 +154,145 @@ export default class Login extends Component {
   }
 
   @action
-  login() {
+  async login() {
     if (this.loginDisabled) {
       return;
     }
 
     if (isEmpty(this.loginName) || isEmpty(this.loginPassword)) {
-      this.flash(I18n.t("login.blank_username_or_password"), "error");
+      this.flash = I18n.t("login.blank_username_or_password");
+      this.flashType = "error";
       return;
     }
 
-    this.loggingIn = true;
+    try {
+      this.loggingIn = true;
+      const result = await ajax("/session", {
+        type: "POST",
+        data: {
+          login: this.loginName,
+          password: this.loginPassword,
+          // secondFactorToken is not getting set anywhere, looks like a problem
+          second_factor_token:
+            this.securityKeyCredential || this.secondFactorToken,
+          second_factor_method: this.secondFactorMethod,
+          timezone: moment.tz.guess(),
+        },
+      });
+      if (result && result.error) {
+        this.loggingIn = false;
+        this.flash = null;
 
-    ajax("/session", {
-      type: "POST",
-      data: {
-        login: this.loginName,
-        password: this.loginPassword,
-        // secondFactorToken is not getting set anywhere, looks like a problem
-        second_factor_token:
-          this.securityKeyCredential || this.secondFactorToken,
-        second_factor_method: this.secondFactorMethod,
-        timezone: moment.tz.guess(),
-      },
-    }).then(
-      (result) => {
-        // Successful login
-        if (result && result.error) {
-          this.set("loggingIn", false);
-          this.clearFlash();
-
-          if (
-            (result.security_key_enabled || result.totp_enabled) &&
-            !this.secondFactorRequired
-          ) {
-            this.setProperties({
-              otherMethodAllowed: result.multiple_second_factor_methods,
-              secondFactorRequired: true,
-              showLoginButtons: false,
-              backupEnabled: result.backup_enabled,
-              totpEnabled: result.totp_enabled,
-              showSecondFactor: result.totp_enabled,
-              showSecurityKey: result.security_key_enabled,
-              secondFactorMethod: result.security_key_enabled
-                ? SECOND_FACTOR_METHODS.SECURITY_KEY
-                : SECOND_FACTOR_METHODS.TOTP,
-              securityKeyChallenge: result.challenge,
-              securityKeyAllowedCredentialIds: result.allowed_credential_ids,
-            });
-
-            // only need to focus the 2FA input for TOTP
-            if (!this.showSecurityKey) {
-              schedule("afterRender", () =>
-                document
-                  .getElementById("second-factor")
-                  .querySelector("input")
-                  .focus()
-              );
-            }
-
-            return;
-          } else if (result.reason === "not_activated") {
-            this.applicationController.send("showNotActivated", {
-              username: this.loginName,
-              sentTo: escape(result.sent_to_email),
-              currentEmail: escape(result.current_email),
-            });
-          } else if (result.reason === "suspended") {
-            this.args.closeModal();
-            this.dialog.alert(result.error);
-          } else {
-            this.flash = result.error;
-            this.flashType = "error";
-          }
-        } else {
-          this.loggedIn = true;
-          // Trigger the browser's password manager using the hidden static login form:
-          const hiddenLoginForm = document.getElementById("hidden-login-form");
-          const applyHiddenFormInputValue = (value, key) => {
-            if (!hiddenLoginForm) {
-              return;
-            }
-
-            hiddenLoginForm.querySelector(`input[name=${key}]`).value = value;
-          };
-
-          const destinationUrl = cookie("destination_url");
-          const ssoDestinationUrl = cookie("sso_destination_url");
-
-          applyHiddenFormInputValue(this.loginName, "username");
-          applyHiddenFormInputValue(this.loginPassword, "password");
-
-          if (ssoDestinationUrl) {
-            removeCookie("sso_destination_url");
-            window.location.assign(ssoDestinationUrl);
-            return;
-          } else if (destinationUrl) {
-            // redirect client to the original URL
-            removeCookie("destination_url");
-
-            applyHiddenFormInputValue(destinationUrl, "redirect");
-          } else {
-            applyHiddenFormInputValue(window.location.href, "redirect");
-          }
-
-          if (hiddenLoginForm) {
-            if (
-              navigator.userAgent.match(/(iPad|iPhone|iPod)/g) &&
-              navigator.userAgent.match(/Safari/g)
-            ) {
-              // In case of Safari on iOS do not submit hidden login form
-              window.location.href = hiddenLoginForm.querySelector(
-                "input[name=redirect]"
-              ).value;
-            } else {
-              hiddenLoginForm.submit();
-            }
-          }
-          return;
-        }
-      },
-      (e) => {
-        // Failed to login
-        if (e.jqXHR && e.jqXHR.status === 429) {
-          this.flash(I18n.t("login.rate_limit"), "error");
-        } else if (
-          e.jqXHR &&
-          e.jqXHR.status === 503 &&
-          e.jqXHR.responseJSON.error_type === "read_only"
+        if (
+          (result.security_key_enabled || result.totp_enabled) &&
+          !this.secondFactorRequired
         ) {
-          this.flash(I18n.t("read_only_mode.login_disabled"), "error");
-        } else if (!areCookiesEnabled()) {
-          this.flash(I18n.t("login.cookies_error"), "error");
-        } else {
-          this.flash(I18n.t("login.error"), "error");
-        }
-        this.set("loggingIn", false);
-      }
-    );
+          this.setProperties({
+            otherMethodAllowed: result.multiple_second_factor_methods,
+            secondFactorRequired: true,
+            showLoginButtons: false,
+            backupEnabled: result.backup_enabled,
+            totpEnabled: result.totp_enabled,
+            showSecondFactor: result.totp_enabled,
+            showSecurityKey: result.security_key_enabled,
+            secondFactorMethod: result.security_key_enabled
+              ? SECOND_FACTOR_METHODS.SECURITY_KEY
+              : SECOND_FACTOR_METHODS.TOTP,
+            securityKeyChallenge: result.challenge,
+            securityKeyAllowedCredentialIds: result.allowed_credential_ids,
+          });
 
-    return false;
+          // only need to focus the 2FA input for TOTP
+          if (!this.showSecurityKey) {
+            schedule("afterRender", () =>
+              document
+                .getElementById("second-factor")
+                .querySelector("input")
+                .focus()
+            );
+          }
+
+          return;
+        } else if (result.reason === "not_activated") {
+          this.applicationController.send("showNotActivated", {
+            username: this.loginName,
+            sentTo: escape(result.sent_to_email),
+            currentEmail: escape(result.current_email),
+          });
+        } else if (result.reason === "suspended") {
+          this.args.closeModal();
+          this.dialog.alert(result.error);
+        } else {
+          this.flash = result.error;
+          this.flashType = "error";
+        }
+      } else {
+        this.loggedIn = true;
+        // Trigger the browser's password manager using the hidden static login form:
+        const hiddenLoginForm = document.getElementById("hidden-login-form");
+        const applyHiddenFormInputValue = (value, key) => {
+          if (!hiddenLoginForm) {
+            return;
+          }
+
+          hiddenLoginForm.querySelector(`input[name=${key}]`).value = value;
+        };
+
+        const destinationUrl = cookie("destination_url");
+        const ssoDestinationUrl = cookie("sso_destination_url");
+
+        applyHiddenFormInputValue(this.loginName, "username");
+        applyHiddenFormInputValue(this.loginPassword, "password");
+
+        if (ssoDestinationUrl) {
+          removeCookie("sso_destination_url");
+          window.location.assign(ssoDestinationUrl);
+          return;
+        } else if (destinationUrl) {
+          // redirect client to the original URL
+          removeCookie("destination_url");
+
+          applyHiddenFormInputValue(destinationUrl, "redirect");
+        } else {
+          applyHiddenFormInputValue(window.location.href, "redirect");
+        }
+
+        if (hiddenLoginForm) {
+          if (
+            navigator.userAgent.match(/(iPad|iPhone|iPod)/g) &&
+            navigator.userAgent.match(/Safari/g)
+          ) {
+            // In case of Safari on iOS do not submit hidden login form
+            window.location.href = hiddenLoginForm.querySelector(
+              "input[name=redirect]"
+            ).value;
+          } else {
+            hiddenLoginForm.submit();
+          }
+        }
+        return;
+      }
+    } catch (e) {
+      // Failed to login
+      if (e.jqXHR && e.jqXHR.status === 429) {
+        this.flash = I18n.t("login.rate_limit");
+        this.flashType = "error";
+      } else if (
+        e.jqXHR &&
+        e.jqXHR.status === 503 &&
+        e.jqXHR.responseJSON.error_type === "read_only"
+      ) {
+        this.flash = I18n.t("read_only_mode.login_disabled");
+        this.flashType = "error";
+      } else if (!areCookiesEnabled()) {
+        this.flash = I18n.t("login.cookies_error");
+        this.flashType = "error";
+      } else {
+        this.flash = I18n.t("login.error");
+        this.flashType = "error";
+      }
+      this.loggingIn = false;
+    }
   }
 
   // I don't think this lives in the right place, this feels like it should live in some kind of auth service / controller
