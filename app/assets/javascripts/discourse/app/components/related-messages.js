6a4a4a9705cf8528f5c6b4b8926504d93ba54b41
Roman Rizzi <rizziromanalejandro@gmail.com>
2023-08-17 15:53:09 -0300

    FIX: Inject currentUser service for related-messages

--- app/assets/javascripts/discourse/app/components/related-messages.js
+++ app/assets/javascripts/discourse/app/components/related-messages.js
@@ -5,6 +5,7 @@ import { inject as service } from "@ember/service";
 
 export default class RelatedMessages extends Component {
   @service moreTopicsPreferenceTracking;
+  @service currentUser;
 
   listId = "related-Messages";
 
