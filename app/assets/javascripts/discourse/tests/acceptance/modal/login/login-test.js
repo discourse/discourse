392a54e323f8b8e72d57f8ded8b2ef5bed85b22e
Isaac Janzen <isaac.janzen@discourse.org>
2023-08-17 17:04:16 -0500

    Add tabbing test

--- app/assets/javascripts/discourse/tests/acceptance/modal/login/login-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/modal/login/login-test.js
@@ -1,6 +1,7 @@
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import { test } from "qunit";
 import { click, tab, visit } from "@ember/test-helpers";
+import I18n from "I18n";
 
 acceptance("Modal - Login", function () {
   test("You can tab to the login button", async function (assert) {
@@ -10,5 +11,35 @@ acceptance("Modal - Login", function () {
     await tab({ unRestrainTabIndex: true });
     await tab({ unRestrainTabIndex: true });
     assert.dom(".modal-footer #login-button").isFocused();
+  });
+});
+
+acceptance("Modal - Login - With 2FA", function (needs) {
+  needs.settings({
+    enable_local_logins_via_email: true,
+  });
+
+  needs.pretender((server, helper) => {
+    server.post(`/session`, () =>
+      helper.response({
+        error: I18n.t("login.invalid_second_factor_code"),
+        multiple_second_factor_methods: false,
+        security_key_enabled: false,
+        totp_enabled: true,
+      })
+    );
+  });
+
+  test("You can tab to 2FA login button", async function (assert) {
+    await visit("/");
+    await click("header .login-button");
+
+    await fillIn("#login-account-name", "isaac@discourse.org");
+    await fillIn("#login-account-password", "password");
+    await click("#login-button");
+
+    assert.dom("#login-second-factor").isFocused();
+    await tab();
+    assert.dom("#login-button").isFocused();
   });
 });
