7d0e2b9e7b8b6252cd57ac8baeadff9861ee5bac
Roman Rizzi <rizziromanalejandro@gmail.com>
2023-08-22 14:10:15 -0300

    FIX: Keep track of suggestion updates during scrolling and navigation. (#23190)
    This PR changes how we track which lists are available for a topic and how we decide which is the active one. The new approach centralizes everything in the service, and exposes functions for adding/removing a list, which each calls via `did-insert/will-destroy` modifiers.
    
    It makes it much easier to track and update state when navigated to another topic or PM, ensuring things get updated correctly.

--- app/assets/javascripts/discourse/app/components/more-topics.js
+++ app/assets/javascripts/discourse/app/components/more-topics.js
@@ -1,8 +1,6 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
-import { next } from "@ember/runloop";
 import { action, computed } from "@ember/object";
-import { tracked } from "@glimmer/tracking";
 import I18n from "I18n";
 import { categoryBadgeHTML } from "discourse/helpers/category-link";
 import getURL from "discourse-common/lib/get-url";
@@ -15,59 +13,24 @@ export default class MoreTopics extends Component {
   @service topicTrackingState;
   @service currentUser;
 
-  @tracked availablePills = [];
-  @tracked singleList = false;
-
   @action
   rememberTopicListPreference(value) {
-    // Don't change the preference when selecting related PMs.
-    // It messes with the topics pref.
-    const rememberPref = value !== "related-messages";
-
-    this.moreTopicsPreferenceTracking.updatePreference(value, rememberPref);
-
-    this.buildListPills();
+    this.moreTopicsPreferenceTracking.updatePreference(value);
   }
 
-  @action
-  buildListPills() {
-    next(() => {
-      const pills = Array.from(
-        document.querySelectorAll(".more-topics__list")
-      ).map((topicList) => {
-        return {
-          name: topicList.dataset.mobileTitle,
-          id: topicList.dataset.listId,
-        };
-      });
+  @computed("moreTopicsPreferenceTracking.topicLists")
+  get singleList() {
+    return this.availableTabs.length === 1;
+  }
 
-      if (pills.length === 0) {
-        return;
-      } else if (pills.length === 1) {
-        this.singleList = true;
-      }
+  @computed("moreTopicsPreferenceTracking.selectedTab")
+  get selectedTab() {
+    return this.moreTopicsPreferenceTracking.selectedTab;
+  }
 
-      let preference = this.moreTopicsPreferenceTracking.preference;
-      // Scenario where we have a preference, but there
-      // are no more elements in it.
-      const listPresent = pills.find((pill) => pill.id === preference);
-
-      if (!listPresent) {
-        const rememberPref = false;
-
-        this.moreTopicsPreferenceTracking.updatePreference(
-          pills[0].id,
-          rememberPref
-        );
-        preference = pills[0].id;
-      }
-
-      pills.forEach((pill) => {
-        pill.selected = pill.id === preference;
-      });
-
-      this.availablePills = pills;
-    });
+  @computed("moreTopicsPreferenceTracking.topicLists")
+  get availableTabs() {
+    return this.moreTopicsPreferenceTracking.topicLists;
   }
 
   @computed(
