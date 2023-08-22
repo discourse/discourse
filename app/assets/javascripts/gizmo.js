67c7ba130496c782a8b38a2b9a18eb98f2d3baa8
Jarek Radosz <jradosz@gmail.com>
2023-08-22 23:39:26 +0200

    imports

--- app/assets/javascripts/gizmo.js
+++ app/assets/javascripts/gizmo.js
@@ -15,10 +15,15 @@ globalThis.console = {
   },
 };
 
-const processor = require("./discourse-js-processor");
+import {
+  compileRawTemplate,
+  getMinifyResult,
+  minify,
+  transpile,
+} from "./discourse-js-processor";
 
 // Make interfaces available via `v8.call`
-globalThis.compileRawTemplate = processor.compileRawTemplate;
-globalThis.transpile = processor.transpile;
-globalThis.minify = processor.minify;
-globalThis.getMinifyResult = processor.getMinifyResult;
+globalThis.compileRawTemplate = compileRawTemplate;
+globalThis.transpile = transpile;
+globalThis.minify = minify;
+globalThis.getMinifyResult = getMinifyResult;
