36c470773542704ded73642b47d4d5102545cc92
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-08-23 15:34:31 +0200

    UI: refines thread list item
    It will now replies count and participants list. Also the title will be OM excerpt or user defined title, no more default "Thread" title. Lastly, the author of the last reply is also shown as prefix of it.

--- plugins/chat/app/serializers/chat/thread_list_serializer.rb
+++ plugins/chat/app/serializers/chat/thread_list_serializer.rb
@@ -12,6 +12,7 @@ module Chat
           membership: object.memberships.find { |m| m.thread_id == thread.id },
           include_thread_preview: true,
           include_thread_original_message: true,
+          participants: object.threads_participants[thread.id],
           root: nil,
         )
       end
