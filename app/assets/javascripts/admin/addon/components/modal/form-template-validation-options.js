9692502416ca28e20c078e9ca5b5cea4e1346606
Isaac Janzen <isaac.janzen@discourse.org>
2023-08-24 14:48:08 -0500

    cleanup

--- app/assets/javascripts/admin/addon/components/modal/form-template-validation-options.js
+++ app/assets/javascripts/admin/addon/components/modal/form-template-validation-options.js
@@ -7,7 +7,7 @@ const VALIDATION_KEYS = ["required", "minimum", "maximum", "pattern", "type"];
 export default class FormTemplateValidationOptions extends Component {
   get tableHeaders() {
     const translatedHeaders = [];
-    this.TABLE_HEADER_KEYS.forEach((header) => {
+    TABLE_HEADER_KEYS.forEach((header) => {
       translatedHeaders.push(
         I18n.t(`admin.form_templates.validations_modal.table_headers.${header}`)
       );
@@ -19,7 +19,7 @@ export default class FormTemplateValidationOptions extends Component {
   get validations() {
     const translatedValidations = [];
     const prefix = "admin.form_templates.validations_modal.validations";
-    this.VALIDATION_KEYS.forEach((validation) => {
+    VALIDATION_KEYS.forEach((validation) => {
       translatedValidations.push({
         key: I18n.t(`${prefix}.${validation}.key`),
         type: I18n.t(`${prefix}.${validation}.type`),
