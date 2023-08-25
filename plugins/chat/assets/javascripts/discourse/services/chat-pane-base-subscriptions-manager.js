251ad5f7c99a515398ae7ad32251c65956eb371f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-08-25 18:01:44 +0200

    FIX: ensures we update cached model last message bus id
    Channels and threads are cached as much as possible, as a result the `last_message_bus_id` can become stalled.
    
    It was for example exhibited with the following actions:
    - open a channel (A)
    - send a message
    - navigate to another channel (B)
    - come back to channel (A), and you would actually get all the messages replayed since you opened (A) for the first time as the `last_message_bus_id` would only refresh on a full page reload
    
    This was technically not causing known bugs ATM, but was probably the source of few hard to repro bugs and would for sure cause issues in the future.
    
    <!-- NOTE: All pull requests should have tests (rspec in Ruby, qunit in JavaScript). If your code does not include test coverage, please include an explanation of why it was omitted. -->
    
    Co-Authored-By: Mark VanLandingham <markvanlan@gmail.com>

--- plugins/chat/assets/javascripts/discourse/services/chat-pane-base-subscriptions-manager.js
+++ plugins/chat/assets/javascripts/discourse/services/chat-pane-base-subscriptions-manager.js
@@ -39,21 +39,25 @@ export default class ChatPaneBaseSubscriptionsManager extends Service {
   @service currentUser;
   @service chatStagedThreadMapping;
 
-  get messageBusChannel() {
-    throw "not implemented";
-  }
-
-  get messageBusLastId() {
-    throw "not implemented";
-  }
+  messageBusChannel = null;
+  messageBusLastId = null;
 
   get messagesManager() {
     return this.model.messagesManager;
   }
 
+  beforeSubscribe() {}
+  afterMessage() {}
+
   subscribe(model) {
+    this.beforeSubscribe(model);
     this.unsubscribe();
     this.model = model;
+
+    if (!this.messageBusChannel || !this.messageBusLastId) {
+      return;
+    }
+
     this.messageBus.subscribe(
       this.messageBusChannel,
       this.onMessage,
@@ -120,6 +124,8 @@ export default class ChatPaneBaseSubscriptionsManager extends Service {
         this.handleNotice(busData);
         break;
     }
+
+    this.afterMessage(this.model, ...arguments);
   }
 
   handleSentMessage() {
