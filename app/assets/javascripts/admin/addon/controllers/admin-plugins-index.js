eeb11141ba0b7973c9372ee8bc53e5373d500a42
Martin Brennan <martin@discourse.org>
2023-08-25 14:41:57 +1000

    FIX: Improve admin plugin list and modernize code
    This commit contains a few improvements:
    
    * Use LinkTo instead of a button with a weird action referencing the
      controller to navigate to the filtered settings for a plugin
    * Add an AdminPlugin model with tracked properties and use that when
      toggling the setting on/off and in the templates
    * Make it so the Settings button for a plugin navigates to the correct
      site setting category instead of always just going to the generic
      "plugins" one if possible

--- app/assets/javascripts/admin/addon/controllers/admin-plugins-index.js
+++ app/assets/javascripts/admin/addon/controllers/admin-plugins-index.js
@@ -1,5 +1,5 @@
 import Controller from "@ember/controller";
-import { action, set } from "@ember/object";
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import SiteSetting from "admin/models/site-setting";
 import { popupAjaxError } from "discourse/lib/ajax-error";
@@ -9,16 +9,15 @@ export default class AdminPluginsIndexController extends Controller {
 
   @action
   async togglePluginEnabled(plugin) {
-    const enabledSettingName = plugin.enabled_setting;
-
     const oldValue = plugin.enabled;
     const newValue = !oldValue;
+
     try {
-      set(plugin, "enabled", newValue);
-      await SiteSetting.update(enabledSettingName, newValue);
+      plugin.enabled = newValue;
+      await SiteSetting.update(plugin.enabledSetting, newValue);
       this.session.requiresRefresh = true;
     } catch (e) {
-      set(plugin, "enabled", oldValue);
+      plugin.enabled = oldValue;
       popupAjaxError(e);
     }
   }
