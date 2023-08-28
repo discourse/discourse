800592d0a1b2afc348706f978743e28db979460b
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-08-28 16:41:27 +0400

    No need to specify type: "GET"
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>

--- app/assets/javascripts/discourse/app/services/users-api.js
+++ app/assets/javascripts/discourse/app/services/users-api.js
@@ -5,7 +5,6 @@ import User from "discourse/models/user";
 export default class UsersApiService extends Service {
   async lookupUsers(usernames) {
     const response = await ajax(`/u/lookup/users.json`, {
-      type: "GET",
       data: { usernames },
     });
     return response.users.map((u) => User.create(u));
