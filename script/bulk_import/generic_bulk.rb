ecee80ad8e9f7e92a5c7bf1277416a71d5460b0d
Gerhard Schlager <gerhard.schlager@discourse.org>
2023-08-30 12:14:55 +0200

    re-enable all import steps

--- script/bulk_import/generic_bulk.rb
+++ script/bulk_import/generic_bulk.rb
@@ -26,40 +26,40 @@ class BulkImport::Generic < BulkImport::Base
   end
 
   def execute
-    # import_uploads
-    #
-    # # needs to happen before users, because keeping group names is more important than usernames
-    # import_groups
-    #
-    # import_users
-    # import_user_emails
-    # import_user_profiles
-    # import_user_options
-    # import_user_fields
-    # import_user_custom_field_values
-    # import_single_sign_on_records
-    # import_muted_users
-    # import_user_histories
-    #
-    # import_user_avatars
-    # update_uploaded_avatar_id
-    #
-    # import_group_members
-    #
-    # import_categories
-    # import_category_permissions
-    #
-    # import_topics
-    # import_posts
-    # import_topic_allowed_users
-    # import_likes
-    # import_tags
-    # import_votes
+    import_uploads
+
+    # needs to happen before users, because keeping group names is more important than usernames
+    import_groups
+
+    import_users
+    import_user_emails
+    import_user_profiles
+    import_user_options
+    import_user_fields
+    import_user_custom_field_values
+    import_single_sign_on_records
+    import_muted_users
+    import_user_histories
+
+    import_user_avatars
+    update_uploaded_avatar_id
+
+    import_group_members
+
+    import_categories
+    import_category_permissions
+
+    import_topics
+    import_posts
+    import_topic_allowed_users
+    import_likes
+    import_tags
+    import_votes
     import_answers
 
-    # import_upload_references
-    # import_user_stats
-    # enable_category_settings
+    import_upload_references
+    import_user_stats
+    enable_category_settings
 
     @source_db.close
     @uploads_db.close if @uploads_db
