b8e6c8cef8fbe64d46d9b185952cac00549fd914
Jarek Radosz <jradosz@gmail.com>
2023-08-30 02:53:41 +0200

    no need

--- app/assets/javascripts/discourse/app/components/group-manage-logs-filter.js
+++ app/assets/javascripts/discourse/app/components/group-manage-logs-filter.js
@@ -1,7 +1,6 @@
 import Component from "@ember/component";
 import I18n from "I18n";
 import discourseComputed from "discourse-common/utils/decorators";
-import { action } from "@ember/object";
 
 export default Component.extend({
   tagName: "",
@@ -16,10 +15,5 @@ export default Component.extend({
     return type === "action"
       ? I18n.t(`group_histories.actions.${value}`)
       : value;
-  },
-
-  @action
-  clearFilter(param) {
-    this.clearFilter(param);
   },
 });
