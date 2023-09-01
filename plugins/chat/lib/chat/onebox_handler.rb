06ca02327d7b15191d1a1ca4d7d43416bfbd64c9
Jan Cernik <jancernik12@gmail.com>
2023-09-01 00:05:08 -0300

    refactor

--- plugins/chat/lib/chat/onebox_handler.rb
+++ plugins/chat/lib/chat/onebox_handler.rb
@@ -17,13 +17,7 @@ module Chat
 
       return if !Guardian.new.can_preview_chat_channel?(chat_channel)
 
-      args = {
-        url: url,
-        channel_id: chat_channel.id,
-        channel_name: chat_channel.name,
-        is_category: chat_channel.category_channel?,
-        color: chat_channel.category_channel? ? chat_channel.chatable.color : nil,
-      }
+      args = build_args(url, chat_channel)
 
       if message.present?
         render_message_onebox(args, message)
@@ -34,39 +28,59 @@ module Chat
 
     private
 
+    def self.build_args(url, chat_channel)
+      args = {
+        url: url,
+        channel_id: chat_channel.id,
+        channel_name: chat_channel.name,
+        is_category: chat_channel.category_channel?,
+        color: chat_channel.category_channel? ? chat_channel.chatable.color : nil,
+      }
+    end
+
     def self.render_message_onebox(args, message)
-      args[:message_id] = message.id
-      args[:username] = message.user.username
-      args[:avatar_url] = message.user.avatar_template_url.gsub("{size}", "20")
-      args[:cooked] = message.cooked
-      args[:created_at] = message.created_at
-      args[:created_at_str] = message.created_at.iso8601
+      args.merge!(
+        message_id: message.id,
+        username: message.user.username,
+        avatar_url: message.user.avatar_template_url.gsub("{size}", "20"),
+        cooked: message.cooked,
+        created_at: message.created_at,
+        created_at_str: message.created_at.iso8601,
+      )
 
       Mustache.render(Chat.message_onebox_template, args)
     end
 
     def self.render_channel_onebox(args, chat_channel)
-      users =
-        Chat::ChannelMembershipsQuery
-          .call(channel: chat_channel, limit: 10)
-          .map do |membership|
-            {
-              username: membership.user.username,
-              avatar_url: membership.user.avatar_template_url.gsub("{size}", "60"),
-            }
-          end
+      users = build_users_list(chat_channel)
 
-      remaining_user_count_str =
-        if chat_channel.user_count > users.size
-          I18n.t("chat.onebox.and_x_others", count: chat_channel.user_count - users.size)
-        end
+      remaining_user_count_str = build_remaining_user_count_str(chat_channel, users)
 
-      args[:users] = users
-      args[:user_count_str] = I18n.t("chat.onebox.x_members", count: chat_channel.user_count)
-      args[:remaining_user_count_str] = remaining_user_count_str
-      args[:description] = chat_channel.description
+      args.merge!(
+        users: users,
+        user_count_str: I18n.t("chat.onebox.x_members", count: chat_channel.user_count),
+        remaining_user_count_str: remaining_user_count_str,
+        description: chat_channel.description,
+      )
 
       Mustache.render(Chat.channel_onebox_template, args)
+    end
+
+    def self.build_users_list(chat_channel)
+      Chat::ChannelMembershipsQuery
+        .call(channel: chat_channel, limit: 10)
+        .map do |membership|
+          {
+            username: membership.user.username,
+            avatar_url: membership.user.avatar_template_url.gsub("{size}", "60"),
+          }
+        end
+    end
+
+    def self.build_remaining_user_count_str(chat_channel, users)
+      if chat_channel.user_count > users.size
+        I18n.t("chat.onebox.and_x_others", count: chat_channel.user_count - users.size)
+      end
     end
   end
 end
