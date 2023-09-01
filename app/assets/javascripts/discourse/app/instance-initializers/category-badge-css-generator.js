6541f62877921f774c8d2deee0ce447eab82d40e
Blake Erickson <o.blakeerickson@gmail.com>
2023-09-01 16:30:41 -0600

    Use a css generator for category badge classes

---
+++ app/assets/javascripts/discourse/app/instance-initializers/category-badge-css-generator.js
@@ -0,0 +1,42 @@
+import Category from "discourse/models/category";
+import { get } from "@ember/object";
+
+export default {
+  after: "category-color-css-generator",
+
+  /**
+   * This generates CSS color classes for each category,
+   * which can be used in themes to render category-specific elements.
+   */
+  initialize(owner) {
+    this.site = owner.lookup("service:site");
+
+    // If the site is login_required and the user is anon there will be no categories preloaded.
+    if (!this.site.categories) {
+      return;
+    }
+
+    const generatedCssClasses = [
+      this.site.categories.map((category) => {
+        let parentCat = Category.findById(get(category, "parent_category_id"));
+        let badgeClass = `
+          .badge-category.badge-category-${category.id}:before {
+            background: var(--category-${category.id}-color);
+          }`;
+        if (parentCat) {
+          badgeClass += `
+            .badge-category.badge-subcategory-${parentCat.id}:before {
+              background: linear-gradient(90deg, var(--category-${parentCat.id}-color) 50%, var(--category-${category.id}-color) 50%);
+            }`;
+        }
+        return badgeClass;
+      }),
+    ];
+
+    const cssTag = document.createElement("style");
+    cssTag.type = "text/css";
+    cssTag.id = "category-badge-css-generator";
+    cssTag.innerHTML = generatedCssClasses.join("\n");
+    document.head.appendChild(cssTag);
+  },
+};
