c7dce90f43ec15f93b95e719d3fafca50e40edb1
David Taylor <david@taylorhq.com>
2023-09-04 17:09:55 +0100

    DEV: Switch to using standard ember-cli test bundle (#23337)
    Previously we were patching ember-cli so that it would split the test bundle into two halves: the helpers, and the tests themselves. This was done so that we could use the helpers for `/theme-qunit` without needing to load all the core tests. This patch has proven problematic to maintain, and will become even harder under Embroider.
    
    This commit removes the patch, so that ember-cli goes back to generating a single `tests.js` bundle. This means that core test definitions will now be included in the bundle when using `/theme-qunit`, and so this commit also updates our test module filter to exclude them from the run. This is the same way that we handle plugin tests on the regular `/tests` route, and is fully supported by qunit.
    
    For now, this keeps `/theme-qunit` working in both development and production environments. However, we are very likely to drop support in production as part of the move to Embroider.

--- app/assets/javascripts/discourse/tests/setup-tests.js
+++ app/assets/javascripts/discourse/tests/setup-tests.js
@@ -359,19 +359,25 @@ export default function setupTests(config) {
 
   const target = getUrlParameter("target") || "core";
 
+  const hasPluginJs = !!document.querySelector("script[data-discourse-plugin]");
+  const hasThemeJs = !!document.querySelector("script[data-theme-id]");
+
   const shouldLoadModule = (name) => {
     if (!/\-test/.test(name)) {
       return false;
     }
 
     const isPlugin = name.match(/\/plugins\//);
-    const isCore = !isPlugin;
+    const isTheme = name.match(/\/theme-\d+\//);
+    const isCore = !isPlugin && !isTheme;
     const pluginName = name.match(/\/plugins\/([\w-]+)\//)?.[1];
 
     const loadCore = target === "core" || target === "all";
     const loadAllPlugins = target === "plugins" || target === "all";
 
-    if (isCore && !loadCore) {
+    if (hasThemeJs) {
+      return isTheme;
+    } else if (isCore && !loadCore) {
       return false;
     } else if (isPlugin && !(loadAllPlugins || pluginName === target)) {
       return false;
@@ -388,9 +394,6 @@ export default function setupTests(config) {
   setupToolbar();
   reportMemoryUsageAfterTests();
   patchFailedAssertion();
-
-  const hasPluginJs = !!document.querySelector("script[data-discourse-plugin]");
-  const hasThemeJs = !!document.querySelector("script[data-theme-id]");
 
   if (!hasPluginJs && !hasThemeJs) {
     configureRaiseOnDeprecation();
