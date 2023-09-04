2a4a53afdff6b4b6c090b5f5b79ea16ce7385222
David Taylor <david@taylorhq.com>
2023-09-04 13:14:49 +0100

    Fix for subfolder

--- lib/ember_cli.rb
+++ lib/ember_cli.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module EmberCli
+  def self.dist_dir
+    "#{Rails.root}/app/assets/javascripts/discourse/dist"
+  end
+
   def self.assets
     @assets ||=
       begin
@@ -40,12 +44,14 @@ module EmberCli
     chunk_infos = {}
 
     begin
-      chunk_infos.merge! parse_chunks_from_html("tests/index.html")
+      test_html = File.read("#{dist_dir}/tests/index.html")
+      chunk_infos.merge! parse_chunks_from_html(test_html)
     rescue Errno::ENOENT
       # production build
     end
 
-    chunk_infos.merge! parse_chunks_from_html("index.html")
+    index_html = File.read("#{dist_dir}/index.html")
+    chunk_infos.merge! parse_chunks_from_html(index_html)
 
     @@chunk_infos = chunk_infos if Rails.env.production?
     chunk_infos
@@ -75,8 +81,7 @@ module EmberCli
       end
   end
 
-  def self.parse_chunks_from_html(path)
-    html = File.read("#{Rails.root}/app/assets/javascripts/discourse/dist/#{path}")
+  def self.parse_chunks_from_html(html)
     doc = Nokogiri::HTML5.parse(html)
 
     chunk_infos = {}
@@ -87,7 +92,9 @@ module EmberCli
         entrypoint = discourse_script.attr("entrypoint")
         chunk_infos[entrypoint] = discourse_script
           .css("script[src]")
-          .map { |script| script.attr("src")[%r{\A/assets/(.*)\.js\z}, 1] }
+          .map do |script|
+            script.attr("src").delete_prefix("#{Discourse.base_path}/assets/").delete_suffix(".js")
+          end
       end
 
     chunk_infos
