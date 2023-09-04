da7011eef25cb34518a213cf3e488837122549ef
Kelvin Tan <kelv@discourse.org>
2023-09-04 11:21:55 +0800

    DEV: convert grant-badge to use component modal API

--- app/assets/javascripts/discourse/app/components/modal/grant-badge.js
+++ app/assets/javascripts/discourse/app/components/modal/grant-badge.js
@@ -1,48 +1,23 @@
-import Controller, { inject as controller } from "@ember/controller";
+import { action } from "@ember/object";
+import Component from "@ember/component";
 import Badge from "discourse/models/badge";
 import GrantBadgeController from "discourse/mixins/grant-badge-controller";
 import I18n from "I18n";
-import ModalFunctionality from "discourse/mixins/modal-functionality";
 import UserBadge from "discourse/models/user-badge";
 import { all } from "rsvp";
 import discourseComputed from "discourse-common/utils/decorators";
-import { flashAjaxError } from "discourse/lib/ajax-error";
+import { extractError } from "discourse/lib/ajax-error";
+import getURL from "discourse-common/lib/get-url";
 
-export default Controller.extend(ModalFunctionality, GrantBadgeController, {
-  topicController: controller("topic"),
-  loading: true,
-  saving: false,
-  selectedBadgeId: null,
+export default class GrantBadgeModal extends Component.extend(
+  GrantBadgeController
+) {
+  loading = true;
+  saving = false;
+  selectedBadgeId = null;
 
   init() {
-    this._super(...arguments);
-
-    this.allBadges = [];
-    this.userBadges = [];
-  },
-
-  @discourseComputed("topicController.selectedPosts")
-  post() {
-    return this.get("topicController.selectedPosts")[0];
-  },
-
-  @discourseComputed("post")
-  badgeReason(post) {
-    const url = post.get("url");
-    const protocolAndHost =
-      window.location.protocol + "//" + window.location.host;
-
-    return url.startsWith("/") ? protocolAndHost + url : url;
-  },
-
-  @discourseComputed("saving", "selectedBadgeGrantable")
-  buttonDisabled(saving, selectedBadgeGrantable) {
-    return saving || !selectedBadgeGrantable;
-  },
-
-  onShow() {
     this.set("loading", true);
-
     all([
       Badge.findAll(),
       UserBadge.findByUsername(this.get("post.username")),
@@ -53,28 +28,53 @@ export default Controller.extend(ModalFunctionality, GrantBadgeController, {
         loading: false,
       });
     });
-  },
+    super.init(...arguments);
+    this.allBadges = [];
+    this.userBadges = [];
+  }
 
-  actions: {
-    grantBadge() {
-      this.set("saving", true);
+  @discourseComputed("model.selectedPost")
+  post() {
+    return this.get("model.selectedPost");
+  }
 
-      this.grantBadge(
-        this.selectedBadgeId,
-        this.get("post.username"),
-        this.badgeReason
-      )
-        .then((newBadge) => {
+  @discourseComputed("post")
+  badgeReason(post) {
+    return getURL(post.get("url"));
+  }
+
+  @discourseComputed("saving", "selectedBadgeGrantable")
+  buttonDisabled(saving, selectedBadgeGrantable) {
+    return saving || !selectedBadgeGrantable;
+  }
+
+  @action
+  performGrantBadge() {
+    this.set("saving", true);
+
+    this.grantBadge(
+      this.selectedBadgeId,
+      this.get("post.username"),
+      this.badgeReason
+    )
+      .then(
+        (newBadge) => {
           this.set("selectedBadgeId", null);
-          this.flash(
-            I18n.t("badges.successfully_granted", {
+          this.setProperties({
+            flash: I18n.t("badges.successfully_granted", {
               username: this.get("post.username"),
               badge: newBadge.get("badge.name"),
             }),
-            "success"
-          );
-        }, flashAjaxError(this))
-        .finally(() => this.set("saving", false));
-    },
-  },
-});
+            flashType: "success",
+          });
+        },
+        (e) => {
+          this.setProperties({
+            flash: extractError(e),
+            flashType: "error",
+          });
+        }
+      )
+      .finally(() => this.set("saving", false));
+  }
+}
