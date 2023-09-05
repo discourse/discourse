discourse/discourse

commit bb2d1f8703ab63f5eb504d37f926670583ca8288

DEV: Convert `login` modal to component-based API (#23093)
# Desktop
##### Before
<img width="865" alt="Screenshot 2023-08-17 at 1 32 02 PM" src="https://github.com/discourse/discourse/assets/50783505/1691ae34-8cc3-4deb-bee0-748851a43f6c">

##### After
<img width="818" alt="Screenshot 2023-08-17 at 1 34 13 PM" src="https://github.com/discourse/discourse/assets/50783505/0dcc6d95-270f-44a1-8582-5f7bf89e7e2c">

# Mobile
##### Before
<img width="364" alt="Screenshot 2023-08-17 at 1 28 20 PM" src="https://github.com/discourse/discourse/assets/50783505/6758b7f9-da65-464e-b289-d43177218026">

##### After
<img width="365" alt="Screenshot 2023-08-17 at 1 28 33 PM" src="https://github.com/discourse/discourse/assets/50783505/f32f28d3-a48c-485f-91eb-dc6bcaf8a2e1">

# Changes Made
- I took the liberty to hide the password <kbd>Show</kbd> / <kbd>Hide</kbd> toggle when no password present.
##### Before
<img width="237" alt="Screenshot 2023-08-15 at 4 46 16 PM" src="https://github.com/discourse/discourse/assets/50783505/dfa46535-27ea-4756-8cb0-2c1108505ec7">
<img width="240" alt="Screenshot 2023-08-15 at 4 43 03 PM" src="https://github.com/discourse/discourse/assets/50783505/b1b9bacd-8b11-4fb5-89ce-53135417193f">
<img width="244" alt="Screenshot 2023-08-15 at 4 42 58 PM" src="https://github.com/discourse/discourse/assets/50783505/88f3176b-fc25-4d0b-8193-967bf898f113">

##### After
<img width="263" alt="Screenshot 2023-08-15 at 4 45 47 PM" src="https://github.com/discourse/discourse/assets/50783505/48241693-5b0b-4c21-8a06-e14262ede79c">
<img width="268" alt="Screenshot 2023-08-15 at 4 45 50 PM" src="https://github.com/discourse/discourse/assets/50783505/3c2c4aeb-6fde-45c5-8e45-2879ecb7ead2">
<img width="221" alt="Screenshot 2023-08-15 at 4 45 39 PM" src="https://github.com/discourse/discourse/assets/50783505/94406f13-6b20-484c-831e-1b828600cccf">

app/assets/javascripts/discourse/app/components/modal/login/local-login-form.js
@@ -1,0 +1,129 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { ajax } from "discourse/lib/ajax";
+import { htmlSafe } from "@ember/template";
+import { isEmpty } from "@ember/utils";
+import { escapeExpression } from "discourse/lib/utilities";
+import { inject as service } from "@ember/service";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+import I18n from "I18n";
+import getWebauthnCredential from "discourse/lib/webauthn";
+import ForgotPassword from "discourse/components/modal/forgot-password";
+
+export default class LocalLoginBody extends Component {
+  @service modal;
+
+  @tracked maskPassword = true;
+  @tracked processingEmailLink = false;
+  @tracked capsLockOn = false;
+
+  get credentialsClass() {
+    return this.args.showSecondFactor || this.args.showSecurityKey
+      ? "hidden"
+      : "";
+  }
+
+  get secondFactorClass() {
+    return this.args.showSecondFactor || this.args.showSecurityKey
+      ? ""
+      : "hidden";
+  }
+
+  get disableLoginFields() {
+    return this.args.showSecondFactor || this.args.showSecurityKey;
+  }
+
+  @action
+  togglePasswordMask() {
+    this.maskPassword = !this.maskPassword;
+  }
+
+  @action
+  async emailLogin(event) {
+    event?.preventDefault();
+
+    if (this.processingEmailLink) {
+      return;
+    }
+
+    if (isEmpty(this.args.loginName)) {
+      this.args.flashChanged(I18n.t("login.blank_username"));
+      this.args.flashTypeChanged("info");
+      return;
+    }
+
+    try {
+      this.processingEmailLink = true;
+      const data = await ajax("/u/email-login", {
+        data: { login: this.args.loginName.trim() },
+        type: "POST",
+      });
+      const loginName = escapeExpression(this.args.loginName);
+      const isEmail = loginName.match(/@/);
+      const key = isEmail
+        ? "email_login.complete_email"
+        : "email_login.complete_username";
+      if (data.user_found === false) {
+        this.args.flashChanged(
+          htmlSafe(
+            I18n.t(`${key}_not_found`, {
+              email: loginName,
+              username: loginName,
+            })
+          )
+        );
+        this.args.flashTypeChanged("error");
+      } else {
+        const postfix = data.hide_taken ? "" : "_found";
+        this.args.flashChanged(
+          htmlSafe(
+            I18n.t(`${key}${postfix}`, {
+              email: loginName,
+              username: loginName,
+            })
+          )
+        );
+        this.args.flashTypeChanged("success");
+      }
+    } catch (e) {
+      popupAjaxError(e);
+    } finally {
+      this.processingEmailLink = false;
+    }
+  }
+
+  @action
+  loginOnEnter(event) {
+    if (event.key === "Enter") {
+      this.args.login();
+    }
+  }
+
+  @action
+  handleForgotPassword(event) {
+    event?.preventDefault();
+
+    this.modal.show(ForgotPassword, {
+      model: {
+        emailOrUsername: this.args.loginName,
+      },
+    });
+  }
+
+  @action
+  authenticateSecurityKey() {
+    getWebauthnCredential(
+      this.args.securityKeyChallenge,
+      this.args.securityKeyAllowedCredentialIds,
+      (credentialData) => {
+        this.args.securityKeyCredentialChanged(credentialData);
+        this.args.login();
+      },
+      (error) => {
+        this.args.flashChanged(error);
+        this.args.flashTypeChanged("error");
+      }
+    );
+  }
+}

app/assets/javascripts/discourse/app/components/login-modal.js
@@ -1,30 +1,0 @@
-import Component from "@ember/component";
-import cookie from "discourse/lib/cookie";
-import { schedule } from "@ember/runloop";
-
-export default Component.extend({
-  didInsertElement() {
-    this._super(...arguments);
-
-    const prefillUsername = $("#hidden-login-form input[name=username]").val();
-    if (prefillUsername) {
-      this.set("loginName", prefillUsername);
-      this.set(
-        "loginPassword",
-        $("#hidden-login-form input[name=password]").val()
-      );
-    } else if (cookie("email")) {
-      this.set("loginName", cookie("email"));
-    }
-
-    schedule("afterRender", () => {
-      $(
-        "#login-account-password, #login-account-name, #login-second-factor"
-      ).keydown((e) => {
-        if (e.key === "Enter") {
-          this.action();
-        }
-      });
-    });
-  },
-});

app/assets/javascripts/discourse/app/components/modal/login.hbs
@@ -1,0 +1,73 @@
+<DModal
+  class="login-modal"
+  @bodyClass={{this.modalBodyClasses}}
+  @closeModal={{@closeModal}}
+  @flash={{this.flash}}
+  @flashType={{this.flashType}}
+  {{did-insert this.preloadLogin}}
+>
+  <:body>
+    <PluginOutlet @name="login-before-modal-body" @connectorTagName="div" />
+
+    {{#if this.site.mobileView}}
+      <Modal::Login::WelcomeHeader
+        @wavingHandURL={{this.wavingHandURL}}
+        @createAccount={{this.createAccount}}
+      />
+      {{#if this.showLoginButtons}}
+        <LoginButtons @externalLogin={{this.externalLogin}} />
+      {{/if}}
+    {{/if}}
+
+    {{#if this.canLoginLocal}}
+      <div class={{if this.site.desktopView "login-left-side"}}>
+        {{#if this.site.desktopView}}
+          <Modal::Login::WelcomeHeader
+            @wavingHandURL={{this.wavingHandURL}}
+            @createAccount={{this.createAccount}}
+          />
+        {{/if}}
+        <Modal::Login::LocalLoginForm
+          @loginName={{this.loginName}}
+          @loginNameChanged={{this.loginNameChanged}}
+          @canLoginLocalWithEmail={{this.canLoginLocalWithEmail}}
+          @loginPassword={{this.loginPassword}}
+          @secondFactorMethod={{this.secondFactorMethod}}
+          @secondFactorToken={{this.secondFactorToken}}
+          @backupEnabled={{this.backupEnabled}}
+          @securityKeyAllowedCredentialIds={{this.securityKeyAllowedCredentialIds}}
+          @securityKeyChallenge={{this.securityKeyChallenge}}
+          @showSecurityKey={{this.showSecurityKey}}
+          @otherMethodAllowed={{this.otherMethodAllowed}}
+          @showSecondFactor={{this.showSecondFactor}}
+          @handleForgotPassword={{this.handleForgotPassword}}
+          @login={{this.login}}
+          @flashChanged={{this.flashChanged}}
+          @flashTypeChanged={{this.flashTypeChanged}}
+          @securityKeyCredentialChanged={{this.securityKeyCredentialChanged}}
+        />
+        <Modal::Login::Footer
+          @canLoginLocal={{this.canLoginLocal}}
+          @showSecurityKey={{this.showSecurityKey}}
+          @login={{this.login}}
+          @loginButtonLabel={{this.loginButtonLabel}}
+          @loginDisabled={{this.loginDisabled}}
+          @showSignupLink={{this.showSignupLink}}
+          @createAccount={{this.createAccount}}
+          @loggingIn={{this.loggingIn}}
+        />
+      </div>
+    {{/if}}
+
+    {{#if (and this.showLoginButtons this.site.desktopView)}}
+      {{#unless this.canLoginLocal}}
+        <div class="login-left-side">
+          <Modal::Login::WelcomeHeader @wavingHandURL={{this.wavingHandURL}} />
+        </div>
+      {{/unless}}
+      <div class="login-right-side">
+        <LoginButtons @externalLogin={{this.externalLogin}} />
+      </div>
+    {{/if}}
+  </:body>
+</DModal>

app/assets/javascripts/discourse/app/components/modal/login.js
@@ -1,0 +1,302 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { SECOND_FACTOR_METHODS } from "discourse/models/user";
+import { ajax } from "discourse/lib/ajax";
+import { findAll } from "discourse/models/login-method";
+import { areCookiesEnabled } from "discourse/lib/utilities";
+import { wavingHandURL } from "discourse/lib/waving-hand-url";
+import { schedule } from "@ember/runloop";
+import cookie, { removeCookie } from "discourse/lib/cookie";
+import { isEmpty } from "@ember/utils";
+import I18n from "I18n";
+import { escape } from "pretty-text/sanitizer";
+
+export default class Login extends Component {
+  @service dialog;
+  @service siteSettings;
+  @service site;
+
+  @tracked loggingIn = false;
+  @tracked loggedIn = false;
+  @tracked showLoginButtons = true;
+  @tracked showSecondFactor = false;
+  @tracked loginPassword = "";
+  @tracked loginName = "";
+  @tracked flash = this.args.model?.flash;
+  @tracked flashType = this.args.model?.flashType;
+  @tracked canLoginLocal = this.siteSettings.enable_local_logins;
+  @tracked
+  canLoginLocalWithEmail = this.siteSettings.enable_local_logins_via_email;
+  @tracked secondFactorMethod = SECOND_FACTOR_METHODS.TOTP;
+  @tracked securityKeyCredential;
+  @tracked otherMethodAllowed;
+  @tracked secondFactorRequired;
+  @tracked backupEnabled;
+  @tracked totpEnabled;
+  @tracked showSecurityKey;
+  @tracked securityKeyChallenge;
+  @tracked securityKeyAllowedCredentialIds;
+  @tracked secondFactorToken;
+
+  constructor() {
+    super(...arguments);
+    if (this.args.model?.isExternalLogin) {
+      this.externalLogin(this.args.model.externalLoginMethod, {
+        signup: this.args.model.signup,
+      });
+    }
+  }
+
+  get awaitingApproval() {
+    return (
+      this.args.model?.awaitingApproval &&
+      !this.canLoginLocal &&
+      !this.canLoginLocalWithEmail
+    );
+  }
+
+  get loginDisabled() {
+    return this.loggingIn || this.loggedIn;
+  }
+
+  get wavingHandURL() {
+    return wavingHandURL();
+  }
+
+  get modalBodyClasses() {
+    const classes = ["login-modal-body"];
+    if (this.awaitingApproval) {
+      classes.push("awaiting-approval");
+    }
+    if (
+      this.hasAtLeastOneLoginButton &&
+      !this.showSecondFactor &&
+      !this.showSecurityKey
+    ) {
+      classes.push("has-alt-auth");
+    }
+    if (!this.canLoginLocal) {
+      classes.push("no-local-login");
+    }
+    if (this.showSecondFactor || this.showSecurityKey) {
+      classes.push("second-factor");
+    }
+    return classes.join(" ");
+  }
+
+  get hasAtLeastOneLoginButton() {
+    return findAll().length > 0;
+  }
+
+  get loginButtonLabel() {
+    return this.loggingIn ? "login.logging_in" : "login.title";
+  }
+
+  get showSignupLink() {
+    return this.args.model.canSignUp && !this.loggingIn;
+  }
+
+  @action
+  preloadLogin() {
+    const prefillUsername = document.querySelector(
+      "#hidden-login-form input[name=username]"
+    )?.value;
+    if (prefillUsername) {
+      this.loginName = prefillUsername;
+      this.loginPassword = document.querySelector(
+        "#hidden-login-form input[name=password]"
+      ).value;
+    } else if (cookie("email")) {
+      this.loginName = cookie("email");
+    }
+  }
+
+  @action
+  securityKeyCredentialChanged(value) {
+    this.securityKeyCredential = value;
+  }
+
+  @action
+  flashChanged(value) {
+    this.flash = value;
+  }
+
+  @action
+  flashTypeChanged(value) {
+    this.flashType = value;
+  }
+
+  @action
+  loginNameChanged(event) {
+    this.loginName = event.target.value;
+  }
+
+  @action
+  async login() {
+    if (this.loginDisabled) {
+      return;
+    }
+
+    if (isEmpty(this.loginName) || isEmpty(this.loginPassword)) {
+      this.flash = I18n.t("login.blank_username_or_password");
+      this.flashType = "error";
+      return;
+    }
+
+    try {
+      this.loggingIn = true;
+      const result = await ajax("/session", {
+        type: "POST",
+        data: {
+          login: this.loginName,
+          password: this.loginPassword,
+          second_factor_token:
+            this.securityKeyCredential || this.secondFactorToken,
+          second_factor_method: this.secondFactorMethod,
+          timezone: moment.tz.guess(),
+        },
+      });
+      if (result && result.error) {
+        this.loggingIn = false;
+        this.flash = null;
+
+        if (
+          (result.security_key_enabled || result.totp_enabled) &&
+          !this.secondFactorRequired
+        ) {
+          this.otherMethodAllowed = result.multiple_second_factor_methods;
+          this.secondFactorRequired = true;
+          this.showLoginButtons = false;
+          this.backupEnabled = result.backup_enabled;
+          this.totpEnabled = result.totp_enabled;
+          this.showSecondFactor = result.totp_enabled;
+          this.showSecurityKey = result.security_key_enabled;
+          this.secondFactorMethod = result.security_key_enabled
+            ? SECOND_FACTOR_METHODS.SECURITY_KEY
+            : SECOND_FACTOR_METHODS.TOTP;
+          this.securityKeyChallenge = result.challenge;
+          this.securityKeyAllowedCredentialIds = result.allowed_credential_ids;
+
+          // only need to focus the 2FA input for TOTP
+          if (!this.showSecurityKey) {
+            schedule("afterRender", () =>
+              document
+                .getElementById("second-factor")
+                .querySelector("input")
+                .focus()
+            );
+          }
+
+          return;
+        } else if (result.reason === "not_activated") {
+          this.args.model.showNotActivated({
+            username: this.loginName,
+            sentTo: escape(result.sent_to_email),
+            currentEmail: escape(result.current_email),
+          });
+        } else if (result.reason === "suspended") {
+          this.args.closeModal();
+          this.dialog.alert(result.error);
+        } else {
+          this.flash = result.error;
+          this.flashType = "error";
+        }
+      } else {
+        this.loggedIn = true;
+        // Trigger the browser's password manager using the hidden static login form:
+        const hiddenLoginForm = document.getElementById("hidden-login-form");
+        const applyHiddenFormInputValue = (value, key) => {
+          if (!hiddenLoginForm) {
+            return;
+          }
+
+          hiddenLoginForm.querySelector(`input[name=${key}]`).value = value;
+        };
+
+        const destinationUrl = cookie("destination_url");
+        const ssoDestinationUrl = cookie("sso_destination_url");
+
+        applyHiddenFormInputValue(this.loginName, "username");
+        applyHiddenFormInputValue(this.loginPassword, "password");
+
+        if (ssoDestinationUrl) {
+          removeCookie("sso_destination_url");
+          window.location.assign(ssoDestinationUrl);
+          return;
+        } else if (destinationUrl) {
+          // redirect client to the original URL
+          removeCookie("destination_url");
+
+          applyHiddenFormInputValue(destinationUrl, "redirect");
+        } else {
+          applyHiddenFormInputValue(window.location.href, "redirect");
+        }
+
+        if (hiddenLoginForm) {
+          if (
+            navigator.userAgent.match(/(iPad|iPhone|iPod)/g) &&
+            navigator.userAgent.match(/Safari/g)
+          ) {
+            // In case of Safari on iOS do not submit hidden login form
+            window.location.href = hiddenLoginForm.querySelector(
+              "input[name=redirect]"
+            ).value;
+          } else {
+            hiddenLoginForm.submit();
+          }
+        }
+        return;
+      }
+    } catch (e) {
+      // Failed to login
+      if (e.jqXHR && e.jqXHR.status === 429) {
+        this.flash = I18n.t("login.rate_limit");
+        this.flashType = "error";
+      } else if (
+        e.jqXHR &&
+        e.jqXHR.status === 503 &&
+        e.jqXHR.responseJSON.error_type === "read_only"
+      ) {
+        this.flash = I18n.t("read_only_mode.login_disabled");
+        this.flashType = "error";
+      } else if (!areCookiesEnabled()) {
+        this.flash = I18n.t("login.cookies_error");
+        this.flashType = "error";
+      } else {
+        this.flash = I18n.t("login.error");
+        this.flashType = "error";
+      }
+      this.loggingIn = false;
+    }
+  }
+
+  @action
+  async externalLogin(loginMethod, { signup = false } = {}) {
+    if (this.loginDisabled) {
+      return;
+    }
+
+    try {
+      this.loggingIn = true;
+      await loginMethod.doLogin({ signup });
+      this.args.closeModal();
+    } catch {
+      this.loggingIn = false;
+    }
+  }
+
+  @action
+  createAccount() {
+    let createAccountProps = {};
+    if (this.loginName && this.loginName.indexOf("@") > 0) {
+      createAccountProps.accountEmail = this.loginName;
+      createAccountProps.accountUsername = null;
+    } else {
+      createAccountProps.accountUsername = this.loginName;
+      createAccountProps.accountEmail = null;
+    }
+    this.args.model.showCreateAccount(createAccountProps);
+  }
+}

app/assets/javascripts/discourse/app/components/modal/login/footer.hbs
@@ -1,0 +1,28 @@
+<div class="modal-footer">
+  {{#if @canLoginLocal}}
+    {{#unless @showSecurityKey}}
+      <DButton
+        @action={{@login}}
+        id="login-button"
+        @form="login-form"
+        @icon="unlock"
+        @label={{@loginButtonLabel}}
+        @disabled={{@loginDisabled}}
+        class="btn btn-large btn-primary"
+        @tabindex="2"
+      />
+    {{/unless}}
+
+    {{#if @showSignupLink}}
+      <DButton
+        class="btn-large"
+        id="new-account-link"
+        @action={{@createAccount}}
+        @label="create_account.title"
+        @tabindex="3"
+      />
+    {{/if}}
+  {{/if}}
+  <ConditionalLoadingSpinner @condition={{@loggingIn}} @size="small" />
+  <PluginOutlet @name="login-after-modal-footer" @connectorTagName="div" />
+</div>

app/assets/javascripts/discourse/app/components/modal/login/local-login-form.hbs
@@ -1,0 +1,98 @@
+<form id="login-form" method="post">
+  <div id="credentials" class={{this.credentialsClass}}>
+    <div class="input-group">
+      <Input
+        @value={{@loginName}}
+        @type="email"
+        id="login-account-name"
+        class={{value-entered @loginName}}
+        autocomplete="username"
+        autocorrect="off"
+        autocapitalize="off"
+        disabled={{@showSecondFactor}}
+        autofocus="autofocus"
+        tabindex="1"
+        {{on "input" @loginNameChanged}}
+        {{on "keydown" this.loginOnEnter}}
+      />
+      <label class="alt-placeholder" for="login-account-name">
+        {{i18n "login.email_placeholder"}}
+      </label>
+      {{#if @canLoginLocalWithEmail}}
+        <a
+          href
+          class={{if @loginName "" "no-login-filled"}}
+          tabindex="3"
+          id="email-login-link"
+          {{on "click" this.emailLogin}}
+        >
+          {{i18n "email_login.login_link"}}
+        </a>
+      {{/if}}
+    </div>
+    <div class="input-group">
+      <PasswordField
+        @value={{@loginPassword}}
+        @type={{if this.maskPassword "password" "text"}}
+        class={{value-entered @loginPassword}}
+        id="login-account-password"
+        autocomplete="current-password"
+        maxlength="200"
+        @capsLockOn={{this.capsLockOn}}
+        disabled={{this.disableLoginFields}}
+        tabindex="1"
+        {{on "keydown" this.loginOnEnter}}
+      />
+      <label class="alt-placeholder" for="login-account-password">
+        {{i18n "login.password"}}
+      </label>
+      <div class="login__password-links">
+        <a
+          href
+          id="forgot-password-link"
+          tabindex="3"
+          {{on "click" this.handleForgotPassword}}
+        >
+          {{i18n "forgot_password.action"}}
+        </a>
+        {{#if @loginPassword}}
+          <TogglePasswordMask
+            @maskPassword={{this.maskPassword}}
+            @togglePasswordMask={{this.togglePasswordMask}}
+            tabindex="3"
+          />
+        {{/if}}
+      </div>
+      <div class="caps-lock-warning {{unless this.capsLockOn 'hidden'}}">
+        {{d-icon "exclamation-triangle"}}
+        {{i18n "login.caps_lock_warning"}}</div>
+    </div>
+  </div>
+  <SecondFactorForm
+    @secondFactorMethod={{@secondFactorMethod}}
+    @secondFactorToken={{@secondFactorToken}}
+    @class={{this.secondFactorClass}}
+    @backupEnabled={{@backupEnabled}}
+    @isLogin={{true}}
+  >
+    {{#if @showSecurityKey}}
+      <SecurityKeyForm
+        @allowedCredentialIds={{@securityKeyAllowedCredentialIds}}
+        @challenge={{@securityKeyChallenge}}
+        @showSecurityKey={{@showSecurityKey}}
+        @showSecondFactor={{@showSecondFactor}}
+        @secondFactorMethod={{@secondFactorMethod}}
+        @otherMethodAllowed={{@otherMethodAllowed}}
+        @action={{this.authenticateSecurityKey}}
+      />
+    {{else}}
+      <SecondFactorInput
+        @value={{@secondFactorToken}}
+        @inputId="login-second-factor"
+        @secondFactorMethod={{@secondFactorMethod}}
+        @backupEnabled={{@backupEnabled}}
+        {{on "keydown" this.loginOnEnter}}
+      />
+    {{/if}}
+  </SecondFactorForm>
+</form>

app/assets/javascripts/discourse/app/components/modal/login/welcome-header.hbs
@@ -1,0 +1,9 @@
+<div class="login-welcome-header">
+  <h1 class="login-title">{{i18n "login.header_title"}}</h1>
+  <img src={{@wavingHandURL}} alt="" class="waving-hand" />
+  <p class="login-subheader">{{i18n "login.subheader_title"}}</p>
+  <PluginOutlet
+    @name="login-header-bottom"
+    @outletArgs={{hash createAccount=@createAccount}}
+  />
+</div>

app/assets/javascripts/discourse/app/controllers/create-account.js
@@ -1,4 +1,4 @@
-import Controller, { inject as controller } from "@ember/controller";
+import Controller from "@ember/controller";
 import cookie, { removeCookie } from "discourse/lib/cookie";
 import discourseComputed, {
   observes,
@@ -23,6 +23,8 @@ import { notEmpty } from "@ember/object/computed";
 import { setting } from "discourse/lib/computed";
 import { userPath } from "discourse/lib/url";
 import { wavingHandURL } from "discourse/lib/waving-hand-url";
+import { inject as service } from "@ember/service";
+import LoginModal from "discourse/components/modal/login";
 
 export default Controller.extend(
   ModalFunctionality,
@@ -31,7 +33,7 @@ export default Controller.extend(
   NameValidation,
   UserFieldsValidation,
   {
-    login: controller(),
+    modal: service(),
 
     complete: false,
     accountChallenge: 0,
@@ -50,27 +52,6 @@ export default Controller.extend(
     @discourseComputed("hasAuthOptions", "canCreateLocal", "skipConfirmation")
     showCreateForm(hasAuthOptions, canCreateLocal, skipConfirmation) {
       return (hasAuthOptions || canCreateLocal) && !skipConfirmation;
-    },
-
-    resetForm() {
-      // We wrap the fields in a structure so we can assign a value
-      this.setProperties({
-        accountName: "",
-        accountEmail: "",
-        accountUsername: "",
-        accountPassword: "",
-        serverAccountEmail: null,
-        serverEmailValidation: null,
-        authOptions: null,
-        complete: false,
-        formSubmitted: false,
-        rejectedEmails: [],
-        rejectedPasswords: [],
-        prefilledUsername: null,
-        isDeveloper: false,
-        maskPassword: true,
-      });
-      this._createUserFields();
     },
 
     @discourseComputed("formSubmitted")
@@ -444,7 +425,14 @@ export default Controller.extend(
 
     actions: {
       externalLogin(provider) {
-        this.login.send("externalLogin", provider, { signup: true });
+        // we will automatically redirect to the external auth service
+        this.modal.show(LoginModal, {
+          model: {
+            isExternalLogin: true,
+            externalLoginMethod: provider,
+            signup: true,
+          },
+        });
       },
 
       createAccount() {

commit 096cdf600cf1c3eddcd6426fe2a26ca16e0cc3a0

FIX: An incorrect import
Introduced in bb2d1f8

app/assets/javascripts/discourse/app/components/modal/login/local-login-form.js
@@ -8,7 +8,7 @@ import { escapeExpression } from "discourse/lib/utilities";
 import { inject as service } from "@ember/service";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import I18n from "I18n";
-import getWebauthnCredential from "discourse/lib/webauthn";
+import { getWebauthnCredential } from "discourse/lib/webauthn";
 import ForgotPassword from "discourse/components/modal/forgot-password";
 
 export default class LocalLoginBody extends Component {