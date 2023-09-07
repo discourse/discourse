d27299b29265a6af4b3977f7542440fedc2c02cc
David Taylor <david@taylorhq.com>
2023-09-07 18:31:03 +0100

    Fix new-new

--- app/assets/javascripts/discourse/app/controllers/tag-show.js
+++ app/assets/javascripts/discourse/app/controllers/tag-show.js
@@ -20,6 +20,7 @@ export default class TagShowController extends DiscoverySortableController.exten
   @service router;
   @service currentUser;
   @service siteSettings;
+  @service topicTrackingState;
 
   @tracked category;
   @tracked filterType;
