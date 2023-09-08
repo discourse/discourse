discourse/discourse

commit 243793ec6ea41a9bf53dc6b9585ba9239dfc4c85

DEV: Migrate `Chat::MessageCreator` to a service (#22390)
Currently, the logic for creating a new chat message is scattered
between a controller and an “old” service.

This patch address this issue by creating a new service (using the “new”
sevice object system) encapsulating all the necessary logic.
(authorization, publishing events, etc.)

plugins/chat/plugin.rb
@@ -417,14 +417,14 @@ after_initialize do
         placeholders = { channel_name: channel.title(sender) }.merge(context["placeholders"] || {})
 
         creator =
-          Chat::MessageCreator.create(
+          Chat::CreateMessage.call(
             chat_channel: channel,
-            user: sender,
+            guardian: sender.guardian,
             content: utils.apply_placeholders(fields.dig("message", "value"), placeholders),
           )
 
-        if creator.failed?
-          Rails.logger.warn "[discourse-automation] Chat message failed to send, error was: #{creator.error}"
+        if creator.failure?
+          Rails.logger.warn "[discourse-automation] Chat message failed to send:\n#{creator.inspect_steps.inspect}\n#{creator.inspect_steps.error}"
         end
       end
     end
@@ -432,7 +432,12 @@ after_initialize do
 
   add_api_key_scope(
     :chat,
-    { create_message: { actions: %w[chat/chat#create_message], params: %i[chat_channel_id] } },
+    {
+      create_message: {
+        actions: %w[chat/api/channel_messages#create],
+        params: %i[chat_channel_id],
+      },
+    },
   )
 
   # Dark mode email styles

plugins/chat/app/controllers/chat/api/channel_messages_controller.rb
@@ -19,4 +19,29 @@ class Chat::Api::ChannelMessagesController < Chat::ApiController
       on_model_not_found(:message) { raise Discourse::NotFound }
     end
   end
+
+  def create
+    Chat::MessageRateLimiter.run!(current_user)
+
+    with_service(Chat::CreateMessage) do
+      on_success { render json: success_json.merge(message_id: result[:message].id) }
+      on_failed_policy(:no_silenced_user) { raise Discourse::InvalidAccess }
+      on_model_not_found(:channel) { raise Discourse::NotFound }
+      on_failed_policy(:allowed_to_join_channel) { raise Discourse::InvalidAccess }
+      on_model_not_found(:channel_membership) { raise Discourse::InvalidAccess }
+      on_failed_policy(:ensure_reply_consistency) { raise Discourse::NotFound }
+      on_failed_policy(:allowed_to_create_message_in_channel) do |policy|
+        render_json_error(policy.reason)
+      end
+      on_failed_policy(:ensure_valid_thread_for_channel) do
+        render_json_error(I18n.t("chat.errors.thread_invalid_for_channel"))
+      end
+      on_failed_policy(:ensure_thread_matches_parent) do
+        render_json_error(I18n.t("chat.errors.thread_does_not_match_parent"))
+      end
+      on_model_errors(:message) do |model|
+        render_json_error(model.errors.map(&:full_message).join(", "))
+      end
+    end
+  end
 end

plugins/chat/app/controllers/chat/chat_controller.rb
@@ -77,81 +77,6 @@ module Chat
       end
     end
 
-    def create_message
-      raise Discourse::InvalidAccess if current_user.silenced?
-
-      Chat::MessageRateLimiter.run!(current_user)
-
-      @user_chat_channel_membership =
-        Chat::ChannelMembershipManager.new(@chat_channel).find_for_user(current_user)
-      raise Discourse::InvalidAccess unless @user_chat_channel_membership
-
-      reply_to_msg_id = params[:in_reply_to_id]
-      if reply_to_msg_id.present?
-        rm = Chat::Message.find(reply_to_msg_id)
-        raise Discourse::NotFound if rm.chat_channel_id != @chat_channel.id
-      end
-
-      content = params[:message]
-
-      chat_message_creator =
-        Chat::MessageCreator.create(
-          chat_channel: @chat_channel,
-          user: current_user,
-          in_reply_to_id: reply_to_msg_id,
-          content: content,
-          staged_id: params[:staged_id],
-          upload_ids: params[:upload_ids],
-          thread_id: params[:thread_id],
-          staged_thread_id: params[:staged_thread_id],
-        )
-
-      return render_json_error(chat_message_creator.error) if chat_message_creator.failed?
-
-      if !chat_message_creator.chat_message.thread_id.present?
-        @user_chat_channel_membership.update!(
-          last_read_message_id: chat_message_creator.chat_message.id,
-        )
-      end
-
-      if @chat_channel.direct_message_channel?
-        # If any of the channel users is ignoring, muting, or preventing DMs from
-        # the current user then we should not auto-follow the channel once again or
-        # publish the new channel.
-        allowed_user_ids =
-          UserCommScreener.new(
-            acting_user: current_user,
-            target_user_ids:
-              @chat_channel.user_chat_channel_memberships.where(following: false).pluck(:user_id),
-          ).allowing_actor_communication
-
-        allowed_user_ids << current_user.id if !@user_chat_channel_membership.following
-
-        if allowed_user_ids.any?
-          Chat::Publisher.publish_new_channel(@chat_channel, User.where(id: allowed_user_ids))
-
-          @chat_channel
-            .user_chat_channel_memberships
-            .where(user_id: allowed_user_ids)
-            .update_all(following: true)
-        end
-      end
-
-      message =
-        (
-          if @user_chat_channel_membership.last_read_message_id &&
-               chat_message_creator.chat_message.in_thread?
-            Chat::Message.find(@user_chat_channel_membership.last_read_message_id)
-          else
-            chat_message_creator.chat_message
-          end
-        )
-
-      Chat::Publisher.publish_user_tracking_state!(current_user, @chat_channel, message)
-
-      render json: success_json.merge(message_id: chat_message_creator.chat_message.id)
-    end
-
     def edit_message
       chat_message_updater =
         Chat::MessageUpdater.update(

plugins/chat/app/controllers/chat/incoming_webhooks_controller.rb
@@ -2,6 +2,8 @@
 
 module Chat
   class IncomingWebhooksController < ::ApplicationController
+    include Chat::WithServiceHelper
+
     requires_plugin Chat::PLUGIN_NAME
 
     WEBHOOK_MESSAGES_PER_MINUTE_LIMIT = 10
@@ -50,20 +52,37 @@ module Chat
     private
 
     def process_webhook_payload(text:, key:)
-      validate_message_length(text)
       webhook = find_and_rate_limit_webhook(key)
+      webhook.chat_channel.add(Discourse.system_user)
 
-      chat_message_creator =
-        Chat::MessageCreator.create(
-          chat_channel: webhook.chat_channel,
-          user: Discourse.system_user,
-          content: text,
-          incoming_chat_webhook: webhook,
-        )
-      if chat_message_creator.failed?
-        render_json_error(chat_message_creator.error)
-      else
-        render json: success_json
+      with_service(
+        Chat::CreateMessage,
+        chat_channel_id: webhook.chat_channel_id,
+        guardian: Discourse.system_user.guardian,
+        message: text,
+        incoming_chat_webhook: webhook,
+      ) do
+        on_success { render json: success_json }
+        on_failed_contract do |contract|
+          raise Discourse::InvalidParameters.new(contract.errors.full_messages)
+        end
+        on_failed_policy(:no_silenced_user) { raise Discourse::InvalidAccess }
+        on_model_not_found(:channel) { raise Discourse::NotFound }
+        on_failed_policy(:allowed_to_join_channel) { raise Discourse::InvalidAccess }
+        on_model_not_found(:channel_membership) { raise Discourse::InvalidAccess }
+        on_failed_policy(:ensure_reply_consistency) { raise Discourse::NotFound }
+        on_failed_policy(:allowed_to_create_message_in_channel) do |policy|
+          render_json_error(policy.reason)
+        end
+        on_failed_policy(:ensure_valid_thread_for_channel) do
+          render_json_error(I18n.t("chat.errors.thread_invalid_for_channel"))
+        end
+        on_failed_policy(:ensure_thread_matches_parent) do
+          render_json_error(I18n.t("chat.errors.thread_does_not_match_parent"))
+        end
+        on_model_errors(:message) do |model|
+          render_json_error(model.errors.map(&:full_message).join(", "))
+        end
       end
     end
 
@@ -79,13 +98,6 @@ module Chat
         1.minute,
       ).performed!
       webhook
-    end
-
-    def validate_message_length(message)
-      return if message.length <= SiteSetting.chat_maximum_message_length
-      raise Discourse::InvalidParameters.new(
-              "Body cannot be over #{SiteSetting.chat_maximum_message_length} characters",
-            )
     end
 
     # The webhook POST body can be in 3 different formats:

plugins/chat/app/models/chat/message.rb
@@ -13,9 +13,9 @@ module Chat
 
     belongs_to :chat_channel, class_name: "Chat::Channel"
     belongs_to :user
-    belongs_to :in_reply_to, class_name: "Chat::Message"
+    belongs_to :in_reply_to, class_name: "Chat::Message", autosave: true
     belongs_to :last_editor, class_name: "User"
-    belongs_to :thread, class_name: "Chat::Thread", optional: true
+    belongs_to :thread, class_name: "Chat::Thread", optional: true, autosave: true
 
     has_many :replies,
              class_name: "Chat::Message",

plugins/chat/app/policies/chat/channel/message_creation_policy.rb
@@ -1,0 +1,45 @@
+# frozen_string_literal: true
+
+class Chat::Channel::MessageCreationPolicy < PolicyBase
+  class DirectMessageStrategy
+    class << self
+      def call(guardian, channel)
+        guardian.can_create_channel_message?(channel) || guardian.can_create_direct_message?
+      end
+
+      def reason(*)
+        I18n.t("chat.errors.user_cannot_send_direct_messages")
+      end
+    end
+  end
+
+  class CategoryStrategy
+    class << self
+      def call(guardian, channel)
+        guardian.can_create_channel_message?(channel)
+      end
+
+      def reason(_, channel)
+        I18n.t("chat.errors.channel_new_message_disallowed.#{channel.status}")
+      end
+    end
+  end
+
+  attr_reader :strategy
+
+  delegate :channel, to: :context
+
+  def initialize(*)
+    super
+    @strategy = CategoryStrategy
+    @strategy = DirectMessageStrategy if channel.direct_message_channel?
+  end
+
+  def call
+    strategy.call(guardian, channel)
+  end
+
+  def reason
+    strategy.reason(guardian, channel)
+  end
+end

plugins/chat/app/services/chat/action/publish_and_follow_direct_message_channel.rb
@@ -1,0 +1,49 @@
+# frozen_string_literal: true
+
+module Chat
+  module Action
+    class PublishAndFollowDirectMessageChannel
+      attr_reader :channel_membership
+
+      delegate :chat_channel, :user, to: :channel_membership
+
+      def self.call(...)
+        new(...).call
+      end
+
+      def initialize(channel_membership:)
+        @channel_membership = channel_membership
+      end
+
+      def call
+        return unless chat_channel.direct_message_channel?
+        return if users_allowing_communication.none?
+
+        chat_channel
+          .user_chat_channel_memberships
+          .where(user: users_allowing_communication)
+          .update_all(following: true)
+        Chat::Publisher.publish_new_channel(chat_channel, users_allowing_communication)
+      end
+
+      private
+
+      def users_allowing_communication
+        @users_allowing_communication ||= User.where(id: user_ids).to_a
+      end
+
+      def user_ids
+        UserCommScreener.new(
+          acting_user: user,
+          target_user_ids:
+            chat_channel.user_chat_channel_memberships.where(following: false).pluck(:user_id),
+        ).allowing_actor_communication + Array.wrap(current_user_id)
+      end
+
+      def current_user_id
+        return if channel_membership.following?
+        user.id
+      end
+    end
+  end
+end

plugins/chat/app/services/chat/create_message.rb
@@ -1,0 +1,191 @@
+# frozen_string_literal: true
+
+module Chat
+  # Service responsible for creating a new message.
+  #
+  # @example
+  #  Chat::CreateMessage.call(chat_channel_id: 2, guardian: guardian, message: "A new message")
+  #
+  class CreateMessage
+    include Service::Base
+
+    # @!method call(chat_channel_id:, guardian:, in_reply_to_id:, message:, staged_id:, upload_ids:, thread_id:, staged_thread_id:, incoming_chat_webhook:)
+    #   @param guardian [Guardian]
+    #   @param chat_channel_id [Integer]
+    #   @param message [String]
+    #   @param in_reply_to_id [Integer] ID of a message to reply to
+    #   @param thread_id [Integer] ID of a thread to reply to
+    #   @param upload_ids [Array<Integer>] IDs of uploaded documents
+    #   @param staged_id [String] arbitrary string that will be sent back to the client
+    #   @param staged_thread_id [String] arbitrary string that will be sent back to the client (for a new thread)
+    #   @param incoming_chat_webhook [Chat::IncomingWebhook]
+
+    policy :no_silenced_user
+    contract
+    model :channel
+    policy :allowed_to_join_channel
+    policy :allowed_to_create_message_in_channel, class_name: Chat::Channel::MessageCreationPolicy
+    model :channel_membership
+    model :reply, optional: true
+    policy :ensure_reply_consistency
+    model :thread, optional: true
+    policy :ensure_valid_thread_for_channel
+    policy :ensure_thread_matches_parent
+    model :uploads, optional: true
+    model :message, :instantiate_message
+    transaction do
+      step :save_message
+      step :delete_drafts
+      step :post_process_thread
+      step :create_webhook_event
+      step :update_channel_last_message
+      step :update_membership_last_read
+      step :process_direct_message_channel
+    end
+    step :publish_new_thread
+    step :publish_new_message_events
+    step :publish_user_tracking_state
+
+    class Contract
+      attribute :chat_channel_id, :string
+      attribute :in_reply_to_id, :string
+      attribute :message, :string
+      attribute :staged_id, :string
+      attribute :upload_ids, :array
+      attribute :thread_id, :string
+      attribute :staged_thread_id, :string
+      attribute :incoming_chat_webhook
+
+      validates :chat_channel_id, presence: true
+      validates :message, presence: true, if: -> { upload_ids.blank? }
+    end
+
+    private
+
+    def no_silenced_user(guardian:, **)
+      !guardian.is_silenced?
+    end
+
+    def fetch_channel(contract:, **)
+      Chat::Channel.find_by_id_or_slug(contract.chat_channel_id)
+    end
+
+    def allowed_to_join_channel(guardian:, channel:, **)
+      guardian.can_join_chat_channel?(channel)
+    end
+
+    def fetch_channel_membership(guardian:, channel:, **)
+      Chat::ChannelMembershipManager.new(channel).find_for_user(guardian.user)
+    end
+
+    def fetch_reply(contract:, **)
+      Chat::Message.find_by(id: contract.in_reply_to_id)
+    end
+
+    def ensure_reply_consistency(channel:, contract:, reply:, **)
+      return true if contract.in_reply_to_id.blank?
+      reply&.chat_channel == channel
+    end
+
+    def fetch_thread(contract:, reply:, channel:, **)
+      return Chat::Thread.find_by(id: contract.thread_id) if contract.thread_id.present?
+      return unless reply
+      reply.thread ||
+        reply.build_thread(
+          original_message: reply,
+          original_message_user: reply.user,
+          channel: channel,
+        )
+    end
+
+    def ensure_valid_thread_for_channel(thread:, contract:, channel:, **)
+      return true if contract.thread_id.blank?
+      thread&.channel == channel
+    end
+
+    def ensure_thread_matches_parent(thread:, reply:, **)
+      return true unless thread && reply
+      reply.thread == thread
+    end
+
+    def fetch_uploads(contract:, guardian:, **)
+      return [] if !SiteSetting.chat_allow_uploads
+      guardian.user.uploads.where(id: contract.upload_ids)
+    end
+
+    def instantiate_message(channel:, guardian:, contract:, uploads:, thread:, reply:, **)
+      channel.chat_messages.new(
+        user: guardian.user,
+        last_editor: guardian.user,
+        in_reply_to: reply,
+        message: contract.message,
+        uploads: uploads,
+        thread: thread,
+      )
+    end
+
+    def save_message(message:, **)
+      message.cook
+      message.save!
+      message.create_mentions
+    end
+
+    def delete_drafts(channel:, guardian:, **)
+      Chat::Draft.where(user: guardian.user, chat_channel: channel).destroy_all
+    end
+
+    def post_process_thread(thread:, message:, guardian:, **)
+      return unless thread
+
+      thread.update!(last_message: message)
+      thread.increment_replies_count_cache
+      thread.add(guardian.user).update!(last_read_message: message)
+      thread.add(thread.original_message_user)
+    end
+
+    def create_webhook_event(contract:, message:, **)
+      return if contract.incoming_chat_webhook.blank?
+      message.create_chat_webhook_event(incoming_chat_webhook: contract.incoming_chat_webhook)
+    end
+
+    def update_channel_last_message(channel:, message:, **)
+      return if message.in_thread?
+      channel.update!(last_message: message)
+    end
+
+    def update_membership_last_read(channel_membership:, message:, **)
+      return if message.in_thread?
+      channel_membership.update!(last_read_message: message)
+    end
+
+    def process_direct_message_channel(channel_membership:, **)
+      Chat::Action::PublishAndFollowDirectMessageChannel.call(
+        channel_membership: channel_membership,
+      )
+    end
+
+    def publish_new_thread(reply:, contract:, channel:, thread:, **)
+      return unless channel.threading_enabled?
+      return unless reply&.thread_id_previously_changed?(from: nil)
+      Chat::Publisher.publish_thread_created!(channel, reply, thread.id, contract.staged_thread_id)
+    end
+
+    def publish_new_message_events(channel:, message:, contract:, guardian:, **)
+      Chat::Publisher.publish_new!(
+        channel,
+        message,
+        contract.staged_id,
+        staged_thread_id: contract.staged_thread_id,
+      )
+      Jobs.enqueue(Jobs::Chat::ProcessMessage, { chat_message_id: message.id })
+      Chat::Notifier.notify_new(chat_message: message, timestamp: message.created_at)
+      DiscourseEvent.trigger(:chat_message_created, message, channel, guardian.user)
+    end
+
+    def publish_user_tracking_state(message:, channel:, channel_membership:, guardian:, **)
+      message_to_publish = message
+      message_to_publish = channel_membership.last_read_message || message if message.in_thread?
+      Chat::Publisher.publish_user_tracking_state!(guardian.user, channel, message_to_publish)
+    end
+  end
+end

commit 127b298b6cc7c052c0ac4660c7b2dfcbdd1bf040

FIX: ensures automation can send chat message
It's been broken in https://github.com/discourse/discourse/commit/243793ec6ea41a9bf53dc6b9585ba9239dfc4c85. Sadly it's not very practical to write cross plugins tests.

plugins/chat/plugin.rb
@@ -417,10 +417,10 @@ after_initialize do
         placeholders = { channel_name: channel.title(sender) }.merge(context["placeholders"] || {})
 
         creator =
-          Chat::CreateMessage.call(
-            chat_channel: channel,
+          ::Chat::CreateMessage.call(
+            chat_channel_id: channel.id,
             guardian: sender.guardian,
-            content: utils.apply_placeholders(fields.dig("message", "value"), placeholders),
+            message: utils.apply_placeholders(fields.dig("message", "value"), placeholders),
           )
 
         if creator.failure?