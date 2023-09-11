afdd7e395356881c0df1430174f60648526ecec8
Blake Erickson <o.blakeerickson@gmail.com>
2023-09-11 14:54:41 -0600

    add tests, remove wrapping array causing commas in output

--- app/assets/javascripts/discourse/app/instance-initializers/category-badge-css-generator.js
+++ app/assets/javascripts/discourse/app/instance-initializers/category-badge-css-generator.js
@@ -16,22 +16,14 @@ export default {
       return;
     }
 
-    const generatedCssClasses = [
-      this.site.categories.map((category) => {
-        let parentCat = Category.findById(get(category, "parent_category_id"));
-        let badgeClass = `
-          .badge-category.badge-category-${category.id}:before {
-            background: var(--category-${category.id}-color);
-          }`;
-        if (parentCat) {
-          badgeClass += `
-            .badge-category.badge-subcategory-${parentCat.id}:before {
-              background: linear-gradient(90deg, var(--category-${parentCat.id}-color) 50%, var(--category-${category.id}-color) 50%);
-            }`;
-        }
-        return badgeClass;
-      }),
-    ];
+    const generatedCssClasses = this.site.categories.map((category) => {
+      let parentCat = Category.findById(get(category, "parent_category_id"));
+      let badgeClass = `.badge-category.badge-category-${category.id}:before { background: var(--category-${category.id}-color); }`;
+      if (parentCat) {
+        badgeClass += `.badge-category.badge-subcategory-${parentCat.id}:before { background: linear-gradient(90deg, var(--category-${parentCat.id}-color) 50%, var(--category-${category.id}-color) 50%); }`;
+      }
+      return badgeClass;
+    });
 
     const cssTag = document.createElement("style");
     cssTag.type = "text/css";
