d393fdca315f82e56f82603b682fceb4a07b38aa
Jarek Radosz <jradosz@gmail.com>
2023-09-11 11:33:08 +0200

    DEV: Fix random typos (#23497)

--- app/assets/javascripts/discourse/tests/unit/lib/ember-action-modifer-test.js
+++ app/assets/javascripts/discourse/tests/unit/lib/ember-action-modifer-test.js
@@ -4,7 +4,7 @@ import { click, doubleClick, render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import ClassicComponent from "@ember/component";
 
-module("Unit | Lib | ember-action-modifer", function (hooks) {
+module("Unit | Lib | ember-action-modifier", function (hooks) {
   setupRenderingTest(hooks);
 
   test("`{{action}}` can target a function", async function (assert) {
