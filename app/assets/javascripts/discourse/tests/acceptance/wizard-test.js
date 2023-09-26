b96f27237cd565abe77df5bb54a516ffb98d5312
Godfrey Chan <godfreykfc@gmail.com>
2023-09-25 22:11:12 -0700

    Simplify Wizard buttons
    This code has gone through a lot of refactors and repurposing, and
    things have become more complicated and confusing than they needed
    to be.
    
    At present, the Wizard goes through a few "required" steps for the
    basics, then shows a "ready" screen directing the user to "jump in"
    but have an option to continue on for optional screens, eventually
    ending on the "corporate" screen which is the final step.
    
    On desktop, there are two groups of buttons –
    
    1. on the left side, it shows a back button except for the first
       screen
    
    2. on the right side, there is a always a primary button (either
       "next" or "jump in"), and then to its left, there is sometimes
       a secondary action styled as a link (either "configure more" or
       "exit setup")
    
    On mobile, these buttons are stacked, with the back button sitting
    at the bottom, if present.
    
    In table form:
    
    Step        Back Button?     Primary Action      Secondary Action
    ------------------------------------------------------------------
    First            No               Next                  N/A
    ------------------------------------------------------------------
    ...             Yes               Next                  N/A
    ------------------------------------------------------------------
    Ready           Yes              Jump In          Configure More
    ------------------------------------------------------------------
    ...             Yes               Next              Exit Setup
    ------------------------------------------------------------------
    Last            Yes              Jump In                N/A
    ------------------------------------------------------------------
    
    Back Button: without saving, go back to the last page
    Next Button: save, and if successful, go to the next page
    Configure More: re-skinned next button
    Exit Setup: without saving, go to the home page ("finish")
    Jump In: on the "ready" page, it exits the setup ("finish"), on the
    last page, it saves, and if successful, go to the home page
    
    This commit re-aligns the code to reflect this reality without the
    unnecessary indirections leftover from previous refactors.

--- app/assets/javascripts/discourse/tests/acceptance/wizard-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/wizard-test.js
@@ -28,19 +28,36 @@ acceptance("Wizard", function (needs) {
       exists(".wizard-container__step.hello-world"),
       "it adds a class for the step id"
     );
-    assert.ok(
-      !exists(".wizard-container__button.finish"),
-      "cannot finish on first step"
-    );
-    assert.ok(exists(".wizard-container__step-progress"));
     assert.ok(exists(".wizard-container__step-title"));
     assert.ok(exists(".wizard-container__step-description"));
     assert.ok(
       !exists(".invalid #full_name"),
       "don't show it as invalid until the user does something"
     );
-    assert.ok(!exists(".wizard-container__button.btn-back"));
     assert.ok(!exists(".wizard-container__field .error"));
+
+    // First step: only next button
+    assert.ok(!exists(".wizard-canvas"), "First step: no confetti");
+    assert.ok(
+      !exists(".wizard-container__button.back"),
+      "First step: no back button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.next"),
+      "First step: next button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.jump-in"),
+      "First step: no jump-in button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.configure-more"),
+      "First step: no configure-more button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.finish"),
+      "First step: no finish button"
+    );
 
     // invalid data
     await click(".wizard-container__button.next");
@@ -55,13 +72,91 @@ acceptance("Wizard", function (needs) {
     // server validation ok
     await fillIn("input#full_name", "Evil Trout");
     await click(".wizard-container__button.next");
+    assert.ok(
+      exists(".wizard-container__step.hello-again"),
+      "step: hello-again"
+    );
     assert.ok(!exists(".wizard-container__field .error"));
     assert.ok(!exists(".wizard-container__step-description"));
+
+    // Pre-ready: back and next buttons
+    assert.ok(!exists(".wizard-canvas"), "Pre-ready step: no confetti");
     assert.ok(
-      exists(".wizard-container__button.finish"),
-      "shows finish on an intermediate step"
+      exists(".wizard-container__button.back"),
+      "Pre-ready step: back button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.next"),
+      "Pre-ready step: next button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.jump-in"),
+      "Pre-ready step: no jump-in button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.configure-more"),
+      "Pre-ready step: no configure-more button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.finish"),
+      "Pre-ready step: no finish button"
     );
 
+    // ok to skip an optional field
+    await click(".wizard-container__button.next");
+    assert.ok(exists(".wizard-container__step.ready"), "step: ready");
+
+    // Ready: back, configure-more and jump-in buttons
+    assert.ok(exists(".wizard-canvas"), "Ready step: confetti");
+    assert.ok(
+      exists(".wizard-container__button.back"),
+      "Ready step: back button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.next"),
+      "Ready step: no next button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.jump-in"),
+      "Ready step: jump-in button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.configure-more"),
+      "Ready step: configure-more button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.finish"),
+      "Ready step: no finish button"
+    );
+
+    // continue on to optional steps
+    await click(".wizard-container__button.configure-more");
+    assert.ok(exists(".wizard-container__step.optional"), "step: optional");
+
+    // Post-ready: back, next and finish buttons
+    assert.ok(!exists(".wizard-canvas"), "Post-ready step: no confetti");
+    assert.ok(
+      exists(".wizard-container__button.back"),
+      "Post-ready step: back button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.next"),
+      "Post-ready step: next button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.jump-in"),
+      "Post-ready step: no jump-in button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.configure-more"),
+      "Post-ready step: no configure-more button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.finish"),
+      "Post-ready step: finish button"
+    );
+
+    // finish early, does not save/validate
     await click(".wizard-container__button.finish");
     assert.strictEqual(
       currentURL(),
@@ -69,9 +164,58 @@ acceptance("Wizard", function (needs) {
       "it should transition to the homepage"
     );
 
-    await visit("/wizard/steps/styling");
+    await visit("/wizard/steps/optional");
+    assert.ok(exists(".wizard-container__step.optional"), "step: optional");
+
+    // Post-ready: back, next and finish buttons
+    assert.ok(!exists(".wizard-canvas"), "Post-ready step: no confetti");
+    assert.ok(
+      exists(".wizard-container__button.back"),
+      "Post-ready step: back button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.next"),
+      "Post-ready step: next button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.jump-in"),
+      "Post-ready step: no jump-in button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.configure-more"),
+      "Post-ready step: no configure-more button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.finish"),
+      "Post-ready step: finish button"
+    );
 
     await click(".wizard-container__button.primary.next");
+    assert.ok(exists(".wizard-container__step.corporate"), "step: corporate");
+
+    // Final step: back and jump-in buttons
+    assert.ok(!exists(".wizard-canvas"), "Finish step: no confetti");
+    assert.ok(
+      exists(".wizard-container__button.back"),
+      "Finish step: back button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.next"),
+      "Finish step: no next button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.jump-in"),
+      "Finish step: jump-in button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.configure-more"),
+      "Finish step: no configure-more button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.finish"),
+      "Finish step: no finish button"
+    );
+
     assert.ok(
       exists(".wizard-container__text-input#company_name"),
       "went to the next step"
@@ -80,31 +224,61 @@ acceptance("Wizard", function (needs) {
       exists(".wizard-container__preview"),
       "renders the component field"
     );
-    assert.ok(
-      exists(".wizard-container__button.jump-in"),
-      "last step shows a jump in button"
-    );
-    assert.ok(
-      exists(".wizard-container__button.btn-back"),
-      "shows the back button"
-    );
     assert.ok(!exists(".wizard-container__step-title"));
+
+    await click(".wizard-container__button.back");
+    assert.ok(exists(".wizard-container__step.optional"), "step: optional");
+
+    // Post-ready: back, next and finish buttons
+    assert.ok(!exists(".wizard-canvas"), "Post-ready step: no confetti");
+    assert.ok(
+      exists(".wizard-container__button.back"),
+      "Post-ready step: back button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.next"),
+      "Post-ready step: next button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.jump-in"),
+      "Post-ready step: no jump-in button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.configure-more"),
+      "Post-ready step: no configure-more button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.finish"),
+      "Post-ready step: finish button"
+    );
+
+    assert.ok(exists(".wizard-container__step-title"), "shows the step title");
+
+    await click(".wizard-container__button.next");
+    assert.ok(exists(".wizard-container__step.corporate"), "step: optional");
+
+    // Final step: back and jump-in buttons
+    assert.ok(!exists(".wizard-canvas"), "Finish step: no confetti");
+    assert.ok(
+      exists(".wizard-container__button.back"),
+      "Finish step: back button"
+    );
     assert.ok(
       !exists(".wizard-container__button.next"),
-      "does not show next button"
+      "Finish step: no next button"
+    );
+    assert.ok(
+      exists(".wizard-container__button.jump-in"),
+      "Finish step: jump-in button"
+    );
+    assert.ok(
+      !exists(".wizard-container__button.configure-more"),
+      "Finish step: no configure-more button"
     );
     assert.ok(
       !exists(".wizard-container__button.finish"),
-      "cannot finish on last step"
+      "Finish step: no finish button"
     );
-
-    await click(".wizard-container__button.btn-back");
-    assert.ok(exists(".wizard-container__step-title"), "shows the step title");
-    assert.ok(
-      exists(".wizard-container__button.next"),
-      "shows the next button"
-    );
-    await click(".wizard-container__button.next");
 
     // server validation fail
     await fillIn("input#company_name", "Server Fail");
