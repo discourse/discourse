e349364b4639080d8d31428a7183989a7ab48aa4
Jarek Radosz <jradosz@gmail.com>
2023-09-28 17:52:45 +0200

    Remove the extra ast transforms

--- app/assets/javascripts/js-processor.js
+++ app/assets/javascripts/js-processor.js
@@ -39,14 +39,7 @@ function manipulateAstNodeForTheme(node, themeId) {
   // Magically add theme id as the first param for each of these helpers)
   if (
     node.path.parts &&
-    [
-      "theme-i18n",
-      "themeI18n",
-      "theme-prefix",
-      "themePrefix",
-      "theme-setting",
-      "themeSetting",
-    ].includes(node.path.parts[0])
+    ["theme-i18n", "theme-prefix", "theme-setting"].includes(node.path.parts[0])
   ) {
     if (node.params.length === 1) {
       node.params.unshift({
