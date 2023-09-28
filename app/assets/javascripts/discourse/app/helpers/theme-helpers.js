6d13c9a372d632d209fdd015c6a85001bbfe89e6
Jarek Radosz <jradosz@gmail.com>
2023-09-28 18:12:41 +0200

    restore

--- app/assets/javascripts/discourse/app/helpers/theme-helpers.js
+++ app/assets/javascripts/discourse/app/helpers/theme-helpers.js
@@ -2,17 +2,15 @@ import { registerUnbound } from "discourse-common/lib/helpers";
 import I18n from "I18n";
 import { getSetting as getThemeSetting } from "discourse/lib/theme-settings-store";
 
-export function themeI18n(themeId, key, params) {
+registerUnbound("theme-i18n", (themeId, key, params) => {
   return I18n.t(`theme_translations.${themeId}.${key}`, params);
-}
-registerUnbound("theme-i18n", themeI18n);
+});
 
-export function themePrefix(themeId, key) {
-  return `theme_translations.${themeId}.${key}`;
-}
-registerUnbound("theme-prefix", themePrefix);
+registerUnbound(
+  "theme-prefix",
+  (themeId, key) => `theme_translations.${themeId}.${key}`
+);
 
-export function themeSetting(themeId, key) {
+registerUnbound("theme-setting", (themeId, key) => {
   return getThemeSetting(themeId, key);
-}
-registerUnbound("theme-setting", themeSetting);
+});
