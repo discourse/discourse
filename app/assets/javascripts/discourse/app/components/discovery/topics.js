9da159009d97c8d0125e623d26760417c55c661a
David Taylor <david@taylorhq.com>
2023-10-03 10:58:17 +0100

    Scope dismissNew to tag correctly

--- app/assets/javascripts/discourse/app/components/discovery/topics.js
+++ app/assets/javascripts/discourse/app/components/discovery/topics.js
@@ -55,6 +55,7 @@ export default class DiscoveryTopics extends Component {
     let topicIds = this.args.bulkSelectHelper.selected.map((topic) => topic.id);
     Topic.resetNew(this.args.category, !this.args.noSubcategories, {
       tracked,
+      tag: this.tag,
       topicIds,
       dismissPosts,
       dismissTopics,
