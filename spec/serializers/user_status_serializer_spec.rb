9fb1c98052a903fc3b7b7f5d763aca268e4625ad
Jarek Radosz <jradosz@gmail.com>
2023-10-05 12:34:55 +0200

    Fix fabrication

--- spec/serializers/user_status_serializer_spec.rb
+++ spec/serializers/user_status_serializer_spec.rb
@@ -3,7 +3,12 @@
 RSpec.describe UserStatusSerializer do
   fab!(:user) { Fabricate(:user) }
   fab!(:user_status) do
-    Fabricate(:user_status, user: user, ends_at: Time.parse("2023-09-29T03:25:00Z"))
+    Fabricate(
+      :user_status,
+      user: user,
+      set_at: Time.parse("2023-09-29T02:20:00Z"),
+      ends_at: Time.parse("2023-09-29T03:25:00Z"),
+    )
   end
 
   describe "#ends_at" do
