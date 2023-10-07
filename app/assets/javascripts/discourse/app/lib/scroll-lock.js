64f3adb64a1e61bedb544414db09c72ebc1bfbdc
Jeff Wong <awole20@gmail.com>
2023-10-07 11:45:57 -0700

    FIX: correctyl set scroll gap

--- app/assets/javascripts/discourse/app/lib/scroll-lock.js
+++ app/assets/javascripts/discourse/app/lib/scroll-lock.js
@@ -5,12 +5,12 @@
 export default class ScrollLock {
   static scrollingElement = document.scrollingElement;
   static lock() {
-    this.scrollingElement.classList.add("scroll-lock");
     const scrollGap = Math.max(
       0,
       window.innerWidth - this.scrollingElement.clientWidth
     );
-    this.scrollingElement.style.setProperty("--scroll-gap", scrollGap);
+    this.scrollingElement.style.setProperty("--scroll-gap", `${scrollGap}px`);
+    this.scrollingElement.classList.add("scroll-lock");
   }
   static unlock() {
     this.scrollingElement.classList.remove("scroll-lock");
