06509ba06dfee306c1e4c557fce32a071eaedf13
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-10-11 11:45:42 +0200

    linting

--- plugins/chat/assets/javascripts/discourse/routes/chat-channel.js
+++ plugins/chat/assets/javascripts/discourse/routes/chat-channel.js
@@ -1,6 +1,6 @@
+import { inject as service } from "@ember/service";
 import DiscourseRoute from "discourse/routes/discourse";
 import withChatChannel from "./chat-channel-decorator";
-import { inject as service } from "@ember/service";
 
 @withChatChannel
 export default class ChatChannelRoute extends DiscourseRoute {
