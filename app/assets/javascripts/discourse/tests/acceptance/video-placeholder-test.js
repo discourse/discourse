d6f1478fd5926549261496311a6b98a697e447a1
Blake Erickson <o.blakeerickson@gmail.com>
2023-10-12 13:37:50 -0600

    Use assert.dom for qunit tests

--- app/assets/javascripts/discourse/tests/acceptance/video-placeholder-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/video-placeholder-test.js
@@ -1,6 +1,6 @@
 import { click, visit } from "@ember/test-helpers";
 import { test } from "qunit";
-import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 
 acceptance("Video Placeholder Test", function () {
   test("placeholder shows up on posts with videos", async function (assert) {
@@ -16,12 +16,12 @@ acceptance("Video Placeholder Test", function () {
 
     const overlay = postWithVideo.querySelector(".video-placeholder-overlay");
 
-    assert.notOk(exists("video"), "The video element does not exist yet");
+    assert.dom("video").doesNotExist("The video element does not exist yet");
 
     await click(overlay);
 
-    assert.ok(exists(".video-container"), "The video container appears");
+    assert.dom(".video-container").exists("The video container appears");
 
-    assert.ok(exists("video"), "The video element appears");
+    assert.dom("video").exists("The video element appears");
   });
 });
