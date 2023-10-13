a5858e60e142b8a27694deea471dd4e8b92bfa1a
Jarek Radosz <jradosz@gmail.com>
2023-10-13 17:23:04 +0200

    FIX: Transitioning to tag-intersection route (#23931)
    …didn't correctly update location query params.
    
    A followup to 1df3ccc903c621b85a43102d8b3e945cd279bd2e (things broke after merging `main` to PR's branch)

--- app/assets/javascripts/discourse/app/controllers/tags-intersection.js
+++ app/assets/javascripts/discourse/app/controllers/tags-intersection.js
@@ -2,12 +2,5 @@ import { queryParams } from "discourse/controllers/discovery-sortable";
 import TagShowController from "discourse/controllers/tag-show";
 
 export default class TagsIntersectionController extends TagShowController {
-  constructor() {
-    super(...arguments);
-
-    this.set("queryParams", [
-      ...Object.keys(queryParams),
-      { categoryParam: "category" },
-    ]);
-  }
+  queryParams = [...Object.keys(queryParams), { categoryParam: "category" }];
 }
--- app/assets/javascripts/discourse/app/routes/tags-intersection.js
+++ app/assets/javascripts/discourse/app/routes/tags-intersection.js
@@ -1,4 +1,3 @@
-import { queryParams } from "discourse/controllers/discovery-sortable";
 import { buildTagRoute } from "discourse/routes/tag-show";
 
 // The tags-intersection route is exactly the same as the tags-show route, but the wildcard at the
@@ -6,15 +5,6 @@ import { buildTagRoute } from "discourse/routes/tag-show";
 // breaking all other tags-show routes. Ember thinks the query params are addition tags and should
 // be handled by the intersection logic. Defining tags-intersection as something separate avoids
 // that confusion.
-export default buildTagRoute().extend({
-  controllerName: "tags.intersection",
-
-  init() {
-    this._super(...arguments);
-
-    // The only difference is support for `category` query param.
-    // Other routes include category in the route path.
-    this.set("queryParams", { ...queryParams });
-    this.queryParams["categoryParam"] = { replace: true, refreshModel: true };
-  },
-});
+export default class extends buildTagRoute() {
+  controllerName = "tags.intersection";
+}
