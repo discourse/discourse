9d883c330ad58c65ad3bb6315d4be5817f72162c
David Taylor <david@taylorhq.com>
2023-10-18 11:00:42 +0100

    Add discovery service and controller shims

---
+++ app/assets/javascripts/discourse/app/instance-initializers/discovery-controller-shims.js
@@ -0,0 +1,50 @@
+import EmberObject from "@ember/object";
+import { dependentKeyCompat } from "@ember/object/compat";
+import { inject as service } from "@ember/service";
+import deprecated from "discourse-common/lib/deprecated";
+
+class NavigationCategoryControllerShim extends EmberObject {
+  @service discovery;
+
+  @dependentKeyCompat
+  get category() {
+    deprecated(
+      "controller:navigation/category no longer exists, and this shim will eventually be removed. Use the discovery service instead.",
+      {
+        id: "discourse.navigation-category-controller",
+      }
+    );
+    return this.discovery.category;
+  }
+}
+
+class DiscoveryTopicsControllerShim extends EmberObject {
+  @service discovery;
+
+  @dependentKeyCompat
+  get model() {
+    deprecated(
+      "controller:navigation/category no longer exists, and this shim will eventually be removed. Use the discovery service instead.",
+      {
+        id: "discourse.navigation-category-controller",
+      }
+    );
+    if (this.discovery.onDiscoveryRoute) {
+      return this.discovery.currentTopicList;
+    }
+  }
+}
+
+export default {
+  initialize(container) {
+    container.register(
+      "controller:navigation/category",
+      NavigationCategoryControllerShim
+    );
+
+    container.register(
+      "controller:discovery/topics",
+      DiscoveryTopicsControllerShim
+    );
+  },
+};
