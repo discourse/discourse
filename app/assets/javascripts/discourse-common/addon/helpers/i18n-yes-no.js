7c5e299e44fabd25f2b0c5099dfcadd11ad61e4e
David Taylor <david@taylorhq.com>
2023-10-19 10:58:12 +0100

    Correct i18n conversion
    modules were backwards

--- app/assets/javascripts/discourse-common/addon/helpers/i18n-yes-no.js
+++ app/assets/javascripts/discourse-common/addon/helpers/i18n-yes-no.js
@@ -1,8 +1,8 @@
 import { registerRawHelper } from "discourse-common/lib/helpers";
 import I18n from "discourse-i18n";
 
-export default function i18n(key, params) {
-  return I18n.t(key, params);
-}
+registerRawHelper("i18n-yes-no", i18nYesNo);
 
-registerRawHelper("i18n", i18n);
+export default function i18nYesNo(value, params) {
+  return I18n.t(value ? "yes_value" : "no_value", params);
+}
