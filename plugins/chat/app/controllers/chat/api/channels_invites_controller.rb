1f8e2f17312c70ee0dc719070e7e211522cb3810
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-10-24 15:48:36 +0200

    not needed

--- plugins/chat/app/controllers/chat/api/channels_invites_controller.rb
+++ plugins/chat/app/controllers/chat/api/channels_invites_controller.rb
@@ -3,7 +3,6 @@
 class Chat::Api::ChannelsInvitesController < Chat::ApiController
   def create
     with_service(Chat::InviteUsersToChannel) do
-      on_success { render json: success_json }
       on_failed_policy(:can_view_channel) { raise Discourse::InvalidAccess }
       on_model_not_found(:channel) { raise Discourse::NotFound }
     end
