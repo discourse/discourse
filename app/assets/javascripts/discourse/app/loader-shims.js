6a9697c80882b3d140e8e7efcdfdc0cf9f412e30
Godfrey Chan <godfreykfc@gmail.com>
2023-10-25 12:26:41 -0700

    DEV: preemptively add `@ember/string` shim
    So when we switch back to the real package plugins
    will continue to have access.

--- app/assets/javascripts/discourse/app/loader-shims.js
+++ app/assets/javascripts/discourse/app/loader-shims.js
@@ -4,6 +4,7 @@ import loaderShim from "discourse-common/lib/loader-shim";
 // AMD shims for the app bunndle, see the comment in loader-shim.js
 // These effectively become public APIs for plugins, so add/remove them carefully
 loaderShim("@discourse/itsatrap", () => importSync("@discourse/itsatrap"));
+loaderShim("@ember/string", () => importSync("@ember/string"));
 loaderShim("@ember-compat/tracked-built-ins", () =>
   importSync("@ember-compat/tracked-built-ins")
 );
