d0fa28df1429e74b80b1858a79d4f2574e5ad0f4
Jarek Radosz <jradosz@gmail.com>
2023-10-26 15:58:03 +0200

    clean up username-validation

--- app/assets/javascripts/discourse/app/mixins/username-validation.js
+++ app/assets/javascripts/discourse/app/mixins/username-validation.js
@@ -8,20 +8,17 @@ import { observes } from "discourse-common/utils/decorators";
 import I18n from "discourse-i18n";
 
 function failedResult(attrs) {
-  let result = EmberObject.create({
+  return EmberObject.create({
     shouldCheck: false,
     failed: true,
     ok: false,
     element: document.querySelector("#new-account-username"),
+    ...attrs,
   });
-  result.setProperties(attrs);
-  return result;
 }
 
 function validResult(attrs) {
-  let result = EmberObject.create({ ok: true });
-  result.setProperties(attrs);
-  return result;
+  return EmberObject.create({ ok: true, ...attrs });
 }
 
 export default Mixin.create({
@@ -29,26 +26,24 @@ export default Mixin.create({
   maxUsernameLength: setting("max_username_length"),
   minUsernameLength: setting("min_username_length"),
 
-  fetchExistingUsername() {
-    User.checkUsername(null, this.accountEmail).then((result) => {
-      if (
-        result.suggestion &&
-        (isEmpty(this.accountUsername) ||
-          this.accountUsername === this.get("authOptions.username"))
-      ) {
-        this.setProperties({
-          accountUsername: result.suggestion,
-          prefilledUsername: result.suggestion,
-        });
-      }
-    });
+  async fetchExistingUsername() {
+    const result = await User.checkUsername(null, this.accountEmail);
+
+    if (
+      result.suggestion &&
+      (isEmpty(this.accountUsername) ||
+        this.accountUsername === this.get("authOptions.username"))
+    ) {
+      this.setProperties({
+        accountUsername: result.suggestion,
+        prefilledUsername: result.suggestion,
+      });
+    }
   },
 
   @observes("accountUsername")
   triggerValidation() {
-    let { accountUsername } = this;
-
-    let result = this.basicUsernameValidation(accountUsername);
+    const result = this.basicUsernameValidation(this.accountUsername);
     if (result.shouldCheck) {
       discourseDebounce(this, this.checkUsernameAvailability, 500);
     }
@@ -83,42 +78,39 @@ export default Mixin.create({
     });
   },
 
-  checkUsernameAvailability() {
-    return User.checkUsername(this.accountUsername, this.accountEmail).then(
-      (result) => {
-        if (this.isDestroying || this.isDestroyed) {
-          return;
-        }
-
-        this.set("isDeveloper", false);
-        if (result.available) {
-          if (result.is_developer) {
-            this.set("isDeveloper", true);
-          }
-          return this.set(
-            "usernameValidation",
-            validResult({ reason: I18n.t("user.username.available") })
-          );
-        } else {
-          if (result.suggestion) {
-            return this.set(
-              "usernameValidation",
-              failedResult({
-                reason: I18n.t("user.username.not_available", result),
-              })
-            );
-          } else {
-            return this.set(
-              "usernameValidation",
-              failedResult({
-                reason: result.errors
-                  ? result.errors.join(" ")
-                  : I18n.t("user.username.not_available_no_suggestion"),
-              })
-            );
-          }
-        }
-      }
+  async checkUsernameAvailability() {
+    const result = await User.checkUsername(
+      this.accountUsername,
+      this.accountEmail
     );
+
+    if (this.isDestroying || this.isDestroyed) {
+      return;
+    }
+
+    this.set("isDeveloper", !!result.is_developer);
+
+    if (result.available) {
+      this.set(
+        "usernameValidation",
+        validResult({ reason: I18n.t("user.username.available") })
+      );
+    } else if (result.suggestion) {
+      this.set(
+        "usernameValidation",
+        failedResult({
+          reason: I18n.t("user.username.not_available", result),
+        })
+      );
+    } else {
+      this.set(
+        "usernameValidation",
+        failedResult({
+          reason: result.errors
+            ? result.errors.join(" ")
+            : I18n.t("user.username.not_available_no_suggestion"),
+        })
+      );
+    }
   },
 });
