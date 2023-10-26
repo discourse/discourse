53bd443cd7e7232d2deccfa51f9dc74bd922e3fa
Jarek Radosz <jradosz@gmail.com>
2023-10-26 14:41:25 +0200

    Move the create account test

---
+++ app/assets/javascripts/discourse/tests/acceptance/create-account-test.js
@@ -0,0 +1,37 @@
+import { click, fillIn, visit } from "@ember/test-helpers";
+import { test } from "qunit";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
+
+acceptance("Create Account", function () {
+  test("create an account", async function (assert) {
+    await visit("/");
+    await click("header .sign-up-button");
+
+    assert.dom(".create-account").exists("it shows the create account modal");
+
+    await fillIn("#new-account-name", "Dr. Good Tuna");
+    await fillIn("#new-account-password", "cool password bro");
+
+    // without this double fill, field will sometimes being empty
+    // got consistent repro by having browser search bar focused when starting test
+    await fillIn("#new-account-email", "good.tuna@test.com");
+    await fillIn("#new-account-email", "good.tuna@test.com");
+
+    // Check username
+    await fillIn("#new-account-username", "taken");
+    assert
+      .dom("#username-validation.bad")
+      .exists("the username validation is bad");
+    await click(".modal-footer .btn-primary");
+
+    await fillIn("#new-account-username", "good-tuna");
+    assert
+      .dom("#username-validation.good")
+      .exists("the username validation is good");
+
+    await click(".modal-footer .btn-primary");
+    assert
+      .dom(".modal-footer .btn-primary:disabled")
+      .exists("create account is disabled");
+  });
+});
