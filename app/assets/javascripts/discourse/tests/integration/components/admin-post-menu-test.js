8c8bd40c37e6fd363cdf55f8eb72cc00a0a7956b
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-10-26 18:35:36 +0200

    DEV: adds tests for admin-post-menu

---
+++ app/assets/javascripts/discourse/tests/integration/components/admin-post-menu-test.js
@@ -0,0 +1,36 @@
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+import { module, test } from "qunit";
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+import I18n from "I18n";
+
+module("Integration | Component | admin-post-menu", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("renders default button", async function (assert) {
+    this.currentUser.admin = true;
+    this.data = { transformedPost: { id: 1 } };
+
+    await render(hbs`<AdminPostMenu @data={{this.data}} />`);
+
+    assert
+      .dom(".moderation-history")
+      .hasText(I18n.t("review.moderation_history"));
+  });
+
+  test("renders button from API", async function (assert) {
+    withPluginApi("1.16.0", (api) => {
+      api.addPostAdminMenuButton(() => {
+        return {
+          translatedLabel: "test",
+          className: "test-button",
+        };
+      });
+    });
+
+    await render(hbs`<AdminPostMenu  />`);
+
+    assert.dom(".test-button").hasText("test");
+  });
+});
