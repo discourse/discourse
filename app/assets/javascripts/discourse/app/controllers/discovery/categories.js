67a6fbdd0bb2b70b3b47bf27e29a7ab4abc78021
David Taylor <david@taylorhq.com>
2023-10-27 16:22:26 +0100

    Refactor categories display mode (#24143)

--- app/assets/javascripts/discourse/app/controllers/discovery/categories.js
+++ app/assets/javascripts/discourse/app/controllers/discovery/categories.js
@@ -2,44 +2,7 @@ import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { reads } from "@ember/object/computed";
 import { inject as service } from "@ember/service";
-import { dasherize } from "@ember/string";
 import discourseComputed from "discourse-common/utils/decorators";
-
-const subcategoryStyleComponentNames = {
-  rows: "categories_only",
-  rows_with_featured_topics: "categories_with_featured_topics",
-  boxes: "categories_boxes",
-  boxes_with_featured_topics: "categories_boxes_with_topics",
-};
-
-const mobileCompatibleViews = [
-  "categories_with_featured_topics",
-  "subcategories_with_featured_topics",
-];
-
-// Todo: make this return a component class instead of string
-export function categoriesComponent({ site, siteSettings, parentCategory }) {
-  let style = siteSettings.desktop_category_page_style;
-
-  if (site.mobileView && !mobileCompatibleViews.includes(style)) {
-    style = mobileCompatibleViews[0];
-  }
-
-  if (parentCategory) {
-    style =
-      subcategoryStyleComponentNames[
-        parentCategory.get("subcategory_list_style")
-      ] || style;
-  }
-
-  const componentName =
-    parentCategory &&
-    (style === "categories_and_latest_topics" ||
-      style === "categories_and_latest_topics_created_date")
-      ? "categories_only"
-      : style;
-  return dasherize(componentName);
-}
 
 export default class CategoriesController extends Controller {
   @service router;
@@ -49,15 +12,6 @@ export default class CategoriesController extends Controller {
   @discourseComputed
   isCategoriesRoute() {
     return this.router.currentRouteName === "discovery.categories";
-  }
-
-  @discourseComputed("model.parentCategory")
-  categoryPageStyle(parentCategory) {
-    return categoriesComponent({
-      site: this.site,
-      siteSettings: this.siteSettings,
-      parentCategory,
-    });
   }
 
   @action
