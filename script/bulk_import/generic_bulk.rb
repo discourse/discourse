724840c7ed85b426f488654b4326f4e667d881c4
Gerhard Schlager <gerhard.schlager@discourse.org>
2023-10-28 15:20:57 +0200

    rename of category about topics could fail because of duplicate topic title

--- script/bulk_import/generic_bulk.rb
+++ script/bulk_import/generic_bulk.rb
@@ -153,8 +153,11 @@ class BulkImport::Generic < BulkImport::Base
 
     categories.each do |row|
       if (about_topic_title = row["about_topic_title"]).present?
-        category_id = category_id_from_imported_id(row["id"])
-        Category.find(category_id).topic.update!(title: about_topic_title) if category_id
+        if (category_id = category_id_from_imported_id(row["id"]))
+          topic = Category.find(category_id).topic
+          topic.title = about_topic_title
+          topic.save!(validate: false)
+        end
       end
     end
 
