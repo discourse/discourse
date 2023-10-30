6ae567ab385b3404ae1b8aab260842e38bcb8b17
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-10-30 20:29:55 +0100

    linting

--- plugins/chat/assets/javascripts/discourse/components/chat/thread-list/item.js
+++ plugins/chat/assets/javascripts/discourse/components/chat/thread-list/item.js
@@ -1,7 +1,6 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import { htmlSafe } from "@ember/template";
 
 export default class ChatThreadListItem extends Component {
   @service router;
--- plugins/chat/assets/javascripts/discourse/components/chat/thread/header.js
+++ plugins/chat/assets/javascripts/discourse/components/chat/thread/header.js
@@ -2,7 +2,6 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import { htmlSafe } from "@ember/template";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import { NotificationLevels } from "discourse/lib/notification-levels";
 import ChatModalThreadSettings from "discourse/plugins/chat/discourse/components/chat/modal/thread-settings";
