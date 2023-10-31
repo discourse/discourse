a88711325b767c11ee70e9c3bb366cb8a94e1c17
Mark VanLandingham <markvanlan@gmail.com>
2023-10-31 09:41:57 -0500

    FIX: Actually highlight search items

--- app/assets/javascripts/discourse/app/components/search-menu/highlighted-search.js
+++ app/assets/javascripts/discourse/app/components/search-menu/highlighted-search.js
@@ -1,9 +1,11 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import { inject as service } from "@ember/service";
 import highlightSearch from "discourse/lib/highlight-search";
 
 export default class HighlightedSearch extends Component {
   @service search;
+  @tracked highlighted;
 
   constructor() {
     super(...arguments);
@@ -11,5 +13,6 @@ export default class HighlightedSearch extends Component {
     span.textContent = this.args.string;
 
     highlightSearch(span, this.search.activeGlobalSearchTerm);
+    this.highlighted = span.innerHTML;
   }
 }
