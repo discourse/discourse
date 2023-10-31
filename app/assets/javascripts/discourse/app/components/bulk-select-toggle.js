c6150fbf52f584c437ff93f00cac39a2a92f59f5
David Taylor <david@taylorhq.com>
2023-10-31 11:05:40 +0000

    Make bulkSelectToggle template-only

--- app/assets/javascripts/discourse/app/components/bulk-select-toggle.js
+++
@@ -1,9 +0,0 @@
-import Component from "@glimmer/component";
-import { action } from "@ember/object";
-
-export default class BulkSelectToggle extends Component {
-  @action
-  toggleBulkSelect() {
-    this.args.bulkSelectHelper.toggleBulkSelect();
-  }
-}
