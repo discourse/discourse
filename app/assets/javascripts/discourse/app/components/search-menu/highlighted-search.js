4259c19e799a934cb4fcac423c6229f7af8e107a
Mark VanLandingham <markvanlan@gmail.com>
2023-10-31 10:02:55 -0500

    FIX: Highlighting

--- app/assets/javascripts/discourse/app/components/search-menu/highlighted-search.js
+++ app/assets/javascripts/discourse/app/components/search-menu/highlighted-search.js
@@ -1,18 +1,13 @@
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import highlightSearch from "discourse/lib/highlight-search";
 
 export default class HighlightedSearch extends Component {
   @service search;
-  @tracked highlighted;
 
-  constructor() {
-    super(...arguments);
-    const span = document.createElement("span");
-    span.textContent = this.args.string;
-
-    highlightSearch(span, this.search.activeGlobalSearchTerm);
-    this.highlighted = span.innerHTML;
+  @action
+  highlight(element) {
+    highlightSearch(element, this.search.activeGlobalSearchTerm);
   }
 }
