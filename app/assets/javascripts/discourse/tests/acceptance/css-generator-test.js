6791d4c84c6bcca4dd2844b6dc82d17d0096934a
awesomerobot <kris.aubuchon@discourse.org>
2023-11-01 17:36:58 -0400

    isolate flakey test

--- app/assets/javascripts/discourse/tests/acceptance/css-generator-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/css-generator-test.js
@@ -1,6 +1,6 @@
 import { visit } from "@ember/test-helpers";
 import { test } from "qunit";
-import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 
 acceptance("CSS Generator", function (needs) {
   needs.user();
@@ -46,28 +46,3 @@ acceptance("CSS Generator", function (needs) {
     );
   });
 });
-
-acceptance(
-  "CSS Generator | Anon user in login_required site",
-  function (needs) {
-    needs.site({ categories: null });
-    needs.settings({ login_required: true });
-
-    test("category CSS variables are not generated", async function (assert) {
-      await visit("/");
-
-      const cssTag = document.querySelector(
-        "style#category-color-css-generator"
-      );
-      assert.notOk(exists(cssTag));
-    });
-
-    test("category badge CSS variables are not generated", async function (assert) {
-      await visit("/");
-      const cssTag = document.querySelector(
-        "style#category-badge-css-generator"
-      );
-      assert.notOk(exists(cssTag));
-    });
-  }
-);
