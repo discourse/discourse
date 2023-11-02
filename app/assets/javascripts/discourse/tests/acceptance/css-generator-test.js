cea37f4204dca3d59f9a67de778ac960adfc2be3
awesomerobot <kris.aubuchon@discourse.org>
2023-11-01 20:42:05 -0400

    test generator test

--- app/assets/javascripts/discourse/tests/acceptance/css-generator-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/css-generator-test.js
@@ -5,20 +5,6 @@ import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 acceptance("CSS Generator", function (needs) {
   needs.user();
 
-  needs.site({
-    categories: [
-      { id: 1, color: "ff0000", text_color: "ffffff", name: "category1" },
-      { id: 2, color: "333", text_color: "ffffff", name: "category2" },
-      {
-        id: 4,
-        color: "2B81AF",
-        text_color: "ffffff",
-        parentCategory: { id: 1 },
-        name: "category3",
-      },
-    ],
-  });
-
   test("category CSS variables are generated", async function (assert) {
     await visit("/");
     const cssTag = document.querySelector("style#category-color-css-generator");
@@ -40,9 +26,10 @@ acceptance("CSS Generator", function (needs) {
   test("category badge CSS variables are generated", async function (assert) {
     await visit("/");
     const cssTag = document.querySelector("style#category-badge-css-generator");
+
     assert.ok(
       cssTag.innerHTML.includes(
-        '.badge-category[data-category="1"] { --category-badge-color: var(--category-1-color); --category-badge-text-color: #ffffff; }'
+        '.badge-category[data-category="1001"] { --category-badge-color: var(--category-1001-color); --category-badge-text-color: #ffffff; }'
       )
     );
   });
