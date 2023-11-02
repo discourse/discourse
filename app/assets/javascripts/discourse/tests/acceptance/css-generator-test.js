e995dd9f71baeb314e6aa063a46fb2691c93aec0
awesomerobot <kris.aubuchon@discourse.org>
2023-11-01 21:25:18 -0400

    so that's why that test wasn't working, helper

--- app/assets/javascripts/discourse/tests/acceptance/css-generator-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/css-generator-test.js
@@ -1,9 +1,23 @@
 import { visit } from "@ember/test-helpers";
 import { test } from "qunit";
-import { acceptance } from "discourse/tests/helpers/qunit-helpers";
+import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
 
 acceptance("CSS Generator", function (needs) {
   needs.user();
+
+  needs.site({
+    categories: [
+      { id: 1, color: "ff0000", text_color: "ffffff", name: "category1" },
+      { id: 2, color: "333", text_color: "ffffff", name: "category2" },
+      {
+        id: 4,
+        color: "2B81AF",
+        text_color: "ffffff",
+        parentCategory: { id: 1 },
+        name: "category3",
+      },
+    ],
+  });
 
   test("category CSS variables are generated", async function (assert) {
     await visit("/");
@@ -26,11 +40,33 @@ acceptance("CSS Generator", function (needs) {
   test("category badge CSS variables are generated", async function (assert) {
     await visit("/");
     const cssTag = document.querySelector("style#category-badge-css-generator");
-
-    assert.ok(
-      cssTag.innerHTML.includes(
-        '.badge-category[data-category="1001"] { --category-badge-color: var(--category-1001-color); --category-badge-text-color: #ffffff; }'
-      )
+    assert.equal(
+      cssTag.innerHTML,
+      '.badge-category[data-category="1"] { --category-badge-color: var(--category-1-color); --category-badge-text-color: #ffffff; }\n.badge-category[data-category="2"] { --category-badge-color: var(--category-2-color); --category-badge-text-color: #ffffff; }\n.badge-category[data-category="4"] { --category-badge-color: var(--category-4-color); --category-badge-text-color: #ffffff; }'
     );
   });
 });
+
+acceptance(
+  "CSS Generator | Anon user in login_required site",
+  function (needs) {
+    needs.site({ categories: null });
+    needs.settings({ login_required: true });
+    test("category CSS variables are not generated", async function (assert) {
+      await visit("/");
+
+      const cssTag = document.querySelector(
+        "style#category-color-css-generator"
+      );
+      assert.notOk(exists(cssTag));
+    });
+
+    test("category badge CSS variables are not generated", async function (assert) {
+      await visit("/");
+      const cssTag = document.querySelector(
+        "style#category-badge-css-generator"
+      );
+      assert.notOk(exists(cssTag));
+    });
+  }
+);
