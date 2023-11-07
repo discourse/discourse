2110f14351a4ca529879258ba9bb90a6f69d996e
Godfrey Chan <godfreykfc@gmail.com>
2023-11-07 11:42:56 -0800

    DEV: {{action}} -> @action admin-report.hbs

--- app/assets/javascripts/admin/addon/components/admin-report.js
+++ app/assets/javascripts/admin/addon/components/admin-report.js
@@ -195,7 +195,7 @@ export default class AdminReport extends Component {
     });
   }
 
-  @action
+  @__action__
   onChangeDateRange(range) {
     this.setProperties({
       startDate: range.from,
@@ -203,7 +203,7 @@ export default class AdminReport extends Component {
     });
   }
 
-  @action
+  @__action__
   applyFilter(id, value) {
     let customFilters = this.get("filters.customFilters") || {};
 
