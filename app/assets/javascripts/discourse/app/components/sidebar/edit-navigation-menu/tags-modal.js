a2da569924a5ddf1acaf64e9afcaffe92dd057aa
Godfrey Chan <godfreykfc@gmail.com>
2023-11-07 17:19:12 -0800

    DEV: {{action}} -> @action tags-modal.hbs

--- app/assets/javascripts/discourse/app/components/sidebar/edit-navigation-menu/tags-modal.js
+++ app/assets/javascripts/discourse/app/components/sidebar/edit-navigation-menu/tags-modal.js
@@ -129,7 +129,7 @@ export default class extends Component {
       this.siteSettings.default_navigation_menu_tags.split("|");
   }
 
-  @action
+  @__action__
   toggleTag(tag) {
     if (this.selectedTags.includes(tag)) {
       this.selectedTags.removeObject(tag);
