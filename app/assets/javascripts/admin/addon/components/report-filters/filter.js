467d96279a5513e8fceb0cb61f0b41e8d4e26b4e
Godfrey Chan <godfreykfc@gmail.com>
2023-11-07 17:29:49 -0800

    DEV: {{action}} -> @action list.hbs

--- app/assets/javascripts/admin/addon/components/report-filters/filter.js
+++ app/assets/javascripts/admin/addon/components/report-filters/filter.js
@@ -3,6 +3,7 @@ import { action } from "@ember/object";
 
 export default class Filter extends Component {
   // DELETE THIS COMMENT: referenced by group.js
+  // DELETE THIS COMMENT: referenced by list.js
   @__action__
   onChange(value) {
     this.applyFilter(this.filter.id, value);
--- app/assets/javascripts/admin/addon/components/report-filters/list.js
+++ app/assets/javascripts/admin/addon/components/report-filters/list.js
@@ -1,3 +1,5 @@
 import FilterComponent from "admin/components/report-filters/filter";
 
-export default class List extends FilterComponent {}
+export default class List extends FilterComponent {
+  // DELETE THIS COMMENT: @action onChange inherited from super class
+}
