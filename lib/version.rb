4f64a829e552993a9e6fcc7f77b826f56683551d
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-11-09 14:36:23 +1100

    Bump version to v3.1.3

--- lib/version.rb
+++ lib/version.rb
@@ -7,7 +7,7 @@ module Discourse
   # work around reloader
   unless defined?(::Discourse::VERSION)
     module VERSION #:nodoc:
-      STRING = "3.1.2"
+      STRING = "3.1.3"
 
       PARTS = STRING.split(".")
       private_constant :PARTS
