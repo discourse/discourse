4d117f2db50f1a9cc2fdeff74690cad32a643fc5
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-11-09 21:57:02 +0100

    Use modern syntax for hashes and don't use strings as hash keys

--- plugins/chat/lib/chat/statistics.rb
+++ plugins/chat/lib/chat/statistics.rb
@@ -46,11 +46,11 @@ module Chat
         Chat::Message.joins(:chat_channel).where.not(chat_channel: { type: "DirectMessageChannel" })
 
       {
-        :last_day => query.where("chat_messages.created_at > ?", 1.days.ago).count,
-        "7_days" => query.where("chat_messages.created_at > ?", 7.days.ago).count,
-        "28_days" => query.where("chat_messages.created_at > ?", 28.days.ago).count,
-        "30_days" => query.where("chat_messages.created_at > ?", 30.days.ago).count,
-        :count => query.count,
+        last_day: query.where("chat_messages.created_at > ?", 1.days.ago).count,
+        "7_days": query.where("chat_messages.created_at > ?", 7.days.ago).count,
+        "28_days": query.where("chat_messages.created_at > ?", 28.days.ago).count,
+        "30_days": query.where("chat_messages.created_at > ?", 30.days.ago).count,
+        count: query.count,
       }
     end
 
@@ -59,29 +59,29 @@ module Chat
         Chat::Message.joins(:chat_channel).where(chat_channel: { type: "DirectMessageChannel" })
 
       {
-        :last_day => query.where("chat_messages.created_at > ?", 1.days.ago).count,
-        "7_days" => query.where("chat_messages.created_at > ?", 7.days.ago).count,
-        "28_days" => query.where("chat_messages.created_at > ?", 28.days.ago).count,
-        "30_days" => query.where("chat_messages.created_at > ?", 30.days.ago).count,
-        :count => query.count,
+        last_day: query.where("chat_messages.created_at > ?", 1.days.ago).count,
+        "7_days": query.where("chat_messages.created_at > ?", 7.days.ago).count,
+        "28_days": query.where("chat_messages.created_at > ?", 28.days.ago).count,
+        "30_days": query.where("chat_messages.created_at > ?", 30.days.ago).count,
+        count: query.count,
       }
     end
 
     def self.open_channels_with_threads_enabled
       query = Chat::Channel.where(threading_enabled: true, status: :open)
 
-      { :last_day => 0, "7_days" => 0, "28_days": 0, "30_days" => 0, :count => query.count }
+      { last_day: 0, "7_days": 0, "28_days": 0, "30_days": 0, count: query.count }
     end
 
     def self.threaded_messages
       query = Chat::Message.where.not(thread: nil)
 
       {
-        :last_day => query.where("chat_messages.created_at > ?", 1.days.ago).count,
-        "7_days" => query.where("chat_messages.created_at > ?", 7.days.ago).count,
-        "28_days" => query.where("chat_messages.created_at > ?", 28.days.ago).count,
-        "30_days" => query.where("chat_messages.created_at > ?", 30.days.ago).count,
-        :count => query.count,
+        last_day: query.where("chat_messages.created_at > ?", 1.days.ago).count,
+        "7_days": query.where("chat_messages.created_at > ?", 7.days.ago).count,
+        "28_days": query.where("chat_messages.created_at > ?", 28.days.ago).count,
+        "30_days": query.where("chat_messages.created_at > ?", 30.days.ago).count,
+        count: query.count,
       }
     end
 
