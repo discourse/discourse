378016f87bf7477508c42f9f28e5b071a16e2c1f
Godfrey Chan <godfreykfc@gmail.com>
2023-11-14 13:43:33 -0800

    DEV: funnel *.hbr for discourse-hbr
    Previosuly, rawHandlebarsCompiler get the entire app tree as its
    input. While it is configured to only act on `.hbr` extensions, it
    still have to process and the files, and more importantly, copy or
    symlink all of the app files, including those it didn't touch, into
    its output tree.
    
    This seems to cause a great deal of confusion for Embroider in the
    upcoming release (#24352), which may or may not be a bug, but we
    really shouldn't be doing that to begin with, and it risks other
    issues like the copied version of the file stomping over the same
    file processed by something else.
    
    This commit funnels down the input tree to just *.hbr files before
    passing it into the raw handlebars compiler.

--- app/assets/javascripts/discourse-hbr/index.js
+++ app/assets/javascripts/discourse-hbr/index.js
@@ -1,11 +1,16 @@
 "use strict";
 
+const Funnel = require("broccoli-funnel");
 const rawHandlebarsCompiler = require("./raw-handlebars-compiler");
 
 module.exports = {
   name: require("./package").name,
 
   treeForApp() {
-    return rawHandlebarsCompiler(this.app.trees.app);
+    const hbr = new Funnel(this.app.trees.app, {
+      include: ["**/*.hbr"],
+    });
+
+    return rawHandlebarsCompiler(hbr);
   },
 };
