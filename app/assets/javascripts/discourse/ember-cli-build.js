30b53e5ba2ab70b752731b8fe5e81f783ce8ddc6
David Taylor <david@taylorhq.com>
2023-11-15 09:35:58 +0000

    DEV: Rebuild JS app when files change in discourse-markdown-it
    Based on a workaround shared in https://github.com/embroider-build/embroider/issues/1635#issue-1935759857

--- app/assets/javascripts/discourse/ember-cli-build.js
+++ app/assets/javascripts/discourse/ember-cli-build.js
@@ -14,6 +14,7 @@ const generateWorkboxTree = require("./lib/workbox-tree-builder");
 const { compatBuild } = require("@embroider/compat");
 const { Webpack } = require("@embroider/webpack");
 const { StatsWriterPlugin } = require("webpack-stats-plugin");
+const withSideWatch = require("./lib/with-side-watch");
 
 process.env.BROCCOLI_ENABLED_MEMOIZE = true;
 
@@ -69,6 +70,10 @@ module.exports = function (defaults) {
       // https://github.com/discourse/backburner.js/commits/discourse-patches
       backburner:
         "node_modules/@discourse/backburner.js/dist/named-amd/backburner.js",
+    },
+
+    trees: {
+      app: withSideWatch("app", { watching: ["../discourse-markdown-it"] }),
     },
   });
 
