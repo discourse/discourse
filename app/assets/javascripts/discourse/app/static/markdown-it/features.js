d4c13084635589503f93fee18025d374aac45c9f
Godfrey Chan <godfreykfc@gmail.com>
2023-11-17 10:04:51 -0800

    v2 markdown features extension point

--- app/assets/javascripts/discourse/app/static/markdown-it/features.js
+++ app/assets/javascripts/discourse/app/static/markdown-it/features.js
@@ -1,25 +1,33 @@
+import FeatureModules from "./-virtual-features";
+
 export default function loadPluginFeatures() {
   const features = [];
 
-  for (let moduleName of Object.keys(requirejs.entries)) {
-    if (moduleName.startsWith("discourse/plugins/")) {
+  for (let { name, module } of FeatureModules) {
+    loadFeature(features, name, module);
+  }
+
+  for (let name of Object.keys(requirejs.entries)) {
+    if (name.startsWith("discourse/plugins/")) {
       // all of the modules under discourse-markdown or markdown-it
       // directories are considered additional markdown "features" which
       // may define their own rules
       if (
-        moduleName.includes("/discourse-markdown/") ||
-        moduleName.includes("/markdown-it/")
+        name.includes("/discourse-markdown/") ||
+        name.includes("/markdown-it/")
       ) {
-        const module = requirejs(moduleName);
-
-        if (module && module.setup) {
-          const id = moduleName.split("/").reverse()[0];
-          const { setup, priority = 0 } = module;
-          features.unshift({ id, setup, priority });
-        }
+        loadFeature(features, name, requirejs(name));
       }
     }
   }
 
   return features;
+}
+
+function loadFeature(features, moduleName, module) {
+  if (module && module.setup) {
+    const id = moduleName.split("/").reverse()[0];
+    const { setup, priority = 0 } = module;
+    features.unshift({ id, setup, priority });
+  }
 }
