cde65a64e1d396641e2d12caad37067fd8d4d18b
awesomerobot <kris.aubuchon@discourse.org>
2023-11-20 18:19:13 -0500

    FIX: render page title on tag routes

--- app/assets/javascripts/discourse/app/routes/tag-show.js
+++ app/assets/javascripts/discourse/app/routes/tag-show.js
@@ -170,26 +170,26 @@ export default class TagShowRoute extends DiscourseRoute {
     const filterText = I18n.t(
       `filters.${this.navMode.replace("/", ".")}.title`
     );
-    const controller = this.controllerFor(this.controllerName);
+    const model = this.currentModel;
 
-    if (controller.tag?.id) {
-      if (controller.category) {
+    if (model?.tag?.id) {
+      if (model.category) {
         return I18n.t("tagging.filters.with_category", {
           filter: filterText,
-          tag: controller.tag.id,
-          category: controller.category.name,
+          tag: model.tag.id,
+          category: model.category.name,
         });
       } else {
         return I18n.t("tagging.filters.without_category", {
           filter: filterText,
-          tag: controller.tag.id,
+          tag: model.tag.id,
         });
       }
     } else {
-      if (controller.category) {
+      if (model.category) {
         return I18n.t("tagging.filters.untagged_with_category", {
           filter: filterText,
-          category: controller.category.name,
+          category: model.category.name,
         });
       } else {
         return I18n.t("tagging.filters.untagged_without_category", {
