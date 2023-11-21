a3a221a4baf5423acdfb0b5a81ee4785d1a0f84d
Keegan George <kgeorge13@gmail.com>
2023-11-20 16:30:33 -0800

    DEV: Add updated test/specs

---
+++ app/assets/javascripts/discourse/tests/acceptance/table-builder-test.js
@@ -0,0 +1,43 @@
+import { click, visit } from "@ember/test-helpers";
+import { test } from "qunit";
+import { clearPopupMenuOptionsCallback } from "discourse/controllers/composer";
+import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
+import selectKit from "discourse/tests/helpers/select-kit-helper";
+import I18n from "discourse-i18n";
+
+acceptance("Table Builder", function (needs) {
+  needs.user();
+  needs.hooks.beforeEach(() => clearPopupMenuOptionsCallback());
+
+  test("Can see table builder button when creating a topic", async function (assert) {
+    await visit("/");
+    await click("#create-topic");
+    await click(".d-editor-button-bar .options");
+    await selectKit(".toolbar-popup-menu-options").expand();
+
+    assert
+      .dom(
+        `.select-kit-row[data-name='${I18n.t(
+          "table_builder.composer.button"
+        )}']`
+      )
+      .exists("it shows the builder button");
+  });
+
+  test("Can see table builder button when editing post", async function (assert) {
+    await visit("/t/internationalization-localization/280");
+    await click("#post_1 .show-more-actions");
+    await click("#post_1 .edit");
+    assert.ok(exists("#reply-control"));
+    await click(".d-editor-button-bar .options");
+    await selectKit(".toolbar-popup-menu-options").expand();
+
+    assert
+      .dom(
+        `.select-kit-row[data-name='${I18n.t(
+          "table_builder.composer.button"
+        )}']`
+      )
+      .exists("it shows the builder button");
+  });
+});
---
+++ app/assets/javascripts/discourse/tests/fixtures/md-table.js
@@ -0,0 +1,3 @@
+export const mdTable = `|Make | Model | Year|\r\n|--- | --- | ---|\r\n|Toyota | Supra | 1998|\r\n|Nissan | Skyline | 1999|\r\n|Honda | S2000 | 2001|\r\n`;
+export const mdTableSpecialChars = `|Make | Model | Price|\r\n|--- | --- | ---|\r\n|Toyota | Supra | $50,000|\r\n| | Celica | $20,000|\r\n|Nissan | GTR | $80,000|\r\n`;
+export const mdTableNonUniqueHeadings = `|col1 | col2 | col1|\r\n|--- | --- | ---|\r\n|Col A | Col B | Col C|\r\n`;
