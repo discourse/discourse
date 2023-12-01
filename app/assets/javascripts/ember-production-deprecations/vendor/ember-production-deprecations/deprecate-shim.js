9f9c82ea9718e372136f80960949dd75820572bb
David Taylor <david@taylorhq.com>
2023-12-01 14:50:15 +0000

    DEV: Only add ember-global deprecation for Ember 3 (#24667)
    (otherwise, we're accidentally extending support into Ember 5!)
    
    Followup to 106c1c317f70c80f968038f0c770ac1d9d480927

--- app/assets/javascripts/ember-production-deprecations/vendor/ember-production-deprecations/deprecate-shim.js
+++ app/assets/javascripts/ember-production-deprecations/vendor/ember-production-deprecations/deprecate-shim.js
@@ -35,27 +35,29 @@ define("discourse/lib/deprecate-shim", ["exports"], function (exports) {
     );
 
     // Patch ember-global deprecation
-    Object.defineProperty(window, "Ember", {
-      enumerable: true,
-      configurable: true,
-      get() {
-        require("@ember/debug").deprecate(
-          "Usage of the Ember Global is deprecated. You should import the Ember module or the specific API instead.",
-          false,
-          {
-            id: "ember-global",
-            until: "4.0.0",
-            url: "https://deprecations.emberjs.com/v3.x/#toc_ember-global",
-            for: "ember-source",
-            since: {
-              enabled: "3.27.0",
-            },
-          }
-        );
+    if (window.hasOwnProperty("Ember")) {
+      Object.defineProperty(window, "Ember", {
+        enumerable: true,
+        configurable: true,
+        get() {
+          require("@ember/debug").deprecate(
+            "Usage of the Ember Global is deprecated. You should import the Ember module or the specific API instead.",
+            false,
+            {
+              id: "ember-global",
+              until: "4.0.0",
+              url: "https://deprecations.emberjs.com/v3.x/#toc_ember-global",
+              for: "ember-source",
+              since: {
+                enabled: "3.27.0",
+              },
+            }
+          );
 
-        return require("ember").default;
-      },
-    });
+          return require("ember").default;
+        },
+      });
+    }
   };
 });
 
