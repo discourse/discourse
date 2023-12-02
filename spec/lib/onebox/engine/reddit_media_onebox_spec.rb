76570ab8080cfb9237ed161705868c95fd7200ff
Jarek Radosz <jradosz@gmail.com>
2023-12-02 13:36:05 +0100

    fix reddit spec now that we actually run it

--- spec/lib/onebox/engine/reddit_media_onebox_spec.rb
+++ spec/lib/onebox/engine/reddit_media_onebox_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Onebox::Engine::RedditMediaOnebox do
   let(:html) { described_class.new(link).to_html }
 
   before do
+    stub_request(:head, "#{link}/").to_return(status: 200)
     stub_request(:get, link).to_return(status: 200, body: onebox_response("reddit_image"))
     stub_request(:get, "#{link}/").to_return(status: 200, body: onebox_response("reddit_image"))
   end
