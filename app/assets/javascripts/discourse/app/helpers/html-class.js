2029777b8696850e08454c0518228bc51f505f32
Jarek Radosz <jradosz@gmail.com>
2023-12-04 12:53:07 +0100

    DEV: Replace AdminWrapper with body-class/html-class (#24683)
    AdminWrapper isn't used (directly) in all-the*
    
    Co-authored-by: David Taylor <david@taylorhq.com>

--- app/assets/javascripts/discourse/app/helpers/html-class.js
+++ app/assets/javascripts/discourse/app/helpers/html-class.js
@@ -1,13 +1,13 @@
 import Helper from "@ember/component/helper";
 import { inject as service } from "@ember/service";
 
-export default class BodyClass extends Helper {
+export default class HtmlClass extends Helper {
   @service elementClasses;
 
   compute([...classes]) {
     this.elementClasses.registerClasses(
       this,
-      document.body,
+      document.documentElement,
       classes.flatMap((c) => c?.split(" ")).filter(Boolean)
     );
   }
