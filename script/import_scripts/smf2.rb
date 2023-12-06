fcc9de58f17df61c100a68676a7cdb2f4cd2aca3
Jarek Radosz <jradosz@gmail.com>
2023-12-06 14:00:12 +0100

    a bugfix

--- script/import_scripts/smf2.rb
+++ script/import_scripts/smf2.rb
@@ -368,7 +368,7 @@ class ImportScripts::Smf2 < ImportScripts::Base
     end
     body.gsub!(XListPattern) do |s|
       r = +"\n[ul]"
-      s.lines.each { |l| "#{r}[li]#{l.strip.sub(/^\[x\]\s*/, "")}[/li]" }
+      s.lines.each { |l| r += "[li]#{l.strip.sub(/^\[x\]\s*/, "")}[/li]" }
       "#{r}[/ul]\n"
     end
 
