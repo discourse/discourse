aad00ea891c7d3504b69423e613a7aa1c60c200f
Mark VanLandingham <markvanlan@gmail.com>
2023-12-08 13:49:41 -0600

    remove unused methods

--- spec/system/page_objects/pages/user_notifications.rb
+++ spec/system/page_objects/pages/user_notifications.rb
@@ -27,22 +27,6 @@ module PageObjects
       def has_no_notification?(notification)
         page.has_no_css?(".notification a[href='#{notification.url}']")
       end
-
-      # def click_edit_avatar_button
-      # page.find_button("edit-avatar").click
-      # end
-
-      # def open_avatar_selector_modal(user)
-      # visit(user).click_edit_avatar_button
-      # end
-
-      # def has_custom_uploaded_avatar_image?
-      # has_css?(".pref-avatar img.avatar[src*='user_avatar']")
-      # end
-
-      # def has_system_avatar_image?
-      # has_css?(".pref-avatar img.avatar[src*='letter_avatar']")
-      # end
     end
   end
 end
