b04b793ca773a7b4e105d53a06717cebc9cbc51c
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-12-12 16:56:18 +1100

    DEV: custom generator for rename site setting migration
    Ability to automatically generate migration when site setting name is changed.
    Example usage: `rails generate site_setting_rename_migration site_description contact_email`

---
+++ lib/generators/site_setting_rename_migration/site_setting_rename_migration_generator.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class SiteSettingRenameMigrationGenerator < Rails::Generators::Base
+  argument :old_name, type: :string, banner: "old setting name", required: true
+  argument :new_name, type: :string, banner: "new setting name", required: true
+
+  class ArgumentError < StandardError
+  end
+
+  def create_migration_file
+    timestamp = Time.zone.now.to_s.tr("^0-9", "")[0..13]
+    file_path = "db/migrate/#{timestamp}_rename_#{old_name}_setting.rb"
+    class_name = "Rename#{old_name.classify}Setting"
+
+    validate_setting_name!(old_name)
+    validate_setting_name!(new_name)
+
+    create_file file_path, <<-MIGRATION_FILE
+# frozen_string_literal: true
+
+class #{class_name} < ActiveRecord::Migration[7.0]
+  def up
+    execute "UPDATE site_settings SET name = '#{new_name}' WHERE name = '#{old_name}'"
+  end
+
+  def down
+    execute "UPDATE site_settings SET name = '#{old_name}' WHERE name = '#{new_name}'"
+  end
+end
+    MIGRATION_FILE
+  end
+
+  private
+
+  def validate_setting_name!(name)
+    begin
+      SiteSetting.send(name)
+    rescue NoMethodError
+      say "Site setting with #{name} does not exist"
+      raise ArgumentError
+    end
+  end
+end
