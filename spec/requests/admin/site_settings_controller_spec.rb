discourse/discourse

commit 00209f03e6bc55c6dd776dbe828b8bb53cd935f4

DEV: Add file_size_restriction site setting type (#24704)
This change will allow admins to configure file sizes in mb instead of
having to convert to kb.

Co-authored-by: Ella <ella.estigoy@gmail.com>

app/controllers/admin/site_settings_controller.rb
@@ -34,6 +34,8 @@ class Admin::SiteSettingsController < Admin::AdminController
     case SiteSetting.type_supervisor.get_type(id)
     when :integer
       value = value.tr("^-0-9", "")
+    when :file_size_restriction
+      value = value.tr("^-0-9", "")
     when :uploaded_image_list
       value = Upload.get_from_urls(value.split("|")).to_a
     end

app/assets/javascripts/admin/addon/components/file-size-input.gjs
@@ -1,0 +1,150 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { on } from "@ember/modifier";
+import { action } from "@ember/object";
+import didInsert from "@ember/render-modifiers/modifiers/did-insert";
+import didUpdate from "@ember/render-modifiers/modifiers/did-update";
+import TextField from "discourse/components/text-field";
+import { allowOnlyNumericInput } from "discourse/lib/utilities";
+import I18n from "discourse-i18n";
+import ComboBox from "select-kit/components/combo-box";
+
+const UNIT_KB = "kb";
+const UNIT_MB = "mb";
+const UNIT_GB = "gb";
+
+export default class FileSizeInput extends Component {
+  @tracked fileSizeUnit;
+  @tracked sizeValue;
+  @tracked pendingSizeValue;
+  @tracked pendingFileSizeUnit;
+
+  constructor(owner, args) {
+    super(owner, args);
+    this.originalSizeKB = this.args.sizeValueKB;
+    this.sizeValue = this.args.sizeValueKB;
+
+    this._defaultUnit();
+  }
+
+  _defaultUnit() {
+    this.fileSizeUnit = UNIT_KB;
+    if (this.originalSizeKB <= 1024) {
+      this.onFileSizeUnitChange(UNIT_KB);
+    } else if (
+      this.originalSizeKB > 1024 &&
+      this.originalSizeKB <= 1024 * 1024
+    ) {
+      this.onFileSizeUnitChange(UNIT_MB);
+    } else if (this.originalSizeKB > 1024 * 1024) {
+      this.onFileSizeUnitChange(UNIT_GB);
+    }
+  }
+
+  @action
+  keyDown(event) {
+    allowOnlyNumericInput(event);
+  }
+
+  get dropdownOptions() {
+    return [
+      { label: I18n.t("number.human.storage_units.units.kb"), value: UNIT_KB },
+      { label: I18n.t("number.human.storage_units.units.mb"), value: UNIT_MB },
+      { label: I18n.t("number.human.storage_units.units.gb"), value: UNIT_GB },
+    ];
+  }
+
+  @action
+  handleFileSizeChange(value) {
+    if (value !== "") {
+      this.pendingSizeValue = value;
+      this._onFileSizeChange(value);
+    }
+  }
+
+  _onFileSizeChange(newSize) {
+    let fileSizeKB;
+    switch (this.fileSizeUnit) {
+      case "kb":
+        fileSizeKB = newSize;
+        break;
+      case "mb":
+        fileSizeKB = newSize * 1024;
+        break;
+      case "gb":
+        fileSizeKB = newSize * 1024 * 1024;
+        break;
+    }
+    if (fileSizeKB > this.args.max) {
+      this.args.updateValidationMessage(
+        I18n.t("file_size_input.error.size_too_large", {
+          provided_file_size: I18n.toHumanSize(fileSizeKB * 1024),
+          max_file_size: I18n.toHumanSize(this.args.max * 1024),
+        })
+      );
+      // Removes the green save checkmark button
+      this.args.onChangeSize(this.originalSizeKB);
+    } else {
+      this.args.onChangeSize(fileSizeKB);
+      this.args.updateValidationMessage(null);
+    }
+  }
+
+  @action
+  onFileSizeUnitChange(newUnit) {
+    if (this.fileSizeUnit === "kb" && newUnit === "kb") {
+      this.pendingSizeValue = this.sizeValue;
+    }
+    if (this.fileSizeUnit === "kb" && newUnit === "mb") {
+      this.pendingSizeValue = this.sizeValue / 1024;
+    }
+    if (this.fileSizeUnit === "kb" && newUnit === "gb") {
+      this.pendingSizeValue = this.sizeValue / 1024 / 1024;
+    }
+    if (this.fileSizeUnit === "mb" && newUnit === "kb") {
+      this.pendingSizeValue = this.sizeValue * 1024;
+    }
+    if (this.fileSizeUnit === "mb" && newUnit === "gb") {
+      this.pendingSizeValue = this.sizeValue / 1024;
+    }
+    if (this.fileSizeUnit === "gb" && newUnit === "mb") {
+      this.pendingSizeValue = this.sizeValue * 1024;
+    }
+    if (this.fileSizeUnit === "gb" && newUnit === "kb") {
+      this.pendingSizeValue = this.sizeValue * 1024 * 1024;
+    }
+    this.pendingFileSizeUnit = newUnit;
+  }
+
+  @action
+  applySizeValueChanges() {
+    this.sizeValue = this.pendingSizeValue;
+  }
+
+  @action
+  applyUnitChanges() {
+    this.fileSizeUnit = this.pendingFileSizeUnit;
+  }
+
+  <template>
+    <div class="file-size-picker">
+      <TextField
+        class="file-size-input"
+        @value={{this.sizeValue}}
+        @onChange={{this.handleFileSizeChange}}
+        {{on "keydown" this.keyDown}}
+        {{didInsert this.applySizeValueChanges}}
+        {{didUpdate this.applySizeValueChanges this.pendingSizeValue}}
+      />
+      <ComboBox
+        class="file-size-unit-selector"
+        @valueProperty="value"
+        @content={{this.dropdownOptions}}
+        @value={{this.fileSizeUnit}}
+        @onChange={{this.onFileSizeUnitChange}}
+        {{didInsert this.applyUnitChanges}}
+        {{didUpdate this.applyUnitChanges this.pendingFileSizeUnit}}
+      />
+    </div>
+  </template>
+}

app/assets/javascripts/admin/addon/components/site-settings/file-size-restriction.gjs
@@ -1,0 +1,34 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { fn } from "@ember/helper";
+import { action } from "@ember/object";
+import { htmlSafe } from "@ember/template";
+import FileSizeInput from "admin/components/file-size-input";
+import SettingValidationMessage from "admin/components/setting-validation-message";
+
+export default class FileSizeRestriction extends Component {
+  @tracked _validationMessage = this.args.validationMessage;
+
+  @action
+  updateValidationMessage(message) {
+    this._validationMessage = message;
+  }
+
+  get validationMessage() {
+    return this._validationMessage ?? this.args.validationMessage;
+  }
+
+  <template>
+    <FileSizeInput
+      @sizeValueKB={{@value}}
+      @onChangeSize={{fn (mut @value)}}
+      @updateValidationMessage={{this.updateValidationMessage}}
+      @min={{if @setting.min @setting.min null}}
+      @max={{if @setting.max @setting.max null}}
+      @message={{this.validationMessage}}
+    />
+
+    <SettingValidationMessage @message={{this.validationMessage}} />
+    <div class="desc">{{htmlSafe @setting.description}}</div>
+  </template>
+}

app/assets/javascripts/admin/addon/mixins/setting-component.js
@@ -33,6 +33,7 @@ const CUSTOM_TYPES = [
   "simple_list",
   "emoji_list",
   "named_list",
+  "file_size_restriction",
 ];
 
 const AUTO_REFRESH_ON_SAVE = ["logo", "logo_small", "large_icon"];

app/assets/javascripts/discourse/app/components/number-field.js
@@ -1,36 +1,13 @@
 import TextField from "discourse/components/text-field";
+import { allowOnlyNumericInput } from "discourse/lib/utilities";
 import discourseComputed from "discourse-common/utils/decorators";
 import I18n from "discourse-i18n";
-
-const ALLOWED_KEYS = [
-  "Enter",
-  "Backspace",
-  "Tab",
-  "Delete",
-  "ArrowLeft",
-  "ArrowUp",
-  "ArrowRight",
-  "ArrowDown",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-];
 
 export default TextField.extend({
   classNameBindings: ["invalid"],
 
   keyDown: function (event) {
-    return (
-      ALLOWED_KEYS.includes(event.key) ||
-      (event.key === "-" && this._minNumber && this._minNumber < 0)
-    );
+    allowOnlyNumericInput(event, this._minNumber && this._minNumber < 0);
   },
 
   get _minNumber() {

app/assets/javascripts/discourse/app/lib/utilities.js
@@ -693,3 +693,34 @@ export function tokenRange(tokens, start, end) {
 
   return contents;
 }
+
+export function allowOnlyNumericInput(event, allowNegative = false) {
+  const ALLOWED_KEYS = [
+    "Enter",
+    "Backspace",
+    "Tab",
+    "Delete",
+    "ArrowLeft",
+    "ArrowUp",
+    "ArrowRight",
+    "ArrowDown",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+
+  if (!ALLOWED_KEYS.includes(event.key)) {
+    if (allowNegative && event.key === "-") {
+      return;
+    } else {
+      event.preventDefault();
+    }
+  }
+}

app/assets/javascripts/discourse/tests/integration/components/file-size-input-test.js
@@ -1,0 +1,128 @@
+import { click, fillIn, render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+import { module, test } from "qunit";
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+import selectKit from "discourse/tests/helpers/select-kit-helper";
+
+module("Integration | Component | file-size-input", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("file size unit selector kb", async function (assert) {
+    this.set("value", 1024);
+    this.set("max", 4096);
+    this.set("onChangeSize", () => {});
+    this.set("updateValidationMessage", () => {});
+
+    await render(hbs`
+      <FileSizeInput
+        @sizeValueKB={{readonly this.value}}
+        class="file-size-input-test"
+        @onChangeSize={{this.onChangeSize}}
+        @updateValidationMessage={{this.updateValidationMessage}}
+        @max=4096
+        @message=""
+      />
+    `);
+
+    assert.dom(".file-size-input").hasValue("1024", "value is present");
+  });
+
+  test("file size unit selector", async function (assert) {
+    this.set("value", 4096);
+    this.set("max", 8192);
+    this.set("onChangeSize", () => {});
+    this.set("updateValidationMessage", () => {});
+
+    await render(hbs`
+      <FileSizeInput
+        @sizeValueKB={{readonly this.value}}
+        class="file-size-input-test"
+        @onChangeSize={{this.onChangeSize}}
+        @updateValidationMessage={{this.updateValidationMessage}}
+        @max=4096
+        @message=""
+      />
+    `);
+
+    await click(".file-size-unit-selector");
+
+    await selectKit(".file-size-unit-selector").expand();
+    await selectKit(".file-size-unit-selector").selectRowByValue("kb");
+
+    assert
+      .dom(".file-size-input")
+      .hasValue("4096", "value is change when then unit is changed");
+
+    await click(".file-size-unit-selector");
+
+    await selectKit(".file-size-unit-selector").expand();
+    await selectKit(".file-size-unit-selector").selectRowByValue("mb");
+
+    assert
+      .dom(".file-size-input")
+      .hasValue("4", "value is changed when the unit is changed");
+
+    await click(".file-size-unit-selector");
+
+    await selectKit(".file-size-unit-selector").expand();
+    await selectKit(".file-size-unit-selector").selectRowByValue("gb");
+
+    // TODO: Implement rounding or limit to X digits.
+    assert
+      .dom(".file-size-input")
+      .hasValue(
+        "0.00390625",
+        "value is changed when the unit is changed to gb"
+      );
+
+    await click(".file-size-unit-selector");
+
+    await selectKit(".file-size-unit-selector").expand();
+    await selectKit(".file-size-unit-selector").selectRowByValue("mb");
+
+    assert
+      .dom(".file-size-input")
+      .hasValue(
+        "4",
+        "value is changed backed to original size with no decimal places"
+      );
+  });
+
+  test("file size input error message", async function (assert) {
+    this.set("value", 4096);
+    this.set("max", 8192);
+    this.set("onChangeSize", () => {});
+
+    let updateValidationMessage = (message) => {
+      this.set("message", message);
+    };
+    this.set("updateValidationMessage", updateValidationMessage);
+
+    await render(hbs`
+      <FileSizeInput
+        @sizeValueKB={{readonly this.value}}
+        class="file-size-input-test"
+        @onChangeSize={{this.onChangeSize}}
+        @updateValidationMessage={{this.updateValidationMessage}}
+        @max={{this.max}}
+        @message={{this.message}}
+      />
+    `);
+
+    await fillIn(".file-size-input", 12);
+
+    assert.strictEqual(
+      this.message,
+      "12 MB is greater than the max allowed 8 MB",
+      "A message is showed when the input is greater than the max"
+    );
+
+    await fillIn(".file-size-input", 4);
+
+    assert.strictEqual(
+      this.message,
+      null,
+      "The message is cleared when the input is less than the max"
+    );
+  });
+});

app/assets/stylesheets/common/components/_index.scss
@@ -14,6 +14,7 @@
 @import "date-picker";
 @import "date-time-input-range";
 @import "date-time-input";
+@import "file-size-input";
 @import "footer-nav";
 @import "form-template-field";
 @import "group-member-dropdown";

commit e827db260b16e8a1e3d8be0d37f5356e1847810b

FIX: Ensure file size restriction types are ints
Settings that are using the new `file_size_restriction` types like the
`max_image_size_kb` setting need to have their values saved as integers.
This was a recent regression in 00209f03e6bc55c6dd776dbe828b8bb53cd935f4
that caused these values to be saved as strings.

This change also removes negatives from the validation regex because
file sizes can't be negative anyways.

Bug report: https://meta.discourse.org/t/289037

app/controllers/admin/site_settings_controller.rb
@@ -35,7 +35,7 @@ class Admin::SiteSettingsController < Admin::AdminController
     when :integer
       value = value.tr("^-0-9", "")
     when :file_size_restriction
-      value = value.tr("^-0-9", "")
+      value = value.tr("^0-9", "").to_i
     when :uploaded_image_list
       value = Upload.get_from_urls(value.split("|")).to_a
     end

app/models/topic.rb
@@ -59,7 +59,7 @@ class Topic < ActiveRecord::Base
 
   def thumbnail_info(enqueue_if_missing: false, extra_sizes: [])
     return nil unless original = image_upload
-    return nil if original.filesize >= SiteSetting.max_image_size_kb.kilobytes
+    return nil if original.filesize >= SiteSetting.max_image_size_kb.to_i.kilobytes
     return nil unless original.read_attribute(:width) && original.read_attribute(:height)
 
     infos = []

spec/requests/admin/site_settings_controller_spec.rb
@@ -276,6 +276,13 @@ RSpec.describe Admin::SiteSettingsController do
         expect(SiteSetting.suggested_topics).to eq(1000)
       end
 
+      it "sanitizes file_size_restriction values" do
+        put "/admin/site_settings/max_image_size_kb.json", params: { max_image_size_kb: "4096" }
+
+        expect(response.status).to eq(200)
+        expect(SiteSetting.max_image_size_kb).to eq(4096)
+      end
+
       it "sanitizes negative integer values correctly" do
         put "/admin/site_settings/pending_users_reminder_delay_minutes.json",
             params: {