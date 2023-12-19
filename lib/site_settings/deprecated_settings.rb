4c0f5df9ed4d079514f8c1a01306a7857a318d8d
Martin Brennan <martin@discourse.org>
2023-12-19 15:55:48 +1000

    DEV: Add auto map from TL -> group site settings in DeprecatedSettings
    When setting an old TL based site setting in the console e.g.
    
    SiteSetting.min_trust_level_to_allow_ignore = TrustLevel[3]
    
    We will silently convert this to the corresponding
    Group::AUTO_GROUP. And vice-versa, when we read the value
    on the old setting, we will automatically get the lowest
    trust level corresponding to the lowest auto group for the
    new setting in the database. E.g. if the setting `min_trust_level_to_allow_ignore`
    equals TrustLevel[3] here, and `ignore_allowed_groups` is "13|14", then
    we give this result:
    
    SiteSetting.min_trust_level_to_allow_ignore
    => 3
    
    There are some caveats:
    
    * If the new `ignore_allowed_groups` setting only uses custom groups
      and no auto ones, then `min_trust_level_to_allow_ignore` will be
      nil
    * admin and staff auto groups will only be used as the minimum if
      the old TL setting had the TrustLevelAndStaffSetting enum

--- lib/site_settings/deprecated_settings.rb
+++ lib/site_settings/deprecated_settings.rb
@@ -35,6 +35,64 @@ module SiteSettings::DeprecatedSettings
     ["min_trust_level_to_allow_ignore", "ignore_allowed_groups", false, "3.3"],
   ]
 
+  OVERRIDE_TL_GROUP_SETTINGS = %w[
+    anonymous_posting_min_trust_level
+    shared_drafts_min_trust_level
+    min_trust_level_for_here_mention
+    approve_unless_trust_level
+    approve_new_topics_unless_trust_level
+    email_in_min_trust
+    min_trust_to_edit_wiki_post
+    allow_uploaded_avatars
+    min_trust_to_create_topic
+    min_trust_to_edit_post
+    min_trust_to_flag_posts
+    tl4_delete_posts_and_topics
+    min_trust_level_to_allow_user_card_background
+    min_trust_level_to_allow_invite
+    min_trust_level_to_allow_ignore
+  ]
+
+  def group_to_tl(old_setting, new_setting)
+    tl_and_staff =
+      SiteSetting.type_supervisor.get_type(old_setting.to_sym) == :enum &&
+        SiteSetting
+          .type_supervisor
+          .get_enum_class(old_setting.to_sym)
+          .is_a?(TrustLevelAndStaffSetting)
+
+    min_trust_from_new_group_values =
+      self
+        .public_send("#{new_setting}_map")
+        .select do |group_id|
+          # We only want auto groups, no actual groups because they cannot be
+          # mapped to TLs.
+          group_id.between?(
+            tl_and_staff ? Group::AUTO_GROUPS[:admins] : Group::AUTO_GROUPS[:trust_level_0],
+            Group::AUTO_GROUPS[:trust_level_4],
+          )
+        end
+        .min
+
+    if min_trust_from_new_group_values == Group::AUTO_GROUPS[:admin]
+      tl_and_staff ? "admin" : nil
+    elsif min_trust_from_new_group_values == Group::AUTO_GROUPS[:staff]
+      tl_and_staff ? "staff" : nil
+    else
+      min_trust_from_new_group_values - Group::AUTO_GROUPS[:trust_level_0]
+    end
+  end
+
+  def tl_to_group(old_setting, val)
+    if val == "admin"
+      Group::AUTO_GROUPS[:admin]
+    elsif val == "staff"
+      Group::AUTO_GROUPS[:staff]
+    else
+      "#{val.to_i + Group::AUTO_GROUPS[:trust_level_0]}"
+    end
+  end
+
   def setup_deprecated_methods
     SETTINGS.each do |old_setting, new_setting, override, version|
       unless override
@@ -45,6 +103,16 @@ module SiteSettings::DeprecatedSettings
         )
       end
 
+      if OVERRIDE_TL_GROUP_SETTINGS.include?(old_setting)
+        define_singleton_method "_group_to_tl_#{old_setting}" do |warn: true|
+          group_to_tl(old_setting, new_setting)
+        end
+
+        define_singleton_method "_group_to_tl_#{old_setting}?" do |warn: true|
+          group_to_tl(old_setting, new_setting)
+        end
+      end
+
       define_singleton_method old_setting do |warn: true|
         if warn
           Discourse.deprecate(
@@ -53,7 +121,11 @@ module SiteSettings::DeprecatedSettings
           )
         end
 
-        self.public_send(override ? new_setting : "_#{old_setting}")
+        if OVERRIDE_TL_GROUP_SETTINGS.include?(old_setting)
+          self.public_send("_group_to_tl_#{old_setting}")
+        else
+          self.public_send(override ? new_setting : "_#{old_setting}")
+        end
       end
 
       unless override
@@ -91,7 +163,14 @@ module SiteSettings::DeprecatedSettings
           )
         end
 
-        self.public_send("#{override ? new_setting : "_" + old_setting}=", val)
+        if OVERRIDE_TL_GROUP_SETTINGS.include?(old_setting)
+          # We want to set both the new group setting here to the equivalent of the
+          # TL, as well as setting the TL value in the DB so they remain in sync.
+          self.public_send("_#{old_setting}=", val)
+          self.public_send("#{new_setting}=", tl_to_group(old_setting, val))
+        else
+          self.public_send("#{override ? new_setting : "_" + old_setting}=", val)
+        end
       end
     end
   end
