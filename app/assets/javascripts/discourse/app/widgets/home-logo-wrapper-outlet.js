e370a29a3cd5911798bd595fcd827d84d70e463c
David Battersby <info@davidbattersby.com>
2023-12-22 19:33:10 +0800

    minimize logo when on topic

--- app/assets/javascripts/discourse/app/widgets/home-logo-wrapper-outlet.js
+++ app/assets/javascripts/discourse/app/widgets/home-logo-wrapper-outlet.js
@@ -4,5 +4,5 @@ import { registerWidgetShim } from "discourse/widgets/render-glimmer";
 registerWidgetShim(
   "home-logo-wrapper-outlet",
   "div.home-logo-wrapper-outlet",
-  hbs`<PluginOutlet @name="home-logo-wrapper"><MountWidget @widget="home-logo" @attrs={{@data}} /></PluginOutlet>`
+  hbs`<PluginOutlet @name="home-logo-wrapper"><MountWidget @widget="home-logo" @attrs={{@data}} @args={{hash minimized=@data.topic}} /></PluginOutlet>`
 );
