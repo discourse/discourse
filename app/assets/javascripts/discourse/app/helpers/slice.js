90e0af851d6e4ba67efa5467910036ce06b3026c
Kelvin Tan <kelv@discourse.org>
2023-12-29 13:09:39 +0800

    DEV: remove registration of slice helper for raw templates

--- app/assets/javascripts/discourse/app/helpers/slice.js
+++ app/assets/javascripts/discourse/app/helpers/slice.js
@@ -1,6 +1,3 @@
-import { registerRawHelper } from "discourse-common/lib/helpers";
-
-registerRawHelper("slice", slice);
 export default function slice(...args) {
   let array = args.pop();
   if (array instanceof Function) {
