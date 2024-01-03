4e6d4193ea22819fc4f63806e05fb9806210d9af
Martin Brennan <martin@discourse.org>
2024-01-03 16:55:08 +1000

    DEV: Fix spec for post menu (#25100)
    Followup to b92993fceea107c6f1cbc04eac6008827a61a000
    I ran out of time to get this working for that fix,
    also here I am making the post.url method have parity
    with post.shareUrl in JS, which omits the post number
    for the first post.

--- spec/system/post_menu_spec.rb
+++ spec/system/post_menu_spec.rb
@@ -2,7 +2,8 @@
 
 describe "Post menu", type: :system, js: true do
   fab!(:current_user) { Fabricate(:user) }
-  fab!(:post)
+  fab!(:topic)
+  fab!(:post) { Fabricate(:post, topic: topic) }
 
   let(:topic_page) { PageObjects::Pages::Topic.new }
 
@@ -13,10 +14,12 @@ describe "Post menu", type: :system, js: true do
 
     before { cdp.allow_clipboard }
 
-    xit "copies the absolute link to the post when clicked" do
+    it "copies the absolute link to the post when clicked" do
       topic_page.visit_topic(post.topic)
       topic_page.click_post_action_button(post, :copy_link)
-      expect(cdp.read_clipboard).to eq(post.full_url + "?u=#{current_user.username}")
+      expect(cdp.read_clipboard).to eq(
+        post.full_url(share_url: true) + "?u=#{current_user.username}",
+      )
     end
   end
 end
