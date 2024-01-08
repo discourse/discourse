f5ab404ae259e00ae86c3dc0b133cfbf72201ac2
David Battersby <info@davidbattersby.com>
2024-01-08 20:34:57 +0800

    cleanup

--- plugins/chat/assets/javascripts/discourse/controllers/chat-index.js
+++ plugins/chat/assets/javascripts/discourse/controllers/chat-index.js
@@ -3,9 +3,4 @@ import { inject as service } from "@ember/service";
 
 export default class ChatIndexController extends Controller {
   @service chat;
-  @service siteSettings;
-
-  get directMessagesEnabled() {
-    return this.siteSettings.chat_max_direct_message_users > 0;
-  }
 }
