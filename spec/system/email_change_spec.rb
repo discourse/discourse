3c75c343f7af52c15a0939bf094419aabd31c8c4
David Taylor <david@taylorhq.com>
2024-01-11 16:55:57 +0000

    DEV: Update confirm-email flows to use central 2fa and ember rendering
    These routes were previously rendered using Rails, and had a fairly fragile 2fa implementation in vanilla-js. This commit refactors the routes to be handled in the Ember app, removes the custom vanilla-js bundles, and leans on our centralized 2fa implementation. It also introduces a set of system specs for the behavior.

---
+++ spec/system/email_change_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+describe "Changing email", type: :system do
+  fab!(:password) { "mysupersecurepassword" }
+  fab!(:user) { Fabricate(:user, active: true, password: password) }
+  let(:new_email) { "newemail@example.com" }
+
+  before { Jobs.run_immediately! }
+
+  def generate_confirm_link
+    visit "/my/preferences/account"
+
+    email_dropdown = PageObjects::Components::SelectKit.new(".email-dropdown")
+    expect(email_dropdown.visible?).to eq(true)
+    email_dropdown.select_row_by_value("updateEmail")
+
+    find("#change-email").fill_in with: "newemail@example.com"
+
+    find(".save-button button").click
+
+    wait_for(timeout: 5) { ActionMailer::Base.deliveries.count === 1 }
+
+    if user.admin?
+      get_link_from_email(:old)
+    else
+      get_link_from_email(:new)
+    end
+  end
+
+  def get_link_from_email(type)
+    mail = ActionMailer::Base.deliveries.last
+    expect(mail.to).to contain_exactly(type == :new ? new_email : user.email)
+
+    mail.body.to_s[%r{/u/confirm-#{type}-email/\S+}, 0]
+  end
+
+  it "allows regular user to change their email" do
+    sign_in user
+
+    visit generate_confirm_link
+
+    find(".confirm-new-email .btn-primary").click
+
+    expect(page).to have_css(".dialog-body", text: I18n.t("js.user.change_email.confirm_success"))
+    find(".dialog-footer .btn-primary").click
+
+    expect(user.reload.email).to eq(new_email)
+  end
+
+  it "works when user has totp 2fa" do
+    second_factor = Fabricate(:user_second_factor_totp, user: user)
+    sign_in user
+
+    visit generate_confirm_link
+
+    find(".confirm-new-email .btn-primary").click
+
+    find(".second-factor-token-input").fill_in with: second_factor.totp_object.now
+
+    find("button[type=submit]").click
+
+    try_until_success { expect(current_url).to match("/u/#{user.username}/preferences/account") }
+
+    expect(user.reload.email).to eq(new_email)
+  end
+
+  it "does not require login to verify" do
+    second_factor = Fabricate(:user_second_factor_totp, user: user)
+    sign_in user
+
+    confirm_link = generate_confirm_link
+
+    Capybara.reset_sessions! # log out
+
+    visit confirm_link
+
+    find(".confirm-new-email .btn-primary").click
+
+    find(".second-factor-token-input").fill_in with: second_factor.totp_object.now
+
+    find("button[type=submit]").click
+
+    try_until_success { expect(current_url).to match("/latest") }
+
+    expect(user.reload.email).to eq(new_email)
+  end
+
+  it "makes admins verify old email" do
+    user.update!(admin: true)
+    sign_in user
+
+    confirm_old_link = generate_confirm_link
+
+    # Confirm old email
+    visit confirm_old_link
+    find(".confirm-old-email .btn-primary").click
+    expect(page).to have_css(
+      ".dialog-body",
+      text: I18n.t("js.user.change_email.authorizing_old.confirm_success"),
+    )
+    find(".dialog-footer .btn-primary").click
+
+    # Confirm new email
+    wait_for(timeout: 5) { ActionMailer::Base.deliveries.count === 2 }
+    confirm_new_link = get_link_from_email(:new)
+
+    visit confirm_new_link
+
+    find(".confirm-new-email .btn-primary").click
+
+    expect(page).to have_css(".dialog-body", text: I18n.t("js.user.change_email.confirm_success"))
+    find(".dialog-footer .btn-primary").click
+
+    expect(user.reload.email).to eq(new_email)
+  end
+end
