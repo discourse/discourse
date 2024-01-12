ab63cae5df7479f3bb834cbb5fe005466dd97c37
Nat <natalie.tay@discourse.org>
2024-01-12 11:24:02 +0800

    Cast

--- db/migrate/20240112021335_set_max_clean_orphan_uploads_grace_period_hours.rb
+++ db/migrate/20240112021335_set_max_clean_orphan_uploads_grace_period_hours.rb
@@ -3,7 +3,7 @@
 class SetMaxCleanOrphanUploadsGracePeriodHours < ActiveRecord::Migration[7.0]
   def up
     DB.exec <<~SQL
-        UPDATE site_settings SET value = '168' WHERE name ='clean_orphan_uploads_grace_period_hours' and value > '168';
+        UPDATE site_settings SET value = '168' WHERE name ='clean_orphan_uploads_grace_period_hours' and value::int > 168;
       SQL
   end
 
