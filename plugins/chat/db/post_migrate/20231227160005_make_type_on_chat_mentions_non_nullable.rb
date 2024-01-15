eeefa7f22c94dc3c4d0e2c8d70de5534a5a1e2e4
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-01-15 21:26:26 +0400

    Use change_column_null in post-migrations

--- plugins/chat/db/post_migrate/20231227160005_make_type_on_chat_mentions_non_nullable.rb
+++ plugins/chat/db/post_migrate/20231227160005_make_type_on_chat_mentions_non_nullable.rb
@@ -2,10 +2,10 @@
 
 class MakeTypeOnChatMentionsNonNullable < ActiveRecord::Migration[7.0]
   def up
-    change_column :chat_mentions, :type, :string, null: false
+    change_column_null :chat_mentions, :type, false
   end
 
   def down
-    change_column :chat_mentions, :type, :string, null: true
+    change_column_null :chat_mentions, :type, true
   end
 end
