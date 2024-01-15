8400b4b2321cde9052fb28af51301a8b86c157ee
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-01-15 21:25:45 +0400

    Use change_column_null in migrations

--- plugins/chat/db/migrate/20231227160001_add_type_and_target_id_to_chat_mentions.rb
+++ plugins/chat/db/migrate/20231227160001_add_type_and_target_id_to_chat_mentions.rb
@@ -4,11 +4,11 @@ class AddTypeAndTargetIdToChatMentions < ActiveRecord::Migration[7.0]
   def up
     add_column :chat_mentions, :type, :string, null: true
     add_column :chat_mentions, :target_id, :integer, null: true
-    change_column :chat_mentions, :user_id, :integer, null: true
+    change_column_null :chat_mentions, :user_id, true
   end
 
   def down
-    change_column :chat_mentions, :user_id, :integer, null: false
+    change_column_null :chat_mentions, :user_id, false
     remove_column :chat_mentions, :target_id
     remove_column :chat_mentions, :type
   end
