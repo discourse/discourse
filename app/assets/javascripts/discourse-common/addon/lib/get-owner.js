bc1bd7c59ba0e54781670b4f1212d350e5ec6c75
Jarek Radosz <jradosz@gmail.com>
2024-01-18 11:08:15 +0100

    DEV: Add more details to getOwner deprecation

--- app/assets/javascripts/discourse-common/addon/lib/get-owner.js
+++ app/assets/javascripts/discourse-common/addon/lib/get-owner.js
@@ -18,14 +18,40 @@ export function getOwnerWithFallback(obj) {
   return obj.container;
 }
 
+const GET_OWNER_DEPRECATION_MESSAGE = `
+Importing getOwner from \`discourse-common/lib/get-owner\` is deprecated.
+
+* In API initializers you have the access to \`api.container\`:
+  \`\`\`
+  apiInitializer("1.0", (api) => {
+    const router = api.container.lookup("service:router");
+  \`\`\`
+* In components/controllers/routes you should use service injections:
+  \`\`\`
+  import { inject as service } from "@ember/service";
+  export default class Something extends Component {
+    @service router;
+  \`\`\`
+* In cases where a service can be unavailable (i.e. it comes from an optional plugin)
+  There's \`optionalService\` injection:
+  \`\`\`
+  import optionalService from "discourse/lib/optional-service";
+  export default class Something extends Component {
+    @optionalService categoryBannerPresence;
+  \`\`\`
+* And for a direct replacement, you can use  \`import { getOwner } from '@ember/application'\`,
+  or if you still need the fallback shim (in non-component/controller/route context),
+  use \`import { getOwnerWithFallback } from 'discourse-common/lib/get-owner';\`.
+`.trim();
+
 /**
  * @deprecated use `getOwnerWithFallback` instead
  */
 export function getOwner(obj) {
-  deprecated(
-    "Importing getOwner from `discourse-common/lib/get-owner` is deprecated. Use `import { getOwner } from '@ember/application'`, or if you still need the fallback shim, use `import { getOwnerWithFallback } from 'discourse-common/lib/get-owner';`.",
-    { since: "3.2", id: "discourse.get-owner-with-fallback" }
-  );
+  deprecated(GET_OWNER_DEPRECATION_MESSAGE, {
+    since: "3.2",
+    id: "discourse.get-owner-with-fallback",
+  });
   return getOwnerWithFallback(obj);
 }
 
