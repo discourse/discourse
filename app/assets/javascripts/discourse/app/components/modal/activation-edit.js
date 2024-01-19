a95154ca70c551ee8ee8345a1bef1674cd1e7009
Jarek Radosz <jradosz@gmail.com>
2024-01-19 22:51:57 +0100

    use the correct variable

--- app/assets/javascripts/discourse/app/components/modal/activation-edit.js
+++ app/assets/javascripts/discourse/app/components/modal/activation-edit.js
@@ -23,11 +23,11 @@ export default class ActivationEdit extends Component {
       await changeEmail({
         username: this.login?.loginName,
         password: this.login?.loginPassword,
-        email: this.args.model.newEmail,
+        email: this.newEmail,
       });
 
       this.modal.show(ActivationResent, {
-        model: { currentEmail: this.args.model.newEmail },
+        model: { currentEmail: this.newEmail },
       });
     } catch (e) {
       this.flash = extractError(e);
