09ba55fd21c34b97c9114edee5c363ec9aa4032d
David Battersby <info@davidbattersby.com>
2024-01-23 18:54:35 +0800

    simplify site setting migration

--- plugins/chat/db/migrate/20240118120825_add_threads_enabled_site_setting.rb
+++ plugins/chat/db/migrate/20240118120825_add_threads_enabled_site_setting.rb
@@ -2,22 +2,14 @@
 
 class AddThreadsEnabledSiteSetting < ActiveRecord::Migration[7.0]
   def up
+    SiteSetting.chat_threads_enabled = false
+
     threading_enabled_channels =
-      DB.query_single(
-        "SELECT threading_enabled FROM chat_channels WHERE threading_enabled = 't' LIMIT 1",
-      )
+      DB.query_single("SELECT name FROM chat_channels WHERE threading_enabled = 't'")
 
-    threads_enabled =
-      DB.query_single("SELECT value FROM site_settings WHERE name = 'chat_threads_enabled'").first
+    return unless threading_enabled_channels.present?
 
-    if threads_enabled.nil?
-      enable_threads = threading_enabled_channels.present? ? "t" : "f"
-      DB.exec(
-        "INSERT INTO site_settings(name, value, data_type, created_at, updated_at) VALUES('chat_threads_enabled', '#{enable_threads}', 1, NOW(), NOW())",
-      )
-    elsif threads_enabled == "f" && threading_enabled_channels.present?
-      DB.exec("UPDATE site_settings SET value = 't' WHERE name = 'chat_threads_enabled'")
-    end
+    DB.exec("UPDATE site_settings SET value = 't' WHERE name = 'chat_threads_enabled'")
   end
 
   def down
