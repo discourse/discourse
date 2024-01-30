bc6942a1d6ec7a8d1f91bf97d166662a65c7f639
Jarek Radosz <jradosz@gmail.com>
2024-01-30 13:50:19 +0100

    convert to glimmer

--- app/assets/javascripts/discourse/app/components/modal/reorder-categories.js
+++ app/assets/javascripts/discourse/app/components/modal/reorder-categories.js
@@ -1,5 +1,5 @@
+import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import Component from "@ember/component";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { ajax } from "discourse/lib/ajax";
