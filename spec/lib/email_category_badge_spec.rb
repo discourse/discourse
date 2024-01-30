f3f956b01e8e2b9f68c09a146f63b4f2a694cd15
awesomerobot <kris.aubuchon@discourse.org>
2024-01-30 16:28:10 -0500

    test, lint... again

--- spec/lib/email_category_badge_spec.rb
+++ spec/lib/email_category_badge_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe EmailCategoryBadge do
     c = Fabricate(:category, color: "123456", text_color: "654321")
     html = EmailCategoryBadge.html_for(c)
 
-    expect(html).to have_selector("span[data-category-id]", style: /color: #654321;/)
+    expect(html).to have_css("a[style*='color: #654321;']")
 
     expect(html).to have_selector(
       "span[data-category-id] > span > span",
