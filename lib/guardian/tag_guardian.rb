2da7c74e60b50e7afbee14f79e22ea6a710f4015
Ted Johansson <ted@discourse.org>
2024-02-02 13:48:53 +0800

    DEV: Remove TagGuardian#can_create_tag? fallback (#25535)
    We've changed access settings to be group membership based rather than based on the TL value directly. We kept both conditions here while we updated any plugins and themes. It should now be safe to remove.

--- lib/guardian/tag_guardian.rb
+++ lib/guardian/tag_guardian.rb
@@ -7,11 +7,7 @@ module TagGuardian
   end
 
   def can_create_tag?
-    SiteSetting.tagging_enabled &&
-      (
-        @user.has_trust_level_or_staff?(SiteSetting.min_trust_to_create_tag) ||
-          @user.in_any_groups?(SiteSetting.create_tag_allowed_groups_map)
-      )
+    SiteSetting.tagging_enabled && @user.in_any_groups?(SiteSetting.create_tag_allowed_groups_map)
   end
 
   def can_tag_topics?
