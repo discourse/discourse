discourse/discourse

commit 19b86e7ea26b8c818e13040da0c68e6e312a804c

FIX: Auto-populate group field in invite modal on group route (#25431)

app/assets/javascripts/discourse/app/routes/group-index.js
@@ -35,7 +35,9 @@ export default DiscourseRoute.extend({
   @action
   showInviteModal() {
     const group = this.modelFor("group");
-    this.modal.show(CreateInvite, { model: { groupIds: [group.id] } });
+    this.modal.show(CreateInvite, {
+      model: { invite: { groupIds: [group.id] } },
+    });
   },
 
   @action

app/assets/javascripts/discourse/tests/acceptance/create-invite-modal-test.js
@@ -255,6 +255,11 @@ acceptance(
             automatic: false,
             name: "Macdonald",
           },
+          {
+            id: 47, // must match group-fixtures.js because lookup is by ID
+            automatic: false,
+            name: "Discourse",
+          },
         ]);
       });
     });
@@ -270,6 +275,12 @@ acceptance(
         .hasText("Welcome to Discourse! :wave:");
       assert.dom(".invite-to-groups .formatted-selection").hasText("Macdonald");
       assert.dom("#invite-email").hasValue("cat.com");
+    });
+
+    test("shows correct saved data in group invite form", async function (assert) {
+      await visit("/g/discourse");
+      await click(".group-members-invite");
+      assert.dom(".invite-to-groups .formatted-selection").hasText("Discourse");
     });
   }
 );

commit ac2911abd114c6f436e98d5ae97e3375155ab30b

FIX: Correctly save group invites
regressed in 19b86e7ea26b8c818e13040da0c68e6e312a804c

app/assets/javascripts/discourse/app/components/modal/create-invite.js
@@ -54,7 +54,7 @@ export default Component.extend(bufferedProperty("invite"), {
         moment()
           .add(this.siteSettings.invite_expiry_days, "days")
           .format(FORMAT),
-      groupIds: this.model.invite?.groupIds,
+      groupIds: this.model.groupIds ?? this.model.invite?.groupIds,
       topicId: this.model.invite?.topicId,
       topicTitle: this.model.invite?.topicTitle,
     });

app/assets/javascripts/discourse/app/routes/group-index.js
@@ -36,7 +36,7 @@ export default DiscourseRoute.extend({
   showInviteModal() {
     const group = this.modelFor("group");
     this.modal.show(CreateInvite, {
-      model: { invite: { groupIds: [group.id] } },
+      model: { groupIds: [group.id] },
     });
   },
 

app/assets/javascripts/discourse/tests/acceptance/create-invite-modal-test.js
@@ -262,6 +262,8 @@ acceptance(
           },
         ]);
       });
+
+      server.post("/invites", () => helper.response({}));
     });
 
     test("shows correct saved data in form", async function (assert) {
@@ -281,6 +283,8 @@ acceptance(
       await visit("/g/discourse");
       await click(".group-members-invite");
       assert.dom(".invite-to-groups .formatted-selection").hasText("Discourse");
+
+      await click(".save-invite");
     });
   }
 );