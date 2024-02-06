21a830b53d6053e829a7d52293125bd1e118c973
Jarek Radosz <jradosz@gmail.com>
2024-02-07 00:21:44 +0100

    FIX: Clicking a notification was triggering an error (#25583)

--- app/assets/javascripts/discourse/app/lib/user-menu/base-item.js
+++ app/assets/javascripts/discourse/app/lib/user-menu/base-item.js
@@ -61,11 +61,12 @@ export default class UserMenuBaseItem {
     if (wantsNewWindow(event)) {
       return;
     }
-    closeUserMenu();
-    const href = this.linkHref;
-    if (href) {
-      DiscourseURL.routeTo(href);
-    }
+
     event.preventDefault();
+    closeUserMenu?.();
+
+    if (this.linkHref) {
+      DiscourseURL.routeTo(this.linkHref);
+    }
   }
 }
