d80345fa83a69f6f7d57849edea59a1812f6d86f
Martin Brennan <martin@discourse.org>
2024-02-12 12:32:52 +1000

    DEV: Chat hashtag test (#25638)
    Followup a2a2785f0b85f1ea90f2cea2d358b20500ee224d, moving
    stuff to an existing test.

--- plugins/chat/spec/services/hashtag_autocomplete_service_spec.rb
+++ plugins/chat/spec/services/hashtag_autocomplete_service_spec.rb
@@ -3,24 +3,13 @@
 RSpec.describe HashtagAutocompleteService do
   subject(:service) { described_class.new(guardian) }
 
-  fab!(:channel1) { Fabricate(:chat_channel, name: "Music Lounge", slug: "music") }
-  fab!(:channel2) { Fabricate(:chat_channel, name: "Random", slug: "random") }
-
-  fab!(:admin)
-  let(:guardian) { Guardian.new(admin) }
+  before { SiteSetting.chat_enabled = true }
 
   describe ".enabled_data_sources" do
     it "only returns data sources that are enabled" do
       expect(HashtagAutocompleteService.enabled_data_sources).to include(
         Chat::ChannelHashtagDataSource,
       )
-    end
-  end
-
-  describe "#lookup" do
-    it "returns hashtags for channels" do
-      result = service.lookup(%w[music::channel random::channel], ["channel"])
-      expect(result[:channel].map(&:slug)).to contain_exactly("music", "random")
     end
   end
 end
