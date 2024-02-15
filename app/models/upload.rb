0bf9a3dab69217ef433d70ed659ae3a81940cac0
OsamaSayegh <asooomaasoooma90@gmail.com>
2024-02-15 15:30:36 +0300

    PERF: Pass the `-ping` option to the `identify` ImageMagick command to speed it up

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -281,6 +281,7 @@ class Upload < ActiveRecord::Base
           begin
             Discourse::Utils.execute_command(
               "identify",
+              "-ping",
               "-format",
               "%w %h",
               path,
