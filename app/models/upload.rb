cc9480b24af177108a71a5310b9f876bea128a8a
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2024-02-16 07:39:49 +0800

    PERF: Use `-ping` option to ImageMagick `identify` command (#25713)
    Why this change?
    
    This adds the `-ping` option to the spots we missed in
    cfdb461e9a4e791dfa604cfb14be503c0932481e.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -414,6 +414,7 @@ class Upload < ActiveRecord::Base
       begin
         Discourse::Utils.execute_command(
           "identify",
+          "-ping",
           "-format",
           "%Q",
           local_path,
