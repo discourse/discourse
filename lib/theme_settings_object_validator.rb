9bb8a06f139df7b20e71ebbc3a96d24e2493611f
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2024-02-16 13:15:50 +0800

    DEV: Support validations options for string and numeral types
    Why this change?
    
    This commit updates `ThemeSettingsObjectValidator` to validate a
    property's value against the validations listed in the schema.
    
    For string types, `min_length`, `max_length` and `url` are supported.
    For integer and float types, `min` and `max` are supported.

--- lib/theme_settings_object_validator.rb
+++ lib/theme_settings_object_validator.rb
@@ -30,17 +30,18 @@ class ThemeSettingsObjectValidator
 
   def validate_properties
     @properties.each do |property_name, property_attributes|
-      next if property_attributes[:required] && validate_required_property(property_name)
-      validate_property_type(property_attributes, property_name)
+      next if property_attributes[:required] && !is_property_present?(property_name)
+      next if !has_valid_property_value_type?(property_attributes, property_name)
+      next if !has_valid_property_value?(property_attributes, property_name)
     end
   end
 
-  def validate_property_type(property_attributes, property_name)
+  def has_valid_property_value_type?(property_attributes, property_name)
     value = @object[property_name]
     type = property_attributes[:type]
 
-    return if (value.nil? && type != "enum")
-    return if type == "objects"
+    return true if (value.nil? && type != "enum")
+    return true if type == "objects"
 
     is_value_valid =
       case type
@@ -56,23 +57,97 @@ class ThemeSettingsObjectValidator
         property_attributes[:choices].include?(value)
       else
         add_error(property_name, I18n.t("themes.settings_errors.objects.invalid_type", type:))
-        return
+        return false
       end
 
-    if !is_value_valid
+    if is_value_valid
+      true
+    else
       add_error(
         property_name,
         I18n.t("themes.settings_errors.objects.not_valid_#{type}_value", property_attributes),
       )
+      false
     end
   end
 
-  def validate_required_property(property_name)
+  def has_valid_property_value?(property_attributes, property_name)
+    validations = property_attributes[:validations]
+
+    return true if validations.blank?
+
+    type = property_attributes[:type]
+    value = @object[property_name]
+
+    case type
+    when "string"
+      if validations[:min_length] && value.length < validations[:min_length]
+        add_error(
+          property_name,
+          I18n.t(
+            "themes.settings_errors.objects.string_value_not_valid_min",
+            min: validations[:min_length],
+          ),
+        )
+
+        return false
+      end
+
+      if validations[:max_length] && value.length > validations[:max_length]
+        add_error(
+          property_name,
+          I18n.t(
+            "themes.settings_errors.objects.string_value_not_valid_max",
+            max: validations[:max_length],
+          ),
+        )
+
+        return false
+      end
+
+      if validations[:url] && !value.match?(URI.regexp)
+        add_error(
+          property_name,
+          I18n.t("themes.settings_errors.objects.string_value_not_valid_url"),
+        )
+
+        return false
+      end
+    when "integer", "float"
+      if validations[:min] && value < validations[:min]
+        add_error(
+          property_name,
+          I18n.t(
+            "themes.settings_errors.objects.number_value_not_valid_min",
+            min: validations[:min],
+          ),
+        )
+
+        return false
+      end
+
+      if validations[:max] && value > validations[:max]
+        add_error(
+          property_name,
+          I18n.t(
+            "themes.settings_errors.objects.number_value_not_valid_max",
+            max: validations[:max],
+          ),
+        )
+
+        return false
+      end
+    end
+
+    true
+  end
+
+  def is_property_present?(property_name)
     if @object[property_name].nil?
       add_error(property_name, I18n.t("themes.settings_errors.objects.required"))
-      true
-    else
       false
+    else
+      true
     end
   end
 
