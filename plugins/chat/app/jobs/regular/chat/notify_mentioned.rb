d8b71240cc540ae7800547c571ae4511df28591d
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-02-20 00:57:31 +0400

    ✔️it "includes group mention specific data into desktop notifications"

--- plugins/chat/app/jobs/regular/chat/notify_mentioned.rb
+++ plugins/chat/app/jobs/regular/chat/notify_mentioned.rb
@@ -7,20 +7,27 @@ module Jobs
       # fixme preload chat_channel and other stuff?
       def execute(args)
         @message = ::Chat::Message.find(args[:message_id])
-        parsed_mentions = @message.parsed_mentions
+        @parsed_mentions = @message.parsed_mentions
 
-        parsed_mentions.direct_mentions.each do |user|
+        @parsed_mentions.direct_mentions.each do |user|
+          # fixme call user_mentions here
           mention = @message.chat_mentions.where(target_id: user.id).first
           create_notification!(mention, user)
           ::Chat::DesktopNotifier.notify_mentioned(mention, user)
         end
 
-        parsed_mentions.global_mentions.each do |user|
+        @parsed_mentions.group_mentions.each do |user|
+          mention = choose_group_mention(user)
+          create_notification!(mention, user)
+          ::Chat::DesktopNotifier.notify_mentioned(mention, user)
+        end
+
+        @parsed_mentions.global_mentions.each do |user|
           create_notification!(@message.all_mention, user)
           ::Chat::DesktopNotifier.notify_mentioned(@message.all_mention, user)
         end
 
-        parsed_mentions.here_mentions.each do |user|
+        @parsed_mentions.here_mentions.each do |user|
           create_notification!(@message.here_mention, user)
           ::Chat::DesktopNotifier.notify_mentioned(@message.here_mention, user)
         end
@@ -41,6 +48,20 @@ module Jobs
             data: mention.notification_data.to_json,
           )
         mention.notifications << notification
+      end
+
+      # fixme andrei refactor in a way so we can get rid of this method
+      def choose_group_mention(user)
+        # fixme andrei get rid of nil
+        mention = nil
+        @parsed_mentions.groups_to_mention.each do |group|
+          if user.groups.include?(group)
+            mention = @message.group_mentions.where(target_id: group.id).first
+            break
+          end
+        end
+
+        mention
       end
     end
   end
