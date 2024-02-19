2de51a7ef69b7ce34c44fe3cb083b198b93e7c85
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-02-20 00:37:00 +0400

    TESTS: Rename a test

--- plugins/chat/spec/jobs/regular/chat/process_message_spec.rb
+++ plugins/chat/spec/jobs/regular/chat/process_message_spec.rb
@@ -1236,7 +1236,7 @@ describe Jobs::Chat::ProcessMessage do
           expect(data_hash[:is_group_mention]).to eq(true)
         end
 
-        it "includes here mention specific data to desktop notifications" do
+        it "includes group mention specific data into desktop notifications" do
           desktop_notification = run_job_and_get_first_desktop_notification(message: message)
 
           expect(desktop_notification.data[:translated_title]).to eq(payload_translated_title)
