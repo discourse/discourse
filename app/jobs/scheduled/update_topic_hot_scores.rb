5084b5f7f701230b246cb46fe7d0687bdd9c38d1
Sam Saffron <sam.saffron@gmail.com>
2024-02-23 16:03:50 +1100

    unconditionally fill up hot every 6 hours
    This means that when people eventually enable the hot top menu, there will
    be topics in it

--- app/jobs/scheduled/update_topic_hot_scores.rb
+++ app/jobs/scheduled/update_topic_hot_scores.rb
@@ -4,8 +4,17 @@ module Jobs
   class UpdateTopicHotScores < ::Jobs::Scheduled
     every 10.minutes
 
+    HOT_SCORE_DAILY_KEY = "hot_score_daily"
+
     def execute(args)
-      TopicHotScore.update_scores if SiteSetting.top_menu_map.include? "hot"
+      if SiteSetting.top_menu_map.include?("hot") ||
+           Discourse.redis.set(HOT_SCORE_DAILY_KEY, 1, ex: 6.hour, nx: true)
+        TopicHotScore.update_scores
+      end
+    end
+
+    def self.clear_once_a_day_cache!
+      Discourse.redis.del(HOT_SCORE_DAILY_KEY)
     end
   end
 end
