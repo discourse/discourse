ccd501a6f137a99ab9e845627084bcb43896b859
awesomerobot <kris.aubuchon@discourse.org>
2024-02-28 12:20:15 -0500

    DEV: remove dead export archive action

--- app/assets/javascripts/discourse/app/controllers/user-activity.js
+++ app/assets/javascripts/discourse/app/controllers/user-activity.js
@@ -1,7 +1,6 @@
 import Controller, { inject as controller } from "@ember/controller";
 import { alias } from "@ember/object/computed";
 import { inject as service } from "@ember/service";
-import { exportUserArchive } from "discourse/lib/export-csv";
 import discourseComputed from "discourse-common/utils/decorators";
 import I18n from "discourse-i18n";
 
@@ -24,14 +23,5 @@ export default Controller.extend({
     return count > 0
       ? I18n.t("pending_posts.label_with_count", { count })
       : I18n.t("pending_posts.label");
-  },
-
-  actions: {
-    exportUserArchive() {
-      this.dialog.yesNoConfirm({
-        message: I18n.t("user.download_archive.confirm"),
-        didConfirm: () => exportUserArchive(),
-      });
-    },
   },
 });
