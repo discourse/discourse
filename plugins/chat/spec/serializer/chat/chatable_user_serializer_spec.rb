b40542de5c48c638d038110e3f83652e7d66ecd2
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-03-04 14:02:27 +0100

    FIX: correctly shows as disabled a user who can't chat
    Prior to this fix we were checking if user was not part of a group which allows to chat, but we were not checking if this user was part of groups who can use direct messages.

--- plugins/chat/spec/serializer/chat/chatable_user_serializer_spec.rb
+++ plugins/chat/spec/serializer/chat/chatable_user_serializer_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 RSpec.describe Chat::ChatableUserSerializer do
-  fab!(:user) { Fabricate(:user) }
+  fab!(:user)
   subject(:serializer) { described_class.new(user, scope: Guardian.new(user), root: false) }
 
-  it "serializes a user" do
-    json = serializer.as_json
+  before { SiteSetting.chat_allowed_groups = Group::AUTO_GROUPS[:everyone] }
 
-    expect(json).to eq(
+  it "serializes a user" do
+    expect(serializer.as_json).to eq(
       {
         id: user.id,
         username: user.username,
@@ -18,5 +18,37 @@ RSpec.describe Chat::ChatableUserSerializer do
         has_chat_enabled: false,
       },
     )
+  end
+
+  context "when chat is disabled" do
+    before { SiteSetting.chat_enabled = false }
+
+    it "can't chat" do
+      expect(serializer.as_json[:can_chat]).to eq(false)
+    end
+  end
+
+  context "when user is not allowed to chat" do
+    before { SiteSetting.chat_allowed_groups = Group::AUTO_GROUPS[:trust_level_4] }
+
+    it "can't chat" do
+      expect(serializer.as_json[:can_chat]).to eq(false)
+    end
+  end
+
+  context "when user has chat disabled" do
+    before { user.user_option.update!(chat_enabled: false) }
+
+    it "has chat disabled" do
+      expect(serializer.as_json[:has_chat_enabled]).to eq(false)
+    end
+  end
+
+  context "when user can't use direct messages" do
+    before { SiteSetting.direct_message_enabled_groups = Group::AUTO_GROUPS[:trust_level_4] }
+
+    it "can't chat" do
+      expect(serializer.as_json[:can_chat]).to eq(false)
+    end
   end
 end
