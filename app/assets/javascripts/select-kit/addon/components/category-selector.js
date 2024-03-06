8dbcfef3fd2d7d05554343901fae690bafb3b518
Bianca Nenciu <nbianca@users.noreply.github.com>
2024-03-06 20:14:36 +0200

    FEATURE: Add "+ subcategories" option back (#26035)
    This option was introduced at some point in the past, but was removed
    during the work necessary to make Discourse work with a large number of
    categories.
    
    Follow up to commit 2e68ead45b4cb94fa65c46694716c0ccf25c949d.

--- app/assets/javascripts/select-kit/addon/components/category-selector.js
+++ app/assets/javascripts/select-kit/addon/components/category-selector.js
@@ -1,5 +1,6 @@
-import { computed } from "@ember/object";
+import EmberObject, { computed } from "@ember/object";
 import { mapBy } from "@ember/object/computed";
+import { categoryBadgeHTML } from "discourse/helpers/category-link";
 import Category from "discourse/models/category";
 import { makeArray } from "discourse-common/lib/helpers";
 import CategoryRow from "select-kit/components/category-row";
@@ -51,34 +52,51 @@ export default MultiSelectComponent.extend({
   },
 
   async search(filter) {
-    if (!this.site.lazy_load_categories) {
-      return this._super(filter);
+    let categories;
+    if (this.site.lazy_load_categories) {
+      const rejectCategoryIds = new Set([
+        ...this.categories.map((c) => c.id),
+        ...this.blockedCategories.map((c) => c.id),
+      ]);
+
+      categories = await Category.asyncSearch(filter, {
+        includeUncategorized:
+          this.options?.allowUncategorized !== undefined
+            ? this.options.allowUncategorized
+            : this.selectKit.options.allowUncategorized,
+        rejectCategoryIds: Array.from(rejectCategoryIds),
+      });
+    } else {
+      categories = this._super(filter);
     }
 
-    const rejectCategoryIds = new Set([
-      ...this.categories.map((c) => c.id),
-      ...this.blockedCategories.map((c) => c.id),
-    ]);
+    // If there is a single match and it has subcategories, add a row for
+    // selecting all
+    if (categories.length === 1) {
+      const descendants = categories[0].descendants;
+      if (descendants.length > 1) {
+        categories.push(
+          EmberObject.create({
+            label: categoryBadgeHTML(descendants[0], {
+              link: false,
+              recursive: true,
+              subcategoryCount: descendants.length - 1,
+            }),
+            categories: [...descendants],
+          })
+        );
+      }
+    }
 
-    return await Category.asyncSearch(filter, {
-      includeUncategorized:
-        this.options?.allowUncategorized !== undefined
-          ? this.options.allowUncategorized
-          : this.selectKit.options.allowUncategorized,
-      rejectCategoryIds: Array.from(rejectCategoryIds),
-    });
+    return categories;
   },
 
   select(value, item) {
-    if (item.multiCategory) {
-      const items = item.multiCategory.map((id) =>
-        Category.findById(parseInt(id, 10))
+    if (item.categories) {
+      this.selectKit.change(
+        makeArray(this.value).concat(item.categories.mapBy("id")),
+        makeArray(this.selectedContent).concat(item.categories)
       );
-
-      const newValues = makeArray(this.value).concat(items.map((i) => i.id));
-      const newContent = makeArray(this.selectedContent).concat(items);
-
-      this.selectKit.change(newValues, newContent);
     } else {
       this._super(value, item);
     }
