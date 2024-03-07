4bf59eb54cdf1f6fa92118512cdb62a3f612ca2c
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-03-07 23:57:43 +0100

    FIX: improves linking of thread messages
    - The thread preview is now a regular link and can be right clicked
    - left gutter date, and regular date of a thread message will not correctly link to the thread's message

--- plugins/chat/test/javascripts/unit/helpers/format-chat-date-test.js
+++ plugins/chat/test/javascripts/unit/helpers/format-chat-date-test.js
@@ -19,4 +19,19 @@ module("Discourse Chat | Unit | Helpers | format-chat-date", function (hooks) {
       `/chat/c/-/${channel.id}/${this.message.id}`
     );
   });
+
+  test("link to chat message thread", async function (assert) {
+    const channel = fabricators.channel();
+    const thread = fabricators.thread();
+    this.message = fabricators.message({ channel, thread });
+
+    await render(
+      hbs`{{format-chat-date this.message (hash threadContext=true)}}`
+    );
+
+    assert.equal(
+      query(".chat-time").getAttribute("href"),
+      `/chat/c/-/${channel.id}/t/${thread.id}/${this.message.id}`
+    );
+  });
 });
