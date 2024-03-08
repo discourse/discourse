discourse/discourse

commit 5c1147adf3339c89a3ccb69839b4a4c5fd75a2ab

UX: Move top dismiss button from `topics` to `d-navigation` (#26032)

app/assets/javascripts/discourse/app/controllers/discovery/list.js
@@ -2,10 +2,12 @@ import { tracked } from "@glimmer/tracking";
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import DismissNew from "discourse/components/modal/dismiss-new";
 import BulkSelectHelper from "discourse/lib/bulk-select-helper";
 import { filterTypeForMode } from "discourse/lib/filter-mode";
 import { disableImplicitInjections } from "discourse/lib/implicit-injections";
 import { defineTrackedProperty } from "discourse/lib/tracked-tools";
+import Topic from "discourse/models/topic";
 
 // Just add query params here to have them automatically passed to topic list filters.
 export const queryParams = {
@@ -49,6 +51,9 @@ export default class DiscoveryListController extends Controller {
   @service siteSettings;
   @service site;
   @service currentUser;
+  @service router;
+  @service topicTrackingState;
+  @service modal;
 
   @tracked model;
 
@@ -111,6 +116,52 @@ export default class DiscoveryListController extends Controller {
     return this.order ?? this.model.list.get("params.order") ?? "activity";
   }
 
+  async callResetNew(
+    dismissPosts = false,
+    dismissTopics = false,
+    untrack = false
+  ) {
+    const isTracked =
+      (this.router.currentRoute.queryParams["f"] ||
+        this.router.currentRoute.queryParams["filter"]) === "tracked";
+
+    let topicIds = this.bulkSelectHelper.selected.map((topic) => topic.id);
+    const result = await Topic.resetNew(
+      this.model.category,
+      !this.model.noSubcategories,
+      {
+        tracked: isTracked,
+        tag: this.model.tag,
+        topicIds,
+        dismissPosts,
+        dismissTopics,
+        untrack,
+      }
+    );
+
+    if (result.topic_ids) {
+      this.topicTrackingState.removeTopics(result.topic_ids);
+    }
+    this.router.refresh();
+  }
+
+  @action
+  resetNew() {
+    if (!this.currentUser.new_new_view_enabled) {
+      return this.callResetNew();
+    }
+
+    this.modal.show(DismissNew, {
+      model: {
+        selectedTopics: this.bulkSelectHelper.selected,
+        subset: this.model.listParams?.subset,
+        dismissCallback: ({ dismissPosts, dismissTopics, untrack }) => {
+          this.callResetNew(dismissPosts, dismissTopics, untrack);
+        },
+      },
+    });
+  }
+
   @action
   createTopic() {
     this.composer.openNewTopic({
@@ -147,5 +198,10 @@ export default class DiscoveryListController extends Controller {
   @action
   toggleTagInfo() {
     this.toggleProperty("showTagInfo");
+  }
+
+  @action
+  dismissRead(operationType, options) {
+    this.bulkSelectHelper.dismissRead(operationType, options);
   }
 }

spec/system/page_objects/components/topic_list_controls.rb
@@ -26,14 +26,14 @@ module PageObjects
       end
 
       def dismiss_unread(untrack: false)
-        click_button("dismiss-topics-bottom")
+        click_button("dismiss-topics-top")
         find(".dismiss-read-modal__stop-tracking").click if untrack
         click_button("dismiss-read-confirm")
         self
       end
 
       def dismiss_new
-        click_button("dismiss-new-bottom")
+        click_button("dismiss-new-top")
         self
       end
     end

app/assets/javascripts/discourse/app/components/d-navigation.hbs
@@ -21,6 +21,16 @@
     <BulkSelectToggle @bulkSelectHelper={{@bulkSelectHelper}} />
   {{/if}}
 
+  <TopicDismissButtons
+    @position="top"
+    @selectedTopics={{@bulkSelectHelper.selected}}
+    @model={{@model}}
+    @showResetNew={{@showResetNew}}
+    @showDismissRead={{@showDismissRead}}
+    @resetNew={{@resetNew}}
+    @dismissRead={{@dismissRead}}
+  />
+
   {{#if this.showCategoryAdmin}}
     {{#if this.fixedCategoryPositions}}
       <CategoriesAdminDropdown

app/assets/javascripts/discourse/app/components/discovery/navigation.hbs
@@ -55,6 +55,11 @@
     @skipCategoriesNavItem={{this.skipCategoriesNavItem}}
     @toggleInfo={{@toggleTagInfo}}
     @tagNotification={{@tagNotification}}
+    @model={{@model}}
+    @showDismissRead={{@showDismissRead}}
+    @showResetNew={{@showResetNew}}
+    @dismissRead={{@dismissRead}}
+    @resetNew={{@resetNew}}
   />
 
   {{#if @category}}

app/assets/javascripts/discourse/app/components/discovery/topics.hbs
@@ -6,16 +6,6 @@
   <div class="alert alert-info">{{this.redirectedReason}}</div>
 {{/if}}
 
-<TopicDismissButtons
-  @position="top"
-  @selectedTopics={{@bulkSelectHelper.selected}}
-  @model={{@model}}
-  @showResetNew={{@showResetNew}}
-  @showDismissRead={{@showDismissRead}}
-  @resetNew={{this.resetNew}}
-  @dismissRead={{this.dismissRead}}
-/>
-
 {{#if @model.sharedDrafts}}
   <TopicList
     @listTitle="shared_drafts.title"
@@ -137,8 +127,8 @@
         @model={{@model}}
         @showResetNew={{@showResetNew}}
         @showDismissRead={{@showDismissRead}}
-        @resetNew={{this.resetNew}}
-        @dismissRead={{this.dismissRead}}
+        @resetNew={{@resetNew}}
+        @dismissRead={{@dismissRead}}
       />
 
       <FooterMessage

app/assets/javascripts/discourse/app/components/discovery/topics.js
@@ -2,11 +2,9 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import DismissNew from "discourse/components/modal/dismiss-new";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import { filterTypeForMode } from "discourse/lib/filter-mode";
 import { userPath } from "discourse/lib/url";
-import Topic from "discourse/models/topic";
 import I18n from "discourse-i18n";
 
 export default class DiscoveryTopics extends Component {
@@ -53,52 +51,6 @@ export default class DiscoveryTopics extends Component {
 
   get new() {
     return filterTypeForMode(this.args.model.filter) === "new";
-  }
-
-  async callResetNew(
-    dismissPosts = false,
-    dismissTopics = false,
-    untrack = false
-  ) {
-    const isTracked =
-      (this.router.currentRoute.queryParams["f"] ||
-        this.router.currentRoute.queryParams["filter"]) === "tracked";
-
-    let topicIds = this.args.bulkSelectHelper.selected.map((topic) => topic.id);
-    const result = await Topic.resetNew(
-      this.args.category,
-      !this.args.noSubcategories,
-      {
-        tracked: isTracked,
-        tag: this.args.tag,
-        topicIds,
-        dismissPosts,
-        dismissTopics,
-        untrack,
-      }
-    );
-
-    if (result.topic_ids) {
-      this.topicTrackingState.removeTopics(result.topic_ids);
-    }
-    this.router.refresh();
-  }
-
-  @action
-  resetNew() {
-    if (!this.currentUser.new_new_view_enabled) {
-      return this.callResetNew();
-    }
-
-    this.modal.show(DismissNew, {
-      model: {
-        selectedTopics: this.args.bulkSelectHelper.selected,
-        subset: this.args.model.listParams?.subset,
-        dismissCallback: ({ dismissPosts, dismissTopics, untrack }) => {
-          this.callResetNew(dismissPosts, dismissTopics, untrack);
-        },
-      },
-    });
   }
 
   // Show newly inserted topics
@@ -226,15 +178,5 @@ export default class DiscoveryTopics extends Component {
 
   get expandAllPinned() {
     return this.args.tag || this.args.category;
-  }
-
-  @action
-  dismissRead(dismissTopics) {
-    const operationType = dismissTopics ? "topics" : "posts";
-    this.args.bulkSelectHelper.dismissRead(operationType, {
-      categoryId: this.args.category?.id,
-      tagName: this.args.tag?.id,
-      includeSubcategories: this.args.noSubcategories,
-    });
   }
 }

app/assets/javascripts/discourse/app/components/topic-dismiss-buttons.hbs
@@ -1,6 +1,6 @@
-{{#if this.showBasedOnPosition}}
+{{~#if this.showBasedOnPosition}}
   <div class="row {{this.containerClass}}">
-    {{#if this.showDismissRead}}
+    {{~#if this.showDismissRead}}
       <DButton
         @action={{this.dismissReadPosts}}
         @translatedLabel={{this.dismissLabel}}
@@ -8,8 +8,8 @@
         id={{this.dismissReadId}}
         class="btn-default dismiss-read"
       />
-    {{/if}}
-    {{#if this.showResetNew}}
+    {{/if~}}
+    {{~#if this.showResetNew}}
       <DButton
         @action={{this.resetNew}}
         @icon="check"
@@ -17,6 +17,6 @@
         id={{this.dismissNewId}}
         class="btn-default dismiss-read"
       />
-    {{/if}}
+    {{/if~}}
   </div>
-{{/if}}
+{{/if~}}

app/assets/javascripts/discourse/app/components/topic-dismiss-buttons.js
@@ -34,10 +34,9 @@ export default Component.extend({
   @discourseComputed("position", "model.topics.length")
   showBasedOnPosition(position, topicCount) {
     if (position !== "top") {
-      return true;
+      return topicCount > 5;
     }
-
-    return this.currentUser?.new_new_view_enabled || topicCount > 5;
+    return true;
   },
 
   @discourseComputed("selectedTopics.length")

commit 7a1cd35997b1836df173e866cce387719575f34c

FIX: Dismissing unread not dismissing correctly
Why this change?

This is a regression from introduced in
5c1147adf3339c89a3ccb69839b4a4c5fd75a2ab where dismissing unread topics
was changing the notification level of the topics instead of just
dismissing the unread posts.

What does this change do?

1. Bring back the previous implementation of the action
2. Fix the system test that was supposed to catch the problem but did
   not.

app/assets/javascripts/discourse/app/controllers/discovery/list.js
@@ -201,7 +201,13 @@ export default class DiscoveryListController extends Controller {
   }
 
   @action
-  dismissRead(operationType, options) {
-    this.bulkSelectHelper.dismissRead(operationType, options);
+  dismissRead(dismissTopics) {
+    const operationType = dismissTopics ? "topics" : "posts";
+
+    this.bulkSelectHelper.dismissRead(operationType, {
+      categoryId: this.model.category?.id,
+      tagName: this.model.tag?.id,
+      includeSubcategories: this.model.noSubcategories,
+    });
   }
 }

spec/system/page_objects/components/topic_list_controls.rb
@@ -11,7 +11,7 @@ module PageObjects
             I18n.t("js.filters.new.title_with_count", count: count)
           end
 
-        has_css?(".nav-item_new", text: text)
+        has_css?(".nav-item_new", exact_text: text)
       end
 
       def has_unread?(count:)
@@ -22,7 +22,7 @@ module PageObjects
             I18n.t("js.filters.unread.title_with_count", count: count)
           end
 
-        has_css?(".nav-item_unread", text: text)
+        has_css?(".nav-item_unread", exact_text: text)
       end
 
       def dismiss_unread(untrack: false)