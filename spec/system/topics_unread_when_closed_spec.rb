5860103d03a3ea39077f638b6944d23d7da95a42
Blake Erickson <o.blakeerickson@gmail.com>
2024-03-08 14:48:16 -0700

    clean up system tests

--- spec/system/topics_unread_when_closed_spec.rb
+++ spec/system/topics_unread_when_closed_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 describe "Topics unread when closed", type: :system do
-  before { SiteSetting.experimental_topic_bulk_actions_enabled_groups = "1" }
   fab!(:topics) { Fabricate.times(10, :post).map(&:topic) }
   let(:topic_list) { PageObjects::Components::TopicList.new }
   let(:topic_page) { PageObjects::Pages::Topic.new }
@@ -14,17 +13,14 @@ describe "Topics unread when closed", type: :system do
       user.user_option.update!(topics_unread_when_closed: false)
       sign_in(user)
       topic = topics.third
-      visit("/t/#{topic.slug}/#{topic.id}")
+      topic_page.visit_topic(topic)
       topic_page.watch_topic
       expect(topic_page).to have_read_post(1)
 
       # Close the topic as an admin
-      sign_in(admin)
-      visit("/t/#{topic.slug}/#{topic.id}")
-      topic_page.close_topic
+      TopicStatusUpdater.new(topic, admin).update!("closed", true)
 
       # Check that the user did not receive a new post notification badge
-      sign_in(user)
       visit("/latest")
       expect(topic_list).to have_no_unread_badge(topics.third)
     end
@@ -33,17 +29,14 @@ describe "Topics unread when closed", type: :system do
       user.user_option.update!(topics_unread_when_closed: true)
       sign_in(user)
       topic = topics.third
-      visit("/t/#{topic.slug}/#{topic.id}")
+      topic_page.visit_topic(topic)
       topic_page.watch_topic
       expect(topic_page).to have_read_post(1)
 
       # Close the topic as an admin
-      sign_in(admin)
-      visit("/t/#{topic.slug}/#{topic.id}")
-      topic_page.close_topic
+      TopicStatusUpdater.new(topic, admin).update!("closed", true)
 
       # Check that the user did receive a new post notification badge
-      sign_in(user)
       visit("/latest")
       expect(topic_list).to have_unread_badge(topics.third)
     end
