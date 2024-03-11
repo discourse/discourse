4b8c537a83e3e6f5d2bdfec0abd80ce55eb49a2f
Ted Johansson <ted@discourse.org>
2024-03-11 10:57:02 +0800

    DEV: Add new AdminNotice model

---
+++ app/models/admin_notice.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class AdminNotice < ActiveRecord::Base
+  validates :identifier, presence: true
+
+  enum :priority, %i[low high].freeze
+  enum :category, %i[problem].freeze
+
+  def message
+    I18n.t("dashboard.admin_notice.#{identifier}", **details.symbolize_keys)
+  end
+end
+
+# == Schema Information
+#
+# Table name: admin_notices
+#
+#  id         :bigint           not null, primary key
+#  category   :integer          not null
+#  priority   :integer          not null
+#  identifier :string           not null
+#  details    :json             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_admin_notices_on_category    (category)
+#  index_admin_notices_on_identifier  (identifier)
+#
---
+++ spec/models/admin_notice_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.describe AdminNotice do
+  it { is_expected.to validate_presence_of(:identifier) }
+
+  describe "#message" do
+    let(:notice) do
+      Fabricate(
+        :admin_notice,
+        identifier: "test",
+        category: "problem",
+        priority: "high",
+        details: {
+          thing: "world",
+        },
+      )
+    end
+
+    before do
+      I18n.backend.store_translations(
+        :en,
+        {
+          "dashboard" => {
+            "admin_notice" => {
+              "dashboard.admin_notice.test" => "Something is wrong with the %{thing}",
+            },
+          },
+        },
+      )
+    end
+
+    it { expect(notice.message).to eq("Something is wrong with the world") }
+  end
+end
