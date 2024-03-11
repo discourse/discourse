b6819fd8c10badca3de4ea62dae27c8e01fb4677
Jarek Radosz <jradosz@gmail.com>
2024-03-11 11:55:32 +0100

    DEV: Convert IframedHtml to gjs (#24836)
    * DEV: Convert IframedHtml to gjs
    * disable lint
    * didUpdate
    * no className

--- app/assets/javascripts/discourse/tests/integration/components/iframed-html-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/iframed-html-test.js
@@ -2,7 +2,6 @@ import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { module, test } from "qunit";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
-import { queryAll } from "discourse/tests/helpers/qunit-helpers";
 
 module("Integration | Component | iframed-html", function (hooks) {
   setupRenderingTest(hooks);
@@ -12,16 +11,13 @@ module("Integration | Component | iframed-html", function (hooks) {
       hbs`<IframedHtml @html="<h1 id='find-me'>hello</h1>" class="this-is-an-iframe" />`
     );
 
-    const iframe = queryAll("iframe.this-is-an-iframe");
-    assert.strictEqual(iframe.length, 1, "inserts an iframe");
+    assert
+      .dom("iframe.this-is-an-iframe")
+      .exists({ count: 1 }, "inserts an iframe");
 
-    assert.ok(
-      iframe[0].classList.contains("this-is-an-iframe"),
-      "Adds className to the iframes classList"
-    );
-
+    const iframe = document.querySelector(".iframed-html");
     assert.strictEqual(
-      iframe[0].contentWindow.document.body.querySelectorAll("#find-me").length,
+      iframe.contentWindow.document.body.querySelectorAll("#find-me").length,
       1,
       "inserts the passed in html into the iframe"
     );
