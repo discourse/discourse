fe624ce0a8ab6b72d1e6d0b142a41ab6b4f63213
Jan Cernik <jancernik12@gmail.com>
2024-03-13 18:51:58 -0300

    DEV: Convert time-gap widget to a component

--- app/assets/javascripts/discourse/app/widgets/time-gap.js
+++ app/assets/javascripts/discourse/app/widgets/time-gap.js
@@ -1,28 +1,20 @@
-import { h } from "virtual-dom";
+import { hbs } from "ember-cli-htmlbars";
+import RenderGlimmer from "discourse/widgets/render-glimmer";
 import { createWidget } from "discourse/widgets/widget";
-import I18n from "discourse-i18n";
 
-function description(attrs) {
-  const daysSince = attrs.daysSince;
-
-  if (daysSince < 30) {
-    return I18n.t("dates.later.x_days", { count: daysSince });
-  } else if (daysSince < 365) {
-    const gapMonths = Math.round(daysSince / 30);
-    return I18n.t("dates.later.x_months", { count: gapMonths });
-  } else {
-    const gapYears = Math.round(daysSince / 365);
-    return I18n.t("dates.later.x_years", { count: gapYears });
-  }
-}
-
-export default createWidget("time-gap", {
+createWidget("time-gap", {
   tagName: "div.time-gap.small-action",
 
   html(attrs) {
     return [
-      h("div.topic-avatar", ""),
-      h("div.small-action-desc.timegap", description(attrs)),
+      new RenderGlimmer(
+        this,
+        "div.time-gap-widget",
+        hbs`<TimeGap @daysSince={{@data.attrs.daysSince}} />`,
+        {
+          attrs,
+        }
+      ),
     ];
   },
 });
