5e8c5ea8e7a2614f7903eafd7d49094cc75b96f5
Nat <natalie.tay@discourse.org>
2024-03-25 15:48:52 +0800

    DEV: Check if bookmarks exist before running job

--- app/jobs/regular/sync_topic_user_bookmarked.rb
+++ app/jobs/regular/sync_topic_user_bookmarked.rb
@@ -5,35 +5,37 @@ module Jobs
     def execute(args = {})
       raise Discourse::InvalidParameters.new(:topic_id) unless args[:topic_id].present?
 
-      DB.exec(<<~SQL, topic_id: args[:topic_id])
-        SELECT bookmarks.user_id, COUNT(*)
-        INTO TEMP TABLE tmp_sync_topic_user_bookmarks
+      bookmarks_exist = DB.exec(<<~SQL, topic_id: args[:topic_id]) > 0
+        SELECT 1
         FROM bookmarks
         LEFT JOIN posts ON posts.id = bookmarks.bookmarkable_id AND bookmarks.bookmarkable_type = 'Post'
-        LEFT JOIN topics ON (topics.id = bookmarks.bookmarkable_id AND bookmarks.bookmarkable_type = 'Topic') OR
-         (topics.id = posts.topic_id)
+        LEFT JOIN topics ON topics.id = bookmarks.bookmarkable_id AND bookmarks.bookmarkable_type = 'Topic'
         WHERE (topics.id = :topic_id OR posts.topic_id = :topic_id)
-        AND posts.deleted_at IS NULL AND topics.deleted_at IS NULL
-        GROUP BY bookmarks.user_id;
+        AND posts.deleted_at IS NULL
+        AND topics.deleted_at IS NULL
+        LIMIT 1
+      SQL
 
+      return unless bookmarks_exist
+
+      DB.exec(<<~SQL, topic_id: args[:topic_id])
         UPDATE topic_users
-        SET bookmarked = true
-        FROM tmp_sync_topic_user_bookmarks
-        WHERE topic_users.user_id = tmp_sync_topic_user_bookmarks.user_id AND
-          topic_users.topic_id = :topic_id AND
-          tmp_sync_topic_user_bookmarks.count > 0;
-
-        UPDATE topic_users
-        SET bookmarked = false
-        FROM tmp_sync_topic_user_bookmarks
-        WHERE topic_users.topic_id = :topic_id AND
-          topic_users.bookmarked = true AND
-          topic_users.user_id NOT IN (
-            SELECT tmp_sync_topic_user_bookmarks.user_id
-            FROM tmp_sync_topic_user_bookmarks
-          );
-
-        DROP TABLE tmp_sync_topic_user_bookmarks;
+        SET bookmarked = CASE
+          WHEN EXISTS (
+            SELECT 1
+            FROM bookmarks
+            JOIN posts ON posts.id = bookmarks.bookmarkable_id AND bookmarks.bookmarkable_type = 'Post'
+            WHERE posts.topic_id = topic_users.topic_id AND bookmarks.user_id = topic_users.user_id
+            AND posts.deleted_at IS NULL
+          ) OR EXISTS (
+            SELECT 1
+            FROM bookmarks
+            WHERE bookmarks.bookmarkable_id = topic_users.topic_id AND bookmarks.bookmarkable_type = 'Topic'
+            AND bookmarks.user_id = topic_users.user_id
+          ) THEN true
+          ELSE false
+        END
+        WHERE topic_users.topic_id = :topic_id;
       SQL
     end
   end
