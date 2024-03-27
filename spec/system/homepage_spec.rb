be65615e1b65d2f156c8ccfbc9fba2cf739ba64f
Penar Musaraj <pmusaraj@gmail.com>
2024-03-26 20:04:05 -0400

    A few more tests

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -6,20 +6,6 @@ describe "Homepage", type: :system do
   fab!(:topics) { Fabricate.times(5, :post).map(&:topic) }
   let(:discovery) { PageObjects::Pages::Discovery.new }
   let!(:theme) { Fabricate(:theme) }
-  let!(:basic_html_field) do
-    Fabricate(
-      :theme_field,
-      theme: theme,
-      type_id: ThemeField.types[:html],
-      target_id: Theme.targets[:common],
-      name: "head_tag",
-      value: <<~HTML,
-        <script type="text/x-handlebars" data-template-name="/connectors/custom-homepage/new-home">
-          Hi friends!
-        </script>
-      HTML
-    )
-  end
 
   before do
     # A workaround to avoid the global notice from interfering with the tests
@@ -74,37 +60,70 @@ describe "Homepage", type: :system do
       theme.set_default!
     end
 
-    it "shows the custom homepage from the theme on the homepage" do
+    it "shows empty state to regular users" do
+      sign_in user
       visit "/"
 
-      expect(page).to have_css(".new-home", text: "Hi friends!")
-      expect(page).not_to have_css(".list-container")
-
-      find("#sidebar-section-content-community .sidebar-section-link:first-child").click
-      expect(page).to have_css(".list-container")
-
-      find("#site-logo").click
-
-      expect(page).not_to have_css(".list-container")
-      # ensure clicking on logo brings user back to the custom homepage
-      expect(page).to have_css(".new-home", text: "Hi friends!")
+      expect(page).to have_no_css(".list-container")
+      expect(page).to have_no_css(".alert-info")
     end
 
-    it "respects the user's homepage choice" do
+    it "shows empty state and notice to admins" do
+      sign_in admin
       visit "/"
 
-      expect(page).not_to have_css(".list-container")
-      expect(page).to have_css(".new-home", text: "Hi friends!")
+      expect(page).to have_no_css(".list-container")
+      expect(page).to have_css(".alert-info")
+    end
 
-      sign_in user
-      # top page ID in UserOption::HOMEPAGES = 5
-      user.user_option.update!(homepage_id: 5)
+    context "when the theme adds content to the [custom-homepage] connector" do
+      let!(:basic_html_field) do
+        Fabricate(
+          :theme_field,
+          theme: theme,
+          type_id: ThemeField.types[:html],
+          target_id: Theme.targets[:common],
+          name: "head_tag",
+          value: <<~HTML,
+            <script type="text/x-handlebars" data-template-name="/connectors/custom-homepage/new-home">
+              Hi friends!
+            </script>
+          HTML
+        )
+      end
 
-      visit "/"
+      it "shows the custom homepage from the theme on the homepage" do
+        visit "/"
 
-      expect(page).to have_css(".navigation-container .top.active", text: "Top")
-      expect(page).to have_css(".top-lists")
-      expect(page).not_to have_css(".new-home", text: "Hi friends!")
+        expect(page).to have_css(".new-home", text: "Hi friends!")
+        expect(page).to have_no_css(".list-container")
+
+        find("#sidebar-section-content-community .sidebar-section-link:first-child").click
+        expect(page).to have_css(".list-container")
+
+        find("#site-logo").click
+
+        expect(page).to have_no_css(".list-container")
+        # ensure clicking on logo brings user back to the custom homepage
+        expect(page).to have_css(".new-home", text: "Hi friends!")
+      end
+
+      it "respects the user's homepage choice" do
+        visit "/"
+
+        expect(page).not_to have_css(".list-container")
+        expect(page).to have_css(".new-home", text: "Hi friends!")
+
+        sign_in user
+        # top page ID in UserOption::HOMEPAGES = 5
+        user.user_option.update!(homepage_id: 5)
+
+        visit "/"
+
+        expect(page).to have_css(".navigation-container .top.active", text: "Top")
+        expect(page).to have_css(".top-lists")
+        expect(page).to have_no_css(".new-home", text: "Hi friends!")
+      end
     end
   end
 end
