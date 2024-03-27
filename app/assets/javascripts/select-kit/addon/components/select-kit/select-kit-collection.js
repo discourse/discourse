752ac0c8f21668bfe86c40773e4c96a3bf0bf692
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-03-27 11:22:37 +0100

    DEV: applies body-scroll-lock on select-kit
    This will prevent any scrolling issues with sk when on mobile, especially on iOS and in modals.
    
    Prior to this fix, scrolling could sometimes stop working in dropdowns.

--- app/assets/javascripts/select-kit/addon/components/select-kit/select-kit-collection.js
+++ app/assets/javascripts/select-kit/addon/components/select-kit/select-kit-collection.js
@@ -1,5 +1,20 @@
 import Component from "@ember/component";
+import { action } from "@ember/object";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+} from "discourse/lib/body-scroll-lock";
 
 export default Component.extend({
   tagName: "",
+
+  @action
+  lock(element) {
+    disableBodyScroll(element);
+  },
+
+  @action
+  unlock(element) {
+    enableBodyScroll(element);
+  },
 });
