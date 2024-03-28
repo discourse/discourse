fa92f8bc94deb5969caf6af6943b0cb3c6725fe5
Penar Musaraj <pmusaraj@gmail.com>
2024-03-28 15:03:40 -0400

    DEV: Add `{{hide-application-sidebar}}` helper (#26310)

--- app/assets/javascripts/discourse/app/controllers/application.js
+++ app/assets/javascripts/discourse/app/controllers/application.js
@@ -13,9 +13,10 @@ export default Controller.extend({
   showTop: true,
   router: service(),
   footer: service(),
+  sidebarState: service(),
   showSidebar: false,
-  navigationMenuQueryParamOverride: null,
   sidebarDisabledRouteOverride: false,
+  navigationMenuQueryParamOverride: null,
   showSiteHeader: true,
 
   init() {
@@ -67,31 +68,24 @@ export default Controller.extend({
     document.body.classList.remove("sidebar-animate");
   },
 
-  @discourseComputed(
-    "navigationMenuQueryParamOverride",
-    "siteSettings.navigation_menu",
-    "canDisplaySidebar",
-    "sidebarDisabledRouteOverride"
-  )
-  sidebarEnabled(
-    navigationMenuQueryParamOverride,
-    navigationMenu,
-    canDisplaySidebar,
-    sidebarDisabledRouteOverride
-  ) {
-    if (!canDisplaySidebar) {
+  get sidebarEnabled() {
+    if (!this.canDisplaySidebar) {
       return false;
     }
 
-    if (sidebarDisabledRouteOverride) {
+    if (this.sidebarState.sidebarHidden) {
       return false;
     }
 
-    if (navigationMenuQueryParamOverride === "sidebar") {
+    if (this.sidebarDisabledRouteOverride) {
+      return false;
+    }
+
+    if (this.navigationMenuQueryParamOverride === "sidebar") {
       return true;
     }
 
-    if (navigationMenuQueryParamOverride === "header_dropdown") {
+    if (this.navigationMenuQueryParamOverride === "header_dropdown") {
       return false;
     }
 
@@ -100,7 +94,7 @@ export default Controller.extend({
       return false;
     }
 
-    return navigationMenu === "sidebar";
+    return this.siteSettings.navigation_menu === "sidebar";
   },
 
   calculateShowSidebar() {
