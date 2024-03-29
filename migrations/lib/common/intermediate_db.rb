afb5310a812727edcb1ac93fc3bcdc3bea43d1e7
Gerhard Schlager <gerhard.schlager@discourse.org>
2024-03-29 14:33:41 +0100

    Split up IntermediateDb

--- migrations/lib/common/intermediate_db.rb
+++ migrations/lib/common/intermediate_db.rb
@@ -9,102 +9,25 @@ module Migrations
     TRANSACTION_BATCH_SIZE = 1000
     PREPARED_STATEMENT_CACHE_SIZE = 5
 
-    class << self
-      def create_connection(path:, journal_mode: DEFAULT_JOURNAL_MODE)
-        db = Extralite::Database.new(path)
-        db.pragma(
-          busy_timeout: 60_000, # 60 seconds
-          journal_mode: journal_mode,
-          synchronous: "off",
-          temp_store: "memory",
-          locking_mode: journal_mode == "wal" ? "normal" : "exclusive",
-          cache_size: -10_000, # 10_000 pages
-        )
-        db
-      end
-
-      def reset!(path)
-        [path, "#{path}-wal", "#{path}-shm"].each { |p| FileUtils.rm_f(p) if File.exist?(p) }
-      end
-
-      def migrate(path)
-        connection = create_connection(path: path)
-        performed_migrations = find_performed_migrations(connection)
-
-        path = File.join(Migrations.root_path, "db", "schema")
-        migrate_from_path(connection, path, performed_migrations)
-
-        connection.close
-      end
-
-      def connect
-        db = self.class.new
-        yield(db)
-      ensure
-        db.close if db
-      end
-
-      private
-
-      def new_database?(connection)
-        connection.query_single_splat(<<~SQL) == 0
-          SELECT COUNT(*)
-          FROM sqlite_schema
-          WHERE type = 'table' AND name = 'schema_migrations'
-        SQL
-      end
-
-      def find_performed_migrations(connection)
-        return Set.new if new_database?(connection)
-
-        connection.query_splat(<<~SQL).to_set
-          SELECT path
-          FROM schema_migrations
-        SQL
-      end
-
-      def migrate_from_path(connection, migration_path, performed_migrations)
-        file_pattern = File.join(migration_path, "*.sql")
-        Dir[file_pattern].sort.each do |path|
-          relative_path = Pathname(path).relative_path_from(Migrations.root_path).to_s
-
-          unless performed_migrations.include?(relative_path)
-            sql = File.read(path)
-            sql_hash = Digest::SHA1.hexdigest(sql)
-            connection.execute(sql)
-
-            connection.execute(<<~SQL, path: relative_path, sql_hash: sql_hash)
-              INSERT INTO schema_migrations (path, created_at, sql_hash)
-              VALUES (:path, datetime('now'), :sql_hash)
-            SQL
-          end
-        end
-      end
+    def self.create_connection(path:, journal_mode: DEFAULT_JOURNAL_MODE)
+      db = ::Extralite::Database.new(path)
+      db.pragma(
+        busy_timeout: 60_000, # 60 seconds
+        journal_mode: journal_mode,
+        synchronous: "off",
+        temp_store: "memory",
+        locking_mode: journal_mode == "wal" ? "normal" : "exclusive",
+        cache_size: -10_000, # 10_000 pages
+      )
+      db
     end
 
-    class PreparedStatementCache < LruRedux::Cache
-      class PreparedStatementHash < Hash
-        def shift
-          result = super
-          if (stmt = result[1])
-            stmt.close
-          end
-          result
-        end
-
-        def clear
-          each_value(&:close)
-          super
-        end
-      end
-
-      def initialize(*args)
-        super
-        @data = PreparedStatementHash.new
-      end
+    def self.connect
+      db = self.class.new
+      yield(db)
+    ensure
+      db.close if db
     end
-
-    private_constant :PreparedStatementCache
 
     attr_reader :connection
     attr_reader :path
