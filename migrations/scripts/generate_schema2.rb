41d0a0c4d912e8d376e2fa9a7ebd333e0bc59710
Gerhard Schlager <gerhard.schlager@discourse.org>
2024-03-29 20:58:37 +0100

    wip: update schema generator

---
+++ migrations/scripts/generate_schema2.rb
@@ -0,0 +1,96 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative "../lib/migrations"
+
+module Migrations
+  load_rails_environment
+  load_gemfiles("common")
+
+  class SchemaGenerator2
+    Table = Data.define(:name, :columns, :indexes, :primary_key_column_names)
+    Column = Data.define(:name, :datatype, :nullable, :primary_key)
+
+    def initialize(opts = {})
+      config = load_config
+
+      @core_db_connection = ActiveRecord::Base.connection
+
+      @table_configs = config[:tables]
+      @column_configs = config[:columns]
+    end
+
+    def run
+      table = generate_table("user_badges")
+      output_table(table)
+    end
+
+    private
+
+    def load_config
+      path = File.expand_path("../config/intermediate_db.yml", __dir__)
+      YAML.load_file(path, symbolize_names: true)
+    end
+
+    def generate_table(name)
+      pk_columns = @core_db_connection.primary_keys(name)
+
+      columns =
+        @core_db_connection
+          .columns(name)
+          .map { |c| Column.new(c.name, convert_datatype(c), c.null, pk_columns.include?(c.name)) }
+
+      Table.new(name, columns, nil, pk_columns)
+    end
+
+    def convert_datatype(column)
+      case column.type
+      when :string, :inet
+        "TEXT"
+      else
+        column.type.to_s.upcase
+      end
+    end
+
+    def output_table(table)
+      puts "CREATE TABLE #{table.name}"
+      puts "("
+      output_columns(table)
+      puts ");"
+    end
+
+    def output_columns(table)
+      columns = table.columns
+      max_column_name_length = columns.map { |c| c.name.length }.max
+      max_datatype_length = columns.map { |c| c.datatype.length }.max
+      is_composite_primary_key = table.primary_key_column_names.size > 1
+
+      column_definitions =
+        columns
+          .sort_by { |c| [c.primary_key ? 0 : 1, c.name] }
+          .map do |c|
+            definition = [
+              c.name.ljust(max_column_name_length),
+              c.datatype.ljust(max_datatype_length),
+              c.nullable ? nil : "NOT NULL",
+              c.primary_key && !is_composite_primary_key ? "PRIMARY KEY" : nil,
+            ]
+
+            definition.compact!
+            definition = definition.join(" ")
+            definition.strip!
+
+            "    #{definition}"
+          end
+
+      if is_composite_primary_key
+        pk_definition = table.primary_key_column_names.join(", ")
+        column_definitions << "    PRIMARY KEY (#{pk_definition})"
+      end
+
+      puts column_definitions.join(",\n")
+    end
+  end
+end
+
+Migrations::SchemaGenerator2.new.run
