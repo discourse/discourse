42f240dfdf7b065c2b675ec939e39a97b9850424
David Taylor <david@taylorhq.com>
2024-04-03 09:49:45 +0100

    demo failure

--- app/assets/javascripts/discourse/tests/acceptance/search-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/search-test.js
@@ -63,6 +63,7 @@ acceptance("Search - Anonymous", function (needs) {
       exists(".show-advanced-search"),
       "it shows full page search button"
     );
+    throw "this is an error";
   });
 
   test("random quick tips", async function (assert) {
