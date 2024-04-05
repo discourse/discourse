7a36e94ea92ceffd2d5f4562d839a9bc6239a5b1
David Taylor <david@taylorhq.com>
2024-04-05 16:37:05 +0100

    remove log

--- app/assets/javascripts/discourse/tests/acceptance/about-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/about-test.js
@@ -1,9 +1,6 @@
 import { visit } from "@ember/test-helpers";
-import { faker } from "@faker-js/faker";
 import { test } from "qunit";
 import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
-
-console.log("Faker says ", faker.person.bio());
 
 acceptance("About", function () {
   test("viewing", async function (assert) {
