19abdb79e41b3722d91e21f65571520c3fa4ed85
David Taylor <david@taylorhq.com>
2024-04-08 11:03:31 +0100

    Move faker loading to lib instead of service

--- app/assets/javascripts/discourse/app/lib/load-faker.js
+++ app/assets/javascripts/discourse/app/lib/load-faker.js
@@ -1,7 +1,14 @@
+let faker;
+
 /*
 Plugins & themes are unable to async-import npm modules directly.
 This wrapper provides them with a way to use fakerjs, while keeping the `import()` in core's codebase.
 */
 export default async function loadFaker() {
-  return await import("@faker-js/faker");
+  faker = await import("@faker-js/faker");
+  return faker;
+}
+
+export function getLoadedFaker() {
+  return faker;
 }
--- plugins/styleguide/assets/javascripts/discourse/routes/styleguide.js
+++ plugins/styleguide/assets/javascripts/discourse/routes/styleguide.js
@@ -1,12 +1,10 @@
 import Route from "@ember/routing/route";
-import { service } from "@ember/service";
+import loadFaker from "discourse/lib/load-faker";
 import { allCategories } from "discourse/plugins/styleguide/discourse/lib/styleguide";
 
 export default class Styleguide extends Route {
-  @service styleguide;
-
   async model() {
-    await this.styleguide.ensureFakerLoaded(); // So that it can be used synchronously in styleguide components
+    await loadFaker(); // So that it can be used synchronously in styleguide components
     return allCategories();
   }
 
