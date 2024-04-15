ca89061d2b991bf6553383f3e4d2ca7120609328
David Battersby <info@davidbattersby.com>
2024-04-15 14:37:41 +0800

    prevent null value on thread title prompts

--- plugins/chat/db/migrate/20240409093348_add_show_thread_title_prompts_to_user_options.rb
+++ plugins/chat/db/migrate/20240409093348_add_show_thread_title_prompts_to_user_options.rb
@@ -2,6 +2,6 @@
 
 class AddShowThreadTitlePromptsToUserOptions < ActiveRecord::Migration[7.0]
   def change
-    add_column :user_options, :show_thread_title_prompts, :boolean, default: true, null: true
+    add_column :user_options, :show_thread_title_prompts, :boolean, default: true, null: false
   end
 end
