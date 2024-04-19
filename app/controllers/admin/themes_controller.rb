a45b2b1124e71a1d9fd8af2b53f69efdf5dba2af
Gabriel Grubba <gabriel@discourse.org>
2024-04-19 11:38:28 -0300

    FIX: Locale mismatch at theme translations picker
    Before, the theme translations picker value was set to the
    site's default locale, which mismatches from the user's locale.
    
    This commit changes the picker value to the user locale.
    
    relates to https://meta.discourse.org/t/locale-mismatch-at-theme-translations/302879

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -176,6 +176,7 @@ class Admin::ThemesController < Admin::AdminController
       themes: serialize_data(@themes, ThemeSerializer),
       extras: {
         color_schemes: serialize_data(@color_schemes, ColorSchemeSerializer),
+        locale: I18n.locale,
       },
     }
 
