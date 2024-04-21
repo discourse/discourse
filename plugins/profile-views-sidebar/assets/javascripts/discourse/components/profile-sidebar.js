a769054e2bfd1b57ca85743d4566294dab69c810
Kedar <kedardeore@gmail.com>
2024-04-21 11:55:08 +0530

    Preview on main page

--- plugins/profile-views-sidebar/assets/javascripts/discourse/components/profile-sidebar.js
+++ plugins/profile-views-sidebar/assets/javascripts/discourse/components/profile-sidebar.js
@@ -1,16 +1,38 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
+import { ajax } from "discourse/lib/ajax";
+import discourseComputed from "discourse-common/utils/decorators";
 
 export default class ProfileSidebar extends Component {
   @service currentUser;
 
-  get course() {
+  constructor() {
+    super(...arguments);
+    this.fetchProfileViews();
+  }
+
+  async fetchProfileViews() {
+    const profileViews = await ajax("/u/profile-views.json");
+    this.set("profileViews", profileViews);
+  }
+
+  get topCollege() {
     return this.currentUser?.custom_fields?.[
-      this.siteSettings.user_enrollment_field
+      this.siteSettings.college_top_preference_field
     ];
   }
 
   get viewCount() {
     return this.currentUser?.profile_view_count || 0;
+  }
+
+  @discourseComputed("profileViews")
+  firstUser(profileViews) {
+    return profileViews?.views?.[0];
+  }
+
+  @discourseComputed("profileViews")
+  secondUser(profileViews) {
+    return profileViews?.views?.[1];
   }
 }
