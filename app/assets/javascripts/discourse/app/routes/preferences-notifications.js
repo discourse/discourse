38e8e3d9d1fa5bca8ee02598d5be6e9c68b349f1
Kedar <kedardeore@gmail.com>
2024-04-22 13:25:49 +0530

    Modified prefs screen

--- app/assets/javascripts/discourse/app/routes/preferences-notifications.js
+++ app/assets/javascripts/discourse/app/routes/preferences-notifications.js
@@ -1,3 +1,40 @@
-import RestrictedUserRoute from "discourse/routes/restricted-user";
+import ViewingActionType from "discourse/mixins/viewing-action-type";
+import DiscourseRoute from "discourse/routes/discourse";
+import I18n from "discourse-i18n";
 
-export default RestrictedUserRoute.extend({});
+const DEFAULT_LIMIT = 60;
+let limit = DEFAULT_LIMIT;
+
+export function setNotificationsLimit(newLimit) {
+  limit = newLimit;
+}
+
+export default DiscourseRoute.extend(ViewingActionType, {
+  controllerName: "user-notifications",
+  queryParams: { filter: { refreshModel: true } },
+
+  model(params) {
+    const username = this.modelFor("user").get("username");
+
+    if (
+      this.get("currentUser.username") === username ||
+      this.get("currentUser.admin")
+    ) {
+      return this.store.find("notification", {
+        username,
+        filter: params.filter,
+        limit,
+      });
+    }
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set("user", this.modelFor("user"));
+    this.viewingActionType(-1);
+  },
+
+  titleToken() {
+    return I18n.t("user.notifications");
+  },
+});
