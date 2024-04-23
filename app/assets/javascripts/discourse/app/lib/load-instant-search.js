68dc0f879084bb80f1be9a889ffcd67d198f49ee
Keegan George <kgeorge13@gmail.com>
2024-04-23 08:27:36 -0700

    DEV: Load all components

--- app/assets/javascripts/discourse/app/lib/load-instant-search.js
+++ app/assets/javascripts/discourse/app/lib/load-instant-search.js
@@ -3,10 +3,8 @@ Plugins & themes are unable to async-import npm modules directly.
 This wrapper provides them with a way to use ember-instantsearch, while keeping the `import()` in core's codebase.
 */
 export default async function loadInstantSearch() {
-  const { AisBaseWidget } = await import(
+  const emberInstantSearch = await import(
     "@discourse/ember-instantsearch/components"
   );
-  // console.log("async import test", AisBaseWidget);
-
-  return { AisBaseWidget };
+  return emberInstantSearch;
 }
