a1b8cc0bc7d6a6ce06e8ca5cfc9700acecef3c30
Arkshine <github@arkshine.me>
2024-04-23 06:34:51 +0200

    FIX: Automatic code highlighting not being applied in fullscreen

--- app/assets/javascripts/discourse/tests/integration/components/highlighted-code-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/highlighted-code-test.js
@@ -1,6 +1,7 @@
 import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { module, test } from "qunit";
+import highlightSyntax from "discourse/lib/highlight-syntax";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
 import { query } from "discourse/tests/helpers/qunit-helpers";
 
@@ -26,5 +27,35 @@ module("Integration | Component | highlighted-code", function (hooks) {
     await render(hbs`<HighlightedCode @lang="ruby" @code={{this.code}} />`);
 
     assert.strictEqual(query("code").innerText.trim(), LONG_CODE_BLOCK.trim());
+  });
+
+  test("highlighting code with lang=auto", async function (assert) {
+    this.set("code", "def test; end");
+
+    await render(hbs`<HighlightedCode @lang="auto" @code={{this.code}} />`);
+
+    const codeElement = query("code.hljs");
+
+    assert.ok(
+      !codeElement.classList.contains("lang-auto"),
+      "lang-auto is removed"
+    );
+    assert.ok(
+      Array.from(codeElement.classList).some((className) => {
+        return className.startsWith("language-");
+      }),
+      "language is detected"
+    );
+
+    await highlightSyntax(
+      codeElement.parentElement, // <pre>
+      this.siteSettings,
+      this.session
+    );
+
+    assert.ok(
+      codeElement.dataset.unknownHljsLang === undefined,
+      "language is found from language- class"
+    );
   });
 });
