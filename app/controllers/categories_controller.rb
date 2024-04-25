dbe923d26e996bafba85f9b53ec57b23cb311b82
Bianca Nenciu <nbianca@users.noreply.github.com>
2024-04-25 16:47:45 +0300

    FIX: Fetch categories for "+subcategories" option (#26622)
    Selecting the +subcategories option does not work sometimes when "lazy
    load categories" is enabled because the subcategories may not be
    fetched. This ensures that subcategories are loaded by requesting them
    before being used.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -362,7 +362,15 @@ class CategoriesController < ApplicationController
     prioritized_category_id = params[:prioritized_category_id].to_i if params[
       :prioritized_category_id
     ].present?
-    limit = params[:limit].to_i.clamp(1, MAX_CATEGORIES_LIMIT) if params[:limit].present?
+    limit =
+      (
+        if params[:limit].present?
+          params[:limit].to_i.clamp(1, MAX_CATEGORIES_LIMIT)
+        else
+          MAX_CATEGORIES_LIMIT
+        end
+      )
+    page = [1, params[:page].to_i].max
 
     categories = Category.secured(guardian)
 
@@ -404,7 +412,8 @@ class CategoriesController < ApplicationController
         )
         .joins("LEFT JOIN topics t on t.id = categories.topic_id")
         .select("categories.*, t.slug topic_slug")
-        .limit(limit || MAX_CATEGORIES_LIMIT)
+        .limit(limit)
+        .offset((page - 1) * limit)
 
     if Site.preloaded_category_custom_fields.present?
       Category.preload_custom_fields(categories, Site.preloaded_category_custom_fields)
