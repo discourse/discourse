3c7cd4d7dc8656d2fdb0247b72e6fefbb9be58ba
David Taylor <david@taylorhq.com>
2024-05-02 18:46:44 +0100

    DEV: Update chat scheduled job loading to match skeleton
    Followup to e949684fc54e629035b8883dabd9ef2916251da1, ref https://github.com/discourse/discourse-plugin-skeleton/pull/47

--- plugins/chat/lib/chat/engine.rb
+++ plugins/chat/lib/chat/engine.rb
@@ -8,8 +8,9 @@ module ::Chat
     engine_name PLUGIN_NAME
     isolate_namespace Chat
     config.autoload_paths << File.join(config.root, "lib")
+    scheduled_job_dir = "#{config.root}/app/jobs/scheduled"
     config.to_prepare do
-      Rails.autoloaders.main.eager_load_dir("#{Chat::Engine.config.root}/app/jobs/scheduled")
+      Rails.autoloaders.main.eager_load_dir(scheduled_job_dir) if Dir.exist?(scheduled_job_dir)
     end
   end
 
