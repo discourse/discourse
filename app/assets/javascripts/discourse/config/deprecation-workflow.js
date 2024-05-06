e245dd8f211d23cf544c33e1977507dd46835c87
Jarek Radosz <jradosz@gmail.com>
2024-05-06 18:00:31 +0200

    DEV: Unsilence select-kit and d-section deprecations

--- app/assets/javascripts/discourse/config/deprecation-workflow.js
+++ app/assets/javascripts/discourse/config/deprecation-workflow.js
@@ -7,8 +7,6 @@ globalThis.deprecationWorkflow.config = {
       handler: "silence",
       matchId: "ember-this-fallback.this-property-fallback",
     },
-    { handler: "silence", matchId: "discourse.select-kit" },
-    { handler: "silence", matchId: "discourse.d-section" },
     {
       handler: "silence",
       matchId: "discourse.decorate-widget.hamburger-widget-links",
