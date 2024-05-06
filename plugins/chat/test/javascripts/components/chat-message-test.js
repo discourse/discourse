26cbb734ee79cad3b8611988fc59c32b93a20a0e
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-05-06 23:34:32 +0200

    FIX: improvements to chat message streaming
    - prevents re-rendering avatars while updating messages quickly in the thread preview indicator
    - ensures the cancel button is shown when you are admin OR when the streamed message is a reply to the current user

--- plugins/chat/test/javascripts/components/chat-message-test.js
+++ plugins/chat/test/javascripts/components/chat-message-test.js
@@ -1,7 +1,8 @@
 import { getOwner } from "@ember/application";
-import { render } from "@ember/test-helpers";
+import { clearRender, render } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 import { module, test } from "qunit";
+import CoreFabricators from "discourse/lib/fabricators";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
 import { exists } from "discourse/tests/helpers/qunit-helpers";
 import ChatFabricators from "discourse/plugins/chat/discourse/lib/fabricators";
@@ -57,5 +58,57 @@ module("Discourse Chat | Component | chat-message", function (hooks) {
       exists(".chat-message-container.has-reply"),
       "has the correct css class"
     );
+  });
+
+  test("Message with streaming", async function (assert) {
+    // admin
+    this.currentUser.admin = true;
+
+    this.message = new ChatFabricators(getOwner(this)).message({
+      inReplyTo: new ChatFabricators(getOwner(this)).message(),
+      streaming: true,
+    });
+    await this.message.cook();
+    await render(template);
+
+    assert
+      .dom(".stop-streaming-btn")
+      .exists("when admin, it has the stop streaming button");
+
+    await clearRender();
+
+    // not admin - not replying to current user
+    this.currentUser.admin = false;
+
+    this.message = new ChatFabricators(getOwner(this)).message({
+      inReplyTo: new ChatFabricators(getOwner(this)).message(),
+      streaming: true,
+    });
+    await this.message.cook();
+    await render(template);
+
+    assert
+      .dom(".stop-streaming-btn")
+      .doesNotExist("when admin, it doesn't have the stop streaming button");
+
+    await clearRender();
+
+    // not admin - replying to current user
+    this.currentUser.admin = false;
+
+    this.message = new ChatFabricators(getOwner(this)).message({
+      inReplyTo: new ChatFabricators(getOwner(this)).message({
+        user: this.currentUser,
+      }),
+      streaming: true,
+    });
+    await this.message.cook();
+    await render(template);
+
+    assert
+      .dom(".stop-streaming-btn")
+      .exists(
+        "when replying to current user, it has the stop streaming button"
+      );
   });
 });
