9bf763d03f020e507aefd3122e5d27a356eed4a0
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-05-08 14:09:00 +0200

    FIX: inject current user service in category model (#26937)

--- app/assets/javascripts/discourse/app/models/category.js
+++ app/assets/javascripts/discourse/app/models/category.js
@@ -1,5 +1,6 @@
 import { warn } from "@ember/debug";
 import { computed, get } from "@ember/object";
+import { service } from "@ember/service";
 import { on } from "@ember-decorators/object";
 import { ajax } from "discourse/lib/ajax";
 import { NotificationLevels } from "discourse/lib/notification-levels";
@@ -420,6 +421,8 @@ export default class Category extends RestModel {
       );
     }
   }
+
+  @service currentUser;
 
   permissions = null;
 
