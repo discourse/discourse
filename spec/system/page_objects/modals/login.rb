cff1f50c7f40b9e27ae23d778da9f5d857cd0e4b
Jan Cernik <jancernik12@gmail.com>
2024-05-11 14:18:34 -0300

    Add page_objects for login/signup modals

--- spec/system/page_objects/modals/login.rb
+++ spec/system/page_objects/modals/login.rb
@@ -6,6 +6,70 @@ module PageObjects
       def open?
         super && has_css?(".login-modal")
       end
+
+      def open
+        visit("/login")
+      end
+
+      def open_from_header
+        find(".login-button").click
+      end
+
+      def open_signup
+        find("#new-account-link").click
+      end
+
+      def confirm_login
+        find("#login-button").click
+      end
+
+      def forgot_password
+        find("#forgot-password-link").click
+      end
+
+      def fill_username(username)
+        find("#login-account-name").fill_in(with: username)
+      end
+
+      def fill_password(password)
+        find("#login-account-password").fill_in(with: password)
+      end
+
+      def login(username: nil, password: nil)
+        if (username.present? && password.present?)
+          fill_username(username)
+          fill_password(password)
+          confirm_login
+        end
+      end
+
+      def select_facebook
+        find(".btn-social.facebook").click
+      end
+
+      def select_google
+        find(".btn-social.google_oauth2").click
+      end
+
+      def select_github
+        find(".btn-social.github").click
+      end
+
+      def select_twitter
+        find(".btn-social.twitter").click
+      end
+
+      def select_discord
+        find(".btn-social.discord").click
+      end
+
+      def select_linkedin
+        find(".btn-social.linkedin_oidc").click
+      end
+
+      def select_passkey
+        find(".btn-social.passkey-login-button").click
+      end
     end
   end
 end
---
+++ spec/system/page_objects/modals/signup.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Modals
+    class Signup < PageObjects::Modals::Base
+      def open?
+        super && has_css?(".modal.create-account")
+      end
+
+      def open
+        visit("/signup")
+      end
+
+      def open_from_header
+        find(".sign-up-button").click
+      end
+
+      def open_login
+        find("#login-link").click
+      end
+
+      def confirm_signup
+        find(".modal.create-account .btn-primary").click
+      end
+
+      def has_password_input?
+        has_css?("#new-account-password")
+      end
+
+      def has_no_password_input?
+        has_no_css?("#new-account-password")
+      end
+
+      def fill_email(email)
+        find("#new-account-email").fill_in(with: email)
+      end
+
+      def fill_username(username)
+        find("#new-account-username").fill_in(with: username)
+      end
+
+      def fill_name(name)
+        find("#new-account-name").fill_in(with: name)
+      end
+
+      def fill_password(password)
+        find("#new-account-password").fill_in(with: password)
+      end
+
+      def has_valid_email?
+        find(".create-account-email").has_css?("#account-email-validation.good")
+      end
+
+      def has_valid_username?
+        find(".create-account__username").has_css?("#username-validation.good")
+      end
+
+      def has_valid_password?
+        find(".create-account__password").has_css?("#password-validation.good")
+      end
+
+      # def has_email?(value)
+      #   find("#new-account-email").value == value
+      # end
+
+      # def has_username?(value)
+      #   find("#new-account-username").value == value
+      # end
+
+      def signup(email: nil, username: nil, name: nil, password: nil)
+        if (email.present? && username.present? && password.present?)
+          fill_email(email)
+          fill_username(username)
+          fill_password(password)
+
+          fill_name(name) if name.present?
+          confirm_signup
+        end
+      end
+
+      def select_facebook
+        find(".btn-social.facebook").click
+      end
+
+      def select_google
+        find(".btn-social.google_oauth2").click
+      end
+
+      def select_github
+        find(".btn-social.github").click
+      end
+
+      def select_twitter
+        find(".btn-social.twitter").click
+      end
+
+      def select_discord
+        find(".btn-social.discord").click
+      end
+
+      def select_linkedin
+        find(".btn-social.linkedin_oidc").click
+      end
+    end
+  end
+end
