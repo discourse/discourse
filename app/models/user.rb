6457b7f0a5568616f781d33de152fe824db7e644
OsamaSayegh <asooomaasoooma90@gmail.com>
2024-05-14 19:32:54 +0300

    FIX: Create directory items for new users when in bootstrap mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -171,6 +171,7 @@ class User < ActiveRecord::Base
   after_create :set_default_categories_preferences
   after_create :set_default_tags_preferences
   after_create :set_default_sidebar_section_links
+  after_create :refresh_user_directory, if: Proc.new { SiteSetting.bootstrap_mode_enabled }
   after_update :set_default_sidebar_section_links, if: Proc.new { self.saved_change_to_staged? }
 
   after_update :trigger_user_updated_event,
@@ -2168,6 +2169,10 @@ class User < ActiveRecord::Base
 
   def validate_status!(status)
     UserStatus.new(status).validate!
+  end
+
+  def refresh_user_directory
+    DirectoryItem.refresh!
   end
 end
 
