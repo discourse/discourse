a5b105c625e71138614a0cf1c06adb911c706cd0
David Battersby <info@davidbattersby.com>
2024-05-16 22:17:15 +0400

    fix batching logic and move to post migrate

--- plugins/chat/db/post_migrate/20240516145911_update_user_options_for_thread_title_prompts.rb
+++ plugins/chat/db/post_migrate/20240516145911_update_user_options_for_thread_title_prompts.rb
@@ -2,18 +2,18 @@
 
 class UpdateUserOptionsForThreadTitlePrompts < ActiveRecord::Migration[7.0]
   def up
-    change_column_default :user_options, :show_thread_title_prompts, true
-
     min, max = DB.query_single("SELECT MIN(user_id), MAX(user_id) FROM user_options")
     while min <= max
       DB.exec(
-        "UPDATE user_options SET show_thread_title_prompts = true WHERE user_id >= #{min} AND user_id <= #{max}",
+        "UPDATE user_options SET show_thread_title_prompts = true WHERE user_id >= #{min} AND user_id <= #{min + 100_000}",
       )
       min += 100_000
     end
 
+    change_column_default :user_options, :show_thread_title_prompts, true
     change_column_null :user_options, :show_thread_title_prompts, false
   end
+
   def down
     raise ActiveRecord::IrreversibleMigration
   end
