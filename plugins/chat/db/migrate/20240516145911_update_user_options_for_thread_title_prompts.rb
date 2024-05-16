591764e646ae4bcc9d08c9ad863cdf3e20b76119
David Battersby <info@davidbattersby.com>
2024-05-16 20:24:21 +0400

    DEV: update thread title prompt migration

---
+++ plugins/chat/db/migrate/20240516145911_update_user_options_for_thread_title_prompts.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class UpdateUserOptionsForThreadTitlePrompts < ActiveRecord::Migration[7.0]
+  def up
+    min, max = DB.query_single("SELECT MIN(user_id), MAX(user_id) FROM user_options")
+    while min <= max
+      DB.exec(
+        "UPDATE user_options SET show_thread_title_prompts = true WHERE user_id >= #{min} AND user_id <= #{max}",
+      )
+      min += 100_000
+    end
+
+    change_column_default :user_options, :show_thread_title_prompts, from: nil, to: true
+    change_column_null :user_options, :show_thread_title_prompts, false
+  end
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
