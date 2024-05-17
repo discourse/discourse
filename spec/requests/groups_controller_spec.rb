discourse/discourse

commit 1deeff2336a3d9180ec6276a8fd5b399ec6165c8

FIX: Serialize categories for group posts  (#26663)
This is necessary when "lazy load categories" feature is enabled to
make sure the categories are rendered for group posts.

app/controllers/groups_controller.rb
@@ -196,7 +196,16 @@ class GroupsController < ApplicationController
 
     posts =
       group.posts_for(guardian, params.permit(:before_post_id, :before, :category_id)).limit(20)
-    render_serialized posts.to_a, GroupPostSerializer
+
+    response = { posts: serialize_data(posts, GroupPostSerializer) }
+
+    if guardian.can_lazy_load_categories?
+      category_ids = posts.map { |p| p.topic.category_id }.compact.uniq
+      categories = Category.secured(guardian).with_parents(category_ids)
+      response[:categories] = serialize_data(categories, CategoryBadgeSerializer)
+    end
+
+    render json: response
   end
 
   def posts_feed

spec/requests/groups_controller_spec.rb
@@ -551,7 +551,7 @@ RSpec.describe GroupsController do
       get "/groups/#{group.name}/posts.json"
 
       expect(response.status).to eq(200)
-      expect(response.parsed_body.first["id"]).to eq(post.id)
+      expect(response.parsed_body["posts"].first["id"]).to eq(post.id)
     end
 
     it "returns moderator actions" do
@@ -560,7 +560,7 @@ RSpec.describe GroupsController do
       get "/groups/#{group.name}/posts.json"
 
       expect(response.status).to eq(200)
-      expect(response.parsed_body.first["id"]).to eq(post.id)
+      expect(response.parsed_body["posts"].first["id"]).to eq(post.id)
     end
   end
 

app/assets/javascripts/discourse/app/models/group.js
@@ -8,6 +8,7 @@ import { ajax } from "discourse/lib/ajax";
 import Category from "discourse/models/category";
 import GroupHistory from "discourse/models/group-history";
 import RestModel from "discourse/models/rest";
+import Site from "discourse/models/site";
 import Topic from "discourse/models/topic";
 import User from "discourse/models/user";
 import discourseComputed from "discourse-common/utils/decorators";
@@ -463,15 +464,16 @@ export default class Group extends RestModel {
       data.category_id = parseInt(opts.categoryId, 10);
     }
 
-    const posts = await ajax(`/groups/${this.name}/${type}.json`, { data });
-    const categories = await Category.asyncFindByIds(
-      posts.map((p) => p.category_id)
-    );
+    const result = await ajax(`/groups/${this.name}/${type}.json`, { data });
 
-    return posts.map((p) => {
+    result.categories?.forEach((category) => {
+      Site.current().updateCategory(category);
+    });
+
+    return result.posts.map((p) => {
       p.user = User.create(p.user);
       p.topic = Topic.create(p.topic);
-      p.category = categories[p.category_id];
+      p.category = Category.findById(p.category_id);
       return EmberObject.create(p);
     });
   }

commit 46f8d8a777085ceb4c427a1f7d9fe24b694de1d6

FIX: group's mentions was broken
In https://github.com/discourse/discourse/commit/1deeff2336a3d9180ec6276a8fd5b399ec6165c8 we changed the format of the results given by the API but we forgot to update the `#mentions` endpoint as well.

Context - https://meta.discourse.org/t/-/308044

app/controllers/groups_controller.rb
@@ -218,26 +218,43 @@ class GroupsController < ApplicationController
       "#{SiteSetting.title} - #{I18n.t("rss_description.group_posts", group_name: group.name)}"
     @link = Discourse.base_url
     @description = I18n.t("rss_description.group_posts", group_name: group.name)
+
     render "posts/latest", formats: [:rss]
   end
 
   def mentions
     raise Discourse::NotFound unless SiteSetting.enable_mentions?
+
     group = find_group(:group_id)
+    guardian.ensure_can_see_group_members!(group)
+
     posts =
       group.mentioned_posts_for(guardian, params.permit(:before_post_id, :category_id)).limit(20)
-    render_serialized posts.to_a, GroupPostSerializer
+
+    response = { posts: serialize_data(posts, GroupPostSerializer) }
+
+    if guardian.can_lazy_load_categories?
+      category_ids = posts.map { |p| p.topic.category_id }.compact.uniq
+      categories = Category.secured(guardian).with_parents(category_ids)
+      response[:categories] = serialize_data(categories, CategoryBadgeSerializer)
+    end
+
+    render json: response
   end
 
   def mentions_feed
     raise Discourse::NotFound unless SiteSetting.enable_mentions?
+
     group = find_group(:group_id)
+    guardian.ensure_can_see_group_members!(group)
+
     @posts =
       group.mentioned_posts_for(guardian, params.permit(:before_post_id, :category_id)).limit(50)
     @title =
       "#{SiteSetting.title} - #{I18n.t("rss_description.group_mentions", group_name: group.name)}"
     @link = Discourse.base_url
     @description = I18n.t("rss_description.group_mentions", group_name: group.name)
+
     render "posts/latest", formats: [:rss]
   end
 

spec/requests/groups_controller_spec.rb
@@ -526,6 +526,46 @@ RSpec.describe GroupsController do
     end
   end
 
+  describe "#mentions" do
+    it "ensures mentions are enabled" do
+      SiteSetting.enable_mentions = false
+
+      sign_in(user)
+      get "/groups/#{group.name}/mentions.json"
+
+      expect(response.status).to eq(404)
+    end
+
+    it "ensures the group can be seen" do
+      sign_in(user)
+      group.update!(visibility_level: Group.visibility_levels[:owners])
+
+      get "/groups/#{group.name}/mentions.json"
+
+      expect(response.status).to eq(404)
+    end
+
+    it "ensures the group members can be seen" do
+      sign_in(user)
+      group.update!(members_visibility_level: Group.visibility_levels[:owners])
+
+      get "/groups/#{group.name}/mentions.json"
+
+      expect(response.status).to eq(403)
+    end
+
+    it "returns the right response" do
+      post = Fabricate(:post)
+      GroupMention.create!(post: post, group: group)
+
+      sign_in(user)
+      get "/groups/#{group.name}/mentions.json"
+
+      expect(response.status).to eq(200)
+      expect(response.parsed_body["posts"].first["id"]).to eq(post.id)
+    end
+  end
+
   describe "#posts" do
     it "ensures the group can be seen" do
       sign_in(user)