73ee4b06c0860015742b445859ed21b1a23c4ccb
Sérgio Saquetim <saquetim@discourse.org>
2024-05-20 17:59:33 -0300

    Better handling of the position for the DAG

--- app/assets/javascripts/discourse/app/lib/plugin-api/value-modifier.js
+++ app/assets/javascripts/discourse/app/lib/plugin-api/value-modifier.js
@@ -18,18 +18,16 @@ export function registerValueModifier(
       "api registerValueModifier requires valueModifier to be set with a value or a callback"
     );
   }
+  if (position && !position.before && !position.after) {
+    throw new Error(
+      "api registerValueModifier requires position if provided, to have a before or after key"
+    );
+  }
 
   // setting the default position to be after the last registered plugin enforces the DAG to be executed in the order
   // the plugins/theme components are registered, which provides a predictable order of execution in case a position is
   // not explicitly set
-  const existingModifiers =
-    modifiersRegistry.get(modifierName) ||
-    new DAG({
-      defaultPosition: {
-        after:
-          modifierLastRegisterPluginId.get(modifierName) || "__discourse:core",
-      },
-    });
+  const existingModifiers = modifiersRegistry.get(modifierName) || new DAG();
 
   if (existingModifiers.has(pluginId)) {
     // eslint-disable-next-line no-console
@@ -39,10 +37,21 @@ export function registerValueModifier(
     existingModifiers.delete(pluginId);
   }
 
-  modifierLastRegisterPluginId.set(pluginId);
-  existingModifiers.add(pluginId, valueModifier, position);
+  existingModifiers.add(
+    pluginId,
+    valueModifier,
+    position || {
+      after: modifierLastRegisterPluginId.get(pluginId) || "__discourse:core",
+    }
+  );
 
   modifiersRegistry.set(modifierName, existingModifiers);
+
+  // just updated the last registered plugin id, if a position is not provided, or else we can bind the next modifier
+  // with the position specified
+  if (!position) {
+    modifierLastRegisterPluginId.set(pluginId);
+  }
 
   // resolve the modifiers DAG to cache the result and avoid resolving it every time the value modifier is used
   resolveModifiersDag(modifierName);
