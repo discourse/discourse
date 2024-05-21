a28ded459c1ba58e627c27a35125322371e4407b
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-05-21 20:35:08 +0200

    FIX: play audio sound on message in non group DMs
    Prior to this change, only mentions would get a notification and a sound. This change will not create a notification for this case, but will play a sound. This is still respecting notification settings, not playing the sound when you are viewing the channel or not following it.

---
+++ plugins/chat/assets/javascripts/discourse/services/chat-channel-notification-sound.js
@@ -0,0 +1,44 @@
+import Service, { service } from "@ember/service";
+
+export default class ChatChannelNotificationSound extends Service {
+  @service chat;
+  @service chatAudioManager;
+  @service currentUser;
+  @service site;
+
+  async play(channel) {
+    if (channel.isCategoryChannel) {
+      return false;
+    }
+
+    if (channel.chatable.group) {
+      return false;
+    }
+
+    if (this.currentUser.isInDoNotDisturb()) {
+      return false;
+    }
+
+    if (!this.currentUser.chat_sound) {
+      return false;
+    }
+
+    if (this.site.mobileView) {
+      return false;
+    }
+
+    const membership = channel.currentUserMembership;
+    if (
+      !membership.following ||
+      membership.desktopNotificationLevel !== "always" ||
+      membership.muted ||
+      this.chat.activeChannel === channel
+    ) {
+      return false;
+    }
+
+    await this.chatAudioManager.play(this.currentUser.chat_sound);
+
+    return true;
+  }
+}
