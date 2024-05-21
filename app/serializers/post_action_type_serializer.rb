dbd21e93638d30f213d8a8b091a16f919ae71f93
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2024-05-21 16:21:07 +1000

    Revert "DEV: move post flags into database (#26951)" (#27102)
    This reverts commit 7aff9806eb3592767313299d7a0b8c413e328d20.

--- app/serializers/post_action_type_serializer.rb
+++ app/serializers/post_action_type_serializer.rb
@@ -14,25 +14,25 @@ class PostActionTypeSerializer < ApplicationSerializer
   end
 
   def name
-    i18n("title", default: object.class.flag_settings.names[object.id])
+    i18n("title")
   end
 
   def description
-    i18n("description", vars: { tos_url: tos_url, base_path: Discourse.base_path })
+    i18n("description", tos_url: tos_url, base_path: Discourse.base_path)
   end
 
   def short_description
-    i18n("short_description", vars: { tos_url: tos_url, base_path: Discourse.base_path })
+    i18n("short_description", tos_url: tos_url, base_path: Discourse.base_path)
   end
 
   def name_key
-    PostActionType.types[object.id].to_s
+    PostActionType.types[object.id]
   end
 
   protected
 
-  def i18n(field, default: nil, vars: nil)
+  def i18n(field, vars = nil)
     key = "post_action_types.#{name_key}.#{field}"
-    vars ? I18n.t(key, vars, default: default) : I18n.t(key, default: default)
+    vars ? I18n.t(key, vars) : I18n.t(key)
   end
 end
