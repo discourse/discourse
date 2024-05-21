87769a83c444faae4e7ee74aefa33f960b38ae83
Jarek Radosz <jradosz@gmail.com>
2024-05-21 14:36:15 +0200

    DEV: Implement glimmer topic-list (#26743)
    (experimental)
    
    The initial implementation of glimmer topic-list and related components. Does not include new APIs and isn't compatible with existing customization. That's gonna come in future PRs.
    
    Enabled by adding groups to `experimental_glimmer_topic_list_groups` setting.

--- spec/system/page_objects/pages/topic.rb
+++ spec/system/page_objects/pages/topic.rb
@@ -228,6 +228,10 @@ module PageObjects
         post_by_number(post).has_css?(".read-state.read", visible: :all, wait: 3)
       end
 
+      def has_suggested_topic?(topic)
+        page.has_css?("#suggested-topics .topic-list-item[data-topic-id='#{topic.id}']")
+      end
+
       def move_to_public_category(category)
         click_admin_menu_button
         find(".topic-admin-menu-content li.topic-admin-convert").click
