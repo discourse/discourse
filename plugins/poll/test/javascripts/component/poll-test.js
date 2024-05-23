93592b2d35896325911363cdfe61ed1c7d8fd93b
merefield <merefield@gmail.com>
2024-05-23 15:41:16 +0100

    fix test import

--- plugins/poll/test/javascripts/component/poll-test.js
+++ plugins/poll/test/javascripts/component/poll-test.js
@@ -1,10 +1,15 @@
 import EmberObject from "@ember/object";
-import { click, exists, render } from "@ember/test-helpers";
+import { click, render } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 import { module, test } from "qunit";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
 import pretender, { response } from "discourse/tests/helpers/create-pretender";
-import { count, query, queryAll } from "discourse/tests/helpers/qunit-helpers";
+import {
+  count,
+  exists,
+  query,
+  queryAll,
+} from "discourse/tests/helpers/qunit-helpers";
 import I18n from "discourse-i18n";
 
 let requests = 0;
