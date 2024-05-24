4a65212ef2a991284600f662ee1aef29acc682a2
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-05-24 18:16:35 +0200

    linting

--- plugins/chat/test/javascripts/unit/services/chat-channel-notification-sound-test.js
+++ plugins/chat/test/javascripts/unit/services/chat-channel-notification-sound-test.js
@@ -12,12 +12,7 @@ import {
 } from "discourse/tests/helpers/qunit-helpers";
 import ChatFabricators from "discourse/plugins/chat/discourse/lib/fabricators";
 
-function buildDirectMessageChannel(owner) {
-  const channel = new ChatFabricators(owner).directMessageChannel();
-  buildMembership(channel);
-  return channel;
-}
-function buildCategoryMessageChannel(owner) {
+function buildChannel(owner) {
   const channel = new ChatFabricators(owner).channel();
   buildMembership(channel);
   return channel;
@@ -63,68 +58,68 @@ acceptance(
 
     test("in do not disturb", async function (assert) {
       updateCurrentUser({ do_not_disturb_until: moment().add(1, "hour") });
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
 
       assert.deepEqual(await this.subject.play(channel), false);
     });
 
     test("no chat sound", async function (assert) {
       updateCurrentUser({ chat_sound: null });
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
 
       assert.deepEqual(await this.subject.play(channel), false);
     });
 
     test("mobile", async function (assert) {
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
       this.site.mobileView = true;
 
       assert.deepEqual(await this.subject.play(channel), false);
     });
 
     test("plays sound", async function (assert) {
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
 
       assert.deepEqual(await this.subject.play(channel), true);
     });
 
     test("muted", async function (assert) {
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
       channel.currentUserMembership.muted = true;
 
       assert.deepEqual(await this.subject.play(channel), false);
     });
 
     test("not following", async function (assert) {
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
       channel.currentUserMembership.following = false;
 
       assert.deepEqual(await this.subject.play(channel), false);
     });
 
     test("no notification", async function (assert) {
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
       channel.currentUserMembership.desktopNotificationLevel = "never";
 
       assert.deepEqual(await this.subject.play(channel), false);
     });
 
     test("currently active channel", async function (assert) {
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
       this.chat.activeChannel = channel;
 
       assert.deepEqual(await this.subject.play(channel), false);
     });
 
     test("not idle", async function (assert) {
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
       resetIdle();
 
       assert.deepEqual(await this.subject.play(channel), true);
     });
 
     test("notifications disabled", async function (assert) {
-      const channel = buildDirectMessageChannel(getOwner(this));
+      const channel = buildChannel(getOwner(this));
       disable();
 
       assert.deepEqual(await this.subject.play(channel), false);
