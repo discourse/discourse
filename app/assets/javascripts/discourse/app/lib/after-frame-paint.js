63b7b598cb9cd93e90e447fea24f2f517c2592c5
David Taylor <david@taylorhq.com>
2024-05-28 13:09:46 +0100

    DEV: Allow afterFramePaint to be used in tests (#27226)
    We need to register a waiter so that `settled()` will wait for `runAfterFramePaint()` callbacks to be run before proceeding.

--- app/assets/javascripts/discourse/app/lib/after-frame-paint.js
+++ app/assets/javascripts/discourse/app/lib/after-frame-paint.js
@@ -1,8 +1,13 @@
+import { registerWaiter } from "@ember/test";
+
 /**
  * Runs `callback` shortly after the next browser Frame is produced.
  * ref: https://webperf.tips/tip/measuring-paint-time
  */
 export default function runAfterFramePaint(callback) {
+  let done = false;
+  registerWaiter(() => done);
+
   // Queue a "before Render Steps" callback via requestAnimationFrame.
   requestAnimationFrame(() => {
     // MessageChannel is one of the highest priority task queues
@@ -10,7 +15,10 @@ export default function runAfterFramePaint(callback) {
     const messageChannel = new MessageChannel();
 
     // Setup the callback to run in a Task
-    messageChannel.port1.onmessage = callback;
+    messageChannel.port1.onmessage = () => {
+      done = true;
+      callback();
+    };
 
     // Queue the Task on the Task Queue
     messageChannel.port2.postMessage(undefined);
