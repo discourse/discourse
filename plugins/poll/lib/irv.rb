b844bf255b37f46f387932c348b33b53fdc4b794
merefield <merefield@gmail.com>
2024-05-29 19:13:43 +0100

    break-up irv algorithm into smaller parts

--- plugins/poll/lib/irv.rb
+++ plugins/poll/lib/irv.rb
@@ -32,18 +32,21 @@ class DiscoursePoll::Irv
     round = 0
     while true
       round += 1
+
       # Count the first place votes for each candidate
       tally = tally_votes(current_votes)
 
-      # Check for a majority
       max_votes = tally.values.max
-      total_votes = tally.values.sum
-      potential_winners = tally.select { |k, v| v == max_votes }
 
-      # If a majority is found or only one candidate remains
-      if max_votes > total_votes / 2 || potential_winners.count == 1
+      # Find the candidate(s) with the most votes
+      potential_winners = find_potential_winners(tally, max_votes)
+
+      # Check for a majority and return if found
+      if majority_check(tally, potential_winners, max_votes)
         majority_candidate = enrich(potential_winners.keys.first, options)
+
         round_activity << { round: round, majority: majority_candidate, eliminated: nil }
+
         return(
           {
             tied: false,
@@ -56,8 +59,7 @@ class DiscoursePoll::Irv
       end
 
       # Find the candidate(s) with the least votes
-      min_votes = tally.values.min
-      losers = tally.select { |k, v| v == min_votes }.keys
+      losers = identify_losers(tally)
 
       # Remove the candidate with the least votes
       current_votes.each do |vote|
@@ -66,10 +68,12 @@ class DiscoursePoll::Irv
       end
 
       losers = losers.map { |loser| enrich(loser, options) }
+
+      round_activity << { round: round, majority: nil, eliminated: losers }
+
       all_empty = current_votes.all? { |arr| arr.empty? }
 
       if all_empty
-        round_activity << { round: round, majority: nil, eliminated: losers }
         return(
           {
             tied: true,
@@ -80,7 +84,6 @@ class DiscoursePoll::Irv
           }
         )
       end
-      round_activity << { round: round, majority: nil, eliminated: losers }
     end
   end
 
@@ -93,6 +96,22 @@ class DiscoursePoll::Irv
     end
     current_votes.each { |vote| tally[vote.first] += 1 if vote.first }
     tally
+  end
+
+  def self.find_potential_winners(tally, max_votes)
+    tally.select { |k, v| v == max_votes }
+  end
+
+  def self.majority_check(tally, potential_winners, max_votes)
+    total_votes = tally.values.sum
+
+    max_votes > total_votes / 2 || potential_winners.count == 1
+  end
+
+  def self.identify_losers(tally)
+    min_votes = tally.values.min
+
+    tally.select { |k, v| v == min_votes }.keys
   end
 
   def self.enrich(digest, options)
