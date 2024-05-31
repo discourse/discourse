e170a7a6a01a5b164d61c6055c58ce6a2ceb9f77
David Taylor <david@taylorhq.com>
2024-05-31 14:15:14 +0100

    DEV: Convert adapters to native class syntax

--- app/assets/javascripts/discourse/app/adapters/topic.js
+++ app/assets/javascripts/discourse/app/adapters/topic.js
@@ -1,12 +1,12 @@
 import RestAdapter from "discourse/adapters/rest";
 import { ajax } from "discourse/lib/ajax";
 
-export default RestAdapter.extend({
+export default class TopicAdapter extends RestAdapter {
   find(store, type, findArgs) {
     if (findArgs.similar) {
       return ajax("/topics/similar_to", { data: findArgs.similar });
     } else {
-      return this._super(store, type, findArgs);
+      return super.find(store, type, findArgs);
     }
-  },
-});
+  }
+}
