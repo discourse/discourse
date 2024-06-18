91742c6e3fc0540be199f332fc5e511d4c59c8f9
Loïc Guitaut <loic@discourse.org>
2024-06-18 16:41:58 +0200

    FIX: Remove references to `Rails.logger.chained`
    `Rails.logger.chained` was provided by Logster before Rails 7.1
    introduced their broadcast logger. Now all the loggers are added to
    `Rails.logger.broadcasts`.
    
    Some code in our initializers was still using `chained` instead of
    `broadcasts`.

--- config/initializers/100-logster.rb
+++ config/initializers/100-logster.rb
@@ -2,7 +2,7 @@
 
 if GlobalSetting.skip_redis?
   Rails.application.reloader.to_prepare do
-    Rails.logger = Rails.logger.chained.first if Rails.logger.respond_to? :chained
+    Rails.logger.stop_broadcasting_to(Logster.logger) if defined?(Logster::Logger) && Logster.logger
   end
   return
 end
@@ -118,10 +118,7 @@ RailsMultisite::ConnectionManagement.each_connection do
 end
 
 if Rails.configuration.multisite
-  if Rails.logger.respond_to? :chained
-    chained = Rails.logger.chained
-    chained && chained.first.formatter = RailsMultisite::Formatter.new
-  end
+  Rails.logger.broadcasts.first.formatter = RailsMultisite::Formatter.new
 end
 
 Logster.config.project_directories = [
