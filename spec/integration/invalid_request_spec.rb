ca4af53be8cb91aa3fe48ecbcf5a578189be32fa
Loïc Guitaut <loic@discourse.org>
2024-06-20 10:33:01 +0200

    DEV: Upgrade Rails to version 7.1 (#27539)
    * DEV: Upgrade Rails to 7.1
    
    * FIX: Remove references to `Rails.logger.chained`
    
    `Rails.logger.chained` was provided by Logster before Rails 7.1
    introduced their broadcast logger. Now all the loggers are added to
    `Rails.logger.broadcasts`.
    
    Some code in our initializers was still using `chained` instead of
    `broadcasts`.
    
    * DEV: Make parameters optional to all FakeLogger methods
    
    * FIX: Set `override_level` on Logster loggers (#27519)
    
    A followup to f595d599dd361b7fb39fb3c82cbc11d19d518c19
    
    * FIX: Don’t duplicate Rack response
    
    ---------
    
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>

--- spec/integration/invalid_request_spec.rb
+++ spec/integration/invalid_request_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe "invalid requests", type: :request do
-  before do
-    @orig_logger = Rails.logger
-    Rails.logger = @fake_logger = FakeLogger.new
-  end
+  let(:fake_logger) { FakeLogger.new }
 
-  after { Rails.logger = @orig_logger }
+  before { Rails.logger.broadcast_to(fake_logger) }
+
+  after { Rails.logger.stop_broadcasting_to(fake_logger) }
 
   it "handles NotFound with invalid json body" do
     post "/latest.json",
@@ -15,8 +14,8 @@ RSpec.describe "invalid requests", type: :request do
            "content-type" => "application/json",
          }
     expect(response.status).to eq(404)
-    expect(@fake_logger.warnings.length).to eq(0)
-    expect(@fake_logger.errors.length).to eq(0)
+    expect(fake_logger.warnings.length).to eq(0)
+    expect(fake_logger.errors.length).to eq(0)
   end
 
   it "handles EOFError when multipart request is malformed" do
@@ -27,14 +26,14 @@ RSpec.describe "invalid requests", type: :request do
            "content-length" => "1",
          }
     expect(response.status).to eq(400)
-    expect(@fake_logger.warnings.length).to eq(0)
-    expect(@fake_logger.errors.length).to eq(0)
+    expect(fake_logger.warnings.length).to eq(0)
+    expect(fake_logger.errors.length).to eq(0)
   end
 
   it "handles invalid parameters" do
     post "/latest.json", params: { "foo" => "\255bar" }
     expect(response.status).to eq(404)
-    expect(@fake_logger.warnings.length).to eq(0)
-    expect(@fake_logger.errors.length).to eq(0)
+    expect(fake_logger.warnings.length).to eq(0)
+    expect(fake_logger.errors.length).to eq(0)
   end
 end
