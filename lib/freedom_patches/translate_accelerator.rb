36b3ecf89ff01f70718d19b17d5a20ddefd68ee0
Renato Atilio <renato@discourse.org>
2024-06-21 11:49:53 -0300

    DEV: fix order between only_overriden and only_untranslated

--- lib/freedom_patches/translate_accelerator.rb
+++ lib/freedom_patches/translate_accelerator.rb
@@ -78,9 +78,7 @@ module I18n
       results = {}
       regexp = I18n::Backend::DiscourseI18n.create_search_regexp(query)
 
-      if opts[:only_overridden]
-        add_if_matches(overrides_by_locale(locale), results, regexp)
-      elsif opts[:only_untranslated]
+      if opts[:only_untranslated]
         target = opts[:backend] || backend
 
         target_strings = target.search(locale, query)
@@ -90,6 +88,8 @@ module I18n
         untranslated =
           original_strings.reject { |key, value| all_locale_strings.key?(key) || !value.present? }
         add_if_matches(untranslated, results, regexp)
+      elsif opts[:only_overridden]
+        add_if_matches(overrides_by_locale(locale), results, regexp)
       else
         target = opts[:backend] || backend
 
