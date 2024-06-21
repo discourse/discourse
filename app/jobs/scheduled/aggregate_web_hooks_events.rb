c018883f744e4490cb786ccc3fdd398c6f8d2e88
Gabriel Grubba <gabriel@discourse.org>
2024-06-21 11:05:57 -0300

    DEV: Address code review feedback.
    Add idempotency to job
    Add has_many to WebHook

--- app/jobs/scheduled/aggregate_web_hooks_events.rb
+++ app/jobs/scheduled/aggregate_web_hooks_events.rb
@@ -4,10 +4,16 @@ module Jobs
   class AggregateWebHooksEvents < ::Jobs::Scheduled
     every 1.day
 
-    def execute(args)
-      WebHook.all.each do |web_hook|
-        WebHookEventsDailyAggregate.create!(web_hook_id: web_hook.id, date: Time.zone.now.to_date)
-      end
+    def execute(args = {})
+      date = args[:date].present? ? Date.parse(args[:date]) : Time.zone.now.to_date
+      WebHook
+        .left_joins(:web_hook_events_daily_aggregates)
+        .where(active: true)
+        .where.not(web_hook_events_daily_aggregates: { date: date })
+        .distinct
+        .each do |web_hook|
+          WebHookEventsDailyAggregate.create!(web_hook_id: web_hook.id, date: date)
+        end
     end
   end
 end
