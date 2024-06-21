160011793ab2c9ac0a13f5eec467efc25e25435a
Loïc Guitaut <loic@discourse.org>
2024-06-21 10:54:03 +0200

    Revert "DEV: Upgrade Rails to version 7.1 (#27539)"
    This reverts commit ca4af53be8cb91aa3fe48ecbcf5a578189be32fa.

--- app/models/translation_override.rb
+++ app/models/translation_override.rb
@@ -45,7 +45,6 @@ class TranslationOverride < ActiveRecord::Base
 
   validate :check_interpolation_keys
 
-  attribute :status, :integer
   enum status: { up_to_date: 0, outdated: 1, invalid_interpolation_keys: 2, deprecated: 3 }
 
   def self.upsert!(locale, key, value)
