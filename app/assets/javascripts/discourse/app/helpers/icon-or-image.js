23ee97b2aa7166ed3f81d81bddf6475bea466f4b
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-06-27 20:00:47 +0200

    better fix

--- app/assets/javascripts/discourse/app/helpers/icon-or-image.js
+++ app/assets/javascripts/discourse/app/helpers/icon-or-image.js
@@ -1,10 +1,11 @@
+import { get } from "@ember/object";
 import { htmlSafe } from "@ember/template";
 import { isEmpty } from "@ember/utils";
 import { convertIconClass, iconHTML } from "discourse-common/lib/icon-library";
 
 export default function iconOrImage(badge) {
-  const icon = badge?.get?.("icon");
-  const image = badge?.get?.("image");
+  const icon = get(badge, "icon");
+  const image = get(badge, "image");
 
   if (!isEmpty(image)) {
     return htmlSafe(`<img src='${image}'>`);
