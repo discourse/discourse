7e748eb03bc876e5e46a73c7e56d824f97932e77
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-06-27 19:59:22 +0200

    fix badge-button test

--- app/assets/javascripts/discourse/app/helpers/icon-or-image.js
+++ app/assets/javascripts/discourse/app/helpers/icon-or-image.js
@@ -3,8 +3,8 @@ import { isEmpty } from "@ember/utils";
 import { convertIconClass, iconHTML } from "discourse-common/lib/icon-library";
 
 export default function iconOrImage(badge) {
-  const icon = badge.get("icon");
-  const image = badge.get("image");
+  const icon = badge?.get?.("icon");
+  const image = badge?.get?.("image");
 
   if (!isEmpty(image)) {
     return htmlSafe(`<img src='${image}'>`);
