d48150021fab44d1d2d702f95bb1c9d11569345d
Jarek Radosz <jradosz@gmail.com>
2024-07-05 21:09:07 +0200

    DEV: Use `async/await` in admin route `model` fns

--- app/assets/javascripts/admin/addon/routes/admin-customize-themes-show-schema.js
+++ app/assets/javascripts/admin/addon/routes/admin-customize-themes-show-schema.js
@@ -1,16 +1,16 @@
 import Route from "@ember/routing/route";
 
 export default class AdminCustomizeThemesShowSchemaRoute extends Route {
-  model(params) {
+  async model(params) {
     const theme = this.modelFor("adminCustomizeThemesShow");
     const setting = theme.settings.findBy("setting", params.setting_name);
 
-    return setting.loadMetadata(theme.id).then(() => {
-      return {
-        theme,
-        setting,
-      };
-    });
+    await setting.loadMetadata(theme.id);
+
+    return {
+      theme,
+      setting,
+    };
   }
 
   setupController() {
