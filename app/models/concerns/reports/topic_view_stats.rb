73a75e5c7c5146fa55e9adf78eb3d3ded842d869
Martin Brennan <martin@discourse.org>
2024-07-09 15:25:42 +1000

    DEV: Review fixes

--- app/models/concerns/reports/topic_view_stats.rb
+++ app/models/concerns/reports/topic_view_stats.rb
@@ -9,24 +9,21 @@ module Reports::TopicViewStats
 
       category_id, include_subcategories = report.add_category_filter
 
-      category_ids = include_subcategories ? Category.subcategory_ids(category_id) : category_id
+      category_ids = include_subcategories ? Category.subcategory_ids(category_id) : [category_id]
       sql = <<~SQL
-        SELECT view_stats.*, topics.title AS topic_title FROM
-          (
-            SELECT topic_id,
-            SUM(anonymous_views) AS total_anonymous_views,
-            SUM(logged_in_views) AS total_logged_in_views,
-            SUM(anonymous_views + logged_in_views) AS total_views
-            FROM topic_view_stats
-            INNER JOIN topics ON topics.id = topic_view_stats.topic_id
-            WHERE viewed_at >= :start_date AND viewed_at <= :end_date
-            #{category_ids.present? ? "AND topics.category_id IN (:category_ids)" : ""}
-            GROUP BY topic_id
-            ORDER BY total_views DESC
-            LIMIT 100
-          ) AS view_stats
-        INNER JOIN topics ON topics.id = view_stats.topic_id
-        ORDER BY view_stats.total_views DESC
+        SELECT
+          topic_view_stats.topic_id,
+          topics.title AS topic_title,
+          SUM(topic_view_stats.anonymous_views) AS total_anonymous_views,
+          SUM(topic_view_stats.logged_in_views) AS total_logged_in_views,
+          SUM(topic_view_stats.anonymous_views + topic_view_stats.logged_in_views) AS total_views
+        FROM topic_view_stats
+        INNER JOIN topics ON topics.id = topic_view_stats.topic_id
+        WHERE viewed_at BETWEEN :start_date AND :end_date
+        #{category_ids.any? ? "AND topics.category_id IN (:category_ids)" : ""}
+        GROUP BY topic_view_stats.topic_id, topics.title
+        ORDER BY total_views DESC
+        LIMIT 100
       SQL
 
       data =
@@ -63,16 +60,16 @@ module Reports::TopicViewStats
         },
       ]
 
-      report.data = []
-      data.each do |row|
-        report.data << {
-          topic_id: row.topic_id,
-          topic_title: row.topic_title,
-          total_anonymous_views: row.total_anonymous_views,
-          total_logged_in_views: row.total_logged_in_views,
-          total_views: row.total_views,
-        }
-      end
+      report.data =
+        data.map do |row|
+          {
+            topic_id: row.topic_id,
+            topic_title: row.topic_title,
+            total_anonymous_views: row.total_anonymous_views,
+            total_logged_in_views: row.total_logged_in_views,
+            total_views: row.total_views,
+          }
+        end
     end
   end
 end
