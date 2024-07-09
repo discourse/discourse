113c86afe880a7fe64e6f68f7a04e8db7e7733c8
Sérgio Saquetim <saquetim@discourse.org>
2024-07-08 21:05:06 -0300

    Silence the `template-action` notification

--- app/assets/javascripts/discourse/config/deprecation-workflow.js
+++ app/assets/javascripts/discourse/config/deprecation-workflow.js
@@ -7,15 +7,15 @@ globalThis.deprecationWorkflow.config = {
       handler: "silence",
       matchId: "ember-this-fallback.this-property-fallback",
     },
-    {
-      handler: "silence",
-      matchId: "template-action",
-    },
     { handler: "silence", matchId: "discourse.select-kit" },
     { handler: "silence", matchId: "discourse.d-section" },
     {
       handler: "silence",
       matchId: "discourse.decorate-widget.hamburger-widget-links",
+    },
+    {
+      handler: "silence",
+      matchId: "template-action",
     },
   ],
 };
