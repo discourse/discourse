7dda935c11b468d5ff151d09fcb1fe73169fc645
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-07-09 18:00:12 +0200

    faster spec

--- plugins/chat/spec/system/page_objects/chat/chat_channel.rb
+++ plugins/chat/spec/system/page_objects/chat/chat_channel.rb
@@ -35,8 +35,9 @@ module PageObjects
       end
 
       def click_composer
-        return if has_css?(".dialog-overlay") # we can't click composer if a dialog is open, in case of error for exampel
-        find(".chat-channel .chat-composer__input").click # ensures autocomplete is closed and not masking anything
+        if has_no_css?(".dialog-overlay") # we can't click composer if a dialog is open, in case of error for exampel
+          find(".chat-channel .chat-composer__input").click # ensures autocomplete is closed and not masking anything
+        end
       end
 
       def click_send_message
