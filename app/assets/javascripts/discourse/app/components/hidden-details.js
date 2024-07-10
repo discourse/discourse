67c3f294860f3e236a1cd882c3461335055ff122
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-07-10 16:05:45 +0200

    DEV: removes hidden-details component (#27833)
    Not used anywhere

--- app/assets/javascripts/discourse/app/components/hidden-details.js
+++
@@ -1,9 +0,0 @@
-import Component from "@ember/component";
-import { action } from "@ember/object";
-
-export default Component.extend({
-  @action
-  expand() {
-    this.set("expanded", true);
-  },
-});
