a8b72e66c11425c1fbd38d8cbff470f388a4706d
Lhc_fl <linca@discourse.org>
2024-07-17 16:20:42 +0800

    add test for frontend

--- app/assets/javascripts/discourse/tests/unit/lib/render-tag-test.js
+++ app/assets/javascripts/discourse/tests/unit/lib/render-tag-test.js
@@ -8,7 +8,7 @@ module("Unit | Utility | render-tag", function (hooks) {
   test("defaultRenderTag", function (assert) {
     assert.strictEqual(
       renderTag("foo", { description: "foo description" }),
-      "<a href='/tag/foo'  data-tag-name=foo title=\"foo description\"  class='discourse-tag simple'>foo</a>",
+      '<a href="/tag/foo" data-tag-name="foo" title="foo description" class="discourse-tag simple">foo</a>',
       "formats tag as link with plain description in hover"
     );
 
@@ -16,8 +16,22 @@ module("Unit | Utility | render-tag", function (hooks) {
       renderTag("foo", {
         description: 'foo description <a href="localhost">link</a>',
       }),
-      "<a href='/tag/foo'  data-tag-name=foo title=\"foo description link\"  class='discourse-tag simple'>foo</a>",
+      '<a href="/tag/foo" data-tag-name="foo" title="foo description link" class="discourse-tag simple">foo</a>',
       "removes any html tags from description"
+    );
+
+    assert.strictEqual(
+      renderTag("foo", { noHref: true }),
+      '<a data-tag-name="foo" class="discourse-tag simple">foo</a>',
+      "allows no href"
+    );
+
+    assert.strictEqual(
+      renderTag("foo", {
+        tagGroups: ["group1", "group2"],
+      }),
+      '<a href="/tag/foo" data-tag-name="foo" data-tag-groups="group1,group2" class="discourse-tag simple">foo</a>',
+      "adds the tag-groups to data"
     );
   });
 });
