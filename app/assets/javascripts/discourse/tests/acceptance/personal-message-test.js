3454b913c1be7dc5c5e99636e66562f60c29d88b
Jan Cernik <jancernik12@gmail.com>
2024-07-22 01:09:03 -0300

    More acceptance tests

--- app/assets/javascripts/discourse/tests/acceptance/personal-message-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/personal-message-test.js
@@ -91,7 +91,9 @@ acceptance("Personal Message - invite", function (needs) {
   test("can open invite modal", async function (assert) {
     await visit("/t/pm-for-testing/12");
     await click(".add-remove-participant-btn");
-    await click(".private-message-map .controls .add-participant-btn");
+    await click(
+      ".topic-map__private-message-map .controls .add-participant-btn"
+    );
 
     assert
       .dom(".d-modal.add-pm-participants .invite-user-control")
@@ -101,7 +103,9 @@ acceptance("Personal Message - invite", function (needs) {
   test("shows errors correctly", async function (assert) {
     await visit("/t/pm-for-testing/12");
     await click(".add-remove-participant-btn");
-    await click(".private-message-map .controls .add-participant-btn");
+    await click(
+      ".topic-map__private-message-map .controls .add-participant-btn"
+    );
 
     assert
       .dom(".d-modal.add-pm-participants .invite-user-control")
