633313017ff6471019d7ca40573d9a28c7c4633f
Gerhard Schlager <gerhard.schlager@discourse.org>
2024-07-24 00:08:27 +0200

    Switch to parallel

--- migrations/lib/converters/pepper/steps/step2.rb
+++ migrations/lib/converters/pepper/steps/step2.rb
@@ -2,6 +2,8 @@
 
 module Migrations::Converters::Pepper
   class Step2 < Migrations::Converters::Base::ProgressStep
+    run_in_parallel true
+
     def items
       [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     end
