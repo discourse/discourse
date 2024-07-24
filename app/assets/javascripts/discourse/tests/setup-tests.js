4b80a4714f923435c3bbff1952ba3d09adcdc0cc
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-07-24 09:41:38 +0200

    removed

--- app/assets/javascripts/discourse/tests/setup-tests.js
+++ app/assets/javascripts/discourse/tests/setup-tests.js
@@ -317,10 +317,6 @@ export default function setupTests(config) {
     resetPretender();
     clearPresenceState();
 
-    // Clean up the DOM. Some tests might leave extra classes or elements behind.
-    Array.from(document.getElementsByClassName("d-modal__backdrop")).forEach(
-      (e) => e.remove()
-    );
     document.body.removeAttribute("class");
     let html = document.documentElement;
     html.removeAttribute("class");
