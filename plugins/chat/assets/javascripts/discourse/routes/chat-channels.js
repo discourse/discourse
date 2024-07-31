633a19fcc09a41a31eadd176519483217b2ec241
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-07-31 14:12:10 -0300

    FIX: Remove chat default channel setting (#28170)
    * FIX: Remove chat default channel being applied to mobile chat and drawer
    
    * DEV: removing chat_default_channel_id setting
    
    * DEV: add migration to remove chat default channel id
    
    * DEV: remove default_channel_validator and tests

--- plugins/chat/assets/javascripts/discourse/routes/chat-channels.js
+++ plugins/chat/assets/javascripts/discourse/routes/chat-channels.js
@@ -12,30 +12,13 @@ export default class ChatChannelsRoute extends DiscourseRoute {
 
   beforeModel() {
     const id = this.currentUser.custom_fields.last_chat_channel_id;
-    const defaultChannelId = this.siteSettings.chat_default_channel_id;
     if (this.site.desktopView) {
       if (id) {
         this.chatChannelsManager.find(id).then((c) => {
           return this.router.replaceWith("chat.channel", ...c.routeModels);
         });
       } else {
-        // first time browsing chat in desktop and the preferred index is channels
-        if (defaultChannelId) {
-          this.chatChannelsManager.find(defaultChannelId).then((c) => {
-            return this.router.replaceWith("chat.channel", ...c.routeModels);
-          });
-        } else {
-          this.router.replaceWith("chat.browse.open");
-        }
-      }
-    } else {
-      if (
-        defaultChannelId &&
-        this.router.currentRoute?.parent?.params?.channelId !== defaultChannelId
-      ) {
-        this.chatChannelsManager.find(defaultChannelId).then((c) => {
-          return this.router.replaceWith("chat.channel", ...c.routeModels);
-        });
+        this.router.replaceWith("chat.browse.open");
       }
     }
   }
