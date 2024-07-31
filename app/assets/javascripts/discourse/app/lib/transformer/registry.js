7b14cd98c7e33b78ddc17be3152d4a252bed203a
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2024-07-31 16:39:22 -0300

    DEV: Add behavior transformers (#27409)
    This commit introduces the `behaviorTransformer` API to safely override behaviors defined in Discourse.
    
    Two new plugin APIs are introduced:
    
    - `addBehaviorTransformerName` which allows plugins and theme-components to add a new valid transformer name if they want to provide overridable behaviors;
    - `registerBehaviorTransformer` to register a transformer to override behaviors.
    
    It also introduces the function `applyBehaviorTransformer` which can be imported from `discourse/lib/transformer`. This is used to mark a callback containing the desired behavior as overridable and applies the transformer logic.
    
    How does it work?
    
    ## Marking a behavior as overridable:
     
    To mark a behavior as overridable, in Discourse core, first the transformer name must be added to `app/assets/javascripts/discourse/app/lib/transformer/registry.js`. For plugins and theme-components, use the plugin API `addBehaviorTransformerName` instead.
    
    Then, in your component or class, use the function `applyBehaviorTransformer` to mark the Behavior as overridable and handle the logic:
    
    - example:
    
    ```js
      ...
      @action
      loadMore() {
        applyBehaviorTransformer(
          "discovery-topic-list-load-more",
          () => {
            this.documentTitle.updateContextCount(0);
            return this.model
              .loadMore()
              .then(({ moreTopicsUrl, newTopics } = {}) => {
                if (
                  newTopics &&
                  newTopics.length &&
                  this.bulkSelectHelper?.bulkSelectEnabled
                ) {
                  this.bulkSelectHelper.addTopics(newTopics);
                }
                if (moreTopicsUrl && $(window).height() >= $(document).height()) {
                  this.send("loadMore");
                }
              });
          },
          { model: this.model }
        );
      },
      ...	
    ```
    
    ## Overriding a behavior in plugins or themes
    
    To override a behavior in plugins, themes, or TCs use the plugin API `registerBehaviorTransformer`:
    
    - Example:
    
    ```js
    withPluginApi("1.35.0", (api) => {
      api.registerBehaviorTransformer("example-transformer", ({ context, next }) => {
        console.log('we can introduce new behavior here instead', context);
    
        next(); // call next to execute the expected behavior
      });
    });
    ```

--- app/assets/javascripts/discourse/app/lib/transformer/registry.js
+++ app/assets/javascripts/discourse/app/lib/transformer/registry.js
@@ -1,3 +1,8 @@
+export const BEHAVIOR_TRANSFORMERS = Object.freeze([
+  // use only lowercase names
+  "discovery-topic-list-load-more",
+]);
+
 export const VALUE_TRANSFORMERS = Object.freeze([
   // use only lowercase names
   "header-notifications-avatar-size",
