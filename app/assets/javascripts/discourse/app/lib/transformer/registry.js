7b31f37cff8e2a47c65b75afa0bc4c2cd1aae382
Sérgio Saquetim <saquetim@discourse.org>
2024-07-31 16:03:15 -0300

    Removed transformer `route-application-activate`

--- app/assets/javascripts/discourse/app/lib/transformer/registry.js
+++ app/assets/javascripts/discourse/app/lib/transformer/registry.js
@@ -1,7 +1,6 @@
 export const BEHAVIOR_TRANSFORMERS = Object.freeze([
   // use only lowercase names
   "discovery-topic-list-load-more",
-  "route-application-activate",
 ]);
 
 export const VALUE_TRANSFORMERS = Object.freeze([
