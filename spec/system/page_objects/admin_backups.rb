2893ec620f9a4d1305cf0d23d7bdb6c3dd927e48
Martin Brennan <martin@discourse.org>
2024-08-02 08:15:19 +1000

    WIP: Backups system spec

---
+++ spec/system/page_objects/admin_backups.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Pages
+    class AdminBackups < PageObjects::Pages::Base
+      def visit_page
+        page.visit "/admin/backups"
+        self
+      end
+
+      def has_backup_listed?(filename)
+        page.has_css?(backup_row_selector(filename))
+      end
+
+      def has_no_backup_listed?(filename)
+        page.has_no_css?(backup_row_selector(filename))
+      end
+
+      def download_backup(filename)
+        find_backup_row(filename).find(".backup-item-row__download").click
+      end
+
+      def delete_backup(filename)
+        find_backup_row(filename).find(".backup-item-menu-trigger").click
+        find(".backup-item-menu-content").find(".backup-item-row__delete").click
+      end
+
+      def find_backup_row(filename)
+        find(backup_row_selector(filename))
+      end
+
+      def backup_row_selector(filename)
+        ".admin-backups-list .backup-item-row[data-backup-filename='#{filename}']"
+      end
+
+      def toggle_read_only
+        find(".admin-backups__toggle-read-only").click
+      end
+    end
+  end
+end
