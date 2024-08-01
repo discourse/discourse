1c84d7c209bae4dbe782ef986c6f317724fc72ea
Martin Brennan <martin@discourse.org>
2024-08-01 23:36:17 +1000

    FIX: Badge image uploader (#28188)
    In the formkit conversion in 2ca06ba2360c200b2be8e0718fcc04c64ca14935
    we missed setting a type for the UppyImageUploader for badges. Also,
    we were not passing down the `image_url` as form data, so when we used
    `data.image` for that field the badge was not updating in the UI after
    page loads and the image URL was not loading for preview.
    
    Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>

--- spec/system/page_objects/components/form_kit.rb
+++ spec/system/page_objects/components/form_kit.rb
@@ -2,6 +2,22 @@
 
 module PageObjects
   module Components
+    class FormKitContainer < PageObjects::Components::Base
+      attr_reader :component
+
+      def initialize(input)
+        if input.is_a?(Capybara::Node::Element)
+          @component = input
+        else
+          @component = find(input)
+        end
+      end
+
+      def has_content?(content)
+        component.has_content?(content)
+      end
+    end
+
     class FormKitField < PageObjects::Components::Base
       attr_reader :component
 
@@ -165,6 +181,16 @@ module PageObjects
         within component do
           FormKitField.new(find(".form-kit__field[data-name='#{name}']"))
         end
+      end
+
+      def container(name)
+        within component do
+          FormKitContainer.new(find(".form-kit__container[data-name='#{name}']"))
+        end
+      end
+
+      def choose_conditional(name)
+        find(".form-kit__conditional-display .form-kit__control-radio[value='#{name}']").click
       end
     end
   end
