56c042b5521e144ffed89e2ca73d4c2d61e5f30a
Jarek Radosz <jradosz@gmail.com>
2024-08-05 16:26:34 +0200

    export config

--- app/assets/javascripts/discourse/app/deprecation-workflow.js
+++ app/assets/javascripts/discourse/app/deprecation-workflow.js
@@ -1,18 +1,22 @@
 import setupDeprecationWorkflow from "ember-cli-deprecation-workflow";
 
+const DEPRECATION_WORKFLOW = [
+  {
+    handler: "silence",
+    matchId: "ember-this-fallback.this-property-fallback",
+  },
+  { handler: "silence", matchId: "discourse.select-kit" },
+  { handler: "silence", matchId: "discourse.d-section" },
+  {
+    handler: "silence",
+    matchId: "discourse.decorate-widget.hamburger-widget-links",
+  },
+];
+
 setupDeprecationWorkflow({
   // We're using RAISE_ON_DEPRECATION in environment.js instead of
   // `throwOnUnhandled` here since it is easier to toggle.
-  workflow: [
-    {
-      handler: "silence",
-      matchId: "ember-this-fallback.this-property-fallback",
-    },
-    { handler: "silence", matchId: "discourse.select-kit" },
-    { handler: "silence", matchId: "discourse.d-section" },
-    {
-      handler: "silence",
-      matchId: "discourse.decorate-widget.hamburger-widget-links",
-    },
-  ],
+  workflow: DEPRECATION_WORKFLOW,
 });
+
+export default DEPRECATION_WORKFLOW;
