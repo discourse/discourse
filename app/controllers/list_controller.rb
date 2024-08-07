e494bafed38f8e9629bc112df02da28e6dbdcc75
Loïc Guitaut <loic@discourse.org>
2024-08-07 11:58:40 +0200

    FIX: Unescape URI properly when redirecting to a category
    Currently, when a badly named category slug is provided, it can lead to
    an infinite redirect.
    
    This patch addresses the issue by properly unescaping `request.fullpath`
    so the path is successfully rewritten and the redirect happens as
    expected.

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -421,7 +421,7 @@ class ListController < ApplicationController
     end
     real_slug = @category.full_slug("/")
     if CGI.unescape(current_slug) != CGI.unescape(real_slug)
-      url = request.fullpath.gsub(current_slug, real_slug)
+      url = CGI.unescape(request.fullpath).gsub(current_slug, real_slug)
       if ActionController::Base.config.relative_url_root
         url = url.sub(ActionController::Base.config.relative_url_root, "")
       end
