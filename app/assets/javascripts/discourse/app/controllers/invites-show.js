75d11bfeba8ab3caf8450b61bbe5cbd079a52016
David Taylor <david@taylorhq.com>
2024-08-07 14:48:29 +0100

    DEV: Convert core controllers to native class syntax (batch 3) (#28224)
    Changes made using the ember-native-class-codemod, plus some manual tweaks

--- app/assets/javascripts/discourse/app/controllers/invites-show.js
+++ app/assets/javascripts/discourse/app/controllers/invites-show.js
@@ -16,368 +16,362 @@ import getUrl from "discourse-common/lib/get-url";
 import discourseComputed from "discourse-common/utils/decorators";
 import I18n from "discourse-i18n";
 
-export default Controller.extend(
+export default class InvitesShowController extends Controller.extend(
   PasswordValidation,
   UsernameValidation,
   NameValidation,
-  UserFieldsValidation,
-  {
-    queryParams: ["t"],
+  UserFieldsValidation
+) {
+  queryParams = ["t"];
 
-    invitedBy: readOnly("model.invited_by"),
-    email: alias("model.email"),
-    accountEmail: alias("email"),
-    existingUserId: readOnly("model.existing_user_id"),
-    existingUserCanRedeem: readOnly("model.existing_user_can_redeem"),
-    existingUserCanRedeemError: readOnly(
-      "model.existing_user_can_redeem_error"
-    ),
-    existingUserRedeeming: bool("existingUserId"),
-    hiddenEmail: alias("model.hidden_email"),
-    emailVerifiedByLink: alias("model.email_verified_by_link"),
-    differentExternalEmail: alias("model.different_external_email"),
-    accountUsername: alias("model.username"),
-    passwordRequired: not("externalAuthsOnly"),
-    successMessage: null,
-    errorMessage: null,
-    userFields: null,
-    authOptions: null,
-    inviteImageUrl: getUrl("/images/envelope.svg"),
-    isInviteLink: readOnly("model.is_invite_link"),
-    rejectedEmails: null,
-    maskPassword: true,
+  @readOnly("model.invited_by") invitedBy;
+  @alias("model.email") email;
+  @alias("email") accountEmail;
+  @readOnly("model.existing_user_id") existingUserId;
+  @readOnly("model.existing_user_can_redeem") existingUserCanRedeem;
+  @readOnly("model.existing_user_can_redeem_error") existingUserCanRedeemError;
+  @bool("existingUserId") existingUserRedeeming;
+  @alias("model.hidden_email") hiddenEmail;
+  @alias("model.email_verified_by_link") emailVerifiedByLink;
+  @alias("model.different_external_email") differentExternalEmail;
+  @alias("model.username") accountUsername;
+  @not("externalAuthsOnly") passwordRequired;
+  @readOnly("model.is_invite_link") isInviteLink;
 
-    init() {
-      this._super(...arguments);
+  successMessage = null;
+  errorMessage = null;
+  userFields = null;
+  authOptions = null;
+  inviteImageUrl = getUrl("/images/envelope.svg");
+  rejectedEmails = [];
+  maskPassword = true;
 
-      this.rejectedEmails = [];
-    },
+  authenticationComplete(options) {
+    const props = {
+      accountUsername: options.username,
+      accountName: options.name,
+      authOptions: EmberObject.create(options),
+    };
 
-    authenticationComplete(options) {
-      const props = {
-        accountUsername: options.username,
-        accountName: options.name,
-        authOptions: EmberObject.create(options),
-      };
+    if (this.isInviteLink) {
+      props.email = options.email;
+    }
 
-      if (this.isInviteLink) {
-        props.email = options.email;
-      }
+    this.setProperties(props);
+  }
 
-      this.setProperties(props);
-    },
+  @discourseComputed
+  discourseConnectEnabled() {
+    return this.siteSettings.enable_discourse_connect;
+  }
 
-    @discourseComputed
-    discourseConnectEnabled() {
-      return this.siteSettings.enable_discourse_connect;
-    },
+  @discourseComputed
+  welcomeTitle() {
+    return I18n.t("invites.welcome_to", {
+      site_name: this.siteSettings.title,
+    });
+  }
 
-    @discourseComputed
-    welcomeTitle() {
-      return I18n.t("invites.welcome_to", {
-        site_name: this.siteSettings.title,
+  @discourseComputed("existingUserId")
+  subheaderMessage(existingUserId) {
+    if (existingUserId) {
+      return I18n.t("invites.existing_user_can_redeem");
+    } else {
+      return I18n.t("create_account.subheader_title");
+    }
+  }
+
+  @discourseComputed("email")
+  yourEmailMessage(email) {
+    return I18n.t("invites.your_email", { email });
+  }
+
+  @discourseComputed
+  externalAuthsEnabled() {
+    return findLoginMethods().length > 0;
+  }
+
+  @discourseComputed
+  externalAuthsOnly() {
+    return (
+      !this.siteSettings.enable_local_logins &&
+      this.externalAuthsEnabled &&
+      !this.siteSettings.enable_discourse_connect
+    );
+  }
+
+  @discourseComputed(
+    "emailValidation.failed",
+    "usernameValidation.failed",
+    "passwordValidation.failed",
+    "nameValidation.failed",
+    "userFieldsValidation.failed",
+    "existingUserRedeeming",
+    "existingUserCanRedeem"
+  )
+  submitDisabled(
+    emailValidationFailed,
+    usernameValidationFailed,
+    passwordValidationFailed,
+    nameValidationFailed,
+    userFieldsValidationFailed,
+    existingUserRedeeming,
+    existingUserCanRedeem
+  ) {
+    if (existingUserRedeeming) {
+      return !existingUserCanRedeem;
+    }
+
+    return (
+      emailValidationFailed ||
+      usernameValidationFailed ||
+      passwordValidationFailed ||
+      nameValidationFailed ||
+      userFieldsValidationFailed
+    );
+  }
+
+  @discourseComputed(
+    "externalAuthsEnabled",
+    "externalAuthsOnly",
+    "discourseConnectEnabled"
+  )
+  showSocialLoginAvailable(
+    externalAuthsEnabled,
+    externalAuthsOnly,
+    discourseConnectEnabled
+  ) {
+    return (
+      externalAuthsEnabled && !externalAuthsOnly && !discourseConnectEnabled
+    );
+  }
+
+  @discourseComputed(
+    "externalAuthsOnly",
+    "authOptions",
+    "emailValidation.failed",
+    "existingUserRedeeming"
+  )
+  shouldDisplayForm(
+    externalAuthsOnly,
+    authOptions,
+    emailValidationFailed,
+    existingUserRedeeming
+  ) {
+    return (
+      (this.siteSettings.enable_local_logins ||
+        (externalAuthsOnly && authOptions && !emailValidationFailed)) &&
+      !this.siteSettings.enable_discourse_connect &&
+      !existingUserRedeeming
+    );
+  }
+
+  @discourseComputed
+  fullnameRequired() {
+    return (
+      this.siteSettings.full_name_required || this.siteSettings.enable_names
+    );
+  }
+
+  @discourseComputed(
+    "email",
+    "rejectedEmails.[]",
+    "authOptions.email",
+    "authOptions.email_valid",
+    "hiddenEmail",
+    "emailVerifiedByLink",
+    "differentExternalEmail"
+  )
+  emailValidation(
+    email,
+    rejectedEmails,
+    externalAuthEmail,
+    externalAuthEmailValid,
+    hiddenEmail,
+    emailVerifiedByLink,
+    differentExternalEmail
+  ) {
+    if (hiddenEmail && !differentExternalEmail) {
+      return EmberObject.create({
+        ok: true,
+        reason: I18n.t("user.email.ok"),
       });
-    },
+    }
 
-    @discourseComputed("existingUserId")
-    subheaderMessage(existingUserId) {
-      if (existingUserId) {
-        return I18n.t("invites.existing_user_can_redeem");
-      } else {
-        return I18n.t("create_account.subheader_title");
-      }
-    },
+    // If blank, fail without a reason
+    if (isEmpty(email)) {
+      return EmberObject.create({
+        failed: true,
+      });
+    }
 
-    @discourseComputed("email")
-    yourEmailMessage(email) {
-      return I18n.t("invites.your_email", { email });
-    },
-
-    @discourseComputed
-    externalAuthsEnabled() {
-      return findLoginMethods().length > 0;
-    },
-
-    @discourseComputed
-    externalAuthsOnly() {
-      return (
-        !this.siteSettings.enable_local_logins &&
-        this.externalAuthsEnabled &&
-        !this.siteSettings.enable_discourse_connect
-      );
-    },
-
-    @discourseComputed(
-      "emailValidation.failed",
-      "usernameValidation.failed",
-      "passwordValidation.failed",
-      "nameValidation.failed",
-      "userFieldsValidation.failed",
-      "existingUserRedeeming",
-      "existingUserCanRedeem"
-    )
-    submitDisabled(
-      emailValidationFailed,
-      usernameValidationFailed,
-      passwordValidationFailed,
-      nameValidationFailed,
-      userFieldsValidationFailed,
-      existingUserRedeeming,
-      existingUserCanRedeem
-    ) {
-      if (existingUserRedeeming) {
-        return !existingUserCanRedeem;
-      }
-
-      return (
-        emailValidationFailed ||
-        usernameValidationFailed ||
-        passwordValidationFailed ||
-        nameValidationFailed ||
-        userFieldsValidationFailed
-      );
-    },
-
-    @discourseComputed(
-      "externalAuthsEnabled",
-      "externalAuthsOnly",
-      "discourseConnectEnabled"
-    )
-    showSocialLoginAvailable(
-      externalAuthsEnabled,
-      externalAuthsOnly,
-      discourseConnectEnabled
-    ) {
-      return (
-        externalAuthsEnabled && !externalAuthsOnly && !discourseConnectEnabled
-      );
-    },
-
-    @discourseComputed(
-      "externalAuthsOnly",
-      "authOptions",
-      "emailValidation.failed",
-      "existingUserRedeeming"
-    )
-    shouldDisplayForm(
-      externalAuthsOnly,
-      authOptions,
-      emailValidationFailed,
-      existingUserRedeeming
-    ) {
-      return (
-        (this.siteSettings.enable_local_logins ||
-          (externalAuthsOnly && authOptions && !emailValidationFailed)) &&
-        !this.siteSettings.enable_discourse_connect &&
-        !existingUserRedeeming
-      );
-    },
-
-    @discourseComputed
-    fullnameRequired() {
-      return (
-        this.siteSettings.full_name_required || this.siteSettings.enable_names
-      );
-    },
-
-    @discourseComputed(
-      "email",
-      "rejectedEmails.[]",
-      "authOptions.email",
-      "authOptions.email_valid",
-      "hiddenEmail",
-      "emailVerifiedByLink",
-      "differentExternalEmail"
-    )
-    emailValidation(
-      email,
-      rejectedEmails,
-      externalAuthEmail,
-      externalAuthEmailValid,
-      hiddenEmail,
-      emailVerifiedByLink,
-      differentExternalEmail
-    ) {
-      if (hiddenEmail && !differentExternalEmail) {
-        return EmberObject.create({
-          ok: true,
-          reason: I18n.t("user.email.ok"),
-        });
-      }
-
-      // If blank, fail without a reason
-      if (isEmpty(email)) {
-        return EmberObject.create({
-          failed: true,
-        });
-      }
-
-      if (rejectedEmails.includes(email)) {
-        return EmberObject.create({
-          failed: true,
-          reason: I18n.t("user.email.invalid"),
-        });
-      }
-
-      if (externalAuthEmail && externalAuthEmailValid) {
-        const provider = this.authProviderDisplayName(
-          this.get("authOptions.auth_provider")
-        );
-
-        if (externalAuthEmail === email) {
-          return EmberObject.create({
-            ok: true,
-            reason: I18n.t("user.email.authenticated", {
-              provider,
-            }),
-          });
-        } else {
-          return EmberObject.create({
-            failed: true,
-            reason: I18n.t("user.email.invite_auth_email_invalid", {
-              provider,
-            }),
-          });
-        }
-      }
-
-      if (emailVerifiedByLink) {
-        return EmberObject.create({
-          ok: true,
-          reason: I18n.t("user.email.authenticated_by_invite"),
-        });
-      }
-
-      if (emailValid(email)) {
-        return EmberObject.create({
-          ok: true,
-          reason: I18n.t("user.email.ok"),
-        });
-      }
-
+    if (rejectedEmails.includes(email)) {
       return EmberObject.create({
         failed: true,
         reason: I18n.t("user.email.invalid"),
       });
-    },
+    }
 
-    authProviderDisplayName(providerName) {
-      const matchingProvider = findLoginMethods().find((provider) => {
-        return provider.name === providerName;
-      });
-      return matchingProvider
-        ? matchingProvider.get("prettyName")
-        : providerName;
-    },
+    if (externalAuthEmail && externalAuthEmailValid) {
+      const provider = this.authProviderDisplayName(
+        this.get("authOptions.auth_provider")
+      );
 
-    @discourseComputed
-    wavingHandURL: () => wavingHandURL(),
-
-    @discourseComputed
-    ssoPath: () => getUrl("/session/sso"),
-
-    @discourseComputed
-    disclaimerHtml() {
-      if (this.site.tos_url && this.site.privacy_policy_url) {
-        return I18n.t("create_account.disclaimer", {
-          tos_link: this.site.tos_url,
-          privacy_link: this.site.privacy_policy_url,
+      if (externalAuthEmail === email) {
+        return EmberObject.create({
+          ok: true,
+          reason: I18n.t("user.email.authenticated", {
+            provider,
+          }),
+        });
+      } else {
+        return EmberObject.create({
+          failed: true,
+          reason: I18n.t("user.email.invite_auth_email_invalid", {
+            provider,
+          }),
         });
       }
-    },
+    }
 
-    @discourseComputed("authOptions.associate_url", "authOptions.auth_provider")
-    associateHtml(url, provider) {
-      if (!url) {
-        return;
-      }
-      return I18n.t("create_account.associate", {
-        associate_link: url,
-        provider: I18n.t(`login.${provider}.name`),
+    if (emailVerifiedByLink) {
+      return EmberObject.create({
+        ok: true,
+        reason: I18n.t("user.email.authenticated_by_invite"),
       });
-    },
+    }
 
-    @action
-    togglePasswordMask() {
-      this.toggleProperty("maskPassword");
-    },
+    if (emailValid(email)) {
+      return EmberObject.create({
+        ok: true,
+        reason: I18n.t("user.email.ok"),
+      });
+    }
 
-    actions: {
-      submit() {
-        const userFields = this.userFields;
-        let userCustomFields = {};
-        if (!isEmpty(userFields)) {
-          userFields.forEach(function (f) {
-            userCustomFields[f.get("field.id")] = f.get("value");
-          });
-        }
-
-        const data = {
-          username: this.accountUsername,
-          name: this.accountName,
-          password: this.accountPassword,
-          user_custom_fields: userCustomFields,
-          timezone: moment.tz.guess(),
-        };
-
-        if (this.isInviteLink) {
-          data.email = this.email;
-        } else {
-          data.email_token = this.t;
-        }
-
-        ajax({
-          url: `/invites/show/${this.get("model.token")}.json`,
-          type: "PUT",
-          data,
-        })
-          .then((result) => {
-            if (result.success) {
-              this.set(
-                "successMessage",
-                result.message || I18n.t("invites.success")
-              );
-              if (result.redirect_to) {
-                DiscourseURL.redirectTo(result.redirect_to);
-              }
-            } else {
-              if (
-                result.errors &&
-                result.errors.email &&
-                result.errors.email.length > 0 &&
-                result.values
-              ) {
-                this.rejectedEmails.pushObject(result.values.email);
-              }
-              if (
-                result.errors &&
-                result.errors.password &&
-                result.errors.password.length > 0
-              ) {
-                this.rejectedPasswords.pushObject(this.accountPassword);
-                this.rejectedPasswordsMessages.set(
-                  this.accountPassword,
-                  result.errors.password[0]
-                );
-              }
-              if (result.message) {
-                this.set("errorMessage", result.message);
-              }
-            }
-          })
-          .catch((error) => {
-            this.set("errorMessage", extractError(error));
-          });
-      },
-
-      externalLogin(provider) {
-        provider.doLogin({
-          signup: true,
-          params: {
-            origin: window.location.href,
-          },
-        });
-      },
-    },
+    return EmberObject.create({
+      failed: true,
+      reason: I18n.t("user.email.invalid"),
+    });
   }
-);
+
+  authProviderDisplayName(providerName) {
+    const matchingProvider = findLoginMethods().find((provider) => {
+      return provider.name === providerName;
+    });
+    return matchingProvider ? matchingProvider.get("prettyName") : providerName;
+  }
+
+  @discourseComputed
+  wavingHandURL() {
+    return wavingHandURL();
+  }
+
+  @discourseComputed
+  ssoPath() {
+    return getUrl("/session/sso");
+  }
+
+  @discourseComputed
+  disclaimerHtml() {
+    if (this.site.tos_url && this.site.privacy_policy_url) {
+      return I18n.t("create_account.disclaimer", {
+        tos_link: this.site.tos_url,
+        privacy_link: this.site.privacy_policy_url,
+      });
+    }
+  }
+
+  @discourseComputed("authOptions.associate_url", "authOptions.auth_provider")
+  associateHtml(url, provider) {
+    if (!url) {
+      return;
+    }
+    return I18n.t("create_account.associate", {
+      associate_link: url,
+      provider: I18n.t(`login.${provider}.name`),
+    });
+  }
+
+  @action
+  togglePasswordMask() {
+    this.toggleProperty("maskPassword");
+  }
+
+  @action
+  submit() {
+    const userFields = this.userFields;
+    let userCustomFields = {};
+    if (!isEmpty(userFields)) {
+      userFields.forEach(function (f) {
+        userCustomFields[f.get("field.id")] = f.get("value");
+      });
+    }
+
+    const data = {
+      username: this.accountUsername,
+      name: this.accountName,
+      password: this.accountPassword,
+      user_custom_fields: userCustomFields,
+      timezone: moment.tz.guess(),
+    };
+
+    if (this.isInviteLink) {
+      data.email = this.email;
+    } else {
+      data.email_token = this.t;
+    }
+
+    ajax({
+      url: `/invites/show/${this.get("model.token")}.json`,
+      type: "PUT",
+      data,
+    })
+      .then((result) => {
+        if (result.success) {
+          this.set(
+            "successMessage",
+            result.message || I18n.t("invites.success")
+          );
+          if (result.redirect_to) {
+            DiscourseURL.redirectTo(result.redirect_to);
+          }
+        } else {
+          if (
+            result.errors &&
+            result.errors.email &&
+            result.errors.email.length > 0 &&
+            result.values
+          ) {
+            this.rejectedEmails.pushObject(result.values.email);
+          }
+          if (
+            result.errors &&
+            result.errors.password &&
+            result.errors.password.length > 0
+          ) {
+            this.rejectedPasswords.pushObject(this.accountPassword);
+            this.rejectedPasswordsMessages.set(
+              this.accountPassword,
+              result.errors.password[0]
+            );
+          }
+          if (result.message) {
+            this.set("errorMessage", result.message);
+          }
+        }
+      })
+      .catch((error) => {
+        this.set("errorMessage", extractError(error));
+      });
+  }
+
+  @action
+  externalLogin(provider) {
+    provider.doLogin({
+      signup: true,
+      params: {
+        origin: window.location.href,
+      },
+    });
+  }
+}
--- app/assets/javascripts/discourse/app/controllers/review-settings.js
+++ app/assets/javascripts/discourse/app/controllers/review-settings.js
@@ -1,32 +1,32 @@
 import Controller from "@ember/controller";
+import { action } from "@ember/object";
 import { ajax } from "discourse/lib/ajax";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import discourseComputed from "discourse-common/utils/decorators";
 import I18n from "discourse-i18n";
 
-export default Controller.extend({
-  saving: false,
-  saved: false,
+export default class ReviewSettingsController extends Controller {
+  saving = false;
+  saved = false;
 
-  actions: {
-    save() {
-      let priorities = {};
-      this.scoreTypes.forEach((st) => {
-        priorities[st.id] = parseFloat(st.reviewable_priority);
-      });
+  @action
+  save() {
+    let priorities = {};
+    this.scoreTypes.forEach((st) => {
+      priorities[st.id] = parseFloat(st.reviewable_priority);
+    });
 
-      this.set("saving", true);
-      ajax("/review/settings", {
-        type: "PUT",
-        data: { reviewable_priorities: priorities },
+    this.set("saving", true);
+    ajax("/review/settings", {
+      type: "PUT",
+      data: { reviewable_priorities: priorities },
+    })
+      .then(() => {
+        this.set("saved", true);
       })
-        .then(() => {
-          this.set("saved", true);
-        })
-        .catch(popupAjaxError)
-        .finally(() => this.set("saving", false));
-    },
-  },
+      .catch(popupAjaxError)
+      .finally(() => this.set("saving", false));
+  }
 
   @discourseComputed("settings.reviewable_score_types")
   scoreTypes(types) {
@@ -36,5 +36,5 @@ export default Controller.extend({
       ...type,
       title: type.title.replace("%{username}", username),
     }));
-  },
-});
+  }
+}
