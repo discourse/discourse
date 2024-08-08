0221b3cd9a4d623e244c7d78ec8876e19f21cf36
Penar Musaraj <pmusaraj@gmail.com>
2024-08-08 11:59:22 -0400

    Update plugins/discourse-narrative-bot/spec/system/user_spec.rb
    Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>

--- plugins/discourse-narrative-bot/spec/system/user_spec.rb
+++ plugins/discourse-narrative-bot/spec/system/user_spec.rb
@@ -22,7 +22,7 @@ describe "Narrative Bot PM", type: :system do
       tooltip = PageObjects::Components::Tooltips.new("user-tip")
       tooltip.find(".btn", text: "Skip tips").click
 
-      expect(page).to have_no_css(".fk-d-tooltip__content .user-tip__title")
+      expect(tooltip).to be_not_present
       expect(page).to have_css(".badge-notification.new-pms")
 
       find("#toggle-current-user").click
