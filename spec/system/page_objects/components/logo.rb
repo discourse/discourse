d346f65c3d28d77861141f4cc34537f65f4aff90
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-08-08 12:44:38 +0200

    oops

--- spec/system/page_objects/components/logo.rb
+++ spec/system/page_objects/components/logo.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module PageObjects
-  module Pages
-    class Components < PageObjects::Pages::Base
+  module Components
+    class Logo < PageObjects::Pages::Base
       SELECTOR = "#site-logo"
 
       def self.click
