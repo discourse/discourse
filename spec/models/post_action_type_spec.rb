15a53e960c4590b83dde1521702d02d57ee2f14c
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2024-08-08 13:25:05 +1000

    FIX: first part of improvements

--- spec/models/post_action_type_spec.rb
+++ spec/models/post_action_type_spec.rb
@@ -4,17 +4,15 @@ RSpec.describe PostActionType do
   describe "Callbacks" do
     describe "#expiry_cache" do
       it "should clear the cache on save" do
-        cache = ApplicationSerializer.fragment_cache
-
-        cache["post_action_types_#{I18n.locale}"] = "test"
-        cache["post_action_flag_types_#{I18n.locale}"] = "test2"
+        Discourse.cache.write("post_action_types_#{I18n.locale}", "test")
+        Discourse.cache.write("post_action_flag_types_#{I18n.locale}", "test2")
 
         PostActionType.new(name_key: "some_key").save!
 
-        expect(cache["post_action_types_#{I18n.locale}"]).to eq(nil)
-        expect(cache["post_action_flag_types_#{I18n.locale}"]).to eq(nil)
+        expect(Discourse.cache.read("post_action_types_#{I18n.locale}")).to eq(nil)
+        expect(Discourse.cache.read("post_action_flag_types_#{I18n.locale}")).to eq(nil)
       ensure
-        ApplicationSerializer.fragment_cache.clear
+        PostActionType.new.expire_cache
       end
     end
   end
