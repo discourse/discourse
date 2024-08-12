f9318eb56f5a7afbf8d06624cdbd7382d20cdc3b
overgrow <admin@overgrow.com>
2024-08-12 10:47:09 +0200

    Removed unnecessary admin setting

--- lib/disk_space.rb
+++ lib/disk_space.rb
@@ -2,11 +2,7 @@
 
 class DiskSpace
   def self.uploads_used_bytes
-    if Discourse.store.external? || !SiteSetting.enable_precise_upload_used_bytes?
-      Upload.sum(:filesize).to_i + OptimizedImage.sum(:filesize).to_i
-    else
-      used(uploads_path)
-    end
+    Upload.sum(:filesize).to_i + OptimizedImage.sum(:filesize).to_i
   end
 
   def self.uploads_free_bytes
