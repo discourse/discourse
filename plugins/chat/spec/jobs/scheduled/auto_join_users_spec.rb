015c64fbc991d3b29282b05a36908c7c0cee083b
Sam Saffron <sam.saffron@gmail.com>
2024-08-16 09:18:34 +1000

    PERF: automatically join users to channels more efficiently
    - Only ever auto join 10k users to channels (ordered by last seen)
    - Join users to all channels at once, instead of batching and splitting

--- plugins/chat/spec/jobs/scheduled/auto_join_users_spec.rb
+++ plugins/chat/spec/jobs/scheduled/auto_join_users_spec.rb
@@ -3,8 +3,18 @@
 describe Jobs::Chat::AutoJoinUsers do
   subject(:job) { described_class.new }
 
+  before { Jobs.run_immediately! }
+
   it "works" do
-    Jobs.run_immediately!
+    _staged_user = Fabricate(:user, staged: true)
+    _suspended_user = Fabricate(:user, suspended_till: 1.day.from_now)
+    _inactive_user = Fabricate(:user, active: false)
+
+    # this is just to avoid test fragility, we should always have negative users
+    bot_id = (User.minimum(:id) - 1)
+    bot_id = -1 if bot_id > 0
+    _bot_user = Fabricate(:user, id: bot_id)
+
     channel = Fabricate(:category_channel, auto_join_users: true)
     user = Fabricate(:user, last_seen_at: 1.minute.ago, active: true)
 
@@ -13,7 +23,25 @@ describe Jobs::Chat::AutoJoinUsers do
 
     job.execute({})
 
+    # should exclude bot / inactive / staged / suspended users
+    expect(Chat::UserChatChannelMembership.where(chat_channel: channel).count).to eq(
+      User.real.not_suspended.not_staged.where(active: true).count,
+    )
+
     membership = Chat::UserChatChannelMembership.find_by(user: user, chat_channel: channel)
     expect(membership.following).to eq(true)
+
+    membership.update!(following: false)
+    job.execute({})
+
+    membership.reload
+    expect(membership.following).to eq(false)
+
+    channel = Fabricate(:category_channel, auto_join_users: false)
+    job.execute({})
+
+    membership = Chat::UserChatChannelMembership.find_by(user: user, chat_channel: channel)
+
+    expect(membership).to be_nil
   end
 end
