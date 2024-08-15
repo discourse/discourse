299934e33b9bc097dd4d3b0617d47d869801e9c9
awesomerobot <kris.aubuchon@discourse.org>
2024-08-15 10:14:07 -0400

    return false instead of null

--- app/assets/javascripts/discourse-markdown-it/src/features/image-controls.js
+++ app/assets/javascripts/discourse-markdown-it/src/features/image-controls.js
@@ -31,11 +31,9 @@ function isUpload(token) {
 }
 
 function hasMetadata(token) {
-  return (
-    token.content
-      .split("|")
-      .find((part) => /^\d{1,4}x\d{1,4}(,\s*\d{1,3}%)?$/.test(part)) || null
-  );
+  return !!token.content
+    .split("|")
+    .find((part) => /^\d{1,4}x\d{1,4}(,\s*\d{1,3}%)?$/.test(part));
 }
 
 function appendMetaData(index, token) {
