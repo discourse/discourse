bb9dfe520f3bdf4ee1073693313f1a8c6756a32e
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-08-17 00:01:48 +0200

    linting

--- plugins/chat/assets/javascripts/discourse/initializers/chat-setup.js
+++ plugins/chat/assets/javascripts/discourse/initializers/chat-setup.js
@@ -3,7 +3,6 @@ import { getOwnerWithFallback } from "discourse-common/lib/get-owner";
 import { replaceIcon } from "discourse-common/lib/icon-library";
 import { bind } from "discourse-common/utils/decorators";
 import I18n from "discourse-i18n";
-import { MENTION_KEYWORDS } from "discourse/plugins/chat/discourse/components/chat-message";
 import { clearChatComposerButtons } from "discourse/plugins/chat/discourse/lib/chat-composer-buttons";
 import ChannelHashtagType from "discourse/plugins/chat/discourse/lib/hashtag-types/channel";
 import ChatHeaderIcon from "../components/chat/header/icon";
