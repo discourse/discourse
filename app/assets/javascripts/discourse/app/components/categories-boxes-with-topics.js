36ed5808ad8ccc8f33270c668217e63745240602
David Taylor <david@taylorhq.com>
2024-08-21 17:56:48 +0100

    DEV: Convert core components to native class syntax (batch 1)
    Changes made using the ember-native-class-codemod, plus some manual tweaks

--- app/assets/javascripts/discourse/app/components/categories-boxes-with-topics.js
+++ app/assets/javascripts/discourse/app/components/categories-boxes-with-topics.js
@@ -1,19 +1,20 @@
 import Component from "@ember/component";
 import { isEmpty } from "@ember/utils";
+import { classNameBindings, tagName } from "@ember-decorators/component";
 import discourseComputed from "discourse-common/utils/decorators";
 
-export default Component.extend({
-  tagName: "section",
-  classNameBindings: [
-    ":category-boxes-with-topics",
-    "anyLogos:with-logos:no-logos",
-  ],
-  lockIcon: "lock",
+@tagName("section")
+@classNameBindings(
+  ":category-boxes-with-topics",
+  "anyLogos:with-logos:no-logos"
+)
+export default class CategoriesBoxesWithTopics extends Component {
+  lockIcon = "lock";
 
   @discourseComputed("categories.[].uploaded_logo.url")
   anyLogos() {
     return this.categories.any((c) => {
       return !isEmpty(c.get("uploaded_logo.url"));
     });
-  },
-});
+  }
+}
