d3d051c058e81ff689ed68cef202f4b75564237a
Bianca Nenciu <nenciu.bianca@gmail.com>
2024-08-21 18:07:34 +0300

    Simplify swapping columns

--- db/migrate/20240820123403_swap_big_int_notifications_id.rb
+++ db/migrate/20240820123403_swap_big_int_notifications_id.rb
@@ -16,18 +16,14 @@ class SwapBigIntNotificationsId < ActiveRecord::Migration[7.0]
     execute "DROP TRIGGER notifications_new_id_trigger ON notifications"
     execute "DROP FUNCTION mirror_notifications_id()"
 
-    execute "ALTER TABLE notifications RENAME COLUMN id TO old_id"
+    execute "ALTER TABLE notifications ALTER COLUMN id DROP DEFAULT"
+    execute "ALTER TABLE notifications ALTER COLUMN new_id SET DEFAULT nextval('notifications_id_seq'::regclass)"
+    execute "ALTER SEQUENCE notifications_id_seq OWNED BY notifications.new_id"
+
+    execute "ALTER TABLE notifications DROP COLUMN id"
     execute "ALTER TABLE notifications RENAME COLUMN new_id TO id"
 
-    execute "ALTER TABLE notifications ALTER COLUMN old_id DROP DEFAULT"
-    execute "ALTER TABLE notifications ALTER COLUMN id SET DEFAULT nextval('notifications_id_seq'::regclass)"
-
-    execute "ALTER SEQUENCE notifications_id_seq OWNED BY notifications.id"
-
-    execute "ALTER TABLE notifications DROP CONSTRAINT notifications_pkey"
     execute "ALTER TABLE notifications ADD CONSTRAINT notifications_pkey PRIMARY KEY USING INDEX notifications_pkey_bigint"
-
-    execute "ALTER TABLE notifications DROP COLUMN old_id"
 
     # Remove `_bigint` suffix from indexes
     results =
