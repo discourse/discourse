4f963c07befc3272963a9d0fe453af44f09967c5
OsamaSayegh <asooomaasoooma90@gmail.com>
2024-08-27 00:20:37 +0300

    SECURITY: Block registrations for encoded emails that are invalid

--- lib/validators/email_address_validator.rb
+++ lib/validators/email_address_validator.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class EmailAddressValidator
-  def self.valid_value?(email)
-    email.match? email_regex
-  end
+  class << self
+    def valid_value?(email)
+      email.match?(email_regex) && decode(email)&.match?(email_regex)
+    end
 
-  def self.email_regex
-    /\A[a-zA-Z0-9!#\$%&'*+\/=?\^_`{|}~\-]+(?:\.[a-zA-Z0-9!#\$%&'\*+\/=?\^_`{|}~\-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-]*[a-zA-Z0-9])?$\z/
+    def email_regex
+      /\A[a-zA-Z0-9!#\$%&'*+\/=?\^_`{|}~\-]+(?:\.[a-zA-Z0-9!#\$%&'\*+\/=?\^_`{|}~\-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-]*[a-zA-Z0-9])?$\z/
+    end
+
+    private
+
+    def decode(email)
+      Mail::Address.new(email).decoded
+    rescue Mail::Field::ParseError, Mail::Field::IncompleteParseError
+      nil
+    end
   end
 end
