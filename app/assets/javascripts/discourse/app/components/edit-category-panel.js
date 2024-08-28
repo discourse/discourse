2caaed72ba8a954772217ba9559645eed2f34645
David Taylor <david@taylorhq.com>
2024-08-28 12:08:24 +0100

    fixup

--- app/assets/javascripts/discourse/app/components/edit-category-panel.js
+++ app/assets/javascripts/discourse/app/components/edit-category-panel.js
@@ -5,11 +5,11 @@ import { classNameBindings } from "@ember-decorators/component";
 export default class EditCategoryPanel extends Component {}
 
 export function buildCategoryPanel(tab) {
-  @classNameBindings([
+  @classNameBindings(
     ":edit-category-tab",
     "activeTab::hide",
-    `:edit-category-tab-${tab}`,
-  ])
+    `:edit-category-tab-${tab}`
+  )
   class BuiltCategoryPanel extends EditCategoryPanel {
     @equal("selectedTab", tab) activeTab;
   }
