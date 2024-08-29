3374ba2e5f295bead390a11eed101148be4ddc2f
amanda <amanda@discourse.org>
2024-08-29 13:58:25 -0300

    FIX: Check existing custom flags before creating a flag id

---
+++ db/migrate/20240829155635_update_custom_flag_sequence.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+class UpdateCustomFlagSequence < ActiveRecord::Migration[7.1]
+  def up
+    # Update flags to consider the sequence when manual ids were added
+    max_id = DB.query_single("SELECT MAX(id) from flags").first
+    DB.exec("SELECT setval('flags_id_seq', #{max_id + 1}, FALSE);")
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
