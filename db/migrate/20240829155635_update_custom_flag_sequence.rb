90e8960fcb5f3a63d5174c864b7b8ed4c1c0497f
amanda <amanda@discourse.org>
2024-08-29 14:38:34 -0300

    add a fallback for flag id

--- db/migrate/20240829155635_update_custom_flag_sequence.rb
+++ db/migrate/20240829155635_update_custom_flag_sequence.rb
@@ -2,7 +2,7 @@
 class UpdateCustomFlagSequence < ActiveRecord::Migration[7.1]
   def up
     # Update flags to consider the sequence when manual ids were added
-    max_id = DB.query_single("SELECT MAX(id) from flags").first
+    max_id = DB.query_single("SELECT MAX(id) from flags").first || Flag::MAX_SYSTEM_FLAG_ID
     DB.exec("SELECT setval('flags_id_seq', #{max_id + 1}, FALSE);")
   end
 
