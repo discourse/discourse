89040a8aaef19a1d65cd740b707f90f14ef83a51
David Taylor <david@taylorhq.com>
2024-08-30 11:48:32 +0100

    DEV: Refactor complex initializers into classes
    We're working to remove all decorators from object-literal-properties. This commit refactors all initializers which were using these decorators into classes, where decorators are allowed. Also adds cleanup logic to the local-dates initializer, which was previously missing.

--- app/assets/javascripts/discourse/app/instance-initializers/read-only.js
+++ app/assets/javascripts/discourse/app/instance-initializers/read-only.js
@@ -1,22 +1,37 @@
+import { setOwner } from "@ember/owner";
+import { service } from "@ember/service";
 import { bind } from "discourse-common/utils/decorators";
 
 // Subscribe to "read-only" status change events via the Message Bus
-export default {
-  after: "message-bus",
+class ReadOnlyInit {
+  @service messageBus;
+  @service site;
 
-  initialize(owner) {
-    this.messageBus = owner.lookup("service:message-bus");
-    this.site = owner.lookup("service:site");
+  constructor(owner) {
+    setOwner(this, owner);
 
     this.messageBus.subscribe("/site/read-only", this.onMessage);
-  },
+  }
 
   teardown() {
     this.messageBus.unsubscribe("/site/read-only", this.onMessage);
-  },
+  }
 
   @bind
   onMessage(enabled) {
     this.site.set("isReadOnly", enabled);
+  }
+}
+
+export default {
+  after: "message-bus",
+
+  initialize(owner) {
+    this.instance = new ReadOnlyInit(owner);
+  },
+
+  teardown() {
+    this.instance.teardown();
+    this.instance = null;
   },
 };
