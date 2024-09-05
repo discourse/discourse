34ea52568b9f24b196c6d6a9096087007b4942a9
Nat <natalie.tay@discourse.org>
2024-09-05 22:04:24 +0800

    DEV: Include a basic oauth faraday formatter in core for usage in managed authenticators

---
+++ lib/auth/oauth_faraday_formatter.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Auth::OauthFaradayFormatter < Faraday::Logging::Formatter
+  def request(env)
+    Rails.logger.warn("DERPPPPP")
+    warn <<~LOG
+      OAuth Debugging: request #{env.method.upcase} #{env.url}
+
+      Headers:
+      #{env.request_headers}
+
+      Body:
+      #{env[:body]}
+    LOG
+  end
+
+  def response(env)
+    warn <<~LOG
+      OAuth Debugging: response status #{env.status}
+
+      From #{env.method.upcase} #{env.url}
+
+      Headers:
+      #{env.request_headers}
+
+      Body:
+      #{env[:body]}
+    LOG
+  end
+end
