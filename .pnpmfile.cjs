9b8df2003c6e70de108552cd661907ea07638465
David Taylor <david@taylorhq.com>
2024-09-12 12:25:24 +0100

    DEV: Automatically add `--ignore-workspace` when pnpm used in plugins

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,7 +1,9 @@
 const fs = require("fs");
-const { execSync } = require("child_process");
+const { execSync, execFileSync } = require("child_process");
 
-if (fs.existsSync("node_modules/.yarn-integrity")) {
+const discourseRoot = __dirname;
+
+if (fs.existsSync(`${discourseRoot}/node_modules/.yarn-integrity`)) {
   console.log(
     "Detected yarn-managed node_modules. Performing one-time cleanup..."
   );
@@ -9,8 +11,31 @@ if (fs.existsSync("node_modules/.yarn-integrity")) {
   // Delete entire contents of all node_modules directories
   // But keep the directories themselves, in case they are volume mounts (e.g. in devcontainer)
   execSync(
-    "find ./node_modules ./app/assets/javascripts/*/node_modules -mindepth 1 -maxdepth 1 -exec rm -rf {} +"
+    `find ${discourseRoot}/node_modules ${discourseRoot}/app/assets/javascripts/*/node_modules -mindepth 1 -maxdepth 1 -exec rm -rf {} +`
   );
 
   console.log("cleanup done");
+}
+
+if (process.cwd().startsWith(`${discourseRoot}/plugins/`)) {
+  console.log(
+    "> pnpm was run inside a plugin directory. Re-executing with --ignore-workspace..."
+  );
+
+  try {
+    execFileSync(
+      process.argv[0],
+      [...process.argv.slice(1), "--ignore-workspace"],
+      {
+        stdio: "inherit",
+      }
+    );
+  } catch (e) {
+    if (e.status) {
+      process.exit(e.status);
+    }
+    throw e;
+  }
+
+  process.exit(0);
 }
