3baf6233aa71eaec0ca5117bdb0a2ee7f250b0c2
Osama Sayegh <asooomaasoooma90@gmail.com>
2024-09-13 15:12:57 +0300

    FIX: Remove anonymous users from users directory (#28892)
    Anonymous users are "shadow" users created when an existing real user desires to post anonymously. This feature is off by default, but it can be enabled via the `allow_anonymous_posting` site setting. Those shadow users shouldn't be included in the users directory (`/u`).

---
+++ db/post_migrate/20240912210450_delete_anonymous_users_from_directory_items.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class DeleteAnonymousUsersFromDirectoryItems < ActiveRecord::Migration[7.1]
+  def up
+    DB.exec(<<~SQL)
+      DELETE FROM directory_items
+      USING anonymous_users
+      WHERE directory_items.user_id = anonymous_users.user_id
+    SQL
+  end
+end
