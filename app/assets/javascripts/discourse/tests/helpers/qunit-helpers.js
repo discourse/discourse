578542ca98b236a3912121fda86e1eed1aac217d
Jarek Radosz <jradosz@gmail.com>
2024-10-10 15:05:05 +0200

    DEV: Remove long-deprecated test helpers (#29160)
    nothing in all-the* is using them anymore

--- app/assets/javascripts/discourse/tests/helpers/qunit-helpers.js
+++ app/assets/javascripts/discourse/tests/helpers/qunit-helpers.js
@@ -438,43 +438,12 @@ export function acceptance(name, optionsOrCallback) {
   }
 }
 
-export function controllerFor(controller, model) {
-  deprecated(
-    'controllerFor is deprecated. Use the standard `getOwner(this).lookup("controller:NAME")` instead',
-    {
-      id: "discourse.controller-for",
-      since: "3.0.0.beta14",
-    }
-  );
-
-  controller = getOwnerWithFallback(this).lookup("controller:" + controller);
-  if (model) {
-    controller.set("model", model);
-  }
-  return controller;
-}
-
 export function fixture(selector) {
   if (selector) {
     return document.querySelector(`#qunit-fixture ${selector}`);
   }
   return document.querySelector("#qunit-fixture");
 }
-
-QUnit.assert.not = function (actual, message) {
-  deprecated("assert.not() is deprecated. Use assert.false() instead.", {
-    since: "2.9.0.beta1",
-    dropFrom: "2.10.0.beta1",
-    id: "discourse.qunit.assert-not",
-  });
-
-  this.pushResult({
-    result: !actual,
-    actual,
-    expected: !actual,
-    message,
-  });
-};
 
 QUnit.assert.blank = function (actual, message) {
   this.pushResult({
