bc4bc986d731ecc16b3ded2164b9285a680e6fcc
David Taylor <david@taylorhq.com>
2024-10-15 15:28:04 +0100

    DEV: Improve isolation and concurrency for minio-based upload specs
    - Uses a temporary, clean, per-test-process directory for minio data
    - Runs a separate minio instance for each test process
    - Unskips minio-based tests in CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -325,6 +325,16 @@ RSpec.configure do |config|
             ["discoursetest"]
           end
         )
+
+      test_i = ENV["TEST_ENV_NUMBER"].to_i
+
+      data_dir = "#{Rails.root}/tmp/test_data_#{test_i}/minio"
+      FileUtils.rm_rf(data_dir)
+      FileUtils.mkdir_p(data_dir)
+      minio_runner_config.minio_data_directory = data_dir
+
+      minio_runner_config.minio_port = 9_000 + 2 * test_i
+      minio_runner_config.minio_console_port = 9_001 + 2 * test_i
     end
 
     WebMock.disable_net_connect!(
