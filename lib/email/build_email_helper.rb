6f07796d8ecb445ae5f132d83d75db26580766ef
Jan Cernik <jancernik12@gmail.com>
2024-10-18 11:54:36 -0300

    Small fixes

--- lib/email/build_email_helper.rb
+++ lib/email/build_email_helper.rb
@@ -5,11 +5,17 @@ module Email
     def build_email(*builder_args)
       builder = Email::MessageBuilder.new(*builder_args)
       headers(builder.header_args) if builder.header_args.present?
-      mail(builder.build_args).tap do |message|
-        if message && h = builder.html_part
-          message.html_part = h
+      a =
+        mail(builder.build_args).tap do |message|
+          if message && h = builder.html_part
+            message.html_part = h
+          end
         end
-      end
+      puts
+      puts "==============================================================="
+      puts a
+      puts "==============================================================="
+      a
     end
   end
 end
