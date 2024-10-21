36c745d07afd8a82b5b88e69837b036dc28e87d0
Keegan George <kgeorge13@gmail.com>
2024-10-21 14:37:52 -0700

    FIX: accidental commenting of code

--- spec/system/composer_uploads_spec.rb
+++ spec/system/composer_uploads_spec.rb
@@ -10,154 +10,154 @@ describe "Uploading files in the composer", type: :system do
 
   before { sign_in(current_user) }
 
-  # it "uploads multiple files at once" do
-  #   visit "/new-topic"
-  #   expect(composer).to be_opened
+  it "uploads multiple files at once" do
+    visit "/new-topic"
+    expect(composer).to be_opened
 
-  #   file_path_1 = file_from_fixtures("logo.png", "images").path
-  #   file_path_2 = file_from_fixtures("logo.jpg", "images").path
-  #   attach_file([file_path_1, file_path_2]) { composer.click_toolbar_button("upload") }
+    file_path_1 = file_from_fixtures("logo.png", "images").path
+    file_path_2 = file_from_fixtures("logo.jpg", "images").path
+    attach_file([file_path_1, file_path_2]) { composer.click_toolbar_button("upload") }
 
-  #   expect(composer).to have_no_in_progress_uploads
-  #   expect(composer.preview).to have_css(".image-wrapper", count: 2)
-  # end
+    expect(composer).to have_no_in_progress_uploads
+    expect(composer.preview).to have_css(".image-wrapper", count: 2)
+  end
 
-  # it "allows cancelling uploads" do
-  #   visit "/new-topic"
-  #   expect(composer).to be_opened
+  it "allows cancelling uploads" do
+    visit "/new-topic"
+    expect(composer).to be_opened
 
-  #   file_path_1 = file_from_fixtures("huge.jpg", "images").path
-  #   cdp.with_slow_upload do
-  #     attach_file(file_path_1) { composer.click_toolbar_button("upload") }
-  #     expect(composer).to have_in_progress_uploads
-  #     find("#cancel-file-upload").click
+    file_path_1 = file_from_fixtures("huge.jpg", "images").path
+    cdp.with_slow_upload do
+      attach_file(file_path_1) { composer.click_toolbar_button("upload") }
+      expect(composer).to have_in_progress_uploads
+      find("#cancel-file-upload").click
 
-  #     expect(composer).to have_no_in_progress_uploads
-  #     expect(composer.preview).to have_no_css(".image-wrapper")
-  #   end
-  # end
+      expect(composer).to have_no_in_progress_uploads
+      expect(composer.preview).to have_no_css(".image-wrapper")
+    end
+  end
 
-  # context "when video thumbnails are enabled" do
-  #   before do
-  #     SiteSetting.video_thumbnails_enabled = true
-  #     SiteSetting.authorized_extensions += "|mp4"
-  #   end
+  context "when video thumbnails are enabled" do
+    before do
+      SiteSetting.video_thumbnails_enabled = true
+      SiteSetting.authorized_extensions += "|mp4"
+    end
 
-  #   it "generates a topic preview thumbnail from the video" do
-  #     visit "/new-topic"
-  #     expect(composer).to be_opened
-  #     topic.fill_in_composer_title("Video upload test")
+    it "generates a topic preview thumbnail from the video" do
+      visit "/new-topic"
+      expect(composer).to be_opened
+      topic.fill_in_composer_title("Video upload test")
 
-  #     file_path_1 = file_from_fixtures("small.mp4", "media").path
-  #     attach_file(file_path_1) { composer.click_toolbar_button("upload") }
+      file_path_1 = file_from_fixtures("small.mp4", "media").path
+      attach_file(file_path_1) { composer.click_toolbar_button("upload") }
 
-  #     expect(composer).to have_no_in_progress_uploads
-  #     expect(composer.preview).to have_css(".onebox-placeholder-container")
+      expect(composer).to have_no_in_progress_uploads
+      expect(composer.preview).to have_css(".onebox-placeholder-container")
 
-  #     composer.submit
+      composer.submit
 
-  #     expect(find("#topic-title")).to have_content("Video upload test")
-  #     expect(Topic.last.image_upload_id).to eq(Upload.last.id)
-  #   end
+      expect(find("#topic-title")).to have_content("Video upload test")
+      expect(Topic.last.image_upload_id).to eq(Upload.last.id)
+    end
 
-  #   it "generates a thumbnail from the video" do
-  #     visit "/new-topic"
-  #     expect(composer).to be_opened
-  #     topic.fill_in_composer_title("Video upload test")
+    it "generates a thumbnail from the video" do
+      visit "/new-topic"
+      expect(composer).to be_opened
+      topic.fill_in_composer_title("Video upload test")
 
-  #     file_path_1 = file_from_fixtures("small.mp4", "media").path
-  #     attach_file(file_path_1) { composer.click_toolbar_button("upload") }
+      file_path_1 = file_from_fixtures("small.mp4", "media").path
+      attach_file(file_path_1) { composer.click_toolbar_button("upload") }
 
-  #     expect(composer).to have_no_in_progress_uploads
-  #     expect(composer.preview).to have_css(".onebox-placeholder-container")
+      expect(composer).to have_no_in_progress_uploads
+      expect(composer.preview).to have_css(".onebox-placeholder-container")
 
-  #     expect(page).to have_css(
-  #       '.onebox-placeholder-container[style*="background-image"]',
-  #       wait: Capybara.default_max_wait_time,
-  #     )
+      expect(page).to have_css(
+        '.onebox-placeholder-container[style*="background-image"]',
+        wait: Capybara.default_max_wait_time,
+      )
 
-  #     composer.submit
+      composer.submit
 
-  #     expect(find("#topic-title")).to have_content("Video upload test")
+      expect(find("#topic-title")).to have_content("Video upload test")
 
-  #     selector = topic.post_by_number_selector(1)
+      selector = topic.post_by_number_selector(1)
 
-  #     expect(page).to have_css(selector)
-  #     within(selector) do
-  #       expect(page).to have_css(".video-placeholder-container[data-thumbnail-src]")
-  #     end
-  #   end
+      expect(page).to have_css(selector)
+      within(selector) do
+        expect(page).to have_css(".video-placeholder-container[data-thumbnail-src]")
+      end
+    end
 
-  #   it "handles a video where dimensions can't be read gracefully" do
-  #     visit "/new-topic"
-  #     expect(composer).to be_opened
-  #     topic.fill_in_composer_title("Zero Width Video Test")
+    it "handles a video where dimensions can't be read gracefully" do
+      visit "/new-topic"
+      expect(composer).to be_opened
+      topic.fill_in_composer_title("Zero Width Video Test")
 
-  #     # Inject JavaScript to mock video dimensions
-  #     page.execute_script <<-JS
-  #       HTMLVideoElement.prototype.__defineGetter__('videoWidth', function() { return 0; });
-  #       HTMLVideoElement.prototype.__defineGetter__('videoHeight', function() { return 0; });
-  #     JS
+      # Inject JavaScript to mock video dimensions
+      page.execute_script <<-JS
+        HTMLVideoElement.prototype.__defineGetter__('videoWidth', function() { return 0; });
+        HTMLVideoElement.prototype.__defineGetter__('videoHeight', function() { return 0; });
+      JS
 
-  #     file_path_1 = file_from_fixtures("small.mp4", "media").path
-  #     attach_file(file_path_1) { composer.click_toolbar_button("upload") }
+      file_path_1 = file_from_fixtures("small.mp4", "media").path
+      attach_file(file_path_1) { composer.click_toolbar_button("upload") }
 
-  #     expect(composer).to have_no_in_progress_uploads
-  #     expect(composer.preview).to have_css(".onebox-placeholder-container")
+      expect(composer).to have_no_in_progress_uploads
+      expect(composer.preview).to have_css(".onebox-placeholder-container")
 
-  #     composer.submit
+      composer.submit
 
-  #     expect(find("#topic-title")).to have_content("Zero Width Video Test")
+      expect(find("#topic-title")).to have_content("Zero Width Video Test")
 
-  #     selector = topic.post_by_number_selector(1)
+      selector = topic.post_by_number_selector(1)
 
-  #     expect(page).to have_css(selector)
-  #     within(selector) do
-  #       expect(page).to have_no_css(".video-placeholder-container[data-thumbnail-src]")
-  #     end
-  #   end
+      expect(page).to have_css(selector)
+      within(selector) do
+        expect(page).to have_no_css(".video-placeholder-container[data-thumbnail-src]")
+      end
+    end
 
-  #   it "handles a video load error gracefully" do
-  #     visit "/new-topic"
-  #     expect(composer).to be_opened
-  #     topic.fill_in_composer_title("Video Load Error Test")
+    it "handles a video load error gracefully" do
+      visit "/new-topic"
+      expect(composer).to be_opened
+      topic.fill_in_composer_title("Video Load Error Test")
 
-  #     # Inject JavaScript to simulate an invalid video file that triggers onerror
-  #     page.execute_script <<-JS
-  #       const originalCreateObjectURL = URL.createObjectURL;
-  #       URL.createObjectURL = function(blob) {
-  #         // Simulate an invalid video source by returning a fake object URL
-  #         return 'invalid_video_source.mp4';
-  #       };
-  #     JS
+      # Inject JavaScript to simulate an invalid video file that triggers onerror
+      page.execute_script <<-JS
+        const originalCreateObjectURL = URL.createObjectURL;
+        URL.createObjectURL = function(blob) {
+          // Simulate an invalid video source by returning a fake object URL
+          return 'invalid_video_source.mp4';
+        };
+      JS
 
-  #     file_path_1 = file_from_fixtures("small.mp4", "media").path
-  #     attach_file(file_path_1) { composer.click_toolbar_button("upload") }
+      file_path_1 = file_from_fixtures("small.mp4", "media").path
+      attach_file(file_path_1) { composer.click_toolbar_button("upload") }
 
-  #     expect(composer).to have_no_in_progress_uploads
-  #     expect(composer.preview).to have_css(".onebox-placeholder-container")
-  #   end
+      expect(composer).to have_no_in_progress_uploads
+      expect(composer.preview).to have_css(".onebox-placeholder-container")
+    end
 
-  #   it "shows video player in composer" do
-  #     SiteSetting.enable_diffhtml_preview = true
+    it "shows video player in composer" do
+      SiteSetting.enable_diffhtml_preview = true
 
-  #     visit "/new-topic"
-  #     expect(composer).to be_opened
-  #     topic.fill_in_composer_title("Video upload test")
+      visit "/new-topic"
+      expect(composer).to be_opened
+      topic.fill_in_composer_title("Video upload test")
 
-  #     file_path_1 = file_from_fixtures("small.mp4", "media").path
-  #     attach_file(file_path_1) { composer.click_toolbar_button("upload") }
+      file_path_1 = file_from_fixtures("small.mp4", "media").path
+      attach_file(file_path_1) { composer.click_toolbar_button("upload") }
 
-  #     expect(composer).to have_no_in_progress_uploads
-  #     expect(composer.preview).to have_css(".video-container video")
+      expect(composer).to have_no_in_progress_uploads
+      expect(composer.preview).to have_css(".video-container video")
 
-  #     expect(page).to have_css(
-  #       ".video-container video source[src]",
-  #       visible: false,
-  #       wait: Capybara.default_max_wait_time,
-  #     )
-  #   end
-  # end
+      expect(page).to have_css(
+        ".video-container video source[src]",
+        visible: false,
+        wait: Capybara.default_max_wait_time,
+      )
+    end
+  end
 
   context "when multiple images are uploaded" do
     before { SiteSetting.experimental_auto_grid_images = true }
