discourse/discourse

commit 597ef1119536693ef0bc7fe799ff485047f9f6f7

DEV: Improve reactivity of user-tips and remove runloop workarounds (#23897)
Previously, the `user-tips` service included a couple of calls to `next()`. These were introduced to work around errors like

```
You attempted to update `availableTips` on `<UserTips:ember659>`, but it had already been used previously in the same computation
```

These errors come from the fact that various `<UserTip>` components are rendering at slightly different times in the runloop and stepping on each other. Normally this doesn't happen in Ember, but the implementation details of our 'Widget' system and its 'RenderGlimmer' helper mean that RenderGlimmer components are rendered later than normal Ember components. Using `next()` avoids the problem because it means that all the updates are scheduled together in the following runloop interation.

However, the use of `next()` can create some subtle timing issues, which have been evident in the recent flakiness of some qunit tests. This commit makes a few changes to improve the situation:

1. Use a TrackedMap to provide fine-grained `shouldRender()` reactivity for each user-tip id. That means that different user tips will not be trying to update the same piece of tracked state (previously the entire `availableTips` array was `@tracked`, and was completely re-assigned every time a new `<UserTip>` was rendered

2. Avoid reassigning any tracked state unless the value has actually changed

3. Remove the `next()` workarounds

app/assets/javascripts/discourse/app/services/user-tips.js
@@ -1,6 +1,5 @@
-import { tracked } from "@glimmer/tracking";
-import { next } from "@ember/runloop";
 import Service, { inject as service } from "@ember/service";
+import { TrackedMap } from "@ember-compat/tracked-built-ins";
 import { disableImplicitInjections } from "discourse/lib/implicit-injections";
 import Site from "discourse/models/site";
 import { isTesting } from "discourse-common/config/environment";
@@ -10,15 +9,17 @@ export default class UserTips extends Service {
   @service site;
   @service currentUser;
 
-  @tracked availableTips = [];
-  @tracked renderedId;
+  #availableTips = new Set();
+  #renderedId;
+  #shouldRenderMap = new TrackedMap();
 
-  computeRenderedId() {
-    if (this.availableTips.find((tip) => tip.id === this.renderedId)) {
-      return this.renderedId;
+  #updateRenderedId() {
+    const tipsArray = [...this.#availableTips];
+    if (tipsArray.find((tip) => tip.id === this.#renderedId)) {
+      return;
     }
 
-    return this.availableTips
+    const newId = tipsArray
       .sortBy("priority")
       .reverse()
       .find((tip) => {
@@ -26,23 +27,26 @@ export default class UserTips extends Service {
           return tip.id;
         }
       })?.id;
+
+    if (this.#renderedId !== newId) {
+      this.#shouldRenderMap.delete(this.#renderedId);
+      this.#shouldRenderMap.set(newId, true);
+      this.#renderedId = newId;
+    }
+  }
+
+  shouldRender(id) {
+    return this.#shouldRenderMap.get(id);
   }
 
   addAvailableTip(tip) {
-    next(() => {
-      this.availableTips = [...this.availableTips, tip];
-      this.renderedId = this.computeRenderedId();
-    });
+    this.#availableTips.add(tip);
+    this.#updateRenderedId();
   }
 
   removeAvailableTip(tip) {
-    next(() => {
-      this.availableTips = this.availableTips.filter((availableTip) => {
-        return tip.id !== availableTip.id;
-      });
-
-      this.renderedId = this.computeRenderedId();
-    });
+    this.#availableTips.delete(tip);
+    this.#updateRenderedId();
   }
 
   canSeeUserTip(tipId) {
@@ -89,7 +93,8 @@ export default class UserTips extends Service {
       return;
     }
 
-    this.removeAvailableTip({ id: tipId });
+    const tipObj = [...this.#availableTips].find((t) => t.id === tipId);
+    this.removeAvailableTip(tipObj);
 
     // Update list of seen user tips.
     let seenUserTips = this.currentUser.user_option?.seen_popups || [];

app/assets/javascripts/discourse/app/components/user-tip.gjs
@@ -15,13 +15,15 @@ export default class UserTip extends Component {
   @service tooltip;
 
   registerTip = modifier(() => {
-    this.userTips.addAvailableTip({
+    const tip = {
       id: this.args.id,
       priority: this.args.priority ?? 0,
-    });
+    };
+
+    this.userTips.addAvailableTip(tip);
 
     return () => {
-      this.userTips.removeAvailableTip({ id: this.args.id });
+      this.userTips.removeAvailableTip(tip);
     };
   });
 
@@ -69,7 +71,7 @@ export default class UserTip extends Component {
   });
 
   get shouldRenderTip() {
-    return this.userTips.renderedId === this.args.id;
+    return this.userTips.shouldRender(this.args.id);
   }
 
   <template>

commit 08b54c9af20cbdf7c324714b80aa681ff25b4382

FIX: Show the last rendered user-tip
…or a tip with the highest priority.

This regressed in 597ef1119536693ef0bc7fe799ff485047f9f6f7 where we got rid of `next()` calls, so we'd render the first tip we encounter.

The commit also adds a test and updates existing ones.

app/assets/javascripts/discourse/app/services/user-tips.js
@@ -2,6 +2,7 @@ import Service, { service } from "@ember/service";
 import { TrackedSet } from "@ember-compat/tracked-built-ins";
 import { disableImplicitInjections } from "discourse/lib/implicit-injections";
 import { isTesting } from "discourse-common/config/environment";
+import discourseDebounce from "discourse-common/lib/debounce";
 
 @disableImplicitInjections
 export default class UserTips extends Service {
@@ -13,6 +14,10 @@ export default class UserTips extends Service {
   #shouldRenderSet = new TrackedSet();
 
   #updateRenderedId() {
+    if (this.isDestroying || this.isDestroyed) {
+      return;
+    }
+
     const tipsArray = [...this.#availableTips];
     if (tipsArray.find((tip) => tip.id === this.#renderedId)) {
       return;
@@ -21,11 +26,7 @@ export default class UserTips extends Service {
     const newId = tipsArray
       .sortBy("priority")
       .reverse()
-      .find((tip) => {
-        if (this.canSeeUserTip(tip.id)) {
-          return tip.id;
-        }
-      })?.id;
+      .find((tip) => this.canSeeUserTip(tip.id))?.id;
 
     if (this.#renderedId !== newId) {
       this.#shouldRenderSet.delete(this.#renderedId);
@@ -41,7 +42,7 @@ export default class UserTips extends Service {
   addAvailableTip(tip) {
     if (this.canSeeUserTip(tip.id) && !this._findAvailableTipById(tip.id)) {
       this.#availableTips.add(tip);
-      this.#updateRenderedId();
+      discourseDebounce(this, this.#updateRenderedId, 0);
     }
   }
 

app/assets/javascripts/discourse/tests/acceptance/user-tips-test.js
@@ -1,7 +1,7 @@
 import { visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import pretender, { response } from "discourse/tests/helpers/create-pretender";
-import { acceptance, query } from "discourse/tests/helpers/qunit-helpers";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import I18n from "discourse-i18n";
 
 acceptance("User Tips - first_notification", function (needs) {
@@ -11,9 +11,8 @@ acceptance("User Tips - first_notification", function (needs) {
   test("Shows first notification user tip", async function (assert) {
     this.siteSettings.enable_user_tips = true;
 
-    let requestsCount = 0;
     pretender.put("/u/eviltrout.json", () => {
-      requestsCount += 1;
+      assert.step("endpoint called");
 
       return response(200, {
         user: {
@@ -25,15 +24,13 @@ acceptance("User Tips - first_notification", function (needs) {
     });
 
     await visit("/t/internationalization-localization/280");
-    assert.equal(
-      query(".user-tip__title").textContent.trim(),
-      I18n.t("user_tips.first_notification.title")
-    );
+    assert
+      .dom(".user-tip__title")
+      .hasText(I18n.t("user_tips.first_notification.title"));
 
-    assert.strictEqual(
-      requestsCount,
-      1,
-      "Seeing the user tip updates the user option via a background request"
+    assert.verifySteps(
+      ["endpoint called"],
+      "seeing the user tip updates the user option via a background request"
     );
   });
 });
@@ -46,10 +43,9 @@ acceptance("User Tips - topic_timeline", function (needs) {
     this.siteSettings.enable_user_tips = true;
 
     await visit("/t/internationalization-localization/280");
-    assert.equal(
-      query(".user-tip__title").textContent.trim(),
-      I18n.t("user_tips.topic_timeline.title")
-    );
+    assert
+      .dom(".user-tip__title")
+      .hasText(I18n.t("user_tips.topic_timeline.title"));
   });
 });
 
@@ -61,10 +57,7 @@ acceptance("User Tips - post_menu", function (needs) {
     this.siteSettings.enable_user_tips = true;
 
     await visit("/t/internationalization-localization/280");
-    assert.equal(
-      query(".user-tip__title").textContent.trim(),
-      I18n.t("user_tips.post_menu.title")
-    );
+    assert.dom(".user-tip__title").hasText(I18n.t("user_tips.post_menu.title"));
   });
 });
 
@@ -77,10 +70,9 @@ acceptance("User Tips - topic_notification_levels", function (needs) {
 
     await visit("/t/internationalization-localization/280");
 
-    assert.equal(
-      query(".user-tip__title").textContent.trim(),
-      I18n.t("user_tips.topic_notification_levels.title")
-    );
+    assert
+      .dom(".user-tip__title")
+      .hasText(I18n.t("user_tips.topic_notification_levels.title"));
   });
 });
 
@@ -92,9 +84,8 @@ acceptance("User Tips - suggested_topics", function (needs) {
     this.siteSettings.enable_user_tips = true;
 
     await visit("/t/internationalization-localization/280");
-    assert.equal(
-      query(".user-tip__title").textContent.trim(),
-      I18n.t("user_tips.suggested_topics.title")
-    );
+    assert
+      .dom(".user-tip__title")
+      .hasText(I18n.t("user_tips.suggested_topics.title"));
   });
 });

app/assets/javascripts/discourse/tests/integration/components/user-tip-test.gjs
@@ -1,0 +1,24 @@
+import { getOwner } from "@ember/owner";
+import { render } from "@ember/test-helpers";
+import { module, test } from "qunit";
+import UserTip from "discourse/components/user-tip";
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+import DTooltips from "float-kit/components/d-tooltips";
+
+module("Integration | Component | UserTip", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("shows the last user tip when there are no priorities", async function (assert) {
+    const site = getOwner(this).lookup("service:site");
+    site.user_tips = { foo: 1, bar: 2, baz: 3 };
+
+    await render(<template>
+      <UserTip @id="foo" @titleText="first tip" />
+      <UserTip @id="bar" @titleText="second tip" />
+      <UserTip @id="baz" @titleText="third tip" />
+      <DTooltips />
+    </template>);
+
+    assert.dom(".user-tip__title").hasText("third tip");
+  });
+});

spec/system/user_tips_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe "Homepage", type: :system do
+describe "User tips", type: :system do
   fab!(:admin)
   fab!(:user)
   fab!(:topics) { Fabricate.times(2, :post).map(&:topic) }