19660354d1565c48898d1fbc9b644bd3305901ab
rngus2344 <rngus2344@gmail.com>
2024-10-23 10:29:33 -0500

    DEV: Delete plugin specific webhook events

--- db/fixtures/007_web_hook_event_types.rb
+++ db/fixtures/007_web_hook_event_types.rb
@@ -230,13 +230,3 @@ WebHookEventType.seed do |b|
   b.name = "chat_message_restored"
   b.group = WebHookEventType.groups[:chat]
 end
-WebHookEventType.seed do |b|
-  b.id = WebHookEventType::TYPES[:category_experts_approved]
-  b.name = "category_experts_approved"
-  b.group = WebHookEventType.groups[:post]
-end
-WebHookEventType.seed do |b|
-  b.id = WebHookEventType::TYPES[:category_experts_unapproved]
-  b.name = "category_experts_unapproved"
-  b.group = WebHookEventType.groups[:post]
-end
