9230d4e7f2439b86bbcfa56ba1c54e31618c2134
David Taylor <david@taylorhq.com>
2024-10-28 19:38:52 +0000

    DEV: Update remaining mixins to remove object-literal decorators (#29444)
    These are unsupported by modern tooling (including ts/glint parsers), so we are working to remove them. The easiest path for mixins is to switch back to the mega-legacy EmberObject syntax for computed/on)

--- app/assets/javascripts/discourse/app/mixins/load-more.js
+++ app/assets/javascripts/discourse/app/mixins/load-more.js
@@ -1,7 +1,7 @@
+import { on } from "@ember/object/evented";
 import Mixin from "@ember/object/mixin";
 import Eyeline from "discourse/lib/eyeline";
 import Scrolling from "discourse/mixins/scrolling";
-import { on } from "discourse-common/utils/decorators";
 
 // Provides the ability to load more items for a view which is scrolled to the bottom.
 export default Mixin.create(Scrolling, {
@@ -9,8 +9,7 @@ export default Mixin.create(Scrolling, {
     return this.eyeline?.update();
   },
 
-  @on("didInsertElement")
-  _bindEyeline() {
+  _bindEyeline: on("didInsertElement", function () {
     const eyeline = Eyeline.create({
       selector: `${this.eyelineSelector}:last`,
     });
@@ -20,10 +19,9 @@ export default Mixin.create(Scrolling, {
     eyeline.update(); // update once to consider current position
 
     this.bindScrolling();
-  },
+  }),
 
-  @on("willDestroyElement")
-  _removeEyeline() {
+  _removeEyeline: on("willDestroyElement", function () {
     this.unbindScrolling();
-  },
+  }),
 });
