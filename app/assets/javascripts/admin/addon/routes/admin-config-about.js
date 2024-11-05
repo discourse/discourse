e2868c0615bcd1537bce5a6c529a69cf82ee32cf
OsamaSayegh <asooomaasoooma90@gmail.com>
2024-11-05 23:53:01 +0300

    DEV: Fetch settings by area name instead of individual settings

--- app/assets/javascripts/admin/addon/routes/admin-config-about.js
+++ app/assets/javascripts/admin/addon/routes/admin-config-about.js
@@ -5,21 +5,7 @@ export default class AdminConfigAboutRoute extends Route {
   model() {
     return ajax("/admin/config/site_settings.json", {
       data: {
-        filter_names: [
-          "title",
-          "site_description",
-          "extended_site_description",
-          "short_site_description",
-          "about_banner_image",
-          "community_owner",
-          "contact_email",
-          "contact_url",
-          "site_contact_username",
-          "site_contact_group_name",
-          "company_name",
-          "governing_law",
-          "city_for_disputes",
-        ],
+        filter_area: "about",
       },
     });
   }
