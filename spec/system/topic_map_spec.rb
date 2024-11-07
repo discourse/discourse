598ce421d1967807b6e2164052b8094655c7a5d7
awesomerobot <kris.aubuchon@discourse.org>
2024-11-07 18:48:48 -0500

    fix spec

--- spec/system/topic_map_spec.rb
+++ spec/system/topic_map_spec.rb
@@ -22,7 +22,7 @@ describe "Topic Map", type: :system do
 
     expect(topic_page).to have_topic_map
     Fabricate(:post, topic: topic, created_at: 1.day.ago, like_count: 3)
-    2.times { Fabricate(:post, topic: topic, created_at: 1.day.ago, like_count: 1) }
+    Fabricate(:post, topic: topic, created_at: 1.day.ago, like_count: 1)
     page.refresh
     expect(topic_page).to have_topic_map
     expect(topic_map).to have_no_users
