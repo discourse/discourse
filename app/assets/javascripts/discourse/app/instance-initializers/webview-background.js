discourse/discourse

commit 501f07ab1fceafcfa56bbfc074951d03d7d30d22

FIX: Retry updating webview background color (#28912)
In some very rare cases, the header element doesn't yet have the bg
when this code is run. This PR adds a simple retry mechanism.

No tests because this relies on specific load timing from the browser.

app/assets/javascripts/discourse/app/instance-initializers/webview-background.js
@@ -4,6 +4,7 @@ import discourseLater from "discourse-common/lib/later";
 // Send bg color to webview so iOS status bar matches site theme
 export default {
   after: "inject-objects",
+  retryCount: 0,
 
   initialize(owner) {
     const caps = owner.lookup("service:capabilities");
@@ -15,19 +16,27 @@ export default {
     }
   },
 
-  teardown() {
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .removeEventListener("change", this.updateAppBackground);
+  updateAppBackground(delay = 500) {
+    discourseLater(() => {
+      if (this.headerBgColor()) {
+        postRNWebviewMessage("headerBg", this.headerBgColor());
+      } else {
+        this.retry();
+      }
+    }, delay);
   },
 
-  updateAppBackground() {
-    discourseLater(() => {
-      const header = document.querySelector(".d-header-wrap .d-header");
-      if (header) {
-        const styles = window.getComputedStyle(header);
-        postRNWebviewMessage("headerBg", styles.backgroundColor);
-      }
-    }, 500);
+  headerBgColor() {
+    const header = document.querySelector(".d-header-wrap .d-header");
+    if (header) {
+      return window.getComputedStyle(header)?.backgroundColor;
+    }
+  },
+
+  retry() {
+    if (this.retryCount < 2) {
+      this.retryCount++;
+      this.updateAppBackground(1000);
+    }
   },
 };

commit 06132755f91d01c991584786e15225b9265303e8

DEV: Fix updating header background in webview
This regressed in https://github.com/discourse/discourse/pull/28912

app/assets/javascripts/discourse/app/instance-initializers/webview-background.js
@@ -11,7 +11,7 @@ export default {
     if (caps.isAppWebview) {
       window
         .matchMedia("(prefers-color-scheme: dark)")
-        .addEventListener("change", this.updateAppBackground);
+        .addEventListener("change", this.updateAppBackground.bind(this));
       this.updateAppBackground();
     }
   },