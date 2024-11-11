22078da7532efb20de272dd6b1f7cd769d01bdb3
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2024-11-11 16:18:55 +1100

    FIX: filterable staff logs for flags
    Flag actions must be added to `staff_actions` method to be filterable.

--- spec/system/admin_staff_action_logs_spec.rb
+++ spec/system/admin_staff_action_logs_spec.rb
@@ -11,6 +11,14 @@ describe "Admin staff action logs", type: :system do
     )
   end
   fab!(:history_2) { Fabricate(:topic_closed_change_history) }
+  fab!(:history_3) do
+    Fabricate(
+      :user_history,
+      action: UserHistory.actions[:custom_staff],
+      details: "flag updated",
+      custom_type: "update_flag",
+    )
+  end
   let(:staff_action_logs_page) { PageObjects::Pages::AdminStaffActionLogs.new }
 
   before { sign_in(current_user) }
@@ -34,6 +42,7 @@ describe "Admin staff action logs", type: :system do
 
     expect(staff_action_logs_page).to have_log_row(history_1)
     expect(staff_action_logs_page).to have_log_row(history_2)
+    expect(staff_action_logs_page).to have_log_row(history_3)
 
     staff_action_logs_page.filter_by_action(:change_site_setting)
 
@@ -44,6 +53,21 @@ describe "Admin staff action logs", type: :system do
 
     expect(staff_action_logs_page).to have_log_row(history_1)
     expect(staff_action_logs_page).to have_no_log_row(history_2)
+    expect(staff_action_logs_page).to have_no_log_row(history_3)
+
+    staff_action_logs_page.clear_filter
+
+    staff_action_logs_page.filter_by_action(:update_flag)
+
+    expect(staff_action_logs_page).to have_no_log_row(history_1)
+    expect(staff_action_logs_page).to have_no_log_row(history_2)
+    expect(staff_action_logs_page).to have_log_row(history_3)
+  end
+
+  it "displays no result" do
+    visit "/admin/logs/staff_action_logs"
+    staff_action_logs_page.filter_by_action(:toggle_flag)
+    expect(page).to have_text(I18n.t("js.search.no_results"))
   end
 
   it "can show details for an action" do
