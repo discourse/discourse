34ed35e1748f4e674714232c566501a43e5f8504
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2024-11-12 11:23:18 +1100

    FIX: filterable staff logs for flags (#29695)
    Flag actions must be added to `staff_actions` method to be filterable.

--- app/assets/javascripts/admin/addon/controllers/admin-config-flags.js
+++ app/assets/javascripts/admin/addon/controllers/admin-config-flags.js
@@ -1,7 +1,7 @@
 import Controller from "@ember/controller";
 import { service } from "@ember/service";
 
-export default class AdminConfigFlagsSettingsController extends Controller {
+export default class AdminConfigFlagsController extends Controller {
   @service router;
 
   get hideTabs() {
