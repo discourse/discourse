ddddddb7f63d47d24c07eb375112a946f324c35b
Régis Hanol <regis@hanol.fr>
2024-11-13 15:26:44 +0100

    DEV: fix chat:*:populate rake tasks

--- plugins/chat/lib/discourse_dev/category_channel.rb
+++ plugins/chat/lib/discourse_dev/category_channel.rb
@@ -5,20 +5,17 @@ require "faker"
 
 module DiscourseDev
   class CategoryChannel < Record
-    def initialize
-      super(::Chat::CategoryChannel, 5)
+    def initialize(ignore_current_count: true, count: 5, channel_id: nil)
+      super(::Chat::CategoryChannel, count)
     end
 
     def data
       chatable = Category.random
+      description = Faker::Lorem.sentence
+      name = Faker::Company.name
+      created_at = Faker::Time.between(from: DiscourseDev.config.start_date, to: DateTime.now)
 
-      {
-        chatable: chatable,
-        description: Faker::Lorem.paragraph,
-        user_count: 1,
-        name: Faker::Company.name,
-        created_at: Faker::Time.between(from: DiscourseDev.config.start_date, to: DateTime.now),
-      }
+      { chatable:, description:, user_count: 1, name:, created_at: }
     end
 
     def create!
@@ -30,14 +27,19 @@ module DiscourseDev
             if Faker::Boolean.boolean(true_ratio: 0.5)
               admin_username =
                 begin
-                  DiscourseDev::Config.new.config[:admin][:username]
+                  DiscourseDev.config.admin[:username]
                 rescue StandardError
                   nil
                 end
-              admin_user = ::User.find_by(username: admin_username) if admin_username
+              admin_user = ::User.find_by_username(admin_username) if admin_username
             end
 
-            user = admin_user || User.new(username: Faker::Internet.username(specifier: 10)).create!
+            user =
+              admin_user ||
+                ::User.create!(
+                  email: Faker::Internet.email,
+                  username: Faker::Internet.username(specifier: 10),
+                )
             Chat::ChannelMembershipManager.new(channel).follow(user)
             users << user
           end
@@ -47,8 +49,10 @@ module DiscourseDev
           .times do
             Chat::CreateMessage.call(
               guardian: users.sample.guardian,
-              chat_channel_id: channel.id,
-              message: Faker::Lorem.paragraph,
+              params: {
+                chat_channel_id: channel.id,
+                message: Faker::Lorem.sentence,
+              },
             )
           end
       end
