597fd9ac4e80ce162f502d802665259db1a292c9
Angus McLeod <angus@mcleod.org.au>
2024-11-14 10:33:56 +0100

    Add wip user api key client creation flow

--- app/controllers/user_api_key_clients_controller.rb
+++ app/controllers/user_api_key_clients_controller.rb
@@ -2,9 +2,43 @@
 class UserApiKeyClientsController < ApplicationController
   layout "no_ember"
 
-  skip_before_action :check_xhr, :preload_json, :verify_authenticity_token
+  requires_login only: %i[create]
+  skip_before_action :redirect_to_login_if_required, :redirect_to_profile_if_required, only: %i[new]
+  skip_before_action :check_xhr, :preload_json
 
-  def register
+  def new
+    require_params
+    validate_params
+
+    unless current_user
+      cookies[:destination_url] = request.fullpath
+
+      if SiteSetting.enable_discourse_connect?
+        redirect_to path("/session/sso")
+      else
+        redirect_to path("/login")
+      end
+      return
+    end
+
+    unless meets_tl?
+      @no_trust_level = true
+      return
+    end
+
+    @application_name = params[:application_name] || @client&.application_name
+    @public_key = params[:public_key] || @client&.public_key
+    @client_id = params[:client_id]
+    @auth_redirect = params[:auth_redirect]
+    @localized_scopes = params[:scopes].split(",").map { |s| I18n.t("user_api_key.scopes.#{s}") }
+    @scopes = params[:scopes]
+  rescue Discourse::InvalidAccess
+    @generic_error = true
+  end
+
+  def create
+    raise Discourse::InvalidAccess unless meets_tl?
+
     require_params
     validate_params
 
@@ -18,11 +52,19 @@ class UserApiKeyClientsController < ApplicationController
       @scopes.each { |scope| client.scopes.create!(name: scope) }
     end
 
-    if client.persisted?
-      render json: success_json
-    else
-      render json: failed_json
+    if !client.persisted?
+      render json: failed_json.merge(errors: client.errors.full_messages), status: 400
     end
+
+    uri = URI.parse(client.auth_redirect)
+    uri.query = "success=true"
+    redirect_to(uri.to_s, allow_other_host: true)
+  end
+
+  protected
+
+  def meets_tl?
+    current_user.staff? || current_user.in_any_groups?(SiteSetting.user_api_key_allowed_groups_map)
   end
 
   def require_params
--- config/routes.rb
+++ config/routes.rb
@@ -1627,7 +1627,8 @@ Discourse::Application.routes.draw do
     get "/user-api-key/otp" => "user_api_keys#otp"
     post "/user-api-key/otp" => "user_api_keys#create_otp"
 
-    post "/user-api-key-client/register" => "user_api_key_clients#register"
+    get "/user-api-key-client/new" => "user_api_key_clients#new"
+    post "/user-api-key-client" => "user_api_key_clients#create"
 
     get "/safe-mode" => "safe_mode#index"
     post "/safe-mode" => "safe_mode#enter", :as => "safe_mode_enter"
