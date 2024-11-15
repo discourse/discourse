b63de1dc589e2e850d3dae07d0c8adaf40e5f2ca
Angus McLeod <angus@mcleod.org.au>
2024-11-15 16:09:21 +0100

    Update db/migrate/20241112124552_create_user_api_key_client_scopes.rb
    Co-authored-by: Penar Musaraj <pmusaraj@gmail.com>

--- db/migrate/20241112124552_create_user_api_key_client_scopes.rb
+++ db/migrate/20241112124552_create_user_api_key_client_scopes.rb
@@ -3,7 +3,7 @@ class CreateUserApiKeyClientScopes < ActiveRecord::Migration[7.1]
   def change
     create_table :user_api_key_client_scopes do |t|
       t.bigint :user_api_key_client_id, null: false
-      t.string :name, null: false
+      t.string :name, null: false, limit: 100
       t.timestamps
     end
   end
