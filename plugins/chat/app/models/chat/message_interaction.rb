9443d20b7ee8615f50bd71f89dab4b8b464a901c
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-11-18 18:24:10 +0100

    model annotation

--- plugins/chat/app/models/chat/message_interaction.rb
+++ plugins/chat/app/models/chat/message_interaction.rb
@@ -8,3 +8,20 @@ module Chat
     belongs_to :message, class_name: "Chat::Message", foreign_key: "chat_message_id"
   end
 end
+
+# == Schema Information
+#
+# Table name: chat_message_interactions
+#
+#  id              :bigint           not null, primary key
+#  user_id         :bigint           not null
+#  chat_message_id :bigint           not null
+#  action          :jsonb            not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_chat_message_interactions_on_chat_message_id  (chat_message_id)
+#  index_chat_message_interactions_on_user_id          (user_id)
+#
