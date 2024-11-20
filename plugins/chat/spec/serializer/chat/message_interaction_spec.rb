833c45b26a0f36aded1aaf48b50501fb4dc2eec0
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-11-20 10:31:03 +0100

    FIX: serializes interaction for direct messages
    Prior to this fix it would cause an error as we need to pass the user to get the title of the channel.
    
    This commit also adds a test for message interaction serializer.

---
+++ plugins/chat/spec/serializer/chat/message_interaction_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+RSpec.describe Chat::MessageInteractionSerializer do
+  subject(:serializer) do
+    interaction = Chat::CreateMessageInteraction.call(params:, guardian: user.guardian).interaction
+    described_class.new(interaction, scope: Guardian.new(user), root: false)
+  end
+
+  fab!(:user)
+
+  let(:message) do
+    Fabricate(
+      :chat_message,
+      chat_channel: channel,
+      user: Discourse.system_user,
+      blocks: [
+        {
+          type: "actions",
+          elements: [
+            { type: "button", text: { type: "plain_text", text: "Like" }, action_id: "like" },
+          ],
+        },
+      ],
+    )
+  end
+  let(:message_id) { message.id }
+  let(:params) { { message_id: message_id, action_id: "like" } }
+
+  before do
+    SiteSetting.chat_allowed_groups = Group::AUTO_GROUPS[:everyone]
+    SiteSetting.direct_message_enabled_groups = Group::AUTO_GROUPS[:everyone]
+  end
+
+  context "when interaction's channel is private" do
+    let(:channel) { Fabricate(:direct_message_channel, users: [user, Fabricate(:user)]) }
+
+    it "serializes the interaction" do
+      expect(serializer.as_json).to match_response_schema("message_interaction")
+    end
+  end
+
+  context "when interaction's channel is public" do
+    let(:channel) { Fabricate(:chat_channel) }
+
+    it "serializes the interaction" do
+      expect(serializer.as_json).to match_response_schema("message_interaction")
+    end
+  end
+end
