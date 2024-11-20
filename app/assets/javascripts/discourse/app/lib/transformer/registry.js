0918ae84e2086bbc8002da297c17e24a88ee1f2e
Amanda Alves Branquinho <amanda@discourse.org>
2024-11-20 19:22:02 -0300

    DEV: add outlet wrapper for small user list (#29763)
    * DEV: add outlet wrapper for small user list
    
    * DEV: use value transformer to extend small user attrs function
    
    * Update app/assets/javascripts/discourse/app/components/small-user-list.gjs
    
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>
    
    ---------
    
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>

--- app/assets/javascripts/discourse/app/lib/transformer/registry.js
+++ app/assets/javascripts/discourse/app/lib/transformer/registry.js
@@ -2,4 +2,10 @@ export const VALUE_TRANSFORMERS = Object.freeze([
   // use only lowercase names
   "header-notifications-avatar-size",
   "home-logo-href",
+  "home-logo-image-url",
+  "invite-simple-mode-topic",
+  "mentions-class",
+  "more-topics-tabs",
+  "post-menu-buttons",
+  "small-user-attrs",
 ]);
