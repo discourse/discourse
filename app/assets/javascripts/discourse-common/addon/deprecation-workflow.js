76c01034127161b0d6142137da4a51cad46af536
David Taylor <david@taylorhq.com>
2024-11-20 14:36:24 +0000

    DEV: Unsilence d-section deprecation

--- app/assets/javascripts/discourse-common/addon/deprecation-workflow.js
+++ app/assets/javascripts/discourse-common/addon/deprecation-workflow.js
@@ -4,7 +4,6 @@ const DEPRECATION_WORKFLOW = [
     matchId: "ember-this-fallback.this-property-fallback",
   },
   { handler: "silence", matchId: "discourse.select-kit" },
-  { handler: "silence", matchId: "discourse.d-section" },
   {
     handler: "silence",
     matchId: "discourse.decorate-widget.hamburger-widget-links",
