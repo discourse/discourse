e6fdfcdcd25d4bf7f009079cace41d0e9ffe4791
Penar Musaraj <pmusaraj@gmail.com>
2024-11-25 10:49:40 -0500

    DEV: Remove `experimental_topics_filter` setting (#29902)

--- spec/requests/list_controller_spec.rb
+++ spec/requests/list_controller_spec.rb
@@ -1321,8 +1321,6 @@ RSpec.describe ListController do
     fab!(:private_message_topic)
     fab!(:topic_in_private_category) { Fabricate(:topic, category: private_category) }
 
-    before { SiteSetting.experimental_topics_filter = true }
-
     it "should not return topics that the user is not allowed to view" do
       sign_in(user)
 
@@ -1343,16 +1341,6 @@ RSpec.describe ListController do
       expect(
         response.parsed_body["topic_list"]["topics"].map { |topic| topic["id"] },
       ).to contain_exactly(topic.id)
-    end
-
-    it "should respond with 404 response code when `experimental_topics_filter` site setting has not been enabled" do
-      SiteSetting.experimental_topics_filter = false
-
-      sign_in(user)
-
-      get "/filter.json"
-
-      expect(response.status).to eq(404)
     end
 
     it "returns category definition topics if `show_category_definitions_in_topic_lists` site setting is enabled" do
