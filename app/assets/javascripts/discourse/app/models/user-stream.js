d47964d992390c492e8d63da10888636b84c3e24
David Taylor <david@taylorhq.com>
2024-11-25 11:31:23 +0000

    SECURITY: Ensure user-stream topic titles are always escaped correctly
    In core, `escapeExpression` was being applied during the model loading phase. However, plugin consumers of the UserStreamItem component were not necessarily doing the same.
    
    This commit moves the emoji-replacement logic (which also safely handles escaping) into the component template, so that it is safe-by-default, regardless of how it's used by plugins.

--- app/assets/javascripts/discourse/app/models/user-stream.js
+++ app/assets/javascripts/discourse/app/models/user-stream.js
@@ -2,8 +2,6 @@ import { A } from "@ember/array";
 import { Promise } from "rsvp";
 import { ajax } from "discourse/lib/ajax";
 import { url } from "discourse/lib/computed";
-import { emojiUnescape } from "discourse/lib/text";
-import { escapeExpression } from "discourse/lib/utilities";
 import RestModel from "discourse/models/rest";
 import Site from "discourse/models/site";
 import UserAction from "discourse/models/user-action";
@@ -117,7 +115,6 @@ export default class UserStream extends RestModel {
           });
 
           result.user_actions.forEach((action) => {
-            action.title = emojiUnescape(escapeExpression(action.title));
             copy.pushObject(UserAction.create(action));
           });
 
