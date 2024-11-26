db1e162e7d841571693223257acf33001d687db4
Keegan George <kgeorge13@gmail.com>
2024-11-26 07:40:02 -0800

    DEV: Remove complexity and add filter in title token

--- app/assets/javascripts/discourse/app/routes/discovery-filter.js
+++ app/assets/javascripts/discourse/app/routes/discovery-filter.js
@@ -1,30 +1,12 @@
 import { setTopicList } from "discourse/lib/topic-list-tracker";
+import { escapeExpression } from "discourse/lib/utilities";
 import DiscourseRoute from "discourse/routes/discourse";
 import { i18n } from "discourse-i18n";
 
 export default class DiscoveryFilterRoute extends DiscourseRoute {
   queryParams = {
-    q: { replace: true, refreshModel: true },
+    q: { refreshModel: true },
   };
-
-  comingFromDifferentRoute = true;
-
-  beforeModel(transition) {
-    if (transition.from && transition.from.name !== this.routeName) {
-      this.comingFromDifferentRoute = true;
-    } else {
-      this.comingFromDifferentRoute = false;
-    }
-
-    super.beforeModel(...arguments);
-  }
-
-  setupController(controller, model) {
-    super.setupController(controller, model);
-
-    const qParamConfig = this.queryParams.q;
-    qParamConfig.replace = !this.comingFromDifferentRoute;
-  }
 
   async model(data) {
     const list = await this.store.findFiltered("topicList", {
@@ -38,7 +20,7 @@ export default class DiscoveryFilterRoute extends DiscourseRoute {
   }
 
   titleToken() {
-    const filterText = i18n("filters.filter.title");
-    return i18n("filters.with_topics", { filter: filterText });
+    const query = this.paramsFor(this.routeName).q;
+    return i18n("filters.filter.title", { filter: escapeExpression(query) });
   }
 }
