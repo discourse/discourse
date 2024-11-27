43414abf834306d2fc5d4840bf8486c16db7b8b3
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-11-27 14:16:29 -0300

    FIX: `TopicTagsChanged` trigger not working with multiple tags (#29961)
    * FIX: `TopicTagsChanged` trigger not working with multiple tags
    
    The check for when had multiple tags was being exclusive, you had to have all tags to trigger the automation, now it's inclusive, you can trigger the automation if you have any of the tags.
    
    * DEV: add specs for when having multiple categories
    
    * DEV: changed to use unions and intersections for tags
    
    added more tests to check for multiple tags

--- plugins/automation/lib/discourse_automation/event_handlers.rb
+++ plugins/automation/lib/discourse_automation/event_handlers.rb
@@ -239,12 +239,8 @@ module DiscourseAutomation
           watching_tags = automation.trigger_field("watching_tags")
 
           if watching_tags["value"]
-            should_skip = false
-            watching_tags["value"].each do |tag|
-              should_skip = true if !removed_tags.empty? && !removed_tags.include?(tag)
-              should_skip = true if !added_tags.empty? && !added_tags.include?(tag)
-            end
-            next if should_skip
+            changed_tags = (removed_tags | added_tags)
+            next if (changed_tags & watching_tags["value"]).empty?
           end
 
           automation.trigger!(
