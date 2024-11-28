bcf5d3c617047f8fe138211b5d48c22fcf5cc449
Renato Atilio <renato@discourse.org>
2024-11-28 17:11:01 -0300

    DEV: refactor placeholder handling from UppyComposerUpload

--- app/assets/javascripts/discourse/app/components/composer-editor.js
+++ app/assets/javascripts/discourse/app/components/composer-editor.js
@@ -200,6 +200,7 @@ export default class ComposerEditor extends Component {
   @bind
   setupEditor(textManipulation) {
     this.textManipulation = textManipulation;
+    this.uppyComposerUpload.textManipulation = textManipulation;
 
     const input = this.element.querySelector(".d-editor-input");
 
