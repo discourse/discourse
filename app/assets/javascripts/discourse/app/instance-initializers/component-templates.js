f1951d206eb77d53d575f7c18ade75ae2401bacf
Sérgio Saquetim <saquetim@discourse.org>
2024-12-02 16:41:54 -0300

    Patched the colocated template resolution

--- app/assets/javascripts/discourse/app/instance-initializers/component-templates.js
+++ app/assets/javascripts/discourse/app/instance-initializers/component-templates.js
@@ -4,8 +4,6 @@ import { isTesting } from "discourse-common/config/environment";
 import deprecated from "discourse-common/lib/deprecated";
 import DiscourseTemplateMap from "discourse-common/lib/discourse-template-map";
 
-const COLOCATED_TEMPLATE_OVERRIDES = new Map();
-
 let THROW_GJS_ERROR = isTesting();
 
 /** For use in tests/integration/component-templates-test only */
@@ -13,16 +11,9 @@ export function overrideThrowGjsError(value) {
   THROW_GJS_ERROR = value;
 }
 
+// We're using a patched version of Ember with a modified GlimmerManager to make the code below work.
 // This patch is not ideal, but Ember does not allow us to change a component template after initial association
 // https://github.com/glimmerjs/glimmer-vm/blob/03a4b55c03/packages/%40glimmer/manager/lib/public/template.ts#L14-L20
-const originalGetTemplate = GlimmerManager.getComponentTemplate;
-// eslint-disable-next-line no-import-assign
-GlimmerManager.getComponentTemplate = (component) => {
-  return (
-    COLOCATED_TEMPLATE_OVERRIDES.get(component) ??
-    originalGetTemplate(component)
-  );
-};
 
 export default {
   after: ["populate-template-map", "mobile"],
@@ -60,7 +51,9 @@ export default {
         return;
       }
 
-      const originalTemplate = originalGetTemplate(component);
+      // patched function: doesn't exist on og GlimmerManager. it corresponds to the og GlimmerManager.getComponentTemplate
+      const originalTemplate =
+        GlimmerManager.getOriginalComponentTemplate(component);
       const isStrictMode = originalTemplate?.()?.parsedLayout?.isStrictMode;
       const finalOverrideModuleName = moduleNames[moduleNames.length - 1];
 
@@ -85,7 +78,11 @@ export default {
 
         const overrideTemplate = require(finalOverrideModuleName).default;
 
-        COLOCATED_TEMPLATE_OVERRIDES.set(component, overrideTemplate);
+        // patched function: doesn't exist on og GlimmerManager
+        GlimmerManager.setColocatedTemplateOverride(
+          component,
+          overrideTemplate
+        );
       }
     });
   },
@@ -112,6 +109,7 @@ export default {
   },
 
   teardown() {
-    COLOCATED_TEMPLATE_OVERRIDES.clear();
+    // patched function: doesn't exist on og GlimmerManager
+    GlimmerManager.clearColocatedTemplateOverrides();
   },
 };
