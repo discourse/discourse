f6083a3e7bcf7836ab55a23c12668971664d37fa
Kelvin Tan <kelv@discourse.org>
2024-12-05 13:25:50 +0800

    revert icon-library test change, deprecation message expected there

--- app/assets/javascripts/discourse/tests/unit/lib/icon-library-test.js
+++ app/assets/javascripts/discourse/tests/unit/lib/icon-library-test.js
@@ -22,7 +22,7 @@ module("Unit | Utility | icon-library", function (hooks) {
   });
 
   test("convert icon names", function (assert) {
-    const faIcon = convertIconClass("fab-facebook");
+    const faIcon = convertIconClass("fab fa-facebook");
     assert.true(iconHTML(faIcon).includes("fab-facebook"), "FA syntax");
 
     const iconC = convertIconClass("  fab fa-facebook  ");
