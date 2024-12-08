2604be67e4ff6c1ca51864cb4970bce090988a9e
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-12-08 22:11:38 +0100

    FIX: ensures sk can be scrolled on iOS in a modal
    The modal was disabling body scroll lock and select-kit collection was not whitelisted which was preventing users to be able to scroll a select-kit collection on iOS.

--- app/assets/javascripts/select-kit/addon/components/select-kit/select-kit-collection.js
+++ app/assets/javascripts/select-kit/addon/components/select-kit/select-kit-collection.js
@@ -1,5 +1,35 @@
 import Component from "@ember/component";
+import { service } from "@ember/service";
 import { tagName } from "@ember-decorators/component";
+import { modifier } from "ember-modifier";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+} from "discourse/lib/body-scroll-lock";
 
 @tagName("")
-export default class SelectKitCollection extends Component {}
+export default class SelectKitCollection extends Component {
+  @service site;
+
+  bodyScrollLock = modifier((element) => {
+    if (!this.site.mobileView) {
+      return;
+    }
+
+    // when opened a modal will disable all scroll but itself
+    // this code is whitelisting the collection to ensure it can be scrolled in this case
+    // however we only want to do this if the modal is open to avoid breaking the scroll on the page
+    // eg: opening a combobox under a topic shouldn't prevent you to scroll the topic page
+    const isModalOpen =
+      document.documentElement.classList.contains("modal-open");
+    if (!isModalOpen) {
+      return;
+    }
+
+    disableBodyScroll(element);
+
+    return () => {
+      enableBodyScroll(element);
+    };
+  });
+}
