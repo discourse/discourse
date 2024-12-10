13edb5b3a689ed7bcbabc5a21aa0751b6245eed8
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2024-12-10 12:28:40 +1100

    FIX: remove uneccessary logic to hide admin header

--- app/assets/javascripts/admin/addon/controllers/admin-embedding.js
+++ app/assets/javascripts/admin/addon/controllers/admin-embedding.js
@@ -1,14 +1,3 @@
 import Controller from "@ember/controller";
-import { service } from "@ember/service";
 
-export default class AdminEmbeddingController extends Controller {
-  @service router;
-  get showHeader() {
-    return [
-      "adminEmbedding.crawlers",
-      "adminEmbedding.index",
-      "adminEmbedding.postsAndTopics",
-      "adminEmbedding.settings",
-    ].includes(this.router.currentRouteName);
-  }
-}
+export default class AdminEmbeddingController extends Controller {}
