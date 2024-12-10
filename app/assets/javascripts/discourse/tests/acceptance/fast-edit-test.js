a4aa36762cab4964967fd91b2df6325d4cc3743c
zogstrip <regis@hanol.fr>
2024-12-10 11:49:47 +0100

    FIX: fast edit with a typographic character
    When a post containing an apostrophe (') is being cooked, the apostrophe is being converted to the "typographic" version (’) (because we enable markdown-it's **typographer** mode by default in Discourse)
    
    When you select text that contains such apostrophe and then try to save your fast edit, it fails miserably without any error.
    
    That's because when you select text from the DOM, it uses the cooked version which has the typographic apostrophe.
    
    When you save your fast edit, we fetch the raw version of the post, which has the "regular" apostrophe. Thus doing `raw.replace(selectedText, editedText)` doesn't work because `raw` has the regular apostrophe but `selectedText` has the typographic apostrophe.
    
    Since it's somewhat complicated to handle all typographic characters, we would basically have to reverse the process done in `custom-typographer-replacements.js`, we instead bail out and show the composer when we detect such character in the selection.
    
    Internal ref - t/143836

--- app/assets/javascripts/discourse/tests/acceptance/fast-edit-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/fast-edit-test.js
@@ -11,9 +11,7 @@ import { cloneJSON } from "discourse-common/lib/object";
 
 acceptance("Fast Edit", function (needs) {
   needs.user();
-  needs.settings({
-    enable_fast_edit: true,
-  });
+  needs.settings({ enable_fast_edit: true });
   needs.pretender((server, helper) => {
     server.get("/posts/419", () => {
       return helper.response(cloneJSON(postFixtures["/posts/398"]));
@@ -78,6 +76,19 @@ acceptance("Fast Edit", function (needs) {
     await visit("/t/internationalization-localization/280");
 
     const textNode = query("#post_2 .cooked");
+
+    await selectText(textNode);
+    await click(".quote-button .quote-edit-label");
+
+    assert.dom("#fast-edit-input").doesNotExist();
+    assert.dom(".d-editor-input").exists();
+  });
+
+  test("Opens full composer when selection has typographic characters", async function (assert) {
+    await visit("/t/internationalization-localization/280");
+
+    query("#post_2 .cooked").append(`That’s what she said!`);
+    const textNode = query("#post_2 .cooked").childNodes[2];
 
     await selectText(textNode);
     await click(".quote-button .quote-edit-label");
