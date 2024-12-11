15059785862d9170973b1c09e2bd682ab3a435b5
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2024-12-11 11:09:25 -0300

    DEV: Upgrade dependencies to Ember 5.12 (#30131)

--- app/assets/javascripts/discourse-common/addon/deprecation-workflow.js
+++ app/assets/javascripts/discourse-common/addon/deprecation-workflow.js
@@ -1,4 +1,6 @@
 const DEPRECATION_WORKFLOW = [
+  { handler: "silence", matchId: "template-action" }, // will be removed in Ember 6.0
+  { handler: "silence", matchId: "deprecate-array-prototype-extensions" }, // will be removed in Ember 6.0
   { handler: "silence", matchId: "discourse.select-kit" },
   {
     handler: "silence",
