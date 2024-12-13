ae21b8f4121338bb566860b838394912f703b2e5
Penar Musaraj <pmusaraj@gmail.com>
2024-12-13 00:13:34 -0500

    skip put-cursor-to-end for edits > 150 chars only

--- app/assets/javascripts/discourse/app/lib/put-cursor-at-end.js
+++ app/assets/javascripts/discourse/app/lib/put-cursor-at-end.js
@@ -3,13 +3,16 @@ import { helperContext } from "discourse-common/lib/helpers";
 export default function (element) {
   const caps = helperContext().capabilities;
 
-  if (caps.isIOS) {
+  const len = element.value.length;
+
+  // don't scroll to end of element with a lot of text in iOS
+  // it causes composer container to go out of viewport
+  if (caps.isIOS && element.tagName === "TEXTAREA" && len > 150) {
     return;
   }
 
   element.focus();
 
-  const len = element.value.length;
   element.setSelectionRange(len, len);
 
   // Scroll to the bottom, in case we're in a tall textarea
