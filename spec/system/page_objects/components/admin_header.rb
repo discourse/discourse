eae7aa661acd9315e0566976d80806d59abf4e08
Martin Brennan <martin@discourse.org>
2024-12-13 10:59:54 +1000

    DEV: Add back AdminHeader page object for now

---
+++ spec/system/page_objects/components/admin_header.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Components
+    # TODO (martin) Delete this after plugins have been updated to use DPageHeader
+    class AdminHeader < PageObjects::Pages::Base
+      def has_tabs?(names)
+        expect(page.all(".d-nav-submenu__tabs a").map(&:text)).to eq(names)
+      end
+
+      def visible?
+        has_css?(".admin-page-header")
+      end
+
+      def hidden?
+        has_no_css?(".admin-page-header")
+      end
+    end
+  end
+end
