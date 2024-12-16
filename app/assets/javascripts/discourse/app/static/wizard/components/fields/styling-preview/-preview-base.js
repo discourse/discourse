52a38f358120ea265bd1fcdad2d6180848e786d5
Martin Brennan <martin@discourse.org>
2024-12-16 15:24:22 +1000

    UX: Move hamburger to left and add chat icon in wizard preview

--- app/assets/javascripts/discourse/app/static/wizard/components/fields/styling-preview/-preview-base.js
+++ app/assets/javascripts/discourse/app/static/wizard/components/fields/styling-preview/-preview-base.js
@@ -302,9 +302,12 @@ export default class PreviewBase extends Component {
     const hamburgerIcon = new Path2D(
       "M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
     );
+    const chatIcon = new Path2D(
+      "M512 240c0 114.9-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6C73.6 471.1 44.7 480 16 480c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c0 0 0 0 0 0s0 0 0 0s0 0 0 0c0 0 0 0 0 0l.3-.3c.3-.3 .7-.7 1.3-1.4c1.1-1.2 2.8-3.1 4.9-5.7c4.1-5 9.6-12.4 15.2-21.6c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208z"
+    );
     ctx.save(); // Save the previous state for translation and scale
     ctx.translate(
-      this.width - avatarSize * 3 - headerMargin * 0.5,
+      this.width - avatarSize * 2 - headerMargin * 0.5,
       avatarSize / 2
     );
     // need to scale paths otherwise they're too large
@@ -313,9 +316,14 @@ export default class PreviewBase extends Component {
     ctx.restore();
     ctx.save();
     ctx.translate(
-      this.width - avatarSize * 2 - headerMargin * 0.5,
+      this.width - avatarSize * 3 - headerMargin * 0.5,
       avatarSize / 2
     );
+    ctx.scale(pathScale, pathScale);
+    ctx.fill(chatIcon);
+    ctx.restore();
+    ctx.save();
+    ctx.translate(headerMargin * 1.75, avatarSize / 2);
     ctx.scale(pathScale, pathScale);
     ctx.fill(hamburgerIcon);
     ctx.restore();
