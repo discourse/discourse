638db46d6003056f1de0d7ab9aff1eeea9ed170e
Jordan Vidrine <jordan@jordanvidrine.com>
2024-12-17 10:16:27 -0600

    test

--- app/assets/javascripts/discourse/tests/acceptance/password-reset-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/password-reset-test.js
@@ -70,17 +70,6 @@ acceptance("Password Reset", function (needs) {
     await fillIn(".password-reset input", "perf3ctly5ecur3");
     assert.dom(".password-reset .tip.good").exists("input looks good");
 
-    await fillIn(".password-reset input", "123");
-    await triggerKeyEvent("#new-account-password", "keypress", 9);
-
-    assert.dom(".password-reset .tip.bad").exists("input is not valid");
-    assert.dom(".password-reset .tip.bad").includesHtml(
-      i18n("user.password.too_short", {
-        count: this.siteSettings.min_password_length,
-      }),
-      "password too short"
-    );
-
     await fillIn(".password-reset input", "jonesyAlienSlayer");
     await click(".password-reset form button[type='submit']");
     assert.dom(".password-reset .tip.bad").exists("input is not valid");
