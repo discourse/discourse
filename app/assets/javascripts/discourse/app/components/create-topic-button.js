284fe89165d3a355dc62833beaf63d1db7bd67f2
Kris <kris.aubuchon@discourse.org>
2024-12-17 15:17:24 -0500

    UX: show correct message when creating topics is disabled by a tag (#30328)

--- app/assets/javascripts/discourse/app/components/create-topic-button.js
+++ app/assets/javascripts/discourse/app/components/create-topic-button.js
@@ -5,4 +5,12 @@ import { tagName } from "@ember-decorators/component";
 export default class CreateTopicButton extends Component {
   label = "topic.create";
   btnClass = "btn-default";
+
+  get disallowedReason() {
+    if (this.canCreateTopicOnTag === false) {
+      return "topic.create_disabled_tag";
+    } else if (this.disabled) {
+      return "topic.create_disabled_category";
+    }
+  }
 }
