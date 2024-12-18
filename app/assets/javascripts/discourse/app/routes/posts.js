65e88e68e373c51e2b07aeda7cdc7df3deb26dc7
Keegan George <kgeorge13@gmail.com>
2024-12-18 11:23:34 -0800

    fix

--- app/assets/javascripts/discourse/app/routes/posts.js
+++ app/assets/javascripts/discourse/app/routes/posts.js
@@ -1,6 +1,6 @@
 import { service } from "@ember/service";
 import DiscourseRoute from "discourse/routes/discourse";
 
-export default class Post extends DiscourseRoute {
+export default class Posts extends DiscourseRoute {
   @service router;
 }
