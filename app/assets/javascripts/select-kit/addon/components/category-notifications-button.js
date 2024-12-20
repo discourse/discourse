a06b2969aae54d1e87a36cb72994784cc52c7bbc
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-12-20 15:50:13 +0100

    DEV: reintroduces category-notifications-button.js
    This file has been incorrectly removed in https://github.com/discourse/discourse/commit/41df7051883dca8404c38e151294058ec8525b83 while it's still being used by plugins (https://github.com/discourse/discourse-circles).

---
+++ app/assets/javascripts/select-kit/addon/components/category-notifications-button.js
@@ -0,0 +1,15 @@
+import { readOnly } from "@ember/object/computed";
+import { classNames } from "@ember-decorators/component";
+import { i18n } from "discourse-i18n";
+import NotificationOptionsComponent from "select-kit/components/notifications-button";
+import { pluginApiIdentifiers, selectKitOptions } from "./select-kit";
+@selectKitOptions({
+  i18nPrefix: "category.notifications",
+  showFullTitle: false,
+  headerAriaLabel: i18n("category.notifications.title"),
+})
+@pluginApiIdentifiers(["category-notifications-button"])
+@classNames("category-notifications-button")
+export default class CategoryNotificationsButton extends NotificationOptionsComponent {
+  @readOnly("category.deleted") isHidden;
+}
