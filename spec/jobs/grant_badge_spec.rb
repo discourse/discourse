f0051b463bfe668a0b8209998a0df38b5dabb8c7
Bianca Nenciu <nenciu.bianca@gmail.com>
2024-12-24 11:06:34 +0200

    Apply suggestions

--- spec/jobs/grant_badge_spec.rb
+++ spec/jobs/grant_badge_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Jobs::GrantBadge do
   subject(:job) { described_class.new }
 
   it "schedules a EnsureBadgeConsistency job" do
-    # Keep only 2 enabled badges to be fast
+    # Keep the test fast by only enabling 2 badges
     badge_ids = Badge.enabled.limit(2).pluck(:id)
     Badge.where.not(id: badge_ids).update_all(enabled: false)
 
-    # Ensures it starts a new batch of `GrantBadge` jobs
+    # Ensures it starts a new batch of GrantBadge jobs
     Jobs::GrantAllBadges.new.execute({})
     expect(Jobs::GrantBadge.jobs.map { |job| job["args"][0]["badge_id"] }).to eq(badge_ids)
 
