88907f5e66dd8ac030c6ce87e7ef32ddad5943b0
Kelvin Tan <kelv@discourse.org>
2024-12-31 14:38:15 +0800

    DEV: refactor singleton class decorator to directly mutate target class and add tests

--- app/assets/javascripts/discourse/app/lib/singleton.js
+++ app/assets/javascripts/discourse/app/lib/singleton.js
@@ -22,39 +22,58 @@
  *
  * // Multiple calls return the same instance
  * UserSettings.current() === UserSettings.current(); // true
+ *
+ * // If you want to customize what logic is executed during creation of the singleton, redefine the `createCurrent` method:
+ * @singleton
+ * class UserSettings {
+ *   theme = 'light';
+ *
+ *   toggleTheme() {
+ *     this.theme = this.theme === 'light' ? 'dark' : 'light';
+ *   }
+ *
+ *   static createCurrent() {
+ *     return this.create({ font: 'Comic-Sans' });
+ *   }
+ * }
+ *
+ * UserSettings.currentProp('font'); // 'Comic-Sans'
  * ```
  */
-export default function singleton(target) {
-  return class SingletonClass extends target {
-    static current() {
-      if (!this._current) {
-        this._current = this.createCurrent();
-      }
-      return this._current;
-    }
+export default function singleton(targetKlass) {
+  targetKlass._current = null;
 
-    static createCurrent() {
+  // check ensures that we don't overwrite a customized createCurrent
+  if (!targetKlass.createCurrent) {
+    targetKlass.createCurrent = function () {
       return this.create();
+    };
+  }
+
+  targetKlass.current = function () {
+    if (!this._current) {
+      this._current = this.createCurrent();
     }
-
-    static currentProp(property, value) {
-      const instance = this.current();
-      if (!instance) {
-        return;
-      }
-
-      if (typeof value !== "undefined") {
-        instance[property] = value;
-        return value;
-      }
-      return instance[property];
-    }
-
-    static resetCurrent(val) {
-      this._current = val;
-      return val;
-    }
-
-    static _current = null;
+    return this._current;
   };
+
+  targetKlass.currentProp = function (property, value) {
+    const instance = this.current();
+    if (!instance) {
+      return;
+    }
+
+    if (typeof value !== "undefined") {
+      instance[property] = value;
+      return value;
+    }
+    return instance[property];
+  };
+
+  targetKlass.resetCurrent = function (val) {
+    this._current = val;
+    return val;
+  };
+
+  return targetKlass;
 }
