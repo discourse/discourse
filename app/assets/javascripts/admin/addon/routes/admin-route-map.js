f886af76684ec44af086da04d36eb8346f6cb55c
Gary <gary@pento.net>
2025-01-06 14:43:10 +1100

    Create a logo settings page, based on the existing branding category.

--- app/assets/javascripts/admin/addon/routes/admin-route-map.js
+++ app/assets/javascripts/admin/addon/routes/admin-route-map.js
@@ -262,6 +262,9 @@ export default function () {
             this.route("settings");
           }
         );
+        this.route("logo", function () {
+          this.route("settings", { path: "/" });
+        });
       }
     );
 
--- config/routes.rb
+++ config/routes.rb
@@ -394,6 +394,7 @@ Discourse::Application.routes.draw do
         resources :site_settings, only: %i[index]
 
         get "login-and-authentication" => "site_settings#index"
+        get "logo" => "site_settings#index"
         get "notifications" => "site_settings#index"
 
         resources :flags, only: %i[index new create update destroy] do
