f98ee310778aed6ea8ea316c080ffebbf211b992
Gary <gary@pento.net>
2025-01-07 11:13:24 +1100

    Missed a string update.

--- app/assets/javascripts/admin/addon/routes/admin-config-security.js
+++ app/assets/javascripts/admin/addon/routes/admin-config-security.js
@@ -1,8 +1,8 @@
 import DiscourseRoute from "discourse/routes/discourse";
 import { i18n } from "discourse-i18n";
 
-export default class AdminConfigLogoRoute extends DiscourseRoute {
+export default class AdminConfigSecurityRoute extends DiscourseRoute {
   titleToken() {
-    return i18n("admin.appearance.sidebar_link.site_logo");
+    return i18n("admin.security.sidebar_link.security");
   }
 }
