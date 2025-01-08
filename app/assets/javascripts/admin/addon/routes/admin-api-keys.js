e877e559cf72ad108743d09634626dff622cf81f
Mark VanLandingham <markvanlan@gmail.com>
2025-01-08 10:45:27 -0600

    Revert "DEV: Convert admin API keys to conform to UI guidelines (#30096)"
    This reverts commit d9ddc258089da0f6cd8cf75af385c8a59eeccac3.

--- app/assets/javascripts/admin/addon/routes/admin-api-keys.js
+++ app/assets/javascripts/admin/addon/routes/admin-api-keys.js
@@ -1,3 +1,17 @@
+import { action } from "@ember/object";
 import Route from "@ember/routing/route";
+import { service } from "@ember/service";
 
-export default class AdminApiKeysRoute extends Route {}
+export default class AdminApiKeysRoute extends Route {
+  @service router;
+
+  @action
+  show(apiKey) {
+    this.router.transitionTo("adminApiKeys.show", apiKey.id);
+  }
+
+  @action
+  new() {
+    this.router.transitionTo("adminApiKeys.new");
+  }
+}
