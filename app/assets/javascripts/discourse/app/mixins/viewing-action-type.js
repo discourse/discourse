2d7ec97131173dccd1640b31ee25d79486edc95f
Kelvin Tan <kelv@discourse.org>
2025-01-10 16:26:09 +0800

    DEV: stop setting userActionType on user controller; appears to be unused for a long time

--- app/assets/javascripts/discourse/app/mixins/viewing-action-type.js
+++ app/assets/javascripts/discourse/app/mixins/viewing-action-type.js
@@ -1,6 +1,5 @@
 export default {
   viewingActionType(userActionType) {
-    this.controllerFor("user").set("userActionType", userActionType);
     this.controllerFor("user-activity").set("userActionType", userActionType);
   },
 };
