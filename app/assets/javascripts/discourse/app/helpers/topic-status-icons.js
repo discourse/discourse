30f796c48e6d39d75c373aac4dc549fa929ce18d
Jarek Radosz <jradosz@gmail.com>
2025-01-14 00:24:55 +0100

    deprecation

--- app/assets/javascripts/discourse/app/helpers/topic-status-icons.js
+++ app/assets/javascripts/discourse/app/helpers/topic-status-icons.js
@@ -1,8 +1,15 @@
+import deprecated from "discourse/lib/deprecated";
+import { RAW_TOPIC_LIST_DEPRECATION_OPTIONS } from "discourse/lib/plugin-api";
+
 const TopicStatusIcons = new (class {
   entries = [];
 
   addObject(entry) {
-    // DEPRECATE
+    deprecated(
+      "TopicStatusIcons is deprecated. Use 'after-topic-status' plugin outlet instead.",
+      RAW_TOPIC_LIST_DEPRECATION_OPTIONS
+    );
+
     const [attribute, iconName, titleKey] = entry;
     this.entries.push({ attribute, iconName, titleKey });
   }
