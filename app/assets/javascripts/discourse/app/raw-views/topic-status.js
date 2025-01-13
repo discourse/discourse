ed5247efb2c719c249ec713de9dacf5de8baa95d
Jarek Radosz <jradosz@gmail.com>
2025-01-13 20:27:17 +0100

    remove hbr impl

--- app/assets/javascripts/discourse/app/raw-views/topic-status.js
+++ app/assets/javascripts/discourse/app/raw-views/topic-status.js
@@ -1,10 +1,8 @@
 import EmberObject from "@ember/object";
 import TopicStatus from "discourse/components/topic-status";
-import discourseComputed from "discourse/lib/decorators";
 import deprecated from "discourse/lib/deprecated";
 import { RAW_TOPIC_LIST_DEPRECATION_OPTIONS } from "discourse/lib/plugin-api";
 import rawRenderGlimmer from "discourse/lib/raw-render-glimmer";
-import { i18n } from "discourse-i18n";
 
 export default class RawTopicStatus extends EmberObject {
   static reopen() {
@@ -23,42 +21,6 @@ export default class RawTopicStatus extends EmberObject {
     );
 
     return super.reopenClass(...arguments);
-  }
-
-  @discourseComputed
-  renderDiv() {
-    return this.statuses.length > 0;
-  }
-
-  @discourseComputed
-  statuses() {
-    const topic = this.topic;
-    const results = [];
-
-    // TODO: implement in gjs variant?
-    if (topic.bookmarked) {
-      const postNumbers = topic.bookmarked_post_numbers;
-      let url = topic.url;
-      let extraClasses = "";
-      if (postNumbers && postNumbers[0] > 1) {
-        url += `/${postNumbers[0]}`;
-      } else {
-        extraClasses = "op-bookmark";
-      }
-
-      results.push({
-        extraClasses,
-        icon: "bookmark",
-        key: "bookmarked",
-        href: url,
-      });
-    }
-
-    results.forEach((result) => {
-      result.title = i18n(`topic_statuses.${result.key}.help`);
-    });
-
-    return results;
   }
 
   get html() {
