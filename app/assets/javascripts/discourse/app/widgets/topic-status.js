6cb8b576d0c7902b99cbb39bc7aaa473d454118a
Jarek Radosz <jradosz@gmail.com>
2025-01-14 13:46:14 +0100

    DEV: Modernize topic-status (#30754)
    Deprecates `TopicStatusIcons`. Use "after-topic-status" plugin outlet instead.
    
    Replaces both the widget and gjs header implementations with a new gjs topic-status component.

--- app/assets/javascripts/discourse/app/widgets/topic-status.js
+++ app/assets/javascripts/discourse/app/widgets/topic-status.js
@@ -1,40 +1,12 @@
-import { h } from "virtual-dom";
-import TopicStatusIcons from "discourse/helpers/topic-status-icons";
-import { iconNode } from "discourse/lib/icon-library";
-import { escapeExpression } from "discourse/lib/utilities";
-import { createWidget } from "discourse/widgets/widget";
-import { i18n } from "discourse-i18n";
+import { hbs } from "ember-cli-htmlbars";
+import { registerWidgetShim } from "discourse/widgets/render-glimmer";
 
-export default createWidget("topic-status", {
-  tagName: "span.topic-statuses",
-
-  html(attrs) {
-    const topic = attrs.topic;
-    const canAct = this.currentUser && !attrs.disableActions;
-
-    const result = [];
-
-    TopicStatusIcons.render(topic, function (name, key) {
-      const iconArgs = { class: key === "unpinned" ? "unpinned" : null };
-      const icon = iconNode(name, iconArgs);
-
-      const attributes = {
-        title: escapeExpression(i18n(`topic_statuses.${key}.help`)),
-      };
-      let klass = "topic-status";
-      if (key === "unpinned" || key === "pinned") {
-        klass += `.pin-toggle-button.${key}`;
-      }
-      result.push(h(`${canAct ? "a" : "span"}.${klass}`, attributes, icon));
-    });
-
-    return result;
-  },
-
-  click(e) {
-    const parent = e.target.closest(".topic-statuses");
-    if (parent?.querySelector(".pin-toggle-button")?.contains(e.target)) {
-      this.attrs.topic.togglePinnedForUser();
-    }
-  },
-});
+registerWidgetShim(
+  "topic-status",
+  "span.topic-statuses",
+  hbs`<TopicStatus
+    @topic={{@data.topic}}
+    @disableActions={{@data.disableActions}}
+    @tagName=""
+  />`
+);
