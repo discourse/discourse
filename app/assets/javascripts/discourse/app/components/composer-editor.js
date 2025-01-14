eb64db828e116edbacdf10266f8102ed7028a2f2
Renato Atilio <renato@discourse.org>
2025-01-14 10:28:46 -0300

    DEV: move d-composer-position to setupEditor (#30717)
    Small refactor to move the `DComposerPosition` component to be a
    function used on the `setupEditor` editor lifecycle, so it's
    recycled/re-added whenever the editor component is mounted.
    
    Additionally, we reference the passed `editor` instead of the
    `event.target`, allowing a `contentEditable` editor (which is a DOM
    tree) to still work with the positioning hack.

--- app/assets/javascripts/discourse/app/components/composer-editor.js
+++ app/assets/javascripts/discourse/app/components/composer-editor.js
@@ -10,6 +10,7 @@ import $ from "jquery";
 import { resolveAllShortUrls } from "pretty-text/upload-short-url";
 import { ajax } from "discourse/lib/ajax";
 import { tinyAvatar } from "discourse/lib/avatar-utils";
+import { setupComposerPosition } from "discourse/lib/composer/composer-position";
 import discourseComputed, { bind, debounce } from "discourse/lib/decorators";
 import {
   fetchUnseenHashtagsInContext,
@@ -200,18 +201,19 @@ export default class ComposerEditor extends Component {
 
     const input = this.element.querySelector(".d-editor-input");
 
-    input?.addEventListener(
-      "scroll",
-      this._throttledSyncEditorAndPreviewScroll
-    );
+    input.addEventListener("scroll", this._throttledSyncEditorAndPreviewScroll);
 
     // Focus on the body unless we have a title
     if (!this.get("composer.model.canEditTitle")) {
       this.textManipulation.putCursorAtEnd();
     }
 
+    const destroyComposerPosition = setupComposerPosition(input);
+
     return () => {
-      input?.removeEventListener(
+      destroyComposerPosition();
+
+      input.removeEventListener(
         "scroll",
         this._throttledSyncEditorAndPreviewScroll
       );
