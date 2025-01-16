5440b1dd009b5c83e75b52acdbea1019b1a8acf9
Gary <gary@pento.net>
2025-01-16 13:49:31 +1100

    FIX: At a DB migration to update the type of the font site settings.

---
+++ db/migrate/20250116024516_update_font_site_settings_type.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+class UpdateFontSiteSettingsType < ActiveRecord::Migration[7.2]
+  def up
+    execute "UPDATE site_settings SET data_type=8 WHERE name IN('base_font', 'heading_font')"
+  end
+
+  def down
+    execute "UPDATE site_settings SET data_type=1 WHERE name IN('base_font', 'heading_font')"
+  end
+end
