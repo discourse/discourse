d3a7b996992e721dd9caab60095414eaf1fc832f
David Taylor <david@taylorhq.com>
2025-01-16 17:55:56 +0000

    DEV: Relax 'unexpectedly found' matcher in deprecation-silencer (#30823)
    We managed to find a slightly different way to trigger the same Ember
    bug. This commit makes sure that's silenced as well.

--- app/assets/javascripts/deprecation-silencer/index.js
+++ app/assets/javascripts/deprecation-silencer/index.js
@@ -1,6 +1,6 @@
 const SILENCED_WARN_PREFIXES = [
   "Setting the `jquery-integration` optional feature flag",
-  'unexpectedly found "!', // https://github.com/emberjs/ember.js/issues/19392
+  'unexpectedly found "', // https://github.com/emberjs/ember.js/issues/19392
 ];
 
 class DeprecationSilencer {
