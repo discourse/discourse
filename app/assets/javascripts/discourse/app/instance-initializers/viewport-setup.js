d9c0b40f5448d9789ee43f122046d3bcbf06294b
David Taylor <david@taylorhq.com>
2025-01-20 23:10:51 +0000

    update

--- app/assets/javascripts/discourse/app/instance-initializers/viewport-setup.js
+++ app/assets/javascripts/discourse/app/instance-initializers/viewport-setup.js
@@ -14,13 +14,15 @@ export default {
     }
 
     let value = viewport.getAttribute("content");
-    if (caps.isIOS()) {
+    if (caps.isIOS) {
       // In iOS Safari, setting user-scalable=no doesn't actually prevent the user from zooming in.
       // But, it does prevent the annoying 'auto zoom' when focussing input fields with small font-sizes.
       value += ", user-scalable=no";
     }
 
-    if (caps.isChrome) {
+    if (!caps.isSafari) {
+      // Safari prints a big red warning because it doesn't support this property.
+      // Add it for all other browsers.
       value += ", interactive-widget=resizes-content";
     }
 
