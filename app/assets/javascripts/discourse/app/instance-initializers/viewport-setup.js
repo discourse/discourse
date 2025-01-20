9ace83d35e62a6f396483d5121a2fe5eab886afd
David Taylor <david@taylorhq.com>
2025-01-20 23:03:39 +0000

    DEV: Make `interactive-widget` viewport config chrome-only
    This stops it printing an error to the console in Safari

---
+++ app/assets/javascripts/discourse/app/instance-initializers/viewport-setup.js
@@ -0,0 +1,29 @@
+let done = false;
+
+export default {
+  initialize(container) {
+    if (done) {
+      return;
+    }
+    done = true;
+
+    const caps = container.lookup("service:capabilities");
+    const viewport = document.querySelector("meta[name=viewport]");
+    if (!viewport) {
+      return;
+    }
+
+    let value = viewport.getAttribute("content");
+    if (caps.isIOS()) {
+      // In iOS Safari, setting user-scalable=no doesn't actually prevent the user from zooming in.
+      // But, it does prevent the annoying 'auto zoom' when focussing input fields with small font-sizes.
+      value += ", user-scalable=no";
+    }
+
+    if (caps.isChrome) {
+      value += ", interactive-widget=resizes-content";
+    }
+
+    viewport.setAttribute("content", value);
+  },
+};
