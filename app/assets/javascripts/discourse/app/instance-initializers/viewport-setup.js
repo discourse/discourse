3b472d832a4e158c085dfb408ceeabf28b50e3b2
David Taylor <david@taylorhq.com>
2025-01-21 22:04:18 +0000

    comment

--- app/assets/javascripts/discourse/app/instance-initializers/viewport-setup.js
+++ app/assets/javascripts/discourse/app/instance-initializers/viewport-setup.js
@@ -25,9 +25,12 @@ export default {
       // iOS 'auto-zooms' into inputs with font sizes smaller than 16px. To prevent this,
       // we use two different strategies.
       if (caps.isiOSPWA || caps.isAppWebview) {
+        // For PWA/Hub, we lock the viewport zoom temporarily during `focusin` events.
+        // Unfortunately this doesn't catch the case when an input is already autofocussed, but the
+        // keyboard isn't open yet. But it's better than nothing.
         this.lockViewportDuringFocus(viewport, value);
       } else {
-        // In Safari, user-scalable=no doesn't actually prevent the user from zooming in.
+        // In the full Safari browser, user-scalable=no doesn't actually prevent the user from zooming in.
         // So we can keep it in place all the time to prevent the auto-zoom.
         value += ", user-scalable=no";
       }
