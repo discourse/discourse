65aa14ec5bbe0cab37ec080b518f8ae3165d6859
Sam Saffron <sam.saffron@gmail.com>
2025-01-24 14:37:23 +1100

    DEV: remove experimental lightbox
    Experimental lightbox did not pan out, we are rolling it back so we can start again from base principles.

--- app/assets/javascripts/discourse/app/lib/lightbox/helpers/open-image-in-new-tab.js
+++
@@ -1,8 +0,0 @@
-export async function openImageInNewTab(lightboxItem) {
-  try {
-    window.open(lightboxItem.fullsizeURL, "_blank");
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  }
-}
