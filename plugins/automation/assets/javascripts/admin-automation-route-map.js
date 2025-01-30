96b79256836a2984ca5bdc5590ddf14190fbb28b
Gary <gary@pento.net>
2025-01-30 16:34:18 +1100

    Don't take over the `/` route in every plugin.

--- plugins/automation/assets/javascripts/admin-automation-route-map.js
+++ plugins/automation/assets/javascripts/admin-automation-route-map.js
@@ -1,10 +1,10 @@
 export default {
   resource: "admin.adminPlugins.show",
 
-  path: "/plugins/automation",
+  path: "/plugins",
 
   map() {
-    this.route("automation", { path: "/" }, function () {
+    this.route("automation", function () {
       this.route("new");
       this.route("edit", { path: "/:id" });
     });
