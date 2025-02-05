ef804d7adb04ac543e48650f4d152f4527b5a015
David Battersby <info@davidbattersby.com>
2025-02-05 11:36:31 +0400

    add specs

--- spec/system/drafts_dropdown_spec.rb
+++ spec/system/drafts_dropdown_spec.rb
@@ -64,5 +64,31 @@ describe "Drafts dropdown", type: :system do
       expect(drafts_dropdown.draft_item_count).to eq(4)
       expect(drafts_dropdown.other_drafts_count).to eq(1)
     end
+
+    it "shows the view all drafts when draft count exceeds the draft menu limit" do
+      page.visit "/"
+      drafts_dropdown.open
+
+      expect(drafts_dropdown).to be_open
+      expect(drafts_dropdown).not_to have_view_all_link
+
+      # remove the last 2 drafts
+      Draft.where(user_id: user.id).order("created_at DESC").limit(2).destroy_all
+
+      page.visit "/"
+      drafts_dropdown.open
+
+      expect(drafts_dropdown).to be_open
+      expect(drafts_dropdown).to have_no_view_all_link
+    end
+  end
+
+  describe "with private category" do
+    fab!(:category) { Fabricate(:private_category) }
+
+    it "disabled the drafts dropdown menu" do
+      page.visit "/c/#{category.slug}"
+      expect(drafts_dropdown).to be_disabled
+    end
   end
 end
--- spec/system/page_objects/components/drafts_menu.rb
+++ spec/system/page_objects/components/drafts_menu.rb
@@ -21,6 +21,14 @@ module PageObjects
         has_no_css?(MENU_SELECTOR + "-content")
       end
 
+      def has_view_all_link?
+        has_css?(MENU_SELECTOR + "-content .view-all-drafts")
+      end
+
+      def has_no_view_all_link?
+        has_no_css?(MENU_SELECTOR + "-content .view-all-drafts")
+      end
+
       def open
         find(MENU_SELECTOR + "-trigger").click
       end
