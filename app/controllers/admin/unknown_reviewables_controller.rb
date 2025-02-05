a089d90e7cdd9fe1b1ac20bbd9704b4cc5f274ab
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2025-02-05 14:15:49 +1100

    FIX: typo

--- app/controllers/admin/unknown_reviewables_controller.rb
+++ app/controllers/admin/unknown_reviewables_controller.rb
@@ -2,7 +2,7 @@
 
 class Admin::UnknownReviewablesController < Admin::AdminController
   def destroy
-    Reviewable.destroy_unknown_types
+    Reviewable.destroy_unknown_types!
     render json: success_json
   end
 end
