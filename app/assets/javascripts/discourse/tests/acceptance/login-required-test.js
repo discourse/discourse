11c4a91974c4805ba8e513c6a353651a7a9a73fc
Penar Musaraj <pmusaraj@gmail.com>
2025-02-07 09:31:58 -0500

    update frontent test

--- app/assets/javascripts/discourse/tests/acceptance/login-required-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/login-required-test.js
@@ -3,7 +3,7 @@ import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 
 acceptance("Login Required", function (needs) {
-  needs.settings({ login_required: true });
+  needs.settings({ login_required: true, full_page_login: false });
 
   test("redirect", async function (assert) {
     await visit("/latest");
@@ -13,17 +13,33 @@ acceptance("Login Required", function (needs) {
       "it redirects them to login"
     );
 
-    await click("#site-logo");
-    assert.strictEqual(
-      currentRouteName(),
-      "login",
-      "clicking the logo keeps them on login"
-    );
-
-    await click("header .login-button");
-    assert.dom(".login-modal").exists("they can still access the login modal");
+    await click(".login-button");
+    assert.dom(".login-modal").exists("login modal is shown");
 
     await click(".d-modal__header .modal-close");
     assert.dom(".login-modal").doesNotExist("closes the login modal");
+  });
+});
+
+acceptance("Login Required - Full page login", function (needs) {
+  needs.settings({ login_required: true, full_page_login: true });
+
+  test("page", async function (assert) {
+    await visit("/");
+    assert.strictEqual(
+      currentRouteName(),
+      "login",
+      "it redirects them to login"
+    );
+
+    await click(".login-button");
+    assert.dom(".login-left-side").exists("login form is shown");
+    assert
+      .dom(".login-welcome")
+      .doesNotExist("login welcome is no longer shown");
+
+    await click(".logo-big");
+    assert.dom(".login-left-side").doesNotExist("closes the login modal");
+    assert.dom(".login-welcome").exists("login welcome is shown");
   });
 });
