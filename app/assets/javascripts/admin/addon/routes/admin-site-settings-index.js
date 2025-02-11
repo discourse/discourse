c87c56b3a5951203c5e44ef8a5a93abf7ac9e9e5
Martin Brennan <martin@discourse.org>
2025-02-11 11:39:05 +1000

    Revert "UX: Hide inner site settings sidebar if admin sidebar enabled (#31047)"
    This reverts commit 91e9c1c81343990d5ebbb3a3bb7c68ec4445d610.
    
    After feedback, for now we are reverting this change. This is not
    permanent, the settings sidebar will be removed again, after we:
    
    * Visually group the settings the same way as the sidebar does
      on All Settings
    * Add more settings pages to the main admin sidebar to cover the ~250
      settings not yet represented there

--- app/assets/javascripts/admin/addon/routes/admin-site-settings-index.js
+++ app/assets/javascripts/admin/addon/routes/admin-site-settings-index.js
@@ -7,15 +7,12 @@ import DiscourseRoute from "discourse/routes/discourse";
 
 export default class AdminSiteSettingsIndexRoute extends DiscourseRoute {
   @service router;
-  @service currentUser;
 
   beforeModel() {
-    if (!this.currentUser.use_admin_sidebar) {
-      this.router.replaceWith(
-        "adminSiteSettingsCategory",
-        this.controllerFor("adminSiteSettings").get("visibleSiteSettings")[0]
-          .nameKey
-      );
-    }
+    this.router.replaceWith(
+      "adminSiteSettingsCategory",
+      this.controllerFor("adminSiteSettings").get("visibleSiteSettings")[0]
+        .nameKey
+    );
   }
 }
