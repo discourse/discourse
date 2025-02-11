e422d22e34f9f10bea0c0527e4ecabf20e4fe9c2
brrusselburg <25828824+brrusselburg@users.noreply.github.com>
2025-02-11 17:58:48 -0600

    FIX: Ensures multi-like notifs respect

--- app/assets/javascripts/discourse/app/lib/notification-types/liked.js
+++ app/assets/javascripts/discourse/app/lib/notification-types/liked.js
@@ -4,14 +4,25 @@ import { i18n } from "discourse-i18n";
 
 export default class extends NotificationTypeBase {
   get label() {
+    let name = "";
+    let name2 = "";
+
+    if (!this.siteSettings.prioritize_full_name_in_ux) {
+      name = this.username;
+      name2 = this.#username2;
+    } else {
+      name = this.notification.data.original_name;
+      name2 = this.#full_name2;
+    }
+
     if (this.count === 2) {
       return i18n("notifications.liked_by_2_users", {
-        username: this.username,
-        username2: this.#username2,
+        username: name,
+        username2: name2,
       });
     } else if (this.count > 2) {
       return i18n("notifications.liked_by_multiple_users", {
-        username: this.username,
+        username: name,
         count: this.count - 1,
       });
     } else {
@@ -33,5 +44,10 @@ export default class extends NotificationTypeBase {
 
   get #username2() {
     return formatUsername(this.notification.data.username2);
+  }
+
+  get #full_name2() {
+    return this.notification.data.fullname2;
+    // this doesn't exist on the returned object yet
   }
 }
