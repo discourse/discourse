92af591784aec005aad6d31461916e9555a2fa82
Bryce Huhtala <100382978+sketchius@users.noreply.github.com>
2025-02-11 22:52:41 -0500

    DEV: add rule to detect hex colors

--- app/assets/javascripts/discourse-markdown-it/src/features/hex-color.js
+++ app/assets/javascripts/discourse-markdown-it/src/features/hex-color.js
@@ -1,5 +1,9 @@
 function hexColorRule(state, silent) {
+  const start = state.pos;
+  const src = state.src;
 
+  const match = /^#[0-9A-Fa-f]{6}/.exec(src.slice(start));
+  return match;
 }
 
 export function setup(helper) {
