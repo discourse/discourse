f0d1ff29087e9bd43c7107b1f511c8a9b832694b
brrusselburg <25828824+brrusselburg@users.noreply.github.com>
2025-02-17 15:55:56 -0600

    Update after core tweak from reactions

--- app/assets/javascripts/discourse/app/lib/notification-types/liked.js
+++ app/assets/javascripts/discourse/app/lib/notification-types/liked.js
@@ -1,28 +1,24 @@
 import NotificationTypeBase from "discourse/lib/notification-types/base";
-import { formatUsername } from "discourse/lib/utilities";
 import { i18n } from "discourse-i18n";
 
 export default class extends NotificationTypeBase {
   get label() {
-    let name = "";
-    let name2 = "";
+    const nameOrUsername = this.siteSettings.prioritize_full_name_in_ux
+      ? this.notification.data.display_name
+      : this.notification.data.display_username;
 
-    if (!this.siteSettings.prioritize_full_name_in_ux) {
-      name = this.username;
-      name2 = this.#username2;
-    } else {
-      name = this.notification.data.original_name;
-      name2 = this.#full_name2;
-    }
+    const nameOrUsername2 = this.siteSettings.prioritize_full_name_in_ux
+      ? this.notification.data.name2
+      : this.notification.data.username2;
 
     if (this.count === 2) {
       return i18n("notifications.liked_by_2_users", {
-        username: name,
-        username2: name2,
+        username: nameOrUsername,
+        username2: nameOrUsername2,
       });
     } else if (this.count > 2) {
       return i18n("notifications.liked_by_multiple_users", {
-        username: name,
+        username: nameOrUsername,
         count: this.count - 1,
       });
     } else {
@@ -40,14 +36,5 @@ export default class extends NotificationTypeBase {
 
   get count() {
     return this.notification.data.count;
-  }
-
-  get #username2() {
-    return formatUsername(this.notification.data.username2);
-  }
-
-  get #full_name2() {
-    return this.notification.data.acting_user_name_2;
-    // this doesn't exist on the returned object yet
   }
 }
