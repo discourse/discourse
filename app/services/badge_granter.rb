6eef300871f9f8f6b26c72632d7fd5d4f8028be6
Bianca Nenciu <nenciu.bianca@gmail.com>
2025-02-17 20:21:52 +0200

    FIX: Ensure revoke_ungranted_titles! works with user_ids
    When `revoke_ungranted_titles!` was invoked, the optional list of
    `user_ids` was not passed from the argument list of to the query. This
    resulted in an exception because the placeholder `:user_ids` existed in
    the query.

--- app/services/badge_granter.rb
+++ app/services/badge_granter.rb
@@ -506,7 +506,7 @@ class BadgeGranter
   def self.revoke_ungranted_titles!(user_ids = nil)
     user_ids = user_ids.join(", ") if user_ids
 
-    DB.exec <<~SQL
+    DB.exec <<~SQL, user_ids: user_ids
       UPDATE users u
       SET title = ''
       FROM user_profiles up
@@ -525,7 +525,7 @@ class BadgeGranter
         #{user_ids.present? ? "AND u.id IN (:user_ids)" : ""}
     SQL
 
-    DB.exec <<~SQL
+    DB.exec <<~SQL, user_ids: user_ids
       UPDATE user_profiles up
       SET granted_title_badge_id = NULL
       FROM users u
