5191503b3ea2a59272c895f3e1737a2bba689fd0
Gerhard Schlager <gerhard.schlager@discourse.org>
2025-02-19 00:52:32 +0100

    rubocop

--- migrations/spec/lib/database/schema/validation/columns_validator_spec.rb
+++ migrations/spec/lib/database/schema/validation/columns_validator_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable RSpec/EmptyExampleGroup
 RSpec.describe ::Migrations::Database::Schema::Validation::ColumnsValidator do
   # Add your tests for JsonSchemaValidator here
 end
--- migrations/spec/lib/database/schema/validation/globally_configured_columns_validator_spec.rb
+++ migrations/spec/lib/database/schema/validation/globally_configured_columns_validator_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable RSpec/EmptyExampleGroup
 RSpec.describe ::Migrations::Database::Schema::Validation::GloballyConfiguredColumnsValidator do
   # Add your tests for JsonSchemaValidator here
 end
