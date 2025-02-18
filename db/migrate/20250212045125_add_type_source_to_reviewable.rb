4cb41d59cf9a0f2e3704d957fa1f0264eab36cd3
Gary <gary@pento.net>
2025-02-19 10:25:20 +1100

    DEV: Simplify the migrate script to only add a type_source value for known reviewables.

--- db/migrate/20250212045125_add_type_source_to_reviewable.rb
+++ db/migrate/20250212045125_add_type_source_to_reviewable.rb
@@ -3,36 +3,19 @@ class AddTypeSourceToReviewable < ActiveRecord::Migration[7.2]
   def change
     add_column :reviewables, :type_source, :string, null: false, default: "unknown"
 
-    # Migrate existing reviewables from plugins to have a type_source
-    DiscoursePluginRegistry.reviewable_types_lookup.each do |reviewable|
-      Reviewable.where(type: reviewable[:klass].sti_name).update_all(
-        type_source: reviewable[:plugin],
-      )
-    end
-
-    # Migrate existing reviewables from core to have a type_source
-    Reviewable.sti_names.each do |type|
-      if DiscoursePluginRegistry
-           .reviewable_types_lookup
-           .map { |r| r[:klass].sti_name }
-           .include?(type)
-        next
-      end
-
-      Reviewable.where(type: type).update_all(type_source: "core")
-    end
-
-    # Migrate existing reviewables from known plugins to have a type_source
+    # Migrate known reviewables to have a type_source
     known_reviewables = {
+      "chat" => %w[ReviewableChatMessage],
+      "core" => %w[ReviewableFlaggedPost ReviewableQueuedPost ReviewableUser ReviewablePost],
       "discourse-ai" => %w[ReviewableAiChatMessage ReviewableAiPost],
       "discourse-akismet" => %w[
         ReviewableAkismetPost
         ReviewableAkismetPostVotingComment
         ReviewableAkismetUser
       ],
-      "discourse-antivirus" => ["ReviewableUpload"],
-      "discourse-category-experts" => ["ReviewableCategoryExpertSuggestion"],
-      "discourse-post-voting" => ["ReviewablePostVotingComment"],
+      "discourse-antivirus" => %w[ReviewableUpload],
+      "discourse-category-experts" => %w[ReviewableCategoryExpertSuggestion],
+      "discourse-post-voting" => %w[ReviewablePostVotingComment],
     }
 
     known_reviewables.each do |plugin, types|
