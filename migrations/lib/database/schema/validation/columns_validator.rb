66f755a5f479e2dceb6168610356a5e812301484
Gerhard Schlager <gerhard.schlager@discourse.org>
2025-02-19 00:49:26 +0100

    wip

--- migrations/lib/database/schema/validation/columns_validator.rb
+++ migrations/lib/database/schema/validation/columns_validator.rb
@@ -3,49 +3,31 @@
 module Migrations::Database::Schema::Validation
   class ColumnsValidator < BaseValidator
     def validate
-      global = ::Migrations::Database::Schema::GlobalConfig.new(@schema_config)
+      @global = ::Migrations::Database::Schema::GlobalConfig.new(@schema_config)
 
       @schema_config[:tables].each do |table_name, table_config|
-        validate_columns_of_table(table_name.to_s, table_config[:columns], global)
+        validate_columns_of_table(table_name.to_s, table_config[:columns])
       end
     end
 
     private
 
-    def validate_columns_of_table(table_name, columns, global)
-      existing_column_names = @db.columns(table_name).map { |c| c.name }
+    def validate_columns_of_table(table_name, columns)
+      @existing_column_names = @db.columns(table_name).map { |c| c.name }
+      @added_column_names = columns[:add]&.map { |column| column[:name] } || []
+      @included_column_names = columns[:include] || []
+      @excluded_column_names = columns[:exclude] || []
+      @modified_column_names = columns[:modify]&.map { |column| column[:name] } || []
 
-      added_column_names = columns[:add]&.map { |column| column[:name] } || []
-      validated_added_columns(table_name, existing_column_names, added_column_names)
-
-      included_column_names = columns[:include] || []
-      validate_included_columns(table_name, existing_column_names, included_column_names)
-
-      excluded_column_names = columns[:exclude] || []
-      validate_excluded_columns(table_name, existing_column_names, excluded_column_names)
-
-      modified_column_names = columns[:modify]&.map { |column| column[:name] } || []
-      validate_modified_columns(
-        table_name,
-        existing_column_names,
-        modified_column_names,
-        included_column_names,
-        excluded_column_names,
-      )
-
-      validate_column_usage(
-        table_name,
-        global,
-        existing_column_names,
-        modified_column_names,
-        included_column_names,
-        excluded_column_names,
-        added_column_names,
-      )
+      validated_added_columns(table_name)
+      validate_included_columns(table_name)
+      validate_excluded_columns(table_name)
+      validate_modified_columns(table_name)
+      validate_column_usage(table_name)
     end
 
-    def validated_added_columns(table_name, existing_column_names, added_column_names)
-      if (column_names = existing_column_names & added_column_names).any?
+    def validated_added_columns(table_name)
+      if (column_names = @existing_column_names & @added_column_names).any?
         @errors << I18n.t(
           "schema.validator.tables.added_columns_exist",
           table_name:,
@@ -54,8 +36,8 @@ module Migrations::Database::Schema::Validation
       end
     end
 
-    def validate_included_columns(table_name, existing_column_names, included_column_names)
-      if (column_names = included_column_names - existing_column_names).any?
+    def validate_included_columns(table_name)
+      if (column_names = @included_column_names - @existing_column_names).any?
         @errors << I18n.t(
           "schema.validator.tables.included_columns_missing",
           table_name:,
@@ -64,8 +46,8 @@ module Migrations::Database::Schema::Validation
       end
     end
 
-    def validate_excluded_columns(table_name, existing_column_names, excluded_column_names)
-      if (column_names = excluded_column_names - existing_column_names).any?
+    def validate_excluded_columns(table_name)
+      if (column_names = @excluded_column_names - @existing_column_names).any?
         @errors << I18n.t(
           "schema.validator.tables.excluded_columns_missing",
           table_name:,
@@ -74,22 +56,16 @@ module Migrations::Database::Schema::Validation
       end
     end
 
-    def validate_modified_columns(
-      table_name,
-      existing_column_names,
-      modified_column_names,
-      included_column_names,
-      excluded_column_names
-    )
-      if (column_names = modified_column_names - existing_column_names).any?
+    def validate_modified_columns(table_name)
+      if (column_names = @modified_column_names - @existing_column_names).any?
         @errors << I18n.t(
           "schema.validator.tables.modified_columns_missing",
-          table_name:,
+          table_name: table_name,
           column_names: sort_and_join(column_names),
         )
       end
 
-      if (column_names = modified_column_names & included_column_names).any?
+      if (column_names = @modified_column_names & @included_column_names).any?
         @errors << I18n.t(
           "schema.validator.tables.modified_columns_included",
           table_name:,
@@ -97,7 +73,7 @@ module Migrations::Database::Schema::Validation
         )
       end
 
-      if (column_names = modified_column_names & excluded_column_names).any?
+      if (column_names = @modified_column_names & @excluded_column_names).any?
         @errors << I18n.t(
           "schema.validator.tables.modified_columns_excluded",
           table_name:,
@@ -106,19 +82,11 @@ module Migrations::Database::Schema::Validation
       end
     end
 
-    def validate_column_usage(
-      table_name,
-      global,
-      existing_column_names,
-      modified_column_names,
-      included_column_names,
-      excluded_column_names,
-      added_column_names
-    )
-      if excluded_column_names.empty?
+    def validate_column_usage(table_name)
+      if @excluded_column_names.empty?
         column_names =
-          existing_column_names - included_column_names - modified_column_names -
-            global.excluded_column_names.to_a + added_column_names
+          @existing_column_names - @included_column_names - @modified_column_names -
+            @global.excluded_column_names.to_a + @added_column_names
 
         if column_names.any?
           @errors << I18n.t(
@@ -129,8 +97,8 @@ module Migrations::Database::Schema::Validation
         end
       else
         column_names =
-          existing_column_names - excluded_column_names + modified_column_names -
-            global.excluded_column_names.to_a + added_column_names
+          @existing_column_names - @excluded_column_names + @modified_column_names -
+            @global.excluded_column_names.to_a + @added_column_names
 
         if column_names.empty?
           @errors << I18n.t(
