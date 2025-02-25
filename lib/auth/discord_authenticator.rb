424da95128bd2e92c4d8e42baf65a947602e9c68
Penar Musaraj <pmusaraj@gmail.com>
2025-02-25 01:33:26 -0500

    DEV: Fix username/name mapping for Discord auth (#31494)
    Previously we were applying the Discord username to both the name and
    the username fields in Discourse.
    
    Supersedes https://github.com/discourse/discourse/pull/30994

--- lib/auth/discord_authenticator.rb
+++ lib/auth/discord_authenticator.rb
@@ -16,7 +16,8 @@ class Auth::DiscordAuthenticator < Auth::ManagedAuthenticator
 
     info do
       {
-        name: raw_info["username"],
+        name: raw_info["global_name"] || raw_info["username"],
+        nickname: raw_info["username"],
         email: raw_info["verified"] ? raw_info["email"] : nil,
         image: "https://cdn.discordapp.com/avatars/#{raw_info["id"]}/#{raw_info["avatar"]}",
       }
