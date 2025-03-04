4d00bc1cfbb7566446d9f9b1eebd6f186974710e
Kelvin Tan <kelv@discourse.org>
2025-03-04 23:11:55 +0800

    DEV: add more default headers via middleware

--- lib/middleware/default_headers.rb
+++ lib/middleware/default_headers.rb
@@ -2,15 +2,27 @@
 
 module Middleware
   class DefaultHeaders
+    HTML_ONLY_HEADERS = Set.new(%w[X-Frame-Options X-XSS-Protection])
+
     def initialize(app, settings = {})
       @app = app
     end
 
     def call(env)
       status, headers, body = @app.call(env)
+      is_html_response = html_response?(headers)
+
+      if default_headers = Rails.application.config.action_dispatch.default_headers
+        default_headers.each do |header_name, value|
+          next if !is_html_response && HTML_ONLY_HEADERS.include?(header_name)
+
+          headers[header_name] = value if headers[header_name].nil?
+        end
+      end
+
       headers[
         "Cross-Origin-Opener-Policy"
-      ] = SiteSetting.cross_origin_opener_policy_header if html_response?(headers) &&
+      ] = SiteSetting.cross_origin_opener_policy_header if is_html_response &&
         headers["Cross-Origin-Opener-Policy"].nil?
 
       [status, headers, body]
