7ee752549e9d2bac8273ba378cffb4ce6aef80f1
Jarek Radosz <jarek@cvx.dev>
2025-03-04 12:57:39 +0100

    todo

--- app/assets/javascripts/discourse/app/components/date-picker-future.js
+++ app/assets/javascripts/discourse/app/components/date-picker-future.js
@@ -1,11 +1,13 @@
-import DatePicker from "discourse/components/date-picker";
+// import DatePicker from "discourse/components/date-picker";
 
-export default class DatePickerFuture extends DatePicker {
-  _opts() {
-    return {
-      defaultDate: this.defaultDate || moment().add(1, "day").toDate(),
-      setDefaultDate: !!this.defaultDate,
-      minDate: this.minDate || moment().toDate(),
-    };
-  }
-}
+// export default class DatePickerFuture extends DatePicker {
+//   _opts() {
+//     return {
+//       defaultDate: this.defaultDate || moment().add(1, "day").toDate(),
+//       setDefaultDate: !!this.defaultDate,
+//       minDate: this.minDate || moment().toDate(),
+//     };
+//   }
+// }
+
+<template>todo</template>;
