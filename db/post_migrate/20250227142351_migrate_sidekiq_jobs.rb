166c301f94986e2ed5fa9babcdc3d78feca20348
Loïc Guitaut <loic@discourse.org>
2025-03-05 17:17:30 +0100

    DEV: Upgrade Sidekiq to v7.3.9

--- db/post_migrate/20250227142351_migrate_sidekiq_jobs.rb
+++ db/post_migrate/20250227142351_migrate_sidekiq_jobs.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-# Delete this migration instead of promoting it (along with the
-# `SidekiqMigration` class)
+# TODO: Remove this when releasing Discourse 3.6
 class MigrateSidekiqJobs < ActiveRecord::Migration[7.2]
   def up
     SidekiqMigration.call
