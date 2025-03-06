1603ebc8ea11c23baa569c660fcbc347adac1893
Amanda Alves Branquinho <amanda@discourse.org>
2025-03-06 17:21:42 -0300

    DEV: backporting to stable - add behavior transformer to topic list item click  (#31664)
    - Add the behavior transformer `topic-list-item-click` to allow the
    customization for the click behavior on the topic list item.

--- app/assets/javascripts/discourse/app/lib/transformer/registry.js
+++ app/assets/javascripts/discourse/app/lib/transformer/registry.js
@@ -3,6 +3,7 @@ export const BEHAVIOR_TRANSFORMERS = Object.freeze([
   "composer-position:editor-touch-move",
   "discovery-topic-list-load-more",
   "full-page-search-load-more",
+  "topic-list-item-click",
 ]);
 
 export const VALUE_TRANSFORMERS = Object.freeze([
