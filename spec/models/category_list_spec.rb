4a7ddd1b11d48786eee51ee35aa410373ad28166
Bianca Nenciu <nenciu.bianca@gmail.com>
2025-03-07 09:03:48 +0200

    Add spec

--- spec/models/category_list_spec.rb
+++ spec/models/category_list_spec.rb
@@ -444,4 +444,31 @@ RSpec.describe CategoryList do
       end
     end
   end
+
+  describe "with many categories (more than MAX_UNOPTIMIZED_CATEGORIES)" do
+    fab!(:category)
+    fab!(:subcategory) { Fabricate(:category, parent_category: category) }
+
+    it "returns at most MAX_UNOPTIMIZED_CATEGORIES categories" do
+      stub_const(CategoryList, "MAX_UNOPTIMIZED_CATEGORIES", 1) do
+        category_list = CategoryList.new(Guardian.new(user))
+
+        expect(category_list.categories).to eq(
+          [Category.find(SiteSetting.uncategorized_category_id), category, subcategory],
+        )
+      end
+    end
+
+    it "returns at most SUBCATEGORIES_PER_CATEGORY subcategories" do
+      subcategory_2 = Fabricate(:category, parent_category: category)
+
+      stub_const(CategoryList, "SUBCATEGORIES_PER_CATEGORY", 1) do
+        stub_const(CategoryList, "MAX_UNOPTIMIZED_CATEGORIES", 1) do
+          category_list = CategoryList.new(Guardian.new(user), parent_category_id: category.id)
+
+          expect(category_list.categories).to eq([subcategory, subcategory_2])
+        end
+      end
+    end
+  end
 end
