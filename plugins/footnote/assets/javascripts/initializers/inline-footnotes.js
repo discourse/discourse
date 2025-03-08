f95ad07bde477879fa2f2175cfc16dacd218d1df
Renato Atilio <renato@discourse.org>
2025-03-08 15:05:11 -0300

    FEATURE: add footnote rich editor extension

--- plugins/footnote/assets/javascripts/initializers/inline-footnotes.js
+++ plugins/footnote/assets/javascripts/initializers/inline-footnotes.js
@@ -1,6 +1,7 @@
 import { createPopper } from "@popperjs/core";
 import { iconHTML } from "discourse/lib/icon-library";
 import { withPluginApi } from "discourse/lib/plugin-api";
+import richEditorExtension from "../lib/rich-editor-extension";
 
 let inlineFootnotePopper;
 
@@ -128,6 +129,8 @@ export default {
         tooltip?.removeAttribute("data-show");
         tooltip?.removeAttribute("data-footnote-id");
       });
+
+      api.registerRichEditorExtension(richEditorExtension);
     });
   },
 
