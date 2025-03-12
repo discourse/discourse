63e7726da3889c972b3dc6bdbd828b4418d393cd
Yuriy Kurant <yuriy.kurant@gmail.com>
2025-03-12 10:40:12 +0800

    DEV: moves mobile check to `site` service
    - Pursues the DRY principle by consolidating the mobile view and device check into the `site` service
    - Introduces `isMobileViewAndDevice` as a single source of truth

--- app/assets/javascripts/discourse/app/components/search-menu.js
+++ app/assets/javascripts/discourse/app/components/search-menu.js
@@ -106,10 +106,6 @@ export default class SearchMenu extends Component {
     return false;
   }
 
-  get isMobileViewAndDevice() {
-    return this.site.mobileView && this.site.isMobileDevice;
-  }
-
   @action
   onKeydown(event) {
     if (event.key === "Escape") {
@@ -297,7 +293,7 @@ export default class SearchMenu extends Component {
 
     if (!this.search.activeGlobalSearchTerm) {
       this.abortPerform({
-        noResults: this.site.mobileView && this.site.isMobileDevice,
+        noResults: this.site.isMobileViewAndDevice,
         invalidTerm: false,
       });
     } else if (
