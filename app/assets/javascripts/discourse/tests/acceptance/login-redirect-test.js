3a83037eae45c000b4a91441eb3fed2eb95cd6bd
Penar Musaraj <pmusaraj@gmail.com>
2025-03-12 21:31:30 -0400

    update more tests

--- app/assets/javascripts/discourse/tests/acceptance/login-redirect-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/login-redirect-test.js
@@ -2,7 +2,9 @@ import { currentRouteName, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 
-acceptance("Login redirect - anonymous", function () {
+acceptance("Login redirect - anonymous", function (needs) {
+  needs.settings({ full_page_login: false });
+
   test("redirects login to default homepage", async function (assert) {
     await visit("/login");
     assert.strictEqual(
@@ -16,6 +18,7 @@ acceptance("Login redirect - anonymous", function () {
 acceptance("Login redirect - categories default", function (needs) {
   needs.settings({
     top_menu: "categories|latest|top|hot",
+    full_page_login: false,
   });
 
   test("when site setting is categories", async function (assert) {
