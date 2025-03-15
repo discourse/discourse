7c448210e1531662c370e69c5ae675ec6e3b6e40
Penar Musaraj <pmusaraj@gmail.com>
2025-03-15 16:28:40 -0400

    adjust

--- app/controllers/custom_homepage_controller.rb
+++ app/controllers/custom_homepage_controller.rb
@@ -2,6 +2,7 @@
 
 class CustomHomepageController < ApplicationController
   skip_before_action :check_xhr, only: [:index]
+
   def index
     respond_to { |format| format.html { render :index } }
   end
