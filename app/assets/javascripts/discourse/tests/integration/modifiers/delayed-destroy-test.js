53dfaa1663b7605a7089a9f3e7b408e9cfb9c247
Yuriy Kurant <yuriy.kurant@gmail.com>
2025-03-18 09:29:23 +0800

    DEV: renames for a better consistency
    is-closing -> is-destroying

--- app/assets/javascripts/discourse/tests/integration/modifiers/delayed-destroy-test.js
+++ app/assets/javascripts/discourse/tests/integration/modifiers/delayed-destroy-test.js
@@ -8,23 +8,23 @@ module("Integration | Modifier | delayed-destroy", function (hooks) {
 
   test("does nothing when animate is false", async function (assert) {
     await render(hbs`<div {{delayed-destroy}}></div>`);
-    assert.dom("div").doesNotHaveClass("is-closing");
+    assert.dom("div").doesNotHaveClass("is-destroying");
   });
 
-  test("adds/removes `.is-closing` class with animation", async function (assert) {
+  test("adds/removes `.is-destroying` class with animation", async function (assert) {
     this.set("hasAnimation", true);
     this.set("onComplete", () => (this.hasAnimation = false));
 
     await render(
       hbs`<div {{delayed-destroy animate=this.hasAnimation onComplete=this.onComplete}}></div>`
     );
-    // TODO: couldn't figure out how I can test `.is-closing` presence,
+    // TODO: couldn't figure out how I can test `.is-destroying` presence,
     // before it's removed after timeout:
-    // assert.dom("div").hasClass("is-closing");
+    // assert.dom("div").hasClass("is-destroying");
 
-    // await waitFor("div:not(.is-closing)", { timeout: 11 });
+    // await waitFor("div:not(.is-destroying)", { timeout: 11 });
 
-    assert.dom("div").doesNotHaveClass("is-closing");
+    assert.dom("div").doesNotHaveClass("is-destroying");
     assert.false(this.hasAnimation, "onComplete callback was executed");
   });
 
@@ -36,14 +36,14 @@ module("Integration | Modifier | delayed-destroy", function (hooks) {
       </div>`
     );
 
-    // TODO: couldn't figure out how I can test `.is-closing` presence,
+    // TODO: couldn't figure out how I can test `.is-destroying` presence,
     // before it's removed after timeout:
     // await waitUntil(function () {
-    //   return assert.dom(".target").hasClass("is-closing");
+    //   return assert.dom(".target").hasClass("is-destroying");
     // });
-    assert.dom(".other").doesNotHaveClass("is-closing");
+    assert.dom(".other").doesNotHaveClass("is-destroying");
 
-    // await waitFor(".target:not(.is-closing)", { timeout: 100 });
-    assert.dom(".target").doesNotHaveClass("is-closing");
+    // await waitFor(".target:not(.is-destroying)", { timeout: 100 });
+    assert.dom(".target").doesNotHaveClass("is-destroying");
   });
 });
