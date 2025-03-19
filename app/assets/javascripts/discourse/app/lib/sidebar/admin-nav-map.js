0e8d20e31f4b78076227edbf811c530934fbb08b
Martin Brennan <martin@discourse.org>
2025-03-19 12:07:44 +1000

    UX: Themes & components I18n adjustments
    Moves the Themes & components I18n text to use the
    admin > config structure, so the description shows
    up properly in admin search

--- app/assets/javascripts/discourse/app/lib/sidebar/admin-nav-map.js
+++ app/assets/javascripts/discourse/app/lib/sidebar/admin-nav-map.js
@@ -244,10 +244,10 @@ export const ADMIN_NAV_MAP = [
         route: "adminConfig.customize.themes",
         currentWhen:
           "adminConfig.customize.themes adminConfig.customize.components",
-        label: "admin.appearance.sidebar_link.themes_and_components.title",
+        label: "admin.config.themes_and_components.title",
+        description: "admin.config.themes_and_components.header_description",
         icon: "paintbrush",
-        keywords:
-          "admin.appearance.sidebar_link.themes_and_components.keywords",
+        keywords: "admin.config.themes_and_components.keywords",
       },
       {
         name: "admin_customize_site_texts",
