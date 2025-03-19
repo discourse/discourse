82a498c51e8b24b783a8bc7edd37d10371c84803
OsamaSayegh <asooomaasoooma90@gmail.com>
2025-03-19 03:57:51 +0300

    overengineered migrations

--- db/migrate/20250314102616_rename_allow_anonymous_posting_to_allow_anonymous_mode.rb
+++ db/migrate/20250314102616_rename_allow_anonymous_posting_to_allow_anonymous_mode.rb
@@ -3,27 +3,17 @@
 class RenameAllowAnonymousPostingToAllowAnonymousMode < ActiveRecord::Migration[7.2]
   def up
     execute(<<~SQL)
-      WITH renamed AS (
-        DELETE FROM site_settings
-        WHERE name = 'allow_anonymous_posting'
-        RETURNING 'allow_anonymous_mode' AS name, data_type, value, created_at, updated_at
-      )
-      INSERT INTO site_settings
-      (name, data_type, value, created_at, updated_at)
-      SELECT * FROM renamed
+      UPDATE site_settings
+      SET name = 'allow_anonymous_mode'
+      WHERE name = 'allow_anonymous_posting'
     SQL
   end
 
   def down
     execute(<<~SQL)
-      WITH renamed AS (
-        DELETE FROM site_settings
-        WHERE name = 'allow_anonymous_mode'
-        RETURNING 'allow_anonymous_posting' AS name, data_type, value, created_at, updated_at
-      )
-      INSERT INTO site_settings
-      (name, data_type, value, created_at, updated_at)
-      SELECT * FROM renamed
+      UPDATE site_settings
+      SET name = 'allow_anonymous_posting'
+      WHERE name = 'allow_anonymous_mode'
     SQL
   end
 end
--- db/migrate/20250314110738_rename_allow_anonymous_likes_to_allow_likes_in_anonymous_mode.rb
+++ db/migrate/20250314110738_rename_allow_anonymous_likes_to_allow_likes_in_anonymous_mode.rb
@@ -3,27 +3,17 @@
 class RenameAllowAnonymousLikesToAllowLikesInAnonymousMode < ActiveRecord::Migration[7.2]
   def up
     execute(<<~SQL)
-      WITH renamed AS (
-        DELETE FROM site_settings
-        WHERE name = 'allow_anonymous_likes'
-        RETURNING 'allow_likes_in_anonymous_mode' AS name, data_type, value, created_at, updated_at
-      )
-      INSERT INTO site_settings
-      (name, data_type, value, created_at, updated_at)
-      SELECT * FROM renamed
+      UPDATE site_settings
+      SET name = 'allow_likes_in_anonymous_mode'
+      WHERE name = 'allow_anonymous_likes'
     SQL
   end
 
   def down
     execute(<<~SQL)
-      WITH renamed AS (
-        DELETE FROM site_settings
-        WHERE name = 'allow_likes_in_anonymous_mode'
-        RETURNING 'allow_anonymous_likes' AS name, data_type, value, created_at, updated_at
-      )
-      INSERT INTO site_settings
-      (name, data_type, value, created_at, updated_at)
-      SELECT * FROM renamed
+      UPDATE site_settings
+      SET name = 'allow_anonymous_likes'
+      WHERE name = 'allow_likes_in_anonymous_mode'
     SQL
   end
 end
