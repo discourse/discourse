2bc12cb767f4f4dd1e1679f812510219ab4548d3
Sam Saffron <sam.saffron@gmail.com>
2025-03-21 15:02:02 +1100

    lint

--- plugins/automation/spec/system/smoke_test_spec.rb
+++ plugins/automation/spec/system/smoke_test_spec.rb
@@ -49,7 +49,7 @@ describe "DiscourseAutomation | smoke test", type: :system do
 
       try_until_success do
         expect(DiscourseAutomation::Automation.exists?(id: automation.id)).to be(false)
-      end  
+      end
     end
   end
 
