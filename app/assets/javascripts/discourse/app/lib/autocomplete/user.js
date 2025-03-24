9fa588f19199e9be64394ab23a7786bb8fa997b2
David Taylor <david@taylorhq.com>
2025-03-24 15:19:23 +0000

    fixup

--- app/assets/javascripts/discourse/app/lib/autocomplete/user.js
+++ app/assets/javascripts/discourse/app/lib/autocomplete/user.js
@@ -1,12 +1,13 @@
 import avatar from "discourse/helpers/avatar";
-import { escapeExpression } from "discourse/lib/utilities";
+import { escapeExpression, formatUsername } from "discourse/lib/utilities";
+import { iconHTML } from "../icon-library";
 
 function renderUserItem(item) {
   return `
     <li data-index="${escapeExpression(item.index?.toString())}">
       <a href title="${escapeExpression(item.name)}" class="${escapeExpression(item.cssClasses)}">
         ${avatar(item, { imageSize: "tiny" })}
-        <span class='username'>${escapeExpression(item.username)}</span>
+        <span class='username'>${escapeExpression(formatUsername(item.username))}</span>
         ${item.name ? `<span class='name'>${escapeExpression(item.name)}</span>` : ""}
         ${item.status ? `<span class='user-status'></span>` : ""}
       </a>
@@ -18,8 +19,8 @@ function renderEmailItem(item) {
   return `
     <li>
       <a href title="${escapeExpression(item.username)}">
-        ${item.icon || ""}
-        <span class='username'>${escapeExpression(item.username)}</span>
+        ${iconHTML("envelope")}
+        <span class='username'>${escapeExpression(formatUsername(item.username))}</span>
       </a>
     </li>
   `;
@@ -29,7 +30,7 @@ function renderGroupItem(item) {
   return `
     <li>
       <a href title="${escapeExpression(item.full_name)}">
-        ${item.icon || ""}
+        ${iconHTML("users")}
         <span class='username'>${escapeExpression(item.name)}</span>
         <span class='name'>${escapeExpression(item.full_name)}</span>
       </a>
