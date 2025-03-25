32aea61e3714566c11b096e438c47f032fe80e36
Martin Brennan <martin@discourse.org>
2025-03-25 14:04:56 +1000

    DEV: Review fixes

--- app/assets/javascripts/discourse/app/static/prosemirror/extensions/bullet-list.js
+++ app/assets/javascripts/discourse/app/static/prosemirror/extensions/bullet-list.js
@@ -1,23 +1,13 @@
+import { schema } from "prosemirror-markdown";
+
 /** @type {RichEditorExtension} */
 const extension = {
-  // This is a 1-1 copy of the prosemirror-markdown bullet_list, but
-  // overrides the tight list default to `true`
   nodeSpec: {
     bullet_list: {
-      content: "list_item+",
-      group: "block",
+      ...schema.nodes.bullet_list.spec,
+
+      // All we are doing here is overriding the tight list default to `true`.
       attrs: { tight: { default: true } },
-      parseDOM: [
-        {
-          tag: "ul",
-          getAttrs: (dom) => ({ tight: dom.hasAttribute("data-tight") }),
-        },
-      ],
-      toDOM: (node) => [
-        "ul",
-        { "data-tight": node.attrs.tight ? "true" : null },
-        0,
-      ],
     },
   },
 };
--- app/assets/javascripts/discourse/app/static/prosemirror/extensions/ordered-list.js
+++ app/assets/javascripts/discourse/app/static/prosemirror/extensions/ordered-list.js
@@ -1,28 +1,13 @@
+import { schema } from "prosemirror-markdown";
+
 /** @type {RichEditorExtension} */
 const extension = {
-  // This is a 1-1 copy of the prosemirror-markdown ordered_list, but
-  // overrides the tight list default to `true`
   nodeSpec: {
     ordered_list: {
-      content: "list_item+",
-      group: "block",
+      ...schema.nodes.ordered_list.spec,
+
+      // All we are doing here is overriding the tight list default to `true`.
       attrs: { order: { default: 1 }, tight: { default: true } },
-      parseDOM: [
-        {
-          tag: "ol",
-          getAttrs: (dom) => ({ tight: dom.hasAttribute("data-tight") }),
-        },
-      ],
-      toDOM: (node) => {
-        return [
-          "ol",
-          {
-            start: node.attrs.order === 1 ? null : node.attrs.order,
-            "data-tight": node.attrs.tight ? "true" : null,
-          },
-          0,
-        ];
-      },
     },
   },
 };
