ce24cf79d2e3dee1c35dc30df96f7067cb42f8c3
David Taylor <david@taylorhq.com>
2025-03-31 17:28:45 +0100

    unneeded comment

--- lib/email/styles.rb
+++ lib/email/styles.rb
@@ -397,8 +397,6 @@ module Email
     end
 
     def to_html
-      # needs to be before class + id strip because we need to style redacted
-      # media and also not double-redact already redacted from lower levels
       replace_secure_uploads_urls if SiteSetting.secure_uploads?
       replace_relative_urls
       deduplicate_styles
