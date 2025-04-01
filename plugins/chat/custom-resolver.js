ce6b853d677398f25f5a58ca1edbdb70835e76ce
Jarek Radosz <jarek@cvx.dev>
2025-04-01 16:50:49 +0200

    resolver

---
+++ plugins/chat/custom-resolver.js
@@ -0,0 +1,5 @@
+export default async function (path) {
+  if (path === "@embroider/virtual/components/chat-message") {
+    return "discourse/plugins/chat/discourse/components/chat-message";
+  }
+}
