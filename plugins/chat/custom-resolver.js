df21ee8a483fd2b122a10856619c838e070666e2
Jarek Radosz <jarek@cvx.dev>
2025-04-01 22:52:50 +0200

    resolver WIP

--- plugins/chat/custom-resolver.js
+++ plugins/chat/custom-resolver.js
@@ -1,5 +1,53 @@
+import fs from "node:fs";
+
+const pluginName = "chat";
+
+const roots = [
+  "discourse/app",
+  "admin/addon",
+  "select-kit/addon",
+  "float-kit/addon",
+  "dialog-holder/addon",
+];
+
+function itemExists(path) {
+  return (
+    fs.globSync(`${path}.{js,gjs,hbs}`).length ||
+    fs.globSync(`${path}/index.{js,gjs,hbs}`).length
+  );
+}
+
 export default async function (path) {
-  if (path === "@embroider/virtual/components/chat-message") {
-    return "discourse/plugins/chat/discourse/components/chat-message";
+  if (!path.startsWith("@embroider/virtual/")) {
+    return;
+  }
+
+  const name = path.replace("@embroider/virtual/", "").replace(".js", "");
+
+  // core and bundled addons
+  for (let location of roots) {
+    const pkg = location.match(/(.+)\//)[1];
+    if (itemExists(`../../app/assets/javascripts/${location}/${name}`)) {
+      return `${pkg}/${name}`;
+    }
+  }
+
+  // target plugin
+  if (itemExists(`./assets/javascripts/discourse/${name}`)) {
+    return `discourse/plugins/${pluginName}/discourse/${name}`;
+  } else if (itemExists(`./admin/assets/javascripts/admin/${name}`)) {
+    return `discourse/plugins/${pluginName}/admin/${name}`;
+  }
+
+  // styleguide
+  if (itemExists(`../styleguide/assets/javascripts/discourse/${name}`)) {
+    return `discourse/plugins/styleguide/discourse/${name}`;
+  }
+
+  // d-lazy-videos
+  if (
+    itemExists(`../discourse-lazy-videos/assets/javascripts/discourse/${name}`)
+  ) {
+    return `discourse/plugins/discourse-lazy-videos/discourse/${name}`;
   }
 }
