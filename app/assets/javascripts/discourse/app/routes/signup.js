921999021314fc54aca2a289080d411b8f966bea
Martin Brennan <martin@discourse.org>
2025-04-07 14:30:27 +1000

    DEV: Remove full_page_login setting
    We are making this the only option for our login/signup
    pages on April 29th, 2025, per
    https://meta.discourse.org/t/introducing-our-new-fullscreen-signup-and-login-pages/340401.
    
    This commit removes the `full_page_login` setting and any logic
    around it, as well as deleting the old login and signup modals,
    and removing leftover problem checks and settings from the database.

--- app/assets/javascripts/discourse/app/routes/signup.js
+++ app/assets/javascripts/discourse/app/routes/signup.js
@@ -1,5 +1,3 @@
-import { action } from "@ember/object";
-import { next } from "@ember/runloop";
 import { service } from "@ember/service";
 import DiscourseRoute from "discourse/routes/discourse";
 
@@ -17,8 +15,6 @@ export default class SignupRoute extends DiscourseRoute {
 
     if (this.login.isOnlyOneExternalLoginMethod && !this.authComplete) {
       this.login.singleExternalLogin({ signup: true });
-    } else {
-      this.showCreateAccount();
     }
   }
 
@@ -27,21 +23,6 @@ export default class SignupRoute extends DiscourseRoute {
 
     if (this.login.isOnlyOneExternalLoginMethod && !this.authComplete) {
       controller.set("isRedirectingToExternalAuth", true);
-    }
-  }
-
-  @action
-  async showCreateAccount() {
-    const { canSignUp } = this.controllerFor("application");
-    if (!canSignUp) {
-      const route = await this.router
-        .replaceWith(
-          this.siteSettings.login_required ? "login" : "discovery.latest"
-        )
-        .followRedirects();
-      if (canSignUp) {
-        next(() => route.send("showCreateAccount"));
-      }
     }
   }
 }
