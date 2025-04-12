e50e43ad273a8cee9d842bba3e70e0835f0eee96
Renato Atilio <renato@discourse.org>
2025-04-12 11:51:57 -0300

    UX: rich editor html_block without escaping and avoiding \n\n

--- app/assets/javascripts/discourse/app/static/prosemirror/extensions/html-block.js
+++ app/assets/javascripts/discourse/app/static/prosemirror/extensions/html-block.js
@@ -26,10 +26,39 @@ const extension = {
   },
   serializeNode: {
     html_block: (state, node) => {
-      state.renderContent(node);
+      state.text(node.textContent, false);
       state.write("\n\n");
     },
   },
+  plugins: ({ pmState: { Plugin }, utils: { changedDescendants } }) =>
+    // When cooking the markdown, a html block is auto-closed when a \n\n
+    // is found, so here we make sure there's at most one \n in the block
+    new Plugin({
+      appendTransaction(transactions, prevState, state) {
+        if (!transactions.some((tr) => tr.docChanged)) {
+          return null;
+        }
+
+        const { tr } = state;
+        changedDescendants(prevState.doc, state.doc, (node, pos) => {
+          if (node.type.name === "html_block") {
+            const content = node.textContent;
+            const normalized = content.replace(/\n{2,}/g, "\n");
+
+            if (content !== normalized) {
+              const textNode = node.content.firstChild;
+              tr.replaceWith(
+                pos + 1,
+                pos + 1 + textNode.nodeSize,
+                state.schema.text(normalized)
+              );
+            }
+          }
+        });
+
+        return tr;
+      },
+    }),
 };
 
 export default extension;
--- app/assets/javascripts/discourse/app/static/prosemirror/lib/plugin-utils.js
+++ app/assets/javascripts/discourse/app/static/prosemirror/lib/plugin-utils.js
@@ -86,3 +86,26 @@ export function atBlockStart(state, view) {
   }
   return $cursor;
 }
+
+// https://github.com/discourse/discourse/pull/31933#discussion_r2019739410
+export function changedDescendants(old, cur, f, offset = 0) {
+  const oldSize = old.childCount,
+    curSize = cur.childCount;
+  outer: for (let i = 0, j = 0; i < curSize; i++) {
+    const child = cur.child(i);
+    for (let scan = j, e = Math.min(oldSize, i + 5); scan < e; scan++) {
+      if (old.child(scan) === child) {
+        j = scan + 1;
+        offset += child.nodeSize;
+        continue outer;
+      }
+    }
+    f(child, offset);
+    if (j < oldSize && old.child(j).sameMarkup(child)) {
+      changedDescendants(old.child(j), child, f, offset + 1);
+    } else {
+      child.nodesBetween(0, child.content.size, f, offset + 1);
+    }
+    offset += child.nodeSize;
+  }
+}
