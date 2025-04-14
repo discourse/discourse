dc9cbeaaacace0e36a9bffbb997e58ab0e484c78
Martin Brennan <martin@discourse.org>
2025-04-14 15:00:11 +1000

    FIX: Escape regex symbols when replaceText is called for ProseMirror
    When we do replaceText in the ProseMirror TextManipulation lib,
    we are creating a regex for the provided markdown. However this markdown
    can have things like * (which is valid for a markdown list), which also
    doubles as a regex symbol. We can escape the markdown provided to
    the regex first to fix the issue.

--- app/assets/javascripts/discourse/app/static/prosemirror/lib/text-manipulation.js
+++ app/assets/javascripts/discourse/app/static/prosemirror/lib/text-manipulation.js
@@ -6,6 +6,7 @@ import { lift, setBlockType, toggleMark, wrapIn } from "prosemirror-commands";
 import { liftListItem, sinkListItem } from "prosemirror-schema-list";
 import { TextSelection } from "prosemirror-state";
 import { bind } from "discourse/lib/decorators";
+import escapeRegExp from "discourse/lib/escape-regexp";
 import { i18n } from "discourse-i18n";
 
 /**
@@ -276,7 +277,7 @@ export default class ProsemirrorTextManipulation {
 
     const markdown = this.convertToMarkdown(this.view.state.doc);
 
-    const regex = opts.regex || new RegExp(oldValue, "g");
+    const regex = opts.regex || new RegExp(escapeRegExp(oldValue), "g");
     const index = opts.index || 0;
     let matchCount = 0;
 
