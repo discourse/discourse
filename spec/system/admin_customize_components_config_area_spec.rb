33b716e556573b8ff239feab9b04f4e123290583
OsamaSayegh <asooomaasoooma90@gmail.com>
2025-04-17 17:21:22 +0300

    DEV: Attempt at stabilizing components listing page system spec

--- spec/system/admin_customize_components_config_area_spec.rb
+++ spec/system/admin_customize_components_config_area_spec.rb
@@ -84,21 +84,21 @@ describe "Admin Customize Themes Config Area Page", type: :system do
       config_area.visit
 
       config_area.status_selector.select("used")
-      expect(config_area).to be_loading
+      config_area.wait_until_done_loading
       expect(config_area.components_shown).to contain_exactly(
         enabled_component.id,
         remote_component.id,
       )
 
       config_area.status_selector.select("unused")
-      expect(config_area).to be_loading
+      config_area.wait_until_done_loading
       expect(config_area.components_shown).to contain_exactly(
         disabled_component.id,
         remote_component_with_update.id,
       )
 
       config_area.status_selector.select("updates_available")
-      expect(config_area).to be_loading
+      config_area.wait_until_done_loading
       expect(config_area.components_shown).to contain_exactly(remote_component_with_update.id)
     end
 
@@ -107,7 +107,7 @@ describe "Admin Customize Themes Config Area Page", type: :system do
 
       config_area.name_filter_input.fill_in(with: "glo")
 
-      expect(config_area).to be_loading
+      config_area.wait_until_done_loading
       expect(config_area.components_shown).to contain_exactly(
         enabled_component.id,
         disabled_component.id,
@@ -341,7 +341,7 @@ describe "Admin Customize Themes Config Area Page", type: :system do
 
           page.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
-          expect(config_area).to be_loading
+          config_area.wait_until_done_loading
           expect(config_area).to have_component(enabled_component.id)
 
           expect(config_area.components_shown.size).to eq(8)
