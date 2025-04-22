c3d85e26f68ac7ae4bbc7e87df53eecfca763fa9
Nat <natalie.tay@discourse.org>
2025-04-22 14:08:26 +0800

    Remove fk

--- db/migrate/20250421074012_create_category_localization_table.rb
+++ db/migrate/20250421074012_create_category_localization_table.rb
@@ -3,7 +3,7 @@
 class CreateCategoryLocalizationTable < ActiveRecord::Migration[7.2]
   def change
     create_table :category_localizations do |t|
-      t.references :category, null: false, foreign_key: true
+      t.references :category, null: false
       t.string :locale, limit: 20, null: false
       t.string :name, limit: 50, null: false
       t.text :description, null: true
