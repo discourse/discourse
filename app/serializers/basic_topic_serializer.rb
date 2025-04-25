b7ee9a186b174d3790037ec54eadfd5422d82693
Isaac Janzen <isaac.janzen@discourse.org>
2025-04-25 12:03:05 -0500

    DEV: Add `last_posted_at` to `BasicTopicSerializer`

--- app/serializers/basic_topic_serializer.rb
+++ app/serializers/basic_topic_serializer.rb
@@ -2,7 +2,7 @@
 
 # The most basic attributes of a topic that we need to create a link for it.
 class BasicTopicSerializer < ApplicationSerializer
-  attributes :id, :title, :fancy_title, :slug, :posts_count
+  attributes :id, :title, :fancy_title, :slug, :posts_count, :last_posted_at
 
   def fancy_title
     f = object.fancy_title
