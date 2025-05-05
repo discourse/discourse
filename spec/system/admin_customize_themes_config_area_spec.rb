ffa406b0b1a2c9a2e38de098b92936b1153961f3
Ella E. <ella@discourse.org>
2025-05-04 19:06:05 -0600

    UX: Add more info to theme cards (#32334)
    Demo:
    <img width="1431" alt="Screenshot 2025-05-02 at 2 24 41 pm"
    src="https://github.com/user-attachments/assets/c8ec824e-25df-4efc-a4be-c774a2a97896"
    />
    <img width="1480" alt="Screenshot 2025-05-02 at 2 24 32 pm"
    src="https://github.com/user-attachments/assets/a4020283-9b6d-4f5e-a8c2-72b7608a6026"
    />
    
    Mobile:
    <img width="424" alt="Screenshot 2025-05-02 at 2 25 29 pm"
    src="https://github.com/user-attachments/assets/af68669a-7bb4-46dc-92ab-6ecc7a3f3842"
    />
    <img width="421" alt="Screenshot 2025-05-02 at 2 25 36 pm"
    src="https://github.com/user-attachments/assets/961d9f5c-8010-4c5f-a319-6e356b088667"
    />

--- spec/system/admin_customize_themes_config_area_spec.rb
+++ spec/system/admin_customize_themes_config_area_spec.rb
@@ -2,6 +2,8 @@
 
 describe "Admin Customize Themes Config Area Page", type: :system do
   fab!(:admin)
+  fab!(:theme) { Theme.where(component: false).first }
+  fab!(:theme_2) { Fabricate(:theme, name: "Second theme") }
 
   let(:config_area) { PageObjects::Pages::AdminCustomizeThemesConfigArea.new }
   let(:install_modal) { PageObjects::Modals::InstallTheme.new }
@@ -18,5 +20,29 @@ describe "Admin Customize Themes Config Area Page", type: :system do
     config_area.install_card.find(".btn-primary").click
     expect(install_modal).to be_open
     expect(install_modal.popular_options.first).to have_text("Air")
+  end
+
+  it "allows to mark theme as active" do
+    config_area.visit
+    expect(config_area).to have_badge(theme, "--active")
+    expect(config_area).to have_no_badge(theme_2, "--active")
+    config_area.mark_as_active(theme_2)
+    expect(config_area).to have_badge(theme_2, "--active")
+    expect(config_area).to have_no_badge(theme, "--active")
+  end
+
+  it "allows to make theme selectable by users" do
+    config_area.visit
+    expect(config_area).to have_no_badge(theme, "--selectable")
+    config_area.toggle_selectable(theme)
+    expect(config_area).to have_badge(theme, "--selectable")
+    config_area.toggle_selectable(theme)
+    expect(config_area).to have_no_badge(theme, "--selectable")
+  end
+
+  it "allows to edit theme" do
+    config_area.visit
+    config_area.click_edit(theme)
+    expect(page).to have_current_path("/admin/customize/themes/#{theme.id}")
   end
 end
