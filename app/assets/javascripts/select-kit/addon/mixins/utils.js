4e06f2070f4ecf9527c704772bf658d2604b54e7
Kelvin Tan <kelv@discourse.org>
2025-05-06 20:26:18 +0800

    DEV: replace utils mixin methods related to processing input with input-utilities lib

--- app/assets/javascripts/select-kit/addon/mixins/utils.js
+++ app/assets/javascripts/select-kit/addon/mixins/utils.js
@@ -2,15 +2,6 @@ import { get } from "@ember/object";
 import Mixin from "@ember/object/mixin";
 
 export default Mixin.create({
-  isValidInput(eventKey) {
-    // relying on passing the event to the input is risky as it could not work
-    // dispatching the event won't work as the event won't be trusted
-    // safest solution is to filter event and prefill filter with it
-    const nonInputKeysRegex =
-      /F\d+|Arrow.+|Meta|Alt|Control|Shift|Delete|Enter|Escape|Tab|Space|Insert|Backspace/;
-    return !nonInputKeysRegex.test(eventKey);
-  },
-
   defaultItem(value, name) {
     if (this.selectKit.valueProperty) {
       const item = {};
@@ -88,21 +79,5 @@ export default Mixin.create({
         return this[getter](contentItem) === name;
       });
     }
-  },
-
-  _isNumeric(input) {
-    return !isNaN(parseFloat(input)) && isFinite(input);
-  },
-
-  _normalize(input) {
-    if (input) {
-      input = input.toLowerCase();
-
-      if (typeof input.normalize === "function") {
-        input = input.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-      }
-    }
-
-    return input;
   },
 });
