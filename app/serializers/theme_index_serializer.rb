15dec70f78c99ee0baa77ff471f3ba55ace3b425
OsamaSayegh <asooomaasoooma90@gmail.com>
2025-05-08 16:16:20 +0300

    fix tests

--- app/serializers/theme_index_serializer.rb
+++ app/serializers/theme_index_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ThemeIndexSerializer < BasicThemeSerializer
-  attributes :user_selectable, :screenshot_url, :remote_theme_id, :enabled
+  attributes :user_selectable, :screenshot_url, :remote_theme_id, :enabled?, :supported?
 
   has_one :color_scheme, serializer: ColorSchemeSerializer, embed: :object
   has_one :remote_theme, serializer: RemoteThemeSerializer, embed: :objects
