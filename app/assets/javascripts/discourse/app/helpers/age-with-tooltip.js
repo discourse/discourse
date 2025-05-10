92213c6c1b3ad4c166921b29f137f84d742f4544
Jarek Radosz <jarek@cvx.dev>
2025-05-10 13:18:24 +0200

    fix2

--- app/assets/javascripts/discourse/app/helpers/age-with-tooltip.js
+++ app/assets/javascripts/discourse/app/helpers/age-with-tooltip.js
@@ -2,6 +2,10 @@ import { htmlSafe } from "@ember/template";
 import { autoUpdatingRelativeAge } from "discourse/lib/formatter";
 
 export default function ageWithTooltip(dt, params = {}) {
-  params.title ??= true;
-  return htmlSafe(autoUpdatingRelativeAge(new Date(dt), params));
+  return htmlSafe(
+    autoUpdatingRelativeAge(new Date(dt), {
+      ...params,
+      title: params.title ?? true,
+    })
+  );
 }
