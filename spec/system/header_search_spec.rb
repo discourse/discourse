81c5b12f932f38455f329cd621c851f016401c83
Jordan Vidrine <jordan@jordanvidrine.com>
2025-05-15 09:15:50 -0500

    more

--- spec/system/header_search_spec.rb
+++ spec/system/header_search_spec.rb
@@ -1,57 +1,60 @@
 # frozen_string_literal: true
 
 RSpec.describe "Header Search - Responsive Behavior", type: :system do
-  fab!(:user)
+  fab!(:current_user) { Fabricate(:user) }
+  let(:search_page) { PageObjects::Pages::Search.new }
 
-  before do
-    SiteSetting.search_experience = "search_field"
-    sign_in(user)
-  end
+  before { SiteSetting.search_experience = "search_field" }
 
-  it "appears based on scroll & screen width with search banner enabled" do
-    SiteSetting.enable_welcome_banner = true
-    visit "/"
+  context "when welcome banner is enabled" do
+    it "appears based on scroll & screen width with search banner enabled" do
+      SiteSetting.enable_welcome_banner = true
+      sign_in(current_user)
+      visit "/"
 
-    expect(page).to have_no_search_field
-    expect(page).to have_no_search_icon
+      expect(search_page).to have_no_search_field
+      expect(search_page).to have_no_search_icon
 
-    fake_scroll_down_long
+      fake_scroll_down_long
 
-    expect(page).to have_search_field
+      expect(search_page).to have_search_field
 
-    page.current_window.resize_to(500, 1024)
+      page.current_window.resize_to(500, 1024)
 
-    expect(page).to have_search_icon
-    expect(page).to have_no_search_field
+      expect(search_page).to have_search_icon
+      expect(search_page).to have_no_search_field
 
-    page.current_window.resize_to(1000, 1024)
+      page.current_window.resize_to(1000, 1024)
 
-    expect(page).to have_search_field
-    expect(page).to have_no_search_icon
-  end
+      expect(search_page).to have_search_field
+      expect(search_page).to have_no_search_icon
+    end
 
-  it "appears when search banner is not enabled & shows / hides based on viewport width" do
-    SiteSetting.enable_welcome_banner = false
-    visit "/"
+    it "appears when search banner is not enabled & shows / hides based on viewport width" do
+      SiteSetting.enable_welcome_banner = false
+      sign_in(current_user)
+      visit "/"
 
-    expect(page).to have_search_field
-    expect(page).to have_no_search_icon
+      expect(search_page).to have_search_field
+      expect(search_page).to have_no_search_icon
 
-    page.current_window.resize_to(500, 1024)
+      page.current_window.resize_to(500, 1024)
 
-    expect(page).to have_search_icon
-    expect(page).to have_no_search_field
+      expect(search_page).to have_search_icon
+      expect(search_page).to have_no_search_field
 
-    page.current_window.resize_to(1000, 1024)
+      page.current_window.resize_to(1000, 1024)
 
-    expect(page).to have_search_field
-    expect(page).to have_no_search_icon
-  end
+      expect(search_page).to have_search_field
+      expect(search_page).to have_no_search_icon
+    end
 
-  it "does not appear when search setting is set to icon" do
-    SiteSetting.search_experience = "search_icon"
-    visit "/"
+    it "does not appear when search setting is set to icon" do
+      SiteSetting.search_experience = "search_icon"
+      sign_in(current_user)
+      visit "/"
 
-    expect(page).to have_no_search_field
+      expect(search_page).to have_no_search_field
+    end
   end
 end
