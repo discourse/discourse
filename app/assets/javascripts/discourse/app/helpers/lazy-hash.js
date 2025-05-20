1823e901231284cbc6116c0a58194e972be74454
David Taylor <david@taylorhq.com>
2025-05-20 21:58:08 +0100

    improvements

--- app/assets/javascripts/discourse/app/helpers/lazy-hash.js
+++ app/assets/javascripts/discourse/app/helpers/lazy-hash.js
@@ -1,12 +1,33 @@
 import { setInternalHelperManager } from "@glimmer/manager";
-import { createConstRef } from "@glimmer/reference";
+import { createConstRef, valueForRef } from "@glimmer/reference";
 
 // Like ember's builtin hash helper, but instead of returning a reified object
 // every time its referenced, it returns a static proxy object with auto-trackable keys.
 export default setInternalHelperManager(({ named }) => {
   const proxy = new Proxy(named, {
     get(target, prop) {
-      return target[prop]?.compute();
+      if (target[prop]) {
+        return valueForRef(target[prop]);
+      }
+    },
+    isExtensible() {
+      return false;
+    },
+    getOwnPropertyDescriptor(target, key) {
+      if (key in target) {
+        return {
+          enumerable: true,
+          configurable: true,
+          get() {
+            if (target[key]) {
+              return valueForRef(target[key]);
+            }
+          },
+        };
+      }
+    },
+    set() {
+      return false;
     },
   });
 
