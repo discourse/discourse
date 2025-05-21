bef22401c0271a29cc5b3f1f9ce2bc07c35c2332
David Taylor <david@taylorhq.com>
2025-05-21 15:40:41 +0100

    Better compat?
    We don't actually need a proxy, since the set of keys on an internalHelper can never change

--- app/assets/javascripts/discourse/app/helpers/lazy-hash.js
+++ app/assets/javascripts/discourse/app/helpers/lazy-hash.js
@@ -1,35 +1,28 @@
 import { setInternalHelperManager } from "@glimmer/manager";
 import { createConstRef, valueForRef } from "@glimmer/reference";
+import { dependentKeyCompat } from "@ember/object/compat";
+
+class LazyHash {
+  constructor(namedRefs) {
+    for (const [key, value] of Object.entries(namedRefs)) {
+      Object.defineProperty(
+        this,
+        key,
+        dependentKeyCompat(this, key, {
+          get() {
+            return valueForRef(value);
+          },
+          enumerable: true,
+          configurable: false,
+        })
+      );
+    }
+    Object.preventExtensions(this);
+  }
+}
 
 // Like ember's builtin hash helper, but instead of returning a reified object
 // every time its referenced, it returns a static proxy object with auto-trackable keys.
-export default setInternalHelperManager(({ named }) => {
-  const proxy = new Proxy(named, {
-    get(target, prop) {
-      if (target[prop]) {
-        return valueForRef(target[prop]);
-      }
-    },
-    isExtensible() {
-      return false;
-    },
-    getOwnPropertyDescriptor(target, key) {
-      if (key in target) {
-        return {
-          enumerable: true,
-          configurable: true,
-          get() {
-            if (target[key]) {
-              return valueForRef(target[key]);
-            }
-          },
-        };
-      }
-    },
-    set() {
-      return false;
-    },
-  });
-
-  return createConstRef(proxy);
+export default setInternalHelperManager(({ named: namedRefs }) => {
+  return createConstRef(new LazyHash(namedRefs));
 }, {});
