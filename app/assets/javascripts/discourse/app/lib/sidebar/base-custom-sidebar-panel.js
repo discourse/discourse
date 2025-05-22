206dff5e13ca18e98c86402502728a7e0fe548d7
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2025-05-22 13:44:25 +0800

    FIX: bring back sidebar filter (#32837)
    In this PR, filter was removed and replaced by search.
    
    #32485
    
    However, moderator should still be able to filter sidebar. Also, plugins like doc-category should have filterable sidebar.

--- app/assets/javascripts/discourse/app/lib/sidebar/base-custom-sidebar-panel.js
+++ app/assets/javascripts/discourse/app/lib/sidebar/base-custom-sidebar-panel.js
@@ -55,7 +55,27 @@ export default class BaseCustomSidebarPanel {
   }
 
   /**
-   * @returns {boolean} Controls whether the search is shown
+   * @returns {boolean} Controls whether the filter is shown.
+   * Filter allows to remove sidebar links which does not match the filter phrase.
+   */
+  get filterable() {
+    return false;
+  }
+
+  /**
+   * @param {string} filter filter applied
+   *
+   * @returns {string | SafeString} Description displayed when the applied filter has no results.
+   * Use `htmlSafe` from `from "@ember/template` to use HTML strings.
+   */
+  // eslint-disable-next-line no-unused-vars
+  filterNoResultsDescription(filter) {
+    return null;
+  }
+
+  /**
+   * @returns {boolean} Controls whether the search is shown.
+   * Displays modal on click allowing searching for admin pages, site settings, themes, components and reports.
    */
   get searchable() {
     return false;
