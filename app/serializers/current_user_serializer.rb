212616e18a2be71cd04ba4d9e5dc22adb09ca317
Keegan George <kgeorge13@gmail.com>
2025-05-26 14:42:40 -0700

    fix

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -77,8 +77,7 @@ class CurrentUserSerializer < BasicUserSerializer
              :has_unseen_features,
              :can_see_emails,
              :use_glimmer_post_stream_mode_auto_mode,
-             :can_localize_content,
-             :can_debug_localizations
+             :can_localize_content
 
   delegate :user_stat, to: :object, private: true
   delegate :any_posts, :draft_count, :pending_posts_count, :read_faq?, to: :user_stat
