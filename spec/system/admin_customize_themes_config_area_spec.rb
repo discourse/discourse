f632857f04674d957dc9896d6145c8a5bfaa6ddc
Ted Johansson <ted@discourse.org>
2025-05-29 10:09:10 +0800

    DEV: Add subheader to admin themes page

--- spec/system/admin_customize_themes_config_area_spec.rb
+++ spec/system/admin_customize_themes_config_area_spec.rb
@@ -11,14 +11,10 @@ describe "Admin Customize Themes Config Area Page", type: :system do
 
   before { sign_in(admin) }
 
-  it "has a special card for installing new themes" do
+  it "has an install button in the subheader" do
     config_area.visit
 
-    expect(config_area.install_card).to have_text(
-      I18n.t("admin_js.admin.config_areas.themes_and_components.themes.new_theme"),
-    )
-
-    config_area.install_card.find(".btn-primary").click
+    config_area.subheader.find(".btn-primary").click
     expect(install_modal).to be_open
     expect(install_modal.popular_options.first).to have_text("Air")
   end
