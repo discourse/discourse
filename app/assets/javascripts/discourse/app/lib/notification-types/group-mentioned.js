f82249599ac52e49814c54f723397eed4de01fdc
Penar Musaraj <pmusaraj@gmail.com>
2025-05-30 23:58:32 -0400

    Revert "DEV: Added the ability to use users' names in group mention n… (#33023)
    …otifications and mentions shown in emails (#32848)"
    
    This reverts commit e147d2afe6682a85070aecc0775050774f2f81f6. The
    original commit has a bug, it replaces multiple usernames mentioned in a
    post with the name of the first user in email templates.
    
    Reverting temporarily, will need to re-roll a fix.

--- app/assets/javascripts/discourse/app/lib/notification-types/group-mentioned.js
+++ app/assets/javascripts/discourse/app/lib/notification-types/group-mentioned.js
@@ -1,19 +1,8 @@
-import { service } from "@ember/service";
 import NotificationTypeBase from "discourse/lib/notification-types/base";
 
 export default class extends NotificationTypeBase {
-  @service siteSettings;
-
   get label() {
-    let name;
-
-    if (this.siteSettings.prioritize_full_name_in_ux) {
-      name = this.notification.acting_user_name || this.username;
-    } else {
-      name = this.username;
-    }
-
-    return `${name} @${this.notification.data.group_name}`;
+    return `${this.username} @${this.notification.data.group_name}`;
   }
 
   get labelClasses() {
