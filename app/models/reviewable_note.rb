59ef4116b9ed357b12b85f1e8ea62db255595984
Gary <gary@pento.net>
2025-06-03 16:45:21 +1000

    FIX: Linting.

--- app/models/reviewable_note.rb
+++ app/models/reviewable_note.rb
@@ -10,3 +10,26 @@ class ReviewableNote < ActiveRecord::Base
 
   scope :ordered, -> { order(:created_at) }
 end
+
+# == Schema Information
+#
+# Table name: reviewable_notes
+#
+#  id            :bigint           not null, primary key
+#  reviewable_id :bigint           not null
+#  user_id       :bigint           not null
+#  content       :text             not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_reviewable_notes_on_reviewable_id                 (reviewable_id)
+#  index_reviewable_notes_on_reviewable_id_and_created_at  (reviewable_id,created_at)
+#  index_reviewable_notes_on_user_id                       (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (reviewable_id => reviewables.id)
+#  fk_rails_...  (user_id => users.id)
+#
