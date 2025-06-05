e79d11767bd5b6e3430daba9ee32795b6637d4cc
David Battersby <info@davidbattersby.com>
2025-06-05 18:47:05 +0400

    update tests

--- app/assets/javascripts/discourse/tests/integration/components/prosemirror-editor/mention-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/prosemirror-editor/mention-test.js
@@ -1,5 +1,6 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+import pretender, { response } from "discourse/tests/helpers/create-pretender";
 import { testMarkdown } from "discourse/tests/helpers/rich-editor-helper";
 
 module(
@@ -7,21 +8,32 @@ module(
   function (hooks) {
     setupRenderingTest(hooks);
 
+    hooks.beforeEach(function () {
+      pretender.get("/u/john.json", () => {
+        return response(404);
+      });
+    });
+
     const testCases = {
       mention: [
-        "@hello",
-        '<p><a class="mention" data-name="hello" contenteditable="false" draggable="true">@hello</a></p>',
-        "@hello",
+        "@eviltrout",
+        '<p><a class="mention" data-name="eviltrout" contenteditable="false" draggable="true">@eviltrout</a></p>',
+        "@eviltrout",
       ],
       "text with mention": [
-        "Hello @dude",
-        '<p>Hello <a class="mention" data-name="dude" contenteditable="false" draggable="true">@dude</a></p>',
-        "Hello @dude",
+        "Hello @eviltrout",
+        '<p>Hello <a class="mention" data-name="eviltrout" contenteditable="false" draggable="true">@eviltrout</a></p>',
+        "Hello @eviltrout",
       ],
       "mention after heading": [
-        "## Hello\n\n@dude",
-        '<h2>Hello</h2><p><a class="mention" data-name="dude" contenteditable="false" draggable="true">@dude</a></p>',
-        "## Hello\n\n@dude",
+        "## Hello\n\n@eviltrout",
+        '<h2>Hello</h2><p><a class="mention" data-name="eviltrout" contenteditable="false" draggable="true">@eviltrout</a></p>',
+        "## Hello\n\n@eviltrout",
+      ],
+      "invalid mention": [
+        "Hello @john, how are you?",
+        "<p>Hello @john, how are you?</p>",
+        "Hello @john, how are you?",
       ],
     };
 
