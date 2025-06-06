033cccdf17a56b43f9f243bb549abe0a41a0de63
David Taylor <david@taylorhq.com>
2025-06-06 16:00:49 +0100

    DEV: Simplify theme translation JS generation (#33104)
    Theme translations are very simple JS, and do not need to be run through
    the theme transpiler. This brings their format in-line with core/plugin
    translations.
    
    Extracted from https://github.com/discourse/discourse/pull/33103

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -6,7 +6,7 @@ require "json_schemer"
 class Theme < ActiveRecord::Base
   include GlobalPath
 
-  BASE_COMPILER_VERSION = 88
+  BASE_COMPILER_VERSION = 89
 
   class SettingsMigrationError < StandardError
   end
