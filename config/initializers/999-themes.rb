094506a08255647cca7f94f8244128e50482d7ac
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2025-06-06 16:52:58 +0800

    FIX: more performant js updates

--- config/initializers/999-themes.rb
+++ config/initializers/999-themes.rb
@@ -10,7 +10,16 @@ Rails.application.config.after_initialize do |config|
         theme_name = filepath.gsub("#{Rails.root}/themes/", "").split("/").first
 
         Rails.logger.info "Theme folder changed. Syncing #{theme_name}..."
-        SystemThemesManager.sync_theme!(theme_name)
+        if modified.length == 1 && added.length == 0 && removed.length == 0
+          theme = Theme.find(Theme::CORE_THEMES[theme_name])
+          relative_path = filepath.gsub("#{Rails.root}/themes/#{theme_name}/javascripts/", "")
+          theme_field = theme.theme_fields.where(name: relative_path).first
+          return SystemThemesManager.sync_theme!(theme_name) if !theme_field
+
+          theme_field.update!(value: File.read(modified[0]))
+        else
+          SystemThemesManager.sync_theme!(theme_name)
+        end
       end
     listener.start
   end
