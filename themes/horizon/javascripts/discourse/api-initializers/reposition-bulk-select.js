62ff20b7988718cc71ae5169b441fd56411fac87
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2025-06-13 21:18:23 +0800

    Revert "FIX: import Horizon system theme (#33193)"
    This reverts commit 7562bc2b158783d37107ec21d6c19cb7aabf1092.

--- themes/horizon/javascripts/discourse/api-initializers/reposition-bulk-select.js
+++
@@ -1,7 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-
-export default apiInitializer("0.8.0", (api) => {
-  api.registerValueTransformer("bulk-select-in-nav-controls", () => {
-    return true;
-  });
-});
--- themes/horizon/javascripts/discourse/components/card/topic-category-column.gjs
+++
@@ -1,7 +0,0 @@
-import { categoryLinkHTML } from "discourse/helpers/category-link";
-
-const TopicCategoryColumn = <template>
-  {{categoryLinkHTML @topic.category}}
-</template>;
-
-export default TopicCategoryColumn;
