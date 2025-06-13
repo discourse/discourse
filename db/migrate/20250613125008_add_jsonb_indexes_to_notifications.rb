b3ddd7875f12b0d8f7709d020326b00df8881877
amanda <amanda@discourse.org>
2025-06-13 09:52:28 -0300

    add index to notifications

---
+++ db/migrate/20250613125008_add_jsonb_indexes_to_notifications.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+class AddJsonbIndexesToNotifications < ActiveRecord::Migration[7.2]
+  def up
+    execute <<-SQL
+      CREATE INDEX index_notifications_on_data_original_username
+      ON notifications ((data :: JSONB ->> 'original_username'))
+      WHERE (data :: JSONB ->> 'original_username') IS NOT NULL;
+
+      CREATE INDEX index_notifications_on_data_display_username
+      ON notifications ((data :: JSONB ->> 'display_username'))
+      WHERE (data :: JSONB ->> 'display_username') IS NOT NULL;
+
+      CREATE INDEX index_notifications_on_data_username
+      ON notifications ((data :: JSONB ->> 'username'))
+      WHERE (data :: JSONB ->> 'username') IS NOT NULL;
+
+      CREATE INDEX index_notifications_on_data_username2
+      ON notifications ((data :: JSONB ->> 'username2'))
+      WHERE (data :: JSONB ->> 'username2') IS NOT NULL;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP INDEX index_notifications_on_data_original_username;
+      DROP INDEX index_notifications_on_data_display_username;
+      DROP INDEX index_notifications_on_data_username;
+      DROP INDEX index_notifications_on_data_username2;
+    SQL
+  end
+end
