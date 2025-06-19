f0870dad70a1740e2469db8461fd69a54ff5e785
amanda <amanda@discourse.org>
2025-06-19 11:20:04 -0300

    PERF: Add indexes to improve user merger performance

---
+++ db/migrate/20250619140551_add_user_index_to_incoming_links.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+class AddUserIndexToIncomingLinks < ActiveRecord::Migration[7.2]
+  def change
+    add_index :incoming_links, :user_id, where: "user_id IS NOT NULL"
+    add_index :incoming_links, :current_user_id, where: "current_user_id IS NOT NULL"
+  end
+end
