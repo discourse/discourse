6103f4e0dfbc23c08a016c49d3228334e7aad007
amanda <amanda@discourse.org>
2025-06-25 15:09:59 -0300

    create index concurrently

--- db/migrate/20250619140551_add_user_index_to_incoming_links.rb
+++ db/migrate/20250619140551_add_user_index_to_incoming_links.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 class AddUserIndexToIncomingLinks < ActiveRecord::Migration[7.2]
-  def change
-    add_index :incoming_links, :user_id, where: "user_id IS NOT NULL"
-    add_index :incoming_links, :current_user_id, where: "current_user_id IS NOT NULL"
+  disable_ddl_transaction!
+
+  def up
+    execute <<~SQL
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS
+      index_incoming_links_on_user_id ON incoming_links(user_id) WHERE user_id IS NOT NULL
+    SQL
+
+    execute <<~SQL
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS
+      index_incoming_links_on_current_user_id ON incoming_links(current_user_id) WHERE current_user_id IS NOT NULL
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      DROP INDEX IF EXISTS index_incoming_links_on_user_id
+    SQL
+
+    execute <<~SQL
+      DROP INDEX IF EXISTS index_incoming_links_on_current_user_id
+    SQL
   end
 end
