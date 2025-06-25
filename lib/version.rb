fd2f543a08dbd1468ccc76163eedb60bbbde39bf
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2025-06-25 10:52:27 +0800

    Bump version to v3.5.0.beta7

--- lib/version.rb
+++ lib/version.rb
@@ -7,7 +7,7 @@ module Discourse
   unless defined?(::Discourse::VERSION)
     module VERSION #:nodoc:
       # Use the `version_bump:*` rake tasks to update this value
-      STRING = "3.5.0.beta7-dev"
+      STRING = "3.5.0.beta7"
 
       PARTS = STRING.split(".")
       private_constant :PARTS
