95dc41a373e8fdd32b7e4c2b8000c5d55da62de7
amanda <amanda@discourse.org>
2025-06-25 16:56:06 -0300

    create index concurrently

--- db/migrate/20250619140809_add_index_to_post_actions.rb
+++ db/migrate/20250619140809_add_index_to_post_actions.rb
@@ -1,9 +1,56 @@
 # frozen_string_literal: true
 class AddIndexToPostActions < ActiveRecord::Migration[7.2]
-  def change
-    add_index :post_actions, :deleted_by_id, where: "deleted_by_id IS NOT NULL"
-    add_index :post_actions, :deferred_by_id, where: "deferred_by_id IS NOT NULL"
-    add_index :post_actions, :agreed_by_id, where: "agreed_by_id IS NOT NULL"
-    add_index :post_actions, :disagreed_by_id, where: "disagreed_by_id IS NOT NULL"
+  disable_ddl_transaction!
+
+  def up
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS index_post_actions_on_agreed_by_id
+    SQL
+
+    execute <<~SQL
+      CREATE INDEX CONCURRENTLY index_post_actions_on_agreed_by_id ON post_actions(agreed_by_id) WHERE agreed_by_id IS NOT NULL
+    SQL
+
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS index_post_actions_on_deferred_by_id
+    SQL
+
+    execute <<~SQL
+      CREATE INDEX CONCURRENTLY index_post_actions_on_deferred_by_id ON post_actions(deferred_by_id) WHERE deferred_by_id IS NOT NULL
+    SQL
+
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS index_post_actions_on_deleted_by_id
+    SQL
+
+    execute <<~SQL
+      CREATE INDEX CONCURRENTLY index_post_actions_on_deleted_by_id ON post_actions(deleted_by_id) WHERE deleted_by_id IS NOT NULL
+    SQL
+
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS index_post_actions_on_disagreed_by_id
+    SQL
+
+    execute <<~SQL
+      CREATE INDEX CONCURRENTLY index_post_actions_on_disagreed_by_id ON post_actions(disagreed_by_id) WHERE disagreed_by_id IS NOT NULL
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      DROP INDEX IF EXISTS index_post_actions_on_agreed_by_id
+    SQL
+
+    execute <<~SQL
+      DROP INDEX IF EXISTS index_post_actions_on_deferred_by_id
+    SQL
+
+    execute <<~SQL
+      DROP INDEX IF EXISTS index_post_actions_on_deleted_by_id
+    SQL
+
+    execute <<~SQL
+      DROP INDEX IF EXISTS index_post_actions_on_disagreed_by_id
+    SQL
   end
 end
