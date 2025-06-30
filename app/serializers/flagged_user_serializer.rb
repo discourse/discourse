daf9660d8ca24f7dbe0478863516fdf1a570951b
Gary <gary@pento.net>
2025-06-30 15:45:40 +1000

    DEV: Make the server-side changes obey the feature flag.

--- app/serializers/flagged_user_serializer.rb
+++ app/serializers/flagged_user_serializer.rb
@@ -47,4 +47,20 @@ class FlaggedUserSerializer < BasicUserSerializer
 
     result
   end
+
+  def include_created_at?
+    scope.can_see_reviewable_ui_refresh?
+  end
+
+  def include_custom_fields?
+    scope.can_see_reviewable_ui_refresh?
+  end
+
+  def include_post_count?
+    scope.can_see_reviewable_ui_refresh?
+  end
+
+  def include_trust_level?
+    scope.can_see_reviewable_ui_refresh?
+  end
 end
