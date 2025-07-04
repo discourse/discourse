4c60bfb8eaeb803b96c84f96e164dac7e5d4d1f1
Nat <natalie.tay@discourse.org>
2025-07-04 16:23:09 +0800

    batch??

--- db/migrate/20250704072726_nullify_blank_locales.rb
+++ db/migrate/20250704072726_nullify_blank_locales.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 class NullifyBlankLocales < ActiveRecord::Migration[7.2]
   def up
-    execute <<~SQL
-      UPDATE topics SET locale = NULL WHERE locale = '';
-      UPDATE posts SET locale = NULL WHERE locale = '';
-    SQL
+    loop do
+      result = execute(<<~SQL)
+          UPDATE topics
+          SET locale = NULL
+          WHERE id IN (SELECT id FROM topics WHERE locale = '' LIMIT 5000)
+        SQL
+
+      break if result.cmd_tuples == 0
+    end
+
+    loop do
+      result = execute(<<~SQL)
+          UPDATE posts
+          SET locale = NULL
+          WHERE id IN (SELECT id FROM posts WHERE locale = '' LIMIT 5000)
+        SQL
+
+      break if result.cmd_tuples == 0
+    end
   end
 
   def down
