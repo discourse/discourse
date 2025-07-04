84a2f909c22515521728316d633dad716554b6ca
Renato Atilio <renato@discourse.org>
2025-07-04 17:52:19 -0300

    DEV: fix lint + plugin tests

--- app/assets/javascripts/discourse/app/static/prosemirror/core/plugin.js
+++ app/assets/javascripts/discourse/app/static/prosemirror/core/plugin.js
@@ -1,16 +1,27 @@
 import { Plugin } from "prosemirror-state";
 
+/*
+  nodeViews: { nodeName: NodeViewClass }
+  nodeViews: { nodeName: (pluginParams) => NodeViewClass }
+  nodeViews: { nodeName: (pluginParams) => (...args) => new NodeViewClass(...args) }
+*/
 export function extractNodeViews(extensions, pluginParams) {
   /** @type {Record<string, import('prosemirror-view').NodeViewConstructor>} */
   const allNodeViews = {};
   for (const { nodeViews } of extensions) {
     if (nodeViews) {
-      for (let [name, NodeViewClass] of Object.entries(nodeViews)) {
-        if (!NodeViewClass.toString().startsWith("class")) {
-          NodeViewClass = NodeViewClass(pluginParams);
+      for (let [name, nodeView] of Object.entries(nodeViews)) {
+        // node view can be a function, to which we pass pluginParams
+        if (!nodeView.toString().startsWith("class")) {
+          nodeView = nodeView(pluginParams);
         }
 
-        allNodeViews[name] = NodeViewClass;
+        // directly or returned by the function, we may have a class
+        if (nodeView.toString().startsWith("class")) {
+          allNodeViews[name] = (...args) => new nodeView(...args);
+        } else {
+          allNodeViews[name] = nodeView;
+        }
       }
     }
   }
