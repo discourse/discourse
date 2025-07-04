3a32266eb9e89631e1a47ff6d3b8e360855206b6
amanda <amanda@discourse.org>
2025-07-04 15:58:50 -0300

    update migration to drop index if exists

--- db/migrate/20250619140858_add_jsonb_indexes_to_notifications.rb
+++ db/migrate/20250619140858_add_jsonb_indexes_to_notifications.rb
@@ -1,19 +1,43 @@
 # frozen_string_literal: true
 class AddJsonbIndexesToNotifications < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+
   def up
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS index_notifications_on_data_original_username
+    SQL
+
     execute <<~SQL
       CREATE INDEX CONCURRENTLY index_notifications_on_data_original_username
       ON notifications ((data :: JSONB ->> 'original_username'))
       WHERE (data :: JSONB ->> 'original_username') IS NOT NULL;
+    SQL
 
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS index_notifications_on_data_display_username
+    SQL
+
+    execute <<~SQL
       CREATE INDEX CONCURRENTLY index_notifications_on_data_display_username
       ON notifications ((data :: JSONB ->> 'display_username'))
       WHERE (data :: JSONB ->> 'display_username') IS NOT NULL;
+    SQL
 
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS index_notifications_on_data_username
+    SQL
+
+    execute <<~SQL
       CREATE INDEX CONCURRENTLY index_notifications_on_data_username
       ON notifications ((data :: JSONB ->> 'username'))
       WHERE (data :: JSONB ->> 'username') IS NOT NULL;
+    SQL
 
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY IF EXISTS index_notifications_on_data_username2
+    SQL
+
+    execute <<~SQL
       CREATE INDEX CONCURRENTLY index_notifications_on_data_username2
       ON notifications ((data :: JSONB ->> 'username2'))
       WHERE (data :: JSONB ->> 'username2') IS NOT NULL;
