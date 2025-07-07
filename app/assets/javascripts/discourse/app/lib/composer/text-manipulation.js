c627ee3c477f1b460ac7225085ea91358af351f1
Martin Brennan <martin@discourse.org>
2025-07-07 17:17:53 +1000

    DEV: Minor changes

--- app/assets/javascripts/discourse/app/lib/composer/text-manipulation.js
+++ app/assets/javascripts/discourse/app/lib/composer/text-manipulation.js
@@ -17,8 +17,8 @@ export const TextManipulation = {};
  * @property {boolean} [inCode]
  * @property {boolean} [inCodeBlock]
  * @property {boolean} [inBlockquote]
- * @property {Array}   [inHeading] - Array with bool as the first element to indicate if in heading
- *                                and the second element is the heading level (1-6)
+ * @property {Object}  [inHeading] - Object with `inNode` boolean and a `level` integer
+ *  which indicates heading level (1-6)
  */
 
 /**
