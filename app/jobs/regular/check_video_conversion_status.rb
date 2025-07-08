49a989d6ec071d37a270f9aa36255860cae8018f
Blake Erickson <blake@discourse.org>
2025-07-08 10:04:41 -0600

    clean up check video conversion status

--- app/jobs/regular/check_video_conversion_status.rb
+++ app/jobs/regular/check_video_conversion_status.rb
@@ -7,7 +7,7 @@ module Jobs
       return if args[:upload_id].blank? || args[:job_id].blank? || args[:adapter_type].blank?
 
       upload = Upload.find_by(id: args[:upload_id])
-      return unless upload
+      return if upload.blank?
 
       adapter =
         VideoConversion::AdapterFactory.get_adapter(upload, adapter_type: args[:adapter_type])
@@ -18,15 +18,19 @@ module Jobs
       when :complete
         if adapter.handle_completion(args[:job_id], args[:output_path], args[:new_sha1])
           # Successfully completed
-          nil
+          Rails.logger.info(
+            "Completed video conversion for upload ID #{upload.id} and job ID #{args[:job_id]}",
+          )
         else
           # Handle completion failed
-          Rails.logger.error("Failed to handle video conversion completion")
-          nil
+          Rails.logger.error(
+            "Failed to handle video conversion completion for upload ID #{upload.id} and job ID #{args[:job_id]}",
+          )
         end
       when :error
-        Rails.logger.error("Video conversion job failed")
-        nil
+        Rails.logger.error(
+          "Video conversion job failed for upload ID #{upload.id} and job ID #{args[:job_id]}",
+        )
       when :pending
         # Re-enqueue the job to check again
         Jobs.enqueue_in(30.seconds, :check_video_conversion_status, args)
