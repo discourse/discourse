b153926e2e57cf790d4ece27d1a00328b4227410
Kelvin Tan <kelv@discourse.org>
2025-07-11 23:28:29 +0800

    Revert "get d-autocomplete menu to follow on grippie dragging without closing but this also disables proper closeOnClickOutside"
    This reverts commit e7915ba8aba4f01cda25cfbf246483d67e262ee2.

--- app/assets/javascripts/discourse/app/modifiers/d-autocomplete.js
+++ app/assets/javascripts/discourse/app/modifiers/d-autocomplete.js
@@ -38,7 +38,6 @@ export default class DAutocompleteModifier extends Modifier {
   debouncedSearch = null;
   targetElement = null;
   menuInstance = null;
-  virtualElement = null;
 
   // Constants
   ALLOWED_LETTERS_REGEXP = /[\s[{(/+]/;
@@ -121,14 +120,8 @@ export default class DAutocompleteModifier extends Modifier {
     event.stopPropagation();
   };
 
-  handleGlobalClick = async (event) => {
+  handleGlobalClick = async () => {
     if (this.expanded) {
-      // Don't close if clicking on the grippie (composer drag handle)
-      if (event.target.closest(".grippie")) {
-        return;
-      }
-
-      // Close on any other outside click
       await this.closeAutocomplete();
     }
   };
@@ -211,9 +204,6 @@ export default class DAutocompleteModifier extends Modifier {
         await this.closeAutocomplete();
       }
     }
-
-    // Update menu position after autocomplete processing
-    this.updateMenuPosition();
   }
 
   async handleBackspace() {
@@ -292,15 +282,7 @@ export default class DAutocompleteModifier extends Modifier {
 
     try {
       // Create virtual element positioned at the caret location
-      this.virtualElement = this.createVirtualElementAtCaret();
-
-      // Find the appropriate container for portal and boundary
-      // Try to find d-editor container, composer container, or fall back to body for textarea compatibility
-      const container =
-        this.targetElement.closest(".d-editor-container") ||
-        this.targetElement.closest(".composer-fields") ||
-        this.targetElement.closest(".ember-view") ||
-        document.body;
+      const virtualElement = this.createVirtualElementAtCaret();
 
       const menuOptions = {
         identifier: "d-autocomplete",
@@ -316,8 +298,6 @@ export default class DAutocompleteModifier extends Modifier {
             this.componentInstance = componentInstance;
           },
         },
-        closeOnClickOutside: false,
-        closeOnEscape: true,
         modalForMobile: true,
         onClose: () => {
           this.expanded = false;
@@ -325,23 +305,11 @@ export default class DAutocompleteModifier extends Modifier {
         },
       };
 
-      // Only add portal configuration if we found a proper container (not document.body)
-      if (container !== document.body) {
-        menuOptions.portalOutletElement = container;
-        menuOptions.boundary = container;
-      }
-
-      this.menuInstance = await this.menu.show(
-        this.virtualElement,
-        menuOptions
-      );
+      this.menuInstance = await this.menu.show(virtualElement, menuOptions);
       this.expanded = true;
 
       // Call onRender callback if provided
       this.options.onRender?.(this.results);
-
-      // Set up position updates for this menu instance
-      this.updateMenuPosition();
     } catch {
       // Error rendering autocomplete
     }
@@ -359,7 +327,6 @@ export default class DAutocompleteModifier extends Modifier {
     this.selectedIndex = -1;
     this.previousTerm = null;
     this.menuInstance = null;
-    this.virtualElement = null; // Clean up virtual element reference
     this.componentInstance = null; // Clean up component reference
 
     cancel(this.debouncedSearch);
@@ -526,42 +493,17 @@ export default class DAutocompleteModifier extends Modifier {
     };
   }
 
-  updateMenuPosition() {
-    // Update position if menu is expanded and we have instances
-    if (this.expanded && this.menuInstance && this.virtualElement) {
-      // Force the menu system to recalculate position by reassigning the trigger
-      // This is similar to how the link toolbar works
-      import("@ember/runloop").then(({ next }) => {
-        next(() => {
-          if (this.menuInstance?.instance) {
-            // Update the trigger element which forces position recalculation
-            this.menuInstance.instance.trigger = this.virtualElement;
-
-            // Try to call updatePosition if available on the float instance
-            if (this.menuInstance.instance.updatePosition) {
-              this.menuInstance.instance.updatePosition();
-            }
-          }
-        });
-      });
-    }
-  }
-
   createVirtualElementAtCaret() {
     const marginLeft = 9; //offset to place autocomplete in front of the trigger character (e.g. "@")
     const marginTop = 10;
-
-    // Create virtual element with dynamic getBoundingClientRect
+    const caretCoords = this.getAbsoluteCaretCoords();
     return {
-      getBoundingClientRect: () => {
-        const caretCoords = this.getAbsoluteCaretCoords();
-        return {
-          left: caretCoords.x + marginLeft,
-          top: caretCoords.y + marginTop,
-          width: 1,
-          height: 20,
-        };
-      },
+      getBoundingClientRect: () => ({
+        left: caretCoords.x + marginLeft,
+        top: caretCoords.y + marginTop,
+        width: 1,
+        height: 20,
+      }),
     };
   }
 }
