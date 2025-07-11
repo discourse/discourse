afc5d13c6362c137d531933a6af007100e387359
Martin Brennan <martin@discourse.org>
2025-07-11 12:29:58 +1000

    FEATURE: Text heading/paragraph menu in composer toolbar (#33461)
    This PR introduces a heading/paragraph dropdown menu for  the
    composer toolbar, that works for both the new rich text editor, and
    the old markdown editor.
    
    Features include:
    
    * Dynamically changing the icon based on the heading level
    * Checking the current heading level in the dropdown list
    * Applying the same heading level to multiple selections
    
    ---------
    
    Co-authored-by: chapoi <101828855+chapoi@users.noreply.github.com>
    Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>
    Co-authored-by: Renato Atilio <renatoat@gmail.com>

--- app/assets/javascripts/discourse/app/lib/textarea-text-manipulation.js
+++ app/assets/javascripts/discourse/app/lib/textarea-text-manipulation.js
@@ -776,9 +776,38 @@ export default class TextareaTextManipulation {
         sel.value = i18n(`composer.${exampleKey}`);
       }
 
-      const number = sel.value.startsWith(hval)
-        ? sel.value.slice(hlen)
-        : `${hval}${sel.value}`;
+      // Special handling for markdown headings starting with #,
+      // they are "list-like" in that they have a character at
+      // the start and a level, rather than having a surrounding format.
+      let number;
+      if (hval.includes("#")) {
+        const currentHeadingLevel = sel.value.search(/[^#]/);
+
+        // Remove existing heading level if same as the new one,
+        // mirrors list behavior.
+        if (sel.value.startsWith(hval) && currentHeadingLevel + 1 === hlen) {
+          number = sel.value.slice(hlen);
+        } else {
+          // Replace the existing heading level with the new one, or
+          // if there is no heading level, add the new one.
+          if (currentHeadingLevel > 0) {
+            number =
+              hval +
+              sel.value.slice("#".repeat(currentHeadingLevel).length + 1);
+          } else {
+            number = hval + sel.value;
+          }
+        }
+      } else {
+        // Remove existing list item if it's the same as the new
+        // head, e.g. if a line is "* list item", then it converts
+        // it to "list item"
+        if (sel.value.startsWith(hval)) {
+          number = sel.value.slice(hlen);
+        } else {
+          number = `${hval}${sel.value}`;
+        }
+      }
 
       const preNewlines = sel.pre.trim() && "\n\n";
       const postNewlines = sel.post.trim() && "\n\n";
@@ -789,10 +818,39 @@ export default class TextareaTextManipulation {
       const postChars = sel.post.length - sel.post.trimStart().length;
 
       this._insertAt(sel.start - preChars, sel.end + postChars, textToInsert);
-      this.selectText(
-        sel.start + (preNewlines.length - preChars),
-        number.length
-      );
+
+      if (opts?.excludeHeadInSelection) {
+        this.selectText(
+          sel.start + (preNewlines.length - preChars) + hval.length,
+          number.length - hval.length
+        );
+      } else {
+        this.selectText(
+          sel.start + (preNewlines.length - preChars),
+          number.length
+        );
+      }
+    }
+  }
+
+  @bind
+  applyHeading(sel, level) {
+    if (level > 0) {
+      this.applyList(sel, "#".repeat(level) + " ", "heading_text", {
+        excludeHeadInSelection: true,
+      });
+    } else {
+      // Remove heading when the Paragrah level (0) is selected.
+      const currentHeadingLevel = sel.lineVal.search(/[^#]/);
+      if (currentHeadingLevel >= 0) {
+        // When you apply the list with the same head chars, then they
+        // are removed, so we can use the same function.
+        this.applyList(
+          sel,
+          "#".repeat(currentHeadingLevel) + " ",
+          "heading_text"
+        );
+      }
     }
   }
 
