3ca57c23779edc27efbf1f01fa7156f5023b8495
Jarek Radosz <jarek@cvx.dev>
2025-07-11 18:34:11 +0200

    DEV: Move discourse-topic-voting to core (#33570)
    https://meta.discourse.org/t/373574
    
    Internal `/t/-/156778`

---
+++ plugins/discourse-topic-voting/lib/discourse_topic_voting/engine.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module ::DiscourseTopicVoting
+  class Engine < ::Rails::Engine
+    engine_name PLUGIN_NAME
+    isolate_namespace DiscourseTopicVoting
+    config.autoload_paths << File.join(config.root, "lib")
+  end
+end
