89f899a6cb6b524bfbf82d9debfe47d636092427
David Taylor <david@taylorhq.com>
2025-07-11 10:15:31 +0100

    discourse_spec

--- spec/lib/discourse_spec.rb
+++ spec/lib/discourse_spec.rb
@@ -633,14 +633,17 @@ RSpec.describe Discourse do
       head_tag_script =
         Nokogiri::HTML5
           .fragment(Theme.lookup_field(theme.id, :desktop, "head_tag"))
-          .css("script")
+          .css("link[rel=modulepreload]")
           .first
-      head_tag_js = JavascriptCache.find_by(digest: head_tag_script[:src][/\h{40}/]).content
+      head_tag_js = JavascriptCache.find_by(digest: head_tag_script[:href][/\h{40}/]).content
       expect(head_tag_js).to include(old_upload_url)
 
       js_file_script =
-        Nokogiri::HTML5.fragment(Theme.lookup_field(theme.id, :extra_js, nil)).css("script").first
-      file_js = JavascriptCache.find_by(digest: js_file_script[:src][/\h{40}/]).content
+        Nokogiri::HTML5
+          .fragment(Theme.lookup_field(theme.id, :extra_js, nil))
+          .css("link[rel=modulepreload]")
+          .first
+      file_js = JavascriptCache.find_by(digest: js_file_script[:href][/\h{40}/]).content
       expect(file_js).to include(old_upload_url)
 
       css_link_tag =
@@ -659,14 +662,17 @@ RSpec.describe Discourse do
       head_tag_script =
         Nokogiri::HTML5
           .fragment(Theme.lookup_field(theme.id, :desktop, "head_tag"))
-          .css("script")
+          .css("link[rel=modulepreload]")
           .first
-      head_tag_js = JavascriptCache.find_by(digest: head_tag_script[:src][/\h{40}/]).content
+      head_tag_js = JavascriptCache.find_by(digest: head_tag_script[:href][/\h{40}/]).content
       expect(head_tag_js).to include(old_upload_url)
 
       js_file_script =
-        Nokogiri::HTML5.fragment(Theme.lookup_field(theme.id, :extra_js, nil)).css("script").first
-      file_js = JavascriptCache.find_by(digest: js_file_script[:src][/\h{40}/]).content
+        Nokogiri::HTML5
+          .fragment(Theme.lookup_field(theme.id, :extra_js, nil))
+          .css("link[rel=modulepreload]")
+          .first
+      file_js = JavascriptCache.find_by(digest: js_file_script[:href][/\h{40}/]).content
       expect(file_js).to include(old_upload_url)
 
       css_link_tag =
@@ -684,14 +690,17 @@ RSpec.describe Discourse do
       head_tag_script =
         Nokogiri::HTML5
           .fragment(Theme.lookup_field(theme.id, :desktop, "head_tag"))
-          .css("script")
+          .css("link[rel=modulepreload]")
           .first
-      head_tag_js = JavascriptCache.find_by(digest: head_tag_script[:src][/\h{40}/]).content
+      head_tag_js = JavascriptCache.find_by(digest: head_tag_script[:href][/\h{40}/]).content
       expect(head_tag_js).to include(new_upload_url)
 
       js_file_script =
-        Nokogiri::HTML5.fragment(Theme.lookup_field(theme.id, :extra_js, nil)).css("script").first
-      file_js = JavascriptCache.find_by(digest: js_file_script[:src][/\h{40}/]).content
+        Nokogiri::HTML5
+          .fragment(Theme.lookup_field(theme.id, :extra_js, nil))
+          .css("link[rel=modulepreload]")
+          .first
+      file_js = JavascriptCache.find_by(digest: js_file_script[:href][/\h{40}/]).content
       expect(file_js).to include(new_upload_url)
 
       css_link_tag =
