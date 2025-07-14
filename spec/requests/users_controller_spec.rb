e8361505b7fd5404afa81d0f6ab93ef6bba6d095
Lhc_fl <lhcfl@outlook.com>
2025-07-14 09:32:54 +0800

    add result check

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -7837,6 +7837,12 @@ RSpec.describe UsersController do
     end
 
     context "when logged in" do
+      before do
+        Fabricate(:user_history, action: UserHistory.actions[:silence_user], target_user: user)
+        Fabricate(:user_history, action: UserHistory.actions[:suspend_user], target_user: user)
+        Fabricate(:reviewable_flagged_post, target_created_by: user)
+      end
+
       it "responds with 403 for normal users" do
         sign_in(user)
         get "/u/#{user.username}/staff-info.json"
@@ -7861,6 +7867,8 @@ RSpec.describe UsersController do
         user_instance = mock
         UsersController.any_instance.stubs(:fetch_user_from_params).returns(user_instance)
 
+        result = {}
+
         %i[
           number_of_deleted_posts
           number_of_flagged_posts
@@ -7869,11 +7877,15 @@ RSpec.describe UsersController do
           number_of_suspensions
           warnings_received_count
           number_of_rejected_posts
-        ].each { |info| user_instance.expects(info) }
+        ].each do |info|
+          user_instance.expects(info).returns(user.public_send(info))
+          result[info.to_s] = user.public_send(info)
+        end
 
         sign_in(admin)
 
         get "/u/#{user.username}/staff-info.json"
+        expect(response.parsed_body).to eq(result)
       end
     end
   end
