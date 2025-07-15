fef3df165b2b49814571ba7d26f61a98ae8456a2
Jarek Radosz <jarek@cvx.dev>
2025-07-15 13:14:51 +0200

    ace hack

--- app/assets/javascripts/discourse/app/lib/load-ace-editor.js
+++ app/assets/javascripts/discourse/app/lib/load-ace-editor.js
@@ -3,5 +3,7 @@ import { waitForPromise } from "@ember/test-waiters";
 export default async function loadAce() {
   const promise = import("discourse/static/ace-editor-bundle");
   waitForPromise(promise);
-  return await promise;
+  await promise;
+  // TODO: fix this!!
+  return window.ace;
 }
