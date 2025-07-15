87826c41e2aeb43f97ff0c0168a96baeab8823c4
Lhc_fl <lhcfl@outlook.com>
2025-07-15 21:30:53 +0800

    fix typo

--- spec/system/admin_reports_spec.rb
+++ spec/system/admin_reports_spec.rb
@@ -16,7 +16,7 @@ describe "Admin Reports", type: :system do
   context "when use_legacy_pageviews is false" do
     before { SiteSetting.use_legacy_pageviews = false }
 
-    it "redirects from site_traffic to consolidated_page_views" do
+    it "won't redirects from site_traffic to consolidated_page_views" do
       visit "/admin/reports/site_traffic"
       expect(page).to have_current_path("/admin/reports/site_traffic")
     end
