aea8833706cfc2a8aca4ac8449cc209df2b285d5
Yuriy Kurant <yuriy@discourse.org>
2025-07-17 18:04:47 +0300

    dev: adds count/indicator to sidebar messages link - take 2

--- app/assets/javascripts/discourse/app/lib/sidebar/user/community-section/my-messages-section-link.js
+++ app/assets/javascripts/discourse/app/lib/sidebar/user/community-section/my-messages-section-link.js
@@ -27,6 +27,36 @@ export default class MyMessagesSectionLink extends BaseSectionLink {
     );
   }
 
+  get showCount() {
+    return !this.currentUser?.sidebarShowCountOfNewItems;
+  }
+
+  get badgeText() {
+    if (!this.showCount) {
+      return;
+    }
+
+    if (this.currentUser.new_new_view_enabled) {
+      return "0";
+    } else {
+      return i18n("sidebar.sections.community.links.my_messages.content");
+    }
+  }
+
+  get suffixCSSClass() {
+    return "unread";
+  }
+
+  get suffixType() {
+    return "icon";
+  }
+
+  get suffixValue() {
+    if (!this.showCount) {
+      return "circle";
+    }
+  }
+
   get shouldDisplay() {
     return this.currentUser?.can_send_private_messages;
   }
