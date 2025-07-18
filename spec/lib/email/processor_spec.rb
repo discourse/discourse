discourse/discourse

commit e05ef50c709afb9b4777223e22881b695868d4c2

FIX: Disallow replies to categories when reply by email disabled (#33641)
When the reply_by_email_enabled setting is set to false, we no longer include a reply link in e-mail notifications. However, we do not prevent actual e-mails to a configured email_in address associated with a category.

This change into consideration the setting in Email::Receiver#check_address.

spec/lib/email/processor_spec.rb
@@ -140,6 +140,10 @@ RSpec.describe Email::Processor do
   end
 
   describe "from reply to email address" do
+    SiteSetting.reply_by_email_address = "reply+%{reply_key}@bar.com"
+    SiteSetting.manual_polling_enabled = true
+    SiteSetting.reply_by_email_enabled = true
+
     let(:mail) do
       "Date: Fri, 15 Jan 2016 00:12:43 +0100\nFrom: reply@bar.com\nTo: reply@bar.com\nSubject: FOO BAR\n\nFoo foo bar bar?"
     end

lib/email/receiver.rb
@@ -870,7 +870,7 @@ module Email
         return group if group
 
         category = Category.find_by_email(address)
-        return category if category
+        return category if category && SiteSetting.reply_by_email_enabled?
       end
 
       # reply

spec/lib/email/receiver_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe Email::Receiver do
     SiteSetting.email_in = true
     SiteSetting.reply_by_email_address = "reply+%{reply_key}@bar.com"
     SiteSetting.alternative_reply_by_email_addresses = "alt+%{reply_key}@bar.com"
+    SiteSetting.manual_polling_enabled = true
+    SiteSetting.reply_by_email_enabled = true
   end
 
   def process(email_name, opts = {})
@@ -1785,23 +1787,49 @@ RSpec.describe Email::Receiver do
     end
   end
 
-  describe "check_address" do
-    before { SiteSetting.reply_by_email_address = "foo+%{reply_key}@bar.com" }
+  describe "#check_address" do
+    context "when e-mail is associated with a reply key" do
+      before { SiteSetting.reply_by_email_address = "foo+%{reply_key}@bar.com" }
 
-    it "returns nil when the key is invalid" do
-      expect(Email::Receiver.check_address("fake@fake.com")).to be_nil
-      expect(
-        Email::Receiver.check_address("foo+4f97315cc828096c9cb34c6f1a0d6fe8@bar.com"),
-      ).to be_nil
+      it "returns nil when the key is invalid" do
+        expect(Email::Receiver.check_address("fake@fake.com")).to be_nil
+        expect(
+          Email::Receiver.check_address("foo+4f97315cc828096c9cb34c6f1a0d6fe8@bar.com"),
+        ).to be_nil
+      end
+
+      context "with a valid reply" do
+        it "returns the destination when the key is valid" do
+          post_reply_key = Fabricate(:post_reply_key, reply_key: "4f97315cc828096c9cb34c6f1a0d6fe8")
+
+          dest = Email::Receiver.check_address("foo+4f97315cc828096c9cb34c6f1a0d6fe8@bar.com")
+
+          expect(dest).to eq(post_reply_key)
+        end
+      end
     end
 
-    context "with a valid reply" do
-      it "returns the destination when the key is valid" do
-        post_reply_key = Fabricate(:post_reply_key, reply_key: "4f97315cc828096c9cb34c6f1a0d6fe8")
+    context "when address is associated with a category" do
+      fab!(:category) { Fabricate(:category, email_in: "category@bar.com") }
 
-        dest = Email::Receiver.check_address("foo+4f97315cc828096c9cb34c6f1a0d6fe8@bar.com")
+      context "when replying by email is enabled" do
+        it "returns the destination category" do
+          SiteSetting.reply_by_email_enabled = true
 
-        expect(dest).to eq(post_reply_key)
+          dest = described_class.check_address("category@bar.com")
+
+          expect(dest).to eq(category)
+        end
+      end
+
+      context "when replying by email is disabled" do
+        it "returns nil" do
+          SiteSetting.reply_by_email_enabled = false
+
+          dest = described_class.check_address("category@bar.com")
+
+          expect(dest).to be_nil
+        end
       end
     end
   end

commit 987ef0d2561a739f9ae12692156379234f5d0630

DEV: Incorrect site setting modifications in specs (#33708)
Modifying site settings outside of an example (or `before/after` blocks)
results in corrupting those settings in the test database.

(the was issue introduced in e05ef50c709afb9b4777223e22881b695868d4c2)

spec/lib/email/processor_spec.rb
@@ -140,9 +140,11 @@ RSpec.describe Email::Processor do
   end
 
   describe "from reply to email address" do
-    SiteSetting.reply_by_email_address = "reply+%{reply_key}@bar.com"
-    SiteSetting.manual_polling_enabled = true
-    SiteSetting.reply_by_email_enabled = true
+    before do
+      SiteSetting.reply_by_email_address = "reply+%{reply_key}@bar.com"
+      SiteSetting.manual_polling_enabled = true
+      SiteSetting.reply_by_email_enabled = true
+    end
 
     let(:mail) do
       "Date: Fri, 15 Jan 2016 00:12:43 +0100\nFrom: reply@bar.com\nTo: reply@bar.com\nSubject: FOO BAR\n\nFoo foo bar bar?"
@@ -160,6 +162,9 @@ RSpec.describe Email::Processor do
 
   describe "mailinglist mirror" do
     before do
+      SiteSetting.reply_by_email_address = "reply+%{reply_key}@bar.com"
+      SiteSetting.manual_polling_enabled = true
+      SiteSetting.reply_by_email_enabled = true
       SiteSetting.email_in = true
       Fabricate(:mailinglist_mirror_category)
     end