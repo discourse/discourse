2fd9cce8875192f0d3fc574a08c98e96613472bc
Blake Erickson <blake@discourse.org>
2025-07-18 14:59:36 -0600

    Store optimized videos in the uploads table

--- db/migrate/20250606170129_create_optimized_videos.rb
+++ db/migrate/20250606170129_create_optimized_videos.rb
@@ -3,15 +3,17 @@
 class CreateOptimizedVideos < ActiveRecord::Migration[7.2]
   def change
     create_table :optimized_videos do |t|
-      t.string :sha1
-      t.string :extension
-      t.integer :upload_id
-      t.string :url
-      t.integer :filesize
-      t.string :etag
+      t.integer :upload_id, null: false
+      t.integer :optimized_upload_id, null: false
       t.string :adapter
-
       t.timestamps
     end
+
+    add_index :optimized_videos, :upload_id
+    add_index :optimized_videos, :optimized_upload_id
+    add_index :optimized_videos, %i[upload_id adapter], unique: true
+
+    add_foreign_key :optimized_videos, :uploads, column: :upload_id
+    add_foreign_key :optimized_videos, :uploads, column: :optimized_upload_id
   end
 end
