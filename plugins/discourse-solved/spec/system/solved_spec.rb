1906e82cf104c30b920d0444e95e966eeacfa9a2
Sérgio Saquetim <saquetim@discourse.org>
2025-07-22 17:35:26 -0300

    DEV: Refactor `solved` plugin specs to extract reusable helpers
    Abstracted repeated actions and verifications into private helper methods in `solved_spec.rb`. This change improves test clarity, reduces duplication, and enhances maintainability.

--- plugins/discourse-solved/spec/system/solved_spec.rb
+++ plugins/discourse-solved/spec/system/solved_spec.rb
@@ -5,7 +5,15 @@ describe "Solved", type: :system do
   fab!(:accepter) { Fabricate(:user) }
   fab!(:topic) { Fabricate(:post, user: admin).topic }
   fab!(:solver_post) { Fabricate(:post, topic:, user: solver, cooked: "The answer is 42") }
+
   let(:topic_page) { PageObjects::Pages::Topic.new }
+
+  UNACCEPTED_BUTTON_SELECTOR = ".post-action-menu__solved-unaccepted"
+  ACCEPTED_BUTTON_SELECTOR = ".post-action-menu__solved-accepted"
+  ACCEPTED_ANSWER_QUOTE_SELECTOR = "aside.accepted-answer.quote"
+  SOLVER_INFO_SELECTOR = ".title .accepted-answer--solver"
+  ACCEPTER_INFO_SELECTOR = ".title .accepted-answer--accepter"
+  QUOTE_TOGGLE_SELECTOR = "aside.accepted-answer.quote button.quote-toggle"
 
   before do
     SiteSetting.solved_enabled = true
@@ -20,55 +28,89 @@ describe "Solved", type: :system do
 
       it "accepts post as solution and shows in OP" do
         sign_in(accepter)
-        topic_page.visit_topic(topic, post_number: 2)
+        visit_solver_post
 
-        expect(topic_page).to have_css(".post-action-menu__solved-unaccepted")
-        find(".post-action-menu__solved-unaccepted").click
-
-        expect(topic_page).to have_css(".post-action-menu__solved-accepted")
-
-        expect(topic_page).to have_css("aside.accepted-answer.quote[data-expanded='false']")
-        expect(topic_page.find("aside.accepted-answer.quote blockquote")).to have_content(
-          "The answer is 42",
-        )
-        expect(topic_page.find(".title .accepted-answer--solver")).to have_content(
-          "Solved by #{solver.username}",
-        )
-        expect(topic_page.find(".title .accepted-answer--accepter")).to have_content(
-          "Marked as solved by #{accepter.username}",
-        )
-
-        topic_page.find("aside.accepted-answer.quote button.quote-toggle").click
-        expect(topic_page).to have_css("aside.accepted-answer.quote[data-expanded='true']")
+        verify_solution_unaccepted_state
+        accept_solution
+        verify_solution_accepted_state
+        verify_solution_quote_content
+        verify_solver_and_accepter_info
+        expand_solution_quote
       end
 
       it "accepts and unaccepts post as solution" do
         sign_in(accepter)
-        topic_page.visit_topic(topic, post_number: 2)
+        visit_solver_post
 
-        expect(topic_page).to have_css(".post-action-menu__solved-unaccepted")
-        find(".post-action-menu__solved-unaccepted").click
-        expect(topic_page).to have_css(".post-action-menu__solved-accepted")
+        verify_solution_unaccepted_state
+        accept_solution
+        verify_solution_accepted_state
+        verify_solution_info_present
 
-        expect(topic_page).to have_css(".accepted-answer.quote")
-        expect(topic_page).to have_css(".title .accepted-answer--solver")
-        expect(topic_page).to have_css(".title .accepted-answer--accepter")
-
-        find(".post-action-menu__solved-accepted").click
-        expect(topic_page).to have_css(".post-action-menu__solved-unaccepted")
-        expect(topic_page).to have_no_css(".accepted-answer.quote")
-        expect(topic_page).to have_no_css(".title .accepted-answer--solver")
-        expect(topic_page).to have_no_css(".title .accepted-answer--accepter")
+        unaccept_solution
+        verify_solution_unaccepted_state
+        verify_solution_info_absent
       end
     end
   end
 
   it "shows the solved post in user activity at /my/activity/solved" do
     Fabricate(:solved_topic, topic:, answer_post: solver_post, accepter:)
-
     sign_in(solver)
     visit "/my/activity/solved"
-
     expect(page.find(".post-list")).to have_content(solver_post.cooked)
+  end
+
+  private
+
+  def visit_solver_post
+    topic_page.visit_topic(topic, post_number: 2)
+  end
+
+  def accept_solution
+    find(UNACCEPTED_BUTTON_SELECTOR).click
+  end
+
+  def unaccept_solution
+    find(ACCEPTED_BUTTON_SELECTOR).click
+  end
+
+  def verify_solution_accepted_state
+    expect(topic_page).to have_css(ACCEPTED_BUTTON_SELECTOR)
+    expect(topic_page).to have_css("#{ACCEPTED_ANSWER_QUOTE_SELECTOR}[data-expanded='false']")
+  end
+
+  def verify_solution_unaccepted_state
+    expect(topic_page).to have_css(UNACCEPTED_BUTTON_SELECTOR)
+  end
+
+  def verify_solution_quote_content
+    expect(topic_page.find("#{ACCEPTED_ANSWER_QUOTE_SELECTOR} blockquote")).to have_content(
+      "The answer is 42",
+    )
+  end
+
+  def verify_solver_and_accepter_info
+    expect(topic_page.find(SOLVER_INFO_SELECTOR)).to have_content("Solved by #{solver.username}")
+    expect(topic_page.find(ACCEPTER_INFO_SELECTOR)).to have_content(
+      "Marked as solved by #{accepter.username}",
+    )
+  end
+
+  def verify_solution_info_present
+    expect(topic_page).to have_css(ACCEPTED_ANSWER_QUOTE_SELECTOR)
+    expect(topic_page).to have_css(SOLVER_INFO_SELECTOR)
+    expect(topic_page).to have_css(ACCEPTER_INFO_SELECTOR)
+  end
+
+  def verify_solution_info_absent
+    expect(topic_page).to have_no_css(ACCEPTED_ANSWER_QUOTE_SELECTOR)
+    expect(topic_page).to have_no_css(SOLVER_INFO_SELECTOR)
+    expect(topic_page).to have_no_css(ACCEPTER_INFO_SELECTOR)
+  end
+
+  def expand_solution_quote
+    topic_page.find(QUOTE_TOGGLE_SELECTOR).click
+    expect(topic_page).to have_css("#{ACCEPTED_ANSWER_QUOTE_SELECTOR}[data-expanded='true']")
   end
 end
