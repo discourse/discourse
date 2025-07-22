26ef789ea6b487e2d5b01f22a5adb2bdfa9889cc
Sérgio Saquetim <saquetim@discourse.org>
2025-07-22 20:59:17 -0300

    DEV: Add deprecation URL to `WIDGET_DEPRECATION_OPTIONS`
    Updated the `url` field in `WIDGET_DEPRECATION_OPTIONS` to include a link to the meta topic detailing the deprecation of widgets.

--- app/assets/javascripts/discourse/app/widgets/widget.js
+++ app/assets/javascripts/discourse/app/widgets/widget.js
@@ -35,7 +35,7 @@ import { i18n } from "discourse-i18n";
 export const WIDGET_DEPRECATION_OPTIONS = {
   since: "v3.5.0.beta8-dev",
   id: "discourse.widgets-end-of-life",
-  url: "", // TODO (widgets-end-of-life): Add URL to meta topic
+  url: "https://meta.discourse.org/t/375332/1",
 };
 
 export const POST_STREAM_DEPRECATION_OPTIONS = {
