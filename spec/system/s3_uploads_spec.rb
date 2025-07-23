c95e8a56576ab02b1d092fd8b050a88c6120591c
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-07-23 22:50:44 +0200

    more resilient specs

--- spec/system/s3_uploads_spec.rb
+++ spec/system/s3_uploads_spec.rb
@@ -17,9 +17,11 @@ describe "Uploading files in the composer to S3", type: :system do
 
         find("#edit-avatar").click
         find("#uploaded-avatar").click
-        attach_file(File.absolute_path(file_from_fixtures("logo.jpg"))) do
-          find("#avatar-uploader").click
-        end
+        attach_file(
+          "#avatar-uploader",
+          File.absolute_path(file_from_fixtures("logo.jpg")),
+          make_visible: true,
+        )
         expect(page).to have_css(".avatar-uploader .avatar-uploader__button[data-uploaded]")
         modal.click_primary_button
         expect(modal).to be_closed
