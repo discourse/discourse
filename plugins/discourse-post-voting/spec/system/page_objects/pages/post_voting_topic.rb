ad73a3d577a507f29b433311b8ab9b2be430d6f0
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2025-07-23 09:05:36 +0800

    FEATURE: Add site setting to allow comments to be disabled
    This commit introduces a post_voting_comment_enabled site setting to
    control whether comments are allowed on posts.

--- plugins/discourse-post-voting/spec/system/page_objects/pages/post_voting_topic.rb
+++ plugins/discourse-post-voting/spec/system/page_objects/pages/post_voting_topic.rb
@@ -6,6 +6,10 @@ module PageObjects
       COMMENT_VOTE_BUTTON = ".post-voting-comment-actions-vote button"
       POST_VOTE_BUTTON = ".post-voting-post button"
       COMMENT_ACTIONS = ".post-voting-comment-actions"
+
+      def has_no_comment_menu?
+        has_no_css?(".post-voting-comments-menu")
+      end
     end
   end
 end
