43b017f1dcc33ad8486626302057f4d8a98d59ac
David Taylor <david@taylorhq.com>
2025-07-24 12:42:29 +0100

    tidy

--- app/assets/javascripts/theme-transpiler/shims.js
+++ app/assets/javascripts/theme-transpiler/shims.js
@@ -4,10 +4,17 @@ import "core-js/actual/url";
 import { TextDecoder, TextEncoder } from "fastestsmallesttextencoderdecoder";
 import path from "path";
 import getRandomValues from "polyfill-crypto.getrandomvalues";
+import BindingsWasm from "./node_modules/@rollup/browser/dist/bindings_wasm_bg.wasm";
 
 const CONSOLE_PREFIX = "[DiscourseJsProcessor] ";
 globalThis.window = {};
 globalThis.console = {
+  debug(...args) {
+    rails.logger.info(CONSOLE_PREFIX + args.join(" "));
+  },
+  info(...args) {
+    rails.logger.info(CONSOLE_PREFIX + args.join(" "));
+  },
   log(...args) {
     rails.logger.info(CONSOLE_PREFIX + args.join(" "));
   },
@@ -27,3 +34,21 @@ path.win32 = {
 };
 
 globalThis.crypto = { getRandomValues };
+
+const oldInstantiate = WebAssembly.instantiate;
+WebAssembly.instantiate = async function (bytes, bindings) {
+  if (bytes === BindingsWasm) {
+    const mod = new WebAssembly.Module(bytes);
+    const instance = new WebAssembly.Instance(mod, bindings);
+    return instance;
+  } else {
+    return oldInstantiate(...arguments);
+  }
+};
+
+globalThis.fetch = function (url) {
+  if (url.toString() === "http://example.com/bindings_wasm_bg.wasm") {
+    return new Promise((resolve) => resolve(BindingsWasm));
+  }
+  throw "fetch not implemented";
+};
