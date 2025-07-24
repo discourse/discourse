f3d645d700e603c667a78640e3d9aa62e1dec2f7
Jordan Vidrine <jordan@jordanvidrine.com>
2025-07-23 19:38:33 -0500

    Revert "init"
    This reverts commit 96eee5dc7ba3b38f888898fe048f9a9f97ee2222.

--- plugins/discourse-ai/assets/javascripts/initializers/ai-semantic-search.js
+++
@@ -1,9 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-
-export default apiInitializer("1.15.0", (api) => {
-  api.modifyClass("component:search-result-entry", {
-    pluginId: "discourse-ai",
-
-    classNameBindings: ["bulkSelectEnabled", "post.generatedByAi:ai-result"],
-  });
-});
