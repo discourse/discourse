322dac9c63e4b5ae5421c5790305d94375637edf
Selase Krakani <selase@discourse.org>
2025-07-30 17:48:06 +0000

    DEV: Add support for converting and importing tags
    This adds converter(Discourse-only, for now) and importer steps for tags

---
+++ migrations/lib/database/intermediate_db/tag.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# This file is auto-generated from the IntermediateDB schema. To make changes,
+# update the "config/intermediate_db.yml" configuration file and then run
+# `bin/cli schema generate` to regenerate this file.
+
+module Migrations::Database::IntermediateDB
+  module Tag
+    SQL = <<~SQL
+      INSERT INTO tags (
+        original_id,
+        created_at,
+        description,
+        name,
+        target_tag_id
+      )
+      VALUES (
+        ?, ?, ?, ?, ?
+      )
+    SQL
+
+    def self.create(original_id:, created_at: nil, description: nil, name:, target_tag_id: nil)
+      ::Migrations::Database::IntermediateDB.insert(
+        SQL,
+        original_id,
+        ::Migrations::Database.format_datetime(created_at),
+        description,
+        name,
+        target_tag_id,
+      )
+    end
+  end
+end
