eb40df05a1abca7116785070466fd7f1bf5aeaa0
Sam Saffron <sam.saffron@gmail.com>
2025-07-30 10:19:11 +1000

    DEV: avoid calling sidekiq in migration

--- plugins/discourse-rss-polling/db/migrate/20180828095129_push_fix_topic_embed_authors_job.rb
+++ plugins/discourse-rss-polling/db/migrate/20180828095129_push_fix_topic_embed_authors_job.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class PushFixTopicEmbedAuthorsJob < ActiveRecord::Migration[5.2]
-  def change
-    Jobs.enqueue("DiscourseRssPolling::FixTopicEmbedAuthors")
+  def up
+    # This migrations was originally performed by the job
+    # Jobs.enqueue("DiscourseRssPolling::FixTopicEmbedAuthors")
+    #
+    # It is now commented out to avoid brittle migrations.
+    # This means that impacted instances that have not run the migration since 2018 will be sensitive to this change where topic embeds may point at the wrong posts
+    #
+    # This risk is acceptable given running a job in sidekiq during a migration is not recommended and more fragile
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
