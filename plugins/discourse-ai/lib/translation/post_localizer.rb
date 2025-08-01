10df7c63dacbf953b31f14e33b9c0e7fb3e0fa89
Nat <natalie.tay@discourse.org>
2025-08-01 15:16:40 +0800

    FEATURE: Allow re-localization twice a day if post version has changed

--- plugins/discourse-ai/lib/translation/post_localizer.rb
+++ plugins/discourse-ai/lib/translation/post_localizer.rb
@@ -4,8 +4,8 @@ module DiscourseAi
   module Translation
     class PostLocalizer
       def self.localize(post, target_locale = I18n.locale)
-        if post.blank? || target_locale.blank? || post.locale == target_locale.to_s ||
-             post.raw.blank?
+        if post.blank? || target_locale.blank? ||
+             LocaleNormalizer.is_same?(post.locale, target_locale) || post.raw.blank?
           return
         end
         return if post.raw.length > SiteSetting.ai_translation_max_post_length
@@ -22,6 +22,33 @@ module DiscourseAi
         localization.localizer_user_id = Discourse.system_user.id
         localization.save!
         localization
+      end
+
+      def self.has_relocalize_quota?(post, locale, skip_incr: false)
+        return false if get_relocalize_quota(post, locale).to_i >= 2
+
+        incr_relocalize_quota(post, locale) unless skip_incr
+        true
+      end
+
+      private
+
+      def self.relocalize_key(post, locale)
+        "post_relocalized_#{post.id}_#{locale}"
+      end
+
+      def self.get_relocalize_quota(post, locale)
+        Discourse.redis.get(relocalize_key(post, locale)) || 0
+      end
+
+      def self.incr_relocalize_quota(post, locale)
+        key = relocalize_key(post, locale)
+
+        if get_relocalize_quota(post, locale).blank?
+          Discourse.redis.set(key, 1, ex: 1.day.to_i)
+        else
+          Discourse.redis.incr(key)
+        end
       end
     end
   end
