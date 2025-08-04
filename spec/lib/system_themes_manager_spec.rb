42fcc14e39dc8b15504cbef87088c1a77a2eab75
OsamaSayegh <asooomaasoooma90@gmail.com>
2025-08-04 12:49:26 +0300

    FIX: Don't force user-selectable on Horizon's default color palettes

--- spec/lib/system_themes_manager_spec.rb
+++ spec/lib/system_themes_manager_spec.rb
@@ -19,4 +19,21 @@ RSpec.describe SystemThemesManager do
     SystemThemesManager.sync!
     expect(Theme.horizon_theme.reload.enabled).to be true
   end
+
+  it "marks Horizon's default color palettes as user selectable only the first time the theme is installed" do
+    Theme.delete_all
+
+    expect { SystemThemesManager.sync! }.to change { Theme.system.count }.by(2)
+
+    expect(Theme.horizon_theme.color_scheme.user_selectable).to eq(true)
+    expect(Theme.horizon_theme.dark_color_scheme.user_selectable).to eq(true)
+
+    Theme.horizon_theme.color_scheme.update!(user_selectable: false)
+    Theme.horizon_theme.dark_color_scheme.update!(user_selectable: false)
+
+    expect { SystemThemesManager.sync! }.not_to change { Theme.system.count }
+
+    expect(Theme.horizon_theme.color_scheme.reload.user_selectable).to eq(false)
+    expect(Theme.horizon_theme.dark_color_scheme.reload.user_selectable).to eq(false)
+  end
 end
