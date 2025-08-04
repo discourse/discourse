7491466b267738ee01a470cca7efb0f6bf5f5296
Gary <gary@pento.net>
2025-08-04 15:16:31 +1000

    DEV: Ensure the image is processed, and the click is handled.

--- spec/system/composer/prosemirror_editor_spec.rb
+++ spec/system/composer/prosemirror_editor_spec.rb
@@ -31,7 +31,9 @@ describe "Composer - ProseMirror editor", type: :system do
     cdp.paste
 
     expect(rich).to have_css(".composer-image-node img", count: 1)
+    expect(rich).to have_no_css(".composer-image-node img[data-placeholder='true']")
     rich.find(".composer-image-node img").click
+    expect(rich).to have_css(".composer-image-node composer-image-toolbar", count: 1)
   end
 
   it "hides the Composer container's preview button" do
