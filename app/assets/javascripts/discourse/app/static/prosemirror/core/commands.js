14e362b643051db51cec22abd9739f906526f0b7
Renato Atilio <renato@discourse.org>
2025-08-07 17:00:43 -0300

    UX: improvements to rich editor spoiler

---
+++ app/assets/javascripts/discourse/app/static/prosemirror/core/commands.js
@@ -0,0 +1,34 @@
+export function buildCommands(extensions, pluginParams, view) {
+  const allCommands = {};
+
+  for (const { commands } of extensions) {
+    if (commands) {
+      for (let [name, command] of Object.entries(
+        commands(pluginParams, view)
+      )) {
+        allCommands[name] = (...args) => {
+          view.focus();
+          return command(...args)(view.state, view.dispatch);
+        };
+      }
+    }
+  }
+  return allCommands;
+}
+
+export function buildCustomState(extensions, params) {
+  return (viewState) => {
+    const allCustomStates = {};
+
+    for (const { state } of extensions) {
+      if (state) {
+        for (let [name, stateResult] of Object.entries(
+          state(params, viewState)
+        )) {
+          allCustomStates[name] = stateResult;
+        }
+      }
+    }
+    return allCustomStates;
+  };
+}
