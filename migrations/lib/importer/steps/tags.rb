7e8c9b5d0e0e60dadd4c9b43a0e20677adadd141
Selase Krakani <selase@discourse.org>
2025-08-08 01:09:20 +0000

    DEV: Improve `max_tag_length` site setting update for tag imports
    1. Update the `max_tag_length` site setting based on the longest tag
    2. Improve synonym tag validation by also checking against existing tags

--- migrations/lib/importer/steps/tags.rb
+++ migrations/lib/importer/steps/tags.rb
@@ -7,10 +7,12 @@ module Migrations::Importer::Steps
     MAX_DESCRIPTION_LENGTH = 1000
     RESERVED_TAGS = Tag::RESERVED_TAGS.to_set.freeze
 
-    store_mapped_ids true
+    requires_set :existing_synonym_tag_ids, "SELECT id FROM tags WHERE target_tag_id IS NOT NULL"
     requires_mapping :existing_tag_by_name, "SELECT LOWER(name), id FROM tags"
 
     column_names %i[id name description target_tag_id created_at updated_at]
+
+    store_mapped_ids true
 
     total_rows_query <<~SQL, MappingType::TAGS
       SELECT COUNT(*)
@@ -30,7 +32,8 @@ module Migrations::Importer::Steps
     SQL
 
     def execute
-      SiteSetting.max_tag_length = 100 if SiteSetting.max_tag_length < 100
+      max_tag_length = @intermediate_db.query_value("SELECT MAX(LENGTH(name)) FROM tags") || 0
+      SiteSetting.max_tag_length = max_tag_length if SiteSetting.max_tag_length < max_tag_length
 
       @mapped_tag_ids = @intermediate_db.query_array(<<~SQL, MappingType::TAGS).to_h
         SELECT original_id, discourse_id FROM  mapped.ids WHERE type = ?
@@ -38,14 +41,14 @@ module Migrations::Importer::Steps
 
       synonym_pairs_sql =
         "SELECT original_id, target_tag_id FROM tags WHERE target_tag_id IS NOT NULL"
-      @synonym_target_ids = Set.new
-      @synonym_ids = Set.new
+      @synonym_target_tag_ids = Set.new
+      @synonym_tag_ids = Set.new
 
       @intermediate_db
         .query_array(synonym_pairs_sql)
         .each do |synonym_id, target_id|
-          @synonym_ids.add(synonym_id)
-          @synonym_target_ids.add(target_id)
+          @synonym_tag_ids.add(synonym_id)
+          @synonym_target_tag_ids.add(target_id)
         end
 
       super
@@ -83,12 +86,13 @@ module Migrations::Importer::Steps
           return nil
         end
 
-        if @synonym_target_ids.include?(row[:original_id])
+        if @synonym_target_tag_ids.include?(row[:original_id])
           puts "    Tag '#{name}' cannot become a synonym because it already has synonyms"
           return nil
         end
 
-        if @synonym_ids.include?(original_target_tag_id)
+        if @synonym_tag_ids.include?(original_target_tag_id) ||
+             @existing_synonym_tag_ids.include?(discourse_target_tag_id)
           puts "    Tag '#{name}' cannot point to another synonym tag (target_tag_id: #{original_target_tag_id})"
           return nil
         end
