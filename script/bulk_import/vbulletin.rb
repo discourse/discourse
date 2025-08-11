83ede7098780ad4763aa4d128e7c42bbec1100b4
Ted Johansson <ted@discourse.org>
2025-08-11 14:56:38 +0800

    DEV: Remove automatic_backups_enabled setting (#33558)
    We're removing the automatic_backups_enabled toggle and depending solely on the backup_frequency value. This PR replaces the setting and adds a migration for any forum that has automatic_backups_enabled disabled.

--- script/bulk_import/vbulletin.rb
+++ script/bulk_import/vbulletin.rb
@@ -48,7 +48,7 @@ class BulkImport::VBulletin < BulkImport::Base
 
   def execute
     # enable as per requirement:
-    # SiteSetting.automatic_backups_enabled = false
+    # SiteSetting.backup_frequency = nil
     # SiteSetting.disable_emails = "non-staff"
     # SiteSetting.authorized_extensions = '*'
     # SiteSetting.max_image_size_kb = 102400
