0531793a9b6a0a94119a092c493f91d99351fcb0
Nat <natalie.tay@discourse.org>
2025-08-11 15:30:48 +0800

    FEATURE: Localize topic titles and category names in crawler view

--- lib/topic_list_responder.rb
+++ lib/topic_list_responder.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# Helps us respond with a topic list from a controller
 module TopicListResponder
   def respond_with_list(list)
     discourse_expires_in 1.minute
@@ -8,6 +7,8 @@ module TopicListResponder
     respond_to do |format|
       format.html do
         @list = list
+        localize_topic_list_content(list)
+
         store_preloaded(
           list.preload_key,
           MultiJson.dump(TopicListSerializer.new(list, scope: guardian)),
@@ -15,6 +16,33 @@ module TopicListResponder
         render "list/list"
       end
       format.json { render_serialized(list, TopicListSerializer) }
+    end
+  end
+
+  private
+
+  def localize_topic_list_content(list)
+    return if list.topics.blank? || !SiteSetting.content_localization_enabled
+    crawl_locale = params[:lang].presence || SiteSetting.default_locale
+
+    list.topics.each { |topic| replace_topic_attributes(crawl_locale, topic) }
+  end
+
+  def replace_topic_attributes(crawl_locale, topic)
+    if topic.locale.present? && !LocaleNormalizer.is_same?(topic.locale, crawl_locale) &&
+         (loc = topic.get_localization(crawl_locale))
+      topic.title = loc.title if loc.title.present?
+      topic.excerpt = loc.excerpt if loc.excerpt.present?
+
+      category = topic.category
+      replace_category_name(category, crawl_locale)
+    end
+  end
+
+  def replace_category_name(category, crawl_locale)
+    if category.locale.present? && !LocaleNormalizer.is_same?(category.locale, crawl_locale) &&
+         (category_loc = category.get_localization(crawl_locale))
+      category.name = category_loc.name if category_loc.name.present?
     end
   end
 end
