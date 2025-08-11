f8ac0bad72eb54fab45e6d5377a235e26e5433b8
Yuriy Kurant <yuriy@discourse.org>
2025-08-11 07:38:50 +0300

    DEV: remove experimental_sidebar_messages_count_enabled_groups (#34167)
    Removes the temporary `experimental_sidebar_messages_count_enabled_groups` site setting.
    
    Reverts #33774.

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -81,7 +81,6 @@ class CurrentUserSerializer < BasicUserSerializer
              :can_localize_content?,
              :effective_locale,
              :use_reviewable_ui_refresh,
-             :use_experimental_sidebar_messages_count,
              :can_see_ip
 
   delegate :user_stat, to: :object, private: true
@@ -353,10 +352,6 @@ class CurrentUserSerializer < BasicUserSerializer
 
   def use_reviewable_ui_refresh
     scope.can_see_reviewable_ui_refresh?
-  end
-
-  def use_experimental_sidebar_messages_count
-    scope.user.in_any_groups?(SiteSetting.experimental_sidebar_messages_count_enabled_groups_map)
   end
 
   def include_use_reviewable_ui_refresh?
