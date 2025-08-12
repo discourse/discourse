507a90e93a6d673548a152308d44217bf48fde39
Osama Sayegh <asooomaasoooma90@gmail.com>
2025-08-12 09:23:20 +0300

    FIX: Don't make Horizon's palettes user-selectable (#34062)
    Internal topic: t/160291.

--- spec/lib/system_themes_manager_spec.rb
+++ spec/lib/system_themes_manager_spec.rb
@@ -19,4 +19,21 @@ RSpec.describe SystemThemesManager do
     SystemThemesManager.sync!
     expect(Theme.horizon_theme.reload.enabled).to be true
   end
+
+  it "sets up the default light and dark palettes for Horizon on the initial install" do
+    Theme.delete_all
+
+    expect { SystemThemesManager.sync! }.to change { Theme.system.count }.by(2)
+
+    expect(Theme.horizon_theme.color_scheme.name).to eq("Horizon")
+    expect(Theme.horizon_theme.dark_color_scheme.name).to eq("Horizon Dark")
+
+    Theme.horizon_theme.update!(color_scheme: nil)
+    Theme.horizon_theme.update!(dark_color_scheme: nil)
+
+    expect { SystemThemesManager.sync! }.not_to change { Theme.system.count }
+
+    expect(Theme.horizon_theme.color_scheme).to eq(nil)
+    expect(Theme.horizon_theme.dark_color_scheme).to eq(nil)
+  end
 end
