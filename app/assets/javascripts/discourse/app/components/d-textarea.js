30ef0fd05239842cc72b4b55bd6992e18b3f849a
Jarek Radosz <jarek@cvx.dev>
2025-08-12 13:18:50 +0200

    built-in-components

--- app/assets/javascripts/discourse/app/components/d-textarea.js
+++ app/assets/javascripts/discourse/app/components/d-textarea.js
@@ -1,5 +1,5 @@
-import { TextArea } from "@ember/legacy-built-in-components";
 import { attributeBindings } from "@ember-decorators/component";
+import TextArea from "discourse/components/textarea";
 
 @attributeBindings("aria-label")
 export default class DTextarea extends TextArea {}
