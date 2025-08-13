c9421c11205a03d9e1da2faf6f657b5d22434d03
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2025-08-13 15:52:19 +0800

    DEV: Promote historic core plugins post_deploy migrations (#34274)
    This commit promotes all post_deploy migrations which existed in
    Discourse v3.4.0 (timestamp <= 20241023041126)

--- plugins/chat/app/models/chat/mention_notification.rb
+++ plugins/chat/app/models/chat/mention_notification.rb
@@ -2,10 +2,6 @@
 
 module Chat
   class MentionNotification < ActiveRecord::Base
-    self.ignored_columns = [
-      :old_notification_id, # TODO: Remove once 20240829140227_drop_chat_mention_notifications_old_id_column has been promoted to pre-deploy
-    ]
-
     self.table_name = "chat_mention_notifications"
 
     belongs_to :chat_mention, class_name: "Chat::Mention"
