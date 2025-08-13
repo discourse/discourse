discourse/discourse

commit 470b2891c5370ecb0232ed299cd0254ae9c7f4d8

FIX: Theme site settings not reloading across processes (#34242)
This commit fixes an issue in multi-process setups where the following
would happen:

1. Admin changes a theme site setting. The new value is stored in memory
   in the process that made the change. For this example, changing
   `enable_welcome_banner` from `true` to `false`
2. Another user visits the site and hits another process, which has the
   old value of `enable_welcome_banner` still set to `true` in memory
3. The other user's value for `enable_welcome_banner` is cached in
   the cache for theme site settings, which is shared across all
   processes

The issue is fixed by doing the same thing as site settings: when the
new value is saved, we send a MessageBus message to all processes via
`SiteSettingExtension` which calls `refresh!` and reloads the in-memory
cache of theme site settings from the DB.

c.f.
https://meta.discourse.org/t/difficulty-understanding-the-enable-welcome-banner-setting/377321

lib/site_setting_extension.rb
@@ -177,7 +177,7 @@ module SiteSettingExtension
   end
 
   def setting_metadata_hash(setting)
-    setting_hash = {
+    {
       setting:,
       default: SiteSetting.defaults[setting],
       description: SiteSetting.description(setting),
@@ -433,6 +433,7 @@ module SiteSettingExtension
 
         theme_site_setting_changes, theme_site_setting_deletions =
           diff_hash(new_theme_site_settings, theme_site_settings)
+
         theme_site_setting_changes.each do |theme_id, settings|
           theme_site_settings[theme_id] ||= {}
           theme_site_settings[theme_id].merge!(settings)
@@ -599,6 +600,7 @@ module SiteSettingExtension
     theme_site_settings[theme_id][name] = val
 
     notify_clients!(name, theme_id: theme_id) if client_settings.include?(name)
+    notify_changed!
 
     clear_cache!(expire_theme_site_setting_cache: true)
 

lib/site_settings/local_process_provider.rb
@@ -21,6 +21,8 @@ class SiteSettings::LocalProcessProvider
     end
   end
 
+  attr_accessor :model
+
   def settings
     @settings[current_site] ||= {}
   end
@@ -46,6 +48,11 @@ class SiteSettings::LocalProcessProvider
       settings[name] = setting
     end
     setting.value = value.to_s
+
+    # So we can simulate the MessageBus notifications
+    # done by the DbProvider
+    @model.notify_changed! if @model
+
     setting
   end
 

spec/lib/site_setting_extension_spec.rb
@@ -41,7 +41,13 @@ RSpec.describe SiteSettingExtension do
     new_settings(provider_local)
   end
 
-  it "Does not leak state cause changes are not linked" do
+  after do
+    settings.provider.model = nil
+    settings.listen_for_changes = false
+    settings2.listen_for_changes = false
+  end
+
+  it "does not leak state cause changes are not linked" do
     t1 =
       Thread.new do
         5.times do
@@ -89,7 +95,7 @@ RSpec.describe SiteSettingExtension do
       expect(settings.hello).to eq(99)
     end
 
-    it "publishes changes cross sites" do
+    it "picks up changes from provider on refresh across processes" do
       settings.setting(:hello, 1)
       settings2.setting(:hello, 1)
 
@@ -101,6 +107,30 @@ RSpec.describe SiteSettingExtension do
       settings.hello = 99
 
       settings2.refresh!
+      expect(settings2.hello).to eq(99)
+    end
+
+    it "publishes changes across processes" do
+      MessageBus.on
+
+      # Only need to do this once, since both settings will use the same provider. Doing it twice leads to confusion with MessageBus
+      settings.provider.model = settings
+
+      settings.listen_for_changes = true
+      settings2.listen_for_changes = true
+
+      settings.refresh!
+      settings2.refresh!
+
+      settings.setting(:hello, 1)
+      settings2.setting(:hello, 1)
+
+      settings.hello = 100
+
+      expect(settings2.hello).to eq(100)
+
+      settings.hello = 99
+
       expect(settings2.hello).to eq(99)
     end
 
@@ -1046,6 +1076,38 @@ RSpec.describe SiteSettingExtension do
       expect(SiteSetting.enable_welcome_banner(theme_id: theme_2.id)).to eq(true)
       expect(SiteSetting.search_experience(theme_id: theme_1.id)).to eq("search_icon")
       expect(SiteSetting.search_experience(theme_id: theme_2.id)).to eq("search_field")
+    end
+
+    it "publishes changes across processes" do
+      MessageBus.on
+
+      settings_tss_instance_1 = new_settings(provider_local)
+      settings_tss_instance_2 = new_settings(provider_local)
+
+      settings_tss_instance_1.listen_for_changes = true
+      settings_tss_instance_2.listen_for_changes = true
+
+      settings_tss_instance_1.load_settings(File.join(Rails.root, "config", "site_settings.yml"))
+      settings_tss_instance_2.load_settings(File.join(Rails.root, "config", "site_settings.yml"))
+
+      settings_tss_instance_1.refresh!
+      settings_tss_instance_2.refresh!
+
+      expect(settings_tss_instance_1.enable_welcome_banner(theme_id: theme_1.id)).to eq(false)
+      expect(settings_tss_instance_2.enable_welcome_banner(theme_id: theme_1.id)).to eq(false)
+
+      tss_1.update!(value: true)
+      settings_tss_instance_1.change_themeable_site_setting(
+        theme_1.id,
+        :enable_welcome_banner,
+        true,
+      )
+
+      # Get through the MessageBus queue
+      try_until_success(frequency: 0.5) do
+        expect(settings_tss_instance_1.enable_welcome_banner(theme_id: theme_1.id)).to eq(true)
+        expect(settings_tss_instance_2.enable_welcome_banner(theme_id: theme_1.id)).to eq(true)
+      end
     end
 
     describe ".theme_site_settings_json_uncached" do

app/models/remote_theme.rb
@@ -484,7 +484,7 @@ class RemoteTheme < ActiveRecord::Base
           value: value,
         },
         guardian: Discourse.system_user.guardian,
-      ) { |result| }
+      )
     end
 
     SiteSetting.refresh!(refresh_site_settings: false, refresh_theme_site_settings: true)

spec/support/helpers.rb
@@ -318,6 +318,17 @@ module Helpers
     SiteSetting.public_send("#{plugin.enabled_site_setting}=", true)
   end
 
+  def try_until_success(timeout: 3, frequency: 0.01)
+    start ||= Time.zone.now
+    backoff ||= frequency
+    yield
+  rescue RSpec::Expectations::ExpectationNotMetError
+    raise if Time.zone.now >= start + timeout.seconds
+    sleep backoff
+    backoff += frequency
+    retry
+  end
+
   private
 
   def directory_from_caller

commit 7075fff64e24bb4f6f9e07cb320b6cd9fcf7a918

DEV: Fix core backend CI tests timing out
The test cases added in 470b2891c5370ecb0232ed299cd0254ae9c7f4d8 caused
core backend tests to start timing out on CI because MessageBus messages
were being processed in a background thread outside of the current
database transation.

We are currently working around the problem by updating the test cases
to just assert that the right MessageBus message is being published.

lib/site_setting_extension.rb
@@ -448,11 +448,13 @@ module SiteSettingExtension
     end
   end
 
+  SITE_SETTINGS_CHANNEL = "/site_settings"
+
   def ensure_listen_for_changes
     return if @listen_for_changes == false
 
     unless @subscribed
-      MessageBus.subscribe("/site_settings") do |message|
+      MessageBus.subscribe(SITE_SETTINGS_CHANNEL) do |message|
         process_message(message) if message.data["process"] != process_id
       end
 

lib/site_settings/local_process_provider.rb
@@ -21,8 +21,6 @@ class SiteSettings::LocalProcessProvider
     end
   end
 
-  attr_accessor :model
-
   def settings
     @settings[current_site] ||= {}
   end
@@ -48,10 +46,6 @@ class SiteSettings::LocalProcessProvider
       settings[name] = setting
     end
     setting.value = value.to_s
-
-    # So we can simulate the MessageBus notifications
-    # done by the DbProvider
-    @model.notify_changed! if @model
 
     setting
   end

spec/lib/site_setting_extension_spec.rb
@@ -41,12 +41,6 @@ RSpec.describe SiteSettingExtension do
     new_settings(provider_local)
   end
 
-  after do
-    settings.provider.model = nil
-    settings.listen_for_changes = false
-    settings2.listen_for_changes = false
-  end
-
   it "does not leak state cause changes are not linked" do
     t1 =
       Thread.new do
@@ -72,7 +66,12 @@ RSpec.describe SiteSettingExtension do
     t2.join
   end
 
-  describe "refresh!" do
+  describe ".refresh!" do
+    it "ensures that the right MessageBus subscription has been set up" do
+      settings.expects(:ensure_listen_for_changes).once
+      settings.refresh!
+    end
+
     it "will reset to default if provider vanishes" do
       settings.setting(:hello, 1)
       settings.hello = 100
@@ -108,30 +107,6 @@ RSpec.describe SiteSettingExtension do
 
       settings2.refresh!
       expect(settings2.hello).to eq(99)
-    end
-
-    it "publishes changes across processes" do
-      MessageBus.on
-
-      # Only need to do this once, since both settings will use the same provider. Doing it twice leads to confusion with MessageBus
-      settings.provider.model = settings
-
-      settings.listen_for_changes = true
-      settings2.listen_for_changes = true
-
-      settings.refresh!
-      settings2.refresh!
-
-      settings.setting(:hello, 1)
-      settings2.setting(:hello, 1)
-
-      settings.hello = 100
-
-      expect(settings2.hello).to eq(100)
-
-      settings.hello = 99
-
-      try_until_success(frequency: 0.5) { expect(settings2.hello).to eq(99) }
     end
 
     it "does not override types in the type supervisor" do
@@ -1078,36 +1053,29 @@ RSpec.describe SiteSettingExtension do
       expect(SiteSetting.search_experience(theme_id: theme_2.id)).to eq("search_field")
     end
 
-    it "publishes changes across processes" do
-      MessageBus.on
-
+    it "publishes the right MessageBus message when a theme site setting is updated" do
       settings_tss_instance_1 = new_settings(provider_local)
-      settings_tss_instance_2 = new_settings(provider_local)
-
-      settings_tss_instance_1.listen_for_changes = true
-      settings_tss_instance_2.listen_for_changes = true
-
       settings_tss_instance_1.load_settings(File.join(Rails.root, "config", "site_settings.yml"))
-      settings_tss_instance_2.load_settings(File.join(Rails.root, "config", "site_settings.yml"))
-
       settings_tss_instance_1.refresh!
-      settings_tss_instance_2.refresh!
 
       expect(settings_tss_instance_1.enable_welcome_banner(theme_id: theme_1.id)).to eq(false)
-      expect(settings_tss_instance_2.enable_welcome_banner(theme_id: theme_1.id)).to eq(false)
 
       tss_1.update!(value: true)
-      settings_tss_instance_1.change_themeable_site_setting(
-        theme_1.id,
-        :enable_welcome_banner,
-        true,
-      )
 
-      # Get through the MessageBus queue
-      try_until_success(frequency: 0.5) do
-        expect(settings_tss_instance_1.enable_welcome_banner(theme_id: theme_1.id)).to eq(true)
-        expect(settings_tss_instance_2.enable_welcome_banner(theme_id: theme_1.id)).to eq(true)
-      end
+      messages =
+        MessageBus.track_publish(described_class::SITE_SETTINGS_CHANNEL) do
+          settings_tss_instance_1.change_themeable_site_setting(
+            theme_1.id,
+            :enable_welcome_banner,
+            true,
+          )
+        end
+
+      expect(messages.length).to eq(1)
+
+      message = messages.first
+
+      expect(message.data[:process]).to eq(settings_tss_instance_1.process_id)
     end
 
     describe ".theme_site_settings_json_uncached" do

spec/lib/site_settings/db_provider_spec.rb
@@ -20,7 +20,10 @@ RSpec.describe SiteSettings::DbProvider do
     expect(provider.all.length).to eq(0)
     expect(provider.find("test")).to eq(nil)
 
+    SiteSetting.expects(:notify_changed!).at_least_once
+
     provider.save("test", "one", 1)
+
     found = provider.find("test")
 
     expect_same_setting(found, setting.new("test", "one", 1))