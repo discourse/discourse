15894b6394bc59a1f977ed22e8d87a59ce5fa6c0
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-08-18 15:44:46 +0200

    linting

--- plugins/discourse-calendar/spec/system/category_calendar_spec.rb
+++ plugins/discourse-calendar/spec/system/category_calendar_spec.rb
@@ -9,7 +9,7 @@ describe "Category calendar", type: :system do
   before do
     SiteSetting.calendar_enabled = true
     SiteSetting.discourse_post_event_enabled = true
-    SiteSetting.events_calendar_categories = "categoryId=#{category.id.to_s}"
+    SiteSetting.events_calendar_categories = "categoryId=#{category.id}"
 
     PostCreator.create!(
       admin,
