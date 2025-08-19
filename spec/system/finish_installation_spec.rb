discourse/discourse

commit bb3ca2c81a3c955e058c558043b653370e1835f3

UX: remove wizard-specific styling and add core classes (#34323)
* Changed the wizard template to use standard classes for buttons.
* Removed obsolete wizard overrules for basic components
* Hid Horizon experimental layout on wizard screens

<img width="1220" height="1666" alt="image"
src="https://github.com/user-attachments/assets/d090c79d-94ad-48b0-b786-16ec96ec5ecb"
/>
<img width="1220" height="1666" alt="image"
src="https://github.com/user-attachments/assets/d027d977-6924-4123-b76d-9cc222aa6165"
/>
<img width="1220" height="1666" alt="image"
src="https://github.com/user-attachments/assets/64262353-0e68-4fa9-b162-544fe62f2ef8"
/>

app/assets/stylesheets/wizard.scss
@@ -17,6 +17,13 @@ $blob-mobile-bg: absolute-image-url("/branded-background-mobile.svg");
   }
 }
 
+// horizon specific style
+.experimental-screen {
+  .wizard & {
+    display: none;
+  }
+}
+
 body.wizard {
   background-color: var(--secondary);
   background-image: $blob-bg;
@@ -24,7 +31,6 @@ body.wizard {
   background-repeat: no-repeat;
   background-position: bottom;
   color: var(--primary-very-high);
-  font-family: system-ui, Arial, sans-serif;
 
   .wrap {
     max-width: 100%;
@@ -272,14 +278,6 @@ body.wizard {
   }
 
   &__button {
-    border-radius: 2px;
-    border: 0;
-    padding: 0.5em;
-    transition: var(--d-button-transition);
-    text-decoration: none;
-    background-color: var(--secondary);
-    color: var(--primary-very-high);
-    cursor: pointer;
     font-size: var(--font-up-1);
   }
 
@@ -312,41 +310,10 @@ body.wizard {
     }
   }
 
-  &__button.primary:hover,
-  &__button.primary:focus {
-    background-color: var(--tertiary-hover);
-  }
-
-  &__button.primary:active {
-    background-color: var(--tertiary-high);
-  }
-
-  &__button.primary:disabled {
-    background-color: var(--tertiary-low);
-  }
-
-  &__button.jump-in {
-    &:hover {
-      background-color: var(--primary-300);
-    }
-  }
-
   &__button.finish {
-    color: var(--tertiary);
-
     @include viewport.until(sm) {
       order: 2;
     }
-  }
-
-  &__button.finish:hover {
-    color: var(--tertiary-hover);
-    background-color: transparent;
-  }
-
-  &__button.finish:active,
-  &__button.finish:disabled {
-    background-color: transparent;
   }
 
   &__button.next {

app/assets/javascripts/discourse/app/static/wizard/components/wizard-step.gjs
@@ -263,7 +263,7 @@ export default class WizardStepComponent extends Component {
                 {{on "click" this.backStep}}
                 disabled={{this.saving}}
                 type="button"
-                class="wizard-container__button back"
+                class="wizard-container__button back btn-transparent"
               >
                 {{i18n "wizard.back"}}
               </button>
@@ -276,7 +276,7 @@ export default class WizardStepComponent extends Component {
                 {{on "click" this.finish}}
                 disabled={{this.saving}}
                 type="button"
-                class="wizard-container__button finish"
+                class="wizard-container__button finish btn-primary"
               >
                 {{i18n "wizard.finish"}}
               </button>
@@ -287,7 +287,7 @@ export default class WizardStepComponent extends Component {
                 {{on "click" this.jumpIn}}
                 disabled={{this.saving}}
                 type="button"
-                class="wizard-container__button primary jump-in"
+                class="wizard-container__button jump-in btn-primary"
               >
                 {{i18n "wizard.jump_in"}}
               </button>
@@ -296,7 +296,7 @@ export default class WizardStepComponent extends Component {
                 {{on "click" this.nextStep}}
                 disabled={{this.saving}}
                 type="button"
-                class="wizard-container__button primary next"
+                class="wizard-container__button next btn-primary"
               >
                 {{i18n "wizard.next"}}
               </button>

commit 80f097925d1889d14e5d94c1d24d308a3dce0205

FIX: Restore styles in finish-installation route
Regressed in bb3ca2c81a3c955e058c558043b653370e1835f3

app/assets/stylesheets/wizard.scss
@@ -300,16 +300,6 @@ body.wizard {
     background-color: var(--primary-medium);
   }
 
-  &__button.primary {
-    background-color: var(--tertiary);
-    color: var(--secondary);
-
-    @include viewport.until(sm) {
-      order: 1;
-      margin-left: 0;
-    }
-  }
-
   &__button.finish {
     @include viewport.until(sm) {
       order: 2;
@@ -607,15 +597,14 @@ body.wizard {
     .help-text {
       margin: 2em 0;
     }
-
-    .primary {
-      display: inline-block;
-    }
   }
 }
 
-// Register admin page... /finish-installation/register
+// Register admin pages
+// /finish-installation and /finish-installation/register
 .wizard-container-contents.finish-installation {
+  font-family: system-ui, Arial, sans-serif;
+
   .wizard-container__combobox {
     background: var(--secondary);
     color: var(--primary);
@@ -632,5 +621,40 @@ body.wizard {
 
   .wizard-container__text-input {
     color: var(--primary);
+  }
+
+  .wizard-container__button {
+    border-radius: 2px;
+    border: 0;
+    padding: 0.5em;
+    transition: var(--d-button-transition);
+    text-decoration: none;
+    background-color: var(--secondary);
+    color: var(--primary-very-high);
+    cursor: pointer;
+
+    &.btn-primary {
+      background-color: var(--tertiary);
+      color: var(--secondary);
+      display: inline-block;
+
+      @include viewport.until(sm) {
+        order: 1;
+        margin-left: 0;
+      }
+    }
+
+    &:hover,
+    &:focus {
+      background-color: var(--tertiary-hover);
+    }
+
+    &primary:active {
+      background-color: var(--tertiary-high);
+    }
+
+    &:disabled {
+      background-color: var(--tertiary-low);
+    }
   }
 }

app/views/finish_installation/confirm_email.html.erb
@@ -3,7 +3,7 @@
 <%= raw(t 'finish_installation.confirm_email.message', email: @email) %>
 
 <div class='row'>
-  <%= button_to(finish_installation_resend_email_path, method: :put, class: 'wizard-container__button primary') do %>
+  <%= button_to(finish_installation_resend_email_path, method: :put, class: 'wizard-container__button btn-primary') do %>
     <%= t 'finish_installation.resend_email.title' %>
   <% end %>
 </div>

app/views/finish_installation/index.html.erb
@@ -3,7 +3,7 @@
     <h1><%= t 'finish_installation.congratulations' %></h1>
     <p><%= t 'finish_installation.register.help' %></p>
 
-    <%= link_to(finish_installation_register_path, class: 'wizard-container__button primary') do %>
+    <%= link_to(finish_installation_register_path, class: 'wizard-container__button btn-primary finish-installation__register') do %>
       <svg xmlns="http://www.w3.org/2000/svg" width="14px" height="14px" viewBox="0 0 448 512">
         <path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path>
       </svg>

app/views/finish_installation/register.html.erb
@@ -45,7 +45,7 @@
       <% end %>
     </div>
 
-    <%= submit_tag(t('finish_installation.register.button'), class: 'wizard-container__button primary') %>
+    <%= submit_tag(t('finish_installation.register.button'), class: 'wizard-container__button btn-primary') %>
 
   <%- end %>
 <%- else -%>

spec/system/finish_installation_spec.rb
@@ -1,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe "Finish Installation", type: :system do
+  before do
+    SiteSetting.has_login_hint = true
+    GlobalSetting.stubs(:developer_emails).returns("dev@example.com")
+  end
+
+  it "renders first screen" do
+    visit "/finish-installation"
+
+    find(".finish-installation__register").click
+
+    expect(page).to have_css(".wizard-container__combobox") # email field
+    expect(page).to have_css(".input-area")
+    expect(page).to have_css(".wizard-container__button") # submit button
+
+    # TODO: add more steps here to ensure full flow works
+  end
+end