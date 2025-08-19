aa3f6f1fa4197bf4a3eb733f9ece4b4f793386a0
Selase Krakani <selase@discourse.org>
2025-08-19 23:32:40 +0000

    DEV: Add Intermediate DB support for `user_associated_accounts`

--- migrations/db/intermediate_db_schema/100-base-schema.sql
+++ migrations/db/intermediate_db_schema/100-base-schema.sql
@@ -191,6 +191,17 @@ CREATE TABLE tags
     name        TEXT     NOT NULL
 );
 
+CREATE TABLE user_associated_accounts
+(
+    provider_name TEXT      NOT NULL,
+    user_id       NUMERIC,
+    created_at    DATETIME,
+    info          JSON_TEXT,
+    last_used     DATETIME  NOT NULL,
+    provider_uid  TEXT      NOT NULL,
+    PRIMARY KEY (user_id, provider_name)
+);
+
 CREATE TABLE user_emails
 (
     email      TEXT     NOT NULL,
