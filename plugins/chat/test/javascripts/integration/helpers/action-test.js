222caf0ec7f8e7509e977b121f72649f36c3094f
Jarek Radosz <jarek@cvx.dev>
2025-08-20 14:47:16 +0200

    more

--- plugins/chat/test/javascripts/integration/helpers/action-test.js
+++ plugins/chat/test/javascripts/integration/helpers/action-test.js
@@ -7,7 +7,16 @@ import { module, test } from "qunit";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
 
 class FooString extends Component {
-  layout = hbs`<button {{on "click" (d-action "bar")}}>test</button>`;
+  layout = hbs`<button {{on "click" (d-action "bar" 123)}}>test</button>`;
+
+  @action
+  bar(value) {
+    this.callback(value);
+  }
+}
+
+class FooReference extends Component {
+  layout = hbs`<button {{on "click" (d-action this.bar)}}>test</button>`;
 
   @action
   bar() {
@@ -20,6 +29,19 @@ module("Integration | Helper | action", function (hooks) {
 
   test("string argument", async function (assert) {
     this.registry.register("component:foo", FooString);
+    this.callback = (value) => {
+      assert.step("called");
+      assert.strictEqual(value, 123);
+    };
+
+    await render(hbs`<Foo @callback={{this.callback}} />`);
+
+    await click("button");
+    assert.verifySteps(["called"]);
+  });
+
+  test("reference argument", async function (assert) {
+    this.registry.register("component:foo", FooReference);
     this.callback = () => assert.step("called");
     await render(hbs`<Foo @callback={{this.callback}} />`);
 
