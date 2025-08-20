34ea57cd845d6b6f5caf75315c1e2846d044962a
Selase Krakani <selase@discourse.org>
2025-08-20 17:20:53 +0000

    DEV: Add validation for `name` and `description` in import step
    Also update how positions are computed. Create incomming position
    starting from current max position

--- migrations/lib/importer/steps/badge_groupings.rb
+++ migrations/lib/importer/steps/badge_groupings.rb
@@ -2,11 +2,14 @@
 
 module Migrations::Importer::Steps
   class BadgeGroupings < ::Migrations::Importer::CopyStep
+    MAX_NAME_LENGTH = 100
+    MAX_DESCRIPTION_LENGTH = 500
+
     store_mapped_ids true
 
     requires_mapping :ids_by_name, "SELECT LOWER(name), id FROM badge_groupings"
 
-    column_names %i[id name description position created_at]
+    column_names %i[id name description position created_at updated_at]
 
     total_rows_query <<~SQL, MappingType::BADGE_GROUPINGS
       SELECT COUNT(*)
@@ -19,17 +22,18 @@ module Migrations::Importer::Steps
 
     rows_query <<~SQL, MappingType::BADGE_GROUPINGS
       SELECT badge_groupings.*,
-             COALESCE(badge_groupings.position, 0) AS position
+             ROW_NUMBER() OVER (
+              ORDER BY COALESCE(badge_groupings.position, 0), badge_groupings.ROWID
+            ) AS normalized_position
       FROM badge_groupings
            LEFT JOIN mapped.ids mapped_badge_grouping
              ON badge_groupings.original_id = mapped_badge_grouping.original_id
                AND mapped_badge_grouping.type = ?
       WHERE mapped_badge_grouping.original_id IS NULL
-      ORDER BY badge_groupings.original_id
+      ORDER BY normalized_position
     SQL
 
     def execute
-      # TODO:(selase) Use @discourse_db.query_value here
       @max_position = BadgeGrouping.maximum(:position) || 0
 
       super
@@ -38,16 +42,20 @@ module Migrations::Importer::Steps
     private
 
     def transform_row(row)
-      name_lower = row[:name].downcase
-      if (existing_id = @ids_by_name[name_lower])
+      if (existing_id = @ids_by_name[row[:name].downcase])
         row[:id] = existing_id
 
         return nil
       end
 
-      # TODO:selase) Fix position calculation
-      @max_position += 1
-      row[:position] += @max_position
+      name = row[:name]
+      description = row[:description]
+
+      row[:name] = name[0, MAX_NAME_LENGTH] if name.length > MAX_NAME_LENGTH
+      if description && description.length > MAX_DESCRIPTION_LENGTH
+        row[:description] = description[0, MAX_DESCRIPTION_LENGTH]
+      end
+      row[:position] = @max_position + row[:normalized_position]
 
       super
     end
