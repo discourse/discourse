a0d5407763a14b3c293ef210f3d1d9bf399b825d
Jarek Radosz <jarek@cvx.dev>
2025-08-20 14:09:03 +0200

    DEV: Vendor-in the action helper

---
+++ plugins/chat/test/javascripts/integration/helpers/action-test.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line ember/no-classic-components
+import Component from "@ember/component";
+import { action } from "@ember/object";
+import { click, render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+import { module, test } from "qunit";
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+
+class Foo extends Component {
+  layout = hbs`<button {{on "click" (d-action "bar")}}>test</button>`;
+
+  @action
+  bar() {
+    debugger;
+  }
+}
+
+module("Integration | Helper | action", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("string argument", async function (assert) {
+    this.registry.register("component:foo", Foo);
+    this.foo = () => assert.step("called");
+    await render(hbs`<Foo />`);
+
+    await click("button");
+    assert.verifySteps(["called"]);
+  });
+});
