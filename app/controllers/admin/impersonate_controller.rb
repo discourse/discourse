06a7c869cda63c9946e29f1e2dcab02ce08a2523
Ted Johansson <ted@discourse.org>
2025-08-21 14:18:15 +0800

    DEV: Allow impersonation without session swapping (#34213)
    The current impersonation feature works by signing you in as the user you are impersonating. This has the side effect of invalidating your own session and forcing you to log out and in again.
    
    In this experimental implementation you keep your existing session, but DefaultCurrentUserProvider returns the user being impersonated, allowing you to see the site from their perspective.

--- app/controllers/admin/impersonate_controller.rb
+++ app/controllers/admin/impersonate_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Admin::ImpersonateController < Admin::AdminController
+  skip_before_action :ensure_admin, only: :destroy
+
   def create
     params.require(:username_or_email)
 
@@ -12,8 +14,27 @@ class Admin::ImpersonateController < Admin::AdminController
     # log impersonate
     StaffActionLogger.new(current_user).log_impersonate(user)
 
-    # Log on as the user
-    log_on_user(user, impersonate: true)
+    if SiteSetting.experimental_impersonation
+      raise Discourse::InvalidAccess if current_user.is_impersonating
+
+      start_impersonating_user(user)
+    else
+      # Log on as the user
+      log_on_user(user, impersonate: true)
+    end
+
+    render body: nil
+  end
+
+  def destroy
+    raise Discourse::NotFound if !SiteSetting.experimental_impersonation
+    raise Discourse::InvalidAccess if !current_user.is_impersonating
+
+    puppet = current_user
+
+    stop_impersonating_user
+
+    StaffActionLogger.new(current_user).log_stop_impersonation(puppet)
 
     render body: nil
   end
