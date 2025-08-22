9acffbd5a6c1b1ccaaf9c16c496daa80761b86ae
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-08-22 13:49:50 +0200

    better change date heuristic

--- plugins/discourse-calendar/test/javascripts/acceptance/category-events-calendar-test.js
+++ plugins/discourse-calendar/test/javascripts/acceptance/category-events-calendar-test.js
@@ -121,9 +121,13 @@ acceptance("Discourse Calendar - Category Events Calendar", function (needs) {
       .dom(".fc-event")
       .exists({ count: 4 }, "Events are displayed on the calendar");
 
-    assert.dom(".fc-daygrid-event-harness a[href='/t/-/18449/1']").hasStyle({
-      "background-color": "rgb(231, 76, 60)",
-    });
+    assert
+      .dom(
+        ".fc-daygrid-event-harness a[href='/t/-/18449/1']  .fc-daygrid-event-dot"
+      )
+      .hasStyle({
+        "border-color": "rgb(231, 76, 60)",
+      });
 
     assert
       .dom(
