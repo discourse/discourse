ab52f039578bec4ec55b6c8da7c5a1a05d6669ba
Ted Johansson <ted@discourse.org>
2025-08-25 11:10:43 +0800

    FIX: Don't unlock profile page if required fields not filled

--- spec/system/user_page/user_preferences_profile_spec.rb
+++ spec/system/user_page/user_preferences_profile_spec.rb
@@ -4,6 +4,7 @@ describe "User preferences | Profile", type: :system do
   fab!(:user) { Fabricate(:user, active: true) }
   let(:user_preferences_profile_page) { PageObjects::Pages::UserPreferencesProfile.new }
   let(:user_preferences_page) { PageObjects::Pages::UserPreferences.new }
+  let(:dialog) { PageObjects::Components::Dialog.new }
 
   before { sign_in(user) }
 
@@ -97,6 +98,23 @@ describe "User preferences | Profile", type: :system do
       visit("/")
 
       expect(page).to have_current_path("/")
+    end
+
+    it "does not allow submitting blank values for required fields" do
+      user_preferences_profile_page.visit(user)
+
+      find(".user-field-updated-terms input").check
+      find(".save-button .btn-primary").click
+
+      expect(dialog).to be_open
+      expect(dialog).to have_content(I18n.t("login.missing_user_field"))
+
+      dialog.click_yes
+
+      expect(page).to have_selector(
+        ".alert-error",
+        text: I18n.t("js.user.preferences.profile.enforced_required_fields"),
+      )
     end
 
     it "allows enabling safe-mode" do
