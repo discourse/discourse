3b10e734c852c05aea7af1190835ea60253f6b0a
Lhc_fl <lhcfl@outlook.com>
2025-08-29 16:34:46 +0800

    annotate topic timer

--- app/models/topic_timer.rb
+++ app/models/topic_timer.rb
@@ -99,22 +99,23 @@ end
 # Table name: topic_timers
 #
 #  id                 :integer          not null, primary key
-#  execute_at         :datetime         not null
-#  status_type        :integer          not null
-#  user_id            :integer          not null
-#  topic_id           :integer          not null
 #  based_on_last_post :boolean          default(FALSE), not null
 #  deleted_at         :datetime
-#  deleted_by_id      :integer
+#  duration_minutes   :integer
+#  execute_at         :datetime         not null
+#  public_type        :boolean          default(TRUE)
+#  status_type        :integer          not null
+#  type               :string           default("TopicTimer"), not null
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  category_id        :integer
-#  public_type        :boolean          default(TRUE)
-#  duration_minutes   :integer
+#  deleted_by_id      :integer
+#  topic_id           :integer          not null
+#  user_id            :integer          not null
 #
 # Indexes
 #
-#  idx_topic_id_public_type_deleted_at  (topic_id) UNIQUE WHERE ((public_type = true) AND (deleted_at IS NULL))
+#  idx_topic_id_public_type_deleted_at  (topic_id) UNIQUE WHERE ((public_type = true) AND (deleted_at IS NULL) AND ((type)::text = 'TopicTimer'::text))
 #  index_topic_timers_on_topic_id       (topic_id) WHERE (deleted_at IS NULL)
 #  index_topic_timers_on_user_id        (user_id)
 #
