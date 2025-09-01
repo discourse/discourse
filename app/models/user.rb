77d2ff605c94c50d8f0e819ce7fcc59ebe17f7ca
Gary Pendergast <gary@pento.net>
2025-09-01 15:17:23 +1000

    FIX: exclude non-text user fields from watch word check (#34651)
    Backport of 73e13fc63b792f440ef9c23fa274bdd6fa3b2b83.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1619,9 +1619,8 @@ class User < ActiveRecord::Base
   end
 
   def validatable_user_fields
-    # ignore multiselect fields since they are admin-set and thus not user generated content
-    @public_user_field_ids ||=
-      UserField.public_fields.where.not(field_type: "multiselect").pluck(:id)
+    # only validate fields that contain text content
+    @public_user_field_ids ||= UserField.public_fields.user_editable_text_fields.pluck(:id)
 
     user_fields(@public_user_field_ids)
   end
