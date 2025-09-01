6e4d7547b124a67aa90d0b9b058b73e5191421b4
Dr. Chat <arkolbed@gmail.com>
2025-09-01 20:22:20 +0000

    Register service worker during early init

--- app/assets/javascripts/discourse/app/lib/register-service-worker.js
+++ app/assets/javascripts/discourse/app/lib/register-service-worker.js
@@ -1,33 +1,42 @@
 import getAbsoluteURL, { isAbsoluteURL } from "discourse/lib/get-url";
 
-export function registerServiceWorker(serviceWorkerURL, registerOptions = {}) {
-  if (window.isSecureContext && "serviceWorker" in navigator) {
-    if (serviceWorkerURL) {
-      navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for (let registration of registrations) {
-          if (
-            registration.active &&
-            !registration.active.scriptURL.includes(serviceWorkerURL)
-          ) {
-            unregister(registration);
-          }
-        }
-      });
+export async function registerServiceWorker(
+  serviceWorkerURL,
+  registerOptions = {}
+) {
+  if (!(window.isSecureContext && "serviceWorker" in navigator)) {
+    return false;
+  }
 
-      navigator.serviceWorker
-        .register(getAbsoluteURL(`/${serviceWorkerURL}`), registerOptions)
-        .catch((error) => {
-          // eslint-disable-next-line no-console
-          console.info(`Failed to register Service Worker: ${error}`);
-        });
-    } else {
-      navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for (let registration of registrations) {
-          unregister(registration);
-        }
-      });
+  if (!serviceWorkerURL) {
+    // Unregister everything.
+    for (let registration of await navigator.serviceWorker.getRegistrations()) {
+      unregister(registration);
+    }
+
+    return true;
+  }
+
+  for (let registration of await navigator.serviceWorker.getRegistrations()) {
+    if (
+      registration.active &&
+      !registration.active.scriptURL.includes(serviceWorkerURL)
+    ) {
+      unregister(registration);
     }
   }
+
+  // N.B: The promise returned by `register` may not actually be resolved in some classes
+  // (e.g. if the worker is already registered)
+  // https://stackoverflow.com/a/71240372
+  navigator.serviceWorker
+    .register(getAbsoluteURL(`/${serviceWorkerURL}`), registerOptions)
+    .catch((err) => {
+      console.log(`failed to register service worker: ${err}`);
+    });
+
+  const registration = await navigator.serviceWorker.ready;
+  return registration !== undefined;
 }
 
 function unregister(registration) {
