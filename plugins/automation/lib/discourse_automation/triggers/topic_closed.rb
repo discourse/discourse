1b116723c886319c8bf06532fef2747f3031808f
chapoi <101828855+chapoi@users.noreply.github.com>
2025-09-03 19:37:50 +0200

    linting

--- plugins/automation/lib/discourse_automation/triggers/topic_closed.rb
+++ plugins/automation/lib/discourse_automation/triggers/topic_closed.rb
@@ -1,5 +1,4 @@
 # plugins/automation/lib/discourse_automation/triggers/topic_closed.rb
 # frozen_string_literal: true
 
-DiscourseAutomation::Triggerable.add(DiscourseAutomation::Triggers::TOPIC_CLOSED) do
-end
+DiscourseAutomation::Triggerable.add(DiscourseAutomation::Triggers::TOPIC_CLOSED)
