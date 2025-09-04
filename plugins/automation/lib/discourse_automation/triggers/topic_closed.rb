08a28fcbaf43beb888af1742cc6e1b283c7b1ad5
OsamaSayegh <asooomaasoooma90@gmail.com>
2025-09-04 08:38:13 +0300

    remove comment

--- plugins/automation/lib/discourse_automation/triggers/topic_closed.rb
+++ plugins/automation/lib/discourse_automation/triggers/topic_closed.rb
@@ -1,4 +1,3 @@
-# plugins/automation/lib/discourse_automation/triggers/topic_closed.rb
 # frozen_string_literal: true
 
 DiscourseAutomation::Triggerable.add(DiscourseAutomation::Triggers::TOPIC_CLOSED)
