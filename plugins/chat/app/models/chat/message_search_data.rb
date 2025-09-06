fa822d15bdd8928ef127dc4c53e678a674611679
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-09-06 14:17:07 +0200

    annotation

--- plugins/chat/app/models/chat/message_search_data.rb
+++ plugins/chat/app/models/chat/message_search_data.rb
@@ -8,3 +8,22 @@ module Chat
     validates_presence_of :search_data
   end
 end
+
+# == Schema Information
+#
+# Table name: chat_message_search_data
+#
+#  locale          :text
+#  raw_data        :text
+#  search_data     :tsvector
+#  version         :integer          default(0)
+#  chat_message_id :bigint           not null, primary key
+#
+# Indexes
+#
+#  idx_search_chat_message  (search_data) USING gin
+#
+# Foreign Keys
+#
+#  fk_rails_...  (chat_message_id => chat_messages.id) ON DELETE => cascade
+#
