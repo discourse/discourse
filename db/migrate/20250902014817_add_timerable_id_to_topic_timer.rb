eeeb7d302f2683731a1e2eb60158963b6c7b7d96
Linca <41134017+Lhcfl@users.noreply.github.com>
2025-09-17 13:19:17 +0800

    DEV: rename topic_id to timerable_id for BaseTimer (#34667)
    To prepare for the introduction of `CategoryDefaultTimer`, which allows
    posts created in certain categories to have a default TopicTimer, we
    need to rename the `topic_id` column to `timerable_id`.
    
    ---------
    
    Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

---
+++ db/migrate/20250902014817_add_timerable_id_to_topic_timer.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+class AddTimerableIdToTopicTimer < ActiveRecord::Migration[8.0]
+  def up
+    add_column :topic_timers, :timerable_id, :integer
+    add_index :topic_timers,
+              :timerable_id,
+              unique: true,
+              name: :idx_timerable_id_public_type_deleted_at,
+              where: "public_type = true AND deleted_at IS NULL AND type = 'TopicTimer'"
+    add_index :topic_timers, :timerable_id, where: "deleted_at IS NULL"
+    change_column_null :topic_timers, :topic_id, true
+    Migration::ColumnDropper.mark_readonly(:topic_timers, :topic_id)
+
+    # Mirror new `topic_id` values to `timerable_id`
+    execute(<<~SQL)
+      CREATE FUNCTION mirror_topic_timers_topic_id()
+      RETURNS trigger AS
+      $$
+      BEGIN
+        IF NEW.topic_id IS NOT NULL THEN
+          NEW.timerable_id = NEW.topic_id;
+        END IF;
+        RETURN NEW;
+      END;
+      $$
+      LANGUAGE plpgsql
+    SQL
+
+    execute(<<~SQL)
+      CREATE TRIGGER topic_timers_topic_id_trigger BEFORE INSERT ON topic_timers
+      FOR EACH ROW EXECUTE PROCEDURE mirror_topic_timers_topic_id()
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
