532d656d17649a76471dabb7709dac494fef5dc8
David Battersby <info@davidbattersby.com>
2025-09-17 16:52:13 +0400

    FEATURE: disable chat DM threads by default (#34840)
    Previously we introduced threads in Chat DMs in #29170 - however setting
    the default to false seems like a better fit.
    
    This will take effect for all new DM chats initiated, existing chats are
    not changed but threads can still be turned off manually within each
    channel's settings.

--- plugins/chat/spec/system/chat_footer_spec.rb
+++ plugins/chat/spec/system/chat_footer_spec.rb
@@ -121,7 +121,9 @@ RSpec.describe "Mobile Chat footer", type: :system, mobile: true do
     end
 
     context "for direct messages" do
-      fab!(:dm_channel) { Fabricate(:direct_message_channel, users: [current_user]) }
+      fab!(:dm_channel) do
+        Fabricate(:direct_message_channel, users: [current_user], threading_enabled: true)
+      end
       fab!(:dm_message) { Fabricate(:chat_message, chat_channel: dm_channel, user: current_user) }
 
       it "is urgent" do
