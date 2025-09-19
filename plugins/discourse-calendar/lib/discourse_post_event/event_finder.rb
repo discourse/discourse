908371191fcd884cde142c03abbf1a8a6a0a4533
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-09-19 09:12:23 +0200

    PERF: removes N+1 when loading events list (#34841)
    There was two issues:
    - using the `TopicListItemSerializer` was fetching way more than we need
    and also expecting includes from other plugins (assign for example),
    switching to our own dedicated serializer seems a better choice here
    - event dates were not preloaded
    - fixed a spec which was supposed to track N+1 but was commented
    
    I also improved our fabricator to limit the boilerplate needed.
    
    We might need few more indices to improve perf event more here but going
    to merge this first.

--- plugins/discourse-calendar/lib/discourse_post_event/event_finder.rb
+++ plugins/discourse-calendar/lib/discourse_post_event/event_finder.rb
@@ -21,6 +21,8 @@ module DiscoursePostEvent
       pms = private_messages(user)
 
       DiscoursePostEvent::Event
+        .visible
+        .open
         .includes(:event_dates, :post, post: :topic)
         .joins(post: :topic)
         .merge(Post.secured(guardian))
@@ -28,7 +30,7 @@ module DiscoursePostEvent
         .joins(latest_event_date_join)
         .select("discourse_post_event_events.*, latest_event_dates.starts_at")
         .where(
-          "(discourse_post_event_events.recurrence IS NOT NULL) OR (latest_event_dates.starts_at IS NOT NULL)",
+          "(discourse_post_event_events.recurrence IS NOT NULL) OR (latest_event_dates.starts_at IS NOT NULL) OR (discourse_post_event_events.original_starts_at IS NOT NULL)",
         )
         .distinct
     end
--- plugins/discourse-calendar/spec/requests/events_controller_spec.rb
+++ plugins/discourse-calendar/spec/requests/events_controller_spec.rb
@@ -10,30 +10,59 @@ module DiscoursePostEvent
     end
 
     describe "#index" do
-      fab!(:event_1) { Fabricate(:event, original_starts_at: 1.day.from_now) }
-
       it "should not result in N+1 queries problem when multiple events are returned" do
-        original_queries = track_sql_queries { get "/discourse-post-event/events.json" }
+        # Warmup
+        get "/discourse-post-event/events.json"
 
+        # Test with 1 event
+        Fabricate(:event, original_starts_at: 1.day.from_now)
+        queries_with_1_event = track_sql_queries { get "/discourse-post-event/events.json" }
         expect(response.status).to eq(200)
         expect(response.parsed_body["events"].length).to eq(1)
 
-        event_2 = Fabricate(:event, original_starts_at: 2.days.from_now)
-        event_3 = Fabricate(:event, original_starts_at: 3.days.from_now)
-
-        new_queries = track_sql_queries { get "/discourse-post-event/events.json" }
-
+        # Test with 3 events
+        Fabricate(:event, original_starts_at: 2.days.from_now)
+        Fabricate(:event, original_starts_at: 3.days.from_now)
+        queries_with_3_events = track_sql_queries { get "/discourse-post-event/events.json" }
         expect(response.status).to eq(200)
         expect(response.parsed_body["events"].length).to eq(3)
 
-        # TODO: There is still N+1 query problem here so uncomment this line when it is fixed
-        # expect(new_queries.count).to eq(original_queries.count)
+        expect(queries_with_3_events.count).to eq(queries_with_1_event.count)
+      end
+
+      it "should not show deleted events" do
+        active_event1 = Fabricate(:event, original_starts_at: 1.day.from_now)
+        active_event2 = Fabricate(:event, original_starts_at: 2.days.from_now)
+        deleted_event =
+          Fabricate(:event, original_starts_at: 3.days.from_now, deleted_at: 1.hour.ago)
+
+        get "/discourse-post-event/events.json"
+
+        expect(response.status).to eq(200)
+        events = response.parsed_body["events"]
+        event_ids = events.map { |e| e["id"] }
+
+        expect(event_ids).to match_array([active_event1.id, active_event2.id])
+      end
+
+      it "should not show closed events" do
+        active_event1 = Fabricate(:event, original_starts_at: 1.day.from_now)
+        active_event2 = Fabricate(:event, original_starts_at: 2.days.from_now)
+        closed_event = Fabricate(:event, original_starts_at: 3.days.from_now, closed: true)
+
+        get "/discourse-post-event/events.json"
+
+        expect(response.status).to eq(200)
+        events = response.parsed_body["events"]
+        event_ids = events.map { |e| e["id"] }
+
+        expect(event_ids).to match_array([active_event1.id, active_event2.id])
       end
     end
 
     context "with an existing post" do
       let(:user) { Fabricate(:user, admin: true) }
-      let(:topic) { Fabricate(:topic, user: user) }
+      let(:topic) { Fabricate(:topic, user: user, category: Fabricate(:category)) }
       let(:post1) { Fabricate(:post, user: user, topic: topic) }
       let(:invitee1) { Fabricate(:user) }
       let(:invitee2) { Fabricate(:user) }
