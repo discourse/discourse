c2c4e95deef8a83db8e8768c8a662582abb267ee
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-09-19 12:19:35 +0200

    better index name

--- plugins/discourse-calendar/db/migrate/20250919094856_add_distinct_on_optimization_index_to_event_dates.rb
+++ plugins/discourse-calendar/db/migrate/20250919094856_add_distinct_on_optimization_index_to_event_dates.rb
@@ -4,7 +4,7 @@ class AddDistinctOnOptimizationIndexToEventDates < ActiveRecord::Migration[8.0]
   def change
     add_index :discourse_calendar_post_event_dates,
               %i[event_id finished_at starts_at],
-              name: "idx_event_dates_distinct_on_optimization",
+              name: "index_discourse_calendar_post_event_dates_on_event_id_and_dates",
               order: {
                 finished_at: :desc,
                 starts_at: :desc,
