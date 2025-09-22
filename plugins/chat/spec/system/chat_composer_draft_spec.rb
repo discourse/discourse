0ba2b0d44b721bd173835425729abc010f3cf140
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2025-09-22 14:00:59 +0800

    DEV: Skip flaky system test (#34895)
    Failing even on reruns on CI:
    https://github.com/discourse/discourse/actions/runs/17905331817/job/50905399086

--- plugins/chat/spec/system/chat_composer_draft_spec.rb
+++ plugins/chat/spec/system/chat_composer_draft_spec.rb
@@ -130,7 +130,9 @@ RSpec.describe "Chat composer draft", type: :system do
         )
       end
 
-      it "loads the draft with replied to message" do
+      # TODO: Failing consistently and often on CI, needs investigation.
+      # https://github.com/discourse/discourse/actions/runs/17905331817/job/50905399086
+      xit "loads the draft with replied to message" do
         chat_page.visit_channel(channel_1)
 
         expect(channel_page.composer).to have_value("draft")
