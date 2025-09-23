4f95c8a8dffd228c855319a68eff3b224c7f9187
David Taylor <david@taylorhq.com>
2025-09-23 09:48:46 +0100

    admin-backups

--- app/assets/javascripts/admin/addon/controllers/admin-backups-logs.js
+++ app/assets/javascripts/admin/addon/controllers/admin-backups-logs.js
@@ -2,7 +2,7 @@ import Controller, { inject as controller } from "@ember/controller";
 import { alias } from "@ember/object/computed";
 
 export default class AdminBackupsLogsController extends Controller {
-  @controller adminBackups;
+  @controller("admin.backups") adminBackups;
 
   @alias("adminBackups.model") status;
 
