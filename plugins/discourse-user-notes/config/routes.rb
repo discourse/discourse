discourse/discourse

commit 9b998f10ce2e4d655a26adce37db40d5b852be89

DEV: Define user-notes namespace earlier (#34933)
Defining it inside `after_initialize` meant that other plugins couldn't
always see it and interact with it during initalization.

plugins/discourse-user-notes/plugin.rb
@@ -13,60 +13,14 @@ register_asset "stylesheets/user_notes.scss"
 
 register_svg_icon "pen-to-square"
 
+module ::DiscourseUserNotes
+  PLUGIN_NAME = "discourse-user-notes"
+end
+
+require_relative "lib/discourse_user_notes/engine"
+
 after_initialize do
   require_dependency "user"
-
-  module ::DiscourseUserNotes
-    PLUGIN_NAME = "discourse-user-notes"
-    COUNT_FIELD = "user_notes_count"
-
-    class Engine < ::Rails::Engine
-      engine_name PLUGIN_NAME
-      isolate_namespace DiscourseUserNotes
-    end
-
-    def self.key_for(user_id)
-      "notes:#{user_id}"
-    end
-
-    def self.notes_for(user_id)
-      PluginStore.get("user_notes", key_for(user_id)) || []
-    end
-
-    def self.add_note(user, raw, created_by, opts = nil)
-      opts ||= {}
-
-      notes = notes_for(user.id)
-      record = {
-        id: SecureRandom.hex(16),
-        user_id: user.id,
-        raw: raw,
-        created_by: created_by,
-        created_at: Time.now,
-      }.merge(opts)
-
-      notes << record
-      ::PluginStore.set("user_notes", key_for(user.id), notes)
-
-      user.custom_fields[DiscourseUserNotes::COUNT_FIELD] = notes.size
-      user.save_custom_fields
-
-      record
-    end
-
-    def self.remove_note(user, note_id)
-      notes = notes_for(user.id)
-      notes.reject! { |n| n[:id] == note_id }
-
-      if notes.size > 0
-        ::PluginStore.set("user_notes", key_for(user.id), notes)
-      else
-        ::PluginStore.remove("user_notes", key_for(user.id))
-      end
-      user.custom_fields[DiscourseUserNotes::COUNT_FIELD] = notes.size
-      user.save_custom_fields
-    end
-  end
 
   require_relative "app/serializers/user_note_serializer.rb"
   require_relative "app/controllers/discourse_user_notes/user_notes_controller.rb"

plugins/discourse-user-notes/lib/discourse_user_notes/engine.rb
@@ -1,0 +1,52 @@
+# frozen_string_literal: true
+
+module ::DiscourseUserNotes
+  COUNT_FIELD = "user_notes_count"
+
+  class Engine < ::Rails::Engine
+    engine_name PLUGIN_NAME
+    isolate_namespace DiscourseUserNotes
+  end
+
+  def self.key_for(user_id)
+    "notes:#{user_id}"
+  end
+
+  def self.notes_for(user_id)
+    PluginStore.get("user_notes", key_for(user_id)) || []
+  end
+
+  def self.add_note(user, raw, created_by, opts = nil)
+    opts ||= {}
+
+    notes = notes_for(user.id)
+    record = {
+      id: SecureRandom.hex(16),
+      user_id: user.id,
+      raw: raw,
+      created_by: created_by,
+      created_at: Time.now,
+    }.merge(opts)
+
+    notes << record
+    ::PluginStore.set("user_notes", key_for(user.id), notes)
+
+    user.custom_fields[DiscourseUserNotes::COUNT_FIELD] = notes.size
+    user.save_custom_fields
+
+    record
+  end
+
+  def self.remove_note(user, note_id)
+    notes = notes_for(user.id)
+    notes.reject! { |n| n[:id] == note_id }
+
+    if notes.size > 0
+      ::PluginStore.set("user_notes", key_for(user.id), notes)
+    else
+      ::PluginStore.remove("user_notes", key_for(user.id))
+    end
+    user.custom_fields[DiscourseUserNotes::COUNT_FIELD] = notes.size
+    user.save_custom_fields
+  end
+end

commit 09543df96224313226aec2c35316b1a6bd611271

FIX: Correctly apply user-notes routes
Regressed in 9b998f10ce2e4d655a26adce37db40d5b852be89

plugins/discourse-user-notes/config/routes.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+DiscourseUserNotes::Engine.routes.draw do
+  get "/" => "user_notes#index"
+  post "/" => "user_notes#create"
+  delete "/:id" => "user_notes#destroy"
+end

plugins/discourse-user-notes/plugin.rb
@@ -27,12 +27,6 @@ after_initialize do
 
   Discourse::Application.routes.append { mount ::DiscourseUserNotes::Engine, at: "/user_notes" }
 
-  DiscourseUserNotes::Engine.routes.draw do
-    get "/" => "user_notes#index"
-    post "/" => "user_notes#create"
-    delete "/:id" => "user_notes#destroy"
-  end
-
   allow_staff_user_custom_field(DiscourseUserNotes::COUNT_FIELD)
 
   add_to_class(Guardian, :can_delete_user_notes?) do