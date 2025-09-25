a47bb34d1deda89b99940890874ed4ca85c169b9
Jarek Radosz <jarek@cvx.dev>
2025-09-25 13:43:27 +0200

    math

--- plugins/discourse-math/plugin.rb
+++ plugins/discourse-math/plugin.rb
@@ -12,6 +12,9 @@ register_asset "stylesheets/ext/discourse-chat.scss"
 
 enabled_site_setting :discourse_math_enabled
 
+module ::DiscourseMath
+end
+
 after_initialize do
   if respond_to?(:chat) && SiteSetting.chat_enabled
     chat&.enable_markdown_feature("discourse-math")
