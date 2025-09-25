ad81fae94826f47e6832b7d718e6b21a91fc491e
Jarek Radosz <jradosz@gmail.com>
2025-09-25 12:13:45 +0200

    Update plugins/discourse-adplugin/config/routes.rb

--- plugins/discourse-adplugin/config/routes.rb
+++ plugins/discourse-adplugin/config/routes.rb
@@ -2,6 +2,6 @@
 
 AdPlugin::Engine.routes.draw do
   root to: "house_ads#index"
-  resources :house_creatives, only: %i[index show create update destroy], controller: "house_ads"
+  resources :house_creatives, except: %i[new edit], controller: "house_ads"
   resources :house_settings, only: [:update], controller: "house_ad_settings"
 end
