29e2619ef0424f909ffd3e830c7b5268a8172b60
Jarek Radosz <jarek@cvx.dev>
2025-09-25 13:39:42 +0200

    local-dates

--- plugins/discourse-local-dates/lib/discourse_local_dates/engine.rb
+++ plugins/discourse-local-dates/lib/discourse_local_dates/engine.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module ::DiscourseLocalDates
+module DiscourseLocalDates
   class Engine < ::Rails::Engine
     engine_name PLUGIN_NAME
     isolate_namespace DiscourseLocalDates
