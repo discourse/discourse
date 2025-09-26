b15e1b91f9f20fa8dad9359245f1540c5aaff064
Arpit Jalan <arpit@arpitjalan.com>
2025-09-26 10:17:15 +0530

    Update the test so that the "new topic" button shows up on category page

--- spec/system/composer/open_composer_without_category_spec.rb
+++ spec/system/composer/open_composer_without_category_spec.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe "Composer category selection", type: :system do
-  fab!(:user) { Fabricate(:user ) }
+  fab!(:moderator)
+  fab!(:default_category) { Fabricate(:category, name: "Features", slug: "features") }
 
   let(:category_page) { PageObjects::Pages::Category.new }
-  let(:composer) {PageObjects::Components::Composer.new}
-  let(:default_category) { Category.find(SiteSetting.default_composer_category) }
+  let(:composer) { PageObjects::Components::Composer.new }
 
   before do
+    SiteSetting.default_composer_category = default_category.id
     SiteSetting.open_composer_without_category = true
-
   end
 
   it "opens the composer with no category selected" do
-    sign_in(user)
+    sign_in(moderator)
     category_page.visit(default_category)
     category_page.new_topic_button.click
 
