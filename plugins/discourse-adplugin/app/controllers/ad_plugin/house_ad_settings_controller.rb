07b9da7810c52ddbce9fba0f916f40493e076718
Jarek Radosz <jarek@cvx.dev>
2025-09-29 17:44:54 +0200

    a lil bit more

--- plugins/discourse-adplugin/app/controllers/ad_plugin/house_ad_settings_controller.rb
+++ plugins/discourse-adplugin/app/controllers/ad_plugin/house_ad_settings_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module ::AdPlugin
+module AdPlugin
   class HouseAdSettingsController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
--- plugins/discourse-adplugin/app/controllers/ad_plugin/house_ads_controller.rb
+++ plugins/discourse-adplugin/app/controllers/ad_plugin/house_ads_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module ::AdPlugin
+module AdPlugin
   class HouseAdsController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
