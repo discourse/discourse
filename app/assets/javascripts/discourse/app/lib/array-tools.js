b3590b2e05df3aba53cbcd4434769bb5a2cf030b
Sérgio Saquetim <saquetim@discourse.org>
2025-09-30 16:27:00 -0300

    DEV: Simplify `removeObject` by eliminating Ember-specific handling
    Refactor `removeObject` in `array-tools` to remove dependencies on
    Ember's `@ember/-internals/metal` and `@ember-compat/tracked-built-ins`.
    This reduces complexity and aligns the function with standard JavaScript
    practices, making it more lightweight and easier to maintain.

--- app/assets/javascripts/discourse/app/lib/array-tools.js
+++ app/assets/javascripts/discourse/app/lib/array-tools.js
@@ -1,19 +1,3 @@
-import {
-  beginPropertyChanges,
-  endPropertyChanges,
-} from "@ember/-internals/metal";
-import { TrackedArray } from "@ember-compat/tracked-built-ins";
-
-function _removeValue(array, value) {
-  let loc = array.length || 0;
-  while (--loc >= 0) {
-    if (array[loc] === value) {
-      array.splice(loc, 1);
-    }
-  }
-  return array;
-}
-
 /**
  * Removes all occurrences of a specified value from an array.
  *
@@ -22,13 +6,11 @@ function _removeValue(array, value) {
  * @return {Array} The modified array with the specified value removed.
  */
 export function removeObject(array, value) {
-  if (array instanceof TrackedArray) {
-    beginPropertyChanges();
-    _removeValue(array, value);
-    endPropertyChanges();
-  } else {
-    _removeValue(array, value);
+  let loc = array.length || 0;
+  while (--loc >= 0) {
+    if (array[loc] === value) {
+      array.splice(loc, 1);
+    }
   }
-
   return array;
 }
