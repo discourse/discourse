e02915865febbd805a2c64c120be784abe9bfecc
Italo Alves <italo@discourse.org>
2025-10-01 15:50:39 -0300

    FIX: change user to moderator on tests

--- spec/system/composer/open_composer_without_category_spec.rb
+++ spec/system/composer/open_composer_without_category_spec.rb
@@ -9,18 +9,33 @@ describe "Composer category selection", type: :system do
   let(:category_page) { PageObjects::Pages::Category.new }
   let(:composer) { PageObjects::Components::Composer.new }
 
-  before do
-    SiteSetting.default_composer_category = default_category.id
-    SiteSetting.open_composer_without_category = true
+  before { SiteSetting.default_composer_category = default_category.id }
+
+  context "when open_composer_without_category is enabled" do
+    before { SiteSetting.open_composer_without_category = true }
+
+    it "opens the composer with no category selected" do
+      sign_in(moderator)
+      category_page.visit(default_category)
+      category_page.new_topic_button.click
+
+      expect(composer).to be_opened
+
+      expect(page).not_to have_selector(".category-chooser .badge-category__name")
+    end
   end
 
-  it "opens the composer with no category selected" do
-    sign_in(moderator)
-    category_page.visit(default_category)
-    category_page.new_topic_button.click
+  context "when open_composer_without_category is disabled" do
+    before { SiteSetting.open_composer_without_category = false }
 
-    expect(composer).to be_opened
+    it "opens the composer with a category pre-selected" do
+      sign_in(moderator)
+      category_page.visit(default_category)
+      category_page.new_topic_button.click
 
-    expect(find(".category-chooser .badge-category__name").text.strip).to eq("")
+      expect(composer).to be_opened
+
+      expect(page).to have_selector(".category-chooser .badge-category__name")
+    end
   end
 end
