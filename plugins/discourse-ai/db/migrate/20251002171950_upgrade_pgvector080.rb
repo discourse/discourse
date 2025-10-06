0e512ec08bd2a6793ca25f430760daecfe8e558a
Roman Rizzi <rizziromanalejandro@gmail.com>
2025-10-06 17:27:50 -0300

    DEV: Revert PG Vector 0.8.0 upgrade (#35220)

--- plugins/discourse-ai/db/migrate/20251002171950_upgrade_pgvector080.rb
+++ plugins/discourse-ai/db/migrate/20251002171950_upgrade_pgvector080.rb
@@ -2,13 +2,7 @@
 
 class UpgradePgvector080 < ActiveRecord::Migration[8.0]
   def up
-    minimum_target_version = "0.8.0"
-    installed_version =
-      DB.query_single("SELECT extversion FROM pg_extension WHERE extname = 'vector';").first
-
-    if Gem::Version.new(installed_version) < Gem::Version.new(minimum_target_version)
-      DB.exec("ALTER EXTENSION vector UPDATE TO '0.8.0';")
-    end
+    # No-op. Migration is failing with "must be owner of extension vector"
   end
 
   def down
