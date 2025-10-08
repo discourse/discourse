e710cca62964b4b8fa96b222fd7d809c5ebe04c7
Penar Musaraj <pmusaraj@gmail.com>
2025-10-08 10:40:50 -0400

    move specs to the right place

--- spec/system/admin_config_login_and_authentication_spec.rb
+++ spec/system/admin_config_login_and_authentication_spec.rb
@@ -15,19 +15,5 @@ describe "Admin Config Login and Authentication", type: :system do
 
     admin_login_page.click_tab("discourseconnect")
     expect(admin_login_page).to have_setting("enable_discourse_connect")
-
-    admin_login_page.click_tab("oauth2")
-    expect(admin_login_page).to have_setting("oauth2_enabled")
-
-    admin_login_page.click_tab("oidc")
-    expect(admin_login_page).to have_setting("openid_connect_enabled")
-  end
-
-  it "shows the tabs registered via plugins and can navigate between them" do
-    admin_login_page.visit("oidc")
-    expect(admin_login_page).to have_setting("openid_connect_enabled")
-
-    admin_login_page.visit("oauth2")
-    expect(admin_login_page).to have_setting("oauth2_enabled")
   end
 end
--- spec/system/admin_search_spec.rb
+++ spec/system/admin_search_spec.rb
@@ -112,22 +112,4 @@ describe "Admin Search", type: :system do
       "/admin/config/login-and-authentication/discourseconnect?filter=enable_discourse_connect",
     )
   end
-
-  it "works for OIDC" do
-    visit "/admin"
-    sidebar.click_search_input
-    search_modal.search("oidc")
-    search_modal.find_result("page", 0).click
-
-    expect(page).to have_current_path("/admin/config/login-and-authentication/oidc")
-  end
-
-  it "works for Oauth2" do
-    visit "/admin"
-    sidebar.click_search_input
-    search_modal.search("oauth2")
-    search_modal.find_result("page", 0).click
-
-    expect(page).to have_current_path("/admin/config/login-and-authentication/oauth2")
-  end
 end
