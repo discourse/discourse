fa43632f1b59d527de453d96ce61e1a17c95bcb2
David Taylor <david@taylorhq.com>
2025-10-14 17:38:46 +0100

    FIX: Missing topic properties in category-featured-topics (#35380)
    `category.topics` is already an array of `Topic` objects. Calling
    `.create()` again is unnecessary, and leads to strange behavior,
    especially when `@tracked` fields are involved.
    
    New `@tracked` were introduced in
    075d0b8ea11811bdf0d9b96547b15ed4f5a1793c, which is what surfaced this
    issue

--- app/assets/javascripts/discourse/app/models/category.js
+++ app/assets/javascripts/discourse/app/models/category.js
@@ -13,7 +13,6 @@ import { applyValueTransformer } from "discourse/lib/transformer";
 import PermissionType from "discourse/models/permission-type";
 import RestModel from "discourse/models/rest";
 import Site from "discourse/models/site";
-import Topic from "discourse/models/topic";
 
 const STAFF_GROUP_NAME = "staff";
 const CATEGORY_ASYNC_SEARCH_CACHE = {};
@@ -864,9 +863,7 @@ export default class Category extends RestModel {
   @discourseComputed("topics")
   featuredTopics(topics) {
     if (topics && topics.length) {
-      return topics
-        .slice(0, this.num_featured_topics || 2)
-        .map((t) => Topic.create(t));
+      return topics.slice(0, this.num_featured_topics || 2);
     }
   }
 
