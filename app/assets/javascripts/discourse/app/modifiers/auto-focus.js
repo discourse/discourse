322f4cb6753f4916eebea0e9d537283ee32775ae
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-10-14 11:25:10 +0200

    not necessary?

--- app/assets/javascripts/discourse/app/modifiers/auto-focus.js
+++ app/assets/javascripts/discourse/app/modifiers/auto-focus.js
@@ -1,21 +1,18 @@
-import { schedule } from "@ember/runloop";
 import Modifier from "ember-modifier";
 
 export default class AutoFocusModifier extends Modifier {
   didFocus = false;
 
   modify(element, _, { selectText }) {
-    schedule("afterRender", () => {
-      if (!this.didFocus) {
-        element.autofocus = true;
-        element.focus();
+    if (!this.didFocus) {
+      element.autofocus = true;
+      element.focus();
 
-        if (selectText) {
-          element.select();
-        }
-
-        this.didFocus = true;
+      if (selectText) {
+        element.select();
       }
-    });
+
+      this.didFocus = true;
+    }
   }
 }
