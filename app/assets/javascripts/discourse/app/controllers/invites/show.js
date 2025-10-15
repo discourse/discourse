ea0555eedf1935407b471da3e7a8bd5a30f19268
Ted Johansson <ted@discourse.org>
2025-10-15 16:06:31 +0800

    FIX: Exclude fields hidden on sign-up from invite signup form (#35405)
    In #34672 we added the ability to hide user fields from the sign-up form. However, we didn't exclude these from the slightly different signup form used when you are invited by another user.
    
    This commit adds a regression test and applies the same treatment to this special version of the form.

--- app/assets/javascripts/discourse/app/controllers/invites/show.js
+++ app/assets/javascripts/discourse/app/controllers/invites/show.js
@@ -39,7 +39,8 @@ export default class InvitesShowController extends Controller {
   });
   passwordValidationHelper = new PasswordValidationHelper(this);
   userFieldsValidationHelper = new UserFieldsValidationHelper({
-    getUserFields: () => this.site.get("user_fields"),
+    getUserFields: () =>
+      this.site.get("user_fields")?.filter((f) => f.show_on_signup),
     getAccountPassword: () => this.accountPassword,
   });
   successMessage = null;
