9ffd072c9e41a9669b92dba7e221241401f1c65e
David Taylor <david@taylorhq.com>
2025-10-16 15:35:25 +0100

    Update app/assets/javascripts/discourse/lib/webpack-customize-chunk-url-plugin.js
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>

--- app/assets/javascripts/discourse/lib/webpack-customize-chunk-url-plugin.js
+++ app/assets/javascripts/discourse/lib/webpack-customize-chunk-url-plugin.js
@@ -9,24 +9,28 @@ export class CustomizeChunkUrlPlugin {
         (source) => {
           return `
             ${source}
-            (function(){
+            (function () {
               // Rewrite chunk URLs to match the encoding of the current script
-              if(typeof __webpack_require__ !== "undefined" && typeof document !== "undefined" && document.currentScript) {
+              if (
+                typeof __webpack_require__ !== "undefined" &&
+                typeof document !== "undefined" &&
+                document.currentScript
+              ) {
                 const currentScriptUrl = document.currentScript.src;
 
                 let targetExt = ".js";
-                if(currentScriptUrl.endsWith(".br.js")) {
+                if (currentScriptUrl.endsWith(".br.js")) {
                   targetExt = ".br.js";
-                }else if(currentScriptUrl.endsWith(".gz.js")) {
+                } else if (currentScriptUrl.endsWith(".gz.js")) {
                   targetExt = ".gz.js";
                 }
 
-                var oldGetScript = __webpack_require__.u;
-                __webpack_require__.u = function(chunkId){
-                  var result = oldGetScript(chunkId);
+                let oldGetScript = __webpack_require__.u;
+                __webpack_require__.u = function (chunkId) {
+                  let result = oldGetScript(chunkId);
                   return result.replace(/\.js$/, targetExt);
                 };
-              };
+              }
             })();
           `;
         }
