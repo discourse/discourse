2dc6919d612667d3a73616e453ca53ba4bce0218
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2025-10-21 16:35:00 +0800

    PERF: Fix N+1 queries due to user custom fields when loading chat DMs
    In `Chat::ChannelFetcher.secured_direct_message_channels_search`,
    `User.preload_custom_fields` is called with `channels.flat_map { _1.chatable.users }`. However,
    the `Chat::DirectMessageSerializer` was getting the users via `object.direct_message_users.map(&:user)` which uses the
    `Chat::DirectMessage.direct_message_users` scope instead of the `Chat::DirectMessage.users` scope resulting in ActiveRecord returing
    new `User` objects.

--- plugins/chat/spec/requests/chat/api/current_user_channels_spec.rb
+++ plugins/chat/spec/requests/chat/api/current_user_channels_spec.rb
@@ -42,6 +42,44 @@ describe Chat::Api::CurrentUserChannelsController do
         expect(response.parsed_body["public_channels"][0]["id"]).to eq(channel.id)
       end
 
+      context "with multiple direct messages" do
+        fab!(:user_1, :user)
+        fab!(:user_2, :user)
+        fab!(:dm_channel_1, :direct_message)
+
+        fab!(:direct_message_channel_1) do
+          Fabricate(:direct_message_channel, chatable: dm_channel_1)
+        end
+
+        it "avoids N+1 queries with user custom fields" do
+          custom_field_name = "#{Time.now.to_i}_custom_field"
+          SiteSetting.public_user_custom_fields = custom_field_name
+
+          Fabricate(
+            :user_chat_channel_membership_for_dm,
+            chat_channel: direct_message_channel_1,
+            user: user_1,
+            following: true,
+          )
+
+          Chat::DirectMessageUser.create!(direct_message: dm_channel_1, user: user_1)
+          Chat::DirectMessageUser.create!(direct_message: dm_channel_1, user: user_2)
+
+          sign_in(user_1)
+
+          queries =
+            track_sql_queries do
+              get "/chat/api/me/channels"
+
+              expect(response.status).to eq(200)
+            end
+
+          expect(
+            queries.count { |q| q.include?("user_custom_fields") && q.include?(custom_field_name) },
+          ).to eq(1)
+        end
+      end
+
       context "with multiple channels and category group moderation" do
         fab!(:group)
         fab!(:channel_1, :category_channel)
