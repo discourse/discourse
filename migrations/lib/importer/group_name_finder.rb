49cfe69f514e00248a323aabf055ec6db8264a76
Gerhard Schlager <gerhard.schlager@discourse.org>
2025-10-21 23:44:41 +0200

    wip

--- migrations/lib/importer/group_name_finder.rb
+++ migrations/lib/importer/group_name_finder.rb
@@ -3,37 +3,51 @@
 module Migrations::Importer
   class GroupNameFinder < UniqueNameFinderBase
     def initialize(shared_data)
-      @used_usernames_lower = shared_data&.load(:usernames) || Set.new
       super(shared_data)
+      build_reserved_username_cache
     end
 
-    protected
+    private
 
-    def load_used_names(shared_data)
-      shared_data&.load(:group_names) || Set.new
+    def load_from_shared_data(shared_data)
+      @used_usernames_lower = shared_data&.load(:usernames) || Set.new
+      @used_group_names_lower = shared_data&.load(:group_names) || Set.new
     end
 
-    def max_length
-      ::Group::MAX_NAME_LENGTH
+    def store_used_name(name_lower)
+      @used_group_names_lower.add(name_lower)
+    end
+
+    def existing_name_collections
+      [@used_usernames_lower, @used_group_names_lower]
     end
 
     def fallback_name
-      "group"
+      I18n.t("importer.fallback_names.group")
     end
 
     def sanitize_name(name)
       UserNameSuggester.sanitize_username(name)
     end
 
-    def additional_used_names_for_suffix_finder
-      [@used_usernames_lower]
+    def name_available?(name_lower)
+      return false if @used_usernames_lower.include?(name_lower)
+      return false if @used_group_names_lower.include?(name_lower)
+      return false if @exact_reserved_usernames.include?(name_lower)
+      true
     end
 
-    private
+    def modify_truncated_name(name)
+      name.gsub!(UsernameValidator::INVALID_TRAILING_CHAR_PATTERN, "")
+      name
+    end
 
-    def name_available?(name_lower, allow_reserved: false)
-      return false if @used_usernames_lower.include?(name_lower)
-      super
+    def build_reserved_username_cache
+      @exact_reserved_usernames = Set.new
+
+      if (here_mention = SiteSetting.here_mention.presence)
+        @exact_reserved_usernames << here_mention.unicode_normalize.downcase
+      end
     end
   end
 end
