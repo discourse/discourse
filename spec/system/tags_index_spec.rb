aecef38249da1a68b8b225c2f1fcbd402dbe1910
awesomerobot <kris.aubuchon@discourse.org>
2025-10-22 12:12:54 -0400

    add spec to check for tag index admin dropdown

---
+++ spec/system/tags_index_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+describe "Tags index page", type: :system do
+  fab!(:admin)
+  fab!(:user)
+
+  context "when visiting as a staff user" do
+    it "shows the admin dropdown" do
+      sign_in(admin)
+      visit("/tags")
+
+      expect(page).to have_css(".tags-admin-dropdown")
+    end
+  end
+
+  context "when visiting as a regular user" do
+    it "does not show the admin dropdown" do
+      sign_in(user)
+      visit("/tags")
+
+      expect(page).to have_no_css(".tags-admin-dropdown")
+    end
+  end
+end
