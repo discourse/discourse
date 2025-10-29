2067fca9308acbb9d93dabfb795e85b3e164ffe4
Martin Brennan <martin@discourse.org>
2025-10-29 16:50:17 +1000

    DEV: Finish up upcoming change system spec

--- spec/system/admin_upcoming_changes_spec.rb
+++ spec/system/admin_upcoming_changes_spec.rb
@@ -33,10 +33,10 @@ describe "Admin upcoming changes", type: :system do
     expect(upcoming_changes_page).to have_change(:enable_upload_debug_mode)
 
     expect(
-      upcoming_changes_page.change_row(:about_page_extra_groups_show_description),
+      upcoming_changes_page.change_item(:about_page_extra_groups_show_description),
     ).to have_status(:stable)
     expect(
-      upcoming_changes_page.change_row(:about_page_extra_groups_show_description),
+      upcoming_changes_page.change_item(:about_page_extra_groups_show_description),
     ).to have_impact_role(:all_members)
   end
 
@@ -44,29 +44,108 @@ describe "Admin upcoming changes", type: :system do
     upcoming_changes_page.visit
     expect(upcoming_changes_page).to have_change(:enable_experimental_sample_plugin_feature)
     expect(
-      upcoming_changes_page.change_row(:enable_experimental_sample_plugin_feature),
+      upcoming_changes_page.change_item(:enable_experimental_sample_plugin_feature),
     ).to have_plugin_name("Sample plugin")
   end
 
   it "can toggle an upcoming change on or off" do
     upcoming_changes_page.visit
 
-    expect(upcoming_changes_page.change_row(:enable_upload_debug_mode)).to be_disabled
-    upcoming_changes_page.change_row(:enable_upload_debug_mode).toggle
+    expect(upcoming_changes_page.change_item(:enable_upload_debug_mode)).to be_disabled
+    upcoming_changes_page.change_item(:enable_upload_debug_mode).toggle
     expect(page).to have_content(I18n.t("admin_js.admin.upcoming_changes.change_enabled"))
-    expect(upcoming_changes_page.change_row(:enable_upload_debug_mode)).to be_enabled
+    expect(upcoming_changes_page.change_item(:enable_upload_debug_mode)).to be_enabled
 
     # Revisit the page to skip the 3s toggle rate limit
     upcoming_changes_page.visit
-    expect(upcoming_changes_page.change_row(:enable_upload_debug_mode)).to be_enabled
-    upcoming_changes_page.change_row(:enable_upload_debug_mode).toggle
+    expect(upcoming_changes_page.change_item(:enable_upload_debug_mode)).to be_enabled
+    upcoming_changes_page.change_item(:enable_upload_debug_mode).toggle
     expect(page).to have_content(I18n.t("admin_js.admin.upcoming_changes.change_disabled"))
   end
 
-  # TODO (martin) Add these specs
-  # it "can add and remove groups for a change" do
-  # end
+  it "can add and remove groups for a change" do
+    SiteSettingGroup.create!(
+      name: "enable_upload_debug_mode",
+      group_ids: Group::AUTO_GROUPS[:trust_level_4].to_s,
+    )
+    SiteSetting.refresh_site_setting_group_ids!
+    SiteSetting.notify_changed!
 
-  # it "can filter by name, description, plugin, status, impact type, or impact role" do
-  # end
+    upcoming_changes_page.visit
+
+    expect(upcoming_changes_page.change_item(:enable_upload_debug_mode)).to have_groups(
+      "trust_level_4",
+    )
+    upcoming_changes_page.change_item(:enable_upload_debug_mode).add_group("staff")
+    expect(page).to have_content(I18n.t("admin_js.admin.upcoming_changes.groups_updated"))
+
+    expect(
+      SiteSettingGroup.find_by(name: "enable_upload_debug_mode").group_ids.split("|").map(&:to_i),
+    ).to match_array([Group::AUTO_GROUPS[:trust_level_4], Group::AUTO_GROUPS[:staff]])
+    expect(SiteSetting.site_setting_group_ids[:enable_upload_debug_mode]).to match_array(
+      [Group::AUTO_GROUPS[:trust_level_4], Group::AUTO_GROUPS[:staff]],
+    )
+
+    upcoming_changes_page.visit
+    expect(upcoming_changes_page.change_item(:enable_upload_debug_mode)).to have_groups(
+      "trust_level_4",
+      "staff",
+    )
+
+    upcoming_changes_page.change_item(:enable_upload_debug_mode).remove_group("trust_level_4")
+    expect(page).to have_content(I18n.t("admin_js.admin.upcoming_changes.groups_updated"))
+
+    expect(
+      SiteSettingGroup.find_by(name: "enable_upload_debug_mode").group_ids.split("|").map(&:to_i),
+    ).to match_array([Group::AUTO_GROUPS[:staff]])
+    expect(SiteSetting.site_setting_group_ids[:enable_upload_debug_mode]).to match_array(
+      [Group::AUTO_GROUPS[:staff]],
+    )
+  end
+
+  it "can filter by name, description, plugin, status, impact type, or enabled/disabled" do
+    upcoming_changes_page.visit
+
+    # Filter by name
+    upcoming_changes_page.filter_controls.type_in_search("upload debug")
+
+    expect(upcoming_changes_page).to have_change(:enable_upload_debug_mode)
+    expect(upcoming_changes_page).to have_no_change(:about_page_extra_groups_show_description)
+
+    upcoming_changes_page.filter_controls.clear_search
+
+    # Filter by plugin
+    upcoming_changes_page.filter_controls.type_in_search("sample plugin")
+
+    expect(upcoming_changes_page).to have_change(:enable_experimental_sample_plugin_feature)
+    expect(upcoming_changes_page).to have_no_change(:about_page_extra_groups_show_description)
+
+    upcoming_changes_page.filter_controls.clear_search
+
+    upcoming_changes_page.filter_controls.toggle_dropdown_filters
+
+    # Filter by status
+    upcoming_changes_page.filter_controls.select_dropdown_option("Stable", dropdown_id: "status")
+
+    expect(upcoming_changes_page).to have_no_change(:enable_upload_debug_mode)
+    expect(upcoming_changes_page).to have_change(:about_page_extra_groups_show_description)
+
+    upcoming_changes_page.filter_controls.select_all_dropdown_option(dropdown_id: "status")
+
+    # Filter by impact type
+    upcoming_changes_page.filter_controls.select_dropdown_option("Feature", dropdown_id: "type")
+
+    expect(upcoming_changes_page).to have_no_change(:enable_upload_debug_mode)
+    expect(upcoming_changes_page).to have_change(:about_page_extra_groups_show_description)
+
+    upcoming_changes_page.filter_controls.select_all_dropdown_option(dropdown_id: "type")
+
+    # Filter by enabled/disabled
+    upcoming_changes_page.filter_controls.select_dropdown_option("Enabled", dropdown_id: "enabled")
+
+    expect(upcoming_changes_page).to have_no_change(:enable_upload_debug_mode)
+    expect(upcoming_changes_page).to have_no_change(:about_page_extra_groups_show_description)
+
+    upcoming_changes_page.filter_controls.select_all_dropdown_option(dropdown_id: "enabled")
+  end
 end
--- spec/system/page_objects/pages/admin_upcoming_changes.rb
+++ spec/system/page_objects/pages/admin_upcoming_changes.rb
@@ -7,15 +7,27 @@ module PageObjects
         page.visit("/admin/config/upcoming-changes")
       end
 
-      def change_row(setting_name)
-        PageObjects::Components::AdminUpcomingChangeRow.new(setting_name)
+      def change_item(setting_name)
+        PageObjects::Components::AdminUpcomingChangeItem.new(setting_name)
       end
 
       def has_change?(setting_name, description: nil)
         description ||= SiteSettings::LabelFormatter.description(setting_name)
-        change_row(setting_name).has_text?(description)
-        change_row(setting_name).has_text?(
+        change_item(setting_name).exists?
+        change_item(setting_name).has_text?(description)
+        change_item(setting_name).has_text?(
           SiteSettings::LabelFormatter.humanized_name(setting_name),
+        )
+      end
+
+      def has_no_change?(setting_name)
+        change_item(setting_name).does_not_exist?
+      end
+
+      def filter_controls
+        PageObjects::Components::AdminFilterControls.new(
+          ".upcoming-changes",
+          has_multiple_dropdowns: true,
         )
       end
     end
