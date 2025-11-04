d475cb48bdd9e3ca50e850e26b370b470aceb355
Jarek Radosz <jarek@cvx.dev>
2025-11-04 14:24:09 +0100

    fixed everything, no need to thank me

--- frontend/discourse-types/external-types.d.ts
+++ frontend/discourse-types/external-types.d.ts
@@ -4,7 +4,55 @@ declare module "@glimmer/component" {
   export { default } from "@types/@glimmer__component";
 }
 
-// TODO: add glint template stuff
+declare module "@glint/template/-private/integration" {
+  export {
+    AnyFunction,
+    AnyContext,
+    AnyBlocks,
+    InvokeDirect,
+    DirectInvokable,
+    Invoke,
+    InvokableInstance,
+    Invokable,
+    Context,
+    HasContext,
+    ModifierReturn,
+    ComponentReturn,
+    TemplateContext,
+    FlattenBlockParams,
+    NamedArgs,
+    NamedArgsMarker,
+    NamedArgNames,
+    UnwrapNamedArgs,
+  } from "@types/@glint__template/-private/integration";
+}
+
+declare module "@glint/ember-tsc/-private/dsl" {
+  export {
+    resolve,
+    resolveOrReturn,
+    templateExpression,
+    Globals,
+    AnyFunction,
+    AnyContext,
+    AnyBlocks,
+    InvokeDirect,
+    DirectInvokable,
+    Invoke,
+    InvokableInstance,
+    Invokable,
+    Context,
+    HasContext,
+    ModifierReturn,
+    ComponentReturn,
+    TemplateContext,
+    FlattenBlockParams,
+    NamedArgs,
+    NamedArgsMarker,
+    NamedArgNames,
+    UnwrapNamedArgs,
+  } from "@types/@glint__ember-tsc/-private/dsl";
+}
 
 declare module "qunit" {
   export { module, test } from "@types/qunit";
