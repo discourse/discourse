discourse/discourse

commit 8c2a2f2f9c46f201e29ff2fbd2d957c92e67833a

UX: lightbox expand btn smooth transition (#35765)
Adds a smooth fade out to the expand image button on mobile when opening
lightbox, and fade out when closing.

frontend/discourse/app/lib/lightbox.js
@@ -45,6 +45,16 @@ export default async function lightbox(elem, siteSettings) {
       appendToEl: isTesting() && document.getElementById("ember-testing"),
     });
 
+    lightboxEl.on("afterInit", () => {
+      const el = lightboxEl.pswp.currSlide.data.element;
+      el.querySelector(".meta").classList.add("open");
+    });
+
+    lightboxEl.on("destroy", () => {
+      const el = lightboxEl.pswp.currSlide.data.element;
+      el.querySelector(".meta").classList.remove("open");
+    });
+
     lightboxEl.on("uiRegister", function () {
       // adds a custom caption to lightbox
       lightboxEl.pswp.ui.registerElement({

app/assets/stylesheets/common/base/lightbox.scss
@@ -35,29 +35,34 @@
   * {
     outline: 0;
   }
-}
 
-.meta {
-  --meta-element-margin: 0.375em;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  color: var(--secondary);
-  background: var(--primary);
-  opacity: 0;
-  transition: opacity 0.2s;
-  display: flex;
-  align-items: center;
-
-  @media (pointer: coarse) {
-    align-items: center;
-    background: var(--secondary);
-    color: var(--primary-high);
-    height: 25px;
-    width: 25px;
+  .lightbox .meta {
+    --meta-element-margin: 0.375em;
+    position: absolute;
     bottom: 0;
-    right: 0;
-    opacity: 0.8;
+    width: 100%;
+    color: var(--secondary);
+    background: var(--primary);
+    opacity: 0;
+    transition: opacity 0.2s;
+    display: flex;
+    align-items: center;
+
+    @media (pointer: coarse) {
+      align-items: center;
+      background: var(--secondary);
+      color: var(--primary-high);
+      height: 25px;
+      width: 25px;
+      bottom: 0;
+      right: 0;
+      opacity: 0.8;
+      transition: opacity 500ms;
+
+      &.open {
+        opacity: 0;
+      }
+    }
   }
 
   .filename {

commit 0ec0d02b1438e182b74639d2a420f49089b07289

FIX: chat images with lightbox dont have meta selector (#35792)
Fixes a bug introduced in #35765 due to chat uploads not having the
image meta class, since we don't have captions for chat uploads
currently.

frontend/discourse/app/lib/lightbox.js
@@ -47,12 +47,12 @@ export default async function lightbox(elem, siteSettings) {
 
     lightboxEl.on("afterInit", () => {
       const el = lightboxEl.pswp.currSlide.data.element;
-      el.querySelector(".meta").classList.add("open");
+      el.querySelector(".meta")?.classList.add("open");
     });
 
     lightboxEl.on("destroy", () => {
       const el = lightboxEl.pswp.currSlide.data.element;
-      el.querySelector(".meta").classList.remove("open");
+      el.querySelector(".meta")?.classList.remove("open");
     });
 
     lightboxEl.on("uiRegister", function () {