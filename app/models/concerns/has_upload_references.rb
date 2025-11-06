832b47cca76e6601efe451a80f686cbf568de52a
Keegan George <kgeorge13@gmail.com>
2025-11-06 11:37:07 -0800

    DEV: Add back comment

--- app/models/concerns/has_upload_references.rb
+++ app/models/concerns/has_upload_references.rb
@@ -14,6 +14,10 @@ module HasUploadReferences
       # Link any video thumbnails
       if SiteSetting.video_thumbnails_enabled && upload.present? &&
            FileHelper.supported_video.include?(upload.extension&.downcase)
+        # Video thumbnails have the filename of the video file sha1 with a .png or .jpg extension.
+        # This is because at time of upload in the composer we don't know the topic/post id yet
+        # and there is no thumbnail info added to the markdown to tie the thumbnail to the topic/post after
+        # creation.
         thumbnail =
           Upload
             .where("original_filename like ?", "#{upload.sha1}.%")
