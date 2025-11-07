13c7891ed7ab5d99f0f1e6781dc09076cb255e34
Juan David Martinez <juan@discourse.org>
2025-11-07 10:24:12 +0100

    add localization for reports and remove extra route

--- plugins/discourse-adplugin/config/routes.rb
+++ plugins/discourse-adplugin/config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 AdPlugin::Engine.routes.draw do
-  scope "/ad_plugin" do
-    resources :ad_impressions, only: [:create]
-  end
   root to: "house_ads#index"
   resources :house_creatives, except: %i[new edit], controller: "house_ads"
   resources :house_settings, only: [:update], controller: "house_ad_settings"
