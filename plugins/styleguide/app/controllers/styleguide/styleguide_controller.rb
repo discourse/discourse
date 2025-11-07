f140f1dbf3a1db6674b5088851a6b96834d4216f
Penar Musaraj <pmusaraj@gmail.com>
2025-11-07 15:23:17 -0500

    FIX: Bug with styleguide group access (#35912)
    As reported in
    https://meta.discourse.org/t/styleguide-allowed-groups-setting-is-ignored/387869

--- plugins/styleguide/app/controllers/styleguide/styleguide_controller.rb
+++ plugins/styleguide/app/controllers/styleguide/styleguide_controller.rb
@@ -6,8 +6,13 @@ module Styleguide
     skip_before_action :check_xhr
 
     def index
-      if !current_user || !current_user.in_any_groups?(SiteSetting.styleguide_allowed_groups_map)
-        raise Discourse::InvalidAccess.new
+      if !current_user &&
+           !SiteSetting.styleguide_allowed_groups_map.include?(Group::AUTO_GROUPS[:everyone])
+        return raise Discourse::NotFound
+      end
+
+      if current_user && !current_user.in_any_groups?(SiteSetting.styleguide_allowed_groups_map)
+        return raise Discourse::NotFound
       end
 
       render "default/empty"
