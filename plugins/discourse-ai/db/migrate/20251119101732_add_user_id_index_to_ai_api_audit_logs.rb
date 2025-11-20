f88fa5b384c2e3a7b7433976ae3519964ef65ce9
Sam Saffron <sam.saffron@gmail.com>
2025-11-20 14:55:31 +1100

    add missing index ... this improves diags window

--- plugins/discourse-ai/db/migrate/20251119101732_add_user_id_index_to_ai_api_audit_logs.rb
+++ plugins/discourse-ai/db/migrate/20251119101732_add_user_id_index_to_ai_api_audit_logs.rb
@@ -7,5 +7,9 @@ class AddUserIdIndexToAiApiAuditLogs < ActiveRecord::Migration[7.2]
     user_idx = "index_ai_api_audit_logs_on_created_at_and_user_id"
     execute "DROP INDEX IF EXISTS #{user_idx}"
     add_index :ai_api_audit_logs, %i[created_at user_id], name: user_idx
+
+    topic_idx = "index_ai_api_audit_logs_on_topic_id"
+    execute "DROP INDEX IF EXISTS #{topic_idx}"
+    add_index :ai_api_audit_logs, [:topic_id], name: topic_idx
   end
 end
