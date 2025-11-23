834b1a034a57a9acd9bf7ac484aa920f17c3780d
Régis Hanol <regis@hanol.fr>
2025-11-23 14:44:30 +0100

    FIX: /my/notifications/reactions-received view (#36163)
    This list of reactions received wasn't using the components from core to
    display the posts.
    
    I've refactored it to use the `PostList` component instead so everything
    relating to the excerpt (for example) works as expected.
    
    This issue was reported when there was an @mention in the excerpt and we
    used to show the full name, instead of showing the username.
    
    **BEFORE**
    
    <img width="1390" height="1155" alt="Screenshot 2025-11-21 at 11 03 14"
    src="https://github.com/user-attachments/assets/5fc257b0-8744-4205-9f30-2c3f1a03d65c"
    />
    
    **AFTER**
    
    <img width="1390" height="1155" alt="Screenshot 2025-11-21 at 11 02 52"
    src="https://github.com/user-attachments/assets/f58df056-2517-4ca2-8d96-9893ab3a0d77"
    />
    
    
    Internal ref - t/168929

--- plugins/discourse-reactions/spec/system/reactions_activity_spec.rb
+++ plugins/discourse-reactions/spec/system/reactions_activity_spec.rb
@@ -25,62 +25,6 @@ describe "Reactions | Activity", type: :system do
       expect(page).to have_css(".user-stream-item [data-post-id='#{post_1.id}']")
     end
 
-    context "when unicode usernames is enabled " do
-      before do
-        SiteSetting.external_system_avatars_enabled = true
-        SiteSetting.external_system_avatars_url =
-          "/letter_avatar_proxy/v4/letter/{first_letter}/{color}/{size}.png"
-        SiteSetting.unicode_usernames = true
-      end
-
-      context "when prioritize_full_name_in_ux SiteSetting is true" do
-        before do
-          SiteSetting.prioritize_full_name_in_ux = true
-          stub_request(
-            :get,
-            "https://avatars.discourse-cdn.com/v4/letter/b/dbc845/48.png",
-          ).to_return(status: 200, body: "image", headers: {})
-
-          stub_request(
-            :get,
-            "https://avatars.discourse-cdn.com/v4/letter/b/90ced4/48.png",
-          ).to_return(status: 200, body: "image", headers: {})
-
-          stub_request(
-            :get,
-            "https://avatars.discourse-cdn.com/v4/letter/b/90ced4/24.png",
-          ).to_return(status: 200, body: "image", headers: {})
-
-          stub_request(
-            :get,
-            "https://avatars.discourse-cdn.com/v4/letter/b/90ced4/144.png",
-          ).to_return(status: 200, body: "image", headers: {})
-
-          stub_request(
-            :get,
-            "https://avatars.discourse-cdn.com/v4/letter/b/9de053/48.png",
-          ).to_return(status: 200, body: "image", headers: {})
-        end
-
-        it "shows the name of the mentioned user instead of the username" do
-          unicode_user = Fabricate(:user)
-          post_2 =
-            Fabricate(:post, raw: "This is a test post with a mention @#{unicode_user.username}")
-          DiscourseReactions::ReactionManager.new(
-            reaction_value: "clap",
-            user: current_user,
-            post: post_2,
-          ).toggle!
-
-          visit("/u/#{current_user.username}/activity/reactions")
-
-          post = find(".user-stream-item [data-post-id='#{post_2.id}']")
-          expect(page).to have_css(".user-stream-item [data-post-id='#{post_2.id}']")
-          expect(post).to have_content("@Bruce Wayne")
-        end
-      end
-    end
-
     context "when the associated post is deleted" do
       before { post_1.trash! }
 
