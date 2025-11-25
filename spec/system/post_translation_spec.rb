a505ca23bdf991323ed559e871dca46baee26183
Nat <natalie.tay@discourse.org>
2025-11-25 14:20:12 +0800

    add back tests that were removed during rebase?

--- spec/system/post_translation_spec.rb
+++ spec/system/post_translation_spec.rb
@@ -139,6 +139,48 @@ describe "Post translations", type: :system do
 
       expect(PostLocalization.exists?(post_id: post.id, locale: "fr")).to be false
     end
+
+    it "prompts to discard changes when abandoning modified translation" do
+      discard_modal = PageObjects::Modals::DiscardDraft.new
+
+      topic_page.visit_topic(topic)
+      topic_page.click_post_action_button(post, :show_more)
+      topic_page.click_post_action_button(post, :add_translation)
+      find(".update-translations-menu__add .post-action-menu__add-translation").click
+      expect(composer).to be_opened
+
+      translation_selector.expand
+      translation_selector.select_row_by_value("fr")
+
+      composer.fill_content("Salut le monde")
+      composer.minimize
+      expect(composer).to be_minimized
+
+      find("#post_#{post.post_number} .post-action-menu__reply").click
+
+      expect(discard_modal).to be_open
+    end
+
+    it "auto-closes when abandoning unchanged translation" do
+      discard_modal = PageObjects::Modals::DiscardDraft.new
+
+      topic_page.visit_topic(topic)
+      topic_page.click_post_action_button(post, :show_more)
+      topic_page.click_post_action_button(post, :add_translation)
+      find(".update-translations-menu__add .post-action-menu__add-translation").click
+      expect(composer).to be_opened
+
+      translation_selector.expand
+      translation_selector.select_row_by_value("fr")
+
+      composer.minimize
+      expect(composer).to be_minimized
+
+      find("#post_#{post.post_number} .post-action-menu__reply").click
+
+      expect(discard_modal).to be_closed
+      expect(composer).to be_opened
+    end
   end
 
   context "when creating a new post in a different locale" do
