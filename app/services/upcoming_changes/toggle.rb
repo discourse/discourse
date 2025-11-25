29b3b2329a9bdef67745744390e9c74a13bc9831
Martin Brennan <martin@discourse.org>
2025-11-25 11:26:50 +1000

    FEATURE: Use dropdown for upcoming change toggle (#36091)
    To make things a bit clearer for admins, and to bury the option to
    enable changes only for specific groups a bit since we don't think
    most sites will do this, this commit changes the upcoming change
    toggle to an "Enabled for:" dropdown with the following options:
    
    * No one
    * Everyone
    * Staff only
    * Specific group(s)
    
    We automatically save the staff site setting group in the background
    if staff only is selected, and we only save specific groups (and toggle
    the
    change on) once more than one group is selected.
    
    
    ---------
    
    Co-authored-by: awesomerobot <kris.aubuchon@discourse.org>

--- app/services/upcoming_changes/toggle.rb
+++ app/services/upcoming_changes/toggle.rb
@@ -5,7 +5,9 @@ class UpcomingChanges::Toggle
 
   params do
     attribute :setting_name, :string
+    attribute :enabled, :boolean
     validates :setting_name, presence: true
+    validates :enabled, inclusion: [true, false]
   end
 
   policy :current_user_is_admin
@@ -29,19 +31,15 @@ class UpcomingChanges::Toggle
     # before we update that UI.
     if SiteSetting.enable_upcoming_changes
       previous_value = SiteSetting.public_send(params.setting_name)
-      SiteSetting.send("#{params.setting_name}=", !previous_value)
+      SiteSetting.send("#{params.setting_name}=", params.enabled)
       StaffActionLogger.new(guardian.user).log_upcoming_change_toggle(
         params.setting_name,
         previous_value,
-        !previous_value,
+        params.enabled,
         { context: I18n.t("staff_action_logs.upcoming_changes.log_manually_toggled") },
       )
     else
-      SiteSetting.set_and_log(
-        params.setting_name,
-        !SiteSetting.public_send(params.setting_name),
-        guardian.user,
-      )
+      SiteSetting.set_and_log(params.setting_name, params.enabled, guardian.user)
     end
   end
 end
