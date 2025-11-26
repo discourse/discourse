4121b7b16d3de3a2989a4acd2162bc0ee2817b32
Blake Erickson <blake@discourse.org>
2025-11-26 16:48:39 -0700

    also update original filename

--- app/serializers/optimized_video_serializer.rb
+++ app/serializers/optimized_video_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OptimizedVideoSerializer < ApplicationSerializer
-  attributes :id, :upload_id, :url, :extension, :filesize, :sha1
+  attributes :id, :upload_id, :url, :extension, :filesize, :sha1, :original_filename
 
   def url
     if object.optimized_upload
@@ -10,5 +10,9 @@ class OptimizedVideoSerializer < ApplicationSerializer
         secure: SiteSetting.secure_uploads? && object.optimized_upload.secure,
       )
     end
+  end
+
+  def original_filename
+    object.optimized_upload&.original_filename
   end
 end
