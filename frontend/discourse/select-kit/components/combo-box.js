7941539a9ce762b15bc5b44bc67cda0ddc0ff21a
Yuriy Kurant <yuriy@discourse.org>
2025-11-26 14:58:38 +0800

    fix: reverts autoFilterable CP for combo-box

--- frontend/discourse/select-kit/components/combo-box.js
+++ frontend/discourse/select-kit/components/combo-box.js
@@ -1,15 +1,23 @@
+import { gte } from "@ember/object/computed";
 import { classNames } from "@ember-decorators/component";
 import SingleSelectComponent from "discourse/select-kit/components/single-select";
 import ComboBoxHeader from "./combo-box/combo-box-header";
-import { pluginApiIdentifiers, selectKitOptions } from "./select-kit";
+import {
+  FILTER_VISIBILITY_THRESHOLD,
+  pluginApiIdentifiers,
+  selectKitOptions,
+} from "./select-kit";
 
 @classNames("combobox", "combo-box")
 @pluginApiIdentifiers(["combo-box"])
 @selectKitOptions({
   caretUpIcon: "caret-up",
   caretDownIcon: "caret-down",
+  autoFilterable: "autoFilterable",
   clearable: false,
   headerComponent: ComboBoxHeader,
   shouldDisplayIcon: false,
 })
-export default class ComboBox extends SingleSelectComponent {}
+export default class ComboBox extends SingleSelectComponent {
+  @gte("content.length", FILTER_VISIBILITY_THRESHOLD) autoFilterable;
+}
