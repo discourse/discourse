b36a9b283ec98f02537d9fbabb6eb668b192c715
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2025-12-01 14:57:50 -0300

    DEV: Add optional `s3_stale_while_revalidate` and `s3_max_age` for uploads (#36298)
    With this change, S3's `stale-while-revalidate` and `max-age` response
    directives can be controlled.
    
    By default, off for `stale-while-revalidate`, and both are hidden site
    settings.

--- lib/file_store/s3_store.rb
+++ lib/file_store/s3_store.rb
@@ -90,8 +90,14 @@ module FileStore
       # cache file locally when needed
       cache_file(file, File.basename(path)) if opts[:cache_locally]
 
+      cache_control = "max-age=#{SiteSetting.s3_max_age}, public, immutable"
+      if SiteSetting.s3_stale_while_revalidate != SiteSetting.defaults[:s3_stale_while_revalidate]
+        cache_control =
+          "#{cache_control}, stale-while-revalidate=#{SiteSetting.s3_stale_while_revalidate}"
+      end
+
       options = {
-        cache_control: "max-age=31556952, public, immutable",
+        cache_control: cache_control,
         content_type:
           opts[:content_type].presence || MiniMime.lookup_by_filename(filename)&.content_type,
       }.merge(default_s3_options(secure: opts[:private]))
