0313dde184063e9160a9c756e7fcf2582511c71d
Juan David Martinez <juan@discourse.org>
2025-12-01 16:29:04 -0500

    FIX: Fix migration of HouseAd records from the plugins-store-rows table when visible_to_logged_in_users and/or visible_to_anons options are not set in the DB

--- plugins/discourse-adplugin/db/post_migrate/20251104121943_migrate_house_ads_from_plugin_store.rb
+++ plugins/discourse-adplugin/db/post_migrate/20251104121943_migrate_house_ads_from_plugin_store.rb
@@ -46,7 +46,7 @@ class MigrateHouseAdsFromPluginStore < ActiveRecord::Migration[7.0]
     ads_values =
       ads_data
         .map do |ad|
-          "(#{ad[:id]}, #{ActiveRecord::Base.connection.quote(ad[:name])}, #{ActiveRecord::Base.connection.quote(ad[:html])}, #{ad[:visible_to_logged_in_users]}, #{ad[:visible_to_anons]}, NOW(), NOW())"
+          "(#{ad[:id]}, #{ActiveRecord::Base.connection.quote(ad[:name])}, #{ActiveRecord::Base.connection.quote(ad[:html])}, #{ad[:visible_to_logged_in_users] ? ad[:visible_to_logged_in_users] : false}, #{ad[:visible_to_anons] ? ad[:visible_to_anons] : false}, NOW(), NOW())"
         end
         .join(",")
 
