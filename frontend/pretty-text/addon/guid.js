a468aac4579933b80999f7f0fda96e6edd3b580c
Jarek Radosz <jradosz@gmail.com>
2025-12-01 15:14:28 +0100

    DEV: Update linting (#36351)
    …and run prettier autofix

--- frontend/pretty-text/addon/guid.js
+++ frontend/pretty-text/addon/guid.js
@@ -7,7 +7,7 @@ export default function () {
     d += performance.now(); //use high-precision timer if available
   }
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const r = (d + Math.random() * 16) % 16 | 0;
+    const r = ((d + Math.random() * 16) % 16) | 0;
     d = Math.floor(d / 16);
     return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
   });
