f453463cfbd24a5a080d3322fe1dd6e3db34bf51
Régis Hanol <regis@hanol.fr>
2025-12-01 18:58:30 +0100

    FIX: Emoji.exists? should be consistent with Emoji[] (#36347)
    This ensures that both "Emoji.exists?" and "Emoji[]" use the same lookup
    "algorithm" when trying to match an emoji "name" with an emoji in the
    database.
    
    This ensures the normalization of the name into a single value handles
    both skin tones and aliases.
    
    Closes #33130

--- spec/models/emoji_spec.rb
+++ spec/models/emoji_spec.rb
@@ -1,113 +1,110 @@
 # frozen_string_literal: true
 
 RSpec.describe Emoji do
-  it "returns the correct codepoints" do
-    expect(Emoji.replacement_code("1f47d").codepoints).to eq([128_125])
-  end
-
-  it "handles multiple codepoints" do
-    expect(Emoji.replacement_code("1f1e9-1f1ea").codepoints).to eq([127_465, 127_466])
+  describe ".replacement_code" do
+    it "returns correct codepoints" do
+      expect(Emoji.replacement_code("1f47d").codepoints).to eq([128_125])
+      expect(Emoji.replacement_code("1f1e9-1f1ea").codepoints).to eq([127_465, 127_466])
+    end
   end
 
   describe ".load_custom" do
-    describe "when a custom emoji has an invalid upload_id" do
-      it "should return the custom emoji without a URL" do
-        CustomEmoji.create!(name: "test", upload_id: 9999)
-
-        emoji = Emoji.load_custom.first
-
-        expect(emoji.name).to eq("test")
-        expect(emoji.url).to eq(nil)
-      end
+    it "returns custom emoji without URL when upload_id is invalid" do
+      CustomEmoji.create!(name: "test", upload_id: 9999)
+      emoji = Emoji.load_custom.first
+      expect(emoji.name).to eq("test")
+      expect(emoji.url).to be_nil
     end
   end
 
   describe ".lookup_unicode" do
-    before do
-      SiteSetting.emoji_deny_list = "peach"
-      Emoji.clear_cache
-    end
-
-    it "doesnt convert (TM) and (tm) do an emoji" do
+    it "returns unicode for emoji, aliases, and skin tones" do
       expect(Emoji.lookup_unicode("trade_mark")).to eq("™")
-    end
-
-    it "should return the emoji" do
       expect(Emoji.lookup_unicode("blonde_man")).to eq("👱‍♂️")
-    end
-
-    it "should return an aliased emoji" do
       expect(Emoji.lookup_unicode("anger_right")).to eq("🗯")
-    end
-
-    it "should return a skin toned emoji" do
       expect(Emoji.lookup_unicode("blonde_woman:t6")).to eq("👱🏿‍♀️")
     end
 
-    it "should not return a fu emoji when emoji is in emoji deny list site setting" do
+    it "respects emoji deny list" do
+      SiteSetting.emoji_deny_list = "peach"
+      Emoji.clear_cache
       expect(Emoji.lookup_unicode("peach")).not_to eq("🍑")
     end
   end
 
   describe ".url_for" do
-    expected_url = "/images/emoji/twitter/blonde_woman.png?v=#{Emoji::EMOJI_VERSION}"
-    expected_toned_url = "/images/emoji/twitter/blonde_woman/6.png?v=#{Emoji::EMOJI_VERSION}"
+    it "returns correct url for all input formats" do
+      url = "/images/emoji/twitter/blonde_woman.png?v=#{Emoji::EMOJI_VERSION}"
+      toned_url = "/images/emoji/twitter/blonde_woman/6.png?v=#{Emoji::EMOJI_VERSION}"
 
-    it "should return url with filename" do
-      expect(Emoji.url_for("blonde_woman")).to eq(expected_url)
-    end
-
-    it "should return url with skin toned filename" do
-      expect(Emoji.url_for("blonde_woman/6")).to eq(expected_toned_url)
-    end
-
-    it "should return url with code" do
-      expect(Emoji.url_for(":blonde_woman:")).to eq(expected_url)
-    end
-
-    it "should return url with skin toned code" do
-      expect(Emoji.url_for(":blonde_woman:t6:")).to eq(expected_toned_url)
-      expect(Emoji.url_for("blonde_woman:t6")).to eq(expected_toned_url)
+      expect(Emoji.url_for("blonde_woman")).to eq(url)
+      expect(Emoji.url_for(":blonde_woman:")).to eq(url)
+      expect(Emoji.url_for("blonde_woman:t6")).to eq(toned_url)
+      expect(Emoji.url_for(":blonde_woman:t6:")).to eq(toned_url)
     end
   end
 
-  describe ".exists?" do
-    it "finds existing emoji" do
-      expect(Emoji.exists?(":blonde_woman:")).to be(true)
+  describe ".resolve_alias" do
+    it "resolves aliases to canonical names" do
+      expect(Emoji.resolve_alias("xray")).to eq("x_ray")
+      expect(Emoji.resolve_alias("blonde_woman")).to eq("blonde_woman")
+    end
+  end
+
+  describe "emoji lookup" do
+    it "finds emoji by name" do
+      expect(Emoji["blonde_woman"].name).to eq("blonde_woman")
+      expect(Emoji[":blonde_woman:"].name).to eq("blonde_woman")
       expect(Emoji.exists?("blonde_woman")).to be(true)
+      expect(Emoji.exists?(":blonde_woman:")).to be(true)
     end
 
-    it "finds existing skin toned emoji" do
-      expect(Emoji.exists?(":blonde_woman:t1:")).to be(true)
+    it "finds emoji by alias" do
+      expect(Emoji["xray"].name).to eq("x_ray")
+      expect(Emoji[":xray:"].name).to eq("x_ray")
+      expect(Emoji.exists?("xray")).to be(true)
+      expect(Emoji.exists?(":xray:")).to be(true)
+    end
+
+    it "finds tonable emoji with skin tone" do
+      expect(Emoji["blonde_woman:t6"].name).to eq("blonde_woman")
+      expect(Emoji[":blonde_woman:t6:"].name).to eq("blonde_woman")
       expect(Emoji.exists?("blonde_woman:t6")).to be(true)
+      expect(Emoji.exists?(":blonde_woman:t1:")).to be(true)
     end
 
-    it "finds existing custom emoji" do
+    it "finds tonable emoji by alias with skin tone" do
+      expect(Emoji["basketball_man:t4"].name).to eq("man_bouncing_ball")
+      expect(Emoji[":basketball_man:t4:"].name).to eq("man_bouncing_ball")
+      expect(Emoji.exists?("basketball_man:t4")).to be(true)
+      expect(Emoji.exists?(":basketball_man:t4:")).to be(true)
+    end
+
+    it "finds custom emoji" do
       CustomEmoji.create!(name: "test", upload_id: 9999)
       Emoji.clear_cache
-      expect(Emoji.exists?(":test:")).to be(true)
       expect(Emoji.exists?("test")).to be(true)
+      expect(Emoji.exists?(":test:")).to be(true)
     end
 
-    it "finds a custom emoji with a name with :t1 in the middle" do
+    it "finds custom emoji with skin tone pattern in name" do
       CustomEmoji.create!(name: "test:t1:foo", upload_id: 9999)
       Emoji.clear_cache
-      expect(Emoji.exists?(":test:t1:foo:")).to be(true)
       expect(Emoji.exists?("test:t1:foo")).to be(true)
+      expect(Emoji.exists?(":test:t1:foo:")).to be(true)
     end
 
-    it "doesn’t find non-existing emoji" do
+    it "returns nil/false for non-existing emoji" do
+      expect(Emoji["foo_bar_baz"]).to be_nil
+      expect(Emoji[":foo_bar_baz:"]).to be_nil
       expect(Emoji.exists?(":foo-bar:")).to be(false)
+    end
+
+    it "returns nil/false for invalid skin tones" do
+      expect(Emoji["apple:t4"]).to be_nil
       expect(Emoji.exists?(":blonde_woman:t7:")).to be(false)
       expect(Emoji.exists?("blonde_woman:t0")).to be(false)
       expect(Emoji.exists?("blonde_woman:t")).to be(false)
-    end
-
-    it "finds aliases" do
-      aliases_list = Emoji.aliases_db.values
-      expect(Emoji.exists?(aliases_list[0][0])).to be(true)
-      expect(Emoji.exists?(aliases_list[1][0])).to be(true)
-      expect(Emoji.exists?(aliases_list[2][0])).to be(true)
     end
   end
 
@@ -117,46 +114,41 @@ RSpec.describe Emoji do
 
     it "replaces emoji codes by images" do
       Plugin::CustomEmoji.register("xxxxxx", "/public/xxxxxx.png")
-
-      str = "This is a good day :xxxxxx: :woman: :man:t4:"
-      replaced_str = described_class.codes_to_img(str)
-
-      expect(replaced_str).to eq(
-        "This is a good day <img src=\"/public/xxxxxx.png\" title=\"xxxxxx\" class=\"emoji\" alt=\"xxxxxx\" loading=\"lazy\" width=\"20\" height=\"20\"> <img src=\"/images/emoji/twitter/woman.png?v=#{Emoji::EMOJI_VERSION}\" title=\"woman\" class=\"emoji\" alt=\"woman\" loading=\"lazy\" width=\"20\" height=\"20\"> <img src=\"/images/emoji/twitter/man/4.png?v=#{Emoji::EMOJI_VERSION}\" title=\"man:t4\" class=\"emoji\" alt=\"man:t4\" loading=\"lazy\" width=\"20\" height=\"20\">",
-      )
+      replaced_str = described_class.codes_to_img("This is a good day :xxxxxx: :woman: :man:t4:")
+      expect(replaced_str).to eq(<<~HTML.chomp)
+        This is a good day <img src="/public/xxxxxx.png" title="xxxxxx" class="emoji" alt="xxxxxx" loading="lazy" width="20" height="20"> <img src="/images/emoji/twitter/woman.png?v=#{Emoji::EMOJI_VERSION}" title="woman" class="emoji" alt="woman" loading="lazy" width="20" height="20"> <img src="/images/emoji/twitter/man/4.png?v=#{Emoji::EMOJI_VERSION}" title="man:t4" class="emoji" alt="man:t4" loading="lazy" width="20" height="20">
+      HTML
     end
 
-    it "doesn't replace if code doesn't exist" do
-      str = "This is a good day :woman: :foo: :bar:t4: :man:t8:"
-      replaced_str = described_class.codes_to_img(str)
-
-      expect(replaced_str).to eq(
-        "This is a good day <img src=\"/images/emoji/twitter/woman.png?v=#{Emoji::EMOJI_VERSION}\" title=\"woman\" class=\"emoji\" alt=\"woman\" loading=\"lazy\" width=\"20\" height=\"20\"> :foo: :bar:t4: :man:t8:",
-      )
+    it "doesn't replace non-existing codes" do
+      replaced_str =
+        described_class.codes_to_img("This is a good day :woman: :foo: :bar:t4: :man:t8:")
+      expect(replaced_str).to eq(<<~HTML.chomp)
+        This is a good day <img src="/images/emoji/twitter/woman.png?v=#{Emoji::EMOJI_VERSION}" title="woman" class="emoji" alt="woman" loading="lazy" width="20" height="20"> :foo: :bar:t4: :man:t8:
+      HTML
     end
   end
 
   describe ".groups" do
-    it "returns an optimized emoji name -> group name datastructure" do
+    it "returns emoji name to group name mapping" do
       expect(Emoji.groups["scotland"]).to eq("flags")
     end
   end
 
   describe ".load_standard" do
     it "removes nil emojis" do
-      expect(Emoji.load_standard.any? { |element| element.nil? }).to be false
+      expect(Emoji.load_standard.any?(&:nil?)).to be(false)
     end
   end
 
   describe "#create_from_db_item" do
-    it "sets the group of the emoji" do
+    it "creates emoji with group when known" do
       emoji = Emoji.create_from_db_item("name" => "scotland")
       expect(emoji.group).to eq("flags")
     end
 
-    it "doesn’t create emoji when group is unknown" do
-      emoji = Emoji.create_from_db_item("name" => "white_hair")
-      expect(emoji).to be_nil
+    it "returns nil when group is unknown" do
+      expect(Emoji.create_from_db_item("name" => "white_hair")).to be_nil
     end
   end
 end
