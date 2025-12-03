caf3906887398081ab36783b787589643b79c300
DevTekVE <devtekve@gmail.com>
2025-12-03 21:05:44 +0100

    Aight this should work now

--- plugins/discourse-ai/spec/lib/discord/bot/persona_replier_spec.rb
+++ plugins/discourse-ai/spec/lib/discord/bot/persona_replier_spec.rb
@@ -12,20 +12,25 @@ RSpec.describe DiscourseAi::Discord::Bot::PersonaReplier do
   before do
     enable_current_plugin
     SiteSetting.ai_discord_search_persona = persona.id.to_s
-
-    DiscourseAi::Personas::Bot
-      .any_instance
-      .stubs(:reply)
-      .with(is_a(DiscourseAi::Personas::BotContext))
-      .returns([["Mock Reply"]])
-
-    persona_replier.stubs(:create_reply)
+    allow_any_instance_of(DiscourseAi::Personas::Bot).to receive(:reply).and_return(
+      "This is a reply from bot!",
+    )
+    allow(persona_replier).to receive(:create_reply)
   end
 
   describe "#handle_interaction!" do
     it "creates and updates replies" do
       persona_replier.handle_interaction!
       expect(persona_replier).to have_received(:create_reply).at_least(:once)
+    end
+
+    it "uses a BotContext to generate the reply" do
+      expect_any_instance_of(DiscourseAi::Personas::Bot).to receive(:reply) do |bot, context|
+        expect(context).to be_a(DiscourseAi::Personas::BotContext)
+        [["This is a reply from bot!"]]
+      end
+
+      persona_replier.handle_interaction!
     end
   end
 end
