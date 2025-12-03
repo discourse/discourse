c64dd819aa4585df145d395bb546de06041cd69b
Natalie Tay <natalie.tay@gmail.com>
2025-12-03 17:43:06 +0800

    FEATURE: Accept tags param when opening a /new-message (#36421)
    We should now be able to do the following (basically the tags param).
    Note: you will need to add groups to "PM tags allowed for groups"
    setting.
    
    
    <http://meta.discourse.org/new-message?username=nat&title=Thank%20you%20nat%21&body=This%20is%20a%20feature%20I%20will%20use&tags=thank-you,tags>

--- frontend/discourse/tests/acceptance/new-message-test.js
+++ frontend/discourse/tests/acceptance/new-message-test.js
@@ -37,3 +37,60 @@ acceptance("New Message - Authenticated", function (needs) {
     );
   });
 });
+
+acceptance("New Message - Authenticated with Tags Enabled", function (needs) {
+  needs.user();
+  needs.site({ can_tag_topics: true, can_tag_pms: true });
+
+  test("accessing new-message route with tags parameter when can_tag_pms is enabled", async function (assert) {
+    await visit(
+      "/new-message?username=tomtom&title=Help&body=Thisbody&tags=tag1,tag2"
+    );
+
+    assert.dom(".composer-fields").exists("opens the composer");
+    assert.dom("#reply-title").hasValue("Help", "pre-fills message title");
+    assert
+      .dom(".d-editor-input")
+      .hasValue("Thisbody", "pre-fills message body");
+
+    const privateMessageUsers = selectKit("#private-message-users");
+    assert.strictEqual(
+      privateMessageUsers.header().value(),
+      "tomtom",
+      "it selects correct username"
+    );
+
+    const tags = selectKit(".mini-tag-chooser");
+    assert.strictEqual(tags.header().value(), "tag1,tag2", "it pre-fills tags");
+  });
+});
+
+acceptance("New Message - Authenticated with Tags Disabled", function (needs) {
+  needs.user();
+  needs.site({ can_tag_topics: true, can_tag_pms: false });
+
+  test("accessing new-message route with tags parameter when can_tag_pms is disabled", async function (assert) {
+    await visit(
+      "/new-message?username=tomtom&title=Help&body=Thisbody&tags=tag1,tag2"
+    );
+
+    assert.dom(".composer-fields").exists("opens the composer");
+    assert.dom("#reply-title").hasValue("Help", "pre-fills message title");
+    assert
+      .dom(".d-editor-input")
+      .hasValue("Thisbody", "pre-fills message body");
+
+    const privateMessageUsers = selectKit("#private-message-users");
+    assert.strictEqual(
+      privateMessageUsers.header().value(),
+      "tomtom",
+      "it selects correct username"
+    );
+
+    assert
+      .dom(".mini-tag-chooser")
+      .doesNotExist(
+        "tags selector is not visible when can_tag_pms is disabled"
+      );
+  });
+});
