dde2467d13ae2ae0330979c2c861c524b8da0131
Ted Johansson <ted@discourse.org>
2025-12-03 13:58:44 +0800

    DEV: Remove deprecated trust level access settings

--- lib/site_settings/deprecated_settings.rb
+++ lib/site_settings/deprecated_settings.rb
@@ -6,75 +6,12 @@ end
 module SiteSettings::DeprecatedSettings
   SETTINGS = [
     # [<old setting>, <new_setting>, <override>, <version to drop>]
-    ["anonymous_posting_min_trust_level", "anonymous_posting_allowed_groups", false, "3.3"],
-    ["shared_drafts_min_trust_level", "shared_drafts_allowed_groups", false, "3.3"],
-    ["min_trust_level_for_here_mention", "here_mention_allowed_groups", false, "3.3"],
-    ["approve_unless_trust_level", "approve_unless_allowed_groups", false, "3.3"],
-    [
-      "approve_new_topics_unless_trust_level",
-      "approve_new_topics_unless_allowed_groups",
-      false,
-      "3.3",
-    ],
-    ["email_in_min_trust", "email_in_allowed_groups", false, "3.3"],
-    ["min_trust_to_edit_wiki_post", "edit_wiki_post_allowed_groups", false, "3.3"],
-    ["allow_uploaded_avatars", "uploaded_avatars_allowed_groups", false, "3.3"],
-    ["min_trust_to_create_topic", "create_topic_allowed_groups", false, "3.3"],
-    ["min_trust_to_edit_post", "edit_post_allowed_groups", false, "3.3"],
-    ["min_trust_to_flag_posts", "flag_post_allowed_groups", false, "3.3"],
-    ["tl4_delete_posts_and_topics", "delete_all_posts_and_topics_allowed_groups", false, "3.3"],
-    [
-      "min_trust_level_to_allow_user_card_background",
-      "user_card_background_allowed_groups",
-      false,
-      "3.3",
-    ],
-    ["min_trust_level_to_allow_invite", "invite_allowed_groups", false, "3.3"],
-    ["min_trust_level_to_allow_ignore", "ignore_allowed_groups", false, "3.3"],
-    ["min_trust_to_allow_self_wiki", "self_wiki_allowed_groups", false, "3.3"],
-    ["min_trust_to_create_tag", "create_tag_allowed_groups", false, "3.3"],
-    ["min_trust_to_send_email_messages", "send_email_messages_allowed_groups", false, "3.3"],
-    ["review_media_unless_trust_level", "skip_review_media_groups", false, "3.3"],
-    ["min_trust_to_post_embedded_media", "embedded_media_post_allowed_groups", false, "3.3"],
-    ["min_trust_to_post_links", "post_links_allowed_groups", false, "3.3"],
-    ["min_trust_level_for_user_api_key", "user_api_key_allowed_groups", false, "3.3"],
-    ["min_trust_level_to_tag_topics", "tag_topic_allowed_groups", false, "3.3"],
-    [
-      "min_trust_level_to_allow_profile_background",
-      "profile_background_allowed_groups",
-      false,
-      "3.3",
-    ],
     ["min_first_post_typing_time", "fast_typing_threshold", false, "3.4"],
     ["twitter_summary_large_image", "x_summary_large_image", false, "3.4"],
     ["external_system_avatars_enabled", "external_system_avatars_url", false, "3.5"],
   ]
 
-  OVERRIDE_TL_GROUP_SETTINGS = %w[
-    anonymous_posting_min_trust_level
-    shared_drafts_min_trust_level
-    min_trust_level_for_here_mention
-    approve_unless_trust_level
-    approve_new_topics_unless_trust_level
-    email_in_min_trust
-    min_trust_to_edit_wiki_post
-    allow_uploaded_avatars
-    min_trust_to_create_topic
-    min_trust_to_edit_post
-    min_trust_to_flag_posts
-    tl4_delete_posts_and_topics
-    min_trust_level_to_allow_user_card_background
-    min_trust_level_to_allow_invite
-    min_trust_level_to_allow_ignore
-    min_trust_to_create_tag
-    min_trust_to_send_email_messages
-    review_media_unless_trust_level
-    min_trust_to_post_embedded_media
-    min_trust_to_post_links
-    min_trust_level_for_user_api_key
-    min_trust_level_to_tag_topics
-    min_trust_level_to_allow_profile_background
-  ]
+  OVERRIDE_TL_GROUP_SETTINGS = %w[]
 
   def group_to_tl(old_setting, new_setting)
     tl_and_staff = is_tl_and_staff_setting?(old_setting)
