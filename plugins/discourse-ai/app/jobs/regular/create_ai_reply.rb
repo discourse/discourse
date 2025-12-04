ee26f95da85c3a7bef3129300a71c60755f76c3b
Sam Saffron <sam.saffron@gmail.com>
2025-12-04 16:53:13 +1100

    FEATURE: add support for retrying a completion
    Also, make it easier to pause a completion which was very hard on mobile
    due to being a moving target

--- plugins/discourse-ai/app/jobs/regular/create_ai_reply.rb
+++ plugins/discourse-ai/app/jobs/regular/create_ai_reply.rb
@@ -7,6 +7,9 @@ module Jobs
     def execute(args)
       return unless bot_user = User.find_by(id: args[:bot_user_id])
       return unless post = Post.includes(:topic).find_by(id: args[:post_id])
+      reply_post = Post.find_by(id: args[:reply_post_id]) if args[:reply_post_id].present?
+      return if args[:reply_post_id].present? && reply_post.nil?
+      return if reply_post && reply_post.topic_id != post.topic_id
       persona_id = args[:persona_id]
 
       begin
@@ -15,7 +18,11 @@ module Jobs
 
         bot = DiscourseAi::Personas::Bot.as(bot_user, persona: persona.new)
 
-        DiscourseAi::AiBot::Playground.new(bot).reply_to(post, feature_name: "bot")
+        DiscourseAi::AiBot::Playground.new(bot).reply_to(
+          post,
+          feature_name: "bot",
+          existing_reply_post: reply_post,
+        )
       rescue DiscourseAi::Personas::Bot::BOT_NOT_FOUND
         Rails.logger.warn(
           "Bot not found for post #{post.id} - perhaps persona was deleted or bot was disabled",
