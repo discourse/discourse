fa8c059806c42bdead8c914249d6522268696db6
Jarek Radosz <jarek@cvx.dev>
2025-12-08 14:01:41 +0100

    local import?

--- frontend/discourse/app/lib/local-dates.js
+++ frontend/discourse/app/lib/local-dates.js
@@ -1,14 +1,14 @@
 import { optionalRequire } from "./utilities";
 
-const _applyLocalDates = optionalRequire(
-  "discourse/plugins/discourse-local-dates/initializers/discourse-local-dates",
-  "applyLocalDates"
-);
-
 export function applyLocalDates(dates, siteSettings, timezone) {
   if (!siteSettings.discourse_local_dates_enabled) {
     return;
   }
+
+  const _applyLocalDates = optionalRequire(
+    "discourse/plugins/discourse-local-dates/initializers/discourse-local-dates",
+    "applyLocalDates"
+  );
 
   _applyLocalDates(dates, siteSettings, timezone);
 }
