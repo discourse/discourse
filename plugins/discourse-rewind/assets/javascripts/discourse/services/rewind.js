03887dabf88da80e46be83cb73e583e9cb46d8a0
Martin Brennan <martin@discourse.org>
2025-12-09 14:50:11 +1000

    FEATURE: Add rewind header icon and tooltip
    This is to make it easier for users to find rewind without
    the distraction of the avatar sparkles. It will go away if
    the user disables rewind in their preferences, or if rewind
    is no longer active (in any month except December and January)

---
+++ plugins/discourse-rewind/assets/javascripts/discourse/services/rewind.js
@@ -0,0 +1,54 @@
+import { tracked } from "@glimmer/tracking";
+import Service, { service } from "@ember/service";
+import KeyValueStore from "discourse/lib/key-value-store";
+
+export default class Rewind extends Service {
+  @service currentUser;
+
+  @tracked dismissed = this.store.getObject("_dismissed") ?? false;
+
+  store = new KeyValueStore("discourse_rewind_" + this.fetchRewindYear);
+
+  @tracked
+  _isDisabled = this.currentUser?.user_option?.discourse_rewind_disabled;
+
+  get disabled() {
+    return this._isDisabled ?? false;
+  }
+
+  set disabled(value) {
+    this._isDisabled = value;
+  }
+
+  // We want to show the previous year's rewind in January
+  // but the current year's rewind in any other month (in
+  // reality, only December).
+  get fetchRewindYear() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+
+    if (currentMonth === 0) {
+      return currentYear - 1;
+    } else {
+      return currentYear;
+    }
+  }
+
+  get fetchRewindNextYear() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+
+    if (currentMonth === 0) {
+      return currentYear;
+    } else {
+      return currentYear + 1;
+    }
+  }
+
+  dismiss() {
+    this.dismissed = true;
+    this.store.setObject({ key: "_dismissed", value: true });
+  }
+}
