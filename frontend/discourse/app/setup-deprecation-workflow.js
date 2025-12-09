722fe5f67e33eccaa89fc8ea8cdcbd87f55fa44c
Jarek Radosz <jarek@cvx.dev>
2025-12-09 12:22:29 +0100

    deprecation-workflow

--- frontend/discourse/app/setup-deprecation-workflow.js
+++ frontend/discourse/app/setup-deprecation-workflow.js
@@ -1,4 +1,4 @@
-// import setupDeprecationWorkflow from "ember-cli-deprecation-workflow";
+import setupDeprecationWorkflow from "ember-cli-deprecation-workflow/addon";
 import DeprecationWorkflow from "./deprecation-workflow";
 
-// setupDeprecationWorkflow({ workflow: DeprecationWorkflow.emberWorkflowList });
+setupDeprecationWorkflow({ workflow: DeprecationWorkflow.emberWorkflowList });
