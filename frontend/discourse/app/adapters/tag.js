8cd2de2afe053c979bb191ba5b71163341cb44a7
Nat <natalie.tay@discourse.org>
2025-12-09 19:34:11 +0800

    change tagadapter primary key for deletion

--- frontend/discourse/app/adapters/tag.js
+++ frontend/discourse/app/adapters/tag.js
@@ -1,16 +1,9 @@
 import RESTAdapter from "discourse/adapters/rest";
-import { ajax } from "discourse/lib/ajax";
 
 export default class TagAdapter extends RESTAdapter {
+  primaryKey = "name";
+
   pathFor(store, type, tagName) {
     return tagName ? `/tag/${tagName}` : `/tags`;
-  }
-
-  // the primaryKey is id,
-  // but since tag routes use the tag name in the URL path, we override here
-  destroyRecord(store, type, record) {
-    return ajax(this.pathFor(store, type, record.get("name")), {
-      type: "DELETE",
-    });
   }
 }
