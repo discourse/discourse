discourse/discourse

commit 7a099ebb6a4c4f177cd416414914d8f902558e90

FEATURE: Allow users to bulk recategorize silently  (#33490)
We want to add an option for admins to carry out the operation silently
when they are updating the category for the selected topics. When the
“Perform this action silently” checkbox has been checked, the
`:notify_category_change sidekiq` job should not be enqueued.

lib/post_revisor.rb
@@ -96,7 +96,7 @@ class PostRevisor
       end
 
       tc.record_change("category_id", current_category&.id, new_category&.id)
-      tc.check_result(tc.topic.change_category_to_id(new_category_id))
+      tc.check_result(tc.topic.change_category_to_id(new_category_id, silent: @silent))
       create_small_action_for_category_change(
         topic: tc.topic,
         user: tc.user,
@@ -215,6 +215,7 @@ class PostRevisor
   # - skip_validations: ask ActiveRecord to skip validations
   # - skip_revision: do not create a new PostRevision record
   # - skip_staff_log: skip creating an entry in the staff action log
+  # - silent: don't send notifications to user
   def revise!(editor, fields, opts = {})
     @editor = editor
     @fields = fields.with_indifferent_access
@@ -270,6 +271,9 @@ class PostRevisor
 
     @skip_revision = false
     @skip_revision = @opts[:skip_revision] if @opts.has_key?(:skip_revision)
+
+    @silent = false
+    @silent = @opts[:silent] if @opts.has_key?(:silent)
 
     @post.incoming_email&.update(imap_sync: true) if @post.incoming_email&.imap_uid
 

spec/lib/topics_bulk_action_spec.rb
@@ -160,6 +160,58 @@ RSpec.describe TopicsBulkAction do
     fab!(:category)
     fab!(:fist_post) { Fabricate(:post, topic: topic) }
 
+    describe "option 'perform action silently'" do
+      fab!(:watcher) { Fabricate(:user) }
+      fab!(:admin)
+
+      before do
+        Jobs.run_immediately!
+        TopicUser.change(
+          watcher,
+          topic.id,
+          notification_level: TopicUser.notification_levels[:watching],
+        )
+      end
+
+      shared_examples "performing with `silent` option provided" do
+        context "when 'silent` option is `false` or not present" do
+          it "will send notification to users watching the topic" do
+            expect do
+              TopicsBulkAction.new(
+                admin,
+                [topic.id],
+                type: "change_category",
+                category_id: category.id,
+              ).perform!
+            end.to change { Notification.where(user: watcher).count }
+          end
+        end
+
+        context "when 'silent' option is `true`" do
+          it "will not send notification to users watching the topic" do
+            expect do
+              TopicsBulkAction.new(
+                admin,
+                [topic.id],
+                type: "change_category",
+                category_id: category.id,
+                silent: true,
+              ).perform!
+            end.to_not change { Notification.where(user: watcher).count }
+          end
+        end
+      end
+
+      context "when 'create_revision_on_bulk_topic_moves' enabled" do
+        SiteSetting.create_revision_on_bulk_topic_moves = true
+        include_examples "performing with `silent` option provided"
+      end
+      context "when 'create_revision_on_bulk_topic_moves' disabled" do
+        SiteSetting.create_revision_on_bulk_topic_moves = false
+        include_examples "performing with `silent` option provided"
+      end
+    end
+
     context "when the user can edit the topic" do
       context "with 'create_revision_on_bulk_topic_moves' setting enabled" do
         before { SiteSetting.create_revision_on_bulk_topic_moves = true }

app/assets/javascripts/discourse/app/components/bulk-select-topics-dropdown.gjs
@@ -221,6 +221,7 @@ export default class BulkSelectTopicsDropdown extends Component {
         break;
       case "update-category":
         this.showBulkTopicActionsModal(actionId, "change_category", {
+          allowSilent: true,
           description: i18n(`topic_bulk_actions.update_category.description`),
         });
         break;

app/assets/javascripts/discourse/app/components/modal/bulk-topic-actions.gjs
@@ -88,9 +88,7 @@ export default class BulkTopicActions extends Component {
     const options = {};
 
     if (this.isSilent) {
-      const newType =
-        operation.type === "close" ? "silent_close" : operation.type;
-      operation.type = newType;
+      operation.silent = true;
     }
 
     if (this.isCloseAction && this.closeNote) {

app/controllers/topics_controller.rb
@@ -1048,11 +1048,16 @@ class TopicsController < ApplicationController
           :category_id,
           :notification_level_id,
           :message,
+          :silent,
           *DiscoursePluginRegistry.permitted_bulk_action_parameters,
           tags: [],
         )
         .to_h
         .symbolize_keys
+
+    if operation.has_key? :silent
+      operation[:silent] = ActiveModel::Type::Boolean.new.cast(operation[:silent])
+    end
 
     raise ActionController::ParameterMissing.new(:operation_type) if operation[:type].blank?
 

app/models/topic.rb
@@ -1021,7 +1021,7 @@ class Topic < ActiveRecord::Base
 
   cattr_accessor :update_featured_topics
 
-  def changed_to_category(new_category)
+  def changed_to_category(new_category, silent: nil)
     return true if new_category.blank? || Category.exists?(topic_id: id)
 
     if new_category.id == SiteSetting.uncategorized_category_id &&
@@ -1053,7 +1053,9 @@ class Topic < ActiveRecord::Base
         CategoryUser.auto_watch(category_id: new_category.id, topic_id: self.id)
         CategoryUser.auto_track(category_id: new_category.id, topic_id: self.id)
 
-        if !SiteSetting.disable_category_edit_notifications && (post = self.ordered_posts.first)
+        skip_alert = silent || SiteSetting.disable_category_edit_notifications
+
+        if !skip_alert && (post = self.ordered_posts.first)
           notified_user_ids = [post.user_id, post.last_editor_id].uniq
           DB.after_commit do
             Jobs.enqueue(
@@ -1130,7 +1132,7 @@ class Topic < ActiveRecord::Base
     new_post
   end
 
-  def change_category_to_id(category_id)
+  def change_category_to_id(category_id, silent: nil)
     return false if private_message?
 
     new_category_id = category_id.to_i
@@ -1144,7 +1146,7 @@ class Topic < ActiveRecord::Base
 
     reviewables.update_all(category_id: new_category_id)
 
-    changed_to_category(cat)
+    changed_to_category(cat, silent: silent)
   end
 
   def remove_allowed_group(removed_by, name)

lib/topics_bulk_action.rb
@@ -13,7 +13,6 @@ class TopicsBulkAction
     @operations ||= %w[
       change_category
       close
-      silent_close
       archive
       change_notification_level
       destroy_post_timing
@@ -136,7 +135,12 @@ class TopicsBulkAction
     updatable_topics = topics.where.not(category_id: @operation[:category_id])
 
     if SiteSetting.create_revision_on_bulk_topic_moves
-      opts = { bypass_bump: true, validate_post: false, bypass_rate_limiter: true }
+      opts = {
+        bypass_bump: true,
+        validate_post: false,
+        bypass_rate_limiter: true,
+        silent: @operation[:silent],
+      }
 
       updatable_topics.each do |t|
         if guardian.can_edit?(t)
@@ -147,7 +151,9 @@ class TopicsBulkAction
     else
       updatable_topics.each do |t|
         if guardian.can_edit?(t)
-          @changed_ids << t.id if t.change_category_to_id(@operation[:category_id])
+          if t.change_category_to_id(@operation[:category_id], silent: @operation[:silent])
+            @changed_ids << t.id
+          end
         end
       end
     end
@@ -169,20 +175,11 @@ class TopicsBulkAction
   def close
     topics.each do |t|
       if guardian.can_moderate?(t)
-        t.update_status("closed", true, @user, { message: @operation[:message] })
-        @changed_ids << t.id
-      end
-    end
-  end
-
-  def silent_close
-    topics.each do |t|
-      if guardian.can_moderate?(t)
         t.update_status(
           "closed",
           true,
           @user,
-          { message: @operation[:message], silent_tracking: true },
+          { message: @operation[:message], silent_tracking: @operation[:silent] },
         )
         @changed_ids << t.id
       end

spec/models/topic_spec.rb
@@ -1909,6 +1909,14 @@ describe Topic do
             expect(topic.category_id).to eq(new_category.id)
           end
 
+          it "should not generate a notification if options: silent is true" do
+            expect do topic.change_category_to_id(new_category.id, silent: true) end.not_to change {
+              Notification.count
+            }
+
+            expect(topic.category_id).to eq(new_category.id)
+          end
+
           it "should generate the modified notification for the topic if already seen" do
             TopicUser.create!(
               topic_id: topic.id,

commit 97f2719231d549b7c38571bd7b8c351275e8d3fa

FIX: silent option not working for bulk category moves via PostRevisor
The `silent` option for bulk category changes was not being respected
when `create_revision_on_bulk_topic_moves` was enabled. This caused
users watching the destination category to receive notifications even
when the "Perform this action silently" checkbox was checked.

The bug was introduced in commit 7a099ebb6a which added `silent: @silent`
to the `track_topic_field(:category_id)` block. However, since blocks
capture `self` at definition time (the class, not the instance), `@silent`
was always nil in that context.

The fix passes the `silent` value through the `TopicChanges` object which
is already available to the block as `tc`.

Also extends test coverage to verify both topic watchers and category
watchers are properly silenced in both code paths.

Ref - meta/t/390993

lib/post_revisor.rb
@@ -10,12 +10,14 @@ class PostRevisor
   # changed a value or not. This is needed for things like custom fields.
   class TopicChanges
     attr_reader :topic, :user
+    attr_accessor :silent
 
     def initialize(topic, user)
       @topic = topic
       @user = user
       @changed = {}
       @errored = false
+      @silent = false
     end
 
     def errored?
@@ -96,7 +98,7 @@ class PostRevisor
       end
 
       tc.record_change("category_id", current_category&.id, new_category&.id)
-      tc.check_result(tc.topic.change_category_to_id(new_category_id, silent: @silent))
+      tc.check_result(tc.topic.change_category_to_id(new_category_id, silent: tc.silent))
       create_small_action_for_category_change(
         topic: tc.topic,
         user: tc.user,
@@ -283,6 +285,7 @@ class PostRevisor
 
     @silent = false
     @silent = @opts[:silent] if @opts.has_key?(:silent)
+    @topic_changes.silent = @silent
 
     @post.incoming_email&.update(imap_sync: true) if @post.incoming_email&.imap_uid
 

spec/lib/topics_bulk_action_spec.rb
@@ -178,7 +178,7 @@ RSpec.describe TopicsBulkAction do
         )
       end
 
-      shared_examples "silent option suppresses topic watcher notifications" do
+      shared_examples "silent option suppresses notifications" do
         it "notifies topic watchers when silent is false" do
           expect do
             TopicsBulkAction.new(
@@ -201,16 +201,6 @@ RSpec.describe TopicsBulkAction do
             ).perform!
           end.to not_change { Notification.where(user: topic_watcher).count }
         end
-      end
-
-      context "when create_revision_on_bulk_topic_moves is enabled" do
-        SiteSetting.create_revision_on_bulk_topic_moves = true
-        include_examples "silent option suppresses topic watcher notifications"
-      end
-
-      context "when create_revision_on_bulk_topic_moves is disabled" do
-        SiteSetting.create_revision_on_bulk_topic_moves = false
-        include_examples "silent option suppresses topic watcher notifications"
 
         it "notifies category watchers when silent is false" do
           expect do
@@ -238,6 +228,16 @@ RSpec.describe TopicsBulkAction do
             ).perform!
           end.to not_change { Notification.where(user: category_watcher).count }
         end
+      end
+
+      context "when create_revision_on_bulk_topic_moves is enabled" do
+        before { SiteSetting.create_revision_on_bulk_topic_moves = true }
+        include_examples "silent option suppresses notifications"
+      end
+
+      context "when create_revision_on_bulk_topic_moves is disabled" do
+        before { SiteSetting.create_revision_on_bulk_topic_moves = false }
+        include_examples "silent option suppresses notifications"
       end
     end
 