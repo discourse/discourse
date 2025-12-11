6365eac08126443db855ba7771a2f91e7690cd52
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2025-12-11 14:38:15 +0800

    FIX: ensure that other user history is not mixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2158,6 +2158,10 @@ RSpec.describe User do
           Fabricate(:user_history, action: UserHistory.actions[:silence_user], target_user: user)
         end
         expect(user.number_of_silencings).to eq(3)
+
+        # ignores other users' history
+        Fabricate(:user_history, action: UserHistory.actions[:silence_user])
+        expect(user.reload.number_of_silencings).to eq(3)
       end
     end
   end
