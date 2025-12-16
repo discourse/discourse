9de71b28496fb86b49efbe832662d6d209cef9e7
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-12-16 22:52:58 +0100

    linting

--- plugins/chat/assets/javascripts/discourse/lib/chat-pane-state.js
+++ plugins/chat/assets/javascripts/discourse/lib/chat-pane-state.js
@@ -1,7 +1,7 @@
+import { tracked } from "@glimmer/tracking";
 import { setOwner } from "@ember/owner";
 import { schedule } from "@ember/runloop";
 import { service } from "@ember/service";
-import { tracked } from "@glimmer/tracking";
 import { bind } from "discourse/lib/decorators";
 import userPresent, {
   onPresenceChange,
