4d20eae44ba39de7ed9277eda54026dfdc978253
David Taylor <david@taylorhq.com>
2025-12-16 13:07:39 +0000

    no module scope side effects

--- frontend/discourse/tests/test-boot-ember-cli.js
+++ frontend/discourse/tests/test-boot-ember-cli.js
@@ -1,3 +1,4 @@
+import { isTesting } from "@embroider/macros";
 import loadEmberExam from "ember-exam/test-support/load";
 import { setupEmberOnerrorValidation, start } from "ember-qunit";
 import * as QUnit from "qunit";
@@ -6,102 +7,103 @@ import { defineModules, loadAdmin, loadThemes } from "discourse/app";
 import config from "discourse/config/environment";
 import setupTests from "discourse/tests/setup-tests";
 
-defineModules(
-  "discourse/tests",
-  Object.fromEntries(
-    Object.entries(import.meta.glob("./**/*.{gjs,js}", { eager: true })).map(
-      ([key, mod]) => {
-        key = key.replace(/\.(gjs|js)$/, "");
-        return [key, mod];
-      }
+if (isTesting()) {
+  defineModules(
+    "discourse/tests",
+    Object.fromEntries(
+      Object.entries(import.meta.glob("./**/*.{gjs,js}", { eager: true })).map(
+        ([key, mod]) => {
+          key = key.replace(/\.(gjs|js)$/, "");
+          return [key, mod];
+        }
+      )
     )
-  )
-);
-
-document.addEventListener("discourse-init", async () => {
-  await loadAdmin();
-  await loadThemes();
-
-  // if (!window.EmberENV.TESTS_FILE_LOADED) {
-  //   throw new Error(
-  //     'The tests file was not loaded. Make sure your tests index.html includes "assets/tests.js".'
-  //   );
-  // }
-
-  const script = document.getElementById("plugin-test-script");
-  if (script && !requirejs.entries["discourse/tests/plugin-tests"]) {
-    throw new Error(
-      `Plugin JS payload failed to load from ${script.src}. Is the Rails server running?`
-    );
-  }
-
-  const params = new URLSearchParams(window.location.search);
-  const target = params.get("target") || "core";
-  const disableAutoStart = params.get("qunit_disable_auto_start") === "1";
-  const hasThemeJs = !!document.querySelector(
-    "link[rel=modulepreload][data-theme-id]"
   );
 
-  document.body.insertAdjacentHTML(
-    "afterbegin",
-    `
+  document.addEventListener("discourse-init", async () => {
+    await loadAdmin();
+    await loadThemes();
+
+    // if (!window.EmberENV.TESTS_FILE_LOADED) {
+    //   throw new Error(
+    //     'The tests file was not loaded. Make sure your tests index.html includes "assets/tests.js".'
+    //   );
+    // }
+
+    const script = document.getElementById("plugin-test-script");
+    if (script && !requirejs.entries["discourse/tests/plugin-tests"]) {
+      throw new Error(
+        `Plugin JS payload failed to load from ${script.src}. Is the Rails server running?`
+      );
+    }
+
+    const params = new URLSearchParams(window.location.search);
+    const target = params.get("target") || "core";
+    const disableAutoStart = params.get("qunit_disable_auto_start") === "1";
+    const hasThemeJs = !!document.querySelector(
+      "link[rel=modulepreload][data-theme-id]"
+    );
+
+    document.body.insertAdjacentHTML(
+      "afterbegin",
+      `
       <div id="qunit"></div>
       <div id="qunit-fixture"></div>
       <div id="ember-testing-container" style="position: fixed">
         <div id="ember-testing"></div>
       </div>
     `
-  );
+    );
 
-  const testingCore = !hasThemeJs && target === "core";
-  if (testingCore) {
-    setupEmberOnerrorValidation();
-  }
-
-  setup(QUnit.assert);
-  setupTests(config.APP);
-  let loader = loadEmberExam();
-
-  if (QUnit.config.seed === undefined) {
-    // If we're running in browser, default to random order. Otherwise, let Ember Exam
-    // handle randomization.
-    QUnit.config.seed = Math.random().toString(36).slice(2);
-  } else {
-    // Don't reorder when specifying a seed
-    QUnit.config.reorder = false;
-  }
-
-  loader.shouldLoadModule = function (name) {
-    if (!/[-_]test$/.test(name)) {
-      return false;
+    const testingCore = !hasThemeJs && target === "core";
+    if (testingCore) {
+      setupEmberOnerrorValidation();
     }
 
-    const isPlugin = name.match(/\/plugins\//);
-    const isTheme = name.match(/\/theme--?\d+\//);
-    const isCore = !isPlugin && !isTheme;
-    const pluginName = name.match(/\/plugins\/([\w-]+)\//)?.[1];
+    setup(QUnit.assert);
+    setupTests(config.APP);
+    let loader = loadEmberExam();
 
-    const loadCore = target === "core" || target === "all";
-    const loadAllPlugins = target === "plugins" || target === "all";
-
-    if (hasThemeJs) {
-      return isTheme;
-    } else if (isCore && !loadCore) {
-      return false;
-    } else if (isPlugin && !(loadAllPlugins || pluginName === target)) {
-      return false;
+    if (QUnit.config.seed === undefined) {
+      // If we're running in browser, default to random order. Otherwise, let Ember Exam
+      // handle randomization.
+      QUnit.config.seed = Math.random().toString(36).slice(2);
+    } else {
+      // Don't reorder when specifying a seed
+      QUnit.config.reorder = false;
     }
 
-    return true;
-  };
-  loader.loadModules();
+    loader.shouldLoadModule = function (name) {
+      if (!/[-_]test$/.test(name)) {
+        return false;
+      }
 
-  start({
-    setupTestContainer: false,
-    startTests: !disableAutoStart,
-    setupEmberOnerrorValidation: testingCore,
-    setupTestIsolationValidation: true,
+      const isPlugin = name.match(/\/plugins\//);
+      const isTheme = name.match(/\/theme--?\d+\//);
+      const isCore = !isPlugin && !isTheme;
+      const pluginName = name.match(/\/plugins\/([\w-]+)\//)?.[1];
+
+      const loadCore = target === "core" || target === "all";
+      const loadAllPlugins = target === "plugins" || target === "all";
+
+      if (hasThemeJs) {
+        return isTheme;
+      } else if (isCore && !loadCore) {
+        return false;
+      } else if (isPlugin && !(loadAllPlugins || pluginName === target)) {
+        return false;
+      }
+
+      return true;
+    };
+    loader.loadModules();
+
+    start({
+      setupTestContainer: false,
+      startTests: !disableAutoStart,
+      setupEmberOnerrorValidation: testingCore,
+      setupTestIsolationValidation: true,
+    });
   });
-});
-
+}
 // window.EmberENV.TESTS_FILE_LOADED = true;
