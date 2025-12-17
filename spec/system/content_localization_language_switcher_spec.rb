8bbbfc5a724c3904cd547f8b15b4a5f39e924192
Penar Musaraj <pmusaraj@gmail.com>
2025-12-17 17:48:09 -0500

    more spec

--- spec/system/content_localization_language_switcher_spec.rb
+++ spec/system/content_localization_language_switcher_spec.rb
@@ -191,6 +191,15 @@ describe "Content localization language switcher", type: :system do
     expect(switcher).to have_no_content("English (UK)")
   end
 
+  it "does not strip (UK) from English (UK) when `en_GB` is not the only English variant" do
+    SiteSetting.content_localization_supported_locales = "en|en_GB|ja"
+    SiteSetting.content_localization_language_switcher = "all"
+
+    visit("/")
+    switcher.expand
+    expect(switcher).to have_content("English")
+    expect(switcher).to have_content("English (UK)")
+  end
   def select_language(locale)
     switcher.expand
     switcher.option("[data-menu-option-id='#{locale}']").click
