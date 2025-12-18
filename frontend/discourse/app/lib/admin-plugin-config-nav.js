1dc776895f2f6b2407e07e10adaf368e37a388a9
awesomerobot <kris.aubuchon@discourse.org>
2025-12-18 12:37:55 -0500

    remove added spaces

--- frontend/discourse/app/lib/admin-plugin-config-nav.js
+++ frontend/discourse/app/lib/admin-plugin-config-nav.js
@@ -3,11 +3,9 @@ let pluginConfigNav = {};
 export function registerAdminPluginConfigNav(pluginId, links) {
   pluginConfigNav[pluginId] = { links };
 }
-
 export function resetAdminPluginConfigNav() {
   pluginConfigNav = {};
 }
-
 export function configNavForPlugin(pluginId) {
   return pluginConfigNav[pluginId];
 }
