89306961bdf442e9d6303a342fd1fc5729e58c64
Jarek Radosz <jarek@cvx.dev>
2025-12-19 13:50:43 +0100

    fix ace

--- frontend/discourse/app/lib/load-ace-editor.js
+++ frontend/discourse/app/lib/load-ace-editor.js
@@ -3,7 +3,6 @@ import { waitForPromise } from "@ember/test-waiters";
 export default async function loadAce() {
   const promise = import("discourse/static/ace-editor-bundle");
   waitForPromise(promise);
-  await promise;
-  // TODO: fix this!!
-  return window.ace;
+
+  return (await promise).default;
 }
