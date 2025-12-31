613dae38e1109a3f09f9b94b79678571ba725bbe
Bannon Tanner <bannon.n.tanner@gmail.com>
2025-12-31 17:49:55 -0600

    add model annotation

--- plugins/discourse-adplugin/app/models/ad_plugin/house_ad_route.rb
+++ plugins/discourse-adplugin/app/models/ad_plugin/house_ad_route.rb
@@ -9,3 +9,19 @@ module AdPlugin
     validates :route_name, presence: true
   end
 end
+
+# == Schema Information
+#
+# Table name: ad_plugin_house_ads_routes
+#
+#  route_name            :string           not null
+#  ad_plugin_house_ad_id :bigint           not null
+#
+# Indexes
+#
+#  index_house_ads_pages  (ad_plugin_house_ad_id,route_name) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (ad_plugin_house_ad_id => ad_plugin_house_ads.id)
+#
