582e5bed03533cd883fa658a02610c20575a47a9
Sam Saffron <sam.saffron@gmail.com>
2026-01-02 12:39:11 +1100

    Moved mathjax to its own bundle via a gem
    This is much cleaner than using ember-cli-build given we are going
    to stop using it anyway in a few months

--- frontend/discourse/lib/empty-module.js
+++
@@ -1,2 +0,0 @@
-// Empty module replacement for Node.js-only imports (e.g., mathjax/require.mjs)
-module.exports = {};
--- plugins/discourse-math/plugin.rb
+++ plugins/discourse-math/plugin.rb
@@ -7,11 +7,25 @@
 # authors: Sam Saffron (sam)
 # url: https://github.com/discourse/discourse/tree/main/plugins/discourse-math
 
+require "discourse_math_bundle"
+
 register_asset "stylesheets/common/discourse-math.scss"
 register_asset "stylesheets/ext/discourse-chat.scss"
 register_svg_icon "square-root-variable"
 
 enabled_site_setting :discourse_math_enabled
+
+# Create symlinks to math library assets from the gem
+plugin_public = File.join(__dir__, "public")
+FileUtils.mkdir_p(plugin_public)
+
+mathjax_link = File.join(plugin_public, "mathjax")
+FileUtils.rm_f(mathjax_link) if File.symlink?(mathjax_link)
+FileUtils.ln_s(DiscourseMathBundle.mathjax_path, mathjax_link)
+
+katex_link = File.join(plugin_public, "katex")
+FileUtils.rm_f(katex_link) if File.symlink?(katex_link)
+FileUtils.ln_s(DiscourseMathBundle.katex_path, katex_link)
 
 after_initialize do
   if respond_to?(:chat) && SiteSetting.chat_enabled
