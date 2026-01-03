ae277d15d73121fec2d2b8562e7c45f318db63cd
Mark McClure <mcmcclur@unca.edu>
2026-01-03 09:46:00 -0500

    Remove inert code in load-mathjax.js that looks like it should load modules but doesn't

--- frontend/discourse/app/lib/load-mathjax.js
+++ frontend/discourse/app/lib/load-mathjax.js
@@ -1,8 +1,6 @@
 import { waitForPromise } from "@ember/test-waiters";
 
 let basePromise;
-let asciimathPromise;
-let a11yPromise;
 
 export default async function loadMathJax(options = {}) {
   const output = options.output === "svg" ? "svg" : "html";
@@ -13,20 +11,7 @@ export default async function loadMathJax(options = {}) {
       return bundle;
     })());
 
-    if (options.enableAsciimath && !asciimathPromise) {
-      asciimathPromise = module.loadAsciiMath?.();
-    }
-
-    if (options.enableAccessibility && !a11yPromise) {
-      // Tell MathJax's loader these modules are being loaded via webpack
-      // to prevent it from trying to fetch them separately
-      window.MathJax?.loader?.preLoad?.("a11y/sre", "a11y/explorer");
-      a11yPromise = module.loadA11yExplorer?.();
-    }
-
-    await Promise.all([asciimathPromise, a11yPromise].filter(Boolean));
     await window.MathJax?.startup?.promise;
-
     return module.getMathJax?.() ?? window.MathJax;
   })();
 
