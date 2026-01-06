247607a10eaa719408a317b1aeb8588daa0772b3
Régis Hanol <regis@hanol.fr>
2026-01-06 13:24:28 +0100

    DEV: Update model annotations after removing prefers_encrypt column

--- plugins/automation/app/models/discourse_automation/pending_pm.rb
+++ plugins/automation/app/models/discourse_automation/pending_pm.rb
@@ -16,13 +16,12 @@ end
 # Table name: discourse_automation_pending_pms
 #
 #  id               :bigint           not null, primary key
-#  target_usernames :string           is an Array
-#  sender           :string
-#  title            :string
-#  raw              :string
-#  automation_id    :bigint           not null
 #  execute_at       :datetime         not null
+#  raw              :string
+#  sender           :string
+#  target_usernames :string           is an Array
+#  title            :string
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
-#  prefers_encrypt  :boolean          default(FALSE), not null
+#  automation_id    :bigint           not null
 #
