discourse/discourse

commit 22e06e715126aec94b01bfcbdb40d9a9b6945441

DEV: Rename discourse_rewind_disabled column to discourse_rewind_enabled (#36961)
Follow-up to #36723.

The original PR added virtual accessors to flip the column semantics
while minimizing deployment risk. Now that the change has been deployed
and verified, this migration properly renames the column to match the
exposed attribute name.

The column values are inverted during migration (disabled=true becomes
enabled=false) so existing user preferences are preserved. The old
column is marked readonly and will be dropped in a post-deploy migration
following our standard column removal pattern.

plugins/discourse-rewind/db/migrate/20260105171115_rename_discourse_rewind_disabled_to_enabled.rb
@@ -1,0 +1,24 @@
+# frozen_string_literal: true
+
+class RenameDiscourseRewindDisabledToEnabled < ActiveRecord::Migration[7.2]
+  def up
+    add_column :user_options, :discourse_rewind_enabled, :boolean, default: true, null: false
+
+    execute <<~SQL
+      UPDATE user_options
+      SET discourse_rewind_enabled = NOT discourse_rewind_disabled
+    SQL
+
+    change_column_default :user_options, :discourse_rewind_disabled, nil
+
+    Migration::ColumnDropper.mark_readonly(:user_options, :discourse_rewind_disabled)
+  end
+
+  def down
+    Migration::ColumnDropper.drop_readonly(:user_options, :discourse_rewind_disabled)
+
+    change_column_default :user_options, :discourse_rewind_disabled, false
+
+    remove_column :user_options, :discourse_rewind_enabled
+  end
+end

plugins/discourse-rewind/db/post_migrate/20260105171226_drop_discourse_rewind_disabled_from_user_options.rb
@@ -1,0 +1,20 @@
+# frozen_string_literal: true
+
+class DropDiscourseRewindDisabledFromUserOptions < ActiveRecord::Migration[7.2]
+  DROPPED_COLUMNS = { user_options: %i[discourse_rewind_disabled] }
+
+  def up
+    # Repeat the backfill in case any data was added during the deploy window
+    execute <<~SQL
+      UPDATE user_options
+      SET discourse_rewind_enabled = NOT discourse_rewind_disabled
+      WHERE discourse_rewind_enabled != (NOT discourse_rewind_disabled)
+    SQL
+
+    DROPPED_COLUMNS.each { |table, columns| Migration::ColumnDropper.execute_drop(table, columns) }
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end

plugins/discourse-rewind/plugin.rb
@@ -45,12 +45,8 @@ require_relative "lib/discourse_rewind/engine"
 after_initialize do
   UserUpdater::OPTION_ATTR.push(:discourse_rewind_enabled, :discourse_rewind_share_publicly)
 
-  # TODO: Rename the `discourse_rewind_disabled` column to `discourse_rewind_enabled` and remove these
-  add_to_class(:user_option, :discourse_rewind_enabled) { !discourse_rewind_disabled }
-
-  add_to_class(:user_option, :discourse_rewind_enabled=) do |value|
-    self.discourse_rewind_disabled = !ActiveModel::Type::Boolean.new.cast(value)
-  end
+  # TODO: Remove once 20260105171115_rename_discourse_rewind_disabled_to_enabled has been promoted to pre-deploy
+  UserOption.ignored_columns += %i[discourse_rewind_disabled]
 
   add_to_class(:user, :discourse_rewind_and_profile_public?) do
     self.user_option.discourse_rewind_share_publicly && !self.user_option.hide_profile

commit 3cdc5dd5f7cda3fd14b58de8e22cb01fff7959b8

FIX: Remove NOT NULL constraint on deprecated discourse_rewind_disabled column
The migration that renamed `discourse_rewind_disabled` to
`discourse_rewind_enabled` set the old column's default to nil and
marked it as readonly, but forgot to remove the NOT NULL constraint.

This caused user creation to fail (during seeding or new signups)
because inserting NULL into a NOT NULL column raises an error 🤦.

This commit:
1. Fixes the original migration to also call `change_column_null`
2. Adds a new migration to fix databases that already ran the
   broken migration (with a guard for cases where the post-deploy
   migration already dropped the column)

Regression from 22e06e7151 (#36961)

plugins/discourse-rewind/db/migrate/20260105171115_rename_discourse_rewind_disabled_to_enabled.rb
@@ -10,6 +10,7 @@ class RenameDiscourseRewindDisabledToEnabled < ActiveRecord::Migration[7.2]
     SQL
 
     change_column_default :user_options, :discourse_rewind_disabled, nil
+    change_column_null :user_options, :discourse_rewind_disabled, true
 
     Migration::ColumnDropper.mark_readonly(:user_options, :discourse_rewind_disabled)
   end
@@ -17,6 +18,7 @@ class RenameDiscourseRewindDisabledToEnabled < ActiveRecord::Migration[7.2]
   def down
     Migration::ColumnDropper.drop_readonly(:user_options, :discourse_rewind_disabled)
 
+    change_column_null :user_options, :discourse_rewind_disabled, false
     change_column_default :user_options, :discourse_rewind_disabled, false
 
     remove_column :user_options, :discourse_rewind_enabled

plugins/discourse-rewind/db/migrate/20260108133149_fix_discourse_rewind_disabled_null_constraint.rb
@@ -1,0 +1,17 @@
+# frozen_string_literal: true
+
+class FixDiscourseRewindDisabledNullConstraint < ActiveRecord::Migration[7.2]
+  def up
+    # The previous migration (20260105171115) changed the default to nil and marked
+    # the column as readonly, but forgot to remove the NOT NULL constraint.
+    # This caused failures when inserting new rows (e.g., during seeding or new user signup).
+    # This migration fixes databases that already ran the broken migration.
+    return unless column_exists?(:user_options, :discourse_rewind_disabled)
+
+    change_column_null :user_options, :discourse_rewind_disabled, true
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end