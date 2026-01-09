645bac6ccbc00087c03b5cf3a954da8ed7a362e0
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2026-01-09 13:07:30 +0800

    PERF: Add `index_topics_on_category_id` (#37022)
    Joining the `topics` table against the `categories` table is a common
    thing we do and the missing index on the `category_id` foreign key is
    hurting performance on sites with lots of rows in the `topics` and
    `categories` tables.

---
+++ db/migrate/20260109041508_add_index_category_on_topics.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+class AddIndexCategoryOnTopics < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
+  def up
+    execute "DROP INDEX IF EXISTS index_topics_on_category_id;"
+    execute "CREATE INDEX CONCURRENTLY index_topics_on_category_id ON topics (category_id) WHERE deleted_at IS NULL AND (archetype <> 'private_message');"
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
