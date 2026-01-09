ab88820da3ba1ef38a4d9749873eb02ca527470f
Natalie Tay <natalie.tay@gmail.com>
2026-01-09 19:52:58 +0800

    DEV: Tag group creation specs (#37034)
    Similar to https://github.com/discourse/discourse/pull/37012, we want to
    cover tag group creation and loading to make sure changes in
    https://github.com/discourse/discourse/pull/36678 are sound. This spec
    ensures existing tag groups are loaded, and new tag groups can be
    created.

---
+++ spec/system/page_objects/pages/admin_tag_groups.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Pages
+    class AdminTagGroups < PageObjects::Pages::Base
+      def tags_chooser
+        PageObjects::Components::SelectKit.new(".group-tags-list .tag-chooser")
+      end
+
+      def parent_tag_chooser
+        PageObjects::Components::SelectKit.new(".parent-tag-section .tag-chooser")
+      end
+
+      def has_tag_group_in_sidebar?(name)
+        has_css?(".tag-groups-sidebar li", text: name)
+      end
+
+      def has_no_tag_group_in_sidebar?(name)
+        has_no_css?(".tag-groups-sidebar li", text: name)
+      end
+
+      def has_tags_label?
+        has_css?(".group-tags-list label", text: I18n.t("js.tagging.groups.tags_label"))
+      end
+
+      def has_parent_tag_label?
+        has_css?(".parent-tag-section label", text: I18n.t("js.tagging.groups.parent_tag_label"))
+      end
+
+      def has_visible_permission_label?
+        has_css?(
+          "label[for='visible-permission']",
+          text: I18n.t("js.tagging.groups.usable_only_by_groups"),
+        )
+      end
+
+      def has_tag_in_group?(tag_name)
+        has_css?(".group-tags-list .tag-chooser", text: tag_name)
+      end
+
+      def visit
+        page.visit("/tag_groups")
+        self
+      end
+
+      def visit_tag_group(tag_group)
+        page.visit("/tag_groups/#{tag_group.id}")
+        self
+      end
+
+      def click_new_tag_group
+        if has_css?(".tag-groups-sidebar .btn-default", wait: 0)
+          find(".tag-groups-sidebar .btn-default").click
+        else
+          find(".tag-group-content .btn-primary").click
+        end
+        self
+      end
+
+      def fill_name(name)
+        find(".group-name input").fill_in(with: name)
+        self
+      end
+
+      def select_public_permission
+        find("#public-permission").click
+        self
+      end
+
+      def select_visible_permission
+        find("#visible-permission").click
+        self
+      end
+
+      def save
+        find(".tag-group-controls .btn-primary").click
+        self
+      end
+    end
+  end
+end
