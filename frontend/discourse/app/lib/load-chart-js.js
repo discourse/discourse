5942b3c7d01694d3ea292ff5d1a079b509ddb529
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2026-01-13 18:19:59 +0100

    linting

--- frontend/discourse/app/lib/load-chart-js.js
+++ frontend/discourse/app/lib/load-chart-js.js
@@ -5,6 +5,10 @@ export default async function loadChartJS() {
   return (await waitForPromise(import("chart.js/auto"))).default;
 }
 
+/**
+ *
+ * @returns {any}
+ */
 export async function loadChartJSDatalabels() {
   return (await waitForPromise(import("chartjs-plugin-datalabels"))).default;
 }
