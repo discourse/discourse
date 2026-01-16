6b20669d3bf759dd98560e5e3d0901bb5b2c29b8
Nat <natalie.tay@discourse.org>
2026-01-16 15:58:20 +0800

    DEV: Consolidate tag (tag-info) settings specs

--- spec/system/tag_settings_spec.rb
+++ spec/system/tag_settings_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-describe "Tag Edit", type: :system do
+describe "Tag Settings", type: :system do
   let(:tags_page) { PageObjects::Pages::Tag.new }
   let(:dialog) { PageObjects::Components::Dialog.new }
   fab!(:tag_1) { Fabricate(:tag, name: "design") }
+  fab!(:tag_2) { Fabricate(:tag, name: "cat") }
   fab!(:user)
   fab!(:admin)
   fab!(:trust_level_4)
@@ -13,26 +14,20 @@ describe "Tag Edit", type: :system do
   context "when signed in as admin" do
     before { sign_in(admin) }
 
-    it "allows the admin to edit a tag name and description" do
+    it "allows the admin to edit a tag name and description, and delete the tag" do
       tags_page.visit_tag(tag_1)
       tags_page.tag_info_btn.click
       tags_page.open_edit_tag
+
       tags_page.fill_tag_name("ux")
       tags_page.fill_tag_description("new description")
       tags_page.save_edit
       expect(tags_page.tag_info).to have_content("ux")
       expect(tags_page.tag_info).to have_content("new description")
-    end
 
-    it "allows the admin to delete the tag" do
-      tags_page.visit_tag(tag_1)
-      tags_page.tag_info_btn.click
       tags_page.delete_tag
-
       expect(dialog).to be_open
-
       dialog.click_danger
-
       expect(tags_page).to have_no_tag("design")
     end
 
@@ -48,25 +43,39 @@ describe "Tag Edit", type: :system do
       tags_page.save_edit
       expect(tags_page.tag_info).to have_content("design")
     end
+
+    it "allows adding tags as synonyms" do
+      tags_page.visit_tag(tag_1)
+      tags_page.tag_info_btn.click
+      tags_page.edit_synonyms_btn.click
+
+      tags_page.select_tag(index: 0)
+      tags_page.search_tags("kittehs")
+      tags_page.select_tag(name: "kittehs")
+      tags_page.add_synonym_btn.click
+
+      expect(tags_page.confirm_synonym_btn).to be_visible
+      tags_page.confirm_synonym_btn.click
+
+      expect(tags_page.tag_box(tag_2.name)).to be_visible
+      expect(tags_page.tag_box("kittehs")).to be_visible
+    end
   end
 
   context "when signed in as a user allowed to edit tags" do
     before { sign_in(trust_level_4) }
 
-    it "allows the user to edit a tag name and description" do
+    it "allows the user to edit a tag name and description, but cannot delete the tag" do
       tags_page.visit_tag(tag_1)
       tags_page.tag_info_btn.click
       tags_page.open_edit_tag
+
       tags_page.fill_tag_name("ux")
       tags_page.fill_tag_description("new description")
       tags_page.save_edit
       expect(tags_page.tag_info).to have_content("ux")
       expect(tags_page.tag_info).to have_content("new description")
-    end
 
-    it "does not allow the user to delete the tag" do
-      tags_page.visit_tag(tag_1)
-      tags_page.tag_info_btn.click
       expect(tags_page.tag_info).to have_no_css(".delete-tag")
     end
   end
