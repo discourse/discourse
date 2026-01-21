654e01aad4100e094b836a2b7276c196ff418eb7
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2026-01-21 10:21:40 +0800

    FEATURE: Allow "hot" as category default view (#37231)
    Adds "hot" as an available option for category default view alongside
    "latest" and "top". Administrators can now configure categories to show
    the hot topics list by default when users visit the category.
    
    <img width="659" height="228" alt="Screenshot 2026-01-21 at 9 26 26 am"
    src="https://github.com/user-attachments/assets/258d1731-094e-4b51-a342-1bec55d6482c"
    />

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -147,7 +147,7 @@ class ListController < ApplicationController
   def category_default
     canonical_url "#{Discourse.base_url_no_prefix}#{@category.url}"
     view_method = @category.default_view
-    view_method = "latest" if %w[latest top].exclude?(view_method)
+    view_method = "latest" if %w[hot latest top].exclude?(view_method)
 
     self.public_send(view_method, category: @category.id)
   end
