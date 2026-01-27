e99dc0c17285a7ab0c0604e37931409a167324b6
Loïc Guitaut <loic@discourse.org>
2026-01-27 17:46:58 +0100

    WIP: handle AS::SafeBuffer

--- lib/multi_json/adapters/json_coder.rb
+++ lib/multi_json/adapters/json_coder.rb
@@ -3,7 +3,15 @@
 module MultiJson
   module Adapters
     class JsonCoder < JsonGem
-      CODER = JSON::Coder.new(&:as_json)
+      CODER =
+        JSON::Coder.new do |object|
+          case object
+          when ActiveSupport::SafeBuffer
+            object.to_str
+          else
+            object.as_json
+          end
+        end
 
       def dump(object, options)
         CODER.dump(object)
