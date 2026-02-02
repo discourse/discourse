5b50db9194ccc9b0912a81780f872796d50d91de
Martin Brennan <martin@discourse.org>
2026-02-02 10:47:00 +1000

    DEV: reiew and spec fixes

--- app/controllers/admin/impersonate_controller.rb
+++ app/controllers/admin/impersonate_controller.rb
@@ -18,7 +18,6 @@ class Admin::ImpersonateController < Admin::AdminController
 
       start_impersonating_user(user)
     else
-      # Log on as the user
       log_on_user(user, impersonate: true)
     end
 
@@ -26,16 +25,15 @@ class Admin::ImpersonateController < Admin::AdminController
   end
 
   def destroy
-    if !UpcomingChanges.enabled_for_user?(:experimental_impersonation, current_user)
+    unless UpcomingChanges.enabled_for_user?(:experimental_impersonation, current_user)
       raise Discourse::NotFound
     end
-    raise Discourse::InvalidAccess if !current_user.is_impersonating
 
-    puppet = current_user
+    raise Discourse::InvalidAccess unless current_user.is_impersonating
 
+    impersonated_user = current_user
     stop_impersonating_user
-
-    StaffActionLogger.new(current_user).log_stop_impersonation(puppet)
+    StaffActionLogger.new(current_user).log_stop_impersonation(impersonated_user)
 
     render body: nil
   end
