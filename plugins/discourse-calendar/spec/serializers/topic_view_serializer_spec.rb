9bb319ef2bf8ba583dc6c62d0ab5e13dd51e12bc
Renato Atilio <renato@discourse.org>
2026-02-04 01:25:26 -0300

    FIX: calendar plugin event timezone mismatch

--- plugins/discourse-calendar/spec/serializers/topic_view_serializer_spec.rb
+++ plugins/discourse-calendar/spec/serializers/topic_view_serializer_spec.rb
@@ -14,22 +14,87 @@ RSpec.describe TopicViewSerializer do
     Jobs.run_immediately!
     SiteSetting.calendar_enabled = true
     SiteSetting.discourse_post_event_enabled = true
-    DiscoursePostEvent::Event.create!(
-      id: first_post.id,
-      original_starts_at: 1.hour.from_now,
-      original_ends_at: 2.hours.from_now,
-    )
   end
 
-  describe "#event_starts_at" do
-    it "returns the start time of the event in proper format" do
-      expect(parsed_json["event_starts_at"]).to eq("2020-04-24T15:10:00.000Z")
+  context "without timezone" do
+    before do
+      DiscoursePostEvent::Event.create!(
+        id: first_post.id,
+        original_starts_at: 1.hour.from_now,
+        original_ends_at: 2.hours.from_now,
+      )
+    end
+
+    describe "#event_starts_at" do
+      it "returns the start time of the event in proper format" do
+        expect(parsed_json["event_starts_at"]).to eq("2020-04-24T15:10:00.000Z")
+      end
+    end
+
+    describe "#event_ends_at" do
+      it "returns the end time of the event in proper format" do
+        expect(parsed_json["event_ends_at"]).to eq("2020-04-24T16:10:00.000Z")
+      end
+    end
+
+    describe "#event_timezone" do
+      it "is not included when event has no timezone" do
+        expect(parsed_json).not_to have_key("event_timezone")
+      end
+    end
+
+    describe "#event_show_local_time" do
+      it "returns false when show_local_time is not set" do
+        expect(parsed_json["event_show_local_time"]).to eq(false)
+      end
     end
   end
 
-  describe "#event_ends_at" do
-    it "returns the end time of the event in proper format" do
-      expect(parsed_json["event_ends_at"]).to eq("2020-04-24T16:10:00.000Z")
+  context "with timezone and show_local_time true" do
+    before do
+      DiscoursePostEvent::Event.create!(
+        id: first_post.id,
+        original_starts_at: 1.hour.from_now,
+        original_ends_at: 2.hours.from_now,
+        timezone: "Australia/Sydney",
+        show_local_time: true,
+      )
+    end
+
+    describe "#event_timezone" do
+      it "returns the timezone of the event" do
+        expect(parsed_json["event_timezone"]).to eq("Australia/Sydney")
+      end
+    end
+
+    describe "#event_show_local_time" do
+      it "returns true when show_local_time is set" do
+        expect(parsed_json["event_show_local_time"]).to eq(true)
+      end
+    end
+  end
+
+  context "with timezone and show_local_time false" do
+    before do
+      DiscoursePostEvent::Event.create!(
+        id: first_post.id,
+        original_starts_at: 1.hour.from_now,
+        original_ends_at: 2.hours.from_now,
+        timezone: "Australia/Sydney",
+        show_local_time: false,
+      )
+    end
+
+    describe "#event_timezone" do
+      it "returns the timezone of the event" do
+        expect(parsed_json["event_timezone"]).to eq("Australia/Sydney")
+      end
+    end
+
+    describe "#event_show_local_time" do
+      it "returns false when show_local_time is explicitly false" do
+        expect(parsed_json["event_show_local_time"]).to eq(false)
+      end
     end
   end
 end
