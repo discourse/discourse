919a1d0849a35a1e3175cce1424dcc1347cc2cbe
Jarek Radosz <jarek@cvx.dev>
2026-02-13 15:27:25 +0100

    DEV: Remove global `exists` test helper (#37781)
    deprecated over 5 years ago

--- frontend/discourse/tests/setup-tests.js
+++ frontend/discourse/tests/setup-tests.js
@@ -35,7 +35,6 @@ import { setupDeprecationCounter } from "discourse/tests/helpers/deprecation-cou
 import { clearState as clearPresenceState } from "discourse/tests/helpers/presence-pretender";
 import {
   applyPretender,
-  exists,
   resetSite,
   testCleanup,
   testsInitialized,
@@ -44,7 +43,6 @@ import {
 import { configureRaiseOnDeprecation } from "discourse/tests/helpers/raise-on-deprecation";
 import { resetSettings } from "discourse/tests/helpers/site-settings";
 import { disableCloaking } from "discourse/modifiers/post-stream-viewport-tracker";
-import deprecated from "discourse/lib/deprecated";
 import { setDefaultOwner } from "discourse/lib/get-owner";
 import { setupS3CDN, setupURL } from "discourse/lib/get-url";
 import { buildResolver } from "discourse/resolver";
@@ -250,20 +248,6 @@ export default function setupTests(config) {
   } else {
     window.Logster = { enabled: false };
   }
-
-  Object.defineProperty(window, "exists", {
-    get() {
-      deprecated(
-        "Accessing the global function `exists` is deprecated. Import it instead.",
-        {
-          since: "2.6.0.beta.4",
-          dropFrom: "2.6.0",
-          id: "discourse.qunit.global-exists",
-        }
-      );
-      return exists;
-    },
-  });
 
   let setupData;
   const setupDataElement = document.getElementById("data-discourse-setup");
