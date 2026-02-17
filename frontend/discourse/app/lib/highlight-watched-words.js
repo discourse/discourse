6b87d69f0309a3ebfc80d84c12f86e9b261320fb
Keegan George <kgeorge13@gmail.com>
2026-02-17 12:47:48 -0800

    SECURITY: XSS in review queue via `highlightWatchedWords`
    This update fixes a XSS issue where a XSS was permitted in the review queue via the `highlightWatchedWords` function leading to arbitrary JS execution when in moderator context.

--- frontend/discourse/app/lib/highlight-watched-words.js
+++ frontend/discourse/app/lib/highlight-watched-words.js
@@ -1,5 +1,4 @@
 import { trustHTML } from "@ember/template";
-import { decode } from "html-entities";
 import highlightHTML from "discourse/lib/highlight-html";
 
 export default function (text, reviewable) {
@@ -31,5 +30,5 @@ export default function (text, reviewable) {
     });
   });
 
-  return trustHTML(decode(elem.innerHTML));
+  return trustHTML(elem.innerHTML);
 }
