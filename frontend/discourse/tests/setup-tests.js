e728ee8d87e7ebfd191bca1f5368efdd39d03e88
David Taylor <david@taylorhq.com>
2026-02-24 11:27:56 +0000

    fix imports

--- frontend/discourse/tests/setup-tests.js
+++ frontend/discourse/tests/setup-tests.js
@@ -21,7 +21,6 @@ import {
   disableLoadMoreObserver,
   enableLoadMoreObserver,
 } from "discourse/components/load-more";
-import deprecated from "discourse/lib/deprecated";
 import { setDefaultOwner } from "discourse/lib/get-owner";
 import { setupS3CDN, setupURL } from "discourse/lib/get-url";
 import { setLoadedFaker } from "discourse/lib/load-faker";
@@ -51,14 +50,6 @@ import {
 } from "discourse/tests/helpers/qunit-helpers";
 import { configureRaiseOnDeprecation } from "discourse/tests/helpers/raise-on-deprecation";
 import { resetSettings } from "discourse/tests/helpers/site-settings";
-import { disableCloaking } from "discourse/modifiers/post-stream-viewport-tracker";
-import { setDefaultOwner } from "discourse/lib/get-owner";
-import { setupS3CDN, setupURL } from "discourse/lib/get-url";
-import { buildResolver } from "discourse/resolver";
-import { loadSprites } from "../lib/svg-sprite-loader";
-import * as FakerModule from "@faker-js/faker";
-import { setLoadedFaker } from "discourse/lib/load-faker";
-import config from "discourse/config/environment";
 
 const REPORT_MEMORY = false;
 let cancelled = false;
