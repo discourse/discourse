1e35d4bffb64875daa69560b9e3422522cc43aa9
Jarek Radosz <jarek@cvx.dev>
2026-02-26 16:47:32 +0100

    cache

--- lib/plugin/js_manager.rb
+++ lib/plugin/js_manager.rb
@@ -2,20 +2,27 @@
 
 module Plugin
   class JsManager
-    def self.digested_logical_path_for(plugin_name, entrypoint_name)
-      manifest_path = "#{Rails.root}/app/assets/generated/#{plugin_name}/manifest.json"
-      manifest = JSON.parse(File.read(manifest_path))
-      entrypoint_filename = manifest[entrypoint_name]["fileName"]
+    @@manifest_data = {}
 
+    def self.read_manifest(plugin_name)
+      manifest_path = "#{Rails.root}/app/assets/generated/#{plugin_name}/manifest.json"
+
+      if Rails.env.production?
+        @@manifest_data[plugin_name] ||= JSON.parse(File.read(manifest_path))
+      else
+        JSON.parse(File.read(manifest_path))
+      end
+    end
+
+    def self.digested_logical_path_for(plugin_name, entrypoint_name)
+      entrypoint_filename = read_manifest(plugin_name)[entrypoint_name]["fileName"]
       "js/plugins/#{entrypoint_filename.delete_suffix(".js")}"
     end
 
     def self.import_paths_for(plugin_name, entrypoint_name)
-      manifest_path = "#{Rails.root}/app/assets/generated/#{plugin_name}/manifest.json"
-      manifest = JSON.parse(File.read(manifest_path))
-      imports = manifest[entrypoint_name]["imports"]
-
-      imports.map { "js/plugins/#{it.delete_suffix(".js")}" }
+      read_manifest(plugin_name)[entrypoint_name]["imports"].map do
+        "js/plugins/#{it.delete_suffix(".js")}"
+      end
     end
 
     def compile!
