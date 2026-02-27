e37832b0b9cd3dea432bbfcea7fe4438a5700e32
David Battersby <info@davidbattersby.com>
2026-02-27 17:57:44 +0400

    use topic.id instead of topic_id

--- plugins/discourse-solved/plugin.rb
+++ plugins/discourse-solved/plugin.rb
@@ -206,12 +206,12 @@ after_initialize do
   )
 
   register_html_builder("server:before-head-close-crawler") do |controller|
-    topic_id = controller.instance_variable_get(:@topic_view)&.topic_id
+    topic_id = controller.instance_variable_get(:@topic_view)&.topic&.id
     DiscourseSolved::BuildSchemaMarkup.html_for(topic_id: topic_id, guardian: controller.guardian)
   end
 
   register_html_builder("server:before-head-close") do |controller|
-    topic_id = controller.instance_variable_get(:@topic_view)&.topic_id
+    topic_id = controller.instance_variable_get(:@topic_view)&.topic&.id
     DiscourseSolved::BuildSchemaMarkup.html_for(topic_id: topic_id, guardian: controller.guardian)
   end
 
