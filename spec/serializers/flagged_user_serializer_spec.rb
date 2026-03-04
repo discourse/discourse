4c0978a8554eddcc23d00e6767f2820fa0bfa074
Isaac Janzen <isaac.janzen@discourse.org>
2026-03-04 10:17:11 -0600

    SECURITY: Hide IP from flagged regular users

--- spec/serializers/flagged_user_serializer_spec.rb
+++ spec/serializers/flagged_user_serializer_spec.rb
@@ -21,4 +21,35 @@ RSpec.describe FlaggedUserSerializer do
     json = serializer.as_json
     expect(json[:email]).to eq("flagged@example.com")
   end
+
+  describe "#ip_address" do
+    fab!(:user)
+
+    before { flagged_user.update!(ip_address: "1.2.3.4") }
+
+    it "includes ip_address for admins" do
+      json =
+        FlaggedUserSerializer.new(flagged_user, scope: Guardian.new(admin), root: false).as_json
+      expect(json[:ip_address]).to eq("1.2.3.4")
+    end
+
+    it "includes ip_address for moderators when moderators_view_ips is enabled" do
+      SiteSetting.moderators_view_ips = true
+      json =
+        FlaggedUserSerializer.new(flagged_user, scope: Guardian.new(moderator), root: false).as_json
+      expect(json[:ip_address]).to eq("1.2.3.4")
+    end
+
+    it "does not include ip_address for moderators when moderators_view_ips is disabled" do
+      SiteSetting.moderators_view_ips = false
+      json =
+        FlaggedUserSerializer.new(flagged_user, scope: Guardian.new(moderator), root: false).as_json
+      expect(json[:ip_address]).to be_nil
+    end
+
+    it "does not include ip_address for regular users" do
+      json = FlaggedUserSerializer.new(flagged_user, scope: Guardian.new(user), root: false).as_json
+      expect(json[:ip_address]).to be_nil
+    end
+  end
 end
