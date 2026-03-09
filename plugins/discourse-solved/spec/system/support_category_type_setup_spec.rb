88d4d16a4208bae79c653d349e9873543e254b43
Martin Brennan <martin@discourse.org>
2026-03-09 13:47:03 +1000

    DEV: Test fix

--- plugins/discourse-solved/spec/system/support_category_type_setup_spec.rb
+++ plugins/discourse-solved/spec/system/support_category_type_setup_spec.rb
@@ -42,4 +42,28 @@ RSpec.describe "Support Category Type Setup", type: :system do
     expect(category.custom_fields["enable_accepted_answers"]).to eq("true")
     expect(category.custom_fields["solved_topics_auto_close_hours"]).to eq("48")
   end
+
+  context "when there is a support category already configured" do
+    fab!(:category)
+
+    before do
+      DiscourseSolved::Categories::Types::Support.configure_category(
+        category,
+        guardian: admin.guardian,
+      )
+    end
+
+    it "does not preload basic data for the support category type" do
+      visit("/new-category/setup")
+      category_type_card.find_type_card("support").click
+
+      expect(page).to have_content(I18n.t("js.category.create_with_type", typeName: "support"))
+      expect(form.field("name").value).to eq("")
+      expect(
+        form.field("style_type").find(".form-kit__control-radio[type='radio'][value='emoji']")[
+          "checked"
+        ],
+      ).to eq(nil)
+    end
+  end
 end
