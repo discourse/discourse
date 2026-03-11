89662178b216ec382c5d25b6c0feeb7310a4ac75
Jarek Radosz <jarek@cvx.dev>
2026-03-11 14:24:36 +0100

    add the rest of attrs for completness sake

--- frontend/discourse/app/models/tag-info.js
+++ frontend/discourse/app/models/tag-info.js
@@ -3,11 +3,16 @@ import { autoTrackedArray } from "discourse/lib/tracked-tools";
 import RestModel from "discourse/models/rest";
 
 export default class TagInfo extends RestModel {
+  @tracked category_restricted;
   @tracked description;
+  @tracked id;
   @tracked name;
   @tracked slug;
+  @tracked staff;
+  @tracked topic_count;
 
   @autoTrackedArray categories;
+  @autoTrackedArray localizations;
   @autoTrackedArray synonyms;
   @autoTrackedArray tag_group_names;
 }
