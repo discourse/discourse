9330edc14f1b579b49d5d210a6eb6ffb6a7d61a1
Gerhard Schlager <gerhard.schlager@discourse.org>
2026-03-11 19:06:30 +0100

    Update schema

--- migrations/db/intermediate_db_schema/100-base-schema.sql
+++ migrations/db/intermediate_db_schema/100-base-schema.sql
@@ -1,6 +1,6 @@
 -- This file is auto-generated from the IntermediateDB schema. To make changes,
--- update the configuration files in "config/schema/" and then run
--- `bin/cli schema generate` to regenerate this file.
+-- update the configuration files in "migrations/config/schema/" and then run
+-- `migrations/bin/cli schema generate` to regenerate this file.
 
 CREATE TABLE badge_groupings
 (
