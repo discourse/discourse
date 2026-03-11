f1a39bec95a2732e0553e18af20a8b696e7cdbb3
Bannon Tanner <bannon@discourse.org>
2026-03-11 12:54:53 -0500

    add migration for index on all day to topic custom fields

---
+++ plugins/discourse-calendar/db/migrate/20260311161955_add_topic_custom_field_all_day_index.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddTopicCustomFieldAllDayIndex < ActiveRecord::Migration[7.2]
+  def change
+    add_index :topic_custom_fields,
+              %i[name topic_id],
+              name: :idx_topic_custom_fields_topic_post_event_all_day,
+              unique: true,
+              where: "name = '#{DiscoursePostEvent::TOPIC_POST_EVENT_ALL_DAY}'"
+  end
+end
