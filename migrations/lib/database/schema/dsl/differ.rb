176ef51f82ca4e0031a43b5ab542a69a380df6a7
Gerhard Schlager <gerhard.schlager@discourse.org>
2026-03-12 21:47:31 +0100

    DEV: Convert schema DSL files from compact to nested module style
    Convert all 18 files in migrations/lib/database/schema/dsl/ from
    compact `module Migrations::Database::Schema::DSL` to nested module
    definitions. Shorten ~60 qualified references that are now resolvable
    via lexical nesting (e.g. ConfigError, Helpers, TableWriter).

--- migrations/lib/database/schema/dsl/differ.rb
+++ migrations/lib/database/schema/dsl/differ.rb
@@ -1,166 +1,175 @@
 # frozen_string_literal: true
 
-module Migrations::Database::Schema::DSL
-  TableInfo = Data.define(:name, :plugin)
-  ColumnInfo = Data.define(:name, :plugin)
-  TableDiff =
-    Data.define(
-      :table_name,
-      :unconfigured_columns,
-      :missing_columns,
-      :stale_ignored_columns,
-      :auto_ignored_columns,
-    )
-  DiffResult =
-    Data.define(:unconfigured_tables, :missing_tables, :stale_ignored_tables, :table_diffs)
+module Migrations
+  module Database
+    module Schema
+      module DSL
+        TableInfo = Data.define(:name, :plugin)
+        ColumnInfo = Data.define(:name, :plugin)
+        TableDiff =
+          Data.define(
+            :table_name,
+            :unconfigured_columns,
+            :missing_columns,
+            :stale_ignored_columns,
+            :auto_ignored_columns,
+          )
+        DiffResult =
+          Data.define(:unconfigured_tables, :missing_tables, :stale_ignored_tables, :table_diffs)
 
-  class Differ
-    def initialize(schema_module)
-      @schema = schema_module
-    end
+        class Differ
+          def initialize(schema_module)
+            @schema = schema_module
+          end
 
-    def diff
-      ActiveRecord::Base.with_connection do |connection|
-        @db = connection
-        @db_table_names = @db.tables.to_set
-        @scope = ColumnScope.new(@schema)
+          def diff
+            ActiveRecord::Base.with_connection do |connection|
+              @db = connection
+              @db_table_names = @db.tables.to_set
+              @scope = ColumnScope.new(@schema)
 
-        unconfigured_tables = find_unconfigured_tables
-        missing_tables = find_missing_tables
-        stale_ignored = find_stale_ignored_tables
-        table_diffs = find_table_diffs
+              unconfigured_tables = find_unconfigured_tables
+              missing_tables = find_missing_tables
+              stale_ignored = find_stale_ignored_tables
+              table_diffs = find_table_diffs
 
-        DiffResult.new(
-          unconfigured_tables:,
-          missing_tables:,
-          stale_ignored_tables: stale_ignored,
-          table_diffs:,
-        )
-      end
-    end
+              DiffResult.new(
+                unconfigured_tables:,
+                missing_tables:,
+                stale_ignored_tables: stale_ignored,
+                table_diffs:,
+              )
+            end
+          end
 
-    private
+          private
 
-    def find_unconfigured_tables
-      configured = @schema.tables.each_value.filter_map(&:source_table_name).to_set
-      ignored = @scope.ignored_table_name_set
+          def find_unconfigured_tables
+            configured = @schema.tables.each_value.filter_map(&:source_table_name).to_set
+            ignored = @scope.ignored_table_name_set
 
-      unconfigured = @db_table_names - configured - ignored
-      unconfigured.sort.map { |name| TableInfo.new(name:, plugin: plugin_for_table(name)) }
-    end
+            unconfigured = @db_table_names - configured - ignored
+            unconfigured.sort.map { |name| TableInfo.new(name:, plugin: plugin_for_table(name)) }
+          end
 
-    def find_missing_tables
-      missing = []
+          def find_missing_tables
+            missing = []
 
-      @schema.tables.each_value do |table_def|
-        next if table_def.source_table_name.nil?
+            @schema.tables.each_value do |table_def|
+              next if table_def.source_table_name.nil?
 
-        source = table_def.source_table_name
-        if @db_table_names.exclude?(source)
-          missing << TableInfo.new(name: table_def.name, plugin: nil)
+              source = table_def.source_table_name
+              if @db_table_names.exclude?(source)
+                missing << TableInfo.new(name: table_def.name, plugin: nil)
+              end
+            end
+
+            missing.sort_by(&:name)
+          end
+
+          def find_stale_ignored_tables
+            ignored = @schema.ignored_tables
+            return [] if ignored.nil?
+
+            stale = []
+            ignored.entries.each do |entry|
+              if @db_table_names.exclude?(entry.name)
+                stale << TableInfo.new(name: entry.name, plugin: nil)
+              end
+            end
+
+            stale.sort_by(&:name)
+          end
+
+          def find_table_diffs
+            diffs = []
+
+            @schema.tables.each_value do |table_def|
+              next if table_def.source_table_name.nil?
+
+              source = table_def.source_table_name
+              next if @db_table_names.exclude?(source)
+
+              table_diff = diff_table(table_def, source)
+              diffs << table_diff if table_diff
+            end
+
+            diffs.sort_by(&:table_name)
+          end
+
+          def diff_table(table_def, source_table)
+            @table_def = table_def
+            @db_column_names = @db.columns(source_table).map(&:name).to_set
+            @source_table = source_table
+
+            configured_columns = @scope.effective_column_names(@table_def, @db_column_names)
+
+            auto_ignored = find_auto_ignored_columns
+            unconfigured = find_unconfigured_columns(configured_columns, auto_ignored)
+            missing = find_missing_columns
+            stale = find_stale_ignored_columns
+
+            has_changes = unconfigured.any? || missing.any? || stale.any? || auto_ignored.any?
+            return nil if !has_changes
+
+            TableDiff.new(
+              table_name: @table_def.name,
+              unconfigured_columns: unconfigured,
+              missing_columns: missing,
+              stale_ignored_columns: stale,
+              auto_ignored_columns: auto_ignored,
+            )
+          end
+
+          def find_unconfigured_columns(configured_columns, auto_ignored)
+            ignored = @table_def.ignored_column_names.to_set
+            globally_ignored = @scope.globally_ignored_columns
+            auto_ignored_names = auto_ignored.map(&:name).to_set
+
+            unconfigured =
+              @db_column_names - configured_columns - ignored - globally_ignored -
+                auto_ignored_names
+            unconfigured.sort.map do |name|
+              ColumnInfo.new(name:, plugin: plugin_for_column(@source_table, name))
+            end
+          end
+
+          def find_missing_columns
+            return [] if @table_def.included_column_names.nil?
+
+            missing = @table_def.included_column_names.to_set - @db_column_names
+            missing.sort.map { |name| ColumnInfo.new(name:, plugin: nil) }
+          end
+
+          def find_stale_ignored_columns
+            stale = []
+            @table_def.ignored_columns_map.each_key do |col_name|
+              if @db_column_names.exclude?(col_name)
+                stale << ColumnInfo.new(name: col_name, plugin: nil)
+              end
+            end
+            stale.sort_by(&:name)
+          end
+
+          def find_auto_ignored_columns
+            auto_ignored = []
+            @scope.each_plugin_ignored_column(@table_def) do |col, plugin|
+              auto_ignored << ColumnInfo.new(name: col, plugin:)
+            end
+            auto_ignored.uniq(&:name).sort_by(&:name)
+          end
+
+          def plugin_for_table(name)
+            manifest = @schema.plugin_manifest
+            manifest.available? ? manifest.plugin_for_table(name) : nil
+          end
+
+          def plugin_for_column(table_name, col_name)
+            manifest = @schema.plugin_manifest
+            manifest.available? ? manifest.plugin_for_column(table_name, col_name) : nil
+          end
         end
       end
-
-      missing.sort_by(&:name)
-    end
-
-    def find_stale_ignored_tables
-      ignored = @schema.ignored_tables
-      return [] if ignored.nil?
-
-      stale = []
-      ignored.entries.each do |entry|
-        if @db_table_names.exclude?(entry.name)
-          stale << TableInfo.new(name: entry.name, plugin: nil)
-        end
-      end
-
-      stale.sort_by(&:name)
-    end
-
-    def find_table_diffs
-      diffs = []
-
-      @schema.tables.each_value do |table_def|
-        next if table_def.source_table_name.nil?
-
-        source = table_def.source_table_name
-        next if @db_table_names.exclude?(source)
-
-        table_diff = diff_table(table_def, source)
-        diffs << table_diff if table_diff
-      end
-
-      diffs.sort_by(&:table_name)
-    end
-
-    def diff_table(table_def, source_table)
-      @table_def = table_def
-      @db_column_names = @db.columns(source_table).map(&:name).to_set
-      @source_table = source_table
-
-      configured_columns = @scope.effective_column_names(@table_def, @db_column_names)
-
-      auto_ignored = find_auto_ignored_columns
-      unconfigured = find_unconfigured_columns(configured_columns, auto_ignored)
-      missing = find_missing_columns
-      stale = find_stale_ignored_columns
-
-      has_changes = unconfigured.any? || missing.any? || stale.any? || auto_ignored.any?
-      return nil if !has_changes
-
-      TableDiff.new(
-        table_name: @table_def.name,
-        unconfigured_columns: unconfigured,
-        missing_columns: missing,
-        stale_ignored_columns: stale,
-        auto_ignored_columns: auto_ignored,
-      )
-    end
-
-    def find_unconfigured_columns(configured_columns, auto_ignored)
-      ignored = @table_def.ignored_column_names.to_set
-      globally_ignored = @scope.globally_ignored_columns
-      auto_ignored_names = auto_ignored.map(&:name).to_set
-
-      unconfigured =
-        @db_column_names - configured_columns - ignored - globally_ignored - auto_ignored_names
-      unconfigured.sort.map do |name|
-        ColumnInfo.new(name:, plugin: plugin_for_column(@source_table, name))
-      end
-    end
-
-    def find_missing_columns
-      return [] if @table_def.included_column_names.nil?
-
-      missing = @table_def.included_column_names.to_set - @db_column_names
-      missing.sort.map { |name| ColumnInfo.new(name:, plugin: nil) }
-    end
-
-    def find_stale_ignored_columns
-      stale = []
-      @table_def.ignored_columns_map.each_key do |col_name|
-        stale << ColumnInfo.new(name: col_name, plugin: nil) if @db_column_names.exclude?(col_name)
-      end
-      stale.sort_by(&:name)
-    end
-
-    def find_auto_ignored_columns
-      auto_ignored = []
-      @scope.each_plugin_ignored_column(@table_def) do |col, plugin|
-        auto_ignored << ColumnInfo.new(name: col, plugin:)
-      end
-      auto_ignored.uniq(&:name).sort_by(&:name)
-    end
-
-    def plugin_for_table(name)
-      manifest = @schema.plugin_manifest
-      manifest.available? ? manifest.plugin_for_table(name) : nil
-    end
-
-    def plugin_for_column(table_name, col_name)
-      manifest = @schema.plugin_manifest
-      manifest.available? ? manifest.plugin_for_column(table_name, col_name) : nil
     end
   end
 end
--- migrations/lib/database/schema/dsl/plugin_introspector.rb
+++ migrations/lib/database/schema/dsl/plugin_introspector.rb
@@ -2,182 +2,191 @@
 
 require "digest/md5"
 
-module Migrations::Database::Schema::DSL
-  class PluginIntrospector
-    def self.compute_checksums(plugins_path)
-      discover_plugins(plugins_path).transform_values { |paths| checksum_for_paths(paths) }
-    end
-
-    private_class_method def self.checksum_for_paths(paths)
-      files =
-        paths.select { |p| File.directory?(p) }.flat_map { |p| Dir[File.join(p, "*.rb")].sort }.uniq
-
-      return "empty" if files.empty?
-
-      digests = files.map { |f| "#{File.basename(f)}:#{Digest::MD5.file(f).hexdigest}" }
-      Digest::MD5.hexdigest(digests.join("\n"))
-    end
-
-    def self.discover_plugins(plugins_path)
-      plugins = {}
-
-      Dir[File.join(plugins_path, "*")].sort.each do |plugin_dir|
-        next if !File.directory?(plugin_dir)
-
-        plugin_name = File.basename(plugin_dir)
-        paths = plugin_migration_paths(plugin_dir)
-        plugins[plugin_name] = paths if paths.any?
-      end
-
-      plugins
-    end
-
-    private_class_method def self.plugin_migration_paths(plugin_dir)
-      %w[db/migrate db/post_migrate]
-        .map { |sub| File.join(plugin_dir, sub) }
-        .select { |path| File.directory?(path) }
-    end
-
-    def initialize(plugins_path: nil)
-      @plugins_path = plugins_path || File.join(Rails.root, "plugins")
-    end
-
-    def introspect
-      with_temporary_database do |stderr|
-        run_core_migrations
-        load_plugin_rake_tasks
-
-        plugins = self.class.discover_plugins(@plugins_path)
-        plugin_data, failed_plugins = introspect_plugins(plugins, stderr)
-        checksums = self.class.compute_checksums(@plugins_path)
-
-        build_result(plugin_data, checksums, failed_plugins)
-      end
-    end
-
-    private
-
-    def with_temporary_database
-      original_config = ActiveRecord::Base.connection_db_config.configuration_hash.dup
-      db = TemporaryDb.new
-
-      suppress_output do |_stdout, stderr|
-        db.start
-
-        db.with_env do
-          ActiveRecord::Base.establish_connection(db.connection_hash)
-
-          yield stderr
-        end
-      ensure
-        ActiveRecord::Base.establish_connection(original_config)
-        db.stop
-        db.remove
-      end
-    end
-
-    def introspect_plugins(plugins, stderr)
-      plugin_data = {}
-      failed_plugins = []
-
-      plugins
-        .sort_by(&:first)
-        .each do |plugin_name, migration_paths|
-          result, failed = introspect_plugin(plugin_name, migration_paths, stderr)
-          if failed
-            failed_plugins << plugin_name
-            stderr.puts(
-              "  Warning: stopping plugin introspection after '#{plugin_name}' failed to avoid partial manifest data",
-            )
-            break
+module Migrations
+  module Database
+    module Schema
+      module DSL
+        class PluginIntrospector
+          def self.compute_checksums(plugins_path)
+            discover_plugins(plugins_path).transform_values { |paths| checksum_for_paths(paths) }
           end
 
-          plugin_data[plugin_name] = result if result
+          private_class_method def self.checksum_for_paths(paths)
+            files =
+              paths
+                .select { |p| File.directory?(p) }
+                .flat_map { |p| Dir[File.join(p, "*.rb")].sort }
+                .uniq
+
+            return "empty" if files.empty?
+
+            digests = files.map { |f| "#{File.basename(f)}:#{Digest::MD5.file(f).hexdigest}" }
+            Digest::MD5.hexdigest(digests.join("\n"))
+          end
+
+          def self.discover_plugins(plugins_path)
+            plugins = {}
+
+            Dir[File.join(plugins_path, "*")].sort.each do |plugin_dir|
+              next if !File.directory?(plugin_dir)
+
+              plugin_name = File.basename(plugin_dir)
+              paths = plugin_migration_paths(plugin_dir)
+              plugins[plugin_name] = paths if paths.any?
+            end
+
+            plugins
+          end
+
+          private_class_method def self.plugin_migration_paths(plugin_dir)
+            %w[db/migrate db/post_migrate]
+              .map { |sub| File.join(plugin_dir, sub) }
+              .select { |path| File.directory?(path) }
+          end
+
+          def initialize(plugins_path: nil)
+            @plugins_path = plugins_path || File.join(Rails.root, "plugins")
+          end
+
+          def introspect
+            with_temporary_database do |stderr|
+              run_core_migrations
+              load_plugin_rake_tasks
+
+              plugins = self.class.discover_plugins(@plugins_path)
+              plugin_data, failed_plugins = introspect_plugins(plugins, stderr)
+              checksums = self.class.compute_checksums(@plugins_path)
+
+              build_result(plugin_data, checksums, failed_plugins)
+            end
+          end
+
+          private
+
+          def with_temporary_database
+            original_config = ActiveRecord::Base.connection_db_config.configuration_hash.dup
+            db = TemporaryDb.new
+
+            suppress_output do |_stdout, stderr|
+              db.start
+
+              db.with_env do
+                ActiveRecord::Base.establish_connection(db.connection_hash)
+
+                yield stderr
+              end
+            ensure
+              ActiveRecord::Base.establish_connection(original_config)
+              db.stop
+              db.remove
+            end
+          end
+
+          def introspect_plugins(plugins, stderr)
+            plugin_data = {}
+            failed_plugins = []
+
+            plugins
+              .sort_by(&:first)
+              .each do |plugin_name, migration_paths|
+                result, failed = introspect_plugin(plugin_name, migration_paths, stderr)
+                if failed
+                  failed_plugins << plugin_name
+                  stderr.puts(
+                    "  Warning: stopping plugin introspection after '#{plugin_name}' failed to avoid partial manifest data",
+                  )
+                  break
+                end
+
+                plugin_data[plugin_name] = result if result
+              end
+
+            [plugin_data, failed_plugins]
+          end
+
+          def introspect_plugin(plugin_name, migration_paths, stderr)
+            snapshot_before = snapshot_schema
+
+            begin
+              run_plugin_migrations(migration_paths)
+            rescue StandardError => e
+              stderr.puts "  Warning: '#{plugin_name}' migration error: #{e.message}"
+              return nil, true
+            end
+
+            snapshot_after = snapshot_schema
+            [diff_schema(snapshot_before, snapshot_after), false]
+          end
+
+          def snapshot_schema
+            connection = ActiveRecord::Base.connection
+            tables = connection.tables.to_set
+            columns = {}
+
+            tables.each { |table| columns[table] = connection.columns(table).map(&:name).to_set }
+
+            { tables:, columns: }
+          end
+
+          def diff_schema(snapshot_before, snapshot_after)
+            new_tables = snapshot_after[:tables] - snapshot_before[:tables]
+
+            new_columns = {}
+            snapshot_after[:columns].each do |table, cols|
+              next if new_tables.include?(table)
+              added = cols - snapshot_before[:columns].fetch(table, Set.new)
+              new_columns[table] = added.sort if added.any?
+            end
+
+            return if new_tables.empty? && new_columns.empty?
+
+            { "tables" => new_tables.sort, "columns" => new_columns }
+          end
+
+          def build_result(plugin_data, checksums, failed_plugins)
+            {
+              "plugins" => plugin_data,
+              "plugin_checksums" => checksums,
+              "failed_plugins" => failed_plugins.sort,
+              "incomplete" => failed_plugins.any?,
+            }
+          end
+
+          def core_migration_paths
+            [File.join(Rails.root, "db", "migrate"), File.join(Rails.root, "db", "post_migrate")]
+          end
+
+          def run_core_migrations
+            paths = core_migration_paths.select { |p| File.directory?(p) }
+            ActiveRecord::MigrationContext.new(paths).migrate if paths.any?
+          end
+
+          # At least one plugin has a migration that invokes a rake task. Loading
+          # all plugin rake files ensures those tasks are defined before migrations
+          # run. The :environment task stub prevents errors from rake files that
+          # depend on it.
+          def load_plugin_rake_tasks
+            Rake::Task.define_task(:environment) if !Rake::Task.task_defined?(:environment)
+            Dir[File.join(@plugins_path, "*/lib/tasks/**/*.rake")].sort.each { |f| load f }
+          end
+
+          def run_plugin_migrations(paths)
+            valid_paths = paths.select { |p| File.directory?(p) }
+            ActiveRecord::MigrationContext.new(valid_paths).migrate if valid_paths.any?
+          end
+
+          def suppress_output
+            old_stdout = $stdout
+            old_stderr = $stderr
+            $stdout = StringIO.new
+            $stderr = StringIO.new
+            yield old_stdout, old_stderr
+          ensure
+            $stdout = old_stdout
+            $stderr = old_stderr
+          end
         end
-
-      [plugin_data, failed_plugins]
-    end
-
-    def introspect_plugin(plugin_name, migration_paths, stderr)
-      snapshot_before = snapshot_schema
-
-      begin
-        run_plugin_migrations(migration_paths)
-      rescue StandardError => e
-        stderr.puts "  Warning: '#{plugin_name}' migration error: #{e.message}"
-        return nil, true
       end
-
-      snapshot_after = snapshot_schema
-      [diff_schema(snapshot_before, snapshot_after), false]
-    end
-
-    def snapshot_schema
-      connection = ActiveRecord::Base.connection
-      tables = connection.tables.to_set
-      columns = {}
-
-      tables.each { |table| columns[table] = connection.columns(table).map(&:name).to_set }
-
-      { tables:, columns: }
-    end
-
-    def diff_schema(snapshot_before, snapshot_after)
-      new_tables = snapshot_after[:tables] - snapshot_before[:tables]
-
-      new_columns = {}
-      snapshot_after[:columns].each do |table, cols|
-        next if new_tables.include?(table)
-        added = cols - snapshot_before[:columns].fetch(table, Set.new)
-        new_columns[table] = added.sort if added.any?
-      end
-
-      return if new_tables.empty? && new_columns.empty?
-
-      { "tables" => new_tables.sort, "columns" => new_columns }
-    end
-
-    def build_result(plugin_data, checksums, failed_plugins)
-      {
-        "plugins" => plugin_data,
-        "plugin_checksums" => checksums,
-        "failed_plugins" => failed_plugins.sort,
-        "incomplete" => failed_plugins.any?,
-      }
-    end
-
-    def core_migration_paths
-      [File.join(Rails.root, "db", "migrate"), File.join(Rails.root, "db", "post_migrate")]
-    end
-
-    def run_core_migrations
-      paths = core_migration_paths.select { |p| File.directory?(p) }
-      ActiveRecord::MigrationContext.new(paths).migrate if paths.any?
-    end
-
-    # At least one plugin has a migration that invokes a rake task. Loading
-    # all plugin rake files ensures those tasks are defined before migrations
-    # run. The :environment task stub prevents errors from rake files that
-    # depend on it.
-    def load_plugin_rake_tasks
-      Rake::Task.define_task(:environment) if !Rake::Task.task_defined?(:environment)
-      Dir[File.join(@plugins_path, "*/lib/tasks/**/*.rake")].sort.each { |f| load f }
-    end
-
-    def run_plugin_migrations(paths)
-      valid_paths = paths.select { |p| File.directory?(p) }
-      ActiveRecord::MigrationContext.new(valid_paths).migrate if valid_paths.any?
-    end
-
-    def suppress_output
-      old_stdout = $stdout
-      old_stderr = $stderr
-      $stdout = StringIO.new
-      $stderr = StringIO.new
-      yield old_stdout, old_stderr
-    ensure
-      $stdout = old_stdout
-      $stderr = old_stderr
     end
   end
 end
