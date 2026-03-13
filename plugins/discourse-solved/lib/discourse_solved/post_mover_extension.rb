5652b3575a863554b3cec1763c0022cc3f2e525e
David Battersby <info@davidbattersby.com>
2026-03-13 14:35:55 +0400

    refactor to check for moving to solved topic

--- plugins/discourse-solved/lib/discourse_solved/post_mover_extension.rb
+++ plugins/discourse-solved/lib/discourse_solved/post_mover_extension.rb
@@ -4,20 +4,23 @@ module DiscourseSolved
   module PostMoverExtension
     private
 
-    # original_topic and post_ids are attr_readers on PostMover
+    # original_topic, post_ids, and user are attr_readers on PostMover
     def move_posts_to(topic)
       solved_topic = DiscourseSolved::SolvedTopic.find_by(topic_id: original_topic.id)
 
       if solved_topic && post_ids.include?(solved_topic.answer_post_id)
-        if DiscourseSolved::SolvedTopic.exists?(topic_id: topic.id)
+        accepts_answers = Guardian.new(user).allow_accepted_answers?(topic)
+        has_no_solution = !DiscourseSolved::SolvedTopic.exists?(topic_id: topic.id)
+
+        if accepts_answers && has_no_solution
+          solved_topic.update!(topic_id: topic.id)
+        else
           DiscourseSolved::UnacceptAnswer.call(
             params: {
               post_id: solved_topic.answer_post_id,
             },
             guardian: Discourse.system_user.guardian,
           )
-        else
-          solved_topic.update!(topic_id: topic.id)
         end
       end
 
