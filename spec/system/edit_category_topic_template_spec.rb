6bf12b78cb8cf1072eb880b9b42a354b65019fa2
Bannon Tanner <bannon@discourse.org>
2026-03-13 10:29:01 -0500

    wait for toast before checking for placeholder

--- spec/system/edit_category_topic_template_spec.rb
+++ spec/system/edit_category_topic_template_spec.rb
@@ -6,11 +6,14 @@ describe "Edit category topic template", type: :system do
 
   before { sign_in admin }
 
+  let(:toasts) { PageObjects::Components::Toasts.new }
+
   it "saves and displays a custom topic title placeholder" do
     visit "/c/#{category.slug}/edit/topic-template"
     find("#category-topic-title-placeholder").fill_in(with: "Describe your issue briefly")
     find("#save-category").click
 
+    expect(toasts).to have_success(I18n.t("js.saved"))
     expect(category.reload.topic_title_placeholder).to eq("Describe your issue briefly")
   end
 end
