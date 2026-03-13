e27a7a33d815434f0a2074ba469b670e286c47cc
David Taylor <david@taylorhq.com>
2026-03-13 17:40:01 +0000

    pretend I know what i'm doing

--- plugins/discourse-ai/db/migrate/20260306000001_rename_ai_personas_to_ai_agents.rb
+++ plugins/discourse-ai/db/migrate/20260306000001_rename_ai_personas_to_ai_agents.rb
@@ -2,11 +2,14 @@
 
 class RenameAiPersonasToAiAgents < ActiveRecord::Migration[7.2]
   def up
+    Migration::SafeMigrate.disable!
     rename_table :ai_personas, :ai_agents
 
     # Backwards-compat view will be dropped immediately after deploy
     # by FinalizeAiAgentsSchema post-deploy migration
     execute "CREATE VIEW ai_personas AS SELECT * FROM ai_agents"
+  ensure
+    Migration::SafeMigrate.enable!
   end
 
   def down
