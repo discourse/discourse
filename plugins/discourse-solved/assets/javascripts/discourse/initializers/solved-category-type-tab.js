1012851c14efa67f6721c2c0f63270a7b55cff5b
Martin Brennan <martin@discourse.org>
2026-03-17 09:54:00 +1000

    FEATURE: Gate Support category type creation behind upcoming change (#38571)
    Adds a new `enable_support_category_type_setup` upcoming change that
    will
    hide the support category type from the category setup page until the
    change is enabled.
    
    
    <img width="976" height="189" alt="image"
    src="https://github.com/user-attachments/assets/e562d912-9097-486c-bd3d-3d8b0598e93f"
    />
    
    This will allow us to fast-track moving the
    `enable_simplified_category_creation` change through the statuses,
    since a lot of work is behind it, while still being able to gradually
    roll out Support type and others to users.
    
    With this change, we introduce `.visible?` onto category types,
    which will be used to determine whether or not to show the type on the
    category setup page. By default, this will return true, but the Support
    type will override it to return false until the upcoming change is
    enabled. This is different from `available?` which still shows the
    type on the setup page, but gates the ability to use it behind other
    conditions on our hosting.
    
    When the /new-category/setup page is loaded and only one type is
    visible, then we just skip to the "New discussion category" page
    beyond.

--- plugins/discourse-solved/assets/javascripts/discourse/initializers/solved-category-type-tab.js
+++ plugins/discourse-solved/assets/javascripts/discourse/initializers/solved-category-type-tab.js
@@ -5,14 +5,17 @@ import UpsertCategorySupport from "../components/upsert-category-support";
 export default {
   name: "solved-category-type-tab",
 
-  initialize() {
+  initialize(container) {
+    const siteSettings = container.lookup("service:site-settings");
     withPluginApi((api) => {
       api.registerEditCategoryTab({
         id: "support",
         name: i18n("solved.category_type_support.title"),
         primary: true,
         component: UpsertCategorySupport,
-        condition: ({ category }) => category.isType("support"),
+        condition: ({ category }) =>
+          category.isType("support") &&
+          siteSettings.enable_support_category_type_setup,
       });
     });
   },
