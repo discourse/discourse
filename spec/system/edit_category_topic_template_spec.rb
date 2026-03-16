4212f6b9398a64903b22b54716d1e010621a0a20
Bannon Tanner <bannon@discourse.org>
2026-03-16 15:01:43 -0500

    update spec to account for both category edit flows

--- spec/system/edit_category_topic_template_spec.rb
+++ spec/system/edit_category_topic_template_spec.rb
@@ -6,11 +6,31 @@ describe "Edit category topic template", type: :system do
 
   before { sign_in admin }
 
-  it "saves and displays a custom topic title placeholder" do
-    visit "/c/#{category.slug}/edit/topic-template"
-    find("#category-topic-title-placeholder").fill_in(with: "Describe your issue briefly")
-    find("#save-category").click
+  let(:changes_banner) { PageObjects::Components::AdminChangesBanner.new }
 
-    expect(category.reload.topic_title_placeholder).to eq("Describe your issue briefly")
+  context "with simplified category creation" do
+    before { SiteSetting.enable_simplified_category_creation = true }
+
+    it "saves and displays a custom topic title placeholder" do
+      visit "/c/#{category.slug}/edit/topic-template"
+      find("#category-topic-title-placeholder").fill_in(with: "Describe your issue briefly")
+      expect(changes_banner).to be_visible
+      changes_banner.click_save
+
+      expect(category.reload.topic_title_placeholder).to eq("Describe your issue briefly")
+    end
+  end
+
+  context "with legacy category editing" do
+    it "saves and displays a custom topic title placeholder" do
+      visit "/c/#{category.slug}/edit/topic-template"
+      find("#category-topic-title-placeholder").fill_in(with: "Describe your issue briefly")
+
+      # sleep to account for debounce
+      sleep 0.6
+      find("#save-category").click
+
+      expect(category.reload.topic_title_placeholder).to eq("Describe your issue briefly")
+    end
   end
 end
