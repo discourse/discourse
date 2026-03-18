c8ccad3a06e265de55746072571f2ed040f7688d
Bannon Tanner <bannon@discourse.org>
2026-03-18 11:04:54 -0500

    use string directly for name on migration

--- plugins/discourse-calendar/db/migrate/20260311161955_add_topic_custom_field_all_day_index.rb
+++ plugins/discourse-calendar/db/migrate/20260311161955_add_topic_custom_field_all_day_index.rb
@@ -6,6 +6,6 @@ class AddTopicCustomFieldAllDayIndex < ActiveRecord::Migration[8.0]
               %i[name topic_id],
               name: :idx_topic_custom_fields_topic_post_event_all_day,
               unique: true,
-              where: "name = '#{DiscoursePostEvent::TOPIC_POST_EVENT_ALL_DAY}'"
+              where: "name = 'TopicEventAllDay'"
   end
 end
