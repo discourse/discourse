918a7884d7d9027aca2bee0a1e8eddcffebf2541
David Taylor <david@taylorhq.com>
2026-03-20 16:06:11 +0000

    make purpose of pseudo-random identifier clear

--- lib/asset_processor.rb
+++ lib/asset_processor.rb
@@ -25,10 +25,11 @@ class AssetProcessor
   end
 
   def self.append_es6_deprecation(content, file_path)
+    pseudo_random_identifier = "deprecated_gjYVqPLMxe" # Just needs to be unique enough to avoid collisions with real code
     <<~JS
       #{content}
-      import deprecated_gjYVqPLMxe from "discourse/lib/deprecated";
-      deprecated_gjYVqPLMxe(
+      import #{pseudo_random_identifier} from "discourse/lib/deprecated";
+      #{pseudo_random_identifier}(
         "The file '#{file_path}' uses the deprecated `.js.es6` extension. Use `.js` instead.",
         {
           id: "discourse.es6-extension",
