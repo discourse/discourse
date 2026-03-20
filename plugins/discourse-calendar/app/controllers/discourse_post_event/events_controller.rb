c6dc12207b0d7e4196a00a32d1e69bf8658d2a01
Rafael Silva <xfalcox@gmail.com>
2026-03-20 14:47:33 -0300

    FIX: Expand recurring events in ICS feed
    ICS feeds only rendered the next occurrence of recurring events.
    Now expands all occurrences up to 1 year out (max 200 per event)
    using the existing ExpandOccurrences service, matching what the
    JSON endpoint already does.
    
    Each occurrence gets a unique UID based on event ID + start timestamp.

--- plugins/discourse-calendar/app/controllers/discourse_post_event/events_controller.rb
+++ plugins/discourse-calendar/app/controllers/discourse_post_event/events_controller.rb
@@ -23,6 +23,25 @@ module DiscoursePostEvent
 
       respond_to do |format|
         format.ics do
+          after_time = filtered_events_params[:after]&.to_datetime || Time.current
+          before_time = filtered_events_params[:before]&.to_datetime || 1.year.from_now
+
+          @ics_events =
+            @events.flat_map do |event|
+              if event.recurring?
+                expanded =
+                  DiscoursePostEvent::Action::ExpandOccurrences.call(
+                    event: event,
+                    after: after_time,
+                    before: before_time,
+                    limit: 200,
+                  )
+                expanded[:occurrences].map { |occ| { event: event, **occ } }
+              else
+                [{ event: event, starts_at: event.starts_at, ends_at: event.ends_at }]
+              end
+            end
+
           filename = "events-#{Digest::SHA1.hexdigest(@events.map(&:id).sort.join("-"))}.ics"
           response.headers["Content-Disposition"] = "attachment; filename=\"#{filename}\""
         end
