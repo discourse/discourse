0ac62c9f288d20b58d7569a9eb5aba33b84fd586
David Taylor <david@taylorhq.com>
2026-03-23 15:11:19 +0000

    FIX: Handle boolean values in version check response
    Since our update to redis gem v5.x in March 2025, the CallDiscourseHub job has been raising an error because it fails to store `TrueClass`/`FalseClass` values which are returned from the API. This means that new-version emails were not sent.
    
    We don't have any need to store the `critical: true|false` values, so this commit slices the response to only include the strings we need. It also adds a spec which would've caught the error.

--- spec/jobs/call_discourse_hub_spec.rb
+++ spec/jobs/call_discourse_hub_spec.rb
@@ -12,5 +12,36 @@ RSpec.describe Jobs::CallDiscourseHub do
         described_class.new.execute({})
       end
     end
+
+    context "when version_checks enabled" do
+      before do
+        SiteSetting.version_checks = true
+        SiteSetting.include_in_discourse_discover = false
+      end
+
+      it "stores missing versions from the hub response" do
+        DiscourseHub.stubs(:discourse_version_check).returns(
+          {
+            "latestVersion" => "2026.3.1",
+            "criticalUpdates" => false,
+            "missingVersionsCount" => 2,
+            "versions" => [
+              { "version" => "2026.3.1", "notes" => "Latest release", "critical" => true },
+              { "version" => "2026.2.5", "notes" => "Security patch", "critical" => false },
+            ],
+          },
+        )
+
+        described_class.new.execute({})
+
+        expect(DiscourseUpdates.latest_version).to eq("2026.3.1")
+        expect(DiscourseUpdates.missing_versions_count).to eq(2)
+
+        missing = DiscourseUpdates.missing_versions
+        expect(missing.length).to eq(2)
+        expect(missing.first).to eq({ "version" => "2026.3.1", "notes" => "Latest release" })
+        expect(missing.second).to eq({ "version" => "2026.2.5", "notes" => "Security patch" })
+      end
+    end
   end
 end
