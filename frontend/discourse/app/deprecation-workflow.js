e7a0a69c05ee979a2baf5d0314190387420f22a6
David Taylor <david@taylorhq.com>
2026-03-25 14:01:43 +0000

    DEV: Drop deprecated `discourse.breadcrumbs.*` code (#38862)

--- frontend/discourse/app/deprecation-workflow.js
+++ frontend/discourse/app/deprecation-workflow.js
@@ -331,18 +331,6 @@ const workflows = [
   // CRITICAL DEPRECATIONS that should trigger admin warnings,
   // To keep warnings meaningful and prevent overflowing users with them,
   // we should only add values here after fixing core and official plugins
-  { handler: "notify-admin", matchId: "discourse.breadcrumbs.childCategories" },
-  { handler: "notify-admin", matchId: "discourse.breadcrumbs.firstCategory" },
-  {
-    handler: "notify-admin",
-    matchId: "discourse.breadcrumbs.parentCategories",
-  },
-  {
-    handler: "notify-admin",
-    matchId: "discourse.breadcrumbs.parentCategoriesSorted",
-  },
-  { handler: "notify-admin", matchId: "discourse.breadcrumbs.parentCategory" },
-  { handler: "notify-admin", matchId: "discourse.breadcrumbs.secondCategory" },
   {
     handler: "notify-admin",
     matchId: "discourse.category-hashtags.categoryHashtagTriggerRule",
