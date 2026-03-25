2eac80093df391a8bf617185683db36cc2745f09
Keegan George <kgeorge13@gmail.com>
2026-03-25 08:08:45 -0700

    UX: enhancements for embed mode (#38848)
    **This update adds a few enhancements to the experience of embed mode:**
    - auto open composer in full screen when no replies
    - clicking reply button opens full screen composer
    - forces rich text editor in composer

--- spec/system/embed_mode_spec.rb
+++ spec/system/embed_mode_spec.rb
@@ -35,4 +35,69 @@ describe "Embed mode" do
     expect(topic_page).to have_topic_title(topic.title)
     expect(page).to have_css("#post_1")
   end
+
+  context "when logged in" do
+    fab!(:user)
+    let(:composer) { PageObjects::Components::Composer.new }
+
+    before do
+      SiteSetting.rich_editor = true
+      sign_in(user)
+    end
+
+    context "with a topic that has no replies" do
+      fab!(:no_reply_topic, :topic)
+      fab!(:no_reply_post) { Fabricate(:post, topic: no_reply_topic) }
+
+      it "auto-opens the composer in fullscreen" do
+        visit("/t/#{no_reply_topic.slug}/#{no_reply_topic.id}?embed_mode=true")
+
+        expect(page).to have_css("#reply-control.fullscreen")
+      end
+
+      it "uses the rich text editor" do
+        visit("/t/#{no_reply_topic.slug}/#{no_reply_topic.id}?embed_mode=true")
+
+        expect(page).to have_css("#reply-control.fullscreen")
+        expect(composer).to have_rich_editor
+      end
+
+      it "does not auto-open the composer outside embed mode" do
+        visit("/t/#{no_reply_topic.slug}/#{no_reply_topic.id}")
+
+        expect(page).to have_no_css("#reply-control.open")
+        expect(page).to have_no_css("#reply-control.fullscreen")
+      end
+    end
+
+    context "with a topic that has replies" do
+      fab!(:reply) { Fabricate(:post, topic: topic) }
+
+      it "does not auto-open the composer" do
+        visit("/t/#{topic.slug}/#{topic.id}?embed_mode=true")
+
+        expect(page).to have_no_css("#reply-control.open")
+        expect(page).to have_no_css("#reply-control.fullscreen")
+      end
+
+      it "opens the composer in fullscreen when clicking reply" do
+        visit("/t/#{topic.slug}/#{topic.id}?embed_mode=true")
+        find("#topic-footer-buttons .btn-primary.create").click
+
+        expect(page).to have_css("#reply-control.fullscreen")
+      end
+    end
+  end
+
+  context "when not logged in" do
+    fab!(:no_reply_topic, :topic)
+    fab!(:no_reply_post) { Fabricate(:post, topic: no_reply_topic) }
+
+    it "does not auto-open the composer" do
+      visit("/t/#{no_reply_topic.slug}/#{no_reply_topic.id}?embed_mode=true")
+
+      expect(page).to have_no_css("#reply-control.open")
+      expect(page).to have_no_css("#reply-control.fullscreen")
+    end
+  end
 end
