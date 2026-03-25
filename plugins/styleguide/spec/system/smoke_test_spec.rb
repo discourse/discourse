8eac3f3a69c1afc4c5584a19fbf7a7fcc4435d50
Jarek Radosz <jarek@cvx.dev>
2026-03-25 12:08:25 +0100

    DEV: Re-enable chat and more-topics styleguide specs

--- plugins/styleguide/spec/system/smoke_test_spec.rb
+++ plugins/styleguide/spec/system/smoke_test_spec.rb
@@ -113,10 +113,6 @@ RSpec.describe "Styleguide Smoke Test" do
     sections.each do |section, items|
       items.each do |item|
         it "renders the #{section}: #{item[:title]} page correctly" do
-          # TODO: fix chat and more-topics pages
-          skip_pages = %w[/organisms/chat /organisms/more-topics]
-          skip "Skipping smoke test for #{item[:href]} page" if skip_pages.include?(item[:href])
-
           visit "/styleguide/#{item[:href]}"
 
           expect(page).to have_css(".styleguide-contents h1.section-title", text: item[:title])
