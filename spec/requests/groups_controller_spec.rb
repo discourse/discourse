309eb5e6b8fcc847b2edb4ebf1f3235db8f6fd92
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2026-03-25 03:39:38 +0000

    DEV: Loosen timestamp assertion in group membership pagination test

--- spec/requests/groups_controller_spec.rb
+++ spec/requests/groups_controller_spec.rb
@@ -753,7 +753,9 @@ RSpec.describe GroupsController do
       get "/groups/#{group.name}/members.json", params: { order: "added_at" }
       members = response.parsed_body["members"]
 
-      expect(members.last["added_at"]).to eq(first_user.created_at.as_json)
+      expect(Time.zone.parse(members.last["added_at"])).to be_within_one_second_of(
+        first_user.created_at,
+      )
     end
 
     it "can sort items" do
