c249dbb6d72ee07a69d8e41d4dafb10ce2f96331
Bannon Tanner <bannon@discourse.org>
2026-03-27 09:27:54 -0500

    DEV: rename group manager public methods

--- spec/lib/group_manager_spec.rb
+++ spec/lib/group_manager_spec.rb
@@ -7,33 +7,33 @@ RSpec.describe GroupManager do
 
   subject(:manager) { GroupManager.new(group) }
 
-  describe "#bulk_add" do
+  describe "#add" do
     it "adds users to group and returns added user IDs" do
-      result = manager.bulk_add([user.id, user2.id])
+      result = manager.add([user.id, user2.id])
 
       expect(result).to contain_exactly(user.id, user2.id)
       expect(group.group_users.map(&:user_id)).to contain_exactly(user.id, user2.id)
     end
 
     it "returns empty array for blank input" do
-      expect(manager.bulk_add([])).to eq([])
-      expect(manager.bulk_add(nil)).to eq([])
+      expect(manager.add([])).to eq([])
+      expect(manager.add(nil)).to eq([])
     end
   end
 
-  describe "#bulk_remove" do
-    before { manager.bulk_add([user.id, user2.id]) }
+  describe "#remove" do
+    before { manager.add([user.id, user2.id]) }
 
     it "removes users from group and returns removed user IDs" do
-      result = manager.bulk_remove([user.id, user2.id])
+      result = manager.remove([user.id, user2.id])
 
       expect(result).to contain_exactly(user.id, user2.id)
       expect(group.group_users.count).to eq(0)
     end
 
     it "returns empty array for blank input" do
-      expect(manager.bulk_remove([])).to eq([])
-      expect(manager.bulk_remove(nil)).to eq([])
+      expect(manager.remove([])).to eq([])
+      expect(manager.remove(nil)).to eq([])
     end
   end
 end
