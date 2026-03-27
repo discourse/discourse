97e89aaceb0de1b62fa5f53dc065a6e6f402b266
Jarek Radosz <jarek@cvx.dev>
2026-03-27 20:39:30 +0100

    DEV: Update fastimage to 2.4.1

--- lib/upload_creator.rb
+++ lib/upload_creator.rb
@@ -64,7 +64,9 @@ class UploadCreator
 
     @image_info =
       begin
-        FastImage.new(@file)
+        image = FastImage.new(@file)
+        image.type # eager load to rescue errors early
+        image
       rescue StandardError
         nil
       end
@@ -289,7 +291,9 @@ class UploadCreator
   def extract_image_info!
     @image_info =
       begin
-        FastImage.new(@file)
+        image = FastImage.new(@file)
+        image.type # eager load to rescue errors early
+        image
       rescue StandardError
         nil
       end
