7975f4fe1a5313f960727d9878defa13b2938251
David Taylor <david@taylorhq.com>
2026-03-31 12:54:44 +0100

    Refactor configs

--- frontend/discourse/rolldown.config.mjs
+++ frontend/discourse/rolldown.config.mjs
@@ -1,5 +1,10 @@
 import { ember } from "@embroider/vite";
-import { viteAliasPlugin, viteImportGlobPlugin } from "rolldown/experimental";
+import * as fs from "fs";
+import {
+  dev,
+  viteAliasPlugin,
+  viteImportGlobPlugin,
+} from "rolldown/experimental";
 import writeResolverConfig from "./lib/embroider-vite-resolver-options.mjs";
 import maybeBabel from "./lib/vite-maybe-babel.mjs";
 
@@ -52,95 +57,107 @@ const aliases = [
   },
 ];
 
-export default {
-  resolve: {
-    extensions,
-  },
-  experimental: {
-    incrementalBuild: true,
-  },
-  input: {
-    discourse: "discourse.js",
-    vendor: "vendor.js",
-    "start-discourse": "start-discourse.js",
-    "media-optimization-bundle": "media-optimization-bundle.js",
-    // ...(process.env.NODE_ENV !== "production" || process.env.FORCE_BUILD_TESTS
-    //   ? {
-    //       tests: "tests/index.html",
-    //       "tests/test-entrypoint": "tests/test-entrypoint.js",
-    //     }
-    //   : undefined),
-  },
-  output: {
-    minify: false,
-    dir: "dist",
-    sourcemap: true,
-    cleanDir: true,
-    hashCharacters: "base36",
-    assetFileNames: "assets/[name]-[hash].digested[extname]",
-    chunkFileNames: "assets/[name]-[hash].digested.js",
-    entryFileNames: "assets/[name]-[hash].digested.js",
-  },
-  watch: {
-    clearScreen: false,
-  },
-  preserveEntrySignatures: "strict",
-  plugins: [
-    viteAliasPlugin({ entries: aliases }),
-    ember(),
-    viteImportGlobPlugin(),
-    maybeBabel({
-      babelHelpers: "runtime",
+export function buildConfig({ devMode } = {}) {
+  return {
+    resolve: {
       extensions,
-      parallel: true,
-      skipPreflightCheck: true,
-    }),
-    {
-      name: "resolve-externals",
-      resolveId(source) {
-        if (
-          source.startsWith("/extra-locales/") ||
-          source.startsWith("/bootstrap/")
-        ) {
-          return { external: true, id: source };
-        }
-      },
     },
-    {
-      name: "css-loader",
-      load(id) {
-        if (id.endsWith(".css")) {
-          return {
-            code: "",
-            moduleType: "js",
-          };
-        }
-        return null;
-      },
+    experimental: {
+      incrementalBuild: true,
     },
-    {
-      name: "bundle-manifest",
-      generateBundle(_outputOptions, bundle) {
-        const manifest = {};
-
-        for (const [fileName, chunk] of Object.entries(bundle)) {
-          if (chunk.type === "chunk") {
-            manifest[`${chunk.name}.js`] = {
-              file: fileName,
-              name: chunk.name,
-              isEntry: chunk.isEntry,
-              isDynamicEntry: chunk.isDynamicEntry,
-              imports: chunk.imports,
+    input: {
+      discourse: "discourse.js",
+      vendor: "vendor.js",
+      "start-discourse": "start-discourse.js",
+      "media-optimization-bundle": "media-optimization-bundle.js",
+      // ...(process.env.NODE_ENV !== "production" || process.env.FORCE_BUILD_TESTS
+      //   ? {
+      //       tests: "tests/index.html",
+      //       "tests/test-entrypoint": "tests/test-entrypoint.js",
+      //     }
+      //   : undefined),
+    },
+    output: {
+      minify: false,
+      dir: "dist",
+      sourcemap: true,
+      cleanDir: !devMode,
+      hashCharacters: "base36",
+      assetFileNames: "assets/[name]-[hash].digested[extname]",
+      chunkFileNames: "assets/[name]-[hash].digested.js",
+      entryFileNames: "assets/[name]-[hash].digested.js",
+    },
+    watch: {
+      clearScreen: false,
+    },
+    preserveEntrySignatures: "strict",
+    plugins: [
+      viteAliasPlugin({ entries: aliases }),
+      ember(),
+      viteImportGlobPlugin(),
+      maybeBabel({
+        babelHelpers: "runtime",
+        extensions,
+        parallel: true,
+        skipPreflightCheck: true,
+      }),
+      {
+        name: "resolve-externals",
+        resolveId(source) {
+          if (
+            source.startsWith("/extra-locales/") ||
+            source.startsWith("/bootstrap/")
+          ) {
+            return { external: true, id: source };
+          }
+        },
+      },
+      {
+        name: "css-loader",
+        load(id) {
+          if (id.endsWith(".css")) {
+            return {
+              code: "",
+              moduleType: "js",
             };
           }
-        }
-
-        this.emitFile({
-          type: "asset",
-          fileName: "manifest.json",
-          source: JSON.stringify(manifest, null, 2),
-        });
+          return null;
+        },
       },
-    },
-  ],
-};
+      {
+        name: "bundle-manifest",
+        generateBundle(_outputOptions, bundle) {
+          const manifest = {};
+
+          for (const [fileName, chunk] of Object.entries(bundle)) {
+            if (chunk.type === "chunk") {
+              manifest[`${chunk.name}.js`] = {
+                file: fileName,
+                name: chunk.name,
+                isEntry: chunk.isEntry,
+                isDynamicEntry: chunk.isDynamicEntry,
+                imports: chunk.imports,
+              };
+            }
+          }
+
+          if (devMode) {
+            // Workaround rolldown devEngine bug?
+            fs.writeFileSync(
+              "./dist/manifest.json",
+              JSON.stringify(manifest, null, 2)
+            );
+          } else {
+            this.emitFile({
+              type: "asset",
+              fileName: "manifest.json",
+              source: JSON.stringify(manifest, null, 2),
+            });
+          }
+        },
+      },
+    ],
+  };
+}
+
+export default buildConfig({ devMode: false });
