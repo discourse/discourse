6c335d9b7c71b9c6a7b77a3eb36aa179392da13c
Jarek Radosz <jarek@cvx.dev>
2026-04-05 00:44:37 +0200

    --no-maglev

--- frontend/discourse/testem.js
+++ frontend/discourse/testem.js
@@ -254,6 +254,7 @@ module.exports = {
       "--window-size=1440,900",
       "--enable-precise-memory-info",
       "--js-flags=--max_old_space_size=4096",
+      "--no-maglev",
     ].filter(Boolean),
     Firefox: ["-headless", "--width=1440", "--height=900"],
   },
