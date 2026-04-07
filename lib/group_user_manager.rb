f3be9b6d16df190d7bf8f674b98fb1adf2b1ee13
Bannon Tanner <bannon@discourse.org>
2026-04-07 15:59:06 -0500

    DEV: move the guard to the method instead

--- lib/group_user_manager.rb
+++ lib/group_user_manager.rb
@@ -54,7 +54,7 @@ class GroupUserManager
   # :increase_group_user_count
   def sync_add_side_effects(added_user_ids)
     update_title(added_user_ids)
-    set_primary_group(added_user_ids) if @group.primary_group?
+    set_primary_group(added_user_ids)
     grant_trust_level(added_user_ids)
     GroupUser.bulk_set_category_notifications(@group, added_user_ids)
     GroupUser.bulk_set_tag_notifications(@group, added_user_ids)
@@ -200,6 +200,8 @@ class GroupUserManager
   end
 
   def set_primary_group(added_user_ids)
+    return unless @group.primary_group?
+
     DB.exec(<<~SQL, user_ids: added_user_ids, new_title: @group.title)
         UPDATE users u
         SET title = :new_title
