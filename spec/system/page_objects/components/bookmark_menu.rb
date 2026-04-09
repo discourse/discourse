b05107f2037ab3b379e54710cb1b354ffa9b7930
Sam Saffron <sam.saffron@gmail.com>
2026-04-09 11:09:30 +1000

    UX: add submenu for post bookmarks
    Split post bookmark actions into a dedicated submenu so topic footer
    bookmarks can show a clearer hierarchy and keep topic-level actions
    separate. This also preserves the topic bookmark button when only post
    bookmarks exist and updates the bookmark menu spacing and styling to
    match the new interaction.
    
    Minor fix sub menu alignment in chat channel submenu as well.

--- spec/system/page_objects/components/bookmark_menu.rb
+++ spec/system/page_objects/components/bookmark_menu.rb
@@ -31,18 +31,32 @@ module PageObjects
         has_css?(".topic-bookmarks-menu-content")
       end
 
-      def has_jump_to_post_option?(post_number)
+      def has_post_bookmark_option?(post_number)
         has_css?(
-          ".topic-bookmarks-menu-content .bookmark-menu__row[data-menu-option-id='jump']",
-          text: "##{post_number}",
+          ".topic-bookmarks-menu-content .bookmark-menu__row[data-menu-option-id='post-#{post_number}']",
         )
       end
 
-      def click_jump_to_post(post_number)
+      def click_post_bookmark(post_number)
         find(
-          ".topic-bookmarks-menu-content .bookmark-menu__row[data-menu-option-id='jump']",
-          text: "##{post_number}",
+          ".topic-bookmarks-menu-content .bookmark-menu__row[data-menu-option-id='post-#{post_number}']",
         ).click
+      end
+
+      def has_post_submenu?
+        has_css?(".topic-bookmark-post-submenu")
+      end
+
+      def click_post_submenu_option(option_id)
+        find(
+          ".topic-bookmark-post-submenu .bookmark-menu__row[data-menu-option-id='#{option_id}'] .btn",
+        ).click
+      end
+
+      def has_bookmark_topic_option?
+        has_css?(
+          ".topic-bookmarks-menu-content .bookmark-menu__row[data-menu-option-id='bookmark-topic']",
+        )
       end
 
       def has_clear_all_option?
