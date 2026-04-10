ee05514e291d26cdb49243fbbe9365585b58a3ed
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2026-04-10 19:30:31 +0200

    refactor(workflows): remove pure-delegation job specs

--- plugins/discourse-workflows/spec/jobs/scheduled/discourse_workflows/check_stale_topics_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Jobs::DiscourseWorkflows::CheckStaleTopics do
-  it "calls the Execution::CheckStaleTopics service" do
-    DiscourseWorkflows::Execution::CheckStaleTopics.expects(:call)
-
-    described_class.new.execute(nil)
-  end
-end
--- plugins/discourse-workflows/spec/jobs/scheduled/discourse_workflows/expire_waiting_executions_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Jobs::DiscourseWorkflows::ExpireWaitingExecutions do
-  it "calls the Execution::ExpireWaiting service" do
-    DiscourseWorkflows::Execution::ExpireWaiting.expects(:call)
-
-    described_class.new.execute({})
-  end
-end
