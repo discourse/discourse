40dab427fac918c084dd4af662ef3f405ea41b95
awesomerobot <kris.aubuchon@discourse.org>
2026-04-13 14:20:57 -0400

    FEATURE: restyle topic voting component, add to docked header

--- plugins/discourse-topic-voting/spec/system/page_objects/pages/topic.rb
+++ plugins/discourse-topic-voting/spec/system/page_objects/pages/topic.rb
@@ -4,50 +4,43 @@ module TopicVotingTopic
   include ::RSpec::Matchers
 
   def vote_count
-    find(".voting .vote-count")
-  end
-
-  def has_locked_popup?
-    expected_html = I18n.t("js.topic_voting.locked_description")
-    selector = ".topic-voting-menu__title.--locked"
-    has_css?(selector)
-    actual_html = find(selector)[:innerHTML].strip
-    actual_html.include?(expected_html)
+    find(".title-voting .voting-wrapper__count-text")
   end
 
   def vote_popup
-    find(".topic-voting-menu__votes-left")
+    find(".see-votes")
   end
 
   def vote
-    find("button.vote-button").click
+    find(".title-voting button.voting-wrapper__button").click
     self
   end
 
   def remove_vote
-    vote
-    find("button.remove-vote").click
+    if SiteSetting.topic_voting_enable_vote_limits
+      vote
+      find("button.remove-vote").click
+    else
+      find(".title-voting button.voting-wrapper__button").click
+    end
     self
   end
 
   def click_my_votes
-    find(".topic-voting-menu__votes-left").click
-  end
-
-  def has_vote_button_label?(text)
-    has_css?("button.vote-button", text: text)
+    find(".see-votes").click
   end
 
   def has_no_remove_vote_button?
     has_no_css?("button.remove-vote")
   end
 
-  def has_see_all_votes_link?
-    has_css?(".see-votes", text: I18n.t("js.topic_voting.see_all_votes"))
+  def has_voted?
+    has_css?(".title-voting button.voting-wrapper__button.btn-success")
   end
 
-  def has_no_votes_left_text?
-    has_no_css?(".see-votes", text: %r{\d+/\d+})
+  def has_not_voted?
+    has_css?(".title-voting button.voting-wrapper__button.btn-default") &&
+      has_no_css?(".title-voting button.voting-wrapper__button.btn-success")
   end
 end
 
