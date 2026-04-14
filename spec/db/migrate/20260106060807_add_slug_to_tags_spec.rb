6975914a3d09ecdf08439da324f2db3d1c0fcace
Jarek Radosz <jarek@cvx.dev>
2026-04-14 10:28:37 +0200

    suppress_messages

--- spec/db/migrate/20260106060807_add_slug_to_tags_spec.rb
+++ spec/db/migrate/20260106060807_add_slug_to_tags_spec.rb
@@ -3,12 +3,7 @@
 require Rails.root.join("db/migrate/20260106060807_add_slug_to_tags.rb")
 
 describe AddSlugToTags do
-  before do
-    @original_verbose = ActiveRecord::Migration.verbose
-    ActiveRecord::Migration.verbose = false
-  end
-
-  after { ActiveRecord::Migration.verbose = @original_verbose }
+  around { |example| ActiveRecord::Migration.suppress_messages { example.run } }
 
   def reset_slug_column
     DB.exec("DROP INDEX IF EXISTS index_tags_on_lower_slug")
