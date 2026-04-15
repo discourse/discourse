discourse/discourse

commit 39d09cf59b8e4749af3b1894c5e4c2d6270854e3

DEV: Improve sidekiq parent monitoring (#39230)
Fixes (hopefully) the infinite loops of:

```
Failed to report error: can't alloc thread 3 Job exception: can't alloc thread sidekiq-exception
```

…that sometimes were happening after a local rails server restart.

lib/demon/base.rb
@@ -225,18 +225,30 @@ class Demon::Base
 
   def monitor_parent
     Thread.new do
-      while true
-        begin
-          unless alive?(@parent_pid)
-            Process.kill "TERM", Process.pid
-            sleep 10
-            Process.kill "KILL", Process.pid
-          end
-        rescue => e
-          log("URGENT monitoring thread had an exception #{e}", level: :error)
-        end
+      loop do
+        monitor_parent_tick
         sleep 1
       end
+    end
+  end
+
+  def monitor_parent_tick
+    if !alive?(@parent_pid)
+      Process.kill "TERM", Process.pid
+      sleep 10
+      Process.kill "KILL", Process.pid
+    end
+  rescue Exception => e
+    log_error(e)
+  end
+
+  def log_error(e)
+    log("URGENT monitoring thread had an exception #{e.class}: #{e.message}", level: :error)
+  rescue Exception
+    # Fall back to STDERR if the logger is broken
+    begin
+      $stderr.puts("[#{self.class}##{Process.pid}] monitor-parent: #{e.class}: #{e.message}")
+    rescue StandardError
     end
   end
 

commit 0fd7e20c4eab7462c552136540e1459b9029b757

DEV: Fix sidekiq subprocess termination
Regressed in 39d09cf59b8e4749af3b1894c5e4c2d6270854e3

It seems that rescuing `Exception` here was preventing termination of subprocesses… Just rescuing `Error` when logging should be enough to avoid the original issue.

lib/demon/base.rb
@@ -238,16 +238,19 @@ class Demon::Base
       sleep 10
       Process.kill "KILL", Process.pid
     end
-  rescue Exception => e
+  rescue => e
     log_error(e)
   end
 
   def log_error(e)
     log("URGENT monitoring thread had an exception #{e.class}: #{e.message}", level: :error)
-  rescue Exception
+  rescue => log_err
     # Fall back to STDERR if the logger is broken
     begin
-      $stderr.puts("[#{self.class}##{Process.pid}] monitor-parent: #{e.class}: #{e.message}")
+      $stderr.puts(
+        "[#{self.class}##{Process.pid}] monitor-parent: #{e.class}: #{e.message} " \
+          "(logger also failed: #{log_err.class}: #{log_err.message})",
+      )
     rescue StandardError
     end
   end