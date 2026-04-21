d266b8ef136553dbd3ec39f228e99370a09e67d9
David Battersby <info@davidbattersby.com>
2026-04-21 11:33:34 +0400

    cleanup

--- plugins/discourse-reactions/lib/discourse_reactions/post_reactions_query.rb
+++ plugins/discourse-reactions/lib/discourse_reactions/post_reactions_query.rb
@@ -3,7 +3,8 @@
 module DiscourseReactions
   class PostReactionsQuery
     def self.call(post:, reaction_filter: nil, limit: 30, offset: 0)
-      new(post: post, reaction_filter: reaction_filter, limit: limit, offset: offset).call
+      query = new(post: post, reaction_filter: reaction_filter, limit: limit, offset: offset)
+      [query.rows, query.total]
     end
 
     def initialize(post:, reaction_filter: nil, limit: 30, offset: 0)
@@ -13,8 +14,51 @@ module DiscourseReactions
       @offset = offset
     end
 
-    def call
-      [rows, total]
+    def rows
+      @rows ||=
+        if specific_reaction?
+          DB.query(<<~SQL, **bindings, reaction_filter: reaction_filter)
+            #{reactions_select_sql}
+            AND dr.reaction_value = :reaction_filter
+            ORDER BY drru.created_at ASC
+            LIMIT :limit OFFSET :offset
+          SQL
+        elsif main_reaction_filter?
+          DB.query(<<~SQL, **bindings)
+            #{plain_likes_select_sql}
+            ORDER BY post_actions.created_at ASC
+            LIMIT :limit OFFSET :offset
+          SQL
+        else
+          DB.query(<<~SQL, **bindings)
+            SELECT * FROM (
+              #{reactions_select_sql}
+              UNION ALL
+              #{plain_likes_select_sql}
+            ) combined
+            ORDER BY created_at ASC
+            LIMIT :limit OFFSET :offset
+          SQL
+        end
+    end
+
+    def total
+      @total ||=
+        if specific_reaction?
+          ReactionUser
+            .joins(:reaction)
+            .where(
+              post_id: post.id,
+              discourse_reactions_reactions: {
+                reaction_value: reaction_filter,
+              },
+            )
+            .count
+        elsif main_reaction_filter?
+          plain_likes_scope.count
+        else
+          ReactionUser.where(post_id: post.id).count + plain_likes_scope.count
+        end
     end
 
     private
@@ -22,11 +66,15 @@ module DiscourseReactions
     attr_reader :post, :reaction_filter, :limit, :offset
 
     def main_reaction
-      @main_reaction ||= DiscourseReactions::Reaction.main_reaction_id
+      @main_reaction ||= Reaction.main_reaction_id
     end
 
     def like_type
       PostActionType::LIKE_POST_ACTION_ID
+    end
+
+    def shadow_like_filter
+      PostActionExtension.strict_filter_reaction_likes_sql
     end
 
     def specific_reaction?
@@ -47,56 +95,8 @@ module DiscourseReactions
       }
     end
 
-    def total
-      if specific_reaction?
-        DiscourseReactions::ReactionUser
-          .joins(:reaction)
-          .where(
-            post_id: post.id,
-            discourse_reactions_reactions: {
-              reaction_value: reaction_filter,
-            },
-          )
-          .count
-      elsif main_reaction_filter?
-        plain_likes_scope.count
-      else
-        DiscourseReactions::ReactionUser.where(post_id: post.id).count + plain_likes_scope.count
-      end
-    end
-
-    def rows
-      if specific_reaction?
-        DB.query(<<~SQL, **bindings, reaction_filter: reaction_filter)
-          #{reactions_select_sql}
-          AND dr.reaction_value = :reaction_filter
-          ORDER BY drru.created_at ASC
-          LIMIT :limit OFFSET :offset
-        SQL
-      elsif main_reaction_filter?
-        DB.query(<<~SQL, **bindings)
-          #{plain_likes_select_sql}
-          ORDER BY post_actions.created_at ASC
-          LIMIT :limit OFFSET :offset
-        SQL
-      else
-        DB.query(<<~SQL, **bindings)
-          SELECT * FROM (
-            #{reactions_select_sql}
-            UNION ALL
-            #{plain_likes_select_sql}
-          ) combined
-          ORDER BY created_at ASC
-          LIMIT :limit OFFSET :offset
-        SQL
-      end
-    end
-
     def plain_likes_scope
-      PostAction.where(post_id: post.id).where(
-        DiscourseReactions::PostActionExtension.strict_filter_reaction_likes_sql,
-        like: like_type,
-      )
+      PostAction.where(post_id: post.id).where(shadow_like_filter, like: like_type)
     end
 
     def reactions_select_sql
@@ -117,7 +117,7 @@ module DiscourseReactions
         FROM post_actions
         INNER JOIN users u ON u.id = post_actions.user_id
         WHERE post_actions.post_id = :post_id
-          AND (#{DiscourseReactions::PostActionExtension.strict_filter_reaction_likes_sql})
+          AND (#{shadow_like_filter})
       SQL
     end
   end
