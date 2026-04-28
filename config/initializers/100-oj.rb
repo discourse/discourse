9d31edf359607cb8d80b87470cff70cb3d294a6d
Jarek Radosz <jarek@cvx.dev>
2026-04-29 01:55:45 +0200

    DEV: Update multi_json to 1.20.1

--- config/initializers/100-oj.rb
+++ config/initializers/100-oj.rb
@@ -5,5 +5,4 @@ Oj::Rails.set_decoder()
 Oj::Rails.optimize()
 Oj.default_options = Oj.default_options.merge(mode: :compat)
 
-# Not sure why it's not using this by default!
-MultiJson.engine = :oj
+MultiJson.use = :oj
