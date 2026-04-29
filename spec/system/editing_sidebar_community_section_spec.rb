f8974f9a76a793e149577dd95741ec419e09eb4e
Jarek Radosz <jarek@cvx.dev>
2026-04-29 15:33:58 +0200

    DEV: Restore and fix a sidebar section spec

--- spec/system/editing_sidebar_community_section_spec.rb
+++ spec/system/editing_sidebar_community_section_spec.rb
@@ -17,29 +17,29 @@ RSpec.describe "Editing Sidebar Community Section" do
     expect(sidebar).to have_no_customize_community_section_button
   end
 
-  xit "allows admin to edit community section and reset to default" do
+  it "allows admin to edit community section and reset to default" do
     sign_in(admin)
 
     visit("/latest")
 
     expect(sidebar.primary_section_icons("community")).to eq(
-      %w[layer-group user flag wrench paper-plane ellipsis-vertical],
+      %w[layer-group user inbox flag wrench paper-plane ellipsis-vertical],
     )
 
     modal = sidebar.click_community_section_more_button.click_customize_community_section_button
     modal.fill_link("Topics", "/latest", "paper-plane")
-    modal.topics_link.drag_to(modal.review_link, delay: 0.4)
+    modal.topics_link.drag_to(modal.review_link, html5: true, delay: 0.4)
     modal.save
     modal.confirm_update
 
     page.refresh
 
     expect(sidebar.primary_section_links("community")).to eq(
-      ["My posts", "Topics", "Review", "Admin", "Invite", "More"],
+      ["My posts", "My messages", "Topics", "Review", "Admin", "Invite", "More"],
     )
 
     expect(sidebar.primary_section_icons("community")).to eq(
-      %w[user paper-plane flag wrench paper-plane ellipsis-vertical],
+      %w[user inbox paper-plane flag wrench paper-plane ellipsis-vertical],
     )
 
     modal = sidebar.click_community_section_more_button.click_customize_community_section_button
@@ -48,11 +48,11 @@ RSpec.describe "Editing Sidebar Community Section" do
     expect(sidebar).to have_section("Community")
 
     expect(sidebar.primary_section_links("community")).to eq(
-      ["Topics", "My posts", "Review", "Admin", "Invite", "More"],
+      ["Topics", "My posts", "My messages", "Review", "Admin", "Invite", "More"],
     )
 
     expect(sidebar.primary_section_icons("community")).to eq(
-      %w[layer-group user flag wrench paper-plane ellipsis-vertical],
+      %w[layer-group user inbox flag wrench paper-plane ellipsis-vertical],
     )
   end
 
