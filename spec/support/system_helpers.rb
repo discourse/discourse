04540ddedd24365b7b1f296302d75d2349c2376c
Jarek Radosz <jarek@cvx.dev>
2026-04-30 17:03:04 +0200

    refresh too

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -54,11 +54,9 @@ module SystemHelpers
     self
   end
 
-  # Waits for the Ember app to boot after `visit`.
+  # Waits for the Ember app to boot before continuing.
   def visit(...)
     super
-
-    # `<discourse-assets>` doesn't appear on non-ember pages
     return if page.has_no_css?("discourse-assets", wait: 0, visible: :all)
 
     page.assert_selector("#main.ember-application", visible: :all)
@@ -417,3 +415,16 @@ module SystemHelpers
     Nokogiri.HTML5(html_translation).at("body").inner_text
   end
 end
+
+module CapybaraSessionEmberWaiter
+  # Waits for the Ember app to boot before continuing.
+  def refresh
+    super
+    return unless RSpec.current_example&.metadata&.[](:type) == :system
+    return if has_no_css?("discourse-assets", wait: 0, visible: :all)
+
+    assert_selector("#main.ember-application", visible: :all)
+  end
+end
+
+Capybara::Session.prepend(CapybaraSessionEmberWaiter)
