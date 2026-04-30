20531d1c27051e681d45c45ba5f5dc5eb136ad5d
David Battersby <info@davidbattersby.com>
2026-04-30 14:25:37 +0400

    update core annotations

--- plugins/discourse-solved/app/models/discourse_solved/me_too.rb
+++ plugins/discourse-solved/app/models/discourse_solved/me_too.rb
@@ -17,13 +17,13 @@ end
 # Table name: discourse_solved_me_toos
 #
 #  id         :bigint           not null, primary key
-#  topic_id   :integer          not null
-#  user_id    :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  topic_id   :integer          not null
+#  user_id    :integer          not null
 #
 # Indexes
 #
-#  index_discourse_solved_me_toos_on_topic_id               (topic_id)
-#  index_discourse_solved_me_toos_on_topic_id_and_user_id   (topic_id,user_id) UNIQUE
+#  index_discourse_solved_me_toos_on_topic_id              (topic_id)
+#  index_discourse_solved_me_toos_on_topic_id_and_user_id  (topic_id,user_id) UNIQUE
 #
