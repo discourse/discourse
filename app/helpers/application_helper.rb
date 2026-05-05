9d08817afec34fc31fee84b2954091f267f953be
awesomerobot <kris.aubuchon@discourse.org>
2026-05-05 12:45:34 -0400

    DEV: remove splash_screen site setting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -529,8 +529,7 @@ module ApplicationHelper
   end
 
   def include_splash_screen?
-    # A bit basic for now but will be expanded later
-    SiteSetting.splash_screen
+    !Rails.env.test?
   end
 
   def custom_splash_screen_enabled?
