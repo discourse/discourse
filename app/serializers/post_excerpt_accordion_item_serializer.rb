06badbe62a1f35b6ee33fa71a54bf076d6e243aa
Chris Alberti <christo@discourse.org>
2026-05-06 12:21:46 -0500

    fix

--- app/serializers/post_excerpt_accordion_item_serializer.rb
+++ app/serializers/post_excerpt_accordion_item_serializer.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class PostExcerptAccordionItemSerializer < BasicPostSerializer
-  attributes :post_number,
-             :topic_id,
-             :post_url,
-             def post_url
-               object&.url
-             end
+  attributes :post_number, :topic_id, :post_url
+
+  def post_url
+    object&.url
+  end
 end
