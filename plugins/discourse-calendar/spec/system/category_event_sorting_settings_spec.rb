5682636514091e348c460cdf1c9b4da84bcec3ec
Martin Brennan <martin@discourse.org>
2026-05-06 11:26:57 +1000

    DEV: Spec fixes

--- plugins/discourse-calendar/spec/system/category_event_sorting_settings_spec.rb
+++ plugins/discourse-calendar/spec/system/category_event_sorting_settings_spec.rb
@@ -16,37 +16,16 @@ RSpec.describe "Event Sorting Category Settings" do
     sign_in(admin)
   end
 
-  context "when simplified category creation is enabled" do
-    before { SiteSetting.enable_simplified_category_creation = true }
+  it "can toggle event sorting custom fields via FormKit" do
+    category_page.visit_settings(category)
 
-    it "can toggle event sorting custom fields via FormKit" do
-      category_page.visit_settings(category)
+    form.field("custom_fields.sort_topics_by_event_start_date").toggle
+    form.field("custom_fields.disable_topic_resorting").toggle
+    banner.click_save
 
-      form.field("custom_fields.sort_topics_by_event_start_date").toggle
-      form.field("custom_fields.disable_topic_resorting").toggle
-      banner.click_save
-
-      expect(toasts).to have_success(I18n.t("js.saved"))
-      category.reload
-      expect(category.custom_fields["sort_topics_by_event_start_date"]).to eq(true)
-      expect(category.custom_fields["disable_topic_resorting"]).to eq(true)
-    end
-  end
-
-  context "when simplified category creation is disabled" do
-    before { SiteSetting.enable_simplified_category_creation = false }
-
-    it "can toggle event sorting custom fields via legacy form" do
-      category_page.visit_settings(category)
-
-      find("#sort-topics-by-event-start-date").click
-      find("#disable-topic-resorting").click
-      category_page.save_settings
-
-      expect(toasts).to have_success(I18n.t("js.saved"))
-      category.reload
-      expect(category.custom_fields["sort_topics_by_event_start_date"]).to eq(true)
-      expect(category.custom_fields["disable_topic_resorting"]).to eq(true)
-    end
+    expect(toasts).to have_success(I18n.t("js.saved"))
+    category.reload
+    expect(category.custom_fields["sort_topics_by_event_start_date"]).to eq(true)
+    expect(category.custom_fields["disable_topic_resorting"]).to eq(true)
   end
 end
