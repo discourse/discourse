5c4fbfb6b93b4319614124b81390750d4cdb61ba
Sam <sam.saffron@gmail.com>
2026-05-07 15:14:14 +1000

    FIX: Unescaped username regex lets self-service rename rewrite other users' mentions

--- spec/jobs/update_username_spec.rb
+++ spec/jobs/update_username_spec.rb
@@ -16,4 +16,29 @@ RSpec.describe Jobs::UpdateUsername do
 
     expect(events).to eq([])
   end
+
+  it "does not rewrite similar mentions when the old username contains a dot" do
+    renamed_user = Fabricate(:user, username: "foo.bar")
+    Fabricate(:user, username: "foo-bar")
+    author = Fabricate(:user)
+    topic = Fabricate(:topic, user: author)
+
+    Jobs.run_immediately!
+    UserActionManager.enable
+    post = create_post(user: author, topic: topic, raw: "@foo.bar @foo-bar")
+
+    described_class.new.execute(
+      user_id: renamed_user.id,
+      old_username: renamed_user.username,
+      new_username: "newname",
+      avatar_template: renamed_user.avatar_template,
+    )
+
+    post.reload
+
+    expect(post.raw).to eq("@newname @foo-bar")
+    expect(post.cooked).to match_html <<~HTML
+      <p><a class="mention" href="/u/newname">@newname</a> <a class="mention" href="/u/foo-bar">@foo-bar</a></p>
+    HTML
+  end
 end
