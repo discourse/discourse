6d1dbe22334747fcd582ef65229dc54e326b6980
Penar Musaraj <pmusaraj@gmail.com>
2026-05-08 15:25:06 -0400

    custom homepage redirect draft

--- frontend/discourse/app/routes/discovery/custom.js
+++ frontend/discourse/app/routes/discovery/custom.js
@@ -1,10 +1,17 @@
 import { applyBehaviorTransformer } from "discourse/lib/transformer";
+import { defaultHomepage } from "discourse/lib/utilities";
 import DiscourseRoute from "discourse/routes/discourse";
 
 export default class DiscoveryCustomRoute extends DiscourseRoute {
   queryParams = {
     q: { refreshModel: true },
   };
+
+  beforeModel() {
+    if (defaultHomepage() !== "custom") {
+      return this.replaceWith("discovery.index");
+    }
+  }
 
   model(data) {
     return applyBehaviorTransformer("custom-homepage-model", () => null, {
