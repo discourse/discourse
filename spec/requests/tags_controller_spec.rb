ae881acc7e4898fbf0474322244e5677a37306ee
Sam <sam.saffron@gmail.com>
2026-05-11 15:47:26 +1000

    FIX: Tag settings leaks hidden tag metadata to non-staff tag editors

--- spec/requests/tags_controller_spec.rb
+++ spec/requests/tags_controller_spec.rb
@@ -860,6 +860,32 @@ RSpec.describe TagsController do
         expect(settings["can_edit"]).to eq(true)
         expect(settings["can_admin"]).to eq(false)
       end
+
+      it "does not leak hidden synonyms or tag groups to non-staff tag editors" do
+        SiteSetting.tags_listed_by_group = true
+        hidden_synonym = Fabricate(:tag, name: "hidden-synonym", target_tag: tag)
+        visible_tag_group = Fabricate(:tag_group, name: "Visible Tag Group", tags: [tag])
+        Fabricate(
+          :tag_group,
+          name: "Hidden Synonym Group",
+          permissions: {
+            "staff" => 1,
+          },
+          tags: [hidden_synonym],
+        )
+        Fabricate(:tag_group, name: "Hidden Tag Group", permissions: { "staff" => 1 }, tags: [tag])
+
+        sign_in(regular_user)
+        get "/tag/#{tag.id}/settings.json"
+        expect(response.status).to eq(200)
+
+        settings = response.parsed_body["tag_settings"]
+        expect(settings["synonyms"].map { |s| s["name"] }).to contain_exactly(synonym.name)
+        expect(settings["tag_group_names"]).to contain_exactly(visible_tag_group.name)
+        expect(settings["tag_groups"].map { |tg| tg["name"] }).to contain_exactly(
+          visible_tag_group.name,
+        )
+      end
     end
   end
 
