68c3c65e8fb46e500a63f0601751eb47b35f8bec
Sam <sam.saffron@gmail.com>
2026-05-11 15:55:34 +1000

    FIX: Screened email list exposes IP addresses to moderators without IP-view permission

--- spec/requests/admin/screened_emails_controller_spec.rb
+++ spec/requests/admin/screened_emails_controller_spec.rb
@@ -30,6 +30,15 @@ RSpec.describe Admin::ScreenedEmailsController do
       end
 
       include_examples "screened emails accessible"
+
+      it "does not include IP addresses without permission to view IPs" do
+        SiteSetting.moderators_view_ips = false
+
+        get "/admin/logs/screened_emails.json"
+
+        expect(response.status).to eq(200)
+        expect(response.parsed_body.first).not_to have_key("ip_address")
+      end
     end
 
     context "when logged in as a non-staff user" do
