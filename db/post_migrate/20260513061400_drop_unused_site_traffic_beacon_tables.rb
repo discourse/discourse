552394c27ab199b79024b868f1c464ed84de668a
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2026-05-13 08:41:57 +0000

    FIX: Drop stale site traffic view before beacon tables

--- db/post_migrate/20260513061400_drop_unused_site_traffic_beacon_tables.rb
+++ db/post_migrate/20260513061400_drop_unused_site_traffic_beacon_tables.rb
@@ -2,6 +2,8 @@
 
 class DropUnusedSiteTrafficBeaconTables < ActiveRecord::Migration[8.0]
   def up
+    execute "DROP VIEW IF EXISTS browser_pageview_events_combined"
+
     drop_table :pageview_daily_aggregates_beacon, if_exists: true
     drop_table :browser_pageview_events_beacon, if_exists: true
   end
