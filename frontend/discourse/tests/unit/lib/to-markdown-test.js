9086347b5fb7c90c37fac699fe75f560478473cb
David Taylor <david@taylorhq.com>
2026-05-15 20:00:30 +0100

    weird whitespace change

--- frontend/discourse/tests/unit/lib/to-markdown-test.js
+++ frontend/discourse/tests/unit/lib/to-markdown-test.js
@@ -72,9 +72,15 @@ module("Unit | Utility | to-markdown", function (hooks) {
 
     \t  <h3>Heading 3</h3>
 
+
     <h4>Heading 4</h4>
 
+
+
   <h5>Heading 5</h5>
+
+
+
 
   <h6>Heading 6</h6>
     `;
