9b5a4d85577167a331036e25ed7173c2adaeca92
Ethan M <176145523+Ethsim12@users.noreply.github.com>
2026-05-16 15:21:23 +0100

    Add upload_id parameter and models to splash screen class

--- app/services/site_setting/splash_screen_image_dark_changed.rb
+++ app/services/site_setting/splash_screen_image_dark_changed.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class SiteSetting::SplashScreenImageDarkChanged < SiteSetting::SplashScreenImageChanged
+  params { attribute :upload_id, :integer }
+
+  model :upload
+  model :svg
+  model :cleaned_svg
+  step :save_cleaned_svg_upload
+  step :clear_cache
+
   private
 
   def site_setting_name
