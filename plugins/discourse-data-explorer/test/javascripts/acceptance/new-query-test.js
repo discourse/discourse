9b02ce2e3088fa031cb22704bbb3b966e9b5cb8a
Natalie Tay <natalie.tay@gmail.com>
2026-05-19 23:09:37 +0800

    UI: Allow users to write sql in DE /new page (step 2/n) (#40166)
    Add a SQL component on the /new page

--- plugins/discourse-data-explorer/test/javascripts/acceptance/new-query-test.js
+++ plugins/discourse-data-explorer/test/javascripts/acceptance/new-query-test.js
@@ -28,7 +28,15 @@ acceptance("New Query", function (needs) {
     });
 
     server.get("/admin/plugins/discourse-data-explorer/schema.json", () => {
-      return helper.response({});
+      return helper.response({
+        topics: [
+          {
+            column_name: "id",
+            data_type: "serial",
+            primary: true,
+          },
+        ],
+      });
     });
 
     server.get("/admin/plugins/discourse-data-explorer/queries", () => {
@@ -74,7 +82,7 @@ acceptance("New Query", function (needs) {
     });
   });
 
-  test("navigates to new query page and creates a query", async function (assert) {
+  test("renders the manual create form and transitions on submit", async function (assert) {
     await visit("/admin/plugins/discourse-data-explorer/queries");
 
     await click(".d-page-subheader .btn-primary");
@@ -83,12 +91,19 @@ acceptance("New Query", function (needs) {
       "/admin/plugins/discourse-data-explorer/queries/new"
     );
 
-    await fillIn(".query-new [data-name='name'] input", "foo");
+    assert
+      .dom(".query-new__manual-form .right-panel .schema")
+      .exists("schema sidebar renders");
+    assert
+      .dom(".query-new__manual-form .editor-panel .ace-wrapper")
+      .exists("SQL editor renders alongside the schema sidebar");
+
+    await fillIn(".query-new__manual-form [data-name='name'] input", "foo");
     await fillIn(
-      ".query-new [data-name='description'] textarea",
+      ".query-new__manual-form [data-name='description'] textarea",
       "a test query"
     );
-    await click(".query-new .btn-primary");
+    await click(".query-new__manual-form .btn-primary");
 
     assert.strictEqual(
       currentURL(),
