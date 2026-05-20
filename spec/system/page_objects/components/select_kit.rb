67a1e3b21a00c281b6f619d48a1bbccd6c237ac8
Régis Hanol <regis@hanol.fr>
2026-05-20 12:08:24 +0200

    DEV: Fix Playwright::Locator has_css? error in select-kit page object

--- spec/system/page_objects/components/select_kit.rb
+++ spec/system/page_objects/components/select_kit.rb
@@ -171,13 +171,10 @@ module PageObjects
         has_no_css?("#{@context}.is-expanded .select-kit-row.is-selected")
       end
 
-      def has_disabled_row_name?(name)
-        expanded_component.has_css?(".select-kit-row.disabled[data-name='#{name}']")
-      end
-
       def has_disabled_row_reason?(name)
-        expanded_component.has_css?(
-          ".select-kit-row.disabled[data-name='#{name}'] .disabled-reason",
+        expanded_component
+        has_css?(
+          "#{@context}.is-expanded .select-kit-row.disabled[data-name='#{name}'] .disabled-reason",
         )
       end
 
