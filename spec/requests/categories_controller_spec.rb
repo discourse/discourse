06fe550daa72f3514c50246a77be8a20c1de25f1
jdmartinez1062 <juan@discourse.org>
2026-05-22 16:32:14 -0500

    fix test

--- spec/requests/categories_controller_spec.rb
+++ spec/requests/categories_controller_spec.rb
@@ -1373,7 +1373,7 @@ RSpec.describe CategoriesController do
               I18n.t(
                 "category_types.requires_plugin",
                 type_name: "Test plugin type",
-                plugin_name: "Test plugin",
+                plugin_name: "Test",
               ),
             )
           end
