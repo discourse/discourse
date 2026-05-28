fc1c0a8869dd308b83e258ece281cfe59d65d355
chapoi <101828855+chapoi@users.noreply.github.com>
2026-05-28 11:32:03 +0200

    specs

--- frontend/discourse/tests/acceptance/user-preferences-second-factor-test.js
+++ frontend/discourse/tests/acceptance/user-preferences-second-factor-test.js
@@ -67,9 +67,9 @@ acceptance("User Preferences - Second Factor", function (needs) {
     await click(".new-totp");
     assert.dom(".qr-code img").exists("shows qr code image");
 
-    await click(".modal a.show-second-factor-key");
+    await click(".d-modal a.show-second-factor-key");
     assert
-      .dom(".modal .second-factor-key")
+      .dom(".d-modal .second-factor-key")
       .exists("displays second factor key");
 
     await click(".add-totp");
