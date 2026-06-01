26c12a14d73854e22ac61c36ee50bb550761ab26
Mark VanLandingham <markvanlan@gmail.com>
2026-06-01 11:34:47 -0500

    FIX: Marking posts as solutions in nested topic (#40451)

--- frontend/discourse/app/lib/process-node.js
+++ frontend/discourse/app/lib/process-node.js
@@ -1,6 +1,11 @@
 export default function processNode(store, topic, nodeData) {
-  const post = store.createRecord("post", nodeData);
-  post.topic = topic;
+  const createdPost = store.createRecord("post", nodeData);
+  const post = topic.postStream?.storePost(createdPost) || createdPost;
+
+  if (post.topic !== topic) {
+    post.topic = topic;
+  }
+
   const children = (nodeData.children || []).map((child) =>
     processNode(store, topic, child)
   );
