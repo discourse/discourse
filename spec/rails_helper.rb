4a6b32a2641752078491032796efa80d52a2fa3f
Jarek Radosz <jarek@cvx.dev>
2026-06-03 21:06:28 +0200

    driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,9 +23,6 @@ require "mocha/api"
 require "certified"
 require "webmock/rspec"
 
-CHROME_REMOTE_DEBUGGING_PORT = (ENV["CHROME_REMOTE_DEBUGGING_PORT"] || 50_062).to_s
-CHROME_REMOTE_DEBUGGING_ADDRESS = ENV["CHROME_REMOTE_DEBUGGING_ADDRESS"] || "127.0.0.1"
-
 require_relative "support/server_error_tracking"
 
 class PlaywrightLogger
@@ -324,50 +321,7 @@ RSpec.configure do |config|
       system_tests_initialized = true
     end
 
-    driver_options = {
-      browser_type: :chromium,
-      channel: :chromium,
-      headless: (ENV["PLAYWRIGHT_HEADLESS"].presence || ENV["SELENIUM_HEADLESS"].presence) != "0",
-      args: apply_base_chrome_args,
-      acceptDownloads: true,
-      downloadsPath: Downloads::FOLDER,
-      slowMo: ENV["PLAYWRIGHT_SLOW_MO_MS"].to_i, # https://playwright.dev/docs/api/class-browsertype#browser-type-launch-option-slow-mo
-      playwright_cli_executable_path: "./node_modules/.bin/playwright",
-      logger: Logger.new(IO::NULL),
-      # NOTE: timezoneId is NOT set here because the driver is cached and reused,
-      # so only the first test's timezone would be applied. Instead, we use CDP
-      # to override the timezone per-test in the before(:each) hook below.
-      colorScheme: example.metadata[:color_scheme],
-    }
-
-    if ENV["CAPYBARA_REMOTE_DRIVER_URL"].present?
-      driver_options[:browser] = :remote
-      driver_options[:url] = ENV["CAPYBARA_REMOTE_DRIVER_URL"]
-    end
-
-    Capybara.register_driver(:playwright_mobile_chrome) do |app|
-      Capybara::Playwright::Driver.new(
-        app,
-        **driver_options,
-        deviceScaleFactor: 3,
-        isMobile: true,
-        hasTouch: true,
-        userAgent:
-          "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Mobile/15E148 Safari/604.1",
-        defaultBrowserType: "webkit",
-        viewport: ENV["PLAYWRIGHT_NO_VIEWPORT"] == "1" ? nil : { width: 390, height: 664 },
-      )
-    end
-
-    Capybara.register_driver(:playwright_chrome) do |app|
-      Capybara::Playwright::Driver.new(
-        app,
-        **driver_options,
-        viewport: ENV["PLAYWRIGHT_NO_VIEWPORT"] == "1" ? nil : { width: 1400, height: 1400 },
-      )
-    end
-
-    Capybara.default_driver = :playwright_chrome
+    SystemDrivers.register!(color_scheme: example.metadata[:color_scheme])
 
     driver = [:playwright]
     driver << :mobile if example.metadata[:mobile]
@@ -570,45 +524,4 @@ def stub_deprecated_settings!(override:)
   defaults = SiteSetting.defaults.instance_variable_get(:@defaults)
   defaults.each { |_, hash| hash.delete(:old_one) }
   defaults.each { |_, hash| hash.delete(:new_one) }
-end
-
-def apply_base_chrome_args(args = [])
-  base_args = %w[
-    --disable-search-engine-choice-screen
-    --no-sandbox
-    --disable-dev-shm-usage
-    --mute-audio
-    --remote-allow-origins=*
-    --disable-smooth-scrolling
-  ]
-
-  if !ENV["CI"]
-    base_args << "--remote-debugging-port=" + CHROME_REMOTE_DEBUGGING_PORT
-    base_args << "--remote-debugging-address=" + CHROME_REMOTE_DEBUGGING_ADDRESS
-  end
-
-  resolver_rules = ["MAP test.localhost:80 127.0.0.1:#{Capybara.server_port}"]
-  if ENV["CI"]
-    # Bypass the OS resolver for localhost lookups inside the browser.
-    resolver_rules.push("MAP localhost [::1]", "MAP *.localhost [::1]")
-  end
-  base_args << "--host-resolver-rules=#{resolver_rules.join(",")}"
-
-  # A file that contains just a list of paths like so:
-  #
-  # /home/me/.config/google-chrome/Default/Extensions/bmdblncegkenkacieihfhpjfppoconhi/4.9.1_0
-  #
-  # These paths can be found for each individual extension via the
-  # chrome://extensions/ page.
-  if ENV["CHROME_LOAD_EXTENSIONS_MANIFEST"].present?
-    File
-      .readlines(ENV["CHROME_LOAD_EXTENSIONS_MANIFEST"])
-      .each { |path| base_args << "--load-extension=#{path}" }
-  end
-
-  if ENV["CHROME_DISABLE_FORCE_DEVICE_SCALE_FACTOR"].blank?
-    base_args << "--force-device-scale-factor=1"
-  end
-
-  base_args + args
 end
