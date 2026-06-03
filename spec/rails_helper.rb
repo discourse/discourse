c77afeaeb6c0879c47e622bd7e32739e6961311c
Jarek Radosz <jarek@cvx.dev>
2026-06-03 13:47:40 +0200

    mocks

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -180,13 +180,6 @@ end
 PER_SPEC_TIMEOUT_SECONDS = 45
 BROWSER_READ_TIMEOUT = 30
 
-# To avoid erasing `any_instance` from Mocha
-require "rspec/mocks/syntax"
-RSpec::Mocks::Syntax.singleton_class.define_method(:enable_should) { |*| nil }
-RSpec::Mocks::Syntax.singleton_class.define_method(:disable_should) { |*| nil }
-
-RSpec::Mocks::ArgumentMatchers.remove_method(:hash_including) # We’re currently relying on the version from Webmock
-
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
@@ -216,15 +209,6 @@ RSpec.configure do |config|
 
   config.order = "random"
   config.infer_spec_type_from_file_location!
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-    mocks.verify_doubled_constant_names = true
-    mocks.syntax = :expect
-  end
-  config.mock_with MultiMock::Adapter.for(:mocha, :rspec)
-
-  config.include Mocha::API
 
   if ENV["GITHUB_ACTIONS"]
     # Enable color output in GitHub Actions
