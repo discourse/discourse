a399fc79794a55fcdbd09c027d44241d132734c3
Nat <natalie.tay@discourse.org>
2026-06-05 19:04:20 +0800

    UX: Data explorer improvements

--- plugins/discourse-data-explorer/config/routes.rb
+++ plugins/discourse-data-explorer/config/routes.rb
@@ -10,6 +10,7 @@ DiscourseDataExplorer::Engine.routes.draw do
     get "schema" => "query#schema"
     get "groups" => "query#groups"
     post "queries/generate" => "query#generate_with_ai"
+    post "queries/preview" => "query#preview"
     post "queries" => "query#create"
     put "queries/:id" => "query#update"
     delete "queries/:id" => "query#destroy"
@@ -42,6 +43,7 @@ Discourse::Application.routes.draw do
     get "/admin/plugins/explorer/groups" => "discourse_data_explorer/query#groups"
     post "/admin/plugins/explorer/queries/generate" =>
            "discourse_data_explorer/query#generate_with_ai"
+    post "/admin/plugins/explorer/queries/preview" => "discourse_data_explorer/query#preview"
     post "/admin/plugins/explorer/queries" => "discourse_data_explorer/query#create"
     put "/admin/plugins/explorer/queries/:id" => "discourse_data_explorer/query#update"
     delete "/admin/plugins/explorer/queries/:id" => "discourse_data_explorer/query#destroy"
