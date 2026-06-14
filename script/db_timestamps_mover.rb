discourse/discourse

commit 08666408a68ca037b272a9ff5f47920083a73018

DEV: Update db_timestamps_mover to never advance beyond `now()` (#40810)

script/db_timestamps_mover.rb
@@ -99,18 +99,21 @@ class TimestampsUpdater
     # add 1000 years to the interval to avoid uniqueness conflicts:
     operator = days < 0 ? "-" : "+"
     interval_expression = "#{operator} INTERVAL '1000 years #{days.abs} days'"
-    update_table(table_name, column_name, interval_expression)
+    update_table(table_name, column_name, interval_expression, clamp: false)
 
     # return back by 1000 years:
     operator = days < 0 ? "+" : "-"
     interval_expression = "#{operator} INTERVAL '1000 years'"
-    update_table(table_name, column_name, interval_expression)
+    update_table(table_name, column_name, interval_expression, clamp: false)
   end
 
-  def update_table(table_name, column_name, interval_expression)
+  def update_table(table_name, column_name, interval_expression, clamp: true)
+    new_value = "#{column_name} #{interval_expression}"
+    new_value = "LEAST(#{new_value}, now())" if clamp
+
     sql = <<~SQL
       UPDATE #{table_name}
-      SET #{column_name} = #{column_name} #{interval_expression}
+      SET #{column_name} = #{new_value}
     SQL
     @raw_connection.exec(sql)
   end

commit 6abbdefda22ebdee937ed07a9013ea366d9872e6

DEV: Ensure db_timestamps_mover only touches non-null columns
The change in 08666408a68ca037b272a9ff5f47920083a73018 caused all timestamp columns to fallback to use `now()`, even if they were previously null. That included things like `users.silenced_at`, `topic.deleted_at`, etc., which caused all sorts of strange behavior.

script/db_timestamps_mover.rb
@@ -114,6 +114,7 @@ class TimestampsUpdater
     sql = <<~SQL
       UPDATE #{table_name}
       SET #{column_name} = #{new_value}
+      WHERE #{column_name} IS NOT NULL
     SQL
     @raw_connection.exec(sql)
   end