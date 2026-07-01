5a8fc75d9dd98b9c28bc898739537575fd3c0a6f
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2026-07-01 07:31:53 +0000

    UX: Preserve dashboard return URL through report redirect

--- frontend/discourse/admin/routes/admin-reports/show.js
+++ frontend/discourse/admin/routes/admin-reports/show.js
@@ -72,6 +72,7 @@ export default class AdminReportsShowRoute extends DiscourseRoute {
         queryParams: {
           ...params,
           type: "consolidated_page_views",
+          return_url: this.controller.return_url,
         },
       });
     }
