8d37a828e951c7f2ed58ec5d0a7228a71f0b7e5c
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2026-07-03 13:43:54 +0200

    FEATURE: adds user-search trigger to workflows
    Adds a new trigger node: user-search.
    
    This will trigger when a user is doing a non empty search through the UI.
    
    To make it possible:
    - a new DiscourseEvent trigger has been created: user_search
    - the workflows cache has been improved/simplified to allow for different kind of triggers/nodes

---
+++ plugins/discourse-workflows/spec/lib/discourse_workflows/nodes/user_search_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe DiscourseWorkflows::Nodes::UserSearch::V1 do
+  describe "#valid?" do
+    it "returns true when the query is present" do
+      trigger = described_class.new("workflow query")
+
+      expect(trigger).to be_valid
+    end
+
+    it "returns false when the query is blank" do
+      trigger = described_class.new("")
+
+      expect(trigger).not_to be_valid
+    end
+  end
+
+  describe "#output" do
+    it "returns the query" do
+      trigger = described_class.new("workflow query")
+
+      expect(trigger.output).to eq(query: "workflow query")
+    end
+  end
+end
