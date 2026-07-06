79902c90e2abcb9fbc75d0a3afd3ac7df670e8c6
Gerhard Schlager <gerhard.schlager@discourse.org>
2026-07-07 01:20:56 +0200

    MT: Cover Reporting::Factory with mutation testing

--- migrations/core/spec/lib/common/reporting/factory_spec.rb
+++ migrations/core/spec/lib/common/reporting/factory_spec.rb
@@ -29,15 +29,48 @@ RSpec.describe Migrations::Reporting::Factory do
       reporter.close
     end
 
+    it "passes the output and titles through to the Tui reporter" do
+      ENV["TERM"] = "xterm-256color"
+      titles = %w[First Second]
+      fake = instance_double(Migrations::Reporting::Tui)
+      allow(Migrations::Reporting::Tui).to receive(:new).with(output: tty, titles:).and_return(fake)
+
+      expect(described_class.build(output: tty, titles:)).to be(fake)
+      expect(Migrations::Reporting::Tui).to have_received(:new).with(output: tty, titles:)
+    end
+
     it "builds the Plain reporter when stdout is not a tty" do
       expect(described_class.build(output: StringIO.new)).to be_a(Migrations::Reporting::Plain)
     end
 
-    it "builds the Plain reporter when TERM is dumb or empty" do
+    it "passes the output through to the Plain reporter" do
+      io = StringIO.new
+      reporter = described_class.build(output: io)
+      reporter.report_start(1, "Importing users")
+      expect(io.string).to include("Importing users")
+    end
+
+    it "defaults the output to $stdout" do
+      ENV["TERM"] = "dumb"
+      fake = instance_double(Migrations::Reporting::Plain)
+      allow(Migrations::Reporting::Plain).to receive(:new).with(output: $stdout).and_return(fake)
+
+      expect(described_class.build).to be(fake)
+      expect(Migrations::Reporting::Plain).to have_received(:new).with(output: $stdout)
+    end
+
+    it "treats output that can't answer tty? as plain" do
+      expect(described_class.build(output: Object.new)).to be_a(Migrations::Reporting::Plain)
+    end
+
+    it "builds the Plain reporter when TERM is dumb, empty, or unset" do
       ENV["TERM"] = "dumb"
       expect(described_class.build(output: tty)).to be_a(Migrations::Reporting::Plain)
 
       ENV["TERM"] = ""
+      expect(described_class.build(output: tty)).to be_a(Migrations::Reporting::Plain)
+
+      ENV.delete("TERM")
       expect(described_class.build(output: tty)).to be_a(Migrations::Reporting::Plain)
     end
   end
