4a72f61a50017fd3d8291f598a3ded6c56763d7b
Martin Brennan <martin@discourse.org>
2026-07-07 15:30:23 +1000

    DEV: Spec fixes

--- spec/requests/topics_controller_spec.rb
+++ spec/requests/topics_controller_spec.rb
@@ -5506,7 +5506,9 @@ RSpec.describe TopicsController do
           tracked_topic = create_post(category: tracked_category).topic
 
           create_post # This is a new post, but is not tracked so a record will not be created for it
-          expect do put "/topics/reset-new.json?tracked=true" end.to change {
+          expect do
+            put "/topics/reset-new.json?tracked=true", params: { dismiss_topics: true }
+          end.to change {
             DismissedTopicUser.where(user_id: user.id, topic_id: tracked_topic.id).count
           }.by(1)
         end
@@ -5522,7 +5524,7 @@ RSpec.describe TopicsController do
           it "creates dismissed topic user records if there are > 30 (default pagination) topics" do
             expect do
               stub_const(TopicQuery, "DEFAULT_PER_PAGE_COUNT", 2) do
-                put "/topics/reset-new.json?tracked=true"
+                put "/topics/reset-new.json?tracked=true", params: { dismiss_topics: true }
               end
             end.to change {
               DismissedTopicUser.where(user_id: user.id, topic_id: @tracked_topic_ids).count
@@ -5536,6 +5538,7 @@ RSpec.describe TopicsController do
               stub_const(TopicQuery, "DEFAULT_PER_PAGE_COUNT", 2) do
                 put "/topics/reset-new.json?tracked=true",
                     params: {
+                      dismiss_topics: true,
                       topic_ids: dismissing_topic_ids,
                     }
               end
@@ -5555,7 +5558,7 @@ RSpec.describe TopicsController do
             it "updates the user_stat new_since column and dismisses all the new topics" do
               old_new_since = user.user_stat.new_since
 
-              put "/topics/reset-new.json?tracked=false"
+              put "/topics/reset-new.json?tracked=false", params: { dismiss_topics: true }
               expect(DismissedTopicUser.where(user_id: user.id, topic_id: @topic_ids).count).to eq(
                 7,
               )
@@ -5568,9 +5571,9 @@ RSpec.describe TopicsController do
               DismissedTopicUser.create(user_id: user.id, topic_id: dismiss_ids.first)
               DismissedTopicUser.create(user_id: user.id, topic_id: dismiss_ids.second)
 
-              expect { put "/topics/reset-new.json?tracked=false" }.to change {
-                DismissedTopicUser.where(user_id: user.id).count
-              }.by(5)
+              expect do
+                put "/topics/reset-new.json?tracked=false", params: { dismiss_topics: true }
+              end.to change { DismissedTopicUser.where(user_id: user.id).count }.by(5)
             end
           end
         end
@@ -5587,7 +5590,7 @@ RSpec.describe TopicsController do
             topic_ids: [category_topic.id],
           )
 
-          put "/topics/reset-new.json?category_id=#{category.id}"
+          put "/topics/reset-new.json?category_id=#{category.id}", params: { dismiss_topics: true }
 
           expect(DismissedTopicUser.where(user_id: user.id).pluck(:topic_id)).to eq(
             [category_topic.id],
@@ -5600,7 +5603,10 @@ RSpec.describe TopicsController do
             topic_ids: [category_topic.id, subcategory_topic.id],
           )
 
-          put "/topics/reset-new.json?category_id=#{category.id}&include_subcategories=true"
+          put "/topics/reset-new.json?category_id=#{category.id}&include_subcategories=true",
+              params: {
+                dismiss_topics: true,
+              }
 
           expect(response.status).to eq(200)
 
@@ -5620,7 +5626,10 @@ RSpec.describe TopicsController do
             topic_ids: [category_topic.id, subcategory_topic.id, sub_subcategory_topic.id],
           )
 
-          put "/topics/reset-new.json?category_id=#{category.id}&include_subcategories=true"
+          put "/topics/reset-new.json?category_id=#{category.id}&include_subcategories=true",
+              params: {
+                dismiss_topics: true,
+              }
 
           expect(response.status).to eq(200)
 
@@ -5650,6 +5659,7 @@ RSpec.describe TopicsController do
               MessageBus.track_publish(TopicTrackingState.unread_channel_key(user.id)) do
                 put "/topics/reset-new.json",
                     params: {
+                      dismiss_topics: true,
                       category_id: category.id,
                       include_subcategories: true,
                     }
@@ -5677,6 +5687,7 @@ RSpec.describe TopicsController do
               MessageBus.track_publish(TopicTrackingState.unread_channel_key(user.id)) do
                 put "/topics/reset-new.json",
                     params: {
+                      dismiss_topics: true,
                       category_id: category.id,
                       include_subcategories: true,
                     }
@@ -5708,7 +5719,11 @@ RSpec.describe TopicsController do
           it "doesn't dismiss topics or publish topic IDs via MessageBus if the user can't access the category" do
             messages =
               MessageBus.track_publish do
-                put "/topics/reset-new.json", params: { category_id: private_category.id }
+                put "/topics/reset-new.json",
+                    params: {
+                      dismiss_topics: true,
+                      category_id: private_category.id,
+                    }
                 expect(response.status).to eq(200)
               end
 
@@ -5720,7 +5735,11 @@ RSpec.describe TopicsController do
             group.add(user)
             messages =
               MessageBus.track_publish do
-                put "/topics/reset-new.json", params: { category_id: private_category.id }
+                put "/topics/reset-new.json",
+                    params: {
+                      dismiss_topics: true,
+                      category_id: private_category.id,
+                    }
               end
             expect(response.status).to eq(200)
             expect(messages.size).to eq(1)
@@ -5743,7 +5762,7 @@ RSpec.describe TopicsController do
 
         it "dismisses topics for tag" do
           TopicTrackingState.expects(:publish_dismiss_new).with(user.id, topic_ids: [tag_topic.id])
-          put "/topics/reset-new.json?tag_name=#{tag.name}"
+          put "/topics/reset-new.json?tag_name=#{tag.name}", params: { dismiss_topics: true }
           expect(DismissedTopicUser.where(user_id: user.id).pluck(:topic_id)).to eq([tag_topic.id])
         end
 
@@ -5765,7 +5784,11 @@ RSpec.describe TopicsController do
             group.add(user)
             messages =
               MessageBus.track_publish do
-                put "/topics/reset-new.json", params: { tag_name: restricted_tag.name }
+                put "/topics/reset-new.json",
+                    params: {
+                      dismiss_topics: true,
+                      tag_name: restricted_tag.name,
+                    }
               end
             expect(messages.size).to eq(1)
             expect(messages[0].data["payload"]["topic_ids"]).to contain_exactly(
@@ -5779,7 +5802,11 @@ RSpec.describe TopicsController do
           it "ignores the tag param and dismisses all topics if the user can't see the tag" do
             messages =
               MessageBus.track_publish do
-                put "/topics/reset-new.json", params: { tag_name: restricted_tag.name }
+                put "/topics/reset-new.json",
+                    params: {
+                      dismiss_topics: true,
+                      tag_name: restricted_tag.name,
+                    }
               end
             expect(messages.size).to eq(1)
             expect(messages[0].data["payload"]["topic_ids"]).to contain_exactly(
@@ -5807,7 +5834,10 @@ RSpec.describe TopicsController do
             user.id,
             topic_ids: [tag_and_category_topic.id],
           )
-          put "/topics/reset-new.json?tag_name=#{tag.name}&category_id=#{category.id}"
+          put "/topics/reset-new.json?tag_name=#{tag.name}&category_id=#{category.id}",
+              params: {
+                dismiss_topics: true,
+              }
           expect(DismissedTopicUser.where(user_id: user.id).pluck(:topic_id)).to eq(
             [tag_and_category_topic.id],
           )
@@ -5824,7 +5854,8 @@ RSpec.describe TopicsController do
             .with(user.id, topic_ids: [topic2.id, topic3.id])
             .at_least_once
 
-          put "/topics/reset-new.json", **{ params: { topic_ids: [topic2.id, topic3.id] } }
+          put "/topics/reset-new.json",
+              **{ params: { dismiss_topics: true, topic_ids: [topic2.id, topic3.id] } }
           expect(response.status).to eq(200)
           user.reload
           expect(user.user_stat.new_since.to_date).not_to eq(old_date.to_date)
@@ -5848,7 +5879,11 @@ RSpec.describe TopicsController do
 
           messages =
             MessageBus.track_publish do
-              put "/topics/reset-new.json", params: { topic_ids: [topic2.id, topic3.id] }
+              put "/topics/reset-new.json",
+                  params: {
+                    dismiss_topics: true,
+                    topic_ids: [topic2.id, topic3.id],
+                  }
             end
           expect(messages.size).to eq(1)
           expect(messages[0].channel).to eq(TopicTrackingState.unread_channel_key(user.id))
@@ -5878,7 +5913,12 @@ RSpec.describe TopicsController do
             create_post # This is a new post, but is not tracked so a record will not be created for it
             expect do
               put "/topics/reset-new.json?tracked=true",
-                  **{ params: { topic_ids: [tracked_topic.id, topic2.id, topic3.id] } }
+                  **{
+                    params: {
+                      dismiss_topics: true,
+                      topic_ids: [tracked_topic.id, topic2.id, topic3.id],
+                    },
+                  }
             end.to change { DismissedTopicUser.where(user_id: user.id).count }.by(2)
             expect(DismissedTopicUser.where(user_id: user.id).pluck(:topic_id)).to match_array(
               [tracked_topic.id, topic2.id],
