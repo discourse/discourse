85d672b9484b38f429361c3aee2d1b7ab1b8e7aa
Penar Musaraj <pmusaraj@gmail.com>
2026-07-09 14:40:00 -0400

    More specs

--- spec/lib/stylesheet/watcher_spec.rb
+++ spec/lib/stylesheet/watcher_spec.rb
@@ -33,5 +33,57 @@ RSpec.describe Stylesheet::Watcher do
         plugin_name: nil,
       )
     end
+
+    it "infers plugin names from stylesheet paths under plugins" do
+      plugin_path = Rails.root.join("plugins/discourse-calendar").to_s
+      path = Rails.root.join("plugins/discourse-calendar/assets/stylesheets/admin/calendar.scss")
+
+      expect(watcher.path_data(path.to_s, [plugin_path])).to include(
+        target: nil,
+        plugin_name: "discourse-calendar",
+      )
+    end
+  end
+
+  describe "#plugin_assets_refresh" do
+    let(:plugin_name) { "test-plugin" }
+    let(:manager) { instance_double(Stylesheet::Manager) }
+
+    before do
+      DiscoursePluginRegistry.stylesheets[plugin_name] = Set.new(["stylesheets/common.scss"])
+      DiscoursePluginRegistry.desktop_stylesheets[plugin_name] = Set.new(
+        ["stylesheets/desktop.scss"],
+      )
+      DiscoursePluginRegistry.mobile_stylesheets[plugin_name] = Set.new(["stylesheets/mobile.scss"])
+      DiscoursePluginRegistry.admin_stylesheets[plugin_name] = Set.new(["stylesheets/admin.scss"])
+
+      allow(Stylesheet::Manager).to receive(:new).and_return(manager)
+      allow(manager).to receive(:stylesheet_data) do |target|
+        [{ target: target, new_href: "/stylesheets/#{target}.css" }]
+      end
+    end
+
+    after { DiscoursePluginRegistry.reset! }
+
+    it "publishes changes for every registered plugin target" do
+      messages =
+        MessageBus.track_publish("/file-change") { watcher.plugin_assets_refresh(plugin_name) }
+
+      expect(messages.first.data).to contain_exactly(
+        { target: :"test-plugin", new_href: "/stylesheets/test-plugin.css" },
+        { target: :"test-plugin_desktop", new_href: "/stylesheets/test-plugin_desktop.css" },
+        { target: :"test-plugin_mobile", new_href: "/stylesheets/test-plugin_mobile.css" },
+        { target: :"test-plugin_admin", new_href: "/stylesheets/test-plugin_admin.css" },
+      )
+    end
+
+    it "skips publishing when the plugin has no registered stylesheets" do
+      DiscoursePluginRegistry.reset!
+
+      messages =
+        MessageBus.track_publish("/file-change") { watcher.plugin_assets_refresh(plugin_name) }
+
+      expect(messages).to be_empty
+    end
   end
 end
