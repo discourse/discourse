8937927cdfcafb1511b8ccc1e3f740e3c796b6c6
Martin Brennan <martin@discourse.org>
2026-07-14 14:20:52 +1000

    DEV: Move SearchGrantees into service

--- app/controllers/access_control_lists_controller.rb
+++ app/controllers/access_control_lists_controller.rb
@@ -3,33 +3,27 @@
 class AccessControlListsController < ApplicationController
   requires_login
 
-  SEARCH_GRANTEES_LIMIT = 50
+  SEARCH_GRANTEES_LIMIT = AccessControlList::SearchGrantees::MAX_RESULTS
 
+  # Used to search for _potential_ users and groups to grant access
+  # to a target for an ACL. Exposes same info as public /u and
+  # /g endpoints, hides groups not visible to the current user.
   def search_grantees
-    term = params[:term].to_s.strip
-
-    if term.blank?
-      render json: { users: [], groups: [] }
-      return
-    end
-
     limit = fetch_limit_from_params(default: SEARCH_GRANTEES_LIMIT, max: SEARCH_GRANTEES_LIMIT)
-    users = UserSearch.new(term, searching_user: current_user, limit: limit).search
-    groups = Group.search_groups(term, groups: visible_groups, sort: :auto).limit(limit)
 
-    render json: { users: serialize_users(users), groups: serialize_groups(groups) }
+    AccessControlList::SearchGrantees.call(
+      service_params.deep_merge(params: { term: params[:term], limit: }),
+    ) do
+      on_success do |users:, groups:|
+        render json: { users: serialize_users(users), groups: serialize_groups(groups) }
+      end
+
+      on_failed_contract { |contract| render_json_error(contract.errors.full_messages) }
+      on_failure { render_json_error(I18n.t("generic_error")) }
+    end
   end
 
   private
-
-  def visible_groups
-    Group.visible_groups(
-      current_user,
-      "groups.name ASC",
-      include_everyone: !SiteSetting.granular_anonymous_and_logged_in_groups_permissions,
-      include_pseudogroups: SiteSetting.granular_anonymous_and_logged_in_groups_permissions,
-    )
-  end
 
   def serialize_users(users)
     ActiveModel::ArraySerializer.new(users, each_serializer: FoundUserSerializer).as_json
