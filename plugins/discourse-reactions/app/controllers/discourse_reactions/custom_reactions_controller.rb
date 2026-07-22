17be94b4346f70c7cc57f9c5e8477b096c2830bb
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2026-07-22 11:59:45 -0500

    SECURITY: anonymous users can read hidden real names via reaction-user endpoints

--- plugins/discourse-reactions/app/controllers/discourse_reactions/custom_reactions_controller.rb
+++ plugins/discourse-reactions/app/controllers/discourse_reactions/custom_reactions_controller.rb
@@ -179,13 +179,12 @@ class DiscourseReactions::CustomReactionsController < ApplicationController
 
     users =
       rows.map do |row|
-        {
+        format_user(
+          row,
           id: row.id,
-          username: row.username,
-          name: row.name,
           avatar_template: User.avatar_template(row.username, row.uploaded_avatar_id),
           reaction: row.reaction,
-        }
+        )
       end
 
     render_json_dump(users: users, total_rows: total)
@@ -287,6 +286,13 @@ class DiscourseReactions::CustomReactionsController < ApplicationController
 
   private
 
+  def format_user(user, avatar_template:, **extra_attributes)
+    attributes = { username: user.username }
+    attributes[:name] = user.name if SiteSetting.enable_names?
+    attributes[:avatar_template] = avatar_template
+    attributes.merge!(extra_attributes)
+  end
+
   def get_users(reaction)
     DiscourseReactions::PostReactionsQuery
       .apply_ignored_users_filter(
@@ -298,13 +304,12 @@ class DiscourseReactions::CustomReactionsController < ApplicationController
       .order("discourse_reactions_reaction_users.created_at desc")
       .limit(MAX_USERS_COUNT + 1)
       .map do |reaction_user|
-        {
-          username: reaction_user.user.username,
-          name: reaction_user.user.name,
+        format_user(
+          reaction_user.user,
           avatar_template: reaction_user.user.avatar_template,
           can_undo: reaction_user.can_undo?,
           created_at: reaction_user.created_at.to_s,
-        }
+        )
       end
   end
 
@@ -335,13 +340,12 @@ class DiscourseReactions::CustomReactionsController < ApplicationController
   end
 
   def format_like_user(like)
-    {
-      username: like.user.username,
-      name: like.user.name,
+    format_user(
+      like.user,
       avatar_template: like.user.avatar_template,
       can_undo: guardian.can_delete_post_action?(like),
       created_at: like.created_at.to_s,
-    }
+    )
   end
 
   def format_likes_users(likes)
