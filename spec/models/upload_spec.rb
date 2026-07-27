a07188016987de1613c961277e2e928aaa7c37ec
David Taylor <david@taylorhq.com>
2026-07-27 18:27:34 +0100

    DEV: Sandbox all image processing via Landlock (#42048)
    - Update all imagemagick calls to go through a new `::Imagemagick`
    wrapper, which wraps the command in `Discourse::SafeExec`
    - Patch image_optim to force its calls through `SafeExec`
    
    This provides robust defense-in-depth against vulnerabilities in image
    processing binaries. Landlock is supported on Linux Kernel 5.13 and
    above.

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -1008,7 +1008,7 @@ RSpec.describe Upload do
     end
 
     it "correctly handles unparsable ImageMagick output" do
-      Discourse::Utils.stubs(:execute_command).returns("someinvalidoutput")
+      ImageMagick.stubs(:magick).returns("someinvalidoutput")
 
       expect(invalid_image.dominant_color).to eq(nil)
 
