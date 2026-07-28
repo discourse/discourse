361869df7c1160f011e0a0140d24e6ac123b3403
Martin Brennan <martin@discourse.org>
2026-07-28 14:15:17 +0000

    SECURITY: Stop exposing hidden tag names through category serializers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -546,8 +546,6 @@ class CategoriesController < ApplicationController
           :uploaded_logo_dark,
           :uploaded_background,
           :uploaded_background_dark,
-          :tags,
-          :tag_groups,
           :form_templates,
           category_required_tag_groups: :tag_group,
         )
