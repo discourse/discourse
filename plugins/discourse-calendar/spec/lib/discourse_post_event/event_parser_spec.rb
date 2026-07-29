7f2c90d9bacd0d0b0a99c3f0c17c1c2afde1584b
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2026-07-30 06:20:34 +0800

    DEV: Remove globally scoped spec helpers

--- plugins/discourse-calendar/spec/lib/discourse_post_event/event_parser_spec.rb
+++ plugins/discourse-calendar/spec/lib/discourse_post_event/event_parser_spec.rb
@@ -1,26 +1,23 @@
 # frozen_string_literal: true
 
-def build_post(user, raw)
-  Post.new(user: user, raw: raw)
-end
-
 describe DiscoursePostEvent::EventParser do
   subject(:parser) { DiscoursePostEvent::EventParser }
 
   let(:user) { Fabricate(:user) }
 
   it "works with no event" do
-    events = parser.extract_events(build_post(user, "this could be a nice event"))
+    events = parser.extract_events(Post.new(user: user, raw: "this could be a nice event"))
     expect(events.length).to eq(0)
   end
 
   it "finds one event" do
-    events = parser.extract_events(build_post(user, '[event start="foo" end="bar"]\n[/event]'))
+    events =
+      parser.extract_events(Post.new(user: user, raw: '[event start="foo" end="bar"]\n[/event]'))
     expect(events.length).to eq(1)
   end
 
   it "finds multiple events" do
-    post_event = build_post user, <<~TXT
+    post_event = Post.new user: user, raw: <<~TXT
       [event start="2020"]
       [/event]
 
@@ -33,21 +30,27 @@ describe DiscoursePostEvent::EventParser do
   end
 
   it "parses options" do
-    events = parser.extract_events(build_post(user, '[event start="foo" end="bar"]\n[/event]'))
+    events =
+      parser.extract_events(Post.new(user: user, raw: '[event start="foo" end="bar"]\n[/event]'))
     expect(events[0][:start]).to eq("foo")
     expect(events[0][:end]).to eq("bar")
   end
 
   it "parses showLocalTime" do
     events =
-      parser.extract_events(build_post(user, '[event start="foo" showLocalTime="true"]\n[/event]'))
+      parser.extract_events(
+        Post.new(user: user, raw: '[event start="foo" showLocalTime="true"]\n[/event]'),
+      )
     expect(events[0][:"show-local-time"]).to eq("true")
   end
 
   it "parses recurrenceUntil" do
     events =
       parser.extract_events(
-        build_post(user, '[event start="foo" recurrenceUntil="2025-06-21 23:59"]\n[/event]'),
+        Post.new(
+          user: user,
+          raw: '[event start="foo" recurrenceUntil="2025-06-21 23:59"]\n[/event]',
+        ),
       )
     expect(events[0][:"recurrence-until"]).to eq("2025-06-21 23:59")
   end
@@ -55,35 +58,44 @@ describe DiscoursePostEvent::EventParser do
   it "works with escaped string" do
     events =
       parser.extract_events(
-        build_post(user, "I am going to get that fixed.\n\n[event start=\"bar\"]\n[/event]"),
+        Post.new(
+          user: user,
+          raw: "I am going to get that fixed.\n\n[event start=\"bar\"]\n[/event]",
+        ),
       )
     expect(events[0][:start]).to eq("bar")
   end
 
   it "parses options where value has spaces" do
-    events = parser.extract_events(build_post(user, '[event start="foo" name="bar baz"]\n[/event]'))
+    events =
+      parser.extract_events(
+        Post.new(user: user, raw: '[event start="foo" name="bar baz"]\n[/event]'),
+      )
     expect(events[0][:name]).to eq("bar baz")
   end
 
   it "doesn’t parse invalid options" do
     events =
       parser.extract_events(
-        build_post(
-          user,
-          "I am going to get that fixed.\n\n[event start=\"foo\" something=\"bar\"]\n[/event]",
+        Post.new(
+          user: user,
+          raw: "I am going to get that fixed.\n\n[event start=\"foo\" something=\"bar\"]\n[/event]",
         ),
       )
     expect(events[0][:something]).to be(nil)
 
     events =
       parser.extract_events(
-        build_post(user, "I am going to get that fixed.\n\n[event something=\"bar\"]\n[/event]"),
+        Post.new(
+          user: user,
+          raw: "I am going to get that fixed.\n\n[event something=\"bar\"]\n[/event]",
+        ),
       )
     expect(events).to eq([])
   end
 
   it "doesn’t parse an event in codeblock" do
-    post_event = build_post user, <<-TXT
+    post_event = Post.new user: user, raw: <<-TXT
       Example event:
       ```
       [event start=\"bar\"]\n[/event]
@@ -96,7 +108,7 @@ describe DiscoursePostEvent::EventParser do
   end
 
   it "doesn’t parse in blockquote" do
-    post_event = build_post user, <<-TXT
+    post_event = Post.new user: user, raw: <<-TXT
       [event start="2020"][/event]
     TXT
 
@@ -107,13 +119,13 @@ describe DiscoursePostEvent::EventParser do
   it "doesn’t escape event name" do
     events =
       parser.extract_events(
-        build_post(user, '[event start="foo" name="bar <script> baz"]\n[/event]'),
+        Post.new(user: user, raw: '[event start="foo" name="bar <script> baz"]\n[/event]'),
       )
     expect(events[0][:name]).to eq("bar <script> baz")
   end
 
   it "doesn't escape urls" do
-    post_event = build_post user, <<~TXT
+    post_event = Post.new user: user, raw: <<~TXT
         [event start="2020" url="https://example.com/?q=foo&all=true" image="upload://6c4fsAgNM6Npo7raNCPqVm2whzz.jpeg"]
         [/event]
       TXT
@@ -124,7 +136,7 @@ describe DiscoursePostEvent::EventParser do
   end
 
   it "doesn't escape location" do
-    post_event = build_post user, <<~TXT
+    post_event = Post.new user: user, raw: <<~TXT
         [event start="2020" location="Joe & Sons (downtown)"]
         [/event]
       TXT
@@ -134,7 +146,7 @@ describe DiscoursePostEvent::EventParser do
   end
 
   it "decodes entities in location so escaped legacy raws self-heal" do
-    post_event = build_post user, <<~TXT
+    post_event = Post.new user: user, raw: <<~TXT
         [event start="2020" location="Joe &amp; Sons"]
         [/event]
       TXT
@@ -144,7 +156,7 @@ describe DiscoursePostEvent::EventParser do
   end
 
   it "extracts description as inline markdown" do
-    post_event = build_post user, <<~TXT
+    post_event = Post.new user: user, raw: <<~TXT
       [event start="2020"]
       Check out https://example.com for details
       [/event]
@@ -155,7 +167,7 @@ describe DiscoursePostEvent::EventParser do
   end
 
   it "preserves markdown links, emoji and mentions in the description" do
-    post_event = build_post user, <<~TXT
+    post_event = Post.new user: user, raw: <<~TXT
       [event start="2020"]
       See [the agenda](https://agenda.example.com) :tada: with @system
       [/event]
@@ -246,7 +258,7 @@ describe DiscoursePostEvent::EventParser do
     before { SiteSetting.discourse_post_event_allowed_custom_fields = "foo-bar|bar" }
 
     it "parses allowed custom fields" do
-      post_event = build_post user, <<~TXT
+      post_event = Post.new user: user, raw: <<~TXT
         [event start="2020" fooBar="1" bar="2"]
         [/event]
       TXT
@@ -257,7 +269,7 @@ describe DiscoursePostEvent::EventParser do
     end
 
     it "doesn’t parse not allowed custom fields" do
-      post_event = build_post user, <<~TXT
+      post_event = Post.new user: user, raw: <<~TXT
         [event start="2020" baz="1"]
         [/event]
       TXT
@@ -274,7 +286,7 @@ describe DiscoursePostEvent::EventParser do
     end
 
     it "parses fields regardless of case or separators" do
-      post_event = build_post user, <<~TXT
+      post_event = Post.new user: user, raw: <<~TXT
         [event start="2020" fieldAa="1" fieldbb="2" fieldCc="3" fielddd="4" myField="5"]
         [/event]
       TXT
