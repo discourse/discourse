4380ba1e75f2aba25b08abc89239c966edd11eb2
David Taylor <david@taylorhq.com>
2026-07-31 21:50:12 +0100

    DEV: Bump release branch to v2026.1.7

--- lib/version.rb
+++ lib/version.rb
@@ -7,7 +7,7 @@ module Discourse
   unless defined?(::Discourse::VERSION)
     module VERSION #:nodoc:
       # Use the `version_bump:*` rake tasks to update this value
-      STRING = "2026.1.6"
+      STRING = "2026.1.7"
 
       PARTS = STRING.split(".")
       private_constant :PARTS
