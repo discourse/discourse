982fba1536cd7d72ee4ce2ff9a63a34d60ecbb71
Martin Brennan <martin@discourse.org>
2026-08-04 09:04:44 +1000

    FIX: Add missing solved_topics_auto_close_hours custom field entries (#42255)
    When `solved_topics_auto_close_hours` is null/missing in the DB for
    Support type categories, the UI defaults to 48 hours for this setting,
    but this can be surprising for admins when changing some other support
    category setting, as it will go from 0 -> 48 hours in the background.
    
    This commit fixes the issue by backfilling a new custom field
    value for `solved_topics_auto_close_hours`, setting it to `0`
    on categories with `enable_accepted_answers` set to true
    and no existing value for the field.
    
    New support categories are unaffected, as we set 48 hours by
    default there.

--- db/structure.sql
+++ db/structure.sql
@@ -23075,6 +23075,7 @@ ALTER TABLE ONLY public.ad_plugin_house_ads_groups
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20260803015314'),
 ('20260731055703'),
 ('20260729153343'),
 ('20260728162521'),
