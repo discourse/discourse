10515b324eaaab1f1df198466951cdbe9fc56bc2
amanda <amanda@discourse.org>
2026-08-05 20:04:10 -0300

    PERF: Add index on reviewables.target_created_by_id for staff_info queries

---
+++ db/migrate/20260805183535_add_target_created_by_index_to_reviewables.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class AddTargetCreatedByIndexToReviewables < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
+  def up
+    execute <<~SQL
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_reviewables_flagged_by_target_created_by
+        ON reviewables(target_created_by_id)
+        WHERE type = 'ReviewableFlaggedPost'
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      DROP INDEX IF EXISTS idx_reviewables_flagged_by_target_created_by
+    SQL
+  end
+end
