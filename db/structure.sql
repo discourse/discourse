8665f97c8bc3f75048efb2bce7ab485c815a96ff
Amanda Alves Branquinho <amanda@discourse.org>
2026-08-11 13:33:15 -0300

    PERF: Add index on reviewables.target_created_by_id for staff_info queries (#42489)
    Fixes slow staff_info queries when viewing user profiles.

--- db/structure.sql
+++ db/structure.sql
@@ -21160,6 +21160,13 @@ CREATE INDEX index_reviewables_on_status_and_type ON public.reviewables USING bt
 
 
 --
+-- Name: index_reviewables_on_target_created_by_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_reviewables_on_target_created_by_id ON public.reviewables USING btree (target_created_by_id);
+
+
+--
 -- Name: index_reviewables_on_target_id_where_post_type_eq_post; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -23135,6 +23142,7 @@ ALTER TABLE ONLY public.ad_plugin_house_ads_groups
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20260810154331'),
 ('20260810012238'),
 ('20260803015314'),
 ('20260731055703'),
