495bd14454653b90fdd278014f0f80e1ba60e105
Jarek Radosz <jarek@cvx.dev>
2026-08-20 11:28:00 +0200

    bump

--- frontend/discourse/admin/routes/admin-badges/award.js
+++ frontend/discourse/admin/routes/admin-badges/award.js
@@ -12,7 +12,7 @@ export default class AdminBadgesAwardRoute extends Route {
     }
 
     return this.adminBadges.badges.find(
-      (value) => String(value.id) === params.badge_id
+      (value) => value.id === parseInt(params.badge_id, 10)
     );
   }
 }
