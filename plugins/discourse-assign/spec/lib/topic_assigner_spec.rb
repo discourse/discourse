fee10d906bdcd32fa0170f11d7c184cb080b7271
Ary Rabelo <aryrabelo@gmail.com>
2026-08-25 22:42:19 -0300

    DEV: use have_received in the TopicAssigner spec
    RSpec/MessageSpies flagged the two message expectations. Setting Assigner
    up as a spy with allow and asserting with have_received satisfies the cop
    without weakening the spec: with force forwarded positionally the recorded
    call is (post, false) and have_received(...).with(post, force: false)
    still fails.

--- plugins/discourse-assign/spec/lib/topic_assigner_spec.rb
+++ plugins/discourse-assign/spec/lib/topic_assigner_spec.rb
@@ -4,16 +4,18 @@ RSpec.describe TopicAssigner do
   describe ".auto_assign" do
     let(:post) { instance_double(Post) }
 
-    it "forwards force to Assigner as a keyword argument" do
-      expect(Assigner).to receive(:auto_assign).with(post, force: true)
+    before { allow(Assigner).to receive(:auto_assign) }
 
+    it "forwards force to Assigner as a keyword argument" do
       described_class.auto_assign(post, force: true)
+
+      expect(Assigner).to have_received(:auto_assign).with(post, force: true)
     end
 
     it "forwards the default force value as a keyword argument" do
-      expect(Assigner).to receive(:auto_assign).with(post, force: false)
-
       described_class.auto_assign(post)
+
+      expect(Assigner).to have_received(:auto_assign).with(post, force: false)
     end
   end
 end
