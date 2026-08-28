38d262204c742d5b3377bfeb3f52df0d50ca7a95
Nat <natalie.tay@discourse.org>
2026-08-28 18:38:12 +0800

    fix specs

--- plugins/discourse-ai/spec/system/admin_ai_features_spec.rb
+++ plugins/discourse-ai/spec/system/admin_ai_features_spec.rb
@@ -93,7 +93,9 @@ RSpec.describe "Admin AI features configuration" do
       "/admin/plugins/discourse-ai/ai-features/#{DiscourseAi::Configuration::Module::SEARCH_ID}/edit",
     )
 
-    expect(page).to have_no_css(".form-kit__field[data-name='ai_discover_enabled']")
+    expect(page).to have_no_css(
+      ".form-kit__field[data-name='ai_discover_enabled']:not([data-disabled])",
+    )
   end
 
   it "shows edit page with grouped settings" do
