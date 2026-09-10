cb96175e93cec8d8e82bd23cc058ca3dfb3c849d
Kris <kris.aubuchon@discourse.org>
2026-09-10 14:28:29 -0400

    UX: keep unresolved review queue items in queue after taking action (#43539)
    We've got some secondary actions like "remove avatar" and "unsilence
    user" in the review queue now, but the interaction is a little odd
    because "remove avatar" doesn't resolve the flag, but the actions will
    be hidden until refresh.
    
    This PR adjusts these so the secondary actions don't hide the primary
    actions, so the flag can still be resolved without a refresh.
    
    Before
    
    
    https://github.com/user-attachments/assets/38e5ecab-ff1d-4200-b5f5-9ef11475f3fa
    
    
    
    
    After 
    
    
    
    https://github.com/user-attachments/assets/8f830358-7064-4090-b12b-f58d6a61cbc3

--- app/models/reviewable_flagged_post.rb
+++ app/models/reviewable_flagged_post.rb
@@ -178,7 +178,7 @@ class ReviewableFlaggedPost < Reviewable
   def perform_unsilence_user(performed_by, _args)
     UserSilencer.unsilence(target_user, performed_by, reviewable_id: id)
 
-    create_result(:success) { |result| result.remove_reviewable_ids = [] }
+    create_result(:success)
   end
 
   def perform_unsilence_user_and_ignore(performed_by, args)
