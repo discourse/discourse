dc5f7b29bb81eb83bbda362cc1b0ece1650faa8e
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2026-09-11 06:51:01 +0800

    DEV: Consolidate post-move request specs
    This commit strengthens the existing success and private-category examples with deferred HTTP response assertions, removes four duplicate examples, and keeps descriptions focused on behavior.

--- spec/requests/topics_controller_spec.rb
+++ spec/requests/topics_controller_spec.rb
@@ -342,7 +342,7 @@ RSpec.describe TopicsController do
         expect(response).to be_forbidden
       end
 
-      it "returns 403 for a restricted category even when other destination options are supplied" do
+      it "does not allow posts to be moved to a private category when other destination options are supplied" do
         [
           { destination_topic_id: dest_topic.id },
           { archetype: Archetype.default },
@@ -625,68 +625,41 @@ RSpec.describe TopicsController do
       end
 
       it "moves the posts" do
+        io = StringIO.new
+
         post "/t/#{topic.id}/move-posts.json",
              params: {
                post_ids: [p2.id],
                destination_topic_id: dest_topic.id,
+             },
+             env: {
+               "rack.hijack" => -> { io },
              }
 
-        expect(response.status).to eq(200)
-        result = response.parsed_body
-        expect(result["success"]).to eq(true)
-        expect(result["url"]).to be_present
+        headers, body = io.string.split("\r\n\r\n", 2)
+        expect(headers).to start_with("HTTP/1.1 200 OK\r\n")
+        expect(JSON.parse(body)).to eq("success" => true, "url" => dest_topic.relative_url)
+        expect(p2.reload.topic_id).to eq(dest_topic.id)
       end
 
       it "does not allow posts to be moved to a private category" do
         dest_topic.update!(category: staff_category)
+        io = StringIO.new
 
         post "/t/#{topic.id}/move-posts.json",
              params: {
                post_ids: [p2.id],
                destination_topic_id: dest_topic.id,
+             },
+             env: {
+               "rack.hijack" => -> { io },
              }
 
-        expect(response).to be_forbidden
-      end
-
-      context "when the request is hijacked" do
-        it "returns 200 with the destination URL after moving the posts" do
-          io = StringIO.new
-
-          post "/t/#{topic.id}/move-posts.json",
-               params: {
-                 post_ids: [p2.id],
-                 destination_topic_id: dest_topic.id,
-               },
-               env: {
-                 "rack.hijack" => -> { io },
-               }
-
-          headers, body = io.string.split("\r\n\r\n", 2)
-          expect(headers).to start_with("HTTP/1.1 200 OK\r\n")
-          expect(JSON.parse(body)).to eq("success" => true, "url" => dest_topic.relative_url)
-          expect(p2.reload.topic_id).to eq(dest_topic.id)
-        end
-
-        it "returns 403 with JSON errors when the destination category is inaccessible" do
-          dest_topic.update!(category: staff_category)
-          io = StringIO.new
-
-          post "/t/#{topic.id}/move-posts.json",
-               params: {
-                 post_ids: [p2.id],
-                 destination_topic_id: dest_topic.id,
-               },
-               env: {
-                 "rack.hijack" => -> { io },
-               }
-
-          headers, body = io.string.split("\r\n\r\n", 2)
-          expect(headers).to start_with("HTTP/1.1 403 Forbidden\r\n")
-          expect(JSON.parse(body)["errors"]).to be_present
-          expect(p2.reload.topic_id).to eq(topic.id)
-          expect(topic.reload.deleted_at).to be_nil
-        end
+        headers, body = io.string.split("\r\n\r\n", 2)
+        expect(headers).to start_with("HTTP/1.1 403 Forbidden\r\n")
+        expect(JSON.parse(body)["errors"]).to be_present
+        expect(p2.reload.topic_id).to eq(topic.id)
+        expect(topic.reload.deleted_at).to be_nil
       end
 
       it "does not allow posts outside of the category to be moved" do
@@ -989,58 +962,39 @@ RSpec.describe TopicsController do
       end
 
       it "moves the posts" do
-        post "/t/#{topic.id}/merge-topic.json", params: { destination_topic_id: dest_topic.id }
+        io = StringIO.new
 
-        expect(response.status).to eq(200)
-        result = response.parsed_body
-        expect(result["success"]).to eq(true)
-        expect(result["url"]).to be_present
+        post "/t/#{topic.id}/merge-topic.json",
+             params: {
+               destination_topic_id: dest_topic.id,
+             },
+             env: {
+               "rack.hijack" => -> { io },
+             }
+
+        headers, body = io.string.split("\r\n\r\n", 2)
+        expect(headers).to start_with("HTTP/1.1 200 OK\r\n")
+        expect(JSON.parse(body)).to eq("success" => true, "url" => dest_topic.relative_url)
+        expect(p2.reload.topic_id).to eq(dest_topic.id)
       end
 
       it "does not allow posts to be moved to a private category" do
         dest_topic.update!(category: staff_category)
+        io = StringIO.new
 
-        post "/t/#{topic.id}/merge-topic.json", params: { destination_topic_id: dest_topic.id }
+        post "/t/#{topic.id}/merge-topic.json",
+             params: {
+               destination_topic_id: dest_topic.id,
+             },
+             env: {
+               "rack.hijack" => -> { io },
+             }
 
-        expect(response).to be_forbidden
-      end
-
-      context "when the request is hijacked" do
-        it "returns 200 with the destination URL after merging the topics" do
-          io = StringIO.new
-
-          post "/t/#{topic.id}/merge-topic.json",
-               params: {
-                 destination_topic_id: dest_topic.id,
-               },
-               env: {
-                 "rack.hijack" => -> { io },
-               }
-
-          headers, body = io.string.split("\r\n\r\n", 2)
-          expect(headers).to start_with("HTTP/1.1 200 OK\r\n")
-          expect(JSON.parse(body)).to eq("success" => true, "url" => dest_topic.relative_url)
-          expect(p2.reload.topic_id).to eq(dest_topic.id)
-        end
-
-        it "returns 403 with JSON errors when the destination category is inaccessible" do
-          dest_topic.update!(category: staff_category)
-          io = StringIO.new
-
-          post "/t/#{topic.id}/merge-topic.json",
-               params: {
-                 destination_topic_id: dest_topic.id,
-               },
-               env: {
-                 "rack.hijack" => -> { io },
-               }
-
-          headers, body = io.string.split("\r\n\r\n", 2)
-          expect(headers).to start_with("HTTP/1.1 403 Forbidden\r\n")
-          expect(JSON.parse(body)["errors"]).to be_present
-          expect(p2.reload.topic_id).to eq(topic.id)
-          expect(topic.reload.deleted_at).to be_nil
-        end
+        headers, body = io.string.split("\r\n\r\n", 2)
+        expect(headers).to start_with("HTTP/1.1 403 Forbidden\r\n")
+        expect(JSON.parse(body)["errors"]).to be_present
+        expect(p2.reload.topic_id).to eq(topic.id)
+        expect(topic.reload.deleted_at).to be_nil
       end
 
       it "does not allow posts outside of the category to be moved" do
