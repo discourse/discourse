438ea4dea8ccc4d5f7bc8460b46fc16d29bff65a
Natalie Tay <natalie.tay@gmail.com>
2026-09-11 15:47:26 +0800

    DEV: Spec fix by removing repeated search (#43568)
    Seen here -
    https://github.com/discourse/discourse/actions/runs/34571145336/job/103173314804?pr=43567

--- spec/system/admin_site_setting_locale_spec.rb
+++ spec/system/admin_site_setting_locale_spec.rb
@@ -16,17 +16,12 @@ describe "Admin Site Setting Locales" do
       settings_page.visit
 
       settings_page.type_in_search("default locale")
-      expect(settings_page.enum_setting("default_locale").value).to eq("es")
+      expect(settings_page).to have_enum_value("default_locale", "es")
 
       settings_page.select_enum_value("default_locale", "en")
       settings_page.save_setting("default_locale")
 
-      settings_page.type_in_search("default locale")
-      expect(settings_page.enum_setting("default_locale").value).to eq("en")
-      expect(settings_page.find_setting("default_locale")).to have_css(
-        "option[value='en']",
-        text: "Inglés (English)",
-      )
+      expect(settings_page).to have_enum_value("default_locale", "en", text: "Inglés (English)")
     end
   end
 
