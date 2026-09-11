1e98f209eba334f2debfdeb54e661ac28ec8fe89
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2026-09-11 08:58:39 +0800

    DEV: Document supported animation probe formats

--- lib/discourse_vips.rb
+++ lib/discourse_vips.rb
@@ -23,6 +23,7 @@ module DiscourseVips
     )
   end
 
+  # Supports GIF, WebP, and HEIF/AVIF images; other formats, including APNG, are unsupported.
   def self.animated?(input_path:, timeout:)
     Client.call(["animated", input_path], operation: :upload_animation_probe, timeout:)
   end
