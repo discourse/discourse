cafe25bc52f3a6cf1c126686715d79d2ea981d79
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2026-09-11 14:10:47 +0800

    DEV: Remove duplicate report Guardian test
    Keep the focused access-policy example as the single test that verifies the supplied Guardian determines the report actor.

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,17 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe Report do
-  describe ".find" do
-    it "derives the report user from the supplied guardian" do
-      guardian = Fabricate(:moderator).guardian
-
-      report = described_class.find("signups", guardian: guardian, current_user: Fabricate(:admin))
-
-      expect(report.guardian).to equal(guardian)
-      expect(report.current_user).to eq(guardian.user)
-    end
-  end
-
   describe ".add_report" do
     after { Report.remove_report("my_custom_report") }
 
