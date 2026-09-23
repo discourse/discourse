ee34ee7bb6784f349f9ca42b95e3a4f4e753a6fd
Keegan George <kgeorge13@gmail.com>
2026-09-23 15:51:28 +0000

    DEV: Annotate the topic embed alias model

--- app/models/topic_embed_alias.rb
+++ app/models/topic_embed_alias.rb
@@ -8,3 +8,18 @@ class TopicEmbedAlias < ActiveRecord::Base
     { url_key:, url_hash: Digest::SHA256.hexdigest(url_key) }
   end
 end
+
+# == Schema Information
+#
+# Table name: topic_embed_aliases
+#
+#  id             :bigint           not null, primary key
+#  url_hash       :string(64)       not null
+#  url_key        :text             not null
+#  topic_embed_id :bigint           not null
+#
+# Indexes
+#
+#  index_topic_embed_aliases_on_topic_embed_id  (topic_embed_id)
+#  index_topic_embed_aliases_on_url_hash        (url_hash) UNIQUE
+#
